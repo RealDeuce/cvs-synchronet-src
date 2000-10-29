3fdff0571640db6aaa4d3a8dd5358052af928adb
rswindell <rswindell>
2000-10-29 04:52:18 +0000

    Renamed EXPORT32 macro to LZHEXPORT.

--- encode/lzh.h
+++ encode/lzh.h
@@ -2,7 +2,7 @@
 
 /* Synchronet LZH compression library */
 
-/* $Id: lzh.h,v 1.6 2000/10/26 15:06:40 rswindell Exp $ */
+/* $Id: lzh.h,v 1.7 2000/10/29 04:52:18 rswindell Exp $ */
 
 /**************************************************************************** 
  * @format.tab-size 4		(Plain Text/Source Code File Header)			* 
@@ -31,8 +31,8 @@
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
 
-#ifdef EXPORT32
-	#undef EXPORT32
+#ifdef LZHEXPORT
+	#undef LZHEXPORT
 #endif
 
 #ifdef _WIN32
@@ -45,13 +45,13 @@
 		#define LZHCALL
 	#endif
 	#ifdef LZHDLL
-		#define EXPORT32 __declspec( dllexport )
+		#define LZHEXPORT __declspec( dllexport )
 	#else
-		#define EXPORT32 __declspec( dllimport )
+		#define LZHEXPORT __declspec( dllimport )
 	#endif
 #else	/* !_WIN32 */
 	#define LZHCALL
-	#define EXPORT32
+	#define LZHEXPORT
 #endif
 
 #ifndef uchar
@@ -61,8 +61,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-EXPORT32 long LZHCALL lzh_encode(uchar *inbuf, long inlen, uchar *outbuf);
-EXPORT32 long LZHCALL lzh_decode(uchar *inbuf, long inlen, uchar *outbuf);
+LZHEXPORT long LZHCALL lzh_encode(uchar *inbuf, long inlen, uchar *outbuf);
+LZHEXPORT long LZHCALL lzh_decode(uchar *inbuf, long inlen, uchar *outbuf);
 #ifdef __cplusplus
 }
 #endif
