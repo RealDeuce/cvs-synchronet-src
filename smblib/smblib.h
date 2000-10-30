27ffbb1ae19d3b85dd9e40a0c19ecdb00b39af52
rswindell <rswindell>
2000-10-30 02:23:34 +0000

    Only declares functions as import/export if SMBDLL is defined. Function declared as export if SMB_EXPORTS is defined, import otherwise.

--- smblib/smblib.h
+++ smblib/smblib.h
@@ -2,7 +2,7 @@
 
 /* Synchronet message base (SMB) library function prototypes */
 
-/* $Id: smblib.h,v 1.7 2000/10/29 04:51:55 rswindell Exp $ */
+/* $Id: smblib.h,v 1.8 2000/10/30 02:23:34 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -53,10 +53,14 @@
 	#else
 		#define SMBCALL
 	#endif
-	#ifdef SMBDLL
-		#define SMBEXPORT __declspec( dllexport )
-	#else
-		#define SMBEXPORT __declspec( dllimport )
+	#ifdef SMBDLL	/* SMBLIB contained in DLL */
+		#ifdef SMB_EXPORTS
+			#define SMBEXPORT __declspec( dllexport )
+		#else
+			#define SMBEXPORT __declspec( dllimport )
+		#endif
+	#else	/* self-contained executable */
+		#define SMBEXPORT
 	#endif
 #elif defined __unix__
 	#define SMBCALL
