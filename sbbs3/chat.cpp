8d6e66ac00e9e1b24c501669e214166810981e27
rswindell <rswindell>
2000-11-07 11:55:08 +0000

    Fixed CR/LF expectations on guru data files.

--- sbbs3/chat.cpp
+++ sbbs3/chat.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet real-time chat functions */
 
-/* $Id: chat.cpp,v 1.9 2000/11/07 11:47:41 rswindell Exp $ */
+/* $Id: chat.cpp,v 1.10 2000/11/07 11:55:08 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1383,19 +1383,21 @@ void sbbs_t::guruchat(char *line, char *gurubuf, int gurunum)
 			answers=0;
 			while(*ptr && answers<100 && ptr<gurubuf+len) {
 				i=0;
-				while(*ptr && *ptr!=CR && i<512 && ptr<gurubuf+len) {
+				while(*ptr && *ptr!=CR && *ptr!=LF && i<512 && ptr<gurubuf+len) {
 					answer[answers][i]=*ptr;
 					ptr++;
 					i++;
-					if(*ptr=='\\' && *(ptr+1)==CR) {	/* multi-line answer */
-						ptr+=3;	/* skip \CRLF */
+					/* multi-line answer */
+					if(*ptr=='\\' && (*(ptr+1)==CR || *(ptr+1)==LF)) {
+						ptr++;	/* skip \ */
+						while(*ptr && *ptr<SP) ptr++;	/* skip [CR]LF */
 						answer[answers][i++]=CR;
 						answer[answers][i++]=LF; } }
 				answer[answers][i]=0;
 				if(!strlen(answer[answers]) || answer[answers][0]=='(') {
 					ptr-=strlen(answer[answers]);
 					break; }
-				ptr+=2;	/* skip CRLF */
+				while(*ptr && *ptr<SP) ptr++;	/* skip [CR]LF */
 				answers++; }
 			if(answers==100)
 				while(*ptr && *ptr!='(' && ptr<gurubuf+len)
