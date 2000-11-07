567769455a066741f60355c38a979c96ee275dbd
rswindell <rswindell>
2000-11-07 11:24:48 +0000

    Eliminated redundant include of smbwrap.h (included with sbbs.h). Added compiler error when __unix__ is defined, but not _POSIX_THREADS.

--- sbbs3/wrappers.c
+++ sbbs3/wrappers.c
@@ -2,7 +2,7 @@
 
 /* Synchronet system-call wrappers */
 
-/* $Id: wrappers.c,v 1.25 2000/11/07 09:17:16 cmartin Exp $ */
+/* $Id: wrappers.c,v 1.26 2000/11/07 11:24:48 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -63,7 +63,6 @@
 #include <errno.h>		/* ENOENT definitions */
 
 #include "sbbs.h"		/* getfname */
-#include "smbwrap.h"    /* SH_DENY* */
 
 #ifdef _WIN32
 #define stat(f,s)	_stat(f,s)
@@ -336,8 +335,11 @@ ulong _beginthread(void( *start_address )( void * )
 
 	return(-1);	/* error */
 }
-#endif  // _POSIX_THREADS
-#endif
+#else	/* _POSIX_THREADS */
+
+#error "Need _beginthread implementation for non-POSIX thread library."
+
+#endif	/* __unix__ */
 
 /****************************************************************************/
 /* Return free disk space in bytes (up to a maximum of 4GB)					*/
