7729c21ef72f5d3465702317790a2514ce403ba1
rswindell <rswindell>
2000-11-16 22:54:22 +0000

    Added new option: BBS_OPT_NO_QWK_EVENTS to prevent QWK events from running in this instance.

--- sbbs3/startup.h
+++ sbbs3/startup.h
@@ -2,7 +2,7 @@
 
 /* Synchronet main/telnet server thread startup structure */
 
-/* $Id: startup.h,v 1.4 2000/11/02 01:11:37 rswindell Exp $ */
+/* $Id: startup.h,v 1.5 2000/11/16 22:54:22 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -96,5 +96,7 @@ typedef struct {
 #define BBS_OPT_SYSOP_AVAILABLE		(1<<4)	// Available for chat
 #define BBS_OPT_ALLOW_RLOGIN		(1<<5)	// Allow logins via BSD RLogin
 #define BBS_OPT_USE_2ND_RLOGIN		(1<<6)	// Use 2nd username in BSD RLogin
+#define BBS_OPT_NO_QWK_EVENTS		(1<<7)	// Don't run QWK-related events
 #define BBS_OPT_NO_HOST_LOOKUP		(1<<11)
 #define BBS_OPT_MUTE				(1<<31)	// Mute sounds
+
