796cf4b0919faf905247a0c51bab1833b6de188e
rswindell <rswindell>
2001-04-12 03:58:56 +0000

    getposts() now DLL-exported.

--- sbbs3/getmsg.cpp
+++ sbbs3/getmsg.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet message retrieval functions */
 
-/* $Id: getmsg.cpp,v 1.4 2001/04/10 01:12:25 rswindell Exp $ */
+/* $Id: getmsg.cpp,v 1.5 2001/04/12 03:58:56 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -309,7 +309,7 @@ void sbbs_t::msgtotxt(smbmsg_t* msg, char *str, int header, int tails)
 /****************************************************************************/
 /* Returns total number of posts in a sub-board 							*/
 /****************************************************************************/
-ulong getposts(scfg_t* cfg, uint subnum)
+ulong DLLCALL getposts(scfg_t* cfg, uint subnum)
 {
 	char str[128];
 	ulong l;
