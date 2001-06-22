7f3f3c2ce0cf18de5bf169fbce3c88256280a082
rswindell <rswindell>
2001-06-22 02:24:12 +0000

    Added console bit to disable user inactivity detection.

--- sbbs3/getkey.cpp
+++ sbbs3/getkey.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet single-key console functions */
 
-/* $Id: getkey.cpp,v 1.7 2001/06/15 03:37:41 rswindell Exp $ */
+/* $Id: getkey.cpp,v 1.8 2001/06/22 02:24:12 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -253,9 +253,9 @@ char sbbs_t::getkey(long mode)
 				,((ushort)timeleft/60)+1,(timeleft/60) ? "s" : nulstr);
 			RESTORELINE; }
 
-		if(online==ON_LOCAL && cfg.node_misc&NM_NO_INACT)
+		if((online==ON_LOCAL && cfg.node_misc&NM_NO_INACT) || console&CON_NO_INACT)
 			timeout=now;
-		if(now-timeout>=cfg.sec_warn) { 					/* warning */
+		else if(now-timeout>=cfg.sec_warn) { 					/* warning */
 			if(sys_status&SS_USERON) {
 				SAVELINE;
 				bputs(text[AreYouThere]); }
