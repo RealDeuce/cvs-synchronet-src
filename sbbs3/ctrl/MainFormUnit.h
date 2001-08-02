77a7411615b33a8402ff9ea99c8204f2c3a8a496
rswindell <rswindell>
2001-08-02 04:15:36 +0000

    Added configure sub-menu to tray icon pop-up menu for configuring the different servers.

--- sbbs3/ctrl/MainFormUnit.h
+++ sbbs3/ctrl/MainFormUnit.h
@@ -1,6 +1,6 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: MainFormUnit.h,v 1.20 2001/07/21 01:46:58 rswindell Exp $ */
+/* $Id: MainFormUnit.h,v 1.21 2001/08/02 04:15:36 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -223,6 +223,10 @@ __published:	// IDE-managed Components
     TMenuItem *BBSEditEmailFilterMenuItem;
     TMenuItem *BBSEditBadEmailMessageMenuItem;
     TMenuItem *ViewHackAttemptLogMenuItem;
+    TMenuItem *ConfigureBBSTrayMenuItem;
+    TMenuItem *ConfigureTelnetTrayMenuItem;
+    TMenuItem *ConfigureFtpTrayMenuItem;
+    TMenuItem *ConfigureMailTrayMenuItem;
     void __fastcall FileExitMenuItemClick(TObject *Sender);
 	void __fastcall ViewToolbarMenuItemClick(TObject *Sender);
 	void __fastcall FormClose(TObject *Sender, TCloseAction &Action);
