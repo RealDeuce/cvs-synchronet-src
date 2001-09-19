addd422de7ca832dd18d50e2e1664f3cd8f17319
rswindell <rswindell>
2001-09-19 01:25:37 +0000

    Global exit method now clears pending exceptions (from earlier error perhaps).

--- sbbs3/js_global.c
+++ sbbs3/js_global.c
@@ -2,7 +2,7 @@
 
 /* Synchronet JavaScript "global" object properties/methods for all servers */
 
-/* $Id: js_global.c,v 1.9 2001/09/19 00:34:34 rswindell Exp $ */
+/* $Id: js_global.c,v 1.10 2001/09/19 01:25:37 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -158,6 +158,7 @@ js_beep(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 static JSBool
 js_exit(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 {
+	JS_ClearPendingException(cx);
 	*rval = JSVAL_VOID;
 	return(JS_FALSE);
 }
