306b4bab958f1573c564349d972f6e19ad7bd54f
rswindell <rswindell>
2001-10-24 02:56:24 +0000

    Changed email property to reflect user's BBS e-mail address (not netmail address).

--- sbbs3/js_user.c
+++ sbbs3/js_user.c
@@ -2,7 +2,7 @@
 
 /* Synchronet JavaScript "User" Object */
 
-/* $Id: js_user.c,v 1.8 2001/10/17 19:20:46 rswindell Exp $ */
+/* $Id: js_user.c,v 1.9 2001/10/24 02:56:24 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -58,6 +58,7 @@ enum {
 	,USER_PROP_COMP		
 	,USER_PROP_COMMENT	
 	,USER_PROP_NETMAIL	
+	,USER_PROP_EMAIL	/* READ ONLY */
 	,USER_PROP_ADDRESS	
 	,USER_PROP_LOCATION	
 	,USER_PROP_ZIPCODE	
@@ -151,6 +152,10 @@ static JSBool js_user_get(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
 			break;
 		case USER_PROP_NETMAIL:
 			s=user.netmail;
+			break;
+		case USER_PROP_EMAIL:
+			s=usermailaddr(p->cfg, tmp
+				,p->cfg->inetmail_misc&NMAIL_ALIAS ? user.alias : user.name);
 			break;
 		case USER_PROP_ADDRESS:
 			s=user.address;
@@ -501,7 +506,7 @@ static struct JSPropertySpec js_user_properties[] = {
 	{	"host_name"			,USER_PROP_COMP		 	,USER_PROP_FLAGS,		NULL,NULL},
 	{	"comment"			,USER_PROP_COMMENT	 	,USER_PROP_FLAGS,		NULL,NULL},
 	{	"netmail"			,USER_PROP_NETMAIL	 	,USER_PROP_FLAGS,		NULL,NULL},
-	{	"email"				,USER_PROP_NETMAIL	 	,USER_PROP_FLAGS,		NULL,NULL},
+	{	"email"				,USER_PROP_EMAIL	 	,USER_PROP_FLAGS|JSPROP_READONLY,		NULL,NULL},
 	{	"address"			,USER_PROP_ADDRESS	 	,USER_PROP_FLAGS,		NULL,NULL},
 	{	"location"			,USER_PROP_LOCATION	 	,USER_PROP_FLAGS,		NULL,NULL},
 	{	"zipcode"			,USER_PROP_ZIPCODE	 	,USER_PROP_FLAGS,		NULL,NULL},
