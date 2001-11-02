a0622c9c8e7b075284642a84b272f6c504d56d9b
rswindell <rswindell>
2001-11-02 00:17:27 +0000

    Sped up getuserdat() significantly by eliminating call to lastuser()->flength().

--- sbbs3/userdat.c
+++ sbbs3/userdat.c
@@ -2,7 +2,7 @@
 
 /* Synchronet user data-related routines (exported) */
 
-/* $Id: userdat.c,v 1.19 2001/10/02 20:10:56 rswindell Exp $ */
+/* $Id: userdat.c,v 1.20 2001/11/02 00:17:27 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -121,7 +121,7 @@ int DLLCALL getuserdat(scfg_t* cfg, user_t *user)
 	char userdat[U_LEN+1],str[U_LEN+1],tmp[64];
 	int i,file;
 
-	if(!user->number || user->number>lastuser(cfg)) {
+	if(!user->number) {
 		memset(user,0,sizeof(user_t));
 		return(-1); 
 	}
@@ -129,6 +129,10 @@ int DLLCALL getuserdat(scfg_t* cfg, user_t *user)
 	if((file=nopen(userdat,O_RDONLY|O_DENYNONE))==-1) {
 		memset(user,0,sizeof(user_t));
 		return(errno); 
+	}
+	if(user->number > (filelength(file)/U_LEN)) {
+		close(file);
+		return(-1);	/* no such user record */
 	}
 	lseek(file,(long)((long)(user->number-1)*U_LEN),SEEK_SET);
 	i=0;
