9572a432258181d1a37d8c7ce49d7119ec3c3018
rswindell <rswindell>
2002-01-17 23:56:40 +0000

    Had EX_NATIVE and XTRN_NATIVE backwards (whoops).

--- sbbs3/writemsg.cpp
+++ sbbs3/writemsg.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet message creation routines */
 
-/* $Id: writemsg.cpp,v 1.23 2002/01/17 21:13:08 rswindell Exp $ */
+/* $Id: writemsg.cpp,v 1.24 2002/01/17 23:56:40 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -297,8 +297,8 @@ bool sbbs_t::writemsg(char *fname, char *top, char *title, long mode, int subnum
 				ex_mode|=(EX_OUTR|EX_INR);
 			if(cfg.xedit[useron.xedit-1]->misc&WWIVCOLOR)
 				ex_mode|=EX_WWIV; }
-		if(useron.xedit && cfg.xedit[useron.xedit-1]->misc&EX_NATIVE)
-			ex_mode|=XTRN_NATIVE;
+		if(useron.xedit && cfg.xedit[useron.xedit-1]->misc&XTRN_NATIVE)
+			ex_mode|=EX_NATIVE;
 
 		if(!linesquoted && fexist(msgtmp))
 			remove(msgtmp);
@@ -775,8 +775,8 @@ void sbbs_t::editfile(char *str)
 		return; }
 	if(useron.xedit) {
 		editor_inf(useron.xedit,nulstr,nulstr,0,INVALID_SUB);
-		if(cfg.xedit[useron.xedit-1]->misc&EX_NATIVE)
-			mode|=XTRN_NATIVE;
+		if(cfg.xedit[useron.xedit-1]->misc&XTRN_NATIVE)
+			mode|=EX_NATIVE;
 		if(cfg.xedit[useron.xedit-1]->misc&IO_INTS) {
 			if(online==ON_REMOTE)
 				mode|=(EX_OUTR|EX_INR);
