3aa1e3f043c01b1854422b1df88da017711599a6
rswindell <rswindell>
2002-01-24 01:32:07 +0000

    Eliminate unused code. Begun Unix port.

--- sbbs3/scfg/scfgsub.c
+++ sbbs3/scfg/scfgsub.c
@@ -1,6 +1,6 @@
 /* scfgsub.c */
 
-/* $Id: scfgsub.c,v 1.3 2002/01/22 17:01:04 rswindell Exp $ */
+/* $Id: scfgsub.c,v 1.4 2002/01/24 01:32:07 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -199,7 +199,7 @@ If you want to delete all the messages for this sub-board, select Yes.
 		if(j==0) {
 				sprintf(str,"%s.s*",cfg.sub[subnum[i]]->code);
 				if(!cfg.sub[subnum[i]]->data_dir[0])
-					sprintf(tmp,"%sSUBS\\",cfg.data_dir);
+					sprintf(tmp,"%ssubs/",cfg.data_dir);
 				else
 					strcpy(tmp,cfg.sub[subnum[i]]->data_dir);
 				delfiles(tmp,str);
@@ -1087,7 +1087,7 @@ should be re-scanned.
 						: cfg.sub[i]->misc&SUB_FAST ? "Fast Allocation"
 						: "Self-packing");
 					if(!cfg.sub[i]->data_dir[0])
-						sprintf(str,"%sSUBS\\",cfg.data_dir);
+						sprintf(str,"%ssubs/",cfg.data_dir);
 					else
 						strcpy(str,cfg.sub[i]->data_dir);
 					sprintf(opt[n++],"%-27.27s%.40s","Storage Directory",str);
@@ -1177,7 +1177,7 @@ by entering the conference number you want to use.
 								if(cfg.sub[i]->data_dir[0])
 									sprintf(str,"%s",cfg.sub[i]->data_dir);
 								else
-									sprintf(str,"%sSUBS\\",cfg.data_dir);
+									sprintf(str,"%ssubs/",cfg.data_dir);
 								sprintf(str2,"%s.*",cfg.sub[i]->code);
 								delfiles(str,str2); }
 
