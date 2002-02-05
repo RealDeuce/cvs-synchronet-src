6e07fe96273fdcadeed9ed515a0b3446e852a412
rswindell <rswindell>
2002-02-05 22:09:59 +0000

    Added subscan_t array parameter to js_CreateMsgAreaObject. Used for new scan_cfg, scan_ptr, and last_read properties.

--- sbbs3/js_msg_area.c
+++ sbbs3/js_msg_area.c
@@ -2,7 +2,7 @@
 
 /* Synchronet JavaScript "Message Area" Object */
 
-/* $Id: js_msg_area.c,v 1.8 2002/02/02 04:13:29 rswindell Exp $ */
+/* $Id: js_msg_area.c,v 1.9 2002/02/05 22:09:59 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -57,7 +57,7 @@ static JSClass js_msg_area_class = {
 };
 
 JSObject* DLLCALL js_CreateMsgAreaObject(JSContext* cx, JSObject* parent, scfg_t* cfg
-										  ,user_t* user)
+										  ,user_t* user, subscan_t* subscan)
 {
 	char		str[128];
 	JSObject*	areaobj;
@@ -211,6 +211,19 @@ JSObject* DLLCALL js_CreateMsgAreaObject(JSContext* cx, JSObject* parent, scfg_t
 				val=BOOLEAN_TO_JSVAL(JS_FALSE);
 			if(!JS_SetProperty(cx, subobj, "is_moderated", &val))
 				return(NULL);
+
+			if(subscan!=NULL) {
+				val=INT_TO_JSVAL(subscan[d].ptr);
+				if(!JS_SetProperty(cx, subobj, "scan_ptr", &val))
+					return(NULL);
+				val=INT_TO_JSVAL(subscan[d].cfg);
+				if(!JS_SetProperty(cx, subobj, "scan_cfg", &val))
+					return(NULL);
+				val=INT_TO_JSVAL(subscan[d].last);
+				if(!JS_SetProperty(cx, subobj, "last_read", &val))
+					return(NULL);
+			}
+
 
 			if(!JS_GetArrayLength(cx, sub_list, &index))
 				return(NULL);							
