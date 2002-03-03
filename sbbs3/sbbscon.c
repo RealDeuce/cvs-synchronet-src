25623b3a03256c345d506a2351b1f18f41273377
rswindell <rswindell>
2002-03-03 22:14:06 +0000

    Added mutex protection to setuid() call in do_setuid.

--- sbbs3/sbbscon.c
+++ sbbs3/sbbscon.c
@@ -2,7 +2,7 @@
 
 /* Synchronet vanilla/console-mode "front-end" */
 
-/* $Id: sbbscon.c,v 1.44 2002/02/28 02:21:28 rswindell Exp $ */
+/* $Id: sbbscon.c,v 1.45 2002/03/03 22:14:06 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -120,19 +120,29 @@ static void lputs(char *str)
 * **********************************************************/
 BOOL do_setuid() 
 {
-	uid_t new_uid;
+	BOOL	result=FALSE;
+	uid_t	new_uid;
 	struct passwd* pw_entry;
+	static pthread_mutex_t mutex;
+	static BOOL mutex_initialized;
 
 	if(new_uid_name==NULL)	/* unspecified */
 		return TRUE;
-	if((pw_entry=getpwnam(new_uid_name)) && (new_uid=pw_entry->pw_uid))
-		if(!setuid(new_uid)) {
-/*			lputs("setuid SUCCESSFUL"); */
-			return TRUE;
-		}
 
-	lputs("!setuid FAILED");
-	return FALSE;
+	if(!mutex_initialized) {
+		pthread_mutex_init(&mutex,NULL);
+		mutex_initialized=TRUE;
+	}
+
+	pthread_mutex_lock(&mutex);
+	if((pw_entry=getpwnam(new_uid_name)) && (new_uid=pw_entry->pw_uid))
+		if(!setuid(new_uid)) 
+			result=TRUE;
+	pthread_mutex_unlock(&mutex);
+
+	if(!result)
+		lputs("!setuid FAILED");
+	return result;
 }
 #endif   /* __unix__ */
 
