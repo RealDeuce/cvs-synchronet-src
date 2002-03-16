5dd8ecc0e64d9ca0f1bbffd47277688ac55844a9
rswindell <rswindell>
2002-03-16 12:21:57 +0000

    New node_misc bit: NM_CLOSENODEDAB

--- sbbs3/sbbsdefs.h
+++ sbbs3/sbbsdefs.h
@@ -2,7 +2,7 @@
 
 /* Synchronet constants, macros, and structure definitions */
 
-/* $Id: sbbsdefs.h,v 1.59 2002/03/13 23:15:40 rswindell Exp $ */
+/* $Id: sbbsdefs.h,v 1.60 2002/03/16 12:21:57 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -181,6 +181,7 @@
 #define NM_7BITONLY		(1L<<16)	/* Except 7-bit input only (E71 terminals)	*/
 #define NM_RESETVID		(1L<<17)	/* Reset video mode between callers?		*/
 #define NM_NOPAUSESPIN	(1L<<18)	/* No spinning cursor at pause prompt		*/
+#define NM_CLOSENODEDAB	(1L<<19)	/* Keep node.dab file closed (for Samba)	*/
 
 									/* Miscellaneous Modem Settings (mdm_misc)  */
 #define MDM_CTS 		(1<<0)		/* Use hardware send flow control			*/
