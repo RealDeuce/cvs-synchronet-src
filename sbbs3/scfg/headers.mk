7d4b36974aadbbb0347831084807f09aca3db308
rswindell <rswindell>
2002-04-12 06:22:08 +0000

    New makefile.

---
+++ sbbs3/scfg/headers.mk
@@ -0,0 +1,10 @@
+# headers.mak
+
+# Make 'include file' for building Synchronet SCFG 
+# Used with GNU and Borland compilers
+
+# $Id: headers.mk,v 1.1 2002/04/12 06:22:08 rswindell Exp $
+
+HEADERS =	scfg.h ../ars_defs.h ../sbbs.h ../sbbswrap.h ../scfgdefs.h \
+			../smbdefs.h ../userdat.h ../gen_defs.h ../sbbsdefs.h \
+			../scfglib.h ../smblib.h ../../uifc/uifc.h
