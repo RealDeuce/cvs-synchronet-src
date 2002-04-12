16728ef171fd9c87b248aa1dba3210f86ceeaa2d
rswindell <rswindell>
2002-04-12 09:56:43 +0000

    New make include files.

---
+++ sbbs3/headers.mk
@@ -0,0 +1,9 @@
+# headers.mk
+
+# Make 'include file' for building Synchronet DLLs 
+# Used with GNU and Borland compilers
+
+# $Id: headers.mk,v 1.1 2002/04/12 09:56:43 rswindell Exp $
+
+HEADERS =	sbbs.h sbbsdefs.h sbbswrap.h sbbsinet.h scfgdefs.h gen_defs.h \
+			nodedefs.h smblib.h smbdefs.h text.h
---
+++ sbbs3/sbbsdefs.mk
@@ -0,0 +1,7 @@
+# sbbsdefs.mk
+
+# C/C++ compiler preprocessor definitions for building SBBS.DLL
+
+# $Id: sbbsdefs.mk,v 1.1 2002/04/12 09:56:43 rswindell Exp $
+
+SBBSDEFS=	-DSBBS -DSBBS_EXPORTS -DSMBDLL -DSMB_EXPORTS -DWRAPPER_DLL
