7f3cf06611877bb2779900022d983096afd4516b
rswindell <rswindell>
2002-04-26 01:16:03 +0000

    Fixed ulist definition (int args, not char).

--- uifc/uifcd.c
+++ uifc/uifcd.c
@@ -2,7 +2,7 @@
 
 /* Unix libdialog implementation of UIFC library (by Deuce)	*/
 
-/* $Id: uifcd.c,v 1.25 2002/04/26 00:40:24 rswindell Exp $ */
+/* $Id: uifcd.c,v 1.26 2002/04/26 01:16:03 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -112,7 +112,7 @@ int uscrn(char *str)
 /**************************************************************************/
 /* General menu display function. see uifc.h for details                  */
 /**************************************************************************/
-int ulist(int mode, char left, int top, char width, int *cur, int *bar
+int ulist(int mode, int left, int top, int width, int *cur, int *bar
 	, char *title, char **option)
 {
 
