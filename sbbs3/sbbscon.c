f8a813ea23eb8d6f4fdc49e65e2fbd9c5175a7ae
rswindell <rswindell>
2002-05-03 00:32:43 +0000

    Eliminating bc++/Linux warnings.

--- sbbs3/sbbscon.c
+++ sbbs3/sbbscon.c
@@ -2,7 +2,7 @@
 
 /* Synchronet vanilla/console-mode "front-end" */
 
-/* $Id: sbbscon.c,v 1.68 2002/04/26 11:06:37 rswindell Exp $ */
+/* $Id: sbbscon.c,v 1.69 2002/05/03 00:32:43 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -191,7 +191,7 @@ static void lputs(char *str)
 /**********************************************************
 * Change uid of the calling process to the user if specified
 * **********************************************************/
-static BOOL do_setuid() 
+static BOOL do_setuid(void) 
 {
 	BOOL	result=FALSE;
 
@@ -985,7 +985,7 @@ int main(int argc, char** argv)
 						{
 							new_gid_name=arg;
 							old_gid = getgid();
-							if((gr_entry=getgrnam(new_gid_name)) && (new_gid=gr_entry->gr_gid))
+							if((gr_entry=getgrnam(new_gid_name))!=0 && (new_gid=gr_entry->gr_gid)!=0)
 								do_seteuid(TRUE);
 						}
 #endif			
