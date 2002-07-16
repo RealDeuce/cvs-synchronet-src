28c608eefbf04f1319053841c80702c5b93790ba
rswindell <rswindell>
2002-07-16 07:27:46 +0000

    Backup level no longer limited to 10 backups. Now changes current directory to ctrl_dir, allowing relative paths to be created successfully by write_xxx_cfg() functions.

--- sbbs3/scfg/scfg.c
+++ sbbs3/scfg/scfg.c
@@ -2,7 +2,7 @@
 
 /* Synchronet configuration utility 										*/
 
-/* $Id: scfg.c,v 1.27 2002/05/03 00:12:27 rswindell Exp $ */
+/* $Id: scfg.c,v 1.28 2002/07/16 07:27:46 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -164,9 +164,12 @@ int main(int argc, char **argv)
             SAFECOPY(cfg.ctrl_dir,argv[i]);
     }
 
-if(backup_level>10) backup_level=10;
-
 backslashcolon(cfg.ctrl_dir);
+
+if(chdir(cfg.ctrl_dir)!=0) {
+	printf("!ERROR %d changing current directory to: %s\n",cfg.ctrl_dir);
+	exit(-1);
+}
 
 uifc.size=sizeof(uifc);
 #if defined(USE_DIALOG)
