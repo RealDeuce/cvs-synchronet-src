fe113a4ae8f797c50141dad94b4a1659e83f3af4
rswindell <rswindell>
2002-07-21 23:04:39 +0000

    Eliminatd SMBDLL and WRAPPER_DLL definitions (no longer used).

--- sbbs3/sbbsdefs.mk
+++ sbbs3/sbbsdefs.mk
@@ -2,6 +2,6 @@
 
 # C/C++ compiler preprocessor definitions for building SBBS.DLL
 
-# $Id: sbbsdefs.mk,v 1.1 2002/04/12 09:56:43 rswindell Exp $
+# $Id: sbbsdefs.mk,v 1.2 2002/07/21 23:04:39 rswindell Exp $
 
-SBBSDEFS=	-DSBBS -DSBBS_EXPORTS -DSMBDLL -DSMB_EXPORTS -DWRAPPER_DLL
+SBBSDEFS=	-DSBBS -DSBBS_EXPORTS -DSMB_EXPORTS -DWRAPPER_EXPORTS
