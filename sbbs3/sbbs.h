a28360872e524bc1ae4b1ec943c4fc822e2065cb
rswindell <rswindell>
2002-09-04 04:05:44 +0000

    Need to use preprocessor definitions for jstype string constants in C.

--- sbbs3/sbbs.h
+++ sbbs3/sbbs.h
@@ -2,7 +2,7 @@
 
 /* Synchronet class (sbbs_t) definition and exported function prototypes */
 
-/* $Id: sbbs.h,v 1.135 2002/09/04 03:46:34 rswindell Exp $ */
+/* $Id: sbbs.h,v 1.136 2002/09/04 04:05:44 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -829,7 +829,7 @@ extern "C" {
 #ifdef JAVASCRIPT
 
 	typedef struct {
-		const char      *name;
+		const char*		name;
 		JSNative        call;
 		uint8           nargs;
 		const char*		type;		/* return type */
@@ -838,10 +838,21 @@ extern "C" {
 		const char**	alias;		/* aliases */
 	} jsMethodSpec;
 
-	const char* jstype_void		= "void";
-	const char* jstype_bool		= "boolean";
-	const char* jstype_str		= "string";
-	const char* jstype_num		= "number";
+	#ifdef __cplusplus
+
+		const char* jstype_void		= "void";
+		const char* jstype_bool		= "boolean";
+		const char* jstype_str		= "string";
+		const char* jstype_num		= "number";
+
+	#else
+
+		#define		jstype_void		"void"
+		#define		jstype_bool		"boolean"
+		#define		jstype_str		"string"
+		#define		jstype_num		"number"
+
+	#endif
 
 	/* main.cpp */
 	DLLEXPORT int		DLLCALL js_MethodsToFunctions(jsMethodSpec meth[], JSFunctionSpec func[]);
