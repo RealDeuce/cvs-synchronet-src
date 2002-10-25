ba3349197a269c0389bb51f8816ec906bb54314e
rswindell <rswindell>
2002-10-25 20:32:35 +0000

    Changed generated MSGID format to be a bit more "standard": removed subcode and message number of originaddr portion, changed serialno to use only 27 bits of the import time and the low 5 bits of the message number to generate a more unique serialno.

--- sbbs3/sbbsecho.c
+++ sbbs3/sbbsecho.c
@@ -2,7 +2,7 @@
 
 /* Synchronet FidoNet EchoMail Scanning/Tossing and NetMail Tossing Utility */
 
-/* $Id: sbbsecho.c,v 1.66 2002/10/13 08:18:55 rswindell Exp $ */
+/* $Id: sbbsecho.c,v 1.67 2002/10/25 20:32:35 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -3738,11 +3738,10 @@ void export_echomail(char *sub_code,faddr_t addr)
 				if(msg.ftn_msgid!=NULL)	/* use original MSGID */
 					f+=sprintf(fmsgbuf+f,"\1MSGID: %.256s\r", msg.ftn_msgid);
 				else					/* generate MSGID */
-					f+=sprintf(fmsgbuf+f,"\1MSGID: <%lu.%s@%s> %08lX\r"
-						,msg.idx.number
-						,scfg.sub[i]->code
+					f+=sprintf(fmsgbuf+f,"\1MSGID: %s %08lX\r"
 						,faddrtoa(&scfg.sub[i]->faddr,NULL)
-						,msg.idx.time);
+						,(msg.idx.time<<5) | (msg.idx.number&0x1f)
+						);
 
 				if(msg.ftn_reply!=NULL)			/* use original REPLYID */
 					f+=sprintf(fmsgbuf+f,"\1REPLY: %.256s\r", msg.ftn_reply);
@@ -3939,7 +3938,7 @@ int main(int argc, char **argv)
 	memset(&msg_path,0,sizeof(addrlist_t));
 	memset(&fakearea,0,sizeof(areasbbs_t));
 
-	sscanf("$Revision: 1.66 $" + 11, "%s", revision);
+	sscanf("$Revision: 1.67 $" + 11, "%s", revision);
 
 	printf("\nSBBSecho v%s-%s (rev %s) - Synchronet FidoNet Packet "
 		"Tosser\n"
