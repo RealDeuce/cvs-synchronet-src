46800da2b057f7b612cdf6908df92d1a987ad6a5
rswindell <rswindell>
2002-11-08 05:26:19 +0000

    Removed carriage-return from FTN PID line.

--- sbbs3/postmsg.cpp
+++ sbbs3/postmsg.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet user create/post public message routine */
 
-/* $Id: postmsg.cpp,v 1.22 2002/11/02 04:04:07 rswindell Exp $ */
+/* $Id: postmsg.cpp,v 1.23 2002/11/08 05:26:19 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -45,7 +45,7 @@ static char* program_id()
 	char compiler[64];
 
 	DESCRIBE_COMPILER(compiler);
-	sprintf(str,"%.10s %s%c-%s%s%s %s %s\r"
+	sprintf(str,"%.10s %s%c-%s%s%s %s %s"
 		,VERSION_NOTICE,VERSION,REVISION,PLATFORM_DESC
 		,beta_version
 #ifdef _DEBUG
