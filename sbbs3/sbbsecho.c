ccffbb4446af433be4172e7a3cafd2a0b6f37c9c
rswindell <rswindell>
2002-11-16 02:00:36 +0000

    The TID (Tosser ID) kludge line is only added when exporting messages.

--- sbbs3/sbbsecho.c
+++ sbbs3/sbbsecho.c
@@ -2,7 +2,7 @@
 
 /* Synchronet FidoNet EchoMail Scanning/Tossing and NetMail Tossing Utility */
 
-/* $Id: sbbsecho.c,v 1.76 2002/11/10 07:38:07 rswindell Exp $ */
+/* $Id: sbbsecho.c,v 1.77 2002/11/16 02:00:36 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -2649,12 +2649,6 @@ int fmsgtosmsg(uchar HUGE16 *fbuf, fmsghdr_t fmsghdr, uint user, uint subnum)
 	if(taillen)
 		smb_dfield(&msg,TEXT_TAIL,taillen+2);
 
-	if(smb_get_hfield(&msg,FIDOTID,NULL)==NULL) { /* generate TID */
-		sprintf(str,"SBBSecho %s-%s r%s %s %s"
-			,SBBSECHO_VER,PLATFORM_DESC,revision,__DATE__,compiler);
-		smb_hfield(&msg,FIDOTID,(ushort)strlen(str),str);
-	}
-
 	i=smb_addmsghdr(smbfile,&msg,storage);
 	smb_freemsgmem(&msg);
 	if(i) {
@@ -3961,7 +3955,7 @@ int main(int argc, char **argv)
 	memset(&msg_path,0,sizeof(addrlist_t));
 	memset(&fakearea,0,sizeof(areasbbs_t));
 
-	sscanf("$Revision: 1.76 $" + 11, "%s", revision);
+	sscanf("$Revision: 1.77 $" + 11, "%s", revision);
 
 	DESCRIBE_COMPILER(compiler);
 
