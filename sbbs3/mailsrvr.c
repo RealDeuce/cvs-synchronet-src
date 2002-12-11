e1e7a61b2896394bcf26d0f080ca43ec06e7426b
rswindell <rswindell>
2002-12-11 20:56:04 +0000

    Changed SMTP temp filenames to include a random number.

--- sbbs3/mailsrvr.c
+++ sbbs3/mailsrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Mail (SMTP/POP3) server and sendmail threads */
 
-/* $Id: mailsrvr.c,v 1.217 2002/12/10 23:25:31 rswindell Exp $ */
+/* $Id: mailsrvr.c,v 1.218 2002/12/11 20:56:04 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1482,7 +1482,7 @@ static void smtp_thread(void* arg)
 		}
 	}
 
-	sprintf(rcptlst_fname,"%sSMTP%d.LST", scfg.data_dir, socket);
+	sprintf(rcptlst_fname,"%sSMTP.%d.%x.lst", scfg.data_dir, socket, xp_random(0x10000));
 	rcptlst=fopen(rcptlst_fname,"w+");
 	if(rcptlst==NULL) {
 		lprintf("%04d !SMTP ERROR %d creating recipient list: %s"
@@ -2332,7 +2332,7 @@ static void smtp_thread(void* arg)
 				if(!(startup->options&MAIL_OPT_DEBUG_RX_BODY))
 					unlink(msgtxt_fname);
 			}
-			sprintf(msgtxt_fname,"%sSMTP%u.RX", scfg.data_dir, socket);
+			sprintf(msgtxt_fname,"%sSMTP.%u.%x.msg", scfg.data_dir, socket, xp_random(0x10000));
 			if((msgtxt=fopen(msgtxt_fname,"w+b"))==NULL) {
 				lprintf("%04d !SMTP ERROR %d opening %s"
 					,socket, errno, msgtxt_fname);
@@ -2899,7 +2899,7 @@ const char* DLLCALL mail_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.217 $" + 11, "%s", revision);
+	sscanf("$Revision: 1.218 $" + 11, "%s", revision);
 
 	sprintf(ver,"Synchronet Mail Server %s%s  SMBLIB %s  "
 		"Compiled %s %s with %s"
