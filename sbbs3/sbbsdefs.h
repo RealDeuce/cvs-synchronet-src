16ffbce5cae7c38e7c4a84fa386b7515923efc2d
rswindell <rswindell>
2002-12-14 01:41:15 +0000

    New xtrn_t.misc bit, XTRN_SH, to force command-shell to be used execute command-line.

--- sbbs3/sbbsdefs.h
+++ sbbs3/sbbsdefs.h
@@ -2,7 +2,7 @@
 
 /* Synchronet constants, macros, and structure definitions */
 
-/* $Id: sbbsdefs.h,v 1.83 2002/12/13 01:51:20 rswindell Exp $ */
+/* $Id: sbbsdefs.h,v 1.84 2002/12/14 01:41:15 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -391,6 +391,7 @@ typedef enum {						/* Values for xtrn_t.event				*/
 #define STRIPKLUDGE (1L<<15)		/* Strip FTN Kludge lines from msg		*/
 #define XTRN_CHKTIME (1<<16)		/* Check time online (EX_CHKTIME)		*/
 #define XTRN_LWRCASE (1<<17)		/* Use lowercase drop-file names		*/
+#define XTRN_SH		 (1<<18)		/* Use command shell to execute			*/
 
 									/* Bits in cfg.xtrn_misc				*/
 #define XTRN_NO_MUTEX	(1<<0)		/* Do not use exec_mutex for FOSSIL VXD	*/
