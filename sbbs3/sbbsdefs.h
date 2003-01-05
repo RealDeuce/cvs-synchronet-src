8710b02d699f63ac3c6346a301f63a48ae2b419e
rswindell <rswindell>
2003-01-05 12:12:55 +0000

    Changed copyright date to 2003.

--- sbbs3/sbbsdefs.h
+++ sbbs3/sbbsdefs.h
@@ -2,13 +2,13 @@
 
 /* Synchronet constants, macros, and structure definitions */
 
-/* $Id: sbbsdefs.h,v 1.84 2002/12/14 01:41:15 rswindell Exp $ */
+/* $Id: sbbsdefs.h,v 1.85 2003/01/05 12:12:55 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -52,8 +52,8 @@
 #define VERSION_NOTICE		"Synchronet BBS for "PLATFORM_DESC\
 								"  Version " VERSION
 #define SYNCHRONET_CRC		0x9BCDD162
-#define COPYRIGHT_NOTICE	"Copyright 2002 Rob Swindell"
-#define COPYRIGHT_CRC		0xF8C6E3DA
+#define COPYRIGHT_NOTICE	"Copyright 2003 Rob Swindell"
+#define COPYRIGHT_CRC		0x65C902AC
 
 #define Y2K_2DIGIT_WINDOW	70
 
--- sbbs3/ver.cpp
+++ sbbs3/ver.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet version display */
 
-/* $Id: ver.cpp,v 1.18 2003/01/01 04:08:42 rswindell Exp $ */
+/* $Id: ver.cpp,v 1.19 2003/01/05 12:12:55 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -37,7 +37,7 @@
 
 #include "sbbs.h"
 
-extern "C" const char* beta_version = " "; /* Space if non-beta, " beta" otherwise */
+extern "C" const char* beta_version = " beta"; /* Space if non-beta, " beta" otherwise */
 
 #if defined(_WINSOCKAPI_)
 	extern WSADATA WSAData;
