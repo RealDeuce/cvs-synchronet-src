20268b56ecf7fc108e943743c7e50686f907d86a
rswindell <rswindell>
2003-01-05 05:25:42 +0000

    smb_getmsghdrlen() returns a ulong. Changed name of SMB_BAD_HDR_LEN to SMB_ERR_HDR_LEN. bugfix: smb_addmsghdr() checks header length before opening sha file. smb_hfield() now calculates and maintains hdr.length to keep from exceeding SMB_MAX_HDR_LEN and returns SMB_ERR_HDR_LEN if the new header field would put the total header length over the max.

--- smblib/smbdefs.h
+++ smblib/smbdefs.h
@@ -2,7 +2,7 @@
 
 /* Synchronet message base constant and structure definitions */
 
-/* $Id: smbdefs.h,v 1.33 2003/01/04 21:51:58 rswindell Exp $ */
+/* $Id: smbdefs.h,v 1.34 2003/01/05 05:25:42 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -109,7 +109,7 @@
 #define SDT_BLOCK_LEN		256 		/* Size of data blocks */
 #define SHD_BLOCK_LEN		256 		/* Size of header blocks */
 
-#define SMB_MAX_HDR_LEN		0xffffL		/* Message header length is 16-bit */
+#define SMB_MAX_HDR_LEN		0xffffU		/* Message header length is 16-bit */
 
 #define SMB_SELFPACK		0			/* Self-packing storage allocation */
 #define SMB_FASTALLOC		1			/* Fast allocation */
@@ -119,7 +119,7 @@
 
 										/* Common smblib errors values */
 #define SMB_ERR_NOT_OPEN	-100		/* Message base not open */
-#define SMB_BAD_HDR_LEN		-101		/* Invalid message header length (>64k) */
+#define SMB_ERR_HDR_LEN		-101		/* Invalid message header length (>64k) */
 
 										/* Time zone macros for when_t.zone */
 #define DAYLIGHT			0x8000		/* Daylight savings is active */
