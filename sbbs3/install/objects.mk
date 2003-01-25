8f5a0032adeb2e418158c99601a9a679d0e9fa9e
deuce <deuce>
2003-01-25 01:42:28 +0000

    Added ftp routines to allow future version to fetch release files via FTP.

--- sbbs3/install/objects.mk
+++ sbbs3/install/objects.mk
@@ -2,7 +2,7 @@
 
 # Make 'include file' listing object files for Synchronet SBBSINST
 
-# $Id: objects.mk,v 1.1 2003/01/21 10:10:18 rswindell Exp $
+# $Id: objects.mk,v 1.2 2003/01/25 01:42:28 deuce Exp $
 
 # LIBODIR, SBBSLIBODIR, SLASH, and OFILE must be pre-defined
 
@@ -11,5 +11,6 @@ OBJS	=	$(ODIR)$(SLASH)sbbsinst.$(OFILE)\
 			$(ODIR)$(SLASH)conwrap.$(OFILE)\
 			$(ODIR)$(SLASH)genwrap.$(OFILE)\
 			$(ODIR)$(SLASH)dirwrap.$(OFILE)\
-			$(ODIR)$(SLASH)uifcx.$(OFILE)\
+			$(ODIR)$(SLASH)ftpio.$(OFILE)\
+			$(ODIR)$(SLASH)uifcx.$(OFILE)
 
