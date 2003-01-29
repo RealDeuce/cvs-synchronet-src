8a253deec3b209c7db6ab8ef33985d7d9a7ef9fd
rswindell <rswindell>
2003-01-29 03:55:17 +0000

    Missing semicolon in last commit.

--- sbbs3/install/sbbsinst.c
+++ sbbs3/install/sbbsinst.c
@@ -2,7 +2,7 @@
 
 /* Synchronet installation utility 										*/
 
-/* $Id: sbbsinst.c,v 1.33 2003/01/29 03:54:35 rswindell Exp $ */
+/* $Id: sbbsinst.c,v 1.34 2003/01/29 03:55:17 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -641,7 +641,7 @@ int choose_server(char **opts)
 				"\nToDo: Add help.";
 	uifc.savnum++;
 	i=uifc.list(WIN_MID|WIN_ACT|WIN_SAV,0,0,0,&srvr_dflt,0
-		,"Select Server",opts)
+		,"Select Server",opts);
 	uifc.savnum--;
 	return(i);
 
