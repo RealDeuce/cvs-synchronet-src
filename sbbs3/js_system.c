1c30535945384cf947608cd4be19a3ae3f4a9ac9
rswindell <rswindell>
2003-02-04 01:08:48 +0000

    Added mods_dir and logs_dir properties.

--- sbbs3/js_system.c
+++ sbbs3/js_system.c
@@ -2,7 +2,7 @@
 
 /* Synchronet JavaScript "system" Object */
 
-/* $Id: js_system.c,v 1.50 2003/01/14 21:52:52 rswindell Exp $ */
+/* $Id: js_system.c,v 1.51 2003/02/04 01:08:48 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -92,6 +92,8 @@ enum {
 	,SYS_PROP_TEXT_DIR
 	,SYS_PROP_TEMP_DIR
 	,SYS_PROP_EXEC_DIR
+	,SYS_PROP_MODS_DIR
+	,SYS_PROP_LOGS_DIR
 
 };
 
@@ -253,6 +255,12 @@ static JSBool js_system_get(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
 		case SYS_PROP_EXEC_DIR:
 			p=cfg->exec_dir;
 			break;
+		case SYS_PROP_MODS_DIR:
+			p=cfg->mods_dir;
+			break;
+		case SYS_PROP_LOGS_DIR:
+			p=cfg->logs_dir;
+			break;
 	}
 
 	if(p!=NULL) {	/* string property */
@@ -340,6 +348,8 @@ static struct JSPropertySpec js_system_properties[] = {
 	{	"text_dir",					SYS_PROP_TEXT_DIR		,SYSOBJ_FLAGS,	NULL,	NULL },	
 	{	"temp_dir",					SYS_PROP_TEMP_DIR		,SYSOBJ_FLAGS,	NULL,	NULL },	
 	{	"exec_dir",					SYS_PROP_EXEC_DIR		,SYSOBJ_FLAGS,	NULL,	NULL },	
+	{	"mods_dir",					SYS_PROP_MODS_DIR		,SYSOBJ_FLAGS,	NULL,	NULL },	
+	{	"logs_dir",					SYS_PROP_LOGS_DIR		,SYSOBJ_FLAGS,	NULL,	NULL },	
 
 	{0}
 };
