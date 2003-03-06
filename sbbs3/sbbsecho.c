54784a66aa2ff38873d8db8493c88f51216e7ca4
rswindell <rswindell>
2003-03-06 21:57:18 +0000

    A little code beautification (no functional change).

--- sbbs3/sbbsecho.c
+++ sbbs3/sbbsecho.c
@@ -2,7 +2,7 @@
 
 /* Synchronet FidoNet EchoMail Scanning/Tossing and NetMail Tossing Utility */
 
-/* $Id: sbbsecho.c,v 1.94 2003/03/06 21:47:25 rswindell Exp $ */
+/* $Id: sbbsecho.c,v 1.95 2003/03/06 21:57:18 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1247,7 +1247,7 @@ void command(char *instr,faddr_t addr)
 			,cfg.arcdef[i].name,0);
 		cfg.nodecfg[node].arctype=i;
 		sprintf(str,"Compression type changed to %s.",cfg.arcdef[i].name);
-		create_netmail(NULL,"Compression Type Change",str,addr,0);
+		create_netmail(NULL,"Compression Type Change",str,addr,FALSE);
 		return; 
 	}
 
@@ -1263,13 +1263,13 @@ void command(char *instr,faddr_t addr)
 		if(!stricmp(temp,cfg.nodecfg[node].password)) {
 			sprintf(str,"Your password was already set to %s."
 				,cfg.nodecfg[node].password);
-			create_netmail(NULL,"Password Change Request",str,addr,0);
+			create_netmail(NULL,"Password Change Request",str,addr,FALSE);
 			return; }
 		alter_config(addr,cfg.nodecfg[node].password,temp,1);
 		sprintf(str,"Your password has been changed from %s to %.25s."
 			,cfg.nodecfg[node].password,temp);
 		sprintf(cfg.nodecfg[node].password,"%.25s",temp);
-		create_netmail(NULL,"Password Change Request",str,addr,0);
+		create_netmail(NULL,"Password Change Request",str,addr,FALSE);
 		return; 
 	}
 
@@ -4043,7 +4043,7 @@ int main(int argc, char **argv)
 	memset(&msg_path,0,sizeof(addrlist_t));
 	memset(&fakearea,0,sizeof(areasbbs_t));
 
-	sscanf("$Revision: 1.94 $", "%*s %s", revision);
+	sscanf("$Revision: 1.95 $", "%*s %s", revision);
 
 	DESCRIBE_COMPILER(compiler);
 
