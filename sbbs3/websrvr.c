0aaa8ae168a4737840a1d9a528eeb8b77dbb389f
deuce <deuce>
2003-03-16 22:40:26 +0000

    Fix typo in last commit... finally.

--- sbbs3/websrvr.c
+++ sbbs3/websrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Web Server */
 
-/* $Id: websrvr.c,v 1.77 2003/03/15 05:50:06 deuce Exp $ */
+/* $Id: websrvr.c,v 1.78 2003/03/16 22:40:26 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1006,7 +1006,7 @@ static int sockreadline(http_session_t * session, char *buf, size_t length)
 	}
 #else
 	for(i=0;TRUE;) {
-		if(revc(session->socket, &ch, 1, 0)!=1)  {
+		if(recv(session->socket, &ch, 1, 0)!=1)  {
 			session->req.keep_alive=FALSE;
 			close_request(session);
 			session->socket=INVALID_SOCKET;
@@ -2362,7 +2362,7 @@ const char* DLLCALL web_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.77 $", "%*s %s", revision);
+	sscanf("$Revision: 1.78 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
