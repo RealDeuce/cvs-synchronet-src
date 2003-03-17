4bc1b30c76296c34004d83bf3716823671be5ac3
deuce <deuce>
2003-03-17 23:46:52 +0000

    Fixed backslash support in cleanpath()

--- sbbs3/websrvr.c
+++ sbbs3/websrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Web Server */
 
-/* $Id: websrvr.c,v 1.81 2003/03/17 00:12:52 deuce Exp $ */
+/* $Id: websrvr.c,v 1.82 2003/03/17 23:46:52 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -499,7 +499,7 @@ static char *cleanpath(char *target, char *path, size_t size)  {
 				memmove(out,out+1,strlen(out));
 			else if(*(out+1)=='.' && (*(out+2)=='/' || *(out+2)=='\\'))
 				memmove(out,out+2,strlen(out)-1);
-			else if(*(out+1)=='.' && *(out+2)=='.' && (*(out+3)=='/' || *(out+3)=='/'))  {
+			else if(*(out+1)=='.' && *(out+2)=='.' && (*(out+3)=='/' || *(out+3)=='\\'))  {
 				*out=0;
 				p=strrchr(target,'/');
 				p2=strrchr(target,'\\');
@@ -1415,7 +1415,6 @@ static BOOL get_req(http_session_t * session)
 		if(p!=NULL) {
 			p=get_request(session,p);
 			session->http_ver=get_version(p);
-			lprintf("VERSION: %s (%s)",http_vers[session->http_ver],p);
 			if(session->http_ver>=HTTP_1_1)
 				session->req.keep_alive=TRUE;
 			if(session->req.dynamic==IS_CGI)  {
@@ -2371,7 +2370,7 @@ const char* DLLCALL web_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.81 $", "%*s %s", revision);
+	sscanf("$Revision: 1.82 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
