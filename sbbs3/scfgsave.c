d7ad109bccf26e73823a086706af91d632680c81
rswindell <rswindell>
2003-04-15 09:12:40 +0000

    fcopy function (used  by backup function) releases time-slices every 10 kbytes.

--- sbbs3/scfgsave.c
+++ sbbs3/scfgsave.c
@@ -2,7 +2,7 @@
 
 /* Synchronet configuration file save routines */
 
-/* $Id: scfgsave.c,v 1.28 2003/03/02 07:46:12 rswindell Exp $ */
+/* $Id: scfgsave.c,v 1.29 2003/04/15 09:12:40 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -81,6 +81,7 @@ BOOL DLLCALL save_cfg(scfg_t* cfg, int backup_level)
 static BOOL fcopy(char* src, char* dest)
 {
 	int		ch;
+	ulong	count=0;
 	FILE*	in;
 	FILE*	out;
 
@@ -96,6 +97,8 @@ static BOOL fcopy(char* src, char* dest)
 		if(ch==EOF)
 			break;
 		fputc(ch,out);
+		if(((count++)%(10*1024))==0)
+			SLEEP(1);
 	}
 
 	fclose(in);
