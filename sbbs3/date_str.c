ad8e971b64847241860d9494d8d03e4de784f764
rswindell <rswindell>
2003-04-18 04:57:08 +0000

    zonestr() now using smb_tzutc to convert "other" timezone values to +/- UTC.

--- sbbs3/date_str.c
+++ sbbs3/date_str.c
@@ -2,7 +2,7 @@
 
 /* Synchronet date/time string conversion routines */
 
-/* $Id: date_str.c,v 1.15 2002/11/13 03:07:59 rswindell Exp $ */
+/* $Id: date_str.c,v 1.16 2003/04/18 04:57:08 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -335,6 +335,7 @@ char* DLLCALL zonestr(short zone)
 		case WEL:   return("WEL");
 		}
 
+	zone=smb_tzutc(zone);
 	if(zone>0)
 		plus="+";
 	else
