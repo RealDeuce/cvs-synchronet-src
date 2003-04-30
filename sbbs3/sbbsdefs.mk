4b3b3b6f0b27e5be33aa73bca726342636d31a56
deuce <deuce>
2003-04-30 03:23:16 +0000

    New usage of _THREAD_SAFE define to indicated threaded calls are needed

--- sbbs3/sbbsdefs.mk
+++ sbbs3/sbbsdefs.mk
@@ -2,6 +2,6 @@
 
 # C/C++ compiler preprocessor definitions for building SBBS.DLL
 
-# $Id: sbbsdefs.mk,v 1.2 2002/07/21 23:04:39 rswindell Exp $
+# $Id: sbbsdefs.mk,v 1.3 2003/04/30 03:23:16 deuce Exp $
 
-SBBSDEFS=	-DSBBS -DSBBS_EXPORTS -DSMB_EXPORTS -DWRAPPER_EXPORTS
+SBBSDEFS=	-DSBBS -DSBBS_EXPORTS -DSMB_EXPORTS -DWRAPPER_EXPORTS -D_THREAD_SAFE
