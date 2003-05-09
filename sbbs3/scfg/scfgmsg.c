c51e5da7fe57e2ac22183fb92d768c0ee5145075
rswindell <rswindell>
2003-05-09 02:59:31 +0000

    Changed sub-boards echomail_sem to post_sem, not only used for echomail.

--- sbbs3/scfg/scfgmsg.c
+++ sbbs3/scfg/scfgmsg.c
@@ -1,6 +1,6 @@
 /* scfgmsg.c */
 
-/* $Id: scfgmsg.c,v 1.17 2003/05/06 09:46:42 rswindell Exp $ */
+/* $Id: scfgmsg.c,v 1.18 2003/05/09 02:59:31 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -389,8 +389,7 @@ of CRCs, maximum age of messages, storage method, and data directory.
 								strcpy(cfg.sub[j]->origline,cfg.sub[k]->origline);
 								strcpy(cfg.sub[j]->tagline,cfg.sub[k]->tagline);
 								strcpy(cfg.sub[j]->data_dir,cfg.sub[k]->data_dir);
-								strcpy(cfg.sub[j]->echomail_sem
-									,cfg.sub[k]->echomail_sem);
+								strcpy(cfg.sub[j]->post_sem,cfg.sub[k]->post_sem);
 								cfg.sub[j]->maxmsgs=cfg.sub[k]->maxmsgs;
 								cfg.sub[j]->maxcrcs=cfg.sub[k]->maxcrcs;
 								cfg.sub[j]->maxage=cfg.sub[k]->maxage;
@@ -511,7 +510,7 @@ export the current message group into.
 						,cfg.sub[j]->misc
 						,cfg.sub[j]->tagline
 						,cfg.sub[j]->origline
-						,cfg.sub[j]->echomail_sem
+						,cfg.sub[j]->post_sem
 						,cfg.sub[j]->newsgroup
 						,faddrtoa(&cfg.sub[j]->faddr,tmp)
 						);
@@ -682,7 +681,7 @@ import into the current message group.
 						sprintf(tmpsub.origline,"%.*s",50,str);
 						if(!fgets(str,128,stream)) break;
 						truncsp(str);
-						sprintf(tmpsub.echomail_sem,"%.*s",LEN_DIR,str);
+						sprintf(tmpsub.post_sem,"%.*s",LEN_DIR,str);
 						if(!fgets(str,128,stream)) break;
 						truncsp(str);
 						SAFECOPY(tmpsub.newsgroup,str);
