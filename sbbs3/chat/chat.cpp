bcdc85a4ee98bc94e553284bbc066cadfb41ad7a
rswindell <rswindell>
2003-05-16 08:26:17 +0000

    Added use of utime() to insure NFS/smbfs compatibility with node/chat.dab (shared-write) files. New look (green on black, System font) simulates Telnet interface. Ctrl-V (paste) and other unexpected control keys are now filtered.

--- sbbs3/chat/chat.cpp
+++ sbbs3/chat/chat.cpp
@@ -2,13 +2,13 @@
 
 /* Local sysop chat module (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: chat.cpp,v 1.2 2001/07/02 18:53:07 rswindell Exp $ */
+/* $Id: chat.cpp,v 1.3 2003/05/16 08:26:17 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -98,7 +98,8 @@ WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR cmdline, int)
     try
     {
          Application->Initialize();
-         Application->CreateForm(__classid(TMainForm), &MainForm);
+         Application->Title = "Synchronet Sysop Chat";
+		Application->CreateForm(__classid(TMainForm), &MainForm);
          Application->Run();
     }
     catch (Exception &exception)
