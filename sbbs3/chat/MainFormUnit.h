a823822c1c7a7b20026dc9228217ce11a49aaeaf
rswindell <rswindell>
2003-07-22 08:33:22 +0000

    Updated copyright date.

--- sbbs3/chat/MainFormUnit.h
+++ sbbs3/chat/MainFormUnit.h
@@ -2,13 +2,13 @@
 
 /* Local sysop chat module (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: MainFormUnit.h,v 1.1 2000/10/10 11:27:23 rswindell Exp $ */
+/* $Id: MainFormUnit.h,v 1.2 2003/07/22 08:33:22 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
