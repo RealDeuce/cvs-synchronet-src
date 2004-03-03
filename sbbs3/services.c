b98a6c04d6cd8db3f02137834a99fda17af40f59
deuce <deuce>
2004-03-03 22:35:07 +0000

    User number is unsigned.

--- sbbs3/services.c
+++ sbbs3/services.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Services */
 
-/* $Id: services.c,v 1.153 2003/12/30 00:19:09 rswindell Exp $ */
+/* $Id: services.c,v 1.154 2004/02/14 07:20:24 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -430,9 +430,11 @@ js_login(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	if((p=JS_GetStringBytes(js_str))==NULL) 
 		return(JS_FALSE);
 
+	memset(&user,0,sizeof(user));
+
 	if(isdigit(*p))
 		user.number=atoi(p);
-	else
+	else if(*p)
 		user.number=matchuser(&scfg,p,FALSE);
 
 	if(getuserdat(&scfg,&user)!=0) {
@@ -1587,7 +1589,7 @@ const char* DLLCALL services_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.153 $", "%*s %s", revision);
+	sscanf("$Revision: 1.154 $", "%*s %s", revision);
 
 	sprintf(ver,"Synchronet Services %s%s  "
 		"Compiled %s %s with %s"
@@ -1704,6 +1706,14 @@ void DLLCALL services_thread(void* arg)
 			cleanup(1);
 			return;
 		}
+
+#if 0	/* ToDo */
+		if(startup->temp_dir[0]) {
+			SAFECOPY(scfg.temp_dir,startup->temp_dir);
+			backslash(scfg.temp_dir);
+		} else
+#endif
+			prep_dir(scfg.data_dir, scfg.temp_dir, sizeof(scfg.temp_dir));
 
 		if(startup->host_name[0]==0)
 			SAFECOPY(startup->host_name,scfg.sys_inetaddr);
