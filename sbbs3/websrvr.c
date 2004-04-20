340cd0e3e9ab74c2cbaefe48e48df6962fe899fe
deuce <deuce>
2004-04-20 08:11:44 +0000

    Log failure to create User Objects.

--- sbbs3/websrvr.c
+++ sbbs3/websrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Web Server */
 
-/* $Id: websrvr.c,v 1.142 2004/04/20 08:10:57 deuce Exp $ */
+/* $Id: websrvr.c,v 1.143 2004/04/20 08:11:44 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -2217,6 +2217,7 @@ static BOOL js_setup(http_session_t* session)
 	lprintf(LOG_INFO,"     JavaScript: Initializing User Objects");
 	if(!js_CreateUserObjects(session->js_cx, session->js_glob, &scfg, NULL
 		,NULL /* ftp index file */, NULL /* subscan */)) {
+		lprintf(LOG_ERR,"%04d !ERROR initializing JavaScript User Objects",session->socket);
 		send_error(session,"500 Error initializing JavaScript User Objects");
 		return(FALSE);
 	}
@@ -2495,7 +2496,7 @@ const char* DLLCALL web_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.142 $", "%*s %s", revision);
+	sscanf("$Revision: 1.143 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
