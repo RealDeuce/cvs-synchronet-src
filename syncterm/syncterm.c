d5bc16764b285ccbef8891e1878acd04fa5da1e1
deuce <deuce>
2004-07-02 23:30:53 +0000

    Disable mouse stuff for ncurses until it's all fixed up. Seems to work for non-weird stuff (SyncTerm still has issues)

--- syncterm/syncterm.c
+++ syncterm/syncterm.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <conio.h>
+#include <ciowrap.h>
 
 #include "bbslist.h"
 #include "rlogin.h"
@@ -40,6 +40,7 @@ int main(int argc, char **argv)
 	if(!winsock_startup())
 		return(1);
 
+	initciowrap(UIFC_IBM|COLOR_MODE);
     gettextinfo(&txtinfo);
 	if((txtinfo.screenwidth<40) || txtinfo.screenheight<24) {
 		fputs("Window too small, must be at lest 80x24\n",stderr);
@@ -48,7 +49,7 @@ int main(int argc, char **argv)
 
 	atexit(uifcbail);
 	while((bbs=show_bbslist(BBSLIST_SELECT))!=NULL) {
-		if(!rlogin_connect(bbs->addr,bbs->port,bbs->user,bbs->password,bbs->dumb)) {
+		if(!rlogin_connect(bbs->addr,bbs->port,bbs->user,bbs->password)) {
 			/* ToDo: Update the entry with new lastconnected */
 			/* ToDo: Disallow duplicate entries */
 			uifcbail();
