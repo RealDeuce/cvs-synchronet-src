93cb23e854c031abd5caf4dc5d0c842ed988278c
rswindell <rswindell>
2004-07-03 00:09:05 +0000

    Bugfix: iniRemoveValue() wasn't checking if key_name() returned INI_NEW_SECTION (an invalid pointer).

--- uifc/keys.h
+++ uifc/keys.h
@@ -1,4 +1,4 @@
-/* $Id: keys.h,v 1.8 2004/07/05 07:53:00 deuce Exp $ */
+/* $Id: keys.h,v 1.6 2004/07/02 22:29:31 deuce Exp $ */
 
 #define CIO_KEY_HOME      0x47 << 8
 #define CIO_KEY_UP        72   << 8
@@ -12,4 +12,53 @@
 #define CIO_KEY_PPAGE     0x49 << 8
 #define CIO_KEY_NPAGE     0x51 << 8
 
-#define CIO_KEY_MOUSE    0xff02
+#define CIO_KEY_BACKSPACE    0xff01   /* Windows never differentiates between a
+				   * backspace keypress and the backspace
+				   * char
+				   */
+#define CIO_KEY_MOUSE	0xff02
+
+#ifndef KEY_HOME
+ #define KEY_HOME      0x47 << 8
+#endif
+#ifndef KEY_UP
+ #define KEY_UP        72   << 8
+#endif
+#ifndef KEY_END
+ #define KEY_END       0x4f << 8
+#endif
+#ifndef KEY_DOWN
+ #define KEY_DOWN      80   << 8
+#endif
+#ifndef KEY_F
+ #define KEY_F(x)      (0x3a+x) << 8
+#endif
+#ifndef KEY_IC
+ #define KEY_IC        0x52 << 8
+#endif
+#ifndef KEY_DC
+ #define KEY_DC        0x53 << 8
+#endif
+#ifndef KEY_LEFT
+ #define KEY_LEFT      0x4b << 8
+#endif
+#ifndef KEY_RIGHT
+ #define KEY_RIGHT     0x4d << 8
+#endif
+#ifndef KEY_PPAGE
+ #define KEY_PPAGE     0x49 << 8
+#endif
+#ifndef KEY_NPAGE
+ #define KEY_NPAGE     0x51 << 8
+#endif
+
+#ifndef KEY_BACKSPACE
+ #define KEY_BACKSPACE    0xff01   /* Windows never differentiates between a
+				   * backspace keypress and the backspace
+				   * char
+				   */
+#endif
+
+#ifndef KEY_MOUSE
+#define KEY_MOUSE	0xff02
+#endif
