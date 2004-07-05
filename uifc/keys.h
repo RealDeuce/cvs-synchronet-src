3eecfa67e0a066cbd899827bd013623f8972f822
deuce <deuce>
2004-07-05 09:11:34 +0000

    Fix STUPID bug in gotoxy() which wouldn't allow going to an x position greater than the window height.

--- uifc/keys.h
+++ uifc/keys.h
@@ -1,20 +1,15 @@
-/* $Id: keys.h,v 1.4 2004/05/31 23:52:58 deuce Exp $ */
+/* $Id: keys.h,v 1.8 2004/07/05 07:53:00 deuce Exp $ */
 
-#define KEY_HOME      0x47 << 8
-#define KEY_UP        72   << 8
-#define KEY_END       0x4f << 8
-#define KEY_DOWN      80   << 8
-#define KEY_F(x)      (0x3a+x) << 8
-#define KEY_IC        0x52 << 8
-#define KEY_DC        0x53 << 8
-#define KEY_LEFT      0x4b << 8
-#define KEY_RIGHT     0x4d << 8
-#define KEY_PPAGE     0x49 << 8
-#define KEY_NPAGE     0x51 << 8
+#define CIO_KEY_HOME      0x47 << 8
+#define CIO_KEY_UP        72   << 8
+#define CIO_KEY_END       0x4f << 8
+#define CIO_KEY_DOWN      80   << 8
+#define CIO_KEY_F(x)      (0x3a+x) << 8
+#define CIO_KEY_IC        0x52 << 8
+#define CIO_KEY_DC        0x53 << 8
+#define CIO_KEY_LEFT      0x4b << 8
+#define CIO_KEY_RIGHT     0x4d << 8
+#define CIO_KEY_PPAGE     0x49 << 8
+#define CIO_KEY_NPAGE     0x51 << 8
 
-#define KEY_BACKSPACE    0xff01   /* Windows never differentiates between a
-				   * backspace keypress and the backspace
-				   * char
-				   */
-
-#define KEY_MOUSE	0xff02
+#define CIO_KEY_MOUSE    0xff02
