8df7cf487730a19e32679eac379b83fea878ff7f
deuce <deuce>
2004-07-05 02:19:37 +0000

    Added movetext(), cgets(), cscanf(), and getpass()

--- uifc/ciowrap.c
+++ uifc/ciowrap.c
@@ -1,680 +1,178 @@
-/* $Id: ciowrap.c,v 1.12 2004/06/01 08:00:21 deuce Exp $ */
-
-#include <sys/time.h>
-#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 #include "ciowrap.h"
-#include "uifc.h"		/* UIFC_IBM */
+#ifndef NO_X
+ #include "x_cio.h"
+#endif
+#include "curs_cio.h"
 
-static int lastattr=0;
-static long mode;
+cioapi_t	cio_api;
 
-short curses_color(short color)
+void initciowrap(int mode)
 {
-	switch(color)
-	{
-		case 0 :
-			return(COLOR_BLACK);
-		case 1 :
-			return(COLOR_BLUE);
-		case 2 :
-			return(COLOR_GREEN);
-		case 3 :
-			return(COLOR_CYAN);
-		case 4 :
-			return(COLOR_RED);
-		case 5 :
-			return(COLOR_MAGENTA);
-		case 6 :
-			return(COLOR_YELLOW);
-		case 7 :
-			return(COLOR_WHITE);
-		case 8 :
-			return(COLOR_BLACK);
-		case 9 :
-			return(COLOR_BLUE);
-		case 10 :
-			return(COLOR_GREEN);
-		case 11 :
-			return(COLOR_CYAN);
-		case 12 :
-			return(COLOR_RED);
-		case 13 :
-			return(COLOR_MAGENTA);
-		case 14 :
-			return(COLOR_YELLOW);
-		case 15 :
-			return(COLOR_WHITE);
-	}
-	return(0);
-}
-
-int puttext(int sx, int sy, int ex, int ey, unsigned char *fill)
-{
-	int x,y;
-	int fillpos=0;
-	unsigned char attr;
-	unsigned char fill_char;
-	unsigned char orig_attr;
-	int oldx, oldy;
-
-	getyx(stdscr,oldy,oldx);	
-	orig_attr=lastattr;
-	for(y=sy-1;y<=ey-1;y++)
-	{
-		for(x=sx-1;x<=ex-1;x++)
-		{
-			fill_char=fill[fillpos++];
-			attr=fill[fillpos++];
-			textattr(attr);
-			move(y, x);
-			_putch(fill_char,FALSE);
-		}
-	}
-	textattr(orig_attr);
-	move(oldy, oldx);
-	refresh();
-	return(1);
-}
-
-int cio_gettext(int sx, int sy, int ex, int ey, unsigned char *fill)
-{
-	int x,y;
-	int fillpos=0;
-	chtype attr;
-	unsigned char attrib;
-	unsigned char colour;
-	int oldx, oldy;
-	unsigned char thischar;
-	int	ext_char;
-
-	getyx(stdscr,oldy,oldx);	
-	for(y=sy-1;y<=ey-1;y++)
-	{
-		for(x=sx-1;x<=ex-1;x++)
-		{
-			attr=mvinch(y, x);
-			if(attr&A_REVERSE) {
-				thischar=attr&255-'A'+1;
-			}
-			else if(attr&A_ALTCHARSET) {
-				if(!(mode&UIFC_IBM)){
-					ext_char=A_ALTCHARSET|(attr&255);
-					/* likely ones */
-					if (ext_char == ACS_CKBOARD)
-					{
-						thischar=176;
-					}
-					else if (ext_char == ACS_BOARD)
-					{
-						thischar=177;
-					}
-					else if (ext_char == ACS_BSSB)
-					{
-						thischar=218;
-					}
-					else if (ext_char == ACS_SSBB)
-					{
-						thischar=192;
-					}
-					else if (ext_char == ACS_BBSS)
-					{
-						thischar=191;
-					}
-					else if (ext_char == ACS_SBBS)
-					{
-						thischar=217;
-					}
-					else if (ext_char == ACS_SBSS)
-					{
-						thischar=180;
-					}
-					else if (ext_char == ACS_SSSB)
-					{
-						thischar=195;
-					}
-					else if (ext_char == ACS_SSBS)
-					{
-						thischar=193;
-					}
-					else if (ext_char == ACS_BSSS)
-					{
-						thischar=194;
-					}
-					else if (ext_char == ACS_BSBS)
-					{
-						thischar=196;
-					}
-					else if (ext_char == ACS_SBSB)
-					{
-						thischar=179;
-					}
-					else if (ext_char == ACS_SSSS)
-					{
-						thischar=197;
-					}
-					else if (ext_char == ACS_BLOCK)
-					{
-						thischar=219;
-					}
-					else if (ext_char == ACS_UARROW)
-					{
-						thischar=30;
-					}
-					else if (ext_char == ACS_DARROW)
-					{
-						thischar=31;
-					}
-
-					/* unlikely (Not in ncurses) */
-					else if (ext_char == ACS_SBSD)
-					{
-						thischar=181;
-					}
-					else if (ext_char == ACS_DBDS)
-					{
-						thischar=182;
-					}
-					else if (ext_char == ACS_BBDS)
-					{
-						thischar=183;
-					}
-					else if (ext_char == ACS_BBSD)
-					{
-						thischar=184;
-					}
-					else if (ext_char == ACS_DBDD)
-					{
-						thischar=185;
-					}
-					else if (ext_char == ACS_DBDB)
-					{
-						thischar=186;
-					}
-					else if (ext_char == ACS_BBDD)
-					{
-						thischar=187;
-					}
-					else if (ext_char == ACS_DBBD)
-					{
-						thischar=188;
-					}
-					else if (ext_char == ACS_DBBS)
-					{
-						thischar=189;
-					}
-					else if (ext_char == ACS_SBBD)
-					{
-						thischar=190;
-					}
-					else if (ext_char == ACS_SDSB)
-					{
-						thischar=198;
-					}
-					else if (ext_char == ACS_DSDB)
-					{
-						thischar=199;
-					}
-					else if (ext_char == ACS_DDBB)
-					{
-						thischar=200;
-					}
-					else if (ext_char == ACS_BDDB)
-					{
-						thischar=201;
-					}
-					else if (ext_char == ACS_DDBD)
-					{
-						thischar=202;
-					}
-					else if (ext_char == ACS_BDDD)
-					{
-						thischar=203;
-					}
-					else if (ext_char == ACS_DDDB)
-					{
-						thischar=204;
-					}
-					else if (ext_char == ACS_BDBD)
-					{
-						thischar=205;
-					}
-					else if (ext_char == ACS_DDDD)
-					{
-						thischar=206;
-					}
-					else if (ext_char == ACS_SDBD)
-					{
-						thischar=207;
-					}
-					else if (ext_char == ACS_DSBS)
-					{
-						thischar=208;
-					}
-					else if (ext_char == ACS_BDSD)
-					{
-						thischar=209;
-					}
-					else if (ext_char == ACS_BSDS)
-					{
-						thischar=210;
-					}
-					else if (ext_char == ACS_DSBB)
-					{
-						thischar=211;
-					}
-					else if (ext_char == ACS_SDBB)
-					{
-						thischar=212;
-					}
-					else if (ext_char == ACS_BDSB)
-					{
-						thischar=213;
-					}
-					else if (ext_char == ACS_BSDB)
-					{
-						thischar=214;
-					}
-					else if (ext_char == ACS_DSDS)
-					{
-						thischar=215;
-					}
-					else if (ext_char == ACS_SDSD)
-					{
-						thischar=216;
-					}
-					else
-					{
-						thischar=attr&255;
-					}
-				}
-				else {
-					if (ext_char == ACS_UARROW)
-					{
-						thischar=30;
-					}
-					else if (ext_char == ACS_DARROW)
-					{
-						thischar=31;
-					}
-					else
-					{
-						thischar=attr&255;
-					}
-				}
-			}
-			else
-				thischar=attr;
-			fill[fillpos++]=(unsigned char)(thischar);
-			attrib=0;
-			if (attr & A_BOLD)  
-			{
-				attrib |= 8;
-			}
-			if (attr & A_BLINK)
-			{
-				attrib |= 128;
-			}
-			colour=PAIR_NUMBER(attr&A_COLOR)-1;
-			colour=((colour&56)<<1)|(colour&7);
-			fill[fillpos++]=colour|attrib;
-		}
-	}
-	move(oldy, oldx);
-	return(1);
-}
-
-void textattr(unsigned char attr)
-{
-	chtype   attrs=A_NORMAL;
-	int	colour;
-
-	if (lastattr==attr)
+#ifndef NO_X
+	if(!console_init()) {
+		cio_api.mode=X_MODE;
+		cio_api.puttext=x_puttext;
+		cio_api.gettext=x_gettext;
+		cio_api.textattr=x_textattr;
+		cio_api.textbackground=x_textbackground;
+		cio_api.textcolor=x_textcolor;
+		cio_api.kbhit=x_kbhit;
+		cio_api.delay=x_delay;
+		cio_api.wherey=x_wherey;
+		cio_api.wherex=x_wherex;
+		cio_api.putch=x_putch;
+		cio_api.c_printf=x_cprintf;
+		cio_api.cputs=x_cputs;
+		cio_api.gotoxy=x_gotoxy;
+		cio_api.clrscr=x_clrscr;
+		cio_api.gettextinfo=x_gettextinfo;
+		cio_api.setcursortype=x_setcursortype;
+		cio_api.clreol=x_clreol;
+		cio_api.getch=x_getch;
+		cio_api.getche=x_getche;
+		cio_api.beep=x_beep;
+		cio_api.highvideo=x_highvideo;
+		cio_api.lowvideo=x_lowvideo;
+		cio_api.normvideo=x_normvideo;
 		return;
-
-	lastattr=attr;
-	
-	if (attr & 8)  {
-		attrs |= A_BOLD;
 	}
-	if (attr & 128)
-	{
-		attrs |= A_BLINK;
-	}
-	colour = COLOR_PAIR( ((attr&7)|((attr>>1)&56))+1 );
-	#ifdef NCURSES_VERSION_MAJOR
-	attrset(attrs);
-	color_set(colour,NULL);
-	#else
-	attrset(attrs|colour);
-	#endif
-	/* bkgdset(colour); */
-	bkgdset(colour);
-}
-
-void textbackground(int colour)
-{
-	unsigned char attr;
-	
-	attr=lastattr;
-	attr&=143;
-	attr|=(colour<<4);
-	textattr(attr);
-}
-
-void textcolor(int colour)
-{
-	unsigned char attr;
-	
-	attr=lastattr;
-	attr&=240;
-	attr|=colour;
-	textattr(attr);
-}
-
-int kbhit(void)
-{
-	struct timeval timeout;
-	fd_set	rfds;
-
-	timeout.tv_sec=0;
-	timeout.tv_usec=0;
-	FD_ZERO(&rfds);
-	FD_SET(fileno(stdin),&rfds);
-
-	return(select(fileno(stdin)+1,&rfds,NULL,NULL,&timeout));
-}
-
-#ifndef __QNX__
-void delay(long msec)
-{
-	usleep(msec*1000);
-}
+	fprintf(stderr,"X init failed\n");
 #endif
-
-int wherey(void)
-{
-	int x,y;
-	getyx(stdscr,y,x);
-	return(y+1);
+	curs_initciowrap(mode);
+	cio_api.mode=CURSES_MODE;
+	cio_api.puttext=curs_puttext;
+	cio_api.gettext=curs_gettext;
+	cio_api.textattr=curs_textattr;
+	cio_api.textbackground=curs_textbackground;
+	cio_api.textcolor=curs_textcolor;
+	cio_api.kbhit=curs_kbhit;
+	cio_api.delay=curs_delay;
+	cio_api.wherey=curs_wherey;
+	cio_api.wherex=curs_wherex;
+	cio_api.putch=curs_putch;
+	cio_api.c_printf=curs_cprintf;
+	cio_api.cputs=curs_cputs;
+	cio_api.gotoxy=curs_gotoxy;
+	cio_api.clrscr=curs_clrscr;
+	cio_api.gettextinfo=curs_gettextinfo;
+	cio_api.setcursortype=curs_setcursortype;
+	cio_api.clreol=curs_clreol;
+	cio_api.getch=curs_getch;
+	cio_api.getche=curs_getche;
+	cio_api.beep=beep;
+	cio_api.highvideo=curs_highvideo;
+	cio_api.lowvideo=curs_lowvideo;
+	cio_api.normvideo=curs_normvideo;
 }
 
-int wherex(void)
+int movetext(int sx, int sy, int ex, int ey, int dx, int dy)
 {
-	int x,y;
-	getyx(stdscr,y,x);
-	return(x+1);
-}
+	int width;
+	int height;
+	char *buf;
 
-void _putch(unsigned char ch, BOOL refresh_now)
-{
-	chtype	cha;
-
-	if(!(mode&UIFC_IBM))
-	{
-		switch(ch)
-		{
-			case 30:
-				cha=ACS_UARROW;
-				break;
-			case 31:
-				cha=ACS_DARROW;
-				break;
-			case 176:
-				cha=ACS_CKBOARD;
-				break;
-			case 177:
-				cha=ACS_BOARD;
-				break;
-			case 178:
-				cha=ACS_BOARD;
-				break;
-			case 179:
-				cha=ACS_SBSB;
-				break;
-			case 180:
-				cha=ACS_SBSS;
-				break;
-			case 181:
-				cha=ACS_SBSD;
-				break;
-			case 182:
-				cha=ACS_DBDS;
-				break;
-			case 183:
-				cha=ACS_BBDS;
-				break;
-			case 184:
-				cha=ACS_BBSD;
-				break;
-			case 185:
-				cha=ACS_DBDD;
-				break;
-			case 186:
-				cha=ACS_DBDB;
-				break;
-			case 187:
-				cha=ACS_BBDD;
-				break;
-			case 188:
-				cha=ACS_DBBD;
-				break;
-			case 189:
-				cha=ACS_DBBS;
-				break;
-			case 190:
-				cha=ACS_SBBD;
-				break;
-			case 191:
-				cha=ACS_BBSS;
-				break;
-			case 192:
-				cha=ACS_SSBB;
-				break;
-			case 193:
-				cha=ACS_SSBS;
-				break;
-			case 194:
-				cha=ACS_BSSS;
-				break;
-			case 195:
-				cha=ACS_SSSB;
-				break;
-			case 196:
-				cha=ACS_BSBS;
-				break;
-			case 197:
-				cha=ACS_SSSS;
-				break;
-			case 198:
-				cha=ACS_SDSB;
-				break;
-			case 199:
-				cha=ACS_DSDB;
-				break;
-			case 200:
-				cha=ACS_DDBB;
-				break;
-			case 201:
-				cha=ACS_BDDB;
-				break;
-			case 202:
-				cha=ACS_DDBD;
-				break;
-			case 203:
-				cha=ACS_BDDD;
-				break;
-			case 204:
-				cha=ACS_DDDB;
-				break;
-			case 205:
-				cha=ACS_BDBD;
-				break;
-			case 206:
-				cha=ACS_DDDD;
-				break;
-			case 207:
-				cha=ACS_SDBD;
-				break;
-			case 208:
-				cha=ACS_DSBS;
-				break;
-			case 209:
-				cha=ACS_BDSD;
-				break;
-			case 210:
-				cha=ACS_BSDS;
-				break;
-			case 211:
-				cha=ACS_DSBB;
-				break;
-			case 212:
-				cha=ACS_SDBB;
-				break;
-			case 213:
-				cha=ACS_BDSB;
-				break;
-			case 214:
-				cha=ACS_BSDB;
-				break;
-			case 215:
-				cha=ACS_DSDS;
-				break;
-			case 216:
-				cha=ACS_SDSD;
-				break;
-			case 217:
-				cha=ACS_SBBS;
-				break;
-			case 218:
-				cha=ACS_BSSB;
-				break;
-			case 219:
-				cha=ACS_BLOCK;
-				break;
-			default:
-				cha=ch;
-		}
+	width=ex-sx;
+	height=ey-sy;
+	buf=(char *)malloc((width+1)*(height+1)*2);
+	if(buf==NULL)
+		return(0);
+	if(!gettext(sx,sy,ex,ey,buf)) {
+		free(buf);
+		return(0);
 	}
-	else
-	{
+	if(!puttext(dx,dy,dx+width,dy+height,buf)) {
+		free(buf);
+		return(0);
+	}
+	free(buf);
+	return(1);
+}
+
+char *cgets(char *str)
+{
+	int	maxlen;
+	int len=0;
+	int chars;
+	int ch;
+
+	maxlen=*(unsigned char *)str;
+	while((ch=getche())!='\n') {
 		switch(ch) {
-			case 30:
-				cha=ACS_UARROW;
+			case 0:	/* Skip extended keys */
+				ch=getche();
 				break;
-			case 31:
-				cha=ACS_DARROW;
+			case '\n':
+				str[len+2]=0;
+				*((unsigned char *)(str+1))=(unsigned char)len;
+				return(&str[2]);
+			case '\r':	/* Skip \r (ToDo: Should this be treeated as a \n? */
+				break;
+			case '\b':
+				if(len==0) {
+					beep();
+					break;
+				}
+				putch('\b');
+				len--;
 				break;
 			default:
-				cha=ch;
+				str[(len++)+2]=ch;
+				if(len==maxlen) {
+					str[len+2]=0;
+					*((unsigned char *)(str+1))=(unsigned char)len;
+					return(&str[2]);
+				}
+				break;
 		}
 	}
-
-	if(ch == ' ')
-		addch(A_BOLD|' ');
-	else if (cha<' ') {
- 		attron(A_REVERSE);
-		addch(cha+'A'-1);
-		attroff(A_REVERSE);
-	}
-	else
-		addch(cha);
-
-	if(refresh_now)
-		refresh();
 }
 
-int cprintf(char *fmat, ...)
+int cscanf (char *format , ...)
 {
+	char str[255];
     va_list argptr;
-	unsigned char	str[MAX_BFLN];
-	int		pos;
+	int ret;
 
-    va_start(argptr,fmat);
-    vsprintf(str,fmat,argptr);
-    va_end(argptr);
-	for(pos=0;str[pos];pos++)
-	{
-		_putch(str[pos],FALSE);
-	}
-	refresh();
-    return(1);
+	str[0]=-1;
+	va_start(argptr,format);
+	ret=vsscanf(cgets(str),format,argptr);
+	va_end(argptr);
+	return(ret);
 }
 
-void cputs(unsigned char *str)
+char *getpass(const char *prompt)
 {
-	int		pos;
+	static char pass[9];
+	int len=0;
+	int chars;
+	int ch;
 
-	for(pos=0;str[pos];pos++)
-	{
-		_putch(str[pos],FALSE);
-	}
-	refresh();
-}
-
-void gotoxy(int x, int y)
-{
-	move(y-1,x-1);
-	refresh();
-}
-
-void clrscr(void)
-{
-    clear();
-	refresh();
-}
-
-void initciowrap(long inmode)
-{
-	short	fg, bg, pair=0;
-
-#ifdef XCURSES
-	char	*argv[2]={"Syhcnronet",NULL};
-
-	Xinitscr(1,argv);
-#else
-	initscr();
-#endif
-	start_color();
-	cbreak();
-	noecho();
-	nonl();
-	keypad(stdscr, TRUE);
-	scrollok(stdscr,FALSE);
-	raw();
-
-	/* Set up color pairs */
-	for(bg=0;bg<8;bg++)  {
-		for(fg=0;fg<8;fg++) {
-			init_pair(++pair,curses_color(fg),curses_color(bg));
+	while((ch=getch())!='\n') {
+		switch(ch) {
+			case 0:	/* Skip extended keys */
+				ch=getch();
+				break;
+			case '\n':
+				pass[len]=0;
+				return(pass);
+			case '\r':	/* Skip \r (ToDo: Should this be treeated as a \n? */
+				break;
+			case '\b':
+				if(len==0) {
+					beep();
+					break;
+				}
+				len--;
+				break;
+			default:
+				if(len==8)
+					beep();
+				else
+					pass[len++]=ch;
+				break;
 		}
 	}
-	mode = inmode;
-}
-
-void gettextinfo(struct text_info *info)
-{
-	getmaxyx(stdscr, info->screenheight, info->screenwidth);
-	if(has_colors())
-		info->currmode=COLOR_MODE;
-	else
-		info->currmode=MONO;
-}
-
-void _setcursortype(int type) {
-	switch(type) {
-		case _NOCURSOR:
-			curs_set(0);
-			break;
-		
-		case _SOLIDCURSOR:
-			curs_set(2);
-			break;
-		
-		default:	/* Normal cursor */
-			curs_set(1);
-			break;
-
-	}
-	refresh();
 }
