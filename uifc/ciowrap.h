76fd96ebb025f8257bc47834ca4113c3be0a5364
deuce <deuce>
2004-07-05 05:22:29 +0000

    Complete conio compatability.  ciowrap now does everything that the old Borland conio library does with the exception of the hardware I/O.
    I may add support for the newer Borland stuff eventually, but I doubt it.
    :-)

--- uifc/ciowrap.h
+++ uifc/ciowrap.h
@@ -1,14 +1,17 @@
-/* $Id: ciowrap.h,v 1.11 2004/06/01 07:29:35 deuce Exp $ */
+/* $Id: ciowrap.h,v 1.24 2004/07/05 05:22:29 deuce Exp $ */
+
+#ifndef _CIOWRAP_H_
+#define _CIOWRAP_H_
 
 #ifndef __unix__
 #include "conio.h"
 #define initciowrap(x)
 #else
 #include "curs_fix.h"
-
-#define MONO	1
-#define	BW80	MONO
-#define COLOR_MODE	2
+#ifndef NEED_CURSES_GETCH
+ #undef getch
+#endif
+#include "keys.h"
 
 #ifndef BOOL
 #define BOOL    int
@@ -22,8 +25,8 @@
 
 enum {
 	 BLACK
-	,BLUE	
-	,GREEN	
+	,BLUE
+	,GREEN
 	,CYAN
 	,RED
 	,MAGENTA
@@ -38,6 +41,17 @@ enum {
 	,YELLOW
 	,WHITE
 };
+#define BLINK 128
+
+#define LASTMODE	-1
+#define BW40		0
+#define C40			1
+#define BW80		2
+#define	C80			3
+#define MONO		7
+#define C4350		64
+
+#define COLOR_MODE	C80
 
 enum
 {
@@ -46,40 +60,92 @@ enum
 	_NORMALCURSOR
 };
 
-struct text_info {
-    unsigned char currmode;
-    unsigned char screenheight;
-	unsigned char screenwidth;
+enum
+{
+	 X_MODE
+	,CURSES_MODE
 };
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-#define clreol()	clrtoeol()
-#define putch(x)	_putch(x,TRUE)
-short curses_color(short color);
-int puttext(int sx, int sy, int ex, int ey, unsigned char *fill);
-#define gettext(x1,y1,x2,y2,z)	cio_gettext(x1,y1,x2,y2,z)
-int cio_gettext(int sx, int sy, int ex, int ey, unsigned char *fill);
-void textattr(unsigned char attr);
+struct text_info {
+	unsigned char winleft;        /* left window coordinate */
+	unsigned char wintop;         /* top window coordinate */
+	unsigned char winright;       /* right window coordinate */
+	unsigned char winbottom;      /* bottom window coordinate */
+	unsigned char attribute;      /* text attribute */
+	unsigned char normattr;       /* normal attribute */
+	unsigned char currmode;       /* current video mode:
+                                	 BW40, BW80, C40, C80, or C4350 */
+	unsigned char screenheight;   /* text screen's height */
+	unsigned char screenwidth;    /* text screen's width */
+	unsigned char curx;           /* x-coordinate in current window */
+	unsigned char cury;           /* y-coordinate in current window */
+};
+
+typedef struct {
+	int		mode;
+	void	(*clreol)		(void);
+	int		(*puttext)		(int,int,int,int,unsigned char *);
+	int		(*gettext)		(int,int,int,int,unsigned char *);
+	void	(*textattr)		(unsigned char);
+	int		(*kbhit)		(void);
+	void	(*delay)		(long);
+	int		(*wherex)		(void);
+	int		(*wherey)		(void);
+	int		(*putch)		(unsigned char);
+	int		(*c_printf)		(char *fmat, ...);
+	int		(*cputs)		(unsigned char *);
+	void	(*gotoxy)		(int,int);
+	void	(*clrscr)		(void);
+	void	(*gettextinfo)	(struct text_info *);
+	void	(*setcursortype)(int);
+	void	(*textbackground)	(int);
+	void	(*textcolor)	(int);
+	int		(*getch)		(void);
+	int		(*getche)		(void);
+	int		(*beep)			(void);
+	void	(*highvideo)	(void);
+	void	(*lowvideo)		(void);
+	void	(*normvideo)	(void);
+	void	(*textmode)		(int);
+} cioapi_t;
+
+int movetext(int sx, int sy, int ex, int ey, int dx, int dy);
+char *cgets(char *str);
+int cscanf (char *format , ...);
 int kbhit(void);
-#ifndef __QNX__
-void delay(long msec);
+#ifndef NEED_CURSES_GETCH
+ int getch(void);
 #endif
-int wherey(void);
-int wherex(void);
-void _putch(unsigned char ch, BOOL refresh_now);
-int cprintf(char *fmat, ...);
-void cputs(unsigned char *str);
-void gotoxy(int x, int y);
-void clrscr(void);
-void initciowrap(long inmode);
+int getche(void);
+int ungetch(int ch);
 void gettextinfo(struct text_info *info);
-void _setcursortype(int type);
-void textbackground(int colour);
-void textcolor(int colour);
-#ifdef __cplusplus
-}
-#endif
+int wherex(void);
+int wherex(void);
+void wscroll(void);
+void gotoxy(int x, int y);
+void clreol(void);
+void clrscr(void);
+
+extern cioapi_t cio_api;
+extern int _wscroll;
+extern int directvideo;
+
+#define _conio_kbhit()		kbhit()
+#define puttext(a,b,c,d,e)	cio_api.puttext(a,b,c,d,e)
+#define gettext(a,b,c,d,e)	cio_api.gettext(a,b,c,d,e)
+#define textattr(a)			cio_api.textattr(a)
+#define delay(a)			cio_api.delay(a)
+#define putch(a)			cio_api.putch(a)
+#define cprintf				cio_api.c_printf
+#define cputs(a)			cio_api.cputs(a)
+#define _setcursortype(a)	cio_api.setcursortype(a)
+#define textbackground(a)	cio_api.textbackground(a)
+#define textcolor(a)		cio_api.textcolor(a)
+#define beep()				cio_api.beep()
+#define highvideo()			cio_api.highvideo()
+#define lowvideo()			cio_api.lowvideo()
+#define normvideo()			cio_api.normvideo()
 
 #endif
+
+#endif	/* Do not add anything after this line */
---
+++ uifc/curs_cio.h
@@ -0,0 +1,37 @@
+/* $Id: curs_cio.h,v 1.8 2004/07/05 05:22:29 deuce Exp $ */
+
+#ifdef __unix__
+#include "ciowrap.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+short curses_color(short color);
+int curs_puttext(int sx, int sy, int ex, int ey, unsigned char *fill);
+int curs_gettext(int sx, int sy, int ex, int ey, unsigned char *fill);
+void curs_textattr(unsigned char attr);
+int curs_kbhit(void);
+void curs_delay(long msec);
+int curs_wherey(void);
+int curs_wherex(void);
+int _putch(unsigned char ch, BOOL refresh_now);
+int curs_putch(unsigned char ch);
+int curs_cprintf(char *fmat, ...);
+int curs_cputs(unsigned char *str);
+void curs_gotoxy(int x, int y);
+void curs_initciowrap(long inmode);
+void curs_gettextinfo(struct text_info *info);
+void curs_setcursortype(int type);
+void curs_textbackground(int colour);
+void curs_textcolor(int colour);
+int curs_getch(void);
+int curs_getche(void);
+void curs_highvideo(void);
+void curs_lowvideo(void);
+void curs_normvideo(void);
+void curs_textmode(int mode);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
