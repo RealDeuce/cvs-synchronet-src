02bf1e6865b586f4b06a977093f0b261bf286bf8
deuce <deuce>
2004-07-06 17:39:26 +0000

    Make some Win32-Happy changes.  (Not that this'll work with Win32 yet or anything)

--- conio/curs_cio.h
+++ conio/curs_cio.h
@@ -1,26 +1,24 @@
-/* $Id: curs_cio.h,v 1.6 2004/07/26 23:56:35 deuce Exp $ */
+/* $Id: curs_cio.h,v 1.1 2004/07/05 21:27:40 deuce Exp $ */
 
 #ifdef __unix__
-#include "ciolib.h"
-#undef getch
-#undef ungetch
+#include "conio.h"
 #include "curs_fix.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 short curses_color(short color);
-int curs_puttext(int sx, int sy, int ex, int ey, void *fill);
-int curs_gettext(int sx, int sy, int ex, int ey, void *fill);
-void curs_textattr(int attr);
+int curs_puttext(int sx, int sy, int ex, int ey, unsigned char *fill);
+int curs_gettext(int sx, int sy, int ex, int ey, unsigned char *fill);
+void curs_textattr(unsigned char attr);
 int curs_kbhit(void);
 void curs_delay(long msec);
 int curs_wherey(void);
 int curs_wherex(void);
 int _putch(unsigned char ch, BOOL refresh_now);
-int curs_putch(int ch);
+int curs_putch(unsigned char ch);
 void curs_gotoxy(int x, int y);
-int curs_initciolib(long inmode);
+void curs_initciowrap(long inmode);
 void curs_gettextinfo(struct text_info *info);
 void curs_setcursortype(int type);
 int curs_getch(void);
@@ -29,5 +27,7 @@ void curs_textmode(int mode);
 #ifdef __cplusplus
 }
 #endif
+
+#define UIFC_IBM	(1<<4)  /* Force use of IBM charset     */
 
 #endif
--- syncterm/term.c
+++ syncterm/term.c
@@ -1,5 +1,5 @@
 #include <genwrap.h>
-#include <ciolib.h>
+#include <conio.h>
 #include <keys.h>
 
 #include "term.h"
@@ -534,7 +534,7 @@ void doterm(void)
 							case 0:
 								break;
 							case 7:			/* Beep */
-								cputs(prn);
+								cprintf(prn);
 								prn[0]=0;
 								#ifdef __unix__
 									putch(7);
@@ -543,18 +543,18 @@ void doterm(void)
 								#endif
 								break;
 							case 12:		/* ^L - Clear screen */
-								cputs(prn);
+								cprintf(prn);
 								prn[0]=0;
 								clearscreen(term.attr);
 								gotoxy(1,1);
 								break;
 							case 27:		/* ESC */
-								cputs(prn);
+								cprintf(prn);
 								prn[0]=0;
 								term.sequence=1;
 								break;
 							case '\t':
-								cputs(prn);
+								cprintf(prn);
 								prn[0]=0;
 								for(k=0;k<11;k++) {
 									if(tabs[k]>wherex()) {
@@ -568,7 +568,7 @@ void doterm(void)
 						}
 					}
 				}
-				cputs(prn);
+				cprintf(prn);
 				prn[0]=0;
 				break;
 		}
