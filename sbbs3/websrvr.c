8ec1bc8743728361b730a2b81da31f819b5a4922
deuce <deuce>
2004-07-13 23:01:55 +0000

    Change comparison to assignment.

--- sbbs3/websrvr.c
+++ sbbs3/websrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Web Server */
 
-/* $Id: websrvr.c,v 1.149 2004/07/13 23:18:30 deuce Exp $ */
+/* $Id: websrvr.c,v 1.148 2004/07/13 23:01:55 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -833,16 +833,14 @@ static BOOL check_ars(http_session_t * session)
 	char	*password;
 	uchar	*ar;
 	BOOL	authorized;
-	char	auth_req[MAX_REQUEST_LINE];
 
 	if(session->req.auth[0]==0) {
 		if(startup->options&WEB_OPT_DEBUG_RX)
 			lprintf(LOG_NOTICE,"%04d !No authentication information",session->socket);
 		return(FALSE);
 	}
-	SAFECOPY(auth_req,session->req.auth);
 
-	username=strtok(auth_req,":");
+	username=strtok(session->req.auth,":");
 	if(username==NULL)
 		username="";
 	password=strtok(NULL,":");
@@ -2500,7 +2498,7 @@ const char* DLLCALL web_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.149 $", "%*s %s", revision);
+	sscanf("$Revision: 1.148 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
