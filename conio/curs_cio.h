4cd193cbc77d2fe76ff1fc43dcc77206c15dbd6b
deuce <deuce>
2004-07-15 05:10:15 +0000

    Fix segfault in installer when viewing non-existant nodes.

--- conio/curs_cio.h
+++ conio/curs_cio.h
@@ -1,37 +1,33 @@
-/* $Id: curs_cio.h,v 1.7 2004/08/02 02:43:59 deuce Exp $ */
+/* $Id: curs_cio.h,v 1.1 2004/07/05 21:27:40 deuce Exp $ */
 
 #ifdef __unix__
-#include "ciolib.h"
-#undef getch
-#undef ungetch
-#undef getmouse
+#include "conio.h"
 #include "curs_fix.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 short curses_color(short color);
-int curs_puttext(int sx, int sy, int ex, int ey, void *fill);
-int curs_gettext(int sx, int sy, int ex, int ey, void *fill);
-void curs_textattr(int attr);
+int curs_puttext(int sx, int sy, int ex, int ey, unsigned char *fill);
+int curs_gettext(int sx, int sy, int ex, int ey, unsigned char *fill);
+void curs_textattr(unsigned char attr);
 int curs_kbhit(void);
 void curs_delay(long msec);
 int curs_wherey(void);
 int curs_wherex(void);
 int _putch(unsigned char ch, BOOL refresh_now);
-int curs_putch(int ch);
+int curs_putch(unsigned char ch);
 void curs_gotoxy(int x, int y);
-int curs_initciolib(long inmode);
+void curs_initciowrap(long inmode);
 void curs_gettextinfo(struct text_info *info);
 void curs_setcursortype(int type);
 int curs_getch(void);
 int curs_getche(void);
 void curs_textmode(int mode);
-int curs_getmouse(struct cio_mouse_event *mevent);
-int curs_showmouse(void);
-int curs_hidemouse(void);
 #ifdef __cplusplus
 }
 #endif
+
+#define UIFC_IBM	(1<<4)  /* Force use of IBM charset     */
 
 #endif
