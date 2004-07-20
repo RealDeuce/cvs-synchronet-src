b4463434227ee6bf6610d4c175b54eea3d2ac39e
rswindell <rswindell>
2004-07-20 23:25:15 +0000

    Updated copyright date in comment header.

--- xpdev/dirwrap.c
+++ xpdev/dirwrap.c
@@ -2,13 +2,13 @@
 
 /* Directory-related system-call wrappers */
 
-/* $Id: dirwrap.c,v 1.39 2004/06/03 21:59:57 rswindell Exp $ */
+/* $Id: dirwrap.c,v 1.40 2004/07/20 23:25:15 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2002 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
