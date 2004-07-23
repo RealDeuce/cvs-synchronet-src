1970a2e9f425298c085e04d58c86cc4fcf69cf57
deuce <deuce>
2004-07-23 01:37:38 +0000

    uifc.h defines WIN_EXTKEYS, not WIN_EXTKEY

--- sbbs3/umonitor/chat.h
+++ sbbs3/umonitor/chat.h
@@ -2,7 +2,7 @@
 
 /* Synchronet for *nix sysop chat routines */
 
-/* $Id: chat.h,v 1.4 2004/08/09 06:27:41 deuce Exp $ */
+/* $Id: chat.h,v 1.2 2003/05/16 02:20:37 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -35,8 +35,10 @@
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
 
+#include <curses.h>
+
 typedef struct {
-	char ls, rs, ts, bs, tl, tr, bl, br;
+	chtype ls, rs, ts, bs, tl, tr, bl, br;
 } box_t;
 
 int chat(scfg_t *cfg, int nodenum, node_t *node, box_t *boxch, void(*timecallback)(void));
