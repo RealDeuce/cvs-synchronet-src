b0027b897c42ac639eba6c67a5cefe1e13b9d02d
deuce <deuce>
2004-07-27 09:11:03 +0000

    Switch to CIOLIB_MODE_CURSES_IBM

--- sbbs3/ctrl/makelibs.bat
+++ sbbs3/ctrl/makelibs.bat
@@ -1,9 +1,7 @@
 @echo off
 setlocal
-if exist ..\bcc.win32.dll.debug 	set dllsrc=..\bcc.win32.dll.debug
-if exist ..\bcc.win32.dll.release 	set dllsrc=..\bcc.win32.dll.release
-if exist ..\msvc.win32.dll.debug	set dllsrc=..\msvc.win32.dll.debug
-if exist ..\msvc.win32.dll.release	set dllsrc=..\msvc.win32.dll.release
+rem set dllsrc=..\bcc.win32.dll.release
+set dllsrc=..\msvc.win32.dll.debug
 implib sbbs.lib 	%dllsrc%\sbbs.dll
 implib mailsrvr.lib 	%dllsrc%\mailsrvr.dll
 implib ftpsrvr.lib 	%dllsrc%\ftpsrvr.dll
