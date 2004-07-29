5f906e981a561b65cc11eb550056d4c35e8e092e
deuce <deuce>
2004-07-29 21:08:38 +0000

    Remove redundant clean target

--- conio/curs_cio.h
+++ conio/curs_cio.h
@@ -1,24 +1,26 @@
-/* $Id: curs_cio.h,v 1.1 2004/07/05 21:27:40 deuce Exp $ */
+/* $Id: curs_cio.h,v 1.6 2004/07/26 23:56:35 deuce Exp $ */
 
 #ifdef __unix__
-#include "conio.h"
+#include "ciolib.h"
+#undef getch
+#undef ungetch
 #include "curs_fix.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 short curses_color(short color);
-int curs_puttext(int sx, int sy, int ex, int ey, unsigned char *fill);
-int curs_gettext(int sx, int sy, int ex, int ey, unsigned char *fill);
-void curs_textattr(unsigned char attr);
+int curs_puttext(int sx, int sy, int ex, int ey, void *fill);
+int curs_gettext(int sx, int sy, int ex, int ey, void *fill);
+void curs_textattr(int attr);
 int curs_kbhit(void);
 void curs_delay(long msec);
 int curs_wherey(void);
 int curs_wherex(void);
 int _putch(unsigned char ch, BOOL refresh_now);
-int curs_putch(unsigned char ch);
+int curs_putch(int ch);
 void curs_gotoxy(int x, int y);
-void curs_initciowrap(long inmode);
+int curs_initciolib(long inmode);
 void curs_gettextinfo(struct text_info *info);
 void curs_setcursortype(int type);
 int curs_getch(void);
@@ -27,7 +29,5 @@ void curs_textmode(int mode);
 #ifdef __cplusplus
 }
 #endif
-
-#define UIFC_IBM	(1<<4)  /* Force use of IBM charset     */
 
 #endif
--- xpdev/csv_file.h
+++ xpdev/csv_file.h
@@ -1,8 +1,8 @@
-/* conwrap.h */
+/* csv_file.h */
 
-/* Cross-platform local console I/O wrapppers */
+/* Functions to deal with comma-separated value (CSV) files and lists */
 
-/* $Id: conwrap.h,v 1.4 2004/07/20 23:19:43 rswindell Exp $ */
+/* $Id: csv_file.h,v 1.2 2004/07/29 11:15:32 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -35,18 +35,21 @@
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
 
-#ifndef _CONWRAP_H
-#define _CONWRAP_H
+#ifndef _CSV_FILE_H
+#define _CSV_FILE_H
 
-#if defined(__unix__)
+#include "str_list.h"
 
-	int kbhit(void);
-	int getch(void);
-
-#else	/* DOS-Based */
-
-	#include <conio.h>
-
+#if defined(__cplusplus)
+extern "C" {
 #endif
 
-#endif	/* _CONWRAP_H */
+str_list_t	csvCreate(str_list_t* records, str_list_t columns);
+str_list_t*	csvParse(str_list_t records);
+str_list_t*	csvReadFile(FILE* fp, str_list_t* columns);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif	/* Don't add anything after this line */
