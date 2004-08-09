f10138e4c2063f5844cd6575f55bb3f41a83775c
rswindell <rswindell>
2004-08-09 23:51:59 +0000

    Fixed warnings reported by Microsoft C++.

--- xpdev/str_list.c
+++ xpdev/str_list.c
@@ -2,7 +2,7 @@
 
 /* Functions to deal with NULL-terminated string lists */
 
-/* $Id: str_list.c,v 1.18 2004/06/03 21:59:57 rswindell Exp $ */
+/* $Id: str_list.c,v 1.20 2004/07/28 10:05:20 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -145,6 +145,9 @@ char* strListReplace(const str_list_t list, size_t index, const char* str)
 	char*	buf;
 	size_t	count;
 
+	if(str==NULL)
+		return(NULL);
+
 	count = strListCount(list);
 
 	if(index==STR_LIST_LAST_INDEX && count)
@@ -162,9 +165,35 @@ char* strListReplace(const str_list_t list, size_t index, const char* str)
 	return(buf);
 }
 
+BOOL strListSwap(const str_list_t list, size_t index1, size_t index2)
+{
+	char*	tmp;
+	size_t	count;
+
+	count = strListCount(list);
+
+	if(index1==STR_LIST_LAST_INDEX && count)
+		index1 = count-1;
+
+	if(index2==STR_LIST_LAST_INDEX && count)
+		index2 = count-1;
+
+	if(index1 >= count || index2 >= count || index1 == index2)	
+		return(FALSE);	/* invalid index, do nothing */
+
+	tmp=list[index1];
+	list[index1]=list[index2];
+	list[index2]=tmp;
+
+	return(TRUE);
+}
+
 char* strListAppend(str_list_t* list, const char* str, size_t index)
 {
 	char* buf;
+
+	if(str==NULL)
+		return(NULL);
 
 	if((buf=(char*)malloc(strlen(str)+1))==NULL)
 		return(NULL);
@@ -193,6 +222,9 @@ char* strListInsert(str_list_t* list, const char* str, size_t index)
 {
 	char* buf;
 
+	if(str==NULL)
+		return(NULL);
+
 	if((buf=(char*)malloc(strlen(str)+1))==NULL)
 		return(NULL);
 
@@ -219,6 +251,9 @@ str_list_t strListSplit(str_list_t* lp, char* str, const char* delimit)
 	char*	token;
 	str_list_t	list;
 
+	if(str==NULL || delimit==NULL)
+		return(NULL);
+
 	if(lp==NULL) {
 		if((list = strListInit())==NULL)
 			return(0);
@@ -237,6 +272,9 @@ str_list_t strListSplit(str_list_t* lp, char* str, const char* delimit)
 str_list_t strListSplitCopy(str_list_t* list, const char* str, const char* delimit)
 {
 	char*	buf;
+
+	if(str==NULL)
+		return(NULL);
 
 	if((buf=(char*)malloc(strlen(str)+1))==NULL)
 		return(NULL);
