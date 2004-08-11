6cd527b331dadff9fdbfc1e2730daccd01a12804
deuce <deuce>
2004-08-11 00:40:24 +0000

    Turn cursor off while redrawing input line.

--- syncterm/menu.c
+++ syncterm/menu.c
@@ -1,5 +1,5 @@
 #include <uifc.h>
-#include <conio.h>
+#include <ciolib.h>
 #include <keys.h>
 
 #include "term.h"
@@ -7,6 +7,7 @@
 
 void viewscroll(void)
 {
+#if 0
 	int	top;
 	int key;
 	int i;
@@ -70,6 +71,7 @@ void viewscroll(void)
 	puttext(1,1,txtinfo.screenwidth,txtinfo.screenheight,buf);
 	free(buf);
 	return;
+#endif
 }
 
 int syncmenu(void)
