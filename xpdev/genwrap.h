aa0abfe91735ee604b7ec46bf761221efc49fe0f
rswindell <rswindell>
2004-08-11 00:28:14 +0000

    Now requires win32cio.obj.

--- xpdev/genwrap.h
+++ xpdev/genwrap.h
@@ -2,7 +2,7 @@
 
 /* General cross-platform development wrappers */
 
-/* $Id: genwrap.h,v 1.61 2004/07/20 01:41:28 rswindell Exp $ */
+/* $Id: genwrap.h,v 1.63 2004/08/04 03:45:50 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -274,13 +274,19 @@ DLLEXPORT char*		DLLCALL os_version(char *str);
 DLLEXPORT char*		DLLCALL	lastchar(const char* str);
 DLLEXPORT int		DLLCALL safe_snprintf(char *dst, size_t size, const char *fmt, ...);
 
-#if defined(_WIN32)
-#define		msclock()	clock()
-#define		MSCLOCKS_PER_SEC	CLOCKS_PER_SEC
+/* C string/char escape-sequence processing */
+DLLEXPORT char*		DLLCALL unescape_cstr(char* str);
+DLLEXPORT char		DLLCALL unescape_char_ptr(const char* str, char** endptr);
+DLLEXPORT char		DLLCALL unescape_char(char ch);
+
+#if !defined(__unix__)
+	#define		msclock()			clock()
+	#define		MSCLOCKS_PER_SEC	CLOCKS_PER_SEC
 #else
-#define		MSCLOCKS_PER_SEC	1000
-clock_t		msclock(void);
+	#define		MSCLOCKS_PER_SEC	1000
+	clock_t		msclock(void);
 #endif
+
 #if defined(__cplusplus)
 }
 #endif
