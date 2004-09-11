47c2cc56f253d1da0ffc08a8efc23b5a49dd77c1
rswindell <rswindell>
2004-09-11 08:30:59 +0000

    Added comment header block.

--- syncterm/objects.mk
+++ syncterm/objects.mk
@@ -1,7 +1,14 @@
-OBJS    =      $(MTOBJODIR)$(DIRSEP)syncterm$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)bbslist$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)uifcinit$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)rlogin$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)term$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)window$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)menu$(OFILE)
+OBJS    =      $(LIBODIR)/syncterm.$(OFILE) \
+                        $(LIBODIR)/bbslist.$(OFILE) \
+                        $(LIBODIR)/uifcinit.$(OFILE) \
+                        $(LIBODIR)/rlogin.$(OFILE) \
+                        $(LIBODIR)/sockwrap.$(OFILE) \
+                        $(LIBODIR)/dirwrap.$(OFILE) \
+                        $(LIBODIR)/filewrap.$(OFILE) \
+                        $(LIBODIR)/cterm.$(OFILE) \
+                        $(LIBODIR)/term.$(OFILE) \
+                        $(LIBODIR)/genwrap.$(OFILE) \
+                        $(LIBODIR)/ini_file.$(OFILE) \
+                        $(LIBODIR)/str_list.$(OFILE) \
+                        $(LIBODIR)/window.$(OFILE) \
+                        $(LIBODIR)/menu.$(OFILE)
