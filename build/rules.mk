ba1f7e1c1677eb261471f2a8f07ef53710c0c547
rswindell <rswindell>
2004-09-11 10:56:48 +0000

    XPDEV(_SRC) is now defined in build/Common.bmake .path.c is now defined in build/Common.bmake Continue to build library even if delete fails (file doesn't exist).

--- build/rules.mk
+++ build/rules.mk
@@ -2,13 +2,10 @@
 #
 # Global build targets for all make systems
 #
-# $Id: rules.mk,v 1.5 2004/09/13 02:54:26 rswindell Exp $
+# $Id: rules.mk,v 1.3 2004/09/11 08:30:59 rswindell Exp $
 
 $(OBJODIR):
 	$(QUIET)$(IFNOTEXIST) mkdir $(OBJODIR)
-
-$(MTOBJODIR):
-	$(QUIET)$(IFNOTEXIST) mkdir $(MTOBJODIR)
 
 $(LIBODIR):
 	$(QUIET)$(IFNOTEXIST) mkdir $(LIBODIR)
@@ -18,10 +15,8 @@ $(EXEODIR):
 
 clean:
 	@echo Deleting $(OBJODIR)$(DIRSEP)
-	-$(QUIET)$(DELETE) $(OBJODIR)$(DIRSEP)*
-	@echo Deleting $(MTOBJODIR)$(DIRSEP)
-	-$(QUIET)$(DELETE) $(MTOBJODIR)$(DIRSEP)*
+	$(QUIET)$(DELETE) $(OBJODIR)$(DIRSEP)*
 	@echo Deleting $(LIBODIR)$(DIRSEP)
-	-$(QUIET)$(DELETE) $(LIBODIR)$(DIRSEP)*
+	$(QUIET)$(DELETE) $(LIBODIR)$(DIRSEP)*
 	@echo Deleting $(EXEODIR)$(DIRSEP)
-	-$(QUIET)$(DELETE) $(EXEODIR)$(DIRSEP)*
+	$(QUIET)$(DELETE) $(EXEODIR)$(DIRSEP)*
