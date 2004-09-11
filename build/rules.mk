7e98a97b0d8b9e133e54df100a7ec69ac901fdcf
rswindell <rswindell>
2004-09-11 08:30:59 +0000

    Added comment header block.

--- build/rules.mk
+++ build/rules.mk
@@ -1,3 +1,9 @@
+# build/rules.mk
+#
+# Global build targets for all make systems
+#
+# $Id: rules.mk,v 1.3 2004/09/11 08:30:59 rswindell Exp $
+
 $(OBJODIR):
 	$(QUIET)$(IFNOTEXIST) mkdir $(OBJODIR)
 
