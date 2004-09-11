486996d9235e43f4502b8e849f97f51999c7ef99
deuce <deuce>
2004-09-11 07:38:30 +0000

    Initial outline of common build system (gmake only)

--- sbbs3/scfglib1.c
+++ sbbs3/scfglib1.c
@@ -2,13 +2,13 @@
 
 /* Synchronet configuration library routines */
 
-/* $Id: scfglib1.c,v 1.54 2004/05/30 06:47:53 deuce Exp $ */
+/* $Id: scfglib1.c,v 1.55 2004/09/11 03:36:18 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
