a5597822bdbaff973b455f7ed384c3538490baa4
rswindell <rswindell>
2004-09-11 10:55:09 +0000

    Added IFNOEXIST definition. Fixed make directives (! must begin line for Borland make). Added path.c/cpp paths. Removed libobjs.mk (for now).

--- smblib/smbdefs.h
+++ smblib/smbdefs.h
@@ -2,7 +2,7 @@
 
 /* Synchronet message base constant and structure definitions */
 
-/* $Id: smbdefs.h,v 1.54 2004/09/03 01:34:03 rswindell Exp $ */
+/* $Id: smbdefs.h,v 1.56 2004/09/10 01:38:23 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -131,6 +131,7 @@
 #define SMB_ERR_HDR_FIELD	-105		/* Missing header field */
 #define SMB_ERR_NOT_FOUND	-110		/* Item not found */
 #define SMB_ERR_DAT_OFFSET	-120		/* Invalid data offset (>2GB) */
+#define SMB_ERR_DAT_LEN		-121		/* Invalid data length (>2GB) */
 #define SMB_ERR_OPEN		-200		/* File open error */
 #define SMB_ERR_SEEK		-201		/* File seek/setpos error */
 #define SMB_ERR_LOCK		-202		/* File lock error */
@@ -552,7 +553,7 @@ typedef struct _PACK {		/* FidoNet address (zone:net/node.point) */
 #pragma pack(pop)		/* original packing */
 #endif
 
-typedef struct _PACK {		/* Network (type and address) */
+typedef struct {		/* Network (type and address) */
 
     ushort  type;
 	void	*addr;
