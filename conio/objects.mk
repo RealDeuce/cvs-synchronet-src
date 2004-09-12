d9fb772d1b3201bc97447d88714cf72b63d92687
deuce <deuce>
2004-09-12 23:54:00 +0000

    Create mtlib target (No single-threaded ciolib can exist currently)

--- conio/objects.mk
+++ conio/objects.mk
@@ -2,11 +2,11 @@
 
 # Make 'include file' listing object files for SMBLIB
 
-# $Id: objects.mk,v 1.1 2004/09/12 12:06:47 deuce Exp $
+# $Id: objects.mk,v 1.2 2004/09/12 23:54:00 deuce Exp $
 
 # OBJODIR, DIRSEP, and OFILE must be pre-defined
 
-OBJS	=	$(OBJODIR)$(DIRSEP)ansi_cio$(OFILE)\
-			$(OBJODIR)$(DIRSEP)ciolib$(OFILE)\
-			$(OBJODIR)$(DIRSEP)cterm$(OFILE)\
-			$(OBJODIR)$(DIRSEP)mouse$(OFILE)
+OBJS	=	$(MTOBJODIR)$(DIRSEP)ansi_cio$(OFILE)\
+			$(MTOBJODIR)$(DIRSEP)ciolib$(OFILE)\
+			$(MTOBJODIR)$(DIRSEP)cterm$(OFILE)\
+			$(MTOBJODIR)$(DIRSEP)mouse$(OFILE)
