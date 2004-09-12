f910fc8abff031b9f4bd22a25ffb541e8e240ab1
deuce <deuce>
2004-09-12 23:25:11 +0000

    Change PTHREAD_* to MT_*

--- sbbs3/install/objects.mk
+++ sbbs3/install/objects.mk
@@ -2,10 +2,15 @@
 
 # Make 'include file' listing object files for Synchronet SBBSINST
 
-# $Id: objects.mk,v 1.6 2004/09/13 22:57:09 deuce Exp $
+# $Id: objects.mk,v 1.5 2004/03/23 23:55:30 deuce Exp $
 
-# MTOBJODIR, DIRSEP, and OFILE must be pre-defined
+# LIBODIR, SBBSLIBODIR, SLASH, and OFILE must be pre-defined
 
 
-OBJS	=	$(MTOBJODIR)$(DIRSEP)sbbsinst$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)httpio$(OFILE)
+OBJS	=	$(LIBODIR)$(SLASH)sbbsinst.$(OFILE)\
+			$(LIBODIR)$(SLASH)sockwrap.$(OFILE)\
+			$(LIBODIR)$(SLASH)genwrap.$(OFILE)\
+			$(LIBODIR)$(SLASH)dirwrap.$(OFILE)\
+			$(LIBODIR)$(SLASH)filewrap.$(OFILE)\
+			$(LIBODIR)$(SLASH)httpio.$(OFILE)\
+			$(LIBODIR)$(SLASH)uifcx.$(OFILE)
