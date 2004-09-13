875485f53d4ed7fa67048ec21d51eb94dad26055
deuce <deuce>
2004-09-13 01:40:55 +0000

    Fix typo

--- sbbs3/scfg/objects.mk
+++ sbbs3/scfg/objects.mk
@@ -2,7 +2,7 @@
 
 # Make 'include file' listing object files for Synchronet SCFG
 
-# $Id: objects.mk,v 1.12 2004/08/30 23:30:30 rswindell Exp $
+# $Id: objects.mk,v 1.15 2004/09/11 00:22:03 rswindell Exp $
 
 # LIBODIR, SBBSLIBODIR, SLASH, and OFILE must be pre-defined
 
@@ -26,6 +26,7 @@ OBJS	=	$(LIBODIR)$(SLASH)scfg.$(OFILE)\
             $(LIBODIR)$(SLASH)crc16.$(OFILE)\
             $(LIBODIR)$(SLASH)crc32.$(OFILE)\
             $(LIBODIR)$(SLASH)md5.$(OFILE)\
+            $(LIBODIR)$(SLASH)lzh.$(OFILE)\
             $(LIBODIR)$(SLASH)dat_rec.$(OFILE)\
             $(LIBODIR)$(SLASH)userdat.$(OFILE)\
             $(LIBODIR)$(SLASH)date_str.$(OFILE)\
