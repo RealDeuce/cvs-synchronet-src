a351cd816b712158abea493090a2fd858939959e
deuce <deuce>
2004-09-13 00:11:22 +0000

    Errant usage of CONIO_SRC (Should bee CIOLIB_SRC)

--- smblib/targets.mk
+++ smblib/targets.mk
@@ -1,2 +1,6 @@
 # smblib/targets.mk
-lib: $(OBJODIR) $(LIBODIR) $(SMBLIB)
+SMBLIB	=	$(LIBODIR)$(DIRSEP)$(LIBPREFIX)smb$(LIBFILE)
+default: lib mtlib
+
+lib:	$(LIBODIR) $(SMBLIB)
+mtlib:	$(LIBODIR) $(SMBLIB-MT)
