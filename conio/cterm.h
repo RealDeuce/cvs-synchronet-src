b951f979f3cb5187d7e02d79531912fc28ff42eb
deuce <deuce>
2004-09-13 01:19:09 +0000

    Add _LIBS macro.

--- conio/cterm.h
+++ conio/cterm.h
@@ -1,32 +1,9 @@
-#ifndef _CTERM_H_
-#define _CTERM_H_
-
-struct cterminal {
-	int	height;
-	int	width;
-	int	x;
-	int	y;
-	char *buffer;
-	int	attr;
-	int save_xpos;
-	int save_ypos;
-	char	escbuf[1024];
-	int	sequence;
-	char	musicbuf[1024];
-	int music;
-	char *scrollback;
-	int backpos;
-	int backlines;
-	int	xpos;
-	int ypos;
-};
+#ifndef _TERM_H_
+#define _TERM_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-extern struct cterminal cterm;
-
 void cterm_init(int height, int width, int xpos, int ypos, int backlines, unsigned char *scrollback);
 char *cterm_write(unsigned char *buf, int buflen, char *retbuf, int retsize);
 void cterm_end(void);
--- sbbs3/scfg/targets.mk
+++ sbbs3/scfg/targets.mk
@@ -2,21 +2,14 @@
 
 # Make 'include file' defining targets for Synchronet SCFG project
 
-# $Id: targets.mk,v 1.6 2004/09/16 19:02:03 deuce Exp $
+# $Id: targets.mk,v 1.4 2004/03/11 06:26:00 deuce Exp $
 
 # LIBODIR, SLASH, LIBFILE, EXEFILE, and DELETE must be pre-defined
 
-SCFG	=	$(EXEODIR)$(DIRSEP)scfg$(EXEFILE) 
-MAKEHELP=	$(EXEODIR)$(DIRSEP)makehelp$(EXEFILE) 
-SCFGHELP=	$(EXEODIR)$(DIRSEP)scfghelp.dat
+SCFG	=	$(EXEODIR)$(SLASH)scfg$(EXEFILE) 
+MAKEHELP=	$(EXEODIR)$(SLASH)makehelp$(EXEFILE) 
+SCFGHELP=	$(EXEODIR)$(SLASH)scfghelp.dat
 
-all:		xpdev-mt \
-		uifc-mt \
-		ciolib-mt \
-		smblib \
-		$(EXEODIR) \
-		$(MTOBJODIR) \
+all:	$(EXEODIR) \
 		$(LIBODIR) \
 		$(SCFG) $(SCFGHELP)
-
-$(SCFG):	$(XPDEV-MT_LIB) $(UIFCLIB-MT) $(CIOLIB-MT)
