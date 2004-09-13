25cd00899e2e4ff2719144ac471823882cba770c
deuce <deuce>
2004-09-13 22:57:09 +0000

    New build system

--- build/rules.mk
+++ build/rules.mk
@@ -1,5 +1,14 @@
+# build/rules.mk
+#
+# Global build targets for all make systems
+#
+# $Id: rules.mk,v 1.5 2004/09/13 02:54:26 rswindell Exp $
+
 $(OBJODIR):
 	$(QUIET)$(IFNOTEXIST) mkdir $(OBJODIR)
+
+$(MTOBJODIR):
+	$(QUIET)$(IFNOTEXIST) mkdir $(MTOBJODIR)
 
 $(LIBODIR):
 	$(QUIET)$(IFNOTEXIST) mkdir $(LIBODIR)
@@ -8,9 +17,11 @@ $(EXEODIR):
 	$(QUIET)$(IFNOTEXIST) mkdir $(EXEODIR)
 
 clean:
-	@echo Deleting $(OBJODIR)$(SLASH)
-	$(QUIET)$(DELETE) $(OBJODIR)$(SLASH)*
-	@echo Deleting $(LIBODIR)$(SLASH)
-	$(QUIET)$(DELETE) $(LIBODIR)$(SLASH)*
-	@echo Deleting $(EXEODIR)$(SLASH)
-	$(QUIET)$(DELETE) $(EXEODIR)$(SLASH)*
+	@echo Deleting $(OBJODIR)$(DIRSEP)
+	-$(QUIET)$(DELETE) $(OBJODIR)$(DIRSEP)*
+	@echo Deleting $(MTOBJODIR)$(DIRSEP)
+	-$(QUIET)$(DELETE) $(MTOBJODIR)$(DIRSEP)*
+	@echo Deleting $(LIBODIR)$(DIRSEP)
+	-$(QUIET)$(DELETE) $(LIBODIR)$(DIRSEP)*
+	@echo Deleting $(EXEODIR)$(DIRSEP)
+	-$(QUIET)$(DELETE) $(EXEODIR)$(DIRSEP)*
---
+++ conio/objects.mk
@@ -0,0 +1,4 @@
+OBJS	=	$(MTOBJODIR)$(DIRSEP)ansi_cio$(OFILE)\
+			$(MTOBJODIR)$(DIRSEP)ciolib$(OFILE)\
+			$(MTOBJODIR)$(DIRSEP)cterm$(OFILE)\
+			$(MTOBJODIR)$(DIRSEP)mouse$(OFILE)
