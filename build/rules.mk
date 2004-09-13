f5774e4bc24d4418eda5891c754086487a5447d8
deuce <deuce>
2004-09-13 23:07:25 +0000

    New make system!

--- build/rules.mk
+++ build/rules.mk
@@ -1,5 +1,14 @@
+# build/rules.mk
+#
+# Global build targets for all make systems
+#
+# $Id: rules.mk,v 1.5 2004/09/13 02:54:26 rswindell Exp $
+
 $(OBJODIR):
 	$(QUIET)$(IFNOTEXIST) mkdir $(OBJODIR)
+
+$(MTOBJODIR):
+	$(QUIET)$(IFNOTEXIST) mkdir $(MTOBJODIR)
 
 $(LIBODIR):
 	$(QUIET)$(IFNOTEXIST) mkdir $(LIBODIR)
@@ -9,8 +18,10 @@ $(EXEODIR):
 
 clean:
 	@echo Deleting $(OBJODIR)$(DIRSEP)
-	$(QUIET)$(DELETE) $(OBJODIR)$(DIRSEP)*
+	-$(QUIET)$(DELETE) $(OBJODIR)$(DIRSEP)*
+	@echo Deleting $(MTOBJODIR)$(DIRSEP)
+	-$(QUIET)$(DELETE) $(MTOBJODIR)$(DIRSEP)*
 	@echo Deleting $(LIBODIR)$(DIRSEP)
-	$(QUIET)$(DELETE) $(LIBODIR)$(DIRSEP)*
+	-$(QUIET)$(DELETE) $(LIBODIR)$(DIRSEP)*
 	@echo Deleting $(EXEODIR)$(DIRSEP)
-	$(QUIET)$(DELETE) $(EXEODIR)$(DIRSEP)*
+	-$(QUIET)$(DELETE) $(EXEODIR)$(DIRSEP)*
