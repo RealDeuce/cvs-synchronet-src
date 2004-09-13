8aa5f699ba0abc29adef70e064eeabd33db82a79
deuce <deuce>
2004-09-13 03:27:59 +0000

    Add _LIBS macros.

--- uifc/uifc32.c
+++ uifc/uifc32.c
@@ -2,7 +2,7 @@
 
 /* Curses implementation of UIFC (user interface) library based on uifc.c */
 
-/* $Id: uifc32.c,v 1.97 2004/09/20 05:40:09 deuce Exp $ */
+/* $Id: uifc32.c,v 1.92 2004/09/11 02:05:10 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -221,14 +221,16 @@ int uifcini32(uifcapi_t* uifcapi)
     clrscr();
 
     gettextinfo(&txtinfo);
+#ifdef _WIN32
     /* unsupported mode? */
     if(txtinfo.screenheight<MIN_LINES
 /*        || txtinfo.screenheight>MAX_LINES */
-        || txtinfo.screenwidth<40) {
+        || txtinfo.screenwidth<80) {
         textmode(C80);  /* set mode to 80x25*/
         gettextinfo(&txtinfo);
     }
 
+#endif
 
     api->scrn_len=txtinfo.screenheight;
     if(api->scrn_len<MIN_LINES) {
@@ -238,8 +240,8 @@ int uifcini32(uifcapi_t* uifcapi)
     }
     api->scrn_len--; /* account for status line */
 
-    if(txtinfo.screenwidth<40) {
-        cprintf("\7UIFC: Screen width (%u) must be at least 40 characters\r\n"
+    if(txtinfo.screenwidth<80) {
+        cprintf("\7UIFC: Screen width (%u) must be at least 80 characters\r\n"
             ,txtinfo.screenwidth);
         return(-3);
     }
@@ -562,7 +564,7 @@ int ulist(int mode, int left, int top, int width, int *cur, int *bar
 			*(ptr++)='[';
 			*(ptr++)=hclr|(bclr<<4);
 			/* *(ptr++)='þ'; */
-			*(ptr++)=0xfe;
+			*(ptr++)='X';
 			*(ptr++)=lclr|(bclr<<4);
 			*(ptr++)=']';
 			*(ptr++)=hclr|(bclr<<4);
@@ -830,13 +832,61 @@ int ulist(int mode, int left, int top, int width, int *cur, int *bar
 					if(mevnt.startx==s_left+left+1
 							&& mevnt.starty==s_top+top+3
 							&& mevnt.event==CIOLIB_BUTTON_1_CLICK) {
-						i=CIO_KEY_PPAGE;
+						if(!opts)
+							continue;
+						*cur -= (height-5);
+						if(*cur<0)
+							*cur = 0;
+						if(bar)
+							*bar=0;
+						y=s_top+top;
+						gotoxy(s_left+left+1,s_top+top+3);
+						textattr(lclr|(bclr<<4));
+						if(*cur && opts>height-3)  /* Scroll mode */
+							putch(30);	   /* put the up arrow */
+						else
+							putch(' ');    /* delete the up arrow */
+						gotoxy(s_left+left+1,s_top+top+height-2);
+						if(opts > height-3 && *cur + height - 4 < opts)
+							putch(31);	   /* put the down arrow */
+						else
+							putch(' ');    /* delete the down arrow */
+						for(i=*cur,j=0;i<=*cur-5+height;i++,j++)
+							uprintf(s_left+left+3,s_top+top+3+j
+								,i==*cur ? lbclr
+									: lclr|(bclr<<4)
+								,"%-*.*s",width-4,width-4,option[i]);
+						continue;
 					}
 					/* Clicked Scroll Down */
 					if(mevnt.startx==s_left+left+1
 							&& mevnt.starty==(s_top+top+height)-2
 							&& mevnt.event==CIOLIB_BUTTON_1_CLICK) {
-						i=CIO_KEY_NPAGE;
+						if(!opts)
+							continue;
+						*cur += (height-5);
+						if(*cur>opts-1)
+							*cur = opts-1;
+						if(bar)
+							*bar = height-5;
+						y=height-5+s_top+top;
+						gotoxy(s_left+left+1,s_top+top+3);
+						textattr(lclr|(bclr<<4));
+						if(*cur>height-5)  /* Scroll mode */
+							putch(30);	   /* put the up arrow */
+						else
+							putch(' ');    /* delete the up arrow */
+						gotoxy(s_left+left+1,s_top+top+height-2);
+						if(*cur < opts-1)
+							putch(31);	   /* put the down arrow */
+						else
+							putch(' ');    /* delete the down arrow */
+						for(i=*cur+5-height,j=0;i<=*cur;i++,j++)
+							uprintf(s_left+left+3,s_top+top+3+j
+								,i==*cur ? lbclr
+									: lclr|(bclr<<4)
+								,"%-*.*s",width-4,width-4,option[i]);
+						continue;
 					}
 				}
 			}
@@ -1309,8 +1359,7 @@ int ulist(int mode, int left, int top, int width, int *cur, int *bar
 							}
 							return(-1);
 						default:
-							if(mode&WIN_EXTKEYS)
-								return(-2-i);
+							return(-2-i);
 				}
 			}
 		}
@@ -1328,7 +1377,7 @@ int ulist(int mode, int left, int top, int width, int *cur, int *bar
 /*************************************************************************/
 /* This function is a windowed input string input routine.               */
 /*************************************************************************/
-int uinput(int mode, int left, int top, char *inprompt, char *str,
+int uinput(int mode, int left, int top, char *prompt, char *str,
 	int max, int kmode)
 {
 	unsigned char save_buf[2048],in_win[2048]
@@ -1337,11 +1386,9 @@ int uinput(int mode, int left, int top, char *inprompt, char *str,
 	int height=3;
 	int i,plen,slen,j;
 	int	iwidth;
-	char *prompt;
 
 	reset_dynamic();
-	plen=strlen(inprompt);
-	prompt=strdup(inprompt);
+	plen=strlen(prompt);
 	if(!plen)
 		slen=4;
 	else
@@ -1361,16 +1408,6 @@ int uinput(int mode, int left, int top, char *inprompt, char *str,
 		gettext(SCRN_LEFT+left,SCRN_TOP+top,SCRN_LEFT+left+width+1
 			,SCRN_TOP+top+height,save_buf);
 	iwidth=width-plen-slen;
-	while(iwidth<1 && plen>4) {
-		plen=strlen(prompt);
-		prompt[plen-1]=0;
-		prompt[plen-2]='.';
-		prompt[plen-3]='.';
-		prompt[plen-4]='.';
-		plen--;
-		iwidth=width-plen-slen;
-	}
-
 	i=0;
 	in_win[i++]='É';
 	in_win[i++]=hclr|(bclr<<4);
@@ -1382,7 +1419,7 @@ int uinput(int mode, int left, int top, char *inprompt, char *str,
 		in_win[2]='[';
 		in_win[3]=hclr|(bclr<<4);
 		/* in_win[4]='þ'; */
-		in_win[4]=0xfe;
+		in_win[4]='X';
 		in_win[5]=lclr|(bclr<<4);
 		in_win[6]=']';
 		in_win[7]=hclr|(bclr<<4);
@@ -1460,7 +1497,6 @@ int uinput(int mode, int left, int top, char *inprompt, char *str,
 	if(mode&WIN_SAV)
 		puttext(SCRN_LEFT+left,SCRN_TOP+top,SCRN_LEFT+left+width+1
 			,SCRN_TOP+top+height,save_buf);
-	free(prompt);
 	return(i);
 }
 
@@ -2028,7 +2064,7 @@ void showbuf(int mode, int left, int top, int width, int height, char *title, ch
 			tmp_buffer2[2]='[';
 			tmp_buffer2[3]=hclr|(bclr<<4);
 			/* tmp_buffer2[4]='þ'; */
-			tmp_buffer2[4]=0xfe;
+			tmp_buffer2[4]='X';
 			tmp_buffer2[5]=lclr|(bclr<<4);
 			tmp_buffer2[6]=']';
 			tmp_buffer2[7]=hclr|(bclr<<4);
