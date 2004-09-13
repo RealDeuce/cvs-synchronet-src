d8ddd9ee2473192cda9e7caa063a570115d8d299
rswindell <rswindell>
2004-09-13 06:00:21 +0000

    truncsp is part of genwrap.c (removing).

--- syncterm/uifcinit.c
+++ syncterm/uifcinit.c
@@ -1,4 +1,3 @@
-#include <gen_defs.h>
 #include <stdio.h>
 
 #include <ciolib.h>
@@ -16,8 +15,6 @@ int	init_uifc(void) {
 		return(0);
 	uifc.size=sizeof(uifc);
 	uifc.mode=UIFC_IBM|UIFC_COLOR;
-	if(uifc.scrn_len)
-		uifc.scrn_len++;
 	if((i=uifcini32(&uifc))!=0) {
 		fprintf(stderr,"uifc library init returned error %d\n",i);
 		return(-1);
