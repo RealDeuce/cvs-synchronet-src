23bb146aa668136e6a8d3265e83581995832a5ea
deuce <deuce>
2004-09-13 02:26:44 +0000

    Clean up .path.*

--- xpdev/objects.mk
+++ xpdev/objects.mk
@@ -2,7 +2,7 @@
 
 # Make 'include file' listing object files for xpdev "wrappers"
 
-# $Id: objects.mk,v 1.6 2004/09/13 06:00:21 rswindell Exp $
+# $Id: objects.mk,v 1.5 2004/09/13 00:13:47 deuce Exp $
 
 # OBJODIR, SLASH, and OFILE must be pre-defined
 
@@ -15,7 +15,8 @@ OBJS	= \
 	$(OBJODIR)$(DIRSEP)ini_file$(OFILE) \
 	$(OBJODIR)$(DIRSEP)link_list$(OFILE) \
 	$(OBJODIR)$(DIRSEP)sockwrap$(OFILE) \
-	$(OBJODIR)$(DIRSEP)str_list$(OFILE)
+	$(OBJODIR)$(DIRSEP)str_list$(OFILE) \
+	$(OBJODIR)$(DIRSEP)truncsp$(OFILE)
 
 MTOBJS	= \
 	$(MTOBJODIR)$(DIRSEP)conwrap$(OFILE) \
@@ -28,4 +29,5 @@ MTOBJS	= \
 	$(MTOBJODIR)$(DIRSEP)semwrap$(OFILE) \
 	$(MTOBJODIR)$(DIRSEP)sockwrap$(OFILE) \
 	$(MTOBJODIR)$(DIRSEP)str_list$(OFILE) \
-	$(MTOBJODIR)$(DIRSEP)threadwrap$(OFILE)
+	$(MTOBJODIR)$(DIRSEP)threadwrap$(OFILE) \
+	$(MTOBJODIR)$(DIRSEP)truncsp$(OFILE)
