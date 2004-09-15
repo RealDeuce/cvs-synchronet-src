ad3bc23b8abce499ec32f5ccb38ff843d34ad630
deuce <deuce>
2004-09-15 06:39:02 +0000

    Use $(OFILE) in Common.gmake Diddle .bmake some more to make implicit rules work better...

--- conio/ansi_cio.c
+++ conio/ansi_cio.c
@@ -1,43 +1,9 @@
-/* $Id: ansi_cio.c,v 1.33 2004/09/22 04:03:06 deuce Exp $ */
-
-/****************************************************************************
- * @format.tab-size 4		(Plain Text/Source Code File Header)			*
- * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
- *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
- *																			*
- * This library is free software; you can redistribute it and/or			*
- * modify it under the terms of the GNU Lesser General Public License		*
- * as published by the Free Software Foundation; either version 2			*
- * of the License, or (at your option) any later version.					*
- * See the GNU Lesser General Public License for more details: lgpl.txt or	*
- * http://www.fsf.org/copyleft/lesser.html									*
- *																			*
- * Anonymous FTP access to the most recent released source is available at	*
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
- *																			*
- * Anonymous CVS access to the development source and modification history	*
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
- *     (just hit return, no password is necessary)							*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
- *																			*
- * For Synchronet coding style and modification guidelines, see				*
- * http://www.synchro.net/source.html										*
- *																			*
- * You are encouraged to submit any modifications (preferably in Unix diff	*
- * format) via e-mail to mods@synchro.net									*
- *																			*
- * Note: If this box doesn't appear square, then you need to fix your tabs.	*
- ****************************************************************************/
-
 #include <fcntl.h>
 #include <stdarg.h>
 #include <stdlib.h>	/* malloc */
 
 #include <genwrap.h>
 #include <threadwrap.h>
-#include <semwrap.h>
 
 #ifdef __unix__
 	#include <termios.h>
@@ -46,13 +12,6 @@
 
 #include "ciolib.h"
 #include "ansi_cio.h"
-
-#define	ANSI_TIMEOUT	500
-
-sem_t	got_key;
-sem_t	used_key;
-sem_t	got_input;
-sem_t	used_input;
 WORD	ansi_curr_attr=0x07<<8;
 
 int ansi_rows=24;
@@ -157,13 +116,15 @@ tODKeySequence aKeySequences[] =
    {"\033OD", ANSI_KEY_LEFT},
    {"\033OH", ANSI_KEY_HOME},
    {"\033OK", ANSI_KEY_END},
-
+   
    /* Terminator */
    {"",0}
 };
 
 void ansi_sendch(char ch)
 {
+	struct text_info ti;
+
 	if(!ch)
 		ch=' ';
 	if(ansi_row<ansi_rows-1 || (ansi_row==ansi_rows-1 && ansi_col<ansi_cols-1)) {
@@ -233,7 +194,7 @@ int ansi_puttext(int sx, int sy, int ex, int ey, void* buf)
 				textattr(sch>>8);
 				attrib=sch>>8;
 			}
-			ansi_sendch((char)(sch&0xff));
+			ansi_sendch(sch&0xff);
 		}
 	}
 
@@ -345,47 +306,42 @@ static void ansi_keyparse(void *par)
 	int		gotesc=0;
 	char	seq[64];
 	int		ch;
+	int		waited=0;
 	int		i;
 	char	*p;
-	int		timeout=0;
-	int		timedout=0;
 
-	seq[0]=0;
 	for(;;) {
-		timedout=0;
-		if(timeout) {
-			if(sem_trywait_block(&got_key,timeout)) {
+		while(!ansi_raw_inch
+				&& (gotesc || (!gotesc && !seq[0]))) {
+			waited++;
+			if(waited>=ansi_esc_delay) {
+				waited=0;
 				gotesc=0;
-				timeout=0;
-				timedout=1;
 			}
+			else
+				SLEEP(1);
 		}
-		else
-			sem_wait(&got_key);
-
-		if(timedout) {
-			for(p=seq;*p;p++) {
-				sem_wait(&used_input);
-				ansi_inch=*p;
-				sem_post(&got_input);
-			}
-			seq[0]=0;
+		if(!gotesc && seq[0]) {
+			while(ansi_inch)
+				SLEEP(1);
+			ch=seq[0];
+			for(p=seq;*p;*p=*(++p));
+			ansi_inch=ch;
 			continue;
 		}
 		else {
 			ch=ansi_raw_inch;
 			ansi_raw_inch=0;
-			sem_post(&used_key);
 		}
 		switch(gotesc) {
-			case 1:	/* Escape Sequence */
-				timeout=ANSI_TIMEOUT;
-				seq[strlen(seq)+1]=0;
-				seq[strlen(seq)]=ch;
+			case 1:	/* Escape */
+				waited=0;
 				if(strlen(seq)>=sizeof(seq)-2) {
 					gotesc=0;
 					break;
 				}
+				seq[strlen(seq)+1]=0;
+				seq[strlen(seq)]=ch;
 				if((ch<'0' || ch>'9')		/* End of ESC sequence */
 						&& ch!=';'
 						&& ch!='?'
@@ -393,19 +349,16 @@ static void ansi_keyparse(void *par)
 						&& (strlen(seq)==2?ch != 'O':1)) {
 					for(i=0;aKeySequences[i].pszSequence[0];i++) {
 						if(!strcmp(seq,aKeySequences[i].pszSequence)) {
+							gotesc=0;
 							seq[0]=0;
-							sem_wait(&used_input);
+							while(ansi_inch)
+								SLEEP(1);
 							ansi_inch=aKeySequences[i].chExtendedKey;
-							sem_post(&got_input);
-							/* Two-byte code, need to post twice and wait for one to
-							   be received */
-							sem_post(&got_input);
-							sem_wait(&used_input);
 							break;
 						}
 					}
-					gotesc=0;
-					timeout=0;
+					if(!aKeySequences[i].pszSequence[0])
+						gotesc=0;
 				}
 				break;
 			default:
@@ -413,12 +366,12 @@ static void ansi_keyparse(void *par)
 					seq[0]=27;
 					seq[1]=0;
 					gotesc=1;
-					timeout=ANSI_TIMEOUT;
+					waited=0;
 					break;
 				}
-				sem_wait(&used_input);
+				while(ansi_inch)
+					SLEEP(1);
 				ansi_inch=ch;
-				sem_post(&got_input);
 				break;
 		}
 	}
@@ -432,9 +385,10 @@ static void ansi_keythread(void *params)
 	_beginthread(ansi_keyparse,1024,NULL);
 
 	for(;;) {
-		sem_wait(&used_key);
-		if(fread(&ansi_raw_inch,1,1,stdin)==1)
-			sem_post(&got_key);
+		if(!ansi_raw_inch) {
+			if(read(fileno(stdin),&ansi_raw_inch,1)!=1)
+				ansi_raw_inch=0;
+		}
 		else
 			SLEEP(1);
 	}
@@ -442,10 +396,7 @@ static void ansi_keythread(void *params)
 
 int ansi_kbhit(void)
 {
-	int sval=1;
-
-	sem_getvalue(&got_input,&sval);
-	return(sval);
+	return(ansi_inch);
 }
 
 void ansi_delay(long msec)
@@ -470,6 +421,7 @@ int ansi_wherex(void)
 int ansi_putch(int ch)
 {
 	struct text_info ti;
+	WORD sch;
 	int i;
 	unsigned char buf[2];
 
@@ -539,6 +491,7 @@ int ansi_putch(int ch)
 
 void ansi_gotoxy(int x, int y)
 {
+	struct text_info ti;
 	char str[16];
 
 	if(x < 1
@@ -625,10 +578,10 @@ int ansi_getch(void)
 {
 	int ch;
 
-	sem_wait(&got_input);
+	while(!ansi_inch)
+		SLEEP(1);
 	ch=ansi_inch&0xff;
 	ansi_inch=ansi_inch>>8;
-	sem_post(&used_input);
 	return(ch);
 }
 
@@ -664,13 +617,6 @@ void ansi_fixterm(void)
 }
 #endif
 
-#ifndef ENABLE_EXTENDED_FLAGS
-#define ENABLE_INSERT_MODE		0x0020
-#define ENABLE_QUICK_EDIT_MODE	0x0040
-#define ENABLE_EXTENDED_FLAGS	0x0080
-#define ENABLE_AUTO_POSITION	0x0100
-#endif
-
 #if defined(__BORLANDC__)
         #pragma argsused
 #endif
@@ -680,18 +626,9 @@ int ansi_initciolib(long inmode)
 	char *init="\033[0m\033[2J\033[1;1H";
 
 #ifdef _WIN32
-	if(isatty(fileno(stdin))) {
-		if(!SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), 0))
-			return(0);
-
-		if(!SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), 0))
-			return(0);
-	}
-	else {
-		setmode(fileno(stdout),_O_BINARY);
-		setmode(fileno(stdin),_O_BINARY);
-		setvbuf(stdout, NULL, _IONBF, 0);
-	}
+	setmode(fileno(stdout),_O_BINARY);
+	setmode(fileno(stdin),_O_BINARY);
+	setvbuf(stdout, NULL, _IONBF, 0);
 #else
 	struct termios tio_raw;
 
@@ -704,13 +641,6 @@ int ansi_initciolib(long inmode)
 		atexit(ansi_fixterm);
 	}
 #endif
-
-	/* Initialize used_* to 1 so they can be immediately waited on */
-	sem_init(&got_key,0,0);
-	sem_init(&used_key,0,1);
-	sem_init(&got_input,0,0);
-	sem_init(&used_input,0,1);
-
 	vmem=(WORD *)malloc(ansi_rows*ansi_cols*sizeof(WORD));
 	ansi_sendstr(init,-1);
 	for(i=0;i<ansi_rows*ansi_cols;i++)
