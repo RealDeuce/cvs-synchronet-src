fa1ccfc7fafe652f3511cbc83cd8016834a95278
rswindell <rswindell>
2004-09-15 05:05:57 +0000

    When smb_addmsg() finds a duplicate message-ID, log the actual Message-ID.

--- sbbs3/uedit/targets.mk
+++ sbbs3/uedit/targets.mk
@@ -1,5 +1,3 @@
 UEDIT	=	$(EXEODIR)$(DIRSEP)uedit$(EXEFILE)
 
-all: smblib uifc-mt ciolib-mt xpdev-mt $(MTOBJODIR) $(EXEODIR) $(UEDIT)
-
-$(UEDIT):	$(SMBLIB) $(UIFCLIB-MT) $(CIOLIB-MT) $(XPDEV-MT_LIB)
+all: $(MTOBJODIR) $(EXEODIR) $(UEDIT)
