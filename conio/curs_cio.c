454772c8b08bc718e60771e68da1551183fc79cf
deuce <deuce>
2004-09-15 07:56:04 +0000

    Dor the command-line shuffle.

--- conio/curs_cio.c
+++ conio/curs_cio.c
@@ -1,49 +1,14 @@
-/* $Id: curs_cio.c,v 1.15 2004/09/22 21:55:32 rswindell Exp $ */
-
-/****************************************************************************
- * @format.tab-size 4		(Plain Text/Source Code File Header)			*
- * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
- *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
- *																			*
- * This library is free software; you can redistribute it and/or			*
- * modify it under the terms of the GNU Lesser General Public License		*
- * as published by the Free Software Foundation; either version 2			*
- * of the License, or (at your option) any later version.					*
- * See the GNU Lesser General Public License for more details: lgpl.txt or	*
- * http://www.fsf.org/copyleft/lesser.html									*
- *																			*
- * Anonymous FTP access to the most recent released source is available at	*
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
- *																			*
- * Anonymous CVS access to the development source and modification history	*
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
- *     (just hit return, no password is necessary)							*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
- *																			*
- * For Synchronet coding style and modification guidelines, see				*
- * http://www.synchro.net/source.html										*
- *																			*
- * You are encouraged to submit any modifications (preferably in Unix diff	*
- * format) via e-mail to mods@synchro.net									*
- *																			*
- * Note: If this box doesn't appear square, then you need to fix your tabs.	*
- ****************************************************************************/
-
+/* $Id: curs_cio.c,v 1.11 2004/09/10 23:24:49 deuce Exp $ */
 #include <sys/time.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#include "gen_defs.h"	/* xpdev, for BOOL/TRUE/FALSE */
-
 #include "ciolib.h"
 #include "curs_cio.h"
 #include "keys.h"
 #include "mouse.h"
-#include "vidmodes.h"
 
 static unsigned char curs_nextgetch=0;
 const int curs_tabs[10]={9,17,25,33,41,49,57,65,73,80};
@@ -1036,28 +1001,23 @@ int curs_getche(void)
 
 void curs_textmode(int mode)
 {
-	return;
 }
 
 int curs_hidemouse(void)
 {
-/*
 	#ifdef NCURSES_VERSION_MAJOR
 		mousemask(0,NULL);
 		return(0);
 	#else
 		return(-1);
 	#endif
-*/
 }
 
 int curs_showmouse(void)
 {
-/*
 	#ifdef NCURSES_VERSION_MAJOR
 		if(mousemask(BUTTON1_PRESSED|BUTTON1_RELEASED|BUTTON2_PRESSED|BUTTON2_RELEASED|BUTTON3_PRESSED|BUTTON3_RELEASED|REPORT_MOUSE_POSITION,NULL)==BUTTON1_PRESSED|BUTTON1_RELEASED|BUTTON2_PRESSED|BUTTON2_RELEASED|BUTTON3_PRESSED|BUTTON3_RELEASED|REPORT_MOUSE_POSITION)
 			return(0);
 	#endif
 	return(-1);
-*/
 }
