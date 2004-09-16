ae0f711bcd8e63e1c1f735f6c7dd558c0cf4ffbd
rswindell <rswindell>
2004-09-16 10:25:27 +0000

    Another attempt to clean up this carriage-return mess. Deuce: you can't edit files with a DOS editor that insists on converted \n to \r\n and then check the file back in using a Unix CVS client.

--- sbbs3/sbbsdefs.mk
+++ sbbs3/sbbsdefs.mk
@@ -2,6 +2,6 @@
 
 # C/C++ compiler preprocessor definitions for building SBBS.DLL
 
-# $Id: sbbsdefs.mk,v 1.8 2004/09/23 18:13:54 rswindell Exp $
+# $Id: sbbsdefs.mk,v 1.6 2004/09/16 10:10:55 rswindell Exp $
 
-SBBSDEFS=	-DSBBS -DSBBS_EXPORTS -DSMB_EXPORTS -DMD5_EXPORTS -DRINGBUF_SEM
+SBBSDEFS=	-DSBBS -DSBBS_EXPORTS -DSMB_EXPORTS -D_THREAD_SAFE -DRINGBUF_SEM
