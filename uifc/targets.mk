a2e8a60d9225b510df0b18ea64fb1aa946e5c1cc
deuce <deuce>
2004-09-16 18:04:35 +0000

    Start fixing all the lib dependencies...

--- uifc/targets.mk
+++ uifc/targets.mk
@@ -1,2 +1,8 @@
-UIFCLIB	=	$(LIBODIR)$(DIRSEP)$(LIBPREFIX)uifc$(LIBFILE)
-all: $(OBJODIR) $(LIBODIR) $(UIFCLIB)
+UIFCTEST	=	$(EXEODIR)$(DIRSEP)uifctest$(EXEFILE)
+
+all: lib mtlib test
+
+test: mtlib ciolib-mt xpdev-mt $(EXEODIR) $(UIFCTEST)
+
+lib: $(OBJODIR) $(LIBODIR) $(UIFCLIB)
+mtlib: $(MTOBJODIR) $(LIBODIR) $(UIFCLIB-MT)
