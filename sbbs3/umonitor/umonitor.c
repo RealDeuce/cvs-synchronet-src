1fa57fa78760f8a28ae3fff633e34f65767decd0
rswindell <rswindell>
2004-09-16 08:59:32 +0000

    Added new-line char at end of file to make gcc happy.

--- sbbs3/umonitor/umonitor.c
+++ sbbs3/umonitor/umonitor.c
@@ -2,7 +2,7 @@
 
 /* Synchronet for *nix node activity monitor */
 
-/* $Id: umonitor.c,v 1.65 2004/09/28 05:39:59 deuce Exp $ */
+/* $Id: umonitor.c,v 1.59 2004/09/16 01:40:55 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -152,7 +152,7 @@ void node_toggles(scfg_t *cfg,int nodenum)  {
 			,(node.misc&NODE_DOWN || (node.status==NODE_OFFLINE)) ? YesStr : NoStr);
 		opt[j][0]=0;
 
-		switch(uifc.list(WIN_MID|WIN_SAV,0,0,0,&i,0,"Node Toggles",opt)) {
+		switch(uifc.list(WIN_MID,0,0,0,&i,0,"Node Toggles",opt)) {
 			case 0:	/* Locked */
 				node.misc ^= NODE_LOCK;
 				break;
@@ -234,7 +234,7 @@ int dospy(int nodenum, bbs_startup_t *bbs_startup)  {
 int sendmessage(scfg_t *cfg, int nodenum,node_t *node)  {
 	char str[80],str2[80];
 
-	uifc.input(WIN_MID|WIN_SAV,0,0,"Telegram",str2,58,K_WRAP|K_MSG);
+	uifc.input(WIN_MID,0,0,"Telegram",str2,58,K_WRAP|K_MSG);
 	sprintf(str,"\1n\1y\1hMessage From Sysop:\1w %s\r\n",str2);
 	if(getnodedat(cfg,nodenum,node,NULL))
 		return(-1);
@@ -726,7 +726,6 @@ int edit_cfg(scfg_t *cfg)
 			allocfail(MAX_OPLN);
 
 	i=0;
-	strcpy(opt[i++],"sbbs.ini");
 	strcpy(opt[i++],"alias.cfg");
 	strcpy(opt[i++],"attr.cfg");
 	strcpy(opt[i++],"dns_blacklist.cfg");
@@ -737,7 +736,6 @@ int edit_cfg(scfg_t *cfg)
 	strcpy(opt[i++],"relay.cfg");
 	strcpy(opt[i++],"sbbsecho.cfg");
 	strcpy(opt[i++],"services.cfg");
-	strcpy(opt[i++],"services.ini");
 	strcpy(opt[i++],"ftpalias.cfg");
 	strcpy(opt[i++],"sockopts.cfg");
 	strcpy(opt[i++],"spambait.cfg");
@@ -817,7 +815,6 @@ int main(int argc, char** argv)  {
 	box_t	boxch;
 	scfg_t	cfg;
 	int		done;
-	int		ciolib_mode=CIOLIB_MODE_AUTO;
 
 	/******************/
 	/* Ini file stuff */
@@ -826,7 +823,7 @@ int main(int argc, char** argv)  {
 	FILE*				fp;
 	bbs_startup_t		bbs_startup;
 
-	sscanf("$Revision: 1.65 $", "%*s %s", revision);
+	sscanf("$Revision: 1.59 $", "%*s %s", revision);
 
     printf("\nSynchronet UNIX Monitor %s-%s  Copyright 2004 "
         "Rob Swindell\n",revision,PLATFORM_DESC);
@@ -908,44 +905,23 @@ int main(int argc, char** argv)  {
                     uifc.esc_delay=atoi(argv[i]+2);
                     break;
 				case 'I':
-					switch(toupper(argv[i][2])) {
-						case 'A':
-							ciolib_mode=CIOLIB_MODE_ANSI;
-							break;
-						case 'C':
-							ciolib_mode=CIOLIB_MODE_CURSES;
-							break;
-						case 0:
-							printf("NOTICE: The -i option is depreciated, use -if instead\r\n");
-							SLEEP(2000);
-						case 'F':
-							ciolib_mode=CIOLIB_MODE_CURSES_IBM;
-							break;
-						case 'X':
-							ciolib_mode=CIOLIB_MODE_X;
-							break;
-						case 'W':
-							ciolib_mode=CIOLIB_MODE_CONIO;
-							break;
-						default:
-							goto USAGE;
-					}
+					/* Set up ex-ascii codes */
+					boxch.ls=186;
+					boxch.rs=186;
+					boxch.ts=205;
+					boxch.bs=205;
+					boxch.tl=201;
+					boxch.tr=187;
+					boxch.bl=200;
+					boxch.br=188;
+					uifc.mode|=UIFC_IBM;
 					break;
                 default:
-					USAGE:
                     printf("\nusage: %s [ctrl_dir] [options]"
                         "\n\noptions:\n\n"
                         "-c  =  force color mode\n"
                         "-e# =  set escape delay to #msec\n"
-						"-iX =  set interface mode to X (default=auto) where X is one of:\r\n"
-#ifdef __unix__
-						"       X = X11 mode\r\n"
-						"       C = Curses mode\r\n"
-						"       F = Curses mode with forced IBM charset\r\n"
-#else
-						"       W = Win32 native mode\r\n"
-#endif
-						"       A = ANSI mode\r\n"
+						"-i  =  force IBM charset\n"
                         "-l# =  set screen lines to #\n"
 						,argv[0]
                         );
@@ -956,12 +932,11 @@ int main(int argc, char** argv)  {
 	signal(SIGPIPE, SIG_IGN);
 
 	uifc.size=sizeof(uifc);
-	i=initciolib(ciolib_mode);
-	if(i!=0) {
-    	printf("ciolib library init returned error %d\n",i);
-    	exit(1);
-	}
+#ifdef USE_CURSES
+	i=uifcinic(&uifc);  /* curses */
+#else
 	i=uifcini32(&uifc);  /* curses */
+#endif
 	if(i!=0) {
 		printf("uifc library init returned error %d\n",i);
 		exit(1);
@@ -995,9 +970,9 @@ int main(int argc, char** argv)  {
 		}
 		mopt[i][0]=0;
 
-		uifc.helpbuf=	"`Synchronet UNIX Monitor\n"
+		uifc.helpbuf=	"`Synchronet Monitor\n"
 		                "`------------------\n"
-		                "Welcome to the Synchronet UNIX Monitor.\n"
+		                "Welcome to the Sychonet UNIX Monitor.\n"
 		                "Displayed on this screen are the statitics for the BBS\n"
 		                "You can scroll through the list starting at \"System Options\" \n"
 		                "Pressing Enter on each will give a menu of option to perform.\n"
@@ -1031,20 +1006,17 @@ int main(int argc, char** argv)  {
 			i=0;
 			strcpy(opt[i++],"Run SCFG");
 			strcpy(opt[i++],"Run User Editor");
-			strcpy(opt[i++],"Run SyncTERM");
 			strcpy(opt[i++],"View logs");
 			strcpy(opt[i++],"Force QWK Net callout");
 			strcpy(opt[i++],"Run event");
 			strcpy(opt[i++],"Recycle servers");
-			strcpy(opt[i++],"Edit CFG/INI files");
+			strcpy(opt[i++],"Edit CFG files");
 			strcpy(opt[i++],"Edit trashcan files");
 			opt[i][0]=0;
 			uifc.helpbuf=	"`System Options`\n"
 			                "`------------`\n\n"
 							"`Run SCFG              : `Run the Synchronet Configuration Utility.\n"
 							"`Run User Editor       : `Call up the User Editor.\n"
-							"`Run SynchTERM         : `Run SyncTERM for RLogin.  SyncTERM must be\n"
-							"                        in the exec directory.\n"
 							"`View logs             : `View the various system logs.\n"
 							"`Force QWK Net callout : `Force a callout to QWK Net Hub.  Select which\n"
 							"                        Hub from a popup list of configured Hubs.\n"
@@ -1071,7 +1043,7 @@ int main(int argc, char** argv)  {
 						do_cmd(str);
 						break;
 					case 1:
-						sprintf(str,"%suedit ",cfg.exec_dir);
+						sprintf(str,"%suedit",cfg.exec_dir);
 						for(j=1; j<argc; j++) {
 							strcat(str,"'");
 							strcat(str,argv[j]);
@@ -1080,30 +1052,21 @@ int main(int argc, char** argv)  {
 						do_cmd(str);
 						break;
 					case 2:
-						sprintf(str,"%ssyncterm",cfg.exec_dir);
-						for(j=1; j<argc; j++) {
-							strcat(str,"'");
-							strcat(str,argv[j]);
-							strcat(str,"' ");
-						}
-						do_cmd(str);
-						break;
-					case 3:
 						view_logs(&cfg);
 						break;
-					case 4:
+					case 3:
 						qwk_callouts(&cfg);
 						break;
-					case 5:
+					case 4:
 						run_events(&cfg);
 						break;
-					case 6:
+					case 5:
 						recycle_servers(&cfg);
 						break;
-					case 7:
+					case 6:
 						edit_cfg(&cfg);
 						break;
-					case 8:
+					case 7:
 						edit_can(&cfg);
 						break;
 				}
@@ -1338,8 +1301,6 @@ int main(int argc, char** argv)  {
        	}
 	}
 }
-
-
 
 
 
