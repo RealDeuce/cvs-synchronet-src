7675eeb221de455de962071ff7af2b12719c563b
rswindell <rswindell>
2004-09-16 10:10:55 +0000

    Don't define WRAPPER_IMPORTS (for now at least).

--- sbbs3/sbbsdefs.mk
+++ sbbs3/sbbsdefs.mk
@@ -2,6 +2,6 @@
 
 # C/C++ compiler preprocessor definitions for building SBBS.DLL
 
-# $Id: sbbsdefs.mk,v 1.5 2004/09/15 07:40:07 deuce Exp $
+# $Id: sbbsdefs.mk,v 1.6 2004/09/16 10:10:55 rswindell Exp $
 
-SBBSDEFS=	-DSBBS -DSBBS_EXPORTS -DSMB_EXPORTS -D_THREAD_SAFE -DWRAPPER_IMPORTS -DRINGBUF_SEM
+SBBSDEFS=	-DSBBS -DSBBS_EXPORTS -DSMB_EXPORTS -D_THREAD_SAFE -DRINGBUF_SEM
