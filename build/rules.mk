cbb7407829df2e2abe76611cb2e26e6a1d8dde54
deuce <deuce>
2004-09-16 19:09:43 +0000

    Fix CIOLIB target.

--- build/rules.mk
+++ build/rules.mk
@@ -2,7 +2,7 @@
 #
 # Global build targets for all make systems
 #
-# $Id: rules.mk,v 1.4 2004/09/12 21:39:08 deuce Exp $
+# $Id: rules.mk,v 1.5 2004/09/13 02:54:26 rswindell Exp $
 
 $(OBJODIR):
 	$(QUIET)$(IFNOTEXIST) mkdir $(OBJODIR)
@@ -18,10 +18,10 @@ $(EXEODIR):
 
 clean:
 	@echo Deleting $(OBJODIR)$(DIRSEP)
-	$(QUIET)$(DELETE) $(OBJODIR)$(DIRSEP)*
+	-$(QUIET)$(DELETE) $(OBJODIR)$(DIRSEP)*
 	@echo Deleting $(MTOBJODIR)$(DIRSEP)
-	$(QUIET)$(DELETE) $(MTOBJODIR)$(DIRSEP)*
+	-$(QUIET)$(DELETE) $(MTOBJODIR)$(DIRSEP)*
 	@echo Deleting $(LIBODIR)$(DIRSEP)
-	$(QUIET)$(DELETE) $(LIBODIR)$(DIRSEP)*
+	-$(QUIET)$(DELETE) $(LIBODIR)$(DIRSEP)*
 	@echo Deleting $(EXEODIR)$(DIRSEP)
-	$(QUIET)$(DELETE) $(EXEODIR)$(DIRSEP)*
+	-$(QUIET)$(DELETE) $(EXEODIR)$(DIRSEP)*
