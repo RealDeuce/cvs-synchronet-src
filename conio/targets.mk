2e212688f41fb82f76793e8c123a82b111b4642c
deuce <deuce>
2004-09-16 19:02:01 +0000

    Fix library dependencies in new build system (Untested on Win32)

--- conio/targets.mk
+++ conio/targets.mk
@@ -1,3 +1,4 @@
 # conio/targets.mk
-CIOLIB-MT	=	$(LIBODIR)$(DIRSEP)$(LIBPREFIX)ciolib-mt$(LIBFILE)
-default: mtlib
+CIOLIB-MT_BUILD	=	$(LIBODIR)$(DIRSEP)$(LIBPREFIX)ciolib_mt$(LIBFILE)
+
+mtlib:	$(MTOBJODIR) $(LIBODIR) $(CIOLIB-MT_BUILD)
--- sbbs3/umonitor/targets.mk
+++ sbbs3/umonitor/targets.mk
@@ -1,1 +1,5 @@
-all: $(LIBODIR) $(BUILD_DEPENDS)$(EXEODIR)/umonitor
+UMONITOR	=	$(EXEODIR)$(DIRSEP)umonitor$(EXEFILE)
+
+all: ciolib-mt uifc-mt xpdev-mt smblib $(MTOBJODIR) $(EXEODIR) $(UMONITOR)
+
+$(UMONITOR):	$(CIOLIB-MT) $(UIFCLIB-MT) $(XPDEV-MT_LIB) $(SMBLIB)
