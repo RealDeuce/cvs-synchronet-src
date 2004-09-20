d3b143b858a28d4a288161332f9c24883a606784
deuce <deuce>
2004-09-20 06:34:56 +0000

    Get rid of unneeded XFlush() calls.

--- sbbs3/download.cpp
+++ sbbs3/download.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet file download routines */
 
-/* $Id: download.cpp,v 1.28 2004/10/14 23:56:35 rswindell Exp $ */
+/* $Id: download.cpp,v 1.26 2003/08/22 10:50:15 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -225,8 +225,11 @@ int sbbs_t::protocol(prot_t* prot, enum XFER_TYPE type
 	spymsg(msg);
 	sys_status|=SS_FILEXFER;	/* disable spy during file xfer */
 	/* enable telnet binary transmission in both directions */
-	request_telnet_opt(TELNET_DO,TELNET_BINARY_TX);
-	request_telnet_opt(TELNET_WILL,TELNET_BINARY_TX);
+	if(!(telnet_mode&TELNET_MODE_BIN_RX)) {
+		send_telnet_cmd(TELNET_DO,TELNET_BINARY);
+		telnet_mode|=TELNET_MODE_BIN_RX;
+	}
+	send_telnet_cmd(TELNET_WILL,TELNET_BINARY);
 	ex_mode=0;
 	if(prot->misc&PROT_NATIVE)
 		ex_mode|=EX_NATIVE;
@@ -235,9 +238,13 @@ int sbbs_t::protocol(prot_t* prot, enum XFER_TYPE type
 #endif
 
 	i=external(cmdline,ex_mode,p);
+	/* disable telnet binary transmission mode */
+	send_telnet_cmd(TELNET_WONT,TELNET_BINARY);
 	/* Got back to Text/NVT mode */
-	request_telnet_opt(TELNET_DONT,TELNET_BINARY_TX);
-	request_telnet_opt(TELNET_WONT,TELNET_BINARY_TX);
+	if(telnet_mode&TELNET_MODE_BIN_RX) {
+		send_telnet_cmd(TELNET_DONT,TELNET_BINARY);
+		telnet_mode&=~TELNET_MODE_BIN_RX;
+	}
 
 	sys_status&=~SS_FILEXFER;
 	if(online==ON_REMOTE)
