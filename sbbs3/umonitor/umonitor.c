c464c4f7cd4743c21934898bfc4733b14bebb790
deuce <deuce>
2004-09-21 04:59:58 +0000

    Forgot to add the USAGE label.

--- sbbs3/umonitor/umonitor.c
+++ sbbs3/umonitor/umonitor.c
@@ -2,7 +2,7 @@
 
 /* Synchronet for *nix node activity monitor */
 
-/* $Id: umonitor.c,v 1.62 2004/09/21 04:59:00 deuce Exp $ */
+/* $Id: umonitor.c,v 1.63 2004/09/21 04:59:58 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -824,7 +824,7 @@ int main(int argc, char** argv)  {
 	FILE*				fp;
 	bbs_startup_t		bbs_startup;
 
-	sscanf("$Revision: 1.62 $", "%*s %s", revision);
+	sscanf("$Revision: 1.63 $", "%*s %s", revision);
 
     printf("\nSynchronet UNIX Monitor %s-%s  Copyright 2004 "
         "Rob Swindell\n",revision,PLATFORM_DESC);
@@ -930,6 +930,7 @@ int main(int argc, char** argv)  {
 					}
 					break;
                 default:
+					USAGE:
                     printf("\nusage: %s [ctrl_dir] [options]"
                         "\n\noptions:\n\n"
                         "-c  =  force color mode\n"
