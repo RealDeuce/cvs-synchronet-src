f583f5759df591c20ff4f2b04eaf82c292a16a57
rswindell <rswindell>
2004-09-21 20:39:36 +0000

    #include "mouse.h" instead of <mouse.h> (not a system include file).

--- conio/curs_cio.h
+++ conio/curs_cio.h
@@ -1,42 +1,10 @@
-/* $Id: curs_cio.h,v 1.10 2004/10/01 01:20:11 deuce Exp $ */
-
-/****************************************************************************
- * @format.tab-size 4		(Plain Text/Source Code File Header)			*
- * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
- *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
- *																			*
- * This library is free software; you can redistribute it and/or			*
- * modify it under the terms of the GNU Lesser General Public License		*
- * as published by the Free Software Foundation; either version 2			*
- * of the License, or (at your option) any later version.					*
- * See the GNU Lesser General Public License for more details: lgpl.txt or	*
- * http://www.fsf.org/copyleft/lesser.html									*
- *																			*
- * Anonymous FTP access to the most recent released source is available at	*
- * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
- *																			*
- * Anonymous CVS access to the development source and modification history	*
- * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
- *     (just hit return, no password is necessary)							*
- * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
- *																			*
- * For Synchronet coding style and modification guidelines, see				*
- * http://www.synchro.net/source.html										*
- *																			*
- * You are encouraged to submit any modifications (preferably in Unix diff	*
- * format) via e-mail to mods@synchro.net									*
- *																			*
- * Note: If this box doesn't appear square, then you need to fix your tabs.	*
- ****************************************************************************/
+/* $Id: curs_cio.h,v 1.8 2004/09/10 23:24:49 deuce Exp $ */
 
 #ifdef __unix__
 #include "ciolib.h"
 #undef getch
 #undef ungetch
 #undef getmouse
-#undef ungetmouse
 #include "curs_fix.h"
 
 #ifdef __cplusplus
