18322f61ebf9fd874cccd19ef78253c98b9dbc19
rswindell <rswindell>
2004-09-23 19:04:02 +0000

    Moved #inclusion of all XPDEV headers to smbdefs.h to eliminate problem of different definitions for DLLCALL/EXPORT at different times (also solved with the removal of multiple inclusion protection from xpdev/wrapdll.h).

--- sbbs3/ctrl/FtpFormUnit.h
+++ sbbs3/ctrl/FtpFormUnit.h
@@ -1,12 +1,12 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: FtpFormUnit.h,v 1.3 2004/10/18 00:04:41 rswindell Exp $ */
+/* $Id: FtpFormUnit.h,v 1.2 2002/03/19 02:31:30 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -61,10 +61,12 @@ __published:	// IDE-managed Components
 	TProgressBar *ProgressBar;
 	TMemo *Log;
 	TToolButton *RecycleButton;
+	void __fastcall FormShow(TObject *Sender);
 	void __fastcall FormHide(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
 	__fastcall TFtpForm(TComponent* Owner);
+    TMainForm* 		MainForm;
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TFtpForm *FtpForm;
