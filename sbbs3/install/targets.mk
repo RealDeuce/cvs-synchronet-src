0622749c2acf38cc1b781a44871343e9f0caced6
deuce <deuce>
2004-09-25 21:51:02 +0000

    Heh, sleep if lock fails, not if it succeeds.  :-)

--- sbbs3/install/targets.mk
+++ sbbs3/install/targets.mk
@@ -2,12 +2,14 @@
 
 # Make 'include file' defining targets for Synchronet SBBSINST project
 
-# $Id: targets.mk,v 1.3 2004/09/13 22:57:09 deuce Exp $
+# $Id: targets.mk,v 1.4 2004/09/16 19:02:02 deuce Exp $
 
 # ODIR, SLASH, LIBFILE, EXEFILE, and DELETE must be pre-defined
 
 SBBSINST	=	$(EXEODIR)$(DIRSEP)sbbsinst$(EXEFILE) 
 
-all:	$(EXEODIR) \
+all:	xpdev-mt ciolib-mt uifc-mt $(EXEODIR) \
 		$(MTOBJODIR) \
 		$(SBBSINST)
+
+$(SBBSINST):	$(XPDEV-MT_LIB) $(CIOLIB-MT) $(UIFCLIB-MT)
