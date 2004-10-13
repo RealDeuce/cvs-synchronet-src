c3af1d2d433273150ba12532ee06b59adbb73026
deuce <deuce>
2004-10-13 22:06:56 +0000

    More fixes for scancode stuff...

--- sbbs3/newuser.cpp
+++ sbbs3/newuser.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet new user routine */
 
-/* $Id: newuser.cpp,v 1.41 2004/10/21 09:03:11 rswindell Exp $ */
+/* $Id: newuser.cpp,v 1.40 2004/05/30 06:47:53 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -37,6 +37,8 @@
 
 #include "sbbs.h"
 #include "cmdshell.h"
+
+static char* ContinueText = "Continue";
 
 /****************************************************************************/
 /* This function is invoked when a user enters "NEW" at the NN: prompt		*/
@@ -202,7 +204,7 @@ BOOL sbbs_t::newuser()
 					|| trashcan(useron.alias,"name")
 					|| (!(cfg.uq&UQ_ALIASES) && !strchr(useron.alias,' '))) {
 					bputs(text[YouCantUseThatName]);
-					if(!yesno(text[ContinueQ]))
+					if(!yesno(ContinueText))
 						return(FALSE);
 					continue; 
 				}
@@ -223,7 +225,7 @@ BOOL sbbs_t::newuser()
 					bputs(text[YouCantUseThatName]);
 				else
 					break; 
-				if(!yesno(text[ContinueQ]))
+				if(!yesno(ContinueText))
 					return(FALSE);
 			} 
 		}
@@ -247,7 +249,7 @@ BOOL sbbs_t::newuser()
 				bputs(text[YouCantUseThatName]);
 			else
 				break; 
-			if(!yesno(text[ContinueQ]))
+			if(!yesno(ContinueText))
 				return(FALSE);
 		}
 		if(!online) return(FALSE);
