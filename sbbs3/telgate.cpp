7e60e45e6fe4f60ea8425d2d1b161a8ba5ea01df
rswindell <rswindell>
2004-10-13 20:13:27 +0000

    Fixed memory leaks by replacing dynamically allocated log_list with statically allocated and calling listFree() in cleanup().

--- sbbs3/telgate.cpp
+++ sbbs3/telgate.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet telnet gateway routines */
 
-/* $Id: telgate.cpp,v 1.24 2004/10/14 23:56:35 rswindell Exp $ */
+/* $Id: telgate.cpp,v 1.22 2004/04/08 05:36:17 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -135,10 +135,11 @@ void sbbs_t::telnet_gate(char* destaddr, ulong mode)
 	}
 	
 	/* This is required for gating to Unix telnetd */
-	request_telnet_opt(TELNET_DONT,TELNET_TERM_TYPE);	// Re-negotiation of terminal type
+	send_telnet_cmd(TELNET_DONT,TELNET_TERM_TYPE);	// Re-negotiation of terminal type
 
 	/* Text/NVT mode by default */
-	request_telnet_opt(TELNET_DONT,TELNET_BINARY_TX);
+	send_telnet_cmd(TELNET_DONT,TELNET_BINARY);
+	telnet_mode&=~TELNET_MODE_BIN_RX;
 
 	while(online) {
 		if(!(mode&TG_NOCHKTIME))
@@ -155,7 +156,7 @@ void sbbs_t::telnet_gate(char* destaddr, ulong mode)
 				lprintf(LOG_DEBUG,"Node %d Telnet cmd from client: %s", cfg.node_num, dump);
 			}
 #endif
-			if(telnet_remote_option[TELNET_BINARY_TX]!=TELNET_WILL) {
+			if(!(telnet_mode&TELNET_MODE_BIN_RX)) {
 				if(*buf==0x1d) { // ^]
 					save_console=console;
 					console&=~CON_RAW_IN;	// Allow Ctrl-U/Ctrl-P
@@ -256,7 +257,7 @@ void sbbs_t::telnet_gate(char* destaddr, ulong mode)
 	telnet_mode&=~TELNET_MODE_GATE;
 
 	/* Disable Telnet Terminal Echo */
-	request_telnet_opt(TELNET_WILL,TELNET_ECHO);
+	send_telnet_cmd(TELNET_WILL,TELNET_ECHO);
 
 	close_socket(remote_socket);
 
