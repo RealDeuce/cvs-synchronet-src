1407cb16a19f0269b1fee6d96ddfe0c090b6bf08
deuce <deuce>
2004-10-20 13:24:06 +0000

    Attempt to fix "Garbage at end of post" issue... it appears that someone (Either me or whatever browser generates this garbage) has an incorrect conecept of what the Content-Length header means.

--- sbbs3/ctrl/ServicesFormUnit.cpp
+++ sbbs3/ctrl/ServicesFormUnit.cpp
@@ -1,12 +1,12 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: ServicesFormUnit.cpp,v 1.2 2002/03/19 02:31:31 rswindell Exp $ */
+/* $Id: ServicesFormUnit.cpp,v 1.3 2004/10/18 00:04:41 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -48,3 +48,9 @@ __fastcall TServicesForm::TServicesForm(TComponent* Owner)
 {
 }
 //---------------------------------------------------------------------------
+void __fastcall TServicesForm::FormHide(TObject *Sender)
+{
+    MainForm->ViewServices->Checked=false;
+}
+//---------------------------------------------------------------------------
+
