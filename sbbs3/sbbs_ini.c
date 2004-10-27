9631e1030092784e6414c61a2376dcc9d7660fd3
rswindell <rswindell>
2004-10-27 21:20:53 +0000

    Streamlined show_msg(), using smb_getmsgtxt(). Eliminated unnecessary lfputs().

--- sbbs3/sbbs_ini.c
+++ sbbs3/sbbs_ini.c
@@ -2,7 +2,7 @@
 
 /* Synchronet console configuration (.ini) file routines */
 
-/* $Id: sbbs_ini.c,v 1.80 2004/10/13 19:58:38 rswindell Exp $ */
+/* $Id: sbbs_ini.c,v 1.86 2004/10/27 11:02:17 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -38,7 +38,6 @@
 #include "dirwrap.h"	/* backslash */
 #include "sbbs_ini.h"
 #include "sbbsdefs.h"	/* JAVASCRIPT_* macros */
-#include "ini_opts.h"	/* bbs_options, ftp_options, etc. */
 
 static const char*	nulstr="";
 static const char*	strOptions="Options";
@@ -46,13 +45,14 @@ static const char*	strInterface="Interface";
 static const char*	strHostName="HostName";
 static const char*	strLogMask="LogMask";
 
-#define DEFAULT_LOG_MASK		0x7f	/* EMERG|ALERT|CRIT|ERR|WARNING|NOTICE|INFO */
+#define DEFAULT_LOG_MASK		0xff	/* EMERG|ALERT|CRIT|ERR|WARNING|NOTICE|INFO|DEBUG */
 #define DEFAULT_MAX_MSG_SIZE    (10*1024*1024)	/* 10MB */
 
 void sbbs_get_ini_fname(char* ini_file, char* ctrl_dir, char* pHostName)
 {
     char host_name[128];
-    
+    char path[MAX_PATH+1];
+
     if(pHostName==NULL) {
 #if defined(_WINSOCKAPI_)
         WSADATA WSAData;
@@ -64,13 +64,15 @@ void sbbs_get_ini_fname(char* ini_file, char* ctrl_dir, char* pHostName)
 #endif
         pHostName=host_name;
     }
-	sprintf(ini_file,"%s%c%s.ini",ctrl_dir,PATH_DELIM,pHostName);
+	SAFECOPY(path,ctrl_dir);
+	backslash(path);
+	sprintf(ini_file,"%s%s.ini",path,pHostName);
 #if defined(__unix__) && defined(PREFIX)
 	if(!fexistcase(ini_file))
 		sprintf(ini_file,PREFIX"/etc/sbbs.ini");
 #endif
 	if(!fexistcase(ini_file))
-		sprintf(ini_file,"%s%csbbs.ini",ctrl_dir,PATH_DELIM);
+		sprintf(ini_file,"%ssbbs.ini",path);
 }
 
 static void read_ini_globals(FILE* fp, global_startup_t* global)
@@ -341,9 +343,6 @@ void sbbs_read_ini(
 		SAFECOPY(mail->outbound_sound
 			,iniReadString(fp,section,"OutboundSound",nulstr,value));
 
-		SAFECOPY(mail->proc_cfg_file
-			,iniReadString(fp,section,"ProcessConfigFile","mailproc.cfg",value));
-
 		/* JavaScript Operating Parameters */
 		mail->js_max_bytes
 			=iniReadInteger(fp,section,strJavaScriptMaxBytes		,global->js.max_bytes);
@@ -385,12 +384,6 @@ void sbbs_read_ini(
 
 		SAFECOPY(services->host_name
 			,iniReadString(fp,section,strHostName,global->host_name,value));
-
-		SAFECOPY(services->ini_file
-			,iniReadString(fp,section,"iniFile","services.ini",value));
-
-		SAFECOPY(services->cfg_file
-			,iniReadString(fp,section,"ConfigFile","services.cfg",value));
 
 		SAFECOPY(services->answer_sound
 			,iniReadString(fp,section,"AnswerSound",nulstr,value));
@@ -739,9 +732,6 @@ BOOL sbbs_write_ini(
 		if(!iniSetString(lp,section,"OutboundSound",mail->outbound_sound,&style))
 			break;
 
-		if(!iniSetString(lp,section,"ProcessConfigFile",mail->proc_cfg_file,&style))
-			break;
-
 		/* JavaScript Operating Parameters */
 		if(mail->js_max_bytes==global->js.max_bytes)
 			iniRemoveValue(lp,section,strJavaScriptMaxBytes);
@@ -810,11 +800,6 @@ BOOL sbbs_write_ini(
             || strcmp(bbs->host_name,cfg->sys_inetaddr)==0)
 			iniRemoveKey(lp,section,strHostName);
 		else if(!iniSetString(lp,section,strHostName,services->host_name,&style))
-			break;
-
-		if(!iniSetString(lp,section,"iniFile",services->ini_file,&style))
-			break;
-		if(!iniSetString(lp,section,"ConfigFile",services->cfg_file,&style))
 			break;
 
 		if(!iniSetString(lp,section,"AnswerSound",services->answer_sound,&style))
