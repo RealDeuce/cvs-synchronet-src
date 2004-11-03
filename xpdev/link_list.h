fce012aadc1f416de3c7ed9dd93fce46420e302c
deuce <deuce>
2004-11-03 05:21:07 +0000

    Use retry_bind() instead of bind() where appropriate.

--- xpdev/link_list.h
+++ xpdev/link_list.h
@@ -2,7 +2,7 @@
 
 /* Double-Linked-list library */
 
-/* $Id: link_list.h,v 1.16 2004/11/18 00:59:21 rswindell Exp $ */
+/* $Id: link_list.h,v 1.10 2004/09/16 05:42:25 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -42,8 +42,7 @@
 #include "str_list.h"	/* string list functions and types */
 
 #if defined(LINK_LIST_THREADSAFE)
-	#include "threadwrap.h"	/* mutexes */
-	#include "semwrap.h"	/* semaphores */
+	#include "threadwrap.h"
 #endif
 
 #if defined(__cplusplus)
@@ -54,31 +53,26 @@ extern "C" {
 
 /* Valid link_list_t.flags bits */
 #define LINK_LIST_MALLOC		(1<<0)	/* List/node allocated with malloc() */
-#define LINK_LIST_ALWAYS_FREE	(1<<1)	/* ALWAYS free node data in listFreeNodes() */
-#define LINK_LIST_NEVER_FREE	(1<<2)	/* NEVER free node data (careful of memory leaks!) */
-#define LINK_LIST_MUTEX			(1<<3)	/* Mutex-protected linked-list */
-#define LINK_LIST_SEMAPHORE		(1<<4)	/* Semaphore attached to linked-list */
-#define LINK_LIST_NODE_LOCKED	(1<<5)	/* Node is locked */
-#define LINK_LIST_ATTACH		(1<<6)	/* Attach during init */
+#define LINK_LIST_ALWAYS_FREE	(1<<1)	/* ALWAYS free node data when removing */
+#define LINK_LIST_NEVER_FREE	(1<<2)	/* NEVER free node data when removing */
+#define LINK_LIST_MUTEX			(1<<3)	/* Mutex protected linked-list */
+#define LINK_LIST_NODE_LOCKED	(1<<4)	/* Node is locked */
 
 typedef struct list_node {
-	void*				data;			/* pointer to some kind of data */
-	struct list_node*	next;			/* next node in list (or NULL) */
-	struct list_node*	prev;			/* previous node in list (or NULL) */
+	void*				data;		/* pointer to some kind of data */
+	struct list_node*	next;		/* next node in list (or NULL) */
+	struct list_node*	prev;		/* previous node in list (or NULL) */
 	struct link_list*	list;
-	unsigned long		flags;			/* private use flags */
+	unsigned long		flags;		/* private use flags */
 } list_node_t;
 
 typedef struct link_list {
-	list_node_t*		first;			/* first node in list (or NULL) */
-	list_node_t*		last;			/* last node in list (or NULL) */
-	unsigned long		flags;			/* private use flags */
-	long				count;			/* number of nodes in list */
-	void*				private_data;	/* for use by the application only */
-	long				refs;			/* reference counter (attached clients) */
+	list_node_t*		first;		/* first node in list (or NULL) */
+	list_node_t*		last;		/* last node in list (or NULL) */
+	unsigned long		flags;		/* private use flags */
+	long				count;		/* number of nodes in list */
 #if defined(LINK_LIST_THREADSAFE)
 	pthread_mutex_t		mutex;
-	sem_t				sem;
 #endif
 } link_list_t;
 
@@ -88,17 +82,6 @@ BOOL			listFree(link_list_t*);
 long			listFreeNodes(link_list_t*);
 BOOL			listFreeNodeData(list_node_t* node);
 
-/* Increment/decrement reference counter (and auto-free when zero), returns -1 on error */
-long			listAttach(link_list_t*);
-long			listDetach(link_list_t*);
-
-#if defined(LINK_LIST_THREADSAFE)
-BOOL			listSemPost(link_list_t*);
-BOOL			listSemWait(link_list_t*);
-BOOL			listSemTryWait(link_list_t*);
-BOOL			listSemTryWaitBlock(link_list_t*, unsigned long timeout);
-#endif
-
 /* Lock/unlock mutex-protected linked lists (no-op for unprotected lists) */
 void			listLock(const link_list_t*);
 void			listUnlock(const link_list_t*);
@@ -106,10 +89,6 @@ void			listUnlock(const link_list_t*);
 /* Return count or index of nodes, or -1 on error */
 long			listCountNodes(const link_list_t*);
 long			listNodeIndex(const link_list_t*, list_node_t*);
-
-/* Get/Set list private data */
-void*			listSetPrivateData(link_list_t*, void*);
-void*			listGetPrivateData(link_list_t*);
 
 /* Return an allocated string list (which must be freed), array of all strings in linked list */
 str_list_t		listStringList(const link_list_t*);
@@ -123,7 +102,7 @@ link_list_t*	listExtract(link_list_t* dest_list, const list_node_t* src_node, lo
 
 /* Simple search functions returning found node or NULL on error */
 list_node_t*	listNodeAt(const link_list_t*, long index);
-list_node_t*	listFindNode(const link_list_t*, const void* data, size_t length);
+list_node_t*	listFindNode(const link_list_t*, void* data, size_t length);
 
 /* Convenience functions */
 list_node_t*	listFirstNode(const link_list_t*);
@@ -171,14 +150,13 @@ BOOL			listSwapNodes(list_node_t* node1, list_node_t* node2);
 #define listInsertNodeString(list, str)			listAddNodeString(list, str, FIRST_NODE)
 #define	listPushStringList(list, str_list)		listAddStringList(list, str_list, listLastNode(list))
 #define listInsertStringList(list, str_list)	listAddStringList(list, str_list, FIRST_NODE)
-#define listPopNode(list)						listRemoveNode(list, listLastNode(list), FALSE)
-#define listShiftNode(list)						listRemoveNode(list, FIRST_NODE, FALSE)
+#define listPopNode(list)						listRemoveNode(list, listLastNode(list))
 
 /* Remove node from list, returning the node's data (if not free'd) */
-void*			listRemoveNode(link_list_t*, list_node_t* /* NULL=first */, BOOL free_data);
+void*			listRemoveNode(link_list_t*, list_node_t* /* NULL=first */);
 
 /* Remove multiple nodes from list, returning the number of nodes removed */
-long			listRemoveNodes(link_list_t*, list_node_t* /* NULL=first */, long count, BOOL free_data);
+long			listRemoveNodes(link_list_t*, list_node_t* /* NULL=first */, long count);
 
 #if defined(__cplusplus)
 }
