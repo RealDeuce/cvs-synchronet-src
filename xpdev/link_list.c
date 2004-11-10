12edf4f031fedce9c6d701a2e21176a85688ca86
rswindell <rswindell>
2004-11-10 04:53:46 +0000

    If listFindNode() is called with a length of 0, just does a data pointer comparison rather than a memcmp().

--- xpdev/link_list.c
+++ xpdev/link_list.c
@@ -2,7 +2,7 @@
 
 /* Double-Linked-list library */
 
-/* $Id: link_list.c,v 1.26 2004/11/22 20:41:15 rswindell Exp $ */
+/* $Id: link_list.c,v 1.23 2004/11/10 04:53:46 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -126,7 +126,7 @@ BOOL listFree(link_list_t* list)
 	MUTEX_DESTROY(list);
 
 #if defined(LINK_LIST_THREADSAFE)
-	if(list->flags&LINK_LIST_SEMAPHORE) {
+	if(list->sem!=NULL) {
 		sem_destroy(&list->sem);
 		list->sem=NULL;
 	}
@@ -187,33 +187,33 @@ void* listGetPrivateData(link_list_t* list)
 
 #if defined(LINK_LIST_THREADSAFE)
 
-BOOL listSemPost(link_list_t* list)
+BOOL listSemPost(const link_list_t* list)
 {
-	if(list==NULL || !(list->flags&LINK_LIST_SEMAPHORE))
+	if(list==NULL || list->sem==NULL)
 		return(FALSE);
 
 	return(sem_post(&list->sem)==0);
 }
 
-BOOL listSemWait(link_list_t* list)
+BOOL listSemWait(const link_list_t* list)
 {
-	if(list==NULL || !(list->flags&LINK_LIST_SEMAPHORE))
+	if(list==NULL || list->sem==NULL)
 		return(FALSE);
 
 	return(sem_wait(&list->sem)==0);
 }
 
-BOOL listSemTryWait(link_list_t* list)
+BOOL listSemTryWait(const link_list_t* list)
 {
-	if(list==NULL || !(list->flags&LINK_LIST_SEMAPHORE))
+	if(list==NULL || list->sem==NULL)
 		return(FALSE);
 
 	return(sem_trywait(&list->sem)==0);
 }
 
-BOOL listSemTryWaitBlock(link_list_t* list, unsigned long timeout)
+BOOL listSemTryWaitBlock(const link_list_t* list, unsigned long timeout)
 {
-	if(list==NULL || !(list->flags&LINK_LIST_SEMAPHORE))
+	if(list==NULL || list->sem==NULL)
 		return(FALSE);
 
 	return(sem_trywait_block(&list->sem,timeout));
@@ -475,7 +475,7 @@ static list_node_t* list_add_node(link_list_t* list, list_node_t* node, list_nod
 	MUTEX_UNLOCK(list);
 
 #if defined(LINK_LIST_THREADSAFE)
-	if(list->flags&LINK_LIST_SEMAPHORE)
+	if(list->sem!=NULL)
 		listSemPost(list);
 #endif
 
@@ -619,7 +619,7 @@ link_list_t* listExtract(link_list_t* dest_list, const list_node_t* node, long m
 	return(list);
 }
 
-void* listRemoveNode(link_list_t* list, list_node_t* node, BOOL free_data)
+void* listRemoveNode(link_list_t* list, list_node_t* node)
 {
 	void*	data;
 
@@ -645,7 +645,8 @@ void* listRemoveNode(link_list_t* list, list_node_t* node, BOOL free_data)
 	if(list->last==node)
 		list->last = node->prev;
 
-	if(free_data)
+	if((list->flags&LINK_LIST_ALWAYS_FREE || node->flags&LINK_LIST_MALLOC)
+		&& !(list->flags&LINK_LIST_DONT_FREE))
 		listFreeNodeData(node);
 
 	data = node->data;
@@ -660,7 +661,7 @@ void* listRemoveNode(link_list_t* list, list_node_t* node, BOOL free_data)
 	return(data);
 }
 
-long listRemoveNodes(link_list_t* list, list_node_t* node, long max, BOOL free_data)
+long listRemoveNodes(link_list_t* list, list_node_t* node, long max)
 {
 	long count;
 
@@ -673,7 +674,7 @@ long listRemoveNodes(link_list_t* list, list_node_t* node, long max, BOOL free_d
 		node=list->first;
 
 	for(count=0; node!=NULL && count<max; node=node->next, count++)
-		if(listRemoveNode(list, node, free_data)==NULL)
+		if(listRemoveNode(list, node)==NULL)
 			break;
 
 	MUTEX_UNLOCK(list);
@@ -726,13 +727,13 @@ int main(int arg, char** argv)
 	char	str[32];
 	link_list_t list;
 
-	listInit(&list,0);
+	listInit(&list,LINK_LIST_NEVER_FREE);
 	for(i=0; i<100; i++) {
 		sprintf(str,"%u",i);
 		listPushNodeString(&list,str);
 	}
 
-	while((p=listShiftNode(&list))!=NULL)
+	while((p=listRemoveNode(&list,NULL))!=NULL)
 		printf("%d %s\n",listCountNodes(&list),p), free(p);
 
 	/* Yes, this test code leaks heap memory. :-) */
