3fbd4ae2b679cc3d3fcd0b0a1ef83af0f7ebf3c2
rswindell <rswindell>
2004-11-18 21:25:31 +0000

    Bugfix: js_CreateGlobalObjects() needed to create the "system" object using a node-specific scfg_t*, so the node_dir and temp_dir properties had the correct values.

--- sbbs3/main.cpp
+++ sbbs3/main.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet main/telnet server thread and related functions */
 
-/* $Id: main.cpp,v 1.363 2004/12/09 08:07:13 rswindell Exp $ */
+/* $Id: main.cpp,v 1.361 2004/11/18 21:25:31 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -374,8 +374,8 @@ DLLCALL js_DefineSyncProperties(JSContext *cx, JSObject *obj, jsSyncPropertySpec
 		return(JS_FALSE);
 
 	for(i=0;props[i].name;i++) {
-		if(!JS_DefinePropertyWithTinyId(cx, obj, /* Never reserve any "slots" for properties */
-			props[i].name,props[i].tinyid, JSVAL_VOID, NULL, NULL, props[i].flags|JSPROP_SHARED))
+		if(!JS_DefinePropertyWithTinyId(cx, obj, 
+			props[i].name,props[i].tinyid, JSVAL_VOID, NULL, NULL, props[i].flags))
 			return(JS_FALSE);
 		if(props[i].flags&JSPROP_ENUMERATE) {	/* No need to version invisible props */
 			val = INT_TO_JSVAL(props[i].ver);
@@ -477,7 +477,7 @@ DLLCALL js_DefineSyncProperties(JSContext *cx, JSObject *obj, jsSyncPropertySpec
 
 	for(i=0;props[i].name;i++) 
 		if(!JS_DefinePropertyWithTinyId(cx, obj, 
-			props[i].name,props[i].tinyid, JSVAL_VOID, NULL, NULL, props[i].flags|JSPROP_SHARED))
+			props[i].name,props[i].tinyid, JSVAL_VOID, NULL, NULL, props[i].flags))
 			return(JS_FALSE);
 
 	return(JS_TRUE);
@@ -884,11 +884,10 @@ bool sbbs_t::js_init()
 		JS_SetContextPrivate(js_cx, this);	/* Store a pointer to sbbs_t instance */
 
 		/* Global Objects (including system, js, client, Socket, MsgBase, File, User, etc. */
-		if((js_glob=js_CreateCommonObjects(js_cx, &scfg, &cfg, js_global_functions
+		if((js_glob=js_CreateGlobalObjects(js_cx, &scfg, &cfg, js_global_functions
 					,uptime, startup->host_name, SOCKLIB_DESC	/* system */
 					,&js_branch									/* js */
 					,&client, client_socket						/* client */
-					,&js_server_props							/* server */
 			))==NULL)
 			break;
 
@@ -904,6 +903,9 @@ bool sbbs_t::js_init()
 
 		/* Console Object */
 		if(js_CreateConsoleObject(js_cx, js_glob)==NULL)
+			break;
+
+		if(js_CreateServerObject(js_cx,js_glob,&js_server_props)==NULL)
 			break;
 
 		success=true;
