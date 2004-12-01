335586d921dc612717adcbf81fdd3503bfc16bb6
deuce <deuce>
2004-12-01 06:49:50 +0000

    Force GC after a script is ran.

--- sbbs3/zmodem.h
+++ sbbs3/zmodem.h
@@ -4,12 +4,10 @@
  * (C) Mattheij Computer Service 1994
  */
 
-/* $Id: zmodem.h,v 1.8 2005/01/15 13:19:32 rswindell Exp $ */
+/* $Id: zmodem.h,v 1.3 2003/09/17 03:51:58 rswindell Exp $ */
 
 #ifndef _ZMODEM_H
 #define _ZMODEM_H
-
-#include <stdio.h>	/* FILE */
 
 /*
  * ascii constants
@@ -228,9 +226,8 @@ typedef struct {
 	int n_bytes_remaining;
 	unsigned char tx_data_subpacket[MAX_SUBPACKETSIZE];
 
-	ulong current_file_size;
+	long current_file_size;
 	time_t transfer_start;
-	time_t last_status;
 
 	int receive_32_bit_data;
 	int raw_trace;
@@ -243,39 +240,19 @@ typedef struct {
 	int n_cans;
 
 	/* Stuff added by RRS */
-
-	/* Status */
-	BOOL		cancelled;
-	BOOL		file_skipped;
-	ulong		sent_successfully;
-
-	/* Configuration */
-	long*		mode;
-	unsigned	send_timeout;
-	unsigned	recv_timeout;
-	unsigned	max_errors;
-	time_t		progress_interval;
-
-	/* Callbacks */
-	void*		cbdata;
-	int			(*lputs)(void*, int level, const char* str);
-	int			(*send_byte)(void*, uchar ch, unsigned timeout);
-	int			(*recv_byte)(void*, unsigned timeout);
-	void		(*progress)(void*, ulong offset, ulong fsize, time_t t);
+	SOCKET	sock;					/* socket descriptor */
+	long	mode;
+	FILE*	statfp;
+	FILE*	errfp;
 
 } zmodem_t;
 
-void		zmodem_init(zmodem_t*, void* cbdata, long* mode
-						,int	(*lputs)(void*, int level, const char* str)
-						,void	(*progress)(void*, ulong offset, ulong fsize, time_t t)
-						,int	(*send_byte)(void*, uchar ch, unsigned timeout)
-						,int	(*recv_byte)(void*, unsigned timeout));
 char*		zmodem_ver(char *buf);
 const char* zmodem_source(void);
-int			zmodem_get_zrinit(zmodem_t*);
-void		zmodem_parse_zrinit(zmodem_t*);
-int			zmodem_send_zfin(zmodem_t*);
-int			zmodem_send_file(zmodem_t*, char* name, FILE* fp, BOOL request_init);
+int			zmodem_get_zrinit(zmodem_t* zm);
+void		zmodem_parse_zrinit(zmodem_t* zm);
+int			zmodem_send_zfin(zmodem_t* zm);
+int			zmodem_send_file(zmodem_t* zm, char* name, FILE* fp);
 
 #endif
 
