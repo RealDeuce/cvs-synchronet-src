38e7e78649660c19fe0cde486ff4d30a8ce5381c
rswindell <rswindell>
2004-12-07 04:39:29 +0000

    Make all msg_area and file_area number properties relative (to what the user has access to). Created file_area.dir[].lib_name property.

--- sbbs3/sbbs.h
+++ sbbs3/sbbs.h
@@ -2,7 +2,7 @@
 
 /* Synchronet class (sbbs_t) definition and exported function prototypes */
 
-/* $Id: sbbs.h,v 1.240 2004/12/02 09:03:59 rswindell Exp $ */
+/* $Id: sbbs.h,v 1.241 2004/12/07 04:39:29 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -987,7 +987,8 @@ extern "C" {
 	DLLEXPORT JSObject* DLLCALL js_CreateMsgAreaObject(JSContext* cx, JSObject* parent, scfg_t* cfg
 													,user_t* user, subscan_t* subscan);
 	DLLEXPORT BOOL		DLLCALL js_CreateMsgAreaProperties(JSContext* cx, scfg_t* cfg
-													,JSObject* subobj, uint subnum);
+													,JSObject* subobj, uint subnum
+													,uint usrgrpnum, uint usrsubnum);
 
 	/* js_xtrn_area.c */
 	DLLEXPORT JSObject* DLLCALL js_CreateXtrnAreaObject(JSContext* cx, JSObject* parent, scfg_t* cfg
