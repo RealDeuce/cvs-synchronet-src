85f2a67af84036ae662b0b1332feb1bda6b1f1ad
deuce <deuce>
2004-12-18 18:20:23 +0000

    Add gmake makefiles.

--- sbbs3/js_queue.c
+++ sbbs3/js_queue.c
@@ -2,7 +2,7 @@
 
 /* Synchronet JavaScript "Queue" Object */
 
-/* $Id: js_queue.c,v 1.5 2004/11/11 03:46:12 rswindell Exp $ */
+/* $Id: js_queue.c,v 1.8 2004/11/18 01:00:54 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -42,11 +42,9 @@ typedef struct
 {
 	char	name[128];
 	int		type;
-	size_t	length;
 	union {
 		JSBool		b;
-		int32		i;
-		jsdouble	d;
+		jsdouble	n;
 		char*		s;
 	} value;
 } queued_value_t;
@@ -65,38 +63,59 @@ static void js_finalize_queue(JSContext *cx, JSObject *obj)
 	if((q=(msg_queue_t*)JS_GetPrivate(cx,obj))==NULL)
 		return;
 	
-	if(msgQueueDetach(q)==0 && (n=listFindNode(&named_queues,q,0))!=NULL)
-		listRemoveNode(&named_queues,n);
+	if(msgQueueDetach(q)==0 && (n=listFindNode(&named_queues,q,/* length=0 for ptr compare */0))!=NULL)
+		listRemoveNode(&named_queues,n,TRUE);
 
 	JS_SetPrivate(cx, obj, NULL);
 }
 
-static void parse_queued_value(JSContext *cx, queued_value_t* v, jsval* rval, BOOL peek)
+static size_t js_decode_value(JSContext *cx, JSObject *parent
+							   ,queued_value_t* v, jsval* rval, BOOL peek)
 {
+	size_t			count=1;
+	size_t			decoded;
+	queued_value_t* pv;
+	queued_value_t	term;
+	jsval	prop_val;
+	jsuint	index=0;
+	JSObject *obj;
+
+	ZERO_VAR(term);
+
 	*rval = JSVAL_VOID;
 
-	if(v==NULL)
-		return;
+	if(v==NULL || v->type==JSTYPE_VOID)
+		return(count);
 
 	switch(v->type) {
-		case JSVAL_BOOLEAN:
+		case JSTYPE_BOOLEAN:
 			*rval = BOOLEAN_TO_JSVAL(v->value.b);
 			break;
-		case JSVAL_INT:
-			JS_NewNumberValue(cx,v->value.i,rval);
+		case JSTYPE_NUMBER:
+			JS_NewNumberValue(cx,v->value.n,rval);
 			break;
-		case JSVAL_DOUBLE:
-			JS_NewNumberValue(cx,v->value.d,rval);
-			break;
-		case JSVAL_STRING:
+		case JSTYPE_STRING:
 			if(v->value.s) {
 				*rval = STRING_TO_JSVAL(JS_NewStringCopyZ(cx,v->value.s));
 				if(!peek)
 					free(v->value.s);
 			}
 			break;
+		case JSTYPE_ARRAY:
+		case JSTYPE_OBJECT:
+			obj = JS_DefineObject(cx, parent, v->name, NULL, NULL
+				,JSPROP_ENUMERATE);
+			for(pv=v+1,count++;memcmp(pv,&term,sizeof(term));pv+=decoded,count+=decoded) {
+				decoded=js_decode_value(cx,obj,pv,&prop_val,peek);
+				if(v->type==JSTYPE_ARRAY)
+					JS_SetElement(cx,obj,index++,&prop_val);
+				else
+					JS_DefineProperty(cx, obj, pv->name, prop_val,NULL,NULL,JSPROP_ENUMERATE);
+			}
+			*rval = OBJECT_TO_JSVAL(obj);
+			break;
 	}
-	free(v);
+	return(count);
 }
 
 /* Queue Object Methods */
@@ -145,7 +164,9 @@ js_read(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	} else
 		v=msgQueueRead(q, /* timeout */0);
 
-	parse_queued_value(cx, v, rval, /* peek */FALSE);
+	js_decode_value(cx, obj, v, rval, /* peek */FALSE);
+
+	FREE_AND_NULL(v);
 
 	return(JS_TRUE);
 }
@@ -163,40 +184,98 @@ js_peek(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 
 	v=msgQueuePeek(q, /* timeout */0);
 
-	parse_queued_value(cx, v, rval, /* peek */TRUE);
+	js_decode_value(cx, obj, v, rval, /* peek */TRUE);
+
+	FREE_AND_NULL(v);
 
 	return(JS_TRUE);
 }
 
-BOOL js_enqueue_value(JSContext *cx, msg_queue_t* q, jsval val, char* name)
+static queued_value_t* js_encode_value(JSContext *cx, jsval val, char* name
+									   ,queued_value_t* v, size_t* count)
 {
-	queued_value_t v;
+	jsint       i;
+	jsval		prop_name;
+	jsval		prop_val;
+    JSObject*	obj;
+	JSIdArray*	id_array;
+	queued_value_t* nv;
 
-	ZERO_VAR(v);
+	if((nv=realloc(v,((*count)+1)*sizeof(queued_value_t)))==NULL) {
+		if(v) free(v);
+		return(NULL);
+	}
+	v=nv;
+	nv=v+(*count);
+	memset(nv,0,sizeof(queued_value_t));
+	(*count)++;
 
 	if(name!=NULL)
-		SAFECOPY(v.name,name);
+		SAFECOPY(nv->name,name);
 
-	switch(v.type=JSVAL_TAG(val)) {
+	switch(JSVAL_TAG(val)) {
 		case JSVAL_BOOLEAN:
-			v.length = sizeof(JSBool);
-			v.value.b=JSVAL_TO_BOOLEAN(val);
+			nv->type=JSTYPE_BOOLEAN;
+			nv->value.b=JSVAL_TO_BOOLEAN(val);
 			break;
-		case JSVAL_DOUBLE:
-			v.length =sizeof(jsdouble);
-			v.value.d = *JSVAL_TO_DOUBLE(val);
+		case JSVAL_OBJECT:
+			nv->type=JSTYPE_OBJECT;
+			obj = JSVAL_TO_OBJECT(val);
+
+			if(JSVAL_IS_NULL(val))
+				break;
+
+			if(JS_IsArrayObject(cx, obj))
+				nv->type=JSTYPE_ARRAY;
+
+			if((id_array=JS_Enumerate(cx,obj))==NULL) {
+				free(v);
+				return(NULL);
+			}
+			for(i=0; i<id_array->length; i++)  {
+				/* property name */
+				JS_IdToValue(cx,id_array->vector[i],&prop_name);
+				if(JSVAL_IS_STRING(prop_name)) {
+					name=JS_GetStringBytes(JSVAL_TO_STRING(prop_name));
+					/* value */
+					JS_GetProperty(cx,obj,name,&prop_val);
+				} else {
+					name=NULL;
+					JS_GetElement(cx,obj,i,&prop_val);
+				}
+				if((v=js_encode_value(cx,prop_val,name,v,count))==NULL)
+					break;
+			}
+			v=js_encode_value(cx,JSVAL_VOID,NULL,v,count);	/* terminate object */
 			break;
 		default:
 			if(JSVAL_IS_NUMBER(val)) {
-				v.type = JSVAL_INT;
-				JS_ValueToInt32(cx,val,&v.value.i);
+				nv->type = JSTYPE_NUMBER;
+				JS_ValueToNumber(cx,val,&nv->value.n);
+			} else if(JSVAL_IS_VOID(val)) {
+				nv->type = JSTYPE_VOID;
 			} else {
-				v.type= JSVAL_STRING;
-				v.value.s = strdup(JS_GetStringBytes(JS_ValueToString(cx,val)));
+				nv->type= JSTYPE_STRING;
+				nv->value.s = strdup(JS_GetStringBytes(JS_ValueToString(cx,val)));
 			}
 			break;
 	}
-	return(msgQueueWrite(q,&v,sizeof(v)));
+
+	return(v);
+}
+
+BOOL js_enqueue_value(JSContext *cx, msg_queue_t* q, jsval val, char* name)
+{
+	queued_value_t* v;
+	size_t			count=0;
+	BOOL			result;
+
+	if((v=js_encode_value(cx,val,name,NULL,&count))==NULL || count<1)
+		return(FALSE);
+
+	result=msgQueueWrite(q,v,count*sizeof(queued_value_t));
+	free(v);
+	return(result);
 }
 
 static JSBool
--- xpdev/msg_queue.c
+++ xpdev/msg_queue.c
@@ -2,7 +2,7 @@
 
 /* Uni or Bi-directional FIFO message queue */
 
-/* $Id: msg_queue.c,v 1.7 2004/11/11 06:12:06 rswindell Exp $ */
+/* $Id: msg_queue.c,v 1.10 2004/11/19 00:51:25 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -39,7 +39,7 @@
 #include <string.h>		/* memset */
 
 #include "genwrap.h"	/* msclock() */
-#include "threadwrap.h"	/* GetCurrentThreadId */
+#include "threadwrap.h"	/* pthread_self */
 #include "msg_queue.h"
 
 msg_queue_t* msgQueueInit(msg_queue_t* q, long flags)
@@ -54,11 +54,11 @@ msg_queue_t* msgQueueInit(msg_queue_t* q, long flags)
 
 	q->flags = flags;
 	q->refs = 1;
-	q->owner_thread_id = GetCurrentThreadId();
+	q->owner_thread_id = pthread_self();
 
 	if(q->flags&MSG_QUEUE_BIDIR)
-		listInit(&q->in,LINK_LIST_DONT_FREE|LINK_LIST_SEMAPHORE);
-	listInit(&q->out,LINK_LIST_DONT_FREE|LINK_LIST_SEMAPHORE);
+		listInit(&q->in,LINK_LIST_SEMAPHORE);
+	listInit(&q->out,LINK_LIST_SEMAPHORE);
 
 	return(q);
 }
@@ -125,7 +125,7 @@ static link_list_t* msgQueueReadList(msg_queue_t* q)
 		return(NULL);
 
 	if((q->flags&MSG_QUEUE_BIDIR)
-		&& q->owner_thread_id == GetCurrentThreadId())
+		&& q->owner_thread_id == pthread_self())
 		return(&q->in);
 	return(&q->out);
 }
@@ -136,7 +136,7 @@ static link_list_t* msgQueueWriteList(msg_queue_t* q)
 		return(NULL);
 
 	if(!(q->flags&MSG_QUEUE_BIDIR)
-		|| q->owner_thread_id == GetCurrentThreadId())
+		|| q->owner_thread_id == pthread_self())
 		return(&q->out);
 	return(&q->in);
 }
@@ -150,11 +150,11 @@ static BOOL list_wait(link_list_t* list, long timeout)
 {
 #if defined(LINK_LIST_THREADSAFE)
 	if(timeout<0)	/* infinite */
-		return listSemWait(list)==0;
+		return listSemWait(list);
 	if(timeout==0)	/* poll */
-		return listSemTryWait(list)==0;
+		return listSemTryWait(list);
 
-	return listSemTryWaitBlock(list,timeout)==0;
+	return listSemTryWaitBlock(list,timeout);
 #else
 	clock_t	start;
 	long	count;
@@ -181,7 +181,7 @@ void* msgQueueRead(msg_queue_t* q, long timeout)
 	if(!list_wait(msgQueueReadList(q),timeout))
 		return(NULL);
 
-	return listPopFirstNode(msgQueueReadList(q));
+	return listShiftNode(msgQueueReadList(q));
 }
 
 void* msgQueuePeek(msg_queue_t* q, long timeout)
@@ -199,7 +199,7 @@ void* msgQueueFind(msg_queue_t* q, const void* data, size_t length)
 
 	if((node=listFindNode(list,data,length))==NULL)
 		return(NULL);
-	return listRemoveNode(list,node);
+	return listRemoveNode(list,node,/* Free Data? */FALSE);
 }
 
 list_node_t* msgQueueFirstNode(msg_queue_t* q)
