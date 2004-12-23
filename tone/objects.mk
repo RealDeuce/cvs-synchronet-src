fdd08d6bfff6648a07040318906c8bf858e2f1dd
rswindell <rswindell>
2004-12-23 22:28:03 +0000

    Support ctrl/<host>.ini in addition to ctrl/<host.domain>.ini in sbbs_get_ini_fname().

--- tone/objects.mk
+++ tone/objects.mk
@@ -2,10 +2,9 @@
 
 # Make 'include file' listing object files for xpdev "wrappers"
 
-# $Id: objects.mk,v 1.2 2005/01/21 03:07:04 deuce Exp $
+# $Id: objects.mk,v 1.1 2004/09/12 22:12:00 deuce Exp $
 
 # OBJODIR, SLASH, and OFILE must be pre-defined
 
 OBJS	= \
 	$(OBJODIR)$(DIRSEP)tone$(OFILE)
-
