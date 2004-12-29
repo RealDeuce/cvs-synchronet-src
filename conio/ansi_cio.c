6c0ea95c35269a12ab60be2dc7ad394976cccae2
rswindell <rswindell>
2004-12-29 04:30:39 +0000

    smb_addhashes() no longer leaves .hash file open on write failure. Eliminated redundant strdup() call in smb_hashstr()/strip_chars(). Eliminated redundant memset() call in smb_getmsgidx_by_hash().

--- conio/ansi_cio.c
+++ conio/ansi_cio.c
@@ -1,4 +1,4 @@
-/* $Id: ansi_cio.c,v 1.38 2005/01/24 10:46:48 deuce Exp $ */
+/* $Id: ansi_cio.c,v 1.37 2004/12/22 11:06:46 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -732,12 +732,7 @@ int ansi_initciolib(long inmode)
 	if (isatty(STDIN_FILENO))  {
 		tcgetattr(STDIN_FILENO,&tio_default);
 		tio_raw = tio_default;
-		/* cfmakeraw(&tio_raw); */
-		tio_raw.c_iflag &= ~(IMAXBEL|IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
-		tio_raw.c_oflag &= ~OPOST;
-		tio_raw.c_lflag &= ~(ECHO|ECHONL|ICANON|ISIG|IEXTEN);
-		tio_raw.c_cflag &= ~(CSIZE|PARENB);
-		tio_raw.c_cflag |= CS8;
+		cfmakeraw(&tio_raw);
 		tcsetattr(STDIN_FILENO,TCSANOW,&tio_raw);
 		setvbuf(stdout, NULL, _IONBF, 0);
 		atexit(ansi_fixterm);
