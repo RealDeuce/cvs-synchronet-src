3c2af90e5bfd86465b24f6c94340066064943200
rswindell <rswindell>
2004-12-31 22:26:54 +0000

    Verify sbbs.dll is v3.12a or later.

--- sbbs3/ctrl/MainFormUnit.cpp
+++ sbbs3/ctrl/MainFormUnit.cpp
@@ -1,6 +1,6 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: MainFormUnit.cpp,v 1.142 2004/12/30 23:07:48 rswindell Exp $ */
+/* $Id: MainFormUnit.cpp,v 1.143 2004/12/31 22:26:54 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -968,7 +968,7 @@ void __fastcall TMainForm::FormCreate(TObject *Sender)
 
     // Verify SBBS.DLL version
     long bbs_ver = bbs_ver_num();
-    if(bbs_ver < (0x31000 | 'M'-'A') || bbs_ver > (0x399<<8)) {
+    if(bbs_ver < (0x31200 | 'A'-'A') || bbs_ver > (0x399<<8)) {
         char str[128];
         sprintf(str,"Incorrect SBBS.DLL Version (%lX)",bbs_ver);
     	Application->MessageBox(str,"ERROR",MB_OK|MB_ICONEXCLAMATION);
