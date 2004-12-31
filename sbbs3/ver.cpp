91b2d4e39082c979c04298e0ccc70f6f19a2adeb
rswindell <rswindell>
2004-12-31 11:47:43 +0000

    Removed "beta" tag (uh oh...)

--- sbbs3/ver.cpp
+++ sbbs3/ver.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet version display */
 
-/* $Id: ver.cpp,v 1.22 2003/12/07 03:53:12 rswindell Exp $ */
+/* $Id: ver.cpp,v 1.23 2004/12/31 11:47:43 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -37,7 +37,7 @@
 
 #include "sbbs.h"
 
-extern "C" const char* beta_version = " beta"; /* Space if non-beta, " beta" otherwise */
+extern "C" const char* beta_version = " "; /* Space if non-beta, " beta" otherwise */
 
 #if defined(_WINSOCKAPI_)
 	extern WSADATA WSAData;
