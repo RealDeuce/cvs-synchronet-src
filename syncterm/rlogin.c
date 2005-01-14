34bafa632f09c90f6fba51c885de4ccff44b9b00
rswindell <rswindell>
2005-01-14 00:39:04 +0000

    Rename WaitEvent() to WaitForEvent() - more closely resembles Win32 function.

--- syncterm/rlogin.c
+++ syncterm/rlogin.c
@@ -3,19 +3,20 @@
 
 #include "uifcinit.h"
 #include "bbslist.h"
-#include "conn.h"
+
+static SOCKET	rlogin_socket=INVALID_SOCKET;
 
 int rlogin_recv(char *buffer, size_t buflen)
 {
 	int	r;
-	size_t	avail;
+	int	avail;
 	int rd;
 
-	if(!socket_check(conn_socket, NULL, NULL, 0))
+	if(!socket_check(rlogin_socket, NULL, NULL, 0))
 		return(-1);
 
-	if(!ioctlsocket(conn_socket,FIONREAD,(void *)&avail) && avail)
-		r=recv(conn_socket,buffer,avail<buflen?avail:buflen,0);
+	if(!ioctlsocket(rlogin_socket,FIONREAD,(void *)&avail) && avail)
+		r=recv(rlogin_socket,buffer,avail<buflen?avail:buflen,0);
 	else
 		return(0);
 
@@ -26,16 +27,16 @@ int rlogin_recv(char *buffer, size_t buflen)
 
 int rlogin_send(char *buffer, size_t buflen, unsigned int timeout)
 {
-	size_t sent=0;
+	int sent=0;
 	int	ret;
 	int	i;
 
 	while(sent<buflen) {
-		if(!socket_check(conn_socket, NULL, &i, timeout))
+		if(!socket_check(rlogin_socket, NULL, &i, timeout))
 			return(-1);
 		if(!i)
 			return(-1);
-		ret=send(conn_socket,buffer+sent,buflen-sent,0);
+		ret=send(rlogin_socket,buffer+sent,buflen-sent,0);
 		if(ret==-1) {
 			switch(errno) {
 				case EAGAIN:
@@ -52,15 +53,11 @@ int rlogin_send(char *buffer, size_t buflen, unsigned int timeout)
 	return(0);
 }
 
-int rlogin_close(void)
-{
-	return(closesocket(conn_socket));
-}
-
-int rlogin_connect(char *addr, int port, char *ruser, char *passwd)
+int rlogin_connect(char *addr, int port, char *ruser, char *passwd, int bedumb)
 {
 	HOSTENT *ent;
 	SOCKADDR_IN	saddr;
+	char	nil=0;
 	char	*p;
 	unsigned int	neta;
 	int	i;
@@ -83,8 +80,8 @@ int rlogin_connect(char *addr, int port, char *ruser, char *passwd)
 		}
 		neta=*((unsigned int*)ent->h_addr_list[0]);
 	}
-	conn_socket=socket(PF_INET, SOCK_STREAM, IPPROTO_IP);
-	if(conn_socket==INVALID_SOCKET) {
+	rlogin_socket=socket(PF_INET, SOCK_STREAM, IPPROTO_IP);
+	if(rlogin_socket==INVALID_SOCKET) {
 		uifcmsg("Cannot create socket!",	"`Unable to create socket`\n\n"
 											"Your system is either dangerously low on resources, or there"
 											"is a problem with your TCP/IP stack.");
@@ -95,20 +92,26 @@ int rlogin_connect(char *addr, int port, char *ruser, char *passwd)
 	saddr.sin_family = AF_INET;
 	saddr.sin_port   = htons(port);
 	
-	if(connect(conn_socket, (struct sockaddr *)&saddr, sizeof(saddr))) {
+	if(connect(rlogin_socket, (struct sockaddr *)&saddr, sizeof(saddr))) {
 		char str[LIST_ADDR_MAX+20];
 
 		rlogin_close();
 		sprintf(str,"Cannot connect to %s!",addr);
 		uifcmsg(str,	"`Unable to connect`\n\n"
-						"Cannot connect to the remote system... it is down or unreachable.");
+						"Cannot connect to the remost system... it is down or unreachable.");
 		return(-1);
 	}
 
-	rlogin_send("",1,1000);
-	rlogin_send(passwd,strlen(passwd)+1,1000);
-	rlogin_send(ruser,strlen(ruser)+1,1000);
-	rlogin_send("ansi-bbs/9600",14,1000);
-
+	if(!bedumb) {
+		rlogin_send("",1,1000);
+		rlogin_send(passwd,strlen(passwd)+1,1000);
+		rlogin_send(ruser,strlen(ruser)+1,1000);
+		rlogin_send("ansi-bbs/9600",14,1000);
+	}
 	return(0);
+}
+
+int rlogin_close(void)
+{
+	return(closesocket(rlogin_socket));
 }
--- xpdev/xpevent.c
+++ xpdev/xpevent.c
@@ -2,7 +2,7 @@
 
 /* *nix emulation of Win32 *Event API */
 
-/* $Id: xpevent.c,v 1.6 2005/01/14 19:25:55 deuce Exp $ */
+/* $Id: xpevent.c,v 1.5 2005/01/14 00:39:04 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -76,10 +76,7 @@ CreateEvent(void *sec, BOOL bManualReset, BOOL bInitialState, void *name)
 BOOL
 SetEvent(xpevent_t event)
 {
-	if (event==NULL || (event->magic != EVENT_MAGIC)) {
-		errno = EINVAL;
-		return(FALSE);
-	}
+	_EVENT_CHECK_VALIDITY(event)
 
 	pthread_mutex_lock(&event->lock);
 
@@ -102,10 +99,7 @@ SetEvent(xpevent_t event)
 BOOL
 ResetEvent(xpevent_t event)
 {
-	if (event==NULL || (event->magic != EVENT_MAGIC)) {
-		errno = EINVAL;
-		return(FALSE);
-	}
+	_EVENT_CHECK_VALIDITY(event)
 
 	pthread_mutex_lock(&event->lock);
 
@@ -119,10 +113,7 @@ ResetEvent(xpevent_t event)
 BOOL
 CloseEvent(xpevent_t event)
 {
-	if (event==NULL || (event->magic != EVENT_MAGIC)) {
-		errno = EINVAL;
-		return(FALSE);
-	}
+	_EVENT_CHECK_VALIDITY(event)
 
 	/* Make sure there are no waiters. */
 	pthread_mutex_lock(&event->lock);
