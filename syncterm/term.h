81d10fdbffcb78ac63d0bb582c15fdcba0e4ff08
rswindell <rswindell>
2005-01-19 05:46:54 +0000

    More modularization (e.g. created xmodem_send_file).

--- syncterm/term.h
+++ syncterm/term.h
@@ -1,20 +1,16 @@
 #ifndef _TERM_H_
 #define _TERM_H_
 
-#include "bbslist.h"
-
 struct terminal {
 	int	height;
 	int	width;
 	int	x;
 	int	y;
-	int nostatus;
 };
 
 extern struct terminal term;
 extern int backlines;
 
-void doterm(struct bbslist *);
-void mousedrag(unsigned char *scrollback);
+void doterm(void);
 
 #endif
