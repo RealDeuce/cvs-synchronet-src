d236e0c913966b5bb2729a6950ca5ddf0932511a
rswindell <rswindell>
2005-01-21 03:32:18 +0000

    Added "-x" option to choose xpbeep() over Beep() on Win32.

--- odoors/ODCom.c
+++ odoors/ODCom.c
@@ -2987,9 +2987,8 @@ keep_going:
 		fd_set  fdset;
 		struct  timeval tv;
 		int             retval=-1;
-		int	loopcount=0;
 
-		while(retval==-1 && loopcount < 10) {
+		while(retval==-1) {
 			FD_ZERO(&fdset);
 			FD_SET(STDOUT_FILENO,&fdset);
 
@@ -2998,11 +2997,6 @@ keep_going:
 
 			retval=select(STDOUT_FILENO+1,NULL,&fdset,NULL,&tv);
 			if(retval!=1) {
-				if(retval==0)  {
-					retval=-1;
-					loopcount++;
-					continue;
-				}
 				if(retval==-1 && errno==EINTR)
 					continue;
 				return(kODRCGeneralFailure);
@@ -3478,7 +3472,6 @@ try_again:
 			fd_set  fdset;
 			struct  timeval tv;
 			int     retval;
-			int	loopcount=0;
 
 			while(pos<nSize) {
 				FD_ZERO(&fdset);
@@ -3489,11 +3482,6 @@ try_again:
 
 				retval=select(STDOUT_FILENO+1,NULL,&fdset,NULL,&tv);
 				if(retval!=1) {
-					if(retval==0) {
-						if(++loopcount>10)
-							return(kODRCGeneralFailure);
-						continue;
-					}
 					if(retval==-1 && errno==EINTR)
 						continue;
 					return(kODRCGeneralFailure);
