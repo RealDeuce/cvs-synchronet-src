6e2c3c3808aa0fca1403de56f61ec8258c31be28
rswindell <rswindell>
2005-02-15 09:21:01 +0000

    Likely bug-fix: word_wrap() can actually triple the number of bytes in a buffer.

--- sbbs3/xmodem.h
+++ sbbs3/xmodem.h
@@ -2,7 +2,7 @@
 
 /* Synchronet X/YMODEM Functions */
 
-/* $Id: xmodem.h,v 1.5 2005/01/12 22:31:56 rswindell Exp $ */
+/* $Id: xmodem.h,v 1.12 2005/02/01 10:13:38 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -38,28 +38,52 @@
 #ifndef _XMODEM_H_
 #define _XMODEM_H_
 
-#include <stdio.h>		/* FILE */
-#include <sockwrap.h>	/* SOCKET */
-#include "crc16.h"
+#include "gen_defs.h"
+
+#define CPMEOF		CTRL_Z	/* CP/M End of file (^Z)					*/
 
 typedef struct {
 
-	SOCKET		sock;	/* socket descriptor */
+	void*		cbdata;
 	long*		mode;
+	BOOL		cancelled;
 	unsigned	block_size;
 	unsigned	ack_timeout;
 	unsigned	byte_timeout;
-	int			(*lprintf)(int level, char *fmt, ...);
+	unsigned	send_timeout;
+	unsigned	recv_timeout;
+	unsigned	max_errors;
+	unsigned	g_delay;
+	unsigned	total_files;
+	unsigned	total_bytes;
+	unsigned	sent_files;
+	unsigned	sent_bytes;
+	int			(*lputs)(void*, int level, const char* str);
+	void		(*progress)(void*, unsigned block_num, ulong offset, ulong fsize, time_t t);
+	int			(*send_byte)(void*, uchar ch, unsigned timeout);
+	int			(*recv_byte)(void*, unsigned timeout);
+	BOOL		(*is_connected)(void*);
 
 } xmodem_t;
 
 
+void		xmodem_init(xmodem_t*, void* cbdata, long* mode
+						,int	(*lputs)(void*, int level, const char* str)
+						,void	(*progress)(void* unused, unsigned block_num, ulong offset, ulong fsize, time_t t)
+						,int	(*send_byte)(void*, uchar ch, unsigned timeout)
+						,int	(*recv_byte)(void*, unsigned timeout)
+						,BOOL	(*is_connected)(void*)
+						);
 char*		xmodem_ver(char *buf);
 const char* xmodem_source(void);
-void		xmodem_cancel(xmodem_t* xm);
-int			xmodem_get_ack(xmodem_t* xm, int tries);
-void		xmodem_put_nak(xmodem_t* xm);
-int			xmodem_get_block(xmodem_t* xm, uchar* block, BOOL hdrblock);
-void		xmodem_put_block(xmodem_t* xm, uchar* block, uint block_size, ulong block_num);
+void		xmodem_cancel(xmodem_t*);
+BOOL		xmodem_get_ack(xmodem_t*, unsigned tries, unsigned block_num);
+BOOL		xmodem_get_mode(xmodem_t*);
+BOOL		xmodem_put_eot(xmodem_t*);
+void		xmodem_put_ack(xmodem_t*);
+void		xmodem_put_nak(xmodem_t*, unsigned block_num);
+int			xmodem_get_block(xmodem_t*, uchar* block, unsigned block_num);
+void		xmodem_put_block(xmodem_t*, uchar* block, unsigned block_size, unsigned block_num);
+BOOL		xmodem_send_file(xmodem_t* xm, const char* fname, FILE* fp, time_t* start, ulong* sent);
 
 #endif	/* Don't add anything after this line */
