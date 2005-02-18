9500b327938505596ed6cd871e30be063dd56355
rswindell <rswindell>
2005-02-18 09:32:22 +0000

    No need to save registry settings here (only .ini settings).

--- conio/ciolib.h
+++ conio/ciolib.h
@@ -1,4 +1,4 @@
-/* $Id: ciolib.h,v 1.20 2004/10/17 02:07:04 deuce Exp $ */
+/* $Id: ciolib.h,v 1.22 2005/01/23 23:30:04 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -34,6 +34,7 @@
 #ifndef _CIOLIB_H_
 #define _CIOLIB_H_
 
+#include <string.h>	/* size_t */
 #include "mouse.h"
 
 enum {
@@ -193,6 +194,8 @@ typedef struct {
 	int		(*hidemouse)	(void);
 	int		(*showmouse)	(void);
 	void	(*settitle)		(const char *);
+	void	(*copytext)		(const char *, size_t);
+	char 	*(*getcliptext)	(void);
 } cioapi_t;
 
 extern cioapi_t cio_api;
@@ -243,6 +246,8 @@ void settitle(const char *title);
 int ciolib_showmouse(void);
 int ciolib_hidemouse(void);
 void ciolib_settitle(const char *title);
+void ciolib_copytext(const char *text, size_t buflen);
+char *ciolib_getcliptext(void);
 #ifdef __cplusplus
 }
 #endif
@@ -286,6 +291,8 @@ void ciolib_settitle(const char *title);
 	#define	hidemouse()				ciolib_hidemouse()
 	#define showmouse()				ciolib_showmouse()
 	#define settitle(a)				ciolib_settitle(a)
+	#define copytext(a,b)			ciolib_copytext(a,b)
+	#define getcliptext()		ciolib_getcliptext()
 #endif
 
 #endif	/* Do not add anything after this line */
