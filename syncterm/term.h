763d7ab23769a3f27085cf2c487a23552049328d
deuce <deuce>
2005-03-13 15:58:21 +0000

    Add error page sending for a couple missing bits.

--- syncterm/term.h
+++ syncterm/term.h
@@ -1,16 +1,20 @@
 #ifndef _TERM_H_
 #define _TERM_H_
 
+#include "bbslist.h"
+
 struct terminal {
 	int	height;
 	int	width;
 	int	x;
 	int	y;
+	int nostatus;
 };
 
 extern struct terminal term;
 extern int backlines;
 
-void doterm(void);
+void doterm(struct bbslist *);
+void mousedrag(unsigned char *scrollback);
 
 #endif
