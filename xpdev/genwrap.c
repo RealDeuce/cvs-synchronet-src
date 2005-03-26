c9c9d0179fd6739e38fb45260307b7e9dc22672b
rswindell <rswindell>
2005-03-26 06:51:15 +0000

    Minor optimization to unescape_char() function.

--- xpdev/genwrap.c
+++ xpdev/genwrap.c
@@ -2,13 +2,13 @@
 
 /* General cross-platform development wrappers */
 
-/* $Id: genwrap.c,v 1.50 2005/02/09 05:10:51 rswindell Exp $ */
+/* $Id: genwrap.c,v 1.51 2005/03/26 06:51:15 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -92,10 +92,6 @@ char* DLLCALL lastchar(const char* str)
 char DLLCALL unescape_char(char ch)
 {
 	switch(ch) {
-		case '\\':	return('\\');
-		case '\'':	return('\'');
-		case '"':	return('"');
-		case '?':	return('?');
 		case 'a':	return('\a');
 		case 'b':	return('\b');
 		case 'f':	return('\f');
