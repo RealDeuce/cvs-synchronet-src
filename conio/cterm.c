facab7fb0dfd50f2420a511449685bdaf73518ff
deuce <deuce>
2005-04-07 03:50:34 +0000

    Make Win32 build create gac_bj32.exe as required by the auto-update.

--- conio/cterm.c
+++ conio/cterm.c
@@ -1,4 +1,4 @@
-/* $Id: cterm.c,v 1.21 2005/05/19 23:54:16 deuce Exp $ */
+/* $Id: cterm.c,v 1.19 2005/02/18 08:48:06 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -381,11 +381,17 @@ void clear2bol(void)
 
 void clear2eol(void)
 {
+	char *buf;
+	int i,j;
+
 	clreol();
 }
 
 void clearscreen(char attr)
 {
+	char *buf;
+	int x,y,j;
+
 	if(cterm.scrollback!=NULL) {
 		cterm.backpos+=cterm.height;
 		if(cterm.backpos>cterm.backlines) {
@@ -805,6 +811,7 @@ void ctputs(char *buf)
 {
 	char *outp;
 	char *p;
+	char outline[80];
 	int		oldscroll;
 	int		cx;
 	int		cy;
@@ -886,12 +893,14 @@ char *cterm_write(unsigned char *buf, int buflen, char *retbuf, int retsize)
 {
 	unsigned char ch[2];
 	unsigned char prn[BUFSIZE];
-	int j,k;
+	int	key;
+	int i,j,k;
+	char	*ret;
 	struct text_info	ti;
 	int	olddmc;
 
-	olddmc=hold_update;
-	hold_update=1;
+	olddmc=dont_move_cursor;
+	dont_move_cursor=1;
 	if(retbuf!=NULL)
 		retbuf[0]=0;
 	gettextinfo(&ti);
@@ -977,7 +986,7 @@ char *cterm_write(unsigned char *buf, int buflen, char *retbuf, int retsize)
 	textattr(ti.attribute);
 #endif
 
-	hold_update=olddmc;
+	dont_move_cursor=olddmc;
 	gotoxy(wherex(),wherey());
 	return(retbuf);
 }
