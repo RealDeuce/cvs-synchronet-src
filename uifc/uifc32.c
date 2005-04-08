2275fd12e4071a202e97190989a4d230dda4afc1
deuce <deuce>
2005-04-08 21:33:29 +0000

    Yet more fixing... *sigh*

--- uifc/uifc32.c
+++ uifc/uifc32.c
@@ -2,7 +2,7 @@
 
 /* Curses implementation of UIFC (user interface) library based on uifc.c */
 
-/* $Id: uifc32.c,v 1.120 2005/04/08 21:32:09 deuce Exp $ */
+/* $Id: uifc32.c,v 1.121 2005/04/08 21:33:29 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -2320,11 +2320,11 @@ void showbuf(int mode, int left, int top, int width, int height, char *title, ch
 	lines=0;
 	k=0;
 	for(j=0;j<len;j++) {
-		k++;
 		if(mode&WIN_HLP && (hbuf[j]==2 || hbuf[j]=='~' || hbuf[j]==1 || hbuf[j]=='`'))
-			k--;
+			continue;
 		if(hbuf[j]==CR)
-			k--;
+			continue;
+		k++;
 		if((hbuf[j]==LF) || (k>width-2-pad-pad && (hbuf[j+1]!='\n' && hbuf[j+1]!='\r'))) {
 			k=0;
 			lines++;
