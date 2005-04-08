304ea1b2d4681e5a68be621d8127267a740b4bcc
deuce <deuce>
2005-04-08 18:58:28 +0000

    Fix line endings.

--- syncterm/objects.mk
+++ syncterm/objects.mk
@@ -4,10 +4,9 @@ OBJS = \
                         $(MTOBJODIR)$(DIRSEP)conn$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)telnet$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)telnet_io$(OFILE) \
+                        $(MTOBJODIR)$(DIRSEP)raw_io$(OFILE) \
+                        $(MTOBJODIR)$(DIRSEP)rlogin$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)term$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)window$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)menu$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)crc16$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)crc32$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)zmodem$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)syncterm$(OFILE)
