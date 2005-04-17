19d046e071eb0a375103b1296fa7f8179153f2e1
deuce <deuce>
2005-04-17 06:03:58 +0000

    Add od_key_pending() function... returns TRUE if a key is... ng.

--- sbbs3/js_system.c
+++ sbbs3/js_system.c
@@ -2,13 +2,13 @@
 
 /* Synchronet JavaScript "system" Object */
 
-/* $Id: js_system.c,v 1.99 2005/05/09 09:30:54 rswindell Exp $ */
+/* $Id: js_system.c,v 1.98 2005/02/20 02:42:46 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -1280,6 +1280,8 @@ js_popen(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	jsval		val;
 	JSObject*	array;
 	JSString*	js_str;
+
+	*rval = JSVAL_VOID;
 
 	if(argc<1)
 		return(JS_TRUE);
