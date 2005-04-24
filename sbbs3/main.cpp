071689fcb64d056fa352560a3b955f45c85b49ed
rswindell <rswindell>
2005-04-24 20:16:43 +0000

    Reverse last commit: if the door.sys created by us needs fixing, this isn't the way to do it. "cur_rate" is a legacy variable (from the serial modem days) representing the current DCE rate, which can really be *any* number, including the popular rates of 14400, 28800, and 33600, all of which are not "legal" FOSSIL baud rates. But since a FOSSIL driver should only be concerned with the DTE rate, this should be irrelevant.

--- sbbs3/main.cpp
+++ sbbs3/main.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet main/telnet server thread and related functions */
 
-/* $Id: main.cpp,v 1.381 2005/04/24 05:02:09 deuce Exp $ */
+/* $Id: main.cpp,v 1.382 2005/04/24 20:16:43 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -3158,7 +3158,7 @@ void sbbs_t::reset_logon_vars(void)
     for(i=0;i<cfg.total_grps;i++)
         cursub[i]=0;
 	cur_cps=3000;
-    cur_rate=38400;
+    cur_rate=30000;
     dte_rate=38400;
 	main_cmds=xfer_cmds=posts_read=0;
 	lastnodemsg=0;
