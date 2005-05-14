bbb7a09d6ae4b5ffd46b5241635e5e013241f80e
deuce <deuce>
2005-05-14 05:37:22 +0000

    Write game state at end of maint.

--- doors/clans-src/maint.c
+++ doors/clans-src/maint.c
@@ -43,7 +43,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "pawn.h"
 #include "npc.h"
 #include "mail.h"
-#include "game.h"
 
 extern struct game Game;
 extern struct system System;
