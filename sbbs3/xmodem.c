6ef71943511127e1858e69c11182feabf86cf760
rswindell <rswindell>
2005-05-26 07:01:57 +0000

    Added more checks of is_connected().

--- sbbs3/xmodem.c
+++ sbbs3/xmodem.c
@@ -2,7 +2,7 @@
 
 /* Synchronet X/YMODEM Functions */
 
-/* $Id: xmodem.c,v 1.19 2005/02/01 10:13:38 rswindell Exp $ */
+/* $Id: xmodem.c,v 1.20 2005/05/26 07:01:57 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -127,7 +127,7 @@ void xmodem_cancel(xmodem_t* xm)
 {
 	int i;
 
-	if(!xm->cancelled) {
+	if(!xm->cancelled && is_connected(xm)) {
 		for(i=0;i<8 && is_connected(xm);i++)
 			putcom(CAN);
 		for(i=0;i<10 && is_connected(xm);i++)
@@ -491,7 +491,7 @@ BOOL xmodem_send_file(xmodem_t* xm, const char* fname, FILE* fp, time_t* start, 
 				sent_bytes+=rd;
 			}
 		}
-		if(sent_bytes >= (ulong)st.st_size && !xm->cancelled) {
+		if(sent_bytes >= (ulong)st.st_size && !xm->cancelled && is_connected(xm)) {
 
 	#if 0 /* !SINGLE_THREADED */
 			lprintf(LOG_DEBUG,"Waiting for output buffer to empty... ");
@@ -521,7 +521,7 @@ const char* xmodem_source(void)
 
 char* xmodem_ver(char *buf)
 {
-	sscanf("$Revision: 1.19 $", "%*s %s", buf);
+	sscanf("$Revision: 1.20 $", "%*s %s", buf);
 
 	return(buf);
 }
