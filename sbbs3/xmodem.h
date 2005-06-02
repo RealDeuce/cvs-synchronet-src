089b32f09cada1660d80ed632ecc76cfe65e4043
deuce <deuce>
2005-06-02 23:13:38 +0000

    Fix strset() to do what it's supposed to (Was actually strspan() apparently)

--- sbbs3/xmodem.h
+++ sbbs3/xmodem.h
@@ -2,7 +2,7 @@
 
 /* Synchronet X/YMODEM Functions */
 
-/* $Id: xmodem.h,v 1.15 2005/06/13 01:38:54 rswindell Exp $ */
+/* $Id: xmodem.h,v 1.13 2005/05/26 07:07:24 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -52,7 +52,6 @@ typedef struct {
 	unsigned	byte_timeout;
 	unsigned	send_timeout;
 	unsigned	recv_timeout;
-	unsigned	errors;
 	unsigned	max_errors;
 	unsigned	g_delay;
 	unsigned	total_files;
