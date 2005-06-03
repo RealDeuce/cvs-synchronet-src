b0900ed5585b2bcc6c4f99e1a7731a2a9cbb925f
rswindell <rswindell>
2005-06-03 18:30:01 +0000

    Introduced zm.data_waiting callback function, needed for monitoring backchannel

--- odoors/ODPlat.c
+++ odoors/ODPlat.c
@@ -494,7 +494,7 @@ tODResult ODSemaphoreDown(tODSemaphoreHandle hSemaphore, tODMilliSec Timeout)
    ASSERT(hSemaphore != NULL);
 
 #ifdef ODPLAT_WIN32
-   if(WaitForSingleObject(hSemaphore, Timeout) == WAIT_FAILED)
+   if(WaitForSingleObject(hSemaphore, Timeout) != WAIT_OBJECT_0)
    {
       return(kODRCTimeout);
    }
@@ -573,7 +573,7 @@ void ODTimerStart(tODTimer *pTimer, tODMilliSec Duration)
 
 #ifdef ODPLAT_WIN32
    /* Store timer start time now. */
-   pTimer->Start = GetCurrentTime();
+   pTimer->Start = GetTickCount();
    pTimer->Duration = Duration;
 #endif /* ODPLAT_WIN32 */
 
@@ -610,8 +610,7 @@ BOOL ODTimerElapsed(tODTimer *pTimer)
 #endif /* ODPLAT_DOS */
 
 #ifdef ODPLAT_WIN32
-   return(GetCurrentTime() > pTimer->Start + pTimer->Duration
-      || GetCurrentTime() < pTimer->Start);
+   return(ODTimerLeft(pTimer)==0);
 #endif /* ODPLAT_WIN32 */
 
 #ifdef ODPLAT_NIX
@@ -673,7 +672,7 @@ tODMilliSec ODTimerLeft(tODTimer *pTimer)
 {
 #ifdef ODPLAT_NIX
    struct timeval tv;
-   tODMilliSec left=0;
+   long long int nowtick;
 #endif
    ASSERT(pTimer != NULL);
 
@@ -695,16 +694,16 @@ tODMilliSec ODTimerLeft(tODTimer *pTimer)
    }
 #elif defined(ODPLAT_NIX)
    gettimeofday(&tv,NULL);
-   left=pTimer->Start+pTimer->Duration-(long long)tv.tv_sec*1000-tv.tv_usec/1000;
-   if(left<0)
-      left=0;
-   return(left);
+   nowtick=(long long)tv.tv_sec*1000+(tv.tv_usec/1000);
+   if((long long)pTimer->Start+pTimer->Duration <= nowtick)
+      return(0);
+   return((tODMilliSec)((long long)pTimer->Start + pTimer->Duration - nowtick));
 #else /* !ODPLAT_DOS */
    {
       tODMilliSec Now;
 
 #ifdef ODPLAT_WIN32      
-      Now = GetCurrentTime();
+      Now = GetTickCount();
 #endif /* ODPLAT_WIN32 */
 
       /* If timer has elapsed, return 0. */
@@ -775,7 +774,7 @@ ODAPIDEF void ODCALL od_sleep(tODMilliSec Milliseconds)
    if(Milliseconds==0)  {
       /* Prevent 100% CPU *only* no delay is actually required here */
       tv.tv_sec=0;
-      tv.tv_usec=1;
+      tv.tv_usec=1000;
       select(0,NULL,NULL,NULL,&tv);
    }
    else  {
