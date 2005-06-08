0d0ecc92c0b22ded3050cb3c69ef79157398a543
deuce <deuce>
2005-06-08 00:15:36 +0000

    Add a getdirname() function which returns a pointer to the last component of a path that is surrounded by path delimiters. Use this with GLOB_MARK to for the directory list.

--- conio/console.c
+++ conio/console.c
@@ -56,7 +56,7 @@
  *
  */ 
 
-/* $Id: console.c,v 1.46 2005/04/18 00:41:27 deuce Exp $ */
+/* $Id: console.c,v 1.47 2005/06/06 23:00:46 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -109,6 +109,7 @@
 
 #define CONSOLE_CLIPBOARD	XA_PRIMARY
 
+#include <threadwrap.h>
 #include <genwrap.h>
 
 #include "console.h"
@@ -377,6 +378,8 @@ struct {
 
 #define	HWM	16
 void resize_window(void);
+int KbdEmpty(void);
+int load_font(char *filename, int width, int height, int scale);
 
 void tty_pause()
 {
@@ -513,7 +516,7 @@ void
 video_update()
 {
 	static clock_t	lastupd=-1;
-	static clock_started=0;
+	static int clock_started=0;
 	clock_t upd;
 
 	upd=msclock();
@@ -1671,7 +1674,7 @@ KbdRead()
 }
 
 int
-KbdEmpty()
+KbdEmpty(void)
 {
 	return(K_NEXT == K_FREE);
 }
