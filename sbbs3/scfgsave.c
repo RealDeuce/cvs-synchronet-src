1627c4977f2e0afe94dc0d67f09ea0cb8e0ba249
rswindell <rswindell>
2005-06-10 07:07:59 +0000

    Only log "resuming" if received ZRPOS offset is different than current.

--- sbbs3/scfgsave.c
+++ sbbs3/scfgsave.c
@@ -2,7 +2,7 @@
 
 /* Synchronet configuration file save routines */
 
-/* $Id: scfgsave.c,v 1.45 2005/09/05 21:53:24 deuce Exp $ */
+/* $Id: scfgsave.c,v 1.44 2004/10/21 08:58:25 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -659,14 +659,12 @@ BOOL DLLCALL write_msgs_cfg(scfg_t* cfg, int backup_level)
 		}
 		if(smb_locksmbhdr(&smb)!=0) {
 			smb_close(&smb);
-			/* errormsg disabled.  Why?  ToDo */
-			/* errormsg(WHERE,ERR_LOCK,smb.file,x); */
+			//errormsg(WHERE,ERR_LOCK,smb.file,x);
 			return(FALSE); 
 		}
 		if(smb_getstatus(&smb)!=0) {
 			smb_close(&smb);
-			/* errormsg disabled.  Why?  ToDo */
-			/* errormsg(WHERE,ERR_READ,smb.file,x); */
+			//errormsg(WHERE,ERR_READ,smb.file,x);
 			return(FALSE); 
 		}
 		smb.status.max_msgs=0;
@@ -674,8 +672,7 @@ BOOL DLLCALL write_msgs_cfg(scfg_t* cfg, int backup_level)
 		smb.status.max_age=cfg->mail_maxage;
 		if(smb_putstatus(&smb)!=0) {
 			smb_close(&smb);
-			/* errormsg disabled.  Why?  ToDo */
-			/* errormsg(WHERE,ERR_WRITE,smb.file,x); */
+			//errormsg(WHERE,ERR_WRITE,smb.file,x);
 			return(FALSE); 
 		}
 		smb_close(&smb); 
