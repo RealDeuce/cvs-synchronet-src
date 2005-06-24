385d6ddec6870accdaffc102b3086dcc28f66213
deuce <deuce>
2005-06-24 06:53:55 +0000

    More fixed for wrapping weirdness. (Update cursor position after a scroll/wrap)

--- syncterm/uifcinit.h
+++ syncterm/uifcinit.h
@@ -1,3 +1,5 @@
+/* $Id: uifcinit.h,v 1.5 2005/06/18 23:26:20 deuce Exp $ */
+
 #ifndef _UIFCINIT_H_
 #define _UIFCINIT_H_
 
@@ -5,7 +7,7 @@
 
 extern	uifcapi_t uifc; /* User Interface (UIFC) Library API */
 extern int uifc_initialized;
-int	init_uifc(void);
+int	init_uifc(BOOL scrn, BOOL bottom);
 void uifcbail(void);
 void uifcmsg(char *msg, char *helpbuf);
 
