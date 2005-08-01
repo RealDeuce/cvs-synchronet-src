6b9576bd5cc573c2803fcf67110aee50c2ec759d
rswindell <rswindell>
2005-08-01 23:21:12 +0000

    No longer calculate, store, or look-up hashes for the mail database. Since we're not using the message-ID for dupe checking or threading and we already have a mechanism in place for body text dupe detection (via CRC-32) the hash mechanism is providing an unnecessary burden on the mail database.

--- syncterm/uifcinit.h
+++ syncterm/uifcinit.h
@@ -1,11 +1,12 @@
+/* $Id: uifcinit.h,v 1.6 2005/07/03 04:38:05 deuce Exp $ */
+
 #ifndef _UIFCINIT_H_
 #define _UIFCINIT_H_
 
 #include <uifc.h>
 
 extern	uifcapi_t uifc; /* User Interface (UIFC) Library API */
-extern int uifc_initialized;
-int	init_uifc(void);
+int	init_uifc(BOOL scrn, BOOL bottom);
 void uifcbail(void);
 void uifcmsg(char *msg, char *helpbuf);
 
