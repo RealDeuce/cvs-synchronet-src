e3651eb743e8f967232da458af1abb691265196f
rswindell <rswindell>
2005-08-02 22:10:07 +0000

    Changed copyright date to 2005.

--- conio/objects.mk
+++ conio/objects.mk
@@ -1,5 +1,4 @@
 OBJS	=	$(MTOBJODIR)$(DIRSEP)ansi_cio$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)ciolib$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)cterm$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)vidmodes$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)mouse$(OFILE)
