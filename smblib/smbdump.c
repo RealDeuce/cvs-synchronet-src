fcfa9ad91fc773c302898b350b59acf8e9113dcd
deuce <deuce>
2005-08-05 18:27:32 +0000

    Make verious editing functions be a submenu rather than cycle through list.
    PR:25->closed

--- smblib/smbdump.c
+++ smblib/smbdump.c
@@ -2,13 +2,13 @@
 
 /* Synchronet message base (SMB) message header dumper */
 
-/* $Id: smbdump.c,v 1.6 2005/09/30 09:05:16 rswindell Exp $ */
+/* $Id: smbdump.c,v 1.5 2005/07/03 03:56:33 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
