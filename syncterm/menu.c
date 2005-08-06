be42e2a2521f5bffcb12bfdc71a5c733430294f2
deuce <deuce>
2005-08-06 02:43:15 +0000

    Add ANSI/ASCII logging (capture) capabilities.
    PR:25->closed

--- syncterm/menu.c
+++ syncterm/menu.c
@@ -1,3 +1,5 @@
+/* $Id: menu.c,v 1.27 2005/08/06 02:43:15 deuce Exp $ */
+
 #include <genwrap.h>
 #include <uifc.h>
 #include <ciolib.h>
@@ -30,12 +32,17 @@ void viewscroll(void)
 	drawwin();
 	top=cterm.backpos;
 	gotoxy(1,1);
+	textattr(uifc.hclr|(uifc.bclr<<4)|BLINK);
 	for(i=0;!i;) {
 		if(top<1)
 			top=1;
 		if(top>cterm.backpos)
 			top=cterm.backpos;
 		puttext(term.x-1,term.y-1,term.x+term.width-2,term.y+term.height-2,scrollback+(term.width*2*top));
+		cputs("Scrollback");
+		gotoxy(71,1);
+		cputs("Scrollback");
+		gotoxy(1,1);
 		key=getch();
 		switch(key) {
 			case 0xff:
@@ -62,7 +69,7 @@ void viewscroll(void)
 						top+=term.height;
 						break;
 					case CIO_KEY_F(1):
-						init_uifc();
+						init_uifc(FALSE, FALSE);
 						uifc.helpbuf=	"`Scrollback Buffer`\n\n"
 										"~ J ~ or ~ Up Arrow ~   Scrolls up one line\n"
 										"~ K ~ or ~ Down Arrow ~ Scrolls down one line\n"
@@ -101,15 +108,21 @@ void viewscroll(void)
 	return;
 }
 
-int syncmenu(struct bbslist *bbs)
+int syncmenu(struct bbslist *bbs, int *speed)
 {
-	char	*opts[4]={
-						 "Scrollback (ALT-S)"
-						,"Disconnect (CTRL-Q)"
-						,"Send Login (ALT-L)"
+	char	*opts[]={
+						 "Scrollback (Alt-B)"
+						,"Disconnect (Ctrl-Q)"
+						,"Send Login (Alt-L)"
+						,"Zmodem Upload (Alt-U)"
+						,"Zmodem Download (Alt-D)"
+						,"Change Output Rate (Alt-Up/Alt-Down)"
+						,"Change Log Level"
+						,"Capture Control (Alt-C)"
+						,"Exit (Alt-X)"
 						,""};
 	int		opt=0;
-	int		i;
+	int		i,j;
 	struct	text_info txtinfo;
 	char	*buf;
 	int		ret;
@@ -121,11 +134,23 @@ int syncmenu(struct bbslist *bbs)
 	if(cio_api.mode!=CIOLIB_MODE_CURSES
 			&& cio_api.mode!=CIOLIB_MODE_CURSES_IBM
 			&& cio_api.mode!=CIOLIB_MODE_ANSI) {
-		opts[1]="Disconnect (ALT-H)";
+		opts[1]="Disconnect (Alt-H)";
 	}
 
 	for(ret=0;!ret;) {
-		init_uifc();
+		init_uifc(FALSE, !(bbs->nostatus));
+		uifc.helpbuf=	"`Online Menu`\n\n"
+						"~ Scrollback ~         allows to you to view the scrollback buffer\n"
+						"~ Disconnect ~         disconnects the current session and returns to the\n"
+						"                     dialing list\n"
+						"~ Send Login ~         Sends the configured user and password pair separated\n"
+						"                     by a \\r\n"
+						"~ Zmodem Upload ~      Initiates a ZModem upload\n"
+						"~ Zmodem Download ~    Initiates a ZModem download\n"
+						"~ Change Output Rate ~ Changes the speed charaters are output to the screen\n"
+						"~ Change Log Level ~   Changes the minimum log leve for ZModem information\n"
+						"~ Capture Control ~    Enables/Disables screen capture\n"
+						"~ Exit ~               Disconnects and closes the Syncterm";
 		i=uifc.list(WIN_MID|WIN_SAV,0,0,0,&opt,NULL,"SyncTERM Online Menu",opts);
 		switch(i) {
 			case -1:	/* Cancel */
@@ -146,6 +171,45 @@ int syncmenu(struct bbslist *bbs)
 				SLEEP(10);
 				conn_send(bbs->password,strlen(bbs->password),0);
 				conn_send("\r",1,0);
+				if(bbs->syspass[0]) {
+					SLEEP(10);
+					conn_send(bbs->syspass,strlen(bbs->syspass),0);
+					conn_send("\r",1,0);
+				}
+				break;
+			case 5:		/* Output rate */
+				if(speed != NULL) {
+					j=get_rate_num(*speed);
+					uifc.helpbuf="`Output Rate`\n\n"
+							"The output rate is the rate in emulated \"bits per second\" to draw incoming\n"
+							"data on the screen.  This rate is a maximum, not guaranteed to be attained\n"
+							"In general, you will only use this option for ANSI animations.";
+					i=uifc.list(WIN_MID|WIN_SAV,0,0,0,&j,NULL,"Output Rate",rate_names);
+					if(i>=0)
+						*speed = rates[i];
+				}
+				ret=5;
+				break;
+			case 6:		/* Change log level (temporarily) */
+				j=log_level;
+				uifc.helpbuf="`Log Level\n\n"
+						"The log level changes the verbosity of messages shown in the transfer\n"
+						"window.  For the selected log level, messages of that level and those above\n"
+						"it will be displayed.";
+				i=uifc.list(WIN_MID|WIN_SAV,0,0,0,&j,NULL,"Log Level",log_levels);
+				if(i>=0)
+					log_level = j;
+				ret=6;
+				break;
+			case 7:		/* Capture control */
+				ret=i;
+				break;
+			default:
+				ret=i;
+				uifcbail();
+				puttext(1,1,txtinfo.screenwidth,txtinfo.screenheight,buf);
+				free(buf);
+				return(ret);
 		}
 	}
 
