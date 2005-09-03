64fab9e8d876dd647d7895c2b324a6fa8f468150
deuce <deuce>
2005-09-03 03:21:32 +0000

    Re-remove the timeout stuff from pipereadline() (This time expunging all remenents of the start variable).
    Even under Win32, there is absolutely no code path that could possibly
    result in a timeout.

--- sbbs3/websrvr.c
+++ sbbs3/websrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Web Server */
 
-/* $Id: websrvr.c,v 1.331 2005/09/03 01:06:16 rswindell Exp $ */
+/* $Id: websrvr.c,v 1.332 2005/09/03 03:21:32 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1344,22 +1344,18 @@ static int pipereadline(int pipe, char *buf, size_t length, char *fullbuf, size_
 {
 	char	ch;
 	DWORD	i;
-	time_t	start;
 	int		ret=0;
 #ifndef _WIN32
 	struct timeval tv={0,0};
 	fd_set  read_set;
 #endif
 
-	start=time(NULL);
 	/* Terminate buffers */
 	if(buf != NULL)
 		buf[0]=0;
 	if(fullbuf != NULL)
 		fullbuf[0]=0;
 	for(i=0;TRUE;) {
-	    if(time(NULL)-start>startup->max_cgi_inactivity)   
-		    return(-1);   
 #if defined(_WIN32)
 		ret=0;
 		ReadFile(pipe, &ch, 1, (DWORD*)&ret, NULL);
@@ -1373,8 +1369,6 @@ static int pipereadline(int pipe, char *buf, size_t length, char *fullbuf, size_
 		ret=read(pipe, &ch, 1);
 #endif
 		if(ret==1)  {
-			start=time(NULL);
-
 			if(fullbuf != NULL && i < (fullbuf_len-1)) {
 				fullbuf[i]=ch;
 				fullbuf[i+1]=0;
@@ -3520,7 +3514,7 @@ const char* DLLCALL web_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.331 $", "%*s %s", revision);
+	sscanf("$Revision: 1.332 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
