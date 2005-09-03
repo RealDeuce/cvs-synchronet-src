d7247aad63b04fcfa34a0ad9febf4cb53c77b001
deuce <deuce>
2005-09-03 03:21:32 +0000

    Re-remove the timeout stuff from pipereadline() (This time expunging all remenents of the start variable).
    Even under Win32, there is absolutely no code path that could possibly
    result in a timeout.

--- syncterm/syncterm.c
+++ syncterm/syncterm.c
@@ -1,4 +1,4 @@
-/* $Id: syncterm.c,v 1.58 2005/10/06 15:59:21 deuce Exp $ */
+/* $Id: syncterm.c,v 1.54 2005/08/30 20:41:09 deuce Exp $ */
 
 #include <sys/stat.h>
 
@@ -9,14 +9,13 @@
 #include <ini_file.h>
 #include <dirwrap.h>
 
-#include "ciolib.h"
 #include "bbslist.h"
 #include "conn.h"
 #include "term.h"
 #include "uifcinit.h"
 #include "window.h"
 
-char* syncterm_version = "SyncTERM 0.4"
+char* syncterm_version = "SyncTERM 0.03"
 #ifdef _DEBUG
 	" Debug ("__DATE__")"
 #endif
