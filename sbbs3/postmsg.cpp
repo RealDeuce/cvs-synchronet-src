0030406cb9e5c521355ab7c865935ac0d1066aeb
rswindell <rswindell>
2005-09-27 09:37:13 +0000

    Created new command ('x') to dump a sub-board's index file (for debugging).

--- sbbs3/postmsg.cpp
+++ sbbs3/postmsg.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet user create/post public message routine */
 
-/* $Id: postmsg.cpp,v 1.63 2004/12/22 10:51:27 rswindell Exp $ */
+/* $Id: postmsg.cpp,v 1.64 2005/09/25 22:56:57 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
