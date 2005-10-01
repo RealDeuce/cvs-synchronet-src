9f79d4364835ca1e42e60e8529663eb3255ae477
deuce <deuce>
2005-10-01 02:34:59 +0000

    Always create QUERY_STRING, REQUEST_METHOD, and SERVER_PROTOCOL env variables since it MAY switch to CGI later.

--- sbbs3/websrvr.c
+++ sbbs3/websrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Web Server */
 
-/* $Id: websrvr.c,v 1.351 2005/10/01 01:21:27 deuce Exp $ */
+/* $Id: websrvr.c,v 1.352 2005/10/01 02:34:59 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1915,10 +1915,8 @@ static BOOL get_req(http_session_t * session, char *request_line)
 				session->req.ld->vhost=strdup(session->req.vhost);
 			session->req.dynamic=is_dynamic_req(session);
 			if(session->req.query_str[0])  {
+				add_env(session,"QUERY_STRING",session->req.query_str);
 				switch(session->req.dynamic) {
-					case IS_CGI:
-						add_env(session,"QUERY_STRING",session->req.query_str);
-						break;
 					case IS_JS:
 					case IS_SSJS:
 						js_add_request_prop(session,"query_string",session->req.query_str);
@@ -1927,11 +1925,9 @@ static BOOL get_req(http_session_t * session, char *request_line)
 				}
 			}
 
-			if(session->req.dynamic==IS_CGI)  {
-				add_env(session,"REQUEST_METHOD",methods[session->req.method]);
-				add_env(session,"SERVER_PROTOCOL",session->http_ver ? 
-					http_vers[session->http_ver] : "HTTP/0.9");
-			}
+			add_env(session,"REQUEST_METHOD",methods[session->req.method]);
+			add_env(session,"SERVER_PROTOCOL",session->http_ver ? 
+				http_vers[session->http_ver] : "HTTP/0.9");
 			return(TRUE);
 		}
 	}
@@ -3812,7 +3808,7 @@ const char* DLLCALL web_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.351 $", "%*s %s", revision);
+	sscanf("$Revision: 1.352 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
