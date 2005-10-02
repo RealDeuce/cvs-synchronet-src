d2e96a4107f2ddb71db9611bb40d0fed2568722e
deuce <deuce>
2005-10-02 11:24:06 +0000

    Add SDL ciolib add-on.  Currently only build with *nix builds.

--- syncterm/uifcinit.h
+++ syncterm/uifcinit.h
@@ -1,4 +1,4 @@
-/* $Id: uifcinit.h,v 1.5 2005/06/18 23:26:20 deuce Exp $ */
+/* $Id: uifcinit.h,v 1.6 2005/07/03 04:38:05 deuce Exp $ */
 
 #ifndef _UIFCINIT_H_
 #define _UIFCINIT_H_
@@ -6,7 +6,6 @@
 #include <uifc.h>
 
 extern	uifcapi_t uifc; /* User Interface (UIFC) Library API */
-extern int uifc_initialized;
 int	init_uifc(BOOL scrn, BOOL bottom);
 void uifcbail(void);
 void uifcmsg(char *msg, char *helpbuf);
