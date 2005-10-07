5fcfa27afd618a8a06ed86a1f210f2f69047950e
rswindell <rswindell>
2005-10-07 07:26:23 +0000

    Include new netwrap module in xpdev library.

--- sbbs3/sbbscon.c
+++ sbbs3/sbbscon.c
@@ -2,7 +2,7 @@
 
 /* Synchronet vanilla/console-mode "front-end" */
 
-/* $Id: sbbscon.c,v 1.190 2005/07/03 08:24:57 deuce Exp $ */
+/* $Id: sbbscon.c,v 1.195 2005/09/25 22:37:47 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -73,9 +73,6 @@
 	#define	NO_SERVICES
 #endif
 
-/* Constants */
-#define SBBS_PID_FILE	"/var/run/sbbs.pid"
-
 /* Global variables */
 BOOL				terminated=FALSE;
 
@@ -124,6 +121,7 @@ char				log_facility[2];
 char				log_ident[128];
 BOOL				std_facilities=FALSE;
 FILE *				pidf;
+char				pid_fname[MAX_PATH+1];
 #endif
 
 static const char* prompt;
@@ -192,6 +190,13 @@ static const char* services_usage  = "Services settings:\n"
 							"\n"
 							"\tso<value>  set Services option value (advanced)\n"
 							"\ts-         disable Services (no services module)\n"
+							"\n"
+							;
+static const char* web_usage  = "Web server settings:\n"
+							"\n"
+							"\twp<port>   set HTTP server port\n"
+							"\two<value>  set Web server option value (advanced)\n"
+							"\tw-         disable Web server (no services module)\n"
 							"\n"
 							;
 
@@ -810,13 +815,15 @@ static void read_startup_ini(BOOL recycle
 #if defined(__unix__)
 	{
 		char	value[INI_MAX_VALUE_LEN];
-		SAFECOPY(new_uid_name,iniReadString(fp,"UNIX","User","",value));
-		SAFECOPY(new_gid_name,iniReadString(fp,"UNIX","Group","",value));
+		char*	section="UNIX";
+		SAFECOPY(new_uid_name,iniReadString(fp,section,"User","",value));
+		SAFECOPY(new_gid_name,iniReadString(fp,section,"Group","",value));
 		if(!recycle)
-			is_daemon=iniReadBool(fp,"UNIX","Daemonize",FALSE);
-		SAFECOPY(log_facility,iniReadString(fp,"UNIX","LogFacility","U",value));
-		SAFECOPY(log_ident,iniReadString(fp,"UNIX","LogIdent","synchronet",value));
-		umask(iniReadInteger(fp,"UNIX","umask",077));
+			is_daemon=iniReadBool(fp,section,"Daemonize",FALSE);
+		SAFECOPY(log_facility,iniReadString(fp,section,"LogFacility","U",value));
+		SAFECOPY(log_ident,iniReadString(fp,section,"LogIdent","synchronet",value));
+		SAFECOPY(pid_fname,iniReadString(fp,section,"PidFile","/var/run/sbbs.pid",value));
+		umask(iniReadInteger(fp,section,"umask",077));
 	}
 #endif
 	/* close .ini file here */
@@ -850,7 +857,7 @@ void recycle(void* cbdata)
 void cleanup(void)
 {
 #ifdef __unix__
-	unlink(SBBS_PID_FILE);
+	unlink(pid_fname);
 #endif
 }
 
@@ -910,6 +917,7 @@ static void handle_sigs(void)
 		if(pidf!=NULL) {
 			fprintf(pidf,"%d",getpid());
 			fclose(pidf);
+			pidf=NULL;
 		}
 	}
 
@@ -961,6 +969,8 @@ static void show_usage(char *cmd)
 		printf(mail_usage);
 	if(has_services)
 		printf(services_usage);
+	if(has_web)
+		printf(web_usage);
 }
 
 #if SBBS_MAGIC_FILENAMES
@@ -1194,7 +1204,8 @@ int main(int argc, char** argv)
 		run_web=has_web=TRUE;
 #endif
 	}
-
+#else
+	has_web=has_bbs=has_ftp=has_mail=has_services=TRUE;
 #endif	/* Removed broken stuff */
 
 	/* Post-INI command-line switches */
@@ -1218,6 +1229,8 @@ int main(int argc, char** argv)
 			printf("Mail POP3 server port:\t%u\n",mail_startup.pop3_port);
 			printf("Mail server options:\t0x%08lX\n",mail_startup.options);
 			printf("Services options:\t0x%08lX\n",services_startup.options);
+			printf("Web server port:\t%u\n",web_startup.port);
+			printf("Web server options:\t0x%08lX\n",web_startup.options);
 			return(0);
 		}
 		switch(toupper(*(arg++))) {
@@ -1361,6 +1374,23 @@ int main(int argc, char** argv)
 						return(1);
 				}
 				break;
+			case 'W':	/* Web server */
+				switch(toupper(*(arg++))) {
+					case '-':	
+						run_web=FALSE;
+						break;
+					case 'P':
+						web_startup.port=atoi(arg);
+						break;
+					case 'O': /* Set options */
+						web_startup.options=strtoul(arg,NULL,0);
+						break;
+					default:
+						show_usage(argv[0]);
+						return(1);
+				}
+				break;
+				break;
 			case 'G':	/* GET */
 				switch(toupper(*(arg++))) {
 					case 'I': /* Identity */
@@ -1368,6 +1398,7 @@ int main(int argc, char** argv)
 						ftp_startup.options|=BBS_OPT_GET_IDENT;
 						mail_startup.options|=BBS_OPT_GET_IDENT;
 						services_startup.options|=BBS_OPT_GET_IDENT;
+						web_startup.options|=BBS_OPT_GET_IDENT;
 						break;
 					default:
 						show_usage(argv[0]);
@@ -1382,11 +1413,13 @@ int main(int argc, char** argv)
 							SAFECOPY(ftp_startup.host_name,arg);
 							SAFECOPY(mail_startup.host_name,arg);
 							SAFECOPY(services_startup.host_name,arg);
+							SAFECOPY(web_startup.host_name,arg);
 						} else {
 							SAFECOPY(bbs_startup.host_name,host_name);
 							SAFECOPY(ftp_startup.host_name,host_name);
 							SAFECOPY(mail_startup.host_name,host_name);
 							SAFECOPY(services_startup.host_name,host_name);
+							SAFECOPY(web_startup.host_name,host_name);
 						}
 						printf("Setting hostname: %s\n",bbs_startup.host_name);
 						break;
@@ -1456,6 +1489,9 @@ int main(int argc, char** argv)
 #if defined(__unix__)
 						is_daemon=FALSE;
 #endif
+						break;
+					case 'W':	/* no web server */
+						run_web=FALSE;
 						break;
 					default:
 						show_usage(argv[0]);
@@ -1543,7 +1579,7 @@ int main(int argc, char** argv)
 		}
 
 		/* Open here to use startup permissions to create the file */
-		pidf=fopen(SBBS_PID_FILE,"w");
+		pidf=fopen(pid_fname,"w");
 	}
 	old_uid = getuid();
 	if((pw_entry=getpwnam(new_uid_name))!=0)
@@ -1559,6 +1595,7 @@ int main(int argc, char** argv)
 	if((gr_entry=getgrnam(new_gid_name))!=0)
 		new_gid=gr_entry->gr_gid;
 	
+	do_seteuid(TRUE);
 #endif
 
 	/* Install Ctrl-C/Break signal handler here */
@@ -1676,7 +1713,8 @@ int main(int argc, char** argv)
 	}
 
 	if(!isatty(fileno(stdin)))  			/* redirected */
-		select(0,NULL,NULL,NULL,NULL);	/* Sleep forever - Should this just exit the thread? */
+		while(1)
+			select(0,NULL,NULL,NULL,NULL);	/* Sleep forever - Should this just exit the thread? */
 	else 								/* interactive */
 #endif
 	{
