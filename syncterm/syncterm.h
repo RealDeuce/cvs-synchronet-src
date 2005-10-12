8c2f3c1d2de73caa2ff07ab832f3bc3df1ace8f9
rswindell <rswindell>
2005-10-12 08:26:51 +0000

    Changed advanced option "Save received mail files.." to "Retain received mail files.." - "save" has a pre-conceived meaning not consistent with the intended function (to "keep" files normally automatically deleted, for debug purposes).

--- syncterm/syncterm.h
+++ syncterm/syncterm.h
@@ -5,8 +5,6 @@
 
 extern char *inpath;
 extern char *syncterm_version;
-void parse_url(char *url, struct bbslist *bbs, int force_defaults);
-extern int default_font;
-extern char *font_names[];
+void parse_url(char *url, struct bbslist *bbs);
 
 #endif
