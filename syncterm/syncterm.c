1412e61bdf1b546d4a11325399954f3496b38e5b
rswindell <rswindell>
2005-10-13 06:49:14 +0000

    Support multiple protocols/servers per section in the sockopts.ini file. For example, [telnet|rlogin] would set the socket options for either telnet or rlogin servers, while [telnet] would set the socket options only for the telnet server.

--- syncterm/syncterm.c
+++ syncterm/syncterm.c
@@ -1,9 +1,6 @@
-/* $Id: syncterm.c,v 1.72 2005/11/28 15:07:34 deuce Exp $ */
+/* $Id: syncterm.c,v 1.58 2005/10/06 15:59:21 deuce Exp $ */
 
 #include <sys/stat.h>
-#ifdef _WIN32
-#include <shlobj.h>
-#endif
 
 #include <gen_defs.h>
 #include <stdlib.h>
@@ -13,28 +10,19 @@
 #include <dirwrap.h>
 
 #include "ciolib.h"
-#include "cterm.h"
-#include "allfonts.h"
-
-#include "syncterm.h"
 #include "bbslist.h"
 #include "conn.h"
 #include "term.h"
 #include "uifcinit.h"
 #include "window.h"
 
-char* syncterm_version = "SyncTERM 0.6"
+char* syncterm_version = "SyncTERM 0.4"
 #ifdef _DEBUG
 	" Debug ("__DATE__")"
-#endif
-#ifdef PCM
-	" Clippy Edition"
 #endif
 	;
 
 char *inpath=NULL;
-int default_font=0;
-char *font_names[sizeof(conio_fontdata)/sizeof(struct conio_font_data_struct)];
 
 #ifdef _WINSOCKAPI_
 
@@ -63,43 +51,48 @@ static BOOL winsock_startup(void)
 
 #endif
 
-void parse_url(char *url, struct bbslist *bbs, int dflt_conn_type, int force_defaults)
+void parse_url(char *url, struct bbslist *bbs)
 {
 	char *p1, *p2, *p3;
 	struct	bbslist	*list[MAX_OPTS+1];
+	char	*home=NULL;
 	char	path[MAX_PATH];
 	char	listpath[MAX_PATH+1];
 	int		listcount=0, i;
 
 	/* User BBS list path */
-	get_syncterm_filename(listpath, sizeof(listpath), SYNCTERM_PATH_LIST, FALSE);
+	if(inpath==NULL) {
+		home=getenv("HOME");
+		if(home==NULL)
+			home=getenv("USERPROFILE");
+	}
+	if(home==NULL)
+		home=path;
+	strcpy(listpath,home);
+	strncat(listpath,"/syncterm.lst",sizeof(listpath));
+	if(strlen(listpath)>MAX_PATH) {
+		fprintf(stderr,"Path to syncterm.lst too long");
+	}
 
 	bbs->id=-1;
 	bbs->added=time(NULL);
 	bbs->calls=0;
+	bbs->user[0]=0;
+	bbs->password[0]=0;
 	bbs->type=USER_BBSLIST;
-	if(force_defaults) {
-		bbs->user[0]=0;
-		bbs->password[0]=0;
-		bbs->reversed=FALSE;
-		bbs->screen_mode=SCREEN_MODE_CURRENT;
-		bbs->conn_type=dflt_conn_type;
-		bbs->port=(dflt_conn_type==CONN_TYPE_TELNET)?23:513;
-		bbs->loglevel=LOG_INFO;
-		bbs->music=CTERM_MUSIC_BANSI;
-		strcpy(bbs->font,"Codepage 437 English");
-	}
+	bbs->reversed=FALSE;
+	bbs->screen_mode=SCREEN_MODE_CURRENT;
+	bbs->conn_type=CONN_TYPE_TELNET;
+	bbs->port=23;
+	bbs->loglevel=LOG_INFO;
 	p1=url;
 	if(!strnicmp("rlogin://",url,9)) {
 		bbs->conn_type=CONN_TYPE_RLOGIN;
 		bbs->port=513;
 		p1=url+9;
 	}
-	else if(!strnicmp("telnet://",url,9)) {
-		bbs->conn_type=CONN_TYPE_TELNET;
-		bbs->port=23;
+	else if(!strnicmp("telnet://",url,9))
 		p1=url+9;
-	}
 	/* Remove trailing / (Win32 adds one 'cause it hates me) */
 	p2=strchr(p1,'/');
 	if(p2!=NULL)
@@ -127,7 +120,7 @@ void parse_url(char *url, struct bbslist *bbs, int dflt_conn_type, int force_def
 
 	/* Find BBS listing in users phone book */
 	if(listpath != NULL) {
-		read_list(listpath, &list[0], NULL, &listcount, USER_BBSLIST);
+		read_list(listpath, &list[0], &listcount, USER_BBSLIST, home);
 		for(i=0;i<listcount;i++) {
 			if((stricmp(bbs->addr,list[i]->addr)==0)
 					&& (bbs->port==list[i]->port)
@@ -140,121 +133,6 @@ void parse_url(char *url, struct bbslist *bbs, int dflt_conn_type, int force_def
 		}
 	}
 	free_list(&list[0],listcount);
-}
-
-char *get_syncterm_filename(char *fn, int fnlen, int type, int shared)
-{
-	char	oldlst[MAX_PATH+1];
-
-#ifdef _WIN32
-	char	*home;
-	home=getenv("HOME");
-	if(home==NULL)
-		home=getenv("USERPROFILE");
-	if(home==NULL) {
-		strcpy(oldlst,"./syncterm.lst");
-	}
-	else {
-		SAFECOPY(oldlst, home);
-		strcat(oldlst, "/syncterm.lst");
-	}
-	if(type==SYNCTERM_DEFAULT_TRANSFER_PATH) {
-		switch(SHGetFolderPath(NULL, CSIDL_PERSONAL|CSIDL_FLAG_CREATE, NULL, SHGFP_TYPE_CURRENT, fn)) {
-			case E_FAIL:
-			case E_INVALIDARG:
-				getcwd(fn, fnlen);
-				backslash(fn);
-				break;
-			default:
-				backslash(fn);
-				strcat(fn,"SyncTERM");
-				break;
-		}
-		return(fn);
-	}
-	switch(SHGetFolderPath(NULL, (shared?CSIDL_COMMON_APPDATA:CSIDL_APPDATA)|CSIDL_FLAG_CREATE, NULL, SHGFP_TYPE_CURRENT, fn)) {
-		case E_FAIL:
-		case E_INVALIDARG:
-			strcpy(fn,".\\");
-			break;
-		default:
-			backslash(fn);
-			strcat(fn,"SyncTERM");
-			break;
-	}
-
-	/* Create if it doesn't exist */
-	if(!isdir(fn)) {
-		if(MKDIR(fn))
-			fn[0]=0;
-	}
-
-	switch(type) {
-		case SYNCTERM_PATH_INI:
-			strncat(fn,"syncterm.ini",fnlen);
-			break;
-		case SYNCTERM_PATH_LIST:
-			strncat(fn,"syncterm.lst",fnlen);
-			break;
-	}
-#else
-	char	*home;
-	int		created;
-
-	if(inpath==NULL)
-		home=getenv("HOME");
-	if(home==NULL) {
-		if(type==SYNCTERM_DEFAULT_TRANSFER_PATH) {
-			getcwd(fn, fnlen);
-			backslash(fn);
-			return(fn);
-		}
-		SAFECOPY(oldlst,"syncterm.lst");
-		strcpy(fn,"./");
-	}
-	else {
-		if(type==SYNCTERM_DEFAULT_TRANSFER_PATH) {
-			strcpy(fn, home);
-			backslash(fn);
-			return(fn);
-		}
-		SAFECOPY(oldlst,home);
-		backslash(oldlst);
-		strcat(oldlst,"syncterm.lst");
-		sprintf(fn,"%.*s",fnlen,home);
-		strncat(fn, "/.syncterm", fnlen);
-		backslash(fn);
-	}
-
-	if(shared) {
-#ifdef PREFIX
-		strcpy(fn,PREFIX);
-		backslash(fn);
-#else
-		strcpy(fn,"/usr/local/etc/");
-#endif
-	}
-
-	/* Create if it doesn't exist */
-	if(!isdir(fn) && !shared) {
-		if(MKDIR(fn))
-			fn[0]=0;
-	}
-
-	switch(type) {
-		case SYNCTERM_PATH_INI:
-			strncat(fn,"syncterm.ini",fnlen);
-			break;
-		case SYNCTERM_PATH_LIST:
-			strncat(fn,"syncterm.lst",fnlen);
-			break;
-	}
-#endif
-
-	/* Copy pre-0.7 version of the syncterm.lst file to new location */
-	if(!shared && type == SYNCTERM_PATH_LIST && (!fexist(fn)) && fexist(oldlst))
-		rename(oldlst, fn);
-	return(fn);
 }
 
 int main(int argc, char **argv)
@@ -276,9 +154,9 @@ int main(int argc, char **argv)
 	str_list_t	inifile;
 	FILE *listfile;
 	char	listpath[MAX_PATH+1];
+	char	*home=NULL;
 	char	*inpath=NULL;
 	BOOL	exit_now=FALSE;
-	int		conn_type=CONN_TYPE_TELNET;
 
 	/* UIFC initialization */
     memset(&uifc,0,sizeof(uifc));
@@ -293,12 +171,24 @@ int main(int argc, char **argv)
 #endif
             )
             switch(toupper(argv[i][1])) {
+		        case 'M':   /* Monochrome mode */
+        			uifc.mode|=UIFC_MONO;
+                    break;
                 case 'C':
         			uifc.mode|=UIFC_COLOR;
+                    break;
+                case 'L':
+                    uifc.scrn_len=atoi(argv[i]+2);
                     break;
                 case 'E':
                     uifc.esc_delay=atoi(argv[i]+2);
                     break;
+				case 'P':
+					if(argv[i][2]==':' && argv[i][3])
+						inpath=argv[i]+3;
+					else
+						goto USAGE;
+					break;
 				case 'I':
 					switch(toupper(argv[i][2])) {
 						case 'A':
@@ -322,18 +212,6 @@ int main(int argc, char **argv)
 						default:
 							goto USAGE;
 					}
-					break;
-                case 'L':
-                    uifc.scrn_len=atoi(argv[i]+2);
-                    break;
-		        case 'M':   /* Monochrome mode */
-        			uifc.mode|=UIFC_MONO;
-                    break;
-				case 'R':
-					conn_type=CONN_TYPE_RLOGIN;
-					break;
-				case 'T':
-					conn_type=CONN_TYPE_TELNET;
 					break;
                 case 'V':
                     textmode(atoi(argv[i]+2));
@@ -366,7 +244,19 @@ int main(int argc, char **argv)
 #endif
 
 	/* User BBS list path */
-	get_syncterm_filename(listpath, sizeof(listpath), SYNCTERM_PATH_LIST, FALSE);
+	if(inpath==NULL) {
+		home=getenv("HOME");
+		if(home==NULL)
+			home=getenv("USERPROFILE");
+	}
+	if(home==NULL)
+		home=path;
+	strcpy(listpath,home);
+	strncat(listpath,"/syncterm.lst",sizeof(listpath));
+	if(strlen(listpath)>MAX_PATH) {
+		fprintf(stderr,"Path to syncterm.lst too long");
+		return(0);
+	}
 
 	/* Auto-connect URL */
 	if(url[0]) {
@@ -374,13 +264,7 @@ int main(int argc, char **argv)
 			uifcmsg("Unable to allocate memory","The system was unable to allocate memory.");
 			return(1);
 		}
-		if((listfile=fopen(listpath,"r"))==NULL)
-			parse_url(url, bbs, conn_type, TRUE);
-		else {
-			read_item(listfile, bbs, NULL, 0, USER_BBSLIST);
-			parse_url(url, bbs, conn_type, FALSE);
-			fclose(listfile);
-		}
+		parse_url(url, bbs);
 		if(bbs->port==0)
 			goto USAGE;
 	}
@@ -388,9 +272,7 @@ int main(int argc, char **argv)
 	if(!winsock_startup())
 		return(1);
 
-	load_font_files();
-	while(bbs!=NULL || (bbs=show_bbslist(BBSLIST_SELECT))!=NULL) {
-    		gettextinfo(&txtinfo);	/* Current mode may have changed while in show_bbslist() */
+	while(bbs!=NULL || (bbs=show_bbslist(listpath,BBSLIST_SELECT,home))!=NULL) {
 		if(!conn_connect(bbs->addr,bbs->port,bbs->reversed?bbs->password:bbs->user,bbs->reversed?bbs->user:bbs->password,bbs->syspass,bbs->conn_type,bbs->bpsrate)) {
 			/* ToDo: Update the entry with new lastconnected */
 			/* ToDo: Disallow duplicate entries */
@@ -410,8 +292,6 @@ int main(int argc, char **argv)
 				}
 			}
 			uifcbail();
-			load_font_files();
-			setfont(find_font_id(bbs->font),TRUE);
 			switch(bbs->screen_mode) {
 				case SCREEN_MODE_80X25:
 					textmode(C80);
@@ -435,14 +315,6 @@ int main(int argc, char **argv)
 			if(drawwin())
 				return(1);
 			exit_now=doterm(bbs);
-			setfont(default_font,TRUE);
-			for(i=CONIO_FIRST_FREE_FONT; i<256; i++) {
-				FREE_AND_NULL(conio_fontdata[i].eight_by_sixteen);
-				FREE_AND_NULL(conio_fontdata[i].eight_by_fourteen);
-				FREE_AND_NULL(conio_fontdata[i].eight_by_eight);
-				FREE_AND_NULL(conio_fontdata[i].desc);
-			}
-			load_font_files();
 			textmode(txtinfo.currmode);
 			settitle("SyncTERM");
 		}
@@ -453,7 +325,7 @@ int main(int argc, char **argv)
 				init_uifc(TRUE, TRUE);
 				switch(uifc.list(WIN_MID|WIN_SAV,0,0,0,&i,NULL,"Save this BBS in directory?",YesNo)) {
 					case 0:	/* Yes */
-						add_bbs(listpath,bbs);
+						add_bbs(path,bbs);
 						break;
 					default: /* ESC/No */
 						break;
@@ -461,6 +333,7 @@ int main(int argc, char **argv)
 				free(bbs);
 			}
 			bbs=NULL;
+			break;
 		}
 		else
 			bbs=NULL;
@@ -491,13 +364,11 @@ int main(int argc, char **argv)
 		"       A = ANSI mode\n"
         "-v# =  set video mode to # (default=auto)\n"
         "-l# =  set screen lines to # (default=auto-detect)\n"
-		"-t  =  use telnet mode if URL does not include the scheme\n"
-		"-r  =  use rlogin mode if URL does not include the scheme\n"
+        "-p:<path> = set path to users BBS list (default=home then userprofile path\n"
 		"\n"
-		"URL format is: [(rlogin|telnet)://][user[:password]@]domainname[:port]\n"
+		"URL format is: (rlogin|telnet)://[user[:password]@]domainname[:port]\n"
 		"examples: rlogin://deuce:password@nix.synchro.net:5885\n"
 		"          telnet://deuce@nix.synchro.net\n"
-		"          nix.synchro.net\n"
 		"          telnet://nix.synchro.net\n\nPress any key to exit..."
         );
 	getch();
