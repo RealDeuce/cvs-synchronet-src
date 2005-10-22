832316314f88b8003b009a7f327d29ee8ac14baf
rswindell <rswindell>
2005-10-22 00:08:04 +0000

    Convert font bit values to character constants: eliminates msvc6 warnings when assigning to char.

--- sbbs3/sbbsdefs.mk
+++ sbbs3/sbbsdefs.mk
@@ -2,6 +2,6 @@
 
 # C/C++ compiler preprocessor definitions for building SBBS.DLL
 
-# $Id: sbbsdefs.mk,v 1.8 2004/09/23 18:13:54 rswindell Exp $
+# $Id: sbbsdefs.mk,v 1.9 2005/10/17 21:04:28 rswindell Exp $
 
-SBBSDEFS=	-DSBBS -DSBBS_EXPORTS -DSMB_EXPORTS -DMD5_EXPORTS -DRINGBUF_SEM
+SBBSDEFS=	-DSBBS -DSBBS_EXPORTS -DSMB_EXPORTS -DMD5_EXPORTS -DRINGBUF_SEM -DRINGBUF_MUTEX
