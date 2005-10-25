981fc27c632ae555e423d661ba1fbd91bfd6f67c
rswindell <rswindell>
2005-10-25 03:02:53 +0000

    Use MTOBJDIR for JSexec (yes, it's multi-threaded now since ciolib is MT).

--- xpdev/genwrap.c
+++ xpdev/genwrap.c
@@ -2,7 +2,7 @@
 
 /* General cross-platform development wrappers */
 
-/* $Id: genwrap.c,v 1.69 2006/01/19 21:13:30 deuce Exp $ */
+/* $Id: genwrap.c,v 1.60 2005/10/15 02:25:35 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -238,35 +238,27 @@ unsigned DLLCALL xp_randomize(void)
 {
 	unsigned seed=~0;
 
-#if defined(HAS_DEV_URANDOM) && defined(URANDOM_DEV)
+#if defined(HAS_DEV_RANDOM) && defined(RANDOM_DEV)
 	int     rf;
 
-	if((rf=open(URANDOM_DEV, O_RDONLY))!=-1) {
+	if((rf=open(RANDOM_DEV, O_RDONLY))!=-1) {
 		read(rf, &seed, sizeof(seed));
 		close(rf);
 	}
-	else {
-#endif
-		unsigned curtime	= (unsigned)time(NULL);
-		unsigned process_id = (unsigned)GetCurrentProcessId();
-
-		seed = curtime ^ BYTE_SWAP_INT(process_id);
-
-		#if defined(_WIN32) || defined(GetCurrentThreadId)
-			seed ^= (unsigned)GetCurrentThreadId();
-		#endif
-
-#if defined(HAS_DEV_URANDOM) && defined(URANDOM_DEV)
-	}
-#endif
-
-#ifdef HAS_RANDOM_FUNC
- 	srandom(seed);
-	return(seed);
 #else
+	unsigned curtime	= (unsigned)time(NULL);
+	unsigned process_id = (unsigned)GetCurrentProcessId();
+
+	seed = curtime ^ BYTE_SWAP_INT(process_id);
+
+	#if defined(_WIN32) || defined(GetCurrentThreadId)
+		seed ^= (unsigned)GetCurrentThreadId();
+	#endif
+
+#endif
+
  	srand(seed);
 	return(seed);
-#endif
 }
 
 /****************************************************************************/
@@ -274,19 +266,13 @@ unsigned DLLCALL xp_randomize(void)
 /****************************************************************************/
 int DLLCALL xp_random(int n)
 {
-#ifdef HAS_RANDOM_FUNC
-	if(n<2)
-		return(0);
-	return(random()%n);
-#else
-	float f=0;
+	float f;
 
 	if(n<2)
 		return(0);
 	f=(float)rand()/(float)RAND_MAX;
 
 	return((int)(n*f));
-#endif
 }
 
 /****************************************************************************/
@@ -513,14 +499,15 @@ int DLLCALL	get_errno(void)
 long double	DLLCALL	xp_timer(void)
 {
 	long double ret;
-#if defined(__unix__)
+#ifdef __unix__
 	struct timeval tv;
 	if(gettimeofday(&tv,NULL)==1)
 		return(-1);
 	ret=tv.tv_usec;
 	ret /= 1000000;
 	ret += tv.tv_sec;
-#elif defined(_WIN32)
+#else
+#ifdef _WIN32
 	LARGE_INTEGER	freq;
 	LARGE_INTEGER	tick;
 	if(QueryPerformanceFrequency(&freq) && QueryPerformanceCounter(&tick)) {
@@ -528,9 +515,7 @@ long double	DLLCALL	xp_timer(void)
 		ret=((long double)tick.HighPart*4294967296)+((long double)tick.LowPart);
 		ret /= ((long double)freq.HighPart*4294967296)+((long double)freq.LowPart);
 #else
-		/* In MSVC, a long double does NOT have 19 decimals of precision */
-		ret=(((long double)(tick.QuadPart%freq.QuadPart))/freq.QuadPart);
-		ret+=tick.QuadPart/freq.QuadPart;
+		ret=((long double)tick.QuadPart)/freq.QuadPart;
 #endif
 	}
 	else {
@@ -538,7 +523,8 @@ long double	DLLCALL	xp_timer(void)
 		ret /= 1000;
 	}
 #else
-	ret=time(NULL);	/* Weak implementation */
+#error Need xp_timer implementation!
+#endif
 #endif
 	return(ret);
 }
