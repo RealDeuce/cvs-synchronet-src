0cc0b92ab18ebc2f5e2e36cf64d934eb40bb14f9
rswindell <rswindell>
2005-11-09 20:47:38 +0000

    Bug-fix: hungup.log wasn't being updated since "online" is now auto-updated via input_thread() - i.e. checkline() does nothing.

--- sbbs3/xtrn_sec.cpp
+++ sbbs3/xtrn_sec.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet external program/door section and drop file routines */
 
-/* $Id: xtrn_sec.cpp,v 1.46 2005/11/08 21:13:15 rswindell Exp $ */
+/* $Id: xtrn_sec.cpp,v 1.47 2005/11/09 20:47:38 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1709,22 +1709,19 @@ bool sbbs_t::exec_xtrn(uint xtrnnum)
 		remove(str);
 		hangup(); 
 	}
-	if(online==ON_REMOTE) {
-		checkline();
-		if(!online) {
-			sprintf(str,"%shungup.log",cfg.logs_dir);
-			if((file=nopen(str,O_WRONLY|O_CREAT|O_APPEND))==-1) {
-				errormsg(WHERE,ERR_OPEN,str,O_WRONLY|O_CREAT|O_APPEND);
-				return(false); 
-			}
-			getnodedat(cfg.node_num,&thisnode,0);
-			now=time(NULL);
-			sprintf(str,hungupstr,useron.alias,cfg.xtrn[thisnode.aux-1]->name
-				,timestr(&now));
-			write(file,str,strlen(str));
-			close(file); 
-		} 
-	}
+	if(!online) {
+		sprintf(str,"%shungup.log",cfg.logs_dir);
+		if((file=nopen(str,O_WRONLY|O_CREAT|O_APPEND))==-1) {
+			errormsg(WHERE,ERR_OPEN,str,O_WRONLY|O_CREAT|O_APPEND);
+			return(false); 
+		}
+		getnodedat(cfg.node_num,&thisnode,0);
+		now=time(NULL);
+		sprintf(str,hungupstr,useron.alias,cfg.xtrn[thisnode.aux-1]->name
+			,timestr(&now));
+		write(file,str,strlen(str));
+		close(file); 
+	} 
 	if(cfg.xtrn[xtrnnum]->misc&MODUSERDAT) 	/* Modify user data */
 		moduserdat(xtrnnum);
 
