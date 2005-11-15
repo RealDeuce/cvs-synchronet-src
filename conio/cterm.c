c686d0108499ffa7d534de93cb8804f4a98db96d
deuce <deuce>
2005-11-15 18:38:26 +0000

    Blank bottom for all removes lines.

--- conio/cterm.c
+++ conio/cterm.c
@@ -1,4 +1,4 @@
-/* $Id: cterm.c,v 1.38 2005/11/15 17:24:11 deuce Exp $ */
+/* $Id: cterm.c,v 1.39 2005/11/15 18:38:26 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -386,7 +386,7 @@ void dellines(int lines)
 		buf[j++]=' ';
 		buf[j++]=cterm.attr;
 	}
-	puttext(cterm.x,cterm.y+cterm.height-1,cterm.x+cterm.width-1,cterm.y+cterm.height-1,buf);
+	puttext(cterm.x,cterm.y+cterm.height-lines,cterm.x+cterm.width-1,cterm.y+cterm.height-1,buf);
 	free(buf);
 	gotoxy(1,wherey());
 }
