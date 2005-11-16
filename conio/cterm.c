11d353be4d989d8fb12251ff2a4a85117e81046e
deuce <deuce>
2005-11-16 05:35:12 +0000

    Add support for attribute 22 ("Normal" intensity)

--- conio/cterm.c
+++ conio/cterm.c
@@ -1,4 +1,4 @@
-/* $Id: cterm.c,v 1.47 2005/11/16 05:33:46 deuce Exp $ */
+/* $Id: cterm.c,v 1.48 2005/11/16 05:35:12 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -726,6 +726,9 @@ void do_ansi(char *retbuf, int retsize, int *speed)
 								j=cterm.attr&112;
 								cterm.attr&=112;
 								cterm.attr |= j>>4;
+								break;
+							case 22:
+								cterm.attr &= 0xf7;
 								break;
 							case 30:
 								cterm.attr&=248;
