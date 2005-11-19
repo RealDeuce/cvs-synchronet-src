d389ea535f2c28446ff6e55ace39c1a3d80ef5f6
deuce <deuce>
2005-11-19 05:04:33 +0000

    Move lastfg/lastbg out of sdl_draw_one_char() and reset them to -1 when the font is loaded to ensure the colours are set correctly on a screen redraw.

--- sbbs3/getmail.c
+++ sbbs3/getmail.c
@@ -2,13 +2,13 @@
 
 /* Synchronet DLL-exported mail-related routines */
 
-/* $Id: getmail.c,v 1.5 2005/09/20 03:39:51 deuce Exp $ */
+/* $Id: getmail.c,v 1.6 2005/09/29 08:44:05 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -166,5 +166,6 @@ mail_t* DLLCALL loadmail(smb_t* smb, long* msgs, uint usernumber
 
 void DLLCALL freemail(mail_t* mail)
 {
-	free(mail);
+	if(mail!=NULL)
+		free(mail);
 }
