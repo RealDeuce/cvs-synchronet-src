ba0d9a9c70e1d6c58ea87e53e0e147c36c13176c
deuce <deuce>
2005-11-20 21:40:57 +0000

    Fix segfault when ESC is pressed in the file pick dialog for capture file and font loading.

--- conio/objects.mk
+++ conio/objects.mk
@@ -2,5 +2,4 @@ OBJS	=	$(MTOBJODIR)$(DIRSEP)ansi_cio$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)ciolib$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)cterm$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)vidmodes$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)allfonts$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)mouse$(OFILE)
