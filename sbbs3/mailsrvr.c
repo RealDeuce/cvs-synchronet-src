d14849545e8ca9f4d6e63d79c15fbc4c4a1c10f3
deuce <deuce>
2005-11-24 03:53:58 +0000

    Show progress while connecting.

--- sbbs3/mailsrvr.c
+++ sbbs3/mailsrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Mail (SMTP/POP3) server and sendmail threads */
 
-/* $Id: mailsrvr.c,v 1.383 2005/10/07 00:57:18 rswindell Exp $ */
+/* $Id: mailsrvr.c,v 1.389 2005/10/21 21:37:28 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -65,6 +65,7 @@
 #include "crc32.h"
 #include "base64.h"
 #include "ini_file.h"
+#include "netwrap.h"	/* getNameServerList() */
 
 /* Constants */
 #define FORWARD			"forward:"
@@ -195,16 +196,18 @@ static void thread_down(void)
 		startup->thread_up(startup->cbdata,FALSE,FALSE);
 }
 
-SOCKET mail_open_socket(int type)
+SOCKET mail_open_socket(int type, const char* protocol)
 {
 	char	error[256];
+	char	section[128];
 	SOCKET	sock;
 
 	sock=socket(AF_INET, type, IPPROTO_IP);
 	if(sock!=INVALID_SOCKET && startup!=NULL && startup->socket_open!=NULL) 
 		startup->socket_open(startup->cbdata,TRUE);
 	if(sock!=INVALID_SOCKET) {
-		if(set_socket_options(&scfg, sock,error))
+		SAFEPRINTF(section,"mail|%s",protocol);
+		if(set_socket_options(&scfg, sock, section, error, sizeof(error)))
 			lprintf(LOG_ERR,"%04d !ERROR %s",sock,error);
 
 		sockets++;
@@ -3457,6 +3460,24 @@ static int remove_msg_intransit(smb_t* smb, smbmsg_t* msg)
 	return(i);
 }
 
+void get_dns_server(char* dns_server, size_t len)
+{
+	str_list_t	list;
+	size_t		count;
+
+	sprintf(dns_server,"%.*s",len,startup->dns_server);
+	if(!isalnum(dns_server[0])) {
+		if((list=getNameServerList())!=NULL) {
+			if((count=strListCount(list))>0) {
+				sprintf(dns_server,"%.*s",len,list[xp_random(count)]);
+				lprintf(LOG_DEBUG,"0000 SEND using auto-detected DNS server address: %s"
+					,dns_server);
+			}
+			freeNameServerList(list);
+		}
+	}
+}
+
 #ifdef __BORLANDC__
 #pragma argsused
 #endif
@@ -3478,6 +3499,7 @@ static void sendmail_thread(void* arg)
 	char		digest[MD5_DIGEST_SIZE];
 	char		numeric_ip[16];
 	char		domain_list[MAX_PATH+1];
+	char		dns_server[16];
 	char*		server;
 	char*		msgtxt=NULL;
 	char*		p;
@@ -3678,14 +3700,15 @@ static void sendmail_thread(void* arg)
 						continue;
 					}
 #endif
-					if((dns=resolve_ip(startup->dns_server))==INADDR_NONE) {
+					get_dns_server(dns_server,sizeof(dns_server));
+					if((dns=resolve_ip(dns_server))==INADDR_NONE) {
 						remove_msg_intransit(&smb,&msg);
 						lprintf(LOG_WARNING,"0000 !SEND INVALID DNS server address: %s"
-							,startup->dns_server);
+							,dns_server);
 						continue;
 					}
 					p++;
-					lprintf(LOG_DEBUG,"0000 SEND getting MX records for %s from %s",p,startup->dns_server);
+					lprintf(LOG_DEBUG,"0000 SEND getting MX records for %s from %s",p,dns_server);
 					if((i=dns_getmx(p, mx, mx2, startup->interface_addr, dns
 						,startup->options&MAIL_OPT_USE_TCP_DNS ? TRUE : FALSE
 						,TIMEOUT_THREAD_WAIT/2))!=0) {
@@ -3702,7 +3725,7 @@ static void sendmail_thread(void* arg)
 			if(!port)
 				port=IPPORT_SMTP;
 
-			if((sock=mail_open_socket(SOCK_STREAM))==INVALID_SOCKET) {
+			if((sock=mail_open_socket(SOCK_STREAM,"smtp|sendmail"))==INVALID_SOCKET) {
 				remove_msg_intransit(&smb,&msg);
 				lprintf(LOG_ERR,"0000 !SEND ERROR %d opening socket", ERROR_VALUE);
 				continue;
@@ -4013,7 +4036,7 @@ const char* DLLCALL mail_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.383 $", "%*s %s", revision);
+	sscanf("$Revision: 1.389 $", "%*s %s", revision);
 
 	sprintf(ver,"Synchronet Mail Server %s%s  SMBLIB %s  "
 		"Compiled %s %s with %s"
@@ -4212,7 +4235,7 @@ void DLLCALL mail_server(void* arg)
 
 		/* open a socket and wait for a client */
 
-		server_socket = mail_open_socket(SOCK_STREAM);
+		server_socket = mail_open_socket(SOCK_STREAM,"smtp");
 
 		if(server_socket == INVALID_SOCKET) {
 			lprintf(LOG_ERR,"!ERROR %d opening socket", ERROR_VALUE);
@@ -4259,7 +4282,7 @@ void DLLCALL mail_server(void* arg)
 
 			/* open a socket and wait for a client */
 
-			pop3_socket = mail_open_socket(SOCK_STREAM);
+			pop3_socket = mail_open_socket(SOCK_STREAM,"pop3");
 
 			if(pop3_socket == INVALID_SOCKET) {
 				lprintf(LOG_ERR,"!ERROR %d opening POP3 socket", ERROR_VALUE);
@@ -4549,8 +4572,12 @@ void DLLCALL mail_server(void* arg)
 				mswait(500);
 			}
 		}
-		if(!sendmail_running)
-			sem_destroy(&sendmail_wakeup_sem);
+		if(!sendmail_running) {
+			while(sem_destroy(&sendmail_wakeup_sem)==-1 && errno==EBUSY) {
+				mswait(1);
+				sem_post(&sendmail_wakeup_sem);
+			}
+		}
 
 		cleanup(0);
 
