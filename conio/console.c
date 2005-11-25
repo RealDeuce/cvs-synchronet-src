2a8b6ad42653c42b3581a00c80acfa0b309c9855
deuce <deuce>
2005-11-25 22:40:52 +0000

    Rename same fonts of differing sized to have same basename but different extenstions.

--- conio/console.c
+++ conio/console.c
@@ -56,7 +56,7 @@
  *
  */ 
 
-/* $Id: console.c,v 1.67 2006/05/05 21:01:33 deuce Exp $ */
+/* $Id: console.c,v 1.65 2005/11/19 08:45:43 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -166,7 +166,6 @@ typedef struct TextLine {
 	u_char	*exposed;
 } TextLine;
 TextLine *lines = NULL;
-unsigned int	x_pending_mousekeys=0;
 
 /* X Variables */
 Display *dpy=NULL;
@@ -392,7 +391,7 @@ struct {
 #define	HWM	16
 void resize_window(void);
 int KbdEmpty(void);
-int load_font(char *filename, int width, int height, int scale, int *newmode);
+int load_font(char *filename, int width, int height, int scale);
 
 void tty_pause()
 {
@@ -573,12 +572,8 @@ KbdWrite(WORD code)
 		kf = K_BUFSTARTP;
 
 	if (kf == K_NEXT) {
-		if(code==CIO_KEY_MOUSE)
-			x_pending_mousekeys++;
-		else {
-			x11.XBell(dpy, 0);
-			return;
-		}
+		x11.XBell(dpy, 0);
+		return;
 	}
 	K_BUF(K_FREE) = code;
 	K_FREE = kf;
@@ -653,7 +648,7 @@ video_event(XEvent *ev)
 					FontScale=newFSH;
 				else
 					FontScale=newFSW;
-				load_font(NULL,FW,FH,FontScale,NULL);
+				load_font(NULL,FW,FH,FontScale);
 				resize_window();
 				break;
 		}
@@ -1134,18 +1129,16 @@ video_async_event(void *crap)
 					init_mode(console_new_mode);
 				if(x_current_font!=new_font) {
 					int oldfont=x_current_font;
-					int newmode=0;
-
 					x_current_font=new_font;
-					if(load_font(NULL,FW/FontScale,FH/FontScale,FontScale,&newmode)) {
-						if(font_force && newmode) {
-							init_mode(newmode);
+					if(load_font(NULL,FW/FontScale,FH/FontScale,FontScale)) {
+						if(font_force) {
+							init_mode(3);
 							sem_wait(&console_mode_changed);
-							if(load_font(NULL,FW/FontScale,FH/FontScale,FontScale,NULL)) {
+							if(load_font(NULL,FW/FontScale,FH/FontScale,FontScale)) {
 								setfont_return=-1;
 								x_current_font=oldfont;
 								new_font=oldfont;
-								load_font(NULL,FW/FontScale,FH/FontScale,FontScale,NULL);
+								load_font(NULL,FW/FontScale,FH/FontScale,FontScale);
 							}
 							else
 								setfont_return=0;
@@ -1154,7 +1147,7 @@ video_async_event(void *crap)
 							setfont_return=-1;
 							x_current_font=oldfont;
 							new_font=oldfont;
-							load_font(NULL,FW/FontScale,FH/FontScale,FontScale,NULL);
+							load_font(NULL,FW/FontScale,FH/FontScale,FontScale);
 						}
 					}
 					else
@@ -1168,7 +1161,7 @@ video_async_event(void *crap)
 					x11.XSetIconName(dpy, win, window_name);
 				if(!sem_trywait(&x11_loadfont)) {
 					int oldfont=x_current_font;
-					x_load_font_ret=load_font(font_filename,FW/FontScale,FH/FontScale,FontScale,NULL);
+					x_load_font_ret=load_font(font_filename,FW/FontScale,FH/FontScale,FontScale);
 					if(x_load_font_ret)
 						x_current_font=oldfont;
 					new_font=x_current_font;
@@ -1345,7 +1338,7 @@ scale_bitmap(char *bitmap, int width, int height, int *multiplier)
 
 /* No longer uses X fonts - pass NULL to use VGA 8x16 font */
 int
-load_font(char *filename, int width, int height, int scale, int *newmode)
+load_font(char *filename, int width, int height, int scale)
 {
     XGCValues gcv;
 	char *font;
@@ -1375,22 +1368,7 @@ load_font(char *filename, int width, int height, int scale, int *newmode)
 		return(-1);
 
 	if(filename != NULL) {
-		int fl=flength(filename);
-
-		if(newmode != NULL) {
-			switch(fl/256) {
-				case 8:
-					*newmode=C80X50;
-					break;
-				case 14:
-					*newmode=C80X28;
-					break;
-				case 16:
-					*newmode=C80;
-					break;
-			}
-		}
-		if(fl!=height*256)
+		if(flength(filename)!=height*256)
 			return(-1);
 		if((fontfile=fopen(filename,"rb"))==NULL)
 			return(-1);
@@ -1404,14 +1382,6 @@ load_font(char *filename, int width, int height, int scale, int *newmode)
 			SAFECOPY(current_filename,filename);
 	}
 	else {
-		if(newmode != NULL) {
-			if(conio_fontdata[x_current_font].eight_by_sixteen!=NULL)
-				*newmode=C80;
-			else if(conio_fontdata[x_current_font].eight_by_fourteen!=NULL)
-				*newmode=C80X28;
-			else if(conio_fontdata[x_current_font].eight_by_eight!=NULL)
-				*newmode=C80X50;
-		}
 		switch(width) {
 			case 8:
 				switch(height) {
@@ -1524,7 +1494,7 @@ init_mode(int mode)
     update_pixels();
 
     /* Update font. */
-    if(load_font(NULL,vmode.charwidth,vmode.charheight,FontScale,NULL)) {
+    if(load_font(NULL,vmode.charwidth,vmode.charheight,FontScale)) {
 		sem_post(&console_mode_changed);
 		return(-1);
 	}
@@ -1804,18 +1774,12 @@ WORD
 KbdRead()
 {
 	int kf = K_NEXT;
-	WORD	ret;
 
 	K_NEXT = K_NEXT + 2;
 	if (K_NEXT == K_BUFENDP)
 		K_NEXT = K_BUFSTARTP;
 
-	ret=K_BUF(kf);
-	if(x_pending_mousekeys) {
-		KbdWrite(CIO_KEY_MOUSE);
-		x_pending_mousekeys--;
-	}
-	return(ret);
+	return(K_BUF(kf));
 }
 
 int
