7eb8d13a7cb95b22d7b42f28910d8ec38475b635
deuce <deuce>
2005-11-25 23:30:24 +0000

    Allow changing of the current screen mode.

--- syncterm/syncterm.c
+++ syncterm/syncterm.c
@@ -1,4 +1,4 @@
-/* $Id: syncterm.c,v 1.69 2005/11/25 22:36:25 deuce Exp $ */
+/* $Id: syncterm.c,v 1.70 2005/11/25 23:30:24 deuce Exp $ */
 
 #include <sys/stat.h>
 
@@ -309,6 +309,7 @@ int main(int argc, char **argv)
 		return(1);
 
 	while(bbs!=NULL || (bbs=show_bbslist(listpath,BBSLIST_SELECT,home))!=NULL) {
+    		gettextinfo(&txtinfo);	/* Current mode may have changed while in show_bbslist() */
 		if(!conn_connect(bbs->addr,bbs->port,bbs->reversed?bbs->password:bbs->user,bbs->reversed?bbs->user:bbs->password,bbs->syspass,bbs->conn_type,bbs->bpsrate)) {
 			/* ToDo: Update the entry with new lastconnected */
 			/* ToDo: Disallow duplicate entries */
