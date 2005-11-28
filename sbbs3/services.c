f507adabe8ab4a582953e21dfaa332cf4aabd4b7
deuce <deuce>
2005-11-28 02:19:35 +0000

    Add in an output thread which handles chunking.  This should improve JS output, and prevent gobs 'o errors.

--- sbbs3/services.c
+++ sbbs3/services.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Services */
 
-/* $Id: services.c,v 1.189 2005/10/12 23:18:00 rswindell Exp $ */
+/* $Id: services.c,v 1.192 2005/10/19 08:26:07 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -212,9 +212,10 @@ static void thread_down(void)
 		startup->thread_up(startup->cbdata,FALSE,FALSE);
 }
 
-static SOCKET open_socket(int type)
+static SOCKET open_socket(int type, const char* protocol)
 {
 	char	error[256];
+	char	section[128];
 	SOCKET	sock;
 
 	sock=socket(AF_INET, type, IPPROTO_IP);
@@ -222,7 +223,8 @@ static SOCKET open_socket(int type)
 		startup->socket_open(startup->cbdata,TRUE);
 	if(sock!=INVALID_SOCKET) {
 		sockets++;
-		if(set_socket_options(&scfg, sock, error))
+		SAFEPRINTF(section,"services|%s", protocol);
+		if(set_socket_options(&scfg, sock, section, error, sizeof(error)))
 			lprintf(LOG_ERR,"%04d !ERROR %s",sock, error);
 
 #if 0 /*def _DEBUG */
@@ -1464,6 +1466,11 @@ static service_t* read_services_ini(service_t* service, DWORD* services)
 		serv.log_level = iniGetLogLevel(list,sec_list[i],"LogLevel",log_level);
 		SAFECOPY(serv.cmd,iniGetString(list,sec_list[i],"Command","",cmd));
 
+		if(serv.cmd[0]==0) {
+			lprintf(LOG_WARNING,"Ignoring service with no command: %s",sec_list[i]);
+			continue;
+		}
+
 		/* JavaScript operating parameters */
 		sbbs_get_js_settings(list, sec_list[i], &serv.js, &startup->js);
 
@@ -1532,7 +1539,7 @@ const char* DLLCALL services_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.189 $", "%*s %s", revision);
+	sscanf("$Revision: 1.192 $", "%*s %s", revision);
 
 	sprintf(ver,"Synchronet Services %s%s  "
 		"Compiled %s %s with %s"
@@ -1696,7 +1703,8 @@ void DLLCALL services_thread(void* arg)
 			service[i].socket=INVALID_SOCKET;
 
 			if((socket = open_socket(
-				(service[i].options&SERVICE_OPT_UDP) ? SOCK_DGRAM : SOCK_STREAM))
+				(service[i].options&SERVICE_OPT_UDP) ? SOCK_DGRAM : SOCK_STREAM
+				,service[i].protocol))
 				==INVALID_SOCKET) {
 				lprintf(LOG_ERR,"!ERROR %d opening %s socket"
 					,ERROR_VALUE, service[i].protocol);
@@ -1892,7 +1900,7 @@ void DLLCALL services_thread(void* arg)
 						continue;
 					}
 
-					if((client_socket = open_socket(SOCK_DGRAM))
+					if((client_socket = open_socket(SOCK_DGRAM, service[i].protocol))
 						==INVALID_SOCKET) {
 						FREE_AND_NULL(udp_buf);
 						lprintf(LOG_ERR,"%04d %s !ERROR %d opening socket"
