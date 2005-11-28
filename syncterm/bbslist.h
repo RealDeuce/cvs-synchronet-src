20f8fde793d7cd18ed3c679f8f88784f347c9b30
deuce <deuce>
2005-11-28 07:31:20 +0000

    Add font management support, add a function to get paths which SyncTERM will wnat to use.

--- syncterm/bbslist.h
+++ syncterm/bbslist.h
@@ -1,4 +1,4 @@
-/* $Id: bbslist.h,v 1.27 2006/02/25 09:13:44 rswindell Exp $ */
+/* $Id: bbslist.h,v 1.22 2005/11/28 07:31:20 deuce Exp $ */
 
 #ifndef _BBSLIST_H_
 #define _BBSLIST_H_
@@ -6,7 +6,6 @@
 #include <stdio.h>	/* FILE * */
 #include "gen_defs.h"
 #include "dirwrap.h"	/* MAX_PATH */
-#include "ini_file.h"
 #include <time.h>
 
 #define LIST_NAME_MAX	30
@@ -51,21 +50,18 @@ struct bbslist {
 	int				reversed;
 	int				screen_mode;
 	int				nostatus;
-	char			dldir[MAX_PATH+1];
-	char			uldir[MAX_PATH+1];
-	char			logfile[MAX_PATH+1];
-	int				xfer_loglevel;
-	int				telnet_loglevel;
+	char			dldir[MAX_PATH];
+	char			uldir[MAX_PATH];
+	int				loglevel;
 	int				bpsrate;
 	int				music;
-	char			font[80];
+	int				font;
 };
 
 struct bbslist *show_bbslist(int mode);
 extern char *log_levels[];
 extern char *rate_names[];
 extern int rates[];
-extern ini_style_t ini_style;
 void read_item(FILE *listfile, struct bbslist *entry, char *bbsname, int id, int type);
 void read_list(char *listpath, struct bbslist **list, struct bbslist *defaults, int *i, int type);
 void free_list(struct bbslist **list, int listcount);
--- syncterm/syncterm.h
+++ syncterm/syncterm.h
@@ -9,22 +9,11 @@ enum {
 	,SYNCTERM_DEFAULT_TRANSFER_PATH
 };
 
-struct syncterm_settings {
-	int		confirm_close;
-	int		startup_mode;
-	int		backlines;
-};
-
 extern char *inpath;
 extern char *syncterm_version;
-extern unsigned char *scrollback_buf;
-extern unsigned int   scrollback_lines;
-extern struct syncterm_settings settings;
 void parse_url(char *url, struct bbslist *bbs, int dflt_conn_type, int force_defaults);
 extern int default_font;
 extern char *font_names[];
-extern int safe_mode;
 char *get_syncterm_filename(char *fn, int fnlen, int type, int shared);
-void load_settings(struct syncterm_settings *set);
 
 #endif
