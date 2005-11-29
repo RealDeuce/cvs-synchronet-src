6e32ce9bbc52cabceefa3350b904cd456f15ce77
rswindell <rswindell>
2005-11-29 01:18:55 +0000

    Reversed the ungetkey() overhaul of Sept-2-2005 (included in v3.13a), this new method would insert keys at end of the input ring buffer. This was fine if there weren't already chars in the buffer. But if there were already chars in the input buffer, ansi_getxy() would then move keys to the end of the input buffer until it found the <ESC>[y;xR response it was looking for. This garbled the user's input in split-screen chat as reported by JasHud and others. If there was a RingBufInsert() routine, we could use that instead, but this separate "ungetkey" buf will do for now.

--- sbbs3/services.c
+++ sbbs3/services.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Services */
 
-/* $Id: services.c,v 1.195 2006/05/08 20:44:23 deuce Exp $ */
+/* $Id: services.c,v 1.192 2005/10/19 08:26:07 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -287,7 +287,7 @@ js_read(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	if(argc)
 		JS_ValueToInt32(cx,argv[0],&len);
 	
-	if((buf=alloca(len))==NULL)
+	if((buf=malloc(len))==NULL)
 		return(JS_TRUE);
 
 	len=recv(client->socket,buf,len,0);
@@ -295,20 +295,16 @@ js_read(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	if(len>0)
 		*rval = STRING_TO_JSVAL(JS_NewStringCopyN(cx,buf,len));
 
+	free(buf);
+
 	return(JS_TRUE);
 }
 
 static JSBool
 js_readln(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 {
-	char		ch;
 	char*		buf;
-	int			i;
 	int32		len=512;
-	BOOL		rd;
-	time_t		start;
-	int32		timeout=30;	/* seconds */
-	JSString*	str;
 	service_client_t* client;
 
 	if((client=(service_client_t*)JS_GetContextPrivate(cx))==NULL)
@@ -316,48 +312,17 @@ js_readln(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 
 	if(argc)
 		JS_ValueToInt32(cx,argv[0],&len);
+	
+	if((buf=malloc(len))==NULL)
+		return(JS_TRUE);
 
-	if((buf=(char*)alloca(len+1))==NULL) {
-		JS_ReportError(cx,"Error allocating %u bytes",len+1);
-		return(JS_FALSE);
-	}
+	len=recv(client->socket,buf,len,0);	/* Need to switch to sockreadline */
 
-	if(argc>1)
-		JS_ValueToInt32(cx,argv[1],(int32*)&timeout);
+	if(len>0)
+		*rval = STRING_TO_JSVAL(JS_NewStringCopyN(cx,buf,len));
 
-	start=time(NULL);
-	for(i=0;i<len;) {
+	free(buf);
 
-		if(!socket_check(client->socket,&rd,NULL,1000))
-			break;		/* disconnected */
-
-		if(!rd) {
-			if(time(NULL)-start>timeout) {
-				*rval = JSVAL_NULL;
-				return(JS_TRUE);	/* time-out */
-			}
-			continue;	/* no data */
-		}
-
-		if(recv(client->socket, &ch, 1, 0)!=1)
-			break;
-
-		if(ch=='\n' /* && i>=1 */) /* Mar-9-2003: terminate on sole LF */
-			break;
-
-		buf[i++]=ch;
-	}
-	if(i>0 && buf[i-1]=='\r')
-		buf[i-1]=0;
-	else
-		buf[i]=0;
-
-	str = JS_NewStringCopyZ(cx, buf);
-	if(str==NULL)
-		return(JS_FALSE);
-
-	*rval = STRING_TO_JSVAL(str);
-		
 	return(JS_TRUE);
 }
 
@@ -1021,7 +986,6 @@ static void js_service_thread(void* arg)
 
 	lprintf(LOG_DEBUG,"%04d %s JavaScript service thread started", socket, service->protocol);
 
-	SetThreadName("JS Service Thread");
 	thread_up(TRUE /* setuid */);
 
 	/* Host name lookup and filtering */
@@ -1187,7 +1151,6 @@ static void js_static_service_thread(void* arg)
 
 	lprintf(LOG_DEBUG,"%04d %s static JavaScript service thread started", service->socket, service->protocol);
 
-	SetThreadName("JS Static Service Thread");
 	thread_up(TRUE /* setuid */);
 
 	memset(&service_client,0,sizeof(service_client));
@@ -1277,7 +1240,6 @@ static void native_static_service_thread(void* arg)
 
 	lprintf(LOG_DEBUG,"%04d %s static service thread started", socket, service->protocol);
 
-	SetThreadName("Native Static Service Thread");
 	thread_up(TRUE /* setuid */);
 
 #ifdef _WIN32
@@ -1341,7 +1303,6 @@ static void native_service_thread(void* arg)
 
 	lprintf(LOG_DEBUG,"%04d %s service thread started", socket, service->protocol);
 
-	SetThreadName("Native Service Thread");
 	thread_up(TRUE /* setuid */);
 
 	/* Host name lookup and filtering */
@@ -1469,7 +1430,6 @@ static service_t* read_services_ini(service_t* service, DWORD* services)
 {
 	uint		i,j;
 	FILE*		fp;
-	char*		p;
 	char		cmd[INI_MAX_VALUE_LEN];
 	char		host[INI_MAX_VALUE_LEN];
 	char		prot[INI_MAX_VALUE_LEN];
@@ -1527,8 +1487,7 @@ static service_t* read_services_ini(service_t* service, DWORD* services)
 			lprintf(LOG_NOTICE,"Ignoring service (%s) for host: %s", sec_list[i], host);
 			continue;
 		}
-		p=iniGetString(list,sec_list[i],"NotHost","",host);
-		if(*p!=0 && stricmp(p, startup->host_name)==0) {
+		if(stricmp(iniGetString(list,sec_list[i],"NotHost","",host), startup->host_name)==0) {
 			lprintf(LOG_NOTICE,"Ignoring service (%s) not for host: %s", sec_list[i], host);
 			continue;
 		}
@@ -1580,7 +1539,7 @@ const char* DLLCALL services_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.195 $", "%*s %s", revision);
+	sscanf("$Revision: 1.192 $", "%*s %s", revision);
 
 	sprintf(ver,"Synchronet Services %s%s  "
 		"Compiled %s %s with %s"
@@ -1654,8 +1613,6 @@ void DLLCALL services_thread(void* arg)
 	served=0;
 	startup->recycle_now=FALSE;
 	startup->shutdown_now=FALSE;
-
-	SetThreadName("Services Thread");
 
 	do {
 
