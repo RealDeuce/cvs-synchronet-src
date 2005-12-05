e7761177e74a5b8f4b4e23627e36f9683e218dbd
rswindell <rswindell>
2005-12-05 00:21:58 +0000

    Don't attempt to send to MX servers with an IP address of 0.0.0.0

--- sbbs3/main.cpp
+++ sbbs3/main.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet main/telnet server thread and related functions */
 
-/* $Id: main.cpp,v 1.407 2005/10/14 07:51:19 rswindell Exp $ */
+/* $Id: main.cpp,v 1.417 2005/11/29 01:18:55 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -48,10 +48,6 @@
 #endif
 
 //---------------------------------------------------------------------------
-
-/* Temporary */
-int	mswtyp=0;
-uint riobp;
 
 #define TELNET_SERVER "Synchronet Telnet Server"
 #define STATUS_WFC	"Listening"
@@ -541,6 +537,20 @@ DLLCALL js_DefineConstIntegers(JSContext* cx, JSObject* obj, jsConstIntSpec* int
 	return(JS_TRUE);
 }
 
+char*
+DLLCALL js_ValueToStringBytes(JSContext* cx, jsval val, size_t* len)
+{
+	JSString* str;
+	
+	if((str=JS_ValueToString(cx, val))==NULL)
+		return(NULL);
+
+	if(len!=NULL)
+		*len = JS_GetStringLength(str);
+
+	return(JS_GetStringBytes(str));
+}
+
 static JSBool
 js_log(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 {
@@ -645,6 +655,26 @@ js_write(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 		*rval = JSVAL_VOID;
 	else
 		*rval = STRING_TO_JSVAL(str);
+    return(JS_TRUE);
+}
+
+static JSBool
+js_write_raw(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+    uintN		i;
+    char*	str=NULL;
+	size_t		len;
+	sbbs_t*		sbbs;
+
+	if((sbbs=(sbbs_t*)JS_GetContextPrivate(cx))==NULL)
+		return(JS_FALSE);
+
+    for (i = 0; i < argc; i++) {
+		if((str=js_ValueToStringBytes(cx, argv[i], &len))==NULL)
+		    return(JS_FALSE);
+		sbbs->putcom(str, len);
+	}
+
     return(JS_TRUE);
 }
 
@@ -799,6 +829,10 @@ static jsSyncMethodSpec js_global_functions[] = {
 	{"write",			js_write,			0,	JSTYPE_VOID,	JSDOCSTR("value [,value]")
 	,JSDOCSTR("send one or more values (typically strings) to the server output")
 	,311
+	},
+	{"write_raw",			js_write_raw,			0,	JSTYPE_VOID,	JSDOCSTR("value [,value]")
+	,JSDOCSTR("send a stream of bytes (possibly containing NULLs or special control code sequences) to the server output")
+	,313
 	},
 	{"print",			js_writeln,			0,	JSTYPE_ALIAS },
     {"writeln",         js_writeln,         0,	JSTYPE_VOID,	JSDOCSTR("value [,value]")
@@ -1422,7 +1456,8 @@ void input_thread(void *arg)
 	if(node_socket[sbbs->cfg.node_num-1]==INVALID_SOCKET)	// Shutdown locally
 		sbbs->terminated = true;	// Signal JS to stop execution
 
-	pthread_mutex_destroy(&sbbs->input_thread_mutex);
+	while(pthread_mutex_destroy(&sbbs->input_thread_mutex)==EBUSY)
+		mswait(1);
 
 	thread_down();
 	lprintf(LOG_DEBUG,"Node %d input thread terminated (received %lu bytes in %lu blocks)"
@@ -1459,17 +1494,42 @@ void output_thread(void* arg)
 	sbbs->console|=CON_R_ECHO;
 
 	while(sbbs->client_socket!=INVALID_SOCKET && !terminate_server) {
+		/*
+		 * I'd like to check the linear buffer against the highwater
+		 * at this point, but it would get too clumsy imho - Deuce
+		 *
+		 * Actually, another option would just be to have the size
+		 * of the linear buffer equal to the MTU... any larger and
+		 * you could have small sends off the end.  this would
+		 * probobly be even clumbsier
+		 */
+		if(bufbot == buftop) {
+			/* Wait for something to output in the RingBuffer */
+			if(sem_trywait_block(&sbbs->outbuf.sem,1000))
+				continue;
 
-    	if(bufbot==buftop)
-	    	avail=RingBufFull(&sbbs->outbuf);
-        else
-        	avail=buftop-bufbot;
+			/* Check for spurious sem post... */
+			if(!RingBufFull(&sbbs->outbuf))
+				continue;
 
-		if(!avail) {
-			sem_wait(&sbbs->outbuf.sem);
+			/* Wait for full buffer or drain timeout */
 			if(sbbs->outbuf.highwater_mark)
 				sem_trywait_block(&sbbs->outbuf.highwater_sem,startup->outbuf_drain_timeout);
-			continue; 
+
+			/*
+			 * At this point, there's something to send and,
+			 * if the highwater mark is set, the timeout has
+			 * passed or we've hit highwater.  Read ring buffer
+			 * into linear buffer.
+			 */
+	    	avail=RingBufFull(&sbbs->outbuf);
+           	if(avail>sizeof(buf)) {
+               	lprintf(LOG_WARNING,"!%s: Insufficient linear output buffer (%lu > %lu)"
+					,node, avail, sizeof(buf));
+               	avail=sizeof(buf);
+           	}
+           	buftop=RingBufRead(&sbbs->outbuf, buf, avail);
+           	bufbot=0;
 		}
 
 		/* Check socket for writability (using select) */
@@ -1481,8 +1541,9 @@ void output_thread(void* arg)
 
 		i=select(sbbs->client_socket+1,NULL,&socket_set,NULL,&tv);
 		if(i==SOCKET_ERROR) {
-			lprintf(LOG_ERR,"!%s: ERROR %d selecting socket %u for send"
-				,node,ERROR_VALUE,sbbs->client_socket);
+			if(sbbs->client_socket!=INVALID_SOCKET)
+				lprintf(LOG_ERR,"!%s: ERROR %d selecting socket %u for send"
+					,node,ERROR_VALUE,sbbs->client_socket);
 			if(sbbs->cfg.node_num)	/* Only break if node output (not server) */
 				break;
 			RingBufReInit(&sbbs->outbuf);	/* Flush output buffer */
@@ -1492,15 +1553,6 @@ void output_thread(void* arg)
 			continue;
 		}
 
-        if(bufbot==buftop) { // linear buf empty, read from ring buf
-            if(avail>sizeof(buf)) {
-                lprintf(LOG_WARNING,"!%s: Insufficient linear output buffer (%lu > %lu)"
-					,node, avail, sizeof(buf));
-                avail=sizeof(buf);
-            }
-            buftop=RingBufRead(&sbbs->outbuf, buf, avail);
-            bufbot=0;
-        }
 		i=sendsocket(sbbs->client_socket, (char*)buf+bufbot, buftop-bufbot);
 		if(i==SOCKET_ERROR) {
         	if(ERROR_VALUE == ENOTSOCK)
@@ -2288,6 +2340,7 @@ sbbs_t::sbbs_t(ushort node_num, DWORD addr, char* name, SOCKET sd,
 	timeleft = 60*10;	/* just incase this is being used for calling gettimeleft() */
 	uselect_total = 0;
 	lbuflen = 0;
+	keybufbot=keybuftop=0;	/* initialize [unget]keybuf pointers */
 	connection="Telnet";
 
 	ZERO_VAR(telnet_local_option);
@@ -2297,7 +2350,7 @@ sbbs_t::sbbs_t(ushort node_num, DWORD addr, char* name, SOCKET sd,
 	telnet_mode=0;
 	telnet_last_rxch=0;
 
-	sys_status=lncntr=tos=criterrs=lbuflen=slcnt=0L;
+	sys_status=lncntr=tos=criterrs=slcnt=0L;
 	curatr=LIGHTGRAY;
 	attr_sp=0;	/* attribute stack pointer */
 	errorlevel=0;
@@ -2887,7 +2940,7 @@ int sbbs_t::mv(char *src, char *dest, char copy)
 	int		ind,outd;
 	uint	chunk=MV_BUFLEN;
 	ulong	length,l;
-	/* struct ftime ftime; */
+	time_t	ftime;
 	FILE *inp,*outp;
 
     if(!stricmp(src,dest))	 /* source and destination are the same! */
@@ -2936,48 +2989,44 @@ int sbbs_t::mv(char *src, char *dest, char copy)
         return(-1); 
 	}
     setvbuf(outp,NULL,_IOFBF,8*1024);
+	ftime=filetime(ind);
     length=filelength(ind);
-    if(!length) {
-        fclose(inp);
-        fclose(outp);
-        errormsg(WHERE,ERR_LEN,src,0);
-        return(-1); 
-	}
-    if((buf=(char *)malloc(MV_BUFLEN))==NULL) {
-        fclose(inp);
-        fclose(outp);
-        errormsg(WHERE,ERR_ALLOC,nulstr,MV_BUFLEN);
-        return(-1); 
-	}
-    l=0L;
-    while(l<length) {
-        bprintf("%2lu%%",l ? (long)(100.0/((float)length/l)) : 0L);
-        if(l+chunk>length)
-            chunk=length-l;
-        if(fread(buf,1,chunk,inp)!=chunk) {
-            free(buf);
-            fclose(inp);
-            fclose(outp);
-            errormsg(WHERE,ERR_READ,src,chunk);
-            return(-1); 
+    if(length) {	/* Something to copy */
+		if((buf=(char *)malloc(MV_BUFLEN))==NULL) {
+			fclose(inp);
+			fclose(outp);
+			errormsg(WHERE,ERR_ALLOC,nulstr,MV_BUFLEN);
+			return(-1); 
 		}
-        if(fwrite(buf,1,chunk,outp)!=chunk) {
-            free(buf);
-            fclose(inp);
-            fclose(outp);
-            errormsg(WHERE,ERR_WRITE,dest,chunk);
-            return(-1); 
+		l=0L;
+		while(l<length) {
+			bprintf("%2lu%%",l ? (long)(100.0/((float)length/l)) : 0L);
+			if(l+chunk>length)
+				chunk=length-l;
+			if(fread(buf,1,chunk,inp)!=chunk) {
+				free(buf);
+				fclose(inp);
+				fclose(outp);
+				errormsg(WHERE,ERR_READ,src,chunk);
+				return(-1); 
+			}
+			if(fwrite(buf,1,chunk,outp)!=chunk) {
+				free(buf);
+				fclose(inp);
+				fclose(outp);
+				errormsg(WHERE,ERR_WRITE,dest,chunk);
+				return(-1); 
+			}
+			l+=chunk;
+			bputs("\b\b\b"); 
 		}
-        l+=chunk;
-        bputs("\b\b\b"); 
+		bputs("   \b\b\b");  /* erase it */
+		attr(atr);
+		free(buf);
 	}
-    bputs("   \b\b\b");  /* erase it */
-    attr(atr);
-    /* getftime(ind,&ftime);
-    setftime(outd,&ftime); */
-    free(buf);
     fclose(inp);
     fclose(outp);
+	setfdate(dest,ftime);	/* Would be nice if we could use futime() instead */
     if(!copy && remove(src)) {
         errormsg(WHERE,ERR_REMOVE,src,0);
         return(-1); 
@@ -3167,6 +3216,7 @@ void sbbs_t::reset_logon_vars(void)
     slcnt=0;
     altul=0;
     timeleft_warn=0;
+	keybufbot=keybuftop=0;
     logon_uls=logon_ulb=logon_dls=logon_dlb=0;
     logon_posts=logon_emails=logon_fbacks=0;
     batdn_total=batup_total=0;
--- xpdev/genwrap.c
+++ xpdev/genwrap.c
@@ -2,7 +2,7 @@
 
 /* General cross-platform development wrappers */
 
-/* $Id: genwrap.c,v 1.59 2005/10/15 01:59:50 rswindell Exp $ */
+/* $Id: genwrap.c,v 1.67 2005/12/01 00:30:30 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -236,9 +236,7 @@ char* strrev(char* str)
 /****************************************************************************/
 unsigned DLLCALL xp_randomize(void)
 {
-	unsigned thread_id = (unsigned)GetCurrentThreadId();
-	unsigned process_id = (unsigned)GetCurrentProcessId();
-	unsigned seed = time(NULL) ^ BYTE_SWAP_INT(thread_id) ^ process_id;
+	unsigned seed=~0;
 
 #if defined(HAS_DEV_RANDOM) && defined(RANDOM_DEV)
 	int     rf;
@@ -247,6 +245,16 @@ unsigned DLLCALL xp_randomize(void)
 		read(rf, &seed, sizeof(seed));
 		close(rf);
 	}
+#else
+	unsigned curtime	= (unsigned)time(NULL);
+	unsigned process_id = (unsigned)GetCurrentProcessId();
+
+	seed = curtime ^ BYTE_SWAP_INT(process_id);
+
+	#if defined(_WIN32) || defined(GetCurrentThreadId)
+		seed ^= (unsigned)GetCurrentThreadId();
+	#endif
+
 #endif
 
  	srand(seed);
@@ -491,15 +499,14 @@ int DLLCALL	get_errno(void)
 long double	DLLCALL	xp_timer(void)
 {
 	long double ret;
-#ifdef __unix__
+#if defined(__unix__)
 	struct timeval tv;
 	if(gettimeofday(&tv,NULL)==1)
 		return(-1);
 	ret=tv.tv_usec;
 	ret /= 1000000;
 	ret += tv.tv_sec;
-#else
-#ifdef _WIN32
+#elif defined(_WIN32)
 	LARGE_INTEGER	freq;
 	LARGE_INTEGER	tick;
 	if(QueryPerformanceFrequency(&freq) && QueryPerformanceCounter(&tick)) {
@@ -507,7 +514,9 @@ long double	DLLCALL	xp_timer(void)
 		ret=((long double)tick.HighPart*4294967296)+((long double)tick.LowPart);
 		ret /= ((long double)freq.HighPart*4294967296)+((long double)freq.LowPart);
 #else
-		ret=((long double)tick.QuadPart)/freq.QuadPart;
+		/* In MSVC, a long double does NOT have 19 decimals of precision */
+		ret=(((long double)(tick.QuadPart%freq.QuadPart))/freq.QuadPart);
+		ret+=tick.QuadPart/freq.QuadPart;
 #endif
 	}
 	else {
@@ -515,8 +524,7 @@ long double	DLLCALL	xp_timer(void)
 		ret /= 1000;
 	}
 #else
-#error Need xp_timer implementation!
-#endif
+	ret=time(NULL);	/* Weak implementation */
 #endif
 	return(ret);
 }
