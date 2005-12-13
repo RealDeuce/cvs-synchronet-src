c8c299c71cac621dbf93ffde28d7ee2bb2caa07c
deuce <deuce>
2005-12-13 21:05:53 +0000

    Make close_request() wait for the output buffer to drain.

--- sbbs3/websrvr.c
+++ sbbs3/websrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Web Server */
 
-/* $Id: websrvr.c,v 1.374 2005/12/13 00:01:42 deuce Exp $ */
+/* $Id: websrvr.c,v 1.375 2005/12/13 21:05:53 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -739,6 +739,7 @@ static void close_request(http_session_t * session)
 	int			i;
 
 	if(session->req.write_chunked) {
+		/* ToDo: This should probobly timeout eventually... */
 		while(RingBufFull(&session->outbuf))
 			SLEEP(1);
 		session->req.write_chunked=0;
@@ -752,6 +753,11 @@ static void close_request(http_session_t * session)
 
 	/* Force the output thread to go NOW */
 	sem_post(&(session->outbuf.highwater_sem));
+
+	/* Wait for the drain */
+	/* ToDo: This should probobly timeout eventually... */
+	while(RingBufFull(&session->outbuf))
+		SLEEP(1);
 
 	if(session->req.ld!=NULL) {
 		now=time(NULL);
@@ -3888,7 +3894,7 @@ const char* DLLCALL web_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.374 $", "%*s %s", revision);
+	sscanf("$Revision: 1.375 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
