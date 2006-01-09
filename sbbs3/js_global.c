facabe091e803849e9eb889a3930a498efc46ce9
deuce <deuce>
2006-01-09 21:28:38 +0000

    Use XP_PRINTF_CONVERT to convert the passed value to the type which is actually used in the format string.

--- sbbs3/js_global.c
+++ sbbs3/js_global.c
@@ -2,7 +2,7 @@
 
 /* Synchronet JavaScript "global" object properties/methods for all servers */
 
-/* $Id: js_global.c,v 1.158 2006/01/07 19:59:27 rswindell Exp $ */
+/* $Id: js_global.c,v 1.159 2006/01/09 21:28:38 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -285,6 +285,7 @@ js_load(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 }
 
 #ifdef USE_XP_PRINTF
+//#if 1
 
 static JSBool
 js_format(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
@@ -299,15 +300,15 @@ js_format(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	fmt=xp_asprintf_start(fmt);
     for(i=1; i<argc; i++) {
 		if(JSVAL_IS_DOUBLE(argv[i]))
-			fmt=xp_asprintf_next(fmt,XP_PRINTF_TYPE_DOUBLE,*JSVAL_TO_DOUBLE(argv[i]));
+			fmt=xp_asprintf_next(fmt,XP_PRINTF_CONVERT|XP_PRINTF_TYPE_DOUBLE,*JSVAL_TO_DOUBLE(argv[i]));
 		else if(JSVAL_IS_INT(argv[i]))
-			fmt=xp_asprintf_next(fmt,XP_PRINTF_TYPE_INT,JSVAL_TO_INT(argv[i]));
+			fmt=xp_asprintf_next(fmt,XP_PRINTF_CONVERT|XP_PRINTF_TYPE_INT,JSVAL_TO_INT(argv[i]));
 		else {
 			if((str=JS_ValueToString(cx, argv[i]))==NULL) {
 				JS_ReportError(cx,"JS_ValueToString failed");
 			    return(JS_FALSE);
 			}
-			fmt=xp_asprintf_next(fmt,XP_PRINTF_TYPE_CHARP,JS_GetStringBytes(str));
+			fmt=xp_asprintf_next(fmt,XP_PRINTF_CONVERT|XP_PRINTF_TYPE_CHARP,JS_GetStringBytes(str));
 		}
 	}
 
