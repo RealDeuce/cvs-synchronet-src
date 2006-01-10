e8f7bd08f127705cc57e14cf4548655c46e7de59
deuce <deuce>
2006-01-10 01:21:44 +0000

    If the sprintf() returns < 0 then do not modify the format specifier.

--- sbbs3/websrvr.h
+++ sbbs3/websrvr.h
@@ -2,13 +2,13 @@
 
 /* Synchronet Web Server */
 
-/* $Id: websrvr.h,v 1.38 2006/01/31 23:11:24 rswindell Exp $ */
+/* $Id: websrvr.h,v 1.37 2005/11/28 02:19:35 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -145,7 +145,7 @@ static ini_bitdesc_t web_options[] = {
 };
 #endif
 
-#define WEB_DEFAULT_ROOT_DIR		"../web/root"
+#define WEB_DEFAULT_ROOT_DIR		"../web/html"
 #define WEB_DEFAULT_ERROR_DIR		"error"
 #define WEB_DEFAULT_CGI_DIR			"cgi-bin"
 #define WEB_DEFAULT_CGI_CONTENT		"text/plain"
