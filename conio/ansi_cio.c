aac8f3bf86e264f528d6f27ff2f3480080ff08ca
deuce <deuce>
2006-01-11 17:42:03 +0000

    Some fix-ups of textattr() in ANSI mode. #ifdef'ed out support for ESC[22m and ESC[25m (Unfortunately not widely supported). Fix old calculations to match new calculations so the equality test is meaningfull (should result in less verbose messages).
    Still no idea what is causing the bright attribute to be stuck high when
    SyncTERM is ran as a door under Synchronet and accessed using mTel.

--- conio/ansi_cio.c
+++ conio/ansi_cio.c
@@ -1,4 +1,4 @@
-/* $Id: ansi_cio.c,v 1.44 2005/10/14 06:21:15 deuce Exp $ */
+/* $Id: ansi_cio.c,v 1.45 2006/01/11 17:42:03 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -317,21 +317,26 @@ void ansi_textattr(int attr)
 	br=attr&0x04;
 
 	oa=ansi_curr_attr>>8;
-	obl=oa>>7;
+	obl=oa&0x80;
 	obg=(oa>>4)&0x7;
 	ofg=oa&0x07;
-	obr=(oa>>3)&0x01;
+	obr=oa&0x04;
 
 	ansi_curr_attr=attr<<8;
 
 	strcpy(str,"\033[");
 	if(obl!=bl) {
-		if(!bl) {
+		if(!bl)
+#if 0
+			strcat(str,"25;");
+#else
+		{
 			strcat(str,"0;");
 			ofg=7;
 			obg=0;
 			obr=0;
 		}
+#endif
 		else
 			strcat(str,"5;");
 	}
@@ -340,10 +345,12 @@ void ansi_textattr(int attr)
 			strcat(str,"1;");
 		else
 #if 0
-			strcat(str,"2;");
+			strcat(str,"22;");
 #else
 		{
 			strcat(str,"0;");
+			if(bl)
+				strcat(str,"5;");
 			ofg=7;
 			obg=0;
 		}
