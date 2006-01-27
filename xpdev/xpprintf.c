757f85a80a43cf8e6f1121d9e689f9fb7ba77d6f
deuce <deuce>
2006-01-27 05:49:47 +0000

    Add xp_asprintf_free() function so Win32 can still keep goats contented with its DLLs.

--- xpdev/xpprintf.c
+++ xpdev/xpprintf.c
@@ -2,7 +2,7 @@
 
 /* Deuce's vs[n]printf() replacement */
 
-/* $Id: xpprintf.c,v 1.32 2006/05/09 21:02:55 deuce Exp $ */
+/* $Id: xpprintf.c,v 1.29 2006/01/27 05:49:47 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -39,9 +39,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#if defined(_WIN32)
- #include <malloc.h>    /* alloca() on Win32 */
-#endif
 
 #include "xpprintf.h"
 
@@ -313,6 +310,7 @@ char *xp_asprintf_next(char *format, int type, ...)
 	char			this_format[MAX_FORMAT_LEN];
 	char			*fmt;
 	int				modifier=0;
+	char			*fmt_start;
 	int				correct_type=0;
 	char			num_str[128];		/* More than enough room for a 256-bit int */
 	size_t			width=0;
@@ -328,6 +326,7 @@ char *xp_asprintf_next(char *format, int type, ...)
 	format_len=strlen(format+sizeof(size_t))+sizeof(size_t)+1;
 	this_format[0]=0;
 	fmt=this_format;
+	fmt_start=p;
 	*(fmt++)=*(p++);
 
 	/*
@@ -1144,7 +1143,7 @@ char *xp_asprintf_next(char *format, int type, ...)
 				if(s<precision)
 					s=precision;
 				if(s>=MAX_ARG_LEN)
-					entry=(char *)alloca(s+1);
+					entry=(char *)malloc(s+1);
 				if(entry==NULL)
 					return(NULL);
 				j=sprintf(entry, this_format, cp);
@@ -1173,13 +1172,18 @@ char *xp_asprintf_next(char *format, int type, ...)
 		 */
 		if(format_len < (format_len-this_format_len+j)) {
 			newbuf=(char *)realloc(format, format_len-this_format_len+j);
-			if(newbuf==NULL)
+			if(newbuf==NULL) {
+				if(entry != entry_buf)
+					free(entry);
 				return(NULL);
+			}
 			format=newbuf;
 		}
 		/* Move trailing end to make space */
 		memmove(format+offset+j, format+offset+this_format_len, format_len-offset-this_format_len);
 		memcpy(format+offset, entry, j);
+		if(entry != entry_buf)
+			free(entry);
 		p=format+offset+j;
 	}
 	else
