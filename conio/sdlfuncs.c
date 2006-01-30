80df3f6337cc761bab4be8cfff853258af52f2c2
rswindell <rswindell>
2006-01-30 22:52:03 +0000

    Introduced K_NOSPIN mode flag for getkey() - to over-ride (temporarily disable) user's spinning cursor setting.

--- conio/sdlfuncs.c
+++ conio/sdlfuncs.c
@@ -1,29 +1,6 @@
-#include <stdlib.h>	/* getenv()/exit()/atexit() */
 #include <stdio.h>	/* NULL */
-#ifdef __unix__
-#include <dlfcn.h>
-#endif
 
-#include <SDL.h>
-#ifndef main
- #define USE_REAL_MAIN
-#endif
-#include "gen_defs.h"
-#ifdef USE_REAL_MAIN
- #undef main
-#endif
 #include "sdlfuncs.h"
-
-#ifndef _WIN32
-struct sdlfuncs sdl;
-#endif
-
-static int sdl_funcs_loaded=0;
-static int sdl_initialized=0;
-static int sdl_audio_initialized=0;
-static int sdl_video_initialized=0;
-
-int XPDEV_main(int argc, char **argv, char **enviro);
 
 #ifdef STATIC_SDL
 int load_sdl_funcs(struct sdlfuncs *sdlf)
@@ -47,7 +24,6 @@ int load_sdl_funcs(struct sdlfuncs *sdlf)
 	sdlf->BlitSurface=SDL_UpperBlit;
 	sdlf->UpdateRects=SDL_UpdateRects;
 	sdlf->SDL_CreateSemaphore=SDL_CreateSemaphore;
-	sdlf->SDL_DestroySemaphore=SDL_DestroySemaphore;
 	sdlf->SDL_CreateMutex=SDL_CreateMutex;
 	sdlf->CreateThread=SDL_CreateThread;
 	sdlf->WaitEvent=SDL_WaitEvent;
@@ -60,16 +36,7 @@ int load_sdl_funcs(struct sdlfuncs *sdlf)
 	sdlf->EnableKeyRepeat=SDL_EnableKeyRepeat;
 	sdlf->GetWMInfo=SDL_GetWMInfo;
 	sdlf->GetError=SDL_GetError;
-	sdlf->InitSubSystem=SDL_InitSubSystem;
-	sdlf->QuitSubSystem=SDL_QuitSubSystem;
-	sdlf->OpenAudio=SDL_OpenAudio;
-	sdlf->CloseAudio=SDL_CloseAudio;
-	sdlf->PauseAudio=SDL_PauseAudio;
-	sdlf->LockAudio=SDL_LockAudio;
-	sdlf->UnlockAudio=SDL_UnlockAudio;
-	sdlf->GetAudioStatus=SDL_GetAudioStatus;
 	sdlf->gotfuncs=1;
-	sdl_funcs_loaded=1;
 	return(0);
 }
 #else
@@ -151,10 +118,6 @@ int load_sdl_funcs(struct sdlfuncs *sdlf)
 		FreeLibrary(sdl_dll);
 		return(-1);
 	}
-	if((sdlf->SDL_DestroySemaphore=GetProcAddress(sdl_dll, "SDL_DestroySemaphore"))==NULL) {
-		FreeLibrary(sdl_dll);
-		return(-1);
-	}
 	if((sdlf->SDL_CreateMutex=GetProcAddress(sdl_dll, "SDL_CreateMutex"))==NULL) {
 		FreeLibrary(sdl_dll);
 		return(-1);
@@ -203,40 +166,7 @@ int load_sdl_funcs(struct sdlfuncs *sdlf)
 		FreeLibrary(sdl_dll);
 		return(-1);
 	}
-	if((sdlf->InitSubSystem=GetProcAddress(sdl_dll, "SDL_InitSubSystem"))==NULL) {
-		FreeLibrary(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->QuitSubSystem=GetProcAddress(sdl_dll, "SDL_QuitSubSystem"))==NULL) {
-		FreeLibrary(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->OpenAudio=GetProcAddress(sdl_dll, "SDL_OpenAudio"))==NULL) {
-		FreeLibrary(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->CloseAudio=GetProcAddress(sdl_dll, "SDL_CloseAudio"))==NULL) {
-		FreeLibrary(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->PauseAudio=GetProcAddress(sdl_dll, "SDL_PauseAudio"))==NULL) {
-		FreeLibrary(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->LockAudio=GetProcAddress(sdl_dll, "SDL_LockAudio"))==NULL) {
-		FreeLibrary(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->UnlockAudio=GetProcAddress(sdl_dll, "SDL_UnlockAudio"))==NULL) {
-		FreeLibrary(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->GetAudioStatus=GetProcAddress(sdl_dll, "SDL_GetAudioStatus"))==NULL) {
-		FreeLibrary(sdl_dll);
-		return(-1);
-	}
 	sdlf->gotfuncs=1;
-	sdl_funcs_loaded=1;
 	return(0);
 }
 #elif defined(__unix__)
@@ -312,10 +242,6 @@ int load_sdl_funcs(struct sdlfuncs *sdlf)
 		dlclose(sdl_dll);
 		return(-1);
 	}
-	if((sdlf->SDL_DestroySemaphore=dlsym(sdl_dll, "SDL_DestroySemaphore"))==NULL) {
-		dlclose(sdl_dll);
-		return(-1);
-	}
 	if((sdlf->SDL_CreateMutex=dlsym(sdl_dll, "SDL_CreateMutex"))==NULL) {
 		dlclose(sdl_dll);
 		return(-1);
@@ -364,139 +290,9 @@ int load_sdl_funcs(struct sdlfuncs *sdlf)
 		dlclose(sdl_dll);
 		return(-1);
 	}
-	if((sdlf->InitSubSystem=dlsym(sdl_dll, "SDL_InitSubSystem"))==NULL) {
-		dlclose(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->QuitSubSystem=dlsym(sdl_dll, "SDL_QuitSubSystem"))==NULL) {
-		dlclose(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->OpenAudio=dlsym(sdl_dll, "SDL_OpenAudio"))==NULL) {
-		dlclose(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->CloseAudio=dlsym(sdl_dll, "SDL_CloseAudio"))==NULL) {
-		dlclose(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->PauseAudio=dlsym(sdl_dll, "SDL_PauseAudio"))==NULL) {
-		dlclose(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->LockAudio=dlsym(sdl_dll, "SDL_LockAudio"))==NULL) {
-		dlclose(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->UnlockAudio=dlsym(sdl_dll, "SDL_UnlockAudio"))==NULL) {
-		dlclose(sdl_dll);
-		return(-1);
-	}
-	if((sdlf->GetAudioStatus=dlsym(sdl_dll, "SDL_GetAudioStatus"))==NULL) {
-		dlclose(sdl_dll);
-		return(-1);
-	}
 	sdlf->gotfuncs=1;
-	sdl_funcs_loaded=1;
 	return(0);
 }
 #endif
 
 #endif
-
-int init_sdl_video(void)
-{
-	/* This is all handled in SDL_main_env() */
-	if(sdl_video_initialized)
-		return(0);
-	else
-		return(-1);
-}
-
-int init_sdl_audio(void)
-{
-	if(!sdl_initialized)
-		return(-1);
-	if(sdl_audio_initialized)
-		return(0);
-	if(sdl.InitSubSystem(SDL_INIT_AUDIO)==0) {
-		sdl_audio_initialized=TRUE;
-		return(0);
-	}
-	return(-1);
-}
-
-/* atexit() function */
-static void sdl_exit(void)
-{
-	sdl.Quit();
-}
-
-#ifndef main
-int main(int argc, char **argv, char **env)
-#else
-int SDL_main_env(int argc, char **argv, char **env)
-#endif
-{
-	unsigned int i;
-	SDL_Event	ev;
-	char	drivername[64];
-
-#ifndef _WIN32
-	load_sdl_funcs(&sdl);
-#endif
-
-	if(sdl.gotfuncs) {
-#ifdef _WIN32
-		/* Fail to windib (ie: No mouse attached) */
-		if(sdl.Init(SDL_INIT_VIDEO)) {
-			if(getenv("SDL_VIDEODRIVER")==NULL) {
-				putenv("SDL_VIDEODRIVER=windib");
-				WinExec(GetCommandLine(), SW_SHOWDEFAULT);
-				exit(0);
-			}
-			sdl.gotfuncs=FALSE;
-		}
-		else {
-			sdl_video_initialized=TRUE;
-		}
-#else
-
-		/*
-		 * On Linux, SDL doesn't properly detect availability of the
-		 * framebuffer apparently.  This results in remote connections
-		 * displaying on the local framebuffer... a definate no-no.
-		 * This ugly hack attempts to prevent this... of course, remote X11
-		 * connections must still be allowed.
-		 */
-		if(getenv("REMOTEHOST")!=NULL && getenv("DISPLAY")==NULL) {
-			/* Sure ,we can't use video, but audio is still valid! */
-			if(sdl.Init(0)==0)
-				sdl_initialized=TRUE;
-			sdl.gotfuncs=FALSE;
-		}
-		else {
-			if(sdl.Init(SDL_INIT_VIDEO))
-				sdl.gotfuncs=FALSE;
-			else {
-				sdl_initialized=TRUE;
-				sdl_video_initialized=TRUE;
-			}
-		}
-#endif
-		if(sdl_video_initialized && sdl.VideoDriverName(drivername, sizeof(drivername))!=NULL) {
-			/* Unacceptable drivers */
-			if((!strcmp(drivername, "caca")) || (!strcmp(drivername,"aalib")) || (!strcmp(drivername,"dummy"))) {
-				sdl.gotfuncs=FALSE;
-				sdl.QuitSubSystem(SDL_INIT_VIDEO);
-				sdl_video_initialized=FALSE;
-			}
-			else {
-				sdl_video_initialized=TRUE;
-			}
-		}
-	}
-	if(sdl_initialized)
-		atexit(sdl_exit);
-	return(XPDEV_main(argc, argv, env));
-}
