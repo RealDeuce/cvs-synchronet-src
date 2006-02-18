9c76ed09fab9b24b672ac0328af5cd9e94b7ca93
deuce <deuce>
2006-02-18 04:59:49 +0000

    Remove the cscope -q flag.  I always forget it, and no need to rebuild all the time. Also, I've recently switched to excflags.

--- sbbs3/download.cpp
+++ sbbs3/download.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet file download routines */
 
-/* $Id: download.cpp,v 1.35 2006/06/06 17:13:34 rswindell Exp $ */
+/* $Id: download.cpp,v 1.33 2006/01/31 02:51:59 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -448,38 +448,4 @@ void sbbs_t::seqwait(uint devnum)
 		mswait(100); 
 	}
 
-}
-
-bool sbbs_t::sendfile(char* fname, char prot)
-{
-	char	keys[128];
-	char	ch;
-	size_t	i;
-	bool	result=false;
-
-	if(prot)
-		ch=toupper(prot);
-	else {
-		xfer_prot_menu(XFER_DOWNLOAD);
-		mnemonics(text[ProtocolOrQuit]);
-		strcpy(keys,"Q");
-		for(i=0;i<cfg.total_prots;i++)
-			if(cfg.prot[i]->dlcmd[0] && chk_ar(cfg.prot[i]->ar,&useron))
-				sprintf(keys+strlen(keys),"%c",cfg.prot[i]->mnemonic);
-
-		ch=(char)getkeys(keys,0);
-
-		if(ch=='Q' || sys_status&SS_ABORT)
-			return(false); 
-	}
-	for(i=0;i<cfg.total_prots;i++)
-		if(cfg.prot[i]->mnemonic==ch && chk_ar(cfg.prot[i]->ar,&useron))
-			break;
-	if(i<cfg.total_prots) {
-		if(protocol(cfg.prot[i],XFER_DOWNLOAD,fname,fname,false)==0)
-			result=true;
-		autohangup(); 
-	}
-
-	return(result);
 }
