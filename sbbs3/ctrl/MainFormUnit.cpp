857abaae585096eb7d117b0f251b0fb2a530fc17
rswindell <rswindell>
2006-03-14 03:17:39 +0000

    sbbsctrl now supports the ctrl/recycle[.ctrl] and ctrl/shutdown[.ctrl] semaphore files so: 1. SCFG will touch ctrl/recycle causing sbbsctrl to reload its copy of the config files resolving the FAQ about why users appear to be running the  wrong external program in the node window after adding/removing external programs in SCFG. 2. the control panel can now be gracefully shutdown by touching ctrl/shutdown.

--- sbbs3/ctrl/MainFormUnit.cpp
+++ sbbs3/ctrl/MainFormUnit.cpp
@@ -1,6 +1,6 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: MainFormUnit.cpp,v 1.156 2006/05/24 06:14:22 rswindell Exp $ */
+/* $Id: MainFormUnit.cpp,v 1.153 2006/03/14 03:17:39 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -129,7 +129,6 @@ int     threads=1;
 time_t  initialized=0;
 static	str_list_t recycle_semfiles;
 static  str_list_t shutdown_semfiles;
-bool    terminating=false;
 
 static void thread_up(void* p, BOOL up, BOOL setuid)
 {
@@ -1118,7 +1117,7 @@ void __fastcall TMainForm::FormCloseQuery(TObject *Sender, bool &CanClose)
 	CanClose=false;
 
     if(TelnetStop->Enabled && !bbsServiceEnabled()) {
-     	if(!terminating && TelnetForm->ProgressBar->Position
+     	if(TelnetForm->ProgressBar->Position
 	        && Application->MessageBox("Shut down the Telnet Server?"
         	,"Telnet Server In Use", MB_OKCANCEL)!=IDOK)
             return;
@@ -1126,7 +1125,7 @@ void __fastcall TMainForm::FormCloseQuery(TObject *Sender, bool &CanClose)
 	}
 
     if(MailStop->Enabled && !mailServiceEnabled()) {
-    	if(!terminating && MailForm->ProgressBar->Position
+    	if(MailForm->ProgressBar->Position
     		&& Application->MessageBox("Shut down the Mail Server?"
         	,"Mail Server In Use", MB_OKCANCEL)!=IDOK)
             return;
@@ -1134,7 +1133,7 @@ void __fastcall TMainForm::FormCloseQuery(TObject *Sender, bool &CanClose)
     }
 
     if(FtpStop->Enabled && !ftpServiceEnabled()) {
-    	if(!terminating && FtpForm->ProgressBar->Position
+    	if(FtpForm->ProgressBar->Position
     		&& Application->MessageBox("Shut down the FTP Server?"
 	       	,"FTP Server In Use", MB_OKCANCEL)!=IDOK)
             return;
@@ -1142,7 +1141,7 @@ void __fastcall TMainForm::FormCloseQuery(TObject *Sender, bool &CanClose)
     }
 
     if(WebStop->Enabled && !webServiceEnabled()) {
-    	if(!terminating && WebForm->ProgressBar->Position
+    	if(WebForm->ProgressBar->Position
     		&& Application->MessageBox("Shut down the Web Server?"
 	       	,"Web Server In Use", MB_OKCANCEL)!=IDOK)
             return;
@@ -1977,6 +1976,9 @@ void __fastcall TMainForm::StartupTimerTick(TObject *Sender)
 
         if(Registry->ValueExists("LastNode"))
             bbs_startup.last_node=Registry->ReadInteger("LastNode");
+
+        if(Registry->ValueExists("ExternalYield"))
+            bbs_startup.xtrn_polls_before_yield=Registry->ReadInteger("ExternalYield");
 
         if(Registry->ValueExists("OutbufHighwaterMark"))
             bbs_startup.outbuf_highwater_mark=Registry->ReadInteger("OutbufHighwaterMark");
@@ -3252,7 +3254,7 @@ void __fastcall TMainForm::BBSConfigWizardMenuItemClick(TObject *Sender)
     Application->CreateForm(__classid(TConfigWizard), &ConfigWizard);
 	if(ConfigWizard->ShowModal()==mrOk) {
         SaveSettings(Sender);
-//        ReloadConfigExecute(Sender);  /* unnecessary since refresh_cfg() is already called */
+        ReloadConfigExecute(Sender);
     }
     delete ConfigWizard;
 
@@ -3267,18 +3269,6 @@ void __fastcall TMainForm::PageControlUnDock(TObject *Sender,
         Allow=UndockableForms;
 }
 //---------------------------------------------------------------------------
-void __fastcall TMainForm::reload_config(void)
-{
-	char error[256];
-	SAFECOPY(error,UNKNOWN_LOAD_ERROR);
-	if(!load_cfg(&cfg, NULL, TRUE, error)) {
-    	Application->MessageBox(error,"ERROR Re-loading Configuration"
-	        ,MB_OK|MB_ICONEXCLAMATION);
-        Application->Terminate();
-    }
-   	semfile_list_check(&initialized,recycle_semfiles);
-}
-//---------------------------------------------------------------------------
 
 void __fastcall TMainForm::ReloadConfigExecute(TObject *Sender)
 {
@@ -3288,7 +3278,15 @@ void __fastcall TMainForm::ReloadConfigExecute(TObject *Sender)
     TelnetRecycleExecute(Sender);
 	ServicesRecycleExecute(Sender);
 
-    reload_config();
+	char error[256];
+	SAFECOPY(error,UNKNOWN_LOAD_ERROR);
+	if(!load_cfg(&cfg, NULL, TRUE, error)) {
+    	Application->MessageBox(error,"ERROR Re-loading Configuration"
+	        ,MB_OK|MB_ICONEXCLAMATION);
+        Application->Terminate();
+    }
+   	semfile_list_check(&initialized,recycle_semfiles);
+
 #if 0   /* This appears to be redundant */
     node_t node;
     for(int i=0;i<cfg.sys_nodes;i++) {
@@ -3700,18 +3698,20 @@ void __fastcall TMainForm::ViewFile(AnsiString filename, AnsiString Caption)
     }
 }
 //---------------------------------------------------------------------------
+
+
+
 void __fastcall TMainForm::SemFileTimerTick(TObject *Sender)
 {
     char* p;
 
     if((p=semfile_list_check(&initialized,shutdown_semfiles))!=NULL) {
 	    StatusBar->Panels->Items[4]->Text=AnsiString(p) + " signaled";
-        terminating=true;
         Close();
     }
     else if((p=semfile_list_check(&initialized,recycle_semfiles))!=NULL) {
 	    StatusBar->Panels->Items[4]->Text=AnsiString(p) + " signaled";
-        reload_config();
+        ReloadConfigExecute(Sender);
     }
 }
 //---------------------------------------------------------------------------
--- syncterm/conn.h
+++ syncterm/conn.h
@@ -1,4 +1,4 @@
-/* $Id: conn.h,v 1.7 2006/08/29 21:27:43 deuce Exp $ */
+/* $Id: conn.h,v 1.5 2005/06/24 04:29:20 deuce Exp $ */
 
 #ifndef _CONN_H_
 #define _CONN_H_
@@ -7,16 +7,12 @@
 
 extern SOCKET conn_socket;
 extern char *conn_types[];
-extern int conn_ports[];
 
 enum {
 	 CONN_TYPE_UNKNOWN
 	,CONN_TYPE_RLOGIN
 	,CONN_TYPE_TELNET
 	,CONN_TYPE_RAW
-#ifdef USE_CRYPTLIB
-	,CONN_TYPE_SSH
-#endif
 	,CONN_TYPE_TERMINATOR
 };
 
