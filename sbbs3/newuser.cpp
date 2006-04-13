12d3ad544cefbd26101c9eafd9a489254fa22b9d
deuce <deuce>
2006-04-13 07:41:57 +0000

    (possible) ALSA support for xpbeep. Completely and utterly untested.  None of my Linux boxes even have /usr/include/alsa on them, so I simply *can't* test this without yet another Linux install.  If anyone cares about this, has ALSA working correctly on their Linux box, is willing to give me a remote account that can play sound via ALSA, and is willing to sit in IRC beside the system in question to tell me if sounds are coming out, I can probobly get this working.
    Until then, use OSS emulation and everythign should be fine.

--- sbbs3/newuser.cpp
+++ sbbs3/newuser.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet new user routine */
 
-/* $Id: newuser.cpp,v 1.49 2006/09/09 06:24:05 deuce Exp $ */
+/* $Id: newuser.cpp,v 1.46 2006/04/07 04:34:40 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -51,7 +51,6 @@ BOOL sbbs_t::newuser()
 	long	kmode;
 	bool	usa;
 
-#if 0
 	if(cur_rate<cfg.node_minbps) {
 		bprintf(text[MinimumModemSpeed],cfg.node_minbps);
 		sprintf(str,"%stooslow.msg",cfg.text_dir);
@@ -63,7 +62,6 @@ BOOL sbbs_t::newuser()
 		hangup();
 		return(FALSE); 
 	}
-#endif
 
 	getnodedat(cfg.node_num,&thisnode,0);
 	if(thisnode.misc&NODE_LOCK) {
@@ -186,11 +184,7 @@ BOOL sbbs_t::newuser()
 		else
 			useron.misc&=~NO_EXASCII;
 
-#ifdef USE_CRYPTLIB
-		if((sys_status&SS_RLOGIN || sys_status&SS_SSH) && rlogin_name[0])
-#else
 		if(sys_status&SS_RLOGIN && rlogin_name[0])
-#endif
 			strcpy(useron.alias,rlogin_name);
 		else {
 			while(online) {
@@ -401,6 +395,8 @@ BOOL sbbs_t::newuser()
 		while(online) {
 			bprintf(text[NewUserPasswordVerify]);
 			console|=CON_R_ECHOX;
+			if(!(cfg.sys_misc&SM_ECHO_PW))
+				console|=CON_L_ECHOX;
 			str[0]=0;
 			getstr(str,LEN_PASS,K_UPPER);
 			console&=~(CON_R_ECHOX|CON_L_ECHOX);
@@ -463,11 +459,11 @@ BOOL sbbs_t::newuser()
 		sprintf(str,text[NewUserFeedbackHdr]
 			,nulstr,getage(&cfg,useron.birth),useron.sex,useron.birth
 			,useron.name,useron.phone,useron.comp,useron.modem);
-		email(cfg.node_valuser,str,"New User Validation",WM_EMAIL|WM_SUBJ_RO);
+		email(cfg.node_valuser,str,"New User Validation",WM_EMAIL);
 		if(!useron.fbacks && !useron.emails) {
 			if(online) {						/* didn't hang up */
 				bprintf(text[NoFeedbackWarning],username(&cfg,cfg.node_valuser,tmp));
-				email(cfg.node_valuser,str,"New User Validation",WM_EMAIL|WM_SUBJ_RO);
+				email(cfg.node_valuser,str,"New User Validation",WM_EMAIL);
 				} /* give 'em a 2nd try */
 			if(!useron.fbacks && !useron.emails) {
         		bprintf(text[NoFeedbackWarning],username(&cfg,cfg.node_valuser,tmp));
