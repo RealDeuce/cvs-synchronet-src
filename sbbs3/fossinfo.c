f6fa1d23be852d7f467eb050fb540e2dabdc9caf
deuce <deuce>
2006-05-05 07:59:38 +0000

    Add notes on what still needs work.

--- sbbs3/fossinfo.c
+++ sbbs3/fossinfo.c
@@ -6,17 +6,15 @@
 #include <string.h>		/* _fstrncpy */
 #include "fossdefs.h"
 
-void main(int argc, char** argv)
+void main()
 {
 	BYTE far*	bp;
 	fossil_info_t info;
 	WORD	ax,bx;
 	char id_string[128];
-	union REGS regs;
-	struct SREGS sregs;
 
 	printf("\nChecking FOSSIL interrupt vector (interrupt 0x%02X)\n", FOSSIL_INTERRUPT);
-	bp=(void far*)_dos_getvect(FOSSIL_INTERRUPT);
+	bp=getvect(FOSSIL_INTERRUPT);
 
 	printf("FOSSIL interrupt vector: 0x%08lX\n",bp);
 	printf("Signature: 0x%04X (should be 0x%04X)\n", *(WORD far*)(bp+6), FOSSIL_SIGNATURE);
@@ -24,11 +22,13 @@ void main(int argc, char** argv)
 
 
 	printf("\nInitializing FOSSIL\n");
-	regs.h.ah = FOSSIL_FUNC_INIT;
-	int86(FOSSIL_INTERRUPT, &regs, &regs);
-	printf("AX=0x%04X (should be 0x%04X)\n", regs.x.ax, FOSSIL_SIGNATURE);
+	_AH = FOSSIL_FUNC_INIT;
+	geninterrupt(FOSSIL_INTERRUPT);
+	ax=_AX;
+	bx=_BX;
+	printf("AX=0x%04X (should be 0x%04X)\n", ax, FOSSIL_SIGNATURE);
 	printf("BX=0x%04X (FOSSIL rev %u, highest function supported: 0x%02X)\n"
-		,regs.x.bx, regs.h.bh, regs.h.bl);
+		,bx, bx>>8, bx&0xff);
 
 	printf("\nGetting FOSSIL Information\n");
 	printf("sizeof(info)=%u\n",sizeof(info));
@@ -47,14 +47,15 @@ void main(int argc, char** argv)
 **/
 
 	bp = (BYTE far*)&info;
-	regs.h.ah = FOSSIL_FUNC_GET_INFO;
-	regs.x.cx = sizeof(info);
-	regs.x.dx = 0;
-	sregs.es = FP_SEG(bp);
-	regs.x.di = FP_OFF(bp);
+	_AH = FOSSIL_FUNC_GET_INFO;
+	_CX = sizeof(info);
+	_DX = 0;
+	_ES = FP_SEG(bp);
+	_DI = FP_OFF(bp);
 
-	int86x(FOSSIL_INTERRUPT, &regs, &regs, &sregs);
-	printf("AX=0x%04X (%u)\n",regs.x.ax,regs.x.ax);
+	geninterrupt(FOSSIL_INTERRUPT);
+	ax=_AX;
+	printf("AX=0x%04X (%u)\n",ax,ax);
 	printf("Information structure size: %u\n", info.info_size);
     printf("FOSSIL specific revision: %u\n", info.curr_fossil);
     printf("FOSSIL driver revision: %u\n", info.curr_rev);
@@ -76,9 +77,4 @@ void main(int argc, char** argv)
 		,fossil_data_bits[(info.baud_rate&FOSSIL_DATA_BITS_MASK)>>FOSSIL_DATA_BITS_SHIFT]
 		,fossil_stop_bits[(info.baud_rate&FOSSIL_STOP_BITS_MASK)>>FOSSIL_STOP_BITS_SHIFT]
 		);
-
-	if(argc>1 && stricmp(argv[1],"pause")==0) {
-		printf("\nHit enter to continue...");
-		getchar();
-	}
 }	
