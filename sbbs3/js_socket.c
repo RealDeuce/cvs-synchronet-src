69036f8602e5da2c77f6daa87e6034320712cd52
rswindell <rswindell>
2006-05-05 02:36:29 +0000

    Constants and structure definitions for writing FOSSIL drivers and applications

--- sbbs3/js_socket.c
+++ sbbs3/js_socket.c
@@ -2,13 +2,13 @@
 
 /* Synchronet JavaScript "Socket" Object */
 
-/* $Id: js_socket.c,v 1.109 2005/10/14 01:06:05 rswindell Exp $ */
+/* $Id: js_socket.c,v 1.112 2006/04/10 23:15:17 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -704,7 +704,7 @@ static JSBool
 js_peek(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 {
 	char*		buf;
-	int32		len;
+	int32		len=512;
 	JSString*	str;
 
 	private_t*	p;
@@ -869,26 +869,38 @@ js_getsockopt(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval
 {
 	int			opt;
 	int			level;
-	socklen_t	len;
 	int			val;
 	private_t*	p;
+	LINGER		linger;
+	void*		vp=&val;
+	socklen_t	len=sizeof(val);
+
+	*rval = INT_TO_JSVAL(-1);
 
 	if((p=(private_t*)JS_GetPrivate(cx,obj))==NULL) {
 		JS_ReportError(cx,getprivate_failure,WHERE);
 		return(JS_FALSE);
 	}
 
-	opt = getSocketOptionByName(JS_GetStringBytes(JS_ValueToString(cx,argv[0])),&level);
-	len = sizeof(val);
+	if((opt = getSocketOptionByName(JS_GetStringBytes(JS_ValueToString(cx,argv[0])),&level)) == -1)
+		return(JS_TRUE);
 
-	if(opt!=-1 && getsockopt(p->sock, level, opt, (void*)&val, &len)==0) {
-		dbprintf(FALSE, p, "option %d = %d",opt,val);
+	if(opt == SO_LINGER) {
+		vp=&linger;
+		len=sizeof(linger);
+	}
+	if(getsockopt(p->sock, level, opt, vp, &len)==0) {
+		if(opt == SO_LINGER) {
+			if(linger.l_onoff==TRUE)
+				val = linger.l_linger;
+			else
+				val = 0;
+		}
 		JS_NewNumberValue(cx,val,rval);
 	} else {
 		p->last_error=ERROR_VALUE;
 		dbprintf(TRUE, p, "error %d getting option %d"
 			,ERROR_VALUE,opt);
-		*rval = INT_TO_JSVAL(-1);
 	}
 
 	return(JS_TRUE);
@@ -902,6 +914,10 @@ js_setsockopt(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval
 	int			level;
 	int32		val=1;
 	private_t*	p;
+	LINGER		linger;
+	void*		vp=&val;
+	socklen_t	len=sizeof(val);
+
 
 	if((p=(private_t*)JS_GetPrivate(cx,obj))==NULL) {
 		JS_ReportError(cx,getprivate_failure,WHERE);
@@ -911,8 +927,19 @@ js_setsockopt(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval
 	opt = getSocketOptionByName(JS_GetStringBytes(JS_ValueToString(cx,argv[0])),&level);
 	JS_ValueToInt32(cx,argv[1],&val);
 
+	if(opt == SO_LINGER) {
+		if(val) {
+			linger.l_onoff = TRUE;
+			linger.l_linger = (ushort)val;
+		} else {
+			ZERO_VAR(linger);
+		}
+		vp=&linger;
+		len=sizeof(linger);
+	}
+
 	*rval = BOOLEAN_TO_JSVAL(
-		setsockopt(p->sock, level, opt, (char*)&val, sizeof(val))==0);
+		setsockopt(p->sock, level, opt, vp, len)==0);
 	p->last_error=ERROR_VALUE;
 
 	return(JS_TRUE);
@@ -1013,7 +1040,7 @@ enum {
 
 };
 
-#ifdef _DEBUG
+#ifdef BUILD_JSDOCS
 static char* socket_prop_desc[] = {
 	 "error status for the last socket operation that failed - <small>READ ONLY</small>"
 	,"<i>true</i> if socket is in a connected state - <small>READ ONLY</small>"
@@ -1222,16 +1249,16 @@ static jsSyncMethodSpec js_socket_functions[] = {
 		"optionally specifying a network interface (via <i>ip_address</i>)")
 	,311
 	},
-	{"connect",     js_connect,     2,	JSTYPE_BOOLEAN,	JSDOCSTR("host, port [,timeout]")
+	{"connect",     js_connect,     2,	JSTYPE_BOOLEAN,	JSDOCSTR("host, port [,timeout=<tt>10.0</tt>]")
 	,JSDOCSTR("connect to a remote port (number or service name) on the specified host (IP address or host name)"
 	", default <i>timeout</i> value is <i>10.0</i> (seconds)")
 	,311
 	},
-	{"listen",		js_listen,		0,	JSTYPE_BOOLEAN,	""					
+	{"listen",		js_listen,		0,	JSTYPE_BOOLEAN,	JSDOCSTR("")
 	,JSDOCSTR("place socket in a state to listen for incoming connections (use before an accept)")
 	,310
 	},
-	{"accept",		js_accept,		0,	JSTYPE_OBJECT,	""					
+	{"accept",		js_accept,		0,	JSTYPE_OBJECT,	JSDOCSTR("")					
 	,JSDOCSTR("accept an incoming connection, returns a new <i>Socket</i> object representing the new connection")
 	,310
 	},
@@ -1244,38 +1271,38 @@ static jsSyncMethodSpec js_socket_functions[] = {
 	,JSDOCSTR("send data to a specific host (IP address or host name) and port (number or service name), for UDP sockets")
 	,310
 	},
-	{"sendfile",	js_sendfile,	1,	JSTYPE_BOOLEAN,	JSDOCSTR("filename")
+	{"sendfile",	js_sendfile,	1,	JSTYPE_BOOLEAN,	JSDOCSTR("path/filename")
 	,JSDOCSTR("send an entire file over the socket")
 	,310
 	},
 	{"writeBin",	js_sendbin,		1,	JSTYPE_ALIAS },
-	{"sendBin",		js_sendbin,		1,	JSTYPE_BOOLEAN,	JSDOCSTR("number value [,number bytes]")
+	{"sendBin",		js_sendbin,		1,	JSTYPE_BOOLEAN,	JSDOCSTR("value [,bytes=<tt>4</tt>]")
 	,JSDOCSTR("send a binary integer over the socket, default number of bytes is 4 (32-bits)")
 	,311
 	},
 	{"read",		js_recv,		1,	JSTYPE_ALIAS },
-	{"recv",		js_recv,		1,	JSTYPE_STRING,	JSDOCSTR("[maxlen]")
+	{"recv",		js_recv,		1,	JSTYPE_STRING,	JSDOCSTR("[maxlen=<tt>512</tt>]")
 	,JSDOCSTR("receive a string, default maxlen is 512 characters (AKA read)")
 	,310
 	},
-	{"peek",		js_peek,		0,	JSTYPE_STRING,	JSDOCSTR("[maxlen]")
+	{"peek",		js_peek,		0,	JSTYPE_STRING,	JSDOCSTR("[maxlen=<tt>512</tt>]")
 	,JSDOCSTR("receive a string, default maxlen is 512 characters, leaves string in receive buffer")
 	,310
 	},
 	{"readline",	js_recvline,	0,	JSTYPE_ALIAS },
 	{"readln",		js_recvline,	0,	JSTYPE_ALIAS },
-	{"recvline",	js_recvline,	0,	JSTYPE_STRING,	JSDOCSTR("[maxlen] [,timeout]")
+	{"recvline",	js_recvline,	0,	JSTYPE_STRING,	JSDOCSTR("[maxlen=<tt>512</tt>] [,timeout=<tt>30.0</tt>]")
 	,JSDOCSTR("receive a line-feed terminated string, default maxlen is 512 characters, default timeout is 30 seconds (AKA readline and readln)")
 	,310
 	},
-	{"recvfrom",	js_recvfrom,	0,	JSTYPE_OBJECT,	JSDOCSTR("[bool binary] [,maxlen or int_size]")
+	{"recvfrom",	js_recvfrom,	0,	JSTYPE_OBJECT,	JSDOCSTR("[binary=<tt>false</tt>] [,maxlen=<tt>512</tt> or int_size=<tt>4</tt>]")
 	,JSDOCSTR("receive data (string or integer) from a socket (typically UDP)"
 	"<p>returns object with <i>ip_address</i> and <i>port</i> of sender along with <i>data</i>"
 	"<p><i>binary</i> defaults to <i>false</i>, <i>maxlen</i> defaults to 512 chars, <i>int_size</i> defaults to 4 bytes (32-bits)")
 	,311
 	},
 	{"readBin",		js_recvbin,		0,	JSTYPE_ALIAS },
-	{"recvBin",		js_recvbin,		0,	JSTYPE_NUMBER,	JSDOCSTR("[number bytes]")
+	{"recvBin",		js_recvbin,		0,	JSTYPE_NUMBER,	JSDOCSTR("[bytes=<tt>4</tt>]")
 	,JSDOCSTR("receive a binary integer from the socket, default number of bytes is 4 (32-bits)")
 	,311
 	},
@@ -1289,11 +1316,11 @@ static jsSyncMethodSpec js_socket_functions[] = {
 	"(see <tt>sockopts</tt> in <tt>sockdefs.js</tt>) or number")
 	,310
 	},
-	{"ioctl",		js_ioctlsocket,	1,	JSTYPE_NUMBER,	JSDOCSTR("command [,argument]")
+	{"ioctl",		js_ioctlsocket,	1,	JSTYPE_NUMBER,	JSDOCSTR("command [,argument=<tt>0</tt>]")
 	,JSDOCSTR("send socket IOCTL (advanced)")					
 	,310
 	},
-	{"poll",		js_poll,		1,	JSTYPE_NUMBER,	JSDOCSTR("[number timeout] [,bool write]")
+	{"poll",		js_poll,		1,	JSTYPE_NUMBER,	JSDOCSTR("[timeout=<tt>0</tt>] [,write=<tt>false</tt>]")
 	,JSDOCSTR("poll socket for read or write ability (default is <i>read</i>), "
 	"default timeout value is 0.0 seconds (immediate timeout)")
 	,310
@@ -1376,7 +1403,7 @@ js_socket_constructor(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsv
 		return(JS_FALSE);
 	}
 
-#ifdef _DEBUG
+#ifdef BUILD_JSDOCS
 	js_DescribeSyncObject(cx,obj,"Class used for TCP/IP socket communications",310);
 	js_DescribeSyncConstructor(cx,obj,"To create a new Socket object: "
 		"<tt>load('sockdefs.js'); var s = new Socket(<i>type</i>)</tt><br>"
--- sbbs3/scfg/scfgnode.c
+++ sbbs3/scfg/scfgnode.c
@@ -1,12 +1,12 @@
 /* scfgnode.c */
 
-/* $Id: scfgnode.c,v 1.24 2005/09/20 03:40:47 deuce Exp $ */
+/* $Id: scfgnode.c,v 1.26 2006/05/03 00:27:37 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2002 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -234,7 +234,6 @@ Options with a trailing ... will produce a sub-menu of more options.
 					,cfg.node_misc&NM_CLOSENODEDAB ? "No":"Yes");
 
 				opt[i][0]=0;
-				uifc.savnum=0;
 SETHELP(WHERE);
 /*
 Node Toggle Options:
@@ -255,7 +254,6 @@ more states, such as Yes and No.
 						strcpy(opt[0],"Yes");
 						strcpy(opt[1],"No");
 						opt[2][0]=0;
-						uifc.savnum=1;
 						SETHELP(WHERE);
 /*
 Low Priority String Input:
@@ -283,7 +281,6 @@ user, but improving aggregate system performance under multitaskers.
 						strcpy(opt[0],"Yes");
 						strcpy(opt[1],"No");
 						opt[2][0]=0;
-						uifc.savnum=1;
 						SETHELP(WHERE);
 /*
 Allow Login by User Number:
@@ -305,7 +302,6 @@ set this option to Yes.
 						strcpy(opt[0],"Yes");
 						strcpy(opt[1],"No");
 						opt[2][0]=0;
-						uifc.savnum=1;
 						SETHELP(WHERE);
 /*
 Allow Login by Real Name:
@@ -327,7 +323,6 @@ their alias, set this option to Yes.
 						strcpy(opt[0],"Yes");
 						strcpy(opt[1],"No");
 						opt[2][0]=0;
-						uifc.savnum=1;
 						SETHELP(WHERE);
 /*
 Always Prompt for Password:
@@ -349,7 +344,6 @@ prompt for a password, set this option to Yes.
 						strcpy(opt[0],"Yes");
 						strcpy(opt[1],"No");
 						opt[2][0]=0;
-						uifc.savnum=1;
 						SETHELP(WHERE);
 /*
 Allow 8-bit Remote Input During Login:
@@ -372,7 +366,6 @@ to send IBM extended ASCII characters during the login sequence.
 						strcpy(opt[0],"Yes");
 						strcpy(opt[1],"No");
 						opt[2][0]=0;
-						uifc.savnum=1;
 						SETHELP(WHERE);
 /*
 Spinning Pause Prompt:
@@ -394,7 +387,6 @@ this option to Yes.
 						strcpy(opt[0],"Yes");
 						strcpy(opt[1],"No");
 						opt[2][0]=0;
-						uifc.savnum=1;
 						SETHELP(WHERE);
 /*
 Keep Node File Open:
@@ -429,11 +421,9 @@ option to No.
 					,cfg.sec_warn);
 				sprintf(opt[i++],"%-27.27s%u seconds","Inactivity Disconnection"
 					,cfg.sec_hangup);
-				sprintf(tmp,"$%d.00",cfg.node_dollars_per_call);
 				sprintf(opt[i++],"%-27.27s%.40s","Daily Event",cfg.node_daily);
 				sprintf(opt[i++],"%-27.27s%.40s","Text Directory",cfg.text_dir);
 				opt[i][0]=0;
-				uifc.savnum=0;
 SETHELP(WHERE);
 /*
 Node Advanced Options:
