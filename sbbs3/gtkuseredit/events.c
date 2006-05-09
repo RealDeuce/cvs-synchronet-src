8c8f872fc6f57d72721202dcdf49cc8df9cceea9
deuce <deuce>
2006-05-09 00:52:34 +0000

    Fix up packing for better resizeing behavour for scrolled areas.

--- sbbs3/gtkuseredit/events.c
+++ sbbs3/gtkuseredit/events.c
@@ -675,10 +675,10 @@ void load_user(GtkWidget *wiggy, gpointer data)
 		if(w==NULL)
 			fprintf(stderr,"Cannot get the default download protocol widget\n");
 		else {
-			gtk_combo_box_set_active(GTK_COMBO_BOX(w),0);
+			gtk_combo_box_set_active(GTK_COMBO_BOX(w),-1);
 			for(i=0;i<cfg.total_prots;i++) {
 				if(cfg.prot[i]->mnemonic==user.prot) {
-					gtk_combo_box_set_active(GTK_COMBO_BOX(w),i+1);
+					gtk_combo_box_set_active(GTK_COMBO_BOX(w),i);
 					break;
 				}
 			}
@@ -801,6 +801,7 @@ void load_user(GtkWidget *wiggy, gpointer data)
 
 void save_user(GtkWidget *wiggy, gpointer data)
 {
+	/* ToDo */
 	GtkWidget	*w;
 	char		str[1024];
 	gboolean	b;
@@ -1602,12 +1603,8 @@ void save_user(GtkWidget *wiggy, gpointer data)
 		w=glade_xml_get_widget(xml, "cDefaultDownloadProtocol");
 		if(w==NULL)
 			fprintf(stderr,"Cannot get the default download protocol widget\n");
-		else {
-			if(gtk_combo_box_get_active(GTK_COMBO_BOX(w))==0)
-				user.prot=' ';
-			else
-				user.prot=cfg.prot[gtk_combo_box_get_active(GTK_COMBO_BOX(w))-1]->mnemonic;
-		}
+		else
+			user.prot=cfg.prot[gtk_combo_box_get_active(GTK_COMBO_BOX(w))]->mnemonic;
 
 		w=glade_xml_get_widget(xml, "cTempQWKFileType");
 		if(w==NULL)
@@ -1903,7 +1900,7 @@ void last_user(GtkWidget *w, gpointer data)
 void show_about_box(GtkWidget *unused, gpointer data)
 {
 	GladeXML	*axml;
-    axml = glade_xml_new(glade_path, "AboutWindow", NULL);
+    axml = glade_xml_new("gtkuseredit.glade", "AboutWindow", NULL);
 	if(axml==NULL) {
 		fprintf(stderr,"Could not locate AboutWindow widget\n");
 		return;
@@ -1965,7 +1962,7 @@ void find_user(GtkWidget *t, gpointer data)
 		nu=matchuser(&cfg, (char *)gtk_entry_get_text(GTK_ENTRY(w)), TRUE);
 		if(nu==0) {
 			GladeXML	*cxml;
-			cxml = glade_xml_new(glade_path, "NotFoundWindow", NULL);
+			cxml = glade_xml_new("gtkuseredit.glade", "NotFoundWindow", NULL);
 		    /* connect the signals in the interface */
 		    glade_xml_signal_autoconnect(cxml);
 			if(cxml==NULL)
@@ -2010,7 +2007,7 @@ int get_date(GtkWidget *t, isoDate_t *date)
 	isoDate_t	odate=*date;
 
 	got_date=0;
-    cxml = glade_xml_new(glade_path, "CalendarWindow", NULL);
+    cxml = glade_xml_new("gtkuseredit.glade", "CalendarWindow", NULL);
 	if(cxml==NULL) {
 		fprintf(stderr,"Could not locate Calendar Window XML\n");
 		return(-1);
