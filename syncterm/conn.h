4dd3455032d109e684eb7882243efdb9067760a2
rswindell <rswindell>
2006-05-10 03:20:57 +0000

    Number the VDD function definitions for easier debugging.

--- syncterm/conn.h
+++ syncterm/conn.h
@@ -1,30 +1,25 @@
-/* $Id: conn.h,v 1.10 2006/09/24 05:07:15 deuce Exp $ */
+/* $Id: conn.h,v 1.5 2005/06/24 04:29:20 deuce Exp $ */
 
 #ifndef _CONN_H_
 #define _CONN_H_
 
 #include "sockwrap.h"
 
-#include "bbslist.h"
-
 extern SOCKET conn_socket;
 extern char *conn_types[];
-extern int conn_ports[];
 
 enum {
 	 CONN_TYPE_UNKNOWN
 	,CONN_TYPE_RLOGIN
 	,CONN_TYPE_TELNET
 	,CONN_TYPE_RAW
-	,CONN_TYPE_SSH
 	,CONN_TYPE_TERMINATOR
 };
 
 int conn_recv(char *buffer, size_t buflen, unsigned int timeout);
 int conn_send(char *buffer, size_t buflen, unsigned int timeout);
-int conn_connect(struct bbslist *bbs);
+int conn_connect(char *addr, int port, char *ruser, char *passwd, char *syspass, int conn_type, int speed);
 int conn_close(void);
-BOOL conn_is_connected(void);
 void conn_settype(int type);
 
 #endif
