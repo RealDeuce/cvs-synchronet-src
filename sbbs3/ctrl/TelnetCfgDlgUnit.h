40740624f21aa1e1b327ab608d641b90c9fe285e
deuce <deuce>
2006-05-10 22:02:58 +0000

    Set up items on the BBS menu: Reload Configuration Force Timed Events Force Network Callout

--- sbbs3/ctrl/TelnetCfgDlgUnit.h
+++ sbbs3/ctrl/TelnetCfgDlgUnit.h
@@ -1,12 +1,12 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: TelnetCfgDlgUnit.h,v 1.13 2006/12/29 02:51:56 rswindell Exp $ */
+/* $Id: TelnetCfgDlgUnit.h,v 1.11 2003/03/12 03:16:43 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -91,21 +91,16 @@ __published:
     TCheckBox *RLogin2ndNameCheckBox;
         TCheckBox *QWKEventsCheckBox;
     TCheckBox *JavaScriptCheckBox;
+    TLabel *XtrnPollsLabel;
+    TEdit *XtrnYieldEdit;
     TCheckBox *IdentityCheckBox;
 	TCheckBox *EventsCheckBox;
-    TTabSheet *SshTabSheet;
-    TLabel *SshPortLabel;
-    TEdit *SshPortEdit;
-    TCheckBox *SshEnabledCheckBox;
-    TLabel *SshInterfaceLabel;
-    TEdit *SshInterfaceEdit;
 	void __fastcall FormShow(TObject *Sender);
 	void __fastcall OKBtnClick(TObject *Sender);
 	void __fastcall AnswerSoundButtonClick(TObject *Sender);
 	void __fastcall HangupSoundButtonClick(TObject *Sender);
     void __fastcall RLoginEnabledCheckBoxClick(TObject *Sender);
     void __fastcall RLoginIPallowButtonClick(TObject *Sender);
-    void __fastcall SshEnabledCheckBoxClick(TObject *Sender);
 private:
 public:
 	virtual __fastcall TTelnetCfgDlg(TComponent* AOwner);
