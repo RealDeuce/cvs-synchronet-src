412f4635e1ded1006c71f5f21b4bb2a05c2f4bfb
deuce <deuce>
2006-05-14 08:31:20 +0000

    Whee!  She's done!  Added missing support for viewing another days log (Pops up a calendar for selecting now) Removed useless glade project file from CVS.

--- sbbs3/ntsvcs.h
+++ sbbs3/ntsvcs.h
@@ -2,13 +2,13 @@
 
 /* Synchronet BBS Windows NT Service Names */
 
-/* $Id: ntsvcs.h,v 1.3 2004/12/04 16:45:30 rswindell Exp $ */
+/* $Id: ntsvcs.h,v 1.4 2006/02/21 23:49:03 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
