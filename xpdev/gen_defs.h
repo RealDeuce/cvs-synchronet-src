498bbc7615707979492b3fce81220c2ccc6f499e
rswindell <rswindell>
2006-05-20 17:20:11 +0000

    Use (XPDEV macro) MSCLOCKS_PER_SEC instead of time.h-defined CLK_TCK as apparently some Linux's clib doesn't define CLK_TCK. <shrug>

--- xpdev/gen_defs.h
+++ xpdev/gen_defs.h
@@ -2,7 +2,7 @@
 
 /* General(ly useful) constant, macro, and type definitions */
 
-/* $Id: gen_defs.h,v 1.36 2006/05/28 22:01:57 deuce Exp $ */
+/* $Id: gen_defs.h,v 1.34 2006/04/07 21:12:34 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -353,14 +353,5 @@ typedef struct {
 	#define LOG_DEBUG       7       /* debug-level messages */
 #endif
 
-/* Special hackery for SDL */
-#ifdef WITH_SDL
-	#include <SDL.h>
-
-	#ifdef main
-		#undef main
-	#endif
-	#define	main	XPDEV_main
-#endif
 
 #endif /* Don't add anything after this #endif statement */
