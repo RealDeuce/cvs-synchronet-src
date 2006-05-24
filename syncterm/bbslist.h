115f94d9ea76313dee2de3aac3339119f43f9fa0
deuce <deuce>
2006-05-24 06:49:25 +0000

    Properly scale the cursor in SDL mode.

--- syncterm/bbslist.h
+++ syncterm/bbslist.h
@@ -1,4 +1,4 @@
-/* $Id: bbslist.h,v 1.29 2006/10/01 19:56:52 deuce Exp $ */
+/* $Id: bbslist.h,v 1.28 2006/05/18 06:20:32 rswindell Exp $ */
 
 #ifndef _BBSLIST_H_
 #define _BBSLIST_H_
@@ -17,7 +17,7 @@
 #define LIST_ADDR_MAX	64
 #define MAX_USER_LEN	30
 #define MAX_PASSWD_LEN	16
-#define MAX_SYSPASS_LEN	40
+#define MAX_SYSPASS_LEN	16
 
 enum {
 	 USER_BBSLIST
