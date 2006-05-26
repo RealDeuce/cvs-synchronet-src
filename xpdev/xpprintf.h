e6c2302e91c05c9b39508977725b61562e7f6753
rswindell <rswindell>
2006-05-26 23:42:06 +0000

    Always generate .map files.

--- xpdev/xpprintf.h
+++ xpdev/xpprintf.h
@@ -1,3 +1,41 @@
+/* xpprintf.h */
+
+/* Deuce's vs[n]printf() replacement */
+
+/* $Id: xpprintf.h,v 1.11 2006/01/27 05:49:47 deuce Exp $ */
+
+/****************************************************************************
+ * @format.tab-size 4		(Plain Text/Source Code File Header)			*
+ * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
+ *																			*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
+ *																			*
+ * This library is free software; you can redistribute it and/or			*
+ * modify it under the terms of the GNU Lesser General Public License		*
+ * as published by the Free Software Foundation; either version 2			*
+ * of the License, or (at your option) any later version.					*
+ * See the GNU Lesser General Public License for more details: lgpl.txt or	*
+ * http://www.fsf.org/copyleft/lesser.html									*
+ *																			*
+ * Anonymous FTP access to the most recent released source is available at	*
+ * ftp://vert.synchro.net, ftp://cvs.synchro.net and ftp://ftp.synchro.net	*
+ *																			*
+ * Anonymous CVS access to the development source and modification history	*
+ * is available at cvs.synchro.net:/cvsroot/sbbs, example:					*
+ * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs login			*
+ *     (just hit return, no password is necessary)							*
+ * cvs -d :pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs checkout src		*
+ *																			*
+ * For Synchronet coding style and modification guidelines, see				*
+ * http://www.synchro.net/source.html										*
+ *																			*
+ * You are encouraged to submit any modifications (preferably in Unix diff	*
+ * format) via e-mail to mods@synchro.net									*
+ *																			*
+ * Note: If this box doesn't appear square, then you need to fix your tabs.	*
+ ****************************************************************************/
+
+
 #ifndef _XPPRINTF_H_
 #define _XPPRINTF_H_
 
@@ -33,6 +71,7 @@
 #if defined(__cplusplus)
 extern "C" {
 #endif
+void xp_asprintf_free(char *format);
 char *xp_asprintf_start(const char *format);
 char *xp_asprintf_next(char *format, int type, ...);
 char *xp_asprintf_end(char *format);
