18af60fc8dbad0c4a17d111cb8109c13f3234cdf
deuce <deuce>
2006-05-28 07:19:53 +0000

    Fix another bug in last commit (last one?)

--- xpdev/xpbeep.c
+++ xpdev/xpbeep.c
@@ -1,4 +1,4 @@
-/* $Id: xpbeep.c,v 1.37 2006/05/28 07:12:27 deuce Exp $ */
+/* $Id: xpbeep.c,v 1.38 2006/05/28 07:19:53 deuce Exp $ */
 
 /* standard headers */
 #include <math.h>
@@ -422,7 +422,7 @@ BOOL xptone(double freq, DWORD duration, enum WAVE_SHAPE shape)
 
 	/* Prepare for next note */
 	curr_wh=1-curr_wh;
-	memset(wave[curr_wh], 128, sizeof(wave));	/* Ensure that if silence DOES timeout, you still get silence */
+	memset(wave[curr_wh], 128, sizeof(wave[0]));	/* Ensure that if silence DOES timeout, you still get silence */
 	wh=&wh_bufs[curr_wh];
 	waveOutWrite(waveOut, wh, sizeof(wh_bufs[0]));
 
