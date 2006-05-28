a7b8ef333cd10ef59712c926fc592bac26db00ab
deuce <deuce>
2006-05-28 22:01:57 +0000

    More main macro fiddling.

--- sbbs3/download.cpp
+++ sbbs3/download.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet file download routines */
 
-/* $Id: download.cpp,v 1.32 2006/01/26 04:14:10 deuce Exp $ */
+/* $Id: download.cpp,v 1.34 2006/02/28 00:47:34 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -67,8 +67,7 @@ void sbbs_t::downloadfile(file_t* f)
 	/****************************/
 	/* Update Downloader's Info */
 	/****************************/
-	useron.dls=(ushort)adjustuserrec(&cfg,useron.number,U_DLS,5,1);
-	useron.dlb=adjustuserrec(&cfg,useron.number,U_DLB,10,length);
+	user_downloaded(&cfg, &useron, 1, length);
 	if(!is_download_free(&cfg,f->dir,&useron))
 		subtract_cdt(&cfg,&useron,f->cdt);
 	/**************************/
@@ -449,4 +448,35 @@ void sbbs_t::seqwait(uint devnum)
 		mswait(100); 
 	}
 
+}
+
+bool sbbs_t::sendfile(char* fname)
+{
+	char	keys[128];
+	char	ch;
+	size_t	i;
+	bool	result=false;
+
+	xfer_prot_menu(XFER_DOWNLOAD);
+	mnemonics(text[ProtocolOrQuit]);
+	strcpy(keys,"Q");
+	for(i=0;i<cfg.total_prots;i++)
+		if(cfg.prot[i]->dlcmd[0] && chk_ar(cfg.prot[i]->ar,&useron))
+			sprintf(keys+strlen(keys),"%c",cfg.prot[i]->mnemonic);
+
+	ch=(char)getkeys(keys,0);
+
+	if(ch=='Q' || sys_status&SS_ABORT)
+		return(false); 
+
+	for(i=0;i<cfg.total_prots;i++)
+		if(cfg.prot[i]->mnemonic==ch && chk_ar(cfg.prot[i]->ar,&useron))
+			break;
+	if(i<cfg.total_prots) {
+		if(protocol(cfg.prot[i],XFER_DOWNLOAD,fname,fname,false)==0)
+			result=true;
+		autohangup(); 
+	}
+
+	return(result);
 }
