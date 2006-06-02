43c7f7b2603c5ba4e5f601fd8ee7713b23826696
deuce <deuce>
2006-06-02 17:21:13 +0000

    Move the mutex destruction until the session destruction... possible the hung threads are hanging while attempting to lock a destroyed semaphore.

--- sbbs3/websrvr.c
+++ sbbs3/websrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Web Server */
 
-/* $Id: websrvr.c,v 1.412 2006/05/23 18:05:02 deuce Exp $ */
+/* $Id: websrvr.c,v 1.413 2006/06/02 17:21:13 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -4026,7 +4026,6 @@ void http_output_thread(void *arg)
 			sock_sendbuf(&session->socket, buf, len, &failed);
 		pthread_mutex_unlock(&session->outbuf_write);
     }
-	pthread_mutex_destroy(&session->outbuf_write);
 	thread_down();
 	sem_post(&session->output_thread_terminated);
 }
@@ -4101,6 +4100,7 @@ void http_session_thread(void* arg)
 			close_socket(&session.socket);
 			sem_wait(&session.output_thread_terminated);
 			RingBufDispose(&session.outbuf);
+			pthread_mutex_destroy(&session->outbuf_write);
 			lprintf(LOG_NOTICE,"%04d !CLIENT BLOCKED in host.can: %s", session.socket, host_name);
 			thread_down();
 			session_threads--;
@@ -4113,6 +4113,7 @@ void http_session_thread(void* arg)
 		close_socket(&session.socket);
 		sem_wait(&session.output_thread_terminated);
 		RingBufDispose(&session.outbuf);
+		pthread_mutex_destroy(&session->outbuf_write);
 		lprintf(LOG_NOTICE,"%04d !CLIENT BLOCKED in ip.can: %s", session.socket, session.host_ip);
 		thread_down();
 		session_threads--;
@@ -4225,6 +4226,7 @@ void http_session_thread(void* arg)
 	sem_wait(&session.output_thread_terminated);
 	sem_destroy(&session.output_thread_terminated);
 	RingBufDispose(&session.outbuf);
+	pthread_mutex_destroy(&session->outbuf_write);
 
 	active_clients--;
 	update_clients();
@@ -4292,7 +4294,7 @@ const char* DLLCALL web_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.412 $", "%*s %s", revision);
+	sscanf("$Revision: 1.413 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
