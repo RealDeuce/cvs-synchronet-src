eec58f719ea7cb287ea7a04c8596da95cdb0e7f1
rswindell <rswindell>
2006-06-15 03:19:31 +0000

    JSDOC fixes.

--- sbbs3/xmodem.h
+++ sbbs3/xmodem.h
@@ -2,13 +2,13 @@
 
 /* Synchronet X/YMODEM Functions */
 
-/* $Id: xmodem.h,v 1.15 2005/06/13 01:38:54 rswindell Exp $ */
+/* $Id: xmodem.h,v 1.16 2006/02/24 09:50:50 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -64,6 +64,7 @@ typedef struct {
 	int			(*send_byte)(void*, uchar ch, unsigned timeout);
 	int			(*recv_byte)(void*, unsigned timeout);
 	BOOL		(*is_connected)(void*);
+	BOOL		(*is_cancelled)(void*);
 
 } xmodem_t;
 
@@ -74,6 +75,7 @@ void		xmodem_init(xmodem_t*, void* cbdata, long* mode
 						,int	(*send_byte)(void*, uchar ch, unsigned timeout)
 						,int	(*recv_byte)(void*, unsigned timeout)
 						,BOOL	(*is_connected)(void*)
+						,BOOL	(*is_cancelled)(void*)
 						);
 char*		xmodem_ver(char *buf);
 const char* xmodem_source(void);
