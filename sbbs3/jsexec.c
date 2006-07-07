2152379666e9a792a8362df5faf8db14e5c4471d
rswindell <rswindell>
2006-07-07 00:21:29 +0000

    Mutex-protect the log-output (to the console typically) so that foreground and background threads don't step on eachother (as well as error reports and status messages).

--- sbbs3/jsexec.c
+++ sbbs3/jsexec.c
@@ -2,7 +2,7 @@
 
 /* Execute a Synchronet JavaScript module from the command-line */
 
-/* $Id: jsexec.c,v 1.104 2006/01/27 06:31:49 rswindell Exp $ */
+/* $Id: jsexec.c,v 1.106 2006/07/07 00:21:29 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -72,6 +72,7 @@ BOOL		terminated=FALSE;
 BOOL		recycled;
 DWORD		log_mask=DEFAULT_LOG_MASK;
 int  		err_level=DEFAULT_ERR_LOG_LVL;
+pthread_mutex_t output_mutex;
 #if defined(__unix__)
 BOOL		daemonize=FALSE;
 #endif
@@ -157,6 +158,10 @@ int lprintf(int level, char *fmt, ...)
 		return(ret);
 	}
 #endif
+
+	/* Mutex-protect stdout/stderr */
+	pthread_mutex_lock(&output_mutex);
+
 	if(level<=err_level) {
 		ret=fprintf(errfp,"%s\n",sbuf);
 		if(errfp!=stderr && confp!=stdout)
@@ -164,6 +169,8 @@ int lprintf(int level, char *fmt, ...)
 	}
 	if(level>err_level || errfp!=stderr)
 		ret=fprintf(confp,"%s\n",sbuf);
+
+	pthread_mutex_unlock(&output_mutex);
     return(ret);
 }
 
@@ -280,7 +287,7 @@ js_read(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 
 	if(argc)
 		JS_ValueToInt32(cx,argv[0],&len);
-	if((buf=malloc(len))==NULL)
+	if((buf=alloca(len))==NULL)
 		return(JS_TRUE);
 
 	rd=fread(buf,sizeof(char),len,stdin);
@@ -288,7 +295,6 @@ js_read(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	if(rd>=0)
 		*rval = STRING_TO_JSVAL(JS_NewStringCopyN(cx,buf,rd));
 
-	free(buf);
     return(JS_TRUE);
 }
 
@@ -301,7 +307,7 @@ js_readln(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 
 	if(argc)
 		JS_ValueToInt32(cx,argv[0],&len);
-	if((buf=malloc(len+1))==NULL)
+	if((buf=alloca(len+1))==NULL)
 		return(JS_TRUE);
 
 	p=fgets(buf,len+1,stdin);
@@ -309,7 +315,6 @@ js_readln(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	if(p!=NULL)
 		*rval = STRING_TO_JSVAL(JS_NewStringCopyZ(cx,truncnl(p)));
 
-	free(buf);
     return(JS_TRUE);
 }
 
@@ -702,7 +707,7 @@ long js_exec(const char *fname, char** args)
 		return(-1);
 	}
 	if((diff=xp_timer()-start) > 0)
-		fprintf(statfp,"%s compiled in %.2Lf seconds\n"
+		lprintf(LOG_INFO,"%s compiled in %.2Lf seconds"
 			,path
 			,diff);
 
@@ -711,14 +716,14 @@ long js_exec(const char *fname, char** args)
 	js_EvalOnExit(js_cx, js_glob, &branch);
 
 	if((diff=xp_timer()-start) > 0)
-		fprintf(statfp,"%s executed in %.2Lf seconds\n"
+		lprintf(LOG_INFO,"%s executed in %.2Lf seconds"
 			,path
 			,diff);
 
 	JS_GetProperty(js_cx, js_glob, "exit_code", &rval);
 
 	if(rval!=JSVAL_VOID && JSVAL_IS_NUMBER(rval)) {
-		fprintf(statfp,"Using JavaScript exit_code: %s\n",JS_GetStringBytes(JS_ValueToString(js_cx,rval)));
+		lprintf(LOG_DEBUG,"Using JavaScript exit_code: %s",JS_GetStringBytes(JS_ValueToString(js_cx,rval)));
 		JS_ValueToInt32(js_cx,rval,&result);
 	}
 
@@ -734,13 +739,13 @@ long js_exec(const char *fname, char** args)
 
 void break_handler(int type)
 {
-	fprintf(statfp,"\n-> Terminated Locally (signal: %d)\n",type);
+	lprintf(LOG_NOTICE,"\n-> Terminated Locally (signal: %d)",type);
 	terminated=TRUE;
 }
 
 void recycle_handler(int type)
 {
-	fprintf(statfp,"\n-> Recycled Locally (signal: %d)\n",type);
+	lprintf(LOG_NOTICE,"\n-> Recycled Locally (signal: %d)",type);
 	recycled=TRUE;
 	branch.terminated=&recycled;
 }
@@ -785,7 +790,7 @@ int main(int argc, char **argv, char** environ)
 	branch.gc_interval=JAVASCRIPT_GC_INTERVAL;
 	branch.auto_terminate=TRUE;
 
-	sscanf("$Revision: 1.104 $", "%*s %s", revision);
+	sscanf("$Revision: 1.106 $", "%*s %s", revision);
 	DESCRIBE_COMPILER(compiler);
 
 	memset(&scfg,0,sizeof(scfg));
@@ -966,10 +971,12 @@ int main(int argc, char **argv, char** environ)
 	signal(SIGPIPE,SIG_IGN);
 #endif
 
+	output_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 	do {
 
 		if(exec_count++)
-			fprintf(statfp,"\nRe-running: %s\n", module);
+			lprintf(LOG_INFO,"\nRe-running: %s", module);
 
 		recycled=FALSE;
 
