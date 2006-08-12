bb9108b94f5ab89ff08ef6a341d62c2e6ab4f5bc
deuce <deuce>
2006-08-12 08:37:44 +0000

    Remove HEAD_SETCOOKIE (unused)

--- sbbs3/websrvr.c
+++ sbbs3/websrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Web Server */
 
-/* $Id: websrvr.c,v 1.427 2006/08/12 08:36:26 deuce Exp $ */
+/* $Id: websrvr.c,v 1.428 2006/08/12 08:37:44 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -276,7 +276,6 @@ enum {
 	,HEAD_RANGE
 	,HEAD_IFRANGE
 	,HEAD_COOKIE
-	,HEAD_SETCOOKIE
 };
 
 static struct {
@@ -306,7 +305,6 @@ static struct {
 	{ HEAD_RANGE,			"Range"					},
 	{ HEAD_IFRANGE,			"If-Range"				},
 	{ HEAD_COOKIE,			"Cookie"				},
-	{ HEAD_SETCOOKIE,		"Set-Cookie"			},
 	{ -1,					NULL /* terminator */	},
 };
 
@@ -4521,7 +4519,7 @@ const char* DLLCALL web_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.427 $", "%*s %s", revision);
+	sscanf("$Revision: 1.428 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
