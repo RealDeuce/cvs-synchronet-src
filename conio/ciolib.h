a2541e032275a1107a28d5d024f8ef11958a4ba2
rswindell <rswindell>
2006-08-23 01:45:05 +0000

    Introducing sbbs_t::term_supports(), exposed via JS as console.term_supports() - returns true/false if the user's terminal supports all the passed flags - returns the supported flags if no flags are passed for comparison - automatically uses the auto-terminal-type flags if no user is logged-on

--- conio/ciolib.h
+++ conio/ciolib.h
@@ -1,4 +1,4 @@
-/* $Id: ciolib.h,v 1.36 2006/01/30 04:11:02 deuce Exp $ */
+/* $Id: ciolib.h,v 1.38 2006/05/28 21:43:23 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -251,6 +251,7 @@ CIOLIBEXPORTVAR cioapi_t cio_api;
 CIOLIBEXPORTVAR int _wscroll;
 CIOLIBEXPORTVAR int directvideo;
 CIOLIBEXPORTVAR int hold_update;
+CIOLIBEXPORTVAR int puttext_can_move;
 
 #define _conio_kbhit()		kbhit()
 
@@ -350,11 +351,6 @@ CIOLIBEXPORT int CIOLIBCALL ciolib_loadfont(char *filename);
 	#define setfont(a,b)			ciolib_setfont(a,b)
 	#define getfont()				ciolib_getfont()
 	#define loadfont(a)				ciolib_loadfont(a)
-#endif
-
-/* Special hackery for SDL */
-#ifdef WITH_SDL
-	#define	main	CIOLIB_main
 #endif
 
 #endif	/* Do not add anything after this line */
