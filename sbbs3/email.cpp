d8b2fbc7ed4b47cb7bb7be1d5b96a6191ab4f858
rswindell <rswindell>
2006-08-23 22:34:32 +0000

    * Unified the msg tmp filename generation (i.e. INPUT.MSG and MSGTMP) in a single function. * editfile() now modifies a temporary copy of the file which solves 2 problems: A. editors restricted to the temp dir (e.g. PICO) can now be used to edit other     files (e.g. sig files) B. DOS editors can now be used to edit files with long filenames     (e.g. .plan files)

--- sbbs3/email.cpp
+++ sbbs3/email.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet email function - for sending private e-mail */
 
-/* $Id: email.cpp,v 1.40 2006/06/07 22:09:57 rswindell Exp $ */
+/* $Id: email.cpp,v 1.41 2006/08/23 22:34:32 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -102,7 +102,7 @@ bool sbbs_t::email(int usernumber, char *top, char *subj, long mode)
 	if(cfg.sys_misc&SM_DELREADM)
 		msgattr|=MSG_KILLREAD;
 
-	sprintf(msgpath,"%sinput.msg", cfg.node_dir);
+	msg_tmp_fname(useron.xedit, msgpath, sizeof(msgpath));
 	username(&cfg,usernumber,str2);
 	if(!writemsg(msgpath,top,title,mode,INVALID_SUB,str2)) {
 		bputs(text[Aborted]);
