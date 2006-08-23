554baac21d7d37669da33d669dba9c002f8fef1f
rswindell <rswindell>
2006-08-23 22:34:32 +0000

    * Unified the msg tmp filename generation (i.e. INPUT.MSG and MSGTMP) in a single function. * editfile() now modifies a temporary copy of the file which solves 2 problems: A. editors restricted to the temp dir (e.g. PICO) can now be used to edit other     files (e.g. sig files) B. DOS editors can now be used to edit files with long filenames     (e.g. .plan files)

--- conio/curs_cio.c
+++ conio/curs_cio.c
@@ -1,4 +1,4 @@
-/* $Id: curs_cio.c,v 1.27 2007/05/23 07:38:24 deuce Exp $ */
+/* $Id: curs_cio.c,v 1.26 2005/10/21 23:26:14 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -657,14 +657,11 @@ void curs_gotoxy(int x, int y)
 
 void curs_suspend(void)
 {
-	noraw();
 	endwin();
 }
 
 void curs_resume(void)
 {
-	raw();
-	timeout(10);
 	refresh();
 }
 
@@ -695,9 +692,8 @@ int curs_initciolib(long inmode)
 	nonl();
 	keypad(stdscr, TRUE);
 	scrollok(stdscr,FALSE);
-	halfdelay(1);
 	raw();
-	timeout(10);
+	halfdelay(1);
 	atexit(curs_suspend);
 
 	/* Set up color pairs */
