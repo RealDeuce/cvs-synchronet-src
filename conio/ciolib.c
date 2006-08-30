98ed7d1e76cc963bb97fba086f20c3c12514a855
deuce <deuce>
2006-08-30 06:30:00 +0000

    Fix Borland warning.

--- conio/ciolib.c
+++ conio/ciolib.c
@@ -1,4 +1,4 @@
-/* $Id: ciolib.c,v 1.76 2006/05/28 20:50:40 deuce Exp $ */
+/* $Id: ciolib.c,v 1.73 2006/01/30 04:16:20 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -37,11 +37,8 @@
 #endif
 
 #include <stdarg.h>
-#include <stdlib.h>	/* alloca */
+#include <stdlib.h>	/* malloc */
 #include <stdio.h>
-#if defined(_WIN32)
- #include <malloc.h>	/* alloca() on Win32 */
-#endif
 
 #include <threadwrap.h>
 
@@ -71,7 +68,6 @@ static int lastmode=3;
 CIOLIBEXPORT int _wscroll=1;
 CIOLIBEXPORT int directvideo=0;
 CIOLIBEXPORT int hold_update=0;
-CIOLIBEXPORT int puttext_can_move=0;
 static int initialized=0;
 
 CIOLIBEXPORT int CIOLIBCALL ciolib_movetext(int sx, int sy, int ex, int ey, int dx, int dy);
@@ -114,7 +110,7 @@ CIOLIBEXPORT char * CIOLIBCALL ciolib_getcliptext(void);
 #ifdef WITH_SDL
 int try_sdl_init(int mode)
 {
-	if(!sdl_initciolib(mode)) {
+	if(!sdl_init(mode)) {
 		cio_api.mouse=1;
 		cio_api.puttext=sdl_puttext;
 		cio_api.gettext=sdl_gettext;
@@ -419,13 +415,18 @@ CIOLIBEXPORT int CIOLIBCALL ciolib_movetext(int sx, int sy, int ex, int ey, int 
 
 	width=ex-sx;
 	height=ey-sy;
-	buf=(unsigned char *)alloca((width+1)*(height+1)*2);
+	buf=(unsigned char *)malloc((width+1)*(height+1)*2);
 	if(buf==NULL)
 		return(0);
-	if(!ciolib_gettext(sx,sy,ex,ey,buf))
+	if(!ciolib_gettext(sx,sy,ex,ey,buf)) {
+		free(buf);
 		return(0);
-	if(!ciolib_puttext(dx,dy,dx+width,dy+height,buf))
+	}
+	if(!ciolib_puttext(dx,dy,dx+width,dy+height,buf)) {
+		free(buf);
 		return(0);
+	}
+	free(buf);
 	return(1);
 }
 
@@ -701,12 +702,13 @@ CIOLIBEXPORT void CIOLIBCALL ciolib_clreol(void)
 
 	width=ti.winright-ti.curx+1;
 	height=1;
-	buf=(unsigned char *)alloca(width*height*2);
+	buf=(unsigned char *)malloc(width*height*2);
 	for(i=0;i<width*height*2;) {
 		buf[i++]=' ';
 		buf[i++]=ti.attribute;
 	}
 	ciolib_puttext(ti.curx+ti.winleft-1,ti.cury+ti.wintop-1,ti.winright,ti.cury+ti.wintop-1,buf);
+	free(buf);
 }
 
 CIOLIBEXPORT void CIOLIBCALL ciolib_clrscr(void)
@@ -722,13 +724,14 @@ CIOLIBEXPORT void CIOLIBCALL ciolib_clrscr(void)
 
 	width=ti.winright-ti.winleft+1;
 	height=ti.winbottom-ti.wintop+1;
-	buf=(unsigned char *)alloca(width*height*2);
+	buf=(unsigned char *)malloc(width*height*2);
 	for(i=0;i<width*height*2;) {
 		buf[i++]=' ';
 		buf[i++]=ti.attribute;
 	}
 	ciolib_puttext(ti.winleft,ti.wintop,ti.winright,ti.winbottom,buf);
 	ciolib_gotoxy(1,1);
+	free(buf);
 }
 
 CIOLIBEXPORT void CIOLIBCALL ciolib_delline(void)
@@ -778,7 +781,7 @@ CIOLIBEXPORT int CIOLIBCALL ciolib_cprintf(char *fmat, ...)
     ret=vsnprintf(NULL,0,fmat,argptr);
 	if(ret<0)
 		return(EOF);
-	str=(char *)alloca(ret+1);
+	str=(char *)malloc(ret+1);
 	if(str==NULL)
 		return(EOF);
 	ret=vsprintf(str,fmat,argptr);
@@ -788,6 +791,9 @@ CIOLIBEXPORT int CIOLIBCALL ciolib_cprintf(char *fmat, ...)
 		ciolib_cputs(str);
 	else
 		ret=EOF;
+#ifndef _MSC_VER
+	free(str);
+#endif
     return(ret);
 }
 
