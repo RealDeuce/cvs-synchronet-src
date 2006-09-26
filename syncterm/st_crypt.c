a120b43bb346368924950820b77fe9aa95299af5
deuce <deuce>
2006-09-26 05:08:43 +0000

    Do not post the output_thread_terminated semaphore until after the outbuf_write mutex is destroyed.
    This *should* fix the crashes as the following is probably happening:
    1) output_thread_terminated is signaled
    2) session thread exists taking the session struct with it
    3) session->outbuf_write mutex is then destroyed

--- syncterm/st_crypt.c
+++ syncterm/st_crypt.c
@@ -10,19 +10,6 @@ int crypt_loaded=0;
 
 int init_crypt(void)
 {
-#ifdef STATIC_LINK
-	cl.PopData=cryptPopData;
-	cl.PushData=cryptPushData;
-	cl.FlushData=cryptFlushData;
-	cl.Init=cryptInit;
-	cl.End=cryptEnd;
-	cl.CreateSession=cryptCreateSession;
-	cl.GetAttribute=cryptGetAttribute;
-	cl.SetAttribute=cryptSetAttribute;
-	cl.SetAttributeString=cryptSetAttributeString;
-	cl.DestroySession=cryptDestroySession;
-	cl.AddRandom=cryptAddRandom;
-#else
 #ifdef _WIN32
 	HMODULE cryptlib;
 
@@ -131,7 +118,6 @@ int init_crypt(void)
 		return(-1);
 	}
 #endif
-#endif	/* !STATIC_LINK */
 	if(cryptStatusOK(cl.Init())) {
 		if(cryptStatusOK(cl.AddRandom(NULL, CRYPT_RANDOM_SLOWPOLL))) {
 			crypt_loaded=1;
