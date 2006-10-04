14ac84086f7d2db521539894acb31ad80f8ca574
deuce <deuce>
2006-10-04 07:02:52 +0000

    Use PREFIX/etc for global list.

--- sbbs3/sbbs.h
+++ sbbs3/sbbs.h
@@ -2,7 +2,7 @@
 
 /* Synchronet class (sbbs_t) definition and exported function prototypes */
 
-/* $Id: sbbs.h,v 1.275 2006/03/14 09:33:29 rswindell Exp $ */
+/* $Id: sbbs.h,v 1.283 2006/09/24 19:16:51 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -47,6 +47,9 @@
 /***************/
 #if defined(_WIN32)			/* Windows */
 
+	#define NOCRYPT     /* Stop windows.h from loading wincrypt.h */
+                    /* Is windows.h REALLY necessary?!?! */
+	#define WIN32_LEAN_AND_MEAN
 	#include <io.h>
 	#include <share.h>
 	#include <windows.h>
@@ -67,6 +70,12 @@
 
 	#include <unistd.h>		/* close */
 
+#endif
+
+#ifdef _THREAD_SUID_BROKEN
+extern int	thread_suid_broken;			/* NPTL is no longer broken */
+#else
+#define thread_suid_broken FALSE
 #endif
 
 /******************/
@@ -101,6 +110,10 @@
 	#include <jsprf.h>		/* JS-safe sprintf functions */
 	#include <jsnum.h>		/* JSDOUBLE_IS_NaN() */
 
+#endif
+
+#ifdef USE_CRYPTLIB
+#include <cryptlib.h>
 #endif
 
 /***********************/
@@ -163,6 +176,11 @@ public:
 	char	client_name[128];
 	char	client_ident[128];
 	DWORD	local_addr;
+#ifdef USE_CRYPTLIB
+	CRYPT_SESSION	ssh_session;
+	bool	ssh_mode;
+	SOCKET	passthru_socket;
+#endif
 
 	scfg_t	cfg;
 
@@ -438,6 +456,7 @@ public:
 	void	automsg(void);
 	bool	writemsg(char *str, char *top, char *title, long mode, int subnum
 				,char *dest);
+	char*	msg_tmp_fname(int xedit, char* fname, size_t len);
 	char	putmsg(char *str, long mode);
 	bool	msgabort(void);
 	bool	email(int usernumber, char *top, char *title, long mode);
@@ -491,6 +510,7 @@ public:
 	void	cursor_down(int count=1);
 	void	cursor_left(int count=1);
 	void	cursor_right(int count=1);
+	long	term_supports(long cmp_flags=0);
 
 	/* getstr.cpp */
 	size_t	getstr_offset;
@@ -521,7 +541,6 @@ public:
 	int		uselect(int add, uint n, char *title, char *item, uchar *ar);
 	uint	uselect_total, uselect_num[500];
 
-	void	riosync(char abortable);
 	void	redrwstr(char *strin, int i, int l, long mode);
 	void	attr(int atr);				/* Change local and remote text attributes */
 	void	ctrl_a(char x);			/* Peforms the Ctrl-Ax attribute changes */
@@ -609,8 +628,8 @@ public:
 	void	autohangup(void);
 	bool	checkdszlog(file_t*);
 	bool	checkprotresult(prot_t*, int error, file_t*);
-	bool	sendfile(char* fname);
-	bool	recvfile(char* fname);
+	bool	sendfile(char* fname, char prot=0);
+	bool	recvfile(char* fname, char prot=0);
 
 	/* file.cpp */
 	void	fileinfo(file_t* f);
@@ -858,6 +877,7 @@ extern "C" {
 	DLLEXPORT BOOL		DLLCALL write_chat_cfg(scfg_t* cfg, int backup_level);
 	DLLEXPORT BOOL		DLLCALL write_xtrn_cfg(scfg_t* cfg, int backup_level);
 	DLLEXPORT BOOL		DLLCALL fcopy(char* src, char* dest);
+	DLLEXPORT BOOL		DLLCALL fcompare(char* fn1, char* fn2);
 	DLLEXPORT BOOL		DLLCALL backup(char *org, int backup_level, BOOL ren);
 	DLLEXPORT void		DLLCALL refresh_cfg(scfg_t* cfg);
 	
