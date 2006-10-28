e5b9bac8b1bd6307d8846f020d571a2fafcadbf6
rswindell <rswindell>
2006-10-28 18:43:02 +0000

    Fix typo in previous commit.

--- sbbs3/ctrl/TelnetCfgDlgUnit.h
+++ sbbs3/ctrl/TelnetCfgDlgUnit.h
@@ -1,12 +1,12 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: TelnetCfgDlgUnit.h,v 1.11 2003/03/12 03:16:43 rswindell Exp $ */
+/* $Id: TelnetCfgDlgUnit.h,v 1.12 2006/05/24 06:14:22 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -91,8 +91,6 @@ __published:
     TCheckBox *RLogin2ndNameCheckBox;
         TCheckBox *QWKEventsCheckBox;
     TCheckBox *JavaScriptCheckBox;
-    TLabel *XtrnPollsLabel;
-    TEdit *XtrnYieldEdit;
     TCheckBox *IdentityCheckBox;
 	TCheckBox *EventsCheckBox;
 	void __fastcall FormShow(TObject *Sender);
