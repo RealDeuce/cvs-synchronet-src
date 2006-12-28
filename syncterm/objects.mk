93b3c4de96f880b6d3af2696cf2fb404ac3fd505
rswindell <rswindell>
2006-12-28 02:43:48 +0000

    Minor JSDOC update.

--- syncterm/objects.mk
+++ syncterm/objects.mk
@@ -3,14 +3,9 @@ OBJS = \
                         $(MTOBJODIR)$(DIRSEP)uifcinit$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)filepick$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)fonts$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)rlogin$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)ssh$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)telnet_io$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)modem$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)conn_telnet$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)conn$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)comio$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)telnet$(OFILE) \
+                        $(MTOBJODIR)$(DIRSEP)telnet_io$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)term$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)window$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)menu$(OFILE) \
@@ -19,4 +14,3 @@ OBJS = \
                         $(MTOBJODIR)$(DIRSEP)zmodem$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)st_crypt$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)syncterm$(OFILE)
-
