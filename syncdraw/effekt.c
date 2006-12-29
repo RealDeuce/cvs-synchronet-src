49904eab20d2e61157a82d3c3076abbd31669ddb
rswindell <rswindell>
2006-12-29 02:54:32 +0000

    Incremented library versions to 3.14.

--- syncdraw/effekt.c
+++ syncdraw/effekt.c
@@ -22,7 +22,7 @@ draweffekt(int xpos, int ypos, int effekt, char *blabla, int highlite)
 
 	col = effect.Colortable[1][1];
 	l=strlen(blabla);
-	buf=(char *)alloca(l*2);
+	buf=(char *)malloc(l*2);
 
 	p=0;
 	for (x = 0; x < l; x++) {
@@ -76,6 +76,7 @@ draweffekt(int xpos, int ypos, int effekt, char *blabla, int highlite)
 		buf[p++]=col+(highlite?16:0);
 	}
 	puttext(xpos,ypos,xpos+l-1,ypos,buf);
+	free(buf);
 }
 
 void 
@@ -262,7 +263,7 @@ select_effekt(void)
 				draweffekt(12,11+x,x, "ABCabc123!@# Sample", 0);
 		}
 		ch = newgetch();
-		switch (ch) {
+		switch (tolower(ch)) {
 		case CIO_KEY_MOUSE:
 			getmouse(&me);
 			if(me.event==CIOLIB_BUTTON_3_CLICK)
@@ -277,7 +278,6 @@ select_effekt(void)
 					changecolor(y);
 			}
 			break;
-		case 'C':
 		case 'c':
 			changecolor(y);
 			break;
