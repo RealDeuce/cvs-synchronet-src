cbe2bd3e0cc4d0b61395e97408c22776c8f12935
deuce <deuce>
2006-12-29 21:49:46 +0000

    No longer require USE_CRYPTLIB=1 on the make command-line.

--- sbbs3/js_file.c
+++ sbbs3/js_file.c
@@ -2,7 +2,7 @@
 
 /* Synchronet JavaScript "File" Object */
 
-/* $Id: js_file.c,v 1.95 2006/05/08 19:51:20 deuce Exp $ */
+/* $Id: js_file.c,v 1.99 2006/07/21 04:26:17 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -238,7 +238,7 @@ js_read(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	if(len<0)
 		len=512;
 
-	if((buf=alloca(len+1))==NULL)
+	if((buf=malloc(len+1))==NULL)
 		return(JS_TRUE);
 
 	len = fread(buf,1,len,p->fp);
@@ -257,8 +257,10 @@ js_read(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 
 	if(p->uuencoded || p->b64encoded || p->yencoded) {
 		uulen=len*2;
-		if((uubuf=alloca(uulen))==NULL)
+		if((uubuf=malloc(uulen))==NULL) {
+			free(buf);
 			return(JS_TRUE);
+		}
 		if(p->uuencoded)
 			uulen=uuencode(uubuf,uulen,buf,len);
 		else if(p->yencoded)
@@ -266,12 +268,16 @@ js_read(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 		else
 			uulen=b64_encode(uubuf,uulen,buf,len);
 		if(uulen>=0) {
+			free(buf);
 			buf=uubuf;
 			len=uulen;
 		}
+		else
+			free(uubuf);
 	}
 
 	str = JS_NewStringCopyN(cx, buf, len);
+	free(buf);
 
 	if(str==NULL)
 		return(JS_FALSE);
@@ -397,7 +403,7 @@ js_readall(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
     array = JS_NewArrayObject(cx, 0, NULL);
 
     while(!feof(p->fp)) {
-		js_readln(cx, obj, 0, NULL, &line); 
+		js_readln(cx, obj, argc, argv, &line);
 		if(line==JSVAL_NULL)
 			break;
         if(!JS_SetElement(cx, array, len++, &line))
@@ -928,15 +934,17 @@ js_write(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	len	= JS_GetStringLength(str);
 
 	if((p->uuencoded || p->b64encoded || p->yencoded)
-		&& len && (uubuf=alloca(len))!=NULL) {
+		&& len && (uubuf=malloc(len))!=NULL) {
 		if(p->uuencoded)
 			len=uudecode(uubuf,len,cp,len);
 		else if(p->yencoded)
 			len=ydecode(uubuf,len,cp,len);
 		else
 			len=b64_decode(uubuf,len,cp,len);
-		if(len<0)
+		if(len<0) {
+			free(uubuf);
 			return(JS_TRUE);
+		}
 		cp=uubuf;
 	}
 
@@ -945,8 +953,10 @@ js_write(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 
 	tlen=len;
 	if(argc>1) {
-		if(!JS_ValueToInt32(cx,argv[1],(int32*)&tlen))
+		if(!JS_ValueToInt32(cx,argv[1],(int32*)&tlen)) {
+			FREE_AND_NULL(uubuf);
 			return(JS_FALSE);
+		}
 		if(len>tlen)
 			len=tlen;
 	}
@@ -954,17 +964,21 @@ js_write(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	if(fwrite(cp,1,len,p->fp)==(size_t)len) {
 		if(tlen>len) {
 			len=tlen-len;
-			if((cp=alloca(len))==NULL) {
-				dbprintf(TRUE, p, "alloca failure of %u bytes", len);
+			if((cp=malloc(len))==NULL) {
+				FREE_AND_NULL(uubuf);
+				dbprintf(TRUE, p, "malloc failure of %u bytes", len);
 				return(JS_TRUE);
 			}
 			memset(cp,p->etx,len);
 			fwrite(cp,1,len,p->fp);
+			free(cp);
 		}
 		dbprintf(FALSE, p, "wrote %u bytes",tlen);
 		*rval = JSVAL_TRUE;
 	} else 
 		dbprintf(TRUE, p, "write of %u bytes failed",len);
+
+	FREE_AND_NULL(uubuf);
 		
 	return(JS_TRUE);
 }
@@ -1696,7 +1710,7 @@ static jsSyncMethodSpec js_file_functions[] = {
 	{"truncate",		js_truncate,		0,	JSTYPE_BOOLEAN,	JSDOCSTR("[length=<tt>0</tt>]")
 	,JSDOCSTR("changes the file <i>length</i> (default: 0) and repositions the file pointer "
 	"(<i>position</i>) to the new end-of-file")
-	,31301
+	,314
 	},
 	{"lock",			js_lock,			2,	JSTYPE_BOOLEAN,	JSDOCSTR("[offset=<tt>0</tt>] [,length=<i>file_length</i>-<i>offset</i>]")
 	,JSDOCSTR("lock file record for exclusive access (file must be opened <i>shareable</i>)")
@@ -1719,8 +1733,8 @@ static jsSyncMethodSpec js_file_functions[] = {
 	,JSDOCSTR("read a binary integer from the file, default number of <i>bytes</i> is 4 (32-bits)")
 	,310
 	},
-	{"readAll",			js_readall,			0,	JSTYPE_ARRAY,	JSDOCSTR("")
-	,JSDOCSTR("read all lines into an array of strings")
+	{"readAll",			js_readall,			0,	JSTYPE_ARRAY,	JSDOCSTR("[maxlen=<tt>512</tt>]")
+	,JSDOCSTR("read all lines into an array of strings, <i>maxlen</i> defaults to 512 characters")
 	,310
 	},
 	{"write",			js_write,			1,	JSTYPE_BOOLEAN,	JSDOCSTR("text [,length=<i>text_length</i>]")
@@ -1798,11 +1812,11 @@ static jsSyncMethodSpec js_file_functions[] = {
 	},
 	{"iniRemoveKey",	js_iniRemoveKey,	2,	JSTYPE_BOOLEAN,	JSDOCSTR("section, key")
 	,JSDOCSTR("remove specified <i>key</i> from specified <i>section</i> in <tt>.ini</tt> file.")
-	,31301
+	,314
 	},
 	{"iniRemoveSection",js_iniRemoveSection,1,	JSTYPE_BOOLEAN,	JSDOCSTR("section")
 	,JSDOCSTR("remove specified <i>section</i> from <tt>.ini</tt> file.")
-	,31301
+	,314
 	},
 	{0}
 };
