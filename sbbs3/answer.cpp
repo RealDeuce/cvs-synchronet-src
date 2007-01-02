5e6422ff2994e0fb90994cd71bcb527e9a9450c6
deuce <deuce>
2007-01-02 21:44:00 +0000

    Fix WIN_SAV interaction with DEL/INS/EDIT

--- sbbs3/answer.cpp
+++ sbbs3/answer.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet answer "caller" function */
 
-/* $Id: answer.cpp,v 1.60 2007/07/30 08:57:56 rswindell Exp $ */
+/* $Id: answer.cpp,v 1.51 2006/12/29 19:32:36 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -37,8 +37,6 @@
 
 #include "sbbs.h"
 #include "telnet.h"
-
-extern "C" void client_on(SOCKET sock, client_t* client, BOOL update);
 
 bool sbbs_t::answer()
 {
@@ -111,7 +109,7 @@ bool sbbs_t::answer()
 			useron.number=userdatdupe(0, U_ALIAS, LEN_ALIAS, rlogin_name, 0);
 			if(useron.number) {
 				getuserdat(&cfg,&useron);
-				useron.misc&=~TERM_FLAGS;
+				useron.misc&=~(ANSI|COLOR|RIP|WIP);
 				SAFEPRINTF(path,"%srlogin.cfg",cfg.ctrl_dir);
 				if(!findstr(client.addr,path)) {
 					SAFECOPY(tmp
@@ -177,10 +175,8 @@ bool sbbs_t::answer()
 		request_telnet_opt(TELNET_WILL,TELNET_ECHO);
 		/* Will suppress Go Ahead */
 		request_telnet_opt(TELNET_WILL,TELNET_SUP_GA);
-		/* Retrieve terminal type and speed from telnet client --RS */
+		/* Retrieve terminal type from telnet client --RS */
 		request_telnet_opt(TELNET_DO,TELNET_TERM_TYPE);
-		request_telnet_opt(TELNET_DO,TELNET_TERM_SPEED);
-		request_telnet_opt(TELNET_DO,TELNET_SEND_LOCATION);
 	}
 #ifdef USE_CRYPTLIB
 	if(sys_status&SS_SSH) {
@@ -193,7 +189,7 @@ bool sbbs_t::answer()
 		useron.number=userdatdupe(0, U_ALIAS, LEN_ALIAS, rlogin_name, 0);
 		if(useron.number) {
 			getuserdat(&cfg,&useron);
-			useron.misc&=~TERM_FLAGS;
+			useron.misc&=~(ANSI|COLOR|RIP|WIP);
 			SAFECOPY(tmp
 				,rlogin_pass);
 			for(i=0;i<3;i++) {
@@ -351,37 +347,7 @@ bool sbbs_t::answer()
 	if(!online) 
 		return(false); 
 
-	if(stricmp(terminal,"sexpots")==0) {	/* dial-up connection (via SexPOTS) */
-		SAFEPRINTF2(str,"%s connection detected at %lu bps", terminal, cur_rate);
-		logline("@S",str);
-		node_connection = (ushort)cur_rate;
-		SAFEPRINTF(connection,"%lu",cur_rate);
-		SAFECOPY(cid,"Unknown");
-		SAFECOPY(client_name,"Unknown");
-		if(telnet_location[0]) {			/* Caller-ID info provided */
-			SAFEPRINTF(str, "CID: %s", telnet_location);
-			logline("@*",str);
-			SAFECOPY(cid,telnet_location);
-			truncstr(cid," ");				/* Only include phone number in CID */
-			char* p=telnet_location;
-			FIND_WHITESPACE(p);
-			SKIP_WHITESPACE(p);
-			if(*p) {
-				SAFECOPY(client_name,p);	/* CID name, if provided (maybe 'P' or 'O' if private or out-of-area) */
-			}
-		}
-		SAFECOPY(client.addr,cid);
-		SAFECOPY(client.host,client_name);
-		client_on(client_socket,&client,TRUE /* update */);
-	} else {
-		if(telnet_location[0]) {			/* Telnet Location info provided */
-			SAFEPRINTF(str, "Telnet Location: %s", telnet_location);
-			logline("@*",str);
-		}
-	}
-
-
-	useron.misc&=~TERM_FLAGS;
+	useron.misc&=~(ANSI|COLOR|RIP|WIP);
 	useron.misc|=autoterm;
 	SAFECOPY(useron.comp,client_name);
 
@@ -417,6 +383,8 @@ bool sbbs_t::answer()
 
 	if(!useron.number)
 		hangup();
+	if(!online) 
+		return(false); 
 
 	/* Save the IP to the user's note */
 	if(cid[0]) {
@@ -430,14 +398,10 @@ bool sbbs_t::answer()
 		putuserrec(&cfg,useron.number,U_COMP,LEN_COMP,useron.comp);
 	}
 
-	if(!online) 
-		return(false); 
-
 	if(!(sys_status&SS_USERON)) {
 		errormsg(WHERE,ERR_CHK,"User not logged on",0);
 		hangup();
-		return(false); 
-	}
+		return(false); }
 
 	return(true);
 }
