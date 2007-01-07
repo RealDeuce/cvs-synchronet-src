2b229e2b8862e9258abb7bf91484c767bb79e78e
rswindell <rswindell>
2007-01-07 20:23:11 +0000

    Fix missing line-end after log level.

--- sbbs3/echocfg.c
+++ sbbs3/echocfg.c
@@ -2,7 +2,7 @@
 
 /* SBBSecho configuration utility 											*/
 
-/* $Id: echocfg.c,v 1.18 2007/01/07 19:25:26 rswindell Exp $ */
+/* $Id: echocfg.c,v 1.19 2007/01/07 20:23:11 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1188,7 +1188,7 @@ int main(int argc, char **argv)
 						fprintf(stream,"LOG NONE\n");
 					else
 						fprintf(stream,"LOG %08lX\n",cfg.log); }
-				fprintf(stream,"LOG_LEVEL %u",cfg.log_level);
+				fprintf(stream,"LOG_LEVEL %u\n",cfg.log_level);
 				if(cfg.inbound[0])
 					fprintf(stream,"INBOUND %s\n",cfg.inbound);
 				if(cfg.secure[0])
