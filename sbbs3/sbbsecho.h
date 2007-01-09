a200ecc2f06e748a3c405f18f0f916cb759ec65a
rswindell <rswindell>
2007-01-09 17:35:55 +0000

    Increment version to 2.12.

--- sbbs3/sbbsecho.h
+++ sbbs3/sbbsecho.h
@@ -2,7 +2,7 @@
 
 /* Synchronet FidoNet Echomail tosser/scanner/areafix program */
 
-/* $Id: sbbsecho.h,v 1.12 2007/01/07 19:25:26 rswindell Exp $ */
+/* $Id: sbbsecho.h,v 1.13 2007/01/09 17:35:55 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -37,7 +37,7 @@
 
 /* Portions written by Allen Christiansen 1994-1996 						*/
 
-#define SBBSECHO_VER "2.11"
+#define SBBSECHO_VER "2.12"
 
 #define IMPORT_NETMAIL  (1L<<0)
 #define IMPORT_PACKETS	(1L<<1)
