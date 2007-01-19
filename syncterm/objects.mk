a8a684fe1c54ce647a04d91b4d02835155eff83b
rswindell <rswindell>
2007-01-19 06:22:23 +0000

    Created a common removefile() sbbs_t method, so removing files will always be logged.

--- syncterm/objects.mk
+++ syncterm/objects.mk
@@ -3,12 +3,9 @@ OBJS = \
                         $(MTOBJODIR)$(DIRSEP)uifcinit$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)filepick$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)fonts$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)rlogin$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)ssh$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)telnet_io$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)conn_telnet$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)conn$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)telnet$(OFILE) \
+                        $(MTOBJODIR)$(DIRSEP)telnet_io$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)term$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)window$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)menu$(OFILE) \
@@ -17,4 +14,3 @@ OBJS = \
                         $(MTOBJODIR)$(DIRSEP)zmodem$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)st_crypt$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)syncterm$(OFILE)
-
