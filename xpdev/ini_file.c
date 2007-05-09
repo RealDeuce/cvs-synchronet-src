994dc17c1b5071cc8791001b4b02856b8514adab
rswindell <rswindell>
2007-05-09 02:34:33 +0000

    Adjust .ini file parsing to allow blank (no) modem command strings and stop over-riding sexpots.ini key values with default values if a secondary .ini file is specified on the command-line.

--- xpdev/ini_file.c
+++ xpdev/ini_file.c
@@ -2,7 +2,7 @@
 
 /* Functions to parse ini files */
 
-/* $Id: ini_file.c,v 1.101 2007/06/17 03:11:29 rswindell Exp $ */
+/* $Id: ini_file.c,v 1.97 2007/05/09 02:28:25 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -262,9 +262,6 @@ static size_t get_value(str_list_t list, const char* section, const char* key, c
 	size_t	i;
 
 	value[0]=0;
-	if(list==NULL)
-		return 0;
-
 	for(i=find_section(list, section); list[i]!=NULL; i++) {
 		SAFECOPY(str, list[i]);
 		if(is_eof(str))
@@ -297,9 +294,6 @@ BOOL iniKeyExists(str_list_t list, const char* section, const char* key)
 {
 	char	val[INI_MAX_VALUE_LEN];
 	size_t	i;
-
-	if(list==NULL)
-		return(FALSE);
 
 	i=get_value(list, section, key, val);
 
@@ -650,18 +644,13 @@ char* iniSetStringList(str_list_t* list, const char* section, const char* key
 	return iniSetString(list, section, key, value, style);
 }
 
-static char* default_value(const char* deflt, char* value)
-{
-	if(deflt!=NULL && deflt!=value)
-		sprintf(value,"%.*s",INI_MAX_VALUE_LEN-1,deflt);
-
-	return((char*)deflt);
-}
-
 char* iniReadString(FILE* fp, const char* section, const char* key, const char* deflt, char* value)
 {
-	if(read_value(fp,section,key,value)==NULL || *value==0 /* blank */)
-		return default_value(deflt,value);
+	if(read_value(fp,section,key,value)==NULL || *value==0 /* blank */) {
+		if(deflt!=NULL && deflt!=value)
+			sprintf(value,"%.*s",INI_MAX_VALUE_LEN-1,deflt);
+		return((char*)deflt);
+	}
 
 	return(value);
 }
@@ -670,29 +659,13 @@ char* iniGetString(str_list_t list, const char* section, const char* key, const 
 {
 	get_value(list, section, key, value);
 
-	if(*value==0 /* blank value or missing key */)
-		return default_value(deflt,value);
+	if(*value==0 /* blank value or missing key */) {
+		if(deflt!=NULL && deflt!=value)
+			sprintf(value,"%.*s",INI_MAX_VALUE_LEN-1,deflt);
+		return((char*)deflt);
+	}
 
 	return(value);
-}
-
-char* iniReadExistingString(FILE* fp, const char* section, const char* key, const char* deflt, char* value)
-{
-	if(read_value(fp,section,key,value)==NULL)
-		return(NULL);
-
-	if(*value==0 /* blank */)
-		return default_value(deflt,value);
-
-	return(value);
-}
-
-char* iniGetExistingString(str_list_t list, const char* section, const char* key, const char* deflt, char* value)
-{
-	if(!iniKeyExists(list, section, key))
-		return(NULL);
-
-	return iniGetString(list, section, key, deflt, value);
 }
 
 static str_list_t splitList(char* list, const char* sep)
