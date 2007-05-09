f888273306de96921b42f045730afa20c3787309
rswindell <rswindell>
2007-05-09 21:25:48 +0000

    Created iniGet/ReadExistingString(), which do nothing (just return NULL) if the key doesn't exist.

--- syncterm/syncterm.h
+++ syncterm/syncterm.h
@@ -1,5 +1,3 @@
-/* Copyright (C), 2007 by Stephen Hurd */
-
 #ifndef _SYNCTERM_H_
 #define _SYNCTERM_H_
 
@@ -13,21 +11,12 @@ enum {
 	 SYNCTERM_PATH_INI
 	,SYNCTERM_PATH_LIST
 	,SYNCTERM_DEFAULT_TRANSFER_PATH
-	,SYNCTERM_PATH_CACHE
-};
-
-struct modem_settings {
-	char	init_string[INI_MAX_VALUE_LEN];
-	char	device_name[INI_MAX_VALUE_LEN+1];
 };
 
 struct syncterm_settings {
 	int		confirm_close;
 	int		startup_mode;
-	int		output_mode;
 	int		backlines;
-	int		prompt_save;
-	struct modem_settings mdm;
 };
 
 extern char *inpath;
@@ -41,9 +30,5 @@ extern char *font_names[];
 extern int safe_mode;
 char *get_syncterm_filename(char *fn, int fnlen, int type, int shared);
 void load_settings(struct syncterm_settings *set);
-extern char *output_types[];
-extern int output_map[];
-extern char *output_descrs[];
-extern char *output_enum[];
 
 #endif
