32748ce7ec6c38cdf74b8e7975db9532238f1472
deuce <deuce>
2007-05-11 08:51:43 +0000

    Build files for *nix

--- sbbs3/answer.cpp
+++ sbbs3/answer.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet answer "caller" function */
 
-/* $Id: answer.cpp,v 1.52 2007/04/21 01:48:30 rswindell Exp $ */
+/* $Id: answer.cpp,v 1.57 2007/05/09 22:00:59 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -37,6 +37,8 @@
 
 #include "sbbs.h"
 #include "telnet.h"
+
+extern "C" void client_on(SOCKET sock, client_t* client, BOOL update);
 
 bool sbbs_t::answer()
 {
@@ -348,6 +350,36 @@ bool sbbs_t::answer()
 
 	if(!online) 
 		return(false); 
+
+	if(stricmp(terminal,"sexpots")==0) {	/* dial-up connection (via SexPOTS) */
+		SAFEPRINTF2(str,"%s connection detected at %lu bps", terminal, cur_rate);
+		logline("@S",str);
+		node_connection = (ushort)cur_rate;
+		SAFEPRINTF(connection,"%lu",cur_rate);
+		SAFECOPY(cid,"Unknown");
+		SAFECOPY(client_name,"Unknown");
+		if(telnet_location[0]) {			/* Caller-ID info provided */
+			SAFEPRINTF(str, "CID: %s", telnet_location);
+			logline("@*",str);
+			SAFECOPY(cid,telnet_location);
+			truncstr(cid," ");				/* Only include phone number in CID */
+			char* p=telnet_location;
+			FIND_WHITESPACE(p);
+			SKIP_WHITESPACE(p);
+			if(*p) {
+				SAFECOPY(client_name,p);	/* CID name, if provided (maybe 'P' or 'O' if private or out-of-area) */
+			}
+		}
+		SAFECOPY(client.addr,cid);
+		SAFECOPY(client.host,client_name);
+		client_on(client_socket,&client,TRUE /* update */);
+	} else {
+		if(telnet_location[0]) {			/* Telnet Location info provided */
+			SAFEPRINTF(str, "Telnet Location: %s", telnet_location);
+			logline("@*",str);
+		}
+	}
+
 
 	useron.misc&=~(ANSI|COLOR|RIP|WIP);
 	useron.misc|=autoterm;
