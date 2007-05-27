6a5cc1882ffd4597491703f65dea131f6f2f21e6
deuce <deuce>
2007-05-27 06:09:16 +0000

    Fix silly copy/paste bugs.

--- syncterm/conn_telnet.c
+++ syncterm/conn_telnet.c
@@ -1,4 +1,4 @@
-/* $Id: conn_telnet.c,v 1.3 2007/05/29 06:58:35 deuce Exp $ */
+/* $Id: conn_telnet.c,v 1.2 2007/04/18 19:06:02 deuce Exp $ */
 
 #include <stdlib.h>
 
@@ -109,26 +109,11 @@ int telnet_connect(struct bbslist *bbs)
 	if(telnet_sock==INVALID_SOCKET)
 		return(-1);
 
-	if(!create_conn_buf(&conn_inbuf, BUFFER_SIZE))
-		return(-1);
-	if(!create_conn_buf(&conn_outbuf, BUFFER_SIZE)) {
-		destroy_conn_buf(&conn_inbuf);
-		return(-1);
-	}
+	create_conn_buf(&conn_inbuf, BUFFER_SIZE);
+	create_conn_buf(&conn_outbuf, BUFFER_SIZE);
 	conn_api.rd_buf=(unsigned char *)malloc(BUFFER_SIZE);
-	if(!conn_api.rd_buf) {
-		destroy_conn_buf(&conn_inbuf);
-		destroy_conn_buf(&conn_outbuf);
-		return(-1);
-	}
 	conn_api.rd_buf_size=BUFFER_SIZE;
 	conn_api.wr_buf=(unsigned char *)malloc(BUFFER_SIZE);
-	if(!conn_api.wr_buf) {
-		destroy_conn_buf(&conn_inbuf);
-		destroy_conn_buf(&conn_outbuf);
-		free(conn_api.wr_buf);
-		return(-1);
-	}
 	conn_api.wr_buf_size=BUFFER_SIZE;
 
 	memset(telnet_local_option,0,sizeof(telnet_local_option));
