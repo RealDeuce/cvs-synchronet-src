d8e1d91715e4717ac58c34baa13ad7f73a288d30
deuce <deuce>
2007-05-27 04:31:32 +0000

    Add support to 8-bit Atari mode (40x24)

--- sbbs3/jsexec.c
+++ sbbs3/jsexec.c
@@ -2,13 +2,13 @@
 
 /* Execute a Synchronet JavaScript module from the command-line */
 
-/* $Id: jsexec.c,v 1.110 2007/08/25 08:30:52 rswindell Exp $ */
+/* $Id: jsexec.c,v 1.109 2006/09/15 21:12:53 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -112,7 +112,6 @@ void usage(FILE* fp)
 		"\t-y<interval>   set yield interval (default=%u, 0=never)\n"
 		"\t-g<interval>   set garbage collection interval (default=%u, 0=never)\n"
 		"\t-h[hostname]   use local or specified host name (instead of SCFG value)\n"
-		"\t-u<mask>       set file creation permissions mask (in octal)\n"
 		"\t-L<level>      set log level (default=%u)\n"
 		"\t-E<level>      set error log level threshold (default=%d)\n"
 		"\t-f             use non-buffered stream for console messages\n"
@@ -811,7 +810,7 @@ int main(int argc, char **argv, char** environ)
 	branch.gc_interval=JAVASCRIPT_GC_INTERVAL;
 	branch.auto_terminate=TRUE;
 
-	sscanf("$Revision: 1.110 $", "%*s %s", revision);
+	sscanf("$Revision: 1.109 $", "%*s %s", revision);
 	DESCRIBE_COMPILER(compiler);
 
 	memset(&scfg,0,sizeof(scfg));
@@ -859,10 +858,6 @@ int main(int argc, char **argv, char** environ)
 						gethostname(host_name=host_name_buf,sizeof(host_name_buf));
 					else
 						host_name=p;
-					break;
-				case 'u':
-					if(*p==0) p=argv[++argn];
-					umask(strtol(p,NULL,8));
 					break;
 				case 'L':
 					if(*p==0) p=argv[++argn];
