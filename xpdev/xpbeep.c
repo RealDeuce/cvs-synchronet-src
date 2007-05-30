cfd8917a181973d7a1b5c287c285e6ff706d2e59
deuce <deuce>
2007-05-30 06:15:01 +0000

    Add a pair of translation tables for PETSCII... not sure yet what I'm going to DO with them, but here they are!

--- xpdev/xpbeep.c
+++ xpdev/xpbeep.c
@@ -1,4 +1,6 @@
-/* $Id: xpbeep.c,v 1.35 2006/05/28 07:08:28 deuce Exp $ */
+/* $Id: xpbeep.c,v 1.47 2007/03/04 00:29:16 deuce Exp $ */
+
+/* TODO: USE PORTAUDIO! */
 
 /* standard headers */
 #include <math.h>
@@ -43,6 +45,9 @@
 #endif
 
 /* xpdev headers */
+#ifdef WITH_SDL
+#include "sdlfuncs.h"
+#endif
 #include "genwrap.h"
 #include "xpbeep.h"
 
@@ -50,24 +55,30 @@
 
 static BOOL sound_device_open_failed=FALSE;
 static BOOL alsa_device_open_failed=FALSE;
+static BOOL sdl_device_open_failed=FALSE;
 
 enum {
 	 SOUND_DEVICE_CLOSED
 	,SOUND_DEVICE_WIN32
 	,SOUND_DEVICE_ALSA
 	,SOUND_DEVICE_OSS
+	,SOUND_DEVICE_SDL
 };
 
 static int handle_type=SOUND_DEVICE_CLOSED;
 
+#ifdef WITH_SDL
+static SDL_AudioSpec	spec;
+static int				sdl_audio_buf_len=0;
+static int				sdl_audio_buf_pos=0;
+static unsigned char	swave[S_RATE*15/2+1];
+static SDL_sem			*sdlToneDone;
+#endif
+
 #ifdef _WIN32
 static	HWAVEOUT		waveOut;
-static	WAVEHDR			*wh;
-static	WAVEHDR			wh_bufs[2];
-static	WAVEHDR			silence;
-static	unsigned char	silence_data=128;
-static	unsigned char	wave[2][S_RATE*15/2+1];
-static	int				curr_wh=0;
+static	WAVEHDR			wh;
+static	unsigned char	wave[S_RATE*15/2+1];
 #endif
 
 #ifdef USE_ALSA_SOUND
@@ -179,25 +190,32 @@ void makewave(double freq, unsigned char *wave, int samples, enum WAVE_SHAPE sha
 			wave[i]=128;
 		}
 	}
+}
 
-#if 0
-	if(wave[0]>128)
-		starthigh=TRUE;
-	else
-		starthigh=FALSE;
-	/* Completely remove the first wave fragment */
-	i=0;
-	if(wave[i]!=128) {
-		for(;i<midpoint;i++) {
-			if(starthigh && wave[i]<128)
-				break;
-			if(!starthigh && wave[i]>128)
-				break;
-			wave[i]=128;
+#ifdef WITH_SDL
+void sdl_fillbuf(void *userdata, Uint8 *stream, int len)
+{
+	int	copylen=len;
+	int maxlen=sdl_audio_buf_len-sdl_audio_buf_pos;
+
+	/* Copy in the current buffer */
+	if(copylen>maxlen)
+		copylen=maxlen;
+	/* Fill with silence */
+	if(len>copylen)
+		memset(stream+copylen, spec.silence, len-copylen);
+	if(copylen) {
+		memcpy(stream, swave+sdl_audio_buf_pos, copylen);
+		sdl_audio_buf_pos+=copylen;
+		/* If we're done, post the semaphore */
+		if(sdl_audio_buf_pos>=sdl_audio_buf_len) {
+			sdl.SemPost(sdlToneDone);
+			sdl_audio_buf_len=0;
+			sdl_audio_buf_pos=0;
 		}
 	}
-#endif
 }
+#endif
 
 BOOL xptone_open(void)
 {
@@ -216,68 +234,58 @@ BOOL xptone_open(void)
 	if(handle_type!=SOUND_DEVICE_CLOSED)
 		return(TRUE);
 
-#ifdef _WIN32
-	w.wFormatTag = WAVE_FORMAT_PCM;
-	w.nChannels = 1;
-	w.nSamplesPerSec = S_RATE;
-	w.wBitsPerSample = 8;
-	w.nBlockAlign = (w.wBitsPerSample * w.nChannels) / 8;
-	w.nAvgBytesPerSec = w.nSamplesPerSec * w.nBlockAlign;
+#ifdef WITH_SDL
+	if(!sdl_device_open_failed) {
+		if(init_sdl_audio()==-1)
+			sdl_device_open_failed=TRUE;
+		else {
+			spec.freq=22050;
+			spec.format=AUDIO_U8;
+			spec.channels=1;
+			spec.samples=256;		/* Size of audio buffer */
+			spec.size=256;
+			spec.callback=sdl_fillbuf;
+			spec.userdata=NULL;
+			if(sdl.OpenAudio(&spec, NULL)==-1) {
+				sdl_device_open_failed=TRUE;
+			}
+			else {
+				sdlToneDone=sdl.SDL_CreateSemaphore(0);
+				sdl_audio_buf_len=0;
+				sdl_audio_buf_pos=0;
+				sdl.PauseAudio(FALSE);
+				handle_type=SOUND_DEVICE_SDL;
+				return(TRUE);
+			}
+		}
+	}
+#endif
 
-	if(!sound_device_open_failed && waveOutOpen(&waveOut, WAVE_MAPPER, &w, 0, 0, 0)!=MMSYSERR_NOERROR)
-		sound_device_open_failed=TRUE;
-	if(sound_device_open_failed)
-		return(FALSE);
-	memset(&wh_bufs, 0, sizeof(wh_bufs));
-	memset(wave, 128, sizeof(wave));	/* Ensure that if silence DOES timeout, you still get silence */
-	wh_bufs[0].lpData=wave;
-	wh_bufs[0].dwBufferLength=S_RATE*15/2+1;
-	if(waveOutPrepareHeader(waveOut, &wh_bufs[0], sizeof(wh_bufs[0]))!=MMSYSERR_NOERROR) {
-		sound_device_open_failed=TRUE;
-		waveOutClose(waveOut);
-		return(FALSE);
+#ifdef _WIN32
+	if(!sound_device_open_failed) {
+		w.wFormatTag = WAVE_FORMAT_PCM;
+		w.nChannels = 1;
+		w.nSamplesPerSec = S_RATE;
+		w.wBitsPerSample = 8;
+		w.nBlockAlign = (w.wBitsPerSample * w.nChannels) / 8;
+		w.nAvgBytesPerSec = w.nSamplesPerSec * w.nBlockAlign;
+
+		if(!sound_device_open_failed && waveOutOpen(&waveOut, WAVE_MAPPER, &w, 0, 0, 0)!=MMSYSERR_NOERROR)
+			sound_device_open_failed=TRUE;
+		if(sound_device_open_failed)
+			return(FALSE);
+		memset(&wh, 0, sizeof(wh));
+		wh.lpData=wave;
+		wh.dwBufferLength=S_RATE*15/2+1;
+		if(waveOutPrepareHeader(waveOut, &wh, sizeof(wh))!=MMSYSERR_NOERROR) {
+			sound_device_open_failed=TRUE;
+			waveOutClose(waveOut);
+			return(FALSE);
+		}
+		handle_type=SOUND_DEVICE_WIN32;
+		if(!sound_device_open_failed)
+			return(TRUE);
 	}
-	wh_bufs[1].lpData=wave;
-	wh_bufs[1].dwBufferLength=S_RATE*15/2+1;
-	if(waveOutPrepareHeader(waveOut, &wh_bufs[1], sizeof(wh_bufs[1]))!=MMSYSERR_NOERROR) {
-		waveOutUnprepareHeader(waveOut, &wh_bufs[0], sizeof(wh_bufs[0]));
-		sound_device_open_failed=TRUE;
-		waveOutClose(waveOut);
-		return(FALSE);
-	}
-	memset(&silence, 0, sizeof(silence));
-	silence.lpData=&silence_data;
-	silence.dwBufferLength=1;
-	silence.dwFlags=WHDR_BEGINLOOP|WHDR_ENDLOOP;
-	silence.dwLoops=0xffffffffUL;	/* Good for 54 hours of silence @ 22010 */
-	if(waveOutPrepareHeader(waveOut, &silence, sizeof(silence))!=MMSYSERR_NOERROR) {
-		waveOutUnprepareHeader(waveOut, &wh_bufs[0], sizeof(wh_bufs[0]));
-		waveOutUnprepareHeader(waveOut, &wh_bufs[1], sizeof(wh_bufs[1]));
-		sound_device_open_failed=TRUE;
-		waveOutClose(waveOut);
-		return(FALSE);
-	}
-	if(waveOutWrite(waveOut, &silence, sizeof(silence))!=MMSYSERR_NOERROR) {
-		waveOutUnprepareHeader(waveOut, &wh_bufs[0], sizeof(wh_bufs[0]));
-		waveOutUnprepareHeader(waveOut, &wh_bufs[1], sizeof(wh_bufs[1]));
-		waveOutUnprepareHeader(waveOut, &silence, sizeof(wh));
-		sound_device_open_failed=TRUE;
-		waveOutClose(waveOut);
-		return(FALSE);
-	}
-	wh=&wh_bufs[0];
-	curr_wh=0;
-	if(waveOutWrite(waveOut, wh, sizeof(wh_bufs[0]))!=MMSYSERR_NOERROR) {
-		waveOutUnprepareHeader(waveOut, &wh_bufs[0], sizeof(wh_bufs[0]));
-		waveOutUnprepareHeader(waveOut, &wh_bufs[1], sizeof(wh_bufs[1]));
-		waveOutUnprepareHeader(waveOut, &silence, sizeof(wh));
-		sound_device_open_failed=TRUE;
-		waveOutClose(waveOut);
-		return(FALSE);
-	}
-	handle_type=SOUND_DEVICE_WIN32;
-	if(!sound_device_open_failed)
-		return(TRUE);
 #endif
 
 #ifdef USE_ALSA_SOUND
@@ -361,16 +369,18 @@ BOOL xptone_open(void)
 
 BOOL xptone_close(void)
 {
+#ifdef WITH_SDL
+	if(handle_type==SOUND_DEVICE_SDL) {
+		sdl.CloseAudio();
+		sdl.SDL_DestroySemaphore(sdlToneDone);
+	}
+#endif
+
 #ifdef _WIN32
 	if(handle_type==SOUND_DEVICE_WIN32) {
-		waveOutBreakLoop(waveOut);
-		while(waveOutUnprepareHeader(waveOut, &wh_bufs[0], sizeof(wh_bufs[0]))==WAVERR_STILLPLAYING)
-			SLEEP(1);
-		while(waveOutUnprepareHeader(waveOut, &wh_bufs[1], sizeof(wh_bufs[1]))==WAVERR_STILLPLAYING)
-			SLEEP(1);
-		while(waveOutUnprepareHeader(waveOut, &silence, sizeof(silence))==WAVERR_STILLPLAYING)
-			SLEEP(1);
 		waveOutClose(waveOut);
+		while(waveOutUnprepareHeader(waveOut, &wh, sizeof(wh))==WAVERR_STILLPLAYING)
+			SLEEP(1);
 	}
 #endif
 
@@ -386,6 +396,7 @@ BOOL xptone_close(void)
 	handle_type=SOUND_DEVICE_CLOSED;
 	sound_device_open_failed=FALSE;
 	alsa_device_open_failed=FALSE;
+	sdl_device_open_failed=FALSE;
 
 	return(TRUE);
 }
@@ -393,49 +404,6 @@ BOOL xptone_close(void)
 /********************************************************************************/
 /* Play a tone through the wave/DSP output device (sound card) - Deuce			*/
 /********************************************************************************/
-
-#if defined(_WIN32)
-
-BOOL xptone(double freq, DWORD duration, enum WAVE_SHAPE shape)
-{
-	BOOL			success=FALSE;
-	BOOL			must_close=FALSE;
-	WAVEHDR			*this_wh;
-
-	if(handle_type==SOUND_DEVICE_CLOSED) {
-		must_close=TRUE;
-		xptone_open();
-	}
-
-	this_wh=wh;
-	wh->dwBufferLength=S_RATE*duration/1000;
-	if(wh->dwBufferLength<=S_RATE/freq*2)
-		wh->dwBufferLength=S_RATE/freq*2;
-
-	makewave(freq,wave[curr_wh],wh.dwBufferLength,shape);
-
-	waveOutBreakLoop(waveOut);
-	success=TRUE;
-
-	/* Put the silence back in */
-	waveOutWrite(waveOut, &silence, sizeof(silence));
-
-	/* Prepare for next note */
-	curr_wh=1-curr_wh;
-	memset(wave[curr_wh], 128, sizeof(wave));	/* Ensure that if silence DOES timeout, you still get silence */
-	wh=wh_bufs[curr_wh];
-	waveOutWrite(waveOut, wh, sizeof(wh_bufs[0]));
-
-	while(!(this_wh->dwFlags & WHDR_DONE))
-		SLEEP(1);
-
-	if(must_close)
-		xptone_close();
-
-	return(success);
-}
-
-#elif defined(__unix__)
 
 BOOL DLLCALL xptone(double freq, DWORD duration, enum WAVE_SHAPE shape)
 {
@@ -461,6 +429,34 @@ BOOL DLLCALL xptone(double freq, DWORD duration, enum WAVE_SHAPE shape)
 		if(!xptone_open())
 			return(FALSE);
 	}
+
+#ifdef WITH_SDL
+	if(handle_type==SOUND_DEVICE_SDL) {
+		sdl.LockAudio();
+		sdl_audio_buf_pos=0;
+		sdl_audio_buf_len=S_RATE*duration/1000;
+		if(sdl_audio_buf_len<=S_RATE/freq*2)
+			sdl_audio_buf_len=S_RATE/freq*2;
+		makewave(freq,swave,sdl_audio_buf_len,shape);
+		sdl.UnlockAudio();
+		sdl.SemWait(sdlToneDone);
+	}
+#endif
+
+#ifdef _WIN32
+	if(handle_type==SOUND_DEVICE_WIN32) {
+		wh.dwBufferLength=S_RATE*duration/1000;
+		if(wh.dwBufferLength<=S_RATE/freq*2)
+			wh.dwBufferLength=S_RATE/freq*2;
+
+		makewave(freq,wave,wh.dwBufferLength,shape);
+
+		if(waveOutWrite(waveOut, &wh, sizeof(wh))==MMSYSERR_NOERROR) {
+			while(!(wh.dwFlags & WHDR_DONE))
+				SLEEP(1);
+		}
+	}
+#endif
 
 #if defined(USE_ALSA_SOUND) || defined(AFMT_U8)
 	if(freq<17)
@@ -506,6 +502,7 @@ BOOL DLLCALL xptone(double freq, DWORD duration, enum WAVE_SHAPE shape)
 	return(FALSE);
 }
 
+#ifdef __unix__
 /****************************************************************************/
 /* Generate a tone at specified frequency for specified milliseconds		*/
 /* Thanks to Casey Martin (and Deuce) for this code							*/
