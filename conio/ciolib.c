75deb0c062a6105c6847bea14fd7dc793f1fbc84
deuce <deuce>
2007-05-31 09:29:33 +0000

    Add newline at end of file.

--- conio/ciolib.c
+++ conio/ciolib.c
@@ -1,4 +1,4 @@
-/* $Id: ciolib.c,v 1.82 2007/06/06 22:47:43 deuce Exp $ */
+/* $Id: ciolib.c,v 1.80 2007/05/30 04:33:37 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -48,7 +48,7 @@
 #define CIOLIB_NO_MACROS
 #include "ciolib.h"
 
-#if defined(WITH_SDL) || defined(WITH_SDL_AUDIO)
+#ifdef WITH_SDL
  #include "sdl_con.h"
 #endif
 #ifdef _WIN32
@@ -111,7 +111,7 @@ CIOLIBEXPORT char * CIOLIBCALL ciolib_getcliptext(void);
 
 #define CIOLIB_INIT()		{ if(initialized != 1) initciolib(CIOLIB_MODE_AUTO); }
 
-#if defined(WITH_SDL) || defined(WITH_SDL_AUDIO)
+#ifdef WITH_SDL
 int try_sdl_init(int mode)
 {
 	if(!sdl_initciolib(mode)) {
@@ -207,7 +207,7 @@ int try_curses_init(int mode)
 		cio_api.hidemouse=curs_hidemouse;
 		cio_api.suspend=curs_suspend;
 		cio_api.resume=curs_resume;
-#if defined(NCURSES_VERSION_MAJOR) || defined (__NetBSD__)
+#ifdef NCURSES_VERSION_MAJOR
 		cio_api.ESCDELAY=&ESCDELAY;
 #endif
 		return(1);
@@ -304,7 +304,7 @@ CIOLIBEXPORT int CIOLIBCALL initciolib(int mode)
 
 	switch(mode) {
 		case CIOLIB_MODE_AUTO:
-#if defined(WITH_SDL) || defined(WITH_SDL_AUDIO)
+#ifdef WITH_SDL
 			if(!try_sdl_init(mode))
 #endif
 #ifdef _WIN32
@@ -337,7 +337,7 @@ CIOLIBEXPORT int CIOLIBCALL initciolib(int mode)
 			try_ansi_init(mode);
 			break;
 
-#if defined(WITH_SDL) || defined(WITH_SDL_AUDIO)
+#ifdef WITH_SDL
 		case CIOLIB_MODE_SDL:
 		case CIOLIB_MODE_SDL_FULLSCREEN:
 			try_sdl_init(mode);
