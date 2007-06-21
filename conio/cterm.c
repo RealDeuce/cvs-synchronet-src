3f190afc8d372e1666a80f5c12caeda5fddabf27
rswindell <rswindell>
2007-06-21 06:11:07 +0000

    Added BE/LE_INT16/32() macros.

--- conio/cterm.c
+++ conio/cterm.c
@@ -1,4 +1,4 @@
-/* $Id: cterm.c,v 1.103 2007/12/13 02:06:18 deuce Exp $ */
+/* $Id: cterm.c,v 1.99 2007/05/30 04:34:11 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -596,14 +596,6 @@ void do_ansi(char *retbuf, size_t retsize, int *speed)
 							}
 						}
 						break;
-					case 'h':
-						if(!strcmp(cterm.escbuf,"[=255h"))
-							cterm.doorway_mode=1;
-						break;
-					case 'l':
-						if(!strcmp(cterm.escbuf,"[=255l"))
-							cterm.doorway_mode=0;
-						break;
 					case '{':
 						if(cterm.escbuf[1] == '=') {	/* Font loading */
 							i=255;
@@ -1185,7 +1177,7 @@ void do_ansi(char *retbuf, size_t retsize, int *speed)
 
 void cterm_init(int height, int width, int xpos, int ypos, int backlines, unsigned char *scrollback, int emulation)
 {
-	char	*revision="$Revision: 1.103 $";
+	char	*revision="$Revision: 1.99 $";
 	char *in;
 	char	*out;
 	int		i;
@@ -1219,8 +1211,7 @@ void cterm_init(int height, int width, int xpos, int ypos, int backlines, unsign
 		memset(cterm.scrollback,0,cterm.width*2*cterm.backlines);
 	textattr(cterm.attr);
 	_setcursortype(_NORMALCURSOR);
-	if(ti.winleft != cterm.x || ti.wintop != cterm.y || ti.winright != cterm.x+cterm.width-1 || ti.winleft != cterm.y+cterm.height-1)
-		window(cterm.x,cterm.y,cterm.x+cterm.width-1,cterm.y+cterm.height-1);
+	window(cterm.x,cterm.y,cterm.x+cterm.width-1,cterm.y+cterm.height-1);
 	clearscreen(cterm.attr);
 	gotoxy(1,1);
 	strcpy(cterm.DA,"\x1b[=67;84;101;114;109;");
@@ -1327,11 +1318,11 @@ void ctputs(char *buf)
 				if(cy==cterm.height
 						&& cx==cterm.width) {
 					char ch;
-					ch=*(p+1);
-					*(p+1)=0;
+					ch=*p;
+					*p=0;
 					cputs(outp);
-					*(p+1)=ch;
-					outp=p+1;
+					*p=ch;
+					outp=p;
 					scrollup();
 					cx=1;
 					gotoxy(cx,cy);
@@ -1369,8 +1360,7 @@ char *cterm_write(unsigned char *buf, int buflen, char *retbuf, size_t retsize, 
 	if(retbuf!=NULL)
 		retbuf[0]=0;
 	gettextinfo(&ti);
-	if(ti.winleft != cterm.x || ti.wintop != cterm.y || ti.winright != cterm.x+cterm.width-1 || ti.winleft != cterm.y+cterm.height-1)
-		window(cterm.x,cterm.y,cterm.x+cterm.width-1,cterm.y+cterm.height-1);
+	window(cterm.x,cterm.y,cterm.x+cterm.width-1,cterm.y+cterm.height-1);
 	gotoxy(cterm.xpos,cterm.ypos);
 	textattr(cterm.attr);
 	ch[1]=0;
@@ -1892,68 +1882,47 @@ char *cterm_write(unsigned char *buf, int buflen, char *retbuf, size_t retsize, 
 						}
 					}
 					else {	/* ANSI-BBS */
-						if(cterm.doorway_char) {
-							ctputs(prn);
-							ch[1]=cterm.attr;
-							puttext(cterm.x+wherex()-1,cterm.y+wherey()-1,cterm.x+wherex()-1,cterm.y+wherey()-1,ch);
-							ch[1]=0;
-							if(wherex()==cterm.width) {
-								if(wherey()==cterm.height) {
-									scrollup();
-									gotoxy(1,wherey());
-								}
-								else
-									gotoxy(1,wherey()+1);
-							}
-							else
-								gotoxy(wherex()+1,wherey());
-							cterm.doorway_char=0;
-						}
-						else {
-							switch(buf[j]) {
-								case 0:
-									if(cterm.doorway_mode)
-										cterm.doorway_char=1;
-									break;
-								case 7:			/* Beep */
-									ctputs(prn);
-									prn[0]=0;
-									if(cterm.log==CTERM_LOG_ASCII && cterm.logfile != NULL)
-										fputs("\x07", cterm.logfile);
-									#ifdef __unix__
-										putch(7);
-									#else
-										MessageBeep(MB_OK);
-									#endif
-									break;
-								case 12:		/* ^L - Clear screen */
-									ctputs(prn);
-									prn[0]=0;
-									if(cterm.log==CTERM_LOG_ASCII && cterm.logfile != NULL)
-										fputs("\x0c", cterm.logfile);
-									clearscreen((char)cterm.attr);
-									gotoxy(1,1);
-									break;
-								case 27:		/* ESC */
-									ctputs(prn);
-									prn[0]=0;
-									cterm.sequence=1;
-									break;
-								case '\t':
-									ctputs(prn);
-									prn[0]=0;
-									if(cterm.log==CTERM_LOG_ASCII && cterm.logfile != NULL)
-										fputs("\t", cterm.logfile);
-									for(k=0;k<11;k++) {
-										if(cterm_tabs[k]>wherex()) {
-											gotoxy(cterm_tabs[k],wherey());
-											break;
-										}
+						switch(buf[j]) {
+							case 0:
+								break;
+							case 7:			/* Beep */
+								ctputs(prn);
+								prn[0]=0;
+								if(cterm.log==CTERM_LOG_ASCII && cterm.logfile != NULL)
+									fputs("\x07", cterm.logfile);
+								#ifdef __unix__
+									putch(7);
+								#else
+									MessageBeep(MB_OK);
+								#endif
+								break;
+							case 12:		/* ^L - Clear screen */
+								ctputs(prn);
+								prn[0]=0;
+								if(cterm.log==CTERM_LOG_ASCII && cterm.logfile != NULL)
+									fputs("\x0c", cterm.logfile);
+								clearscreen((char)cterm.attr);
+								gotoxy(1,1);
+								break;
+							case 27:		/* ESC */
+								ctputs(prn);
+								prn[0]=0;
+								cterm.sequence=1;
+								break;
+							case '\t':
+								ctputs(prn);
+								prn[0]=0;
+								if(cterm.log==CTERM_LOG_ASCII && cterm.logfile != NULL)
+									fputs("\t", cterm.logfile);
+								for(k=0;k<11;k++) {
+									if(cterm_tabs[k]>wherex()) {
+										gotoxy(cterm_tabs[k],wherey());
+										break;
 									}
-									break;
-								default:
-									strcat(prn,ch);
-							}
+								}
+								break;
+							default:
+								strcat(prn,ch);
 						}
 					}
 				}
@@ -1965,8 +1934,7 @@ char *cterm_write(unsigned char *buf, int buflen, char *retbuf, size_t retsize, 
 	cterm.xpos=wherex();
 	cterm.ypos=wherey();
 #if 0
-	if(ti.winleft != cterm.x || ti.wintop != cterm.y || ti.winright != cterm.x+cterm.width-1 || ti.winleft != cterm.y+cterm.height-1)
-		window(ti.winleft,ti.wintop,ti.winright,ti.winbottom);
+	window(ti.winleft,ti.wintop,ti.winright,ti.winbottom);
 	gotoxy(ti.curx,ti.cury);
 	textattr(ti.attribute);
 #endif
