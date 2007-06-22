a1b30b42f7a865c7953b02d953d1d41934e20f73
deuce <deuce>
2007-06-22 20:43:08 +0000

    Real for partial scroll detection... ansivmem uses shorts of all the stupid things. This will need to be fixed for ANSI output on BE platforms.

--- xpdev/gen_defs.h
+++ xpdev/gen_defs.h
@@ -2,7 +2,7 @@
 
 /* General(ly useful) constant, macro, and type definitions */
 
-/* $Id: gen_defs.h,v 1.38 2007/07/08 20:17:35 deuce Exp $ */
+/* $Id: gen_defs.h,v 1.37 2007/05/31 09:09:58 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -61,9 +61,6 @@
 
 
 #include <sys/types.h>
-#ifdef HAS_INTTYPES_H
-#include <inttypes.h>
-#endif
 
 									/* Control characters */
 #ifndef STX
@@ -139,21 +136,6 @@ enum {
 	#endif
 #endif
 
-#ifdef HAS_INTTYPES_H
-#ifndef BYTE
-#define BYTE	uint8_t
-#endif
-#ifndef WORD
-#define WORD	uint16_t
-#endif
-#ifndef DWORD
-#define DWORD	uint32_t
-#endif
-#ifndef BOOL
-#define BOOL	int_fast8_t
-#endif
-#else
-
 /* Windows Types */
 #ifndef BYTE
 #define BYTE	uchar
@@ -167,15 +149,6 @@ enum {
 #ifndef BOOL
 #define BOOL	int
 #endif
-#define int8_t		char
-#define int16_t		short
-#define int32_t		int
-#define uint8_t		uchar
-#define uint16_t	ushort
-#define uint32_t	uint
-
-#endif
-
 #ifndef TRUE
 #define TRUE	1
 #define FALSE	0
