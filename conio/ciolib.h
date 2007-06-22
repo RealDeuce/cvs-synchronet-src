e2c553396c8ae98d59e7795012092a0b42611577
deuce <deuce>
2007-06-22 03:31:04 +0000

    Fix CTRL-A CTRL-A

--- conio/ciolib.h
+++ conio/ciolib.h
@@ -1,4 +1,4 @@
-/* $Id: ciolib.h,v 1.48 2007/10/21 02:28:35 deuce Exp $ */
+/* $Id: ciolib.h,v 1.43 2007/05/31 09:37:25 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -78,8 +78,6 @@ enum {
 	,CIOLIB_MODE_CONIO
 	,CIOLIB_MODE_SDL
 	,CIOLIB_MODE_SDL_FULLSCREEN
-	,CIOLIB_MODE_SDL_YUV
-	,CIOLIB_MODE_SDL_YUV_FULLSCREEN
 };
 
 #if defined(_WIN32)	/* presumably, Win32 */
@@ -202,8 +200,6 @@ struct text_info {
 	unsigned char cury;           /* y-coordinate in current window */
 };
 
-extern struct text_info cio_textinfo;
-
 typedef struct {
 	int		mode;
 	int		mouse;
@@ -254,7 +250,6 @@ typedef struct {
 	int		(*setfont)		(int font, int force);
 	int		(*getfont)		(void);
 	int		(*loadfont)		(char *filename);
-	int		(*get_window_info)		(int* width, int* height, int* xpos, int* ypos);
 	int		*ESCDELAY;
 } cioapi_t;
 
@@ -314,11 +309,6 @@ CIOLIBEXPORT char * CIOLIBCALL ciolib_getcliptext(void);
 CIOLIBEXPORT int CIOLIBCALL ciolib_setfont(int font, int force);
 CIOLIBEXPORT int CIOLIBCALL ciolib_getfont(void);
 CIOLIBEXPORT int CIOLIBCALL ciolib_loadfont(char *filename);
-CIOLIBEXPORT int CIOLIBCALL ciolib_get_window_info(int *width, int *height, int *xpos, int *ypos);
-CIOLIBEXPORT int CIOLIBCALL ciolib_beep(void);
-
-/* DoorWay specific stuff that's only applicable to ANSI mode. */
-CIOLIBEXPORT void CIOLIBCALL ansi_ciolib_setdoorway(int enable);
 #ifdef __cplusplus
 }
 #endif
@@ -369,8 +359,6 @@ CIOLIBEXPORT void CIOLIBCALL ansi_ciolib_setdoorway(int enable);
 	#define setfont(a,b)			ciolib_setfont(a,b)
 	#define getfont()				ciolib_getfont()
 	#define loadfont(a)				ciolib_loadfont(a)
-	#define get_window_info(a,b,c,d)	ciolib_get_window_info(a,b,c,d)
-	#define beep()				ciolib_beep()
 #endif
 
 #ifdef WITH_SDL
