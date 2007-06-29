67bafad60a69d8fe5cf59d91f3da9ef86eee56ff
deuce <deuce>
2007-06-29 05:33:48 +0000

    Fix doorway mode toogles

--- syncterm/telnet_io.c
+++ syncterm/telnet_io.c
@@ -1,6 +1,4 @@
-/* Copyright (C), 2007 by Stephen Hurd */
-
-/* $Id: telnet_io.c,v 1.23 2008/01/21 07:00:51 rswindell Exp $ */
+/* $Id: telnet_io.c,v 1.20 2007/03/03 12:24:05 deuce Exp $ */
 
 #include <stdlib.h>
 #include <string.h>
@@ -24,7 +22,6 @@ char	terminal[TELNET_TERM_MAXLEN+1];
 uchar	telnet_local_option[0x100];
 uchar	telnet_remote_option[0x100];
 
-extern char *log_levels[];
 extern FILE*	log_fp;
 int	telnet_log_level;
 
@@ -40,7 +37,7 @@ static int lprintf(int level, const char *fmt, ...)
     vsnprintf(sbuf,sizeof(sbuf),fmt,argptr);
 	sbuf[sizeof(sbuf)-1]=0;
     va_end(argptr);
-    return(fprintf(log_fp, "Telnet %s %s\n", log_levels[level], sbuf));
+    return(fprintf(log_fp, "%s\n", sbuf));
 }
 
 void putcom(BYTE* buf, size_t len)
@@ -50,7 +47,7 @@ void putcom(BYTE* buf, size_t len)
 	size_t i;
 
 	for(i=0;i<len;i++)
-		p+=sprintf(p,"%u ", buf[i]);
+		p+=sprintf(p,"%02X ", buf[i]);
 
 	lprintf(LOG_DEBUG,"TX: %s", str);
 	send(telnet_sock, buf, len, 0);
@@ -61,12 +58,12 @@ static void send_telnet_cmd(uchar cmd, uchar opt)
 	char buf[16];
 	
 	if(cmd<TELNET_WILL) {
-		lprintf(LOG_INFO,"TX: %s"
+		lprintf(LOG_INFO,"TX Telnet command: %s"
 			,telnet_cmd_desc(cmd));
 		sprintf(buf,"%c%c",TELNET_IAC,cmd);
 		putcom(buf,2);
 	} else {
-		lprintf(LOG_INFO,"TX: %s %s"
+		lprintf(LOG_INFO,"TX Telnet command: %s %s"
 			,telnet_cmd_desc(cmd), telnet_opt_desc(opt));
 		sprintf(buf,"%c%c%c",TELNET_IAC,cmd,opt);
 		putcom(buf,3);
@@ -136,7 +133,7 @@ BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen)
 							,TELNET_IAC,TELNET_SB
 							,TELNET_TERM_TYPE,TELNET_TERM_IS
 							,TELNET_IAC,TELNET_SE);
-						lprintf(LOG_INFO,"TX: Terminal Type is ANSI");
+						lprintf(LOG_INFO,"TX Telnet command: Terminal Type is ANSI");
 						putcom(buf,len);
 						request_telnet_opt(TELNET_WILL, TELNET_NEGOTIATE_WINDOW_SIZE);
 					}
@@ -148,7 +145,7 @@ BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen)
             }
             else if(telnet_cmdlen>=3) {	/* telnet option negotiation */
 
-				lprintf(LOG_INFO,"RX: %s %s"
+				lprintf(LOG_INFO,"RX Telnet command: %s %s"
 					,telnet_cmd_desc(command),telnet_opt_desc(option));
 
 				if(command==TELNET_DO || command==TELNET_DONT) {	/* local options */
@@ -180,7 +177,7 @@ BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen)
 						buf[6]=term.height&0xff;
 						buf[7]=TELNET_IAC;
 						buf[8]=TELNET_SE;
-						lprintf(LOG_INFO,"TX: Window Size is %u x %u"
+						lprintf(LOG_INFO,"TX Telnet command: Window Size is %u x %u"
 							,term.width, term.height);
 						putcom(buf,9);
 					}
