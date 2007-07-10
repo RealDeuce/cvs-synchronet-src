23ac859a2303ec8deba5db59bda3152a2d93a904
deuce <deuce>
2007-07-10 23:49:41 +0000

    Type updates for 64-bit Silly hack for writing a 32-bit value. (With TODO comment)

--- sbbs3/sbbs.h
+++ sbbs3/sbbs.h
@@ -2,13 +2,13 @@
 
 /* Synchronet class (sbbs_t) definition and exported function prototypes */
 
-/* $Id: sbbs.h,v 1.277 2006/06/18 05:08:13 rswindell Exp $ */
+/* $Id: sbbs.h,v 1.298 2007/07/10 23:16:56 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -47,6 +47,9 @@
 /***************/
 #if defined(_WIN32)			/* Windows */
 
+	#define NOCRYPT     /* Stop windows.h from loading wincrypt.h */
+                    /* Is windows.h REALLY necessary?!?! */
+	#define WIN32_LEAN_AND_MEAN
 	#include <io.h>
 	#include <share.h>
 	#include <windows.h>
@@ -67,6 +70,12 @@
 
 	#include <unistd.h>		/* close */
 
+#endif
+
+#ifdef _THREAD_SUID_BROKEN
+extern int	thread_suid_broken;			/* NPTL is no longer broken */
+#else
+#define thread_suid_broken FALSE
 #endif
 
 /******************/
@@ -101,6 +110,10 @@
 	#include <jsprf.h>		/* JS-safe sprintf functions */
 	#include <jsnum.h>		/* JSDOUBLE_IS_NaN() */
 
+#endif
+
+#ifdef USE_CRYPTLIB
+#include <cryptlib.h>
 #endif
 
 /***********************/
@@ -163,6 +176,13 @@ public:
 	char	client_name[128];
 	char	client_ident[128];
 	DWORD	local_addr;
+#ifdef USE_CRYPTLIB
+	CRYPT_SESSION	ssh_session;
+	bool	ssh_mode;
+	SOCKET	passthru_socket;
+    bool	passthru_output_thread_running;
+    bool	passthru_input_thread_running;
+#endif
 
 	scfg_t	cfg;
 
@@ -194,6 +214,7 @@ public:
     uint	telnet_cmdlen;
 	ulong	telnet_mode;
 	uchar	telnet_last_rxch;
+	char	telnet_location[128];
 	char	terminal[TELNET_TERM_MAXLEN+1];
 
 	time_t	event_time;				// Time of next exclusive event
@@ -255,10 +276,13 @@ public:
 	/*********************************/
 	char 	*text[TOTAL_TEXT];			/* Text from ctrl\text.dat */
 	char 	*text_sav[TOTAL_TEXT];		/* Text from ctrl\text.dat */
+
 	char 	dszlog[127];	/* DSZLOG enviornment variable */
     int     keybuftop,keybufbot;    /* Keyboard input buffer pointers (for ungetkey) */
 	char    keybuf[KEY_BUFSIZE];    /* Keyboard input buffer */ 
-	char *	connection;		/* Connection Description */
+
+	ushort	node_connection;
+	char	connection[LEN_MODEM+1];	/* Connection Description */
 	ulong	cur_rate;		/* Current Connection (DCE) Rate */
 	ulong	cur_cps;		/* Current Average Transfer CPS */
 	ulong	dte_rate;		/* Current COM Port (DTE) Rate */
@@ -281,7 +305,9 @@ public:
 	char 	latr;			/* Starting attribute of line buffer */
 	ulong	console;		/* Defines current Console settings */
 	char 	wordwrap[81];	/* Word wrap buffer */
-	time_t	now,			/* Used to store current time in Unix format */
+	/* TODO: we don't really want to do this... */
+	/* But we're writing it in logon.cpp */
+	time32_t	now,			/* Used to store current time in Unix format */
 			answertime, 	/* Time call was answered */
 			logontime,		/* Time user logged on */
 			starttime,		/* Time stamp to use for time left calcs */
@@ -343,13 +369,13 @@ public:
 			/* Global command shell variables */
 	uint	global_str_vars;
 	char **	global_str_var;
-	long *	global_str_var_name;
+	int32_t *	global_str_var_name;
 	uint	global_int_vars;
-	long *	global_int_var;
-	long *	global_int_var_name;
+	int32_t *	global_int_var;
+	int32_t *	global_int_var_name;
 	char *	sysvar_p[MAX_SYSVARS];
 	uint	sysvar_pi;
-	long	sysvar_l[MAX_SYSVARS];
+	int32_t	sysvar_l[MAX_SYSVARS];
 	uint	sysvar_li;
 
     /* ansi_term.cpp */
@@ -364,11 +390,11 @@ public:
 	int		exec_net(csi_t *csi);
 	int		exec_msg(csi_t *csi);
 	int		exec_file(csi_t *csi);
-	long	exec_bin(char *mod, csi_t *csi);
+	long	exec_bin(const char *mod, csi_t *csi);
 	void	clearvars(csi_t *bin);
 	void	freevars(csi_t *bin);
-	char**	getstrvar(csi_t *bin, long name);
-	long*	getintvar(csi_t *bin, long name);
+	char**	getstrvar(csi_t *bin, int32_t name);
+	int32_t*	getintvar(csi_t *bin, int32_t name);
 	char*	copystrvar(csi_t *csi, char *p, char *str);
 	void	skipto(csi_t *csi, uchar inst);
 	bool	ftp_cmd(csi_t* csi, SOCKET ctrl_sock, char* cmdsrc, char* rsp);
@@ -438,6 +464,7 @@ public:
 	void	automsg(void);
 	bool	writemsg(char *str, char *top, char *title, long mode, int subnum
 				,char *dest);
+	char*	msg_tmp_fname(int xedit, char* fname, size_t len);
 	char	putmsg(char *str, long mode);
 	bool	msgabort(void);
 	bool	email(int usernumber, char *top, char *title, long mode);
@@ -491,6 +518,7 @@ public:
 	void	cursor_down(int count=1);
 	void	cursor_left(int count=1);
 	void	cursor_right(int count=1);
+	long	term_supports(long cmp_flags=0);
 
 	/* getstr.cpp */
 	size_t	getstr_offset;
@@ -527,7 +555,7 @@ public:
 
 	/* atcodes.cpp */
 	int		show_atcode(char *code);
-	char*	atcode(char* sp, char* str);
+	char*	atcode(char* sp, char* str, size_t maxlen);
 
 	/* getnode.cpp */
 	int		getsmsg(int usernumber);
@@ -591,6 +619,7 @@ public:
 	bool	chksyspass(void);
 	bool	chk_ar(uchar * str, user_t * user); /* checks access requirements */
 	bool	ar_exp(uchar ** ptrptr, user_t * user);
+	void	daily_maint(void);
 
 	/* upload.cpp */
 	bool	uploadfile(file_t* f);
@@ -616,6 +645,7 @@ public:
 	void	openfile(file_t* f);
 	void	closefile(file_t* f);
 	bool	removefcdt(file_t* f);
+	bool	removefile(file_t* f);
 	bool	movefile(file_t* f, int newdir);
 	char *	getfilespec(char *str);
 	bool	checkfname(char *fname);
@@ -798,6 +828,7 @@ extern "C" {
 
 	/* filedat.c */
 	DLLEXPORT BOOL		DLLCALL getfileixb(scfg_t* cfg, file_t* f);
+	DLLEXPORT BOOL		DLLCALL putfileixb(scfg_t* cfg, file_t* f);
 	DLLEXPORT BOOL		DLLCALL getfiledat(scfg_t* cfg, file_t* f);
 	DLLEXPORT BOOL		DLLCALL putfiledat(scfg_t* cfg, file_t* f);
 	DLLEXPORT void		DLLCALL putextdesc(scfg_t* cfg, uint dirnum, ulong datoffset, char *ext);
@@ -857,6 +888,7 @@ extern "C" {
 	DLLEXPORT BOOL		DLLCALL write_chat_cfg(scfg_t* cfg, int backup_level);
 	DLLEXPORT BOOL		DLLCALL write_xtrn_cfg(scfg_t* cfg, int backup_level);
 	DLLEXPORT BOOL		DLLCALL fcopy(char* src, char* dest);
+	DLLEXPORT BOOL		DLLCALL fcompare(char* fn1, char* fn2);
 	DLLEXPORT BOOL		DLLCALL backup(char *org, int backup_level, BOOL ren);
 	DLLEXPORT void		DLLCALL refresh_cfg(scfg_t* cfg);
 	
@@ -920,6 +952,9 @@ extern "C" {
 		 JSTYPE_ARRAY=JSTYPE_LIMIT
 		,JSTYPE_ALIAS
 		,JSTYPE_UNDEF
+#if !defined(JSTYPE_NULL)	/* JSTYPE_NULL was removed after 1.5 rc 6a (?) */
+		,JSTYPE_NULL
+#endif
 	};
 
 	#ifdef BUILD_JSDOCS	/* String compiled into debug build only, for JS documentation generation */
