2709b82346116bc05c81acf855bdc3202b116e44
deuce <deuce>
2007-07-27 01:34:16 +0000

    Close open HTML windows on disconnect.

--- xpdev/filewrap.c
+++ xpdev/filewrap.c
@@ -2,7 +2,7 @@
 
 /* File-related system-call wrappers */
 
-/* $Id: filewrap.c,v 1.33 2006/05/18 04:09:35 deuce Exp $ */
+/* $Id: filewrap.c,v 1.34 2007/07/11 03:52:20 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -92,7 +92,7 @@ int DLLCALL lock(int fd, long pos, long len)
 		int	flags;
 		if((flags=fcntl(fd,F_GETFL))==-1)
 			return -1;
-		if(flags==O_RDONLY)
+		if((flags & (O_RDONLY|O_RDWR|O_WRONLY))==O_RDONLY)
 			alock.l_type = F_RDLCK; /* set read lock to prevent writes */
 		else
 			alock.l_type = F_WRLCK; /* set write lock to prevent all access */
@@ -306,7 +306,7 @@ FILE *_fsopen(char *pszFilename, char *pszMode, int shmode)
 	}
 	switch(Mode)  {
 		case 1:
-			Mode=O_RDONLY;
+			Mode =O_RDONLY;
 			break;
 		case 2:
 			Mode=O_WRONLY|O_CREAT|O_TRUNC;
