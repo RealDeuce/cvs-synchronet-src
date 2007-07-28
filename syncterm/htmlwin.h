6f6d1e347d5f9716a2ebaf8db2208e5dd06b25fb
deuce <deuce>
2007-07-28 08:34:35 +0000

    Use progressive HTML loading.

--- syncterm/htmlwin.h
+++ syncterm/htmlwin.h
@@ -1,5 +1,3 @@
-/* Copyright (C), 2007 by Stephen Hurd */
-
 #ifndef _HTMLWIN_H_
 #define _HTMLWIN_H_
 
@@ -7,21 +5,13 @@
 extern "C" {
 #endif
 
-enum url_actions {
-	 URL_ACTION_REDIRECT
-	,URL_ACTION_DOWNLOAD
-	,URL_ACTION_ISGOOD
-};
-
-int run_html(int width, int height, int xpos, int ypos, void(*callback)(const char *), int(*ucallback)(const char *, char *, size_t, char *, size_t));
+int run_html(void);
 void hide_html(void);
 void iconize_html(void);
 void raise_html(void);
-void lower_html(void);
 void add_html_char(char ch);
 void add_html(const char *buf);
-void html_commit(void);
-void show_html(const char *page);
+void show_html(const char *address, int width, int height, int xpos, int ypos, void(*callback)(const char *), const char *page);
 
 #ifdef __cplusplus
 }
