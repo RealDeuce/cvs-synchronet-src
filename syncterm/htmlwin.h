1a653f40808bbe64bff1417895babc207b2bb2cd
deuce <deuce>
2007-07-28 13:32:55 +0000

    Pass keypresses from the HTML window to the remote. Currently, it appears you need to click insode the window once in order to "activate" keyboard input.  :-(

--- syncterm/htmlwin.h
+++ syncterm/htmlwin.h
@@ -1,5 +1,3 @@
-/* Copyright (C), 2007 by Stephen Hurd */
-
 #ifndef _HTMLWIN_H_
 #define _HTMLWIN_H_
 
@@ -13,15 +11,13 @@ enum url_actions {
 	,URL_ACTION_ISGOOD
 };
 
-int run_html(int width, int height, int xpos, int ypos, void(*callback)(const char *), int(*ucallback)(const char *, char *, size_t, char *, size_t));
+int run_html(void);
 void hide_html(void);
 void iconize_html(void);
 void raise_html(void);
-void lower_html(void);
 void add_html_char(char ch);
 void add_html(const char *buf);
-void html_commit(void);
-void show_html(const char *page);
+void show_html(int width, int height, int xpos, int ypos, void(*callback)(const char *), int(*url_callback)(const char *, char *, size_t, char *, size_t), const char *page);
 
 #ifdef __cplusplus
 }
