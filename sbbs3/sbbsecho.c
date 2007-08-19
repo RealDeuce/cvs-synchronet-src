bba99a896e69038b562ce27c29902edf7cc16ec4
rswindell <rswindell>
2007-08-19 04:59:57 +0000

    New global JS functions: file_getdosname() returns the Micros~1 shortened version of the specified path/filename on Windows, the unmodified path/filename on other platforms.

--- sbbs3/sbbsecho.c
+++ sbbs3/sbbsecho.c
@@ -2,13 +2,13 @@
 
 /* Synchronet FidoNet EchoMail Scanning/Tossing and NetMail Tossing Utility */
 
-/* $Id: sbbsecho.c,v 1.194 2008/02/25 08:25:29 rswindell Exp $ */
+/* $Id: sbbsecho.c,v 1.192 2007/08/13 23:27:50 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -434,7 +434,6 @@ int create_netmail(char *to, char *subject, char *body, faddr_t dest, BOOL file_
 			if(i<cfg.nodecfgs)
 				attr=cfg.nodecfg[i].attr; } }
 
-	MKDIR(scfg.netmail_dir);
 	do {
 		for(i=startmsg;i;i++) {
 			sprintf(fname,"%s%u.msg",scfg.netmail_dir,i);
@@ -2517,7 +2516,7 @@ int fmsgtosmsg(uchar* fbuf, fmsghdr_t fmsghdr, uint user, uint subnum)
 		msg.idx.number=smbfile->status.last_msg+1;		/* needed for MSG-ID generation */
 
 		/* Generate default (RFC822) message-id (always) */
-		get_msgid(&scfg,subnum,&msg,msg_id,sizeof(msg_id));
+		SAFECOPY(msg_id,get_msgid(&scfg,subnum,&msg));
 		smb_hfield_str(&msg,RFC822MSGID,msg_id);
 	}
 	if(smbfile->status.max_crcs==0)
@@ -3499,7 +3498,6 @@ int import_netmail(char *path,fmsghdr_t hdr, FILE *fidomsg)
 void export_echomail(char *sub_code,faddr_t addr)
 {
 	char	str[1025],tear,cr;
-	char	msgid[256];
 	char*	buf=NULL;
 	char*	minus;
 	uchar*	fmsgbuf=NULL;
@@ -3689,7 +3687,7 @@ void export_echomail(char *sub_code,faddr_t addr)
 					f+=sprintf(fmsgbuf+f,"\1FLAGS %.256s\r", msg.ftn_flags);
 
 				f+=sprintf(fmsgbuf+f,"\1MSGID: %.256s\r"
-					,ftn_msgid(scfg.sub[i],&msg,msgid,sizeof(msgid)));
+					,ftn_msgid(scfg.sub[i],&msg));
 
 				if(msg.ftn_reply!=NULL)			/* use original REPLYID */
 					f+=sprintf(fmsgbuf+f,"\1REPLY: %.256s\r", msg.ftn_reply);
@@ -3703,7 +3701,7 @@ void export_echomail(char *sub_code,faddr_t addr)
 						smb_getmsghdr(&smb[cur_smb],&orig_msg);
 						smb_unlockmsghdr(&smb[cur_smb],&orig_msg);
 						f+=sprintf(fmsgbuf+f,"\1REPLY: %.256s\r"
-							,ftn_msgid(scfg.sub[i],&orig_msg,msgid,sizeof(msgid)));	
+							,ftn_msgid(scfg.sub[i],&orig_msg));	
 					}
 				}
 				if(msg.ftn_pid!=NULL)	/* use original PID */
@@ -3910,7 +3908,7 @@ int main(int argc, char **argv)
 	memset(&msg_path,0,sizeof(addrlist_t));
 	memset(&fakearea,0,sizeof(areasbbs_t));
 
-	sscanf("$Revision: 1.194 $", "%*s %s", revision);
+	sscanf("$Revision: 1.192 $", "%*s %s", revision);
 
 	DESCRIBE_COMPILER(compiler);
 
--- sbbs3/un_qwk.cpp
+++ sbbs3/un_qwk.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet QWK unpacking routine */
 
-/* $Id: un_qwk.cpp,v 1.36 2008/02/26 21:05:27 rswindell Exp $ */
+/* $Id: un_qwk.cpp,v 1.31 2007/01/13 21:38:34 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -45,11 +45,12 @@ bool sbbs_t::unpack_qwk(char *packet,uint hubnum)
 {
 	char	str[MAX_PATH+1],fname[MAX_PATH+1];
 	char 	tmp[512];
+	char	from[26];
+	char	to[26];
 	char	inbox[MAX_PATH+1];
 	uchar	block[QWK_BLOCK_LEN];
 	int 	k,file;
 	uint	i,j,n,lastsub=INVALID_SUB;
-	uint	usernum;
 	uint	blocks;
 	long	l,size,misc;
 	ulong	t;
@@ -60,28 +61,22 @@ bool sbbs_t::unpack_qwk(char *packet,uint hubnum)
 	DIR*	dir;
 	DIRENT*	dirent;
 	FILE*	qwk;
-	FILE*	fp;
-	smbmsg_t	msg;
-	str_list_t	headers=NULL;
-	str_list_t	ip_can=NULL;
-	str_list_t	host_can=NULL;
-	str_list_t	subject_can=NULL;
-	str_list_t	twit_list=NULL;
+	BOOL	twit_list;
 
-	memset(&msg,0,sizeof(msg));
+	sprintf(fname,"%stwitlist.cfg",cfg.ctrl_dir);
+	twit_list=fexist(fname);
 
 	start=time(NULL);
 	if((l=flength(packet))<1) {
 		errormsg(WHERE,ERR_LEN,packet,l);
 		return(false);
 	}
-	delfiles(cfg.temp_dir,ALLFILES);
 	i=external(cmdstr(cfg.qhub[hubnum]->unpack,packet,ALLFILES,NULL),EX_OFFLINE);
 	if(i) {
 		errormsg(WHERE,ERR_EXEC,cmdstr(cfg.qhub[hubnum]->unpack,packet,ALLFILES,NULL),i);
 		return(false); 
 	}
-	SAFEPRINTF(str,"%sMESSAGES.DAT",cfg.temp_dir);
+	sprintf(str,"%sMESSAGES.DAT",cfg.temp_dir);
 	if(!fexistcase(str)) {
 		SAFEPRINTF2(tmp,"%s doesn't contain MESSAGES.DAT (%s)",packet,str);
 		errorlog(tmp);
@@ -92,32 +87,10 @@ bool sbbs_t::unpack_qwk(char *packet,uint hubnum)
 		return(false); 
 	}
 	size=filelength(file);
-
-	SAFEPRINTF(str,"%sHEADERS.DAT",cfg.temp_dir);
-	if(fexistcase(str)) {
-		if((fp=fopen(str,"r")) == NULL)
-			errormsg(WHERE,ERR_OPEN,str,O_RDONLY);
-		else {
-			headers=iniReadFile(fp);
-			fclose(fp);
-		}
-		remove(str);
-	}
-
 	/********************/
 	/* Process messages */
 	/********************/
 	eprintf(LOG_INFO,"Importing QWK Network Packet: %s",packet);
-
-	ip_can=trashcan_list(&cfg,"ip");
-	host_can=trashcan_list(&cfg,"host");
-	subject_can=trashcan_list(&cfg,"subject");
-
-	SAFEPRINTF(fname,"%stwitlist.cfg",cfg.ctrl_dir);
-	if((fp=fopen(fname,"r"))!=NULL) {
-		twit_list=strListReadFile(fp,NULL,128);
-		fclose(fp);
-	}
 
 	for(l=QWK_BLOCK_LEN;l<size;l+=blocks*QWK_BLOCK_LEN) {
 		if(terminated) {
@@ -145,53 +118,32 @@ bool sbbs_t::unpack_qwk(char *packet,uint hubnum)
 		/*********************************/
 		n=(uint)block[123]|(((uint)block[124])<<8);  /* conference number */
 
-		qwk_new_msg(&msg,(char*)block,/*offset: */l,headers);
-
-		if(findstr_in_list(msg.from_ip,ip_can)) {
-			eprintf(LOG_NOTICE,"!Filtering message from %s due to blocked IP: %s"
-				,msg.from
-				,msg.from_ip); 
-			continue;
-		}
-
-		if(findstr_in_list(msg.from_host,host_can)) {
-			eprintf(LOG_NOTICE,"!Filtering message from %s due to blocked hostname: %s"
-				,msg.from
-				,msg.from_host); 
-			continue;
-		}
-
-		if(findstr_in_list(msg.subj,subject_can)) {
-			eprintf(LOG_NOTICE,"!Filtering message from %s due to filtered subject: %s"
-				,msg.from
-				,msg.subj); 
-			continue;
-		}
-
 		if(!n) {		/* NETMAIL */
-			eprintf(LOG_INFO,"NetMail from %s to %s", cfg.qhub[hubnum]->id, msg.to);
-			if(!stricmp(msg.to,"NETMAIL")) {  /* QWK to FidoNet NetMail */
+			sprintf(str,"%25.25s",block+21);
+			truncsp(str);
+			eprintf(LOG_INFO,"NetMail from %s to %s", cfg.qhub[hubnum]->id, str);
+			if(!stricmp(str,"NETMAIL")) {  /* QWK to FidoNet NetMail */
 				qwktonetmail(qwk,(char *)block,NULL,hubnum+1);
 				continue; 
 			}
-			if(strchr(msg.to,'@')) {
-				qwktonetmail(qwk,(char *)block,msg.to,hubnum+1);
+			if(strchr(str,'@')) {
+				qwktonetmail(qwk,(char *)block,str,hubnum+1);
 				continue; 
 			}
-			usernum=atoi(msg.to);
-			if(usernum && usernum>lastuser(&cfg))
-				usernum=0;
-			if(!usernum)
-				usernum=matchuser(&cfg,msg.to,TRUE /* sysop_alias */);
-			if(!usernum) {
-				eprintf(LOG_NOTICE,"!NetMail from %s to UNKNOWN USER: %s", cfg.qhub[hubnum]->id, msg.to);
+			j=atoi(str);
+			if(j && j>lastuser(&cfg))
+				j=0;
+			if(!j)
+				j=matchuser(&cfg,str,TRUE /* sysop_alias */);
+			if(!j) {
+				eprintf(LOG_NOTICE,"!NetMail from %s to UNKNOWN USER: %s", cfg.qhub[hubnum]->id, str);
 				continue; 
 			}
 
-			getuserrec(&cfg,usernum,U_MISC,8,str);
+			getuserrec(&cfg,j,U_MISC,8,str);
 			misc=ahtoul(str);
 			if(misc&NETMAIL && cfg.sys_misc&SM_FWDTONET) {
-				getuserrec(&cfg,usernum,U_NETMAIL,LEN_NETMAIL,str);
+				getuserrec(&cfg,j,U_NETMAIL,LEN_NETMAIL,str);
 				qwktonetmail(qwk,(char*)block,str,hubnum+1);
 				continue; 
 			}
@@ -212,27 +164,29 @@ bool sbbs_t::unpack_qwk(char *packet,uint hubnum)
 				smb.status.attr=SMB_EMAIL;
 				if((k=smb_create(&smb))!=0) {
 					smb_close(&smb);
-					errormsg(WHERE,ERR_CREATE,smb.file,k,smb.last_error);
+					errormsg(WHERE,ERR_CREATE,smb.file,k);
 					smb_stack(&smb,SMB_STACK_POP);
 					continue; 
 				} 
 			}
 			if((k=smb_locksmbhdr(&smb))!=0) {
 				smb_close(&smb);
-				errormsg(WHERE,ERR_LOCK,smb.file,k,smb.last_error);
+				errormsg(WHERE,ERR_LOCK,smb.file,k);
 				smb_stack(&smb,SMB_STACK_POP);
 				continue; 
 			}
 			if((k=smb_getstatus(&smb))!=0) {
 				smb_close(&smb);
-				errormsg(WHERE,ERR_READ,smb.file,k,smb.last_error);
+				errormsg(WHERE,ERR_READ,smb.file,k);
 				smb_stack(&smb,SMB_STACK_POP);
 				continue; 
 			}
 			smb_unlocksmbhdr(&smb);
-			if(qwk_import_msg(qwk,(char *)block,blocks,hubnum+1,INVALID_SUB,usernum,&msg)) {
-				SAFEPRINTF(str,text[UserSentYouMail],msg.from);
-				putsmsg(&cfg,usernum,str);
+			if(qwktomsg(qwk,(char *)block,hubnum+1,INVALID_SUB,j)) {
+				sprintf(tmp,"%-25.25s",block+46);
+				truncsp(tmp);
+				sprintf(str,text[UserSentYouMail],tmp);
+				putsmsg(&cfg,j,str);
 				tmsgs++;
 			}
 			smb_close(&smb);
@@ -252,12 +206,21 @@ bool sbbs_t::unpack_qwk(char *packet,uint hubnum)
 		j=cfg.qhub[hubnum]->sub[j];
 
 		/* TWIT FILTER */
-		if(findstr_in_list(msg.from,twit_list) || findstr_in_list(msg.to,twit_list)) {
-			eprintf(LOG_NOTICE,"!Filtering post from %s to %s on %s %s"
-				,msg.from
-				,msg.to
-				,cfg.grp[cfg.sub[j]->grp]->sname,cfg.sub[j]->lname); 
-			continue; 
+		if(twit_list) {
+			sprintf(fname,"%stwitlist.cfg",cfg.ctrl_dir);
+
+			sprintf(from,"%25.25s",block+46);  /* From user */
+			truncsp(from);
+			sprintf(to,"%25.25s",block+21);		/* To user */
+			truncsp(to);
+
+			if(findstr(from,fname) || findstr(to,fname)) {
+				eprintf(LOG_NOTICE,"!Filtering post from %s to %s on %s %s"
+					,from
+					,to
+					,cfg.grp[cfg.sub[j]->grp]->sname,cfg.sub[j]->lname); 
+				continue; 
+			}
 		}
 
 		if(j!=lastsub) {
@@ -291,54 +254,52 @@ bool sbbs_t::unpack_qwk(char *packet,uint hubnum)
 				smb.status.attr=cfg.sub[j]->misc&SUB_HYPER ? SMB_HYPERALLOC :0;
 				if((k=smb_create(&smb))!=0) {
 					smb_close(&smb);
-					errormsg(WHERE,ERR_CREATE,smb.file,k,smb.last_error);
+					errormsg(WHERE,ERR_CREATE,smb.file,k);
 					continue; 
 				} 
 			}
 			if((k=smb_locksmbhdr(&smb))!=0) {
 				smb_close(&smb);
-				errormsg(WHERE,ERR_LOCK,smb.file,k,smb.last_error);
+				errormsg(WHERE,ERR_LOCK,smb.file,k);
 				continue; 
 			}
 			if((k=smb_getstatus(&smb))!=0) {
 				smb_close(&smb);
-				errormsg(WHERE,ERR_READ,smb.file,k,smb.last_error);
+				errormsg(WHERE,ERR_READ,smb.file,k);
 				continue; 
 			}
 			smb_unlocksmbhdr(&smb);
 			lastsub=j; 
 		}
 
-		if(qwk_import_msg(qwk,(char *)block,blocks,hubnum+1,/*subnum: */j,/*touser: */0,&msg)) {
-			signal_sub_sem(&cfg,j);
-			msgs++;
-			tmsgs++;
+		if(!qwktomsg(qwk,(char *)block,hubnum+1,j,0)) {
+//			eprintf("!QWKTOMSG failed");
+			continue;
 		}
+
+		signal_sub_sem(&cfg,j);
+/*
+		eprintf("Message from %s Posted on %s %s"
+			,cfg.qhub[hubnum]->id,cfg.grp[cfg.sub[j]->grp]->sname,cfg.sub[j]->lname); 
+*/
+		msgs++;
+		tmsgs++;
 	}
 
 	update_qwkroute(NULL);		/* Write ROUTE.DAT */
 
-	smb_freemsgmem(&msg);
 	fclose(qwk);
-
-	iniFreeStringList(headers);
-
-	strListFree(&ip_can);
-	strListFree(&host_can);
-	strListFree(&subject_can);
-	strListFree(&twit_list);
-
 	if(lastsub!=INVALID_SUB)
 		smb_close(&smb);
 
 	delfiles(cfg.temp_dir,"*.NDX");
-	SAFEPRINTF(str,"%sMESSAGES.DAT",cfg.temp_dir);
+	sprintf(str,"%sMESSAGES.DAT",cfg.temp_dir);
 	remove(str);
-	SAFEPRINTF(str,"%sDOOR.ID",cfg.temp_dir);
+	sprintf(str,"%sDOOR.ID",cfg.temp_dir);
 	remove(str);
-	SAFEPRINTF(str,"%sCONTROL.DAT",cfg.temp_dir);
+	sprintf(str,"%sCONTROL.DAT",cfg.temp_dir);
 	remove(str);
-	SAFEPRINTF(str,"%sNETFLAGS.DAT",cfg.temp_dir);
+	sprintf(str,"%sNETFLAGS.DAT",cfg.temp_dir);
 	remove(str);
 
 	dir=opendir(cfg.temp_dir);
@@ -368,9 +329,6 @@ bool sbbs_t::unpack_qwk(char *packet,uint hubnum)
 		eprintf(LOG_INFO,"Finished Importing QWK Network Packet from %s: "
 			"(%lu msgs) in %lu seconds (%lu msgs/sec)"
 			,cfg.qhub[hubnum]->id, tmsgs, t, tmsgs/t);
-		/* trigger timed event with internal code of 'qnet-qwk' to run */
-		sprintf(str,"%sqnet-qwk.now",cfg.data_dir);
-		ftouch(str);
 	}
 	delfiles(cfg.temp_dir,ALLFILES);
 	return(true);
