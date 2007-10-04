0af90f14a019e7ee17c56780e1fd56806f406f20
deuce <deuce>
2007-10-04 00:56:47 +0000

    Fix up hold_update usage.

--- sbbs3/scfg/makehelp.c
+++ sbbs3/scfg/makehelp.c
@@ -8,12 +8,9 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#if defined(__unix__)
-	#include <unistd.h>		/* write */
-	#define O_BINARY 0
-#else
-	#include <io.h>	/* open */
-#endif
+
+/* XPDEV */
+#include <filewrap.h>
 
 int main(int argc, char **argv)
 {
@@ -45,7 +42,7 @@ int main(int argc, char **argv)
 		return(-1); }
 
 	sprintf(str,"%s/scfghelp.ixb",dir);
-	if((ixb=open(str,O_WRONLY|O_CREAT|O_BINARY,S_IWRITE|S_IREAD))==-1) {
+	if((ixb=open(str,O_WRONLY|O_CREAT|O_BINARY,DEFFILEMODE))==-1) {
 		fprintf(stderr,"!error opening %s\n",str);
 		return(-1); }
 
