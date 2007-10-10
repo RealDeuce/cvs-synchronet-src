c41af039a9d5212287c3369c90c80b47597e2f7e
deuce <deuce>
2007-10-10 03:52:59 +0000

    Use DAC colours, not palette colours.

--- conio/x_cio.h
+++ conio/x_cio.h
@@ -1,4 +1,4 @@
-/* $Id: x_cio.h,v 1.18 2008/01/21 07:37:45 deuce Exp $ */
+/* $Id: x_cio.h,v 1.16 2007/08/25 05:29:22 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -54,7 +54,6 @@ int x_wherey(void);
 int x_wherex(void);
 int x_putch(int ch);
 void x_gotoxy(int x, int y);
-int x_init(void);
 void x_initciolib(long inmode);
 void x_gettextinfo(struct text_info *info);
 void x_setcursortype(int type);
@@ -71,7 +70,6 @@ int x_getfont(void);
 int x_loadfont(char *filename);
 int x_get_window_info(int *width, int *height, int *xpos, int *ypos);
 void x11_drawrect(int xoffset,int yoffset,int width,int height,unsigned char *data);
-void x11_flush(void);
 #ifdef __cplusplus
 }
 #endif
