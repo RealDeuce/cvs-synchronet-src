47ddb0385f5b1b3a7401d9ce31e86f60e4958a89
deuce <deuce>
2007-10-10 04:49:58 +0000

    Don't swap case for ATASCII (Requested)

--- sbbs3/readmsgs.cpp
+++ sbbs3/readmsgs.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet public message reading function */
 
-/* $Id: readmsgs.cpp,v 1.40 2008/02/25 08:18:30 rswindell Exp $ */
+/* $Id: readmsgs.cpp,v 1.37 2007/08/23 07:53:46 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -84,8 +84,9 @@ long sbbs_t::listmsgs(uint subnum, long mode, post_t *post, long i, long posts)
 	return(listed);
 }
 
-char *binstr(uchar *buf, ushort length, char* str)
+char *binstr(uchar *buf, ushort length)
 {
+	static char str[512];
 	char tmp[128];
 	int i;
 
@@ -99,10 +100,6 @@ char *binstr(uchar *buf, ushort length, char* str)
 	for(i=0;i<length;i++) {
 		sprintf(tmp,"%02X ",buf[i]);
 		strcat(str,tmp); 
-		if(i >= 100) {
-			strcat(str,"...");
-			break;
-		}
 	}
 	return(str);
 }
@@ -111,7 +108,6 @@ char *binstr(uchar *buf, ushort length, char* str)
 void sbbs_t::msghdr(smbmsg_t* msg)
 {
 	int i;
-	char str[512];
 
 	CRLF;
 
@@ -119,17 +115,13 @@ void sbbs_t::msghdr(smbmsg_t* msg)
 	for(i=0;i<msg->total_hfields;i++)
 		bprintf("%-16.16s %s\r\n"
 			,smb_hfieldtype(msg->hfield[i].type)
-			,binstr((uchar *)msg->hfield_dat[i],msg->hfield[i].length,str));
+			,binstr((uchar *)msg->hfield_dat[i],msg->hfield[i].length));
 
 	/* fixed fields */
-	bprintf("%-16.16s %08lX %04hX %.24s %s\r\n","when_written"	
-		,msg->hdr.when_written.time, msg->hdr.when_written.zone
-		,ctime((time_t*)&msg->hdr.when_written.time)
-		,smb_zonestr(msg->hdr.when_written.zone,NULL));
-	bprintf("%-16.16s %08lX %04hX %.24s %s\r\n","when_imported"	
-		,msg->hdr.when_imported.time, msg->hdr.when_imported.zone
-		,ctime((time_t*)&msg->hdr.when_imported.time)
-		,smb_zonestr(msg->hdr.when_imported.zone,NULL));
+	bprintf("%-16.16s %s %s\r\n","when_written"	
+		,timestr(msg->hdr.when_written.time), smb_zonestr(msg->hdr.when_written.zone,NULL));
+	bprintf("%-16.16s %s %s\r\n","when_imported"	
+		,timestr(msg->hdr.when_imported.time), smb_zonestr(msg->hdr.when_imported.zone,NULL));
 	bprintf("%-16.16s %04Xh\r\n","type"				,msg->hdr.type);
 	bprintf("%-16.16s %04Xh\r\n","version"			,msg->hdr.version);
 	bprintf("%-16.16s %04Xh\r\n","attr"				,msg->hdr.attr);
@@ -823,25 +815,21 @@ int sbbs_t::scanposts(uint subnum, long mode, char *find)
 					,msg.subj
 					,timestr(msg.hdr.when_written.time));
 				if(msg.from_net.addr==NULL)
-					SAFECOPY(str,msg.from);
+					strcpy(str,msg.from);
 				else if(msg.from_net.type==NET_FIDO)
-					SAFEPRINTF2(str,"%s@%s",msg.from
+					sprintf(str,"%s@%s",msg.from
 						,smb_faddrtoa((faddr_t *)msg.from_net.addr,tmp));
-				else if(msg.from_net.type==NET_INTERNET || strchr((char*)msg.from_net.addr,'@')!=NULL) {
-					if(msg.replyto_net.type==NET_INTERNET)
-						SAFECOPY(str,(char *)msg.replyto_net.addr);
-					else
-						SAFECOPY(str,(char *)msg.from_net.addr);
-				}
+				else if(msg.from_net.type==NET_INTERNET)
+					strcpy(str,(char *)msg.from_net.addr);
 				else
-					SAFEPRINTF2(str,"%s@%s",msg.from,(char *)msg.from_net.addr);
+					sprintf(str,"%s@%s",msg.from,(char *)msg.from_net.addr);
 				bputs(text[Email]);
 				if(!getstr(str,60,K_EDIT|K_AUTODEL))
 					break;
 
 				FREE_AND_NULL(post);
 				quotemsg(&msg,1);
-				if(smb_netaddr_type(str)==NET_INTERNET)
+				if(msg.from_net.type==NET_INTERNET && strchr(str,'@'))
 					inetmail(str,msg.subj,WM_QUOTE|WM_NETMAIL);
 				else {
 					p=strrchr(str,'@');
