c1a6a6e1d0df12779ba551b7f62d275f20bb0a7e
deuce <deuce>
2007-10-11 11:55:09 +0000

    Make conn_data_waiting() return the number of bytes waiting. Prevent remote throughput from blocking keyboard input.

--- conio/x_cio.h
+++ conio/x_cio.h
@@ -1,4 +1,4 @@
-/* $Id: x_cio.h,v 1.13 2005/11/19 07:52:34 deuce Exp $ */
+/* $Id: x_cio.h,v 1.17 2007/10/11 10:45:06 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -40,7 +40,7 @@
 #endif
 
 #include "ciolib.h"
-#include "console.h"
+#include "x_events.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -68,6 +68,9 @@ char *x_getcliptext(void);
 int x_setfont(int font, int force);
 int x_getfont(void);
 int x_loadfont(char *filename);
+int x_get_window_info(int *width, int *height, int *xpos, int *ypos);
+void x11_drawrect(int xoffset,int yoffset,int width,int height,unsigned char *data);
+void x11_flush(void);
 #ifdef __cplusplus
 }
 #endif
