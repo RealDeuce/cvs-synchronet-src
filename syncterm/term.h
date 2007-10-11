0a1f66c419f5439a6324d121c539be7354e1f80e
deuce <deuce>
2007-10-11 11:55:09 +0000

    Make conn_data_waiting() return the number of bytes waiting. Prevent remote throughput from blocking keyboard input.

--- syncterm/term.h
+++ syncterm/term.h
@@ -1,6 +1,4 @@
-/* Copyright (C), 2007 by Stephen Hurd */
-
-/* $Id: term.h,v 1.15 2007/11/13 01:37:56 deuce Exp $ */
+/* $Id: term.h,v 1.14 2005/11/28 16:57:12 deuce Exp $ */
 
 #ifndef _TERM_H_
 #define _TERM_H_
