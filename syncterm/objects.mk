88eb971847f34ae83fd1d17f2f61b1ddf53e483a
deuce <deuce>
2007-10-21 04:09:07 +0000

    Add macro to disable defining C99 integer types.

--- syncterm/objects.mk
+++ syncterm/objects.mk
@@ -3,9 +3,14 @@ OBJS = \
                         $(MTOBJODIR)$(DIRSEP)uifcinit$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)filepick$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)fonts$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)conn$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)telnet$(OFILE) \
+                        $(MTOBJODIR)$(DIRSEP)rlogin$(OFILE) \
+                        $(MTOBJODIR)$(DIRSEP)ssh$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)telnet_io$(OFILE) \
+                        $(MTOBJODIR)$(DIRSEP)modem$(OFILE) \
+                        $(MTOBJODIR)$(DIRSEP)conn_telnet$(OFILE) \
+                        $(MTOBJODIR)$(DIRSEP)conn$(OFILE) \
+                        $(MTOBJODIR)$(DIRSEP)comio$(OFILE) \
+                        $(MTOBJODIR)$(DIRSEP)telnet$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)term$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)window$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)menu$(OFILE) \
@@ -14,3 +19,4 @@ OBJS = \
                         $(MTOBJODIR)$(DIRSEP)zmodem$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)st_crypt$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)syncterm$(OFILE)
+
