f74bdeb49f11d4455c1fadca0e6d291695368927
deuce <deuce>
2007-10-21 02:28:35 +0000

    Add support for SDL overlays. This is untested since my system has a terrible video card.

--- sbbs3/ars_defs.h
+++ sbbs3/ars_defs.h
@@ -2,13 +2,13 @@
 
 /* Synchronet Access Requirement Strings (ARS) constants */
 
-/* $Id: ars_defs.h,v 1.5 2005/09/05 18:16:33 rswindell Exp $ */
+/* $Id: ars_defs.h,v 1.8 2007/09/30 22:30:10 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -111,6 +111,15 @@ enum {                              /* Access requirement binaries */
 	,AR_PROT
 	,AR_GUEST
 	,AR_QNODE
+	,AR_ACTIVE
+	,AR_INACTIVE
+	,AR_DELETED
+	,AR_ULS
+	,AR_ULK
+	,AR_ULM
+	,AR_DLS
+	,AR_DLK
+	,AR_DLM
     };
 
 #endif		/* Don't add anything after this line */
