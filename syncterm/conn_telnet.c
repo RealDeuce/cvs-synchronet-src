3f988742fffd9c6728fde971d64e17c6c55e659d
deuce <deuce>
2007-10-21 18:27:48 +0000

    malloc/free paranoia.

--- syncterm/conn_telnet.c
+++ syncterm/conn_telnet.c
@@ -1,6 +1,4 @@
-/* Copyright (C), 2007 by Stephen Hurd */
-
-/* $Id: conn_telnet.c,v 1.8 2008/01/20 22:56:40 rswindell Exp $ */
+/* $Id: conn_telnet.c,v 1.5 2007/10/21 18:27:48 deuce Exp $ */
 
 #include <stdlib.h>
 
@@ -16,7 +14,6 @@
 #include "telnet_io.h"
 
 SOCKET telnet_sock=INVALID_SOCKET;
-extern int	telnet_log_level;
 
 #ifdef __BORLANDC__
 #pragma argsused
@@ -82,7 +79,6 @@ void telnet_output_thread(void *args)
 	conn_api.output_thread_running=1;
 	while(telnet_sock != INVALID_SOCKET && !conn_api.terminate) {
 		pthread_mutex_lock(&(conn_outbuf.mutex));
-		ret=0;
 		wr=conn_buf_wait_bytes(&conn_outbuf, 1, 100);
 		if(wr) {
 			wr=conn_buf_get(&conn_outbuf, conn_api.wr_buf, conn_api.wr_buf_size);
@@ -126,8 +122,6 @@ void telnet_output_thread(void *args)
 int telnet_connect(struct bbslist *bbs)
 {
 	init_uifc(TRUE, TRUE);
-
-	telnet_log_level = bbs->telnet_loglevel;
 
 	telnet_sock=conn_socket_connect(bbs);
 	if(telnet_sock==INVALID_SOCKET)
--- syncterm/rlogin.c
+++ syncterm/rlogin.c
@@ -1,6 +1,4 @@
-/* Copyright (C), 2007 by Stephen Hurd */
-
-/* $Id: rlogin.c,v 1.30 2008/01/20 06:53:00 rswindell Exp $ */
+/* $Id: rlogin.c,v 1.27 2007/10/21 18:27:48 deuce Exp $ */
 
 #include <stdlib.h>
 
@@ -70,7 +68,6 @@ void rlogin_output_thread(void *args)
 	conn_api.output_thread_running=1;
 	while(sock != INVALID_SOCKET && !conn_api.terminate) {
 		pthread_mutex_lock(&(conn_outbuf.mutex));
-		ret=0;
 		wr=conn_buf_wait_bytes(&conn_outbuf, 1, 100);
 		if(wr) {
 			wr=conn_buf_get(&conn_outbuf, conn_api.wr_buf, conn_api.wr_buf_size);
@@ -119,7 +116,7 @@ int rlogin_connect(struct bbslist *bbs)
 
 	ruser=bbs->user;
 	passwd=bbs->password;
-	if(bbs->conn_type==CONN_TYPE_RLOGIN_REVERSED) {
+	if(bbs->conn_type==CONN_TYPE_RLOGIN && bbs->reversed) {
 		passwd=bbs->user;
 		ruser=bbs->password;
 	}
@@ -148,7 +145,7 @@ int rlogin_connect(struct bbslist *bbs)
 	}
 	conn_api.wr_buf_size=BUFFER_SIZE;
 
-	if(bbs->conn_type == CONN_TYPE_RLOGIN || bbs->conn_type == CONN_TYPE_RLOGIN_REVERSED) {
+	if(bbs->conn_type == CONN_TYPE_RLOGIN) {
 		conn_send("",1,1000);
 		conn_send(passwd,strlen(passwd)+1,1000);
 		conn_send(ruser,strlen(ruser)+1,1000);
