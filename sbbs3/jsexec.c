6115c8df4c43047c5b9eca99b226559e307ac213
deuce <deuce>
2007-10-22 19:07:52 +0000

    Start of dialing list sorting configuration.

--- sbbs3/jsexec.c
+++ sbbs3/jsexec.c
@@ -2,7 +2,7 @@
 
 /* Execute a Synchronet JavaScript module from the command-line */
 
-/* $Id: jsexec.c,v 1.113 2008/06/04 04:38:47 deuce Exp $ */
+/* $Id: jsexec.c,v 1.110 2007/08/25 08:30:52 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -43,8 +43,9 @@
 #include <signal.h>
 #endif
 
-#include "sbbs.h"
 #include "ciolib.h"
+
+#include "sbbs.h"
 
 #define DEFAULT_LOG_LEVEL	LOG_DEBUG	/* Display all LOG levels */
 #define DEFAULT_ERR_LOG_LVL	LOG_WARNING
@@ -159,7 +160,7 @@ int mfprintf(FILE* fp, char *fmt, ...)
 }
 
 /* Log printf */
-int lprintf(int level, const char *fmt, ...)
+int lprintf(int level, char *fmt, ...)
 {
 	va_list argptr;
 	char sbuf[1024];
@@ -810,7 +811,7 @@ int main(int argc, char **argv, char** environ)
 	branch.gc_interval=JAVASCRIPT_GC_INTERVAL;
 	branch.auto_terminate=TRUE;
 
-	sscanf("$Revision: 1.113 $", "%*s %s", revision);
+	sscanf("$Revision: 1.110 $", "%*s %s", revision);
 	DESCRIBE_COMPILER(compiler);
 
 	memset(&scfg,0,sizeof(scfg));
@@ -958,6 +959,9 @@ int main(int argc, char **argv, char** environ)
 
 	if(host_name==NULL)
 		host_name=scfg.sys_inetaddr;
+
+	if(!(scfg.sys_misc&SM_LOCAL_TZ))
+		putenv("TZ=UTC0");
 
 #if defined(__unix__)
 	if(daemonize) {
