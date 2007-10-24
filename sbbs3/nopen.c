a544a4847be79484f4deb2794f8fe59c5ba8d547
deuce <deuce>
2007-10-24 06:26:07 +0000

    Support headers from versions < 1.2.10
    When old headers are used, overlay mode is not guaranteed to go edge-to-edge
    on the display.

--- sbbs3/nopen.c
+++ sbbs3/nopen.c
@@ -2,7 +2,7 @@
 
 /* Network open functions (nopen and fnopen) */
 
-/* $Id: nopen.c,v 1.21 2007/05/10 01:02:14 rswindell Exp $ */
+/* $Id: nopen.c,v 1.24 2007/08/25 08:08:04 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -60,12 +60,13 @@ int nopen(const char* str, int access)
 	if(!(access&O_TEXT))
 		access|=O_BINARY;
 #endif
-    while(((file=sopen(str,access,share,S_IREAD|S_IWRITE))==-1)
+    while(((file=sopen(str,access,share,DEFFILEMODE))==-1)
         && (errno==EACCES || errno==EAGAIN || errno==EDEADLOCK) && count++<LOOP_NOPEN)
         if(count)
             mswait(100);
     return(file);
 }
+
 /****************************************************************************/
 /* This function performs an nopen, but returns a file stream with a buffer */
 /* allocated.																*/
@@ -110,14 +111,18 @@ FILE* fnopen(int* fd, const char* str, int access)
 BOOL ftouch(const char* fname)
 {
 	int file;
+	struct utimbuf ut;
 
-	if(!fexist(fname)) {	/* create the file */
-		if((file=nopen(fname,O_WRONLY|O_CREAT))<0)
-			return(FALSE);
-		close(file);
-	}
 	/* update the time stamp */
-	return utime(fname,NULL)==0;
+	ut.actime = ut.modtime = time(NULL);
+	if(utime(fname, &ut)==0)
+		return(TRUE);
+
+	/* create the file */
+	if((file=nopen(fname,O_WRONLY|O_CREAT))<0)
+		return(FALSE);
+	close(file);
+	return(TRUE);
 }
 
 BOOL fmutex(const char* fname, const char* text, long max_age)
@@ -134,7 +139,7 @@ BOOL fmutex(const char* fname, const char* text, long max_age)
 		if(remove(fname)!=0)
 			return(FALSE);
 	}
-	if((file=open(fname,O_CREAT|O_WRONLY|O_EXCL,S_IREAD|S_IWRITE))<0)
+	if((file=open(fname,O_CREAT|O_WRONLY|O_EXCL,DEFFILEMODE))<0)
 		return(FALSE);
 	if(text!=NULL)
 		write(file,text,strlen(text));
