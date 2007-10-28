2adcb5f2764e297fa5d2094292fec8e2455ce457
deuce <deuce>
2007-10-28 04:22:04 +0000

    Fix bug from last commit... one more place i was used as the entered key.

--- comio/comio_nix.c
+++ comio/comio_nix.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Serial Communications I/O Library Functions for *nix */
 
-/* $Id: comio_nix.c,v 1.5 2007/05/22 19:55:32 deuce Exp $ */
+/* $Id: comio_nix.c,v 1.8 2007/05/23 07:00:10 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -35,6 +35,7 @@
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
 
+#include <sys/ioctl.h>
 #include <sys/file.h>
 #include "comio.h"
 #include "genwrap.h"
@@ -43,7 +44,7 @@ char* comVersion(char* str, size_t len)
 {
 	char revision[16];
 
-	sscanf("$Revision: 1.5 $", "%*s %s", revision);
+	sscanf("$Revision: 1.8 $", "%*s %s", revision);
 
 	safe_snprintf(str,len,"Synchronet Communications I/O Library for "PLATFORM_DESC" v%s", revision);
 	return str;
@@ -150,26 +151,14 @@ int comGetModemStatus(COM_HANDLE handle)
 
 BOOL comRaiseDTR(COM_HANDLE handle)
 {
-	int status;
-
-	if(ioctl(handle, TIOCMGET, &status)==-1)
-		return FALSE;
-	status |= TIOCM_DTR;
-	if(ioctl(handle, TIOCMSET, &status)==-1)
-		return FALSE;
-	return TRUE;
+	int flags = TIOCM_DTR;
+	return(ioctl(handle, TIOCMBIS, &flags)==0);
 }
 
 BOOL comLowerDTR(COM_HANDLE handle)
 {
-	int status;
-
-	if(ioctl(handle, TIOCMGET, &status)==-1)
-		return FALSE;
-	status &= ~TIOCM_DTR;
-	if(ioctl(handle, TIOCMSET, &status)==-1)
-		return FALSE;
-	return TRUE;
+	int flags = TIOCM_DTR;
+	return(ioctl(handle, TIOCMBIC, &flags)==0);
 }
 
 BOOL comWriteByte(COM_HANDLE handle, BYTE ch)
