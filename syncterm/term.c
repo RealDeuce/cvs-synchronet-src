9820d50270a008922fb9ea9275de8c625a5acac9
deuce <deuce>
2008-01-06 21:41:23 +0000

    Use a sys_status bit rather than a node bit for MOFF.

--- syncterm/term.c
+++ syncterm/term.c
@@ -1,4 +1,6 @@
-/* $Id: term.c,v 1.191 2007/10/28 04:40:32 deuce Exp $ */
+/* Copyright (C), 2007 by Stephen Hurd */
+
+/* $Id: term.c,v 1.192 2007/11/13 01:37:56 deuce Exp $ */
 
 #include <genwrap.h>
 #include <ciolib.h>
