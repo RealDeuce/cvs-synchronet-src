2e58c7511d50a21c559eb65e6d3f80a215e57bb4
deuce <deuce>
2008-01-11 22:24:30 +0000

    Don't use INT_TO_JSVAL() in LAZY_*() macros

--- syncterm/uifcinit.h
+++ syncterm/uifcinit.h
@@ -1,4 +1,6 @@
-/* $Id: uifcinit.h,v 1.8 2006/09/24 05:03:27 deuce Exp $ */
+/* Copyright (C), 2007 by Stephen Hurd */
+
+/* $Id: uifcinit.h,v 1.9 2007/11/13 01:37:56 deuce Exp $ */
 
 #ifndef _UIFCINIT_H_
 #define _UIFCINIT_H_
