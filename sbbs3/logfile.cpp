3b3f0e68157e61e7a7b68ae76c01aee6084302a3
deuce <deuce>
2008-01-11 22:39:51 +0000

    Fix User class (didn't have enumerate and resolve callbacks set)

--- sbbs3/logfile.cpp
+++ sbbs3/logfile.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet log file routines */
 
-/* $Id: logfile.cpp,v 1.42 2009/01/12 02:57:44 deuce Exp $ */
+/* $Id: logfile.cpp,v 1.40 2007/08/25 08:08:03 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -110,7 +110,7 @@ extern "C" BOOL DLLCALL spamlog(scfg_t* cfg, char* prot, char* action
 	return(TRUE);
 }
 
-void sbbs_t::logentry(const char *code, const char *entry)
+void sbbs_t::logentry(char *code, char *entry)
 {
 	char str[512];
 
@@ -132,15 +132,13 @@ void sbbs_t::log(char *str)
 		fprintf(logfile_fp,"   ");
 		logcol=4; }
 	fprintf(logfile_fp,str);
-	if(str[strlen(str)-1]==LF) {
+	if(str[strlen(str)-1]==LF)
 		logcol=1;
-		fflush(logfile_fp);
-	}
 	else
 		logcol+=strlen(str);
 }
 
-bool sbbs_t::syslog(const char* code, const char *entry)
+bool sbbs_t::syslog(char* code, char *entry)
 {		
 	char	fname[MAX_PATH+1];
 	char	str[128];
@@ -168,7 +166,7 @@ bool sbbs_t::syslog(const char* code, const char *entry)
 /****************************************************************************/
 /* Writes 'str' on it's own line in node.log								*/
 /****************************************************************************/
-void sbbs_t::logline(const char *code, const char *str)
+void sbbs_t::logline(char *code, char *str)
 {
 	if(strchr(str,'\n')==NULL) {	// Keep the console log pretty
 		if(online==ON_LOCAL)
@@ -181,7 +179,6 @@ void sbbs_t::logline(const char *code, const char *str)
 		fprintf(logfile_fp,"\r\n");
 	fprintf(logfile_fp,"%-2.2s %s\r\n",code,str);
 	logcol=1;
-	fflush(logfile_fp);
 }
 
 /****************************************************************************/
@@ -345,7 +342,7 @@ void sbbs_t::errormsg(int line, const char *source, const char* action, const ch
 /*****************************************************************************/
 /* Error logging to NODE.LOG and DATA\ERROR.LOG function                     */
 /*****************************************************************************/
-void sbbs_t::errorlog(const char *text)
+void sbbs_t::errorlog(char *text)
 {
     char hdr[256],str[256],tmp2[256];
     int file;
