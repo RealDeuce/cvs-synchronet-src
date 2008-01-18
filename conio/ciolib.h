e59736bd9030d8d1ab9b4060f4ff75ce59655d2d
deuce <deuce>
2008-01-18 06:06:54 +0000

    Make File.writeBin() and File.readBin() accept/return arrays. Allows read/write of multiple binary integers at a time.

--- conio/ciolib.h
+++ conio/ciolib.h
@@ -1,4 +1,4 @@
-/* $Id: ciolib.h,v 1.51 2008/01/20 10:21:26 rswindell Exp $ */
+/* $Id: ciolib.h,v 1.48 2007/10/21 02:28:35 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -76,7 +76,6 @@ enum {
 	,CIOLIB_MODE_ANSI
 	,CIOLIB_MODE_X
 	,CIOLIB_MODE_CONIO
-	,CIOLIB_MODE_CONIO_FULLSCREEN
 	,CIOLIB_MODE_SDL
 	,CIOLIB_MODE_SDL_FULLSCREEN
 	,CIOLIB_MODE_SDL_YUV
@@ -177,16 +176,6 @@ enum text_modes
     C128_40X25,		/* Commodore 128 40x25 colour mode */
     C128_80X25,		/* Commodore 128 40x25 colour mode */
 	ATARI_40X24,	/* Atari 800 40x24 colour text mode */
-
-	/* VESA Modes */
-	VESA_132X21	= 235,
-	VESA_132X25	= 231,
-	VESA_132X28	= 228,
-	VESA_132X30	= 226,
-	VESA_132X34	= 222,
-	VESA_132X43	= 213,
-	VESA_132X50	= 206,
-	VESA_132X60	= 196,
 };
 
 #define COLOR_MODE	C80
