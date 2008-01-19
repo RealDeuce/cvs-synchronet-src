190fb4668cb8ea1b0efa9f4146d06ab9b5304e7f
deuce <deuce>
2008-01-19 01:05:33 +0000

    Check for NULL msg.summary before creating the property. Fix for problem reported by Frank Reid (and possibly others... ie: Manitu?)

--- sbbs3/js_msgbase.c
+++ sbbs3/js_msgbase.c
@@ -2,13 +2,13 @@
 
 /* Synchronet JavaScript "MsgBase" Object */
 
-/* $Id: js_msgbase.c,v 1.133 2008/01/19 20:00:26 rswindell Exp $ */
+/* $Id: js_msgbase.c,v 1.132 2008/01/19 01:05:33 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
