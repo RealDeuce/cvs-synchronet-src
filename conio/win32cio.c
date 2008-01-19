9bdff5c2c0d0d1dcdf17557e09a0ccccb74c5fe1
deuce <deuce>
2008-01-19 23:18:49 +0000

    Allow win32 console modes >=132 columns to be detected as a 132-col mode.

--- conio/win32cio.c
+++ conio/win32cio.c
@@ -1,4 +1,4 @@
-/* $Id: win32cio.c,v 1.79 2007/10/11 08:28:24 deuce Exp $ */
+/* $Id: win32cio.c,v 1.80 2008/01/19 23:18:49 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -456,6 +456,22 @@ int win32_initciolib(long inmode)
 		/* Switch to closest mode to current screen size */
 		i=sbuff.srWindow.Right-sbuff.srWindow.Left+1;
 		j=sbuff.srWindow.Bottom-sbuff.srWindow.Top+1;
+		if(i>=132) {
+			if(j<21)
+				win32_textmode(VESA_132X14);
+			else if(j<25)
+				win32_textmode(VESA_132X21);
+			else if(j<28)
+				win32_textmode(VESA_132X25);
+			else if(j<43)
+				win32_textmode(VESA_132X28);
+			else if(j<50)
+				win32_textmode(VESA_132X43);
+			else if(j<60)
+				win32_textmode(VESA_132X50);
+			else
+				win32_textmode(VESA_132X60);
+		}
 		if(i>=80) {
 			if(j<21)
 				win32_textmode(C80X14);
