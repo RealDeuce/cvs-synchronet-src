9bf4155c374be6539ec37f9f39a2cd19b8021379
deuce <deuce>
2008-01-20 10:02:39 +0000

    This apparently forced character pacing if the list had pacing set. Remove

--- syncterm/conn_telnet.c
+++ syncterm/conn_telnet.c
@@ -1,4 +1,6 @@
-/* $Id: conn_telnet.c,v 1.3 2007/05/29 06:58:35 deuce Exp $ */
+/* Copyright (C), 2007 by Stephen Hurd */
+
+/* $Id: conn_telnet.c,v 1.7 2007/11/13 01:37:56 deuce Exp $ */
 
 #include <stdlib.h>
 
@@ -31,7 +33,16 @@ void telnet_input_thread(void *args)
 	while(telnet_sock != INVALID_SOCKET && !conn_api.terminate) {
 		FD_ZERO(&rds);
 		FD_SET(telnet_sock, &rds);
+#ifdef __linux__
+		{
+			struct timeval tv;
+			tv.tv_sec=0;
+			tv.tv_usec=500000;
+			rd=select(telnet_sock+1, &rds, NULL, NULL, &tv);
+		}
+#else
 		rd=select(telnet_sock+1, &rds, NULL, NULL, NULL);
+#endif
 		if(rd==-1) {
 			if(errno==EBADF)
 				break;
@@ -70,6 +81,7 @@ void telnet_output_thread(void *args)
 	conn_api.output_thread_running=1;
 	while(telnet_sock != INVALID_SOCKET && !conn_api.terminate) {
 		pthread_mutex_lock(&(conn_outbuf.mutex));
+		ret=0;
 		wr=conn_buf_wait_bytes(&conn_outbuf, 1, 100);
 		if(wr) {
 			wr=conn_buf_get(&conn_outbuf, conn_api.wr_buf, conn_api.wr_buf_size);
@@ -79,7 +91,16 @@ void telnet_output_thread(void *args)
 			while(sent < wr) {
 				FD_ZERO(&wds);
 				FD_SET(telnet_sock, &wds);
+#ifdef __linux__
+				{
+					struct timeval tv;
+					tv.tv_sec=0;
+					tv.tv_usec=500000;
+					ret=select(telnet_sock+1, NULL, &wds, NULL, &tv);
+				}
+#else
 				ret=select(telnet_sock+1, NULL, &wds, NULL, NULL);
+#endif
 				if(ret==-1) {
 					if(errno==EBADF)
 						break;
@@ -124,9 +145,9 @@ int telnet_connect(struct bbslist *bbs)
 	conn_api.rd_buf_size=BUFFER_SIZE;
 	conn_api.wr_buf=(unsigned char *)malloc(BUFFER_SIZE);
 	if(!conn_api.wr_buf) {
+		FREE_AND_NULL(conn_api.rd_buf);
 		destroy_conn_buf(&conn_inbuf);
 		destroy_conn_buf(&conn_outbuf);
-		free(conn_api.wr_buf);
 		return(-1);
 	}
 	conn_api.wr_buf_size=BUFFER_SIZE;
