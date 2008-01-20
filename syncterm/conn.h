5851a087ba074770875da1d2087007ad2136819b
deuce <deuce>
2008-01-20 09:43:20 +0000

    Fix character pacing correctly.

--- syncterm/conn.h
+++ syncterm/conn.h
@@ -1,4 +1,6 @@
-/* $Id: conn.h,v 1.12 2007/05/14 01:05:42 deuce Exp $ */
+/* Copyright (C), 2007 by Stephen Hurd */
+
+/* $Id: conn.h,v 1.19 2008/01/20 06:53:00 rswindell Exp $ */
 
 #ifndef _CONN_H_
 #define _CONN_H_
@@ -9,15 +11,19 @@
 #include "bbslist.h"
 
 extern char *conn_types[];
-extern int conn_ports[];
+extern short unsigned int conn_ports[];
 
 enum {
 	 CONN_TYPE_UNKNOWN
 	,CONN_TYPE_RLOGIN
+	,CONN_TYPE_RLOGIN_REVERSED
 	,CONN_TYPE_TELNET
 	,CONN_TYPE_RAW
 	,CONN_TYPE_SSH
 	,CONN_TYPE_MODEM
+#ifdef __unix__
+	,CONN_TYPE_SHELL
+#endif
 	,CONN_TYPE_TERMINATOR
 };
 
@@ -42,6 +48,7 @@ struct conn_buffer {
 	size_t			bufsize;
 	size_t			buftop;
 	size_t			bufbot;
+	int				isempty;
 	pthread_mutex_t	mutex;
 	sem_t			in_sem;
 	sem_t			out_sem;
@@ -56,7 +63,7 @@ int conn_send(char *buffer, size_t buflen, unsigned int timeout);
 int conn_connect(struct bbslist *bbs);
 int conn_close(void);
 BOOL conn_connected(void);
-BOOL conn_data_waiting(void);
+size_t conn_data_waiting(void);
 void conn_binary_mode_on(void);
 void conn_binary_mode_off(void);
 
@@ -76,7 +83,7 @@ size_t conn_buf_bytes(struct conn_buffer *buf);
 size_t conn_buf_peek(struct conn_buffer *buf, unsigned char *outbuf, size_t outlen);
 size_t conn_buf_get(struct conn_buffer *buf, unsigned char *outbuf, size_t outlen);
 size_t conn_buf_put(struct conn_buffer *buf, const unsigned char *outbuf, size_t outlen);
-size_t conn_buf_wait_cond(struct conn_buffer *buf, size_t bcount, unsigned long timeout, int free);
+size_t conn_buf_wait_cond(struct conn_buffer *buf, size_t bcount, unsigned long timeout, int do_free);
 #define conn_buf_wait_bytes(buf, count, timeout)	conn_buf_wait_cond(buf, count, timeout, 0)
 #define conn_buf_wait_free(buf, count, timeout)	conn_buf_wait_cond(buf, count, timeout, 1)
 int conn_socket_connect(struct bbslist *bbs);
