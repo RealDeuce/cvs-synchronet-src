5d1ab6114546b7209b147fe52138d4f5efa5ed99
rswindell <rswindell>
2008-01-20 06:53:00 +0000

    * Changed the reversed RLogin connection to its own connection type * Use the same help buffer for redundant configuration options * Elimianted unused variables * Removed "BBS" from menu options and help since this is not a BBS-only client * TCP port is only displayed/configurable for TCP connection types * Change "Video Mode" and "Screen Setup" to "Screen Mode" - to be consistent * #ifdef'd out the configurable mouse actions (since they're not yet) * Prompt for connection type when creating a new directory entry

--- syncterm/modem.c
+++ syncterm/modem.c
@@ -1,4 +1,6 @@
-/* $Id: modem.c,v 1.7 2007/05/29 06:58:35 deuce Exp $ */
+/* Copyright (C), 2007 by Stephen Hurd */
+
+/* $Id: modem.c,v 1.11 2008/01/20 03:53:37 rswindell Exp $ */
 
 #include <stdlib.h>
 
@@ -33,7 +35,7 @@ void modem_input_thread(void *args)
 			buffered+=conn_buf_put(&conn_inbuf, conn_api.rd_buf+buffered, buffer);
 			pthread_mutex_unlock(&(conn_inbuf.mutex));
 		}
-		if(comGetModemStatus(com)&COM_DCD == 0)
+		if((comGetModemStatus(com)&COM_DCD) == 0)
 			break;
 	}
 	conn_api.input_thread_running=0;
@@ -67,7 +69,7 @@ void modem_output_thread(void *args)
 		}
 		else
 			pthread_mutex_unlock(&(conn_outbuf.mutex));
-		if(comGetModemStatus(com)&COM_DCD == 0)
+		if((comGetModemStatus(com)&COM_DCD) == 0)
 			break;
 	}
 	conn_api.output_thread_running=0;
@@ -170,7 +172,7 @@ int modem_connect(struct bbslist *bbs)
 			modem_close();
 			uifc.pop(NULL);
 			uifcmsg("No Answer",	"`No Answer`\n\n"
-							"The modem did not connect withing 30 seconds.\n");
+							"The modem did not connect within 30 seconds.\n");
 			conn_api.terminate=-1;
 			return(-1);
 		}
@@ -210,7 +212,7 @@ int modem_connect(struct bbslist *bbs)
 		modem_close();
 		destroy_conn_buf(&conn_inbuf);
 		destroy_conn_buf(&conn_outbuf);
-		free(conn_api.rd_buf);
+		FREE_AND_NULL(conn_api.rd_buf);
 		return(-1);
 	}
 	conn_api.wr_buf_size=BUFFER_SIZE;
