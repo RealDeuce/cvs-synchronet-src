1510733d500f66ff209659922c7ff30129bb57c9
deuce <deuce>
2008-01-21 01:19:22 +0000

    Restore cursor posistion before return from begin_upload()

--- syncterm/objects.mk
+++ syncterm/objects.mk
@@ -4,6 +4,7 @@ OBJS = \
                         $(MTOBJODIR)$(DIRSEP)filepick$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)fonts$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)rlogin$(OFILE) \
+                        $(MTOBJODIR)$(DIRSEP)ssh$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)telnet_io$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)modem$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)conn_telnet$(OFILE) \
@@ -16,7 +17,6 @@ OBJS = \
                         $(MTOBJODIR)$(DIRSEP)crc16$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)crc32$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)zmodem$(OFILE) \
-                        $(MTOBJODIR)$(DIRSEP)xmodem$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)st_crypt$(OFILE) \
                         $(MTOBJODIR)$(DIRSEP)syncterm$(OFILE)
 
