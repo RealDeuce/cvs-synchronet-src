bc51d4e57347867cb8eb5adb715f04bded48087c
deuce <deuce>
2008-01-21 08:20:01 +0000

    Add some suggested parentheses.

--- syncterm/conn.c
+++ syncterm/conn.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: conn.c,v 1.56 2008/04/05 07:46:47 deuce Exp $ */
+/* $Id: conn.c,v 1.54 2008/01/21 08:18:54 deuce Exp $ */
 
 #include <stdlib.h>
 
@@ -300,18 +300,9 @@ int conn_connect(struct bbslist *bbs)
 			conn_api.close=raw_close;
 			break;
 		case CONN_TYPE_SSH:
-#ifdef WITHOUT_CRYPTLIB
-			init_uifc(TRUE, TRUE);
-			uifcmsg("SSH inoperative",	"`Compiled without cryptlib`\n\n"
-					"This binary was compiled without Cryptlib,\n"
-					"which is required for SSH support."
-					);
-			return(-1);
-#else
 			conn_api.connect=ssh_connect;
 			conn_api.close=ssh_close;
 			break;
-#endif
 		case CONN_TYPE_SERIAL:
 			conn_api.connect=modem_connect;
 			conn_api.close=serial_close;
@@ -457,7 +448,7 @@ connected:
 	nonblock=0;
 	ioctlsocket(sock, FIONBIO, &nonblock);
 	if(!socket_check(sock, NULL, NULL, 0)) {
-		failcode=FAILURE_DISCONNECTED;
+		failure=FAILURE_DISCONNECTED;
 		goto connect_failed;
 	}
 
