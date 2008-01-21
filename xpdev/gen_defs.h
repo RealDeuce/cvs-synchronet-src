d222256bc99cf9030d8ade9171a94cbdf284637a
rswindell <rswindell>
2008-01-21 00:05:42 +0000

    Set zm.local_abort for locally aborted transfers (X/Ymodem or Zmodem).

--- xpdev/gen_defs.h
+++ xpdev/gen_defs.h
@@ -2,13 +2,13 @@
 
 /* General(ly useful) constant, macro, and type definitions */
 
-/* $Id: gen_defs.h,v 1.37 2007/05/31 09:09:58 deuce Exp $ */
+/* $Id: gen_defs.h,v 1.43 2007/10/21 04:09:07 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -61,6 +61,9 @@
 
 
 #include <sys/types.h>
+#ifdef HAS_INTTYPES_H
+#include <inttypes.h>
+#endif
 
 									/* Control characters */
 #ifndef STX
@@ -136,19 +139,35 @@ enum {
 	#endif
 #endif
 
+#if !defined(HAS_INTTYPES_H) && !defined(XPDEV_DONT_DEFINE_INTTYPES)
+
+typedef char	int8_t;
+typedef short	int16_t;
+typedef long	int32_t;
+typedef uchar	uint8_t;
+typedef ushort	uint16_t;
+typedef ulong	uint32_t;
+
+#endif
+
+/* Legacy 32-bit time_t */
+typedef int32_t		time32_t;
+
 /* Windows Types */
+
 #ifndef BYTE
-#define BYTE	uchar
+#define BYTE	uint8_t
 #endif
 #ifndef WORD
-#define WORD	ushort
+#define WORD	uint16_t
 #endif
 #ifndef DWORD
-#define DWORD	ulong
+#define DWORD	uint32_t
 #endif
 #ifndef BOOL
 #define BOOL	int
 #endif
+
 #ifndef TRUE
 #define TRUE	1
 #define FALSE	0
