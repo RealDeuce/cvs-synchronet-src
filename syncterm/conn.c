7739c8af4395386bf2765daefac792bba69de8d8
deuce <deuce>
2008-02-05 00:31:55 +0000

    Add xmodem.c

--- syncterm/conn.c
+++ syncterm/conn.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: conn.c,v 1.52 2008/01/21 05:50:07 rswindell Exp $ */
+/* $Id: conn.c,v 1.55 2008/01/21 08:22:39 deuce Exp $ */
 
 #include <stdlib.h>
 
@@ -317,6 +317,8 @@ int conn_connect(struct bbslist *bbs)
 			conn_api.close=pty_close;
 			break;
 #endif
+		default:
+			conn_api.terminate=1;
 	}
 	if(conn_api.connect) {
 		if(conn_api.connect(bbs)) {
@@ -355,6 +357,7 @@ enum failure_reason {
 	,FAILURE_CONNECT_ERROR
 	,FAILURE_ABORTED
 	,FAILURE_GENERAL
+	,FAILURE_DISCONNECTED
 };
 
 int conn_socket_connect(struct bbslist *bbs)
@@ -444,8 +447,10 @@ int conn_socket_connect(struct bbslist *bbs)
 connected:
 	nonblock=0;
 	ioctlsocket(sock, FIONBIO, &nonblock);
-	if(!socket_check(sock, NULL, NULL, 0))
+	if(!socket_check(sock, NULL, NULL, 0)) {
+		failcode=FAILURE_DISCONNECTED;
 		goto connect_failed;
+	}
 
 	uifc.pop(NULL);
 	return(sock);
@@ -486,6 +491,12 @@ connect_failed:
 				uifcmsg(str
 								,"`SyncTERM failed to connect`\n\n"
 								 "The call to select() returned an unexpected error code.");
+				break;
+			case FAILURE_DISCONNECTED:
+				sprintf(str,"Connect error (%d)!",ERROR_VALUE);
+				uifcmsg(str
+								,"`SyncTERM failed to connect`\n\n"
+								 "After connect() succeeded, the socket was in a disconnected state.");
 				break;
 		}
 		conn_close();
