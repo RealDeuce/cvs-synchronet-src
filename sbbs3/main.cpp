9021256ad6bc321fbb1315374b77eb116b253c5f
deuce <deuce>
2008-02-06 03:44:43 +0000

    Add ECMA 048 escape codes that SyncTERM generates.

--- sbbs3/main.cpp
+++ sbbs3/main.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet main/telnet server thread and related functions */
 
-/* $Id: main.cpp,v 1.493 2008/02/23 10:47:26 rswindell Exp $ */
+/* $Id: main.cpp,v 1.490 2008/01/11 08:45:52 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -38,7 +38,6 @@
 #include "sbbs.h"
 #include "ident.h"
 #include "telnet.h" 
-#include "netwrap.h"
 
 #ifdef __unix__
 	#include <sys/un.h>
@@ -511,20 +510,16 @@ DLLCALL js_SyncResolve(JSContext* cx, JSObject* obj, char *name, jsSyncPropertyS
 {
 	JSBool	ret=JS_TRUE;
 
-	if(props) {
+	if(props)
 		if(!js_DefineSyncProperties(cx, obj, props))
 			ret=JS_FALSE;
-	}
 		
-	if(funcs) {
-		if(!js_DefineSyncMethods(cx, obj, funcs, 0))
+	if(funcs)
+		if(!js_DefineSyncMethods(cx, obj, funcs))
 			ret=JS_FALSE;
-	}
 
-	if(consts) {
-		if(!js_DefineConstIntegers(cx, obj, consts, flags))
-			ret=JS_FALSE;
-	}
+	if(consts)
+		if(!js_DefineConstIntegers(JSContext* cx, JSObject* obj, consts, flags)
 
 	return(ret);
 }
@@ -4290,7 +4285,7 @@ void DLLCALL bbs_thread(void* arg)
 
 	t=time(NULL);
 	lprintf(LOG_INFO,"Initializing on %.24s with options: %lx"
-		,ctime_r(&t,str),startup->options);
+		,CTIME_R(&t,str),startup->options);
 
 	if(chdir(startup->ctrl_dir)!=0)
 		lprintf(LOG_ERR,"!ERROR %d changing directory to: %s", errno, startup->ctrl_dir);
@@ -5117,7 +5112,7 @@ NO_SSH:
 			/*****************************/
     		memset(&tmp_addr, 0, sizeof(tmp_addr));
 
-			tmp_addr.sin_addr.s_addr = htonl(IPv4_LOCALHOST);
+			tmp_addr.sin_addr.s_addr = htonl(0x7f000001U);
     		tmp_addr.sin_family = AF_INET;
     		tmp_addr.sin_port   = 0;
 
