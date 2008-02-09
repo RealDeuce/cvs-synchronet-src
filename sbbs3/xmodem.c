aa00dd4d71f384dee057ba26580538b12d99bbc4
deuce <deuce>
2008-02-09 04:19:34 +0000

    Fix short read test behaviour on the last block.

--- sbbs3/xmodem.c
+++ sbbs3/xmodem.c
@@ -2,7 +2,7 @@
 
 /* Synchronet X/YMODEM Functions */
 
-/* $Id: xmodem.c,v 1.30 2008/02/09 04:05:09 deuce Exp $ */
+/* $Id: xmodem.c,v 1.31 2008/02/09 04:19:34 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -490,14 +490,14 @@ BOOL xmodem_send_file(xmodem_t* xm, const char* fname, FILE* fp, time_t* start, 
 			memset(block,CPMEOF,xm->block_size);
 			if(!sent_header) {
 				if(xm->block_size>128) {
-					if((sent_bytes+xm->block_size) > st.st_size) {
+					if((long)(sent_bytes+xm->block_size) > st.st_size) {
 						lprintf(xm,LOG_INFO,"Falling back to 128 byte blocks for end of file");
 						xm->block_size=128;
 					}
 				}
 			}
 			if((rd=fread(block,1,xm->block_size,fp))!=xm->block_size 
-				&& (long)(sent_bytes + xm->block_size) <= st.st_size) {
+				&& (long)(sent_bytes + rd) != st.st_size) {
 				lprintf(xm,LOG_ERR,"READ ERROR %d instead of %d at offset %lu"
 					,rd,xm->block_size,sent_bytes);
 				xm->errors++;
@@ -549,7 +549,7 @@ const char* xmodem_source(void)
 
 char* xmodem_ver(char *buf)
 {
-	sscanf("$Revision: 1.30 $", "%*s %s", buf);
+	sscanf("$Revision: 1.31 $", "%*s %s", buf);
 
 	return(buf);
 }
