addcf952e510e6b6e2b09a5c4d8fdd233e31a8c0
deuce <deuce>
2008-02-09 05:31:06 +0000

    Add a NOT_YMODEM return code. Indicates that block zero was expected, but block one was recieved.

--- syncterm/uifcinit.c
+++ syncterm/uifcinit.c
@@ -1,4 +1,6 @@
-/* $Id: uifcinit.c,v 1.25 2007/05/27 06:30:19 deuce Exp $ */
+/* Copyright (C), 2007 by Stephen Hurd */
+
+/* $Id: uifcinit.c,v 1.27 2008/01/24 02:45:39 deuce Exp $ */
 
 #include <gen_defs.h>
 #include <stdio.h>
@@ -22,6 +24,7 @@ static void (*bottomfunc)(int);
 int	init_uifc(BOOL scrn, BOOL bottom) {
 	int	i;
 	struct	text_info txtinfo;
+	char	top[80];
 
     gettextinfo(&txtinfo);
 	if(!uifc_initialized) {
@@ -40,7 +43,8 @@ int	init_uifc(BOOL scrn, BOOL bottom) {
 	}
 
 	if(scrn) {
-		if(uifc.scrn(syncterm_version)) {
+		sprintf(top, "%.40s - %.30s", syncterm_version, output_descrs[cio_api.mode]);
+		if(uifc.scrn(top)) {
 			printf(" USCRN (len=%d) failed!\n",uifc.scrn_len+1);
 			uifc_initialized=0;
 			uifc.bail();
