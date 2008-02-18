76843da01c74b15864c15ab2257e66d8555296fe
deuce <deuce>
2008-02-18 01:55:20 +0000

    Fix bug which prevented dynamic font changes from workin in a UIFC menu.

--- syncterm/bbslist.h
+++ syncterm/bbslist.h
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: bbslist.h,v 1.42 2008/01/28 21:47:01 deuce Exp $ */
+/* $Id: bbslist.h,v 1.45 2008/02/10 06:30:30 rswindell Exp $ */
 
 #ifndef _BBSLIST_H_
 #define _BBSLIST_H_
@@ -49,6 +49,7 @@ enum {
 	,SCREEN_MODE_C128_40
 	,SCREEN_MODE_C128_80
 	,SCREEN_MODE_ATARI
+	,SCREEN_MODE_ATARI_XEP80
 	,SCREEN_MODE_TERMINATOR
 };
 
@@ -71,6 +72,7 @@ struct bbslist {
 	char			dldir[MAX_PATH+1];
 	char			uldir[MAX_PATH+1];
 	char			logfile[MAX_PATH+1];
+	BOOL			append_logfile;
 	int				xfer_loglevel;
 	int				telnet_loglevel;
 	int				bpsrate;
@@ -78,7 +80,7 @@ struct bbslist {
 	char			font[80];
 };
 
-struct bbslist *show_bbslist(int id, int connected);
+struct bbslist *show_bbslist(char *current, int connected);
 extern char *log_levels[];
 extern char *rate_names[];
 extern int rates[];
