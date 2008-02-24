9b6dc9125c4d1c56bc9ea13e87373c0e6931aed8
rswindell <rswindell>
2008-02-24 07:53:33 +0000

    Fixed iniGetSection() (was returning the section header in the string list). Created iniPopKey(), a combination of iniGetString() and iniRemoveKey().

--- xpdev/targets.mk
+++ xpdev/targets.mk
@@ -2,11 +2,12 @@
 
 # Make 'include file' defining targets for xpdel wrappers
 
-# $Id: targets.mk,v 1.10 2005/09/28 19:44:11 deuce Exp $
+# $Id: targets.mk,v 1.13 2008/02/23 10:59:48 rswindell Exp $
 
 # ODIR, DIRSEP, LIBFILE, EXEFILE, and DELETE must be pre-defined
 
 WRAPTEST		= $(EXEODIR)$(DIRSEP)wraptest$(EXEFILE)
+XPTIME			= $(EXEODIR)$(DIRSEP)xptime$(EXEFILE)
 XPDEV_LIB_BUILD	= $(LIBODIR)$(DIRSEP)$(LIBPREFIX)xpdev$(LIBFILE)
 XPDEV-MT_LIB_BUILD	= $(LIBODIR)$(DIRSEP)$(LIBPREFIX)xpdev_mt$(LIBFILE)
 XPDEV_SHLIB_BUILD	= $(LIBODIR)$(DIRSEP)$(LIBPREFIX)xpdev$(SOFILE)
@@ -14,7 +15,7 @@ XPDEV-MT_SHLIB_BUILD	= $(LIBODIR)$(DIRSEP)$(LIBPREFIX)xpdev_mt$(SOFILE)
 
 all: lib mtlib
 
-tests: $(WRAPTEST)
+tests: $(MTOBJODIR) $(LIBODIR) $(EXEODIR) $(WRAPTEST)
 
 dl: dl-lib dl-mtlib
 
@@ -25,5 +26,7 @@ dl-lib: $(OBJODIR) $(LIBODIR) $(XPDEV_SHLIB_BUILD)
 mtlib:	$(MTOBJODIR) $(LIBODIR) $(XPDEV-MT_LIB_BUILD)
 
 dl-mtlib: $(MTOBJODIR) $(LIBODIR) $(XPDEV-MT_SHLIB_BUILD)
+
+xptime: $(OBJODIR) $(LIBODIR) $(EXEODIR) $(XPTIME)
 
 $(WRAPTEST): $(XPDEV-MT_LIB_BUILD)
