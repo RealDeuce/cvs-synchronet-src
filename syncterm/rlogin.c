d7373ed07a8f1883523f1c08b4393707c23e1c19
rswindell <rswindell>
2008-03-02 23:15:43 +0000

    If a second SENDER header field is added or read and the first SENDER value is an empty string, use the second SENDER for the "from" convenience pointer.

--- syncterm/rlogin.c
+++ syncterm/rlogin.c
@@ -1,4 +1,6 @@
-/* $Id: rlogin.c,v 1.25 2007/05/29 06:58:35 deuce Exp $ */
+/* Copyright (C), 2007 by Stephen Hurd */
+
+/* $Id: rlogin.c,v 1.30 2008/01/20 06:53:00 rswindell Exp $ */
 
 #include <stdlib.h>
 
@@ -24,7 +26,16 @@ void rlogin_input_thread(void *args)
 	while(sock != INVALID_SOCKET && !conn_api.terminate) {
 		FD_ZERO(&rds);
 		FD_SET(sock, &rds);
+#ifdef __linux__
+		{
+			struct timeval tv;
+			tv.tv_sec=0;
+			tv.tv_usec=500000;
+			rd=select(sock+1, &rds, NULL, NULL, &tv);
+		}
+#else
 		rd=select(sock+1, &rds, NULL, NULL, NULL);
+#endif
 		if(rd==-1) {
 			if(errno==EBADF)
 				break;
@@ -59,6 +70,7 @@ void rlogin_output_thread(void *args)
 	conn_api.output_thread_running=1;
 	while(sock != INVALID_SOCKET && !conn_api.terminate) {
 		pthread_mutex_lock(&(conn_outbuf.mutex));
+		ret=0;
 		wr=conn_buf_wait_bytes(&conn_outbuf, 1, 100);
 		if(wr) {
 			wr=conn_buf_get(&conn_outbuf, conn_api.wr_buf, conn_api.wr_buf_size);
@@ -67,7 +79,16 @@ void rlogin_output_thread(void *args)
 			while(sent < wr) {
 				FD_ZERO(&wds);
 				FD_SET(sock, &wds);
+#ifdef __linux__
+				{
+					struct timeval tv;
+					tv.tv_sec=0;
+					tv.tv_usec=500000;
+					ret=select(sock+1, NULL, &wds, NULL, &tv);
+				}
+#else
 				ret=select(sock+1, NULL, &wds, NULL, NULL);
+#endif
 				if(ret==-1) {
 					if(errno==EBADF)
 						break;
@@ -98,7 +119,7 @@ int rlogin_connect(struct bbslist *bbs)
 
 	ruser=bbs->user;
 	passwd=bbs->password;
-	if(bbs->conn_type==CONN_TYPE_RLOGIN && bbs->reversed) {
+	if(bbs->conn_type==CONN_TYPE_RLOGIN_REVERSED) {
 		passwd=bbs->user;
 		ruser=bbs->password;
 	}
@@ -120,14 +141,14 @@ int rlogin_connect(struct bbslist *bbs)
 	}
 	conn_api.rd_buf_size=BUFFER_SIZE;
 	if(!(conn_api.wr_buf=(unsigned char *)malloc(BUFFER_SIZE))) {
-		free(conn_api.rd_buf);
+		FREE_AND_NULL(conn_api.rd_buf);
 		destroy_conn_buf(&conn_inbuf);
 		destroy_conn_buf(&conn_outbuf);
 		return(-1);
 	}
 	conn_api.wr_buf_size=BUFFER_SIZE;
 
-	if(bbs->conn_type == CONN_TYPE_RLOGIN) {
+	if(bbs->conn_type == CONN_TYPE_RLOGIN || bbs->conn_type == CONN_TYPE_RLOGIN_REVERSED) {
 		conn_send("",1,1000);
 		conn_send(passwd,strlen(passwd)+1,1000);
 		conn_send(ruser,strlen(ruser)+1,1000);
