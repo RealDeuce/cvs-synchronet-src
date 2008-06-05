c65a812768af0810f44bffdb5bd7dcdce42d23d6
rswindell <rswindell>
2008-06-05 05:25:06 +0000

    Fixed compile warning.

--- sbbs3/jsexec.c
+++ sbbs3/jsexec.c
@@ -2,7 +2,7 @@
 
 /* Execute a Synchronet JavaScript module from the command-line */
 
-/* $Id: jsexec.c,v 1.110 2007/08/25 08:30:52 rswindell Exp $ */
+/* $Id: jsexec.c,v 1.113 2008/06/04 04:38:47 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -43,9 +43,8 @@
 #include <signal.h>
 #endif
 
-#include "ciolib.h"
-
 #include "sbbs.h"
+#include "ciolib.h"
 
 #define DEFAULT_LOG_LEVEL	LOG_DEBUG	/* Display all LOG levels */
 #define DEFAULT_ERR_LOG_LVL	LOG_WARNING
@@ -160,7 +159,7 @@ int mfprintf(FILE* fp, char *fmt, ...)
 }
 
 /* Log printf */
-int lprintf(int level, char *fmt, ...)
+int lprintf(int level, const char *fmt, ...)
 {
 	va_list argptr;
 	char sbuf[1024];
@@ -811,7 +810,7 @@ int main(int argc, char **argv, char** environ)
 	branch.gc_interval=JAVASCRIPT_GC_INTERVAL;
 	branch.auto_terminate=TRUE;
 
-	sscanf("$Revision: 1.110 $", "%*s %s", revision);
+	sscanf("$Revision: 1.113 $", "%*s %s", revision);
 	DESCRIBE_COMPILER(compiler);
 
 	memset(&scfg,0,sizeof(scfg));
@@ -959,9 +958,6 @@ int main(int argc, char **argv, char** environ)
 
 	if(host_name==NULL)
 		host_name=scfg.sys_inetaddr;
-
-	if(!(scfg.sys_misc&SM_LOCAL_TZ))
-		putenv("TZ=UTC0");
 
 #if defined(__unix__)
 	if(daemonize) {
--- syncterm/st_crypt.c
+++ syncterm/st_crypt.c
@@ -1,3 +1,5 @@
+/* Copyright (C), 2007 by Stephen Hurd */
+
 #ifndef _WIN32
  #include <dlfcn.h>
 #endif
@@ -5,12 +7,26 @@
 
 #include "st_crypt.h"
 
-struct crypt_funcs cl;
 int crypt_loaded=0;
+
+#ifdef WITHOUT_CRYPTLIB
+
+int init_crypt()
+{
+	return(-1);
+}
+
+void exit_crypt()
+{
+}
+
+#else
+
+struct crypt_funcs cl;
 
 int init_crypt(void)
 {
-#ifdef STATIC_LINK
+#ifdef STATIC_CRYPTLIB
 	cl.PopData=cryptPopData;
 	cl.PushData=cryptPushData;
 	cl.FlushData=cryptFlushData;
@@ -157,3 +173,5 @@ void exit_crypt(void)
 	if(crypt_loaded)
 		cl.End();
 }
+
+#endif
