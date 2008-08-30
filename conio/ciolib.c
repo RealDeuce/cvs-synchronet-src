198a86319bb9fa2a4079354190245615e6ba2a48
rswindell <rswindell>
2008-08-30 19:03:50 +0000

    Add js_conio to jsexec msvc6 project.

--- conio/ciolib.c
+++ conio/ciolib.c
@@ -1,4 +1,4 @@
-/* $Id: ciolib.c,v 1.97 2007/10/23 00:21:41 deuce Exp $ */
+/* $Id: ciolib.c,v 1.104 2008/02/03 11:34:08 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -128,6 +128,9 @@ int try_sdl_init(int mode)
 		cio_api.setfont=bitmap_setfont;
 		cio_api.getfont=bitmap_getfont;
 		cio_api.loadfont=bitmap_loadfont;
+		cio_api.movetext=bitmap_movetext;
+		cio_api.clreol=bitmap_clreol;
+		cio_api.clrscr=bitmap_clrscr;
 
 		cio_api.kbhit=sdl_kbhit;
 		cio_api.getch=sdl_getch;
@@ -166,6 +169,9 @@ int try_x_init(int mode)
 		cio_api.getfont=bitmap_getfont;
 		cio_api.loadfont=bitmap_loadfont;
 		cio_api.beep=x_beep;
+		cio_api.movetext=bitmap_movetext;
+		cio_api.clreol=bitmap_clreol;
+		cio_api.clrscr=bitmap_clrscr;
 
 		cio_api.kbhit=x_kbhit;
 		cio_api.getch=x_getch;
@@ -184,7 +190,9 @@ int try_x_init(int mode)
 int try_curses_init(int mode)
 {
 	if(curs_initciolib(mode)) {
-		cio_api.mode=CIOLIB_MODE_CURSES_IBM;
+		if(mode==CIOLIB_MODE_AUTO)
+			mode=CIOLIB_MODE_CURSES;
+		cio_api.mode=mode;
 		cio_api.puttext=curs_puttext;
 		cio_api.gettext=curs_gettext;
 		cio_api.textattr=curs_textattr;
@@ -235,7 +243,10 @@ int try_conio_init(int mode)
 {
 	/* This should test for something or other */
 	if(win32_initciolib(mode)) {
-		cio_api.mode=CIOLIB_MODE_CONIO;
+		if(mode==CIOLIB_MODE_AUTO)
+			cio_api.mode=CIOLIB_MODE_CONIO;
+		else
+			cio_api.mode=mode;	/* CIOLIB_MODE_CONIO or CIOLIB_MODE_CONIO_FULLSCREEN */
 		cio_api.mouse=1;
 		cio_api.puttext=win32_puttext;
 		cio_api.gettext=win32_gettext;
@@ -285,9 +296,6 @@ CIOLIBEXPORT int CIOLIBCALL initciolib(int mode)
 	switch(mode) {
 		case CIOLIB_MODE_AUTO:
 #if defined(WITH_SDL) || defined(WITH_SDL_AUDIO)
-#if (defined(__MACH__) && defined(__APPLE__))
-			if(!try_sdl_init(CIOLIB_MODE_SDL_YUV))
-#endif
 			if(!try_sdl_init(CIOLIB_MODE_SDL))
 #endif
 #ifdef _WIN32
@@ -302,6 +310,7 @@ CIOLIBEXPORT int CIOLIBCALL initciolib(int mode)
 			break;
 #ifdef _WIN32
 		case CIOLIB_MODE_CONIO:
+		case CIOLIB_MODE_CONIO_FULLSCREEN:
 			try_conio_init(mode);
 			break;
 #else
@@ -590,20 +599,8 @@ CIOLIBEXPORT void CIOLIBCALL ciolib_gettextinfo(struct text_info *info)
 		return;
 	}
 	
-	if(info!=&cio_textinfo) {
-		info->winleft=cio_textinfo.winleft;        /* left window coordinate */
-		info->wintop=cio_textinfo.wintop;         /* top window coordinate */
-		info->winright=cio_textinfo.winright;       /* right window coordinate */
-		info->winbottom=cio_textinfo.winbottom;      /* bottom window coordinate */
-		info->attribute=cio_textinfo.attribute;      /* text attribute */
-		info->normattr=cio_textinfo.normattr;       /* normal attribute */
-		info->currmode=cio_textinfo.currmode;       /* current video mode:
-                               			 BW40, BW80, C40, C80, or C4350 */
-		info->screenheight=cio_textinfo.screenheight;   /* text screen's height */
-		info->screenwidth=cio_textinfo.screenwidth;    /* text screen's width */
-		info->curx=cio_textinfo.curx;           /* x-coordinate in current window */
-		info->cury=cio_textinfo.cury;           /* y-coordinate in current window */
-	}
+	if(info!=&cio_textinfo)
+		*info=cio_textinfo;
 }
 
 /* Optional */
@@ -743,7 +740,7 @@ CIOLIBEXPORT void CIOLIBCALL ciolib_clreol(void)
 	int width,height;
 
 	CIOLIB_INIT();
-	
+
 	if(cio_api.clreol) {
 		cio_api.clreol();
 		return;
@@ -835,7 +832,9 @@ CIOLIBEXPORT int CIOLIBCALL ciolib_cprintf(char *fmat, ...)
 	char	str[16384];
 #else
 	char	*str;
+#ifndef HAVE_VASPRINTF
 	va_list argptr2;
+#endif
 #endif
 
 	CIOLIB_INIT();
