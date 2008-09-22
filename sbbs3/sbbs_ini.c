bd66961952b02e163cb7ee9ca4d309913293dc2e
deuce <deuce>
2008-09-22 01:28:15 +0000

    Add OOII stuff to the Borland makefile.

--- sbbs3/sbbs_ini.c
+++ sbbs3/sbbs_ini.c
@@ -2,13 +2,13 @@
 
 /* Synchronet initialization (.ini) file routines */
 
-/* $Id: sbbs_ini.c,v 1.123 2007/10/15 07:35:44 deuce Exp $ */
+/* $Id: sbbs_ini.c,v 1.125 2008/01/07 07:04:44 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -436,6 +436,8 @@ void sbbs_read_ini(
 			=iniGetIpAddress(list,section,strInterface,global->interface_addr);
 		mail->smtp_port
 			=iniGetShortInt(list,section,"SMTPPort",IPPORT_SMTP);
+		mail->submission_port
+			=iniGetShortInt(list,section,"SubmissionPort",IPPORT_SUBMISSION);
 		mail->pop3_port
 			=iniGetShortInt(list,section,"POP3Port",IPPORT_POP3);
 		mail->relay_port
@@ -576,6 +578,8 @@ void sbbs_read_ini(
 			,iniGetString(list,section,"ErrorDirectory",WEB_DEFAULT_ERROR_DIR,value));
 		SAFECOPY(web->cgi_dir
 			,iniGetString(list,section,"CGIDirectory",WEB_DEFAULT_CGI_DIR,value));
+		SAFECOPY(web->default_auth_list
+			,iniGetString(list,section,"Authentication",WEB_DEFAULT_AUTH_LIST,value));
 		SAFECOPY(web->logfile_base
 			,iniGetString(list,section,"HttpLogFile",nulstr,value));
 
@@ -1089,6 +1093,8 @@ BOOL sbbs_write_ini(
 		if(!iniSetString(lp,section,"ErrorDirectory",web->error_dir,&style))
 			break;
 		if(!iniSetString(lp,section,"CGIDirectory",web->cgi_dir,&style))
+			break;
+		if(!iniSetString(lp,section,"Authentication",web->default_auth_list,&style))
 			break;
 		if(!iniSetString(lp,section,"HttpLogFile",web->logfile_base,&style))
 			break;
