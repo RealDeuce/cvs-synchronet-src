75ad6f0681c1702d6045f1ce3f1b373125bd0e12
deuce <deuce>
2008-09-23 06:42:46 +0000

    Add a duplicate_filename() callback called when the file exists and the CRC is different.  If it returns true, it is assumed the current_file_name has changed.

--- syncterm/ssh.c
+++ syncterm/ssh.c
@@ -1,4 +1,6 @@
-/* $Id: ssh.c,v 1.6 2007/10/21 18:13:45 deuce Exp $ */
+/* Copyright (C), 2007 by Stephen Hurd */
+
+/* $Id: ssh.c,v 1.8 2008/01/19 21:08:44 deuce Exp $ */
 
 #include <stdlib.h>
 
@@ -119,6 +121,7 @@ int ssh_connect(struct bbslist *bbs)
 	int off=1;
 	int status;
 	char password[MAX_PASSWD_LEN+1];
+	char username[MAX_USER_LEN+1];
 
 	init_uifc(TRUE, TRUE);
 
@@ -158,11 +161,16 @@ int ssh_connect(struct bbslist *bbs)
 	setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, ( char * )&off, sizeof ( off ) );
 
 	SAFECOPY(password,bbs->password);
+	SAFECOPY(username,bbs->user);
 
 	uifc.pop(NULL);
+
+	if(!username[0])
+		uifcinput("UserID",MAX_USER_LEN,username,0,"No stored UserID.");
+
 	uifc.pop("Setting Username");
 	/* Add username/password */
-	status=cl.SetAttributeString(ssh_session, CRYPT_SESSINFO_USERNAME, bbs->user, strlen(bbs->user));
+	status=cl.SetAttributeString(ssh_session, CRYPT_SESSINFO_USERNAME, username, strlen(username));
 	if(cryptStatusError(status)) {
 		char	str[1024];
 		sprintf(str,"Error %d setting username",status);
