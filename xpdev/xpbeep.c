cac0c818b7aa696717f6668a36c2530687432f6c
deuce <deuce>
2008-09-23 20:00:35 +0000

    Fix Win32 single-thread xp_play_sample implementation

--- xpdev/xpbeep.c
+++ xpdev/xpbeep.c
@@ -1,4 +1,4 @@
-/* $Id: xpbeep.c,v 1.54 2008/09/22 21:41:50 deuce Exp $ */
+/* $Id: xpbeep.c,v 1.55 2008/09/23 20:00:35 deuce Exp $ */
 
 /* TODO: USE PORTAUDIO! */
 
@@ -665,7 +665,7 @@ BOOL DLLCALL xp_play_sample(const unsigned char *sample, size_t sample_size, BOO
 	if(handle_type==SOUND_DEVICE_WIN32) {
 		wh.lpData=sample;
 		wh.dwBufferLength=sample_size;
-		wh.lpData=sample_buffer;
+		wh.lpData=sample;
 		wh.dwBufferLength=sample_size;
 		if(waveOutPrepareHeader(waveOut, &wh, sizeof(wh))==MMSYSERR_NOERROR) {
 			if(waveOutWrite(waveOut, &wh, sizeof(wh))==MMSYSERR_NOERROR) {
