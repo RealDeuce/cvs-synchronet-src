2972d376754043dd96e67017a3e3b649484e6ab5
deuce <deuce>
2008-09-24 01:18:59 +0000

    Work around possible NULL dereference if the mode is not initialized within .5 seconds of initialization.  This may fix the reported crashes on OS X at startup using SDL mode.

--- conio/bitmap_con.c
+++ conio/bitmap_con.c
@@ -1,4 +1,4 @@
-/* $Id: bitmap_con.c,v 1.23 2008/02/23 06:23:18 deuce Exp $ */
+/* $Id: bitmap_con.c,v 1.24 2008/09/24 01:18:59 deuce Exp $ */
 
 #include <stdarg.h>
 #include <stdio.h>		/* NULL */
@@ -25,7 +25,7 @@
 #include "allfonts.h"
 #include "bitmap_con.h"
 
-static char *screen;
+static char *screen=NULL;
 int screenwidth;
 int screenheight;
 #define PIXEL_OFFSET(x,y)	( (y)*screenwidth+(x) )
@@ -62,7 +62,9 @@ static void blinker_thread(void *data)
 	int count=0;
 
 	while(1) {
-		SLEEP(10);
+		do {
+			SLEEP(10);
+		} while(screen==NULL);
 		count++;
 		pthread_mutex_lock(&vstatlock);
 		if(count==50) {
