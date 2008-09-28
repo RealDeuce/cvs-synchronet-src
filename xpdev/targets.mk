558ac449c470dbbdbf3e692d2da0f5784e50e7dc
deuce <deuce>
2008-09-28 02:00:08 +0000

    More xp_dl updates... actually use it everywhere that uses run-time linking (exept Win32-specific stuff)

--- xpdev/targets.mk
+++ xpdev/targets.mk
@@ -2,11 +2,12 @@
 
 # Make 'include file' defining targets for xpdel wrappers
 
-# $Id: targets.mk,v 1.12 2006/04/20 20:54:39 deuce Exp $
+# $Id: targets.mk,v 1.13 2008/02/23 10:59:48 rswindell Exp $
 
 # ODIR, DIRSEP, LIBFILE, EXEFILE, and DELETE must be pre-defined
 
 WRAPTEST		= $(EXEODIR)$(DIRSEP)wraptest$(EXEFILE)
+XPTIME			= $(EXEODIR)$(DIRSEP)xptime$(EXEFILE)
 XPDEV_LIB_BUILD	= $(LIBODIR)$(DIRSEP)$(LIBPREFIX)xpdev$(LIBFILE)
 XPDEV-MT_LIB_BUILD	= $(LIBODIR)$(DIRSEP)$(LIBPREFIX)xpdev_mt$(LIBFILE)
 XPDEV_SHLIB_BUILD	= $(LIBODIR)$(DIRSEP)$(LIBPREFIX)xpdev$(SOFILE)
@@ -25,5 +26,7 @@ dl-lib: $(OBJODIR) $(LIBODIR) $(XPDEV_SHLIB_BUILD)
 mtlib:	$(MTOBJODIR) $(LIBODIR) $(XPDEV-MT_LIB_BUILD)
 
 dl-mtlib: $(MTOBJODIR) $(LIBODIR) $(XPDEV-MT_SHLIB_BUILD)
+
+xptime: $(OBJODIR) $(LIBODIR) $(EXEODIR) $(XPTIME)
 
 $(WRAPTEST): $(XPDEV-MT_LIB_BUILD)
