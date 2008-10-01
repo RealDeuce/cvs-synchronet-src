ce05bb24806e73ad9be2d97ea326451085884ee9
rswindell <rswindell>
2008-10-01 01:46:10 +0000

    Create a log entry every time a user edits a text file (using internal editor).

--- sbbs3/objects.mk
+++ sbbs3/objects.mk
@@ -2,7 +2,7 @@
 
 # Make 'include file' listing object files for SBBS.DLL
 
-# $Id: objects.mk,v 1.46 2006/03/14 09:33:29 rswindell Exp $(DIRSEP)09$(DIRSEP)08 07:23:54 deuce Exp $
+# $Id: objects.mk,v 1.47 2008/08/20 04:45:20 deuce Exp $(DIRSEP)09$(DIRSEP)08 07:23:54 deuce Exp $
 
 # OBJODIR, SLASH, and OFILE must be pre-defined
 
@@ -223,7 +223,8 @@ MAKEUSER_OBJS = \
 
 JSEXEC_OBJS = \
 			$(MTOBJODIR)$(DIRSEP)jsexec$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_uifc$(OFILE)
+			$(MTOBJODIR)$(DIRSEP)js_uifc$(OFILE) \
+			$(MTOBJODIR)$(DIRSEP)js_conio$(OFILE)
 
 SEXYZ_OBJS = \
 			$(MTOBJODIR)$(DIRSEP)sexyz$(OFILE) \
