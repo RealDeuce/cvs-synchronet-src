de9fc85b6c3d55953f2a80a23bbf805932ede632
rswindell <rswindell>
2009-01-10 08:26:55 +0000

    Bug-fix in getage() for users born after 1999 (Eliza's the first?).

--- syncterm/menu.c
+++ syncterm/menu.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: menu.c,v 1.41 2007/11/13 01:37:56 deuce Exp $ */
+/* $Id: menu.c,v 1.47 2008/09/25 02:31:24 deuce Exp $ */
 
 #include <genwrap.h>
 #include <uifc.h>
@@ -127,25 +127,29 @@ void viewscroll(void)
 	}
 	puttext(1,1,txtinfo.screenwidth,txtinfo.screenheight,scrollback+(cterm.backpos)*cterm.width*2);
 	gotoxy(x,y);
-	FREE_AND_NULL(scrollback);
+	free(scrollback);
 	return;
 }
 
 int syncmenu(struct bbslist *bbs, int *speed)
 {
 	char	*opts[]={
-						 "Scrollback (Alt-B)"
+						 "Scrollback ("ALT_KEY_NAMEP"-B)"
 						,"Disconnect (Ctrl-Q)"
-						,"Send Login (Alt-L)"
-						,"Upload (Alt-U)"
-						,"Download (Alt-D)"
-						,"Change Output Rate (Alt-Up/Alt-Down)"
+						,"Send Login ("ALT_KEY_NAMEP"-L)"
+						,"Upload ("ALT_KEY_NAMEP"-U)"
+						,"Download ("ALT_KEY_NAMEP"-D)"
+						,"Change Output Rate ("ALT_KEY_NAMEP"-Up/"ALT_KEY_NAMEP"-Down)"
 						,"Change Log Level"
-						,"Capture Control (Alt-C)"
-						,"ANSI Music Control (Alt-M)"
-						,"Font Control (Alt-F)"
+						,"Capture Control ("ALT_KEY_NAMEP"-C)"
+						,"ANSI Music Control ("ALT_KEY_NAMEP"-M)"
+						,"Font Control ("ALT_KEY_NAMEP"-F)"
 						,"Toggle Doorway Mode"
-						,"Exit (Alt-X)"
+#ifndef WITHOUT_OOII
+						,"Toggle Operation Overkill ][ Mode"
+#endif
+						,"Exit ("ALT_KEY_NAMEP"-X)"
+						,"Edit Dialing Directory ("ALT_KEY_NAMEP"-E)"
 						,""};
 	int		opt=0;
 	int		i,j;
@@ -160,26 +164,27 @@ int syncmenu(struct bbslist *bbs, int *speed)
 	if(cio_api.mode!=CIOLIB_MODE_CURSES
 			&& cio_api.mode!=CIOLIB_MODE_CURSES_IBM
 			&& cio_api.mode!=CIOLIB_MODE_ANSI) {
-		opts[1]="Disconnect (Alt-H)";
+		opts[1]="Disconnect ("ALT_KEY_NAMEP"-H)";
 	}
 
 	for(ret=0;!ret;) {
 		init_uifc(FALSE, !(bbs->nostatus));
 		uifc.helpbuf=	"`Online Menu`\n\n"
-						"~ Scrollback ~          allows to you to view the scrollback buffer\n"
-						"~ Disconnect ~          disconnects the current session and returns to the\n"
-						"                      dialing list\n"
-						"~ Send Login ~          Sends the configured user and password pair separated\n"
-						"                      by a \\r\n"
-						"~ Upload ~              Initiates a ZModem upload\n"
-						"~ Download ~            Initiates a ZModem download\n"
-						"~ Change Output Rate ~  Changes the speed charaters are output to the screen\n"
-						"~ Change Log Level ~    Changes the minimum log leve for ZModem information\n"
-						"~ Capture Control ~     Enables/Disables screen capture\n"
-						"~ ANSI Music Control ~  Enables/Disables ANSI Music\n"
-						"~ Font Control ~        Changes the current font\n"
-						"~ Toggle Doorway Mode ~ Toggle the current DoorWay setting\n"
-						"~ Exit ~                Disconnects and closes the Syncterm";
+						"`Scrollback`     Allows to you to view the scrollback buffer\n"
+						"`Disconnect`     Disconnects the current connection\n"
+						"`Send Login`     Sends the username and password pair separated by CR\n"
+						"`Upload`         Initiates a file upload (ZMODEM or ASCII)\n"
+						"`Download`       Initiates a file download (ZMODEM)\n"
+						"`Log Level`      Changes the minimum log level for ZMODEM information\n"
+						"`Output Rate`    Changes the speed characters are output to the screen\n"
+						"`Capture`        Enables/Disables screen capture\n"
+						"`ANSI Music`     Enables/Disables ANSI Music\n"
+						"`Font`           Changes the current font (when supported)\n"
+						"`Doorway Mode`   Toggles the current DoorWay (keyboard input) setting\n"
+#ifndef WITHOUT_OOII
+						"`Operation Overkill ][ Mode`   Toggles the current Operation Overkill ][ setting\n"
+#endif
+						"`Exit`           Disconnects and closes Syncterm";
 		i=uifc.list(WIN_MID|WIN_SAV,0,0,0,&opt,NULL,"SyncTERM Online Menu",opts);
 		switch(i) {
 			case -1:	/* Cancel */
@@ -207,7 +212,10 @@ int syncmenu(struct bbslist *bbs, int *speed)
 				}
 				break;
 			case 5:		/* Output rate */
-				if(speed != NULL) {
+				if(bbs->conn_type==CONN_TYPE_MODEM || bbs->conn_type==CONN_TYPE_SERIAL)
+					uifcmsg("Not supported for this connection type"
+						,"Cannot change the display rate for Modem/Serial connections.");
+				else if(speed != NULL) {
 					j=get_rate_num(*speed);
 					uifc.helpbuf="`Output Rate`\n\n"
 							"The output rate is the rate in emulated \"bits per second\" to draw incoming\n"
