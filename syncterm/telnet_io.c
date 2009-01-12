6c5d56f4d3b83cfebbe18607f99d0977b663ce7c
deuce <deuce>
2009-01-12 00:43:37 +0000

    Add possibility of DENYNONE mode to nopen() intro comment.

--- syncterm/telnet_io.c
+++ syncterm/telnet_io.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: telnet_io.c,v 1.22 2008/01/20 23:11:14 rswindell Exp $ */
+/* $Id: telnet_io.c,v 1.27 2008/02/23 06:31:40 deuce Exp $ */
 
 #include <stdlib.h>
 #include <string.h>
@@ -50,7 +50,7 @@ void putcom(BYTE* buf, size_t len)
 	size_t i;
 
 	for(i=0;i<len;i++)
-		p+=sprintf(p,"%02X ", buf[i]);
+		p+=sprintf(p,"%u ", buf[i]);
 
 	lprintf(LOG_DEBUG,"TX: %s", str);
 	send(telnet_sock, buf, len, 0);
@@ -91,7 +91,8 @@ BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen)
 {
 	BYTE	command;
 	BYTE	option;
-	BYTE*   first_iac;
+	BYTE*   first_cr=NULL;
+	BYTE*   first_int=NULL;
 	int 	i;
 
 	if(inlen<1) {
@@ -99,20 +100,42 @@ BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen)
 		return(inbuf);	/* no length? No interpretation */
 	}
 
-    first_iac=(BYTE*)memchr(inbuf, TELNET_IAC, inlen);
+    first_int=(BYTE*)memchr(inbuf, TELNET_IAC, inlen);
+	if(telnet_remote_option[TELNET_BINARY_TX]!=TELNET_WILL) {
+		first_cr=(BYTE*)memchr(inbuf, '\r', inlen);
+		if(first_cr) {
+			if(first_int==NULL || first_cr < first_int)
+				first_int=first_cr;
+		}
+	}
 
-    if(!telnet_cmdlen	&& first_iac==NULL) {
+    if(telnet_cmdlen==0 && first_int==NULL) {
         *outlen=inlen;
         return(inbuf);	/* no interpretation needed */
     }
 
-    if(first_iac!=NULL) {
-   		*outlen=first_iac-inbuf;
+    if(telnet_cmdlen==0 /* If we haven't returned and telnet_cmdlen==0 then first_int is not NULL */  ) {
+   		*outlen=first_int-inbuf;
 	    memcpy(outbuf, inbuf, *outlen);
     } else
     	*outlen=0;
 
     for(i=*outlen;i<inlen;i++) {
+		if(telnet_remote_option[TELNET_BINARY_TX]!=TELNET_WILL) {
+			if(telnet_cmdlen==1 && telnet_cmd[0]=='\r') {
+            	outbuf[(*outlen)++]='\r';
+				if(inbuf[i]!=0 && inbuf[i]!=TELNET_IAC)
+	            	outbuf[(*outlen)++]=inbuf[i];
+				telnet_cmdlen=0;
+				if(inbuf[i]!=TELNET_IAC)
+					continue;
+			}
+			if(inbuf[i]=='\r' && telnet_cmdlen==0) {
+				telnet_cmd[telnet_cmdlen++]='\r';
+				continue;
+			}
+		}
+
         if(inbuf[i]==TELNET_IAC && telnet_cmdlen==1) { /* escaped 255 */
             telnet_cmdlen=0;
             outbuf[(*outlen)++]=TELNET_IAC;
@@ -217,22 +240,32 @@ BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen)
 BYTE* telnet_expand(BYTE* inbuf, size_t inlen, BYTE* outbuf, size_t *newlen)
 {
 	BYTE*   first_iac;
+	BYTE*   first_cr=NULL;
 	ulong	i,outlen;
 
     first_iac=(BYTE*)memchr(inbuf, TELNET_IAC, inlen);
+	if(telnet_local_option[TELNET_BINARY_TX]!=TELNET_DO)
+	    first_cr=(BYTE*)memchr(inbuf, '\r', inlen);
 
-	if(first_iac==NULL) {	/* Nothing to expand */
+	if(first_iac==NULL && first_cr==NULL) {	/* Nothing to expand */
 		*newlen=inlen;
 		return(inbuf);
 	}
 
-	outlen=first_iac-inbuf;
+	if(first_iac!=NULL && (first_cr==NULL || first_iac < first_cr))
+		outlen=first_iac-inbuf;
+	else
+		outlen=first_cr-inbuf;
 	memcpy(outbuf, inbuf, outlen);
 
     for(i=outlen;i<inlen;i++) {
 		if(inbuf[i]==TELNET_IAC)
 			outbuf[outlen++]=TELNET_IAC;
 		outbuf[outlen++]=inbuf[i];
+		if(telnet_local_option[TELNET_BINARY_TX]!=TELNET_DO) {
+			if(inbuf[i]=='\r')
+				outbuf[outlen++]='\n';
+		}
 	}
     *newlen=outlen;
     return(outbuf);
