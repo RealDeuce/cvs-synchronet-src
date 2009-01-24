4636149ccff2c326a531e6f4ac8c43b1dab6fb82
rswindell <rswindell>
2009-01-24 02:33:53 +0000

    Enforce log_level specified in startup structure in server's lprintf/lputs.

--- conio/cterm.c
+++ conio/cterm.c
@@ -1,4 +1,4 @@
-/* $Id: cterm.c,v 1.115 2009/02/06 08:08:31 deuce Exp $ */
+/* $Id: cterm.c,v 1.112 2009/01/09 07:56:07 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -250,11 +250,6 @@ void play_music(void)
 	struct	note_params *np;
 	int		fore_count;
 
-	if(cterm.quiet) {
-		cterm.music=0;
-		cterm.musicbuf[0]=0;
-		return;
-	}
 	p=cterm.musicbuf;
 	fore_count=0;
 	if(cterm.music==1) {
@@ -1141,7 +1136,7 @@ void do_ansi(char *retbuf, size_t retsize, int *speed)
 
 void cterm_init(int height, int width, int xpos, int ypos, int backlines, unsigned char *scrollback, int emulation)
 {
-	char	*revision="$Revision: 1.115 $";
+	char	*revision="$Revision: 1.112 $";
 	char *in;
 	char	*out;
 	int		i;
@@ -1325,7 +1320,7 @@ char *cterm_write(unsigned char *buf, int buflen, char *retbuf, size_t retsize, 
 	if(retbuf!=NULL)
 		retbuf[0]=0;
 	gettextinfo(&ti);
-	if(ti.winleft != cterm.x || ti.wintop != cterm.y || ti.winright != cterm.x+cterm.width-1 || ti.winbottom != cterm.y+cterm.height-1)
+	if(ti.winleft != cterm.x || ti.wintop != cterm.y || ti.winright != cterm.x+cterm.width-1 || ti.winleft != cterm.y+cterm.height-1)
 		window(cterm.x,cterm.y,cterm.x+cterm.width-1,cterm.y+cterm.height-1);
 	gotoxy(cterm.xpos,cterm.ypos);
 	textattr(cterm.attr);
@@ -1586,13 +1581,11 @@ char *cterm_write(unsigned char *buf, int buflen, char *retbuf, size_t retsize, 
 									cterm.escbuf[wherex()]=1;
 									break;
 								case 253:	/* Beep */
-									if(!cterm.quiet) {
-										#ifdef __unix__
-											putch(7);
-										#else
-											MessageBeep(MB_OK);
-										#endif
-									}
+									#ifdef __unix__
+										putch(7);
+									#else
+										MessageBeep(MB_OK);
+									#endif
 									break;
 								case 254:	/* Delete Char */
 									j=wherex();
@@ -1870,13 +1863,11 @@ char *cterm_write(unsigned char *buf, int buflen, char *retbuf, size_t retsize, 
 
 							/* Extras */
 							case 7:			/* Beep */
-								if(!cterm.quiet) {
-									#ifdef __unix__
-										putch(7);
-									#else
-										MessageBeep(MB_OK);
-									#endif
-								}
+								#ifdef __unix__
+									putch(7);
+								#else
+									MessageBeep(MB_OK);
+								#endif
 								break;
 
 							/* Translate to screen codes */
@@ -1957,13 +1948,11 @@ char *cterm_write(unsigned char *buf, int buflen, char *retbuf, size_t retsize, 
 									prn[0]=0;
 									if(cterm.log==CTERM_LOG_ASCII && cterm.logfile != NULL)
 										fputs("\x07", cterm.logfile);
-									if(!cterm.quiet) {
-										#ifdef __unix__
-											putch(7);
-										#else
-											MessageBeep(MB_OK);
-										#endif
-									}
+									#ifdef __unix__
+										putch(7);
+									#else
+										MessageBeep(MB_OK);
+									#endif
 									break;
 								case 12:		/* ^L - Clear screen */
 									ctputs(prn);
