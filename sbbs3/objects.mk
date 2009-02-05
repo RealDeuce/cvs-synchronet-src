de7029357248b0c4f41d5a4dff4cda9d8e769853
rswindell <rswindell>
2009-02-05 19:56:16 +0000

    Add 'SSH' to the description of the BBS experience.

--- sbbs3/objects.mk
+++ sbbs3/objects.mk
@@ -2,7 +2,7 @@
 
 # Make 'include file' listing object files for SBBS.DLL
 
-# $Id: objects.mk,v 1.50 2009/01/10 07:04:06 rswindell Exp $(DIRSEP)09$(DIRSEP)08 07:23:54 deuce Exp $
+# $Id: objects.mk,v 1.51 2009/01/23 08:04:28 deuce Exp $(DIRSEP)09$(DIRSEP)08 07:23:54 deuce Exp $
 
 # OBJODIR, SLASH, and OFILE must be pre-defined
 
@@ -82,6 +82,7 @@ OBJS	=	$(MTOBJODIR)$(DIRSEP)ansiterm$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)qwktomsg$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)readmail$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)readmsgs$(OFILE)\
+			$(MTOBJODIR)$(DIRSEP)readtext$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)ringbuf$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)scandirs$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)scansubs$(OFILE)\
@@ -94,6 +95,7 @@ OBJS	=	$(MTOBJODIR)$(DIRSEP)ansiterm$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)str_util$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)telgate$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)telnet$(OFILE)\
+			$(MTOBJODIR)$(DIRSEP)text_defaults$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)text_sec$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)tmp_xfer$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)un_qwk$(OFILE)\
