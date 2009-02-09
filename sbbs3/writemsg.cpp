b3e9cfab4faa6af0c9b96f13d23a4e921a460c90
deuce <deuce>
2009-02-09 09:56:49 +0000

    Import (mostly) ported CRT library which includes a font editing program. Makefiles for *nix only so far.

--- sbbs3/writemsg.cpp
+++ sbbs3/writemsg.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet message creation routines */
 
-/* $Id: writemsg.cpp,v 1.73 2008/02/23 10:43:29 rswindell Exp $ */
+/* $Id: writemsg.cpp,v 1.79 2008/10/01 01:46:10 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -65,8 +65,8 @@ char* sbbs_t::msg_tmp_fname(int xedit, char* fname, size_t len)
 /* message and 'title' is the title (70chars max) for the message.          */
 /* 'dest' contains a text description of where the message is going.        */
 /****************************************************************************/
-bool sbbs_t::writemsg(char *fname, char *top, char *title, long mode, int subnum
-	,char *dest)
+bool sbbs_t::writemsg(const char *fname, const char *top, char *title, long mode, int subnum
+	,const char *dest)
 {
 	char	str[256],quote[128],c,*buf,*p,*tp
 				,useron_level;
@@ -256,16 +256,20 @@ bool sbbs_t::writemsg(char *fname, char *top, char *title, long mode, int subnum
 
 	if(!(mode&(WM_EXTDESC|WM_SUBJ_RO))) {
 		if(mode&WM_FILE) {
+#if 0
 			max_title_len=12;	/* ToDo: implied 8.3 filename limit! */
+#endif
 			CRLF;
 			bputs(text[Filename]); 
 		}
 		else {
+#if 0
 			max_title_len=LEN_TITLE;
 			if(mode&WM_QWKNET
 				|| (subnum!=INVALID_SUB 
 					&& (cfg.sub[subnum]->misc&(SUB_QNET|SUB_INET|SUB_FIDO))==SUB_QNET))
 				max_title_len=25;
+#endif
 			bputs(text[SubjectPrompt]); 
 		}
 		if(!getstr(title,max_title_len,mode&WM_FILE ? K_LINE : K_LINE|K_EDIT|K_AUTODEL)
@@ -363,7 +367,7 @@ bool sbbs_t::writemsg(char *fname, char *top, char *title, long mode, int subnum
 				fgets(str,sizeof(str),fp);
 				fgets(str,sizeof(str),fp);
 				truncsp(str);
-				SAFEPRINTF2(title,"%.*s",max_title_len,str);
+				safe_snprintf(title,max_title_len,"%s",str);
 				fclose(fp);
 			}
 		}
@@ -485,7 +489,7 @@ void quotestr(char *str)
 	remove_ctrl_a(str,NULL);
 }
 
-void sbbs_t::editor_inf(int xeditnum,char *dest, char *title, long mode
+void sbbs_t::editor_inf(int xeditnum, const char *dest, const char *title, long mode
 	,uint subnum)
 {
 	char str[MAX_PATH+1];
@@ -601,7 +605,7 @@ void sbbs_t::removeline(char *str, char *str2, char num, char skip)
 /* The Synchronet editor.                                                    */
 /* Returns the number of lines edited.                                       */
 /*****************************************************************************/
-ulong sbbs_t::msgeditor(char *buf, char *top, char *title)
+ulong sbbs_t::msgeditor(char *buf, const char *top, char *title)
 {
 	int		i,j,line,lines=0,maxlines;
 	char	strin[256],**str,done=0;
@@ -897,6 +901,7 @@ void sbbs_t::editfile(char *fname)
 {
 	char *buf,path[MAX_PATH+1];
 	char msgtmp[MAX_PATH+1];
+	char str[MAX_PATH+1];
     int file;
 	long length,maxlines,lines,l,mode=0;
 
@@ -928,8 +933,11 @@ void sbbs_t::editfile(char *fname)
 		CLS;
 		rioctl(IOCM|PAUSE|ABORT);
 		external(cmdstr(cfg.xedit[useron.xedit-1]->rcmd,msgtmp,nulstr,NULL),mode,cfg.node_dir);
-		if(stricmp(msgtmp,path) && !fcompare(msgtmp, path))	/* file changed */
+		if(stricmp(msgtmp,path) && !fcompare(msgtmp, path))	{ /* file changed */
 			fcopy(msgtmp, path);
+			SAFEPRINTF2(str,"%s created or edited file: %s",useron.alias, path);
+			logline(nulstr,str);
+		}
 		rioctl(IOSM|PAUSE|ABORT); 
 		return; 
 	}
@@ -982,6 +990,9 @@ void sbbs_t::editfile(char *fname)
 	bprintf(text[SavedNBytes],l,lines);
 	close(file);
 	free(buf);
+	SAFEPRINTF2(str,"%s created or edited file: %s",useron.alias, fname);
+	logline(nulstr,str);
+
 	return;
 }
 
@@ -1051,6 +1062,9 @@ void sbbs_t::forwardmail(smbmsg_t *msg, int usernumber)
 	smb_hfield_str(msg,SENDER,useron.alias);
 	SAFEPRINTF(str,"%u",useron.number);
 	smb_hfield_str(msg,SENDEREXT,str);
+
+	/* Security logging */
+	msg_client_hfields(msg,&client);
 
 	username(&cfg,usernumber,touser);
 	smb_hfield_str(msg,RECIPIENT,touser);
