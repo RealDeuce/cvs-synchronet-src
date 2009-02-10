cb648efb49e9f59a84c11033ee7a93c2841c5ee3
deuce <deuce>
2009-02-10 02:42:08 +0000

    Fix build system.

--- xpdev/str_list.h
+++ xpdev/str_list.h
@@ -2,13 +2,13 @@
 
 /* Functions to deal with NULL-terminated string lists */
 
-/* $Id: str_list.h,v 1.20 2005/10/12 22:46:36 rswindell Exp $ */
+/* $Id: str_list.h,v 1.21 2008/02/24 07:51:25 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -100,6 +100,9 @@ size_t		strListMerge(str_list_t*, str_list_t append_list);
 
 /* Count the number of strings in the list and returns the count */
 size_t		strListCount(const str_list_t);
+
+/* Returns the index of the specified str (by ptr compare) or -1 if not found */
+int			strListIndexOf(const str_list_t, const char* str);
 
 /* Sort the strings in the string list */
 void		strListSortAlpha(str_list_t);
