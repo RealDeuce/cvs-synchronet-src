d2c5c04e0c85d0226d09272ae1d6aff15fe03630
rswindell <rswindell>
2009-02-16 04:10:25 +0000

    In html_encode(), convert ^A< to a destructive backspace.

--- sbbs3/inkey.cpp
+++ sbbs3/inkey.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet single key input function (no wait) */
 
-/* $Id: inkey.cpp,v 1.35 2008/05/30 23:27:35 deuce Exp $ */
+/* $Id: inkey.cpp,v 1.36 2009/01/16 03:51:46 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -312,7 +312,7 @@ char sbbs_t::handle_ctrlkey(char ch, long mode)
 					return(ESC); 
 				}
 				if(ch=='R') {       /* cursor position report */
-					if(i && !(useron.rows)) {	/* auto-detect rows */
+					if(mode&K_ANSI_CPR && i && !(useron.rows)) {	/* auto-detect rows */
 						str[i]=0;
 						rows=atoi(str);
 						lprintf(LOG_DEBUG,"Node %d ANSI cursor position report: %u rows"
