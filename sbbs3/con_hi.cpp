4c625a89654cb1e99664abea0050e55d03ede901
rswindell <rswindell>
2009-02-25 00:08:26 +0000

    Change sbbs_t::chksyspass() to use K_NOECHO instead of fiddling with global console mode.

--- sbbs3/con_hi.cpp
+++ sbbs3/con_hi.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet hi-level console routines */
 
-/* $Id: con_hi.cpp,v 1.16 2009/02/19 10:55:15 rswindell Exp $ */
+/* $Id: con_hi.cpp,v 1.17 2009/02/25 00:08:26 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -108,25 +108,15 @@ bool sbbs_t::chksyspass()
 		logline("S!","Remote sysop access disabled");
 		return(false);
 	}
-#if 0	/* no local logins in v3 */
-	if(online==ON_LOCAL) {
-		if(!(cfg.sys_misc&SM_L_SYSOP))
-			return(false);
-		if(!(cfg.node_misc&NM_SYSPW) && !(cfg.sys_misc&SM_REQ_PW))
-			return(false); 
-	}
-#endif
 	bputs(text[SystemPassword]);
-	console&=~(CON_R_ECHO|CON_L_ECHO);
-	getstr(str,40,K_UPPER);
-	console=orgcon;
+	getstr(str,40,K_UPPER|K_NOECHO);
 	CRLF;
 	if(strcmp(cfg.sys_pass,str)) {
 		if(cfg.sys_misc&SM_ECHO_PW) 
-			sprintf(str2,"%s #%u System password attempt: '%s'"
+			SAFEPRINTF3(str2,"%s #%u System password attempt: '%s'"
 				,useron.alias,useron.number,str);
 		else
-			sprintf(str2,"%s #%u System password verification failure"
+			SAFEPRINTF2(str2,"%s #%u System password verification failure"
 				,useron.alias,useron.number);
 		logline("S!",str2);
 		return(false); 
