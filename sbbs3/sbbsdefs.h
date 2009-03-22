23213fb63643b5870a7b9ecf6790b959da4b37c1
rswindell <rswindell>
2009-03-22 02:34:17 +0000

    Fix typo in last commit.

--- sbbs3/sbbsdefs.h
+++ sbbs3/sbbsdefs.h
@@ -2,7 +2,7 @@
 
 /* Synchronet constants, macros, and structure definitions */
 
-/* $Id: sbbsdefs.h,v 1.159 2009/03/22 02:31:58 rswindell Exp $ */
+/* $Id: sbbsdefs.h,v 1.160 2009/03/22 02:34:17 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -293,7 +293,7 @@ typedef struct {
 #define ERR_CHK		"checking"		/* checking */
 #define ERR_LEN		"checking length"
 #define ERR_EXEC	"executing"		/* executing */
-#define ERR_CHDIR	"changing directory */
+#define ERR_CHDIR	"changing directory"
 #define ERR_CREATE	"creating" 		/* creating */
 #define ERR_LOCK	"locking"		/* locking */
 #define ERR_UNLOCK 	"unlocking"		/* unlocking */
