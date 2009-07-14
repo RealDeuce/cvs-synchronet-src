92fb7cd304281718340fb6a98d1727c774802203
rswindell <rswindell>
2009-07-14 01:40:26 +0000

    Added text.dat entry: ValidatePostQ Added prompts for sub-board operators/sysops, after reading an unvalidated moderated messages to validate or delete the message.

--- sbbs3/ans2asc.c
+++ sbbs3/ans2asc.c
@@ -2,13 +2,13 @@
 
 /* Convert ANSI messages to Synchronet .asc (Ctrl-A code) format */
 
-/* $Id: ans2asc.c,v 1.4 2006/05/09 23:03:09 deuce Exp $ */
+/* $Id: ans2asc.c,v 1.5 2009/02/16 02:49:24 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
 	int i,ch,ni;
 	FILE *in,*out;
 
-	sscanf("$Revision: 1.4 $", "%*s %s", revision);
+	sscanf("$Revision: 1.5 $", "%*s %s", revision);
 
 	if(argc<2) {
 		fprintf(stderr,"\nans2asc %s\n",revision);
@@ -77,6 +77,7 @@ int main(int argc, char **argv)
 	while((ch=fgetc(in))!=EOF) {
 		if(ch=='[' && esc) {    /* ANSI escape sequence */
 			ni=0;				/* zero number index */
+			memset(n,1,sizeof(n));
 			while((ch=fgetc(in))!=EOF) {
 				if(isdigit(ch)) {			/* 1 digit */
 					n[ni]=ch&0xf;
@@ -185,8 +186,20 @@ int main(int argc, char **argv)
 									fputc('7',out);
 									break; } }
 						break;
-					case 'C':
+					case 'B':	/* cursor down */
+						while(n[0]) {
+							fprintf(out,"\n");
+							n[0]--;
+						}
+						break;
+					case 'C':	/* cursor right */
 						fprintf(out,"\1%c",0x7f+n[0]);
+						break;
+					case 'D':	/* cursor left */
+						while(n[0]) {
+							fprintf(out,"\b");
+							n[0]--;
+						}
 						break;
 					default:
 						fprintf(stderr,"Unsupported ANSI code '%c' (0x%02X)\r\n",ch,ch);
