ada1166941f287e62090837a09fc750392d3e783
deuce <deuce>
2009-08-03 23:57:00 +0000

    Make token parsing deal with trailing commas.  This fixed Digest authentication with IE8.

--- sbbs3/sbbscon.c
+++ sbbs3/sbbscon.c
@@ -2,13 +2,13 @@
 
 /* Synchronet vanilla/console-mode "front-end" */
 
-/* $Id: sbbscon.c,v 1.236 2011/03/21 01:30:29 sbbs Exp $ */
+/* $Id: sbbscon.c,v 1.230 2009/02/13 04:22:55 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -105,10 +105,9 @@ BOOL				web_running=FALSE;
 BOOL				web_stopped=FALSE;
 BOOL				has_web=FALSE;
 web_startup_t		web_startup;
-ulong				thread_count=1;
-ulong				socket_count=0;
-ulong				client_count=0;
-ulong				error_count=0;
+uint				thread_count=1;
+uint				socket_count=0;
+uint				client_count=0;
 int					prompt_len=0;
 static scfg_t		scfg;					/* To allow rerun */
 static ulong		served=0;
@@ -251,7 +250,7 @@ static int lputs(int level, char *str)
 	printf("\r%*s\r",prompt_len,"");
 	if(str!=NULL) {
 		for(p=str; *p; p++) {
-			if(iscntrl((unsigned char)*p))
+			if(iscntrl(*p))
 				printf("^%c",'@'+*p);
 			else
 				printf("%c",*p);
@@ -260,16 +259,11 @@ static int lputs(int level, char *str)
 	}
 	/* re-display prompt with current stats */
 	if(prompt!=NULL)
-		prompt_len = printf(prompt, thread_count, socket_count, client_count, served, error_count);
+		prompt_len = printf(prompt, thread_count, socket_count, client_count, served);
 	fflush(stdout);
 	pthread_mutex_unlock(&mutex);
 
     return(prompt_len);
-}
-
-static void errormsg(void* cbdata, int level, const char* fmt)
-{
-	error_count++;
 }
 
 static int lprintf(int level, const char *fmt, ...)
@@ -374,11 +368,6 @@ BOOL do_setuid(BOOL force)
 	}
 
 	if(getuid() != new_uid || geteuid() != new_uid) {
-		if(initgroups(new_uid_name, new_gid)) {
-			lputs(LOG_ERR,"!initgroups FAILED");
-			lputs(LOG_ERR,strerror(errno));
-			result=FALSE;
-		}	
 		if(setreuid(new_uid,new_uid))
 		{
 			lputs(LOG_ERR,"!setuid FAILED");
@@ -627,7 +616,7 @@ static int bbs_lputs(void* p, int level, const char *str)
 		if (std_facilities)
 			syslog(level|LOG_AUTH,"%s",str);
 		else
-			syslog(level,"term %s",str);
+			syslog(level,"     %s",str);
 		return(strlen(str));
 	}
 #endif
@@ -640,7 +629,7 @@ static int bbs_lputs(void* p, int level, const char *str)
 			,tm.tm_mon+1,tm.tm_mday
 			,tm.tm_hour,tm.tm_min,tm.tm_sec);
 
-	sprintf(logline,"%sterm %.*s",tstr,(int)sizeof(logline)-32,str);
+	sprintf(logline,"%s     %.*s",tstr,(int)sizeof(logline)-32,str);
 	truncsp(logline);
 	lputs(level,logline);
 	
@@ -963,7 +952,7 @@ static void terminate(void)
 	while(bbs_running || ftp_running || web_running || mail_running || services_running)  {
 		if(count && (count%10)==0) {
 			if(bbs_running)
-				lputs(LOG_INFO,"Terminal Server thread still running");
+				lputs(LOG_INFO,"BBS System thread still running");
 			if(ftp_running)
 				lputs(LOG_INFO,"FTP Server thread still running");
 			if(web_running)
@@ -1055,7 +1044,7 @@ void cleanup(void)
 }
 
 #if defined(_WIN32)
-BOOL WINAPI ControlHandler(unsigned long CtrlType)
+BOOL WINAPI ControlHandler(DWORD CtrlType)
 {
 	terminated=TRUE;
 	return TRUE;
@@ -1236,7 +1225,6 @@ int main(int argc, char** argv)
 	bbs_startup.log_level = LOG_DEBUG;
 	bbs_startup.lputs=bbs_lputs;
 	bbs_startup.event_lputs=event_lputs;
-	bbs_startup.errormsg=errormsg;
     bbs_startup.started=bbs_started;
 	bbs_startup.recycle=recycle;
     bbs_startup.terminated=bbs_terminated;
@@ -1259,7 +1247,6 @@ int main(int argc, char** argv)
 	ftp_startup.cbdata=&ftp_startup;
 	ftp_startup.log_level = LOG_DEBUG;
 	ftp_startup.lputs=ftp_lputs;
-	ftp_startup.errormsg=errormsg;
     ftp_startup.started=ftp_started;
 	ftp_startup.recycle=recycle;
     ftp_startup.terminated=ftp_terminated;
@@ -1279,7 +1266,6 @@ int main(int argc, char** argv)
 	web_startup.cbdata=&web_startup;
 	web_startup.log_level = LOG_DEBUG;
 	web_startup.lputs=web_lputs;
-	web_startup.errormsg=errormsg;
     web_startup.started=web_started;
 	web_startup.recycle=recycle;
     web_startup.terminated=web_terminated;
@@ -1298,7 +1284,6 @@ int main(int argc, char** argv)
 	mail_startup.cbdata=&mail_startup;
 	mail_startup.log_level = LOG_DEBUG;
 	mail_startup.lputs=mail_lputs;
-	mail_startup.errormsg=errormsg;
     mail_startup.started=mail_started;
 	mail_startup.recycle=recycle;
     mail_startup.terminated=mail_terminated;
@@ -1317,7 +1302,6 @@ int main(int argc, char** argv)
 	services_startup.cbdata=&services_startup;
 	services_startup.log_level = LOG_DEBUG;
 	services_startup.lputs=services_lputs;
-	services_startup.errormsg=errormsg;
     services_startup.started=services_started;
 	services_startup.recycle=recycle;
     services_startup.terminated=services_terminated;
@@ -1914,14 +1898,12 @@ int main(int argc, char** argv)
 	}
 
     if(!isatty(fileno(stdin)))  			/* redirected */
-	   	while(1) {
+	   	while(1)
 	    	select(0,NULL,NULL,NULL,NULL);	/* Sleep forever - Should this just exit the thread? */
-		lputs(LOG_WARNING,"select(NULL) returned!");
-	}
 	else 								/* interactive */
 #endif
 	{
-	    prompt = "[Threads: %d  Sockets: %d  Clients: %d  Served: %lu  Errors: %lu] (?=Help): ";
+	    prompt = "[Threads: %d  Sockets: %d  Clients: %d  Served: %lu] (?=Help): ";
 	    lputs(LOG_INFO,NULL);	/* display prompt */
 
 		while(!terminated) {
