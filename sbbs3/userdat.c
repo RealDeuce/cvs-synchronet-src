eadedc31fdc8b68060c1f45357ae01957105fbd6
deuce <deuce>
2009-08-04 01:17:44 +0000

    Undo accidental commit again.

--- sbbs3/userdat.c
+++ sbbs3/userdat.c
@@ -2,7 +2,7 @@
 
 /* Synchronet user data-related routines (exported) */
 
-/* $Id: userdat.c,v 1.120 2009/10/10 07:13:55 rswindell Exp $ */
+/* $Id: userdat.c,v 1.117 2009/03/20 00:39:46 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -52,13 +52,12 @@ char* nulstr="";
 /* Makes dots and underscores synomynous with spaces for comparisions		*/
 /* Returns the number of the perfect matched username or 0 if no match		*/
 /****************************************************************************/
-uint DLLCALL matchuser(scfg_t* cfg, const char *name, BOOL sysop_alias)
+uint DLLCALL matchuser(scfg_t* cfg, char *name, BOOL sysop_alias)
 {
 	int		file,c;
 	char*	p;
 	char	dat[LEN_ALIAS+2];
 	char	str[256];
-	char	tmp[256];
 	ulong	l,length;
 	FILE*	stream;
 
@@ -111,11 +110,6 @@ uint DLLCALL matchuser(scfg_t* cfg, const char *name, BOOL sysop_alias)
 		strcpy(str,dat);
 		REPLACE_CHARS(str,'_',' ',p);
 		if(!stricmp(str,name)) 
-			break;
-		/* strip spaces (from both) */
-		strip_space(dat,str);
-		strip_space(name,tmp);
-		if(!stricmp(str,tmp)) 
 			break;
 	}
 	fclose(stream);
@@ -2201,7 +2195,7 @@ void DLLCALL resetdailyuserdat(scfg_t* cfg, user_t* user)
 
 /****************************************************************************/
 /****************************************************************************/
-char* DLLCALL usermailaddr(scfg_t* cfg, char* addr, const char* name)
+char* DLLCALL usermailaddr(scfg_t* cfg, char* addr, char* name)
 {
 	int i;
 
@@ -2212,25 +2206,21 @@ char* DLLCALL usermailaddr(scfg_t* cfg, char* addr, const char* name)
 		strcpy(addr,name);
 		return(addr);
 	}
-	if(strchr(name,'.') && strchr(name,' ')) {
-		/* convert "Dr. Seuss" to "Dr.Seuss" */
-		strip_space(name,addr);
-	} else if(strchr(name,'!')) {
-		sprintf(addr,"\"%s\"",name);
-	} else {
-		strcpy(addr,name);
-		/* convert "first last" to "first.last" */
+	if(strchr(name,'!') || (strchr(name,'.') && strchr(name,' ')))
+		sprintf(addr,"\"%s\"@",name);
+	else {
+		sprintf(addr,"%s@",name);
+		/* convert "first last@" to "first.last@" */
 		for(i=0;addr[i];i++)
 			if(addr[i]==' ' || addr[i]&0x80)
 				addr[i]='.';
 		strlwr(addr);
 	}
-	strcat(addr,"@");
 	strcat(addr,cfg->sys_inetaddr);
 	return(addr);
 }
 
-char* DLLCALL alias(scfg_t* cfg, const char* name, char* buf)
+char* DLLCALL alias(scfg_t* cfg, char* name, char* buf)
 {
 	char	line[128];
 	char*	p;
@@ -2245,11 +2235,11 @@ char* DLLCALL alias(scfg_t* cfg, const char* name, char* buf)
 	if(!VALID_CFG(cfg) || name==NULL || buf==NULL)
 		return(NULL);
 
-	p=(char*)name;
+	p=name;
 
 	sprintf(fname,"%salias.cfg",cfg->ctrl_dir);
 	if((fp=fopen(fname,"r"))==NULL)
-		return((char*)name);
+		return(name);
 
 	while(!feof(fp)) {
 		if(!fgets(line,sizeof(line),fp))
@@ -2485,10 +2475,14 @@ int DLLCALL user_rec_len(int offset)
 }
 
 /****************************************************************************/
-/* Determine if the specified user can or cannot access the specified sub	*/
+/* Determine if the specified user can or cannot post on the specified sub	*/
+/* 'reason' is an (optional) pointer to a text.dat item number, indicating	*/
+/* the reason the user cannot post, when returning FALSE.					*/
 /****************************************************************************/
-BOOL DLLCALL can_user_access_sub(scfg_t* cfg, uint subnum, user_t* user, client_t* client)
+BOOL DLLCALL can_user_post(scfg_t* cfg, uint subnum, user_t* user, client_t* client, uint* reason)
 {
+	if(reason!=NULL)
+		*reason=CantPostOnSub;
 	if(!VALID_CFG(cfg))
 		return FALSE;
 	if(subnum>=cfg->total_subs)
@@ -2497,33 +2491,6 @@ BOOL DLLCALL can_user_access_sub(scfg_t* cfg, uint subnum, user_t* user, client_
 		return FALSE;
 	if(!chk_ar(cfg,cfg->sub[subnum]->ar,user,client))
 		return FALSE;
-
-	return TRUE;
-}
-
-/****************************************************************************/
-/* Determine if the specified user can or cannot read the specified sub		*/
-/****************************************************************************/
-BOOL DLLCALL can_user_read_sub(scfg_t* cfg, uint subnum, user_t* user, client_t* client)
-{
-	if(!can_user_access_sub(cfg, subnum, user, client))
-		return FALSE;
-	return chk_ar(cfg,cfg->sub[subnum]->read_ar,user,client);
-}
-
-/****************************************************************************/
-/* Determine if the specified user can or cannot post on the specified sub	*/
-/* 'reason' is an (optional) pointer to a text.dat item number, indicating	*/
-/* the reason the user cannot post, when returning FALSE.					*/
-/****************************************************************************/
-BOOL DLLCALL can_user_post(scfg_t* cfg, uint subnum, user_t* user, client_t* client, uint* reason)
-{
-	if(reason!=NULL)
-		*reason=NoAccessSub;
-	if(!can_user_access_sub(cfg, subnum, user, client))
-		return FALSE;
-	if(reason!=NULL)
-		*reason=CantPostOnSub;
 	if(!chk_ar(cfg,cfg->sub[subnum]->post_ar,user,client))
 		return FALSE;
 	if(cfg->sub[subnum]->misc&(SUB_QNET|SUB_FIDO|SUB_PNET|SUB_INET)
@@ -2539,19 +2506,6 @@ BOOL DLLCALL can_user_post(scfg_t* cfg, uint subnum, user_t* user, client_t* cli
 		return FALSE;
 
 	return TRUE;
-}
-
-/****************************************************************************/
-/* Determine if the specified user is a sub-board operator					*/
-/****************************************************************************/
-BOOL DLLCALL is_user_subop(scfg_t* cfg, uint subnum, user_t* user, client_t* client)
-{
-	if(!can_user_access_sub(cfg, subnum, user, client))
-		return FALSE;
-	if(user->level>=SYSOP_LEVEL)
-		return TRUE;
-
-	return cfg->sub[subnum]->op_ar[0]!=0 && chk_ar(cfg,cfg->sub[subnum]->op_ar,user,client);
 }
 
 /****************************************************************************/
@@ -2652,7 +2606,7 @@ time_t DLLCALL gettimeleft(scfg_t* cfg, user_t* user, time_t starttime)
 /*************************************************************************/
 /* Check a supplied name/alias and see if it's valid by our standards.   */
 /*************************************************************************/
-BOOL DLLCALL check_name(scfg_t* cfg, const char* name)
+BOOL DLLCALL check_name(scfg_t* cfg, char* name)
 {
 	char	tmp[512];
 	size_t	len;
