71d146f6b9aba11b0c630ec9d46c2c03e2a764b7
rswindell <rswindell>
2009-10-05 04:50:57 +0000

    The thread_up(), socket_open(), and client_add() callbacks no longer modify the status bar directly (this is now handled in the UpTimerTick), thus preventing crashes if 2 callbacks are called concurrently.

--- sbbs3/getmsg.cpp
+++ sbbs3/getmsg.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet message retrieval functions */
 
-/* $Id: getmsg.cpp,v 1.39 2011/08/25 19:22:44 rswindell Exp $ */
+/* $Id: getmsg.cpp,v 1.36 2009/03/20 09:36:20 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -104,7 +104,7 @@ void sbbs_t::show_msgattr(ushort attr)
 		,attr&MSG_MODERATED ? "Moderated  " :nulstr
 		,attr&MSG_VALIDATED ? "Validated  " :nulstr
 		,attr&MSG_REPLIED	? "Replied  "	:nulstr
-		,attr&MSG_NOREPLY	? "NoReply  "	:nulstr
+		,nulstr
 		,nulstr
 		,nulstr
 		,nulstr
@@ -144,13 +144,13 @@ void sbbs_t::show_msghdr(smbmsg_t* msg)
 	if(msg->to_ext)
 		bprintf(text[MsgToExt],msg->to_ext);
 	if(msg->to_net.addr)
-		bprintf(text[MsgToNet],smb_netaddrstr(&msg->to_net,str));
+		bprintf(text[MsgToNet],smb_netaddr(&msg->to_net));
 	if(!(msg->hdr.attr&MSG_ANONYMOUS) || SYSOP) {
 		bprintf(text[MsgFrom],msg->from);
 		if(msg->from_ext)
 			bprintf(text[MsgFromExt],msg->from_ext);
 		if(msg->from_net.addr && !strchr(msg->from,'@'))
-			bprintf(text[MsgFromNet],smb_netaddrstr(&msg->from_net,str)); 
+			bprintf(text[MsgFromNet],smb_netaddr(&msg->from_net)); 
 	}
 	bprintf(text[MsgDate]
 		,timestr(msg->hdr.when_written.time)
@@ -199,7 +199,6 @@ void sbbs_t::show_msg(smbmsg_t* msg, long mode)
 void sbbs_t::msgtotxt(smbmsg_t* msg, char *str, int header, int tails)
 {
 	char	*buf;
-	char	tmp[128];
 	int 	i;
 	FILE	*out;
 
@@ -214,12 +213,12 @@ void sbbs_t::msgtotxt(smbmsg_t* msg, char *str, int header, int tails)
 		if(msg->to_ext)
 			fprintf(out," #%s",msg->to_ext);
 		if(msg->to_net.addr)
-			fprintf(out," (%s)",smb_netaddrstr(&msg->to_net,tmp));
+			fprintf(out," (%s)",smb_netaddr(&msg->to_net));
 		fprintf(out,"\r\nFrom : %s",msg->from);
 		if(msg->from_ext && !(msg->hdr.attr&MSG_ANONYMOUS))
 			fprintf(out," #%s",msg->from_ext);
 		if(msg->from_net.addr)
-			fprintf(out," (%s)",smb_netaddrstr(&msg->from_net,tmp));
+			fprintf(out," (%s)",smb_netaddr(&msg->from_net));
 		fprintf(out,"\r\nDate : %.24s %s"
 			,timestr(msg->hdr.when_written.time)
 			,smb_zonestr(msg->hdr.when_written.zone,NULL));
@@ -256,7 +255,7 @@ ulong sbbs_t::getmsgnum(uint subnum, time_t t)
 		return(0); 
 	}
 
-	total=(long)filelength(fileno(smb.sid_fp))/sizeof(idxrec_t);
+	total=filelength(fileno(smb.sid_fp))/sizeof(idxrec_t);
 
 	if(!total) {		   /* Empty base */
 		smb_close(&smb);
@@ -417,7 +416,7 @@ ulong sbbs_t::getlastmsg(uint subnum, uint32_t *ptr, time_t *t)
 		errormsg(WHERE,ERR_READ,smb.file,i,smb.last_error);
 		return(0); 
 	}
-	total=(long)filelength(fileno(smb.sid_fp))/sizeof(idxrec_t);
+	total=filelength(fileno(smb.sid_fp))/sizeof(idxrec_t);
 	smb_unlocksmbhdr(&smb);
 	smb_close(&smb);
 	if(ptr)
