232c335674faeba789e063494a74cfd1751d4fe0
deuce <deuce>
2009-10-22 13:34:25 +0000

    Start work on Shady Shoppes, add a simple test harness to allow testing of code.

--- doors/u32rr/macros.h
+++ doors/u32rr/macros.h
@@ -10,6 +10,32 @@
 
 #define CRASHF(...)	({ fprintf(stderr, __VA_ARGS__); CRASH; })
 
+enum colors {
+	BLACK,
+	BLUE,
+	GREEN,
+	CYAN,
+	RED,
+	MAGENTA,
+	BROWN,
+	GREY,
+	LIGHT_GREY=GREY,
+
+	BRIGHT_BLACK,
+	DARK_GREY=BRIGHT_BLACK,
+	BRIGHT_BLUE,
+	BRIGHT_GREEN,
+	BRIGHT_CYAN,
+	BRIGHT_RED,
+	BRIGHT_MAGENTA,
+	BRIGHT_BROWN,
+	YELLOW=BRIGHT_BROWN,
+	BRIGHT_GREY,
+	WHITE=BRIGHT_GREY,
+
+	D_DONE = 127
+};
+
 #define mkstring(len, ch) ({ \
 	char *mkstring_tmp_str = (char *)alloca((len)+1); \
 \
@@ -20,67 +46,48 @@
 	mkstring_tmp_str; \
 })
 
-#define supcasestr(dest, str) ({ \
-	char *upcasestr_tmp_ptr; \
-\
-	strcpy(dest, str); \
-	for(upcasestr_tmp_ptr=dest; *upcasestr_tmp_ptr; upcasestr_tmp_ptr++) \
-		*upcasestr_tmp_ptr=toupper(*upcasestr_tmp_ptr); \
-	dest; \
-})
-
 #define upcasestr(str) ({ \
 	char *upcasestr_tmp_str = (char *)alloca(strlen(str)+1); \
-	supcasestr(upcasestr_tmp_str,str); \
-})
-
-#define scommastr(str, val) (char *)({ \
-	char *scommastr_tmp_ptr; \
+	char *upcasestr_tmp_ptr; \
 \
-	if(sprintf(str, "%lld", (long long)(val))<0) \
-		CRASH; \
-	for(scommastr_tmp_ptr=strchr(str, 0)-3; scommastr_tmp_ptr > str; scommastr_tmp_ptr-=3) { \
-		memmove(scommastr_tmp_ptr+1, scommastr_tmp_ptr, strlen(scommastr_tmp_ptr)+1); \
-		*scommastr_tmp_ptr=','; \
-	} \
-	str; \
+	strcpy(upcasestr_tmp_str, str); \
+	for(upcasestr_tmp_ptr=upcasestr_tmp_str; *upcasestr_tmp_ptr; upcasestr_tmp_ptr++) \
+		*upcasestr_tmp_ptr=toupper(*upcasestr_tmp_ptr); \
+	upcasestr_tmp_str; \
 })
 
 #define commastr(val) (char *)({ \
 	char *acommastr_tmp_str = (char *)alloca(64); \
-	scommastr(acommastr_tmp_str, val); \
+	char *acommastr_tmp_ptr; \
+\
+	sprintf(acommastr_tmp_str, "%lld", (long long)(val)); \
+	for(acommastr_tmp_ptr=strchr(acommastr_tmp_str, 0)-3; acommastr_tmp_ptr > acommastr_tmp_str; acommastr_tmp_ptr-=3) { \
+		memmove(acommastr_tmp_ptr+1, acommastr_tmp_ptr, strlen(acommastr_tmp_ptr)+1); \
+		*acommastr_tmp_ptr=','; \
+	} \
 	acommastr_tmp_str; \
 })
 
 #define Asprintf(fmt, ...) (char *)({ \
 	char *Asprintf_tmp_str; \
-	int  len=snprintf(NULL, 0, fmt, __VA_ARGS__); \
+	int  len=vsnprintf(NULL, 0, fmt, __VA_ARGS__); \
 \
 	Asprintf_tmp_str=(char *)alloca(len+1); \
-	snprintf(Asprintf_tmp_str, len+1, fmt, __VA_ARGS__); \
-	Asprintf_tmp_str; \
+	vsnprintf(Asprintf_tmp_str, len+1, fmt, __VA_ARGS__); \
+	Asprintf_tmp_strAsprintf_tmp_str; \
 })
 
-#define ljust(str, len) Asprintf("%-*s", len, str)
+#define ljust(str, len) Asprintf("%.*s", len, str)
 
-#define kingstring(sex)		(sex==Male?"King":"Queen")
+#define PART(text)	d(config.textcolor, text, NULL)		// GREEN
+#define PLAYER(text)	d(config.plycolor, text, NULL)		// BRIGHT_GREEN
 
-#define MANY_MONEY(x)		((x)==1?config.moneytype2:config.moneytype3)
-
-#define SLEEP(x)		({	int sleep_msecs=x; struct timeval tv; \
-						tv.tv_sec=(sleep_msecs/1000); tv.tv_usec=((sleep_msecs%1000)*1000); \
-						select(0,NULL,NULL,NULL,&tv); })
-
-#define PART(...)	d(config.textcolor, __VA_ARGS__, NULL)			// GREEN
-#define PLAYER(...)	d(config.plycolor, __VA_ARGS__, NULL)			// BRIGHT_GREEN
-
-#define TEXT(...)	d(config.textcolor, __VA_ARGS__, NULL); nl()	// GREEN
-#define SAY(...)	d(config.talkcolor, __VA_ARGS__, NULL); nl()			// BRIGHT_MAGENTA
-#define BAD(...)	d(config.badcolor, __VA_ARGS__, NULL); nl()			// BRIGHT_RED
-#define GOOD(...)	d(config.goodcolor, __VA_ARGS__, NULL); nl()			// WHITE
-#define HEADER(...)	d(config.headercolor, __VA_ARGS__, NULL); nl()		// MAGENTA
-#define NOTICE(...)	d(config.noticecolor, __VA_ARGS__, NULL); nl()
-#define TITLE(...)	d(config.titlecolor, __VA_ARGS__, NULL); nl()
-#define EVENT(...)	d(config.eventcolor, __VA_ARGS__, NULL); nl()
+#define TEXT(text)	dl(config.textcolor, text, NULL)	// GREEN
+#define SAY(text)	dl(config.talkcolor, text, NULL)	// BRIGHT_MAGENTA
+#define BAD(text)	dl(config.badcolor, text, NULL)		// BRIGHT_RED
+#define GOOD(text)	dl(config.goodcolor, text, NULL)	// WHITE
+#define HEADER(text)	dl(config.headercolor, text, NULL)	// MAGENTA
+#define NOTICE(text)	dl(config.noticecolor, text, NULL)
+#define TITLE(text)	dl(config.titlecolor, text, NULL)
 
 #endif
--- sbbs3/websrvr.c
+++ sbbs3/websrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Web Server */
 
-/* $Id: websrvr.c,v 1.519 2009/11/11 05:30:06 rswindell Exp $ */
+/* $Id: websrvr.c,v 1.515 2009/10/05 22:20:17 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -98,11 +98,11 @@ enum {
 static scfg_t	scfg;
 static BOOL		scfg_reloaded=TRUE;
 static BOOL		http_logging_thread_running=FALSE;
-static ulong	active_clients=0;
+static DWORD	active_clients=0;
 static ulong	sockets=0;
 static BOOL		terminate_server=FALSE;
 static BOOL		terminate_http_logging_thread=FALSE;
-static ulong	thread_count=0;
+static uint		thread_count=0;
 static SOCKET	server_socket=INVALID_SOCKET;
 static SOCKET	server_socket6=INVALID_SOCKET;
 static char		revision[16];
@@ -113,12 +113,12 @@ static char		cgi_dir[MAX_PATH+1];
 static char		cgi_env_ini[MAX_PATH+1];
 static char		default_auth_list[MAX_PATH+1];
 static time_t	uptime=0;
-static ulong	served=0;
+static DWORD	served=0;
 static web_startup_t* startup=NULL;
 static js_server_props_t js_server_props;
 static str_list_t recycle_semfiles;
 static str_list_t shutdown_semfiles;
-static ulong session_threads=0;
+static int session_threads=0;
 
 static named_string_t** mime_types;
 static named_string_t** cgi_handlers;
@@ -490,11 +490,8 @@ static int lprintf(int level, const char *fmt, ...)
 	sbuf[sizeof(sbuf)-1]=0;
     va_end(argptr);
 
-	if(level <= LOG_ERR) {
-		errorlog(&scfg,startup==NULL ? NULL:startup->host_name, sbuf);
-		if(startup!=NULL && startup->errormsg!=NULL)
-			startup->errormsg(startup->cbdata,level,sbuf);
-	}
+	if(level <= LOG_ERR)
+		errorlog(&scfg,sbuf);
 
     if(startup==NULL || startup->lputs==NULL || level > startup->log_level)
         return(0);
@@ -5180,9 +5177,8 @@ static void cleanup(int code)
 	thread_down();
 	status("Down");
 	if(terminate_server || code)
-		lprintf(LOG_INFO,"#### Web Server thread terminated (%lu clients served)", served);
-	if(thread_count)
-		lprintf(LOG_WARNING,"#### !Web Server threads (%u) remain after termination", thread_count);
+		lprintf(LOG_INFO,"#### Web Server thread terminated (%u threads remain, %lu clients served)"
+			,thread_count, served);
 	if(startup!=NULL && startup->terminated!=NULL)
 		startup->terminated(startup->cbdata,code);
 }
@@ -5194,7 +5190,7 @@ const char* DLLCALL web_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.519 $", "%*s %s", revision);
+	sscanf("$Revision: 1.515 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
