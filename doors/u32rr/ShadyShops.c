44c21a457bc6b9c3359c458f68cda27ec094643a
deuce <deuce>
2009-10-22 16:54:14 +0000

    Add various.c which includes a common Display_Menu() function.

--- doors/u32rr/ShadyShops.c
+++ doors/u32rr/ShadyShops.c
@@ -24,9 +24,6 @@ Copyright 2007 Jakob Dangarden
 #include <ctype.h>
 #include <string.h>
 
-#include "Config.h"
-#include "IO.h"
-
 #include "macros.h"
 #include "files.h"
 #include "various.h"
@@ -38,25 +35,25 @@ Copyright 2007 Jakob Dangarden
 static const char *name="Shady Shops";
 static const char *expert_prompt="(B,A,G,O,S,D,R,?)";
 
-static void Meny(void *cbdata)
+static void Meny(void)
 {
 	const int offset = 25;
 
-	clr();
-	nl();
+	newscreen();
+	pbreak();
 	HEADER("-*- Shady Shops -*-");
-	nl();
+	pbreak();
 	TEXT("You stumble in to the dark areas of the town. "
 			"It is here where you can get what you want, "
 			"without any questions being asked. Trouble "
 			"is never far away in these neighbourhood.");
-	nl();
+	pbreak();
 
 	menu2(ljust("(D)rug Palace", offset));
 	menu("(S)teroid Shop");
 
 	menu2(ljust("(O)rbs Health Club", offset));
-	menu(Asprintf("(G) %s%s%s Magic Services", config.plycolor, config.groggo_name, config.textcolor));
+	menu(Asprintf("(G) %s%s%s Magic Services", uplc, config.groggo_name, config.textcol1));
 
 	menu2(ljust("(B)eer Hut", offset));
 	menu("(A)lchemists Heaven");
@@ -64,123 +61,120 @@ static void Meny(void *cbdata)
 	menu("(R)eturn to street");
 }
 
-static bool Menu(bool * refresh)
-{
-	char cho=0;
-
-	if(onliner->location != ONLOC_DarkAlley) {
-		*refresh=true;
-		onliner->location=ONLOC_DarkAlley;
-		strcpy(onliner->doing, location_desc(onliner->location));
-	}
-	// auto-travel
-	switch(player->auto_probe) {
-		case NoWhere:
-			Display_Menu(true, true, refresh, name, expert_prompt, Meny, NULL);
-			cho=toupper(gchar());
-			break;
-		case UmanCave:
-		case MainStreet:
-		case Slottet:
-		case Inn:
-		case Dormy:
-		case Prison:
-			cho='R';
-			break;
-	}
-
-	// Filter out disabled options
-	if(cho=='D' && (!config.allow_drugs)) {
-		nl();
-		BAD("Drugs are banned in this game.");
-		upause();
-		cho=' ';
-	}
-	else if(cho=='S' && (!config.allow_steroids)) {
-		nl();
-		BAD("Steroids are banned in this game.");
-		upause();
-		cho=' ';
-	}
-
-	switch(cho) {
-		case '?':
-			Display_Menu(player->expert, false, refresh, name, expert_prompt, Meny, NULL);
-			break;
-		case 'R':	// Return
-			return false;
-			break;
-		case 'O':	// Orbs drink cener
-			if((!king->shop_orbs) && (!player->king)) {
-				nl();
-				DL(config.badcolor, "Orbs Health Club is closed! (The ", upcasestr(kingstring(king->sexy)), "s order!");
-			}
-			else {
-				nl();
-				nl();
-				TEXT("You decide to enter this somewhat dubious place.");
-				Orb_Center();
-			}
-			break;
-		case 'A':	// alchemist secret order
-			if(player->class != Alchemist) {
-				nl();
-				DL(magenta, "The guards outside the building humiliate you and block the entrance.");
-				DL(magenta, "It seems as only Alchemists are allowed.");
-			}
-			else {
-				Alchemisty();
-			}
-			break;
-		case 'B':	// Bobs Beer Hut
-			if((!king->shop_bobs) && (!player->king)) {
-				nl();
-				BAD(config.bobsplace, " is closed! (The ", upcasestr(kingstring(king->sexy)), "s order!)");
-			}
-			else {
-				nl();
-				nl();
-				DL(config.textcolor, "You enter ", config.placecolor, config.bobsplace);
-				Bobs_Inn();
-			}
-			break;
-		case 'G':
-			if((!king->shop_evilmagic) && (!player->king)) {
-				nl();
-				BAD(config.groggo_name, "s place is closed! (The ", upcasestr(kingstring(king->sexy)), "s order!");
-			}
-			else {
-				Groggos_Magic();
-			}
-			break;
-		case 'D':	// Drugs
-			if((!king->shop_drugs) && (!player->king)) {
-				nl();
-				BAD("The Drug Palace is closed! (The ", upcasestr(kingstring(king->sexy)), "s order!)");
-			}
-			else {
-				Drug_Store();
-			}
-			break;
-		case 'S':	// Steroids
-			if((!king->shop_steroids) && (!player->king)) {
-				nl();
-				BAD("The Steroid Shop is closed! (The ", upcasestr(kingstring(king->sexy)), "s order!)");
-			}
-			else {
-				Steroid_Store();
-			}
-			break;
-	}
-	return true;
-}
-
 void Shady_Shops(void)
 {
+	char cho=0;
+	bool done=false;
 	bool refresh=false;
 
-	while(Menu(&refresh))
-		;
+	do {
+		if(onliner->location != ONLOC_DarkAlley) {
+			refresh=true;
+			onliner->location=ONLOC_DarkAlley;
+			strcpy(onliner->doing, location_desc(onliner->location));
+		}
+		// auto-travel
+		switch(player->auto_probe) {
+			case NoWhere:
+				Display_Menu(true, true, &refresh, name, expert_prompt, Meny);
+				cho=toupper(gchar());
+				break;
+			case UmanCave:
+			case MainStreet:
+			case Slottet:
+			case Inn:
+			case Dormy:
+			case Prison:
+				cho='R';
+				break;
+		}
 
-	nl();
+		// Filter out disabled options
+		if(cho=='D' && (!config.allow_drugs)) {
+			pbreak();
+			BAD("Drugs are banned in this game.");
+			upause();
+			cho=' ';
+		}
+		else if(cho=='S' && (!config.allow_steroids)) {
+			pbreak();
+			BAD("Steroids are banned in this game.");
+			upause();
+			cho=' ';
+		}
+
+		switch(cho) {
+			case '?':
+				if(player->expert)
+					Display_Menu(true, false, &refresh, name, expert_prompt, Meny);
+				else
+					Display_Menu(false, false, &refresh, name, expert_prompt, Meny);
+				break;
+			case 'R':	// Return
+				done=true;
+				break;
+			case 'O':	// Orbs drink cener
+				if((!king->shop_orbs) && (!player->king)) {
+					pbreak();
+					dl(config.badcolor, "Orbs Health Club is closed! (The ", upcasestr(kingstring(king->sexy)), "s order!", NULL);
+				}
+				else {
+					pbreak();
+					pbreak();
+					TEXT("You decide to enter this somewhat dubious place.");
+					Orb_Center();
+				}
+				break;
+			case 'A':	// alchemist secret order
+				if(player->class != Alchemist) {
+					pbreak();
+					dl(MAGENTA, "The guards outside the building humiliate you and block the entrance.", NULL);
+					dl(MAGENTA, "It seems as only Alchemists are allowed.", NULL);
+				}
+				else {
+					Alchemisty();
+				}
+				break;
+			case 'B':	// Bobs Beer Hut
+				if((!king->shop_bobs) && (!player->king)) {
+					pbreak();
+					BAD(config.bobsplace, " is closed! (The ", upcasestr(kingstring(king->sexy)), "s order!)");
+				}
+				else {
+					pbreak();
+					pbreak();
+					dlc(config.textcolor, "You enter ", config.placecolor, config.bobsplace, D_DONE);
+					Bobs_Inn();
+				}
+				break;
+			case 'G':
+				if((!king->shop_evilmagic) && (!player->king)) {
+					pbreak();
+					BAD(config.groggo_name, "s place is closed! (The ", upcasestr(kingstring(king->sexy)), "s order!");
+				}
+				else {
+					Groggos_Magic();
+				}
+				break;
+			case 'D':	// Drugs
+				if((!king->shop_drugs) && (!player->king)) {
+					pbreak();
+					BAD("The Drug Palace is closed! (The ", upcasestr(kingstring(king->sexy)), "s order!)");
+				}
+				else {
+					Drug_Store();
+				}
+				break;
+			case 'S':	// Steroids
+				if((!king->shop_steroids) && (!player->king)) {
+					pbreak();
+					BAD("The Steroid Shop is closed! (The ", upcasestr(kingstring(king->sexy)), "s order!)");
+				}
+				else {
+					Steroid_Store();
+				}
+				break;
+		}
+	} while(!done);
+	pbreak();
 }
