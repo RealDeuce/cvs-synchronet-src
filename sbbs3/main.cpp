cd6d8a2895df913c6e9745254890071eeee944e8
deuce <deuce>
2009-10-24 09:54:32 +0000

    Chug chug chug... Status display is working now.

--- sbbs3/main.cpp
+++ sbbs3/main.cpp
@@ -1,8 +1,8 @@
 /* main.cpp */
 
-/* Synchronet main/telnet server thread and related functions */
+/* Synchronet terminal server thread and related functions */
 
-/* $Id: main.cpp,v 1.519 2009/02/01 21:39:46 rswindell Exp $ */
+/* $Id: main.cpp,v 1.538 2009/10/24 05:03:35 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -52,7 +52,7 @@
 
 //---------------------------------------------------------------------------
 
-#define TELNET_SERVER "Synchronet Telnet Server"
+#define TELNET_SERVER "Synchronet Terminal Server"
 #define STATUS_WFC	"Listening"
 
 #define TIMEOUT_THREAD_WAIT		60			// Seconds (was 15)
@@ -152,7 +152,7 @@ static void thread_down()
 int lputs(int level, const char* str)
 {
 	if(level <= LOG_ERR)
-		errorlog(&scfg,str);
+		errorlog(&scfg,startup==NULL ? NULL:startup->host_name, str);
 
 	if(startup==NULL || startup->lputs==NULL || str==NULL || level > startup->log_level)
     	return(0);
@@ -189,8 +189,8 @@ int eprintf(int level, const char *fmt, ...)
     vsnprintf(sbuf,sizeof(sbuf),fmt,argptr);
 	sbuf[sizeof(sbuf)-1]=0;
     va_end(argptr);
-	strip_ctrl(sbuf);
-    return(startup->event_lputs(level,sbuf));
+	strip_ctrl(sbuf, sbuf);
+    return(startup->event_lputs(startup->event_cbdata,level,sbuf));
 }
 
 SOCKET open_socket(int type, const char* protocol)
@@ -230,7 +230,7 @@ int close_socket(SOCKET sock)
 	if(startup!=NULL && startup->socket_open!=NULL)
 		startup->socket_open(startup->cbdata,FALSE);
 	if(result!=0 && ERROR_VALUE!=ENOTSOCK)
-		lprintf(LOG_ERR,"!ERROR %d closing socket %d",ERROR_VALUE,sock);
+		lprintf(LOG_WARNING,"!ERROR %d closing socket %d",ERROR_VALUE,sock);
 	return(result);
 }
 
@@ -669,9 +669,9 @@ js_log(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 		rc=JS_SUSPENDREQUEST(cx);
 		if(sbbs->online==ON_LOCAL) {
 			if(startup!=NULL && startup->event_lputs!=NULL && level <= startup->log_level)
-				startup->event_lputs(level,JS_GetStringBytes(str));
+				startup->event_lputs(startup->event_cbdata,level,JS_GetStringBytes(str));
 		} else
-			lputs(level,JS_GetStringBytes(str));
+			lprintf(level,"Node %d %s", sbbs->cfg.node_num, JS_GetStringBytes(str));
 		JS_RESUMEREQUEST(cx, rc);
 	}
 
@@ -976,12 +976,18 @@ js_ErrorReporter(JSContext *cx, const char *message, JSErrorReport *report)
 	const char*	warning;
 	jsrefcount	rc;
 	int		log_level;
+	char	nodestr[128];
 
 	if((sbbs=(sbbs_t*)JS_GetContextPrivate(cx))==NULL)
 		return;
+
+    if(sbbs->cfg.node_num)
+    	SAFEPRINTF(nodestr,"Node %d",sbbs->cfg.node_num);
+    else
+    	SAFECOPY(nodestr,sbbs->client_name);
 	
 	if(report==NULL) {
-		lprintf(LOG_ERR,"!JavaScript: %s", message);
+		lprintf(LOG_ERR,"%s !JavaScript: %s", nodestr, message);
 		return;
     }
 
@@ -1010,7 +1016,7 @@ js_ErrorReporter(JSContext *cx, const char *message, JSErrorReport *report)
 	if(sbbs->online==ON_LOCAL) 
 		eprintf(log_level,"!JavaScript %s%s%s: %s",warning,file,line,message);
 	else {
-		lprintf(log_level,"!JavaScript %s%s%s: %s",warning,file,line,message);
+		lprintf(log_level,"%s !JavaScript %s%s%s: %s",nodestr,warning,file,line,message);
 		sbbs->bprintf("!JavaScript %s%s%s: %s\r\n",warning,file,line,message);
 	}
 	JS_RESUMEREQUEST(cx, rc);
@@ -1060,6 +1066,7 @@ bool sbbs_t::js_init(ulong* stack_frame)
 		if((js_glob=js_CreateCommonObjects(js_cx, &scfg, &cfg, js_global_functions
 					,uptime, startup->host_name, SOCKLIB_DESC	/* system */
 					,&js_branch									/* js */
+					,&startup->js
 					,&client, client_socket						/* client */
 					,&js_server_props							/* server */
 			))==NULL)
@@ -1123,7 +1130,7 @@ void sbbs_t::js_create_user_objects(void)
 		return;
 	
 	JS_BEGINREQUEST(js_cx);
-	if(!js_CreateUserObjects(js_cx, js_glob, &cfg, &useron, NULL, subscan)) 
+	if(!js_CreateUserObjects(js_cx, js_glob, &cfg, &useron, &client, NULL, subscan)) 
 		lprintf(LOG_ERR,"!JavaScript ERROR creating user objects");
 	JS_ENDREQUEST(js_cx);
 }
@@ -1557,7 +1564,7 @@ void input_thread(void *arg)
 				if(err==CRYPT_ERROR_TIMEOUT)
 					continue;
 				/* Handle the SSH error here... */
-				lprintf(LOG_ERR,"Node %d !ERROR %d receiving on Cryptlib session", sbbs->cfg.node_num, err);
+				lprintf(LOG_WARNING,"Node %d !ERROR %d receiving on Cryptlib session", sbbs->cfg.node_num, err);
 				break;
 			}
 			else {
@@ -1977,7 +1984,7 @@ void output_thread(void* arg)
 			 * into linear buffer.
 			 */
            	if(avail>sizeof(buf)) {
-               	lprintf(LOG_WARNING,"!%s: Insufficient linear output buffer (%lu > %lu)"
+               	lprintf(LOG_WARNING,"%s !Insufficient linear output buffer (%lu > %lu)"
 					,node, avail, sizeof(buf));
                	avail=sizeof(buf);
            	}
@@ -1993,12 +2000,14 @@ void output_thread(void* arg)
 		tv.tv_usec=1000;
 
 		FD_ZERO(&socket_set);
+		if(sbbs->client_socket==INVALID_SOCKET)		// Make the race condition less likely to actually happen... TODO: Fix race
+			continue;
 		FD_SET(sbbs->client_socket,&socket_set);
 
 		i=select(sbbs->client_socket+1,NULL,&socket_set,NULL,&tv);
 		if(i==SOCKET_ERROR) {
 			if(sbbs->client_socket!=INVALID_SOCKET)
-				lprintf(LOG_ERR,"!%s: ERROR %d selecting socket %u for send"
+				lprintf(LOG_ERR,"%s !ERROR %d selecting socket %u for send"
 					,node,ERROR_VALUE,sbbs->client_socket);
 			if(sbbs->cfg.node_num)	/* Only break if node output (not server) */
 				break;
@@ -2015,7 +2024,7 @@ void output_thread(void* arg)
 			int err;
 			if(!cryptStatusOK((err=cryptPushData(sbbs->ssh_session, (char*)buf+bufbot, buftop-bufbot, &i)))) {
 				/* Handle the SSH error here... */
-				lprintf(LOG_ERR,"!%s: ERROR %d sending on Cryptlib session", node, err);
+				lprintf(LOG_WARNING,"%s !ERROR %d sending on Cryptlib session", node, err);
 				i=-1;
 				sbbs->online=FALSE;
 				i=buftop-bufbot;	// Pretend we sent it all
@@ -2034,7 +2043,7 @@ void output_thread(void* arg)
             else if(ERROR_VALUE==ECONNABORTED) 
 				lprintf(LOG_NOTICE,"%s connection aborted by peer on send", node);
 			else
-				lprintf(LOG_WARNING,"!%s: ERROR %d sending on socket %d"
+				lprintf(LOG_WARNING,"%s !ERROR %d sending on socket %d"
                 	,node, ERROR_VALUE, sbbs->client_socket);
 			sbbs->online=FALSE;
 			/* was break; on 4/7/00 */
@@ -2061,7 +2070,7 @@ void output_thread(void* arg)
 		}
 
 		if(i!=(int)(buftop-bufbot)) {
-			lprintf(LOG_WARNING,"!%s: Short socket send (%u instead of %u)"
+			lprintf(LOG_WARNING,"%s !Short socket send (%u instead of %u)"
 				,node, i ,buftop-bufbot);
 			short_sends++;
 		}
@@ -2289,7 +2298,7 @@ void event_thread(void* arg)
 
 					if(sbbs->useron.number
 						&& !(sbbs->useron.misc&(DELETED|INACTIVE))	 /* Pre-QWK */
-						&& sbbs->chk_ar(sbbs->cfg.preqwk_ar,&sbbs->useron)) { 
+						&& sbbs->chk_ar(sbbs->cfg.preqwk_ar,&sbbs->useron,/* client: */NULL)) { 
 						for(k=1;k<=sbbs->cfg.sys_nodes;k++) {
 							if(sbbs->getnodedat(k,&node,0)!=0)
 								continue;
@@ -2576,7 +2585,9 @@ void event_thread(void* arg)
 				&& (now_tm.tm_mday!=tm.tm_mday || now_tm.tm_mon!=tm.tm_mon)))
 				&& sbbs->cfg.event[i]->days&(1<<now_tm.tm_wday)
 				&& (sbbs->cfg.event[i]->mdays==0 
-					|| sbbs->cfg.event[i]->mdays&(1<<now_tm.tm_mday)))) 
+					|| sbbs->cfg.event[i]->mdays&(1<<now_tm.tm_mday))
+				&& (sbbs->cfg.event[i]->months==0
+					|| sbbs->cfg.event[i]->months&(1<<now_tm.tm_mon)))) 
 			{
 				if(sbbs->cfg.event[i]->misc&EVENT_EXCL) { /* exclusive event */
 
@@ -2716,10 +2727,15 @@ void event_thread(void* arg)
 						ex_mode |= EX_SH;
 					ex_mode|=(sbbs->cfg.event[i]->misc&EX_NATIVE);
 					sbbs->online=ON_LOCAL;
-					sbbs->external(
-						 sbbs->cmdstr(sbbs->cfg.event[i]->cmd,nulstr,sbbs->cfg.event[i]->dir,NULL)
-						,ex_mode
-						,sbbs->cfg.event[i]->dir);
+					{
+						int result=
+						sbbs->external(
+							 sbbs->cmdstr(sbbs->cfg.event[i]->cmd,nulstr,sbbs->cfg.event[i]->dir,NULL)
+							,ex_mode
+							,sbbs->cfg.event[i]->dir);
+						if(!(ex_mode&EX_BG))
+							eprintf(LOG_INFO,"Timed event: %s returned %d",strupr(str), result);
+					}
 					sbbs->cfg.event[i]->last=time(NULL);
 					SAFEPRINTF(str,"%stime.dab",sbbs->cfg.ctrl_dir);
 					if((file=sbbs->nopen(str,O_WRONLY))==-1) {
@@ -2746,9 +2762,9 @@ void event_thread(void* arg)
 		mswait(1000);
 	}
 	sbbs->cfg.node_num=0;
-    sbbs->event_thread_running = false;
-
 	sbbs->js_cleanup(sbbs->client_name);
+
+    sbbs->event_thread_running = false;
 
 	thread_down();
 	eprintf(LOG_INFO,"BBS Events thread terminated");
@@ -2854,6 +2870,7 @@ sbbs_t::sbbs_t(ushort node_num, DWORD addr, const char* name, SOCKET sd,
 	telnet_last_rxch=0;
 
 	sys_status=lncntr=tos=criterrs=slcnt=0L;
+	column=0;
 	curatr=LIGHTGRAY;
 	attr_sp=0;	/* attribute stack pointer */
 	errorlevel=0;
@@ -3512,7 +3529,7 @@ void sbbs_t::hangup(void)
 {
 	if(online) {
 		lprintf(LOG_DEBUG,"Node %d disconnecting client", cfg.node_num);
-		online=FALSE;	// moved from the bottom of this fucntion on Jan-25-2009
+		online=FALSE;	// moved from the bottom of this function on Jan-25-2009
 	}
 	if(client_socket_dup!=INVALID_SOCKET && client_socket_dup!=client_socket)
 		closesocket(client_socket_dup);
@@ -3555,14 +3572,16 @@ int sbbs_t::outcom(uchar ch)
 	return(0);
 }
 
-void sbbs_t::putcom(const char *str, int len)
+int sbbs_t::putcom(const char *str, size_t len)
 {
-	int i;
+	size_t i;
 
-    if(!len)
-    	len=strlen(str);
-    for(i=0;i<len && online; i++)
-        outcom(str[i]);
+	if(!len)
+		len=strlen(str);
+    for(i=0;i<len && online;i++)
+        if(outcom(str[i])!=0)
+			break;
+	return i;
 }
 
 /* Legacy Remote I/O Control Interface */
@@ -4102,7 +4121,7 @@ void sbbs_t::daily_maint(void)
 			> sbbs->cfg.sys_autodel)) {			/* Inactive too long */
 			SAFEPRINTF2(str,"Auto-Deleted %s #%u",user.alias,user.number);
 			sbbs->logentry("!*",str);
-			sbbs->delallmail(user.number);
+			sbbs->delallmail(user.number, MAIL_ANY);
 			putusername(&sbbs->cfg,user.number,nulstr);
 			putuserrec(&sbbs->cfg,user.number,U_MISC,8,ultoa(user.misc|DELETED,str,16)); 
 		}
@@ -4868,9 +4887,9 @@ NO_SSH:
 			if(i==0)
 				continue;
 			if(ERROR_VALUE==EINTR)
-				lprintf(LOG_DEBUG,"Telnet Server listening interrupted");
+				lprintf(LOG_DEBUG,"Terminal Server listening interrupted");
 			else if(ERROR_VALUE == ENOTSOCK)
-            	lprintf(LOG_NOTICE,"Telnet Server sockets closed");
+            	lprintf(LOG_NOTICE,"Terminal Server sockets closed");
 			else
 				lprintf(LOG_WARNING,"!ERROR %d selecting sockets",ERROR_VALUE);
 			continue;
@@ -5067,11 +5086,14 @@ NO_SSH:
 		else
 			host_name="<no name>";
 
+#if	0 /* gethostbyaddr() is apparently not (always) thread-safe
+	     and getnameinfo() doesn't return alias information */
 		if(!(startup->options&BBS_OPT_NO_HOST_LOOKUP)) {
 			lprintf(LOG_INFO,"%04d Hostname: %s", client_socket, host_name);
 			for(i=0;h!=NULL && h->h_aliases!=NULL && h->h_aliases[i]!=NULL;i++)
 				lprintf(LOG_INFO,"%04d HostAlias: %s", client_socket, h->h_aliases[i]);
 		}
+#endif
 
 		if(sbbs->trashcan(host_name,"host")) {
 			SSH_END();
@@ -5161,7 +5183,7 @@ NO_SSH:
 			SAFECOPY(new_node->client_ident,identity);
 
 		if(new_node->init()==false) {
-			lprintf(LOG_INFO,"%04d !Node %d Initialization failure"
+			lprintf(LOG_INFO,"%04d Node %d !Initialization failure"
 				,client_socket,new_node->cfg.node_num);
 			SAFEPRINTF(str,"%snonodes.txt",scfg.text_dir);
 			if(fexist(str))
@@ -5395,6 +5417,3 @@ NO_PASSTHRU:
 	} while(!terminate_server);
 
 }
-
-
-
