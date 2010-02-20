14ff3a1edf1266f24020ad7f2db77d0b826fdfa6
rswindell <rswindell>
2010-02-20 03:23:29 +0000

    CGI optimization: don't re-read the ctrl/cgi_env.ini file for each request (just upon [re]init). The cgi_env.ini file is now monitored to force recycle. Removed hard-coded CGI environment kludge for PHP-CGI and moved to default ctrl/cgi_env.ini. If you're using PHP-CGI, you'll want to add the following to your cgi_env.ini file: [REDIRECT_STATUS] value=200

--- sbbs3/qwk.h
+++ sbbs3/qwk.h
@@ -2,13 +2,13 @@
 
 /* Synchronet QWK-related structures, constants, and prototypes */ 
 
-/* $Id: qwk.h,v 1.5 2011/07/21 11:28:23 rswindell Exp $ */
+/* $Id: qwk.h,v 1.4 2002/11/10 01:44:26 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -37,7 +37,6 @@
 
 #define QWK_NEWLINE		'\xe3'	/* QWK line terminator */
 #define QWK_BLOCK_LEN	128
-#define QWK_HFIELD_LEN	25		/* Header field (To/From/Subject) length */
 
 /* QWK mode bits */
 #define QM_TAGLINE 	(1<<5)	/* Place tagline at end of qwk message */
