adc58b5be29398e542dff3ba47ba5bff95e873e8
rswindell <rswindell>
2010-02-25 02:45:42 +0000

    Fix some signed vs. unsigned 32-bit values (considering testing > 2GB file xfers). Bug-fix in zmodem_recv_file_data(), if receiving a file larger than the size specified in the ZFILE data block wouldn't work and couldn't receive a 0 byte file (would never send the ZRPOS frame).

--- sbbs3/exec.cpp
+++ sbbs3/exec.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet command shell/module interpretter */
 
-/* $Id: exec.cpp,v 1.89 2010/03/15 18:42:38 rswindell Exp $ */
+/* $Id: exec.cpp,v 1.83 2009/12/17 00:57:16 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -560,9 +560,8 @@ static const char* js_ext(const char* fname)
 	return("");
 }
 
-long sbbs_t::js_execfile(const char *cmd, const char* startup_dir)
+long sbbs_t::js_execfile(const char *cmd)
 {
-	ulong		stack_frame;
 	char*		p;
 	char*		args=NULL;
 	char*		fname;
@@ -573,21 +572,12 @@ long sbbs_t::js_execfile(const char *cmd, const char* startup_dir)
 	JSScript*	js_script=NULL;
 	jsval		rval;
 	int32		result=0;
-	JSRuntime	*old_runtime=js_runtime;
-	JSContext	*old_context=js_cx;
-	JSObject	*old_glob=js_glob;
-	js_branch_t	old_branch;
-
-	memcpy(&old_branch, &js_branch, sizeof(old_branch));
-
-	js_init(&stack_frame);
-	js_create_user_objects();
-
+	BOOL		auto_terminate = js_branch.auto_terminate;
+	
 	if(js_cx==NULL) {
 		errormsg(WHERE,ERR_CHK,"JavaScript support",0);
 		errormsg(WHERE,ERR_EXEC,cmd,0);
-		result=-1;
-		goto reset_js;
+		return(-1);
 	}
 
 	SAFECOPY(cmdline,cmd);
@@ -598,22 +588,16 @@ long sbbs_t::js_execfile(const char *cmd, const char* startup_dir)
 	}
 	fname=cmdline;
 
-	path[0]=0;
 	if(strcspn(fname,"/\\")==strlen(fname)) {
-		if(startup_dir!=NULL && *startup_dir)
-			SAFEPRINTF3(path,"%s%s%s",startup_dir,fname,js_ext(fname));
-		if(path[0]==0 || !fexistcase(path)) {
-			SAFEPRINTF3(path,"%s%s%s",cfg.mods_dir,fname,js_ext(fname));
-			if(cfg.mods_dir[0]==0 || !fexistcase(path))
-				SAFEPRINTF3(path,"%s%s%s",cfg.exec_dir,fname,js_ext(fname));
-		}
+		SAFEPRINTF3(path,"%s%s%s",cfg.mods_dir,fname,js_ext(fname));
+		if(cfg.mods_dir[0]==0 || !fexistcase(path))
+			SAFEPRINTF3(path,"%s%s%s",cfg.exec_dir,fname,js_ext(fname));
 	} else
 		SAFECOPY(path,fname);
 
 	if(!fexistcase(path)) {
 		errormsg(WHERE,ERR_OPEN,path,O_RDONLY);
-		result=-1;
-		goto reset_js;
+		return(-1); 
 	}
 
 	JS_BEGINREQUEST(js_cx);
@@ -657,15 +641,14 @@ long sbbs_t::js_execfile(const char *cmd, const char* startup_dir)
 		JS_ReportPendingException(js_cx);	/* Added Feb-2-2006, rswindell */
 		JS_ENDREQUEST(js_cx);
 		errormsg(WHERE,"compiling",path,0);
-		result=-1;
-		goto reset_js;
+		return(-1);
 	}
 
 	js_branch.counter=0;	// Reset loop counter
 
 	JS_SetBranchCallback(js_cx, js_BranchCallback);
 
-	js_PrepareToExecute(js_cx, js_glob, path, startup_dir);
+	js_PrepareToExecute(js_cx, js_glob, path);
 	JS_ExecuteScript(js_cx, js_scope, js_script, &rval);
 
 	JS_GetProperty(js_cx, js_scope, "exit_code", &rval);
@@ -684,20 +667,15 @@ long sbbs_t::js_execfile(const char *cmd, const char* startup_dir)
 
 	JS_ENDREQUEST(js_cx);
 
-reset_js:
-	js_cleanup(client_name);
-	js_runtime=old_runtime;
-	js_cx=old_context;
-	js_glob=old_glob;
-
-	memcpy(&js_branch, &old_branch, sizeof(old_branch));
-
+	// Restore saved auto_terminate state
+	js_branch.auto_terminate = auto_terminate;
+	
 	return(result);
 }
 #endif
 
 /* Important change as of Nov-16-2006, 'cmdline' may contain args */
-long sbbs_t::exec_bin(const char *cmdline, csi_t *csi, const char* startup_dir)
+long sbbs_t::exec_bin(const char *cmdline, csi_t *csi)
 {
     char    str[MAX_PATH+1];
 	char	mod[MAX_PATH+1];
@@ -719,16 +697,11 @@ long sbbs_t::exec_bin(const char *cmdline, csi_t *csi, const char* startup_dir)
 
 #ifdef JAVASCRIPT
 	if((p=getfext(mod))!=NULL && stricmp(p,".js")==0)
-		return(js_execfile(cmdline, startup_dir));
-	if(p==NULL && startup_dir!=NULL && *startup_dir) {
-		SAFEPRINTF2(str,"%s%s.js", startup_dir, mod);
-		if(fexistcase(str))
-			return(js_execfile(cmdline, startup_dir));
-	}
+		return(js_execfile(cmdline));
 	if(cfg.mods_dir[0]) {
 		SAFEPRINTF2(str,"%s%s.js",cfg.mods_dir,mod);
 		if(fexistcase(str)) 
-			return(js_execfile(cmdline, startup_dir));
+			return(js_execfile(cmdline));
 	}
 #endif
 
@@ -742,7 +715,7 @@ long sbbs_t::exec_bin(const char *cmdline, csi_t *csi, const char* startup_dir)
 #ifdef JAVASCRIPT
 		SAFEPRINTF2(str,"%s%s.js",cfg.exec_dir,mod);
 		if(fexistcase(str)) 
-			return(js_execfile(cmdline, startup_dir));
+			return(js_execfile(cmdline));
 #endif
 
 		SAFEPRINTF2(str,"%s%s",cfg.exec_dir,modname);
@@ -755,7 +728,7 @@ long sbbs_t::exec_bin(const char *cmdline, csi_t *csi, const char* startup_dir)
 
 	memcpy(&bin,csi,sizeof(csi_t));
 	clearvars(&bin);
-	bin.length=(uint32_t)filelength(file);
+	bin.length=filelength(file);
 	if((bin.cs=(uchar *)malloc(bin.length))==NULL) {
 		close(file);
 		errormsg(WHERE,ERR_ALLOC,str,bin.length);
@@ -1272,7 +1245,7 @@ int sbbs_t::exec(csi_t *csi)
 				external(cmdstr((char*)csi->ip,path,csi->str,(char*)buf),0);
 				break;
 			case CS_EXEC_INT:
-				external(cmdstr((char*)csi->ip,path,csi->str,(char*)buf),EX_STDIO);
+				external(cmdstr((char*)csi->ip,path,csi->str,(char*)buf),EX_OUTR|EX_INR|EX_OUTL);
 				break;
 			case CS_EXEC_XTRN:
 				for(i=0;i<cfg.total_xtrns;i++)
@@ -1282,7 +1255,7 @@ int sbbs_t::exec(csi_t *csi)
 					exec_xtrn(i);
 				break;
 			case CS_EXEC_BIN:
-				exec_bin(cmdstr((char*)csi->ip,path,csi->str,(char*)buf),csi,/* startup_dir: */NULL);
+				exec_bin(cmdstr((char*)csi->ip,path,csi->str,(char*)buf),csi);
 				break;
 			case CS_YES_NO:
 				csi->logic=!yesno(cmdstr((char*)csi->ip,path,csi->str,(char*)buf));
