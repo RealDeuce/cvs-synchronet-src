ee7fb8ac1c93308726309a0cd4bc5b8a0150b30a
deuce <deuce>
2010-03-03 06:49:18 +0000

    Fix recv_upto()

--- syncterm/conn.c
+++ syncterm/conn.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: conn.c,v 1.57 2010/03/03 06:34:10 deuce Exp $ */
+/* $Id: conn.c,v 1.58 2010/03/03 06:49:18 deuce Exp $ */
 
 #include <stdlib.h>
 
@@ -245,12 +245,11 @@ BOOL conn_connected(void)
 
 int conn_recv_upto(char *buffer, size_t buflen, unsigned timeout)
 {
-	size_t found;
+	size_t	found;
 
 	pthread_mutex_lock(&(conn_inbuf.mutex));
-	found=conn_buf_wait_bytes(&conn_inbuf, 1, timeout);
-	if(found)
-		found=conn_buf_get(&conn_inbuf, buffer, found);
+	if(conn_buf_wait_bytes(&conn_inbuf, 1, timeout))
+		found=conn_buf_get(&conn_inbuf, buffer, buflen);
 	pthread_mutex_unlock(&(conn_inbuf.mutex));
 	return(found);
 }
