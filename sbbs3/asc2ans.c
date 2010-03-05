de117a46ff56160c04081b4704ddb5a859d822b7
rswindell <rswindell>
2010-03-05 03:30:53 +0000

    Define int64_t and use it for return type of flength() to support files >2G in size (or >4G-1byte depending on how the return value was used). This is going to trigger a bunch of warnings like this: conversion from '__int64 ' to 'unsigned long ', possible loss of data which will be fixed over type (either with typecasts or by supporting 64-bit file sizes, as the case may be).

--- sbbs3/asc2ans.c
+++ sbbs3/asc2ans.c
@@ -2,13 +2,13 @@
 
 /* Converts Synchronet Ctrl-A codes into ANSI escape sequences */
 
-/* $Id: asc2ans.c,v 1.4 2006/05/09 23:03:09 deuce Exp $ */
+/* $Id: asc2ans.c,v 1.5 2009/02/16 02:52:45 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2003 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -36,10 +36,11 @@
  ****************************************************************************/
 
 #include <stdio.h>
-#include <ctype.h>	/* toupper */
-#include <string.h>	/* strcmp */
+#include <ctype.h>		/* toupper */
+#include <string.h>		/* strcmp */
 
-#define ANSI fprintf(out,"\x1b[")
+#define CTRL_A	'\1'
+#define ANSI	fprintf(out,"\x1b[")
 
 int main(int argc, char **argv)
 {
@@ -48,7 +49,7 @@ int main(int argc, char **argv)
 	FILE*	in;
 	FILE*	out;
 
-	sscanf("$Revision: 1.4 $", "%*s %s", revision);
+	sscanf("$Revision: 1.5 $", "%*s %s", revision);
 
 	if(argc<2) {
 		fprintf(stderr,"\nasc2ans %s\n",revision);
@@ -75,13 +76,16 @@ int main(int argc, char **argv)
 		out=stdout;
 
 	while((ch=fgetc(in))!=EOF) {
-		if(ch==1) { /* ctrl-a */
+		if(ch==CTRL_A) { /* ctrl-a */
 			ch=fgetc(in);
-			if(ch==EOF)
+			if(ch==EOF || toupper(ch)=='Z')	/* EOF */
 				break;
-			if(ch>=0x7f) {					/* move cursor right x columns */
+			if(ch>0x7f) {					/* move cursor right x columns */
+				int cnt=ch-0x7f;
 				ANSI;
-				fprintf(out,"%uC",ch-0x7f);
+				if(cnt>1)
+					fprintf(out,"%u",cnt);
+				fputc('C',out);
 				continue; 
 			}
 			switch(toupper(ch)) {
@@ -102,8 +106,10 @@ int main(int argc, char **argv)
 					fputc('\n',out);
 					break;
 				case 'L':
-					ANSI;
-					fprintf(out,"2J");
+					ANSI;	
+					fprintf(out,"2J");	/* clear screen */
+					ANSI;	
+					fprintf(out,"H");	/* home cursor */
 					break;
 				case '-':
 				case '_':
--- syncterm/term.c
+++ syncterm/term.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: term.c,v 1.266 2008/12/04 03:38:06 deuce Exp $ */
+/* $Id: term.c,v 1.282 2010/03/05 02:29:48 deuce Exp $ */
 
 #include <genwrap.h>
 #include <ciolib.h>
@@ -260,15 +260,20 @@ static BOOL zmodem_check_abort(void* vp)
 {
 	struct zmodem_cbdata	*zcb=(struct zmodem_cbdata *)vp;
 	zmodem_t*				zm=zcb->zm;
+	static time_t			last_check=0;
+	time_t					now=time(NULL);
 
-	if(zm!=NULL && kbhit()) {
-		switch(getch()) {
-			case ESC:
-			case CTRL_C:
-			case CTRL_X:
-				zm->cancelled=TRUE;
-				zm->local_abort=TRUE;
-				break;
+	if(last_check != now) {
+		last_check=now;
+		if(zm!=NULL && kbhit()) {
+			switch(getch()) {
+				case ESC:
+				case CTRL_C:
+				case CTRL_X:
+						zm->cancelled=TRUE;
+					zm->local_abort=TRUE;
+					break;
+			}
 		}
 	}
 	return(zm->cancelled);
@@ -350,15 +355,17 @@ void zmodem_progress(void* cbdata, uint32_t current_pos)
 	time_t		l;
 	time_t		t;
 	time_t		now;
-	static time_t last_progress;
+	static time_t last_progress=0;
 	int			old_hold=hold_update;
 	struct zmodem_cbdata *zcb=(struct zmodem_cbdata *)cbdata;
 	zmodem_t*	zm=zcb->zm;
-
-	zmodem_check_abort(cbdata);
+	BOOL		growing=FALSE;
 
 	now=time(NULL);
-	if(now-last_progress>0 || current_pos >= zm->current_file_size) {
+	if(current_pos > zm->current_file_size)
+		growing=TRUE;
+	if(now != last_progress || (current_pos >= zm->current_file_size && growing==FALSE)) {
+		zmodem_check_abort(cbdata);
 		hold_update = TRUE;
 		window(((trans_ti.screenwidth-TRANSFER_WIN_WIDTH)/2)+2
 				, ((trans_ti.screenheight-TRANSFER_WIN_HEIGHT)/2)+1
@@ -400,9 +407,15 @@ void zmodem_progress(void* cbdata, uint32_t current_pos)
 			);
 		clreol();
 		cputs("\r\n");
-		cprintf("%*s%3d%%\r\n", TRANSFER_WIN_WIDTH/2-5, ""
-			,(long)(((float)current_pos/(float)zm->current_file_size)*100.0));
-		l = (long)(60*((float)current_pos/(float)zm->current_file_size));
+		if(zm->current_file_size==0) {
+			cprintf("%*s%3d%%\r\n", TRANSFER_WIN_WIDTH/2-5, "", 100);
+			l = 60;
+		}
+		else{
+			cprintf("%*s%3d%%\r\n", TRANSFER_WIN_WIDTH/2-5, ""
+				,(long)(((float)current_pos/(float)zm->current_file_size)*100.0));
+			l = (long)(60*((float)current_pos/(float)zm->current_file_size));
+		}
 		cprintf("[%*.*s%*s]", l, l, 
 				"\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1"
 				"\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1"
@@ -421,21 +434,58 @@ void zmodem_progress(void* cbdata, uint32_t current_pos)
 #if defined(__BORLANDC__)
 	#pragma argsused
 #endif
+
+unsigned char transfer_buffer[BUFFER_SIZE/2];
+unsigned transfer_buf_len=0;
+
+static void flush_send(void *unused)
+{
+	int	sent;
+
+	sent=conn_send(transfer_buffer, transfer_buf_len, 120*1000);
+	if(sent < transfer_buf_len) {
+		memmove(transfer_buffer, transfer_buffer+sent, transfer_buf_len-sent);
+		transfer_buf_len -= sent;
+	}
+	else
+		transfer_buf_len=0;
+}
+
 static int send_byte(void* unused, uchar ch, unsigned timeout /* seconds */)
 {
-	return(conn_send(&ch,sizeof(ch),timeout*1000)!=1);
+	transfer_buffer[transfer_buf_len++]=ch;
+	if(transfer_buf_len==sizeof(transfer_buffer))
+		flush_send(unused);
+	return(!(transfer_buf_len < sizeof(transfer_buffer)));
 }
 
 #if defined(__BORLANDC__)
 	#pragma argsused
 #endif
+BYTE	recv_byte_buffer[BUFFER_SIZE];
+unsigned recv_byte_buffer_len=0;
+unsigned recv_byte_buffer_pos=0;
+
+static void recv_bytes(unsigned timeout /* Milliseconds */)
+{
+	if(recv_byte_buffer_len == 0)
+		recv_byte_buffer_len=conn_recv_upto(recv_byte_buffer, sizeof(recv_byte_buffer), timeout);
+}
+
 static int recv_byte(void* unused, unsigned timeout /* seconds */)
 {
-	BYTE	ch;
+	BYTE ch;
 
-	if(conn_recv(&ch, sizeof(ch), timeout*1000))
-		return(ch);
-	return(-1);
+	recv_bytes(timeout*1000);
+
+	if(recv_byte_buffer_len > 0) {
+		ch=recv_byte_buffer[recv_byte_buffer_pos++];
+		if(recv_byte_buffer_pos == recv_byte_buffer_len)
+			recv_byte_buffer_len=recv_byte_buffer_pos=0;
+		return ch;
+	}
+
+	return -1;
 }
 
 #if defined(__BORLANDC__)
@@ -443,7 +493,15 @@ static int recv_byte(void* unused, unsigned timeout /* seconds */)
 #endif
 BOOL data_waiting(void* unused, unsigned timeout)
 {
+	if(recv_byte_buffer_len)
+		return TRUE;
 	return(conn_data_waiting()!=0);
+}
+
+size_t count_data_waiting(void)
+{
+	recv_bytes(0);
+	return recv_byte_buffer_len;
 }
 
 void draw_transfer_window(char* title)
@@ -702,6 +760,8 @@ void begin_download(struct bbslist *bbs)
 #endif
 static BOOL is_connected(void* unused)
 {
+	if(recv_byte_buffer_len)
+		return TRUE;
 	return(conn_connected());
 }
 
@@ -740,14 +800,14 @@ static int guts_send_byte(void* cbdata, uchar ch, unsigned timeout)
 
 static int guts_recv_byte(void* cbdata, unsigned timeout)
 {
-	BOOL	data_waiting;
+	BOOL	data_is_waiting;
 	BYTE	ch;
 	struct GUTS_info *gi=cbdata;
 
-	if(!socket_check(gi->oob_socket, &data_waiting, NULL, timeout*1000))
+	if(!socket_check(gi->oob_socket, &data_is_waiting, NULL, timeout*1000))
 		return(-1);
 
-	if(!data_waiting)
+	if(!data_is_waiting)
 		return(-1);
 
 	if(recv(gi->oob_socket,&ch,1,0)!=1)
@@ -781,12 +841,15 @@ void guts_background_download(void *cbdata)
 
 	zmodem_mode=ZMODEM_MODE_RECV;
 
+	transfer_buf_len=0;
 	zmodem_init(&zm
 		,&gi
 		,guts_lputs, guts_zmodem_progress
 		,guts_send_byte,guts_recv_byte,guts_is_connected
 		,NULL /* is_cancelled */
-		,guts_data_waiting);
+		,guts_data_waiting
+		,guts_flush_send);
+	zm.log_level=&log_level;
 
 	/* ToDo: This would be a good time to detach or something. */
 	zmodem_recv_files(&zm,gi.files[0],&bytes_received);
@@ -812,12 +875,15 @@ void guts_background_upload(void *cbdata)
 
 	zmodem_mode=ZMODEM_MODE_SEND;
 
+	transfer_buf_len=0;
 	zmodem_init(&zm
 		,&gi
 		,guts_lputs, guts_zmodem_progress
 		,guts_send_byte,guts_recv_byte,guts_is_connected
 		,NULL /* is_cancelled */
-		,guts_data_waiting);
+		,guts_data_waiting
+		,guts_flush_send);
+	zm.log_level=&log_level;
 
 	zm.current_file_num = zm.total_files = 1;	/* ToDo: support multi-file/batch uploads */
 
@@ -972,13 +1038,16 @@ void zmodem_upload(struct bbslist *bbs, FILE *fp, char *path)
 	cbdata.zm=&zm;
 	cbdata.bbs=bbs;
 	conn_binary_mode_on();
+	transfer_buf_len=0;
 	zmodem_init(&zm
 		,/* cbdata */&cbdata
 		,lputs, zmodem_progress
 		,send_byte,recv_byte
 		,is_connected
 		,zmodem_check_abort
-		,data_waiting);
+		,data_waiting
+		,flush_send);
+	zm.log_level=&log_level;
 
 	zm.current_file_num = zm.total_files = 1;	/* ToDo: support multi-file/batch uploads */
 	
@@ -1078,13 +1147,16 @@ void zmodem_download(struct bbslist *bbs)
 	conn_binary_mode_on();
 	cbdata.zm=&zm;
 	cbdata.bbs=bbs;
+	transfer_buf_len=0;
 	zmodem_init(&zm
 		,/* cbdata */&cbdata
 		,lputs, zmodem_progress
 		,send_byte,recv_byte
 		,is_connected
 		,zmodem_check_abort
-		,data_waiting);
+		,data_waiting
+		,flush_send);
+	zm.log_level=&log_level;
 
 	zm.duplicate_filename=zmodem_duplicate_callback;
 
@@ -1111,13 +1183,19 @@ ulong	block_num;						/* Block number 					*/
 static BOOL xmodem_check_abort(void* vp)
 {
 	xmodem_t* xm = (xmodem_t*)vp;
-	if(xm!=NULL && kbhit()) {
-		switch(getch()) {
-			case ESC:
-			case CTRL_C:
-			case CTRL_X:
-				xm->cancelled=TRUE;
-				break;
+	static time_t			last_check=0;
+	time_t					now=time(NULL);
+
+	if(last_check != now) {
+		last_check=now;
+		if(xm!=NULL && kbhit()) {
+			switch(getch()) {
+				case ESC:
+				case CTRL_C:
+				case CTRL_X:
+					xm->cancelled=TRUE;
+					break;
+			}
 		}
 	}
 	return(xm->cancelled);
@@ -1150,10 +1228,10 @@ void xmodem_progress(void* cbdata, unsigned block_num, ulong offset, ulong fsize
 	int			old_hold=hold_update;
 	xmodem_t*	xm=(xmodem_t*)cbdata;
 
-	xmodem_check_abort(cbdata);
-
 	now=time(NULL);
 	if(now-last_progress>0 || offset >= fsize) {
+		xmodem_check_abort(cbdata);
+
 		hold_update = TRUE;
 		window(((trans_ti.screenwidth-TRANSFER_WIN_WIDTH)/2)+2
 				, ((trans_ti.screenheight-TRANSFER_WIN_HEIGHT)/2)+1
@@ -1185,13 +1263,13 @@ void xmodem_progress(void* cbdata, unsigned block_num, ulong offset, ulong fsize
 				,l/60L
 				,l%60L
 				,cps
-				,(long)(((float)offset/(float)fsize)*100.0)
+				,fsize?(long)(((float)offset/(float)fsize)*100.0):100
 				);
 			clreol();
 			cputs("\r\n");
 			cprintf("%*s%3d%%\r\n", TRANSFER_WIN_WIDTH/2-5, ""
-				,(long)(((float)offset/(float)fsize)*100.0));
-			l = (long)(((float)offset/(float)fsize)*60.0);
+				,fsize?(long)(((float)offset/(float)fsize)*100.0):100);
+			l = fsize?(long)(((float)offset/(float)fsize)*60.0):60;
 			cprintf("[%*.*s%*s]", l, l, 
 					"\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1"
 					"\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1"
@@ -1217,8 +1295,8 @@ void xmodem_progress(void* cbdata, unsigned block_num, ulong offset, ulong fsize
 			clreol();
 			cputs("\r\n");
 			cprintf("%*s%3d%%\r\n", TRANSFER_WIN_WIDTH/2-5, ""
-				,(long)(((float)offset/(float)fsize)*100.0));
-			l = (long)(((float)offset/(float)fsize)*60.0);
+				,fsize?(long)(((float)offset/(float)fsize)*100.0):100);
+			l = fsize?(long)(((float)offset/(float)fsize)*60.0):60;
 			cprintf("[%*.*s%*s]", l, l, 
 					"\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1"
 					"\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xb1"
@@ -1287,7 +1365,9 @@ void xmodem_upload(struct bbslist *bbs, FILE *fp, char *path, long mode, int las
 		,send_byte
 		,recv_byte
 		,is_connected
-		,xmodem_check_abort);
+		,xmodem_check_abort
+		,flush_send);
+	xm.log_level=&log_level;
 	if(!data_waiting(&xm, 0)) {
 		switch(lastch) {
 			case 'G':
@@ -1466,7 +1546,9 @@ void xmodem_download(struct bbslist *bbs, long mode, char *path)
 		,send_byte
 		,recv_byte
 		,is_connected
-		,xmodem_check_abort);
+		,xmodem_check_abort
+		,flush_send);
+	xm.log_level=&log_level;
 	while(is_connected(NULL)) {
 		if(mode&XMODEM) {
 			if(isfullpath(path))
@@ -1794,7 +1876,7 @@ void font_control(struct bbslist *bbs)
 					filepick_free(&fpick);
 				}
 				else {
-					setfont(i,FALSE);
+					setfont(i,FALSE,1);
 					uifc_old_font=getfont();
 				}
 			}
@@ -1986,9 +2068,39 @@ int html_urlredirect(const char *uri, char *buf, size_t bufsize, char *uribuf, s
 
 #endif
 
+#define OUTBUF_SIZE	2048
+
+#ifdef WITH_WXWIDGETS
+#define WRITE_OUTBUF()	\
+	if(outbuf_size > 0) { \
+		cterm_write(outbuf, outbuf_size, prn, sizeof(prn), &speed); \
+		outbuf_size=0; \
+		if(html_mode==HTML_MODE_RAISED) { \
+			if(html_startx != wherex() || html_starty != wherey()) { \
+				iconize_html(); \
+				html_mode=HTML_MODE_ICONIZED; \
+			} \
+		} \
+		if(prn[0]) \
+			conn_send(prn, strlen(prn), 0); \
+		updated=TRUE; \
+	}
+#else
+#define WRITE_OUTBUF()	\
+	if(outbuf_size > 0) { \
+		cterm_write(outbuf, outbuf_size, prn, sizeof(prn), &speed); \
+		outbuf_size=0; \
+		if(prn[0]) \
+			conn_send(prn, strlen(prn), 0); \
+		updated=TRUE; \
+	}
+#endif
+
 BOOL doterm(struct bbslist *bbs)
 {
 	unsigned char ch[2];
+	unsigned char outbuf[OUTBUF_SIZE];
+	size_t outbuf_size=0;
 	unsigned char prn[ANSI_REPLY_BUFSIZE];
 	int	key;
 	int i,j;
@@ -2025,6 +2137,7 @@ BOOL doterm(struct bbslist *bbs)
 	BYTE ooii_init2[] = "\xdb\b \xdb\b \xdb\b[\xdb\b[\xdb\b \xdb\bM\xdb\ba\xdb\bi\xdb\bn\xdb\bt\xdb\be\xdb\bn\xdb\ba\xdb\bn\xdb\bc\xdb\be\xdb\b \xdb\bC\xdb\bo\xdb\bm\xdb\bp\xdb\bl\xdb\be\xdb\bt\xdb\be\xdb\b \xdb\b]\xdb\b]\xdb\b \b\r\n\r\n\x1b[0m\x1b[2J\r\n\r\n\x1b[0;1;30mHX Force retinal scan in progress ... \x1b[0;0;30m";	/* for OOII auto-enable */
 #endif
 	int ooii_mode=0;
+	recv_byte_buffer_len=recv_byte_buffer_pos=0;
 
 	gettextinfo(&txtinfo);
 	if(bbs->conn_type == CONN_TYPE_SERIAL)
@@ -2077,7 +2190,7 @@ BOOL doterm(struct bbslist *bbs)
 		sleep=TRUE;
 		if(!term.nostatus)
 			update_status(bbs, (bbs->conn_type == CONN_TYPE_SERIAL)?bbs->bpsrate:speed, ooii_mode);
-		for(remain=conn_data_waiting() /* Hack for connection check */ + (!conn_connected()); remain; remain--) {
+		for(remain=count_data_waiting() /* Hack for connection check */ + (!is_connected(NULL)); remain; remain--) {
 			if(speed)
 				thischar=xp_timer();
 
@@ -2087,7 +2200,8 @@ BOOL doterm(struct bbslist *bbs)
 
 				switch(inch) {
 					case -1:
-						if(!conn_connected()) {
+						if(!is_connected(NULL)) {
+							WRITE_OUTBUF();
 							hold_update=oldmc;
 #ifdef WITH_WXWIDGETS
 							if(html_mode != HTML_MODE_HIDDEN) {
@@ -2117,6 +2231,7 @@ BOOL doterm(struct bbslist *bbs)
 							gutsbuf[j]=inch;
 							gutsbuf[++j]=0;
 							if(j==sizeof(gutsinit)) { /* Have full sequence */
+								WRITE_OUTBUF();
 								guts_transfer(bbs);
 								remain=1;
 							}
@@ -2144,6 +2259,7 @@ BOOL doterm(struct bbslist *bbs)
 							htmldet[j]=inch;
 							htmldet[++j]=0;
 							if(j==sizeof(htmldetect)-1) {
+								WRITE_OUTBUF();
 								if(!strcmp(htmldet, htmldetect)) {
 									if(html_supported==HTML_SUPPORT_UNKNOWN) {
 										int width,height,xpos,ypos;
@@ -2176,6 +2292,7 @@ BOOL doterm(struct bbslist *bbs)
 							zrqbuf[j]=inch;
 							zrqbuf[++j]=0;
 							if(j==sizeof(zrqinit)-1) {	/* Have full sequence (Assumes zrinit and zrqinit are same length */
+								WRITE_OUTBUF();
 								if(!strcmp(zrqbuf, zrqinit))
 									zmodem_download(bbs);
 								else
@@ -2202,6 +2319,7 @@ BOOL doterm(struct bbslist *bbs)
 								ooii_buf[j++]=inch;
 								ooii_buf[j]=0;
 								if(inch == '|') {
+									WRITE_OUTBUF();
 									if(handle_ooii_code(ooii_buf, &ooii_mode, prn, sizeof(prn))) {
 										ooii_mode=0;
 										xptone_close();
@@ -2241,20 +2359,9 @@ BOOL doterm(struct bbslist *bbs)
 								ooii_buf[0]=0;
 						}
 #endif
-
-						ch[0]=inch;
-						cterm_write(ch, 1, prn, sizeof(prn), &speed);
-#ifdef WITH_WXWIDGETS
-						if(html_mode==HTML_MODE_RAISED) {
-							if(html_startx!=wherex() || html_starty!=wherey()) {
-								iconize_html();
-								html_mode=HTML_MODE_ICONIZED;
-							}
-						}
-#endif
-						if(prn[0])
-							conn_send(prn, strlen(prn), 0);
-						updated=TRUE;
+						if(outbuf_size >= sizeof(outbuf))
+							WRITE_OUTBUF();
+						outbuf[outbuf_size++]=inch;
 						continue;
 				}
 			}
@@ -2264,7 +2371,8 @@ BOOL doterm(struct bbslist *bbs)
 				break;
 			}
 		}
-		if(updated && sleep) {
+		WRITE_OUTBUF();
+		if(updated) {
 			hold_update=FALSE;
 			gotoxy(wherex(), wherey());
 		}
@@ -2355,15 +2463,17 @@ BOOL doterm(struct bbslist *bbs)
 					key = 0;
 					break;
 				case 0x2600:	/* ALT-L */
-					if(bbs->user[0]) {
-						conn_send(bbs->user,strlen(bbs->user),0);
-						conn_send(cterm.emulation==CTERM_EMULATION_ATASCII?"\x9b":"\r",1,0);
-						SLEEP(10);
-					}
-					if(bbs->password[0]) {
-						conn_send(bbs->password,strlen(bbs->password),0);
-						conn_send(cterm.emulation==CTERM_EMULATION_ATASCII?"\x9b":"\r",1,0);
-						SLEEP(10);
+					if(bbs->conn_type != CONN_TYPE_RLOGIN && bbs->conn_type != CONN_TYPE_RLOGIN_REVERSED && bbs->conn_type != CONN_TYPE_SSH) {
+						if(bbs->user[0]) {
+							conn_send(bbs->user,strlen(bbs->user),0);
+							conn_send(cterm.emulation==CTERM_EMULATION_ATASCII?"\x9b":"\r",1,0);
+							SLEEP(10);
+						}
+						if(bbs->password[0]) {
+							conn_send(bbs->password,strlen(bbs->password),0);
+							conn_send(cterm.emulation==CTERM_EMULATION_ATASCII?"\x9b":"\r",1,0);
+							SLEEP(10);
+						}
 					}
 					if(bbs->syspass[0]) {
 						conn_send(bbs->syspass,strlen(bbs->syspass),0);
