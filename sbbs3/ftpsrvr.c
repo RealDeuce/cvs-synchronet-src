8d0ebf42659086b904488be1ba0e5f3e1a37587a
rswindell <rswindell>
2010-03-05 23:54:13 +0000

    Moved fileoff_t and filelen_t typedefs to gen_defs.h (used in more places than just filewrap.c or it's consumers).

--- sbbs3/ftpsrvr.c
+++ sbbs3/ftpsrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet FTP server */
 
-/* $Id: ftpsrvr.c,v 1.360 2009/10/25 03:05:58 rswindell Exp $ */
+/* $Id: ftpsrvr.c,v 1.364 2009/12/09 19:00:36 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -79,11 +79,11 @@
 static ftp_startup_t*	startup=NULL;
 static scfg_t	scfg;
 static SOCKET	server_socket=INVALID_SOCKET;
-static DWORD	active_clients=0;
-static DWORD	sockets=0;
-static DWORD	thread_count=0;
+static ulong	active_clients=0;
+static ulong	sockets=0;
+static ulong	thread_count=0;
 static time_t	uptime=0;
-static DWORD	served=0;
+static ulong	served=0;
 static BOOL		terminate_server=FALSE;
 static char		revision[16];
 static char 	*text[TOTAL_TEXT];
@@ -342,8 +342,8 @@ int getdir(char* p, user_t* user, client_t* client)
 {
 	char*	tp;
 	char	path[MAX_PATH+1];
-	int		dir;
-	int		lib;
+	uint	dir;
+	uint	lib;
 
 	SAFECOPY(path,p);
 	p=path;
@@ -2090,8 +2090,8 @@ void parsepath(char** pp, user_t* user, client_t* client, int* curlib, int* curd
 	char*	p;
 	char*	tp;
 	char	path[MAX_PATH+1];
-	int		dir=*curdir;
-	int		lib=*curlib;
+	uint	dir=*curdir;
+	uint	lib=*curlib;
 
 	SAFECOPY(path,*pp);
 	p=path;
@@ -2473,14 +2473,8 @@ static void ctrl_thread(void* arg)
 	else
 		host_name="<no name>";
 
-#if	0 /* gethostbyaddr() is apparently not (always) thread-safe
-	     and getnameinfo() doesn't return alias information */
-	if(!(startup->options&FTP_OPT_NO_HOST_LOOKUP)) {
+	if(!(startup->options&FTP_OPT_NO_HOST_LOOKUP))
 		lprintf(LOG_INFO,"%04d Hostname: %s", sock, host_name);
-		for(i=0;host!=NULL && host->h_aliases!=NULL && host->h_aliases[i]!=NULL;i++)
-			lprintf(LOG_INFO,"%04d HostAlias: %s", sock, host->h_aliases[i]);
-	}
-#endif
 
 	if(trashcan(&scfg,host_ip,"ip")) {
 		lprintf(LOG_NOTICE,"%04d !CLIENT BLOCKED in ip.can: %s", sock, host_ip);
@@ -4590,7 +4584,7 @@ static void cleanup(int code, int line)
 	if(terminate_server || code)
 		lprintf(LOG_INFO,"#### FTP Server thread terminated (%lu clients served)", served);
 	if(thread_count)
-		lprintf(LOG_ERR,"#### !FTP Server threads (%u) remain active after termination", thread_count);
+		lprintf(LOG_WARNING,"#### !FTP Server threads (%u) remain after termination", thread_count);
 	if(startup!=NULL && startup->terminated!=NULL)
 		startup->terminated(startup->cbdata,code);
 }
@@ -4602,7 +4596,7 @@ const char* DLLCALL ftp_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.360 $", "%*s %s", revision);
+	sscanf("$Revision: 1.364 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
