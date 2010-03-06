713ff3db22ce0a1e6021b220f8e9c3a52c363bab
rswindell <rswindell>
2010-03-06 00:05:04 +0000

    More 64-bit file size goodness (only when XPDEV_LARGE_FILE_SUPPORT is defined).

--- sbbs3/logon.cpp
+++ sbbs3/logon.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet user logon routines */
 
-/* $Id: logon.cpp,v 1.50 2009/03/20 08:58:45 rswindell Exp $ */
+/* $Id: logon.cpp,v 1.52 2009/11/21 20:36:30 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -102,7 +102,7 @@ bool sbbs_t::logon()
 		bputs(text[NoNodeAccess]);
 		sprintf(str,"(%04u)  %-25s  Insufficient node access"
 			,useron.number,useron.alias);
-		logline("+!",str);
+		logline(LOG_NOTICE,"+!",str);
 		return(false); 
 	}
 
@@ -113,7 +113,7 @@ bool sbbs_t::logon()
 			bputs(text[NodeLocked]);
 			sprintf(str,"(%04u)  %-25s  Locked node logon attempt"
 				,useron.number,useron.alias);
-			logline("+!",str);
+			logline(LOG_NOTICE,"+!",str);
 			return(false); 
 		}
 		if(yesno(text[RemoveNodeLockQ])) {
@@ -213,14 +213,7 @@ bool sbbs_t::logon()
 		CLS;
 		user_event(EVENT_BIRTHDAY); 
 	}
-	unixtodstr(&cfg,useron.laston,tmp);
-	if(strcmp(str,tmp)) {			/* str still equals logon time */
-		useron.ltoday=1;
-		useron.ttoday=useron.etoday=useron.ptoday=useron.textra=0;
-		useron.freecdt=cfg.level_freecdtperday[useron.level]; 
-	}
-	else
-		useron.ltoday++;
+	useron.ltoday++;
 
 	gettimeleft();
 	sprintf(str,"%sfile/%04u.dwn",cfg.data_dir,useron.number);
@@ -277,7 +270,7 @@ bool sbbs_t::logon()
 			bputs(text[NoMoreLogons]);
 			sprintf(str,"(%04u)  %-25s  Out of logons"
 				,useron.number,useron.alias);
-			logline("+!",str);
+			logline(LOG_NOTICE,"+!",str);
 			hangup();
 			return(false); 
 		}
@@ -285,7 +278,7 @@ bool sbbs_t::logon()
 			bputs(text[R_Logons]);
 			sprintf(str,"(%04u)  %-25s  Out of logons"
 				,useron.number,useron.alias);
-			logline("+!",str);
+			logline(LOG_NOTICE,"+!",str);
 			hangup();
 			return(false); 
 		}
@@ -390,7 +383,7 @@ bool sbbs_t::logon()
 	if(!online) {
 		sprintf(str,"(%04u)  %-25s  Unsuccessful logon"
 			,useron.number,useron.alias);
-		logline("+!",str);
+		logline(LOG_NOTICE,"+!",str);
 		return(false); 
 	}
 	SAFECOPY(useron.modem,connection);
@@ -490,7 +483,7 @@ bool sbbs_t::logon()
 				strcpy(tmp,"On two nodes at the same time");
 				sprintf(str,"(%04u)  %-25s  %s"
 					,useron.number,useron.alias,tmp);
-				logline("+!",str);
+				logline(LOG_NOTICE,"+!",str);
 				errorlog(tmp);
 				bputs(text[UserOnTwoNodes]);
 				hangup();
