88cb416110657fd0b1b21b77b5c0bc045fdfb0cd
rswindell <rswindell>
2010-03-09 03:53:09 +0000

    Print signed 64-bit ints using PRId64 instead of PRIu64 (in most cases). (oops, wrong commit message on last commit of xtrn.cpp)

--- sbbs3/atcodes.cpp
+++ sbbs3/atcodes.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet "@code" functions */
 
-/* $Id: atcodes.cpp,v 1.52 2009/01/06 03:29:25 rswindell Exp $ */
+/* $Id: atcodes.cpp,v 1.57 2010/03/07 22:10:25 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -57,7 +57,7 @@ int sbbs_t::show_atcode(const char *instr)
 	bool	padded_right=false;
 	const char *cp;
 
-	sprintf(str,"%.80s",instr);
+	SAFECOPY(str,instr);
 	tp=strchr(str+1,'@');
 	if(!tp)                 /* no terminating @ */
 		return(0);
@@ -84,11 +84,11 @@ int sbbs_t::show_atcode(const char *instr)
 		return(0);
 
 	if(padded_left)
-		rprintf("%-*.*s",disp_len,disp_len,cp);
+		bprintf("%-*.*s",disp_len,disp_len,cp);
 	else if(padded_right)
-		rprintf("%*.*s",disp_len,disp_len,cp);
+		bprintf("%*.*s",disp_len,disp_len,cp);
 	else
-		rputs(cp);
+		bputs(cp);
 
 	return(len);
 }
@@ -636,44 +636,32 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 		return("\r\n");
 
 	if(!strcmp(sp,"PUSHXY")) {
-		ANSI_SAVE();
+		ansi_save();
 		return(nulstr);
 	}
 
 	if(!strcmp(sp,"POPXY")) {
-		ANSI_RESTORE();
+		ansi_restore();
 		return(nulstr);
 	}
 
-	if(!strcmp(sp,"UP"))
-		return("\x1b[A");
-
-	if(!strcmp(sp,"DOWN"))
-		return("\x1b[B");
-
-	if(!strcmp(sp,"RIGHT"))
-		return("\x1b[C");
-
-	if(!strcmp(sp,"LEFT"))
-		return("\x1b[D");
-
 	if(!strncmp(sp,"UP:",3)) {
-		safe_snprintf(str,maxlen,"\x1b[%dA",atoi(sp+3));
+		cursor_up(atoi(sp+3));
 		return(str);
 	}
 
 	if(!strncmp(sp,"DOWN:",5)) {
-		safe_snprintf(str,maxlen,"\x1b[%dB",atoi(sp+5));
+		cursor_down(atoi(sp+5));
 		return(str);
 	}
 
 	if(!strncmp(sp,"LEFT:",5)) {
-		safe_snprintf(str,maxlen,"\x1b[%dC",atoi(sp+5));
+		cursor_left(atoi(sp+5));
 		return(str);
 	}
 
 	if(!strncmp(sp,"RIGHT:",6)) {
-		safe_snprintf(str,maxlen,"\x1b[%dD",atoi(sp+6));
+		cursor_right(atoi(sp+6));
 		return(str);
 	}
 
@@ -681,7 +669,7 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 		tp=strchr(sp,',');
 		if(tp!=NULL) {
 			tp++;
-			GOTOXY(atoi(sp+7),atoi(tp));
+			ansi_gotoxy(atoi(sp+7),atoi(tp));
 		}
 		return(nulstr);
 	}
@@ -975,7 +963,7 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 	if(!strcmp(sp,"MSG_TIMEZONE") && current_msg!=NULL)
 		return(smb_zonestr(current_msg->hdr.when_written.zone,NULL));
 	if(!strcmp(sp,"MSG_ATTR") && current_msg!=NULL) {
-		safe_snprintf(str,maxlen,"%s%s%s%s%s%s%s%s%s%s"
+		safe_snprintf(str,maxlen,"%s%s%s%s%s%s%s%s%s%s%s"
 			,current_msg->hdr.attr&MSG_PRIVATE		? "Private  "   :nulstr
 			,current_msg->hdr.attr&MSG_READ			? "Read  "      :nulstr
 			,current_msg->hdr.attr&MSG_DELETE		? "Deleted  "   :nulstr
@@ -986,6 +974,7 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 			,current_msg->hdr.attr&MSG_MODERATED	? "Moderated  " :nulstr
 			,current_msg->hdr.attr&MSG_VALIDATED	? "Validated  " :nulstr
 			,current_msg->hdr.attr&MSG_REPLIED		? "Replied  "	:nulstr
+			,current_msg->hdr.attr&MSG_NOREPLY		? "NoReply  "	:nulstr
 			);
 		return(str);
 	}
