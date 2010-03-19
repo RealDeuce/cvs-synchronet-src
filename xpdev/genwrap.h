95ecfe09975a6c19d85dba97b9bee067139ae687
deuce <deuce>
2010-03-19 01:20:59 +0000

    Fix suspend/resume during mod dir search.

--- xpdev/genwrap.h
+++ xpdev/genwrap.h
@@ -2,13 +2,13 @@
 
 /* General cross-platform development wrappers */
 
-/* $Id: genwrap.h,v 1.95 2010/06/03 01:34:41 rswindell Exp $ */
+/* $Id: genwrap.h,v 1.92 2009/01/14 07:06:30 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2008 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -60,7 +60,6 @@
 	#endif
 #elif defined(_WIN32)
 	#include <process.h>	/* getpid() */
-	typedef DWORD pid_t;
 #endif
 
 #if !defined(_WIN32)
@@ -156,32 +155,6 @@ extern "C" {
 	#define PLATFORM_DESC	"Unix"
 #else
 	#error "Need to describe target platform"
-#endif
-
-#if defined(_M_X64) || defined(_M_AMD64) || defined(__x86_64__) || defined(__amd64__)
-	#define ARCHITECTURE_DESC "x64"
-#elif defined(__i386__) || _M_IX86 == 300
-	#define ARCHITECTURE_DESC "i386"
-#elif defined(__i486__) || _M_IX86 == 400
-	#define ARCHITECTURE_DESC "i486"
-#elif defined(__i586__) || _M_IX86 == 500
-	#define ARCHITECTURE_DESC "i586"
-#elif defined(__i686__) || _M_IX86 == 600
-	#define ARCHITECTURE_DESC "i686"
-#elif defined(__i786__) || _M_IX86 == 700
-	#define ARCHITECTURE_DESC "i786"
-#elif defined(_X86_) || defined(__x86__) || defined(_M_IX86)
-	#define ARCHITECTURE_DESC "x86"
-#elif defined(__mips__)
-	#define ARCHITECTURE_DESC "mips"
-#elif defined(__arm__)
-	#define ARCHITECTURE_DESC "arm"
-#elif defined(_M_PPC) || defined(__ppc__)
-	#define ARCHITECTURE_DESC "ppc"
-#elif defined(_M_IA64) || defined(__ia64__)
-	#define ARCHITECTURE_DESC "ia64"
-#else
-	#error "Need to describe target architecture"
 #endif
 
 /*********************/
@@ -348,9 +321,6 @@ typedef		clock_t				msclock_t;
 	#define		MSCLOCKS_PER_SEC	1000
 	msclock_t	msclock(void);
 #endif
-
-DLLEXPORT BOOL		DLLCALL check_pid(pid_t);
-DLLEXPORT BOOL		DLLCALL	terminate_pid(pid_t);
 
 #if defined(__cplusplus)
 }
