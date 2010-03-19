012b7824193cf94a0935575f1341c12f8df413eb
deuce <deuce>
2010-03-19 07:36:53 +0000

    Initial checking of ZuulTerm (working title).
    Currently suports RLogin to my BBS as me only.

--- ZuulTerm/chrome/content/RLogin.js
+++ ZuulTerm/chrome/content/RLogin.js
@@ -1,7 +1,6 @@
 function RLoginConnection(address, port, recvfunc)
 {
 	this.recvfunc=recvfunc;
-	this.unicode={remainder:''};
 
 	this.onStartRequest=function(request, context) {};
 	this.onStopRequest=function(request, context, status) {
@@ -9,8 +8,7 @@ function RLoginConnection(address, port, recvfunc)
 		endTerm();
 	};
 	this.onDataAvailable=function(request, context, inputStream, offset, count) {
-		this.unicode=bytesToUnicode(this.unicode.remainder+this.sock.read(count, count, 0));
-		this.recvfunc(this.unicode.unicode);
+		this.recvfunc(this.sock.read(count, count, 0));
 	};
 
 	this.write=function(data)
@@ -28,7 +26,7 @@ function RLoginConnection(address, port, recvfunc)
 
 	this.sock=new Socket();
 	this.sock.connect(address,port);
-	this.write("\x00\x00\x00ZuulTerm/115200\x00");
+	this.write("\x00admin\x00deuce\x00ZuulTerm/115200\x00");
 	if(this.sock.read(1,1,10000)!='\x00')
 		alert("No RLogin ack!");
 	this.sock.asyncRead(this);
