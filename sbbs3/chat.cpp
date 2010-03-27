bbc1f553aeb8e7a70dd99660e182e385b9c32e62
deuce <deuce>
2010-03-27 19:21:33 +0000

    Move globals (connection etc) into a single Zuul object which is pushed into the content window.  Make HTML escaping optional.

--- sbbs3/chat.cpp
+++ sbbs3/chat.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet real-time chat functions */
 
-/* $Id: chat.cpp,v 1.60 2011/03/01 22:27:02 rswindell Exp $ */
+/* $Id: chat.cpp,v 1.58 2010/03/12 08:27:57 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -1282,7 +1282,7 @@ int sbbs_t::getnodetopage(int all, int telegram)
 		return(-1);
 
 	if(str[0]=='\'') {
-		j=userdatdupe(0,U_HANDLE,LEN_HANDLE,str+1);
+		j=userdatdupe(0,U_HANDLE,LEN_HANDLE,str+1,0);
 		if(!j) {
 			bputs(text[UnknownUser]);
 			return(0); 
