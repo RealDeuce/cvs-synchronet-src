4f2f4341b8f1ad328be68260d554eb3b65f5b314
deuce <deuce>
2010-03-27 19:21:33 +0000

    Move globals (connection etc) into a single Zuul object which is pushed into the content window.  Make HTML escaping optional.

--- syncterm/conn.c
+++ syncterm/conn.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: conn.c,v 1.57 2010/03/03 06:34:10 deuce Exp $ */
+/* $Id: conn.c,v 1.60 2010/03/08 05:18:12 rswindell Exp $ */
 
 #include <stdlib.h>
 
@@ -220,7 +220,7 @@ size_t conn_buf_wait_cond(struct conn_buffer *buf, size_t bcount, unsigned long 
 		}
 		if(sem_trywait_block(sem, timeleft))
 			retnow=1;
-		pthread_mutex_lock(&(buf->mutex));
+		pthread_mutex_lock(&(buf->mutex));	/* term.c data_waiting() blocks here, seemingly forever */
 		found=cond(buf);
 		if(found > bcount)
 			found=bcount;
@@ -245,12 +245,11 @@ BOOL conn_connected(void)
 
 int conn_recv_upto(char *buffer, size_t buflen, unsigned timeout)
 {
-	size_t found;
+	size_t	found=0;
 
 	pthread_mutex_lock(&(conn_inbuf.mutex));
-	found=conn_buf_wait_bytes(&conn_inbuf, 1, timeout);
-	if(found)
-		found=conn_buf_get(&conn_inbuf, buffer, found);
+	if(conn_buf_wait_bytes(&conn_inbuf, 1, timeout))
+		found=conn_buf_get(&conn_inbuf, buffer, buflen);
 	pthread_mutex_unlock(&(conn_inbuf.mutex));
 	return(found);
 }
