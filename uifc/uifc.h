b7745618eeff4f14b5f90214ea9bd643535141cd
deuce <deuce>
2010-03-27 19:20:01 +0000

    Switch old (now unsupported) HTMLTerm detection and flag to indicate the new ZuulTerm protocol and add an autodetection.

--- uifc/uifc.h
+++ uifc/uifc.h
@@ -2,13 +2,13 @@
 
 /* Rob Swindell's Text-mode User Interface Library */
 
-/* $Id: uifc.h,v 1.77 2008/01/21 20:39:41 deuce Exp $ */
+/* $Id: uifc.h,v 1.79 2010/03/12 06:21:32 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -86,8 +86,9 @@
 #define MAX_OPLN	75		/* Maximum length of each option per menu call */
 #define MAX_BUFS	7		/* Maximum number of screen buffers to save */
 #define MIN_LINES   14      /* Minimum number of screen lines supported */
-#define MAX_LINES   60      /* Maximum number of screen rows supported */ 
-#define MAX_BFLN	80*MAX_LINES*2	/* Maximum size of screen buffers */
+#define MAX_LINES   255     /* Maximum number of screen lines supported (ciolib screenheight is a uchar) */ 
+#define MAX_COLS	255		/* Maximum number of screen columns supported (ciolib screenwidth is a uchar) */ 
+#define MAX_BFLN	(MAX_COLS*MAX_LINES*2)	/* Maximum size of screen buffers, char + attr */
 
 #ifndef uint
 #define uint unsigned int
@@ -405,6 +406,18 @@ typedef struct {
 
 } uifcapi_t;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Return value from uifc_api.list() when uifcYesNoOpts is used */
+enum {
+	uifcYes=0,
+	uifcNo=1
+};
+
+extern char* uifcYesNoOpts[];
+
 /****************************************************************************/
 /* Initialization routines for each UIFC implementation.					*/
 /* Returns 0 on success, non-zero on failure.								*/
@@ -413,5 +426,9 @@ int uifcini(uifcapi_t*);	/* Original implementation based on conio		*/
 int uifcinix(uifcapi_t*);	/* Standard I/O implementation					*/
 int uifcini32(uifcapi_t*);	/* conio/curses implementation					*/
 /****************************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* Don't add anything after this line! */
