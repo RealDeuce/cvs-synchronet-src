499512216e36d7d22aef5c037a02717aa1cbcf78
rswindell <rswindell>
2010-05-24 05:17:57 +0000

    Fix buffer overrun in get_header_field() - by one byte. Caught by MSVC10.

--- xpdev/genwrap.h
+++ xpdev/genwrap.h
@@ -2,13 +2,13 @@
 
 /* General cross-platform development wrappers */
 
-/* $Id: genwrap.h,v 1.97 2011/10/18 09:18:07 rswindell Exp $ */
+/* $Id: genwrap.h,v 1.94 2010/05/24 01:13:10 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -60,7 +60,6 @@
 	#endif
 #elif defined(_WIN32)
 	#include <process.h>	/* getpid() */
-	typedef DWORD pid_t;
 #endif
 
 #if !defined(_WIN32)
@@ -324,7 +323,7 @@ DLLEXPORT int DLLCALL	get_errno(void);
 
 /* Mimic the Borland randomize() and random() CRTL functions */
 DLLEXPORT void		DLLCALL xp_randomize(void);
-DLLEXPORT long		DLLCALL	xp_random(int);
+DLLEXPORT int		DLLCALL	xp_random(int);
 
 DLLEXPORT long double  	DLLCALL	xp_timer(void);
 DLLEXPORT char*		DLLCALL os_version(char *str);
@@ -348,9 +347,6 @@ typedef		clock_t				msclock_t;
 	#define		MSCLOCKS_PER_SEC	1000
 	msclock_t	msclock(void);
 #endif
-
-DLLEXPORT BOOL		DLLCALL check_pid(pid_t);
-DLLEXPORT BOOL		DLLCALL	terminate_pid(pid_t);
 
 #if defined(__cplusplus)
 }
