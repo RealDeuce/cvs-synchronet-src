ab2cc3eb1864ad9bd2b6c2dc07636ce97c933208
rswindell <rswindell>
2010-06-01 07:29:30 +0000

    strftime() will throw an assertion in MSVC10 when passed an invalid struct tm (e.g. zeroed-out).

--- sbbs3/js_global.c
+++ sbbs3/js_global.c
@@ -2,7 +2,7 @@
 
 /* Synchronet JavaScript "global" object properties/methods for all servers */
 
-/* $Id: js_global.c,v 1.257 2010/04/02 23:12:02 deuce Exp $ */
+/* $Id: js_global.c,v 1.258 2010/06/01 07:29:30 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -2955,9 +2955,8 @@ js_strftime(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 	rc=JS_SUSPENDREQUEST(cx);
 	strcpy(str,"-Invalid time-");
 	t=i;
-	if(localtime_r(&t,&tm)==NULL)
-		memset(&tm,0,sizeof(tm));
-	strftime(str,sizeof(str),fmt,&tm);
+	if(localtime_r(&t,&tm)!=NULL)
+		strftime(str,sizeof(str),fmt,&tm);
 	JS_RESUMEREQUEST(cx, rc);
 
 	if((js_str=JS_NewStringCopyZ(cx, str))==NULL)
