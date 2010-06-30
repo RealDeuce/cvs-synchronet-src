fb5da784ef6841af5367bdf445bda53eb3136ba8
deuce <deuce>
2010-06-30 02:29:34 +0000

    Fix internal redirects (ie: index files) with digest authentication.

--- sbbs3/websrvr.c
+++ sbbs3/websrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Web Server */
 
-/* $Id: websrvr.c,v 1.529 2010/06/30 00:33:08 deuce Exp $ */
+/* $Id: websrvr.c,v 1.530 2010/06/30 02:29:34 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1635,20 +1635,18 @@ static BOOL check_ars(http_session_t * session)
 				}
 
 				/* Validate nonce */
-				p=strtok_r(session->req.auth.nonce, "@", &last);
+				p=strchr(session->req.auth.nonce, '@');
 				if(p==NULL) {
 					session->req.auth.stale=TRUE;
 					return(FALSE);
 				}
-				if(strcmp(p, session->client.addr)) {
+				*p=0;
+				if(strcmp(session->req.auth.nonce, session->client.addr)) {
 					session->req.auth.stale=TRUE;
 					return(FALSE);
 				}
-				p=strtok_r(NULL, "", &last);
-				if(p==NULL) {
-					session->req.auth.stale=TRUE;
-					return(FALSE);
-				}
+				*p='@';
+				p++;
 				nonce_time=strtoul(p, &p, 10);
 				if(*p) {
 					session->req.auth.stale=TRUE;
@@ -5204,7 +5202,7 @@ const char* DLLCALL web_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.529 $", "%*s %s", revision);
+	sscanf("$Revision: 1.530 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
