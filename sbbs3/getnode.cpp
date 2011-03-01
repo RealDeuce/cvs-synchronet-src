e7d28e9a91a12cebbf60a1e3726e8381af3cca05
rswindell <rswindell>
2011-03-01 21:46:53 +0000

    Don't attempt to close the nodefile descriptor if it is an invalid file descriptor (-1), this is part of the node.dab error recovery scheme.

--- sbbs3/getnode.cpp
+++ sbbs3/getnode.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet node information retrieval functions */
 
-/* $Id: getnode.cpp,v 1.41 2010/11/19 06:39:24 rswindell Exp $ */
+/* $Id: getnode.cpp,v 1.42 2011/03/01 21:46:53 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -91,7 +91,8 @@ int sbbs_t::getnodedat(uint number, node_t *node, bool lockit)
 
 	if(count==LOOP_NODEDAB) {
 		errormsg(WHERE,rd==sizeof(node_t) ? ERR_LOCK : ERR_READ,"node.dab",number+1);
-		close(nodefile);
+		if(nodefile!=-1)
+			close(nodefile);
 		nodefile=-1;
 		return(-2);
 	}
