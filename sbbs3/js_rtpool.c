09cc51a5e5b0f4623ce7f782eb50ec611bae2299
rswindell <rswindell>
2011-07-13 09:44:18 +0000

    Resolve msvc6 warning: warning C4229: anachronism used : modifiers on data are ignored

--- sbbs3/js_rtpool.c
+++ sbbs3/js_rtpool.c
@@ -1,8 +1,7 @@
-/* $Id: js_rtpool.c,v 1.21 2011/10/28 01:53:29 deuce Exp $ */
+/* $Id: js_rtpool.c,v 1.18 2010/04/03 00:16:53 deuce Exp $ */
 
 #include "js_rtpool.h"
 #include <threadwrap.h>		/* Must be included after jsapi.h */
-#include <genwrap.h>		/* SLEEP() */
 
 #ifdef DLLCALL
 #undef DLLCALL
@@ -39,21 +38,6 @@ static int			initialized=0;
 static sem_t			jsrt_sem;
 #endif
 
-static void trigger_thread(void *args)
-{
-	int	i;
-
-	for(;;) {
-		pthread_mutex_lock(&jsrt_mutex);
-		for(i=0; i<JSRT_QUEUE_SIZE; i++) {
-			if(jsrt_queue[i].created)
-				JS_TriggerAllOperationCallbacks(jsrt_queue[i].rt);
-		}
-		pthread_mutex_unlock(&jsrt_mutex);
-		SLEEP(100);
-	}
-}
-
 JSRuntime * DLLCALL jsrt_GetNew(int maxbytes, unsigned long timeout, const char *filename, long line)
 {
 #ifdef SHARED_RUNTIMES
@@ -61,7 +45,6 @@ JSRuntime * DLLCALL jsrt_GetNew(int maxbytes, unsigned long timeout, const char 
 
 	if(!initialized) {
 		pthread_mutex_init(&jsrt_mutex, NULL);
-		_beginthread(trigger_thread, 65536, NULL);
 		initialized=TRUE;
 	}
 
@@ -146,8 +129,9 @@ void DLLCALL jsrt_Release(JSRuntime *rt)
 
 void DLLCALL jsrt_TriggerAll(void)
 {
-#if JS_VERSION>180
+#ifdef USE_JS_OPERATION_CALLBACK
 	int	i;
+	int j;
 	JSContext	*iterp,*cx;
 
 	if(!initialized)
