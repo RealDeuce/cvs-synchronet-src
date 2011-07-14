afe3bbcbde1591851074da080bdb6bf32f936f11
rswindell <rswindell>
2011-07-14 03:05:23 +0000

    Updated Help menu items (e.g. added links to wiki.synchro.net).

--- sbbs3/websrvr.c
+++ sbbs3/websrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Web Server */
 
-/* $Id: websrvr.c,v 1.535 2011/07/16 07:55:02 rswindell Exp $ */
+/* $Id: websrvr.c,v 1.534 2011/04/29 20:17:13 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -2697,7 +2697,7 @@ static BOOL get_req(http_session_t * session, char *request_line)
 		if(len<0)
 			return(FALSE);
 		if(req_line[0])
-			lprintf(LOG_INFO,"%04d Request: %s",session->socket,req_line);
+			lprintf(LOG_DEBUG,"%04d Request: %s",session->socket,req_line);
 		if(session->req.ld!=NULL && session->req.ld->request==NULL)
 			/* FREE()d in http_logging_thread() */
 			session->req.ld->request=strdup(req_line);
@@ -3920,12 +3920,12 @@ js_ErrorReporter(JSContext *cx, const char *message, JSErrorReport *report)
     }
 
 	if(report->filename)
-		SAFEPRINTF(file," %s",report->filename);
+		sprintf(file," %s",report->filename);
 	else
 		file[0]=0;
 
 	if(report->lineno)
-		SAFEPRINTF(line," line %u",report->lineno);
+		sprintf(line," line %u",report->lineno);
 	else
 		line[0]=0;
 
@@ -3940,8 +3940,7 @@ js_ErrorReporter(JSContext *cx, const char *message, JSErrorReport *report)
 		warning="";
 	}
 
-	lprintf(log_level,"%04d !JavaScript %s%s%s: %s, Request: %s"
-		,session->socket,warning,file,line,message, session->req.request_line);
+	lprintf(log_level,"%04d !JavaScript %s%s%s: %s",session->socket,warning,file,line,message);
 	if(session->req.fp!=NULL)
 		fprintf(session->req.fp,"!JavaScript %s%s%s: %s",warning,file,line,message);
 }
@@ -5208,7 +5207,7 @@ const char* DLLCALL web_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.535 $", "%*s %s", revision);
+	sscanf("$Revision: 1.534 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
