7ddf36c1506562119cea63c3b0d22ecb272f8104
rswindell <rswindell>
2011-08-11 01:50:17 +0000

    Flag inbound messages destined for external mail processing (e.g. by matching the RCPT TO user), before checking for the "sub:code" recipient. This allows SMTP posts (e.g. using the "sub:code" syntax in the ctrl/alias.cfg file) to be processed by external mail processors before being posted (e.g. to strip HTML).

--- xpdev/ini_file.c
+++ xpdev/ini_file.c
@@ -2,7 +2,7 @@
 
 /* Functions to create and parse .ini files */
 
-/* $Id: ini_file.c,v 1.122 2011/09/29 23:46:51 rswindell Exp $ */
+/* $Id: ini_file.c,v 1.120 2011/07/21 10:47:44 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -257,7 +257,7 @@ static char* read_value(FILE* fp, const char* section, const char* key, char* va
 
 static size_t get_value(str_list_t list, const char* section, const char* key, char* value, char** vpp)
 {
-	char*	str;
+	char	str[INI_MAX_LINE_LEN];
 	char*	p;
 	char*	vp;
 	size_t	i;
@@ -270,7 +270,7 @@ static size_t get_value(str_list_t list, const char* section, const char* key, c
 		return 0;
 
 	for(i=find_section(list, section); list[i]!=NULL; i++) {
-		str=list[i];
+		SAFECOPY(str, list[i]);
 		if(is_eof(str))
 			break;
 		if((p=key_name(str,&vp))==NULL)
@@ -365,9 +365,10 @@ BOOL iniRemoveKey(str_list_t* list, const char* section, const char* key)
 
 BOOL iniRemoveValue(str_list_t* list, const char* section, const char* key)
 {
+	size_t	i;
 	char*	vp=NULL;
 
-	get_value(*list, section, key, NULL, &vp);
+	i=get_value(*list, section, key, NULL, &vp);
 
 	if(vp==NULL)
 		return(FALSE);
