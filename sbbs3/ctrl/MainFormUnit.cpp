d9546d796d8685e484e86cc016e6a5da83ad4bcb
rswindell <rswindell>
2011-09-01 06:37:25 +0000

    Added StatusBar popup menu for clearing error counters and viewing error log. Added menu item for editing modopts.ini.

--- sbbs3/ctrl/MainFormUnit.cpp
+++ sbbs3/ctrl/MainFormUnit.cpp
@@ -1,6 +1,6 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: MainFormUnit.cpp,v 1.180 2011/09/01 02:50:44 rswindell Exp $ */
+/* $Id: MainFormUnit.cpp,v 1.181 2011/09/01 06:37:25 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -2997,15 +2997,6 @@ void __fastcall TMainForm::UpTimerTick(TObject *Sender)
 }
 //---------------------------------------------------------------------------
 
-void __fastcall TMainForm::BBSViewErrorLogMenuItemClick(TObject *Sender)
-{
-	char filename[MAX_PATH+1];
-
-    sprintf(filename,"%sERROR.LOG"
-    	,MainForm->cfg.logs_dir);
-    ViewFile(filename,"Error Log");
-}
-//---------------------------------------------------------------------------
 
 void __fastcall TMainForm::ChatToggleExecute(TObject *Sender)
 {
@@ -3823,6 +3814,30 @@ TFont* __fastcall TMainForm::LogAttributes(int log_level, TColor Color, TFont* F
         return Font;
 
     return LogFont[log_level];
+}
+//---------------------------------------------------------------------------
+void __fastcall TMainForm::ClearErrorsExecute(TObject *Sender)
+{
+    errors=0;
+
+    node_t node;
+    for(int i=0;i<cfg.sys_nodes;i++) {
+    	int file;
+       	if(NodeForm->getnodedat(i+1,&node,&file))
+            break;
+        node.errors=0;
+        if(NodeForm->putnodedat(i+1,&node,file))
+            break;
+    }
+}
+//---------------------------------------------------------------------------
+void __fastcall TMainForm::ViewErrorLogExecute(TObject *Sender)
+{
+	char filename[MAX_PATH+1];
+
+    sprintf(filename,"%sERROR.LOG"
+    	,MainForm->cfg.logs_dir);
+    ViewFile(filename,"Error Log");
 }
 //---------------------------------------------------------------------------
 void __fastcall TMainForm::ViewLoginAttemptsMenuItemClick(TObject *Sender)
