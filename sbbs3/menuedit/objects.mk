fdfaa304292ac2a51a3e0a93ec3c26c54e952617
rswindell <rswindell>
2011-09-03 09:50:42 +0000

    Did not mean to commit HeapAlloc experiment in previous commit.

--- sbbs3/menuedit/objects.mk
+++ sbbs3/menuedit/objects.mk
@@ -2,14 +2,9 @@
 
 # Make 'include file' listing object files for Synchronet MenuEdit
 
-# $Id: objects.mk,v 1.2 2005/02/20 20:51:40 deuce Exp $
+# $Id: objects.mk,v 1.3 2011/05/27 06:03:31 deuce Exp $
 
 # LIBODIR, SLASH, and OFILE must be pre-defined
 
 
-OBJS	=	$(MTOBJODIR)$(DIRSEP)menuedit$(OFILE)\
-	        $(MTOBJODIR)$(DIRSEP)ini_file$(OFILE)\
-        	$(MTOBJODIR)$(DIRSEP)str_list$(OFILE)\
-		$(MTOBJODIR)$(DIRSEP)genwrap$(OFILE)\
-		$(MTOBJODIR)$(DIRSEP)dirwrap$(OFILE)\
-		$(MTOBJODIR)$(DIRSEP)uifcx$(OFILE)
+OBJS	=	$(MTOBJODIR)$(DIRSEP)menuedit$(OFILE)
