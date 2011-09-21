1ef00f1abe7d13e6fa44288c1e728c44bff4bfea
rswindell <rswindell>
2011-09-21 03:10:53 +0000

    Eliminated sbbs_t::errorlog(), as it didn't actually write to the error.log any longer (use lprintf(LOG_ERR,...) instead, which does). Added sbbs_t::hacklog() for convenience.

--- sbbs3/sbbs.h
+++ sbbs3/sbbs.h
@@ -2,7 +2,7 @@
 
 /* Synchronet class (sbbs_t) definition and exported function prototypes */
 
-/* $Id: sbbs.h,v 1.377 2011/10/16 07:35:57 deuce Exp $ */
+/* $Id: sbbs.h,v 1.358 2011/09/21 03:10:53 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -98,42 +98,19 @@ extern int	thread_suid_broken;			/* NPTL is no longer broken */
 
 #include <sys/stat.h>
 
-#ifdef __unix__
-	#define XP_UNIX
-#else
-	#define XP_PC
-	#define XP_WIN
+#ifdef JAVASCRIPT
+	#ifdef __unix__
+		#define XP_UNIX
+	#else
+		#define XP_PC
+		#define XP_WIN
+	#endif
+	#define JS_THREADSAFE	/* Required! */
+	#include <jsapi.h>
+	#include <jsprf.h>		/* JS-safe sprintf functions */
+	#include <jsnum.h>		/* JSDOUBLE_IS_NaN() */
+
 #endif
-#include <jsversion.h>
-#include <jsapi.h>
-#define JS_DestroyScript(cx,script)
-
-#define JSSTRING_TO_STRING(cx, str, ret, lenptr) \
-{ \
-	size_t			*JSSTSlenptr=lenptr; \
-	size_t			JSSTSlen; \
-	size_t			JSSTSpos; \
-	const jschar	*JSSTSstrval; \
-\
-	if(JSSTSlenptr==NULL) \
-		JSSTSlenptr=&JSSTSlen; \
-	(ret)=NULL; \
-	if((str) != NULL) { \
-		if((JSSTSstrval=JS_GetStringCharsAndLength((cx), (str), JSSTSlenptr))) { \
-			if(((ret)=(char *)alloca(*JSSTSlenptr+1))) { \
-				for(JSSTSpos=0; JSSTSpos<*JSSTSlenptr; JSSTSpos++) \
-					(ret)[JSSTSpos]=JSSTSstrval[JSSTSpos]; \
-				(ret)[*JSSTSlenptr]=0; \
-			} \
-		} \
-	} \
-}
-
-#define JSVALUE_TO_STRING(cx, val, ret, lenptr) \
-{ \
-	JSString	*JSVTSstr=JS_ValueToString((cx), (val)); \
-	JSSTRING_TO_STRING((cx), JSVTSstr, (ret), lenptr); \
-}
 
 #ifdef USE_CRYPTLIB
 #include <cryptlib.h>
@@ -1030,8 +1007,9 @@ extern "C" {
 	DLLEXPORT JSBool	DLLCALL js_DefineConstIntegers(JSContext* cx, JSObject* obj, jsConstIntSpec*, int flags);
 	DLLEXPORT JSBool	DLLCALL js_CreateArrayOfStrings(JSContext* cx, JSObject* parent
 														,const char* name, char* str[], uintN flags);
+	DLLEXPORT char*		DLLCALL js_ValueToStringBytes(JSContext* cx, jsval val, size_t* len);
 
-	#define JSVAL_IS_NUM(v)		(JSVAL_IS_NUMBER(v))
+	#define JSVAL_IS_NUM(v)		(JSVAL_IS_NUMBER(v) && (!JSVAL_IS_DOUBLE(v) || !JSDOUBLE_IS_NaN(*JSVAL_TO_DOUBLE(v))))
 
 	/* js_server.c */
 	DLLEXPORT JSObject* DLLCALL js_CreateServerObject(JSContext* cx, JSObject* parent
@@ -1058,7 +1036,6 @@ extern "C" {
 	DLLEXPORT JSBool	DLLCALL js_CommonBranchCallback(JSContext*, js_branch_t*);
 	DLLEXPORT void		DLLCALL js_EvalOnExit(JSContext*, JSObject*, js_branch_t*);
 	DLLEXPORT void		DLLCALL	js_PrepareToExecute(JSContext*, JSObject*, const char *filename, const char* startup_dir);
-	DLLEXPORT char*		DLLCALL js_getstring(JSContext *cx, JSString *str);
 
 	/* js_system.c */
 	DLLEXPORT JSObject* DLLCALL js_CreateSystemObject(JSContext* cx, JSObject* parent
