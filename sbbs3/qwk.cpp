14195c81014a0e03719e1dea86261443d0ab3399
rswindell <rswindell>
2011-09-29 23:55:33 +0000

    Previous fix was bad. Reverting to use automatic variable buffer for copy of each line, but return pointer into str list (with no automatic white-space truncation). Thanks to Deuce for pointing out both issues.

--- sbbs3/qwk.cpp
+++ sbbs3/qwk.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet QWK packet-related functions */
 
-/* $Id: qwk.cpp,v 1.58 2011/11/06 03:58:57 rswindell Exp $ */
+/* $Id: qwk.cpp,v 1.56 2011/09/21 03:10:53 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -151,6 +151,7 @@ int sbbs_t::qwk_route(char *inaddr, char *fulladdr)
 		if(!fgets(str,256,stream))
 			break;
 		if(!strnicmp(str+9,node,strlen(node))) {
+			fclose(stream);
 			truncsp(str);
 			sprintf(fulladdr,"%s/%s",str+9+strlen(node),inaddr);
 			break; 
@@ -207,7 +208,7 @@ void sbbs_t::update_qwkroute(char *via)
 			for(i=0;i<total_nodes;i++)
 				if(qwk_time[i]>t)
 					fprintf(stream,"%s %s:%s\r\n"
-						,unixtodstr(&cfg,(time32_t)qwk_time[i],str),qwk_node[i],qwk_path[i]);
+						,unixtodstr(&cfg,qwk_time[i],str),qwk_node[i],qwk_path[i]);
 			fclose(stream); 
 		}
 		else
@@ -805,12 +806,12 @@ void sbbs_t::qwk_sec()
 
 void sbbs_t::qwksetptr(uint subnum, char *buf, int reset)
 {
-	long		l;
+	long	l;
 	uint32_t	last;
 
 	if(buf[2]=='/' && buf[5]=='/') {    /* date specified */
-		time_t t=dstrtounix(&cfg,buf);
-		subscan[subnum].ptr=getmsgnum(subnum,t);
+		l=dstrtounix(&cfg,buf);
+		subscan[subnum].ptr=getmsgnum(subnum,l);
 		return; 
 	}
 	l=atol(buf);
