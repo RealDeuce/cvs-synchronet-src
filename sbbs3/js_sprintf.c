ecd4b1817bab2b11d8c2b61dc0f2a9331053e2b4
deuce <deuce>
2011-10-09 17:47:42 +0000

    Remove booby trap.

--- sbbs3/js_sprintf.c
+++ sbbs3/js_sprintf.c
@@ -2,13 +2,13 @@
 
 /* Synchronet JavaScript "[s]printf" implementation */
 
-/* $Id: js_sprintf.c,v 1.8 2011/10/16 12:27:01 rswindell Exp $ */
+/* $Id: js_sprintf.c,v 1.4 2011/10/09 01:02:52 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -41,10 +41,10 @@
 char* DLLCALL
 js_sprintf(JSContext *cx, uint argn, uintN argc, jsval *argv)
 {
-	char*		p,*p2;
+	char*		p;
+    JSString*	str;
 
-	JSVALUE_TO_STRING(cx, argv[argn++], p, NULL);
-	if(p==NULL)
+	if((p=js_ValueToStringBytes(cx, argv[argn++], NULL))==NULL)
 		return(NULL);
 
 	p=xp_asprintf_start(p);
@@ -56,10 +56,9 @@ js_sprintf(JSContext *cx, uint argn, uintN argc, jsval *argv)
 		else if(JSVAL_IS_BOOLEAN(argv[argn]) && xp_printf_get_type(p)!=XP_PRINTF_TYPE_CHARP)
 			p=xp_asprintf_next(p,XP_PRINTF_CONVERT|XP_PRINTF_TYPE_INT,JSVAL_TO_BOOLEAN(argv[argn]));
 		else {
-			JSVALUE_TO_STRING(cx, argv[argn], p2, NULL);
-			if(p2==NULL)
-				return NULL;
-			p=xp_asprintf_next(p,XP_PRINTF_CONVERT|XP_PRINTF_TYPE_CHARP,p2);
+			if((str=JS_ValueToString(cx, argv[argn]))==NULL)
+			    return(NULL);
+			p=xp_asprintf_next(p,XP_PRINTF_CONVERT|XP_PRINTF_TYPE_CHARP,JS_GetStringBytes(str));
 		}
 	}
 
