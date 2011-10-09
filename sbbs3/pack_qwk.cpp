43d9261a8facbde8eb55168ce2773e66a202f96b
deuce <deuce>
2011-10-09 17:47:42 +0000

    Remove booby trap.

--- sbbs3/pack_qwk.cpp
+++ sbbs3/pack_qwk.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet pack QWK packet routine */
 
-/* $Id: pack_qwk.cpp,v 1.57 2009/03/20 00:39:46 rswindell Exp $ */
+/* $Id: pack_qwk.cpp,v 1.63 2011/09/21 03:10:53 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -70,7 +70,7 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 	const char* p;
 	const char* fmode;
 
-	ex=EX_OUTL|EX_OUTR;	/* Need sh for wildcard expansion */
+	ex=EX_STDOUT;
 	if(prepack)
 		ex|=EX_OFFLINE;
 
@@ -205,6 +205,66 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 		}
 	}
 
+	if(useron.qwk&QWK_EXT) {
+		/****************************/
+		/* Create TOREADER.EXT file */
+		/****************************/
+		SAFEPRINTF(str,"%sTOREADER.EXT",cfg.temp_dir);
+		if((stream=fopen(str,"wb"))==NULL) {
+			errormsg(WHERE,ERR_OPEN,str,0);
+			return(false); 
+		}
+
+		fprintf(stream,"ALIAS %s\r\n", useron.alias);
+
+		/* Double-checked with multimail (qwk.cc): */
+		for(i=0;i<usrgrps;i++) 
+			for(j=0;j<usrsubs[i];j++) {
+				fprintf(stream,"AREA %u "
+					,cfg.sub[usrsub[i][j]]->qwkconf ? cfg.sub[usrsub[i][j]]->qwkconf : ((i+1)*1000)+j+1);
+				switch(subscan[usrsub[i][j]].cfg&(SUB_CFG_NSCAN|SUB_CFG_YSCAN)) {
+					case SUB_CFG_NSCAN|SUB_CFG_YSCAN:
+						fputc('p', stream);	// p   for personal messages
+						break;
+					case SUB_CFG_NSCAN:
+						fputc('a', stream); // a   for all messages
+						break;
+				}
+				switch(cfg.sub[usrsub[i][j]]->misc&(SUB_PRIV|SUB_PONLY)) {
+					case SUB_PRIV|SUB_PONLY:
+						fputc('P', stream);	// P   if the area is private mail only
+						break;
+					case SUB_PRIV:
+						fputc('X', stream); // X   if either private or public mail is allowed
+						break;
+					default:
+						fputc('O', stream);	// O   if the area is public mail only
+						break;
+				}
+				if(useron.qwk&QWK_BYSELF)
+					fputc('w', stream);		// w   if this area should include mail written by themselves
+				if(cfg.sub[usrsub[i][j]]->misc&SUB_FORCED)
+					fputc('F', stream);		// F   if this area is forced to be read
+				if(!chk_ar(cfg.sub[usrsub[i][j]]->post_ar,&useron,&client))
+					fputc('R', stream);		// R   if the area is read-only (no posting at all allowed)
+				if(cfg.sub[usrsub[i][j]]->misc&SUB_QNET)
+					fputc('Q', stream);		// I   if the area is an internet area
+				if(cfg.sub[usrsub[i][j]]->misc&SUB_INET)
+					fputc('I', stream);		// I   if the area is an internet area
+				if(cfg.sub[usrsub[i][j]]->misc&SUB_FIDO)
+					fputc('E', stream);		// E   if the area is an echomail area
+				if((cfg.sub[usrsub[i][j]]->misc&(SUB_FIDO|SUB_INET|SUB_QNET))==0)
+					fputc('L', stream);		// L   if the area is a local message area
+				if((cfg.sub[usrsub[i][j]]->misc&SUB_NAME)==0)
+					fputc('H', stream);		// H   if the area is an handles only message area
+				if(cfg.sub[usrsub[i][j]]->misc&SUB_ANON)
+					fputc('A', stream);		// A   if the area allows messages 'from' any name (pick-an-alias)
+				
+				fprintf(stream,"\r\n");
+			}
+		fclose(stream);
+	}
+
 	/****************************************************/
 	/* Create MESSAGES.DAT, write header and leave open */
 	/****************************************************/
@@ -225,7 +285,7 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 			return(false); 
 		}
 	}
-	l=filelength(fileno(qwk));
+	l=(long)filelength(fileno(qwk));
 	if(l<1) {
 		fprintf(qwk,"%-128.128s","Produced by " VERSION_NOTICE "  " COPYRIGHT_NOTICE);
 		msgndx=1; 
@@ -278,7 +338,7 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 		qwkmail_last=0;
 		mail=loadmail(&smb,&mailmsgs,useron.number,0,useron.qwk&QWK_ALLMAIL ? 0
 			: LM_UNREAD);
-		if(mailmsgs && !(sys_status&SS_ABORT)) {
+		if(mailmsgs && (online==ON_LOCAL || !(sys_status&SS_ABORT))) {
 			bputs(text[QWKPackingEmail]);
 			if(!(useron.qwk&QWK_NOINDEX)) {
 				SAFEPRINTF(str,"%s000.NDX",cfg.temp_dir);
@@ -318,7 +378,7 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 						,cfg.data_dir,useron.number,msg.subj);
 					SAFEPRINTF2(tmp,"%s%s",cfg.temp_dir,msg.subj);
 					if(fexistcase(str) && !fexistcase(tmp))
-						mv(str,tmp,1); 
+						mv(str,tmp,/* copy: */TRUE); 
 				}
 
 				size=msgtoqwk(&msg,qwk,mode,INVALID_SUB,0,hdrs);
@@ -537,24 +597,29 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 	}
 	CRLF;
 
-	if(!prepack && (sys_status&SS_ABORT || !online))
+	if(!prepack && online!=ON_LOCAL && ((sys_status&SS_ABORT) || !online)) {
+		bputs(text[Aborted]);
 		return(false);
+	}
 
 	if(/*!prepack && */ useron.rest&FLAG('Q')) { /* If QWK Net node, check for files */
-		SAFEPRINTF2(str,"%sqnet/%s.out/",cfg.data_dir,useron.alias);
+		char id[LEN_QWKID+1];
+		SAFECOPY(id,useron.alias);
+		strlwr(id);
+		SAFEPRINTF2(str,"%sqnet/%s.out/",cfg.data_dir,id);
 		dir=opendir(str);
 		while(dir!=NULL && (dirent=readdir(dir))!=NULL) {    /* Move files into temp dir */
-			SAFEPRINTF3(str,"%sqnet/%s.out/%s",cfg.data_dir,useron.alias,dirent->d_name);
+			SAFEPRINTF3(str,"%sqnet/%s.out/%s",cfg.data_dir,id,dirent->d_name);
 			if(isdir(str))
 				continue;
 			SAFEPRINTF2(tmp2,"%s%s",cfg.temp_dir,dirent->d_name);
-			lncntr=0;	/* Default pause */
+			lncntr=0;	/* Defeat pause */
 			if(online==ON_LOCAL)
 				eprintf(LOG_INFO,"Including %s in packet",str);
 			else
 				lprintf(LOG_INFO,"Including %s in packet",str);
 			bprintf(text[RetrievingFile],str);
-			if(!mv(str,tmp2,1))
+			if(!mv(str,tmp2,/* copy: */TRUE))
 				netfiles++;
 		}
 		if(dir!=NULL)
@@ -591,8 +656,8 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 							? cfg.altpath[batdn_alt[i]-1]
 							: cfg.dir[batdn_dir[i]]->path
 							,tmp);
-						mv(str,tmp2,1); /* copy the file to temp dir */
-						getnodedat(cfg.node_num,&thisnode,1);
+						mv(str,tmp2,/* copy: */TRUE); /* copy the file to temp dir */
+						getnodedat(cfg.node_num,&thisnode,/* copy: */TRUE);
 						thisnode.aux=0xfe;
 						putnodedat(cfg.node_num,&thisnode);
 						CRLF; 
@@ -615,17 +680,17 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 		SAFEPRINTF(str,"%sQWK/HELLO",cfg.text_dir);
 		if(fexistcase(str)) {
 			SAFEPRINTF(tmp2,"%sHELLO",cfg.temp_dir);
-			mv(str,tmp2,1); 
+			mv(str,tmp2,/* copy: */TRUE); 
 		}
 		SAFEPRINTF(str,"%sQWK/BBSNEWS",cfg.text_dir);
 		if(fexistcase(str)) {
 			SAFEPRINTF(tmp2,"%sBBSNEWS",cfg.temp_dir);
-			mv(str,tmp2,1); 
+			mv(str,tmp2,/* copy: */TRUE); 
 		}
 		SAFEPRINTF(str,"%sQWK/GOODBYE",cfg.text_dir);
 		if(fexistcase(str)) {
 			SAFEPRINTF(tmp2,"%sGOODBYE",cfg.temp_dir);
-			mv(str,tmp2,1); 
+			mv(str,tmp2,/* copy: */TRUE); 
 		}
 		SAFEPRINTF(str,"%sQWK/BLT-*",cfg.text_dir);
 		glob(str,0,NULL,&g);
@@ -635,7 +700,7 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 			if(isdigit(str[4]) && isdigit(str[9])) {
 				SAFEPRINTF2(str,"%sQWK/%s",cfg.text_dir,fname);
 				SAFEPRINTF2(tmp2,"%s%s",cfg.temp_dir,fname);
-				mv(str,tmp2,1); 
+				mv(str,tmp2,/* copy: */TRUE); 
 			}
 		}
 		globfree(&g);
@@ -663,7 +728,7 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 		if(i)
 			errormsg(WHERE,ERR_EXEC,cmdstr(temp_cmd(),packet,tmp2,NULL),i);
 		else
-			errorlog("Couldn't compress QWK packet");
+			lprintf(LOG_ERR, "Couldn't compress QWK packet");
 		return(false); 
 	}
 
