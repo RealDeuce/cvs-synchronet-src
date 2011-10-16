0d7c22c337bfe34c4e15b7380b559bab462eec37
rswindell <rswindell>
2011-10-16 12:38:38 +0000

    No longer define JAVASCRIPT when building this module (it doesn't use JS).

--- xpdev/genwrap.h
+++ xpdev/genwrap.h
@@ -2,7 +2,7 @@
 
 /* General cross-platform development wrappers */
 
-/* $Id: genwrap.h,v 1.94 2010/05/24 01:13:10 rswindell Exp $ */
+/* $Id: genwrap.h,v 1.96 2011/05/11 00:46:32 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -60,6 +60,7 @@
 	#endif
 #elif defined(_WIN32)
 	#include <process.h>	/* getpid() */
+	typedef DWORD pid_t;
 #endif
 
 #if !defined(_WIN32)
@@ -323,7 +324,7 @@ DLLEXPORT int DLLCALL	get_errno(void);
 
 /* Mimic the Borland randomize() and random() CRTL functions */
 DLLEXPORT void		DLLCALL xp_randomize(void);
-DLLEXPORT int		DLLCALL	xp_random(int);
+DLLEXPORT long		DLLCALL	xp_random(int);
 
 DLLEXPORT long double  	DLLCALL	xp_timer(void);
 DLLEXPORT char*		DLLCALL os_version(char *str);
@@ -347,6 +348,9 @@ typedef		clock_t				msclock_t;
 	#define		MSCLOCKS_PER_SEC	1000
 	msclock_t	msclock(void);
 #endif
+
+DLLEXPORT BOOL		DLLCALL check_pid(pid_t);
+DLLEXPORT BOOL		DLLCALL	terminate_pid(pid_t);
 
 #if defined(__cplusplus)
 }
