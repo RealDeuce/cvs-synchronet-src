d6ddb9e9bf4f659550023f1ba10d70d1fc3dc9a8
rswindell <rswindell>
2011-10-21 11:17:23 +0000

    Updated MSVC10 project file (using property sheets).

--- sbbs3/chk_ar.cpp
+++ sbbs3/chk_ar.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet ARS checking routine */
 
-/* $Id: chk_ar.cpp,v 1.21 2009/03/20 09:36:20 rswindell Exp $ */
+/* $Id: chk_ar.cpp,v 1.22 2011/10/16 12:20:07 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
