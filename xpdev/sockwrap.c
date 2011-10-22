485e75d480a8afefde302024a517247038fee4f4
rswindell <rswindell>
2011-10-22 07:08:15 +0000

    Updated for msvc 2010.

--- xpdev/sockwrap.c
+++ xpdev/sockwrap.c
@@ -2,7 +2,7 @@
 
 /* Berkley/WinSock socket API wrappers */
 
-/* $Id: sockwrap.c,v 1.45 2013/08/31 10:14:56 deuce Exp $ */
+/* $Id: sockwrap.c,v 1.42 2011/09/19 03:08:56 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -252,7 +252,7 @@ int recvfilesocket(int sock, int file, off_t *offset, off_t count)
 		return(-1);
 	}
 		
-	if((buf=(char*)malloc((size_t)count))==NULL) {
+	if((buf=(char*)alloca((size_t)count))==NULL) {
 		errno=ENOMEM;
 		return(-1);
 	}
@@ -262,17 +262,14 @@ int recvfilesocket(int sock, int file, off_t *offset, off_t count)
 			return(-1);
 
 	rd=read(sock,buf,(size_t)count);
-	if(rd!=count) {
-		free(buf);
+	if(rd!=count)
 		return(-1);
-	}
 
 	wr=write(file,buf,rd);
 
 	if(offset!=NULL)
 		(*offset)+=wr;
 
-	free(buf);
 	return(wr);
 }
 
@@ -347,7 +344,7 @@ int retry_bind(SOCKET s, const struct sockaddr *addr, socklen_t addrlen
 	uint	i;
 
 	if(addr->sa_family==AF_INET)
-		SAFEPRINTF(port_str," to port %u",ntohs(((SOCKADDR_IN *)(addr))->sin_port));
+		SAFEPRINTF(port_str," to port %u",ntohs(((SOCKADDR_IN *)(addr))->sin_port)); 
 	else
 		port_str[0]=0;
 	for(i=0;i<=retries;i++) {
@@ -385,28 +382,4 @@ int nonblocking_connect(SOCKET sock, struct sockaddr* addr, size_t size, unsigne
 			getsockopt(sock, SOL_SOCKET, SO_ERROR, (void*)&result, &optlen);
 	}
 	return result;
-}
-
-const char *inet_addrtop(SOCKADDR *in, char *dest, size_t size)
-{
-	switch(in->sa_family) {
-		case AF_INET:
-			return inet_ntop(in->sa_family, &((struct sockaddr_in *)in)->sin_addr, dest, size);
-		case AF_INET6:
-			return inet_ntop(in->sa_family, &((struct sockaddr_in6 *)in)->sin6_addr, dest, size);
-		default:
-			return NULL;
-	}
-}
-
-uint16_t inet_addrport(SOCKADDR *in)
-{
-	switch(in->sa_family) {
-		case AF_INET:
-			return ntohs(((struct sockaddr_in *)in)->sin_port);
-		case AF_INET6:
-			return ntohs(((struct sockaddr_in6 *)in)->sin6_port);
-		default:
-			return 0;
-	}
 }
