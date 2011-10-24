632555f9c4fdbd8bbf4eb60fbc94c4a7ca6f413f
deuce <deuce>
2011-10-24 06:08:32 +0000

    Make the ARS filter work.
    Still to do:
    - Enable sorting
    - Enable Quick Validation Sets
    - Make the default window size and column widths sane

--- sbbs3/exec.cpp
+++ sbbs3/exec.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet command shell/module interpretter */
 
-/* $Id: exec.cpp,v 1.90 2010/04/02 23:12:02 deuce Exp $ */
+/* $Id: exec.cpp,v 1.96 2011/10/09 01:02:52 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -537,7 +537,7 @@ char * sbbs_t::copystrvar(csi_t *csi, char *p, char *str)
 #ifdef JAVASCRIPT
 
 static JSBool
-js_BranchCallback(JSContext *cx, JSScript *script)
+js_BranchCallback(JSContext *cx, JSObject *script)
 {
 	sbbs_t*		sbbs;
 
@@ -553,7 +553,7 @@ js_BranchCallback(JSContext *cx, JSScript *script)
 	return(js_CommonBranchCallback(cx,&sbbs->js_branch));
 }
 
-#ifdef USE_JS_OPERATION_CALLBACK
+#if JS_VERSION>180
 static JSBool
 js_OperationCallback(JSContext *cx)
 {
@@ -573,34 +573,23 @@ static const char* js_ext(const char* fname)
 	return("");
 }
 
-long sbbs_t::js_execfile(const char *cmd, const char* startup_dir)
+long sbbs_t::js_execfile(const char *cmd, const char* startup_dir, JSObject* scope)
 {
-	ulong		stack_frame;
 	char*		p;
 	char*		args=NULL;
 	char*		fname;
 	int			argc=0;
 	char		cmdline[MAX_PATH+1];
 	char		path[MAX_PATH+1];
-	JSObject*	js_scope=NULL;
-	JSScript*	js_script=NULL;
+	JSObject*	js_scope=scope;
+	JSObject*	js_script=NULL;
 	jsval		rval;
 	int32		result=0;
-	JSRuntime	*old_runtime=js_runtime;
-	JSContext	*old_context=js_cx;
-	JSObject	*old_glob=js_glob;
-	js_branch_t	old_branch;
-
-	memcpy(&old_branch, &js_branch, sizeof(old_branch));
-
-	js_init(&stack_frame);
-	js_create_user_objects();
 
 	if(js_cx==NULL) {
 		errormsg(WHERE,ERR_CHK,"JavaScript support",0);
 		errormsg(WHERE,ERR_EXEC,cmd,0);
-		result=-1;
-		goto reset_js;
+		return -1;
 	}
 
 	SAFECOPY(cmdline,cmd);
@@ -625,12 +614,12 @@ long sbbs_t::js_execfile(const char *cmd, const char* startup_dir)
 
 	if(!fexistcase(path)) {
 		errormsg(WHERE,ERR_OPEN,path,O_RDONLY);
-		result=-1;
-		goto reset_js;
+		return -1;
 	}
 
 	JS_BEGINREQUEST(js_cx);
-	js_scope=JS_NewObject(js_cx, NULL, NULL, js_glob);
+	if(js_scope==NULL)
+		js_scope=JS_NewObject(js_cx, NULL, NULL, js_glob);
 
 	if(js_scope!=NULL) {
 
@@ -670,44 +659,40 @@ long sbbs_t::js_execfile(const char *cmd, const char* startup_dir)
 		JS_ReportPendingException(js_cx);	/* Added Feb-2-2006, rswindell */
 		JS_ENDREQUEST(js_cx);
 		errormsg(WHERE,"compiling",path,0);
-		result=-1;
-		goto reset_js;
+		return -1;
 	}
 
-	js_branch.counter=0;	// Reset loop counter
+	if(scope==NULL) {
+		js_branch.counter=0;	// Reset loop counter
 
-#ifdef USE_JS_OPERATION_CALLBACK
-	JS_SetOperationCallback(js_cx, js_OperationCallback);
+#if JS_VERSION>180
+		JS_SetOperationCallback(js_cx, js_OperationCallback);
 #else
-	JS_SetBranchCallback(js_cx, js_BranchCallback);
+		JS_SetBranchCallback(js_cx, js_BranchCallback);
 #endif
 
-	js_PrepareToExecute(js_cx, js_glob, path, startup_dir);
+		js_PrepareToExecute(js_cx, js_glob, path, startup_dir);
+	}
 	JS_ExecuteScript(js_cx, js_scope, js_script, &rval);
 
-	JS_GetProperty(js_cx, js_scope, "exit_code", &rval);
-	if(rval!=JSVAL_VOID)
-		JS_ValueToInt32(js_cx,rval,&result);
+	if(scope==NULL) {
+		JS_GetProperty(js_cx, js_scope, "exit_code", &rval);
+		if(rval!=JSVAL_VOID)
+			JS_ValueToInt32(js_cx,rval,&result);
 
-	js_EvalOnExit(js_cx, js_scope, &js_branch);
+		js_EvalOnExit(js_cx, js_scope, &js_branch);
+	}
 
 	JS_ReportPendingException(js_cx);	/* Added Dec-4-2005, rswindell */
 
 	JS_DestroyScript(js_cx, js_script);
 
-	JS_ClearScope(js_cx, js_scope);
+	if(scope==NULL)
+		JS_ClearScope(js_cx, js_scope);
 
 	JS_GC(js_cx);
 
 	JS_ENDREQUEST(js_cx);
-
-reset_js:
-	js_cleanup(client_name);
-	js_runtime=old_runtime;
-	js_cx=old_context;
-	js_glob=old_glob;
-
-	memcpy(&js_branch, &old_branch, sizeof(old_branch));
 
 	return(result);
 }
@@ -1603,8 +1588,7 @@ int sbbs_t::exec(csi_t *csi)
 						if(trashcan(csi->str,"name"))
 							break;
 						if(cfg.uq&UQ_DUPREAL
-							&& userdatdupe(useron.number,U_NAME,LEN_NAME
-							,csi->str,0))
+							&& userdatdupe(useron.number,U_NAME,LEN_NAME,csi->str))
 							break;
 						sprintf(useron.name,"%.*s",LEN_NAME,csi->str);
 						putuserrec(&cfg,useron.number,U_NAME,LEN_NAME
@@ -1615,8 +1599,7 @@ int sbbs_t::exec(csi_t *csi)
 						if(trashcan(csi->str,"name"))
 							break;
 						if(cfg.uq&UQ_DUPHAND
-							&& userdatdupe(useron.number,U_HANDLE,LEN_HANDLE
-							,csi->str,0))
+							&& userdatdupe(useron.number,U_HANDLE,LEN_HANDLE,csi->str))
 							break;
 						sprintf(useron.handle,"%.*s",LEN_HANDLE,csi->str);
 						putuserrec(&cfg,useron.number,U_HANDLE,LEN_HANDLE
