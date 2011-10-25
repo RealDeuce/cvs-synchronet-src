78678a897985368d6dd9d401202dcac2dd8f7802
deuce <deuce>
2011-10-25 00:48:12 +0000

    SetSizeHints() was the magic we needed... we can now use autogenreated UI code for ListMain.

--- sbbs3/userlist/SBBS_User_ListMain.h
+++ sbbs3/userlist/SBBS_User_ListMain.h
@@ -10,17 +10,7 @@
 #ifndef SBBS_USER_LISTMAIN_H
 #define SBBS_USER_LISTMAIN_H
 
-//(*Headers(SBBS_User_ListFrame)
-#include <wx/listctrl.h>
-#include <wx/sizer.h>
-#include <wx/stattext.h>
-#include <wx/menu.h>
-#include <wx/textctrl.h>
-#include <wx/panel.h>
-#include <wx/choice.h>
-#include <wx/button.h>
-#include <wx/frame.h>
-//*)
+//(*Headers(SBBS_User_ListFrame)#include <wx/listctrl.h>#include <wx/sizer.h>#include <wx/stattext.h>#include <wx/menu.h>#include <wx/textctrl.h>#include <wx/panel.h>#include <wx/choice.h>#include <wx/button.h>#include <wx/frame.h>#include <wx/statusbr.h>//*)
 
 #include "SBBS_User_ListApp.h"
 
@@ -34,9 +24,6 @@ class SBBS_User_ListFrame: public wxFrame
     private:
 
         void fillUserList(void);
-		void CopyItems(int state);
-		void applyARS(void);
-		int	sort;
 
         //(*Handlers(SBBS_User_ListFrame)
         void OnQuit(wxCommandEvent& event);
@@ -46,44 +33,11 @@ class SBBS_User_ListFrame: public wxFrame
         void OnClearButtonClick(wxCommandEvent& event);
         void OnUserListItemSelect(wxListEvent& event);
         void OnQVChoiceSelect(wxCommandEvent& event);
-        void OnUserListItemRClick(wxListEvent& event);
-        void CopyMenuItemSelected(wxCommandEvent& event);
-        void CopyAllMenuItemSelected(wxCommandEvent& event);
-        void OnUserListColumnClick(wxListEvent& event);
         //*)
 
-        //(*Identifiers(SBBS_User_ListFrame)
-        static const long ID_STATICTEXT1;
-        static const long ID_ARSTEXTCTRL;
-        static const long ID_CLEARBUTTON;
-        static const long ID_USERLISTCTRL;
-        static const long ID_STATICTEXT2;
-        static const long ID_QVCHOICE;
-        static const long ID_REFRESHBUTTON;
-        static const long ID_EDITBUTTON;
-        static const long ID_PANEL1;
-        static const long ID_EDITUSER;
-        static const long ID_COPY;
-        static const long ID_COPYALL;
-        static const long ID_REFRESH;
-        //*)
+        //(*Identifiers(SBBS_User_ListFrame)        static const long ID_STATICTEXT1;        static const long ID_ARSTEXTCTRL;        static const long ID_CLEARBUTTON;        static const long ID_USERLISTCTRL;        static const long ID_STATICTEXT2;        static const long ID_QVCHOICE;        static const long ID_REFRESHBUTTON;        static const long ID_EDITBUTTON;        static const long ID_PANEL1;        static const long idMenuQuit;        static const long idMenuAbout;        static const long ID_STATUSBAR1;        //*)
 
-        //(*Declarations(SBBS_User_ListFrame)
-        wxButton* RefreshButton;
-        wxMenuItem* MenuItem5;
-        wxStaticText* StaticText2;
-        wxMenuItem* MenuItem4;
-        wxPanel* Panel1;
-        wxStaticText* StaticText1;
-        wxMenu ContextMenu;
-        wxListCtrl* UserList;
-        wxMenuItem* MenuItem3;
-        wxMenuItem* MenuItem6;
-        wxButton* ClearButton;
-        wxChoice* QVChoice;
-        wxButton* EditButton;
-        wxTextCtrl* ARSFilter;
-        //*)
+        //(*Declarations(SBBS_User_ListFrame)        wxButton* RefreshButton;        wxStaticText* StaticText2;        wxPanel* Panel1;        wxStaticText* StaticText1;        wxListCtrl* UserList;        wxStatusBar* StatusBar1;        wxButton* ClearButton;        wxChoice* QVChoice;        wxButton* EditButton;        wxTextCtrl* ARSFilter;        //*)
 
         unsigned char *ars;
         DECLARE_EVENT_TABLE()
