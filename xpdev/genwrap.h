02dd16920c34af3469b480501e095bd656d54f23
deuce <deuce>
2011-10-25 07:58:35 +0000

    Enable sorting.

--- xpdev/genwrap.h
+++ xpdev/genwrap.h
@@ -2,13 +2,13 @@
 
 /* General cross-platform development wrappers */
 
-/* $Id: genwrap.h,v 1.95 2010/06/03 01:34:41 rswindell Exp $ */
+/* $Id: genwrap.h,v 1.98 2011/10/24 19:18:03 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -60,7 +60,9 @@
 	#endif
 #elif defined(_WIN32)
 	#include <process.h>	/* getpid() */
-	typedef DWORD pid_t;
+	#ifndef __MINGW32__
+        typedef DWORD pid_t;
+	#endif
 #endif
 
 #if !defined(_WIN32)
@@ -87,7 +89,7 @@ extern "C" {
 #if defined(__BORLANDC__)
 
 	#define DESCRIBE_COMPILER(str) SAFEPRINTF2(str,"BCC %X.%02X" \
-		,__BORLANDC__>>8,__BORLANDC__&0xff);	
+		,__BORLANDC__>>8,__BORLANDC__&0xff);
 
 #elif defined(_MSC_VER)
 
@@ -111,7 +113,7 @@ extern "C" {
 #elif defined(__DMC__)	/* Digital Mars C/C++ */
 
 	#define DESCRIBE_COMPILER(str) SAFEPRINTF(str,"DMC %X.%02X" \
-		,__DMC__>>8,__DMC__&0xff);	
+		,__DMC__>>8,__DMC__&0xff);
 
 #else /* Unknown compiler */
 
@@ -317,14 +319,14 @@ DLLEXPORT int DLLCALL	get_errno(void);
 #endif
 
 /* Win32 implementations of recursive (thread-safe) std C time functions on Unix */
-#if !defined(__unix__)	
+#if !defined(__unix__)
 
 	DLLEXPORT char*			DLLCALL		strtok_r(char *str, const char *delim, char **last);
 #endif
 
 /* Mimic the Borland randomize() and random() CRTL functions */
 DLLEXPORT void		DLLCALL xp_randomize(void);
-DLLEXPORT int		DLLCALL	xp_random(int);
+DLLEXPORT long		DLLCALL	xp_random(int);
 
 DLLEXPORT long double  	DLLCALL	xp_timer(void);
 DLLEXPORT char*		DLLCALL os_version(char *str);
