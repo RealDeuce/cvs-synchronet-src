fadb5c96a083fca893ca5d1966cb924963cd637f
deuce <deuce>
2011-10-26 16:38:20 +0000

    Move the setting of LD_RUN_PATH into sbbs3/Common.gmake

--- sbbs3/sbbsecho.h
+++ sbbs3/sbbsecho.h
@@ -2,13 +2,13 @@
 
 /* Synchronet FidoNet Echomail tosser/scanner/areafix program */
 
-/* $Id: sbbsecho.h,v 1.26 2014/01/15 02:28:03 rswindell Exp $ */
+/* $Id: sbbsecho.h,v 1.14 2009/01/15 08:02:29 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -37,10 +37,7 @@
 
 /* Portions written by Allen Christiansen 1994-1996 						*/
 
-#define SBBSECHO_VERSION_MAJOR		2
-#define SBBSECHO_VERSION_MINOR		26
-
-#define SBBSECHO_PRODUCT_CODE		0x12FF	/* from http://ftsc.org/docs/ftscprod.013 */
+#define SBBSECHO_VER "2.12"
 
 #define IMPORT_NETMAIL  (1L<<0)
 #define IMPORT_PACKETS	(1L<<1)
@@ -143,9 +140,9 @@ typedef struct {						/* Type 2+ Packet Header Info */
 	} two_plus_t;
 
 typedef struct {						/* Type 2.2 Packet Header Info */
-	char origdomn[8],					/* Origination Domain */
-		  destdomn[8];					/* Destination Domain */
-	uchar	  empty[4]; 					/* Product Specific Data */
+	uchar origdomn[8],					/* Origination Domain */
+		  destdomn[8],					/* Destination Domain */
+		  empty[4]; 					/* Product Specific Data */
 	} two_two_t;
 
 typedef struct {
@@ -170,9 +167,9 @@ typedef struct {
 	} area_t;
 
 typedef struct {
-	FILE *stream;				/* The stream associated with this packet (NULL if not-open) */
+	FILE *stream;				/* The stream associated with this packet */
 	faddr_t uplink; 			/* The current uplink for this packet */
-	char filename[MAX_PATH+1];	/* Name of the file */
+	uchar filename[128],curopen;/* Name of the file and if it's open or not */
     } outpkt_t;
 
 typedef struct {
@@ -224,8 +221,7 @@ typedef struct {
 			   ,outbound[82]		/* Outbound directory */
 			   ,areafile[128]		/* AREAS.BBS path/filename */
 			   ,logfile[128]		/* LOG path/filename */
-			   ,cfgfile[128]		/* Configuration path/filename */
-			   ,sysop_alias[FIDO_NAME_LEN];
+			   ,cfgfile[128];		/* Configuration path/filename */
 	ulong		maxpktsize			/* Maximum size for packets */
 			   ,maxbdlsize			/* Maximum size for bundles */
 			   ,log					/* What do we log? */
@@ -241,8 +237,6 @@ typedef struct {
 	echolist_t *listcfg;			/* Each echolist configuration */
 	areasbbs_t *area;				/* Each area configuration */
 	BOOL		check_path;			/* Enable circular path detection */
-	BOOL		zone_blind;			/* Pretend zones don't matter when parsing and constructing PATH and SEEN-BY lines (per Wilfred van Velzen, 2:280/464) */
-	uint16_t	zone_blind_threshold;	/* Zones below this number (e.g. 4) will be treated as the same zone when zone_blind is enabled */
 	} config_t;
 
 #ifdef __WATCOMC__
