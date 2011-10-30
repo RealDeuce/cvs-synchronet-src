aa5f067e336f64db5a00983157f9f580262250dc
rswindell <rswindell>
2011-10-30 00:43:26 +0000

    Added lots of PRI and SCN macros for Win32 builds.

--- sbbs3/userdat.h
+++ sbbs3/userdat.h
@@ -2,7 +2,7 @@
 
 /* Synchronet user data access routines (exported) */
 
-/* $Id: userdat.h,v 1.43 2011/09/03 05:56:13 rswindell Exp $ */
+/* $Id: userdat.h,v 1.47 2011/10/24 21:46:45 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -50,15 +50,20 @@
 #endif
 
 #ifdef _WIN32
-	#ifdef SBBS_EXPORTS
-		#define DLLEXPORT __declspec(dllexport)
-	#else
-		#define DLLEXPORT __declspec(dllimport)
-	#endif
-	#ifdef __BORLANDC__
-		#define DLLCALL __stdcall
-	#else
+	#ifdef __MINGW32__
+		#define DLLEXPORT
 		#define DLLCALL
+	#else
+		#ifdef SBBS_EXPORTS
+			#define DLLEXPORT __declspec(dllexport)
+		#else
+			#define DLLEXPORT __declspec(dllimport)
+		#endif
+		#ifdef __BORLANDC__
+			#define DLLCALL __stdcall
+		#else
+			#define DLLCALL
+		#endif
 	#endif
 #else
 	#define DLLEXPORT
@@ -112,7 +117,8 @@ DLLEXPORT BOOL	DLLCALL can_user_read_sub(scfg_t* cfg, uint subnum, user_t* user,
 DLLEXPORT BOOL	DLLCALL can_user_post(scfg_t* cfg, uint subnum, user_t* user, client_t* client, uint* reason);
 DLLEXPORT BOOL	DLLCALL is_user_subop(scfg_t* cfg, uint subnum, user_t* user, client_t* client);
 DLLEXPORT BOOL	DLLCALL is_download_free(scfg_t* cfg, uint dirnum, user_t* user, client_t* client);
-DLLEXPORT BOOL	DLLCALL filter_ip(scfg_t* cfg, char* prot, char* reason, char* host, char* ip_addr, char* username, char* fname);
+DLLEXPORT BOOL	DLLCALL filter_ip(scfg_t* cfg, const char* prot, const char* reason, const char* host
+								  ,const char* ip_addr, const char* username, const char* fname);
 
 /* New atomic numeric user field adjustment functions: */
 DLLEXPORT BOOL	DLLCALL user_posted_msg(scfg_t* cfg, user_t* user, int count);
@@ -131,8 +137,8 @@ typedef struct {
 	IN_ADDR		addr;	/* host with consecutive failed login attmepts */
 	ulong		count;	/* number of consecutive failed login attempts */
 	ulong		dupes;	/* number of consecutive dupliate login attempts (same name and password) */
-	time_t		time;	/* time of last attempt */
-	const char*	prot;	/* protocol used in last attempt */
+	time32_t	time;	/* time of last attempt */
+	char		prot[32];	/* protocol used in last attempt */
 	char		user[128];
 	char		pass[128];
 } login_attempt_t;
