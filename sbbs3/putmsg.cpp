0097a05c16ca12d952f7648b8a38ebebef597e8f
rswindell <rswindell>
2011-11-04 10:03:17 +0000

    Log sender's email address in brackets on "Created message" log line.

--- sbbs3/putmsg.cpp
+++ sbbs3/putmsg.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet message/menu display routine */
  
-/* $Id: putmsg.cpp,v 1.31 2013/07/10 04:30:03 rswindell Exp $ */
+/* $Id: putmsg.cpp,v 1.27 2011/11/04 09:55:08 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2013 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -51,7 +51,6 @@
 char sbbs_t::putmsg(const char *buf, long mode)
 {
 	char	tmpatr,tmp2[256],tmp3[128];
-	char	ret;
 	char*	str=(char*)buf;
 	uchar	exatr=0;
 	int 	orgcon=console,i;
@@ -68,12 +67,10 @@ char sbbs_t::putmsg(const char *buf, long mode)
 		putcom("\x02\x02");
 	if(mode&P_WORDWRAP) {
 		char *wrapped;
-		if((wrapped=::wordwrap((char*)buf, cols, 79, /* handle_quotes: */TRUE)) == NULL)
+		if((wrapped=::wordwrap((char*)buf, cols-1, 79, WORDWRAP_FLAG_QUOTES)) == NULL)
 			errormsg(WHERE,ERR_ALLOC,"wordwrap buffer",0);
-		else {
-			truncsp_lines(wrapped);
+		else
 			str=wrapped;
-		}
 	}
 
 	while(str[l] && (mode&P_NOABORT || !msgabort()) && online) {
@@ -81,7 +78,7 @@ char sbbs_t::putmsg(const char *buf, long mode)
 			if(str[l+1]=='"' && !(sys_status&SS_NEST_PF)) {  /* Quote a file */
 				l+=2;
 				i=0;
-				while(i<(int)sizeof(tmp2)-1 && isprint((unsigned char)str[l]) && str[l]!='\\' && str[l]!='/')
+				while(i<(int)sizeof(tmp2)-1 && isprint(str[l]) && str[l]!='\\' && str[l]!='/')
 					tmp2[i++]=str[l++];
 				tmp2[i]=0;
 				sys_status|=SS_NEST_PF; 	/* keep it only one message deep! */
@@ -101,14 +98,14 @@ char sbbs_t::putmsg(const char *buf, long mode)
 			l++; 
 		}
 		else if(cfg.sys_misc&SM_PCBOARD && str[l]=='@' && str[l+1]=='X'
-			&& isxdigit((unsigned char)str[l+2]) && isxdigit((unsigned char)str[l+3])) {
+			&& isxdigit(str[l+2]) && isxdigit(str[l+3])) {
 			sprintf(tmp2,"%.2s",str+l+2);
 			attr(ahtoul(tmp2));
 			exatr=1;
 			l+=4; 
 		}
 		else if(cfg.sys_misc&SM_WILDCAT && str[l]=='@' && str[l+3]=='@'
-			&& isxdigit((unsigned char)str[l+1]) && isxdigit((unsigned char)str[l+2])) {
+			&& isxdigit(str[l+1]) && isxdigit(str[l+2])) {
 			sprintf(tmp2,"%.2s",str+l+1);
 			attr(ahtoul(tmp2));
 			// exatr=1;
@@ -129,7 +126,7 @@ char sbbs_t::putmsg(const char *buf, long mode)
 			exatr=1;
 			l+=3;	/* Skip |xx */
 		}	
-		else if(cfg.sys_misc&SM_CELERITY && str[l]=='|' && isalpha((unsigned char)str[l+1])
+		else if(cfg.sys_misc&SM_CELERITY && str[l]=='|' && isalpha(str[l+1])
 			&& !(useron.misc&(RIP|WIP))) {
 			switch(str[l+1]) {
 				case 'k':
@@ -293,13 +290,12 @@ char sbbs_t::putmsg(const char *buf, long mode)
 			pause();
 	}
 
-	ret=str[l];
 	if(str!=buf)	/* malloc'd copy of buffer */
 		free(str);
 
 	/* Restore original settings of Forced Pause On/Off */
 	sys_status&=~(SS_PAUSEOFF|SS_PAUSEON);
 	sys_status|=(sys_status_sav&(SS_PAUSEOFF|SS_PAUSEON));
-	return(ret);
+	return(str[l]);
 }
 
