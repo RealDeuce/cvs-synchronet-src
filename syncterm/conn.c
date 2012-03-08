6a88e7a0ad09e454f585df7ca7bb378f541d2bb1
deuce <deuce>
2012-03-08 18:15:09 +0000

    "Fix" command line ordering for the new Linux linker command line rules.

--- syncterm/conn.c
+++ syncterm/conn.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: conn.c,v 1.70 2014/02/06 12:07:53 deuce Exp $ */
+/* $Id: conn.c,v 1.66 2012/02/15 19:41:45 deuce Exp $ */
 
 #include <stdlib.h>
 
@@ -35,9 +35,7 @@
 #include "rlogin.h"
 #include "raw.h"
 #include "ssh.h"
-#ifndef __HAIKU__
 #include "modem.h"
-#endif
 #ifdef __unix__
 #include "conn_pty.h"
 #endif
@@ -344,7 +342,6 @@ int conn_connect(struct bbslist *bbs)
 			conn_api.close=ssh_close;
 			break;
 #endif
-#ifndef __HAIKU__
 		case CONN_TYPE_SERIAL:
 			conn_api.connect=modem_connect;
 			conn_api.close=serial_close;
@@ -353,7 +350,6 @@ int conn_connect(struct bbslist *bbs)
 			conn_api.connect=modem_connect;
 			conn_api.close=modem_close;
 			break;
-#endif
 #ifdef __unix__
 		case CONN_TYPE_SHELL:
 			conn_api.connect=pty_connect;
@@ -420,18 +416,6 @@ int conn_socket_connect(struct bbslist *bbs)
 	uifc.pop("Looking up host");
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_flags=PF_UNSPEC;
-	switch(bbs->address_family) {
-		case ADDRESS_FAMILY_INET:
-			hints.ai_family=PF_INET;
-			break;
-		case ADDRESS_FAMILY_INET6:
-			hints.ai_family=PF_INET6;
-			break;
-		case ADDRESS_FAMILY_UNSPEC:
-		default:
-			hints.ai_family=PF_UNSPEC;
-			break;
-	}
 	hints.ai_socktype=SOCK_STREAM;
 	hints.ai_protocol=IPPROTO_TCP;
 	hints.ai_flags=AI_NUMERICSERV;
@@ -470,7 +454,7 @@ int conn_socket_connect(struct bbslist *bbs)
 #if (EAGAIN!=EWOULDBLOCK)
 				case EWOULDBLOCK:
 #endif
-					for(;sock!=INVALID_SOCKET;) {
+					for(;;) {
 						tv.tv_sec=1;
 						tv.tv_usec=0;
 
@@ -488,11 +472,7 @@ int conn_socket_connect(struct bbslist *bbs)
 								sock=INVALID_SOCKET;
 								continue;
 							case 1:
-								if(socket_check(sock, NULL, NULL, 0))
-									goto connected;
-								closesocket(sock);
-								sock=INVALID_SOCKET;
-								continue;
+								goto connected;
 							default:
 								break;
 						}
