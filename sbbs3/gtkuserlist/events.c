e7d987c0cd8853771b1769bc8d22f0f0e52b24bb
deuce <deuce>
2012-04-26 19:55:14 +0000

    Fix warnings and bugs I don't really care about.

--- sbbs3/gtkuserlist/events.c
+++ sbbs3/gtkuserlist/events.c
@@ -2,6 +2,8 @@
 #include "sbbsdefs.h"
 #include "gtkuserlist.h"
 
+GladeXML		*lxml;
+
 char *complete_path(char *dest, char *path, char *filename)
 {
 	if(path != NULL) {
@@ -25,7 +27,7 @@ void exec_cmdline(void *cmdline)
 		return;
 	system((char *)cmdline);
 	free(cmdline);
-	w=GTK_WIDGET(gtk_builder_get_object(builder, "UserListWindow"));
+	w=glade_xml_get_widget(lxml, "UserListWindow");
 	gtk_widget_set_sensitive(GTK_WIDGET(w), TRUE);
 }
 
@@ -34,7 +36,7 @@ void run_external(char *path, char *filename)
 	static char	cmdline[MAX_PATH*2];
 	GtkWidget	*w;
 
-	w=GTK_WIDGET(gtk_builder_get_object(builder, "UserListWindow"));
+	w=glade_xml_get_widget(lxml, "UserListWindow");
 	gtk_widget_set_sensitive(GTK_WIDGET(w), FALSE);
 	complete_path(cmdline,path,filename);
 	_beginthread(exec_cmdline, 0, strdup(cmdline));
@@ -45,7 +47,7 @@ void display_message(char *title, char *message, char *icon)
 	GtkWidget *dialog, *label;
 
 	dialog=gtk_dialog_new_with_buttons(title
-			,GTK_WINDOW(gtk_builder_get_object(builder, "UserListWindow"))
+			,GTK_WINDOW(glade_xml_get_widget(lxml, "UserListWindow"))
 			,GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT
 			,GTK_STOCK_OK
 			,GTK_RESPONSE_NONE
@@ -65,17 +67,17 @@ void display_message(char *title, char *message, char *icon)
 	gtk_dialog_run(GTK_DIALOG(dialog));
 }
 
-G_MODULE_EXPORT void update_userlist_sensitive_callback(GtkTreeSelection *wiggy, gpointer data)
+void update_userlist_sensitive_callback(GtkTreeSelection *wiggy, gpointer data)
 {
 	GtkWidget	*w;
 	int selected;
 
-	w=GTK_WIDGET(gtk_builder_get_object(builder, "bUserListEditUser"));
+	w=glade_xml_get_widget(lxml, "bUserListEditUser");
 	selected=gtk_tree_selection_count_selected_rows(wiggy);
 	gtk_widget_set_sensitive(w, selected==1);
 }
 
-G_MODULE_EXPORT void update_userlist_item(GtkListStore *lstore, GtkTreeIter *curr, int usernum)
+void update_userlist_item(GtkListStore *lstore, GtkTreeIter *curr, int usernum)
 {
 	char			sex[2];
 	char			first[9];
@@ -115,7 +117,7 @@ G_MODULE_EXPORT void update_userlist_item(GtkListStore *lstore, GtkTreeIter *cur
 		,-1);
 }
 
-G_MODULE_EXPORT void update_userlist_callback(GtkWidget *wiggy, gpointer data)
+void update_userlist_callback(GtkWidget *wiggy, gpointer data)
 {
 	GtkWidget		*w;
 	GtkListStore	*lstore = NULL;
@@ -130,7 +132,7 @@ G_MODULE_EXPORT void update_userlist_callback(GtkWidget *wiggy, gpointer data)
 		return;
     }
 
-	w=GTK_WIDGET(gtk_builder_get_object(builder, "lUserList"));
+	w=glade_xml_get_widget(lxml, "lUserList");
 	lstore=GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(w)));
 	gtk_list_store_clear(lstore);
 	totalusers=lastuser(&cfg);
@@ -171,7 +173,7 @@ void quick_validate(int usernum, int set)
 	}
 }
 
-G_MODULE_EXPORT void userlist_do_quick_validate(GtkTreeModel *model, GtkTreePath *path, GtkTreeIter *iter, gpointer data)
+void userlist_do_quick_validate(GtkTreeModel *model, GtkTreePath *path, GtkTreeIter *iter, gpointer data)
 {
 	int	*set=data;
 	int	usernum;
@@ -181,12 +183,12 @@ G_MODULE_EXPORT void userlist_do_quick_validate(GtkTreeModel *model, GtkTreePath
 	update_userlist_item(GTK_LIST_STORE(model), iter, usernum);
 }
 
-G_MODULE_EXPORT void on_userlist_quick_validate(GtkWidget *wiggy, gpointer data)
+void on_userlist_quick_validate(GtkWidget *wiggy, gpointer data)
 {
 	int		set;
 	GtkWidget	*w;
 
-	w=GTK_WIDGET(gtk_builder_get_object(builder, "lUserList"));
+	w=glade_xml_get_widget(lxml, "lUserList");
 	set=gtk_combo_box_get_active(GTK_COMBO_BOX(wiggy))-1;
 	if(set>=0) {
 		gtk_tree_selection_selected_foreach(gtk_tree_view_get_selection (GTK_TREE_VIEW (w))
@@ -196,7 +198,7 @@ G_MODULE_EXPORT void on_userlist_quick_validate(GtkWidget *wiggy, gpointer data)
 	}
 }
 
-G_MODULE_EXPORT void get_lastselected_user(GtkTreeModel *model, GtkTreePath *path, GtkTreeIter *iter, gpointer data)
+void get_lastselected_user(GtkTreeModel *model, GtkTreePath *path, GtkTreeIter *iter, gpointer data)
 {
 	int	*i=data;
 	int	user;
@@ -205,13 +207,13 @@ G_MODULE_EXPORT void get_lastselected_user(GtkTreeModel *model, GtkTreePath *pat
 	*i=user;
 }
 
-G_MODULE_EXPORT void userlist_edituser(GtkWidget *wiggy, gpointer data)
+void userlist_edituser(GtkWidget *wiggy, gpointer data)
 {
 	char	str[MAX_PATH+1];
 	int		i;
 	GtkWidget	*w;
 
-	w=GTK_WIDGET(gtk_builder_get_object(builder, "lUserList"));
+	w=glade_xml_get_widget(lxml, "lUserList");
 	gtk_tree_selection_selected_foreach(gtk_tree_view_get_selection (GTK_TREE_VIEW (w))
 			,get_lastselected_user
 			,&i);
@@ -220,20 +222,20 @@ G_MODULE_EXPORT void userlist_edituser(GtkWidget *wiggy, gpointer data)
 	run_external(cfg.exec_dir,str);
 }
 
-G_MODULE_EXPORT void apply_ars_filter(GtkWidget *wiggy, gpointer data)
+void apply_ars_filter(GtkWidget *wiggy, gpointer data)
 {
 	GtkWidget	*w;
 
-	w=GTK_WIDGET(gtk_builder_get_object(builder, "eArsFilter"));
+	w=glade_xml_get_widget(lxml, "eArsFilter");
 	arbuf=arstr(NULL, (char *)gtk_entry_get_text(GTK_ENTRY(w)), &cfg);
 	update_userlist_callback(wiggy, data);
 }
 
-G_MODULE_EXPORT void clear_ars_filter(GtkWidget *wiggy, gpointer data)
+void clear_ars_filter(GtkWidget *wiggy, gpointer data)
 {
 	GtkWidget	*w;
 
-	w=GTK_WIDGET(gtk_builder_get_object(builder, "eArsFilter"));
+	w=glade_xml_get_widget(lxml, "eArsFilter");
 	gtk_entry_set_text(GTK_ENTRY(w),"");
 	arbuf=NULL;
 	update_userlist_callback(wiggy, data);
