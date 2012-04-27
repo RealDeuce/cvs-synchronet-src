a577f15095992d433faf9708cbe118b48fe7cc92
deuce <deuce>
2012-04-27 16:18:57 +0000

    Fix rounding error where random(6) would return 6 one time in 20000 or so.

--- xpdev/genwrap.c
+++ xpdev/genwrap.c
@@ -2,13 +2,13 @@
 
 /* General cross-platform development wrappers */
 
-/* $Id: genwrap.c,v 1.90 2012/04/30 03:49:10 rswindell Exp $ */
+/* $Id: genwrap.c,v 1.88 2012/04/27 16:18:57 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2012 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -177,14 +177,15 @@ char* DLLCALL c_escape_char(char ch)
 char* DLLCALL c_escape_str(const char* src, char* dst, size_t maxlen, BOOL ctrl_only)
 {
 	const char*	s;
-	char*		d;
-	const char*	e;
+	char*	d;
+	char*	e;
 
-	for(s=src,d=dst;*s && (size_t)(d-dst)<maxlen;s++) {
+	for(s=src,d=dst;*s && (size_t)(d-dst)<maxlen;s++,d++) {
 		if((!ctrl_only || (uchar)*s < ' ') && (e=c_escape_char(*s))!=NULL) {
-			strncpy(d,e,maxlen-(d-dst));
-			d+=strlen(d);
-		} else *d++=*s;
+			*d=0;
+			strncat(dst,e,maxlen-(d-dst));
+			d++;
+		} else *d=*s;
 	}
 	*d=0;
 
@@ -341,9 +342,9 @@ long DLLCALL xp_random(int n)
 	if(n<2)
 		return(0);
 	do {
-		f=(double)rand()/(double)(RAND_MAX+1);
-		ret=(int)(n*f);
-	} while(ret==n);
+		f=(double)rand()/(double)RAND_MAX;
+		ret=(n*f);
+	} while ret==n;
 
 	return(ret);
 #endif
