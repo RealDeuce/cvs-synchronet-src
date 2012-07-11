98ceb076ca4ad93e776f57f85e95f5f92076ee3a
deuce <deuce>
2012-07-11 23:38:27 +0000

    Fix inverted logic in last commit.

--- sbbs3/scfg/scfgxfr2.c
+++ sbbs3/scfg/scfgxfr2.c
@@ -1,12 +1,12 @@
 /* scfgxfr2.c */
 
-/* $Id: scfgxfr2.c,v 1.31 2011/09/08 03:25:16 rswindell Exp $ */
+/* $Id: scfgxfr2.c,v 1.33 2012/06/15 00:50:32 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2012 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -547,6 +547,7 @@ command: `DIR /ON /AD /B > DIRS.RAW`
 						p+=5;
 						while(*p && *p<=' ') p++;
 						SAFECOPY(tmp_code,p);
+						truncstr(tmp_code," \t");
 						while(*p>' ') p++;			/* Skip areaname */
 						while(*p && *p<=' ') p++;	/* Skip space */
 						while(*p>' ') p++;			/* Skip level */
@@ -718,7 +719,7 @@ To configure a directory, select it with the arrow keys and hit ~ ENTER ~.
 		return;
 	if((i&MSK_ON)==MSK_INS) {
 		i&=MSK_OFF;
-		strcpy(str,"Games");
+		strcpy(str,"My Brand-New File Directory");
 		SETHELP(WHERE);
 /*
 `Directory Long Name:`
