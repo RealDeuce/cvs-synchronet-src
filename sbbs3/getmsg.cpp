3100b3a6a2bca9924a3346b0af837c5805f70c3f
deuce <deuce>
2012-07-11 23:33:03 +0000

    Don't redisplay message after validating or deleting.

--- sbbs3/getmsg.cpp
+++ sbbs3/getmsg.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet message retrieval functions */
 
-/* $Id: getmsg.cpp,v 1.41 2011/11/02 23:30:11 rswindell Exp $ */
+/* $Id: getmsg.cpp,v 1.43 2011/11/06 03:54:14 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -187,8 +187,9 @@ void sbbs_t::show_msg(smbmsg_t* msg, long mode)
 	show_msghdr(msg);
 
 	if((text=smb_getmsgtxt(&smb,msg,GETMSGTXT_ALL))!=NULL) {
-		truncsp_lines(text);
-		putmsg(text, mode|P_WORDWRAP);
+		if(!(console&CON_RAW_IN))
+			mode|=P_WORDWRAP;
+		putmsg(text, mode);
 		smb_freemsgtxt(text);
 	}
 }
--- sbbs3/sbbs.h
+++ sbbs3/sbbs.h
@@ -2,7 +2,7 @@
 
 /* Synchronet class (sbbs_t) definition and exported function prototypes */
 
-/* $Id: sbbs.h,v 1.386 2011/11/02 09:07:24 rswindell Exp $ */
+/* $Id: sbbs.h,v 1.391 2012/07/11 22:25:46 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -226,6 +226,7 @@ public:
 	HANDLE	input_thread;
 	pthread_mutex_t	input_thread_mutex;
 	bool	input_thread_mutex_locked;	// by someone other than the input_thread
+	pthread_mutex_t	ssh_mutex;
 
 	int 	outcom(uchar ch); 	   // send character
 	int 	incom(unsigned long timeout=0);		   // receive character
@@ -533,7 +534,7 @@ public:
 	void	delallmail(uint usernumber, int which, bool permanent=true);
 
 	/* getmsg.cpp */
-	post_t* loadposts(int32_t *posts, uint subnum, ulong ptr, long mode);
+	post_t* loadposts(int32_t *posts, uint subnum, ulong ptr, long mode, ulong *unvalidated_num);
 
 	/* readmail.cpp */
 	void	readmail(uint usernumber, int sent);
@@ -921,6 +922,7 @@ extern "C" {
 	DLLEXPORT size_t	DLLCALL strip_invalid_attr(char *str);
 	DLLEXPORT char *	DLLCALL ultoac(ulong l,char *str);
 	DLLEXPORT char *	DLLCALL rot13(char* str);
+	DLLEXPORT uint32_t	DLLCALL str_to_bits(uint32_t currval, const char *str);
 
 	/* msg_id.c */
 	DLLEXPORT char *	DLLCALL ftn_msgid(sub_t* sub, smbmsg_t* msg, char* msgid, size_t);
@@ -1045,6 +1047,15 @@ extern "C" {
 										,js_server_props_t* props);
 
 	/* js_global.c */
+	typedef struct {
+		scfg_t*				cfg;
+		jsSyncMethodSpec*	methods;
+		js_startup_t*		startup;
+		unsigned			bg_count;
+		sem_t				bg_sem;
+		str_list_t			exit_func;
+	} global_private_t;
+	DLLEXPORT BOOL DLLCALL js_argc(JSContext *cx, uintN argc, uintN min);
 	DLLEXPORT BOOL DLLCALL js_CreateGlobalObject(JSContext* cx, scfg_t* cfg, jsSyncMethodSpec* methods, js_startup_t*, JSObject**);
 	DLLEXPORT BOOL	DLLCALL js_CreateCommonObjects(JSContext* cx
 													,scfg_t* cfg				/* common */
