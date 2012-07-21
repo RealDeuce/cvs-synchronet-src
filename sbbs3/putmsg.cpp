2069c950dd3526f39ed926b8f6705a6b8a855993
deuce <deuce>
2012-07-21 21:30:04 +0000

    We can't change the configuration until after we call cryptInit().

--- sbbs3/putmsg.cpp
+++ sbbs3/putmsg.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet message/menu display routine */
  
-/* $Id: putmsg.cpp,v 1.29 2011/11/08 02:48:16 rswindell Exp $ */
+/* $Id: putmsg.cpp,v 1.30 2011/11/11 06:47:14 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -51,6 +51,7 @@
 char sbbs_t::putmsg(const char *buf, long mode)
 {
 	char	tmpatr,tmp2[256],tmp3[128];
+	char	ret;
 	char*	str=(char*)buf;
 	uchar	exatr=0;
 	int 	orgcon=console,i;
@@ -292,12 +293,13 @@ char sbbs_t::putmsg(const char *buf, long mode)
 			pause();
 	}
 
+	ret=str[l];
 	if(str!=buf)	/* malloc'd copy of buffer */
 		free(str);
 
 	/* Restore original settings of Forced Pause On/Off */
 	sys_status&=~(SS_PAUSEOFF|SS_PAUSEON);
 	sys_status|=(sys_status_sav&(SS_PAUSEOFF|SS_PAUSEON));
-	return(str[l]);
+	return(ret);
 }
 
--- sbbs3/wordwrap.c
+++ sbbs3/wordwrap.c
@@ -1,4 +1,4 @@
-/* $Id: wordwrap.c,v 1.14 2011/11/08 02:48:16 rswindell Exp $ */
+/* $Id: wordwrap.c,v 1.16 2011/11/30 03:18:46 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -231,6 +231,9 @@ char* wordwrap(char* inbuf, int len, int oldlen, BOOL handle_quotes)
 		old_prefix_bytes=prefix_bytes;
 	}
 	for(; inbuf[i]; i++) {
+		if(oldlen == 0)
+			icol=-256;
+
 		if(l>=len*2+2) {
 			l-=4;
 			linebuf[l]=0;
@@ -275,7 +278,7 @@ char* wordwrap(char* inbuf, int len, int oldlen, BOOL handle_quotes)
 					ocol=prefix_len+1;
 					old_prefix_bytes=prefix_bytes;
 				}
-				else if(isspace((unsigned char)inbuf[i+1]) && inbuf[i+1] != '\n' && inbuf[i+1] != '\r') {	/* Next line starts with whitespace.  This is a "hard" CR. */
+				else if(isspace((unsigned char)inbuf[i+1])) {	/* Next line starts with whitespace.  This is a "hard" CR. */
 					linebuf[l++]='\r';
 					linebuf[l++]='\n';
 					outbuf_append(&outbuf, &outp, linebuf, l, &outbuf_size);
