4369279c9ad290157cbd1c14966f1e417384336c
rswindell <rswindell>
2012-09-21 22:15:04 +0000

    Address lack of error logging reported by Mindless Automation: use lprintf(LOG_ERR,...) instead of fprintf(stderr,...) to report errors opening or allocating memory for areas.bbs file.

--- sbbs3/getmail.c
+++ sbbs3/getmail.c
@@ -2,7 +2,7 @@
 
 /* Synchronet DLL-exported mail-related routines */
 
-/* $Id: getmail.c,v 1.12 2012/10/24 19:03:13 deuce Exp $ */
+/* $Id: getmail.c,v 1.11 2011/09/10 08:57:55 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -122,7 +122,7 @@ void DLLCALL delfattach(scfg_t* cfg, smbmsg_t* msg)
 /* of pointers to mail_t (message numbers and attributes)                   */
 /* smb_open(&smb) must be called prior										*/
 /****************************************************************************/
-mail_t* DLLCALL loadmail(smb_t* smb, uint32_t* msgs, uint usernumber
+mail_t* DLLCALL loadmail(smb_t* smb, int32_t* msgs, uint usernumber
 			   ,int which, long mode)
 {
 	ulong		l=0;
