e5f32412aebcb2fba3deb0d6c96dca782c3f474c
deuce <deuce>
2012-10-18 17:48:16 +0000

    Add SetThreadName() calls.

--- sbbs3/services.c
+++ sbbs3/services.c
@@ -2,7 +2,7 @@
 
 /* Synchronet Services */
 
-/* $Id: services.c,v 1.271 2013/02/11 22:52:13 deuce Exp $ */
+/* $Id: services.c,v 1.268 2011/11/03 21:22:06 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -270,8 +270,8 @@ js_read(JSContext *cx, uintN argc, jsval *arglist)
 			return JS_FALSE;
 	}
 	
-	if((buf=malloc(len))==NULL)
-		return(JS_FALSE);
+	if((buf=alloca(len))==NULL)
+		return(JS_TRUE);
 
 	rc=JS_SUSPENDREQUEST(cx);
 	len=recv(client->socket,buf,len,0);
@@ -279,7 +279,6 @@ js_read(JSContext *cx, uintN argc, jsval *arglist)
 
 	if(len>0)
 		JS_SET_RVAL(cx, arglist, STRING_TO_JSVAL(JS_NewStringCopyN(cx,buf,len)));
-	free(buf);
 
 	return(JS_TRUE);
 }
@@ -309,16 +308,14 @@ js_readln(JSContext *cx, uintN argc, jsval *arglist)
 			return JS_FALSE;
 	}
 
-	if((buf=(char*)malloc(len+1))==NULL) {
+	if((buf=(char*)alloca(len+1))==NULL) {
 		JS_ReportError(cx,"Error allocating %u bytes",len+1);
 		return(JS_FALSE);
 	}
 
 	if(argc>1) {
-		if(!JS_ValueToInt32(cx,argv[1],(int32*)&timeout)) {
-			free(buf);
+		if(!JS_ValueToInt32(cx,argv[1],(int32*)&timeout))
 			return JS_FALSE;
-		}
 	}
 
 	rc=JS_SUSPENDREQUEST(cx);
@@ -332,7 +329,6 @@ js_readln(JSContext *cx, uintN argc, jsval *arglist)
 			if(time(NULL)-start>timeout) {
 				JS_SET_RVAL(cx, arglist, JSVAL_NULL);
 				JS_RESUMEREQUEST(cx, rc);
-				free(buf);
 				return(JS_TRUE);	/* time-out */
 			}
 			continue;	/* no data */
@@ -353,7 +349,6 @@ js_readln(JSContext *cx, uintN argc, jsval *arglist)
 	JS_RESUMEREQUEST(cx, rc);
 
 	str = JS_NewStringCopyZ(cx, buf);
-	free(buf);
 	if(str==NULL)
 		return(JS_FALSE);
 
@@ -367,9 +362,7 @@ js_write(JSContext *cx, uintN argc, jsval *arglist)
 {
 	jsval *argv=JS_ARGV(cx, arglist);
 	uintN		i;
-	char*		cp=NULL;
-	size_t		cp_sz=0;
-	size_t		len;
+	char*		cp;
 	service_client_t* client;
 	jsrefcount	rc;
 
@@ -381,15 +374,13 @@ js_write(JSContext *cx, uintN argc, jsval *arglist)
 	JS_SET_RVAL(cx, arglist, argv[0]);
 
 	for(i=0; i<argc; i++) {
-		JSVALUE_TO_RASTRING(cx, argv[i], cp, &cp_sz, &len);
+		JSVALUE_TO_STRING(cx, argv[i], cp, NULL);
 		if(cp==NULL)
 			continue;
 		rc=JS_SUSPENDREQUEST(cx);
-		sendsocket(client->socket,cp,len);
+		sendsocket(client->socket,cp,strlen(cp));
 		JS_RESUMEREQUEST(cx, rc);
 	}
-	if(cp)
-		free(cp);
 
 	return(JS_TRUE);
 }
@@ -442,12 +433,10 @@ js_log(JSContext *cx, uintN argc, jsval *arglist)
 
 	str[0]=0;
     for(;i<argc && strlen(str)<(sizeof(str)/2);i++) {
-		JSVALUE_TO_MSTRING(cx, argv[i], line, NULL);
-		HANDLE_PENDING(cx);
+		JSVALUE_TO_STRING(cx, argv[i], line, NULL);
 		if(line==NULL)
 		    return(JS_FALSE);
 		strncat(str,line,sizeof(str)/2);
-		free(line);
 		strcat(str," ");
 	}
 
@@ -497,12 +486,12 @@ js_login(JSContext *cx, uintN argc, jsval *arglist)
 		return(JS_FALSE);
 
 	/* User name or number */
-	JSVALUE_TO_ASTRING(cx, argv[0], user, (LEN_ALIAS > LEN_NAME) ? LEN_ALIAS+2 : LEN_NAME+2, NULL);
-	if(user==NULL)
+	JSVALUE_TO_STRING(cx, argv[0], user, NULL);
+	if(user==NULL) 
 		return(JS_FALSE);
 
 	/* Password */
-	JSVALUE_TO_ASTRING(cx, argv[1], pass, LEN_PASS+2, NULL);
+	JSVALUE_TO_STRING(cx, argv[1], pass, NULL);
 	if(pass==NULL) 
 		return(JS_FALSE);
 
@@ -703,7 +692,7 @@ js_client_add(JSContext *cx, uintN argc, jsval *arglist)
 	SOCKADDR_IN	addr;
 	service_client_t* service_client;
 	jsrefcount	rc;
-	char		*cstr=NULL;
+	char		*cstr;
 
 	JS_SET_RVAL(cx, arglist, JSVAL_VOID);
 
@@ -730,13 +719,14 @@ js_client_add(JSContext *cx, uintN argc, jsval *arglist)
 	}
 
 	if(argc>1) {
-		JSVALUE_TO_MSTRING(cx, argv[1], cstr, NULL);
-		HANDLE_PENDING(cx);
+		JSVALUE_TO_STRING(cx, argv[1], cstr, NULL);
 		client.user=cstr;
 	}
 
-	if(argc>2)
-		JSVALUE_TO_STRBUF(cx, argv[2], client.host, sizeof(client.host), NULL);
+	if(argc>2) {
+		JSVALUE_TO_STRING(cx, argv[2], cstr, NULL);
+		SAFECOPY(client.host,cstr);
+	}
 
 	rc=JS_SUSPENDREQUEST(cx);
 	client_on(sock, &client, /* update? */ FALSE);
@@ -745,8 +735,6 @@ js_client_add(JSContext *cx, uintN argc, jsval *arglist)
 		,service_client->service->socket,service_client->service->protocol
 		,sock,client.user,client.host);
 #endif
-	if(cstr)
-		free(cstr);
 	JS_RESUMEREQUEST(cx, rc);
 	return(JS_TRUE);
 }
@@ -761,7 +749,7 @@ js_client_update(JSContext *cx, uintN argc, jsval *arglist)
 	SOCKADDR_IN	addr;
 	service_client_t* service_client;
 	jsrefcount	rc;
-	char		*cstr=NULL;
+	char		*cstr;
 
 	JS_SET_RVAL(cx, arglist, JSVAL_VOID);
 
@@ -783,12 +771,14 @@ js_client_update(JSContext *cx, uintN argc, jsval *arglist)
 	}
 
 	if(argc>1) {
-		JSVALUE_TO_MSTRING(cx, argv[1], cstr, NULL);
+		JSVALUE_TO_STRING(cx, argv[1], cstr, NULL);
 		client.user=cstr;
 	}
 
-	if(argc>2)
-		JSVALUE_TO_STRBUF(cx, argv[2], client.host, sizeof(client.host), NULL);
+	if(argc>2) {
+		JSVALUE_TO_STRING(cx, argv[2], cstr, NULL);
+		SAFECOPY(client.host,cstr);
+	}
 
 	rc=JS_SUSPENDREQUEST(cx);
 	client_on(sock, &client, /* update? */ TRUE);
@@ -797,8 +787,6 @@ js_client_update(JSContext *cx, uintN argc, jsval *arglist)
 		,service_client->service->socket,service_client->service->protocol
 		,sock,client.user,client.host);
 #endif
-	if(cstr)
-		free(cstr);
 	JS_RESUMEREQUEST(cx, rc);
 	return(JS_TRUE);
 }
@@ -1176,7 +1164,7 @@ static void js_service_thread(void* arg)
 	if(service->options&SERVICE_OPT_UDP 
 		&& service_client.udp_buf != NULL
 		&& service_client.udp_len > 0) {
-		datagram = JS_NewStringCopyN(js_cx, (char*)service_client.udp_buf, service_client.udp_len);
+		datagram = JS_NewStringCopyN(js_cx, service_client.udp_buf, service_client.udp_len);
 		if(datagram==NULL)
 			val=JSVAL_VOID;
 		else
@@ -1689,7 +1677,7 @@ const char* DLLCALL services_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.271 $", "%*s %s", revision);
+	sscanf("$Revision: 1.268 $", "%*s %s", revision);
 
 	sprintf(ver,"Synchronet Services %s%s  "
 		"Compiled %s %s with %s"
--- sbbs3/telgate.cpp
+++ sbbs3/telgate.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet telnet gateway routines */
 
-/* $Id: telgate.cpp,v 1.38 2013/08/09 05:13:00 rswindell Exp $ */
+/* $Id: telgate.cpp,v 1.31 2012/03/19 23:31:59 echicken Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2013 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -38,7 +38,7 @@
 #include "sbbs.h"
 #include "telnet.h" 
 
-void sbbs_t::telnet_gate(char* destaddr, ulong mode, char* name, char* passwd)
+void sbbs_t::telnet_gate(char* destaddr, ulong mode)
 {
 	char*	p;
 	uchar	buf[512];
@@ -121,11 +121,9 @@ void sbbs_t::telnet_gate(char* destaddr, ulong mode, char* name, char* passwd)
 	if(mode&TG_RLOGIN) {
 		p=(char*)buf;
 		*(p++)=0;
-		p+=sprintf(p,"%s",name==NULL ? useron.alias : name);
+		p+=sprintf(p,"%s",useron.alias);
 		p++;	// Add NULL
-		if(passwd!=NULL)
-			p+=sprintf(p,"%s",passwd);
-		else if(mode&TG_SENDPASS) {
+		if(mode&TG_SENDPASS) {
 			p+=sprintf(p,"%s",useron.pass);
 		} else {
 			p+=sprintf(p,"%s",useron.name);
@@ -144,7 +142,7 @@ void sbbs_t::telnet_gate(char* destaddr, ulong mode, char* name, char* passwd)
 	/* Text/NVT mode by default */
 	request_telnet_opt(TELNET_DONT,TELNET_BINARY_TX, 3000);
 
-	if(!(telnet_mode&TELNET_MODE_OFF) && (mode&TG_PASSTHRU))
+	if(mode&(TG_PASSTHRU|TG_RLOGIN))
 		telnet_mode|=TELNET_MODE_GATE;	// Pass-through telnet commands
 
 	while(online) {
@@ -211,25 +209,21 @@ void sbbs_t::telnet_gate(char* destaddr, ulong mode, char* name, char* passwd)
 				}
 				if(mode&TG_CRLF && buf[rd-1]=='\r')
 					buf[rd++]='\n';
-				else if(mode&TG_NOLF && buf[rd-1]=='\n')
-					rd--;
-				if(!gotline && (mode&TG_ECHO) && rd) {
+				if(!gotline && mode&TG_ECHO) {
 					RingBufWrite(&outbuf,buf,rd);
 				}
 			}
-			if(rd > 0) {
-				for(attempts=0;attempts<60 && online; attempts++) /* added retry loop here, Jan-20-2003 */
-				{
-					if((i=sendsocket(remote_socket,(char*)buf,rd))>=0)
-						break;
-					if(ERROR_VALUE!=EWOULDBLOCK)
-						break;
-					mswait(500);
-				} 
-				if(i<0) {
-					lprintf(LOG_NOTICE,"!TELGATE ERROR %d sending on socket %d",ERROR_VALUE,remote_socket);
+			for(attempts=0;attempts<60 && online; attempts++) /* added retry loop here, Jan-20-2003 */
+			{
+				if((i=sendsocket(remote_socket,(char*)buf,rd))>=0)
 					break;
-				}
+				if(ERROR_VALUE!=EWOULDBLOCK)
+					break;
+				mswait(500);
+			} 
+			if(i<0) {
+				lprintf(LOG_NOTICE,"!TELGATE ERROR %d sending on socket %d",ERROR_VALUE,remote_socket);
+				break;
 			}
 		}
 		rd=recv(remote_socket,(char*)buf,sizeof(buf),0);
