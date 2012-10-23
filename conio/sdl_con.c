aaeaade9312e6f4f9c9cb638aff63c02e0d6f76a
deuce <deuce>
2012-10-23 08:07:08 +0000

    Fix warnings.

--- conio/sdl_con.c
+++ conio/sdl_con.c
@@ -491,9 +491,7 @@ void sdl_copytext(const char *text, size_t buflen)
 	sdl.mutexP(sdl_copybuf_mutex);
 	FREE_AND_NULL(sdl_copybuf);
 
-	sdl_copybuf=(char *)malloc(buflen+1);
-	if(sdl_copybuf!=NULL)
-		strcpy(sdl_copybuf, text);
+	sdl_copybuf=strdup(text);
 	sdl.mutexV(sdl_copybuf_mutex);
 	return;
 }
@@ -535,9 +533,8 @@ char *sdl_getcliptext(void)
 	}
 #endif
 	sdl.mutexP(sdl_copybuf_mutex);
-	ret=(char *)malloc(strlen(sdl_pastebuf)+1);
-	if(ret!=NULL)
-		strcpy(ret,sdl_copybuf);
+	if(sdl_copybuf)
+		ret=strdup(sdl_copybuf);
 	sdl.mutexV(sdl_copybuf_mutex);
 	return(ret);
 }
