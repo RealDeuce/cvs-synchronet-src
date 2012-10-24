3f263703b2cd6547aa6ed7b6c896dbe34ea423b8
deuce <deuce>
2012-10-24 19:03:13 +0000

    Fix pretty much all of the warnings (except for the hard ones).
    I've likely broken something though, so if you see weird behaviour with
    extended ASCII, let me know please.

--- sbbs3/userdat.c
+++ sbbs3/userdat.c
@@ -2,13 +2,13 @@
 
 /* Synchronet user data-related routines (exported) */
 
-/* $Id: userdat.c,v 1.152 2014/10/30 08:39:23 rswindell Exp $ */
+/* $Id: userdat.c,v 1.148 2012/10/20 20:49:37 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2012 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -384,10 +384,9 @@ static void dirtyuserdat(scfg_t* cfg, uint usernumber)
 		getnodedat(cfg, i,&node,NULL);
 		if(node.useron==usernumber && (node.status==NODE_INUSE
 			|| node.status==NODE_QUIET)) {
-			if(getnodedat(cfg, i,&node,&file) == 0) {
-				node.misc|=NODE_UDAT;
-				putnodedat(cfg, i,&node,file);
-			}
+			getnodedat(cfg, i,&node,&file);
+			node.misc|=NODE_UDAT;
+			putnodedat(cfg, i,&node,file);
 			break; 
 		} 
 	}
@@ -730,10 +729,8 @@ int DLLCALL putnodedat(scfg_t* cfg, uint number, node_t* node, int file)
 	int		wrerr=0;
 	int		attempts;
 
-	if(file<0)
-		return -1;
 	if(!VALID_CFG(cfg) 
-		|| node==NULL || number<1 || number>cfg->sys_nodes) {
+		|| node==NULL || number<1 || number>cfg->sys_nodes || file<0) {
 		close(file);
 		return(-1);
 	}
@@ -954,10 +951,8 @@ char* DLLCALL nodestatus(scfg_t* cfg, node_t* node, char* buf, size_t buflen)
 						,"paging node %u for private chat",node->aux);
                     break;
                 case NODE_PCHT:
-                    if(node->aux==0)
-                        sprintf(str+strlen(str)
-							,"in local chat with %s"
-							,cfg->sys_op);
+                    if(!node->aux)
+                        strcat(str,"in local chat with sysop");
                     else
                         sprintf(str+strlen(str)
 							,"in private chat with node %u"
@@ -1143,10 +1138,9 @@ int DLLCALL putsmsg(scfg_t* cfg, int usernumber, char *strin)
 		if(node.useron==usernumber
 			&& (node.status==NODE_INUSE || node.status==NODE_QUIET)
 			&& !(node.misc&NODE_MSGW)) {
-			if(getnodedat(cfg,i,&node,&file)==0) {
-				node.misc|=NODE_MSGW;
-				putnodedat(cfg,i,&node,file); 
-			}
+			getnodedat(cfg,i,&node,&file);
+			node.misc|=NODE_MSGW;
+			putnodedat(cfg,i,&node,file); 
 		} 
 	}
 	return(0);
@@ -1171,10 +1165,9 @@ char* DLLCALL getsmsg(scfg_t* cfg, int usernumber)
 		if(node.useron==usernumber
 			&& (node.status==NODE_INUSE || node.status==NODE_QUIET)
 			&& node.misc&NODE_MSGW) {
-			if(getnodedat(cfg,i,&node,&file) == 0) {
-				node.misc&=~NODE_MSGW;
-				putnodedat(cfg,i,&node,file); 
-			}
+			getnodedat(cfg,i,&node,&file);
+			node.misc&=~NODE_MSGW;
+			putnodedat(cfg,i,&node,file); 
 		} 
 	}
 
@@ -1211,10 +1204,9 @@ char* DLLCALL getnmsg(scfg_t* cfg, int node_num)
 	if(!VALID_CFG(cfg) || node_num<1)
 		return(NULL);
 
-	if(getnodedat(cfg,node_num,&node,&file) == 0) {
-		node.misc&=~NODE_NMSG;          /* clear the NMSG flag */
-		putnodedat(cfg,node_num,&node,file);
-	}
+	getnodedat(cfg,node_num,&node,&file);
+	node.misc&=~NODE_NMSG;          /* clear the NMSG flag */
+	putnodedat(cfg,node_num,&node,file);
 
 	SAFEPRINTF2(str,"%smsgs/n%3.3u.msg",cfg->data_dir,node_num);
 	if(flength(str)<1L)
@@ -1270,10 +1262,9 @@ int DLLCALL putnmsg(scfg_t* cfg, int num, char *strin)
 	getnodedat(cfg,num,&node,NULL);
 	if((node.status==NODE_INUSE || node.status==NODE_QUIET)
 		&& !(node.misc&NODE_NMSG)) {
-		if(getnodedat(cfg,num,&node,&file) == 0) {
-			node.misc|=NODE_NMSG;
-			putnodedat(cfg,num,&node,file); 
-		}
+		getnodedat(cfg,num,&node,&file);
+		node.misc|=NODE_NMSG;
+		putnodedat(cfg,num,&node,file); 
 	}
 
 	return(0);
@@ -2734,7 +2725,7 @@ long DLLCALL loginAttemptListClear(link_list_t* list)
 }
 
 /****************************************************************************/
-static list_node_t* login_attempted(link_list_t* list, const SOCKADDR_IN* addr)
+static list_node_t* login_attempted(link_list_t* list, SOCKADDR_IN* addr)
 {
 	list_node_t*		node;
 	login_attempt_t*	attempt;
@@ -2748,7 +2739,7 @@ static list_node_t* login_attempted(link_list_t* list, const SOCKADDR_IN* addr)
 }
 
 /****************************************************************************/
-long DLLCALL loginAttempts(link_list_t* list, const SOCKADDR_IN* addr)
+long DLLCALL loginAttempts(link_list_t* list, SOCKADDR_IN* addr)
 {
 	long				count=0;
 	list_node_t*		node;
@@ -2762,7 +2753,7 @@ long DLLCALL loginAttempts(link_list_t* list, const SOCKADDR_IN* addr)
 }
 
 /****************************************************************************/
-void DLLCALL loginSuccess(link_list_t* list, const SOCKADDR_IN* addr)
+void DLLCALL loginSuccess(link_list_t* list, SOCKADDR_IN* addr)
 {
 	list_node_t*		node;
 
@@ -2775,16 +2766,16 @@ void DLLCALL loginSuccess(link_list_t* list, const SOCKADDR_IN* addr)
 /****************************************************************************/
 /* Returns number of *unique* login attempts (excludes consecutive dupes)	*/
 /****************************************************************************/
-ulong DLLCALL loginFailure(link_list_t* list, const SOCKADDR_IN* addr, const char* prot, const char* user, const char* pass)
+ulong DLLCALL loginFailure(link_list_t* list, SOCKADDR_IN* addr, const char* prot, const char* user, const char* pass)
 {
 	list_node_t*		node;
-	login_attempt_t		first;
+	login_attempt_t		first = {0};
 	login_attempt_t*	attempt=&first;
 	ulong				count=0;
 
 	if(list==NULL)
 		return 0;
-	memset(&first, 0, sizeof(first));
+
 	listLock(list);
 	if((node=login_attempted(list, addr)) != NULL) {
 		attempt=node->data;
