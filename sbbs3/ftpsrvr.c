5cabca0cf452435dbf417c77b34d6a7cf27ba08d
deuce <deuce>
2013-02-07 00:45:47 +0000

    Check for NULL before free()ing.

--- sbbs3/ftpsrvr.c
+++ sbbs3/ftpsrvr.c
@@ -2,13 +2,13 @@
 
 /* Synchronet FTP server */
 
-/* $Id: ftpsrvr.c,v 1.406 2014/10/30 08:37:15 rswindell Exp $ */
+/* $Id: ftpsrvr.c,v 1.399 2013/02/07 00:42:51 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2012 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -191,7 +191,7 @@ static void status(char* str)
 static void update_clients(void)
 {
 	if(startup!=NULL && startup->clients!=NULL)
-		startup->clients(startup->cbdata,protected_uint32_value(active_clients));
+		startup->clients(startup->cbdata,active_clients.value);
 }
 
 static void client_on(SOCKET sock, client_t* client, BOOL update)
@@ -206,10 +206,12 @@ static void client_off(SOCKET sock)
 		startup->client_on(startup->cbdata,FALSE,sock,NULL,FALSE);
 }
 
-static void thread_up(BOOL setuid)
+static int32_t thread_up(BOOL setuid)
 {
+	int32_t	count =	protected_uint32_adjust(&thread_count,1);
 	if(startup!=NULL && startup->thread_up!=NULL)
 		startup->thread_up(startup->cbdata,TRUE, setuid);
+	return count;
 }
 
 static int32_t thread_down(void)
@@ -387,7 +389,6 @@ js_write(JSContext *cx, uintN argc, jsval *arglist)
 	FILE*	fp;
 	jsrefcount	rc;
 	char		*p;
-	size_t		len;
 
 	JS_SET_RVAL(cx, arglist, JSVAL_VOID);
 
@@ -398,13 +399,14 @@ js_write(JSContext *cx, uintN argc, jsval *arglist)
 		str = JS_ValueToString(cx, argv[i]);
 		if (!str)
 		    return JS_FALSE;
-		JSSTRING_TO_MSTRING(cx, str, p, &len);
-		HANDLE_PENDING(cx);
+		JSSTRING_TO_MSTRING(cx, str, p, NULL);
 		rc=JS_SUSPENDREQUEST(cx);
 		if(p) {
-			fwrite(p, len, 1, fp);
+			fputs(p, fp);
 			free(p);
 		}
+		else
+			return JS_FALSE;
 		JS_RESUMEREQUEST(cx, rc);
 	}
 
@@ -1916,7 +1918,6 @@ static void filexfer(SOCKADDR_IN* addr, SOCKET ctrl_sock, SOCKET pasv_sock, SOCK
 		xfer->dir=dir;
 		xfer->desc=desc;
 		SAFECOPY(xfer->filename,filename);
-		protected_uint32_adjust(&thread_count,1);
 		if(receiving)
 			result=_beginthread(receive_thread,0,(void*)xfer);
 		else
@@ -2187,6 +2188,12 @@ static BOOL ftp_hacklog(char* prot, char* user, char* text, char* host, SOCKADDR
 
 /****************************************************************************/
 /* Consecutive failed login (possible password hack) attempt tracking		*/
+/****************************************************************************/
+/* Counter is global so it is tracked between multiple connections.			*/
+/* Failed consecutive login attempts > 10 will generate a hacklog entry	and	*/
+/* immediately disconnect (after the usual failed-login delay).				*/
+/* A failed login from a different host resets the counter.					*/
+/* A successful login from the same host resets the counter.				*/
 /****************************************************************************/
 
 static BOOL badlogin(SOCKET sock, ulong* login_attempts, char* user, char* passwd, char* host, SOCKADDR_IN* addr)
@@ -2538,9 +2545,9 @@ static void ctrl_thread(void* arg)
 			user.number=matchuser(&scfg,user.alias,FALSE /*sysop_alias*/);
 			if(!user.number) {
 				if(scfg.sys_misc&SM_ECHO_PW)
-					lprintf(LOG_WARNING,"%04d !UNKNOWN USER: '%s' (password: %s)",sock,user.alias,p);
+					lprintf(LOG_WARNING,"%04d !UNKNOWN USER: %s, Password: %s",sock,user.alias,p);
 				else
-					lprintf(LOG_WARNING,"%04d !UNKNOWN USER: '%s'",sock,user.alias);
+					lprintf(LOG_WARNING,"%04d !UNKNOWN USER: %s",sock,user.alias);
 				if(badlogin(sock, &login_attempts, user.alias, p, host_name, &ftp.client_addr))
 					break;
 				continue;
@@ -2709,7 +2716,7 @@ static void ctrl_thread(void* arg)
 				if(node.status==NODE_INUSE)
 					sockprintf(sock," Node %3d: %s",i+1, username(&scfg,node.useron,str));
 			}
-			sockprintf(sock,"211 End (%d active FTP clients)", protected_uint32_value(active_clients));
+			sockprintf(sock,"211 End (%d active FTP clients)", active_clients.value);
 			continue;
 		}
 		if(!stricmp(cmd, "SITE VER")) {
@@ -2838,8 +2845,6 @@ static void ctrl_thread(void* arg)
 				pasv_addr.sin_port = htons(port);
 
 				if((result=bind(pasv_sock, (struct sockaddr *) &pasv_addr,sizeof(pasv_addr)))==0)
-					break;
-				if(port==startup->pasv_port_high)
 					break;
 			}
 			if(result!= 0) {
@@ -4493,13 +4498,6 @@ static void cleanup(int code, int line)
 	lprintf(LOG_DEBUG,"0000 cleanup called from line %d",line);
 #endif
 
-	if(protected_uint32_value(thread_count) > 1) {
-		lprintf(LOG_DEBUG,"#### FTP Server waiting for %d child threads to terminate", protected_uint32_value(thread_count)-1);
-		while(protected_uint32_value(thread_count) > 1) {
-			mswait(100);
-		}
-	}
-
 	free_cfg(&scfg);
 	free_text(text);
 
@@ -4509,12 +4507,19 @@ static void cleanup(int code, int line)
 	if(server_socket!=INVALID_SOCKET)
 		ftp_close_socket(&server_socket,__LINE__);
 
-	update_clients();	/* active_clients is destroyed below */
+	if(thread_count.value > 1) {
+		lprintf(LOG_DEBUG,"#### Waiting for %d child threads to terminate", thread_count.value-1);
+		while(thread_count.value > 1) {
+			mswait(100);
+		}
+	}
 
-	if(protected_uint32_value(active_clients))
-		lprintf(LOG_WARNING,"#### !FTP Server terminating with %ld active clients", protected_uint32_value(active_clients));
+	if(active_clients.value)
+		lprintf(LOG_WARNING,"#### !FTP Server terminating with %ld active clients", active_clients.value);
 	else
 		protected_uint32_destroy(active_clients);
+
+	update_clients();
 
 #ifdef _WINSOCKAPI_
 	if(WSAInitialized && WSACleanup()!=0) 
@@ -4536,7 +4541,7 @@ const char* DLLCALL ftp_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.406 $", "%*s %s", revision);
+	sscanf("$Revision: 1.399 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
@@ -4631,7 +4636,6 @@ void DLLCALL ftp_server(void* arg)
 
 	do {
 
-		protected_uint32_adjust(&thread_count,1);
 		thread_up(FALSE /* setuid */);
 
 		status("Initializing");
@@ -4796,7 +4800,7 @@ void DLLCALL ftp_server(void* arg)
 
 		while(server_socket!=INVALID_SOCKET && !terminate_server) {
 
-			if(protected_uint32_value(thread_count) <= 1) {
+			if(thread_count.value <= 1) {
 				if(!(startup->options&FTP_OPT_NO_RECYCLE)) {
 					if((p=semfile_list_check(&initialized,recycle_semfiles))!=NULL) {
 						lprintf(LOG_INFO,"0000 Recycle semaphore file (%s) detected",p);
@@ -4868,7 +4872,7 @@ void DLLCALL ftp_server(void* arg)
 				continue;
 			}
 			
-			if(protected_uint32_value(active_clients)>=startup->max_clients) {
+			if(active_clients.value>=startup->max_clients) {
 				lprintf(LOG_WARNING,"%04d !MAXIMUM CLIENTS (%d) reached, access denied"
 					,client_socket, startup->max_clients);
 				sockprintf(client_socket,"421 Maximum active clients reached, please try again later.");
@@ -4889,7 +4893,6 @@ void DLLCALL ftp_server(void* arg)
 			ftp->socket=client_socket;
 			ftp->client_addr=client_addr;
 
-			protected_uint32_adjust(&thread_count,1);
 			_beginthread(ctrl_thread, 0, ftp);
 			served++;
 		}
@@ -4898,14 +4901,14 @@ void DLLCALL ftp_server(void* arg)
 		lprintf(LOG_DEBUG,"0000 server_socket: %d",server_socket);
 		lprintf(LOG_DEBUG,"0000 terminate_server: %d",terminate_server);
 #endif
-		if(protected_uint32_value(active_clients)) {
+		if(active_clients.value) {
 			lprintf(LOG_DEBUG,"%04d Waiting for %d active clients to disconnect..."
-				,server_socket, protected_uint32_value(active_clients));
+				,server_socket, active_clients.value);
 			start=time(NULL);
-			while(protected_uint32_value(active_clients)) {
+			while(active_clients.value) {
 				if(time(NULL)-start>startup->max_inactivity) {
 					lprintf(LOG_WARNING,"%04d !TIMEOUT waiting for %d active clients"
-						,server_socket, protected_uint32_value(active_clients));
+						,server_socket, active_clients.value);
 					break;
 				}
 				mswait(100);
