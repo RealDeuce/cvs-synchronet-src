ec2c69f10be65f0c8798b4a56bea0395184fa483
deuce <deuce>
2013-02-07 00:37:50 +0000

    Stop using alloca() macros from string conversions.

--- sbbs3/js_conio.c
+++ sbbs3/js_conio.c
@@ -2,7 +2,7 @@
 
 /* Synchronet "conio" (console IO) object */
 
-/* $Id: js_conio.c,v 1.27 2013/02/07 05:56:02 deuce Exp $ */
+/* $Id: js_conio.c,v 1.24 2013/02/07 00:37:50 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -238,7 +238,6 @@ static JSBool js_set(JSContext *cx, JSObject *obj, jsid id, JSBool strict, jsval
 				char	*bytes;
 
 				JSVALUE_TO_MSTRING(cx, *vp, bytes, &len);
-				HANDLE_PENDING(cx);
 				if(!bytes)
 					return JS_FALSE;
 				rc=JS_SUSPENDREQUEST(cx);
@@ -307,23 +306,26 @@ js_conio_init(JSContext *cx, uintN argc, jsval *arglist)
 {
 	jsval *argv=JS_ARGV(cx, arglist);
 	int			ciolib_mode=CIOLIB_MODE_AUTO;
-	char		mode[7];
+	char*		mode;
 	jsrefcount	rc;
 
 	JS_SET_RVAL(cx, arglist, JSVAL_FALSE);
 
 	if(argc>0) {
-		JSVALUE_TO_STRBUF(cx, argv[0], mode, sizeof(mode), NULL);
-		if(!stricmp(mode,"STDIO"))
-			ciolib_mode=-1;
-		else if(!stricmp(mode,"AUTO"))
-			ciolib_mode=CIOLIB_MODE_AUTO;
-		else if(!stricmp(mode,"X"))
-			ciolib_mode=CIOLIB_MODE_X;
-		else if(!stricmp(mode,"ANSI"))
-			ciolib_mode=CIOLIB_MODE_ANSI;
-		else if(!stricmp(mode,"CONIO"))
-			ciolib_mode=CIOLIB_MODE_CONIO;
+		JSVALUE_TO_MSTRING(cx, argv[0], mode, NULL);
+		if(mode != NULL) {
+			if(!stricmp(mode,"STDIO"))
+				ciolib_mode=-1;
+			else if(!stricmp(mode,"AUTO"))
+				ciolib_mode=CIOLIB_MODE_AUTO;
+			else if(!stricmp(mode,"X"))
+				ciolib_mode=CIOLIB_MODE_X;
+			else if(!stricmp(mode,"ANSI"))
+				ciolib_mode=CIOLIB_MODE_ANSI;
+			else if(!stricmp(mode,"CONIO"))
+				ciolib_mode=CIOLIB_MODE_CONIO;
+			free(mode);
+		}
 	}
 
 	rc=JS_SUSPENDREQUEST(cx);
@@ -568,7 +570,6 @@ js_conio_loadfont(JSContext *cx, uintN argc, jsval *arglist)
 
 	if(argc==1) {
 		JSVALUE_TO_MSTRING(cx, argv[0], str, NULL);
-		HANDLE_PENDING(cx);
 		if(str != NULL) {
 			rc=JS_SUSPENDREQUEST(cx);
 			JS_SET_RVAL(cx, arglist,INT_TO_JSVAL(loadfont(str)));
@@ -590,7 +591,6 @@ js_conio_settitle(JSContext *cx, uintN argc, jsval *arglist)
 
 	if(argc==1) {
 		JSVALUE_TO_MSTRING(cx, argv[0], str, NULL);
-		HANDLE_PENDING(cx);
 		if(str != NULL) {
 			rc=JS_SUSPENDREQUEST(cx);
 			settitle(str);
@@ -613,7 +613,6 @@ js_conio_setname(JSContext *cx, uintN argc, jsval *arglist)
 
 	if(argc==1) {
 		JSVALUE_TO_MSTRING(cx, argv[0], str, NULL);
-		HANDLE_PENDING(cx);
 		if(str != NULL) {
 			rc=JS_SUSPENDREQUEST(cx);
 			setname(str);
@@ -636,7 +635,6 @@ js_conio_cputs(JSContext *cx, uintN argc, jsval *arglist)
 
 	if(argc==1) {
 		JSVALUE_TO_MSTRING(cx, argv[0], str, NULL);
-		HANDLE_PENDING(cx);
 		if(str != NULL) {
 			rc=JS_SUSPENDREQUEST(cx);
 			JS_SET_RVAL(cx, arglist,INT_TO_JSVAL(cputs(str)));
@@ -696,7 +694,6 @@ js_conio_getpass(JSContext *cx, uintN argc, jsval *arglist)
 
 	if(argc==1) {
 		JSVALUE_TO_MSTRING(cx, argv[0], str, NULL);
-		HANDLE_PENDING(cx);
 		if(str != NULL) {
 			rc=JS_SUSPENDREQUEST(cx);
 			pwd=getpass(str);
@@ -1134,15 +1131,13 @@ static JSBool js_conio_resolve(JSContext *cx, JSObject *obj, jsid id)
 		JS_IdToValue(cx, id, &idval);
 		if(JSVAL_IS_STRING(idval)) {
 			JSSTRING_TO_MSTRING(cx, JSVAL_TO_STRING(idval), name, NULL);
-			HANDLE_PENDING(cx);
 			if(name==NULL)
 				return JS_FALSE;
 		}
 	}
 
 	ret=js_SyncResolve(cx, obj, name, js_properties, js_functions, NULL, 0);
-	if(name)
-		free(name);
+	free(name);
 	return ret;
 }
 
