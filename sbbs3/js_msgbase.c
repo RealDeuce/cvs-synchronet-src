a3815cec14a82a840a2eefeaf78a9f55b8107ae2
deuce <deuce>
2013-02-08 05:32:13 +0000

    Remove *_TO_STRING() usage.

--- sbbs3/js_msgbase.c
+++ sbbs3/js_msgbase.c
@@ -2,13 +2,13 @@
 
 /* Synchronet JavaScript "MsgBase" Object */
 
-/* $Id: js_msgbase.c,v 1.187 2014/04/06 06:18:28 rswindell Exp $ */
+/* $Id: js_msgbase.c,v 1.181 2013/02/08 05:23:03 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2012 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -40,6 +40,8 @@
 #include "userdat.h"
 
 #ifdef JAVASCRIPT
+
+static scfg_t* 		scfg=NULL;
 
 typedef struct
 {
@@ -950,7 +952,6 @@ js_get_msg_index(JSContext *cx, uintN argc, jsval *arglist)
 
 #define LAZY_INTEGER(PropName, PropValue, flags) \
 	if(name==NULL || strcmp(name, (PropName))==0) { \
-		if(name) free(name); \
 		v=INT_TO_JSVAL((PropValue)); \
 		JS_DefineProperty(cx, obj, (PropName), v, NULL,NULL,flags); \
 		if(name) return(JS_TRUE); \
@@ -958,7 +959,6 @@ js_get_msg_index(JSContext *cx, uintN argc, jsval *arglist)
 
 #define LAZY_UINTEGER(PropName, PropValue, flags) \
 	if(name==NULL || strcmp(name, (PropName))==0) { \
-		if(name) free(name); \
 		v=UINT_TO_JSVAL((PropValue)); \
 		JS_DefineProperty(cx, obj, (PropName), v, NULL,NULL,flags); \
 		if(name) return(JS_TRUE); \
@@ -966,7 +966,6 @@ js_get_msg_index(JSContext *cx, uintN argc, jsval *arglist)
 
 #define LAZY_UINTEGER_EXPAND(PropName, PropValue, flags) \
 	if(name==NULL || strcmp(name, (PropName))==0) { \
-		if(name) free(name); \
 		if(p->expand_fields || (PropValue)) { \
 			v=UINT_TO_JSVAL((PropValue)); \
 			JS_DefineProperty(cx, obj, (PropName), v, NULL,NULL,flags); \
@@ -977,7 +976,6 @@ js_get_msg_index(JSContext *cx, uintN argc, jsval *arglist)
 
 #define LAZY_UINTEGER_COND(PropName, Condition, PropValue, flags) \
 	if(name==NULL || strcmp(name, (PropName))==0) { \
-		if(name) free(name); \
 		if(Condition) { \
 			v=UINT_TO_JSVAL((uint32_t)(PropValue)); \
 			JS_DefineProperty(cx, obj, (PropName), v, NULL,NULL,flags); \
@@ -988,7 +986,6 @@ js_get_msg_index(JSContext *cx, uintN argc, jsval *arglist)
 
 #define LAZY_STRING(PropName, PropValue, flags) \
 	if(name==NULL || strcmp(name, (PropName))==0) { \
-		if(name) free(name); \
 		if((js_str=JS_NewStringCopyZ(cx, (PropValue)))!=NULL) { \
 			JS_DefineProperty(cx, obj, PropName, STRING_TO_JSVAL(js_str), NULL, NULL, flags); \
 			if(name) return(JS_TRUE); \
@@ -998,7 +995,6 @@ js_get_msg_index(JSContext *cx, uintN argc, jsval *arglist)
 
 #define LAZY_STRING_TRUNCSP(PropName, PropValue, flags) \
 	if(name==NULL || strcmp(name, (PropName))==0) { \
-		if(name) free(name); \
 		if((js_str=JS_NewStringCopyZ(cx, truncsp(PropValue)))!=NULL) { \
 			JS_DefineProperty(cx, obj, PropName, STRING_TO_JSVAL(js_str), NULL, NULL, flags); \
 			if(name) return(JS_TRUE); \
@@ -1008,7 +1004,6 @@ js_get_msg_index(JSContext *cx, uintN argc, jsval *arglist)
 
 #define LAZY_STRING_COND(PropName, Condition, PropValue, flags) \
 	if(name==NULL || strcmp(name, (PropName))==0) { \
-		if(name) free(name); \
 		if((Condition) && (js_str=JS_NewStringCopyZ(cx, (PropValue)))!=NULL) { \
 			JS_DefineProperty(cx, obj, PropName, STRING_TO_JSVAL(js_str), NULL, NULL, flags); \
 			if(name) return(JS_TRUE); \
@@ -1018,7 +1013,6 @@ js_get_msg_index(JSContext *cx, uintN argc, jsval *arglist)
 
 #define LAZY_STRING_TRUNCSP_NULL(PropName, PropValue, flags) \
 	if(name==NULL || strcmp(name, (PropName))==0) { \
-		if(name) free(name); \
 		if((PropValue) != NULL && (js_str=JS_NewStringCopyZ(cx, truncsp(PropValue)))!=NULL) { \
 			JS_DefineProperty(cx, obj, PropName, STRING_TO_JSVAL(js_str), NULL, NULL, flags); \
 			if(name) return(JS_TRUE); \
@@ -1043,28 +1037,21 @@ static JSBool js_get_msg_header_resolve(JSContext *cx, JSObject *obj, jsid id)
 	privatemsg_t*	p;
 	char*			name=NULL;
 	jsrefcount		rc;
-	scfg_t*			scfg;
-
-	scfg=JS_GetRuntimePrivate(JS_GetRuntime(cx));
 
 	if(id != JSID_VOID && id != JSID_EMPTY) {
 		jsval idval;
 		
 		JS_IdToValue(cx, id, &idval);
 		if(JSVAL_IS_STRING(idval))
-			JSSTRING_TO_MSTRING(cx, JSVAL_TO_STRING(idval), name, NULL);
+			JSSTRING_TO_ASTRING(cx, JSVAL_TO_STRING(idval), name, 32, NULL);
 	}
 
 	/* If we have already enumerated, we're done here... */
-	if((p=(privatemsg_t*)JS_GetPrivate(cx,obj))==NULL) {
-		if(name) free(name);
+	if((p=(privatemsg_t*)JS_GetPrivate(cx,obj))==NULL)
 		return(JS_TRUE);
-	}
 
-	if((p->msg).hdr.number==0) { /* No valid message number/id/offset specified */
-		if(name) free(name);
+	if((p->msg).hdr.number==0) /* No valid message number/id/offset specified */
 		return(JS_TRUE);
-	}
 
 	LAZY_UINTEGER("number", p->msg.hdr.number, JSPROP_ENUMERATE);
 	LAZY_UINTEGER("offset", p->msg.offset, JSPROP_ENUMERATE);
@@ -1125,7 +1112,6 @@ static JSBool js_get_msg_header_resolve(JSContext *cx, JSObject *obj, jsid id)
 	LAZY_STRING("date", msgdate((p->msg).hdr.when_written,date), JSPROP_ENUMERATE);
 
 	if(name==NULL || strcmp(name,"reply_id")==0) {
-		if(name) free(name);
 		/* Reply-ID (References) */
 		if((p->msg).reply_id!=NULL)
 			val=(p->msg).reply_id;
@@ -1156,7 +1142,6 @@ static JSBool js_get_msg_header_resolve(JSContext *cx, JSObject *obj, jsid id)
 
 	/* Message-ID */
 	if(name==NULL || strcmp(name,"id")==0) {
-		if(name) free(name);
 		if(p->expand_fields || (p->msg).id!=NULL) {
 			get_msgid(scfg,p->p->smb.subnum,&(p->msg),msg_id,sizeof(msg_id));
 			val=msg_id;
@@ -1187,7 +1172,6 @@ static JSBool js_get_msg_header_resolve(JSContext *cx, JSObject *obj, jsid id)
 	LAZY_STRING_TRUNCSP_NULL("ftn_flags", p->msg.ftn_flags, JSPROP_ENUMERATE);
 
 	if(name==NULL || strcmp(name,"field_list")==0) {
-		if(name) free(name);
 		/* Create hdr.field_list[] with repeating header fields (including type and data) */
 		if((array=JS_NewArrayObject(cx,0,NULL))!=NULL) {
 			JS_DefineProperty(cx,obj,"field_list",OBJECT_TO_JSVAL(array)
@@ -1239,7 +1223,6 @@ static JSBool js_get_msg_header_resolve(JSContext *cx, JSObject *obj, jsid id)
 	}
 
 	if(name==NULL || strcmp(name, "can_read")==0) {
-		if(name) free(name);
 		v=BOOLEAN_TO_JSVAL(JS_FALSE);
 
 		do {
@@ -1315,11 +1298,11 @@ static JSBool js_get_msg_header_resolve(JSContext *cx, JSObject *obj, jsid id)
 		if(name)
 			return(JS_TRUE);
 	}
-	if(name) free(name);
+
 
 	/* DO NOT RETURN JS_FALSE on unknown names */
-	/* Doing so will prevent toString() among others from working. */
-
+	/* Doing so will preven toString() among others from working. */
+	
 	return(JS_TRUE);
 }
 
@@ -1483,101 +1466,6 @@ js_get_msg_header(JSContext *cx, uintN argc, jsval *arglist)
 	}
 
 	JS_SET_RVAL(cx, arglist, OBJECT_TO_JSVAL(hdrobj));
-
-	return(JS_TRUE);
-}
-
-static JSBool
-js_get_all_msg_headers(JSContext *cx, uintN argc, jsval *arglist)
-{
-	JSObject *obj=JS_THIS_OBJECT(cx, arglist);
-	JSObject*	hdrobj;
-	jsrefcount	rc;
-	privatemsg_t*	p;
-	private_t*	priv;
-	JSObject*	proto;
-	jsval		val;
-	uint32_t	off;
-    JSObject*	retobj;
-	char		numstr[16];
-
-	JS_SET_RVAL(cx, arglist, JSVAL_NULL);
-
-	if((priv=(private_t*)JS_GetPrivate(cx,obj))==NULL) {
-		JS_ReportError(cx,getprivate_failure,WHERE);
-		return(JS_FALSE);
-	}
-
-	if(!SMB_IS_OPEN(&(priv->smb)))
-		return(JS_TRUE);
-
-    retobj = JS_NewObject(cx, NULL, NULL, obj);
-    JS_SET_RVAL(cx, arglist, OBJECT_TO_JSVAL(retobj));
-
-	rc=JS_SUSPENDREQUEST(cx);
-	if((priv->status=smb_locksmbhdr(&(priv->smb)))!=SMB_SUCCESS) {
-		JS_RESUMEREQUEST(cx, rc);
-		return(JS_TRUE);
-	}
-	JS_RESUMEREQUEST(cx, rc);
-
-	if(JS_GetProperty(cx, JS_GetGlobalObject(cx), "MsgBase", &val) && !JSVAL_NULL_OR_VOID(val)) {
-		JS_ValueToObject(cx,val,&proto);
-		if(JS_GetProperty(cx, proto, "HeaderPrototype", &val) && !JSVAL_NULL_OR_VOID(val))
-			JS_ValueToObject(cx,val,&proto);
-		else
-			proto=NULL;
-	}
-	else
-		proto=NULL;
-
-	for(off=0; off < priv->smb.status.total_msgs; off++) {
-		if((p=(privatemsg_t*)malloc(sizeof(privatemsg_t)))==NULL) {
-			smb_unlocksmbhdr(&(priv->smb)); 
-			JS_ReportError(cx,"malloc failed");
-			return(JS_FALSE);
-		}
-
-		memset(p,0,sizeof(privatemsg_t));
-
-		/* Parse boolean arguments first */
-		p->p=priv;
-		p->expand_fields=JS_TRUE;	/* This parameter defaults to true */
-
-		p->msg.offset=off;
-		rc=JS_SUSPENDREQUEST(cx);
-		if((priv->status=smb_getmsgidx(&(priv->smb), &(p->msg)))!=SMB_SUCCESS) {
-			smb_unlocksmbhdr(&(priv->smb)); 
-			JS_RESUMEREQUEST(cx, rc);
-			return(JS_TRUE);
-		}
-
-		if((priv->status=smb_getmsghdr(&(priv->smb), &(p->msg)))!=SMB_SUCCESS) {
-			smb_unlocksmbhdr(&(priv->smb)); 
-			JS_RESUMEREQUEST(cx, rc);
-			return(JS_TRUE);
-		}
-
-		JS_RESUMEREQUEST(cx, rc);
-
-		if((hdrobj=JS_NewObject(cx,&js_msghdr_class,proto,obj))==NULL) {
-			smb_freemsgmem(&(p->msg));
-			smb_unlocksmbhdr(&(priv->smb)); 
-			return(JS_TRUE);
-		}
-
-		if(!JS_SetPrivate(cx, hdrobj, p)) {
-			JS_ReportError(cx,"JS_SetPrivate failed");
-			free(p);
-			smb_unlocksmbhdr(&(priv->smb));
-			return(JS_FALSE);
-		}
-
-		val=OBJECT_TO_JSVAL(hdrobj);
-		sprintf(numstr,"%"PRIu32, p->msg.hdr.number);
-		JS_SetProperty(cx, retobj, numstr, &val);
-	}
-	smb_unlocksmbhdr(&(priv->smb));
 
 	return(JS_TRUE);
 }
@@ -2049,9 +1937,6 @@ js_save_msg(JSContext *cx, uintN argc, jsval *arglist)
 	private_t*	p;
 	JSBool		ret=JS_TRUE;
 	jsrefcount	rc;
-	scfg_t*			scfg;
-
-	scfg=JS_GetRuntimePrivate(JS_GetRuntime(cx));
 
 	JS_SET_RVAL(cx, arglist, JSVAL_FALSE);
 
@@ -2367,10 +2252,6 @@ static jsSyncMethodSpec js_msgbase_functions[] = {
 	"if you will be re-writing the header later with <i>put_msg_header()</i>")
 	,312
 	},
-	{"get_all_msg_headers", js_get_all_msg_headers, 0, JSTYPE_ARRAY, JSDOCSTR("")
-	,JSDOCSTR("returns an object of all message headers indexed by message number.")
-	,316
-	},
 	{"put_msg_header",	js_put_msg_header,	2, JSTYPE_BOOLEAN,	JSDOCSTR("[by_offset=<tt>false</tt>,] number, object header")
 	,JSDOCSTR("write a message header")
 	,310
@@ -2521,16 +2402,13 @@ static JSClass js_msgbase_class = {
 static JSBool
 js_msgbase_constructor(JSContext *cx, uintN argc, jsval *arglist)
 {
-	JSObject *		obj;
-	jsval *			argv=JS_ARGV(cx, arglist);
-	JSString*		js_str;
-	JSObject*		cfgobj;
-	char*			base;
-	private_t*		p;
-	scfg_t*			scfg;
+	JSObject *obj;
+	jsval *argv=JS_ARGV(cx, arglist);
+	JSString*	js_str;
+	JSObject*	cfgobj;
+	char*		base;
+	private_t*	p;
 
-	scfg=JS_GetRuntimePrivate(JS_GetRuntime(cx));
-	
 	obj=JS_NewObject(cx, &js_msgbase_class, NULL, NULL);
 	JS_SET_RVAL(cx, arglist, OBJECT_TO_JSVAL(obj));
 	if((p=(private_t*)malloc(sizeof(private_t)))==NULL) {
@@ -2747,6 +2625,7 @@ JSObject* DLLCALL js_CreateMsgBaseClass(JSContext* cx, JSObject* parent, scfg_t*
 	JSObject*	constructor;
 	jsval		val;
 
+	scfg = cfg;
 	obj = JS_InitClass(cx, parent, NULL
 		,&js_msgbase_class
 		,js_msgbase_constructor
