36fbd56fe0cccd7554e47eded9ba7f7099d4a96d
rswindell <rswindell>
2013-02-13 00:49:40 +0000

    Fix apparent bug introduced in rev 57 (part of the great gcc warning removal of 2012) - as reported by Mike (First Officer).

--- sbbs3/readmail.cpp
+++ sbbs3/readmail.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet private mail reading function */
 
-/* $Id: readmail.cpp,v 1.58 2013/01/06 06:54:11 deuce Exp $ */
+/* $Id: readmail.cpp,v 1.59 2013/02/13 00:49:40 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2013 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -521,6 +521,7 @@ void sbbs_t::readmail(uint usernumber, int which)
 					bputs(text[MailOnSystemLstHdr]);
 				else
 					bputs(text[MailWaitingLstHdr]);
+				u=i;
 				for(;u<smb.msgs && !msgabort();u++) {
 					if(msg.total_hfields)
 						smb_freemsgmem(&msg);
