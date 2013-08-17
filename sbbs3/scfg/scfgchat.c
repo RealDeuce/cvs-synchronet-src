5d019083dddaa5cdd5fcfcc0d8882d26078061ad
deuce <deuce>
2013-08-17 02:02:07 +0000

    Add WINDRES macro

--- sbbs3/scfg/scfgchat.c
+++ sbbs3/scfg/scfgchat.c
@@ -1,6 +1,6 @@
 /* scfgchat.c */
 
-/* $Id: scfgchat.c,v 1.13 2013/09/18 16:52:44 deuce Exp $ */
+/* $Id: scfgchat.c,v 1.11 2012/12/19 12:19:00 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -205,7 +205,7 @@ when running this chat pager, set this option to either `Standard` or ~Console~.
 void chan_cfg()
 {
 	static int chan_dflt,chan_bar,opt_dflt;
-	char str[128],code[128],done=0;
+	char str[128],code[128],done=0,*p;
 	int j,k;
 	uint i;
 	static chan_t savchan;
@@ -315,7 +315,7 @@ channel name.
 		k=0;
 		sprintf(opt[k++],"%-27.27s%s","Name",cfg.chan[i]->name);
 		sprintf(opt[k++],"%-27.27s%s","Internal Code",cfg.chan[i]->code);
-		sprintf(opt[k++],"%-27.27s%"PRIu32,"Cost in Credits",cfg.chan[i]->cost);
+		sprintf(opt[k++],"%-27.27s%lu","Cost in Credits",cfg.chan[i]->cost);
 		sprintf(opt[k++],"%-27.27s%.40s","Access Requirements"
 			,cfg.chan[i]->arstr);
 		sprintf(opt[k++],"%-27.27s%s","Password Protection"
@@ -611,7 +611,7 @@ This is the output string that results from this chat action.
 void guru_cfg()
 {
 	static int guru_dflt,guru_bar,opt_dflt;
-	char str[128],code[128],done=0;
+	char str[128],code[128],done=0,*p;
 	int j,k;
 	uint i;
 	static guru_t savguru;
