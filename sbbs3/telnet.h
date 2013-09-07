0e7798533faffe64ac2221fb02b7f78365826479
deuce <deuce>
2013-09-07 00:31:16 +0000

    Fix off-by-one error when calculating the max socket.

--- sbbs3/telnet.h
+++ sbbs3/telnet.h
@@ -2,7 +2,7 @@
 
 /* Synchronet telnet-related constants and function prototypes */
 
-/* $Id: telnet.h,v 1.14 2012/04/11 04:10:39 rswindell Exp $ */
+/* $Id: telnet.h,v 1.15 2013/08/17 01:25:30 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -146,7 +146,7 @@ enum {
 #undef DLLCALL
 #endif
 
-#ifdef _WIN32
+#if defined(_WIN32) && !defined(TELNET_NO_DLL)
 	#ifdef SBBS_EXPORTS
 		#define DLLEXPORT __declspec(dllexport)
 	#else
