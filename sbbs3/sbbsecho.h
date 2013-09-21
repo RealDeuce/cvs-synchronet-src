680c1e7df812fb4a061d872fdd0d9b78520e86a2
rswindell <rswindell>
2013-09-21 06:34:49 +0000

    Updated version number.

--- sbbs3/sbbsecho.h
+++ sbbs3/sbbsecho.h
@@ -2,13 +2,13 @@
 
 /* Synchronet FidoNet Echomail tosser/scanner/areafix program */
 
-/* $Id: sbbsecho.h,v 1.14 2009/01/15 08:02:29 rswindell Exp $ */
+/* $Id: sbbsecho.h,v 1.20 2013/09/16 03:08:11 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2013 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -37,7 +37,7 @@
 
 /* Portions written by Allen Christiansen 1994-1996 						*/
 
-#define SBBSECHO_VER "2.12"
+#define SBBSECHO_VER "2.21"
 
 #define IMPORT_NETMAIL  (1L<<0)
 #define IMPORT_PACKETS	(1L<<1)
@@ -140,9 +140,9 @@ typedef struct {						/* Type 2+ Packet Header Info */
 	} two_plus_t;
 
 typedef struct {						/* Type 2.2 Packet Header Info */
-	uchar origdomn[8],					/* Origination Domain */
-		  destdomn[8],					/* Destination Domain */
-		  empty[4]; 					/* Product Specific Data */
+	char origdomn[8],					/* Origination Domain */
+		  destdomn[8];					/* Destination Domain */
+	uchar	  empty[4]; 					/* Product Specific Data */
 	} two_two_t;
 
 typedef struct {
@@ -167,9 +167,9 @@ typedef struct {
 	} area_t;
 
 typedef struct {
-	FILE *stream;				/* The stream associated with this packet */
+	FILE *stream;				/* The stream associated with this packet (NULL if not-open) */
 	faddr_t uplink; 			/* The current uplink for this packet */
-	uchar filename[128],curopen;/* Name of the file and if it's open or not */
+	char filename[MAX_PATH+1];	/* Name of the file */
     } outpkt_t;
 
 typedef struct {
@@ -221,7 +221,8 @@ typedef struct {
 			   ,outbound[82]		/* Outbound directory */
 			   ,areafile[128]		/* AREAS.BBS path/filename */
 			   ,logfile[128]		/* LOG path/filename */
-			   ,cfgfile[128];		/* Configuration path/filename */
+			   ,cfgfile[128]		/* Configuration path/filename */
+			   ,sysop_alias[FIDO_NAME_LEN];
 	ulong		maxpktsize			/* Maximum size for packets */
 			   ,maxbdlsize			/* Maximum size for bundles */
 			   ,log					/* What do we log? */
