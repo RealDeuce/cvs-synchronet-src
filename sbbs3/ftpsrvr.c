731858ff3f38f64eda384ac4d525c33534fd6258
deuce <deuce>
2013-10-01 02:03:27 +0000

    Fix last commit... was still using the offset, just wasn't an array. this one really does use the message number now.

--- sbbs3/ftpsrvr.c
+++ sbbs3/ftpsrvr.c
@@ -2,7 +2,7 @@
 
 /* Synchronet FTP server */
 
-/* $Id: ftpsrvr.c,v 1.400 2013/02/07 01:09:50 deuce Exp $ */
+/* $Id: ftpsrvr.c,v 1.401 2013/02/07 06:20:21 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -389,6 +389,7 @@ js_write(JSContext *cx, uintN argc, jsval *arglist)
 	FILE*	fp;
 	jsrefcount	rc;
 	char		*p;
+	size_t		len;
 
 	JS_SET_RVAL(cx, arglist, JSVAL_VOID);
 
@@ -399,12 +400,11 @@ js_write(JSContext *cx, uintN argc, jsval *arglist)
 		str = JS_ValueToString(cx, argv[i]);
 		if (!str)
 		    return JS_FALSE;
-		JSSTRING_TO_MSTRING(cx, str, p, NULL);
-		if(JS_IsExceptionPending(cx))
-			return JS_FALSE;
+		JSSTRING_TO_MSTRING(cx, str, p, &len);
+		HANDLE_PENDING(cx);
 		rc=JS_SUSPENDREQUEST(cx);
 		if(p) {
-			fputs(p, fp);
+			fwrite(p, len, 1, fp);
 			free(p);
 		}
 		JS_RESUMEREQUEST(cx, rc);
@@ -4541,7 +4541,7 @@ const char* DLLCALL ftp_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.400 $", "%*s %s", revision);
+	sscanf("$Revision: 1.401 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
