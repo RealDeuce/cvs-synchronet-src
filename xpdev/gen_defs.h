ebf6a11c3059e954a267270e430371df191f9992
rswindell <rswindell>
2014-01-04 10:41:06 +0000

    Update copyright date to 2014.

--- xpdev/gen_defs.h
+++ xpdev/gen_defs.h
@@ -2,7 +2,7 @@
 
 /* General(ly useful) constant, macro, and type definitions */
 
-/* $Id: gen_defs.h,v 1.66 2014/04/04 00:17:52 deuce Exp $ */
+/* $Id: gen_defs.h,v 1.61 2013/09/04 00:22:44 deuce Exp $ */
 																			
 /****************************************************************************
  * @format.tab-size 4           (Plain Text/Source Code File Header)        *
@@ -137,12 +137,14 @@ enum {
 
 /* Unsigned type short-hands    */
 #ifndef uchar
-    #define uchar   unsigned char
+        #define uchar   unsigned char
 #endif
-#ifndef ushort
-	#define ushort  unsigned short
-	typedef unsigned int uint;			/* Incompatible with Spidermonkey header files when #define'd */
-	#define ulong   unsigned long
+#ifndef __GLIBC__
+        #ifndef ushort
+        #define ushort  unsigned short
+        typedef unsigned int uint;			/* Incompatible with Spidermonkey header files when #define'd */
+        #define ulong   unsigned long
+        #endif
 #endif
 
 #if !defined(HAS_INTTYPES_H) && !defined(XPDEV_DONT_DEFINE_INTTYPES)
@@ -172,11 +174,6 @@ typedef uint64_t	uintmax_t;
 #define _UINTMAX_T_DECLARED
 typedef int64_t		intmax_t;
 #define _INTMAX_T_DECLARED
-
-#if !defined(HAS_STDINT_H) && !defined(_UINTPTR_T_DEFINED)
-typedef uintmax_t	uintptr_t;
-typedef intmax_t	intptr_t;
-#endif
 
 /* printf integer formatters: */
 
