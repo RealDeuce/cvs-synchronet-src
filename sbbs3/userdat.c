87479be46029c2a2229b948b9b27e7284da37b63
rswindell <rswindell>
2014-01-04 10:49:10 +0000

    Improve multi-monitor support in Synchronet Control Panel by setting each undockable form's DefaultMonitor property to dmPrimary (rather than dmActiveForm) and save/restore the value from the registry (and export-to import-from .ini file) allowing over-ride of default monitor for undocked windows to be customized on a per-form basis (very advanced use). For sysops not using undockable forms in the control panel (the vast majority), this enhancement has no effect.

--- sbbs3/userdat.c
+++ sbbs3/userdat.c
@@ -2,13 +2,13 @@
 
 /* Synchronet user data-related routines (exported) */
 
-/* $Id: userdat.c,v 1.153 2015/01/23 02:55:40 rswindell Exp $ */
+/* $Id: userdat.c,v 1.150 2013/09/15 08:39:47 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2015 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2013 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -384,10 +384,9 @@ static void dirtyuserdat(scfg_t* cfg, uint usernumber)
 		getnodedat(cfg, i,&node,NULL);
 		if(node.useron==usernumber && (node.status==NODE_INUSE
 			|| node.status==NODE_QUIET)) {
-			if(getnodedat(cfg, i,&node,&file) == 0) {
-				node.misc|=NODE_UDAT;
-				putnodedat(cfg, i,&node,file);
-			}
+			getnodedat(cfg, i,&node,&file);
+			node.misc|=NODE_UDAT;
+			putnodedat(cfg, i,&node,file);
 			break; 
 		} 
 	}
@@ -730,10 +729,8 @@ int DLLCALL putnodedat(scfg_t* cfg, uint number, node_t* node, int file)
 	int		wrerr=0;
 	int		attempts;
 
-	if(file<0)
-		return -1;
 	if(!VALID_CFG(cfg) 
-		|| node==NULL || number<1 || number>cfg->sys_nodes) {
+		|| node==NULL || number<1 || number>cfg->sys_nodes || file<0) {
 		close(file);
 		return(-1);
 	}
@@ -1143,10 +1140,9 @@ int DLLCALL putsmsg(scfg_t* cfg, int usernumber, char *strin)
 		if(node.useron==usernumber
 			&& (node.status==NODE_INUSE || node.status==NODE_QUIET)
 			&& !(node.misc&NODE_MSGW)) {
-			if(getnodedat(cfg,i,&node,&file)==0) {
-				node.misc|=NODE_MSGW;
-				putnodedat(cfg,i,&node,file); 
-			}
+			getnodedat(cfg,i,&node,&file);
+			node.misc|=NODE_MSGW;
+			putnodedat(cfg,i,&node,file); 
 		} 
 	}
 	return(0);
@@ -1171,10 +1167,9 @@ char* DLLCALL getsmsg(scfg_t* cfg, int usernumber)
 		if(node.useron==usernumber
 			&& (node.status==NODE_INUSE || node.status==NODE_QUIET)
 			&& node.misc&NODE_MSGW) {
-			if(getnodedat(cfg,i,&node,&file) == 0) {
-				node.misc&=~NODE_MSGW;
-				putnodedat(cfg,i,&node,file); 
-			}
+			getnodedat(cfg,i,&node,&file);
+			node.misc&=~NODE_MSGW;
+			putnodedat(cfg,i,&node,file); 
 		} 
 	}
 
@@ -1211,10 +1206,9 @@ char* DLLCALL getnmsg(scfg_t* cfg, int node_num)
 	if(!VALID_CFG(cfg) || node_num<1)
 		return(NULL);
 
-	if(getnodedat(cfg,node_num,&node,&file) == 0) {
-		node.misc&=~NODE_NMSG;          /* clear the NMSG flag */
-		putnodedat(cfg,node_num,&node,file);
-	}
+	getnodedat(cfg,node_num,&node,&file);
+	node.misc&=~NODE_NMSG;          /* clear the NMSG flag */
+	putnodedat(cfg,node_num,&node,file);
 
 	SAFEPRINTF2(str,"%smsgs/n%3.3u.msg",cfg->data_dir,node_num);
 	if(flength(str)<1L)
@@ -1270,10 +1264,9 @@ int DLLCALL putnmsg(scfg_t* cfg, int num, char *strin)
 	getnodedat(cfg,num,&node,NULL);
 	if((node.status==NODE_INUSE || node.status==NODE_QUIET)
 		&& !(node.misc&NODE_NMSG)) {
-		if(getnodedat(cfg,num,&node,&file) == 0) {
-			node.misc|=NODE_NMSG;
-			putnodedat(cfg,num,&node,file); 
-		}
+		getnodedat(cfg,num,&node,&file);
+		node.misc|=NODE_NMSG;
+		putnodedat(cfg,num,&node,file); 
 	}
 
 	return(0);
@@ -2734,13 +2727,11 @@ long DLLCALL loginAttemptListClear(link_list_t* list)
 }
 
 /****************************************************************************/
-static list_node_t* login_attempted(link_list_t* list, const SOCKADDR_IN* addr)
+static list_node_t* login_attempted(link_list_t* list, SOCKADDR_IN* addr)
 {
 	list_node_t*		node;
 	login_attempt_t*	attempt;
 
-	if(list==NULL)
-		return NULL;
 	for(node=list->first; node!=NULL; node=node->next) {
 		attempt=node->data;
 		if(memcmp(&attempt->addr,&addr->sin_addr,sizeof(attempt->addr))==0)
@@ -2750,7 +2741,7 @@ static list_node_t* login_attempted(link_list_t* list, const SOCKADDR_IN* addr)
 }
 
 /****************************************************************************/
-long DLLCALL loginAttempts(link_list_t* list, const SOCKADDR_IN* addr)
+long DLLCALL loginAttempts(link_list_t* list, SOCKADDR_IN* addr)
 {
 	long				count=0;
 	list_node_t*		node;
@@ -2764,7 +2755,7 @@ long DLLCALL loginAttempts(link_list_t* list, const SOCKADDR_IN* addr)
 }
 
 /****************************************************************************/
-void DLLCALL loginSuccess(link_list_t* list, const SOCKADDR_IN* addr)
+void DLLCALL loginSuccess(link_list_t* list, SOCKADDR_IN* addr)
 {
 	list_node_t*		node;
 
@@ -2777,7 +2768,7 @@ void DLLCALL loginSuccess(link_list_t* list, const SOCKADDR_IN* addr)
 /****************************************************************************/
 /* Returns number of *unique* login attempts (excludes consecutive dupes)	*/
 /****************************************************************************/
-ulong DLLCALL loginFailure(link_list_t* list, const SOCKADDR_IN* addr, const char* prot, const char* user, const char* pass)
+ulong DLLCALL loginFailure(link_list_t* list, SOCKADDR_IN* addr, const char* prot, const char* user, const char* pass)
 {
 	list_node_t*		node;
 	login_attempt_t		first;
