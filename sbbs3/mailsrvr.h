8f731be076225f51a47e8fb28c69610e4f33bc10
rswindell <rswindell>
2014-01-08 10:37:49 +0000

    Less broken thread_count checking before debug log output. We don't really need to check active_clients (redundant with thread_count) before allowing recycle.

--- sbbs3/mailsrvr.h
+++ sbbs3/mailsrvr.h
@@ -2,13 +2,13 @@
 
 /* Synchronet Mail (SMTP/POP3/SendMail) server */
 
-/* $Id: mailsrvr.h,v 1.70 2011/09/10 10:32:07 rswindell Exp $ */
+/* $Id: mailsrvr.h,v 1.71 2014/01/04 10:41:06 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
