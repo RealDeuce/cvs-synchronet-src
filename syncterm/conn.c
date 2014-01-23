fc75ca1fcba095699da8308e2cc1628b5bce2243
deuce <deuce>
2014-01-23 08:31:40 +0000

    Add an entry config option to force IPv4 or IPv6 resolution only. Add -4 and -6 command-line arguments. Add csr capability to built-in terminfo.

--- syncterm/conn.c
+++ syncterm/conn.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: conn.c,v 1.70 2014/02/06 12:07:53 deuce Exp $ */
+/* $Id: conn.c,v 1.68 2014/01/23 08:31:40 deuce Exp $ */
 
 #include <stdlib.h>
 
@@ -470,7 +470,7 @@ int conn_socket_connect(struct bbslist *bbs)
 #if (EAGAIN!=EWOULDBLOCK)
 				case EWOULDBLOCK:
 #endif
-					for(;sock!=INVALID_SOCKET;) {
+					for(;;) {
 						tv.tv_sec=1;
 						tv.tv_usec=0;
 
@@ -488,11 +488,7 @@ int conn_socket_connect(struct bbslist *bbs)
 								sock=INVALID_SOCKET;
 								continue;
 							case 1:
-								if(socket_check(sock, NULL, NULL, 0))
-									goto connected;
-								closesocket(sock);
-								sock=INVALID_SOCKET;
-								continue;
+								goto connected;
 							default:
 								break;
 						}
