c84d4aac9179f616f3411f0a74c1812abefb2648
deuce <deuce>
2014-02-05 20:36:58 +0000

    Don't link against portaudio, we used dlopen() and friends for that.

--- xpdev/xpprintf.c
+++ xpdev/xpprintf.c
@@ -2,7 +2,7 @@
 
 /* Deuce's vs[n]printf() replacement */
 
-/* $Id: xpprintf.c,v 1.48 2014/10/06 21:45:36 deuce Exp $ */
+/* $Id: xpprintf.c,v 1.39 2014/02/05 10:22:17 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -39,7 +39,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "genwrap.h"    /* alloca() */
+#if defined(_WIN32)
+ #include <malloc.h>    /* alloca() on Win32 */
+#endif
 
 #include "xpprintf.h"
 #include "gen_defs.h"
@@ -53,17 +55,17 @@
 /* Maximum length of a format specifier including the % */
 #define MAX_FORMAT_LEN	256
 
-void DLLCALL xp_asprintf_free(char *format)
+void xp_asprintf_free(char *format)
 {
 	free(format);
 }
 
-int DLLCALL xp_printf_get_type(const char *format)
+int xp_printf_get_type(const char *format)
 {
 	const char	*p;
 	int		modifier=0;
 	int		j;
-	int		correct_type=0;
+	int		correct_type;
 
 	if(!*(size_t *)format)
 		return(0);
@@ -122,7 +124,7 @@ int DLLCALL xp_printf_get_type(const char *format)
 			}
 			break;
 		case 'l':
-			modifier='l';
+			modifier='h';
 			p++;
 			if(*p=='l') {
 				p++;
@@ -280,29 +282,29 @@ int DLLCALL xp_printf_get_type(const char *format)
  *
  * Does not currently support the $ argument selector.
  *
- * Currently, the type is not overly useful, but this could be used for
+ * Currently, the type is not overly usefull, but this could be used for
  * automatic type conversions (ie: int to char *).  Right now it just assures
  * that the type passed to sprintf() is the type passed to
  * xp_asprintf_next().
  */
-char* DLLCALL xp_asprintf_next(char *format, int type, ...)
+char *xp_asprintf_next(char *format, int type, ...)
 {
 	va_list vars;
 	char			*p;
 	char			*newbuf;
-	int				i=0,j;
-	unsigned int	ui=0;
-	long int		l=0;
-	unsigned long int	ul=0;
+	int				i,j;
+	unsigned int	ui;
+	long int		l;
+	unsigned long int	ul;
 #if defined(XP_PRINTF_TYPE_LONGLONG)
-	long long int	ll=0;
-	unsigned long long int	ull=0;
+	long long int	ll;
+	unsigned long long int	ull;
 #endif
-	double			d=0;
-	long double		ld=0;
-	char*			cp=NULL;
-	void*			pntr=NULL;
-	size_t			s=0;
+	double			d;
+	long double		ld;
+	char*			cp;
+	void*			pntr;
+	size_t			s;
 	unsigned long	offset=0;
 	unsigned long	offset2=0;
 	size_t			format_len;
@@ -458,7 +460,7 @@ char* DLLCALL xp_asprintf_next(char *format, int type, ...)
 			}
 			break;
 		case 'l':
-			modifier='l';
+			modifier='h';
 			*(fmt++)=*(p++);
 			if(*p=='l') {
 				*(fmt++)=*(p++);
@@ -1078,13 +1080,13 @@ char* DLLCALL xp_asprintf_next(char *format, int type, ...)
 							pntr=(void *)((intptr_t)i);
 							break;
 						case XP_PRINTF_TYPE_UINT:
-							pntr=(void *)((uintptr_t)ui);
+							pntr=(void *)((intptr_t)ui);
 							break;
 						case XP_PRINTF_TYPE_LONG:
 							pntr=(void *)((intptr_t)l);
 							break;
 						case XP_PRINTF_TYPE_ULONG:
-							pntr=(void *)((uintptr_t)ul);
+							pntr=(void *)((intptr_t)ul);
 							break;
 #if defined(XP_PRINTF_TYPE_LONGLONG)
 						case XP_PRINTF_TYPE_LONGLONG:
@@ -1095,7 +1097,7 @@ char* DLLCALL xp_asprintf_next(char *format, int type, ...)
 							break;
 #endif
 						case XP_PRINTF_TYPE_CHARP:
-							pntr=(void *)(cp);
+							pntr=(void *)((intptr_t)cp);
 							break;
 						case XP_PRINTF_TYPE_DOUBLE:
 							pntr=(void *)((intptr_t)d);
@@ -1253,7 +1255,7 @@ char* DLLCALL xp_asprintf_next(char *format, int type, ...)
 	return(format);
 }
 
-char* DLLCALL xp_asprintf_start(const char *format)
+char *xp_asprintf_start(const char *format)
 {
 	char	*ret;
 	char	*p;
@@ -1284,7 +1286,7 @@ char* DLLCALL xp_asprintf_start(const char *format)
 	return(ret);
 }
 
-char* DLLCALL xp_asprintf_end(char *format, size_t *lenret)
+char *xp_asprintf_end(char *format, size_t *lenret)
 {
 	char	*p;
 	size_t	len;
@@ -1304,7 +1306,7 @@ char* DLLCALL xp_asprintf_end(char *format, size_t *lenret)
 	return(format);
 }
 
-char* DLLCALL xp_vasprintf(const char *format, va_list va)
+char *xp_vasprintf(const char *format, va_list va)
 {
 	char	*working;
 	char	*next;
@@ -1371,7 +1373,7 @@ char* DLLCALL xp_vasprintf(const char *format, va_list va)
 	return(next);
 }
 
-char* DLLCALL xp_asprintf(const char *format, ...)
+char *xp_asprintf(const char *format, ...)
 {
 	char	*ret;
 	va_list	va;
