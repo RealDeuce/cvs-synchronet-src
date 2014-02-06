3870b5b2a0297bf3e134f9fb7a19f1a5e3e71e7d
deuce <deuce>
2014-02-06 20:35:15 +0000

    Remove keys.h

--- sbbs3/js_msgbase.c
+++ sbbs3/js_msgbase.c
@@ -2,7 +2,7 @@
 
 /* Synchronet JavaScript "MsgBase" Object */
 
-/* $Id: js_msgbase.c,v 1.182 2013/02/08 19:17:02 deuce Exp $ */
+/* $Id: js_msgbase.c,v 1.186 2013/10/05 14:29:22 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1487,6 +1487,101 @@ js_get_msg_header(JSContext *cx, uintN argc, jsval *arglist)
 }
 
 static JSBool
+js_get_all_msg_headers(JSContext *cx, uintN argc, jsval *arglist)
+{
+	JSObject *obj=JS_THIS_OBJECT(cx, arglist);
+	JSObject*	hdrobj;
+	jsrefcount	rc;
+	privatemsg_t*	p;
+	private_t*	priv;
+	JSObject*	proto;
+	jsval		val;
+	uint32_t	off;
+    JSObject*	retobj;
+	char		numstr[16];
+
+	JS_SET_RVAL(cx, arglist, JSVAL_NULL);
+
+	if((priv=(private_t*)JS_GetPrivate(cx,obj))==NULL) {
+		JS_ReportError(cx,getprivate_failure,WHERE);
+		return(JS_FALSE);
+	}
+
+	if(!SMB_IS_OPEN(&(priv->smb)))
+		return(JS_TRUE);
+
+    retobj = JS_NewObject(cx, NULL, NULL, obj);
+    JS_SET_RVAL(cx, arglist, OBJECT_TO_JSVAL(retobj));
+
+	rc=JS_SUSPENDREQUEST(cx);
+	if((priv->status=smb_locksmbhdr(&(priv->smb)))!=SMB_SUCCESS) {
+		JS_RESUMEREQUEST(cx, rc);
+		return(JS_TRUE);
+	}
+	JS_RESUMEREQUEST(cx, rc);
+
+	if(JS_GetProperty(cx, JS_GetGlobalObject(cx), "MsgBase", &val) && !JSVAL_NULL_OR_VOID(val)) {
+		JS_ValueToObject(cx,val,&proto);
+		if(JS_GetProperty(cx, proto, "HeaderPrototype", &val) && !JSVAL_NULL_OR_VOID(val))
+			JS_ValueToObject(cx,val,&proto);
+		else
+			proto=NULL;
+	}
+	else
+		proto=NULL;
+
+	for(off=0; off < priv->smb.status.total_msgs; off++) {
+		if((p=(privatemsg_t*)malloc(sizeof(privatemsg_t)))==NULL) {
+			smb_unlocksmbhdr(&(priv->smb)); 
+			JS_ReportError(cx,"malloc failed");
+			return(JS_FALSE);
+		}
+
+		memset(p,0,sizeof(privatemsg_t));
+
+		/* Parse boolean arguments first */
+		p->p=priv;
+		p->expand_fields=JS_TRUE;	/* This parameter defaults to true */
+
+		p->msg.offset=off;
+		rc=JS_SUSPENDREQUEST(cx);
+		if((priv->status=smb_getmsgidx(&(priv->smb), &(p->msg)))!=SMB_SUCCESS) {
+			smb_unlocksmbhdr(&(priv->smb)); 
+			JS_RESUMEREQUEST(cx, rc);
+			return(JS_TRUE);
+		}
+
+		if((priv->status=smb_getmsghdr(&(priv->smb), &(p->msg)))!=SMB_SUCCESS) {
+			smb_unlocksmbhdr(&(priv->smb)); 
+			JS_RESUMEREQUEST(cx, rc);
+			return(JS_TRUE);
+		}
+
+		JS_RESUMEREQUEST(cx, rc);
+
+		if((hdrobj=JS_NewObject(cx,&js_msghdr_class,proto,obj))==NULL) {
+			smb_freemsgmem(&(p->msg));
+			smb_unlocksmbhdr(&(priv->smb)); 
+			return(JS_TRUE);
+		}
+
+		if(!JS_SetPrivate(cx, hdrobj, p)) {
+			JS_ReportError(cx,"JS_SetPrivate failed");
+			free(p);
+			smb_unlocksmbhdr(&(priv->smb));
+			return(JS_FALSE);
+		}
+
+		val=OBJECT_TO_JSVAL(hdrobj);
+		sprintf(numstr,"%"PRIu32, p->msg.hdr.number);
+		JS_SetProperty(cx, retobj, numstr, &val);
+	}
+	smb_unlocksmbhdr(&(priv->smb));
+
+	return(JS_TRUE);
+}
+
+static JSBool
 js_put_msg_header(JSContext *cx, uintN argc, jsval *arglist)
 {
 	JSObject *obj=JS_THIS_OBJECT(cx, arglist);
@@ -2267,6 +2362,10 @@ static jsSyncMethodSpec js_msgbase_functions[] = {
 	"<br><i>New in v3.12:</i> Pass <i>false</i> for the <i>expand_fields</i> argument (default: <i>true</i>) "
 	"if you will be re-writing the header later with <i>put_msg_header()</i>")
 	,312
+	},
+	{"get_all_msg_headers", js_get_all_msg_headers, 0, JSTYPE_ARRAY, JSDOCSTR("")
+	,JSDOCSTR("returns an object of all message headers indexed by message number.")
+	,316
 	},
 	{"put_msg_header",	js_put_msg_header,	2, JSTYPE_BOOLEAN,	JSDOCSTR("[by_offset=<tt>false</tt>,] number, object header")
 	,JSDOCSTR("write a message header")
