d3060d83896b9f311fa5f5244cd6ee77f4ebdb12
deuce <deuce>
2014-02-06 10:43:19 +0000

    CMake file for ciolib.

--- sbbs3/umonitor/spyon.c
+++ sbbs3/umonitor/spyon.c
@@ -2,7 +2,7 @@
 
 /* Synchronet for *nix node spy */
 
-/* $Id: spyon.c,v 1.10 2014/02/13 08:50:02 deuce Exp $ */
+/* $Id: spyon.c,v 1.9 2011/09/09 23:58:03 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -35,29 +35,20 @@
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
 
+#include <unistd.h>
 #include <stdlib.h>
 #include "sockwrap.h"	/* Must go before <sys/un.h> */
-#ifndef _WIN32
 #include <sys/un.h>
-#endif
 #include <stdio.h>
 #include <string.h>
-
-#include <genwrap.h>
-#include <sockwrap.h>
+#include "sockwrap.h"
 #include "spyon.h"
 #include "ciolib.h"
 #include "cterm.h"
-#include "uifc.h"
 
 struct cterminal *cterm;
-extern uifcapi_t uifc; /* User Interface (UIFC) Library API */
 
 int spyon(char *sockname)  {
-#if defined _WIN32
-	uifc.msg("Spying not supported on Win32 yet!");
-	return SPY_SOCKETLOST;
-#else
 	SOCKET		spy_sock=INVALID_SOCKET;
 	struct sockaddr_un spy_name;
 	socklen_t	spy_len;
@@ -169,6 +160,5 @@ int spyon(char *sockname)  {
 	textattr(ti.attribute);
 	gotoxy(ti.curx,ti.cury);
 	return(retval);
-#endif
 }
 
