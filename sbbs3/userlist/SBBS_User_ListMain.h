4c657e5b55bbfbc6c265ad1b9e23449c5d2e1551
deuce <deuce>
2014-02-07 09:56:45 +0000

    Pass the correct socket handle to sock_destroy callback.

--- sbbs3/userlist/SBBS_User_ListMain.h
+++ sbbs3/userlist/SBBS_User_ListMain.h
@@ -20,7 +20,6 @@
 #include <wx/choice.h>
 #include <wx/button.h>
 #include <wx/frame.h>
-#include <wx/statusbr.h>
 //*)
 
 #include "SBBS_User_ListApp.h"
@@ -35,6 +34,9 @@ class SBBS_User_ListFrame: public wxFrame
     private:
 
         void fillUserList(void);
+		void CopyItems(int state);
+		void applyARS(void);
+		int	sort;
 
         //(*Handlers(SBBS_User_ListFrame)
         void OnQuit(wxCommandEvent& event);
@@ -44,6 +46,10 @@ class SBBS_User_ListFrame: public wxFrame
         void OnClearButtonClick(wxCommandEvent& event);
         void OnUserListItemSelect(wxListEvent& event);
         void OnQVChoiceSelect(wxCommandEvent& event);
+        void OnUserListItemRClick(wxListEvent& event);
+        void CopyMenuItemSelected(wxCommandEvent& event);
+        void CopyAllMenuItemSelected(wxCommandEvent& event);
+        void OnUserListColumnClick(wxListEvent& event);
         //*)
 
         //(*Identifiers(SBBS_User_ListFrame)
@@ -56,18 +62,23 @@ class SBBS_User_ListFrame: public wxFrame
         static const long ID_REFRESHBUTTON;
         static const long ID_EDITBUTTON;
         static const long ID_PANEL1;
-        static const long idMenuQuit;
-        static const long idMenuAbout;
-        static const long ID_STATUSBAR1;
+        static const long ID_EDITUSER;
+        static const long ID_COPY;
+        static const long ID_COPYALL;
+        static const long ID_REFRESH;
         //*)
 
         //(*Declarations(SBBS_User_ListFrame)
         wxButton* RefreshButton;
+        wxMenuItem* MenuItem5;
         wxStaticText* StaticText2;
+        wxMenuItem* MenuItem4;
         wxPanel* Panel1;
         wxStaticText* StaticText1;
+        wxMenu ContextMenu;
         wxListCtrl* UserList;
-        wxStatusBar* StatusBar1;
+        wxMenuItem* MenuItem3;
+        wxMenuItem* MenuItem6;
         wxButton* ClearButton;
         wxChoice* QVChoice;
         wxButton* EditButton;
