9d54c0f550954028e4826882deb98ca45696aa2a
deuce <deuce>
2014-02-09 11:37:07 +0000

    DLLEXPORT/DLLCALL-ify

--- conio/sdlfuncs.c
+++ conio/sdlfuncs.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>	/* getenv()/exit()/atexit() */
 #include <stdio.h>	/* NULL */
 
+#include "gen_defs.h"
+#include "threadwrap.h"
 #include <SDL.h>
 #ifndef main
  #define USE_REAL_MAIN
 #endif
-#include "gen_defs.h"
 #ifdef USE_REAL_MAIN
  #undef main
 #endif
@@ -32,6 +33,11 @@ static SDL_sem *sdl_main_sem;
 SDL_sem *sdl_exit_sem;
 
 int CIOLIB_main(int argc, char **argv, char **enviro);
+int XPDEV_main(int argc, char **argv, char **enviro)
+{
+	return CIOLIB_main(argc, argv, enviro);
+}
+
 
 int load_sdl_funcs(struct sdlfuncs *sdlf)
 {
@@ -74,6 +80,10 @@ int load_sdl_funcs(struct sdlfuncs *sdlf)
 		return(-1);
 	}
 	if((sdlf->SemWait=xp_dlsym(sdl_dll, SDL_SemWait))==NULL) {
+		xp_dlclose(sdl_dll);
+		return(-1);
+	}
+	if((sdlf->SemWaitTimeout=xp_dlsym(sdl_dll, SDL_SemWaitTimeout))==NULL) {
 		xp_dlclose(sdl_dll);
 		return(-1);
 	}
@@ -316,6 +326,12 @@ void run_sdl_drawing_thread(int (*drawing_thread)(void *data), void (*exit_drawi
 	sdl.SemPost(sdl_main_sem);
 }
 
+static void QuitWrap(void)
+{
+	if(sdl.Quit)
+		sdl.Quit();
+}
+
 #ifndef main
 int main(int argc, char **argv, char **env)
 #else
@@ -402,7 +418,8 @@ int SDL_main_env(int argc, char **argv, char **env)
 		}
 	}
 	if(sdl_video_initialized) {
-		atexit(sdl.Quit);
+		SetThreadName("SDL Main");
+		atexit(QuitWrap);
 		sdl_main_sem=sdl.SDL_CreateSemaphore(0);
 		sdl_exit_sem=sdl.SDL_CreateSemaphore(0);
 		main_thread=sdl.CreateThread(sdl_run_main,&ma);
