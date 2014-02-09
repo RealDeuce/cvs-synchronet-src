a1a2999f731caf3adbc1faec27ce8b80c1ace8d4
deuce <deuce>
2014-02-09 11:37:07 +0000

    DLLEXPORT/DLLCALL-ify

--- sbbs3/ctrl/PropertiesDlgUnit.h
+++ sbbs3/ctrl/PropertiesDlgUnit.h
@@ -1,10 +1,10 @@
-/* $Id: PropertiesDlgUnit.h,v 1.16 2009/10/25 05:05:54 rswindell Exp $ */
+/* $Id: PropertiesDlgUnit.h,v 1.17 2011/10/28 08:08:34 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -96,14 +96,12 @@ __published:
 	TLabel *Label11;
 	TEdit *JS_ContextStackEdit;
 	TLabel *Label12;
-	TEdit *JS_BranchLimitEdit;
+    TEdit *JS_TimeLimitEdit;
 	TLabel *Label13;
 	TEdit *JS_GcIntervalEdit;
 	TLabel *Label14;
 	TEdit *JS_YieldIntervalEdit;
     TCheckBox *FileAssociationsCheckBox;
-    TLabel *Label15;
-    TEdit *JS_ThreadStackEdit;
     TGroupBox *LogFontGroupBox;
     TComboBox *LogLevelComboBox;
     TEdit *LogFontExampleEdit;
