01cd728c88b5dd350c7071b9c1cba4f81e030cea
deuce <deuce>
2014-02-10 00:16:56 +0000

    Add a #error so we can track problems with them.

--- conio/x_cio.c
+++ conio/x_cio.c
@@ -1,4 +1,4 @@
-/* $Id: x_cio.c,v 1.37 2015/04/30 00:14:39 deuce Exp $ */
+/* $Id: x_cio.c,v 1.34 2014/02/06 10:58:00 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -268,10 +268,6 @@ int x_init(void)
 		xp_dlclose(dl);
 		return(-1);
 	}
-	if((x11.XCloseDisplay=xp_dlsym(dl,XCloseDisplay))==NULL) {
-		xp_dlclose(dl);
-		return(-1);
-	}
 	if((x11.XCreateSimpleWindow=xp_dlsym(dl,XCreateSimpleWindow))==NULL) {
 		xp_dlclose(dl);
 		return(-1);
@@ -352,32 +348,19 @@ int x_init(void)
 		xp_dlclose(dl);
 		return(-1);
 	}
-	if((x11.XSetWMProtocols=xp_dlsym(dl,XSetWMProtocols))==NULL) {
-		xp_dlclose(dl);
-		return(-1);
-	}
-	if((x11.XInternAtom=xp_dlsym(dl,XInternAtom))==NULL) {
-		xp_dlclose(dl);
-		return(-1);
-	}
 
-	if(sem_init(&pastebuf_set, 0, 0)) {
-		xp_dlclose(dl);
+	if(sem_init(&pastebuf_set, 0, 0))
 		return(-1);
-	}
 	if(sem_init(&pastebuf_used, 0, 0)) {
-		xp_dlclose(dl);
 		sem_destroy(&pastebuf_set);
 		return(-1);
 	}
 	if(sem_init(&init_complete, 0, 0)) {
-		xp_dlclose(dl);
 		sem_destroy(&pastebuf_set);
 		sem_destroy(&pastebuf_used);
 		return(-1);
 	}
 	if(sem_init(&mode_set, 0, 0)) {
-		xp_dlclose(dl);
 		sem_destroy(&pastebuf_set);
 		sem_destroy(&pastebuf_used);
 		sem_destroy(&init_complete);
@@ -385,7 +368,6 @@ int x_init(void)
 	}
 
 	if(pthread_mutex_init(&copybuf_mutex, 0)) {
-		xp_dlclose(dl);
 		sem_destroy(&pastebuf_set);
 		sem_destroy(&pastebuf_used);
 		sem_destroy(&init_complete);
@@ -397,7 +379,6 @@ int x_init(void)
 	_beginthread(x11_mouse_thread,1<<16,NULL);
 	sem_wait(&init_complete);
 	if(!x11_initialized) {
-		xp_dlclose(dl);
 		sem_destroy(&pastebuf_set);
 		sem_destroy(&pastebuf_used);
 		sem_destroy(&init_complete);
