3b8bd16e41da33bca831e34d3a9de9c5d217e4c8
deuce <deuce>
2014-02-10 02:18:18 +0000

    ciolib.h isn't a system header from here.

--- sbbs3/gtkuserlist/objects.mk
+++ sbbs3/gtkuserlist/objects.mk
@@ -1,5 +1,4 @@
 OBJS	:=	\
 			$(MTOBJODIR)$(DIRSEP)gtkuserlist$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)gtkuserlist_glade$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)events$(OFILE) \
 #			$(MTOBJODIR)$(DIRSEP)util_funcs$(OFILE) \
