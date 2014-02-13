e7c772573a25ec402404d118177be8cd09da928a
deuce <deuce>
2014-02-13 20:42:13 +0000

    The forkpty() stuff is a libsbbs thing.  Make it so.

--- sbbs3/sexyz.c
+++ sbbs3/sexyz.c
@@ -2,13 +2,13 @@
 
 /* Synchronet External X/Y/ZMODEM Transfer Protocols */
 
-/* $Id: sexyz.c,v 1.136 2012/02/24 01:46:45 rswindell Exp $ */
+/* $Id: sexyz.c,v 2.0 2013/05/16 00:08:59 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2012 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2013 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -1121,7 +1121,7 @@ static int receive_files(char** fname_list, int fnames)
 				}
 				ftime=total_files=0;
 				total_bytes=0;
-				i=sscanf(block+strlen(block)+1,"%"SCNd64" %"SCNoMAX" %lo %lo %u %"SCNd64
+				i=sscanf((char*)block+strlen((char*)block)+1,"%"SCNd64" %"SCNoMAX" %lo %lo %u %"SCNd64
 					,&file_bytes			/* file size (decimal) */
 					,&ftime 				/* file time (octal unix format) */
 					,&fmode 				/* file mode (not used) */
@@ -1129,8 +1129,8 @@ static int receive_files(char** fname_list, int fnames)
 					,&total_files			/* remaining files to be sent */
 					,&total_bytes			/* remaining bytes to be sent */
 					);
-				lprintf(LOG_DEBUG,"YMODEM header (%u fields): %s", i, block+strlen(block)+1);
-				SAFECOPY(fname,block);
+				lprintf(LOG_DEBUG,"YMODEM header (%u fields): %s", i, block+strlen((char*)block)+1);
+				SAFECOPY(fname,(char*)block);
 
 			} else {	/* Zmodem */
 				lprintf(LOG_INFO,"Waiting for ZMODEM sender...");
@@ -1167,7 +1167,7 @@ static int receive_files(char** fname_list, int fnames)
 			lprintf(LOG_DEBUG,"Incoming filename: %.64s ",fname);
 
 			if(mode&RECVDIR)
-				sprintf(str,"%s%s",fname_list[0],getfname(fname));
+				SAFEPRINTF2(str,"%s%s",fname_list[0],getfname(fname));
 			else {
 				SAFECOPY(str,getfname(fname));
 				for(i=0;i<fnames;i++) {
@@ -1390,7 +1390,6 @@ static int receive_files(char** fname_list, int fnames)
 
 void bail(int code)
 {
-	fcloseall();
 	if(pause_on_exit || (pause_on_abend && code!=0)) {
 		printf("Hit enter to continue...");
 		getchar();
@@ -1517,7 +1516,7 @@ int main(int argc, char **argv)
 	statfp=stdout;
 #endif
 
-	sscanf("$Revision: 1.136 $", "%*s %s", revision);
+	sscanf("$Revision: 2.0 $", "%*s %s", revision);
 
 	fprintf(statfp,"\nSynchronet External X/Y/ZMODEM  v%s-%s"
 		"  Copyright %s Rob Swindell\n\n"
@@ -1768,7 +1767,7 @@ int main(int argc, char **argv)
 				bail(1); 
 				return -1;
 			}
-			sprintf(str,"%s",argv[i]+1);
+			SAFEPRINTF(str,"%s",argv[i]+1);
 			if((fp=fopen(str,"r"))==NULL) {
 				fprintf(statfp,"!Error %d opening filelist: %s\n",errno,str);
 				bail(1); 
