86903f7e32e2e6357127e171c2ce72ccc002e6e6
deuce <deuce>
2014-02-13 11:27:11 +0000

    Sluuuuurp. Ahh.

--- conio/win32cio.c
+++ conio/win32cio.c
@@ -1,4 +1,4 @@
-/* $Id: win32cio.c,v 1.101 2014/11/13 07:40:55 deuce Exp $ */
+/* $Id: win32cio.c,v 1.99 2014/02/10 02:18:18 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -517,6 +517,7 @@ static BOOL WINAPI ControlHandler(unsigned long CtrlType)
 int win32_initciolib(long inmode)
 {
 	DWORD	conmode;
+	int		i,j;
 	HANDLE	h;
 	CONSOLE_SCREEN_BUFFER_INFO	sbuff;
 
@@ -544,58 +545,58 @@ int win32_initciolib(long inmode)
 		return(0);
 
 	if(GetConsoleScreenBufferInfo(h, &sbuff)==0) {
-		win32_textmode(C80);	// TODO: This likely won't work...
+		win32_textmode(C80);
 	}
 	else {
 		/* Switch to closest mode to current screen size */
-		cio_textinfo.screenwidth=sbuff.srWindow.Right-sbuff.srWindow.Left+1;
-		cio_textinfo.screenheight=sbuff.srWindow.Bottom-sbuff.srWindow.Top+1;
-		if(cio_textinfo.screenwidth>=132) {
-			if(cio_textinfo.screenheight<25)
+		i=sbuff.srWindow.Right-sbuff.srWindow.Left+1;
+		j=sbuff.srWindow.Bottom-sbuff.srWindow.Top+1;
+		if(i>=132) {
+			if(j<25)
 				win32_textmode(VESA_132X21);
-			else if(cio_textinfo.screenheight<28)
+			else if(j<28)
 				win32_textmode(VESA_132X25);
-			else if(cio_textinfo.screenheight<30)
+			else if(j<30)
 				win32_textmode(VESA_132X28);
-			else if(cio_textinfo.screenheight<34)
+			else if(j<34)
 				win32_textmode(VESA_132X30);
-			else if(cio_textinfo.screenheight<43)
+			else if(j<43)
 				win32_textmode(VESA_132X34);
-			else if(cio_textinfo.screenheight<50)
+			else if(j<50)
 				win32_textmode(VESA_132X43);
-			else if(cio_textinfo.screenheight<60)
+			else if(j<60)
 				win32_textmode(VESA_132X50);
 			else
 				win32_textmode(VESA_132X60);
 		}
-		else if(cio_textinfo.screenwidth>=80) {
-			if(cio_textinfo.screenheight<21)
+		else if(i>=80) {
+			if(j<21)
 				win32_textmode(C80X14);
-			else if(cio_textinfo.screenheight<25)
+			else if(j<25)
 				win32_textmode(C80X21);
-			else if(cio_textinfo.screenheight<28)
+			else if(j<28)
 				win32_textmode(C80);
-			else if(cio_textinfo.screenheight<43)
+			else if(j<43)
 				win32_textmode(C80X28);
-			else if(cio_textinfo.screenheight<50)
+			else if(j<50)
 				win32_textmode(C80X43);
-			else if(cio_textinfo.screenheight<60)
+			else if(j<60)
 				win32_textmode(C80X50);
 			else
 				win32_textmode(C80X60);
 		}
 		else {
-			if(cio_textinfo.screenheight<21)
+			if(j<21)
 				win32_textmode(C40X14);
-			else if(cio_textinfo.screenheight<25)
+			else if(j<25)
 				win32_textmode(C40X21);
-			else if(cio_textinfo.screenheight<28)
+			else if(j<28)
 				win32_textmode(C40);
-			else if(cio_textinfo.screenheight<43)
+			else if(j<43)
 				win32_textmode(C40X28);
-			else if(cio_textinfo.screenheight<50)
+			else if(j<50)
 				win32_textmode(C40X43);
-			else if(cio_textinfo.screenheight<60)
+			else if(j<60)
 				win32_textmode(C40X50);
 			else
 				win32_textmode(C40X60);
@@ -634,23 +635,18 @@ void win32_textmode(int mode)
 		if(vparams[i].mode==mode)
 			modeidx=i;
 	}
-	sz.X = cio_textinfo.screenwidth > vparams[modeidx].cols ? cio_textinfo.screenwidth : vparams[modeidx].cols;
-	sz.Y = cio_textinfo.screenheight > vparams[modeidx].rows ? cio_textinfo.screenheight : vparams[modeidx].rows;
+	sz.X=vparams[modeidx].cols;
+	sz.Y=vparams[modeidx].rows;
 	rc.Left=0;
 	rc.Right=vparams[modeidx].cols-1;
 	rc.Top=0;
 	rc.Bottom=vparams[modeidx].rows-1;
 
-	if ((h=GetStdHandle(STD_OUTPUT_HANDLE)) == INVALID_HANDLE_VALUE)
-		return;
-	if (!SetConsoleScreenBufferSize(h,sz))
-		return;
-	if (!SetConsoleWindowInfo(h,TRUE,&rc))
-		return;
-	sz.X=vparams[modeidx].cols;
-	sz.Y=vparams[modeidx].rows;
-	if (!SetConsoleScreenBufferSize(h,sz))
-		return;
+	if((h=GetStdHandle(STD_OUTPUT_HANDLE)) != INVALID_HANDLE_VALUE) {
+		SetConsoleScreenBufferSize(h,sz);
+		SetConsoleWindowInfo(h,TRUE,&rc);
+		SetConsoleScreenBufferSize(h,sz);
+	}
 
 	cio_textinfo.attribute=7;
 	cio_textinfo.normattr=7;
