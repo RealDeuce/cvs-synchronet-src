b0335186b23a24a3c274ad1bb466f42c92e229cd
deuce <deuce>
2014-02-13 09:09:18 +0000

    include dirwrap.h for MAX_PATH definition.

--- xpdev/sockwrap.c
+++ xpdev/sockwrap.c
@@ -2,7 +2,7 @@
 
 /* Berkley/WinSock socket API wrappers */
 
-/* $Id: sockwrap.c,v 1.59 2014/03/07 23:55:07 deuce Exp $ */
+/* $Id: sockwrap.c,v 1.58 2014/02/09 13:37:21 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -377,17 +377,14 @@ int DLLCALL nonblocking_connect(SOCKET sock, struct sockaddr* addr, size_t size,
 
 	if(result==SOCKET_ERROR
 		&& (ERROR_VALUE==EWOULDBLOCK || ERROR_VALUE==EINPROGRESS)) {
-		fd_set		wsocket_set;
-		fd_set		esocket_set;
+		fd_set		socket_set;
 		struct		timeval tv;
 		socklen_t	optlen=sizeof(result);
 		tv.tv_sec = timeout;
 		tv.tv_usec = 0;
-		FD_ZERO(&wsocket_set);
-		FD_SET(sock,&wsocket_set);
-		FD_ZERO(&esocket_set);
-		FD_SET(sock,&esocket_set);
-		if(select(sock+1,NULL,&wsocket_set,&esocket_set,&tv)==1)
+		FD_ZERO(&socket_set);
+		FD_SET(sock,&socket_set);
+		if(select(sock+1,NULL,&socket_set,NULL,&tv)==1)
 			getsockopt(sock, SOL_SOCKET, SO_ERROR, (void*)&result, &optlen);
 	}
 	return result;
