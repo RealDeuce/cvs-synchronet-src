59250cbf4b948d0a3486e9596460a56f0fd85876
rswindell <rswindell>
2014-03-08 07:42:35 +0000

    Log message and display warning to user when protocol-specified user name does not match database and beginning new user sing-up.

--- sbbs3/scfg/scfgsys.c
+++ sbbs3/scfg/scfgsys.c
@@ -1,12 +1,12 @@
 /* scfgsys.c */
 
-/* $Id: scfgsys.c,v 1.39 2015/04/29 07:04:37 rswindell Exp $ */
+/* $Id: scfgsys.c,v 1.34 2014/02/16 06:28:52 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2015 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -840,7 +840,6 @@ while(1) {
 							"options available.\n"
 						;
 						j=0;
-						k=0;
                         while(1) {
                             i=0;
 							sprintf(opt[i++],"%-27.27s %-3.3s"
@@ -880,7 +879,7 @@ while(1) {
 								,"Auto Hang-up After Xfer"
 								,cfg.new_misc&AUTOHANG ? "Yes":"No");
 							opt[i][0]=0;
-							j=uifc.list(WIN_BOT|WIN_RHT,2,1,0,&j,&k
+							j=uifc.list(WIN_BOT|WIN_RHT,2,1,0,&j,0
 								,"Default Toggle Options",opt);
                             if(j==-1)
                                 break;
@@ -934,7 +933,6 @@ while(1) {
 							"user.\n"
 						;
 						j=0;
-						k=0;
                         while(1) {
                             i=0;
 							sprintf(opt[i++],"%-27.27s %-3.3s"
@@ -950,10 +948,10 @@ while(1) {
 								,"Company Name"
 								,cfg.uq&UQ_COMPANY ? "Yes":"No");
 							sprintf(opt[i++],"%-27.27s %-3.3s"
-								,"Chat Handle / Call Sign"
+								,"Multinode Chat Handle"
 								,cfg.uq&UQ_HANDLE ? "Yes":"No");
 							sprintf(opt[i++],"%-27.27s %-3.3s"
-								,"Force Unique Handle / Call Sign"
+								,"Force Unique Chat Handle"
 								,cfg.uq&UQ_DUPHAND ? "Yes":"No");
                             sprintf(opt[i++],"%-27.27s %-3.3s"
                                 ,"E-mail/NetMail Address"
@@ -988,11 +986,8 @@ while(1) {
 							sprintf(opt[i++],"%-27.27s %-3.3s"
 								,"Default Settings"
 								,cfg.uq&UQ_NODEF ? "No":"Yes");
-							sprintf(opt[i++],"%-27.27s %-3.3s"
-								,"Color Terminal"
-								,cfg.uq&UQ_COLORTERM ? "Yes":"No");
 							opt[i][0]=0;
-							j=uifc.list(WIN_BOT|WIN_RHT,2,1,0,&j,&k
+							j=uifc.list(WIN_BOT|WIN_RHT|WIN_SAV,2,1,0,&j,0
 								,"New User Questions",opt);
                             if(j==-1)
                                 break;
@@ -1049,9 +1044,6 @@ while(1) {
 								case 16:
 									cfg.uq^=UQ_NODEF;
                                     break;
-								case 17:
-									cfg.uq^=UQ_COLORTERM;
-									break;
 							} 
 						}
 					break; 
@@ -1383,9 +1375,6 @@ while(1) {
 				sprintf(opt[i++],"%-16.16s%s","Logout",cfg.logout_mod);
 				sprintf(opt[i++],"%-16.16s%s","New User",cfg.newuser_mod);
 				sprintf(opt[i++],"%-16.16s%s","Expired User",cfg.expire_mod);
-				sprintf(opt[i++],"%-16.16s%s","Read Mail",cfg.readmail_mod);
-				sprintf(opt[i++],"%-16.16s%s","Scan Msgs",cfg.scanposts_mod);
-				sprintf(opt[i++],"%-16.16s%s","Scan Subs",cfg.scansubs_mod);
 				opt[i][0]=0;
 				uifc.helpbuf=
 					"`Loadable Modules:`\n"
@@ -1393,21 +1382,15 @@ while(1) {
 					"Baja modules (`.bin` files) or JavaScript modules (`.js` files) can be\n"
 					"automatically loaded and executed during certain Terminal Server\n"
 					"operations. The name (root filename) of the module can be specified for\n"
-					"each of the available operations listed below:\n"
+					"each of the available operations listed here:\n"
 					"\n"
 					"`Login`        Required module for interactive terminal logins (answer)\n"
 					"`Logon`        Executed during terminal logon procedure\n"
-					"`Sync`         Executed when terminal nodes are periodically synchronized\n"
-					"`Logoff`       Executed during terminal logoff procedure (interactive)\n"
-					"`Logout`       Executed during terminal logout procedure (offline)\n"
-					"`New User`     Executed at end of new terminal user creation process\n"
-					"`Expired User` Executed during daily event when user expires (offline)\n"
-					"\n"
-					"Full module command-lines may be used for the operations listed below:\n"
-					"\n"
-					"`Read Mail`    Executed when a user reads email/netmail\n"
-					"`Scan Msgs`    Executed when a user reads or scans a message sub-board\n"
-					"`Scan Subs`    Executed when a user scans one or more sub-boards for msgs\n"
+					"`Sync          Executed when terminal nodes are periodically synchronized\n"
+					"`Logoff        Executed during terminal logoff procedure (interactive)\n"
+					"`Logout        Executed during terminal logout procedure (offline)\n"
+					"`New User      Executed at end of new terminal user creation process\n"
+					"`Expired User  Executed during daily event when user expires (offline)\n"
 					"\n"
 					"`Note:` JavaScript modules take precedence over Baja modules if both exist\n"
 					"in your `exec` or `mods` directories.\n"
@@ -1447,18 +1430,7 @@ while(1) {
 						uifc.input(WIN_MID|WIN_SAV,0,0,"Expired User Module"
 							,cfg.expire_mod,sizeof(cfg.expire_mod)-1,K_EDIT);
                         break;
-					case 7:
-						uifc.input(WIN_MID|WIN_SAV,0,0,"Read Mail Command"
-							,cfg.readmail_mod,sizeof(cfg.readmail_mod)-1,K_EDIT);
-                        break;
-					case 8:
-						uifc.input(WIN_MID|WIN_SAV,0,0,"Scan Msgs Command"
-							,cfg.scanposts_mod,sizeof(cfg.scanposts_mod)-1,K_EDIT);
-                        break;
-					case 9:
-						uifc.input(WIN_MID|WIN_SAV,0,0,"Scan Subs Command"
-							,cfg.scansubs_mod,sizeof(cfg.scansubs_mod)-1,K_EDIT);
-                        break;
+
 				} 
 			}
 			break;
@@ -1487,14 +1459,14 @@ while(1) {
 					"This menu allows you to change the security options for every possible\n"
 					"security level from 0 to 99. The available options for each level are:\n"
 					"\n"
-					"    Time Per Day           Maximum online time per day\n"
-					"    Time Per Call          Maximum online time per call\n"
-					"    Calls Per Day          Maximum number of calls per day\n"
-					"    Email Per Day          Maximum number of email per day\n"
-					"    Posts Per Day          Maximum number of posts per day\n"
-					"    Lines Per Message      Maximum number of lines per message\n"
+					"    Time Per Da            Maximum online time per day\n"
+					"    Time Per Cal           Maximum online time per call\n"
+					"    Calls Per Da           Maximum number of calls per day\n"
+					"    Email Per Da           Maximum number of email per day\n"
+					"    Posts Per Da           Maximum number of posts per day\n"
+					"    Lines Per Messag       Maximum number of lines per message\n"
 					"    Free Credits Per Day   Number of free credits per day\n"
-					"    Expire To              Level or validation set to Expire to\n"
+					"    Expire T               Level or validation set to Expire to\n"
 				;
 				i=uifc.list(WIN_RHT|WIN_ACT,0,3,0,&dflt,&bar
 					,"Level   T/D   T/C   C/D   E/D   P/D   L/M   F/D   "
