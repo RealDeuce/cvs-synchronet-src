4dc7aaa06f1ed729651eb0e86efe03f4d285c89f
rswindell <rswindell>
2014-03-13 08:22:57 +0000

    JS runtimes are no longer thread - cannot be shared among mutliple threads, so the runtime "pool" concept might need to go away. For now, we'll just change the RT_TYPE to RT_UNIQUE (no sharing of runtimes, using more memory) and double the size of the RT pool from 128 to 256. Without sharing, the pool may not make sense unless there is a significant time penalty when first creating RTs (in which case reusing RTs will be faster).

--- sbbs3/js_rtpool.c
+++ sbbs3/js_rtpool.c
@@ -1,4 +1,4 @@
-/* $Id: js_rtpool.c,v 1.26 2012/10/18 17:36:04 deuce Exp $ */
+/* $Id: js_rtpool.c,v 1.27 2014/03/13 08:22:57 rswindell Exp $ */
 
 #include "js_rtpool.h"
 #include <threadwrap.h>		/* Must be included after jsapi.h */
@@ -20,7 +20,7 @@
 #define RT_UNIQUE	0
 #define RT_SHARED	1
 #define RT_SINGLE	2
-#define RT_TYPE		RT_SHARED
+#define RT_TYPE		RT_UNIQUE
 
 struct jsrt_queue {
 	JSRuntime       *rt;
@@ -37,7 +37,7 @@ struct jsrt_queue {
 #if (RT_TYPE == RT_SINGLE)
 #define JSRT_QUEUE_SIZE		1
 #else
-#define JSRT_QUEUE_SIZE		128
+#define JSRT_QUEUE_SIZE		256
 #endif
 struct jsrt_queue jsrt_queue[JSRT_QUEUE_SIZE];
 static pthread_mutex_t		jsrt_mutex;
