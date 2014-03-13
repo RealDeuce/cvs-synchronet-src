12564c9ea39abad982b2262b6a375af7b8ceeaa2
rswindell <rswindell>
2014-03-13 08:15:05 +0000

    Fix the log output for Telnet environment options.

--- conio/sdlfuncs.c
+++ conio/sdlfuncs.c
@@ -2,6 +2,7 @@
 #include <stdio.h>	/* NULL */
 
 #include "gen_defs.h"
+#include "threadwrap.h"
 #include <SDL.h>
 #ifndef main
  #define USE_REAL_MAIN
@@ -32,6 +33,11 @@ static SDL_sem *sdl_main_sem;
 SDL_sem *sdl_exit_sem;
 
 int CIOLIB_main(int argc, char **argv, char **enviro);
+int XPDEV_main(int argc, char **argv, char **enviro)
+{
+	return CIOLIB_main(argc, argv, enviro);
+}
+
 
 int load_sdl_funcs(struct sdlfuncs *sdlf)
 {
@@ -74,6 +80,10 @@ int load_sdl_funcs(struct sdlfuncs *sdlf)
 		return(-1);
 	}
 	if((sdlf->SemWait=xp_dlsym(sdl_dll, SDL_SemWait))==NULL) {
+		xp_dlclose(sdl_dll);
+		return(-1);
+	}
+	if((sdlf->SemWaitTimeout=xp_dlsym(sdl_dll, SDL_SemWaitTimeout))==NULL) {
 		xp_dlclose(sdl_dll);
 		return(-1);
 	}
@@ -408,6 +418,7 @@ int SDL_main_env(int argc, char **argv, char **env)
 		}
 	}
 	if(sdl_video_initialized) {
+		SetThreadName("SDL Main");
 		atexit(QuitWrap);
 		sdl_main_sem=sdl.SDL_CreateSemaphore(0);
 		sdl_exit_sem=sdl.SDL_CreateSemaphore(0);
