2bc345c9755215b9f432f93839df88f1c326d111
deuce <deuce>
2014-04-03 06:54:13 +0000

    We abuse CFLAGS by passing it to ${CXX}.  Change the implicit rules instead.

--- sbbs3/sbbsecho.c
+++ sbbs3/sbbsecho.c
@@ -2,7 +2,7 @@
 
 /* Synchronet FidoNet EchoMail Scanning/Tossing and NetMail Tossing Utility */
 
-/* $Id: sbbsecho.c,v 1.234 2014/01/20 04:11:00 rswindell Exp $ */
+/* $Id: sbbsecho.c,v 1.237 2014/04/03 06:48:07 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -550,6 +550,17 @@ void file_to_netmail(FILE *infile,char *title,faddr_t addr,char *to)
 	}
 	free(buf);
 }
+
+/* Returns TRUE if area is linked with specified node address */
+BOOL area_is_linked(unsigned area_num, faddr_t* addr)
+{
+	unsigned i;
+	for(i=0;i<cfg.area[area_num].uplinks;i++)
+		if(!memcmp(addr,&cfg.area[area_num].uplink[i],sizeof(faddr_t)))
+			return TRUE;
+	return FALSE;
+}
+
 /******************************************************************************
  This function sends a notify list to applicable nodes, this list includes the
  settings configured for the node, as well as a list of areas the node is
@@ -594,65 +605,71 @@ void notify_list(void)
 			sprintf(str,"%s\r\n",cfg.area[i].name);
 			if(str[0]=='*')
 				continue;
-			for(j=0;j<cfg.area[i].uplinks;j++)
-				if(!memcmp(&cfg.nodecfg[k].faddr,&cfg.area[i].uplink[j]
-					,sizeof(faddr_t)))
-					break;
-			if(j<cfg.area[i].uplinks)
-				fprintf(tmpf,"%s",str); }
+			if(area_is_linked(i,&cfg.nodecfg[k].faddr))
+				fprintf(tmpf,"%s",str); 
+		}
 
 		if(ftell(tmpf))
 			file_to_netmail(tmpf,"SBBSecho Notify List",cfg.nodecfg[k].faddr, /* To: */NULL);
 		fclose(tmpf); }
 }
+
 /******************************************************************************
  This function creates a netmail to addr showing a list of available areas (0),
  a list of connected areas (1), or a list of removed areas (2).
 ******************************************************************************/
-void netmail_arealist(int type, faddr_t addr, char* to)
+enum arealist_type {
+	 AREALIST_ALL			// %LIST
+	,AREALIST_CONNECTED		// %QUERY
+	,AREALIST_UNLINKED		// %UNLINKED
+};
+void netmail_arealist(enum arealist_type type, faddr_t addr, char* to)
 {
 	FILE *stream,*tmpf;
 	char str[256],title[128],match,*p,*tp;
 	int i,j,k,x,y;
+	unsigned areas=0;
 
-	if(!type)
+	if(type == AREALIST_ALL)
 		strcpy(title,"List of Available Areas");
-	else if(type==1)
+	else if(type == AREALIST_CONNECTED)
 		strcpy(title,"List of Connected Areas");
 	else
 		strcpy(title,"List of Unlinked Areas");
 
 	if((tmpf=tmpfile())==NULL) {
 		lprintf(LOG_ERR,"ERROR line %d couldn't open tmpfile",__LINE__);
-		return; }
+		return; 
+	}
 
-	if(type==1 || !(misc&ELIST_ONLY)) {
+	if(type == AREALIST_CONNECTED || !(misc&ELIST_ONLY)) {
+		/* Include relevant areas from the area file (e.g. areas.bbs): */
 		for(i=0;i<cfg.areas;i++) {
-			if(type) {
-				for(j=0;j<cfg.area[i].uplinks;j++)
-					if(!memcmp(&addr,&cfg.area[i].uplink[j],sizeof(faddr_t)))
-						break;
-				if((type==1 && j<cfg.area[i].uplinks) ||
-					(type==2 && j==cfg.area[i].uplinks))
-						fprintf(tmpf,"%s\r\n",cfg.area[i].name); }
-			else
-				fprintf(tmpf,"%s\r\n",cfg.area[i].name); } }
+			if(type == AREALIST_CONNECTED && area_is_linked(i,&addr))
+				continue;
+			if(type == AREALIST_UNLINKED && !area_is_linked(i,&addr))
+				continue;
+			fprintf(tmpf,"%s\r\n",cfg.area[i].name); 
+			areas++;
+		} 
+	}
 
-	if(!type) {
+	if(type == AREALIST_ALL || (type == AREALIST_UNLINKED && (misc&ELIST_ONLY))) {
 		i=matchnode(addr,0);
 		if(i<cfg.nodecfgs) {
 			for(j=0;j<cfg.listcfgs;j++) {
 				match=0;
 				for(k=0;k<cfg.listcfg[j].numflags;k++) {
 					if(match) break;
-					for(x=0;x<cfg.nodecfg[i].numflags;x++)
+					for(x=0;x<cfg.nodecfg[i].numflags;x++) {
 						if(!stricmp(cfg.listcfg[j].flag[k].flag
 							,cfg.nodecfg[i].flag[x].flag)) {
 							if((stream=fopen(cfg.listcfg[j].listpath,"r"))==NULL) {
 								lprintf(LOG_ERR,"ERROR %u (%s) line %d opening %s"
 									,errno,strerror(errno),__LINE__,cfg.listcfg[j].listpath);
 								match=1;
-								break; }
+								break; 
+							}
 							while(!feof(stream)) {
 								if(!fgets(str,sizeof(str),stream))
 									break;
@@ -663,24 +680,32 @@ void netmail_arealist(int type, faddr_t addr, char* to)
 								tp=p;
 								FIND_WHITESPACE(tp);
 								*tp=0;
-								if(!(misc&ELIST_ONLY)) {
-									for(y=0;y<cfg.areas;y++)
-										if(!stricmp(cfg.area[y].name,p))
-											break;
-									if(y==cfg.areas)
-										fprintf(tmpf,"%s\r\n",p); }
-								else
-									fprintf(tmpf,"%s\r\n",p); }
+								for(y=0;y<cfg.areas;y++)
+									if(!stricmp(cfg.area[y].name,p))
+										break;
+								if(y>=cfg.areas || !area_is_linked(y,&addr)) {
+									fprintf(tmpf,"%s\r\n",p); 
+									areas++;
+								}
+							}
 							fclose(stream);
 							match=1;
-							break; } } } } }
+							break; 
+						}
+					}
+				} 
+			} 
+		} 
+	}
 
 	if(!ftell(tmpf))
 		create_netmail(to,title,"None.",addr,FALSE);
 	else
 		file_to_netmail(tmpf,title,addr,to);
+	lprintf(LOG_INFO,"Created AreaFix response netmail with %s (%u bytes, %u areas)", title, ftell(tmpf), areas);
 	fclose(tmpf);
 }
+
 /******************************************************************************
  Imitation of Borland's tempnam function because Watcom doesn't have it
 ******************************************************************************/
@@ -807,11 +832,7 @@ void alter_areas(area_t* add_area, area_t* del_area, faddr_t addr, char* to)
 			if(i<del_area->tags) {
 				for(i=0;i<cfg.areas;i++) {
 					if(!stricmp(field2,cfg.area[i].name)) {
-						for(j=0;j<cfg.area[i].uplinks;j++)
-							if(!memcmp(&cfg.area[i].uplink[j],&addr
-								,sizeof(faddr_t)))
-								break;
-						if(j==cfg.area[i].uplinks) {
+						if(!area_is_linked(i,&addr)) {
 							fprintf(afileout,"%s\n",fields);
 							/* bugfix here Mar-25-2004 (wasn't breaking for "-ALL") */
 							if(stricmp(del_area->tag[0],"-ALL"))
@@ -864,11 +885,7 @@ void alter_areas(area_t* add_area, area_t* del_area, faddr_t addr, char* to)
 					add_area->tag[i][0]=0;  /* So we can check other lists */
 				for(i=0;i<cfg.areas;i++) {
 					if(!stricmp(field2,cfg.area[i].name)) {
-						for(j=0;j<cfg.area[i].uplinks;j++)
-							if(!memcmp(&cfg.area[i].uplink[j],&addr
-								,sizeof(faddr_t)))
-								break;
-						if(j<cfg.area[i].uplinks) {
+						if(area_is_linked(i,&addr)) {
 							fprintf(afileout,"%s\n",fields);
 							fprintf(nmfile,"%s already connected.\r\n",field2);
 							break; }
@@ -1179,17 +1196,17 @@ void command(char* instr, faddr_t addr, char* to)
 	}
 
 	if((p=strstr(instr,"LIST"))!=NULL) {
-		netmail_arealist(0,addr,to);
+		netmail_arealist(AREALIST_ALL,addr,to);
 		return; 
 	}
 
 	if((p=strstr(instr,"QUERY"))!=NULL) {
-		netmail_arealist(1,addr,to);
+		netmail_arealist(AREALIST_CONNECTED,addr,to);
 		return; 
 	}
 
 	if((p=strstr(instr,"UNLINKED"))!=NULL) {
-		netmail_arealist(2,addr,to);
+		netmail_arealist(AREALIST_UNLINKED,addr,to);
 		return; 
 	}
 
@@ -3507,6 +3524,8 @@ int import_netmail(char *path,fmsghdr_t hdr, FILE *fidomsg)
 		addr.net=hdr.orignet;
 		addr.node=hdr.orignode;
 		addr.point=hdr.origpoint;
+		if(cfg.log&LOG_AREAFIX)
+			logprintf(info);
 		p=process_areafix(addr,fmsgbuf,/* Password: */hdr.subj, /* To: */hdr.from);
 		if(p && cfg.notify) {
 			SAFECOPY(hdr.to,scfg.sys_op);
@@ -3520,8 +3539,6 @@ int import_netmail(char *path,fmsghdr_t hdr, FILE *fidomsg)
 		}
 		if(fmsgbuf)
 			free(fmsgbuf);
-		if(cfg.log&LOG_AREAFIX)
-			logprintf(info);
 		return(-2); 
 	}
 
@@ -3718,10 +3735,7 @@ void export_echomail(char *sub_code,faddr_t addr)
 		if(i<0 || i>=scfg.total_subs)	/* Don't scan pass-through areas */
 			continue;
 		if(addr.zone) { 		/* Skip areas not meant for this address */
-			for(k=0;k<cfg.area[area].uplinks;k++)
-				if(!memcmp(&cfg.area[area].uplink[k],&addr,sizeof(faddr_t)))
-					break;
-			if(k==cfg.area[area].uplinks)
+			if(!area_is_linked(area,&addr))
 				continue; 
 		}
 		if(sub_code[0] && stricmp(sub_code,scfg.sub[i]->code))
@@ -4105,7 +4119,7 @@ int main(int argc, char **argv)
 	memset(&msg_path,0,sizeof(addrlist_t));
 	memset(&fakearea,0,sizeof(areasbbs_t));
 
-	sscanf("$Revision: 1.234 $", "%*s %s", revision);
+	sscanf("$Revision: 1.237 $", "%*s %s", revision);
 
 	DESCRIBE_COMPILER(compiler);
 
@@ -4669,10 +4683,7 @@ int main(int argc, char **argv)
 				}
 
 				if((misc&SECURE) && cfg.area[i].sub!=INVALID_SUB) {
-					for(j=0;j<cfg.area[i].uplinks;j++)
-						if(!memcmp(&cfg.area[i].uplink[j],&pkt_faddr,sizeof(faddr_t)))
-							break;
-					if(j==cfg.area[i].uplinks) {
+					if(!area_is_linked(i,&pkt_faddr)) {
 						if(cfg.log&LOG_SECURE)
 							logprintf("%s: Security violation - %s not in AREAS.BBS"
 								,areatagstr,smb_faddrtoa(&pkt_faddr,NULL));
