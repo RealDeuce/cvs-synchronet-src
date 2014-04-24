8a457674b0f8903582841067c6af41848b7562fc
deuce <deuce>
2014-04-24 07:56:29 +0000

    Finish the memory leak fix for xp_play_sample_thread() to account for the case where the handle_type becomes invalid somehow (exceptionall unlikely)

--- sbbs3/js_system.c
+++ sbbs3/js_system.c
@@ -2,13 +2,13 @@
 
 /* Synchronet JavaScript "system" Object */
 
-/* $Id: js_system.c,v 1.157 2014/08/19 01:14:24 rswindell Exp $ */
+/* $Id: js_system.c,v 1.156 2014/04/06 06:20:15 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2012 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -431,7 +431,7 @@ static char* sys_prop_desc[] = {
 	 "BBS name"
 	,"operator name"
 	,"system QWK-ID (for QWK packets)"
-	,"settings bitfield (see <tt>SYS_*</tt> in <tt>sbbsdefs.js</tt> for bit definitions)"
+	,"settings bitfield (see <tt>SS_*</tt> in <tt>sbbsdefs.js</tt> for bit definitions)"
 	,"PostLink name"
 	,"PostLink system number"
 	,"Internet address (host or domain name)"
