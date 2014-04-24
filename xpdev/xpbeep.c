10c4e3951001d9979263a382be526608519b0b61
deuce <deuce>
2014-04-24 06:24:37 +0000

    Coverity: Fix unlikely memory leak in unused (sbbs4) dataParseList() function.

--- xpdev/xpbeep.c
+++ xpdev/xpbeep.c
@@ -1,4 +1,4 @@
-/* $Id: xpbeep.c,v 1.90 2014/04/24 07:56:29 deuce Exp $ */
+/* $Id: xpbeep.c,v 1.88 2014/04/24 05:54:12 deuce Exp $ */
 
 /* TODO: USE PORTAUDIO! */
 
@@ -669,7 +669,7 @@ void DLLCALL xp_play_sample_thread(void *data)
 	BOOL			must_close=FALSE;
 	BOOL			posted_last=TRUE;
 	BOOL			waited=FALSE;
-	unsigned char	*sample=NULL;
+	unsigned char	*sample;
 	size_t			this_sample_size;
 
 #ifdef AFMT_U8
@@ -699,7 +699,6 @@ void DLLCALL xp_play_sample_thread(void *data)
 			}
 		}
 		this_sample_size=sample_size;
-		FREE_AND_NULL(sample);
 		sample=(unsigned char *)malloc(sample_size);
 		if(sample==NULL) {
 				sem_post(&sample_complete_sem);
@@ -713,12 +712,11 @@ void DLLCALL xp_play_sample_thread(void *data)
 		if(handle_type==SOUND_DEVICE_PORTAUDIO) {
 			if(pa_api->ver >= 1899) {
 				pa_api->write(portaudio_stream, sample, this_sample_size);
-				FREE_AND_NULL(sample);
+				free(sample);
 			}
 			else {
 				xptone_complete();
 				pawave=sample;
-				sample=NULL;
 				portaudio_buf_pos=0;
 				portaudio_buf_len=this_sample_size;
 				pa_api->start(portaudio_stream);
@@ -730,7 +728,6 @@ void DLLCALL xp_play_sample_thread(void *data)
 		if(handle_type==SOUND_DEVICE_SDL) {
 			sdl.LockAudio();
 			swave=sample;
-			sample=NULL;
 			sdl_audio_buf_pos=0;
 			sdl_audio_buf_len=this_sample_size;
 			sdl.UnlockAudio();
@@ -746,7 +743,6 @@ void DLLCALL xp_play_sample_thread(void *data)
 			}
 			FREE_AND_NULL(wh[curr_wh].lpData);
 			wh[curr_wh].lpData=sample;
-			sample=NULL;
 			wh[curr_wh].dwBufferLength=this_sample_size;
 			if(waveOutPrepareHeader(waveOut, &wh[curr_wh], sizeof(wh[curr_wh]))==MMSYSERR_NOERROR) {
 				if(waveOutWrite(waveOut, &wh[curr_wh], sizeof(wh[curr_wh]))==MMSYSERR_NOERROR) {
@@ -775,7 +771,7 @@ void DLLCALL xp_play_sample_thread(void *data)
 				written += ret;
 			}
 #ifndef AFMT_U8
-			FREE_AND_NULL(sample);
+			free(sample);
 #endif
 		}
 	#endif
@@ -788,7 +784,7 @@ void DLLCALL xp_play_sample_thread(void *data)
 				if(i>=0)
 					wr+=i;
 			}
-			FREE_AND_NULL(sample);
+			free(sample);
 		}
 	#endif
 		sem_post(&sample_complete_sem);
@@ -802,17 +798,6 @@ void DLLCALL xp_play_sample_thread(void *data)
 	}
 
 error_return:
-#ifdef _WIN32
-	if(handle_type==SOUND_DEVICE_WIN32) {
-		if(wh[curr_wh].dwFlags & WHDR_PREPARED) {
-			while(waveOutUnprepareHeader(waveOut, &wh[curr_wh], sizeof(wh[curr_wh]))==WAVERR_STILLPLAYING)
-				SLEEP(1);
-		}
-		FREE_AND_NULL(wh[curr_wh].lpData);
-	}
-#endif
-
-	FREE_AND_NULL(sample);
 	xptone_close();
 	if(!posted_last)
 		sem_post(&sample_complete_sem);
