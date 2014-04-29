934f25d174bdb55d0af59c8633e3ea2b5a57bd81
deuce <deuce>
2014-04-29 09:16:31 +0000

    Ensure alloca() is defined for Win32 by genwrap.h as well.

--- sbbs3/ntsvcs.c
+++ sbbs3/ntsvcs.c
@@ -2,13 +2,13 @@
 
 /* Synchronet BBS as a set of Windows NT Services */
 
-/* $Id: ntsvcs.c,v 1.42 2013/02/11 22:52:13 deuce Exp $ */
+/* $Id: ntsvcs.c,v 1.44 2014/01/09 09:51:59 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -721,7 +721,7 @@ static DWORD get_service_info(SC_HANDLE hSCManager, char* name, DWORD* state)
 		return(-1);
 	}
     CloseServiceHandle(hService);
-	ret=service_config->dwStartType
+	ret=service_config->dwStartType;
 	free(service_config);
 
 	return ret;
@@ -916,7 +916,7 @@ static void control_service(SC_HANDLE hSCManager, char* name, char* disp_name, D
 		return;
 
     /* try to stop the service */
-    if(!ControlService( hService, SERVICE_CONTROL_STOP, &status)) {
+    if(!ControlService( hService, ctrl, &status)) {
 		if((err=GetLastError())==ERROR_SERVICE_NOT_ACTIVE)
 			printf("Not active\n");
 		else
