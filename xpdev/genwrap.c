f627c06a291d0cf8bcc48936aaf2039e21139c07
deuce <deuce>
2014-05-07 03:36:36 +0000

    Since the beginning of time, a "PASSWORD" areafix command would remove all AREAFIX lines except for the one that was updated.
    This is apparently "bad".  Stop doing this bad thing (We'll still delete
    AREAFIX lines without an address though).

--- xpdev/genwrap.c
+++ xpdev/genwrap.c
@@ -2,7 +2,7 @@
 
 /* General cross-platform development wrappers */
 
-/* $Id: genwrap.c,v 1.91 2014/02/05 10:17:28 deuce Exp $ */
+/* $Id: genwrap.c,v 1.96 2014/04/28 05:19:11 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -48,7 +48,10 @@
 	#include <sys/ioctl.h>		/* ioctl() */
 	#include <sys/utsname.h>	/* uname() */
 	#include <signal.h>
-#endif	/* __unix__ */
+#elif defined(_WIN32)
+	#include <windows.h>
+	#include <lm.h>		/* NetWkstaGetInfo() */
+#endif
 
 #include "genwrap.h"	/* Verify prototypes */
 #include "xpendian.h"	/* BYTE_SWAP */
@@ -278,15 +281,17 @@ char* DLLCALL strtok_r(char *str, const char *delim, char **last)
 /****************************************************************************/
 void DLLCALL xp_randomize(void)
 {
+#if !(defined(HAS_SRANDOMDEV_FUNC) && defined(HAS_RANDOM_FUNC))
 	unsigned seed=~0;
 #if defined(HAS_DEV_URANDOM) && defined(URANDOM_DEV)
 	int		rf;
+#endif
 #endif
 
 #if defined(HAS_SRANDOMDEV_FUNC) && defined(HAS_RANDOM_FUNC)
 	srandomdev();
 	return;
-#endif
+#else
 
 #if defined(HAS_DEV_URANDOM) && defined(URANDOM_DEV)
 	if((rf=open(URANDOM_DEV, O_RDONLY))!=-1) {
@@ -312,6 +317,7 @@ void DLLCALL xp_randomize(void)
  	srandom(seed);
 #else
  	srand(seed);
+#endif
 #endif
 }
 
@@ -404,10 +410,23 @@ char* DLLCALL os_version(char *str)
 			break;
 	}
 
-	sprintf(str,"Windows %sVersion %u.%u (Build %u) %s"
+	/* Work-around Microsoft Windows 8.1 stupidity where GetVersionEx() lies about the current OS version */
+	if(winver.dwMajorVersion == 6 && winver.dwMinorVersion == 2) {
+		WKSTA_INFO_100* wksta_info;
+		if(NetWkstaGetInfo(NULL, 100, (LPBYTE*)&wksta_info) == NERR_Success) {
+			winver.dwMajorVersion = wksta_info->wki100_ver_major;
+			winver.dwMinorVersion = wksta_info->wki100_ver_minor;
+			winver.dwBuildNumber = 0;
+		}
+	}
+
+	sprintf(str,"Windows %sVersion %u.%u"
 			,winflavor
-			,winver.dwMajorVersion, winver.dwMinorVersion
-			,winver.dwBuildNumber,winver.szCSDVersion);
+			,winver.dwMajorVersion, winver.dwMinorVersion);
+	if(winver.dwBuildNumber)
+		sprintf(str+strlen(str), " (Build %u)", winver.dwBuildNumber);
+	if(winver.szCSDVersion[0])
+		sprintf(str+strlen(str), " %s", winver.szCSDVersion);
 
 #elif defined(__unix__)
 
@@ -457,7 +476,7 @@ clock_t DLLCALL msclock(void)
 	struct timeval tv;
 	if(gettimeofday(&tv,NULL)==1)
 		return(-1);
-	usecs=tv.tv_sec*1000000+tv.tv_usec;
+	usecs=((long long int)tv.tv_sec)*((long long int)1000000)+tv.tv_usec;
 	return((clock_t)(usecs/(1000000/MSCLOCKS_PER_SEC)));
 }
 #endif
