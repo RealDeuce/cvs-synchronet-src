c3cd345f92ba0eaeaa204034253c98a9e4a325ff
deuce <deuce>
2014-10-14 04:58:58 +0000

    Include CRYPT_CFLAGS... needed for sbbs.h usage now.

--- sbbs3/js_global.c
+++ sbbs3/js_global.c
@@ -2,7 +2,7 @@
 
 /* Synchronet JavaScript "global" object properties/methods for all servers */
 
-/* $Id: js_global.c,v 1.332 2013/05/10 18:10:31 deuce Exp $ */
+/* $Id: js_global.c,v 1.337 2014/05/04 07:09:19 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -239,33 +239,6 @@ static jsval* js_CopyValue(JSContext* cx, jsrefcount *cx_rc, jsval val, JSContex
 	*cx_rc=JS_SUSPENDREQUEST(cx);
 
 	return rval;
-}
-
-JSBool BGContextCallback(JSContext *cx, uintN contextOp)
-{
-	JSObject	*gl=JS_GetGlobalObject(cx);
-	global_private_t*	p;
-
-	if(!gl)
-		return JS_TRUE;
-
-	if((p=(global_private_t*)JS_GetPrivate(cx,gl))==NULL)
-		return(JS_TRUE);
-
-	switch(contextOp) {
-		case JSCONTEXT_DESTROY:
-			while(p->bg_count) {
-				while(p->bg_count && sem_trywait(&p->bg_sem)==0)
-					p->bg_count--;
-				if(!p->bg_count)
-					break;
-
-				if(sem_wait(&p->bg_sem)==0)
-					p->bg_count--;
-			}
-			break;
-	}
-	return JS_TRUE;
 }
 
 static JSBool
@@ -613,7 +586,6 @@ js_load(JSContext *cx, uintN argc, jsval *arglist)
 		success = _beginthread(background_thread,0,bg)!=-1;
 		JS_RESUMEREQUEST(cx, rc);
 		if(success) {
-			JS_SetContextCallback(JS_GetRuntime(cx), BGContextCallback);
 			p->bg_count++;
 		}
 
@@ -2468,13 +2440,16 @@ js_internal_charfunc(JSContext *cx, uintN argc, jsval *arglist, char *(*func)(ch
 	if(str==NULL) 
 		return(JS_TRUE);
 	if(extra_bytes) {
-		rastr=realloc(str, strlen+extra_bytes);
-		if(rastr==NULL)
+		rastr=realloc(str, strlen+extra_bytes+1 /* for terminator */);
+		if(rastr==NULL) {
+			free(str);
 			return JS_TRUE;
+		}
+		str=rastr;
 	}
 
 	js_str = JS_NewStringCopyZ(cx, func(str));
-	free(str);	/* MSVC detected heap corruption here! */
+	free(str);
 	if(js_str==NULL)
 		return(JS_FALSE);
 
@@ -4125,8 +4100,14 @@ static void js_global_finalize(JSContext *cx, JSObject *obj)
 
 	p=(global_private_t*)JS_GetPrivate(cx,obj);
 
-	if(p!=NULL)
+	if(p!=NULL) {
+		while(p->bg_count) { 
+			if(sem_wait(&p->bg_sem)==0) 
+				p->bg_count--; 
+		}
+		sem_destroy(&p->bg_sem);
 		free(p);
+	}
 
 	p=NULL;
 	JS_SetPrivate(cx,obj,p);
@@ -4183,6 +4164,10 @@ static JSClass js_global_class = {
 BOOL DLLCALL js_CreateGlobalObject(JSContext* cx, scfg_t* cfg, jsSyncMethodSpec* methods, js_startup_t* startup, JSObject**glob)
 {
 	global_private_t*	p;
+	JSRuntime*			rt;
+
+	if((rt=JS_GetRuntime(cx)) != NULL)
+		JS_SetRuntimePrivate(rt, cfg);
 
 	if((p = (global_private_t*)malloc(sizeof(global_private_t)))==NULL)
 		return(FALSE);
