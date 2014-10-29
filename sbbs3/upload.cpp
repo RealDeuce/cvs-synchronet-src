415036ea1580d89f782578cde660f92902be5ae4
rswindell <rswindell>
2014-10-29 06:55:12 +0000

    Bug-fix: when SSH or RLogin supplied username is not a valid user, the new user signup process would be started without the current client IP address being added to the 'failed login attempt' list. This means that brute force login attempts using SSH or RLogin would usually not be subject to the loginAttempt delays and logging/filtering settings (in sbbs.ini), since the usernames attempted (e.g. root, admin) are usually not valid usernames.
    More:
    - Log failed password attempts before calling badlogin() -which can delay.
    - Stop RLogin and SSH password prompt loop immediately if disconnected.
    - Log RLogin and SSH passwords used for invalid usernames (when password
      logging is enabled in SCFG).
    - Log attempted usernames in quotes (so prepenned or trailing whitespace is more
      obvious)

--- sbbs3/upload.cpp
+++ sbbs3/upload.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet file upload-related routines */
 
-/* $Id: upload.cpp,v 1.60 2015/08/28 02:04:33 rswindell Exp $ */
+/* $Id: upload.cpp,v 1.58 2011/10/19 07:08:32 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -470,7 +470,7 @@ bool sbbs_t::upload(uint dirnum)
 	} else {
 		xfer_prot_menu(XFER_UPLOAD);
 		SYNC;
-		sprintf(keys,"%c",text[YNQP][2]);
+		strcpy(keys,"Q");
 		if(dirnum==cfg.user_dir || !cfg.max_batup)  /* no batch user to user xfers */
 			mnemonics(text[ProtocolOrQuit]);
 		else {
@@ -483,7 +483,7 @@ bool sbbs_t::upload(uint dirnum)
 				strcat(keys,tmp); 
 			}
 		ch=(char)getkeys(keys,0);
-		if(ch==text[YNQP][2] || (sys_status&SS_ABORT))
+		if(ch=='Q')
 			return(false);
 		if(ch=='B') {
 			if(batup_total>=cfg.max_batup)
@@ -590,7 +590,6 @@ bool sbbs_t::bulkupload(uint dirnum)
 		padfname(getfname(spath),str);
 
 		if(findfile(&cfg,f.dir,str)==0) {
-			f.misc=0;
 			strcpy(f.name,str);
 			f.cdt=(long)flength(spath);
 			bprintf(text[BulkUploadDescPrompt],f.name,f.cdt/1024);
@@ -621,14 +620,14 @@ bool sbbs_t::recvfile(char *fname, char prot)
 	else {
 		xfer_prot_menu(XFER_UPLOAD);
 		mnemonics(text[ProtocolOrQuit]);
-		sprintf(keys,"%c",text[YNQP][2]);
+		strcpy(keys,"Q");
 		for(i=0;i<cfg.total_prots;i++)
 			if(cfg.prot[i]->ulcmd[0] && chk_ar(cfg.prot[i]->ar,&useron,&client))
 				sprintf(keys+strlen(keys),"%c",cfg.prot[i]->mnemonic);
 
 		ch=(char)getkeys(keys,0);
 
-		if(ch==text[YNQP][2] || sys_status&SS_ABORT)
+		if(ch=='Q' || sys_status&SS_ABORT)
 			return(false); 
 	}
 	for(i=0;i<cfg.total_prots;i++)
