da2c95f60b56f3bf6125948486fb590169a8eaa0
rswindell <rswindell>
2014-11-20 05:13:38 +0000

    Move FTP, Mail, and Web server default numeric startup values (e.g. max clients, timeouts, etc.) into the various *srvr.h files and use them instead of hard-coded constants sprinkled through-out the .c files. If the web server "max_clients" key isn't specified in the sbbs.ini file, use 0 (unlimited) for the max_clients value.

--- sbbs3/userdat.c
+++ sbbs3/userdat.c
@@ -2,13 +2,13 @@
 
 /* Synchronet user data-related routines (exported) */
 
-/* $Id: userdat.c,v 1.159 2015/11/24 16:28:01 rswindell Exp $ */
+/* $Id: userdat.c,v 1.152 2014/10/30 08:39:23 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -263,7 +263,6 @@ int DLLCALL getuserdat(scfg_t* cfg, user_t *user)
 	getrec(userdat,U_PHONE,LEN_PHONE,user->phone);
 	getrec(userdat,U_BIRTH,LEN_BIRTH,user->birth);
 	getrec(userdat,U_MODEM,LEN_MODEM,user->modem);
-	getrec(userdat,U_IPADDR,LEN_IPADDR,user->ipaddr);
 	getrec(userdat,U_LASTON,8,str); user->laston=ahtoul(str);
 	getrec(userdat,U_FIRSTON,8,str); user->firston=ahtoul(str);
 	getrec(userdat,U_EXPIRE,8,str); user->expire=ahtoul(str);
@@ -322,7 +321,7 @@ int DLLCALL getuserdat(scfg_t* cfg, user_t *user)
 
 	getrec(userdat,U_XEDIT,8,str);
 	for(i=0;i<cfg->total_xedits;i++)
-		if(!stricmp(str,cfg->xedit[i]->code))
+		if(!stricmp(str,cfg->xedit[i]->code) && chk_ar(cfg,cfg->xedit[i]->ar,user,/* client: */NULL))
 			break;
 	user->xedit=i+1;
 	if(user->xedit>cfg->total_xedits)
@@ -434,7 +433,6 @@ int DLLCALL putuserdat(scfg_t* cfg, user_t* user)
 	putrec(userdat,U_PHONE,LEN_PHONE,user->phone);
 	putrec(userdat,U_BIRTH,LEN_BIRTH,user->birth);
 	putrec(userdat,U_MODEM,LEN_MODEM,user->modem);
-	putrec(userdat,U_IPADDR,LEN_IPADDR,user->ipaddr);
 	putrec(userdat,U_LASTON,8,ultoa((ulong)user->laston,str,16));
 	putrec(userdat,U_FIRSTON,8,ultoa((ulong)user->firston,str,16));
 	putrec(userdat,U_EXPIRE,8,ultoa((ulong)user->expire,str,16));
@@ -483,7 +481,7 @@ int DLLCALL putuserdat(scfg_t* cfg, user_t* user)
 
 	putrec(userdat,U_XFER_CMD+LEN_XFER_CMD,2,crlf);
 
-	putrec(userdat,U_IPADDR+LEN_IPADDR,2,crlf);
+	putrec(userdat,U_MAIL_CMD+LEN_MAIL_CMD,2,crlf);
 
 	putrec(userdat,U_FREECDT,10,ultoa(user->freecdt,str,10));
 
@@ -1837,7 +1835,7 @@ static BOOL ar_exp(scfg_t* cfg, uchar **ptrptr, user_t* user, client_t* client)
 				if(client!=NULL)
 					p=client->addr;
 				else if(user!=NULL)
-					p=user->ipaddr;
+					p=user->note;
 				else
 					p=NULL;
 				if(!findstr_in_string(p,(char*)*ptrptr))
@@ -2197,15 +2195,12 @@ void DLLCALL resetdailyuserdat(scfg_t* cfg, user_t* user, BOOL write)
 }
 
 /****************************************************************************/
-/* Get dotted-equivalent email address for user 'name'.						*/ 
-/* 'addr' is the target buffer for the full address.						*/
-/* Pass cfg=NULL to NOT have "@address" portion appended.					*/
 /****************************************************************************/
 char* DLLCALL usermailaddr(scfg_t* cfg, char* addr, const char* name)
 {
 	int i;
 
-	if(addr==NULL || name==NULL)
+	if(!VALID_CFG(cfg) || addr==NULL || name==NULL)
 		return(NULL);
 
 	if(strchr(name,'@')!=NULL) { /* Avoid double-@ */
@@ -2225,10 +2220,8 @@ char* DLLCALL usermailaddr(scfg_t* cfg, char* addr, const char* name)
 				addr[i]='.';
 		strlwr(addr);
 	}
-	if(VALID_CFG(cfg)) {
-		strcat(addr,"@");
-		strcat(addr,cfg->sys_inetaddr);
-	}
+	strcat(addr,"@");
+	strcat(addr,cfg->sys_inetaddr);
 	return(addr);
 }
 
@@ -2414,7 +2407,6 @@ int DLLCALL user_rec_len(int offset)
 		case U_PHONE:		return(LEN_PHONE);
 		case U_BIRTH:		return(LEN_BIRTH);
 		case U_MODEM:		return(LEN_MODEM);
-		case U_IPADDR:		return(LEN_IPADDR);
 
 		/* Internal codes (16 chars) */
 		case U_CURSUB:
@@ -2552,25 +2544,21 @@ BOOL DLLCALL can_user_post(scfg_t* cfg, uint subnum, user_t* user, client_t* cli
 /* 'reason' is an (optional) pointer to a text.dat item number				*/
 /* usernumber==0 for netmail												*/
 /****************************************************************************/
-BOOL DLLCALL can_user_send_mail(scfg_t* cfg, enum smb_net_type net_type, uint usernumber, user_t* user, uint* reason)
+BOOL DLLCALL can_user_send_mail(scfg_t* cfg, uint usernumber, user_t* user, uint* reason)
 {
 	if(reason!=NULL)
 		*reason=R_Email;
 	if(user==NULL || user->number==0)
 		return FALSE;
-	if(net_type==NET_NONE && usernumber>1 && user->rest&FLAG('E'))			/* local mail restriction? */
+	if(usernumber>1 && user->rest&FLAG('E'))			/* local mail restriction? */
 		return FALSE;
 	if(reason!=NULL)
 		*reason=NoNetMailAllowed;
-	if(net_type!=NET_NONE && user->rest&FLAG('M'))							/* netmail restriction */
-		return FALSE;
-	if(net_type==NET_FIDO && !(cfg->netmail_misc&NMAIL_ALLOW))				/* Fido netmail globally disallowed */
-		return FALSE;
-	if(net_type==NET_INTERNET && !(cfg->inetmail_misc&NMAIL_ALLOW))			/* Internet mail globally disallowed */
+	if(usernumber==0 && user->rest&FLAG('M'))			/* netmail restriction */
 		return FALSE;
 	if(reason!=NULL)
 		*reason=R_Feedback;
-	if(net_type==NET_NONE && usernumber==1 && user->rest&FLAG('S'))			/* feedback restriction? */
+	if(usernumber==1 && user->rest&FLAG('S'))			/* feedback restriction? */
 		return FALSE;
 	if(reason!=NULL)
 		*reason=TooManyEmailsToday;
@@ -2746,39 +2734,25 @@ long DLLCALL loginAttemptListClear(link_list_t* list)
 }
 
 /****************************************************************************/
-static list_node_t* login_attempted(link_list_t* list, const union xp_sockaddr* addr)
+static list_node_t* login_attempted(link_list_t* list, const SOCKADDR_IN* addr)
 {
 	list_node_t*		node;
 	login_attempt_t*	attempt;
-	struct in6_addr		ia;
 
-	if(list==NULL)
-		return NULL;
 	for(node=list->first; node!=NULL; node=node->next) {
 		attempt=node->data;
-		switch(addr->addr.sa_family) {
-			case AF_INET:
-				memset(&ia, 0, sizeof(ia));
-				memcpy(&ia, &addr->in.sin_addr, sizeof(addr->in.sin_addr));
-				break;
-			case AF_INET6:
-				ia = addr->in6.sin6_addr;
-				break;
-		}
-		if(memcmp(&attempt->addr,&ia,sizeof(attempt->addr))==0)
+		if(memcmp(&attempt->addr,&addr->sin_addr,sizeof(attempt->addr))==0)
 			break;
 	}
 	return node;
 }
 
 /****************************************************************************/
-long DLLCALL loginAttempts(link_list_t* list, const union xp_sockaddr* addr)
+long DLLCALL loginAttempts(link_list_t* list, const SOCKADDR_IN* addr)
 {
 	long				count=0;
 	list_node_t*		node;
 
-	if(addr->addr.sa_family != AF_INET && addr->addr.sa_family != AF_INET6)
-		return 0;
 	listLock(list);
 	if((node=login_attempted(list, addr))!=NULL)
 		count = ((login_attempt_t*)node->data)->count - ((login_attempt_t*)node->data)->dupes;
@@ -2788,12 +2762,10 @@ long DLLCALL loginAttempts(link_list_t* list, const union xp_sockaddr* addr)
 }
 
 /****************************************************************************/
-void DLLCALL loginSuccess(link_list_t* list, const union xp_sockaddr* addr)
+void DLLCALL loginSuccess(link_list_t* list, const SOCKADDR_IN* addr)
 {
 	list_node_t*		node;
 
-	if(addr->addr.sa_family != AF_INET && addr->addr.sa_family != AF_INET6)
-		return;
 	listLock(list);
 	if((node=login_attempted(list, addr)) != NULL)
 		listRemoveNode(list, node, /* freeData: */TRUE);
@@ -2803,15 +2775,13 @@ void DLLCALL loginSuccess(link_list_t* list, const union xp_sockaddr* addr)
 /****************************************************************************/
 /* Returns number of *unique* login attempts (excludes consecutive dupes)	*/
 /****************************************************************************/
-ulong DLLCALL loginFailure(link_list_t* list, const union xp_sockaddr* addr, const char* prot, const char* user, const char* pass)
+ulong DLLCALL loginFailure(link_list_t* list, const SOCKADDR_IN* addr, const char* prot, const char* user, const char* pass)
 {
 	list_node_t*		node;
 	login_attempt_t		first;
 	login_attempt_t*	attempt=&first;
 	ulong				count=0;
 
-	if(addr->addr.sa_family != AF_INET && addr->addr.sa_family != AF_INET6)
-		return 0;
 	if(list==NULL)
 		return 0;
 	memset(&first, 0, sizeof(first));
@@ -2824,7 +2794,7 @@ ulong DLLCALL loginFailure(link_list_t* list, const union xp_sockaddr* addr, con
 	}
 	SAFECOPY(attempt->prot,prot);
 	attempt->time=time32(NULL);
-	memcpy(&attempt->addr, addr, sizeof(*addr));
+	attempt->addr=addr->sin_addr;
 	SAFECOPY(attempt->user, user);
 	SAFECOPY(attempt->pass, pass);
 	attempt->count++;
