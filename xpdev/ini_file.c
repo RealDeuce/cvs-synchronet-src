627d7700501dad833de8456b3248d1b2eff925b7
deuce <deuce>
2015-02-06 18:50:37 +0000

    Fix copy pasta in the last commit.

--- xpdev/ini_file.c
+++ xpdev/ini_file.c
@@ -2,13 +2,13 @@
 
 /* Functions to create and parse .ini files */
 
-/* $Id: ini_file.c,v 1.139 2015/02/12 23:11:29 rswindell Exp $ */
+/* $Id: ini_file.c,v 1.137 2014/08/20 10:18:36 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2015 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -401,11 +401,9 @@ BOOL DLLCALL iniRenameSection(str_list_t* list, const char* section, const char*
 	if(section==ROOT_SECTION)
 		return(FALSE);
 
-	if(stricmp(section, newname)!=0) {
-		i=find_section_index(*list,newname);
-		if((*list)[i]!=NULL)	/* duplicate */
-			return(FALSE);
-	}
+	i=find_section_index(*list,newname);
+	if((*list)[i]!=NULL)	/* duplicate */
+		return(FALSE);
 
 	i=find_section_index(*list,section);
 	if((*list)[i]==NULL)	/* not found */
