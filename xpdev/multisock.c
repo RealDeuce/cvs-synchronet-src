dd9fb76062b78d80f6a3caf83c0fded345167058
deuce <deuce>
2015-02-07 07:58:29 +0000

    Put 3rdp CFLAGS first to ensure that the include files are search for there first.

--- xpdev/multisock.c
+++ xpdev/multisock.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <gen_defs.h>
-#include <sockwrap.h>
-#include <dirwrap.h>
-#include <multisock.h>
+#include "gen_defs.h"
+#include "sockwrap.h"
+#include "dirwrap.h"
+#include "multisock.h"
+#include <stdarg.h>
 
-struct xpms_set *xpms_create(unsigned int retries, unsigned int wait_secs,
+struct xpms_set* DLLCALL xpms_create(unsigned int retries, unsigned int wait_secs,
 	int (*lprintf)(int level, const char *fmt, ...))
 {
 	struct xpms_set *ret=(struct xpms_set *)calloc(1, sizeof(struct xpms_set));
@@ -21,7 +22,7 @@ struct xpms_set *xpms_create(unsigned int retries, unsigned int wait_secs,
 	return ret;
 }
 
-void xpms_destroy(struct xpms_set *xpms_set)
+void DLLCALL xpms_destroy(struct xpms_set *xpms_set, void (*sock_destroy)(SOCKET, void *), void *cbdata)
 {
 	int		i;
 
@@ -34,6 +35,8 @@ void xpms_destroy(struct xpms_set *xpms_set)
 						, xpms_set->socks[i].sock, xpms_set->socks[i].prot?xpms_set->socks[i].prot:"unknown"
 						, xpms_set->socks[i].port);
 			closesocket(xpms_set->socks[i].sock);
+			if(sock_destroy)
+				sock_destroy(xpms_set->socks[i].sock, cbdata);
 		}
 		xpms_set->socks[i].sock = INVALID_SOCKET;
 		FREE_AND_NULL(xpms_set->socks[i].address);
@@ -43,7 +46,7 @@ void xpms_destroy(struct xpms_set *xpms_set)
 	free(xpms_set);
 }
 
-BOOL xpms_add(struct xpms_set *xpms_set, int domain, int type,
+BOOL DLLCALL xpms_add(struct xpms_set *xpms_set, int domain, int type,
 	int protocol, const char *addr, uint16_t port, const char *prot, 
 	void (*sock_init)(SOCKET, void *), int(*bind_init)(BOOL), void *cbdata)
 {
@@ -61,6 +64,7 @@ BOOL xpms_add(struct xpms_set *xpms_set, int domain, int type,
 
 	if(domain == AF_UNIX) {
 		memset(&dummy, 0, sizeof(dummy));
+		memset(&un_addr, 0, sizeof(un_addr));
 		dummy.ai_family = AF_UNIX;
 		dummy.ai_socktype = type;
 		dummy.ai_addr = (struct sockaddr *)&un_addr;
@@ -68,13 +72,12 @@ BOOL xpms_add(struct xpms_set *xpms_set, int domain, int type,
 
 		if(strlen(addr) >= sizeof(un_addr.sun_path)) {
 			if(xpms_set->lprintf)
-				xpms_set->lprintf(LOG_ERR, "!ERROR %s is too long for a AF_UNIX socket", addr);
+				xpms_set->lprintf(LOG_ERR, "!ERROR %s is too long for a portable AF_UNIX socket", addr);
 			return FALSE;
 		}
 		strcpy(un_addr.sun_path,addr);
 		if(fexist(addr))
 			unlink(addr);
-		dummy.ai_addrlen = sizeof(un_addr);
 		res = &dummy;
 	}
 #endif
@@ -85,7 +88,9 @@ BOOL xpms_add(struct xpms_set *xpms_set, int domain, int type,
 		hints.ai_socktype=type;
 		hints.ai_protocol=protocol;
 		hints.ai_flags|=AI_NUMERICSERV;
+#ifdef AI_ADDRCONFIG
 		hints.ai_flags|=AI_ADDRCONFIG;
+#endif
 		sprintf(port_str, "%hu", port);
 		if((ret=getaddrinfo(addr, port_str, &hints, &res))!=0) {
 			if(xpms_set->lprintf)
@@ -138,14 +143,16 @@ BOOL xpms_add(struct xpms_set *xpms_set, int domain, int type,
 				bind_init(TRUE);
 		}
 
-		if(listen(xpms_set->socks[xpms_set->sock_count].sock, SOMAXCONN)==-1) {
-			if(xpms_set->lprintf)
-				xpms_set->lprintf(LOG_WARNING, "%04d !ERROR %d listen()ing on port %d"
-						, xpms_set->socks[xpms_set->sock_count].sock, ERROR_VALUE, port);
-			closesocket(xpms_set->socks[xpms_set->sock_count].sock);
-			FREE_AND_NULL(xpms_set->socks[xpms_set->sock_count].address);
-			FREE_AND_NULL(xpms_set->socks[xpms_set->sock_count].prot);
-			continue;
+		if(type != SOCK_DGRAM) {
+			if(listen(xpms_set->socks[xpms_set->sock_count].sock, SOMAXCONN)==-1) {
+				if(xpms_set->lprintf)
+					xpms_set->lprintf(LOG_WARNING, "%04d !ERROR %d listen()ing on port %d"
+							, xpms_set->socks[xpms_set->sock_count].sock, ERROR_VALUE, port);
+				closesocket(xpms_set->socks[xpms_set->sock_count].sock);
+				FREE_AND_NULL(xpms_set->socks[xpms_set->sock_count].address);
+				FREE_AND_NULL(xpms_set->socks[xpms_set->sock_count].prot);
+				continue;
+			}
 		}
 
 		added++;
@@ -161,7 +168,7 @@ BOOL xpms_add(struct xpms_set *xpms_set, int domain, int type,
 	return FALSE;
 }
 
-BOOL xpms_add_list(struct xpms_set *xpms_set, int domain, int type,
+BOOL DLLCALL xpms_add_list(struct xpms_set *xpms_set, int domain, int type,
 	int protocol, str_list_t list, uint16_t default_port, const char *prot, 
 	void (*sock_init)(SOCKET, void *), int(*bind_init)(BOOL), void *cbdata)
 {
@@ -172,13 +179,20 @@ BOOL xpms_add_list(struct xpms_set *xpms_set, int domain, int type,
 	BOOL	one_good=FALSE;
 	
 	for(iface=list; iface && *iface; iface++) {
-		host=strdup(*iface);
 		WORD	port=default_port;
+
+		host=strdup(*iface);
 
 		host_str=host;
 		if(xpms_set->lprintf)
 			xpms_set->lprintf(LOG_INFO, "Adding %s listening socket on %s", prot, host);
 		p = strrchr(host, ':');
+		/*
+		 * If there isn't a [, and the first and last colons aren't the same
+		 * it's assumed to be an IPv6 address
+		 */
+		if(strchr(host,'[')==NULL && p != NULL && strchr(host, ':') != p)
+			p=NULL;
 		if(host[0]=='[') {
 			host_str++;
 			p2=strrchr(host,']');
@@ -191,18 +205,17 @@ BOOL xpms_add_list(struct xpms_set *xpms_set, int domain, int type,
 			*(p++)=0;
 			sscanf(p, "%hu", &port);
 		}
-		if(xpms_add(xpms_set, PF_UNSPEC, SOCK_STREAM, 0, host_str, port, prot, sock_init, bind_init, cbdata))
+		if(xpms_add(xpms_set, domain, type, protocol, host_str, port, prot, sock_init, bind_init, cbdata))
 			one_good=TRUE;
 		free(host);
 	}
 	return one_good;
 }
 
-SOCKET xpms_accept(struct xpms_set *xpms_set, union xp_sockaddr * addr, 
+SOCKET DLLCALL xpms_accept(struct xpms_set *xpms_set, union xp_sockaddr * addr, 
 	socklen_t * addrlen, unsigned int timeout, void **cb_data)
 {
 	fd_set			read_fs;
-	fd_set			except_fs;
 	int				i;
 	struct timeval	tv;
 	struct timeval	*tvp;
@@ -213,8 +226,7 @@ SOCKET xpms_accept(struct xpms_set *xpms_set, union xp_sockaddr * addr,
 		if(xpms_set->socks[i].sock == INVALID_SOCKET)
 			continue;
 		FD_SET(xpms_set->socks[i].sock, &read_fs);
-		FD_SET(xpms_set->socks[i].sock, &except_fs);
-		if(xpms_set->socks[i].sock > max_sock)
+		if(xpms_set->socks[i].sock >= max_sock)
 			max_sock=xpms_set->socks[i].sock+1;
 	}
 
@@ -225,7 +237,7 @@ SOCKET xpms_accept(struct xpms_set *xpms_set, union xp_sockaddr * addr,
 		tv.tv_usec=(timeout%1000)*1000;
 		tvp=&tv;
 	}
-	switch(select(max_sock, &read_fs, NULL, &except_fs, tvp)) {
+	switch(select(max_sock, &read_fs, NULL, NULL, tvp)) {
 		case 0:
 			return INVALID_SOCKET;
 		case -1:
@@ -239,13 +251,8 @@ SOCKET xpms_accept(struct xpms_set *xpms_set, union xp_sockaddr * addr,
 						*cb_data=xpms_set->socks[i].cb_data;
 					return accept(xpms_set->socks[i].sock, &addr->addr, addrlen);
 				}
-				if(FD_ISSET(xpms_set->socks[i].sock, &except_fs)) {
-					closesocket(xpms_set->socks[i].sock);
-					xpms_set->socks[i].sock = INVALID_SOCKET;
-				}
 			}
 	}
 
 	return INVALID_SOCKET;
 }
-
