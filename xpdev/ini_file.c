9c35abc8ff2191d9eeae1b897f6c33eba3183afc
deuce <deuce>
2015-02-09 00:28:45 +0000

    Ensure cfile is initialized.  Previously since the current column was always the directory list on entry, it would always get initialized first time through the loop.
    Since file selection now defaults to the file column, this was no longer
    happening leaving the path/filename as a zero-length string meaning if you
    wanted to enter a filename directly (as in creating a capture file), you
    had to either select a directory first, or type the full path.

--- xpdev/ini_file.c
+++ xpdev/ini_file.c
@@ -2,13 +2,13 @@
 
 /* Functions to create and parse .ini files */
 
-/* $Id: ini_file.c,v 1.140 2015/04/18 11:02:13 rswindell Exp $ */
+/* $Id: ini_file.c,v 1.137 2014/08/20 10:18:36 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2015 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -401,11 +401,9 @@ BOOL DLLCALL iniRenameSection(str_list_t* list, const char* section, const char*
 	if(section==ROOT_SECTION)
 		return(FALSE);
 
-	if(stricmp(section, newname)!=0) {
-		i=find_section_index(*list,newname);
-		if((*list)[i]!=NULL)	/* duplicate */
-			return(FALSE);
-	}
+	i=find_section_index(*list,newname);
+	if((*list)[i]!=NULL)	/* duplicate */
+		return(FALSE);
 
 	i=find_section_index(*list,section);
 	if((*list)[i]==NULL)	/* not found */
@@ -1140,25 +1138,7 @@ iniGetNamedStringList(str_list_t list, const char* section)
 
 static BOOL isTrue(const char* value)
 {
-	char*	str;
-	char*	p;
-	BOOL	is_true;
-	
-	if(!isalpha(*value))
-		return FALSE;
-
-	if((str=strdup(value)) == NULL)
-		return FALSE;
-
-	/* Truncate value at first space, tab or semicolon for purposes of checking for special boolean words. */
-	/* This allows comments or white-space to immediately follow a special boolean word: "True", "Yes", or "On" */
-	p=str;
-	FIND_CHARSET(p, "; \t");
-	*p=0;
-	
-	is_true = (stricmp(str,"TRUE")==0 || stricmp(str,"YES")==0 || stricmp(str,"ON")==0);
-	free(str);
-	return is_true;
+	return(stricmp(value,"TRUE")==0 || stricmp(value,"YES")==0 || stricmp(value,"ON")==0);
 }
 
 static long parseInteger(const char* value)
