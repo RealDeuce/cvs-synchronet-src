626cf73db95edb64adc3a8c87c74da0724164636
deuce <deuce>
2015-02-09 10:55:22 +0000

    Log da changes.

--- sbbs3/mailsrvr.h
+++ sbbs3/mailsrvr.h
@@ -2,7 +2,7 @@
 
 /* Synchronet Mail (SMTP/POP3/SendMail) server */
 
-/* $Id: mailsrvr.h,v 1.76 2015/08/29 20:53:29 rswindell Exp $ */
+/* $Id: mailsrvr.h,v 1.72 2014/11/20 05:13:38 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -61,10 +61,7 @@ typedef struct {
 	WORD	max_recipients;
 #define MAIL_DEFAULT_MAX_RECIPIENTS			100
 	WORD	sem_chk_freq;		/* semaphore file checking frequency (in seconds) */
-	struct in_addr outgoing4;
-	struct in6_addr	outgoing6;
-    str_list_t   interfaces;
-    str_list_t   pop3_interfaces;
+    DWORD   interface_addr;
     DWORD	options;			/* See MAIL_OPT definitions */
     DWORD	max_msg_size;		/* Max msg size in bytes (0=unlimited) */
 #define MAIL_DEFAULT_MAX_MSG_SIZE			(20*1024*1024)	/* 20MB */
@@ -133,7 +130,7 @@ typedef struct {
 static struct init_field mail_init_fields[] = { 
 	 OFFSET_AND_SIZE(mail_startup_t,smtp_port)
 	,OFFSET_AND_SIZE(mail_startup_t,pop3_port)
-	,OFFSET_AND_SIZE(mail_startup_t,interfaces)
+	,OFFSET_AND_SIZE(mail_startup_t,interface_addr)
 	,OFFSET_AND_SIZE(mail_startup_t,ctrl_dir)
 	,{ 0,0 }	/* terminator */
 };
@@ -241,10 +238,6 @@ extern "C" {
 DLLEXPORT void			DLLCALL mail_server(void* arg);
 DLLEXPORT void			DLLCALL mail_terminate(void);
 DLLEXPORT const	char*	DLLCALL mail_ver(void);
-
-/* for mxlookup.c: */
-void mail_open_socket(SOCKET sock, void* cb_protocol);
-int mail_close_socket(SOCKET sock);
 #ifdef __cplusplus
 }
 #endif
