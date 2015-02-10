6bc952f658c090123fe063f9128ceaabbe2132d4
deuce <deuce>
2015-02-10 08:41:17 +0000

    Use better corners for ASCII box drawing.

--- sbbs3/wordwrap.c
+++ sbbs3/wordwrap.c
@@ -1,4 +1,4 @@
-/* $Id: wordwrap.c,v 1.22 2015/05/07 11:02:06 deuce Exp $ */
+/* $Id: wordwrap.c,v 1.18 2015/02/10 08:34:27 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -178,7 +178,7 @@ int get_word_len(char *buf, int starting_pos)
 			return 0;
 		if (!buf[starting_pos+pos])
 			break;
-		else if (isspace((unsigned char)buf[starting_pos+pos]))
+		else if (isspace(buf[starting_pos+pos]))
 			break;
 		else if (buf[starting_pos+pos]=='\x1f')
 			continue;
@@ -216,7 +216,6 @@ char* wordwrap(char* inbuf, int len, int oldlen, BOOL handle_quotes)
 	int			outbuf_size=0;
 	int			inbuf_len=strlen(inbuf);
 	unsigned	next_len;
-	BOOL		chopped = FALSE;
 
 	outbuf_size=inbuf_len*3+1;
 	if((outbuf=(char*)malloc(outbuf_size))==NULL)
@@ -347,23 +346,20 @@ char* wordwrap(char* inbuf, int len, int oldlen, BOOL handle_quotes)
 					icol=prefix_len+1;
 					continue;
 				}
-				else if(chopped || isspace((unsigned char)inbuf[i+1])) {	/* Next line starts with whitespace.  This is a "hard" CR. */
+				else if(isspace((unsigned char)inbuf[i+1])) {	/* Next line starts with whitespace.  This is a "hard" CR. */
 					linebuf[l++]='\r';
 					linebuf[l++]='\n';
 					outbuf_append(&outbuf, &outp, linebuf, l, &outbuf_size);
-					if(prefix)
-						memcpy(linebuf,prefix,prefix_bytes);
 					l=prefix_bytes;
 					ocol=prefix_len+1;
 					icol=prefix_len+1;
-					chopped = FALSE;
 					continue;
 				}
 				else {
 					if(icol < oldlen) {			/* If this line is overly long, It's impossible for the next word to fit */
 						/* k will equal the length of the first word on the next line */
 						k = get_word_len(inbuf, i+1);
-						if(icol+k < oldlen) {	/* The next word would have fit but isn't here.  Must be a hard CR */
+						if(icol+k+1 < oldlen) {	/* The next word would have fit but isn't here.  Must be a hard CR */
 							linebuf[l++]='\r';
 							linebuf[l++]='\n';
 							outbuf_append(&outbuf, &outp, linebuf, l, &outbuf_size);
@@ -388,7 +384,6 @@ char* wordwrap(char* inbuf, int len, int oldlen, BOOL handle_quotes)
 						}
 					}
 				}
-				icol=prefix_len+1;
 				/* Fall-through soft CRs for wrapping! */
 			default:
 				if (inbuf[i] != '\n') {
@@ -396,7 +391,7 @@ char* wordwrap(char* inbuf, int len, int oldlen, BOOL handle_quotes)
 					ocol++;
 					icol++;
 				}
-				if(ocol>len && inbuf[i+1] && !isspace((unsigned char)inbuf[i+1])) {		/* Need to wrap here */
+				if(ocol>len && inbuf[i+1] && !isspace(inbuf[i+1])) {		/* Need to wrap here */
 					/* Find the start of the last word */
 					k=l;									/* Original next char */
 					l--;									/* Move back to the last char */
@@ -416,7 +411,6 @@ char* wordwrap(char* inbuf, int len, int oldlen, BOOL handle_quotes)
 							l--;
 						if(l>0)
 							l--;
-						chopped = TRUE;
 					}
 					t=l+1;									/* Store start position of next line */
 					/* Move to start of whitespace */
