8d34bbc0e962446fa590e99d04c4259aec0f451d
rswindell <rswindell>
2015-02-12 23:11:29 +0000

    Updated style of previous commit to match the rest of the file/project. Updated copyright date.

--- xpdev/ini_file.c
+++ xpdev/ini_file.c
@@ -2,13 +2,13 @@
 
 /* Functions to create and parse .ini files */
 
-/* $Id: ini_file.c,v 1.138 2015/02/12 09:26:33 deuce Exp $ */
+/* $Id: ini_file.c,v 1.139 2015/02/12 23:11:29 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2015 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -401,7 +401,7 @@ BOOL DLLCALL iniRenameSection(str_list_t* list, const char* section, const char*
 	if(section==ROOT_SECTION)
 		return(FALSE);
 
-	if (stricmp(section, newname)) {
+	if(stricmp(section, newname)!=0) {
 		i=find_section_index(*list,newname);
 		if((*list)[i]!=NULL)	/* duplicate */
 			return(FALSE);
