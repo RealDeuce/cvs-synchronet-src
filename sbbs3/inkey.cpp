c50a7c09cbf8030208403387672c13600fb24add
rswindell <rswindell>
2015-02-21 02:32:57 +0000

    Use parens more aggresively. Fix bug with Ctrl-Z introduced in rev 1.46

--- sbbs3/inkey.cpp
+++ sbbs3/inkey.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet single key input function (no wait) */
 
-/* $Id$ */
+/* $Id: inkey.cpp,v 1.48 2015/02/21 02:32:57 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2015 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -107,7 +107,7 @@ char sbbs_t::handle_ctrlkey(char ch, long mode)
 			backspace();
 		return(0); 
 	}
-	if(ch==CTRL_Z && !(mode&K_MSG|K_GETSTR)
+	if(ch==CTRL_Z && !(mode&(K_MSG|K_GETSTR))
 		&& action!=NODE_PCHT) {	 /* Ctrl-Z toggle raw input mode */
 		if(hotkey_inside>1)	/* only allow so much recursion */
 			return(0);
