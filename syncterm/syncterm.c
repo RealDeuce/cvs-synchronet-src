006f14baf48928480e546f20eef84f0235ed7807
deuce <deuce>
2015-03-15 09:15:39 +0000

    Move comment to better place in last commit, tidy up formatting.

--- syncterm/syncterm.c
+++ syncterm/syncterm.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: syncterm.c,v 1.192 2015/03/15 09:07:45 deuce Exp $ */
+/* $Id: syncterm.c,v 1.193 2015/03/15 09:15:39 deuce Exp $ */
 
 #if defined(__APPLE__) && defined(__MACH__)
 #include <CoreServices/CoreServices.h>	// FSFindFolder() and friends
@@ -1022,11 +1022,10 @@ char *get_syncterm_filename(char *fn, int fnlen, int type, int shared)
 			}
 			if(we_got_this) {
 				if (type != SYNCTERM_DEFAULT_TRANSFER_PATH) {
-					// Convert unicode to string.
 					if(snprintf(fn, fnlen, "%S\\SyncTERM", path) >= fnlen) {
 						we_got_this=FALSE;
-					}
 				}
+				// Convert unicode to string.
 				CTMF(path);
 			}
 		}
