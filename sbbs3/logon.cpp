4d346f34450bcc00353d160ec02da045b7507c41
deuce <deuce>
2015-05-02 01:00:13 +0000

    Add values for NODE_WFC and NODE_OFFLINE to the help text.

--- sbbs3/logon.cpp
+++ sbbs3/logon.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet user logon routines */
 
-/* $Id: logon.cpp,v 1.60 2015/05/02 03:20:54 rswindell Exp $ */
+/* $Id: logon.cpp,v 1.59 2014/03/08 04:40:28 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -521,7 +521,7 @@ bool sbbs_t::logon()
 	if(usrgrps && useron.misc&ASK_NSCAN && text[NScanAllGrpsQ][0] && yesno(text[NScanAllGrpsQ]))
 		scanallsubs(SCAN_NEW);
 	if(usrgrps && useron.misc&ASK_SSCAN && text[SScanAllGrpsQ][0] && yesno(text[SScanAllGrpsQ]))
-		scanallsubs(SCAN_TOYOU|SCAN_UNREAD);
+		scanallsubs(SCAN_TOYOU);
 	return(true);
 }
 
--- sbbs3/sbbsdefs.h
+++ sbbs3/sbbsdefs.h
@@ -2,7 +2,7 @@
 
 /* Synchronet constants, macros, and structure definitions */
 
-/* $Id: sbbsdefs.h,v 1.192 2015/05/02 03:20:55 rswindell Exp $ */
+/* $Id: sbbsdefs.h,v 1.191 2015/04/29 06:56:41 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -810,7 +810,6 @@ enum XFER_TYPE {				/* Values for type in xfer_prot_select()	*/
 #define SCAN_BACK	(1<<2)		/* Scan the last message if no new			*/
 #define SCAN_TOYOU	(1<<3)		/* Scan for messages to you 				*/
 #define SCAN_FIND	(1<<4)		/* Scan for text in messages				*/
-#define SCAN_UNREAD	(1<<5)		/* Display un-read messages only			*/
 								
 								/* Bits in misc of chan_t					*/
 #define CHAN_PW 	(1<<0)		/* Can be password protected				*/
@@ -987,10 +986,7 @@ typedef struct {						/* File (transfers) Data */
 
 } file_t;
 
-typedef struct {
-	idxrec_t	idx;					/* defined in smbdefs.h */
-	uint32_t	num;					/* 1-based offset */
-} post_t;
+typedef idxrec_t post_t;				/* defined in smbdefs.h */
 typedef idxrec_t mail_t;				/* defined in smbdefs.h */
 typedef fidoaddr_t faddr_t;				/* defined in smbdefs.h */
 
