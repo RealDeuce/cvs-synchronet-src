0a8541a4239069062473332a0872fa387f08bac6
rswindell <rswindell>
2015-05-02 03:20:54 +0000

    Implemented the SCAN_UNREAD mode bit for scansubs(), scanallsubs(), scanposts(), and listsub(). These functions are exposed in the JS 'bbs' object as scan_subs(), scan_msgs(), and list_msgs(). Wherease previously, all of "your messages" (messages addressed to you) would be found/displayed if the user answered (Y)es to the "Subjects Only?" prompt, and only un-read message (to you) would be displayed if they answered (N)o to the "Subjects Only?", providing long-standing confusion about the different behavior, now the user is prompted whether to display un-read messages only (in scansubs/scanallsubs), and in the case of the logon question to "Search all subs for un-read messages to you?", only un-read messages are listed/displayed, regardless of how the user answers the "Subjects Only?" question. SCAN_UNREAD is only applicable when combined with SCAN_TOYOU.
    Added new commands to the msg reading prompt: 'N' (list new messages), 'U'
    list un-read messages to-you.
    
    2 new text.dat entries: DisplayUnreadMessagesOnlyQ and NoMessagesFound

--- syncterm/conn.h
+++ syncterm/conn.h
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: conn.h,v 1.24 2015/02/10 22:06:15 deuce Exp $ */
+/* $Id: conn.h,v 1.25 2015/02/27 10:44:47 deuce Exp $ */
 
 #ifndef _CONN_H_
 #define _CONN_H_
@@ -57,10 +57,10 @@ struct conn_buffer {
 /*
  * Functions for stuff using connections
  */
-int conn_recv_upto(char *buffer, size_t buflen, unsigned int timeout);
-int conn_recv(char *buffer, size_t buflen, unsigned int timeout);
-int conn_peek(char *buffer, size_t buflen);
-int conn_send(char *buffer, size_t buflen, unsigned int timeout);
+int conn_recv_upto(void *buffer, size_t buflen, unsigned int timeout);
+int conn_recv(void *buffer, size_t buflen, unsigned int timeout);
+int conn_peek(void *buffer, size_t buflen);
+int conn_send(void *buffer, size_t buflen, unsigned int timeout);
 int conn_connect(struct bbslist *bbs);
 int conn_close(void);
 BOOL conn_connected(void);
@@ -81,9 +81,9 @@ extern struct conn_api conn_api;
 struct conn_buffer *create_conn_buf(struct conn_buffer *buf, size_t size);
 void destroy_conn_buf(struct conn_buffer *buf);
 size_t conn_buf_bytes(struct conn_buffer *buf);
-size_t conn_buf_peek(struct conn_buffer *buf, unsigned char *outbuf, size_t outlen);
-size_t conn_buf_get(struct conn_buffer *buf, unsigned char *outbuf, size_t outlen);
-size_t conn_buf_put(struct conn_buffer *buf, const unsigned char *outbuf, size_t outlen);
+size_t conn_buf_peek(struct conn_buffer *buf, void *voutbuf, size_t outlen);
+size_t conn_buf_get(struct conn_buffer *buf, void *outbuf, size_t outlen);
+size_t conn_buf_put(struct conn_buffer *buf, const void *outbuf, size_t outlen);
 size_t conn_buf_wait_cond(struct conn_buffer *buf, size_t bcount, unsigned long timeout, int do_free);
 #define conn_buf_wait_bytes(buf, count, timeout)	conn_buf_wait_cond(buf, count, timeout, 0)
 #define conn_buf_wait_free(buf, count, timeout)	conn_buf_wait_cond(buf, count, timeout, 1)
