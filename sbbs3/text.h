cfc71212e53aed9306a8a377162feef6dd554e76
rswindell <rswindell>
2015-05-13 00:11:44 +0000

    If the file's 'timetodl' (transfer time) value is 0, don't display the transfer time (e.g. "00:00:00"). Synchronet v3 has always forced this value to 0 since it's inaccurate over TCP/IP with the current hard-coded "CPS" rate of 3000 (i.e. bytes-per-second).

--- sbbs3/text.h
+++ sbbs3/text.h
@@ -2,13 +2,13 @@
 
 /* Synchronet static text string constants */
 
-/* $Id: text.h,v 1.24 2016/11/13 21:29:57 rswindell Exp $ */
+/* $Id: text.h,v 1.19 2015/05/02 03:20:55 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -811,25 +811,6 @@ enum {
 	,GetStrMenu
 	,DisplayUnreadMessagesOnlyQ
 	,NoMessagesFound
-	,UserIpAddr
-	,Seconds
-	,Minutes
-	,Hours
-	,Days
-	,Months
-	,Years
-	,InThePast
-	,InTheFuture
-	,AgeOfPostedItem
-	,NodeStatusLogout
-	,VotingNotAllowed
-	,VotedAlready
-	,R_Voting
-	,QWKSettingsVoting
-	,VoteMsgUpDownOrQuit
-	,MsgVotes
-	,MsgUpVoteNotice
-	,MsgDownVoteNotice
 
 	,TOTAL_TEXT
 };
