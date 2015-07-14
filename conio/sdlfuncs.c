7566b85a9035ff8ce327ebe1a70e2a1652ab6010
deuce <deuce>
2015-07-14 21:22:38 +0000

    Fix parsing of prefx that ends the line... ie: " >\r\n" since it doesn't have padding after it, it wasn't previously considered the same prefix.
    Fix prefix comparison by adding a function for it.

--- conio/sdlfuncs.c
+++ conio/sdlfuncs.c
@@ -25,7 +25,7 @@ struct sdlfuncs sdl;
 static int sdl_funcs_loaded=0;
 static int sdl_initialized=0;
 static int sdl_audio_initialized=0;
-static int sdl_video_initialized=0;
+int sdl_video_initialized=0;
 static int (*sdl_drawing_thread)(void *data)=NULL;
 static void (*sdl_exit_drawing_thread)(void)=NULL;
 static int main_returned=0;
@@ -148,6 +148,10 @@ int load_sdl_funcs(struct sdlfuncs *sdlf)
 		return(-1);
 	}
 	if((sdlf->WaitEvent=xp_dlsym(sdl_dll, SDL_WaitEvent))==NULL) {
+		xp_dlclose(sdl_dll);
+		return(-1);
+	}
+	if((sdlf->PollEvent=xp_dlsym(sdl_dll, SDL_PollEvent))==NULL) {
 		xp_dlclose(sdl_dll);
 		return(-1);
 	}
@@ -328,7 +332,7 @@ void run_sdl_drawing_thread(int (*drawing_thread)(void *data), void (*exit_drawi
 
 static void QuitWrap(void)
 {
-	if(sdl.Quit)
+	if(sdl.Quit && sdl_initialized)
 		sdl.Quit();
 }
 
@@ -341,7 +345,7 @@ int SDL_main_env(int argc, char **argv, char **env)
 	char	drivername[64];
 	struct main_args ma;
 	SDL_Thread	*main_thread;
-	int		main_ret;
+	int		main_ret=0;
 	int		use_sdl_video=FALSE;
 #ifdef _WIN32
 	char		*driver_env=NULL;
