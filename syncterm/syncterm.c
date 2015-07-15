f954c7283634e0a5f5858700bb808a06c4966195
deuce <deuce>
2015-07-15 00:55:30 +0000

    Write to the *new* line length, not the old one (doh!)

--- syncterm/syncterm.c
+++ syncterm/syncterm.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: syncterm.c,v 1.198 2015/08/05 09:49:32 deuce Exp $ */
+/* $Id: syncterm.c,v 1.196 2015/05/01 04:05:49 deuce Exp $ */
 
 #if defined(__APPLE__) && defined(__MACH__)
 #include <CoreServices/CoreServices.h>	// FSFindFolder() and friends
@@ -48,7 +48,7 @@ static const KNOWNFOLDERID FOLDERID_ProgramData =		{0x62AB5D82,0xFDC1,0x4DC3,{0x
 #include "uifcinit.h"
 #include "window.h"
 
-char* syncterm_version = "SyncTERM 1.1b"
+char* syncterm_version = "SyncTERM 1.0b"
 #ifdef _DEBUG
 	" Debug ("__DATE__")"
 #endif
