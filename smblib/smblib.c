038805e71f26bcf9273e11b622bd62e73c22ae81
rswindell <rswindell>
2015-07-15 23:43:31 +0000

    Add call to fflush() to smb_getstatus() to attempt to fix reported problem (on Linux) where the "total messages" value (stored in the SMB status header) is stale when the header is updated in another open instance of the message base.  Sorry, I forgot to complete the commit earlier than I originally said I did.

--- smblib/smblib.c
+++ smblib/smblib.c
@@ -2,13 +2,13 @@
 
 /* Synchronet message base (SMB) library routines */
 
-/* $Id$ */
+/* $Id: smblib.c,v 1.149 2015/07/15 23:43:31 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2012 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -368,6 +368,7 @@ int SMBCALL smb_getstatus(smb_t* smb)
 		safe_snprintf(smb->last_error,sizeof(smb->last_error),"msgbase not open");
 		return(SMB_ERR_NOT_OPEN);
 	}
+	fflush(smb->shd_fp);
 	clearerr(smb->shd_fp);
 	if(fseek(smb->shd_fp,sizeof(smbhdr_t),SEEK_SET)) {
 		safe_snprintf(smb->last_error,sizeof(smb->last_error)
