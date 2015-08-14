ddc1fd552afece2ee98177abfcca0d94134cef00
rswindell <rswindell>
2015-08-14 08:03:35 +0000

    Removed Help->Sysop Manual (old) menu item. Added Help->Donate menu item.

--- sbbs3/con_out.cpp
+++ sbbs3/con_out.cpp
@@ -1,12 +1,14 @@
+/* con_out.cpp */
+
 /* Synchronet console output routines */
 
-/* $Id: con_out.cpp,v 1.73 2016/11/16 11:05:33 rswindell Exp $ */
+/* $Id: con_out.cpp,v 1.70 2015/04/28 10:55:11 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2015 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -299,8 +301,6 @@ void sbbs_t::cursor_home(void)
 		rputs("\x1b[H");
 	else
 		outchar(FF);	/* this will clear some terminals, do nothing with others */
-	tos=1;
-	column=0;
 }
 
 void sbbs_t::cursor_up(int count)
@@ -619,23 +619,4 @@ bool sbbs_t::msgabort()
 	return(false);
 }
 
-int sbbs_t::backfill(const char* str, float pct)
-{
-	uint8_t	atr;
-	int len;
 
-	if(!term_supports(ANSI))
-		return bputs(str);
-
-	len = strlen(str);
-	for(int i=0; i<len; i++) {
-		if(((float)(i+1) / len)*100.0 <= pct)
-			atr = cfg.color[clr_backfill];
-		else
-			atr = cfg.color[clr_unfill];
-		if(curatr != atr) attr(atr);
-		outchar(str[i]);
-	}
-	attr(LIGHTGRAY);
-	return len;
-}
--- sbbs3/fixsmb.c
+++ sbbs3/fixsmb.c
@@ -1,12 +1,14 @@
+/* fixsmb.c */
+
 /* Synchronet message base (SMB) index re-generator */
 
-/* $Id: fixsmb.c,v 1.39 2016/11/08 20:17:12 rswindell Exp $ */
+/* $Id: fixsmb.c,v 1.37 2015/03/24 03:35:38 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2015 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -46,7 +48,7 @@
 smb_t	smb;
 BOOL	renumber=FALSE;
 BOOL	smb_undelete=FALSE;
-char*	usage="usage: fixsmb [-renumber] [-undelete] <smb_file> [[smb_file] [...]]";
+char*	usage="usage: fixsmb [-renumber] [-undelete] <smb_file> [[smb_file] [...]]\n";
 
 int compare_index(const idxrec_t* idx1, const idxrec_t* idx2)
 {
@@ -280,7 +282,7 @@ int main(int argc, char **argv)
 	int 		i;
 	str_list_t	list;
 
-	sscanf("$Revision: 1.39 $", "%*s %s", revision);
+	sscanf("$Revision: 1.37 $", "%*s %s", revision);
 
 	printf("\nFIXSMB v2.10-%s (rev %s) SMBLIB %s - Rebuild Synchronet Message Base\n\n"
 		,PLATFORM_DESC,revision,smb_lib_ver());
@@ -298,7 +300,7 @@ int main(int argc, char **argv)
 	}
 
 	if(!strListCount(list)) {
-		puts(usage);
+		printf(usage);
 		exit(1); 
 	}
 
