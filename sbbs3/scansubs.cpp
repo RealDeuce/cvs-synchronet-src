151a2bd29ef06e2233d084e75a0e49581f14688f
deuce <deuce>
2015-08-22 10:40:09 +0000

    Make the listening socket added log entry always show the port.

--- sbbs3/scansubs.cpp
+++ sbbs3/scansubs.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet message database scanning routines */
 
-/* $Id: scansubs.cpp,v 1.22 2016/11/27 23:13:06 rswindell Exp $ */
+/* $Id: scansubs.cpp,v 1.19 2015/05/02 03:20:55 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -118,21 +118,15 @@ void sbbs_t::scansubs(long mode)
 			menu("msgscan"); 
 		}
 		for(i=0;i<usrsubs[curgrp] && !msgabort();i++) {
-			if((mode&SCAN_NEW) && !(subscan[usrsub[curgrp][i]].cfg&SUB_CFG_NSCAN) && !(cfg.sub[usrsub[curgrp][i]]->misc&SUB_FORCED))
-				continue;
-			if((mode&SCAN_TOYOU) && !(subscan[usrsub[curgrp][i]].cfg&SUB_CFG_SSCAN))
-				continue;
-			if((mode&SCAN_POLLS) && cfg.sub[usrsub[curgrp][i]]->misc&SUB_NOVOTING)
-				continue;
-			if(mode&SCAN_POLLS)
-				progress("Scanning", i, usrsubs[curgrp]);
-			if(scanposts(usrsub[curgrp][i],mode,str)) 
-				break;
-			subs_scanned++;
-		}
-		if(mode&SCAN_POLLS) {
-			progress("Done", subs_scanned, usrsubs[curgrp]);
-			cleartoeol();
+			if(((mode&SCAN_NEW &&
+				(subscan[usrsub[curgrp][i]].cfg&SUB_CFG_NSCAN
+					|| cfg.sub[usrsub[curgrp][i]]->misc&SUB_FORCED))
+				|| (mode&SCAN_TOYOU && subscan[usrsub[curgrp][i]].cfg&SUB_CFG_SSCAN)
+				|| mode&SCAN_FIND)) {
+				if(scanposts(usrsub[curgrp][i],mode,str)) 
+					break;
+				subs_scanned++;
+			}
 		}
 		bputs(text[MessageScan]);
 		if(i==usrsubs[curgrp]) bprintf(text[MessageScanComplete],subs_scanned);
@@ -144,7 +138,7 @@ void sbbs_t::scansubs(long mode)
 }
 
 /****************************************************************************/
-/* Performs a new message scan of all sub-boards							*/
+/* Performs a new message scan all all sub-boards							*/
 /****************************************************************************/
 void sbbs_t::scanallsubs(long mode)
 {
@@ -152,8 +146,6 @@ void sbbs_t::scanallsubs(long mode)
 	char 	tmp[512];
 	uint	i,j,found=0;
 	ulong	subs_scanned=0;
-	uint*	sub;
-	ulong	total_subs=0;
 	bool	subj_only=false;
 
 	if(cfg.scansubs_mod[0] && !scansubs_inside) {
@@ -209,40 +201,27 @@ void sbbs_t::scanallsubs(long mode)
 	if(useron.misc&(RIP|WIP|HTML) && !(useron.misc&EXPERT)) {
 		menu("msgscan"); 
 	}
-	if((sub = (uint*)malloc(sizeof(uint) * cfg.total_subs)) == NULL) {
-		errormsg(WHERE, ERR_ALLOC, "subs", sizeof(uint)*cfg.total_subs);
-		return;
-	}
-
-	for(i=0; i<usrgrps; i++)
-		for(j=0; j<usrsubs[i]; j++) {
-			if((mode&SCAN_NEW) && !(subscan[usrsub[i][j]].cfg&SUB_CFG_NSCAN) && !(cfg.sub[usrsub[i][j]]->misc&SUB_FORCED))
-				continue;
-			if((mode&SCAN_TOYOU) && !(subscan[usrsub[i][j]].cfg&SUB_CFG_SSCAN))
-				continue;
-			if((mode&SCAN_POLLS) && cfg.sub[usrsub[i][j]]->misc&SUB_NOVOTING)
-				continue;
-			sub[total_subs++] = usrsub[i][j];
+	for(i=0;i<usrgrps;i++) {
+		for(j=0;j<usrsubs[i] && !msgabort();j++) {
+			if(((mode&SCAN_NEW && subscan[usrsub[i][j]].cfg&SUB_CFG_NSCAN)
+				|| cfg.sub[usrsub[i][j]]->misc&SUB_FORCED
+				|| mode&SCAN_FIND
+				|| (mode&SCAN_TOYOU && subscan[usrsub[i][j]].cfg&SUB_CFG_SSCAN))) {
+				if(scanposts(usrsub[i][j],mode,str)) 
+					break;
+				subs_scanned++;
+				}
 		}
-	for(i=0; i<total_subs && !msgabort(); i++) {
-		if(mode&SCAN_POLLS)
-			progress("Scanning", i, total_subs);
-		if(scanposts(sub[i],mode,str)) 
-			break;
-	}
-	subs_scanned = i;
-	free(sub);
-	if(mode&SCAN_POLLS) {
-		progress("Done", subs_scanned, total_subs);
-		cleartoeol();
+		if(j<usrsubs[i])
+			break; 
 	}
 	bputs(text[MessageScan]);
-	if(subs_scanned<total_subs) {
+	if(i<usrgrps) {
 		bputs(text[MessageScanAborted]);
 		return; 
 	}
 	bprintf(text[MessageScanComplete],subs_scanned);
-	if(mode&SCAN_NEW && !(mode&(SCAN_MSGSONLY|SCAN_BACK|SCAN_TOYOU))
+	if(mode&SCAN_NEW && !(mode&(SCAN_BACK|SCAN_TOYOU))
 		&& useron.misc&ANFSCAN && !(useron.rest&FLAG('T'))) {
 		xfer_cmds++;
 		scanalldirs(FL_ULTIME); 
