b25195de04ec7fff2ecac7862bfbbda3dd5863a1
deuce <deuce>
2015-08-23 10:11:01 +0000

    Add get_crypt_attribute() and get_crypt_error() functions which return allocated attribute string values or NULL for an error.
    Also, add free_crypt_attrstr() because Windows sucks.

--- sbbs3/ssl.h
+++ sbbs3/ssl.h
@@ -4,9 +4,13 @@
 #include "sbbs.h"	// For DLLEXPORT
 #include <cryptlib.h>
 #include "scfgdefs.h"
+#include "wrapdll.h"
 
 #define SSL_ESTR_LEN	CRYPT_MAX_TEXTSIZE + 1024 /* File name, line number, status code, and some static text */
 
+DLLEXPORT void* DLLCALL free_crypt_attrstr(char *attr);
+DLLEXPORT char* DLLCALL get_crypt_attribute(CRYPT_SESSION sess, C_IN CRYPT_ATTRIBUTE_TYPE attr);
+DLLEXPORT char* DLLCALL get_crypt_error(CRYPT_SESSION sess);
 DLLEXPORT CRYPT_CONTEXT DLLCALL get_ssl_cert(scfg_t *cfg, char *estr);
 
 #endif
--- sbbs3/userdat.h
+++ sbbs3/userdat.h
@@ -2,7 +2,7 @@
 
 /* Synchronet user data access routines (exported) */
 
-/* $Id: userdat.h,v 1.50 2015/08/20 05:19:45 deuce Exp $ */
+/* $Id: userdat.h,v 1.51 2015/08/22 07:11:07 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -135,7 +135,7 @@ DLLEXPORT BOOL	DLLCALL check_name(scfg_t* cfg, const char* name);
 
 /* Login attempt/hack tracking */
 typedef struct {
-	struct in6_addr addr;	/* host with consecutive failed login attmepts */
+	union xp_sockaddr addr;	/* host with consecutive failed login attmepts */
 	sa_family_t	family;
 	ulong		count;	/* number of consecutive failed login attempts */
 	ulong		dupes;	/* number of consecutive dupliate login attempts (same name and password) */
