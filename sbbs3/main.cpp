bacfaaf2bd24a974cf05cf8c1ab4a533481aafef
rswindell <rswindell>
2015-08-25 06:13:12 +0000

    Re-parenthesize the if() expression for QWKnet call-out event to address clang warning: warning: '&&' within '||' [-Wlogical-op-parentheses]

--- sbbs3/main.cpp
+++ sbbs3/main.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet terminal server thread and related functions */
 
-/* $Id: main.cpp,v 1.620 2015/08/22 10:48:32 deuce Exp $ */
+/* $Id: main.cpp,v 1.621 2015/08/25 06:13:12 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -2610,13 +2610,12 @@ void event_thread(void* arg)
 			tmptime=sbbs->cfg.qhub[i]->last;
 			if(localtime_r(&tmptime,&tm)==NULL)
 				memset(&tm,0,sizeof(tm));
-			if((sbbs->cfg.qhub[i]->last==-1L					/* or frequency */
-				|| ((sbbs->cfg.qhub[i]->freq
-					&& (now-sbbs->cfg.qhub[i]->last)/60>=sbbs->cfg.qhub[i]->freq)
+			if(sbbs->cfg.qhub[i]->last==-1L					/* or frequency */
+				|| (((sbbs->cfg.qhub[i]->freq && (now-sbbs->cfg.qhub[i]->last)/60>=sbbs->cfg.qhub[i]->freq)
 					|| (sbbs->cfg.qhub[i]->time
 						&& (now_tm.tm_hour*60)+now_tm.tm_min>=sbbs->cfg.qhub[i]->time
 						&& (now_tm.tm_mday!=tm.tm_mday || now_tm.tm_mon!=tm.tm_mon)))
-						&& sbbs->cfg.qhub[i]->days&(1<<now_tm.tm_wday))) {
+					&& sbbs->cfg.qhub[i]->days&(1<<now_tm.tm_wday))) {
 				SAFEPRINTF2(str,"%sqnet/%s.now"
 					,sbbs->cfg.data_dir,sbbs->cfg.qhub[i]->id);
 				if(fexistcase(str))
