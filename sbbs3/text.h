eb70a6238118ec7c48583645f88ee6a8fde2e262
rswindell <rswindell>
2015-08-26 23:58:58 +0000

    Updated the online user editor: - Display the user's last IP address (no longer stored in the "Note" field) - Moved the deleted/inactive user indicator to the last line before the prompt - Requires new text.dat entry: UserIpAddr (line 768).

--- sbbs3/text.h
+++ sbbs3/text.h
@@ -2,13 +2,13 @@
 
 /* Synchronet static text string constants */
 
-/* $Id: text.h,v 1.19 2015/05/02 03:20:55 rswindell Exp $ */
+/* $Id: text.h,v 1.20 2015/08/26 23:58:58 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -811,6 +811,7 @@ enum {
 	,GetStrMenu
 	,DisplayUnreadMessagesOnlyQ
 	,NoMessagesFound
+	,UserIpAddr
 
 	,TOTAL_TEXT
 };
