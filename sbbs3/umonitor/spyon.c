88f609ee1d36357298bd295bcd07374076614c61
Stephen Hurd <shurd@sasktel.net>
2015-08-27 00:38:07 +0000

    Fix warnings.

--- sbbs3/umonitor/spyon.c
+++ sbbs3/umonitor/spyon.c
@@ -2,7 +2,7 @@
 
 /* Synchronet for *nix node spy */
 
-/* $Id$ */
+/* $Id: spyon.c,v 1.11 2015/08/27 00:38:07 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -67,8 +67,6 @@ int spyon(char *sockname)  {
 	fd_set	rd;
 	BOOL	b;
 	int		retval=0;
-	char	ANSIbuf[32];
-	int		parsing=0;
 	int		telnet_strip=0;
 	struct text_info ti;
 	char *scrn;
