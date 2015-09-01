728a66a25a1a6ff80aa2a98dc500b3447b74a1e2
rswindell <rswindell>
2015-09-01 03:19:42 +0000

    Create a C version of sbbs_t::ansi(), call it ansi_attr() and DLL-export it.

--- sbbs3/answer.cpp
+++ sbbs3/answer.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet answer "caller" function */
 
-/* $Id: answer.cpp,v 1.92 2016/10/06 06:37:51 rswindell Exp $ */
+/* $Id: answer.cpp,v 1.88 2015/08/27 00:35:51 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -64,7 +64,7 @@ bool sbbs_t::answer()
         ,mon[tm.tm_mon],tm.tm_mday,tm.tm_year+1900,cfg.node_num);
 	logline("@ ",str);
 
-	safe_snprintf(str,sizeof(str),"%s  %s [%s]", connection, client_name, client_ipaddr);
+	safe_snprintf(str,sizeof(str),"%s  %s [%s]", connection, client_name, cid);
 	logline("@+:",str);
 
 	if(client_ident[0]) {
@@ -191,7 +191,6 @@ bool sbbs_t::answer()
 	}
 #ifdef USE_CRYPTLIB
 	if(sys_status&SS_SSH) {
-		tmp[0]=0;
 		pthread_mutex_lock(&ssh_mutex);
 		ctmp = get_crypt_attribute(ssh_session, CRYPT_SESSINFO_USERNAME);
 		if (ctmp) {
@@ -202,6 +201,8 @@ bool sbbs_t::answer()
 				SAFECOPY(tmp, ctmp);
 				free_crypt_attrstr(ctmp);
 			}
+			else
+				tmp[0] = 0;
 			pthread_mutex_unlock(&ssh_mutex);
 			lprintf(LOG_DEBUG,"Node %d SSH login: '%s'"
 				,cfg.node_num, rlogin_name);
@@ -264,7 +265,7 @@ bool sbbs_t::answer()
 		}
 		else {
 			if(cfg.sys_misc&SM_ECHO_PW)
-				lprintf(LOG_INFO,"Node %d SSH: UNKNOWN USER: '%s' (password: %s)",cfg.node_num,rlogin_name, truncsp(tmp));
+				lprintf(LOG_INFO,"Node %d SSH: UNKNOWN USER: '%s' (password: %s)",cfg.node_num,rlogin_name, tmp);
 			else
 				lprintf(LOG_INFO,"Node %d SSH: UNKNOWN USER: '%s'",cfg.node_num,rlogin_name);
 			badlogin(rlogin_name, tmp);
@@ -359,8 +360,8 @@ bool sbbs_t::answer()
 
 	/* AutoLogon via IP or Caller ID here */
 	if(!useron.number && !(sys_status&SS_RLOGIN)
-		&& (startup->options&BBS_OPT_AUTO_LOGON) && client_ipaddr[0]) {
-		useron.number=userdatdupe(0, U_IPADDR, LEN_IPADDR, client_ipaddr);
+		&& (startup->options&BBS_OPT_AUTO_LOGON) && cid[0]) {
+		useron.number=userdatdupe(0, U_IPADDR, LEN_IPADDR, cid);
 		if(useron.number) {
 			getuserdat(&cfg, &useron);
 			if(!(useron.misc&AUTOLOGON) || !(useron.exempt&FLAG('V')))
@@ -368,10 +369,8 @@ bool sbbs_t::answer()
 		}
 	}
 
-	if(!online) {
-		useron.number=0;
+	if(!online) 
 		return(false); 
-	}
 
 	if(stricmp(terminal,"sexpots")==0) {	/* dial-up connection (via SexPOTS) */
 		SAFEPRINTF2(str,"%s connection detected at %lu bps", terminal, cur_rate);
@@ -405,7 +404,6 @@ bool sbbs_t::answer()
 
 	useron.misc&=~TERM_FLAGS;
 	useron.misc|=autoterm;
-	SAFECOPY(client_ipaddr, cid);	/* Over-ride IP address with Caller-ID info */
 	SAFECOPY(useron.comp,client_name);
 
 	if(!useron.number && rlogin_name[0]!=0 && !(cfg.sys_misc&SM_CLOSED) && !matchuser(&cfg, rlogin_name, /* Sysop alias: */FALSE)) {
@@ -444,8 +442,21 @@ bool sbbs_t::answer()
 		if(logon()==false)
 			return(false);
 
+
 	if(!useron.number)
 		hangup();
+
+	/* Save the client IP or Caller-ID string to the user's record */
+	if(cid[0]) {
+		SAFECOPY(useron.ipaddr,cid);
+		putuserrec(&cfg,useron.number,U_IPADDR,LEN_IPADDR,useron.ipaddr);
+	}
+
+	/* Save the client hostname to the user's record */
+	if(client_name[0]) {
+		SAFECOPY(useron.comp,client_name);
+		putuserrec(&cfg,useron.number,U_COMP,LEN_COMP,useron.comp);
+	}
 
 	if(!online) 
 		return(false); 
--- sbbs3/startup.h
+++ sbbs3/startup.h
@@ -1,13 +1,14 @@
+/* startup.h */
+
 /* Synchronet main/telnet server thread startup structure */
 
-/* $Id: startup.h,v 1.79 2016/11/28 10:17:14 rswindell Exp $ */
-// vi: tabstop=4
+/* $Id: startup.h,v 1.75 2015/08/22 01:37:51 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -57,16 +58,6 @@ typedef struct {
 	char	load_path[INI_MAX_VALUE_LEN];	/* additional (comma-separated) directories to search for load()ed scripts */
 } js_startup_t;
 
-/* Login Attempt parameters */
-struct login_attempt_settings {
-	ulong	delay;				/* in milliseconds */
-	ulong	throttle;			/* in milliseconds */
-	ulong	hack_threshold;
-	ulong	tempban_threshold;
-	ulong	tempban_duration;	/* in seconds */
-	ulong	filter_threshold;
-};
-
 typedef struct {
 
 	char	ctrl_dir[INI_MAX_VALUE_LEN];
@@ -80,7 +71,10 @@ typedef struct {
 	js_startup_t js;
 	uint	bind_retry_count;		/* Number of times to retry bind() calls */
 	uint	bind_retry_delay;		/* Time to wait between each bind() retry */
-	struct login_attempt_settings login_attempt;
+	ulong	login_attempt_delay;
+	ulong	login_attempt_throttle;
+	ulong	login_attempt_hack_threshold;
+	ulong	login_attempt_filter_threshold;
 
 } global_startup_t;
 
@@ -129,7 +123,6 @@ typedef struct {
     char	temp_dir[128];
 	char	answer_sound[128];
 	char	hangup_sound[128];
-	char	ini_fname[128];
 
 	/* Miscellaneous */
 	char	xtrn_term_ansi[32];		/* external ANSI terminal type (e.g. "ansi-bbs") */
@@ -144,13 +137,14 @@ typedef struct {
 	/* JavaScript operating parameters */
 	js_startup_t js;
 
-	struct login_attempt_settings login_attempt;
+	/* Login Attempt parameters */
+	ulong	login_attempt_delay;
+	ulong	login_attempt_throttle;
+	ulong	login_attempt_hack_threshold;
+	ulong	login_attempt_filter_threshold;
 	link_list_t* login_attempt_list;
-	uint	max_concurrent_connections;
 
 } bbs_startup_t;
-
-#define DEFAULT_SEM_CHK_FREQ	2
 
 /* startup options that requires re-initialization/recycle when changed */
 #define OFFSET_AND_SIZE(s, f)	{ offsetof(s,f), sizeof(((s *)0)->f) }
