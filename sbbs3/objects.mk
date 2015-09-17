8b20fd076e210ee1bd5905e25dede630b3cd27b6
deuce <deuce>
2015-09-17 19:26:55 +0000

    We can't use 0x2584 for \x16 since that is the same as \xCD and the two are visually distinct, and \x16 doesn't connect to the right or to the bottom.
    This glyph is in Courier New, so simply revert to the previous "black rectange"
    value.

--- sbbs3/objects.mk
+++ sbbs3/objects.mk
@@ -2,7 +2,7 @@
 
 # Make 'include file' listing object files for SBBS.DLL
 
-# $Id: objects.mk,v 1.63 2015/11/24 11:05:07 rswindell Exp $(DIRSEP)09$(DIRSEP)08 07:23:54 deuce Exp $
+# $Id: objects.mk,v 1.60 2015/08/23 11:16:14 deuce Exp $(DIRSEP)09$(DIRSEP)08 07:23:54 deuce Exp $
 
 # OBJODIR, SLASH, and OFILE must be pre-defined
 
@@ -129,7 +129,6 @@ FTP_OBJS	= $(MTOBJODIR)$(DIRSEP)ftpsrvr$(OFILE) \
 MAIL_OBJS	= $(MTOBJODIR)$(DIRSEP)mailsrvr$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)mxlookup$(OFILE) \
  		  	$(MTOBJODIR)$(DIRSEP)mime$(OFILE) \
- 		  	$(MTOBJODIR)$(DIRSEP)nopen$(OFILE) \
  		  	$(MTOBJODIR)$(DIRSEP)ars$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)base64$(OFILE)
 
@@ -188,7 +187,6 @@ SBBSECHO_OBJS = \
 			$(OBJODIR)$(DIRSEP)userdat$(OFILE) \
 			$(OBJODIR)$(DIRSEP)rechocfg$(OFILE) \
 			$(OBJODIR)$(DIRSEP)msg_id$(OFILE) \
-			$(OBJODIR)$(DIRSEP)getmail$(OFILE) \
 			$(SMB_OBJS)
 
 ECHOCFG_OBJS = \
@@ -239,43 +237,6 @@ JSEXEC_OBJS = \
 			$(MTOBJODIR)$(DIRSEP)jsexec$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)js_uifc$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)js_conio$(OFILE)
-
-JSDOOR_OBJS = \
-			$(MTOBJODIR)$(DIRSEP)ars$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)base64$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)date_str$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)dat_rec$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)jsdoor$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)jsdebug$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_uifc$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_conio$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_request$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_socket$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)comio$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)js_client$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_com$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_cryptcon$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_global$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_rtpool$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_sprintf$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_file$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_internal$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_queue$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)js_server$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)nopen$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)str_util$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)sockopts$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)ssl$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)load_cfg$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)readtext$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)text_defaults$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)scfgsave$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)scfglib1$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)scfglib2$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)wordwrap$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)userdat$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)uucode$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)yenc$(OFILE)\
 
 SEXYZ_OBJS = \
 			$(MTOBJODIR)$(DIRSEP)sexyz$(OFILE) \
--- sbbs3/sbbs.h
+++ sbbs3/sbbs.h
@@ -2,13 +2,13 @@
 
 /* Synchronet class (sbbs_t) definition and exported function prototypes */
 
-/* $Id: sbbs.h,v 1.424 2015/12/04 10:06:05 rswindell Exp $ */
+/* $Id: sbbs.h,v 1.416 2015/09/01 03:19:42 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2015 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -589,7 +589,6 @@ public:
 	/* str.cpp */
 	char*	timestr(time_t intime);
     char	timestr_output[60];
-	char*	age_of_posted_item(char* buf, size_t max, time_t);
 	void	userlist(long mode);
 	size_t	gettmplt(char *outstr, const char *tmplt, long mode);
 	void	sif(char *fname, char *answers, long len);	/* Synchronet Interface File */
@@ -611,8 +610,8 @@ public:
 
 	/* writemsg.cpp */
 	void	automsg(void);
-	bool	writemsg(const char *str, const char *top, char *subj, long mode, uint subnum
-				,const char *to, const char* from, char** editor=NULL);
+	bool	writemsg(const char *str, const char *top, char *title, long mode, uint subnum
+				,const char *dest, char** editor=NULL);
 	char*	quotes_fname(int xedit, char* buf, size_t len);
 	char*	msg_tmp_fname(int xedit, char* fname, size_t len);
 	char	putmsg(const char *str, long mode);
@@ -625,7 +624,7 @@ public:
 	ushort	chmsgattr(ushort attr);
 	void	quotemsg(smbmsg_t* msg, int tails);
 	void	editmsg(smbmsg_t* msg, uint subnum);
-	void	editor_inf(int xeditnum, const char *to, const char* from, const char *subj, long mode
+	void	editor_inf(int xeditnum, const char *dest, const char *title, long mode
 				,uint subnum, const char* tagfile);
 	void	copyfattach(uint to, uint from, char *title);
 	bool	movemsg(smbmsg_t* msg, uint subnum);
@@ -645,7 +644,7 @@ public:
 	void	show_msgattr(ushort attr);
 	void	show_msghdr(smbmsg_t* msg);
 	void	show_msg(smbmsg_t* msg, long mode);
-	void	msgtotxt(smbmsg_t* msg, char *str, bool header, ulong mode);
+	void	msgtotxt(smbmsg_t* msg, char *str, int header, int tails);
 	ulong	getlastmsg(uint subnum, uint32_t *ptr, time_t *t);
 	time_t	getmsgtime(uint subnum, ulong ptr);
 	ulong	getmsgnum(uint subnum, time_t t);
@@ -754,9 +753,8 @@ public:
 	long	listsub(uint subnum, long mode, long start, const char* search);
 	long	listmsgs(uint subnum, long mode, post_t* post, long start, long posts);
 	long	searchposts(uint subnum, post_t* post, long start, long msgs, const char* find);
-	long	showposts_toyou(uint subnum, post_t* post, ulong start, long posts, long mode=0);
+	long	showposts_toyou(post_t* post, ulong start, long posts, long mode=0);
 	void	msghdr(smbmsg_t* msg);
-	char	msg_listing_flag(uint subnum, smbmsg_t*);
 
 	/* chat.cpp */
 	void	chatsection(void);
@@ -855,7 +853,7 @@ public:
 
 	/* xtrn_sec.cpp */
 	int		xtrn_sec(void);					/* The external program section  */
-	void	xtrndat(const char* name, const char* dropdir, uchar type, ulong tleft
+	void	xtrndat(char* name, char* dropdir, uchar type, ulong tleft
 				,ulong misc);
 	bool	exec_xtrn(uint xtrnnum);			/* Executes online external program */
 	bool	user_event(user_event_t);			/* Executes user event(s) */
@@ -879,6 +877,7 @@ public:
 	bool	qwklogon;
 	ulong	qwkmail_last;
 	void	qwk_sec(void);
+	int		qwk_route(char *inaddr, char *fulladdr);
 	uint	total_qwknodes;
 	struct qwknode {
 		char	id[LEN_QWKID+1];
@@ -1099,6 +1098,10 @@ extern "C" {
 	/* data.cpp */
 	DLLEXPORT time_t	DLLCALL getnextevent(scfg_t* cfg, event_t* event);
 
+	/* data_ovl.cpp */
+	DLLEXPORT BOOL		DLLCALL getmsgptrs(scfg_t* cfg, uint usernumber, subscan_t* subscan);
+	DLLEXPORT BOOL		DLLCALL putmsgptrs(scfg_t* cfg, uint usernumber, subscan_t* subscan);
+
 	/* sockopt.c */
 	DLLEXPORT int		DLLCALL set_socket_options(scfg_t* cfg, SOCKET sock, const char* section
 		,char* error, size_t errlen);
@@ -1106,9 +1109,6 @@ extern "C" {
 	/* xtrn.cpp */
 	DLLEXPORT char*		DLLCALL cmdstr(scfg_t* cfg, user_t* user, const char* instr
 									,const char* fpath, const char* fspec, char* cmd);
-
-	/* qwk.cpp */
-	DLLEXPORT int		qwk_route(scfg_t*, const char *inaddr, char *fulladdr, size_t maxlen);
 
 #ifdef JAVASCRIPT
 
@@ -1166,20 +1166,6 @@ extern "C" {
 	DLLEXPORT JSBool	DLLCALL js_DefineConstIntegers(JSContext* cx, JSObject* obj, jsConstIntSpec*, int flags);
 	DLLEXPORT JSBool	DLLCALL js_CreateArrayOfStrings(JSContext* cx, JSObject* parent
 														,const char* name, char* str[], unsigned flags);
-	DLLEXPORT BOOL	DLLCALL js_CreateCommonObjects(JSContext* cx
-													,scfg_t* cfg				/* common */
-													,scfg_t* node_cfg			/* node-specific */
-													,jsSyncMethodSpec* methods	/* global */
-													,time_t uptime				/* system */
-													,char* host_name			/* system */
-													,char* socklib_desc			/* system */
-													,js_callback_t*				/* js */
-													,js_startup_t*				/* js */
-													,client_t* client			/* client */
-													,SOCKET client_socket		/* client */
-													,js_server_props_t* props	/* server */
-													,JSObject** glob
-													);
 
 	/* js_server.c */
 	DLLEXPORT JSObject* DLLCALL js_CreateServerObject(JSContext* cx, JSObject* parent
@@ -1196,6 +1182,20 @@ extern "C" {
 	} global_private_t;
 	DLLEXPORT BOOL DLLCALL js_argc(JSContext *cx, unsigned argc, unsigned min);
 	DLLEXPORT BOOL DLLCALL js_CreateGlobalObject(JSContext* cx, scfg_t* cfg, jsSyncMethodSpec* methods, js_startup_t*, JSObject**);
+	DLLEXPORT BOOL	DLLCALL js_CreateCommonObjects(JSContext* cx
+													,scfg_t* cfg				/* common */
+													,scfg_t* node_cfg			/* node-specific */
+													,jsSyncMethodSpec* methods	/* global */
+													,time_t uptime				/* system */
+													,char* host_name			/* system */
+													,char* socklib_desc			/* system */
+													,js_callback_t*				/* js */
+													,js_startup_t*				/* js */
+													,client_t* client			/* client */
+													,SOCKET client_socket		/* client */
+													,js_server_props_t* props	/* server */
+													,JSObject** glob
+													);
 
 	/* js_internal.c */
 	DLLEXPORT JSObject* DLLCALL js_CreateInternalJsObject(JSContext*, JSObject* parent, js_callback_t*, js_startup_t*);
@@ -1212,7 +1212,7 @@ extern "C" {
 
 	/* js_client.c */
 	DLLEXPORT JSObject* DLLCALL js_CreateClientObject(JSContext* cx, JSObject* parent
-													,const char* name, client_t* client, SOCKET sock);
+													,char* name, client_t* client, SOCKET sock);
 	/* js_user.c */
 	DLLEXPORT JSObject*	DLLCALL js_CreateUserClass(JSContext* cx, JSObject* parent, scfg_t* cfg);
 	DLLEXPORT JSObject* DLLCALL js_CreateUserObject(JSContext* cx, JSObject* parent, scfg_t* cfg
@@ -1258,7 +1258,6 @@ extern "C" {
 
 	/* js_file.c */
 	DLLEXPORT JSObject* DLLCALL js_CreateFileClass(JSContext* cx, JSObject* parent);
-	DLLEXPORT JSObject* DLLCALL js_CreateFileObject(JSContext* cx, JSObject* parent, char *name, FILE* fp);
 
 	/* js_sprintf.c */
 	DLLEXPORT char*		DLLCALL js_sprintf(JSContext* cx, uint argn, unsigned argc, jsval *argv);
