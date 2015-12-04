0e0f4debfad3a99c1c5504b1b6a927cc0181ac1f
rswindell <rswindell>
2015-12-04 10:03:15 +0000

    smb_getplaintext() MIME/multipart plain-text decode is now tolerant of Content-Type boundary values not enclosed in quotes (e.g. like those inserted by "Apple-Mail").

--- encode/base64.c
+++ encode/base64.c
@@ -2,13 +2,13 @@
 
 /* Base64 encoding/decoding routines */
 
-/* $Id: base64.c,v 1.24 2017/11/26 00:36:50 rswindell Exp $ */
+/* $Id: base64.c,v 1.23 2006/05/08 21:11:40 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
