83ea0231fe9a73a3a9fba7680a4ae59c1b194757
rswindell <rswindell>
2015-12-07 09:10:01 +0000

    Address a couple of concerns from Accession: Don't add TZUTC kludge lines to exported netmail and echomail messages when there is already a TZUTC FidoNet control line in the message header (unexpected but apparently, GoldEd+ does this). If there are other duplicate FTN control/kludge lines created by SBBSecho exporting messages added to SMBs by GoldEd+, we should address those too, even though I believe in most cases, you should configure the editor to *not* add these header fields in the first place. Do not convert exisiting Origin Line's to effectively comments (replacing * with # in " * Origin") if the current sub-board has been configured to not add network tag/origin-lines.

--- xpdev/xpprintf.c
+++ xpdev/xpprintf.c
@@ -2,7 +2,7 @@
 
 /* Deuce's vs[n]printf() replacement */
 
-/* $Id: xpprintf.c,v 1.49 2015/09/28 06:05:20 deuce Exp $ */
+/* $Id: xpprintf.c,v 1.57 2015/09/29 00:48:52 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -35,6 +35,7 @@
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
 
+#define _GNU_SOURCE	// asprintf() on Linux
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
@@ -44,11 +45,37 @@
 #include "xpprintf.h"
 #include "gen_defs.h"
 
-/* MSVC Sucks - can't tell the required len of a *printf() */
-#define MAX_ARG_LEN		1024			/* MAX_ARG_LEN is the maximum length
-										 * possible as a result of a format
-										 * which is not %s
-										 */
+#if defined(_MSC_VER) || defined(__MSVCRT__)
+int DLLCALL vasprintf(char **strptr, char *format, va_list va)
+{
+	va_list	va2;
+	int		ret;
+
+	if (strptr == NULL)
+		return -1;
+	va_copy(va2, va);
+	ret = _vscprintf(format, va);
+	*strptr = (char *)malloc(ret+1);
+	if (*strptr == NULL)
+		return -1;
+	ret = vsprintf(*strptr, format, va2);
+	va_end(va2);
+	return ret;
+}
+
+int DLLCALL asprintf(char **strptr, char *format, ...)
+{
+	va_list	va;
+	int		ret;
+
+	if (strptr == NULL)
+		return -1;
+	va_start(va, format);
+	ret=vasprintf(strptr, format, va);
+	va_end(va);
+	return ret;
+}
+#endif
 
 /* Maximum length of a format specifier including the % */
 #define MAX_FORMAT_LEN	256
@@ -307,15 +334,13 @@ char* DLLCALL xp_asprintf_next(char *format, int type, ...)
 	unsigned long	offset2=0;
 	size_t			format_len;
 	size_t			this_format_len;
-	char			entry_buf[MAX_ARG_LEN];
+	char			int_buf[MAX_FORMAT_LEN];
 	char			*entry;
 	char			this_format[MAX_FORMAT_LEN];
 	char			*fmt;
 	int				modifier=0;
 	int				correct_type=0;
 	char			num_str[128];		/* More than enough room for a 256-bit int */
-	size_t			width=0;
-	size_t			precision=0;
 
 	/*
 	 * Check if we're already done...
@@ -366,7 +391,7 @@ char* DLLCALL xp_asprintf_next(char *format, int type, ...)
 	 */
 	if(*p=='*') {		/* The argument is this width */
 		va_start(vars, type);
-		i=sprintf(entry_buf,"%d", va_arg(vars, int));
+		i=sprintf(int_buf,"%d", va_arg(vars, int));
 		va_end(vars);
 		if(i > 1) {
 			/*
@@ -383,19 +408,17 @@ char* DLLCALL xp_asprintf_next(char *format, int type, ...)
 			 * Move trailing end to make space... leaving the * where it
 			 * is so it can be overwritten
 			 */
-			memmove(p+i, p+1, format-p+format_len);
-			memcpy(p, entry_buf, i);
+			memmove(p+i, p+1, format-p+format_len-1);
+			memcpy(p, int_buf, i);
 			*(size_t *)(format+sizeof(size_t))+=i-1;
 		}
 		else
-			*p=entry_buf[0];
+			*p=int_buf[0];
 		p=format+offset;
 		*(size_t *)format=p-format;
 		return(format);
 	}
 	/* Skip width */
-	if(*p >= '0' && *p <= '9')
-		width=strtoul(p, NULL, 10);
 	while(*p >= '0' && *p <= '9')
 		*(fmt++)=*(p++);
 	/* Check for precision */
@@ -407,7 +430,7 @@ char* DLLCALL xp_asprintf_next(char *format, int type, ...)
 		 */
 		if(*p=='*') {
 			va_start(vars, type);
-			i=sprintf(entry_buf,"%d", va_arg(vars, int));
+			i=sprintf(int_buf,"%d", va_arg(vars, int));
 			va_end(vars);
 			if(i > 1) {
 				/*
@@ -424,19 +447,17 @@ char* DLLCALL xp_asprintf_next(char *format, int type, ...)
 				 * Move trailing end to make space... leaving the * where it
 				 * is so it can be overwritten
 				 */
-				memmove(p+i, p+1, format-p+format_len);
-				memcpy(p, entry_buf, i);
+				memmove(p+i, p+1, format-p+format_len-1);
+				memcpy(p, int_buf, i);
 				*(size_t *)(format+sizeof(size_t))+=i-1;
 			}
 			else
-				*p=entry_buf[0];
+				*p=int_buf[0];
 			p=format+offset;
 			*(size_t *)format=p-format;
 			return(format);
 		}
 		/* Skip precision */
-		if(*p >= '0' && *p <= '9')
-			precision=strtoul(p, NULL, 10);
 		while(*p >= '0' && *p <= '9')
 			*(fmt++)=*(p++);
 	}
@@ -1154,64 +1175,59 @@ char* DLLCALL xp_asprintf_next(char *format, int type, ...)
 		}
 	}
 
-	/* The next char is now the type... perform native sprintf() using it */
+	/* The next char is now the type... check the length required to spore the printf()ed string */
 	*(fmt++)=*p;
 	*fmt=0;
-	entry=entry_buf;
+	entry=NULL;
 	switch(type) {
 		case XP_PRINTF_TYPE_CHAR:	/* Also includes char and short */
 		case XP_PRINTF_TYPE_INT:	/* Also includes char and short */
-			j=sprintf(entry, this_format, i);
+			j=asprintf(&entry, this_format, i);
 			break;
 		case XP_PRINTF_TYPE_UINT:	/* Also includes char and short */
-			j=sprintf(entry, this_format, ui);
+			j=asprintf(&entry, this_format, ui);
 			break;
 		case XP_PRINTF_TYPE_LONG:
-			j=sprintf(entry, this_format, l);
+			j=asprintf(&entry, this_format, l);
 			break;
 		case XP_PRINTF_TYPE_ULONG:
-			j=sprintf(entry, this_format, ul);
+			j=asprintf(&entry, this_format, ul);
 			break;
 #if defined(XP_PRINTF_TYPE_LONGLONG)
 		case XP_PRINTF_TYPE_LONGLONG:
-			j=sprintf(entry, this_format, ll);
+			j=asprintf(&entry, this_format, ll);
 			break;
 		case XP_PRINTF_TYPE_ULONGLONG:
-			j=sprintf(entry, this_format, ull);
+			j=asprintf(&entry, this_format, ull);
 			break;
 #endif
 		case XP_PRINTF_TYPE_CHARP:
 			if(cp==NULL)
-				j=sprintf(entry, this_format, "<null>");
-			else {
-				s=strlen(cp);
-				if(s<width)
-					s=width;
-				if(s<precision)
-					s=precision;
-				if(s>=MAX_ARG_LEN)
-					entry=(char *)alloca(s+1);
-				if(entry==NULL)
-					return(NULL);
-				j=sprintf(entry, this_format, cp);
-			}
+				j=asprintf(&entry, this_format, "<null>");
+			else
+				j=asprintf(&entry, this_format, cp);
 			break;
 		case XP_PRINTF_TYPE_DOUBLE:
-			j=sprintf(entry, this_format, d);
+			j=asprintf(&entry, this_format, d);
 			break;
 		case XP_PRINTF_TYPE_LONGDOUBLE:
-			j=sprintf(entry, this_format, ld);
+			j=asprintf(&entry, this_format, ld);
 			break;
 		case XP_PRINTF_TYPE_VOIDP:
-			j=sprintf(entry, this_format, pntr);
+			j=asprintf(&entry, this_format, pntr);
 			break;
 		case XP_PRINTF_TYPE_SIZET:
-			j=sprintf(entry, this_format, s);
+			j=asprintf(&entry, this_format, s);
+			break;
+		default:
+			j = -1;
+			entry = NULL;
 			break;
 	}
 
 	if (j<0) {
-		strcmp(entry, "<error>");
+		FREE_AND_NULL(entry);
+		entry = strdup("<error>");
 		j=strlen(entry);
 	}
 
@@ -1235,6 +1251,7 @@ char* DLLCALL xp_asprintf_next(char *format, int type, ...)
 	}
 	else
 		p=format+offset+this_format_len;
+	FREE_AND_NULL(entry);
 
 	*(size_t *)(format+sizeof(size_t))=format_len-this_format_len+j-sizeof(size_t)*2-1;
 
