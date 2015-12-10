4087192a2435ae31a4b21f4346227ac1a9d9f12c
rswindell <rswindell>
2015-12-10 20:01:14 +0000

    Change TO_NET and FROM_NET @-code logic and revert last change to show_msghdr() - it actually *is* an expected condition that a to_net.addr or from_net.addr msg header field is populated with a valid address (string) even though the to_net.type or from_net.type value is NET_NONE.
    The nntpservice.js and newslink.js modules rely on this behavior to display
    and export local NNTP-posters' email addresses in message headers, but still be
    able to identify newslink-imported messages as *not* local (the from_net.type =
    NET_INTERNET) and thus not export them back to the network.
    
    This was not the original intention behind these header fields in SMB, but it
    was retrofitted to support this condition 10+ years ago and I forgot.

--- sbbs3/getmsg.cpp
+++ sbbs3/getmsg.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet message retrieval functions */
 
-/* $Id: getmsg.cpp,v 1.52 2015/12/07 03:39:46 rswindell Exp $ */
+/* $Id: getmsg.cpp,v 1.53 2015/12/10 20:01:15 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -140,7 +140,7 @@ void sbbs_t::show_msghdr(smbmsg_t* msg)
 		show_msgattr(msg->hdr.attr);
 
 	bprintf(text[MsgTo],msg->to);
-	if(msg->to_net.type!=NET_NONE && msg->to_net.addr!=NULL)
+	if(msg->to_net.addr!=NULL)
 		bprintf(text[MsgToNet],smb_netaddrstr(&msg->to_net,str));
 	if(msg->to_ext)
 		bprintf(text[MsgToExt],msg->to_ext);
@@ -148,7 +148,7 @@ void sbbs_t::show_msghdr(smbmsg_t* msg)
 		bprintf(text[MsgFrom],msg->from);
 		if(msg->from_ext)
 			bprintf(text[MsgFromExt],msg->from_ext);
-		if(msg->from_net.type!=NET_NONE && msg->from_net.addr!=NULL && strchr(msg->from,'@')==NULL)
+		if(msg->from_net.addr!=NULL && strchr(msg->from,'@')==NULL)
 			bprintf(text[MsgFromNet],smb_netaddrstr(&msg->from_net,str)); 
 	}
 	bprintf(text[MsgDate]
