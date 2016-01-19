ddf29fa8806fec171842823ab8b0b802907fb09b
rswindell <rswindell>
2016-01-19 08:10:30 +0000

    New functions for dealing with named_ulong's: iniReadNamedLongInt() iniGetNamedLongInt() iniSetNamedLongInt() iniSetNamedHexInt()

--- sbbs3/pack_rep.cpp
+++ sbbs3/pack_rep.cpp
@@ -1,6 +1,8 @@
+/* pack_rep.cpp */
+
 /* Synchronet QWK reply (REP) packet creation routine */
 
-/* $Id: pack_rep.cpp,v 1.42 2016/11/10 10:06:30 rswindell Exp $ */
+/* $Id: pack_rep.cpp,v 1.41 2015/05/02 03:20:55 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -58,7 +60,6 @@ bool sbbs_t::pack_rep(uint hubnum)
 	mail_t*		mail;
 	FILE*		rep;
 	FILE*		hdrs=NULL;
-	FILE*		voting=NULL;
 	DIR*		dir;
 	DIRENT*		dirent;
 	smbmsg_t	msg;
@@ -99,10 +100,6 @@ bool sbbs_t::pack_rep(uint hubnum)
 	fexistcase(str);
 	if((hdrs=fopen(str,"a"))==NULL)
 		errormsg(WHERE,ERR_CREATE,str,0);
-	SAFEPRINTF(str,"%sVOTING.DAT",cfg.temp_dir);
-	fexistcase(str);
-	if((voting=fopen(str,"a"))==NULL)
-		errormsg(WHERE,ERR_CREATE,str,0);
 
 	/*********************/
 	/* Pack new messages */
@@ -114,8 +111,6 @@ bool sbbs_t::pack_rep(uint hubnum)
 		fclose(rep);
 		if(hdrs!=NULL)
 			fclose(hdrs);
-		if(voting!=NULL)
-			fclose(voting);
 		errormsg(WHERE,ERR_OPEN,smb.file,i,smb.last_error);
 		return(false); 
 	}
@@ -183,7 +178,7 @@ bool sbbs_t::pack_rep(uint hubnum)
 			continue; 
 		}
 
-		post=loadposts(&posts,j,subscan[j].ptr,LP_BYSELF|LP_OTHERS|LP_PRIVATE|LP_REP|LP_VOTES|LP_POLLS,NULL);
+		post=loadposts(&posts,j,subscan[j].ptr,LP_BYSELF|LP_OTHERS|LP_PRIVATE|LP_REP,NULL);
 		eprintf(LOG_INFO,remove_ctrl_a(text[NScanStatusFmt],tmp)
 			,cfg.grp[cfg.sub[j]->grp]->sname
 			,cfg.sub[j]->lname,posts,msgs);
@@ -222,7 +217,7 @@ bool sbbs_t::pack_rep(uint hubnum)
 			if(msg.from_net.type!=NET_QWK)
 				mode|=QM_TAGLINE;
 
-			msgtoqwk(&msg,rep,mode,j,cfg.qhub[hubnum]->conf[i],hdrs,voting);
+			msgtoqwk(&msg,rep,mode,j,cfg.qhub[hubnum]->conf[i],hdrs);
 
 			smb_freemsgmem(&msg);
 			smb_unlockmsghdr(&smb,&msg);
@@ -239,8 +234,6 @@ bool sbbs_t::pack_rep(uint hubnum)
 
 	if(hdrs!=NULL)
 		fclose(hdrs);
-	if(voting!=NULL)
-		fclose(voting);
 	fclose(rep);			/* close HUB_ID.MSG */
 	CRLF;
 							/* Look for extra files to send out */
