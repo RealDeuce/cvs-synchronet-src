e97850db72baac31a8c1d1582e8aed871a1a33bd
rswindell <rswindell>
2016-01-19 08:10:30 +0000

    New functions for dealing with named_ulong's: iniReadNamedLongInt() iniGetNamedLongInt() iniSetNamedLongInt() iniSetNamedHexInt()

--- sbbs3/ctrl/MainFormUnit.h
+++ sbbs3/ctrl/MainFormUnit.h
@@ -1,12 +1,12 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: MainFormUnit.h,v 1.84 2015/08/14 08:08:31 rswindell Exp $ */
+/* $Id: MainFormUnit.h,v 1.85 2015/11/30 09:18:51 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -227,6 +227,7 @@ __published:	// IDE-managed Components
     TMenuItem *BBSEditEmailFilterMenuItem;
     TMenuItem *BBSEditBadEmailMessageMenuItem;
     TMenuItem *ViewHackAttemptLogMenuItem;
+	TMenuItem *ViewGuruChatLogMenuItem;
     TMenuItem *ConfigureBBSTrayMenuItem;
     TMenuItem *ConfigureTelnetTrayMenuItem;
     TMenuItem *ConfigureFtpTrayMenuItem;
--- xpdev/ini_file.c
+++ xpdev/ini_file.c
@@ -2,7 +2,7 @@
 
 /* Functions to create and parse .ini files */
 
-/* $Id: ini_file.c,v 1.143 2015/09/28 06:58:12 rswindell Exp $ */
+/* $Id: ini_file.c,v 1.145 2016/01/19 08:10:30 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -570,6 +570,28 @@ char* DLLCALL iniSetBytes(str_list_t* list, const char* section, const char* key
 	return iniSetString(list, section, key, str, style);
 }
 
+char* DLLCALL iniSetDuration(str_list_t* list, const char* section, const char* key
+					,double value, ini_style_t* style)
+{
+	char	str[INI_MAX_VALUE_LEN];
+
+	if(fmod(value,365.0*24.0*60.0*60.0)==0)
+		SAFEPRINTF(str,"%gY",value/(365.0*24.0*60.0*60.0));
+	else if(fmod(value,7.0*24.0*60.0*60.0)==0)
+		SAFEPRINTF(str,"%gW",value/(7.0*24.0*60.0*60.0));
+	else if(fmod(value,24.0*60.0*60.0)==0)
+		SAFEPRINTF(str,"%gD",value/(24.0*60.0*60.0));
+	else if(fmod(value,60.0*60.0)==0)
+		SAFEPRINTF(str,"%gH",value/(60.0*60.0));
+	else if(fmod(value,60.0)==0)
+		SAFEPRINTF(str,"%gM",value/60.0);
+	else
+		SAFEPRINTF(str,"%gS",value);
+
+	return iniSetString(list, section, key, str, style);
+}
+
+
 #if !defined(NO_SOCKET_SUPPORT)
 char* DLLCALL iniSetIpAddress(str_list_t* list, const char* section, const char* key, ulong value
 					,ini_style_t* style)
@@ -663,6 +685,30 @@ char* DLLCALL iniSetNamedInt(str_list_t* list, const char* section, const char* 
 			return iniSetString(list, section, key, names[i].name, style);
 
 	return iniSetInteger(list, section, key, value, style);
+}
+
+char* DLLCALL iniSetNamedHexInt(str_list_t* list, const char* section, const char* key, named_ulong_t* names
+					 ,ulong value, ini_style_t* style)
+{
+	size_t	i;
+
+	for(i=0;names[i].name!=NULL;i++)
+		if(names[i].value==value)
+			return iniSetString(list, section, key, names[i].name, style);
+
+	return iniSetHexInt(list, section, key, value, style);
+}
+
+char* DLLCALL iniSetNamedLongInt(str_list_t* list, const char* section, const char* key, named_ulong_t* names
+					 ,ulong value, ini_style_t* style)
+{
+	size_t	i;
+
+	for(i=0;names[i].name!=NULL;i++)
+		if(names[i].value==value)
+			return iniSetString(list, section, key, names[i].name, style);
+
+	return iniSetLongInt(list, section, key, value, style);
 }
 
 char* DLLCALL iniSetNamedFloat(str_list_t* list, const char* section, const char* key, named_double_t* names
@@ -1270,6 +1316,32 @@ int64_t DLLCALL iniGetBytes(str_list_t list, const char* section, const char* ke
 	return(parse_byte_count(vp,unit));
 }
 
+double DLLCALL iniReadDuration(FILE* fp, const char* section, const char* key, double deflt)
+{
+	char*	value;
+	char	buf[INI_MAX_VALUE_LEN];
+
+	if((value=read_value(fp,section,key,buf))==NULL)
+		return(deflt);
+
+	if(*value==0)		/* blank value */
+		return(deflt);
+
+	return(parse_duration(value));
+}
+
+double DLLCALL iniGetDuration(str_list_t list, const char* section, const char* key, double deflt)
+{
+	char*	vp=NULL;
+
+	get_value(list, section, key, NULL, &vp);
+
+	if(vp==NULL || *vp==0)	/* blank value or missing key */
+		return(deflt);
+
+	return(parse_duration(vp));
+}
+
 #if !defined(NO_SOCKET_SUPPORT)
 
 int DLLCALL iniGetSocketOptions(str_list_t list, const char* section, SOCKET sock
@@ -1839,6 +1911,51 @@ long DLLCALL iniGetNamedInt(str_list_t list, const char* section, const char* ke
 		return(deflt);
 
 	return(parseNamedInt(vp,names));
+}
+
+static ulong parseNamedLongInt(const char* value, named_ulong_t* names)
+{
+	unsigned i;
+
+	/* Look for exact matches first */
+	for(i=0; names[i].name!=NULL; i++)
+		if(stricmp(names[i].name,value)==0)
+			return(names[i].value);
+
+	/* Look for partial matches second */
+	for(i=0; names[i].name!=NULL; i++)
+		if(strnicmp(names[i].name,value,strlen(value))==0)
+			return(names[i].value);
+
+	return(parseLongInteger(value));
+}
+
+ulong DLLCALL iniReadNamedLongInt(FILE* fp, const char* section, const char* key
+					 ,named_ulong_t* names, ulong deflt)
+{
+	char	buf[INI_MAX_VALUE_LEN];
+	char*	value;
+
+	if((value=read_value(fp,section,key,buf))==NULL)
+		return(deflt);
+
+	if(*value==0)		/* blank value */
+		return(deflt);
+
+	return(parseNamedLongInt(value,names));
+}
+
+ulong DLLCALL iniGetNamedLongInt(str_list_t list, const char* section, const char* key
+					,named_ulong_t* names, ulong deflt)
+{
+	char*	vp=NULL;
+
+	get_value(list, section, key, NULL, &vp);
+
+	if(vp==NULL || *vp==0)		/* blank value or missing key */
+		return(deflt);
+
+	return(parseNamedLongInt(vp,names));
 }
 
 static double parseNamedFloat(const char* value, named_double_t* names)
