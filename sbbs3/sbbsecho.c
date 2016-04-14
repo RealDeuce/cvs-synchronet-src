5451bce64b33e75d36e6c485f481e3a3d8e270cd
rswindell <rswindell>
2016-04-14 02:08:43 +0000

    Better error message for AREAS.BBS parsing problem (discovered by Lord Time): Display parsed FTN addresss instead of string from AREAS.BBS file. Hopefully this provides a better clue as to what is happening with his configuration.

--- sbbs3/sbbsecho.c
+++ sbbs3/sbbsecho.c
@@ -2,7 +2,7 @@
 
 /* Synchronet FidoNet EchoMail Scanning/Tossing and NetMail Tossing Utility */
 
-/* $Id: sbbsecho.c,v 3.1 2016/04/11 11:40:52 rswindell Exp $ */
+/* $Id: sbbsecho.c,v 3.2 2016/04/14 02:08:43 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -4838,7 +4838,7 @@ int main(int argc, char **argv)
 		memset(&smb[i],0,sizeof(smb_t));
 	memset(&cfg,0,sizeof(cfg));
 
-	sscanf("$Revision: 3.1 $", "%*s %s", revision);
+	sscanf("$Revision: 3.2 $", "%*s %s", revision);
 
 	DESCRIBE_COMPILER(compiler);
 
@@ -5089,7 +5089,7 @@ int main(int argc, char **argv)
 			cfg.area[cfg.areas].link[cfg.area[cfg.areas].links] = link;
 			if(findnodecfg(&cfg, link, /* exact: */true) == NULL)
 				lprintf(LOG_WARNING, "Configuration for %s-linked-node (%s) not found in %s"
-					,cfg.area[cfg.areas].name, p, cfg.cfgfile);
+					,cfg.area[cfg.areas].name, faddrtoa(&link), cfg.cfgfile);
 			else
 				cfg.area[cfg.areas].links++; 
 			FIND_WHITESPACE(p);	/* Skip address */
