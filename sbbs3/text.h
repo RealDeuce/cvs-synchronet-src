1ed8e524b1f28c693c2277a125f8f449be8890b2
rswindell <rswindell>
2016-04-14 02:08:43 +0000

    Better error message for AREAS.BBS parsing problem (discovered by Lord Time): Display parsed FTN addresss instead of string from AREAS.BBS file. Hopefully this provides a better clue as to what is happening with his configuration.

--- sbbs3/text.h
+++ sbbs3/text.h
@@ -2,7 +2,7 @@
 
 /* Synchronet static text string constants */
 
-/* $Id: text.h,v 1.23 2016/11/10 10:06:31 rswindell Exp $ */
+/* $Id: text.h,v 1.22 2015/11/30 09:07:44 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -822,13 +822,6 @@ enum {
 	,InTheFuture
 	,AgeOfPostedItem
 	,NodeStatusLogout
-	,VotingNotAllowed
-	,VotedAlready
-	,QWKSettingsVoting
-	,VoteMsgUpDownOrQuit
-	,MsgVotes
-	,MsgVoteNotice
-	,R_Voting
 
 	,TOTAL_TEXT
 };
