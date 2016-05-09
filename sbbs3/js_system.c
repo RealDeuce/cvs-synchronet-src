c07825280adecf9c41d3a47bb59fbb734041f3b9
rswindell <rswindell>
2016-05-09 05:50:06 +0000

    Fix login_attempted() for IPv4 at least (I can't test for IPv6, Deuce?). This has been broken (did not track consecutive failed login attempts from the same client IP address) since the IPv6 commit of Aug-2015. I only noticed this wasn't working when I was documenting the "auto-block hackers" feature on the Wiki and noticed that every failed login (even those from the same IP) was listed as a separate entry in the "Failed Logins" list (e.g. sbbsctrl->View->Login Attempts). Ugh.

--- sbbs3/js_system.c
+++ sbbs3/js_system.c
@@ -1,13 +1,14 @@
-/* Synchronet JavaScript "system" Object */
-// vi: tabstop=4
+/* js_system.c */
 
-/* $Id: js_system.c,v 1.166 2017/11/16 09:41:37 rswindell Exp $ */
+/* Synchronet JavaScript "system" Object */
+
+/* $Id: js_system.c,v 1.162 2015/11/10 22:53:27 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -637,12 +638,12 @@ static JSBool js_sysstats_get(JSContext *cx, JSObject *obj, jsid id, jsval *vp)
 			break;
 		case SYSSTAT_PROP_TOTALMAIL:
 			rc=JS_SUSPENDREQUEST(cx);
-			*vp = INT_TO_JSVAL(getmail(cfg, /* user: */0, /* Sent: */FALSE, /* SPAM: */FALSE));
+			*vp = INT_TO_JSVAL(getmail(cfg, 0,0));
 			JS_RESUMEREQUEST(cx, rc);
 			break;
 		case SYSSTAT_PROP_FEEDBACK:
 			rc=JS_SUSPENDREQUEST(cx);
-			*vp = INT_TO_JSVAL(getmail(cfg, /* user: */1, /* Sent: */FALSE, /* SPAM: */FALSE));
+			*vp = INT_TO_JSVAL(getmail(cfg, 1,0));
 			JS_RESUMEREQUEST(cx, rc);
 			break;
 	}
@@ -887,7 +888,7 @@ js_matchuserdata(JSContext *cx, uintN argc, jsval *arglist)
 	}
 	
 	rc=JS_SUSPENDREQUEST(cx);
-	JS_SET_RVAL(cx, arglist, INT_TO_JSVAL(userdatdupe(cfg,usernumber,offset,len,p,FALSE,match_next,NULL,NULL)));
+	JS_SET_RVAL(cx, arglist, INT_TO_JSVAL(userdatdupe(cfg,usernumber,offset,len,p,FALSE,match_next)));
 	JS_RESUMEREQUEST(cx, rc);
 	return(JS_TRUE);
 }
@@ -1659,11 +1660,6 @@ js_sys_exec(JSContext *cx, uintN argc, jsval *arglist)
 		JS_ReportError(cx, "Illegal NULL command");
 		return JS_FALSE;
 	}
-	if(*cmd == 0) {
-		free(cmd);
-		JS_ReportError(cx, "Missing or invalid argument");
-		return JS_FALSE;
-	}
 	rc=JS_SUSPENDREQUEST(cx);
 	ret=system(cmd);
 	free(cmd);
@@ -1906,7 +1902,7 @@ static jsSyncMethodSpec js_system_functions[] = {
 	,316
 	},
 #endif
-	{"exec",			js_sys_exec,		0,	JSTYPE_NUMBER,	JSDOCSTR("command-line")
+	{"exec",			js_sys_exec,		1,	JSTYPE_NUMBER,	JSDOCSTR("command-line")
 	,JSDOCSTR("executes a native system/shell command-line, returns <i>0</i> on success")
 	,311
 	},
