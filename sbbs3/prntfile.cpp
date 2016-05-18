67f1a036606f4e2ccb538a21b28e4cb1862e082f
rswindell <rswindell>
2016-05-18 10:20:17 +0000

    If a full path is passed to menu(), use it as the base menu filename (don't prepend the text directory).

--- sbbs3/prntfile.cpp
+++ sbbs3/prntfile.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet file print/display routines */
 
-/* $Id$ */
+/* $Id: prntfile.cpp,v 1.21 2016/05/18 10:20:17 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -193,12 +193,16 @@ void sbbs_t::menu(const char *code)
 	if(menu_file[0])
 		strcpy(path,menu_file);
 	else {
-		sprintf(str,"%smenu/",cfg.text_dir);
-		if(menu_dir[0]) {
-			strcat(str,menu_dir);
-			strcat(str,"/"); 
+		if(isfullpath(code))
+			SAFECOPY(str, code);
+		else {
+			sprintf(str,"%smenu/",cfg.text_dir);
+			if(menu_dir[0]) {
+				strcat(str,menu_dir);
+				strcat(str,"/"); 
+			}
+			strcat(str,code);
 		}
-		strcat(str,code);
 		strcat(str,".");
 		sprintf(path,"%s%s",str,term_supports(WIP) ? "wip": term_supports(RIP) ? "rip" : "html");
 		if(!(term_supports()&(RIP|WIP|HTML)) || !fexistcase(path)) {
