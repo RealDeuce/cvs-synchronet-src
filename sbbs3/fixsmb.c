323f19199b635cce8bbc8e8664c166eff8e0602a
rswindell <rswindell>
2016-05-19 05:18:27 +0000

    Implement IP temporary ban: By default, after 20 consecutive (unique) failed login attempts, *or* a failed login attempt wtih a name from the name.can filter file. The default temporary ban duration is 10 minutes. The temporary ban thershold is configurable via LoginAttemptTempBanThreshold in sbbs.ini and the ban duration is configurable via LoginAttemptTempBanDuration (in seconds).

--- sbbs3/fixsmb.c
+++ sbbs3/fixsmb.c
@@ -1,13 +1,14 @@
+/* fixsmb.c */
+
 /* Synchronet message base (SMB) index re-generator */
 
-/* $Id: fixsmb.c,v 1.41 2017/11/28 06:41:45 rswindell Exp $ */
-// vi: tabstop=4
+/* $Id: fixsmb.c,v 1.38 2015/08/22 06:29:20 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2015 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -34,7 +35,7 @@
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
 
-#include <stdio.h>
+#include <stdio.h>	
 #include <stdlib.h>	/* atoi, qsort */
 #include <string.h>	/* strnicmp */
 #include <ctype.h>	/* toupper */
@@ -105,9 +106,6 @@ int fixsmb(char* sub)
 	int 		i,w;
 	ulong		l,length,size,n;
 	smbmsg_t	msg;
-	uint32_t*	numbers = NULL;
-	uint32_t	total = 0;
-	BOOL		dupe_msgnum;
 
 	memset(&smb,0,sizeof(smb));
 
@@ -120,7 +118,7 @@ int fixsmb(char* sub)
 
 	if((i=smb_open(&smb))!=0) {
 		printf("smb_open returned %d: %s\n",i,smb.last_error);
-		exit(1);
+		exit(1); 
 	}
 
 	if((i=smb_lock(&smb))!=0) {
@@ -131,14 +129,14 @@ int fixsmb(char* sub)
 	if((i=smb_locksmbhdr(&smb))!=0) {
 		smb_close(&smb);
 		printf("smb_locksmbhdr returned %d: %s\n",i,smb.last_error);
-		exit(1);
+		exit(1); 
 	}
 
 	if((i=smb_getstatus(&smb))!=0) {
 		smb_unlocksmbhdr(&smb);
 		smb_close(&smb);
 		printf("smb_getstatus returned %d: %s\n",i,smb.last_error);
-		exit(1);
+		exit(1); 
 	}
 
 	if(!(smb.status.attr&SMB_HYPERALLOC)) {
@@ -146,13 +144,13 @@ int fixsmb(char* sub)
 		if((i=smb_open_ha(&smb))!=0) {
 			smb_close(&smb);
 			printf("smb_open_ha returned %d: %s\n",i,smb.last_error);
-			exit(1);
+			exit(1); 
 		}
 
 		if((i=smb_open_da(&smb))!=0) {
 			smb_close(&smb);
 			printf("smb_open_da returned %d: %s\n",i,smb.last_error);
-			exit(1);
+			exit(1); 
 		}
 
 		rewind(smb.sha_fp);
@@ -182,8 +180,8 @@ int fixsmb(char* sub)
 
 		length=filelength(fileno(smb.shd_fp));
 		c=0;
-		for(l=smb.status.header_offset;l<length;l+=SHD_BLOCK_LEN)	/* Init .SHD file to NULL */
-			fwrite(&c,1,1,smb.sha_fp);
+		for(l=0;l<length;l+=SHD_BLOCK_LEN)	/* Init .SHD file to NULL */
+			fwrite(&c,1,1,smb.sha_fp); 
 	} else
 		length=filelength(fileno(smb.shd_fp));
 
@@ -195,34 +193,18 @@ int fixsmb(char* sub)
 		msg.idx.offset=l;
 		if((i=smb_lockmsghdr(&smb,&msg))!=0) {
 			printf("\n(%06lX) smb_lockmsghdr returned %d:\n%s\n",l,i,smb.last_error);
-			continue;
+			continue; 
 		}
 		i=smb_getmsghdr(&smb,&msg);
 		smb_unlockmsghdr(&smb,&msg);
 		if(i!=0) {
 			printf("\n(%06lX) smb_getmsghdr returned %d:\n%s\n",l,i,smb.last_error);
-			continue;
+			continue; 
 		}
 		size=smb_hdrblocks(smb_getmsghdrlen(&msg))*SHD_BLOCK_LEN;
 		printf("#%-5"PRIu32" (%06lX) %-25.25s ",msg.hdr.number,l,msg.from);
 
-		dupe_msgnum = FALSE;
-		for(i=0; i<total && !dupe_msgnum; i++)
-			if(msg.hdr.number == numbers[i])
-				dupe_msgnum = TRUE;
-
-		if(!dupe_msgnum) {
-			total++;
-			if((numbers = realloc(numbers, total * sizeof(*numbers))) == NULL) {
-				fprintf(stderr, "realloc failure: %lu\n", total * sizeof(*numbers));
-				return EXIT_FAILURE;
-			}
-			numbers[total-1] = msg.hdr.number;
-		}
-
-		if(dupe_msgnum)
-			msg.hdr.attr|=MSG_DELETE;
-		else if(smb_undelete)
+		if(smb_undelete)
 			msg.hdr.attr&=~MSG_DELETE;
 
 		/* Create hash record */
@@ -237,13 +219,11 @@ int fixsmb(char* sub)
 			free(text);
 
 		/* Index the header */
-		if(dupe_msgnum)
-			printf("Not indexing duplicate message number (%u)\n", msg.hdr.number);
-		else if(msg.hdr.attr&MSG_DELETE)
+		if(msg.hdr.attr&MSG_DELETE)
 			printf("Not indexing deleted message\n");
 		else if(msg.hdr.number==0)
 			printf("Not indexing invalid message number (0)!\n");
-		else {
+		else {   
 			msg.offset=n;
 			if(renumber)
 				msg.hdr.number=n+1;
@@ -253,9 +233,9 @@ int fixsmb(char* sub)
 			}
 			if((i=smb_putmsg(&smb,&msg))!=0) {
 				printf("\nsmb_putmsg returned %d: %s\n",i,smb.last_error);
-				continue;
+				continue; 
 			}
-			n++;
+			n++; 
 		}
 
 		if(!(smb.status.attr&SMB_HYPERALLOC)) {
@@ -277,7 +257,7 @@ int fixsmb(char* sub)
 				smb_incmsg_dfields(&smb,&msg,1);
 		}
 
-		smb_freemsgmem(&msg);
+		smb_freemsgmem(&msg); 
 	}
 	printf("\r%79s\r100%%\n","");
 	smb.status.total_msgs=n;
@@ -293,7 +273,6 @@ int fixsmb(char* sub)
 	smb_close(&smb);
 	unlock_msgbase();
 	printf("Done.\n");
-	FREE_AND_NULL(numbers);
 	return(0);
 }
 
@@ -302,9 +281,8 @@ int main(int argc, char **argv)
 	char		revision[16];
 	int 		i;
 	str_list_t	list;
-	int			retval = EXIT_SUCCESS;
 
-	sscanf("$Revision: 1.41 $", "%*s %s", revision);
+	sscanf("$Revision: 1.38 $", "%*s %s", revision);
 
 	printf("\nFIXSMB v2.10-%s (rev %s) SMBLIB %s - Rebuild Synchronet Message Base\n\n"
 		,PLATFORM_DESC,revision,smb_lib_ver());
@@ -323,13 +301,13 @@ int main(int argc, char **argv)
 
 	if(!strListCount(list)) {
 		puts(usage);
-		exit(1);
+		exit(1); 
 	}
 
 	atexit(unlock_msgbase);
 
-	for(i=0;list[i]!=NULL && retval == EXIT_SUCCESS;i++)
-		retval = fixsmb(list[i]);
+	for(i=0;list[i]!=NULL;i++)
+		fixsmb(list[i]);
 
-	return retval;
+	return(0);
 }
