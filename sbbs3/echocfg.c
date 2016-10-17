39f935b23c3771655a9e1505d0700fd8e07c5fe2
rswindell <rswindell>
2016-10-17 21:44:54 +0000

    Updated command-line help for v3.

--- sbbs3/echocfg.c
+++ sbbs3/echocfg.c
@@ -2,7 +2,7 @@
 
 /* SBBSecho configuration utility 											*/
 
-/* $Id: echocfg.c,v 3.4 2016/04/21 01:51:02 deuce Exp $ */
+/* $Id: echocfg.c,v 3.6 2016/10/17 21:44:54 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -137,7 +137,7 @@ int main(int argc, char **argv)
                     break;
                 default:
 					USAGE:
-                    printf("\nusage: echocfg [ctrl_dir] [options]"
+                    printf("\nusage: echocfg [path/to/sbbsecho.ini] [options]"
                         "\n\noptions:\n\n"
                         "-c  =  force color mode\r\n"
 						"-m  =  force monochrome mode\r\n"
@@ -165,7 +165,7 @@ int main(int argc, char **argv)
 		if(!p) {
 			p=getenv("SBBSNODE");
 			if(!p) {
-				printf("usage: echocfg [cfg_file]\n");
+				goto USAGE;
 				exit(1); 
 			}
 			strcpy(str,p);
@@ -758,6 +758,8 @@ int main(int argc, char **argv)
 				j=0;
 				while(1) {
 					uifc.helpbuf=
+	"~ NetMail Settings ~\r\n"
+	"\r\n"
 	"`Sysop Aliases` is a comma-separated list of names by which the sysop\r\n"
 	"    (user #1) may receive NetMail messages, in addition to the alias\r\n"
 	"    and real name associated with their BBS user account.\r\n"
@@ -918,6 +920,8 @@ int main(int argc, char **argv)
 				j=0;
 				while(1) {
 					uifc.helpbuf=
+	"~ EchoMail Settings ~\r\n"
+	"\r\n"
 	"`Area Manager` is the BBS user name or alias to notify (via email) of\r\n"
 	"    AreaFix activities and errors.  This setting defaults to `SYSOP`.\r\n"
 	"\r\n"
--- sbbs3/js_global.c
+++ sbbs3/js_global.c
@@ -2,7 +2,7 @@
 
 /* Synchronet JavaScript "global" object properties/methods for all servers */
 
-/* $Id: js_global.c,v 1.357 2016/02/26 07:19:55 rswindell Exp $ */
+/* $Id: js_global.c,v 1.362 2016/04/24 00:41:34 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -254,6 +254,9 @@ js_load(JSContext *cx, uintN argc, jsval *arglist)
 	global_private_t*	p;
 	jsval		val;
 	JSObject*	js_argv;
+	jsval		old_js_argv = JSVAL_VOID;
+	jsval		old_js_argc = JSVAL_VOID;
+	BOOL		restore_args = FALSE;
 	JSObject*	exec_obj;
 	JSObject*	js_internal;
 	JSContext*	exec_cx=cx;
@@ -448,6 +451,13 @@ js_load(JSContext *cx, uintN argc, jsval *arglist)
 			rc=JS_SUSPENDREQUEST(cx);
 			JS_RESUMEREQUEST(bg->cx, brc);
 		}
+		else {
+			JS_GetProperty(exec_cx, exec_obj, "argv", &old_js_argv);
+			JS_AddValueRoot(exec_cx, &old_js_argv);
+			JS_GetProperty(exec_cx, exec_obj, "argc", &old_js_argc);
+			JS_AddValueRoot(exec_cx, &old_js_argc);
+			restore_args = TRUE;
+		}
 
 		if((js_argv=JS_NewArrayObject(exec_cx, 0, NULL)) == NULL) {
 			if(background) {
@@ -598,6 +608,21 @@ js_load(JSContext *cx, uintN argc, jsval *arglist)
 			free(bg);
 			JS_RESUMEREQUEST(cx, rc);
 		}
+		// Restore args
+		if (restore_args) {
+			if (old_js_argv == JSVAL_VOID) {
+				JS_DeleteProperty(exec_cx, exec_obj, "argv");
+				JS_DeleteProperty(exec_cx, exec_obj, "argc");
+			}
+			else {
+				JS_DefineProperty(exec_cx, exec_obj, "argv", old_js_argv
+					,NULL,NULL,JSPROP_ENUMERATE|JSPROP_READONLY);
+				JS_DefineProperty(exec_cx, exec_obj, "argc", old_js_argc
+					,NULL,NULL,JSPROP_ENUMERATE|JSPROP_READONLY);
+			}
+		}
+		JS_RemoveValueRoot(exec_cx, &old_js_argv);
+		JS_RemoveValueRoot(exec_cx, &old_js_argc);
 		return(JS_FALSE);
 	}
 
@@ -624,9 +649,87 @@ js_load(JSContext *cx, uintN argc, jsval *arglist)
 
 		success = JS_ExecuteScript(exec_cx, exec_obj, script, &rval);
 		JS_SET_RVAL(cx, arglist, rval);
+		if (restore_args) {
+			if (old_js_argv == JSVAL_VOID) {
+				JS_DeleteProperty(exec_cx, exec_obj, "argv");
+				JS_DeleteProperty(exec_cx, exec_obj, "argc");
+			}
+			else {
+				JS_DefineProperty(exec_cx, exec_obj, "argv", old_js_argv
+					,NULL,NULL,JSPROP_ENUMERATE|JSPROP_READONLY);
+				JS_DefineProperty(exec_cx, exec_obj, "argc", old_js_argc
+					,NULL,NULL,JSPROP_ENUMERATE|JSPROP_READONLY);
+			}
+			JS_RemoveValueRoot(exec_cx, &old_js_argv);
+			JS_RemoveValueRoot(exec_cx, &old_js_argc);
+		}
 	}
 
     return(success);
+}
+
+/*
+ * This is hacky, but a but less hacky than using a magic '2'
+ * It does assume the args are always last though (which seems reasonable
+ * since it's variable length)
+ */
+#define JS_ARGS_OFFSET	((unsigned)(JS_ARGV(0, (jsval *)NULL))/sizeof(jsval *))
+
+static JSBool
+js_require(JSContext *cx, uintN argc, jsval *arglist)
+{
+	uintN argn = 0;
+	uintN fnarg;
+	JSObject*	exec_obj;
+	JSObject*	tmp_obj;
+    char*		property;
+    char*		filename;
+    JSBool		found = JS_FALSE;
+    JSBool		ret;
+	jsval *argv=JS_ARGV(cx, arglist);
+
+	exec_obj=JS_GetScopeChain(cx);
+	if(JSVAL_IS_BOOLEAN(argv[argn])) {
+		JS_ReportError(cx,"cannot require() background processes");
+		return(JS_FALSE);
+	}
+
+	if(JSVAL_IS_OBJECT(argv[argn])) {
+		tmp_obj=JSVAL_TO_OBJECT(argv[argn++]);
+		if(!JS_ObjectIsFunction(cx,tmp_obj))	/* Scope specified */
+			exec_obj=tmp_obj;
+	}
+
+	// Skip filename
+	fnarg = argn++;
+
+	if(argn==argc) {
+		JS_ReportError(cx,"no symbol name specified");
+		return(JS_FALSE);
+	}
+	JSVALUE_TO_MSTRING(cx, argv[argn], property, NULL);
+
+	// TODO: Does this support sub-objects?
+	if (JS_HasProperty(cx, exec_obj, property, &found) && found) {
+		JS_SET_RVAL(cx, arglist,JSVAL_VOID);
+		free(property);
+		return JS_TRUE;
+	}
+
+	// Remove symbol name from args
+	if (argc > argn+1)
+		memmove(&arglist[argn+JS_ARGS_OFFSET], &arglist[argn+JS_ARGS_OFFSET+1], sizeof(arglist[0]) * (argc - argn - 1));
+
+	ret = js_load(cx, argc-1, arglist);
+
+	if (!JS_HasProperty(cx, exec_obj, property, &found) || !found) {
+		JSVALUE_TO_MSTRING(cx, argv[fnarg], filename, NULL);
+		JS_ReportError(cx,"symbol '%s' not defined by script '%s'", property, filename);
+		free(filename);
+		return(JS_FALSE);
+	}
+	free(property);
+	return ret;
 }
 
 static JSBool
@@ -4180,6 +4283,18 @@ static jsSyncMethodSpec js_global_functions[] = {
 	"(returns number OR string) - (added in v3.13)")
 	,313
 	},
+	{"require",         js_require,         1,	JSTYPE_UNDEF
+	,JSDOCSTR("[<i>object</i> scope,] <i>string</i> filename, propname [,args]")
+	,JSDOCSTR("load and execute a JavaScript module (<i>filename</i>), "
+		"optionally specifying a target <i>scope</i> object (default: <i>this</i>) "
+		"and a list of arguments to pass to the module (as <i>argv</i>) "
+		"IF AND ONLY IF the property named <i>propname</i> is not defined in "
+		"the target scope (a defined symbol with a value of undefined will not "
+		"cause the script to be loaded). "
+		"Returns the result (last executed statement) of the executed script "
+		"or null if the script is not executed. ")
+	,317
+	},		
 	{0}
 };
 
