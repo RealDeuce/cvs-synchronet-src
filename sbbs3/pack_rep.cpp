210766c7e767af50172b86c6e09c682067ce362e
rswindell <rswindell>
2016-11-16 05:32:50 +0000

    Changed SMB_MSG_TYPE_VOTE to SMB_MSG_TYPE_BALLOT: There (will be) the possibilty to cast mutliple votes in a single vote-msg (ballot), when multi-choice polls are introduced later. There were just too many things call vote/votes and it was getting confusing in my own head. So the msg type for messages that contain votes is now "ballot".

--- sbbs3/pack_rep.cpp
+++ sbbs3/pack_rep.cpp
@@ -1,14 +1,12 @@
-/* pack_rep.cpp */
-
 /* Synchronet QWK reply (REP) packet creation routine */
 
-/* $Id: pack_rep.cpp,v 1.40 2012/10/24 19:03:13 deuce Exp $ */
+/* $Id: pack_rep.cpp,v 1.42 2016/11/10 10:06:30 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -60,6 +58,7 @@ bool sbbs_t::pack_rep(uint hubnum)
 	mail_t*		mail;
 	FILE*		rep;
 	FILE*		hdrs=NULL;
+	FILE*		voting=NULL;
 	DIR*		dir;
 	DIRENT*		dirent;
 	smbmsg_t	msg;
@@ -100,6 +99,10 @@ bool sbbs_t::pack_rep(uint hubnum)
 	fexistcase(str);
 	if((hdrs=fopen(str,"a"))==NULL)
 		errormsg(WHERE,ERR_CREATE,str,0);
+	SAFEPRINTF(str,"%sVOTING.DAT",cfg.temp_dir);
+	fexistcase(str);
+	if((voting=fopen(str,"a"))==NULL)
+		errormsg(WHERE,ERR_CREATE,str,0);
 
 	/*********************/
 	/* Pack new messages */
@@ -111,6 +114,8 @@ bool sbbs_t::pack_rep(uint hubnum)
 		fclose(rep);
 		if(hdrs!=NULL)
 			fclose(hdrs);
+		if(voting!=NULL)
+			fclose(voting);
 		errormsg(WHERE,ERR_OPEN,smb.file,i,smb.last_error);
 		return(false); 
 	}
@@ -178,7 +183,7 @@ bool sbbs_t::pack_rep(uint hubnum)
 			continue; 
 		}
 
-		post=loadposts(&posts,j,subscan[j].ptr,LP_BYSELF|LP_OTHERS|LP_PRIVATE|LP_REP,NULL);
+		post=loadposts(&posts,j,subscan[j].ptr,LP_BYSELF|LP_OTHERS|LP_PRIVATE|LP_REP|LP_VOTES|LP_POLLS,NULL);
 		eprintf(LOG_INFO,remove_ctrl_a(text[NScanStatusFmt],tmp)
 			,cfg.grp[cfg.sub[j]->grp]->sname
 			,cfg.sub[j]->lname,posts,msgs);
@@ -194,8 +199,8 @@ bool sbbs_t::pack_rep(uint hubnum)
 	//		bprintf("\b\b\b\b\b%-5lu",u+1);
 
 			memset(&msg,0,sizeof(msg));
-			msg.idx=post[u];
-			if(!loadmsg(&msg,post[u].number))
+			msg.idx=post[u].idx;
+			if(!loadmsg(&msg,post[u].idx.number))
 				continue;
 
 			if(msg.from_net.type && msg.from_net.type!=NET_QWK &&
@@ -217,7 +222,7 @@ bool sbbs_t::pack_rep(uint hubnum)
 			if(msg.from_net.type!=NET_QWK)
 				mode|=QM_TAGLINE;
 
-			msgtoqwk(&msg,rep,mode,j,cfg.qhub[hubnum]->conf[i],hdrs);
+			msgtoqwk(&msg,rep,mode,j,cfg.qhub[hubnum]->conf[i],hdrs,voting);
 
 			smb_freemsgmem(&msg);
 			smb_unlockmsghdr(&smb,&msg);
@@ -234,6 +239,8 @@ bool sbbs_t::pack_rep(uint hubnum)
 
 	if(hdrs!=NULL)
 		fclose(hdrs);
+	if(voting!=NULL)
+		fclose(voting);
 	fclose(rep);			/* close HUB_ID.MSG */
 	CRLF;
 							/* Look for extra files to send out */
