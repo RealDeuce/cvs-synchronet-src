ee326603d007c5e9d4ff92445adc71ee02a2bbab
rswindell <rswindell>
2016-11-19 21:14:32 +0000

    Fixed problem with votes and polls coming from QWKnet hubs (e.g. VERT) not being imported: resolve_qwkconf() now supports both users (e.g. QWKnet nodes uploading REP packets) and QWKnet hubs (e.g. importing a QWK packet from VERT). Also re-factored resolve_qwkconf() in the process (that was some *old* code).

--- conio/vidmodes.h
+++ conio/vidmodes.h
@@ -1,4 +1,4 @@
-/* $Id: vidmodes.h,v 1.25 2018/02/13 05:11:20 deuce Exp $ */
+/* $Id: vidmodes.h,v 1.20 2015/07/05 09:07:34 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -43,7 +43,7 @@
 
 #include "ciolib.h"
 
-#define TOTAL_DAC_SIZE	274
+#define TOTAL_DAC_SIZE	34
 
 /* Entry type for the DAC table. */
 struct dac_colors {
@@ -64,17 +64,9 @@ struct  video_params {
 	int	vmultiplier;
 };
 
-struct vmem_cell {
-	uint8_t legacy_attr;
-	uint8_t ch;
-	uint8_t font;
-	uint32_t fg;	// RGB 00RRGGBB High bit indicates palette colour
-	uint32_t bg;	// RGB 00RRGGBB High bit indicates palette colour
-};
-
 struct vstat_vmem {
 	unsigned refcount;
-	struct vmem_cell *vmem;
+	unsigned short *vmem;
 };
 
 struct video_stats {
@@ -100,9 +92,8 @@ struct video_stats {
 	int currattr;
 	int scaling;
 	int	vmultiplier;
-	uint32_t flags;
-#define VIDMODES_FLAG_PALETTE_VMEM	1
-	uint32_t palette[16];
+	struct dac_colors dac_colors[256];
+	unsigned char palette[16];
 	struct vstat_vmem *vmem;
 };
 
@@ -114,9 +105,9 @@ enum {
 	,ATARI_PALETTE
 };
 
-extern struct video_params vparams[52];
+extern struct video_params vparams[49];
 #define NUMMODES      (sizeof(vparams) / sizeof(struct video_params))
-extern uint32_t palettes[5][16];
+extern unsigned char palettes[5][16];
 extern struct dac_colors dac_default[TOTAL_DAC_SIZE];
 extern char vga_font_bitmap[4096];
 extern char vga_font_bitmap14[3584];
