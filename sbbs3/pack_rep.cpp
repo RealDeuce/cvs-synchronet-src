650676c69f1743307a03398aae0a1e9fedcea5af
sbbs <sbbs>
2016-11-21 22:10:14 +0000

    Fixed crash (null pointer defefernce) when reading email/netmail, as reported by Bill McGarrity: new bug.

--- sbbs3/pack_rep.cpp
+++ sbbs3/pack_rep.cpp
@@ -1,6 +1,6 @@
 /* Synchronet QWK reply (REP) packet creation routine */
 
-/* $Id: pack_rep.cpp,v 1.42 2016/11/10 10:06:30 rswindell Exp $ */
+/* $Id: pack_rep.cpp,v 1.44 2016/11/20 11:18:55 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -135,7 +135,7 @@ bool sbbs_t::pack_rep(uint hubnum)
 			msg.idx=mail[u];
 			if(msg.idx.number>qwkmail_last)
 				qwkmail_last=msg.idx.number;
-			if(!loadmsg(&msg,mail[u].number))
+			if(loadmsg(&msg,mail[u].number) < 1)
 				continue;
 
 			SAFEPRINTF(str,"%s/",cfg.qhub[hubnum]->id);
@@ -200,7 +200,7 @@ bool sbbs_t::pack_rep(uint hubnum)
 
 			memset(&msg,0,sizeof(msg));
 			msg.idx=post[u].idx;
-			if(!loadmsg(&msg,post[u].idx.number))
+			if(loadmsg(&msg,post[u].idx.number) < 1)
 				continue;
 
 			if(msg.from_net.type && msg.from_net.type!=NET_QWK &&
@@ -237,10 +237,13 @@ bool sbbs_t::pack_rep(uint hubnum)
 		YIELD();	/* yield */
 	}
 
+	BOOL voting_data = FALSE;
 	if(hdrs!=NULL)
 		fclose(hdrs);
-	if(voting!=NULL)
+	if(voting!=NULL) {
+		voting_data = ftell(voting);
 		fclose(voting);
+	}
 	fclose(rep);			/* close HUB_ID.MSG */
 	CRLF;
 							/* Look for extra files to send out */
@@ -260,7 +263,7 @@ bool sbbs_t::pack_rep(uint hubnum)
 	if(netfiles)
 		CRLF;
 
-	if(!msgcnt && !netfiles && !packedmail) {
+	if(!msgcnt && !netfiles && !packedmail && !voting_data) {
 		eprintf(LOG_INFO,remove_ctrl_a(text[QWKNoNewMessages],tmp));
 		return(true);	// Changed from false Mar-11-2005 (needs to be true to save updated ptrs)
 	}
@@ -317,7 +320,7 @@ bool sbbs_t::pack_rep(uint hubnum)
 				continue;
 			memset(&msg,0,sizeof(msg));
 			/* !IMPORTANT: search by number (do not initialize msg.idx.offset) */
-			if(!loadmsg(&msg,mail[u].number))
+			if(loadmsg(&msg,mail[u].number) < 1)
 				continue;
 
 			SAFEPRINTF(str,"%s/",cfg.qhub[hubnum]->id);
