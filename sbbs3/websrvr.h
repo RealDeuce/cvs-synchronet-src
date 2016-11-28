ab6bb426657e32f88c9eb304c9ba69e7767495dd
rswindell <rswindell>
2016-11-28 10:18:34 +0000

    Automatically recycle whenever the startup file (e.g. sbbs.ini) is modified.

--- sbbs3/websrvr.h
+++ sbbs3/websrvr.h
@@ -1,14 +1,12 @@
-/* websrvr.h */
-
 /* Synchronet Web Server */
 
-/* $Id: websrvr.h,v 1.48 2015/08/22 01:37:51 deuce Exp $ */
+/* $Id: websrvr.h,v 1.51 2016/11/28 02:59:08 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -43,17 +41,17 @@
 #include "semwrap.h"			/* sem_t */
 
 typedef struct {
-	DWORD		size;				/* sizeof(web_startup_t) */
-	WORD		max_clients;
+	size_t		size;				/* sizeof(web_startup_t) */
+	uint16_t	max_clients;
 #define WEB_DEFAULT_MAX_CLIENTS			0	/* 0=unlimited */
-	WORD		max_inactivity;
+	uint16_t	max_inactivity;
 #define WEB_DEFAULT_MAX_INACTIVITY		120	/* seconds */
-	WORD		max_cgi_inactivity;
+	uint16_t	max_cgi_inactivity;
 #define WEB_DEFAULT_MAX_CGI_INACTIVITY	120	/* seconds */
-	WORD		sem_chk_freq;		/* semaphore file checking frequency (in seconds) */
-    DWORD		options;
-	WORD		port;
-	WORD		tls_port;
+	uint16_t	sem_chk_freq;		/* semaphore file checking frequency (in seconds) */
+    uint32_t	options;
+	uint16_t	port;
+	uint16_t	tls_port;
 	struct in_addr outgoing4;
 	struct in6_addr	outgoing6;
     str_list_t	interfaces;
@@ -77,7 +75,7 @@ typedef struct {
 
 	/* Paths */
 	char	ssjs_ext[16];			/* Server-Side JavaScript file extension */
-	char	js_ext[16];			/* Embedded JavaScript file extension */
+	char	js_ext[16];				/* Embedded JavaScript file extension */
 	char**	cgi_ext;				/* CGI Extensions */
 	char	cgi_dir[128];			/* relative to root_dir (all files executable) */
     char    ctrl_dir[128];
@@ -89,6 +87,7 @@ typedef struct {
 	char	answer_sound[128];
 	char	hangup_sound[128];
     char	hack_sound[128];
+	char	ini_fname[128];
 
 	/* Misc */
     char	host_name[128];
@@ -99,17 +98,14 @@ typedef struct {
 	uint	bind_retry_delay;		/* Time to wait between each bind() retry */
 	char	default_cgi_content[128];
 	char	default_auth_list[128];
-	WORD	outbuf_highwater_mark;	/* output block size control */
-	WORD	outbuf_drain_timeout;
+	uint16_t	outbuf_highwater_mark;	/* output block size control */
+	uint16_t	outbuf_drain_timeout;
 
 	/* JavaScript operating parameters */
 	js_startup_t js;
 
 	/* Login Attempt parameters */
-	ulong	login_attempt_delay;
-	ulong	login_attempt_throttle;
-	ulong	login_attempt_hack_threshold;
-	ulong	login_attempt_filter_threshold;
+	struct login_attempt_settings login_attempt;
 	link_list_t* login_attempt_list;
 
 } web_startup_t;
@@ -136,6 +132,7 @@ static struct init_field web_init_fields[] = {
 #define WEB_OPT_VIRTUAL_HOSTS		(1<<4)	/* Use virutal host html subdirs	*/
 #define WEB_OPT_NO_CGI				(1<<5)	/* Disable CGI support				*/
 #define WEB_OPT_HTTP_LOGGING		(1<<6)	/* Create/write-to HttpLogFile		*/
+#define WEB_OPT_ALLOW_TLS			(1<<7)	/* Enable HTTPS						*/
 
 /* web_startup_t.options bits that require re-init/recycle when changed */
 #define WEB_INIT_OPTS	(WEB_OPT_HTTP_LOGGING)
@@ -149,6 +146,7 @@ static ini_bitdesc_t web_options[] = {
 	{ WEB_OPT_VIRTUAL_HOSTS			,"VIRTUAL_HOSTS"		},
 	{ WEB_OPT_NO_CGI				,"NO_CGI"				},
 	{ WEB_OPT_HTTP_LOGGING			,"HTTP_LOGGING"			},
+	{ WEB_OPT_ALLOW_TLS				,"ALLOW_TLS"			},
 
 	/* shared bits */
 	{ BBS_OPT_NO_HOST_LOOKUP		,"NO_HOST_LOOKUP"		},
