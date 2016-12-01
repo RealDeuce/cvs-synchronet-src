32866769d55869186642eee580a6b3f7474b991c
rswindell <rswindell>
2016-12-01 21:22:12 +0000

    Detect control characters a certain header fields (to/from/subj) and report as errors.

--- sbbs3/wordwrap.c
+++ sbbs3/wordwrap.c
@@ -1,4 +1,4 @@
-/* $Id: wordwrap.c,v 1.38 2015/07/29 03:52:31 deuce Exp $ */
+/* $Id: wordwrap.c,v 1.42 2015/12/16 09:47:40 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -255,7 +255,7 @@ static struct section_len get_word_len(char *buf, int maxlen)
 			ret.len--;
 			continue;
 		}
-		if (maxlen > 0 && ret.len >= maxlen)
+		if (maxlen > 0 && ret.len >= (size_t)maxlen)
 			break;
 		ret.len++;
 	}
@@ -272,6 +272,7 @@ struct paragraph {
 	struct prefix prefix;
 	char *text;
 	size_t alloc_size;
+	size_t len;
 };
 
 /*
@@ -296,18 +297,18 @@ static void free_paragraphs(struct paragraph *paragraph, int count)
  */
 static BOOL paragraph_append(struct paragraph *paragraph, const char *bytes, size_t count)
 {
-	size_t len = strlen(paragraph->text);
 	char *new_text;
 
-	while (len + count + 1 > paragraph->alloc_size) {
+	while (paragraph->len + count + 1 > paragraph->alloc_size) {
 		new_text = realloc(paragraph->text, paragraph->alloc_size * 2);
 		if (new_text == NULL)
 			return FALSE;
 		paragraph->text = new_text;
 		paragraph->alloc_size *= 2;
 	}
-	memcpy(paragraph->text + len, bytes, count);
-	paragraph->text[len+count] = 0;
+	memcpy(paragraph->text + paragraph->len, bytes, count);
+	paragraph->text[paragraph->len+count] = 0;
+	paragraph->len += count;
 	return TRUE;
 }
 
@@ -328,7 +329,11 @@ static struct paragraph *word_unwrap(char *inbuf, int oldlen, BOOL handle_quotes
 	struct paragraph *newret = NULL;
 	BOOL paragraph_done;
 	int next_word_len;
+	size_t new_prefix_len;
+	size_t alloc_len = oldlen+1;
 
+	if (alloc_len > 4096)
+		alloc_len = 4096;
 	if(has_crs)
 		*has_crs = FALSE;
 	while(inbuf[inpos]) {
@@ -340,13 +345,14 @@ static struct paragraph *word_unwrap(char *inbuf, int oldlen, BOOL handle_quotes
 			return NULL;
 		}
 		ret = newret;
-		ret[paragraph].text = (char *)malloc(oldlen+1);
+		ret[paragraph].text = (char *)malloc(alloc_len);
+		ret[paragraph].len = 0;
 		ret[paragraph].prefix.bytes = NULL;
 		if (ret[paragraph].text == NULL) {
 			free_paragraphs(ret, paragraph+1);
 			return NULL;
 		}
-		ret[paragraph].alloc_size = oldlen+1;
+		ret[paragraph].alloc_size = alloc_len;
 		ret[paragraph].text[0] = 0;
 		if (handle_quotes) {
 			ret[paragraph].prefix = parse_prefix(inbuf+inpos);
@@ -384,17 +390,21 @@ static struct paragraph *word_unwrap(char *inbuf, int oldlen, BOOL handle_quotes
 						break;
 					}
 					// Now, if the prefix changes, it's hard.
-					if (handle_quotes)
+					if (handle_quotes) {
 						new_prefix = parse_prefix(&inbuf[inpos+1]);
-					else
+						new_prefix_len = strlen(new_prefix.bytes);
+					}
+					else {
 						memset(&new_prefix, 0, sizeof(new_prefix));
+						new_prefix_len = 0;
+					}
 					if (cmp_prefix(&new_prefix, &ret[paragraph].prefix) != 0) {
 						paragraph_done = TRUE;
 						FREE_AND_NULL(new_prefix.bytes);
 						break;
 					}
 					// If the next line start with whitespace, it's hard
-					switch(inbuf[inpos+1+strlen(new_prefix.bytes)]) {
+					switch(inbuf[inpos+1+new_prefix_len]) {
 						case 0:
 						case ' ':
 						case '\t':
@@ -417,14 +427,14 @@ static struct paragraph *word_unwrap(char *inbuf, int oldlen, BOOL handle_quotes
 					}
 
 					// If the first word on the next line would have fit here, it's hard
-					next_word_len = get_word_len(inbuf+inpos+1+strlen(new_prefix.bytes), -1).len;
+					next_word_len = get_word_len(inbuf+inpos+1+new_prefix_len, -1).len;
 					if ((incol + next_word_len + 1 - 1) < oldlen) {
 						FREE_AND_NULL(new_prefix.bytes);
 						paragraph_done = TRUE;
 						break;
 					}
 					// Skip the new prefix...
-					inpos += strlen(new_prefix.bytes);
+					inpos += new_prefix_len;
 					incol = new_prefix.cols;
 					FREE_AND_NULL(new_prefix.bytes);
 					if (!paragraph_append(&ret[paragraph], " ", 1))
@@ -471,7 +481,7 @@ fail_return:
  * 
  * Returns a malloc()ed string.
  */
-static char *wrap_paragraphs(struct paragraph *paragraph, int outlen, BOOL handle_quotes, BOOL has_crs)
+static char *wrap_paragraphs(struct paragraph *paragraph, size_t outlen, BOOL handle_quotes, BOOL has_crs)
 {
 	int outcol;
 	char *outbuf = NULL;
@@ -557,6 +567,8 @@ char* wordwrap(char* inbuf, int len, int oldlen, BOOL handle_quotes)
 	BOOL		has_crs;
 
 	paragraphs = word_unwrap(inbuf, oldlen, handle_quotes, &has_crs);
+	if (paragraphs == NULL)
+		return NULL;
 
 #if 0
 	for(int i=0;paragraphs[i].text;i++)
