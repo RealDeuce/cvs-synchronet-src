9d86c793e187a9e6f3ec52959ef43a0cba2cbf60
rswindell <rswindell>
2017-01-13 01:32:38 +0000

    MSVC++ 2013 solution and project file that successfully builds SyncTERM from current source in CVS without SDL (graphics modes) and without cryptlib (SSH).

--- sbbs3/ident.c
+++ sbbs3/ident.c
@@ -2,13 +2,13 @@
 
 /* Synchronet Indentification (RFC1413) functions */
 
-/* $Id: ident.c,v 1.15 2018/02/20 11:56:26 rswindell Exp $ */
+/* $Id: ident.c,v 1.14 2015/08/20 05:19:41 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
