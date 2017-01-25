8b0a38b237212891b4f369d6358d0bf2016be388
rswindell <rswindell>
2017-01-25 06:33:10 +0000

    Fix bug in Windows build: creates directories with garbage/random characters in your USERPROFILE and (in my case at least) root of C: When the 'type' parameter to get_syncterm_filename() was is 2 (SYNCTERM_DEFAULT_TRANSFER_PATH), the fn (filename) argument would remain uninitialized (random/garbage) at the point that it creates the directory stored in 'fn' if it doesn't exist. Often garbage would be an invalid filename (or just 0) which would cause Windows to fail the request to create the directory. But other times, the garbage would still be a valid directory name (usually just one or two characters).
    The code after this assumes 'fn' is initialized and (like I just pointed out),
    in some cases, it's not. So there might be another fix here needed.

--- syncterm/term.h
+++ syncterm/term.h
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: term.h,v 1.18 2018/02/13 08:11:36 deuce Exp $ */
+/* $Id: term.h,v 1.16 2011/09/08 23:25:30 deuce Exp $ */
 
 #ifndef _TERM_H_
 #define _TERM_H_
@@ -20,6 +20,6 @@ extern struct cterminal	*cterm;
 extern int log_level;
 
 BOOL doterm(struct bbslist *);
-void mousedrag(struct vmem_cell *scrollback);
+void mousedrag(unsigned char *scrollback);
 
 #endif
