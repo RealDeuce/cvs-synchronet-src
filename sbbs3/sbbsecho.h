5eb3e79e5e29b92a6f3a1bd9a69c78f8ce77e396
rswindell <rswindell>
2017-03-01 05:30:19 +0000

    Fix GCC warnings: sbbsecho.h:186:7: warning: array pktTypeStringList assumed to have one element Warning: size of symbol `pktTypeStringList' changed from 8 in .../rechocfg.o

--- sbbs3/sbbsecho.h
+++ sbbs3/sbbsecho.h
@@ -2,7 +2,7 @@
 
 /* Synchronet FidoNet Echomail tosser/scanner/areafix program */
 
-/* $Id: sbbsecho.h,v 3.10 2017/03/01 05:25:38 rswindell Exp $ */
+/* $Id: sbbsecho.h,v 3.11 2017/03/01 05:30:19 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -59,6 +59,7 @@ enum pkt_type {
 	,PKT_TYPE_2_EXT					/* Type-2e  Packet Header (FSC-39)		*/
 	,PKT_TYPE_2_2					/* Type-2.2 Packet Header (FSC-45)		*/
 	,PKT_TYPE_2	 					/* Type-2   Packet Header (FTS-1)		*/	
+	,PKT_TYPES_SUPPORTED
 };
 
 #define DFLT_PKT_SIZE   (250*1024L)
@@ -183,7 +184,7 @@ typedef struct {
 	struct zone_mapping *zone_map;	// 
 } sbbsecho_cfg_t;
 
-char* pktTypeStringList[];
+char* pktTypeStringList[PKT_TYPES_SUPPORTED];
 char* mailStatusStringList[4];
 
 /***********************/
