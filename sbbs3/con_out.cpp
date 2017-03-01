90d2de24c27709f5c128ead03a359656b417d57f
rswindell <rswindell>
2017-03-01 05:25:37 +0000

    Packet Type-2e (FSC-39.4) support: For packet creation, the only difference (from Type-2+) is with packets created from point nodes: the origNet field won't be set to 0xffff (as is the case with FSC-48, Type-2+ packets). The auxNet field is now always set to the originating net (ala Squish) when configured for Type-2+ packet generation. When importing Type-2e (FSC-39) packets, the detected/logged packet type may be reported as "2e" for valid "2+" packets. This is normal and expected when the originating node is not a point and the auxNet field is 0. The second set of orig/dest zone fields in a type 2e/2+ pkt header will now be used when importing if the old/QM orig/dest zone fields are 0. Also, the originating point field may now be imported even if the auxNet field is not used (origNet != 0xffff).
    New umask sbbsecho.ini configuration option (global), maybe used to over-ride
    the default *nix file creation mode mask (077), similar to sbbs.ini.

--- sbbs3/con_out.cpp
+++ sbbs3/con_out.cpp
@@ -1,8 +1,6 @@
-/* con_out.cpp */
-
 /* Synchronet console output routines */
 
-/* $Id: con_out.cpp,v 1.72 2016/11/16 05:41:07 rswindell Exp $ */
+/* $Id: con_out.cpp,v 1.76 2016/12/10 08:02:24 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -379,6 +377,12 @@ void sbbs_t::cleartoeol(void)
 	}
 }
 
+void sbbs_t::cleartoeos(void)
+{
+	if(term_supports(ANSI))
+		rputs("\x1b[J");
+}
+
 /****************************************************************************/
 /* performs the correct attribute modifications for the Ctrl-A code			*/
 /****************************************************************************/
@@ -485,8 +489,14 @@ void sbbs_t::ctrl_a(char x)
 		case 'S':   /* Synchronize */
 			ASYNC;
 			break;
+		case 'J':	/* clear to end-of-screen */
+			cleartoeos();
+			break;
 		case 'L':	/* CLS (form feed) */
 			CLS;
+			break;
+		case '`':	/* Home cursor */
+			cursor_home();
 			break;
 		case '>':   /* CLREOL */
 			cleartoeol();
@@ -621,23 +631,39 @@ bool sbbs_t::msgabort()
 	return(false);
 }
 
-int sbbs_t::backfill(const char* str, float pct)
+int sbbs_t::backfill(const char* instr, float pct, int full_attr, int empty_attr)
 {
-	uint8_t	atr;
+	int	atr;
+	int save_atr = curatr;
 	int len;
-
-	if(!term_supports(ANSI))
-		return bputs(str);
+	char* str = strip_ctrl(instr, NULL);
 
 	len = strlen(str);
-	for(int i=0; i<len; i++) {
-		if(((float)i / len)*100.0 <= pct)
-			atr = cfg.color[clr_backfill];
-		else
-			atr = cfg.color[clr_unfill];
-		if(curatr != atr) attr(atr);
-		outchar(str[i]);
+	if(!term_supports(ANSI))
+		bputs(str);
+	else {
+		for(int i=0; i<len; i++) {
+			if(((float)(i+1) / len)*100.0 <= pct)
+				atr = full_attr;
+			else
+				atr = empty_attr;
+			if(curatr != atr) attr(atr);
+			outchar(str[i]);
+		}
+		attr(save_atr);
 	}
-	attr(LIGHTGRAY);
+	free(str);
 	return len;
+}
+
+void sbbs_t::progress(const char* text, int count, int total, int interval)
+{
+	char str[128];
+
+	if((count%interval) != 0)
+		return;
+	if(text == NULL) text = "";
+	float pct = ((float)count/total)*100.0F;
+	SAFEPRINTF2(str, "[ %-8s  %4.1f%% ]", text, pct);
+	cursor_left(backfill(str, pct, cfg.color[clr_progress_full], cfg.color[clr_progress_empty]));
 }
