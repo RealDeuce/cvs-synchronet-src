f65adfb03ca8719bce494a86aa4d467a183be75b
rswindell <rswindell>
2017-06-09 02:20:20 +0000

    More verbose log messages with regards to max_concurrent_connections being reached (connections with a successful login aren't counted).

--- sbbs3/main.cpp
+++ sbbs3/main.cpp
@@ -1,6 +1,6 @@
 /* Synchronet terminal server thread and related functions */
 
-/* $Id: main.cpp,v 1.647 2017/06/04 00:57:04 rswindell Exp $ */
+/* $Id: main.cpp,v 1.648 2017/06/09 02:20:20 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -5146,11 +5146,11 @@ NO_SSH:
 			
 			if(connections - logins >= (int)startup->max_concurrent_connections
 				&& !is_host_exempt(&scfg, host_ip, /* host_name */NULL)) {
-				lprintf(LOG_NOTICE, "%04d !Maximum concurrent connections (%u) reached from host: %s"
+				lprintf(LOG_NOTICE, "%04d !Maximum concurrent connections without login (%u) reached from host: %s"
  					,client_socket, startup->max_concurrent_connections, host_ip);
 				SSH_END();
 				close_socket(client_socket);
-				SAFEPRINTF(logstr, "Too many concurrent connections from host: %s",host_ip);
+				SAFEPRINTF(logstr, "Too many concurrent connections without login from host: %s",host_ip);
 				sbbs->syslog("@!",logstr);
 				continue;
 			}
