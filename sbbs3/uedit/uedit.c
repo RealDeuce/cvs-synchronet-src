1009a31e7ebfb6f65b8ef7706c252286d2c2e14c
rswindell <rswindell>
2017-08-03 01:22:32 +0000

    Add a per-directory toggle option to enable access to files not in the database (currently only via FTP) - simiilar to the global FTP option: DIR_FILES. I intend to extend this option to the terminal server as well.

--- sbbs3/uedit/uedit.c
+++ sbbs3/uedit/uedit.c
@@ -1,14 +1,13 @@
-/* uedit.c */
-
 /* Synchronet for *nix user editor */
 
-/* $Id: uedit.c,v 1.51 2015/08/27 00:09:16 deuce Exp $ */
+/* $Id: uedit.c,v 1.54 2016/11/28 21:45:49 rswindell Exp $ */
+// vi: tabstop=4
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -48,6 +47,8 @@
 #endif
 
 #include "ciolib.h"
+#include "curs_cio.h"
+#undef OK
 #include "sbbs.h"
 
 #include "genwrap.h"
@@ -1739,7 +1740,6 @@ int finduser(scfg_t *cfg, user_t *user)
 int getuser(scfg_t *cfg, user_t *user, char* str)
 {
 	int i,j,last;
-	ushort un;
 	struct user_list **opt;
 	int done=0;
 
@@ -1894,10 +1894,10 @@ int main(int argc, char** argv)  {
 	char	revision[16];
 	char	str[256],ctrl_dir[41],*p;
 	char	title[256];
-	int		i,j,result;
+	int		i,j;
 	scfg_t	cfg;
 	int		done;
-	int		last, newlast;
+	int		last;
 	user_t	user;
 	int		edtuser=0;
 	int		ciolib_mode=CIOLIB_MODE_AUTO;
@@ -1909,7 +1909,7 @@ int main(int argc, char** argv)  {
 	FILE*				fp;
 	bbs_startup_t		bbs_startup;
 
-	sscanf("$Revision: 1.51 $", "%*s %s", revision);
+	sscanf("$Revision: 1.54 $", "%*s %s", revision);
 
     printf("\nSynchronet User Editor %s-%s  Copyright %s "
         "Rob Swindell\n",revision,PLATFORM_DESC,__DATE__+7);
@@ -1938,21 +1938,20 @@ int main(int argc, char** argv)  {
 	/* Read .ini file here */
 	if(ini_file[0]!=0 && (fp=fopen(ini_file,"r"))!=NULL) {
 		printf("Reading %s\n",ini_file);
+		/* We call this function to set defaults, even if there's no .ini file */
+		sbbs_read_ini(fp, ini_file,
+			NULL,		/* global_startup */
+			NULL, &bbs_startup,
+			NULL, NULL, /* ftp_startup */
+			NULL, NULL, /* web_startup */
+			NULL, NULL, /* mail_startup */
+			NULL, NULL  /* services_startup */
+			);
+
+		/* close .ini file here */
+		if(fp!=NULL)
+			fclose(fp);
 	}
-	/* We call this function to set defaults, even if there's no .ini file */
-	sbbs_read_ini(fp,
-		NULL,		/* global_startup */
-		NULL, &bbs_startup,
-		NULL, NULL, /* ftp_startup */
-		NULL, NULL, /* web_startup */
-		NULL, NULL, /* mail_startup */
-		NULL, NULL  /* services_startup */
-		);
-
-	/* close .ini file here */
-	if(fp!=NULL)
-		fclose(fp);
-
 	chdir(bbs_startup.ctrl_dir);
 
 	/* Read .cfg files here */
