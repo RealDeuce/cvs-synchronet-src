e2e93cb2be45d97bc4c3e3cd36b057f885d9b5ce
rswindell <rswindell>
2017-08-17 19:45:25 +0000

    Remove all last references to makehelp/scfghelp

--- sbbs3/scfg/targets.mk
+++ sbbs3/scfg/targets.mk
@@ -2,7 +2,7 @@
 
 # Make 'include file' defining targets for Synchronet SCFG project
 
-# $Id$
+# $Id: targets.mk,v 1.8 2017/08/17 19:45:25 rswindell Exp $
 
 # LIBODIR, SLASH, LIBFILE, EXEFILE, and DELETE must be pre-defined
 
@@ -15,6 +15,6 @@ all:		xpdev-mt \
 		$(EXEODIR) \
 		$(MTOBJODIR) \
 		$(LIBODIR) \
-		$(SCFG) $(SCFGHELP)
+		$(SCFG)
 
 $(SCFG):	$(XPDEV-MT_LIB) $(UIFCLIB-MT) $(CIOLIB-MT)
