aa1cbd5ba0360690e0e5bd2d3387f505f2295e8d
rswindell <rswindell>
2017-09-19 03:12:04 +0000

    Enhancement: don't allow inbox or outbox to configured for nodes with addresses that contains wildcards ("ALL") - prevents misconfiguration. Fileboxes are not for multiple-nodes (one node per in/outbox only).

--- sbbs3/ftpsrvr.c
+++ sbbs3/ftpsrvr.c
@@ -1,6 +1,6 @@
 /* Synchronet FTP server */
 
-/* $Id: ftpsrvr.c,v 1.430 2016/11/28 02:59:07 rswindell Exp $ */
+/* $Id: ftpsrvr.c,v 1.432 2017/08/03 01:23:53 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -2463,7 +2463,7 @@ static void ctrl_thread(void* arg)
 	SAFECOPY(client.host,host_name);
 	client.port=inet_addrport(&ftp.client_addr);
 	client.protocol="FTP";
-	client.user="<unknown>";
+	client.user=STR_UNKNOWN_USER;
 	client_on(sock,&client,FALSE /* update */);
 
 	if(startup->login_attempt.throttle
@@ -3732,7 +3732,8 @@ static void ctrl_thread(void* arg)
 					padfname(getfname(str),f.name);
 					f.dir=dir;
 					if((filedat=getfileixb(&scfg,&f))==FALSE
-						&& !(startup->options&FTP_OPT_DIR_FILES))
+						&& !(startup->options&FTP_OPT_DIR_FILES)
+						&& !(scfg.dir[dir]->misc&DIR_FILES))
 						continue;
 					if(detail) {
 						f.size=flength(g.gl_pathv[i]);
@@ -4183,7 +4184,7 @@ static void ctrl_thread(void* arg)
 				f.cdt=0;
 				f.size=-1;
 				filedat=getfileixb(&scfg,&f);
-				if(!filedat && !(startup->options&FTP_OPT_DIR_FILES)) {
+				if(!filedat && !(startup->options&FTP_OPT_DIR_FILES) && !(scfg.dir[dir]->misc&DIR_FILES)) {
 					sockprintf(sock,"550 File not found: %s",p);
 					lprintf(LOG_WARNING,"%04d !%s file (%s%s) not in database for %.4s command"
 						,sock,user.alias,genvpath(lib,dir,str),p,cmd);
@@ -4740,7 +4741,7 @@ const char* DLLCALL ftp_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.430 $", "%*s %s", revision);
+	sscanf("$Revision: 1.432 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
