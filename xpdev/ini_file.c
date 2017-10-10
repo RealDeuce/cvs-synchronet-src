b08d0218f82901005802d584b24a24d719ada630
rswindell <rswindell>
2017-10-10 22:45:00 +0000

    Added support for "Cut" operations in uifc.list() - must be called with WIN_GET - returns index with MSK_CUT in upper nibble - Cut is initiated with Ctrl-X or Shif-Delete - Same as Get/Copy (F6/Ctrl-V) except the source item is removed
    * Ctrl-Insert is now supported for Copy (in addition to Ctrl-C and F5)
    * Shift-Insert is now supported for Paste (in addition to Ctrl-V and F6)
    
    * Fixed a couple of potential NULL savbuf dereferences (one sighted when using
    mouse).

--- xpdev/ini_file.c
+++ xpdev/ini_file.c
@@ -1,6 +1,6 @@
 /* Functions to create and parse .ini files */
 
-/* $Id: ini_file.c,v 1.151 2017/10/11 19:55:35 rswindell Exp $ */
+/* $Id: ini_file.c,v 1.150 2017/08/26 19:11:17 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -98,7 +98,7 @@ static char* section_name(char* p)
 	return(p);
 }
 
-static BOOL section_match(const char* name, const char* compare, BOOL case_sensitive)
+static BOOL section_match(const char* name, const char* compare)
 {
 	BOOL found=FALSE;
 	str_list_t names=strListSplitCopy(NULL,name,INI_SECTION_NAME_SEP);
@@ -107,7 +107,7 @@ static BOOL section_match(const char* name, const char* compare, BOOL case_sensi
 	char*	n;
 	char*	c;
 
-	/* Ignore trailing whitespace */
+	/* Ignore trailing whitepsace */
 	for(i=0; names[i]!=NULL; i++)
 		truncsp(names[i]);
 	for(i=0; comps[i]!=NULL; i++)
@@ -120,10 +120,8 @@ static BOOL section_match(const char* name, const char* compare, BOOL case_sensi
 			SKIP_WHITESPACE(n);
 			c=comps[j];
 			SKIP_WHITESPACE(c);
-			if (case_sensitive)
-				found = strcmp(n, c) == 0;
-			else
-				found = stricmp(n, c) == 0;
+			if(stricmp(n,c)==0)
+				found=TRUE;
 		}
 
 	strListFree(&names);
@@ -142,7 +140,6 @@ static BOOL seek_section(FILE* fp, const char* section)
 	if(section==ROOT_SECTION)
 		return(TRUE);
 
-	/* Perform case-sensitive search first */
 	while(!feof(fp)) {
 		if(fgets(str,sizeof(str),fp)==NULL)
 			break;
@@ -150,23 +147,9 @@ static BOOL seek_section(FILE* fp, const char* section)
 			break;
 		if((p=section_name(str))==NULL)
 			continue;
-		if(section_match(p, section, /* case-sensitive */TRUE))
+		if(section_match(p,section))
 			return(TRUE);
 	}
-
-	/* Then perform case-insensitive search */
-	rewind(fp);
-	while (!feof(fp)) {
-		if (fgets(str, sizeof(str), fp) == NULL)
-			break;
-		if (is_eof(str))
-			break;
-		if ((p = section_name(str)) == NULL)
-			continue;
-		if (section_match(p, section, /* case-sensitive */FALSE))
-			return(TRUE);
-	}
-
 	return(FALSE);
 }
 
@@ -176,21 +159,11 @@ static size_t find_section_index(str_list_t list, const char* section)
 	char	str[INI_MAX_VALUE_LEN];
 	size_t	i;
 
-	/* Perform case-sensitive search first */
-	for (i = 0; list[i] != NULL; i++) {
-		SAFECOPY(str, list[i]);
+	for(i=0; list[i]!=NULL; i++) {
+		SAFECOPY(str,list[i]);
 		if(is_eof(str))
 			return(strListCount(list));
-		if((p=section_name(str))!=NULL && section_match(p, section, /* case-sensitive */TRUE))
-			return(i);
-	}
-
-	/* Then perform case-insensitive search */
-	for (i = 0; list[i] != NULL; i++) {
-		SAFECOPY(str, list[i]);
-		if (is_eof(str))
-			return(strListCount(list));
-		if ((p = section_name(str)) != NULL && section_match(p, section, /* case-sensitive */FALSE))
+		if((p=section_name(str))!=NULL && section_match(p,section))
 			return(i);
 	}
 
