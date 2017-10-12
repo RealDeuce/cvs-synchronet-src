063f439d01dead72e449ad42c80bf0dcde55e22d
rswindell <rswindell>
2017-10-12 07:06:06 +0000

    Update copy/cut/paste-over/insert support. You can now cut a group/lib/section and when pasted, all the child subs/dirs/xtrns will be pasted with it. If however, you cut a group/lib/section and then exit SCFG, you'll lose all the children subs/dir/xtrns that were on the "clipboard" along with the parent.
    Copy - F5, Ctrl-C, Ctrl-Insert
    Cut - Shift-Del, Ctrl-X
    Paste Over - F6, Ctrl-V
    Paste Insert - Shift-Insert

--- uifc/uifc.h
+++ uifc/uifc.h
@@ -1,14 +1,12 @@
-/* uifc.h */
+/* Text-mode User Interface Library (inspired by Novell SYSCON look & feel) */
 
-/* Rob Swindell's Text-mode User Interface Library */
-
-/* $Id: uifc.h,v 1.84 2015/02/17 07:28:33 deuce Exp $ */
+/* $Id: uifc.h,v 1.88 2017/10/11 19:30:45 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2010 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -98,15 +96,22 @@
 	#endif
 #endif
 
-#define MAX_OPTS	10000
-#define MSK_ON		0xf0000000
-#define MSK_OFF 	0x0fffffff
-#define MSK_INS 	0x10000000
-#define MSK_DEL 	0x20000000
-#define MSK_GET 	0x30000000
-#define MSK_PUT 	0x40000000
-#define MSK_EDIT 	0x50000000
-/* Dont forget, negative return values are used for extended keys (if WIN_EXTKEYS used)! */
+#define MAX_OPTS			10000
+#define MSK_ON				0xf0000000
+#define MSK_OFF 			0x0fffffff
+#define MSK_INS 			0x10000000
+#define MSK_DEL 			0x20000000
+#define MSK_COPY 			0x30000000
+#define MSK_CUT 			0x40000000
+#define MSK_PASTE_OVER		0x50000000	/* Overwrite selected item with previously copied item */
+#define MSK_PASTE_INSERT 	0x60000000	/* Insert new item (above) current item with previously copied item */
+#define MSK_EDIT			0x70000000
+
+/* Legacy terms (get/put instead of copy/paste) */
+#define MSK_GET		MSK_COPY
+#define MSK_PUT		MSK_PASTE_OVER
+
+/* Don't forget, negative return values are used for extended keys (if WIN_EXTKEYS used)! */
 #define MAX_OPLN	75		/* Maximum length of each option per menu call */
 #define MAX_BUFS	7		/* Maximum number of screen buffers to save */
 #define MIN_LINES   14      /* Minimum number of screen lines supported */
@@ -118,17 +123,18 @@
 #define uint unsigned int
 #endif
 
-							/**************************/
-                            /* Bits in uifcapi_t.mode */
-							/**************************/
-#define UIFC_INMSG	(1<<0)	/* Currently in Message Routine non-recursive */
-#define UIFC_MOUSE	(1<<1)	/* Mouse installed and available */
-#define UIFC_MONO	(1<<2)	/* Force monochrome mode */
-#define UIFC_COLOR	(1<<3)	/* Force color mode */
-#define UIFC_IBM	(1<<4)	/* Force use of IBM charset	*/
-#define UIFC_NOCTRL	(1<<5)	/* Don't allow useage of CTRL keys for movement 
-							 * etc in menus (Still available in text boxes) */
-#define UIFC_NHM	(1<<6)	/* Don't hide the mouse pointer */
+								/**************************/
+						        /* Bits in uifcapi_t.mode */
+								/**************************/
+#define UIFC_INMSG		(1<<0)	/* Currently in Message Routine non-recursive */
+#define UIFC_MOUSE		(1<<1)	/* Mouse installed and available */
+#define UIFC_MONO		(1<<2)	/* Force monochrome mode */
+#define UIFC_COLOR		(1<<3)	/* Force color mode */
+#define UIFC_IBM		(1<<4)	/* Force use of IBM charset	*/
+#define UIFC_NOCTRL		(1<<5)	/* Don't allow usage of CTRL keys for movement 
+								 * etc in menus (Still available in text boxes) */
+#define UIFC_NHM		(1<<6)	/* Don't hide the mouse pointer */
+#define UIFC_NOMOUSE	(1<<7)	/* Don't enable/use the mouse */
 
 							/*******************************/
                             /* Bits in uifcapi_t.list mode */
@@ -144,9 +150,9 @@
 #define WIN_DELACT	(1<<8)	/* Remains active after delete key */
 #define WIN_ESC 	(1<<9)	/* Screen is active when escape is hit			 */
 #define WIN_RHT 	(1<<10) /* Place window against right side of screen */
-#define WIN_BOT 	(1<<11) /* Place window against botton of screen */
-#define WIN_GET 	(1<<12) /* Allows F5 to Get a menu item */
-#define WIN_PUT 	(1<<13) /* Allows F6 to Put a menu item */
+#define WIN_BOT 	(1<<11) /* Place window against bottom of screen */
+#define WIN_COPY 	(1<<12) /* Allows F5 to Get (copy) a menu item */
+#define WIN_PASTE 	(1<<13) /* Allows F6 to Put (paste) a menu item */
 #define WIN_CHE 	(1<<14) /* Stay active after escape if changes */
 #define WIN_XTR 	(1<<15) /* Add extra line at end for inserting at end */
 #define WIN_DYN 	(1<<16) /* Dynamic window - return at least every second */
@@ -162,15 +168,17 @@
 #define WIN_FIXEDHEIGHT	(1<<25)	/* Use list_height from uifc struct */
 #define WIN_UNGETMOUSE  (1<<26) /* If the mouse is clicked outside the window, */
 								/* Put the mouse event back into the event queue */
-#define WIN_EDIT	(1<<27)	/* Allow F2 to edit a menu item */
-#define WIN_EDITACT	(1<<28)	/* Remain active after edit key */
-#define WIN_INACT	(1<<29)	/* Draw window inactive... intended for use with WIN_IMM */
-#define WIN_POP		(1<<30)	/* Exit the list. Act as though ESC was pressed. */
-							/* Intended for use after a WIN_EXTKEYS or WIN_DYN */
-#define WIN_SEL		(1<<31)	/* Exit the list. Act as though ENTER was pressed. */
-							/* Intended for use after a WIN_EXTKEYS or WIN_DYN */
+#define WIN_EDIT		(1<<27)	/* Allow F2 to edit a menu item */
+#define WIN_PASTEXTR	(1<<28)	/* Allow paste into extra (blank) item */
+#define WIN_INACT		(1<<29)	/* Draw window inactive... intended for use with WIN_IMM */
+#define WIN_POP			(1<<30)	/* Exit the list. Act as though ESC was pressed. */
+								/* Intended for use after a WIN_EXTKEYS or WIN_DYN */
+#define WIN_SEL			(1<<31)	/* Exit the list. Act as though ENTER was pressed. */
+								/* Intended for use after a WIN_EXTKEYS or WIN_DYN */
 
 #define WIN_MID WIN_L2R|WIN_T2B  /* Place window in middle of screen */
+#define WIN_GET	WIN_COPY
+#define WIN_PUT WIN_PASTE
 
 #define SCRN_TOP	3
 #define SCRN_LEFT	5
