8c042376481a7f4897e1772ef9f3f24a52864efa
rswindell <rswindell>
2017-10-12 09:05:13 +0000

    Fixed potential NULL-dereference in qwk_import_msg().

--- sbbs3/userdat.c
+++ sbbs3/userdat.c
@@ -1,7 +1,6 @@
 /* Synchronet user data-related routines (exported) */
-// vi: tabstop=4
 
-/* $Id: userdat.c,v 1.184 2018/02/20 11:43:08 rswindell Exp $ */
+/* $Id: userdat.c,v 1.181 2017/10/10 23:05:22 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -203,11 +202,6 @@ int DLLCALL openuserdat(scfg_t* cfg, BOOL for_modify)
 
 	SAFEPRINTF(path,"%suser/user.dat",cfg->data_dir);
 	return nopen(path, for_modify ? (O_RDWR|O_CREAT|O_DENYNONE) : (O_RDONLY|O_DENYNONE)); 
-}
-
-int DLLCALL closeuserdat(int file)
-{
-	return close(file);
 }
 
 /****************************************************************************/
@@ -1129,8 +1123,8 @@ char* DLLCALL nodestatus(scfg_t* cfg, node_t* node, char* buf, size_t buflen)
             strcat(str,"C");
         strcat(str,"]"); 
 	}
-	if(node->errors)
-		sprintf(str+strlen(str)
+    if(node->errors)
+        sprintf(str+strlen(str)
 			," %d error%c",node->errors, node->errors>1 ? 's' : '\0' );
 
 	strncpy(buf,str,buflen);
@@ -2039,13 +2033,8 @@ int DLLCALL putuserrec(scfg_t* cfg, int usernumber,int start, uint length, const
 		return(-4);
 	}
 
-	if(length==0) {	/* auto-length */
+	if(length==0)	/* auto-length */
 		length=user_rec_len(start);
-		if((long)length < 0) {
-			close(file);
-			return -2;
-		}
-	}
 
 	strcpy(str2,str);
 	if(strlen(str2)<length) {
@@ -2063,10 +2052,8 @@ int DLLCALL putuserrec(scfg_t* cfg, int usernumber,int start, uint length, const
 		i++; 
 	}
 
-	if(i>=LOOP_NODEDAB) {
-		close(file);
+	if(i>=LOOP_NODEDAB) 
 		return(-3);
-	}
 
 	write(file,str2,length);
 	unlock(file,(long)((long)(usernumber-1)*U_LEN)+start,length);
@@ -2700,7 +2687,7 @@ BOOL DLLCALL is_user_subop(scfg_t* cfg, uint subnum, user_t* user, client_t* cli
 	if(user->level>=SYSOP_LEVEL)
 		return TRUE;
 
-	return cfg->sub[subnum]->op_ar!=NULL && cfg->sub[subnum]->op_ar[0]!=0 && chk_ar(cfg,cfg->sub[subnum]->op_ar,user,client);
+	return cfg->sub[subnum]->op_ar[0]!=0 && chk_ar(cfg,cfg->sub[subnum]->op_ar,user,client);
 }
 
 /****************************************************************************/
