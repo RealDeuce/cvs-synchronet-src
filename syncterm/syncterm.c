0dac849438f3dab04aad0158558ee9d62b6c0676
rswindell <rswindell>
2017-11-15 07:12:52 +0000

    Fix long standing bug when setting misc, useron, and aux values: was truncating numeric value to 8-bits (0-255). Added support for hexadecimal notation (prefix with 0x) while I was at it.

--- syncterm/syncterm.c
+++ syncterm/syncterm.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: syncterm.c,v 1.200 2017/01/13 01:25:02 rswindell Exp $ */
+/* $Id: syncterm.c,v 1.202 2017/01/25 08:11:30 deuce Exp $ */
 
 #if defined(__APPLE__) && defined(__MACH__)
 #include <CoreServices/CoreServices.h>	// FSFindFolder() and friends
@@ -951,6 +951,7 @@ char *get_syncterm_filename(char *fn, int fnlen, int type, int shared)
 {
 	char	oldlst[MAX_PATH+1];
 
+	memset(fn, 0, fnlen);
 #ifdef _WIN32
 	char	*home;
 	static	dll_handle	shell32=NULL;
@@ -1021,11 +1022,15 @@ char *get_syncterm_filename(char *fn, int fnlen, int type, int shared)
 					break;
 			}
 			if(we_got_this) {
-				if (type != SYNCTERM_DEFAULT_TRANSFER_PATH) {
+				// Convert unicode to string using snprintf()
+				if (type == SYNCTERM_DEFAULT_TRANSFER_PATH) {
+					if(snprintf(fn, fnlen, "%S", path) >= fnlen)
+						we_got_this=FALSE;
+				}
+				else {
 					if(snprintf(fn, fnlen, "%S\\SyncTERM", path) >= fnlen)
 						we_got_this=FALSE;
 				}
-				// Convert unicode to string.
 				CTMF(path);
 			}
 		}
@@ -1065,7 +1070,7 @@ char *get_syncterm_filename(char *fn, int fnlen, int type, int shared)
 	}
 
 	/* Create if it doesn't exist */
-	if(!isdir(fn)) {
+	if(*fn && !isdir(fn)) {
 		if(MKDIR(fn))
 			fn[0]=0;
 	}
