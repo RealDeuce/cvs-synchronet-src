cc843840b63290b7769ee20bcbfddf1c0d7ffb79
rswindell <rswindell>
2017-11-15 10:01:11 +0000

    Don't re-write the sbbs.ini file during initialization (the OnChange callback for the Log Level controls calls SaveIniSettings).

--- sbbs3/ctrl/MainFormUnit.cpp
+++ sbbs3/ctrl/MainFormUnit.cpp
@@ -1,6 +1,6 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: MainFormUnit.cpp,v 1.193 2017/11/13 19:55:04 rswindell Exp $ */
+/* $Id: MainFormUnit.cpp,v 1.194 2017/11/15 10:01:11 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -2344,7 +2344,6 @@ void __fastcall TMainForm::StartupTimerTick(TObject *Sender)
 
     StatsTimer->Interval=cfg.node_stat_check*1000;
 	StatsTimer->Enabled=true;
-    Initialized=true;
 
     UpTimer->Enabled=true; /* Start updating the status bar */
     LogTimer->Enabled=true;
@@ -2364,6 +2363,8 @@ void __fastcall TMainForm::StartupTimerTick(TObject *Sender)
 
     if(!Application->Active)	/* Starting up minimized? */
     	FormMinimize(Sender);   /* Put icon in systray */
+
+    Initialized=true;
 }
 //---------------------------------------------------------------------------
 void __fastcall TMainForm::SaveRegistrySettings(TObject* Sender)
@@ -2553,7 +2554,7 @@ void __fastcall TMainForm::SaveSettings(TObject* Sender)
 bool __fastcall TMainForm::SaveIniSettings(TObject* Sender)
 {
     FILE* fp=NULL;
-   	if(ini_file[0]==0)
+   	if(ini_file[0]==0 || !Initialized)
         return(false);
 
     if((fp=fopen(ini_file,"r+"))==NULL) {
