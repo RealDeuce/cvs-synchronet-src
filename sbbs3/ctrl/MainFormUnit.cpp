212270b889d3462ee1d74c4420a6494747544509
rswindell <rswindell>
2017-11-15 10:16:52 +0000

    When toggling SPAM visibility, go to the first msg, not the last.

--- sbbs3/ctrl/MainFormUnit.cpp
+++ sbbs3/ctrl/MainFormUnit.cpp
@@ -1,6 +1,6 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: MainFormUnit.cpp,v 1.196 2017/12/06 04:51:26 rswindell Exp $ */
+/* $Id: MainFormUnit.cpp,v 1.194 2017/11/15 10:01:11 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -843,7 +843,7 @@ __fastcall TMainForm::TMainForm(TComponent* Owner)
     bbs_startup.event_cbdata=&event_log_list;    
     bbs_startup.first_node=1;
     bbs_startup.last_node=4;
-	bbs_startup.options=BBS_OPT_XTRN_MINIMIZED;
+	bbs_startup.options=BBS_OPT_XTRN_MINIMIZED|BBS_OPT_SYSOP_AVAILABLE;
 	bbs_startup.telnet_port=IPPORT_TELNET;
     bbs_startup.rlogin_port=513;
 	bbs_startup.lputs=lputs;
@@ -1114,8 +1114,8 @@ void __fastcall TMainForm::FormClose(TObject *Sender, TCloseAction &Action)
         TrayIcon->Visible=false;    /* restore to avoid crash */
         
     /* This is necessary to save form sizes/positions */
-    if(Initialized) /* Don't overwrite registry and .ini settings with defaults */
-        SaveSettings(Sender);
+    if(Initialized) /* Don't overwrite registry settings with defaults */
+        SaveRegistrySettings(Sender);
 
 	StatusBar->Panels->Items[STATUSBAR_LAST_PANEL]->Text="Terminating servers...";
     time_t start=time(NULL);
@@ -2248,7 +2248,7 @@ void __fastcall TMainForm::StartupTimerTick(TObject *Sender)
     else
     	SoundToggle->Checked=true;
 
-    if(sysop_available(&cfg))
+    if(bbs_startup.options&BBS_OPT_SYSOP_AVAILABLE)
     	ChatToggle->Checked=true;
     else
     	ChatToggle->Checked=false;
@@ -2959,12 +2959,6 @@ void __fastcall TMainForm::UpTimerTick(TObject *Sender)
     char    days[64];
     static  time_t start;
     ulong   up;
-    static  bool sysop_available;
-
-    if(ChatToggle->Checked != sysop_available) {
-        sysop_available = ChatToggle->Checked;
-        set_sysop_availability(&cfg, sysop_available);
-    }
 
     if(!start)
         start=time(NULL);
@@ -3038,8 +3032,16 @@ void __fastcall TMainForm::UpTimerTick(TObject *Sender)
 void __fastcall TMainForm::ChatToggleExecute(TObject *Sender)
 {
     ChatToggle->Checked=!ChatToggle->Checked;
-}
+    if(ChatToggle->Checked)
+	    bbs_startup.options|=BBS_OPT_SYSOP_AVAILABLE;
+    else
+        bbs_startup.options&=~BBS_OPT_SYSOP_AVAILABLE;
 
+	if(bbs_svc!=NULL && controlService!=NULL)
+        controlService(bbs_svc
+            ,ChatToggle->Checked ? SERVICE_CONTROL_SYSOP_AVAILABLE : SERVICE_CONTROL_SYSOP_UNAVAILABLE
+            ,&bbs_svc_status);
+}
 //---------------------------------------------------------------------------
 void __fastcall TMainForm::UserEditExecute(TObject *Sender)
 {
@@ -3372,7 +3374,7 @@ void __fastcall TMainForm::reload_config(void)
    	StatusBar->Panels->Items[STATUSBAR_LAST_PANEL]->Text="Configuration reloaded";
    	semfile_list_check(&initialized,recycle_semfiles);
 
-    if(sysop_available(&cfg))
+    if(bbs_startup.options&BBS_OPT_SYSOP_AVAILABLE)
     	ChatToggle->Checked=true;
     else
     	ChatToggle->Checked=false;
