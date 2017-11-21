fc458ac322037dd993d704513ddb86a8fe31cd1a
rswindell <rswindell>
2017-11-21 23:32:23 +0000

    smb_freemsgdat() [called by smb_freemsg_dfield() and smb_freemsg()] and smb_incmsg_dfields(), now lock and unlock the SMB header. This shouldprevent some mail base corruption as well as this error: -203 (smb_incmsgdat 13 'Permission denied' reading allocation record at offset 795568) incrementing data allocation units

--- sbbs3/str.cpp
+++ sbbs3/str.cpp
@@ -1,6 +1,6 @@
 /* Synchronet high-level string i/o routines */
 
-/* $Id: str.cpp,v 1.77 2018/01/12 22:15:43 rswindell Exp $ */
+/* $Id: str.cpp,v 1.75 2017/11/13 08:31:25 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -842,11 +842,6 @@ char* sbbs_t::timestr(time_t intime)
 	return(::timestr(&cfg,(time32_t)intime,timestr_output));
 }
 
-char* sbbs_t::datestr(time_t t)
-{
-	return unixtodstr(&cfg, (time32_t)t, datestr_output);
-}
-
 void sbbs_t::sys_info()
 {
 	char	tmp[128];
@@ -934,8 +929,11 @@ void sbbs_t::user_info()
 
 void sbbs_t::xfer_policy()
 {
+	char	str[MAX_PATH+1];
+
 	if(!usrlibs) return;
-	if(menu_exists("tpolicy"))
+	sprintf(str,"%smenu/tpolicy.*", cfg.text_dir);
+	if(fexist(str))
 		menu("tpolicy");
 	else {
 		bprintf(text[TransferPolicyHdr],cfg.sys_name);
@@ -956,7 +954,10 @@ const char* prot_menu_file[] = {
 
 void sbbs_t::xfer_prot_menu(enum XFER_TYPE type)
 {
-	if(menu_exists(prot_menu_file[type])) {
+	char path[MAX_PATH+1];
+
+	sprintf(path,"%smenu/%s.*",cfg.text_dir,prot_menu_file[type]);
+	if(fexistcase(path)) {
 		menu(prot_menu_file[type]);
 		return;
 	}
