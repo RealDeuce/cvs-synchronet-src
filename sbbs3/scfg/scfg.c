2ea58b80be435b239a0a434541d40f8685699cfc
rswindell <rswindell>
2017-11-24 23:35:20 +0000

    Address numerous new warnings raised by GCC v6.3.0

--- sbbs3/scfg/scfg.c
+++ sbbs3/scfg/scfg.c
@@ -1,6 +1,6 @@
 /* Synchronet configuration utility 										*/
 
-/* $Id: scfg.c,v 1.89 2017/10/23 04:17:13 rswindell Exp $ */
+/* $Id: scfg.c,v 1.91 2017/11/11 10:19:19 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -55,7 +55,7 @@ char **opt;
 char tmp[256];
 char error[256];
 int  backup_level=5;
-char* area_sort_desc[] = { "Index Position", "Long Name", "Short Name", "Internal Code" };
+char* area_sort_desc[] = { "Index Position", "Long Name", "Short Name", "Internal Code", NULL };
 
 char *invalid_code=
 	"`Invalid Internal Code:`\n\n"
@@ -192,6 +192,10 @@ int main(int argc, char **argv)
             ) {
 			if(strncmp(argv[i]+1, "import=", 7) == 0) {
 				import = argv[i] + 8;
+				continue;
+			}
+			if(strcmp(argv[i]+1, "insert") == 0) {
+				uifc.insert_mode = TRUE;
 				continue;
 			}
             switch(toupper(argv[i][1])) {
