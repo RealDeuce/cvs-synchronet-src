2d8693407bff433a1f69da708074472331a69ce1
rswindell <rswindell>
2017-11-24 23:35:20 +0000

    Address numerous new warnings raised by GCC v6.3.0

--- sbbs3/str.cpp
+++ sbbs3/str.cpp
@@ -1,6 +1,6 @@
 /* Synchronet high-level string i/o routines */
 
-/* $Id: str.cpp,v 1.79 2018/02/20 11:43:08 rswindell Exp $ */
+/* $Id: str.cpp,v 1.75 2017/11/13 08:31:25 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -159,7 +159,6 @@ void sbbs_t::sif(char *fname, char *answers, long len)
 	}
 	if(lread(file,buf,length)!=length) {
 		close(file);
-		free(buf);
 		errormsg(WHERE,ERR_READ,str,length);
 		answers[0]=0;
 		return; 
@@ -329,7 +328,6 @@ void sbbs_t::sof(char *fname, char *answers, long len)
 		close(file);
 		errormsg(WHERE,ERR_READ,str,length);
 		answers[0]=0;
-		free(buf);
 		return; 
 	}
 	close(file);
@@ -844,15 +842,9 @@ char* sbbs_t::timestr(time_t intime)
 	return(::timestr(&cfg,(time32_t)intime,timestr_output));
 }
 
-char* sbbs_t::datestr(time_t t)
-{
-	return unixtodstr(&cfg, (time32_t)t, datestr_output);
-}
-
 void sbbs_t::sys_info()
 {
 	char	tmp[128];
-	char	path[MAX_PATH+1];
 	uint	i;
 	stats_t stats;
 
@@ -878,13 +870,12 @@ void sbbs_t::sys_info()
 	bprintf(text[SiTotalTime],ultoac(stats.timeon,tmp));
 	bprintf(text[SiTimeToday],ultoac(stats.ttoday,tmp));
 	ver();
-	SAFEPRINTF(path, "%ssystem.msg", cfg.text_dir);
-	if(fexistcase(path) && text[ViewSysInfoFileQ][0] && yesno(text[ViewSysInfoFileQ])) {
+	if(text[ViewSysInfoFileQ][0] && yesno(text[ViewSysInfoFileQ])) {
 		CLS;
-		printfile(path,0); 
+		sprintf(tmp,"%ssystem.msg", cfg.text_dir);
+		printfile(tmp,0); 
 	}
-	SAFEPRINTF(path, "%smenu/logon.asc", cfg.text_dir);
-	if(fexistcase(path) && text[ViewLogonMsgQ][0] && yesno(text[ViewLogonMsgQ])) {
+	if(text[ViewLogonMsgQ][0] && yesno(text[ViewLogonMsgQ])) {
 		CLS;
 		menu("logon"); 
 	}
@@ -938,8 +929,11 @@ void sbbs_t::user_info()
 
 void sbbs_t::xfer_policy()
 {
+	char	str[MAX_PATH+1];
+
 	if(!usrlibs) return;
-	if(menu_exists("tpolicy"))
+	sprintf(str,"%smenu/tpolicy.*", cfg.text_dir);
+	if(fexist(str))
 		menu("tpolicy");
 	else {
 		bprintf(text[TransferPolicyHdr],cfg.sys_name);
@@ -960,7 +954,10 @@ const char* prot_menu_file[] = {
 
 void sbbs_t::xfer_prot_menu(enum XFER_TYPE type)
 {
-	if(menu_exists(prot_menu_file[type])) {
+	char path[MAX_PATH+1];
+
+	sprintf(path,"%smenu/%s.*",cfg.text_dir,prot_menu_file[type]);
+	if(fexistcase(path)) {
 		menu(prot_menu_file[type]);
 		return;
 	}
