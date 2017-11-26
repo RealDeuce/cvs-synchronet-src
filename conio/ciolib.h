7ce4e62efa524297425f2fcedc65614253f82955
rswindell <rswindell>
2017-11-26 05:12:23 +0000

    Fix for sendfile(), when the protocol char (mnemonic) is the space (ASCII 32) char, don't try to match it with a transfer protocol. When a user doesn't have a default transfer protocol selected, their "prot" property may be a space.

--- conio/ciolib.h
+++ conio/ciolib.h
@@ -1,10 +1,10 @@
-/* $Id: ciolib.h,v 1.72 2015/02/27 10:42:08 deuce Exp $ */
+/* $Id: ciolib.h,v 1.73 2017/10/10 22:29:59 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2005 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -521,6 +521,10 @@ CIOLIBEXPORT int CIOLIBCALL ciomouse_delevent(int event);
 #define CIO_KEY_F(x)      ((x<11)?((0x3a+x) << 8):((0x7a+x) << 8))
 #define CIO_KEY_IC        (0x52 << 8)
 #define CIO_KEY_DC        (0x53 << 8)
+#define CIO_KEY_SHIFT_IC  (0x30 << 8)	/* Shift-Insert */
+#define CIO_KEY_SHIFT_DC  (0x2e << 8)	/* Shift-Delete */
+#define CIO_KEY_CTRL_IC   (0x92 << 8)	/* Ctrl-Insert */
+#define CIO_KEY_CTRL_DC   (0x93 << 8)	/* Ctrl-Delete */
 #define CIO_KEY_LEFT      (0x4b << 8)
 #define CIO_KEY_RIGHT     (0x4d << 8)
 #define CIO_KEY_PPAGE     (0x49 << 8)
