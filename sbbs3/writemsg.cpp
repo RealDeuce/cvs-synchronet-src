a2a14efa109386f42267e8b191165258bebf0518
rswindell <rswindell>
2017-11-26 05:09:38 +0000

    Added prototype for smb_getattachment()

--- sbbs3/writemsg.cpp
+++ sbbs3/writemsg.cpp
@@ -1,6 +1,6 @@
 /* Synchronet message creation routines */
 
-/* $Id: writemsg.cpp,v 1.115 2016/11/27 23:03:03 rswindell Exp $ */
+/* $Id: writemsg.cpp,v 1.117 2017/11/24 21:53:40 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1207,7 +1207,7 @@ void sbbs_t::forwardmail(smbmsg_t *msg, int usernumber)
 	smb_close_da(&smb);
 
 
-	if((i=smb_addmsghdr(&smb,msg,SMB_SELFPACK))!=SMB_SUCCESS) {
+	if((i=smb_addmsghdr(&smb,msg,smb_storage_mode(&cfg, &smb)))!=SMB_SUCCESS) {
 		errormsg(WHERE,ERR_WRITE,smb.file,i,smb.last_error);
 		smb_freemsg_dfields(&smb,msg,1);
 		return; 
@@ -1549,6 +1549,9 @@ ushort sbbs_t::chmsgattr(smbmsg_t msg)
 		switch(ch) {
 			case 'P':
 				msg.hdr.attr^=MSG_PRIVATE;
+				break;
+			case 'S':
+				msg.hdr.attr^=MSG_SPAM;
 				break;
 			case 'R':
 				msg.hdr.attr^=MSG_READ;
