2e68b3aa543378acace94031420e71dd73f7bad9
rswindell <rswindell>
2017-11-26 00:08:16 +0000

    White-space (spaces vs. tabs) is now significant to GCC. :-(

--- sbbs3/targets.mk
+++ sbbs3/targets.mk
@@ -2,7 +2,7 @@
 
 # Make 'include file' defining targets for Synchronet project
 
-# $Id: targets.mk,v 1.45 2018/02/28 03:37:58 rswindell Exp $
+# $Id: targets.mk,v 1.40 2015/10/28 02:00:58 deuce Exp $
 
 # LIBODIR, EXEODIR, DIRSEP, LIBFILE, EXEFILE, and DELETE must be pre-defined
 
@@ -36,7 +36,6 @@ DELFILES	= $(EXEODIR)$(DIRSEP)delfiles$(EXEFILE)
 DUPEFIND	= $(EXEODIR)$(DIRSEP)dupefind$(EXEFILE)
 SMBACTIV	= $(EXEODIR)$(DIRSEP)smbactiv$(EXEFILE)
 DSTSEDIT	= $(EXEODIR)$(DIRSEP)dstsedit$(EXEFILE)
-READSAUCE	= $(EXEODIR)$(DIRSEP)readsauce$(EXEFILE)
 
 UTILS		= $(FIXSMB) $(CHKSMB) \
 			  $(SMBUTIL) $(BAJA) $(NODE) \
@@ -45,9 +44,9 @@ UTILS		= $(FIXSMB) $(CHKSMB) \
 			  $(ANS2ASC) $(ASC2ANS)  $(UNBAJA) \
 			  $(QWKNODES) $(SLOG) $(ALLUSERS) \
 			  $(DELFILES) $(DUPEFIND) $(SMBACTIV) \
-			  $(SEXYZ) $(DSTSEDIT) $(READSAUCE)
+			  $(SEXYZ) $(DSTSEDIT)
 
-all:	dlls utils console scfg uedit umonitor
+all:	dlls utils console
 
 console:	$(JS_DEPS) xpdev-mt smblib \
 		$(MTOBJODIR) $(LIBODIR) $(EXEODIR) \
@@ -65,31 +64,6 @@ dlls:	$(JS_DEPS) smblib xpdev-mt \
 mono:	xpdev-mt smblib \
 		$(MTOBJODIR) $(EXEODIR) \
 		$(SBBSMONO)
-
-.PHONY: scfg
-scfg:
-	$(MAKE) -C scfg
-
-.PHONY: uedit
-uedit:
-	$(MAKE) -C uedit
-
-.PHONY: umonitor
-umonitor:
-	$(MAKE) -C umonitor
-
-
-ifdef SBBSEXEC
-.PHONY: install
-install: all
-	install $(EXEODIR)/* $(SBBSEXEC)
-	install $(LIBODIR)/* $(SBBSEXEC)
-
-.PHONY: symlinks
-symlinks: all
-	ln -sfr $(EXEODIR)/* $(SBBSEXEC)
-	ln -sfr $(LIBODIR)/* $(SBBSEXEC)
-endif
 
 .PHONY: sexyz
 sexyz:	$(SEXYZ)
@@ -128,4 +102,3 @@ $(DELFILES): $(XPDEV_LIB)
 $(DUPEFIND): $(XPDEV_LIB) $(SMBLIB)
 $(SMBACTIV): $(XPDEV_LIB) $(SMBLIB)
 $(DSTSEDIT): $(XPDEV_LIB)
-$(READSAUCE): $(XPDEV_LIB)
