e3225c56d757a1f0f6a9b6c3628c2541657d19bd
rswindell <rswindell>
2017-11-27 06:30:33 +0000

    Support the decoding/downloading of multiple MIME attachments.

--- sbbs3/js_console.cpp
+++ sbbs3/js_console.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet JavaScript "Console" Object */
 
-/* $Id: js_console.cpp,v 1.115 2018/02/20 11:25:55 rswindell Exp $ */
+/* $Id: js_console.cpp,v 1.109 2015/11/08 04:57:26 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2014 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -47,14 +47,12 @@ enum {
 	 CON_PROP_STATUS
 	,CON_PROP_LNCNTR 
 	,CON_PROP_COLUMN
-	,CON_PROP_LASTLINELEN
 	,CON_PROP_ATTR
 	,CON_PROP_TOS
 	,CON_PROP_ROWS
 	,CON_PROP_COLUMNS
 	,CON_PROP_AUTOTERM
 	,CON_PROP_TERMINAL
-	,CON_PROP_CTERM_VERSION
 	,CON_PROP_WORDWRAP
 	,CON_PROP_QUESTION
 	,CON_PROP_INACTIV_WARN
@@ -97,9 +95,6 @@ static JSBool js_console_get(JSContext *cx, JSObject *obj, jsid id, jsval *vp)
 		case CON_PROP_COLUMN:
 			val=sbbs->column;
 			break;
-		case CON_PROP_LASTLINELEN:
-			val=sbbs->lastlinelen;
-			break;
 		case CON_PROP_ATTR:
 			val=sbbs->curatr;
 			break;
@@ -120,9 +115,6 @@ static JSBool js_console_get(JSContext *cx, JSObject *obj, jsid id, jsval *vp)
 				return(JS_FALSE);
 			*vp = STRING_TO_JSVAL(js_str);
 			return(JS_TRUE);
-		case CON_PROP_CTERM_VERSION:
-			val=sbbs->cterm_version;
-			break;
 
 		case CON_PROP_INACTIV_WARN:
 			val=sbbs->cfg.sec_warn;
@@ -214,9 +206,6 @@ static JSBool js_console_set(JSContext *cx, JSObject *obj, jsid id, JSBool stric
 		case CON_PROP_COLUMN:
 			sbbs->column=val;
 			break;
-		case CON_PROP_LASTLINELEN:
-			sbbs->lastlinelen=val;
-			break;
 		case CON_PROP_ATTR:
 			if(JSVAL_IS_STRING(*vp)) {
 				JSVALUE_TO_MSTRING(cx, *vp, sval, NULL);
@@ -247,9 +236,6 @@ static JSBool js_console_set(JSContext *cx, JSObject *obj, jsid id, JSBool stric
 				break;
 			SAFECOPY(sbbs->terminal,sval);
 			free(sval);
-			break;
-		case CON_PROP_CTERM_VERSION:
-			sbbs->cterm_version = val;
 			break;
 		case CON_PROP_INACTIV_WARN:
 			sbbs->cfg.sec_warn = (uint16_t)val;
@@ -316,14 +302,12 @@ static jsSyncPropertySpec js_console_properties[] = {
 	{	"status"			,CON_PROP_STATUS			,CON_PROP_FLAGS	,310},
 	{	"line_counter"		,CON_PROP_LNCNTR 			,CON_PROP_FLAGS	,310},
 	{	"current_column"	,CON_PROP_COLUMN			,CON_PROP_FLAGS ,315},
-	{	"last_line_length"	,CON_PROP_LASTLINELEN		,CON_PROP_FLAGS	,317},
 	{	"attributes"		,CON_PROP_ATTR				,CON_PROP_FLAGS	,310},
 	{	"top_of_screen"		,CON_PROP_TOS				,CON_PROP_FLAGS	,310},
 	{	"screen_rows"		,CON_PROP_ROWS				,CON_PROP_FLAGS	,310},
 	{	"screen_columns"	,CON_PROP_COLUMNS			,CON_PROP_FLAGS	,311},
 	{	"autoterm"			,CON_PROP_AUTOTERM			,CON_PROP_FLAGS	,310},
 	{	"terminal"			,CON_PROP_TERMINAL			,CON_PROP_FLAGS ,311},
-	{	"cterm_version"		,CON_PROP_CTERM_VERSION		,CON_PROP_FLAGS ,317},
 	{	"inactivity_warning",CON_PROP_INACTIV_WARN		,CON_PROP_FLAGS, 31401},
 	{	"inactivity_hangup"	,CON_PROP_INACTIV_HANGUP	,CON_PROP_FLAGS, 31401},
 	{	"timeout"			,CON_PROP_TIMEOUT			,CON_PROP_FLAGS	,310},
@@ -344,15 +328,14 @@ static jsSyncPropertySpec js_console_properties[] = {
 
 #ifdef BUILD_JSDOCS
 static char* con_prop_desc[] = {
-	 "status bit-field (see <tt>CON_*</tt> in <tt>sbbsdefs.js</tt> for bit definitions)"
+	 "status bitfield (see <tt>CON_*</tt> in <tt>sbbsdefs.js</tt> for bit definitions)"
 	,"current 0-based line counter (used for automatic screen pause)"
 	,"current 0-based column counter (used to auto-increment <i>line_counter</i> when screen wraps)"
-	,"length of last line sent to terminal (before a carriage-return or line-wrap)"
 	,"current display attributes (set with number or string value)"
 	,"set to <i>true</i> if the terminal cursor is already at the top of the screen"
 	,"number of remote terminal screen rows (in lines)"
 	,"number of remote terminal screen columns (in character cells)"
-	,"bit-field of automatically detected terminal settings "
+	,"bitfield of automatically detected terminal settings "
 		"(see <tt>USER_*</tt> in <tt>sbbsdefs.js</tt> for bit definitions)"
 	,"terminal type description (e.g. 'ANSI')"
 	,"number of seconds before displaying warning (Are you really there?) due to user/keyboard inactivity"
@@ -361,11 +344,11 @@ static char* con_prop_desc[] = {
 	,"number of low time-left (5 or fewer minutes remaining) warnings displayed to user"
 	,"input/output has been aborted"
 	,"remote output can be asynchronously aborted with Ctrl-C"
-	,"current Telnet mode bit-field (see <tt>TELNET_MODE_*</tt> in <tt>sbbsdefs.js</tt> for bit definitions)"
+	,"current Telnet mode bitfield (see <tt>TELNET_MODE_*</tt> in <tt>sbbsdefs.js</tt> for bit definitions)"
 	,"word-wrap buffer (used by getstr) - <small>READ ONLY</small>"
 	,"current yes/no question (set by yesno and noyes)"
 	,"cursor position offset for use with <tt>getstr(K_USEOFFSET)</tt>"
-	,"control key pass-through bit-mask, set bits represent control key combinations "
+	,"control key pass-through bitmask, set bits represent control key combinations "
 		"<i>not</i> handled by <tt>inkey()</tt> method "
 		"This may optionally be specified as a string of characters. "
 		"The format of this string is [+-][@-_]. If neither plus nor minus is "
@@ -531,10 +514,8 @@ js_getstr(JSContext *cx, uintN argc, jsval *arglist)
 
 	if(js_str!=NULL) {
 		JSSTRING_TO_MSTRING(cx, js_str, p2, NULL);
-		if(p2==NULL) {
-			free(p);
+		if(p2==NULL)
 			return JS_FALSE;
-		}
 		sprintf(p,"%.*s",(int)maxlen,p2);
 		free(p2);
 	}
@@ -1237,24 +1218,17 @@ js_uselect(JSContext *cx, uintN argc, jsval *arglist)
 	
 	for(i=0;i<argc;i++) {
 		if(JSVAL_IS_NUMBER(argv[i])) {
-			if(!JS_ValueToInt32(cx,argv[i],&num)) {
-				FREE_AND_NULL(title);
-				FREE_AND_NULL(item);
+			if(!JS_ValueToInt32(cx,argv[i],&num))
 				return JS_FALSE;
-			}
 			continue;
 		}
-		if((js_str=JS_ValueToString(cx, argv[i]))==NULL) {
-			FREE_AND_NULL(title);
-			FREE_AND_NULL(item);
+		if((js_str=JS_ValueToString(cx, argv[i]))==NULL)
 			return(JS_FALSE);
-		}
+
 		if(title==NULL) {
 			JSSTRING_TO_MSTRING(cx, js_str, title, NULL)	// Magicsemicolon
-			if(title==NULL) {
-				FREE_AND_NULL(item);
+			if(title==NULL)
 				return JS_FALSE;
-			}
 		}
 		else if(item==NULL) {
 			JSSTRING_TO_MSTRING(cx, js_str, item, NULL)	// Magicsemicolon
@@ -1680,7 +1654,6 @@ js_telnet_cmd(JSContext *cx, uintN argc, jsval *arglist)
 	jsval *argv=JS_ARGV(cx, arglist);
 	sbbs_t*		sbbs;
 	int32		cmd,opt=0;
-	int32		wait=0;
 	jsrefcount	rc;
 
 	JS_SET_RVAL(cx, arglist, JSVAL_VOID);
@@ -1694,20 +1667,9 @@ js_telnet_cmd(JSContext *cx, uintN argc, jsval *arglist)
 		if(!JS_ValueToInt32(cx,argv[1],&opt))
 			return JS_FALSE;
 	}
-	if(argc>2) {
-		if(!JS_ValueToInt32(cx,argv[2],&wait))
-			return JS_FALSE;
-	}
 
 	rc=JS_SUSPENDREQUEST(cx);
-	if(wait) {
-		if(sbbs->request_telnet_opt((uchar)cmd, (uchar)opt, wait) == true) {
-			JS_SET_RVAL(cx, arglist, JSVAL_TRUE);
-		} else {
-			JS_SET_RVAL(cx, arglist, JSVAL_FALSE);
-		}
-	} else
-		sbbs->send_telnet_cmd((uchar)cmd, (uchar)opt);
+	sbbs->send_telnet_cmd((uchar)cmd,(uchar)opt);
 	JS_RESUMEREQUEST(cx, rc);
 
     return(JS_TRUE);
@@ -1771,8 +1733,8 @@ static jsSyncMethodSpec js_console_functions[] = {
 	,JSDOCSTR("get a string based on template")
 	,310
 	},		
-	{"ungetstr",		js_ungetstr,		1, JSTYPE_VOID,		JSDOCSTR("keys")
-	,JSDOCSTR("put a data (e.g. a string of characters) in the keyboard input buffer")
+	{"ungetstr",		js_ungetstr,		1, JSTYPE_VOID,		JSDOCSTR("")
+	,JSDOCSTR("put a string in the keyboard buffer")
 	,310
 	},		
 	{"yesno",			js_yesno,			1, JSTYPE_BOOLEAN,	JSDOCSTR("question")
@@ -1887,8 +1849,8 @@ static jsSyncMethodSpec js_console_functions[] = {
 	},
 	{"ansi_gotoxy",		js_gotoxy,			1, JSTYPE_ALIAS },
 	{"gotoxy",			js_gotoxy,			1, JSTYPE_VOID,		JSDOCSTR("[x,y] or [object { x,y }]")
-	,JSDOCSTR("move cursor to a specific screen coordinate (ANSI, 1-based values), "
-	"arguments can be separate x and y coordinates or an object with x and y properties "
+	,JSDOCSTR("move cursor to a specific screen coordinate (ANSI), "
+	"arguments can be separate x and y cooridinates or an object with x and y properites "
 	"(like that returned from <tt>console.getxy()</tt>)")
 	,311
 	},
@@ -1928,11 +1890,9 @@ static jsSyncMethodSpec js_console_functions[] = {
 	,JSDOCSTR("lock the user input thread (allowing direct client socket access)")
 	,310
 	},
-	{"telnet_cmd",		js_telnet_cmd,		3, JSTYPE_BOOLEAN,		JSDOCSTR("command [,option=<tt>0</tt>] [,timeout=<tt>0</tt>]")
-	,JSDOCSTR("send Telnet command (with optional command option) to remote client"
-		", if the optional timeout is specified (in milliseconds, added in v3.17), then an acknowledgment will be expected and"
-		" the return value will indicate whether or not one was received")
-	,317
+	{"telnet_cmd",		js_telnet_cmd,		2, JSTYPE_VOID,		JSDOCSTR("command [,option=<tt>0</tt>]")
+	,JSDOCSTR("send Telnet command (with optional command option) to remote client")
+	,310
 	},
 	{"handle_ctrlkey",	js_handle_ctrlkey,	1, JSTYPE_BOOLEAN,	JSDOCSTR("key [,mode=<tt>K_NONE</tt>]")
 	,JSDOCSTR("call internal control key handler for specified control key, returns <tt>true</tt> if handled")
@@ -1967,7 +1927,7 @@ static JSBool js_console_resolve(JSContext *cx, JSObject *obj, jsid id)
 		JS_IdToValue(cx, id, &idval);
 		if(JSVAL_IS_STRING(idval)) {
 			JSSTRING_TO_MSTRING(cx, JSVAL_TO_STRING(idval), name, NULL);
-			HANDLE_PENDING(cx, name);
+			HANDLE_PENDING(cx);
 		}
 	}
 
