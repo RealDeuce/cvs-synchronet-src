3be01f579f7b6ccd604fb882e44f8f230a859ebe
rswindell <rswindell>
2017-11-28 03:42:09 +0000

    smb_getplaintext() now returns NULL if there is no MIME-encoded plain-text. This allows a message displayer to indicate to the user that some MIME-decoding took place.

--- sbbs3/jsexec.c
+++ sbbs3/jsexec.c
@@ -1,8 +1,7 @@
-/* jsexec.c */
-
 /* Execute a Synchronet JavaScript module from the command-line */
 
-/* $Id: jsexec.c,v 1.184 2016/11/15 22:03:14 rswindell Exp $ */
+/* $Id: jsexec.c,v 1.187 2017/11/27 22:38:59 rswindell Exp $ */
+// vi: tabstop=4
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -877,6 +876,7 @@ long js_exec(const char *fname, char** args)
 	int32		result=0;
 	long double	start;
 	long double	diff;
+	JSBool		exec_result;
 
 	if(fname!=NULL) {
 		if(isfullpath(fname)) {
@@ -982,7 +982,7 @@ long js_exec(const char *fname, char** args)
 	start=xp_timer();
 	if(debugger)
 		debug_prompt(js_cx, js_script);
-	JS_ExecuteScript(js_cx, js_glob, js_script, &rval);
+	exec_result = JS_ExecuteScript(js_cx, js_glob, js_script, &rval);
 	JS_GetProperty(js_cx, js_glob, "exit_code", &rval);
 	if(rval!=JSVAL_VOID && JSVAL_IS_NUMBER(rval)) {
 		char	*p;
@@ -1004,6 +1004,8 @@ long js_exec(const char *fname, char** args)
 	if(js_buf!=NULL)
 		free(js_buf);
 
+	if(result == 0 && !exec_result)
+		return EXIT_FAILURE;
 	return(result);
 }
 
@@ -1108,7 +1110,7 @@ int main(int argc, char **argv, char** environ)
 	cb.gc_interval=JAVASCRIPT_GC_INTERVAL;
 	cb.auto_terminate=TRUE;
 
-	sscanf("$Revision: 1.184 $", "%*s %s", revision);
+	sscanf("$Revision: 1.187 $", "%*s %s", revision);
 	DESCRIBE_COMPILER(compiler);
 
 	memset(&scfg,0,sizeof(scfg));
