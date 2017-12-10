41f8c3680425acafb92e6edb048aee46ec10b33c
rswindell <rswindell>
2017-12-10 12:05:48 +0000

    When importing QWK messages from a QWKnet hub, parse/strip all QWK kludge lines - don't check the (invalid) sbbs_t:useron.qwk settings for QWKE support in this case (!). If you've been seeing the occasional message with "Subject:" as the first line followed by some @-kludge lines (e.g. @MSGID, @TZ, etc.) this would be the cause: you have QWKE support enabled on your hub BBS (e.g. VERT), which adds the "Subject:" kludge lines for subjects > 25 chars and when importing QWK messages from a *hub*, these lines weren't (normally) parsed/stripped.

--- sbbs3/scfg/scfg.c
+++ sbbs3/scfg/scfg.c
@@ -1,6 +1,6 @@
 /* Synchronet configuration utility 										*/
 
-/* $Id: scfg.c,v 1.93 2017/12/29 06:04:36 rswindell Exp $ */
+/* $Id: scfg.c,v 1.92 2017/11/26 00:08:16 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -475,11 +475,11 @@ int main(int argc, char **argv)
 			"    Networks             : Message networking configuration\n"
 			"    File Areas           : File area configuration\n"
 			"    File Options         : File area options\n"
-			"    Chat Features        : Chat actions, sections, pagers, and robots\n"
+			"    Chat Features        : Chat actions, sections, pagers, and gurus\n"
 			"    Message Areas        : Message area configuration\n"
-			"    Message Options      : Message and e-mail options\n"
-			"    External Programs    : Events, editors, and online programs (doors)\n"
-			"    Text File Sections   : Text file areas available for online viewing\n"
+			"    Message Options      : Message and email options\n"
+			"    External Programs    : Events, editors, and online programs\n"
+			"    Text File Sections   : General text file area\n"
 			"\n"
 			"Use the arrow keys and ~ ENTER ~ to select an option, or ~ ESC ~ to exit.\n"
 		;
@@ -593,10 +593,6 @@ int main(int argc, char **argv)
 					strcpy(opt[i++],"Multinode Chat Channels");
 					strcpy(opt[i++],"External Sysop Chat Pagers");
 					opt[i][0]=0;
-					uifc.helpbuf=
-						"`Chat Features:`\n"
-						"\n"
-						"Here you may configure the real-time chat-related features of the BBS.";
 					j=uifc.list(WIN_ORG|WIN_ACT|WIN_CHE,0,0,0,&chat_dflt,0
 						,"Chat Features",opt);
 					if(j==-1) {
