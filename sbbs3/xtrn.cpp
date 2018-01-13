f4bcc73df76652a03f8043c9870b8b3b8e094525
rswindell <rswindell>
2018-01-13 09:07:47 +0000

    Fix typo and add const qualifier.

--- sbbs3/xtrn.cpp
+++ sbbs3/xtrn.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet external program support routines */
 
-/* $Id: xtrn.cpp,v 1.227 2018/01/13 09:00:24 rswindell Exp $ */
+/* $Id: xtrn.cpp,v 1.228 2018/01/13 09:07:47 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1558,7 +1558,7 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 		/* change to the drive where the parent of the startup_dir is mounted */
 		fprintf(dosemubat,"%s\r\n",xtrndrive);
 
-		char* gamedir = "";
+		const char* gamedir = "";
 		if(startup_dir!=NULL && startup_dir[0]) {
 			SAFECOPY(str, startup_dir);
 			*lastchar(str) = 0;
@@ -1566,7 +1566,7 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 		}
 		if(*gamedir == 0) {
 			lprintf(LOG_ERR, "No startup directory configured for: %s", cmdline);
-			reutrn -1;
+			return -1;
 		}
 		fprintf(dosemubat,"cd %s\r\n", gamedir);
 
