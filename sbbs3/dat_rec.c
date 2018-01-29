c4b80d767957998de4f1ca583f240c5d01411c6b
deuce <deuce>
2018-01-29 17:24:20 +0000

    Remove note about doorway mode being saved/restored.

--- sbbs3/dat_rec.c
+++ sbbs3/dat_rec.c
@@ -2,13 +2,13 @@
 
 /* Synchronet text data-related routines (exported) */
 
-/* $Id: dat_rec.c,v 1.3 2018/02/20 11:56:26 rswindell Exp $ */
+/* $Id: dat_rec.c,v 1.2 2008/06/04 04:38:47 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2000 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
