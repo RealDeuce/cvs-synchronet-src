4f71ec6f22bb33823d99aae27239160acff6a451
deuce <deuce>
2018-01-29 22:42:55 +0000

    Set SSH timeout to 1 second.
    Previous versions of Cryptlib defaulted to a zero second timeout... this
    is no longer the case as it appears to be 15 seconds now.  Setting it to
    zero makes it not work at all.

--- sbbs3/fixsmb.c
+++ sbbs3/fixsmb.c
@@ -1,6 +1,6 @@
 /* Synchronet message base (SMB) index re-generator */
 
-/* $Id: fixsmb.c,v 1.45 2018/02/21 05:44:02 rswindell Exp $ */
+/* $Id: fixsmb.c,v 1.41 2017/11/28 06:41:45 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -46,10 +46,8 @@
 
 smb_t	smb;
 BOOL	renumber=FALSE;
-BOOL	rehash=FALSE;
-BOOL	fixnums=FALSE;
 BOOL	smb_undelete=FALSE;
-char*	usage="usage: fixsmb [-renumber] [-undelete] [-fixnums] [-rehash] <smb_file> [[smb_file] [...]]";
+char*	usage="usage: fixsmb [-renumber] [-undelete] <smb_file> [[smb_file] [...]]";
 
 int compare_index(const idxrec_t* idx1, const idxrec_t* idx2)
 {
@@ -110,7 +108,6 @@ int fixsmb(char* sub)
 	uint32_t*	numbers = NULL;
 	uint32_t	total = 0;
 	BOOL		dupe_msgnum;
-	uint32_t	highest = 0;
 
 	memset(&smb,0,sizeof(smb));
 
@@ -118,13 +115,6 @@ int fixsmb(char* sub)
 
 	if((p=getfext(smb.file))!=NULL && stricmp(p,".shd")==0)
 		*p=0;	/* Chop off .shd extension, if supplied on command-line */
-
-	char path[MAX_PATH+1];
-	SAFEPRINTF(path, "%s.shd", smb.file);
-	if(!fexistcase(path)) {
-		printf("%s does not exist\n", path);
-		exit(1);
-	}
 
 	printf("Opening %s\n",smb.file);
 
@@ -151,8 +141,6 @@ int fixsmb(char* sub)
 		exit(1);
 	}
 
-	uint32_t last_msg = smb.status.last_msg;
-
 	if(!(smb.status.attr&SMB_HYPERALLOC)) {
 
 		if((i=smb_open_ha(&smb))!=0) {
@@ -176,8 +164,8 @@ int fixsmb(char* sub)
 	rewind(smb.sid_fp);
 	chsize(fileno(smb.sid_fp),0L);			/* Truncate the index */
 
-	if(renumber || rehash) {
-		printf("Truncating hash file (due to renumbering/rehashing)\n");
+	if(renumber) {
+		printf("Truncating hash file (due to renumbering)\n");
 		if((i=smb_open_hash(&smb))!=SMB_SUCCESS) {
 			printf("smb_open_hash returned %d: %s\n", i, smb.last_error);
 			exit(1);
@@ -223,11 +211,6 @@ int fixsmb(char* sub)
 			if(msg.hdr.number == numbers[i])
 				dupe_msgnum = TRUE;
 
-		if(dupe_msgnum && fixnums && msg.hdr.number >= last_msg) {
-			printf("Fixed message number (%lu -> %lu)\n", (ulong)msg.hdr.number, (ulong)highest + 1);
-			msg.hdr.number = highest + 1;
-			dupe_msgnum = FALSE;
-		}
 		if(!dupe_msgnum) {
 			total++;
 			if((numbers = realloc(numbers, total * sizeof(*numbers))) == NULL) {
@@ -264,8 +247,6 @@ int fixsmb(char* sub)
 			msg.offset=n;
 			if(renumber)
 				msg.hdr.number=n+1;
-			if(msg.hdr.number > highest)
-				highest = msg.hdr.number;
 			if(msg.hdr.netattr&MSG_INTRANSIT) {
 				printf("Removing 'in transit' attribute\n");
 				msg.hdr.netattr&=~MSG_INTRANSIT;
@@ -301,12 +282,9 @@ int fixsmb(char* sub)
 	printf("\r%79s\r100%%\n","");
 	smb.status.total_msgs=n;
 	if(renumber)
-		smb.status.last_msg = highest;
-	else {
-		if(highest > smb.status.last_msg)
-			smb.status.last_msg = highest;
+		smb.status.last_msg=n;
+	else
 		sort_index(&smb);
-	}
 	printf("Saving message base status (%lu total messages).\n",n);
 	if((i=smb_putstatus(&smb))!=0)
 		printf("\nsmb_putstatus returned %d: %s\n",i,smb.last_error);
@@ -326,7 +304,7 @@ int main(int argc, char **argv)
 	str_list_t	list;
 	int			retval = EXIT_SUCCESS;
 
-	sscanf("$Revision: 1.45 $", "%*s %s", revision);
+	sscanf("$Revision: 1.41 $", "%*s %s", revision);
 
 	printf("\nFIXSMB v2.10-%s (rev %s) SMBLIB %s - Rebuild Synchronet Message Base\n\n"
 		,PLATFORM_DESC,revision,smb_lib_ver());
@@ -337,12 +315,8 @@ int main(int argc, char **argv)
 		if(argv[i][0]=='-') {
 			if(!stricmp(argv[i],"-renumber"))
 				renumber=TRUE;
-			else if(!stricmp(argv[i],"-rehash"))
-				rehash=TRUE;
 			else if(!stricmp(argv[i],"-undelete"))
 				smb_undelete=TRUE;
-			else if(!stricmp(argv[i],"-fixnums"))
-				fixnums=TRUE;
 		} else
 			strListPush(&list,argv[i]);
 	}
