ec4bf3c2d871dfc0d05ac53f3f09d6c1410644d2
deuce <deuce>
2018-01-29 17:20:34 +0000

    Attempt to fix CSI ? s/n
    These basically never worked as documented.  Now they should, but they can't
    save/restore the doorway mode setting since it's not set/cleared using
    CSI ? h/l.  If that's really needed (you need to know the doorway state
    regardless, so it's more likely that a query doorway command is needed if
    anything) it can be added as a separate command.

--- sbbs3/getmsg.cpp
+++ sbbs3/getmsg.cpp
@@ -1,8 +1,7 @@
-/* getmsg.cpp */
-
 /* Synchronet message retrieval functions */
 
-/* $Id: getmsg.cpp,v 1.50 2015/12/04 08:48:22 rswindell Exp $ */
+/* $Id: getmsg.cpp,v 1.73 2018/01/12 22:21:50 rswindell Exp $ */
+// vi: tabstop=4
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -44,7 +43,7 @@
 /****************************************************************************/
 /* Loads an SMB message from the open msg base the fastest way possible 	*/
 /* first by offset, and if that's the wrong message, then by number.        */
-/* Returns 1 if the message was loaded and left locked, otherwise			*/
+/* Returns >=0 if the message was loaded and left locked, otherwise < 0.	*/
 /* !WARNING!: If you're going to write the msg index back to disk, you must */
 /* Call this function with a msg->idx.offset of 0 (so msg->offset will be	*/
 /* initialized correctly)													*/
@@ -56,15 +55,15 @@ int sbbs_t::loadmsg(smbmsg_t *msg, ulong number)
 
 	if(msg->idx.offset) {				/* Load by offset if specified */
 
-		if((i=smb_lockmsghdr(&smb,msg))!=0) {
+		if((i=smb_lockmsghdr(&smb,msg)) != SMB_SUCCESS) {
 			errormsg(WHERE,ERR_LOCK,smb.file,i,smb.last_error);
-			return(0); 
+			return i; 
 		}
 
 		i=smb_getmsghdr(&smb,msg);
 		if(i==SMB_SUCCESS) {
 			if(msg->hdr.number==number)
-				return(1);
+				return msg->total_hfields;
 			/* Wrong offset  */
 			smb_freemsgmem(msg);
 		}
@@ -74,27 +73,31 @@ int sbbs_t::loadmsg(smbmsg_t *msg, ulong number)
 
 	msg->hdr.number=number;
 	if((i=smb_getmsgidx(&smb,msg))!=SMB_SUCCESS)				 /* Message is deleted */
-		return(0);
+		return i;
 	if((i=smb_lockmsghdr(&smb,msg))!=SMB_SUCCESS) {
 		errormsg(WHERE,ERR_LOCK,smb.file,i,smb.last_error);
-		return(0); 
+		return i;
 	}
 	if((i=smb_getmsghdr(&smb,msg))!=SMB_SUCCESS) {
-		SAFEPRINTF4(str,"(%06"PRIX32") #%"PRIu32"/%lu %s",msg->idx.offset,msg->idx.number
+		SAFEPRINTF4(str,"(%06" PRIX32 ") #%" PRIu32 "/%lu %s",msg->idx.offset,msg->idx.number
 			,number,smb.file);
 		smb_unlockmsghdr(&smb,msg);
 		errormsg(WHERE,ERR_READ,str,i,smb.last_error);
-		return(0); 
+		return i;
 	}
-	return(msg->total_hfields);
+	return msg->total_hfields;
 }
 
-
-void sbbs_t::show_msgattr(ushort attr)
+/* Synchronized with atcode()! */
+void sbbs_t::show_msgattr(smbmsg_t* msg)
 {
+	uint16_t attr = msg->hdr.attr;
+	uint16_t poll = attr&MSG_POLL_VOTE_MASK;
+	uint32_t auxattr = msg->hdr.auxattr;
 
 	bprintf(text[MsgAttr]
 		,attr&MSG_PRIVATE	? "Private  "   :nulstr
+		,attr&MSG_SPAM		? "SPAM  "      :nulstr
 		,attr&MSG_READ		? "Read  "      :nulstr
 		,attr&MSG_DELETE	? "Deleted  "   :nulstr
 		,attr&MSG_KILLREAD	? "Kill  "      :nulstr
@@ -105,8 +108,8 @@ void sbbs_t::show_msgattr(ushort attr)
 		,attr&MSG_VALIDATED ? "Validated  " :nulstr
 		,attr&MSG_REPLIED	? "Replied  "	:nulstr
 		,attr&MSG_NOREPLY	? "NoReply  "	:nulstr
-		,nulstr
-		,nulstr
+		,poll == MSG_POLL	? "Poll  "		:nulstr
+		,poll == MSG_POLL && auxattr&POLL_CLOSED ? "(Closed)  "	:nulstr
 		,nulstr
 		,nulstr
 		,nulstr
@@ -129,35 +132,37 @@ void sbbs_t::show_msghdr(smbmsg_t* msg)
 	else
 		CRLF;
 
-	SAFEPRINTF(str,"%smenu/msghdr.*", cfg.text_dir);
-	if(fexist(str)) {
+	if(menu_exists("msghdr")) {
 		menu("msghdr");
-		return; 
+	} else {
+		bprintf(text[MsgSubj],msg->subj);
+		if(msg->hdr.attr)
+			show_msgattr(msg);
+		if(msg->to && *msg->to) {
+			bprintf(text[MsgTo],msg->to);
+			if(msg->to_net.addr!=NULL)
+				bprintf(text[MsgToNet],smb_netaddrstr(&msg->to_net,str));
+			if(msg->to_ext)
+				bprintf(text[MsgToExt],msg->to_ext);
+		}
+		if(!(msg->hdr.attr&MSG_ANONYMOUS) || SYSOP) {
+			bprintf(text[MsgFrom],msg->from);
+			if(msg->from_ext)
+				bprintf(text[MsgFromExt],msg->from_ext);
+			if(msg->from_net.addr!=NULL && strchr(msg->from,'@')==NULL)
+				bprintf(text[MsgFromNet],smb_netaddrstr(&msg->from_net,str)); 
+		}
+		if(!(msg->hdr.attr&MSG_POLL) && (msg->upvotes || msg->downvotes))
+			bprintf(text[MsgVotes]
+				,msg->upvotes, msg->user_voted==1 ? text[PollAnswerChecked] : nulstr
+				,msg->downvotes, msg->user_voted==2 ? text[PollAnswerChecked] : nulstr
+				,msg->upvotes - msg->downvotes);
+		bprintf(text[MsgDate]
+			,timestr(msg->hdr.when_written.time)
+			,smb_zonestr(msg->hdr.when_written.zone,NULL)
+			,age_of_posted_item(age, sizeof(age), msg->hdr.when_written.time - (smb_tzutc(msg->hdr.when_written.zone) * 60)));
+		bputs(text[MsgHdrBodySeparator]);
 	}
-
-	bprintf(text[MsgSubj],msg->subj);
-	if(msg->hdr.attr)
-		show_msgattr(msg->hdr.attr);
-
-	bprintf(text[MsgTo],msg->to);
-	if(msg->to_net.addr)
-		bprintf(text[MsgToNet],smb_netaddrstr(&msg->to_net,str));
-	if(msg->to_ext)
-		bprintf(text[MsgToExt],msg->to_ext);
-	if(!(msg->hdr.attr&MSG_ANONYMOUS) || SYSOP) {
-		bprintf(text[MsgFrom],msg->from);
-		if(msg->from_ext)
-			bprintf(text[MsgFromExt],msg->from_ext);
-		if(msg->from_net.addr && !strchr(msg->from,'@'))
-			bprintf(text[MsgFromNet],smb_netaddrstr(&msg->from_net,str)); 
-	}
-	bprintf(text[MsgDate]
-		,timestr(msg->hdr.when_written.time)
-		,smb_zonestr(msg->hdr.when_written.zone,NULL)
-		,age_of_posted_item(age, sizeof(age), msg->hdr.when_written.time - (smb_tzutc(msg->hdr.when_written.zone) * 60)));
-
-	CRLF;
-
 	for(i=0;i<msg->total_hfields;i++) {
 		if(msg->hfield[i].type==SENDER)
 			sender=(char *)msg->hfield_dat[i];
@@ -165,34 +170,109 @@ void sbbs_t::show_msghdr(smbmsg_t* msg)
 			bprintf(text[ForwardedFrom],sender
 				,timestr(*(time32_t *)msg->hfield_dat[i])); 
 	}
-	CRLF;
 }
 
 /****************************************************************************/
 /* Displays message header and text (if not deleted)                        */
 /****************************************************************************/
-void sbbs_t::show_msg(smbmsg_t* msg, long mode)
+void sbbs_t::show_msg(smbmsg_t* msg, long mode, post_t* post)
 {
-	char*	text;
+	char*	txt;
+
+	if((msg->hdr.type == SMB_MSG_TYPE_NORMAL && post != NULL && (post->upvotes || post->downvotes))
+		|| msg->hdr.type == SMB_MSG_TYPE_POLL)
+		msg->user_voted = smb_voted_already(&smb, msg->hdr.number
+					,cfg.sub[smb.subnum]->misc&SUB_NAME ? useron.name : useron.alias, NET_NONE, NULL);
 
 	show_msghdr(msg);
 
-	if((text=smb_getmsgtxt(&smb,msg,(console&CON_RAW_IN) ? 0:GETMSGTXT_PLAIN)) != NULL) {
-		if(!(console&CON_RAW_IN))
-			mode|=P_WORDWRAP;
-		putmsg(text, mode);
-		smb_freemsgtxt(text);
+	if(msg->hdr.type == SMB_MSG_TYPE_POLL && post != NULL && smb.subnum < cfg.total_subs) {
+		char* answer;
+		int longest_answer = 0;
+
+		int comments=0;
+		for(int i = 0; i < msg->total_hfields; i++)
+			if(msg->hfield[i].type == SMB_COMMENT) {
+				bprintf("%s\r\n", (char*)msg->hfield_dat[i]);
+				comments++;
+			}
+		if(comments)
+			CRLF;
+
+		for(int i = 0; i < msg->total_hfields; i++) {
+			if(msg->hfield[i].type != SMB_POLL_ANSWER)
+				continue;
+			answer = (char*)msg->hfield_dat[i];
+			int len = strlen(answer);
+			if(len > longest_answer)
+				longest_answer = len;
+		}
+		unsigned answers = 0;
+		for(int i = 0; i < msg->total_hfields; i++) {
+			if(msg->hfield[i].type != SMB_POLL_ANSWER)
+				continue;
+			answer = (char*)msg->hfield_dat[i];
+			float pct = post->total_votes ? ((float)post->votes[answers] / post->total_votes)*100.0F : 0.0F;
+			char str[128];
+			int width = longest_answer;
+			if(width < cols/3) width = cols/3;
+			else if(width > cols-20)
+				width = cols-20;
+			bprintf(text[PollAnswerNumber], answers+1);
+			bool results_visible = false;
+			if((msg->hdr.auxattr&POLL_RESULTS_MASK) == POLL_RESULTS_OPEN)
+				results_visible = true;
+			else if((msg->from_net.type == NET_NONE && sub_op(smb.subnum)) 
+				|| smb_msg_is_from(msg, cfg.sub[smb.subnum]->misc&SUB_NAME ? useron.name : useron.alias, NET_NONE, NULL))
+				results_visible = true;
+			else if((msg->hdr.auxattr&POLL_RESULTS_MASK) == POLL_RESULTS_CLOSED)
+				results_visible = (msg->hdr.auxattr&POLL_CLOSED) ? true : false;
+			else if((msg->hdr.auxattr&POLL_RESULTS_MASK) != POLL_RESULTS_SECRET)
+				results_visible = msg->user_voted ? true : false;
+			if(results_visible) {
+				safe_snprintf(str, sizeof(str), text[PollAnswerFmt]
+					,width, width, answer, post->votes[answers], pct);
+				backfill(str, pct, cfg.color[clr_votes_full], cfg.color[clr_votes_empty]);
+				if(msg->user_voted&(1<<answers))
+					bputs(text[PollAnswerChecked]);
+			} else {
+				attr(cfg.color[clr_votes_empty]);
+				bputs(answer);
+			}
+			CRLF;
+			answers++;
+		}
+		if(!msg->user_voted && !(useron.misc&EXPERT) && !(msg->hdr.auxattr&POLL_CLOSED) && !(useron.rest&FLAG('V')))
+			mnemonics(text[VoteInThisPollNow]);
+		return;
 	}
-	if((text=smb_getmsgtxt(&smb,msg,GETMSGTXT_TAIL_ONLY))!=NULL) {
-		putmsg(text, mode&(~P_WORDWRAP));
-		smb_freemsgtxt(text);
+	if((txt=smb_getmsgtxt(&smb, msg, 0)) != NULL) {
+		char* p = txt;
+		if(!(console&CON_RAW_IN)) {
+			mode|=P_WORDWRAP;
+			p = smb_getplaintext(msg, txt);
+			if(p == NULL)
+				p = txt;
+			else
+				bputs(text[MIMEDecodedPlainText]);
+		}
+		truncsp(p);
+		SKIP_CRLF(p);
+		putmsg(p, mode);
+		smb_freemsgtxt(txt);
+		if(column)
+			CRLF;
+	}
+	if((txt=smb_getmsgtxt(&smb,msg,GETMSGTXT_TAIL_ONLY))!=NULL) {
+		putmsg(txt, mode&(~P_WORDWRAP));
+		smb_freemsgtxt(txt);
 	}
 }
 
 /****************************************************************************/
 /* Writes message header and text data to a text file						*/
 /****************************************************************************/
-void sbbs_t::msgtotxt(smbmsg_t* msg, char *str, int header, int tails)
+void sbbs_t::msgtotxt(smbmsg_t* msg, char *str, bool header, ulong mode)
 {
 	char	*buf;
 	char	tmp[128];
@@ -207,10 +287,10 @@ void sbbs_t::msgtotxt(smbmsg_t* msg, char *str, int header, int tails)
 		fprintf(out,"\r\n");
 		fprintf(out,"Subj : %s\r\n",msg->subj);
 		fprintf(out,"To   : %s",msg->to);
-		if(msg->to_ext)
-			fprintf(out," #%s",msg->to_ext);
 		if(msg->to_net.addr)
 			fprintf(out," (%s)",smb_netaddrstr(&msg->to_net,tmp));
+		if(msg->to_ext)
+			fprintf(out," #%s",msg->to_ext);
 		fprintf(out,"\r\nFrom : %s",msg->from);
 		if(msg->from_ext && !(msg->hdr.attr&MSG_ANONYMOUS))
 			fprintf(out," #%s",msg->from_ext);
@@ -222,7 +302,7 @@ void sbbs_t::msgtotxt(smbmsg_t* msg, char *str, int header, int tails)
 		fprintf(out,"\r\n\r\n"); 
 	}
 
-	buf=smb_getmsgtxt(&smb,msg,tails);
+	buf=smb_getmsgtxt(&smb,msg,mode);
 	if(buf!=NULL) {
 		strip_invalid_attr(buf);
 		fputs(buf,out);
--- sbbs3/mail.cpp
+++ sbbs3/mail.cpp
@@ -1,14 +1,12 @@
-/* mail.cpp */
-
 /* Synchronet mail-related routines */
 
-/* $Id: mail.cpp,v 1.26 2012/10/24 19:03:13 deuce Exp $ */
+/* $Id: mail.cpp,v 1.30 2017/12/29 06:02:39 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -38,8 +36,9 @@
 #include "sbbs.h"
 
 /****************************************************************************/
-/* Deletes all mail messages for usernumber that have been marked 'deleted' */
+/* Removes all mail messages for usernumber that have been marked 'deleted' */
 /* smb_locksmbhdr() should be called prior to this function 				*/
+/* Returns the number of messages removed									*/
 /****************************************************************************/
 int sbbs_t::delmail(uint usernumber, int which)
 {
@@ -47,28 +46,29 @@ int sbbs_t::delmail(uint usernumber, int which)
 	time_t	now;
 	idxrec_t *idxbuf;
 	smbmsg_t msg;
+	int		removed = 0;
 
 	now=time(NULL);
 	if((i=smb_getstatus(&smb))!=0) {
 		errormsg(WHERE,ERR_READ,smb.file,i,smb.last_error);
-		return(2); 
+		return 0;
 	}
 	if(!smb.status.total_msgs)
-		return(0);
+		return 0;
 	if((idxbuf=(idxrec_t *)malloc(smb.status.total_msgs*sizeof(idxrec_t)))==NULL) {
 		errormsg(WHERE,ERR_ALLOC,smb.file,smb.status.total_msgs*sizeof(idxrec_t));
-		return(-1); 
+		return 0;
 	}
 	if((i=smb_open_da(&smb))!=0) {
 		errormsg(WHERE,ERR_OPEN,smb.file,i,smb.last_error);
 		free(idxbuf);
-		return(i); 
+		return 0;
 	}
 	if((i=smb_open_ha(&smb))!=0) {
 		smb_close_da(&smb);
 		errormsg(WHERE,ERR_OPEN,smb.file,i,smb.last_error);
 		free(idxbuf);
-		return(i); 
+		return 0;
 	}
 	smb_rewind(smb.sid_fp);
 	for(l=0;l<smb.status.total_msgs;) {
@@ -81,6 +81,10 @@ int sbbs_t::delmail(uint usernumber, int which)
 				|| which==MAIL_ALL)) {
 			if(smb.status.max_age && (now<0?0:(uintmax_t)now)>msg.idx.time
 				&& (now-msg.idx.time)/(24L*60L*60L)>smb.status.max_age)
+				msg.idx.attr|=MSG_DELETE;
+			else if((msg.idx.attr&MSG_SPAM)
+				&& cfg.max_spamage && (now<0?0:(uintmax_t)now) > msg.idx.time
+				&& (now-msg.idx.time)/(24L*60L*60L) > cfg.max_spamage)
 				msg.idx.attr|=MSG_DELETE;
 			else if(msg.idx.attr&MSG_KILLREAD && msg.idx.attr&MSG_READ)
 				msg.idx.attr|=MSG_DELETE;
@@ -100,6 +104,7 @@ int sbbs_t::delmail(uint usernumber, int which)
 						delfattach(&cfg,&msg);
 					smb_freemsgmem(&msg); 
 				}
+				removed++;
 				continue; 
 			}
 		}
@@ -116,7 +121,7 @@ int sbbs_t::delmail(uint usernumber, int which)
 	smb_fflush(smb.sid_fp);
 	smb_close_ha(&smb);
 	smb_close_da(&smb);
-	return(0);
+	return removed;
 }
 
 
@@ -161,7 +166,7 @@ void sbbs_t::telluser(smbmsg_t* msg)
 /************************************************************************/
 /* Deletes all mail waiting for user number 'usernumber'                */
 /************************************************************************/
-void sbbs_t::delallmail(uint usernumber, int which, bool permanent)
+void sbbs_t::delallmail(uint usernumber, int which, bool permanent, long lm_mode)
 {
 	int 		i;
 	long		deleted=0;
@@ -182,7 +187,7 @@ void sbbs_t::delallmail(uint usernumber, int which, bool permanent)
 		return; 
 	}
 
-	mail=loadmail(&smb,&msgs,usernumber,which,0);
+	mail=loadmail(&smb,&msgs,usernumber,which,lm_mode);
 	if(!msgs) {
 		smb_close(&smb);
 		smb_stack(&smb,SMB_STACK_POP);
@@ -199,7 +204,7 @@ void sbbs_t::delallmail(uint usernumber, int which, bool permanent)
 		msg.idx.offset=0;						/* search by number */
 		if((mail[u].attr&MSG_PERMANENT) && !permanent)
 			continue;
-		if(loadmsg(&msg,mail[u].number)) {	   /* message still there */
+		if(loadmsg(&msg,mail[u].number) >= 0) {	   /* message still there */
 			msg.hdr.attr|=MSG_DELETE;
 			msg.hdr.attr&=~MSG_PERMANENT;
 			msg.idx.attr=msg.hdr.attr;
