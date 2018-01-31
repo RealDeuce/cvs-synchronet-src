fcb3ce924be9d8d34ae9b8276b9661f2c23995a9
deuce <deuce>
2018-01-31 09:45:35 +0000

    Fix documentation and handling of 8x14 custom fonts.
    CTerm (and therefore SyncTERM 1.0) required an extra two ignored garbage
    bytes in the font data.

--- xpdev/dirwrap.c
+++ xpdev/dirwrap.c
@@ -1,8 +1,7 @@
-/* dirwrap.c */
-
 /* Directory-related system-call wrappers */
+// vi: tabstop=4
 
-/* $Id: dirwrap.c,v 1.92 2017/08/26 06:44:14 rswindell Exp $ */
+/* $Id: dirwrap.c,v 1.95 2018/01/13 06:11:56 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -153,7 +152,7 @@ void DLLCALL _splitpath(const char *path, char *drive, char *dir, char *fname, c
 static int __cdecl glob_compare( const void *arg1, const void *arg2 )
 {
    /* Compare all of both strings: */
-   return stricmp( * ( char** ) arg1, * ( char** ) arg2 );
+   return strcmp( * ( char** ) arg1, * ( char** ) arg2 );
 }
 
 #if defined(__BORLANDC__)
@@ -388,6 +387,22 @@ void DLLCALL rewinddir(DIR* dir)
 	dir->handle=_findfirst(dir->filespec,&dir->finddata);
 }
 #endif /* defined(_MSC_VER) */
+
+/****************************************************************************/
+/* Returns the creation time of the file 'filename' in time_t format		*/
+/****************************************************************************/
+time_t DLLCALL fcdate(const char* filename)
+{
+	struct stat st;
+
+	if(access(filename, 0) < 0)
+		return -1;
+
+	if(stat(filename, &st) != 0)
+		return -1;
+
+	return st.st_ctime;
+}
 
 /****************************************************************************/
 /* Returns the time/date of the file in 'filename' in time_t (unix) format  */
@@ -871,7 +886,7 @@ static ulong getdiskspace(const char* path, ulong unit, BOOL freespace)
 	struct statfs fs;
 	unsigned long blocks;
 
-    if (statfs(path, &fs) < 0)
+	if(statfs(path, &fs) < 0)
     	return 0;
 
 	if(freespace)
