646717757fb0d3384e8a901e6be353728985ed03
Stephen Hurd <shurd@sasktel.net>
2018-02-02 00:58:05 +0000

    Initial support for XTerm 256-color mode.
    You can now use CSI [ 38 ; 5 ; X m to set the foreground colour, and
    CSI [ 48 ; 5 ; X m to set the background colour from the palette and use
    any of the 256 standard colours, any of the 16 Commodore 64 colours (start
    at 256), or the two Atari colors (after the C64 colours).  In addition,
    you can use the palette redefinition commands to extend the palette past
    those entries.
    
    NOTE: Since this is only CTerm so far, using SyncTERM and UIFC features
    will lose the palette blocks for the characters, and they will revert to
    their current 16-color value.  Further, I haven't testing how this interacts
    with alternat fonts, blinking, etc yet.
    
    But it looks cool!

--- conio/cterm.c
+++ conio/cterm.c
@@ -1,4 +1,4 @@
-/* $Id: cterm.c,v 1.178 2018/02/01 23:22:43 deuce Exp $ */
+/* $Id: cterm.c,v 1.179 2018/02/02 00:58:05 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -2085,6 +2085,12 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 									cterm->attr|=3;
 									attr2palette(cterm->attr, &cterm->fg_color, NULL);
 									break;
+								case 38:
+									if (i+2 < seq->param_count && seq->param_int[i+1] == 5) {
+										cterm->fg_color = seq->param_int[i+2];
+										i+=2;
+									}
+									break;
 								case 37:
 								case 39:
 									cterm->attr&=248;
@@ -2130,6 +2136,12 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 									cterm->attr&=143;
 									cterm->attr|=7<<4;
 									attr2palette(cterm->attr, NULL, &cterm->bg_color);
+									break;
+								case 48:
+									if (i+2 < seq->param_count && seq->param_int[i+1] == 5) {
+										cterm->bg_color = seq->param_int[i+2];
+										i+=2;
+									}
 									break;
 							}
 						}
@@ -2357,7 +2369,7 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 
 struct cterminal* CIOLIBCALL cterm_init(int height, int width, int xpos, int ypos, int backlines, unsigned char *scrollback, int emulation)
 {
-	char	*revision="$Revision: 1.178 $";
+	char	*revision="$Revision: 1.179 $";
 	char *in;
 	char	*out;
 	int		i;
@@ -2464,6 +2476,7 @@ void CIOLIBCALL cterm_start(struct cterminal *cterm)
 	if(!cterm->started) {
 		GETTEXTINFO(&ti);
 		cterm->attr=ti.normattr;
+		attr2palette(cterm->attr, &cterm->fg_color, &cterm->bg_color);
 		TEXTATTR(cterm->attr);
 		SETCURSORTYPE(cterm->cursor);
 		cterm->started=1;
