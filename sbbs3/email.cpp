f7eedcb6d7c3db0fc14f0bb90f81ce7a728016b0
deuce <deuce>
2018-02-03 01:38:47 +0000

    Fix default rlogin message

--- sbbs3/email.cpp
+++ sbbs3/email.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet email function - for sending private e-mail */
 
-/* $Id: email.cpp,v 1.67 2018/04/17 04:32:37 rswindell Exp $ */
+/* $Id: email.cpp,v 1.65 2017/11/24 21:53:39 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -126,10 +126,9 @@ bool sbbs_t::email(int usernumber, const char *top, const char *subj, long mode)
 		return(false); 
 	}
 
-	if(mode&WM_FILE && !SYSOP && !(cfg.sys_misc&SM_FILE_EM)) {
-		bputs(text[EmailFilesNotAllowed]);
+	if(mode&WM_FILE && !SYSOP && !(cfg.sys_misc&SM_FILE_EM))
 		mode&=~WM_FILE;
-	}
+
 
 	if(mode&WM_FILE) {
 		if(!checkfname(title)) {
@@ -151,7 +150,7 @@ bool sbbs_t::email(int usernumber, const char *top, const char *subj, long mode)
 		for(x=0;x<cfg.total_prots;x++)
 			if(cfg.prot[x]->ulcmd[0] && chk_ar(cfg.prot[x]->ar,&useron,&client)) {
 				sprintf(tmp,"%c",cfg.prot[x]->mnemonic);
-				SAFECAT(str,tmp); 
+				strcat(str,tmp); 
 			}
 		ch=(char)getkeys(str,0);
 		if(ch==text[YNQP][2] || sys_status&SS_ABORT) {
