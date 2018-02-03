aa527e6da8576b4dfe4a735bc39f35da041aca23
Stephen Hurd <shurd@sasktel.net>
2018-02-03 20:35:53 +0000

    Fix vmem leak.

--- conio/bitmap_con.c
+++ conio/bitmap_con.c
@@ -1,4 +1,4 @@
-/* $Id: bitmap_con.c,v 1.77 2018/02/03 13:06:46 deuce Exp $ */
+/* $Id: bitmap_con.c,v 1.78 2018/02/03 20:35:53 deuce Exp $ */
 
 #include <stdarg.h>
 #include <stdio.h>		/* NULL */
@@ -1147,6 +1147,7 @@ static int update_rect(int sx, int sy, int width, int height, int force)
 	memcpy(cvstat.vmem->fgvmem, vmem_ptr->fgvmem, vstat.cols*vstat.rows*sizeof(vmem_ptr->fgvmem[0]));
 	memcpy(cvstat.vmem->bgvmem, vmem_ptr->bgvmem, vstat.cols*vstat.rows*sizeof(vmem_ptr->bgvmem[0]));
 	pthread_mutex_unlock(&vmem_lock);
+	release_vmem(vmem_ptr);
 
 	if (hold_update)
 		redraw_cursor = 0;
