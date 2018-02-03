90cbd0afd57ee95eda6a8e636881dedead7be3eb
deuce <deuce>
2018-02-03 09:15:02 +0000

    Switch to using safe_snprintf() where the return value is used.
    With the supported compilers, we can't rely on the return value of snprintf()
    and historically have replaced it with safe_snprintf().  For macOS however,
    snprintf() is apparently a macro, not a function, so gendefs.h throws and
    error due to redefinition.
    
    Rather than hack around on that, just ensure that we never use the return
    value of snprintf() and use safe_snprintf() to ensure we know what the
    return value will be.

--- sbbs3/objects.mk
+++ sbbs3/objects.mk
@@ -2,7 +2,7 @@
 
 # Make 'include file' listing object files for SBBS.DLL
 
-# $Id: objects.mk,v 1.63 2015/11/24 11:05:07 rswindell Exp $(DIRSEP)09$(DIRSEP)08 07:23:54 deuce Exp $
+# $Id: objects.mk,v 1.65 2017/11/26 00:09:56 rswindell Exp $(DIRSEP)09$(DIRSEP)08 07:23:54 deuce Exp $
 
 # OBJODIR, SLASH, and OFILE must be pre-defined
 
@@ -11,7 +11,6 @@ OBJS	=	$(MTOBJODIR)$(DIRSEP)ansiterm$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)ars$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)atcodes$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)bat_xfer$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)base64$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)bulkmail$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)chat$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)chk_ar$(OFILE)\
@@ -130,12 +129,10 @@ MAIL_OBJS	= $(MTOBJODIR)$(DIRSEP)mailsrvr$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)mxlookup$(OFILE) \
  		  	$(MTOBJODIR)$(DIRSEP)mime$(OFILE) \
  		  	$(MTOBJODIR)$(DIRSEP)nopen$(OFILE) \
- 		  	$(MTOBJODIR)$(DIRSEP)ars$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)base64$(OFILE)
+ 		  	$(MTOBJODIR)$(DIRSEP)ars$(OFILE)
 
 # Must add new additions to MONO_OBJS too!
 WEB_OBJS	= $(MTOBJODIR)$(DIRSEP)websrvr$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)base64$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)ars$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)ringbuf$(OFILE)
 			
@@ -182,6 +179,7 @@ SBBSECHO_OBJS = \
 			$(OBJODIR)$(DIRSEP)load_cfg$(OFILE) \
 			$(OBJODIR)$(DIRSEP)scfglib1$(OFILE) \
 			$(OBJODIR)$(DIRSEP)scfglib2$(OFILE) \
+			$(OBJODIR)$(DIRSEP)scfgsave$(OFILE) \
 			$(OBJODIR)$(DIRSEP)nopen$(OFILE) \
 			$(OBJODIR)$(DIRSEP)str_util$(OFILE) \
 			$(OBJODIR)$(DIRSEP)dat_rec$(OFILE) \
@@ -242,7 +240,6 @@ JSEXEC_OBJS = \
 
 JSDOOR_OBJS = \
 			$(MTOBJODIR)$(DIRSEP)ars$(OFILE) \
-			$(MTOBJODIR)$(DIRSEP)base64$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)date_str$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)dat_rec$(OFILE) \
 			$(MTOBJODIR)$(DIRSEP)jsdoor$(OFILE) \
--- xpdev/genwrap.h
+++ xpdev/genwrap.h
@@ -1,8 +1,7 @@
-/* genwrap.h */
-
 /* General cross-platform development wrappers */
 
-/* $Id: genwrap.h,v 1.108 2016/05/26 08:36:14 rswindell Exp $ */
+/* $Id: genwrap.h,v 1.110 2017/11/05 04:20:05 rswindell Exp $ */
+// vi: tabstop=4
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -371,6 +370,7 @@ DLLEXPORT char		DLLCALL c_unescape_char(char ch);
 DLLEXPORT int64_t	DLLCALL	parse_byte_count(const char*, ulong unit);
 DLLEXPORT double	DLLCALL parse_duration(const char*);
 DLLEXPORT char*		DLLCALL duration_to_str(double value, char* str, size_t size);
+DLLEXPORT char*		DLLCALL duration_to_vstr(double value, char* str, size_t size);
 DLLEXPORT char*		DLLCALL byte_count_to_str(int64_t bytes, char* str, size_t size);
 
 /* Microsoft (e.g. DOS/Win32) real-time system clock API (ticks since process started) */
