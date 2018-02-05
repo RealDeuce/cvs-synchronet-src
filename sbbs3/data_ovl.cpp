fac66d8dff3dae7c98352422c87314cae88cecf9
deuce <deuce>
2018-02-05 09:37:39 +0000

    Fix up setpixels() some more.

--- sbbs3/data_ovl.cpp
+++ sbbs3/data_ovl.cpp
@@ -1,6 +1,6 @@
 /* Synchronet hi-level data access routines */
 
-/* $Id: data_ovl.cpp,v 1.26 2018/07/24 23:55:29 rswindell Exp $ */
+/* $Id: data_ovl.cpp,v 1.22 2016/12/10 21:29:04 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -50,18 +50,13 @@ void sbbs_t::getmsgptrs()
 {
 	if(!useron.number)
 		return;
-	msclock_t start = msclock();
-	::getmsgptrs(&cfg,&useron,subscan,online == ON_REMOTE ? ProgressLoadingMsgPtrs : NULL,this);
-	lprintf(LOG_DEBUG, "Node %d %s read %u message scan cfg/pointers in %lu ms"
-		, cfg.node_num, useron.alias, cfg.total_subs, msclock() - start);
-	if(online == ON_REMOTE)
-		bputs(text[LoadedMsgPtrs]);
+	::getmsgptrs(&cfg,&useron,subscan,ProgressLoadingMsgPtrs,this);
+	bputs(text[LoadedMsgPtrs]);
 }
 
 void sbbs_t::putmsgptrs()
 {
-	if(!::putmsgptrs(&cfg,&useron,subscan))
-		errormsg(WHERE, ERR_WRITE, "message pointers", 0);
+	::putmsgptrs(&cfg,&useron,subscan);
 }
 
 static void ProgressSearchingUsers(void* cbdata, int count, int total)
@@ -81,8 +76,7 @@ static void ProgressSearchingUsers(void* cbdata, int count, int total)
 uint sbbs_t::userdatdupe(uint usernumber, uint offset, uint datlen, char *dat
     ,bool del, bool next)
 {
-	uint i=::userdatdupe(&cfg, usernumber, offset, datlen, dat, del, next, online == ON_REMOTE ? ProgressSearchingUsers : NULL, this);
-	if(online == ON_REMOTE)
-		bputs(text[SearchedForDupes]);
+	uint i=::userdatdupe(&cfg, usernumber, offset, datlen, dat, del, next, ProgressSearchingUsers, this);
+	bputs(text[SearchedForDupes]);
 	return(i);
 }
