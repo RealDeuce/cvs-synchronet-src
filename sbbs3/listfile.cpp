60e5a4b99325418fe1651dc1d9524f8ac0fbfea3
deuce <deuce>
2018-02-09 23:20:54 +0000

    cio_api.mode isn't the video mode.  Derp.

--- sbbs3/listfile.cpp
+++ sbbs3/listfile.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet file database listing functions */
 
-/* $Id: listfile.cpp,v 1.64 2018/08/03 06:18:56 rswindell Exp $ */
+/* $Id: listfile.cpp,v 1.60 2018/01/12 22:15:42 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -294,7 +294,7 @@ int sbbs_t::listfiles(uint dirnum, const char *filespec, int tofile, long mode)
 				if(tofile) {
 					write(tofile,crlf,2);
 					sprintf(hdr,"%*s",c,nulstr);
-					memset(hdr,0xC4,c);
+					memset(hdr,'Ä',c);
 					strcat(hdr,crlf);
 					write(tofile,hdr,strlen(hdr)); 
 				}
@@ -302,7 +302,7 @@ int sbbs_t::listfiles(uint dirnum, const char *filespec, int tofile, long mode)
 					CRLF;
 					attr(cfg.color[clr_filelstline]);
 					while(c--)
-						outchar('\xC4');
+						outchar('Ä');
 					CRLF; 
 				} 
 			} 
@@ -530,7 +530,7 @@ bool sbbs_t::listfile(const char *fname, const char *buf, uint dirnum
 			attr(cfg.color[clr_filedesc]^HIGH);
 			bprintf("%.*s",i,str+j);
 			attr(cfg.color[clr_filedesc]);
-			bprintf("%.*s",(int)(strlen(str)-(j+i)),str+j+i); 
+			bprintf("%.*s",strlen(str)-(j+i),str+j+i); 
 		}
 		else
 			bputs(str);
@@ -614,7 +614,8 @@ bool sbbs_t::removefile(file_t* f)
 	char str[256];
 
 	if(removefiledat(&cfg,f)) {
-		SAFEPRINTF3(str,"removed %s from %s %s"
+		SAFEPRINTF4(str,"%s removed %s from %s %s"
+			,useron.alias
 			,f->name
 			,cfg.lib[cfg.dir[f->dir]->lib]->sname,cfg.dir[f->dir]->sname);
 		logline("U-",str);
@@ -646,7 +647,8 @@ bool sbbs_t::movefile(file_t* f, int newdir)
 	addfiledat(&cfg,f);
 	bprintf(text[MovedFile],f->name
 		,cfg.lib[cfg.dir[f->dir]->lib]->sname,cfg.dir[f->dir]->sname);
-	sprintf(str,"moved %s to %s %s",f->name
+	sprintf(str,"%s moved %s to %s %s",f->name
+		,useron.alias
 		,cfg.lib[cfg.dir[f->dir]->lib]->sname,cfg.dir[f->dir]->sname);
 	logline(nulstr,str);
 	if(!f->altpath) {	/* move actual file */
@@ -714,9 +716,10 @@ int sbbs_t::batchflagprompt(uint dirnum, file_t* bf, uint total
 				f.datoffset=bf[0].datoffset;
 				f.size=0;
 				getfiledat(&cfg,&f);
-				addtobatdl(&f);
 				if(ch=='D')
-					start_batch_download();
+					downloadfile(&f);
+				else
+					addtobatdl(&f);
 				CRLF;
 				return(2); 
 			}
@@ -1006,13 +1009,11 @@ int sbbs_t::listfileinfo(uint dirnum, char *filespec, long mode)
 		return(0);
 	l=(long)filelength(file);
 	if(!l) {
-		FREE_AND_NULL(usrxfrbuf);
 		close(file);
 		return(0); 
 	}
 	if((ixbbuf=(uchar *)malloc(l))==NULL) {
 		close(file);
-		FREE_AND_NULL(usrxfrbuf);
 		errormsg(WHERE,ERR_ALLOC,str,l);
 		return(0); 
 	}
@@ -1212,7 +1213,8 @@ int sbbs_t::listfileinfo(uint dirnum, char *filespec, long mode)
 							if(remove(str))
 								bprintf(text[CouldntRemoveFile],str);
 							else {
-								sprintf(tmp,"deleted %s"
+								sprintf(tmp,"%s deleted %s"
+									,useron.alias
 									,str);
 								logline(nulstr,tmp); 
 							} 
@@ -1230,7 +1232,8 @@ int sbbs_t::listfileinfo(uint dirnum, char *filespec, long mode)
 								if(remove(str))
 									bprintf(text[CouldntRemoveFile],str);
 								else {
-									sprintf(tmp,"deleted %s"
+									sprintf(tmp,"%s deleted %s"
+										,useron.alias
 										,str);
 									logline(nulstr,tmp); 
 								} 
