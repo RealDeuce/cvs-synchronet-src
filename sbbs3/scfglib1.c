521c0aaf861ac91685a67a18242bdcb06c40430d
deuce <deuce>
2018-02-09 06:16:45 +0000

    Heh, index isn't a uint16_t

--- sbbs3/scfglib1.c
+++ sbbs3/scfglib1.c
@@ -1,6 +1,6 @@
 /* Synchronet configuration library routines */
 
-/* $Id: scfglib1.c,v 1.69 2016/11/15 21:38:12 rswindell Exp $ */
+/* $Id: scfglib1.c,v 1.75 2018/01/07 23:11:03 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -119,9 +119,7 @@ BOOL read_node_cfg(scfg_t* cfg, char* error)
 	if(!cfg->node_sem_check) cfg->node_sem_check=60;
 	get_int(cfg->node_stat_check,instream);
 	if(!cfg->node_stat_check) cfg->node_stat_check=10;
-	get_str(cfg->scfg_cmd,instream);
-	if(!cfg->scfg_cmd[0])
-		strcpy(cfg->scfg_cmd,"%!scfg %k");
+	get_str(cfg->scfg_cmd,instream);	// unused
 	get_int(cfg->sec_warn,instream);
 	if(!cfg->sec_warn)
 		cfg->sec_warn=180;
@@ -441,7 +439,8 @@ BOOL read_msgs_cfg(scfg_t* cfg, char* error)
 	if(!cfg->smb_retry_time)
 		cfg->smb_retry_time=30;
 	get_int(cfg->max_qwkmsgage, instream);
-	for(i=0;i<233;i++)	/* NULL */
+	get_int(cfg->max_spamage, instream);
+	for(i=0;i<232;i++)	/* NULL */
 		get_int(n,instream);
 	get_int(cfg->msg_misc,instream);
 	for(i=0;i<255;i++)	/* 0xff */
@@ -475,9 +474,10 @@ BOOL read_msgs_cfg(scfg_t* cfg, char* error)
 		get_str(cfg->grp[i]->code_prefix,instream);
 
 		get_int(c,instream);
+		cfg->grp[i]->sort = c;
 		for(j=0;j<43;j++)
 			get_int(n,instream);
-		}
+	}
 	cfg->total_grps=i;
 
 	/**********************/
@@ -497,6 +497,8 @@ BOOL read_msgs_cfg(scfg_t* cfg, char* error)
 		if((cfg->sub[i]=(sub_t *)malloc(sizeof(sub_t)))==NULL)
 			return allocerr(instream,error,offset,fname,sizeof(sub_t));
 		memset(cfg->sub[i],0,sizeof(sub_t));
+
+		cfg->sub[i]->subnum = i;
 
 		get_int(cfg->sub[i]->grp,instream);
 		get_str(cfg->sub[i]->lname,instream);
@@ -527,6 +529,8 @@ BOOL read_msgs_cfg(scfg_t* cfg, char* error)
 		cfg->sub[i]->op_ar=ARSTR(cfg->sub[i]->op_arstr,cfg);
 
 		get_int(cfg->sub[i]->misc,instream);
+		if((cfg->sub[i]->misc&(SUB_FIDO|SUB_INET)) && !(cfg->sub[i]->misc&SUB_QNET))
+			cfg->sub[i]->misc|=SUB_NOVOTING;
 
 		get_str(cfg->sub[i]->tagline,instream);
 		get_str(cfg->sub[i]->origline,instream);
@@ -623,7 +627,7 @@ BOOL read_msgs_cfg(scfg_t* cfg, char* error)
 		get_int(k,instream);
 
 		if(k) {
-			if((cfg->qhub[i]->sub=(ulong *)malloc(sizeof(ulong)*k))==NULL)
+			if((cfg->qhub[i]->sub=(sub_t**)malloc(sizeof(sub_t*)*k))==NULL)
 				return allocerr(instream,error,offset,fname,sizeof(ulong)*k);
 			if((cfg->qhub[i]->conf=(ushort *)malloc(sizeof(ushort)*k))==NULL)
 				return allocerr(instream,error,offset,fname,sizeof(ushort)*k);
@@ -632,19 +636,23 @@ BOOL read_msgs_cfg(scfg_t* cfg, char* error)
 		}
 
 		for(j=0;j<k;j++) {
+			uint16_t	confnum;
 			uint16_t	subnum;
+			uint8_t		mode;
 			if(feof(instream)) break;
-			get_int(cfg->qhub[i]->conf[cfg->qhub[i]->subs],instream);
-			get_int(subnum,instream);
-			cfg->qhub[i]->sub[cfg->qhub[i]->subs]=subnum;
-			get_int(cfg->qhub[i]->mode[cfg->qhub[i]->subs],instream);
-			if(cfg->qhub[i]->sub[cfg->qhub[i]->subs]<cfg->total_subs)
-				cfg->sub[cfg->qhub[i]->sub[cfg->qhub[i]->subs]]->misc|=SUB_QNET;
-			else
-				continue;
-			if(cfg->qhub[i]->sub[cfg->qhub[i]->subs]!=INVALID_SUB)
-				cfg->qhub[i]->subs++; }
-		for(j=0;j<32;j++)
+			get_int(confnum,instream);
+			get_int(subnum, instream);
+			get_int(mode, instream);
+			if(subnum < cfg->total_subs) {
+				cfg->sub[subnum]->misc |= SUB_QNET;
+				cfg->qhub[i]->sub[cfg->qhub[i]->subs]	= cfg->sub[subnum];
+				cfg->qhub[i]->mode[cfg->qhub[i]->subs]	= mode;
+				cfg->qhub[i]->conf[cfg->qhub[i]->subs]	= confnum;
+				cfg->qhub[i]->subs++;
+			}
+		}
+		get_int(cfg->qhub[i]->misc, instream);
+		for(j=0;j<30;j++)
 			get_int(n,instream);
 	}
 
@@ -845,45 +853,3 @@ void make_data_dirs(scfg_t* cfg)
 	}
 #endif
 }
-
-int smb_storage_mode(scfg_t* cfg, smb_t* smb)
-{
-	if(smb->subnum == INVALID_SUB)
-		return (cfg->sys_misc&SM_FASTMAIL) ? SMB_FASTALLOC : SMB_SELFPACK;
-	if(smb->subnum >= cfg->total_subs)
-		return -1;
-	if(cfg->sub[smb->subnum]->misc&SUB_HYPER) {
-		smb->status.attr |= SMB_HYPERALLOC;
-		return SMB_HYPERALLOC;
-	}
-	if(cfg->sub[smb->subnum]->misc&SUB_FAST)
-		return SMB_FASTALLOC;
-	return SMB_SELFPACK;
-}
-
-int smb_open_sub(scfg_t* cfg, smb_t* smb, unsigned int subnum)
-{
-	int retval;
-
-	if(subnum != INVALID_SUB && subnum >= cfg->total_subs)
-		return SMB_FAILURE;
-	memset(smb, 0, sizeof(smb_t));
-	if(subnum == INVALID_SUB) {
-		SAFEPRINTF(smb->file, "%smail", cfg->data_dir);
-		smb->status.max_crcs	= cfg->mail_maxcrcs;
-		smb->status.max_msgs	= 0;
-		smb->status.max_age		= cfg->mail_maxage;
-		smb->status.attr		= SMB_EMAIL;
-	} else {
-		SAFEPRINTF2(smb->file, "%s%s", cfg->sub[subnum]->data_dir, cfg->sub[subnum]->code);
-		smb->status.max_crcs	= cfg->sub[subnum]->maxcrcs;
-		smb->status.max_msgs	= cfg->sub[subnum]->maxmsgs;
-		smb->status.max_age		= cfg->sub[subnum]->maxage;
-		smb->status.attr		= cfg->sub[subnum]->misc&SUB_HYPER ? SMB_HYPERALLOC :0;
-	}
-	smb->retry_time = cfg->smb_retry_time;
-	if((retval = smb_open(smb)) == SMB_SUCCESS)
-		smb->subnum = subnum;
-	return retval;
-}
-
