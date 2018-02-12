56eca92232432e27dc82a0f1a505a50be41df5f2
deuce <deuce>
2018-02-12 09:06:52 +0000

    Limit string sizes to 512MB  This puts a cap of on the max asset size of 384MB

--- conio/cterm.c
+++ conio/cterm.c
@@ -1,4 +1,4 @@
-/* $Id: cterm.c,v 1.219 2018/02/12 07:51:58 deuce Exp $ */
+/* $Id: cterm.c,v 1.220 2018/02/12 09:06:52 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -2801,7 +2801,7 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 
 struct cterminal* CIOLIBCALL cterm_init(int height, int width, int xpos, int ypos, int backlines, unsigned char *scrollback, uint32_t *scrollbackf, uint32_t *scrollbackb, int emulation)
 {
-	char	*revision="$Revision: 1.219 $";
+	char	*revision="$Revision: 1.220 $";
 	char *in;
 	char	*out;
 	int		i;
@@ -3218,14 +3218,20 @@ CIOLIBEXPORT char* CIOLIBCALL cterm_write(struct cterminal * cterm, const void *
 									if (cterm->strbuflen == cterm->strbufsize) {
 										char *p;
 
-										cterm->strbufsize += 1024;
-										p = realloc(cterm->strbuf, cterm->strbufsize);
-										if (p == NULL) {
+										cterm->strbufsize *= 2;
+										if (cterm->strbufsize > 1024 * 1024 * 512) {
 											FREE_AND_NULL(cterm->strbuf);
 											cterm->strbuflen = cterm->strbufsize = 0;
 										}
-										else
-											cterm->strbuf = p;
+										else {
+											p = realloc(cterm->strbuf, cterm->strbufsize);
+											if (p == NULL) {
+												FREE_AND_NULL(cterm->strbuf);
+												cterm->strbuflen = cterm->strbufsize = 0;
+											}
+											else
+												cterm->strbuf = p;
+										}
 									}
 								}
 							}
@@ -3254,11 +3260,17 @@ CIOLIBEXPORT char* CIOLIBCALL cterm_write(struct cterminal * cterm, const void *
 										char *p;
 
 										cterm->strbufsize *= 2;
-										p = realloc(cterm->strbuf, cterm->strbufsize);
-										if (p == NULL) {
-											cterm->string = 0;
+										if (cterm->strbufsize > 1024 * 1024 * 512) {
 											FREE_AND_NULL(cterm->strbuf);
 											cterm->strbuflen = cterm->strbufsize = 0;
+										}
+										else {
+											p = realloc(cterm->strbuf, cterm->strbufsize);
+											if (p == NULL) {
+												cterm->string = 0;
+												FREE_AND_NULL(cterm->strbuf);
+												cterm->strbuflen = cterm->strbufsize = 0;
+											}
 										}
 									}
 								}
