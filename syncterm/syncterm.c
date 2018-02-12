8816daa6eb06d44c2a840fe541a25d089a147e72
deuce <deuce>
2018-02-12 08:59:47 +0000

    Track the highest palette entry, and ensure no pixels are set above it. Also, clamp the palette size at one million entries since we can't fit a million pixels on the screen at one time anyway.

--- syncterm/syncterm.c
+++ syncterm/syncterm.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: syncterm.c,v 1.216 2018/02/28 07:28:52 deuce Exp $ */
+/* $Id: syncterm.c,v 1.210 2018/02/10 09:07:33 deuce Exp $ */
 
 #if defined(__APPLE__) && defined(__MACH__)
 #include <CoreServices/CoreServices.h>	// FSFindFolder() and friends
@@ -77,7 +77,6 @@ char	*usage =
 		"-6  =  Only resolve IPv6 addresses\n"
 		"-s  =  enable \"Safe Mode\" which prevents writing/browsing local files\n"
 		"-T  =  when the ONLY argument, dumps the terminfo entry to stdout and exits\n"
-		"-v  =  when the ONLY argument, dumps the version info to stdout and exits\n"
 		"\n"
 		"URL format is: [(rlogin|telnet|ssh|raw)://][user[:password]@]domainname[:port]\n"
 		"raw:// URLs MUST include a port.\n"
@@ -94,7 +93,9 @@ char *inpath=NULL;
 int default_font=0;
 struct syncterm_settings settings;
 char *font_names[sizeof(conio_fontdata)/sizeof(struct conio_font_data_struct)];
-struct vmem_cell *scrollback_buf=NULL;
+unsigned char *scrollback_buf=NULL;
+uint32_t *scrollback_fbuf=NULL;
+uint32_t *scrollback_bbuf=NULL;
 unsigned int  scrollback_lines=0;
 unsigned int  scrollback_mode=C80;
 unsigned int  scrollback_cols=80;
@@ -1105,49 +1106,44 @@ char *get_syncterm_filename(char *fn, int fnlen, int type, int shared)
 	}
 #else
 	/* UNIX */
-	char	*home=getenv("HOME");
+	char	*home=NULL;
 
-	home=getenv("HOME");
-	if (!shared) {
-		if((home==NULL || strlen(home) > MAX_PATH-32)) {	/* $HOME just too damn big */
-			if(type==SYNCTERM_DEFAULT_TRANSFER_PATH || type==SYNCTERM_PATH_CACHE) {
-				getcwd(fn, fnlen);
-				backslash(fn);
-				if(type==SYNCTERM_PATH_CACHE) {
-					strcat(fn,"cache");
-					backslash(fn);
-				}
-				return(fn);
-			}
-			SAFECOPY(oldlst,"syncterm.lst");
-			strcpy(fn,"./");
-		}
-		else {
-			if(type==SYNCTERM_DEFAULT_TRANSFER_PATH || type==SYNCTERM_PATH_CACHE) {
-				strcpy(fn, home);
-				backslash(fn);
-#if defined(__APPLE__) && defined(__MACH__)
-				if(get_new_OSX_filename(oldlst, sizeof(oldlst), type, shared)!=NULL)
-					strcpy(fn, oldlst);
-#endif
-				if(!isdir(fn))
-					if(MKDIR(fn))
-						fn[0]=0;
-				if(type==SYNCTERM_PATH_CACHE) {
-					strcat(fn,"cache");
-					backslash(fn);
-				}
-				return(fn);
-			}
-			SAFECOPY(oldlst,home);
-			backslash(oldlst);
-			strcat(oldlst,"syncterm.lst");
-			sprintf(fn,"%.*s",fnlen,home);
-			strncat(fn, "/.syncterm", fnlen-strlen(fn)-1);
+	if(inpath==NULL)
+		home=getenv("HOME");
+	if(!shared && (home==NULL || strlen(home) > MAX_PATH-32)) {	/* $HOME just too damn big */
+		if(type==SYNCTERM_DEFAULT_TRANSFER_PATH || type==SYNCTERM_PATH_CACHE) {
+			getcwd(fn, fnlen);
 			backslash(fn);
+			if(type==SYNCTERM_PATH_CACHE) {
+				strcat(fn,"cache");
+				backslash(fn);
+			}
+			return(fn);
 		}
+		SAFECOPY(oldlst,"syncterm.lst");
+		strcpy(fn,"./");
 	}
 	else {
+		if(type==SYNCTERM_DEFAULT_TRANSFER_PATH) {
+			strcpy(fn, home);
+			backslash(fn);
+#if defined(__APPLE__) && defined(__MACH__)
+			if(get_new_OSX_filename(oldlst, sizeof(oldlst), type, shared)!=NULL)
+				strcpy(fn, oldlst);
+#endif
+			if(!isdir(fn))
+				MKDIR(fn);
+			return(fn);
+		}
+		SAFECOPY(oldlst,home);
+		backslash(oldlst);
+		strcat(oldlst,"syncterm.lst");
+		sprintf(fn,"%.*s",fnlen,home);
+		strncat(fn, "/.syncterm", fnlen-strlen(fn)-1);
+		backslash(fn);
+	}
+
+	if(shared) {
 #ifdef SYSTEM_LIST_DIR
 		strcpy(fn,SYSTEM_LIST_DIR);
 		backslash(fn);
@@ -1254,7 +1250,6 @@ void load_settings(struct syncterm_settings *set)
 int main(int argc, char **argv)
 {
 	struct bbslist *bbs=NULL;
-	BOOL bbs_alloc=FALSE;
 	struct	text_info txtinfo;
 	char	str[MAX_PATH+1];
 	char	drive[MAX_PATH+1];
@@ -1282,8 +1277,8 @@ int main(int argc, char **argv)
 				"	cbt=\\E[Z,bel=^G,cr=^M,csr=\\E[%i%p1%d;%p2%dr,clear=\\E[2J,el1=\\E[1K,el=\\E[K,ed=\\E[J,\n"
 				"	hpa=\\E[%i%p1%dG,cup=\\E[%i%p1%d;%p2%dH,cud1=^J,home=\\E[H,civis=\\E[?25l,cub1=\\E[D,cnorm=\\E[?25h,\n"
 				"	cuf1=\\E[C,ll=\\E[255H,cuu1=\\E[A,dch1=\\E[P,dl1=\\E[M,smam=\\E[?7h,blink=\\E[5m,bold=\\E[1m,\n"
-				"	ech=\\E[%p1%dX,rmam=\\E[7l,sgr0=\\E[m,\n"
-				"	is1=\\E[?7h\\E[?25h\\E[?31l\\E[?32l\\E[?33l\\E[*r\\E[ D\\E[m\\E[?s,\n"
+				"	ech=\\E[%p1%dX,rmam=\\E[7l,sgr0=\\E[0m,\n"
+				"	is1=\\E[?7h\\E[?25h\\E[?31l\\E[?32l\\E[?33l\\E[*r\\E[ D\\E[0m\\E[?s,\n"
 				"	ich1=\\E[@,il1=\\E[L,\n"
 				"	kbs=^H,kdch1=\\177,kcud1=\\E[B,kend=\\E[K,\n"
 				"	kf1=\\EOP,kf2=\\EOQ,kf3=\\EOR,kf4=\\EOS,kf5=\\EOt,kf6=\\E[17~,\n"
@@ -1293,11 +1288,11 @@ int main(int argc, char **argv)
 				"	nel=^M^J,\n"
 				"	dch=\\E[%p1%dP,dl=\\E[%p1%dM,cud=\\E[%p1%dB,ich=\\E[%p1%d@,indn=\\E[%p1%dS,\n"
 				"	il=\\E[%p1%dL,cub=\\E[%p1%dD,cuf=\\E[%p1%dC,rin=\\E[%p1%dT,cuu=\\E[%p1%dA,\n"
-				"	rs1=\\E[?7h\\E[?25h\\E[?31l\\E[?32l\\E[?33l\\E[*r\\E[ D\\E[m\\E[?s,\n"
+				"	rs1=\\E[?7h\\E[?25h\\E[?31l\\E[?32l\\E[?33l\\E[*r\\E[ D\\E[0m\\E[?s,\n"
 				"	rc=\\E[u,sc=\\E[s,ind=\\E[S,ri=\\E[T,\n"
 				"	ht=\t,setab=\\E[4%p1%dm,setaf=\\E[3%p1%dm,\n"
 				"	sgr=\\E[0%?%p1%p6%|%t;1%;%?%p4%|%t;5%;%?%p1%p3%|%t;7%;%?%p7%|%t;8%;m,\n"
-				"	smso=\\E[0;1;7m,rmso=\\E[m,\n"
+				"	smso=\\E[0;1;7m,\n"
 				"syncterm-24|SyncTERM 80x25,\n"
 				"	lines#24,use=syncterm,\n"
 				"syncterm-25|SyncTERM No Status Line,\n"
@@ -1352,11 +1347,6 @@ int main(int argc, char **argv)
 		write(STDOUT_FILENO, syncterm_termcap, strlen(syncterm_termcap));
 		return 0;
 	}
-	if(argc==2 && strcmp(argv[1],"-v")==0) {
-		fprintf(stdout, "%s\n", syncterm_version);
-		return 0;
-	}
-
 
 #if !defined(WITHOUT_CRYPTLIB)
 	/* Cryptlib initialization MUST be done before ciolib init */
@@ -1406,7 +1396,6 @@ int main(int argc, char **argv)
 						case 0:
 							printf("NOTICE: The -i option is depreciated, use -if instead\r\n");
 							SLEEP(2000);
-							/* Fall-through */
 						case 'F':
 							ciolib_mode=CIOLIB_MODE_CURSES_IBM;
 							break;
@@ -1531,7 +1520,6 @@ int main(int argc, char **argv)
 			uifcmsg("Unable to allocate memory","The system was unable to allocate memory.");
 			return(1);
 		}
-		bbs_alloc=TRUE;
 		memset(bbs, 0, sizeof(struct bbslist));
 		if((listfile=fopen(settings.list_path,"r"))==NULL)
 			parse_url(url, bbs, conn_type, TRUE);
@@ -1641,24 +1629,12 @@ int main(int argc, char **argv)
 					}
 				}
 			}
-			if (bbs_alloc) {
-				bbs_alloc=FALSE;
-				free(bbs);
-			}
 			bbs=NULL;
 			break;
 		}
 		else
 			last_bbs=strdup(bbs->name);
-		if (bbs_alloc) {
-			bbs_alloc=FALSE;
-			free(bbs);
-		}
 		bbs=NULL;
-	}
-	if (bbs_alloc) {
-		bbs_alloc=FALSE;
-		free(bbs);
 	}
 	if (last_bbs)
 		free(last_bbs);
@@ -1690,10 +1666,6 @@ int main(int argc, char **argv)
 	return(0);
 
 	USAGE:
-	if (bbs_alloc) {
-		bbs_alloc=FALSE;
-		free(bbs);
-	}
 	uifcbail();
 	clrscr();
     gettextinfo(&txtinfo);
