e77867ef194c7d7fe0a6984144971678b8fda0f7
deuce <deuce>
2018-02-13 19:37:52 +0000

    Use integer math for RGB -> YUV conversion.

--- sbbs3/ftpsrvr.c
+++ sbbs3/ftpsrvr.c
@@ -1,6 +1,6 @@
 /* Synchronet FTP server */
 
-/* $Id: ftpsrvr.c,v 1.434 2017/10/11 03:00:19 rswindell Exp $ */
+/* $Id: ftpsrvr.c,v 1.435 2017/10/12 08:55:00 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -125,7 +125,7 @@ BOOL direxist(char *dir)
 BOOL dir_op(scfg_t* cfg, user_t* user, client_t* client, uint dirnum)
 {
 	return(user->level>=SYSOP_LEVEL
-		|| (cfg->dir[dirnum]->op_ar[0] && chk_ar(cfg,cfg->dir[dirnum]->op_ar,user,client)));
+		|| (cfg->dir[dirnum]->op_ar!=NULL && cfg->dir[dirnum]->op_ar[0] && chk_ar(cfg,cfg->dir[dirnum]->op_ar,user,client)));
 }
 
 static int lprintf(int level, const char *fmt, ...)
@@ -1193,8 +1193,10 @@ int ftp_remove(SOCKET sock, int line, const char* fname)
 {
 	int ret=0;
 
-	if(fexist(fname) && (ret=remove(fname))!=0)
-		lprintf(LOG_ERR,"%04d !ERROR %d (line %d) removing file: %s", sock, ret, line, fname);
+	if(fexist(fname) && (ret=remove(fname))!=0) {
+		if(fexist(fname))	// In case there was a race condition (other host deleted file first)
+			lprintf(LOG_ERR,"%04d !ERROR %d (%s) (line %d) removing file: %s", sock, errno, STRERROR(errno), line, fname);
+	}
 	return ret;
 }
 
@@ -4741,7 +4743,7 @@ const char* DLLCALL ftp_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.434 $", "%*s %s", revision);
+	sscanf("$Revision: 1.435 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
