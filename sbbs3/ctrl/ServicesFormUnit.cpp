76d838d65bc0f0bb26244a9d79152122c22c839c
deuce <deuce>
2018-02-14 22:55:46 +0000

    Try adding DNS and Country names to the certificate.

--- sbbs3/ctrl/ServicesFormUnit.cpp
+++ sbbs3/ctrl/ServicesFormUnit.cpp
@@ -1,12 +1,12 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: ServicesFormUnit.cpp,v 1.4 2009/01/24 22:23:49 rswindell Exp $ */
+/* $Id: ServicesFormUnit.cpp,v 1.5 2017/11/28 05:21:27 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -64,7 +64,6 @@ void __fastcall TServicesForm::LogLevelUpDownChangingEx(TObject *Sender,
     else {
         MainForm->services_startup.log_level = NewValue;
         LogLevelText->Caption = LogLevelDesc[NewValue];
-        MainForm->SaveIniSettings(Sender);
     }
 }
 //---------------------------------------------------------------------------
