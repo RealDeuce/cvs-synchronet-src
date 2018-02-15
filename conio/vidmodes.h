3fad1b46fc0cabcb175cc9eadaa10a37a3774fd2
deuce <deuce>
2018-02-15 20:23:20 +0000

    Rename curs_blink to curs_blinks.

--- conio/vidmodes.h
+++ conio/vidmodes.h
@@ -1,4 +1,4 @@
-/* $Id: vidmodes.h,v 1.20 2015/07/05 09:07:34 deuce Exp $ */
+/* $Id: vidmodes.h,v 1.27 2018/02/15 20:23:20 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -43,7 +43,7 @@
 
 #include "ciolib.h"
 
-#define TOTAL_DAC_SIZE	34
+#define TOTAL_DAC_SIZE	274
 
 /* Entry type for the DAC table. */
 struct dac_colors {
@@ -66,7 +66,7 @@ struct  video_params {
 
 struct vstat_vmem {
 	unsigned refcount;
-	unsigned short *vmem;
+	struct vmem_cell *vmem;
 };
 
 struct video_stats {
@@ -76,7 +76,7 @@ struct video_stats {
 	int curs_col;
 	int curs_start;
 	int curs_end;
-	int curs_blink;
+	int curs_blinks;
 	int curs_visible;
 	int default_curs_start;
 	int default_curs_end;
@@ -92,8 +92,9 @@ struct video_stats {
 	int currattr;
 	int scaling;
 	int	vmultiplier;
-	struct dac_colors dac_colors[256];
-	unsigned char palette[16];
+	uint32_t flags;
+#define VIDMODES_FLAG_PALETTE_VMEM	1
+	uint32_t palette[16];
 	struct vstat_vmem *vmem;
 };
 
@@ -105,9 +106,9 @@ enum {
 	,ATARI_PALETTE
 };
 
-extern struct video_params vparams[49];
+extern struct video_params vparams[52];
 #define NUMMODES      (sizeof(vparams) / sizeof(struct video_params))
-extern unsigned char palettes[5][16];
+extern uint32_t palettes[5][16];
 extern struct dac_colors dac_default[TOTAL_DAC_SIZE];
 extern char vga_font_bitmap[4096];
 extern char vga_font_bitmap14[3584];
