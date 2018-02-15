6b489f4ad9dab1ae936acfa297ac852cd399fb8e
deuce <deuce>
2018-02-15 20:28:49 +0000

    Cursor blink rate is 2x character blink rate.

--- conio/bitmap_con.c
+++ conio/bitmap_con.c
@@ -1,4 +1,4 @@
-/* $Id: bitmap_con.c,v 1.137 2018/04/18 06:33:39 deuce Exp $ */
+/* $Id: bitmap_con.c,v 1.131 2018/02/15 20:28:49 deuce Exp $ */
 
 #include <stdarg.h>
 #include <stdio.h>		/* NULL */
@@ -302,8 +302,6 @@ static void set_vmem_cell(struct vstat_vmem *vmem_ptr, size_t pos, uint16_t cell
 	if (!vstat.blink_altcharset)
 		altfont &= ~0x02;
 	font=current_font[altfont];
-	if (font == -99)
-		font = default_font;
 	if (font < 0 || font > 255)
 		font = 0;
 
@@ -467,10 +465,9 @@ static uint32_t color_value(uint32_t col)
 static struct rectlist *get_full_rectangle_locked(void)
 {
 	size_t i;
-	struct rectlist *rect;
+	struct rectlist *rect = alloc_full_rect();
 
 	if(callbacks.drawrect) {
-		rect = alloc_full_rect();
 		if (!rect)
 			return rect;
 		for (i=0; i<screen.screenwidth*screen.screenheight; i++)
@@ -507,7 +504,6 @@ static int bitmap_draw_one_char(unsigned int xpos, unsigned int ypos)
 	WORD	sch;
 	struct vstat_vmem *vmem_ptr;
 	BOOL	changed = FALSE;
-	BOOL	draw_fg = TRUE;
 
 	if(!bitmap_initialized) {
 		return(-1);
@@ -545,28 +541,26 @@ static int bitmap_draw_one_char(unsigned int xpos, unsigned int ypos)
 		case 16:
 			this_font = (unsigned char *)conio_fontdata[vmem_ptr->vmem[(ypos-1)*cio_textinfo.screenwidth+(xpos-1)].font].eight_by_sixteen;
 			break;
-		default:
-			pthread_mutex_unlock(&screen.screenlock);
-			return(-1);
 	}
 	if (this_font == NULL)
 		this_font = font[0];
 	fontoffset=(sch&0xff)*vstat.charheight;
 
-	draw_fg = ((!((sch & 0x8000) && !vstat.blink)) || vstat.no_blink);
 	for(y=0; y<vstat.charheight; y++) {
-		for(x=0; x<vstat.charwidth; x++) {
-			if(this_font[fontoffset] & (0x80 >> x) && draw_fg) {
-				if (screen.screen[PIXEL_OFFSET(screen, xoffset+x, yoffset+y)]!=fg) {
-					changed=TRUE;
-					screen.screen[PIXEL_OFFSET(screen, xoffset+x, yoffset+y)]=fg;
+		if ((!((sch & 0x8000) && !vstat.blink)) || vstat.no_blink) {
+			for(x=0; x<vstat.charwidth; x++) {
+				if(this_font[fontoffset] & (0x80 >> x)) {
+					if (screen.screen[PIXEL_OFFSET(screen, xoffset+x, yoffset+y)]!=fg) {
+						changed=TRUE;
+						screen.screen[PIXEL_OFFSET(screen, xoffset+x, yoffset+y)]=fg;
+					}
 				}
+				else
+					if (screen.screen[PIXEL_OFFSET(screen, xoffset+x, yoffset+y)]!=bg) {
+						changed=TRUE;
+						screen.screen[PIXEL_OFFSET(screen, xoffset+x, yoffset+y)]=bg;
+					}
 			}
-			else
-				if (screen.screen[PIXEL_OFFSET(screen, xoffset+x, yoffset+y)]!=bg) {
-					changed=TRUE;
-					screen.screen[PIXEL_OFFSET(screen, xoffset+x, yoffset+y)]=bg;
-				}
 		}
 		fontoffset++;
 	}
@@ -682,6 +676,7 @@ static int update_from_vmem(int force)
 	unsigned int pos;
 
 	int	redraw_cursor=0;
+	int	lastcharupdated=0;
 	int bright_attr_changed=0;
 	int blink_attr_changed=0;
 
@@ -755,6 +750,7 @@ static int update_from_vmem(int force)
 			}
 			pos++;
 		}
+		lastcharupdated=0;
 	}
 	release_vmem(vmem_ptr);
 
@@ -989,8 +985,6 @@ int bitmap_setfont(int font, int force, int font_num)
 			new=malloc(ti.screenwidth*ti.screenheight*sizeof(*new));
 			if(!new) {
 				free(old);
-				pthread_mutex_unlock(&vstatlock);
-				pthread_mutex_unlock(&blinker_lock);
 				return 0;
 			}
 			pold=old;
@@ -1461,7 +1455,6 @@ void bitmap_replace_font(uint8_t id, char *name, void *data, size_t size)
 	if (id < CONIO_FIRST_FREE_FONT) {
 		free(name);
 		free(data);
-		pthread_mutex_unlock(&blinker_lock);
 		return;
 	}
 
@@ -1610,13 +1603,11 @@ int bitmap_drv_init(void (*drawrect_cb) (struct rectlist *data)
 	pthread_mutex_lock(&vstatlock);
 	vstat.vmem=NULL;
 	vstat.flags = VIDMODES_FLAG_PALETTE_VMEM;
-	pthread_mutex_lock(&screen.screenlock);
 	for (i = 0; i < sizeof(dac_default)/sizeof(struct dac_colors); i++) {
 		palette[i].red = dac_default[i].red;
 		palette[i].green = dac_default[i].green;
 		palette[i].blue = dac_default[i].blue;
 	}
-	pthread_mutex_unlock(&screen.screenlock);
 	pthread_mutex_unlock(&vstatlock);
 
 	callbacks.drawrect=drawrect_cb;
--- sbbs3/xtrn.cpp
+++ sbbs3/xtrn.cpp
@@ -1,9 +1,8 @@
 /* xtrn.cpp */
-// vi: tabstop=4
 
 /* Synchronet external program support routines */
 
-/* $Id: xtrn.cpp,v 1.238 2018/08/03 06:18:57 rswindell Exp $ */
+/* $Id: xtrn.cpp,v 1.228 2018/01/13 09:07:47 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -35,7 +34,7 @@
  *																			*
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
-#define XTERN_LOG_STDERR
+
 #include "sbbs.h"
 #include "cmdshell.h"
 #include "telnet.h"
@@ -400,7 +399,10 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 	sbbsexec_start_t start;
 	OPENVXDHANDLE OpenVxDHandle;
 
-	lprintf(LOG_DEBUG,"Executing external: %s",cmdline);
+	if(online!=ON_REMOTE || cfg.node_num==0)
+		eprintf(LOG_DEBUG,"Executing external: %s",cmdline);
+	else
+		lprintf(LOG_DEBUG,"Node %d Executing external: %s",cfg.node_num,cmdline);
 
 	if(startup_dir!=NULL && startup_dir[0] && !isdir(startup_dir)) {
 		errormsg(WHERE, ERR_CHK, startup_dir, 0);
@@ -792,7 +794,8 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 			gettimeleft();
         if(!online && !(mode&EX_OFFLINE)) { // Tell VXD/VDD and external that user hung-up
         	if(was_online) {
-				logline(LOG_NOTICE,"X!","hung-up in external program");
+				sprintf(str,"%s hung-up in external program",useron.alias);
+				logline(LOG_NOTICE,"X!",str);
             	hungup=time(NULL);
 				if(!native) {
 					if(nt)
@@ -814,7 +817,7 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 	            was_online=false;
             }
             if(hungup && time(NULL)-hungup>5 && !processTerminated) {
-				lprintf(LOG_INFO,"Terminating process from line %d", __LINE__);
+				lprintf(LOG_INFO,"Node %d Terminating process from line %d",cfg.node_num,__LINE__);
 				processTerminated=TerminateProcess(process_info.hProcess, 2112);
 			}
         }
@@ -841,9 +844,9 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 							,FILE_ATTRIBUTE_NORMAL
 							,(HANDLE) NULL);
 						if(wrslot==INVALID_HANDLE_VALUE)
-							lprintf(LOG_DEBUG,"!ERROR %u opening %s", GetLastError(), str);
+							lprintf(LOG_DEBUG,"Node %d !ERROR %u opening %s", cfg.node_num, GetLastError(), str);
 						else
-							lprintf(LOG_DEBUG,"CreateFile(%s)=0x%x", str, wrslot);
+							lprintf(LOG_DEBUG,"Node %d CreateFile(%s)=0x%x", cfg.node_num, str, wrslot);
 					}
 					
 					/* CR expansion */
@@ -854,16 +857,16 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 
 					len=0;
 					if(wrslot==INVALID_HANDLE_VALUE)
-						lprintf(LOG_WARNING,"VDD Open failed (not loaded yet?)");
+						lprintf(LOG_WARNING,"Node %d VDD Open failed (not loaded yet?)",cfg.node_num);
 					else if(!WriteFile(wrslot,bp,wr,&len,NULL)) {
-						lprintf(LOG_ERR,"!VDD WriteFile(0x%x, %u) FAILURE (Error=%u)", wrslot, wr, GetLastError());
+						lprintf(LOG_ERR,"Node %d !VDD WriteFile(0x%x, %u) FAILURE (Error=%u)", cfg.node_num, wrslot, wr, GetLastError());
 						if(GetMailslotInfo(wrslot,&wr,NULL,NULL,NULL))
-							lprintf(LOG_DEBUG,"!VDD MailSlot max_msg_size=%u", wr);
+							lprintf(LOG_DEBUG,"Node %d !VDD MailSlot max_msg_size=%u", cfg.node_num, wr);
 						else
-							lprintf(LOG_DEBUG,"!GetMailslotInfo(0x%x)=%u", wrslot, GetLastError());
+							lprintf(LOG_DEBUG,"Node %d !GetMailslotInfo(0x%x)=%u", cfg.node_num, wrslot, GetLastError());
 					} else {
 						if(len!=wr)
-							lprintf(LOG_WARNING,"VDD short write (%u instead of %u)", len,wr);
+							lprintf(LOG_WARNING,"Node %d VDD short write (%u instead of %u)",cfg.node_num,len,wr);
 						RingBufRead(&inbuf, NULL, len);
 						if(use_pipes && !(mode&EX_NOECHO)) {
 							/* echo */
@@ -1302,6 +1305,7 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 	char	fullpath[MAX_PATH+1];
 	char	fullcmdline[MAX_PATH+1];
 	char*	argv[MAX_ARGS];
+	char*	p;
 	BYTE*	bp;
 	BYTE	buf[XTRN_IO_BUF_LEN];
     BYTE 	output_buf[XTRN_IO_BUF_LEN*2];
@@ -1323,13 +1327,9 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 	fd_set ibits;
 	int	high_fd;
 	struct timeval timeout;
-    BYTE 	wwiv_buf[XTRN_IO_BUF_LEN*2];
-    bool	wwiv_flag=false;
-#if defined(__FreeBSD__) || (defined(__linux__) && defined(USE_DOSEMU))
- 	char* p;
-#endif
 
-	lprintf(LOG_DEBUG, "Executing external: %s", cmdline);
+	if(online!=ON_REMOTE || cfg.node_num==0)
+		eprintf(LOG_DEBUG,"Executing external: %s",cmdline);
 
 	if(startup_dir!=NULL && startup_dir[0] && !isdir(startup_dir)) {
 		errormsg(WHERE, ERR_CHK, startup_dir, 0);
@@ -1423,7 +1423,7 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 		FILE *dosemubat;
 		int setup_override;
 		char tok[MAX_PATH+1];
-
+ 
 		char dosemuconf[MAX_PATH+1];
 		char dosemubinloc[MAX_PATH+1];
 		char virtualconf[75];
@@ -1513,6 +1513,7 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 		/* same deal for emusetup.bat. */
 
 		sprintf(str,"%semusetup.bat",startup_dir);
+		fprintf(stderr, str);
 		if (!fexist(str)) {
 
 		/* If we can't find it in the door dir, look for a global one
@@ -1564,7 +1565,7 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 			gamedir = getfname(str);
 		}
 		if(*gamedir == 0) {
-			lprintf(LOG_ERR, "No startup directory configured for DOS command-line: %s", cmdline);
+			lprintf(LOG_ERR, "No startup directory configured for: %s", cmdline);
 			return -1;
 		}
 		fprintf(dosemubat,"cd %s\r\n", gamedir);
@@ -1835,7 +1836,8 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 				gettimeleft();
 			
 			if(!online && !(mode&EX_OFFLINE)) {
-				logline(LOG_NOTICE,"X!","hung-up in external program");
+				sprintf(str,"%s hung-up in external program",useron.alias);
+				logline(LOG_NOTICE,"X!",str);
 				break;
 			}
 
@@ -1931,11 +1933,8 @@ int sbbs_t::external(const char* cmdline, long mode, const char* startup_dir)
 			} else if ((mode & EX_STDIO) != EX_STDIO) {
 				/* LF to CRLF expansion */
 				bp=lf_expand(buf, rd, output_buf, output_len);
-			} else if(mode&EX_WWIV) {
-                bp=wwiv_expand(buf, rd, wwiv_buf, output_len, useron.misc, wwiv_flag);
-				if(output_len > sizeof(wwiv_buf))
-					lprintf(LOG_ERR, "WWIV_BUF OVERRUN");
-			} else {
+			}
+			else {
 				bp=buf;
 				output_len=rd;
 			}
@@ -2066,7 +2065,7 @@ char* sbbs_t::cmdstr(const char *instr, const char *fpath, const char *fspec, ch
     else
         cmd=outstr;
     len=strlen(instr);
-    for(i=j=0; i<len && j < (int)sizeof(cmdstr_output)-1; i++) {
+    for(i=j=0;i<len && j<(int)sizeof(cmdstr_output);i++) {
         if(instr[i]=='%') {
             i++;
             cmd[j]=0;
@@ -2230,7 +2229,7 @@ char* DLLCALL cmdstr(scfg_t* cfg, user_t* user, const char* instr, const char* f
 
 	if(cmd==NULL)	cmd=buf;
     len=strlen(instr);
-    for(i=j=0; i<len && j < (int)sizeof(buf)-1; i++) {
+    for(i=j=0;i<len && j<MAX_PATH;i++) {
         if(instr[i]=='%') {
             i++;
             cmd[j]=0;
