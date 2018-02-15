7cf157b91f118983e2691198ac4b2f6938be61ad
deuce <deuce>
2018-02-15 06:29:42 +0000

    We also need to set the SELFSIGNED attribute.

--- conio/mouse.c
+++ conio/mouse.c
@@ -1,10 +1,10 @@
-/* $Id: mouse.c,v 1.45 2018/07/24 01:10:58 rswindell Exp $ */
+/* $Id: mouse.c,v 1.44 2018/01/22 09:32:01 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
