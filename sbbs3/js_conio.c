bbde47ef460531fe0a6e45cd34a43dbac5e04005
rswindell <rswindell>
2018-02-16 09:01:01 +0000

    Remove unused variable.

--- sbbs3/js_conio.c
+++ sbbs3/js_conio.c
@@ -2,7 +2,7 @@
 
 /* Synchronet "conio" (console IO) object */
 
-/* $Id: js_conio.c,v 1.29 2017/11/24 23:05:56 rswindell Exp $ */
+/* $Id: js_conio.c,v 1.31 2018/02/14 18:29:46 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -467,7 +467,8 @@ js_conio_beep(JSContext *cx, uintN argc, jsval *arglist)
 	jsrefcount	rc;
 
 	rc=JS_SUSPENDREQUEST(cx);
-    JS_SET_RVAL(cx, arglist, INT_TO_JSVAL(beep()));
+    	beep();
+	JS_SET_RVAL(cx, arglist, JSVAL_TRUE);
 	JS_RESUMEREQUEST(cx, rc);
 	return(JS_TRUE);
 }
@@ -475,12 +476,19 @@ js_conio_beep(JSContext *cx, uintN argc, jsval *arglist)
 static JSBool
 js_conio_getfont(JSContext *cx, uintN argc, jsval *arglist)
 {
+	jsval *argv=JS_ARGV(cx, arglist);
+	int32	fnum;
 	jsrefcount	rc;
 
-	rc=JS_SUSPENDREQUEST(cx);
-    JS_SET_RVAL(cx, arglist, INT_TO_JSVAL(getfont()));
-	JS_RESUMEREQUEST(cx, rc);
-	return(JS_TRUE);
+	if(argc==1 && JSVAL_IS_NUMBER(argv[0]) && JS_ValueToInt32(cx,argv[0],&fnum)) {
+		rc=JS_SUSPENDREQUEST(cx);
+		JS_SET_RVAL(cx, arglist, INT_TO_JSVAL(getfont(fnum)));
+		JS_SET_RVAL(cx, arglist, JSVAL_TRUE);
+		JS_RESUMEREQUEST(cx, rc);
+		return(JS_TRUE);
+	}
+
+	return(JS_FALSE);
 }
 
 static JSBool
@@ -1080,8 +1088,8 @@ static jsSyncMethodSpec js_functions[] = {
 		,JSTYPE_VOID,JSDOCSTR("")
 		,JSDOCSTR("Beeps."),315
 	},
-	{"getfont",			js_conio_getfont,		0
-		,JSTYPE_NUMBER,JSDOCSTR("")
+	{"getfont",			js_conio_getfont,		1
+		,JSTYPE_NUMBER,JSDOCSTR("fnum")
 		,JSDOCSTR("Returns the current font ID or -1 if fonts aren't supported."),315
 	},
 	{"hidemouse",		js_conio_hidemouse,		0
