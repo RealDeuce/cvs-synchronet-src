415cd59e8f1b2ff8c9008540cd992905562a103d
rswindell <rswindell>
2018-02-18 02:18:07 +0000

    JS login() method: Convert up to 128 characters of the passed user name to a string we can use and log in case of failure (helps to debug the full actual user name passed to the sevice, e.g. by an IMAP client).

--- sbbs3/str.cpp
+++ sbbs3/str.cpp
@@ -1,6 +1,6 @@
 /* Synchronet high-level string i/o routines */
 
-/* $Id: str.cpp,v 1.83 2018/10/26 03:33:14 rswindell Exp $ */
+/* $Id: str.cpp,v 1.77 2018/01/12 22:15:43 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -61,10 +61,7 @@ void sbbs_t::userlist(long mode)
 		if(sort && (online==ON_LOCAL || !rioctl(TXBC)))
 			bprintf("%-4d\b\b\b\b",i);
 		user.number=i;
-		if(fgetuserdat(&cfg, &user, userfile) != 0)
-			continue;
-		if(user.alias[0] <= ' ')
-			continue;
+		fgetuserdat(&cfg, &user, userfile);
 		if(user.misc&(DELETED|INACTIVE))
 			continue;
 		users++;
@@ -162,7 +159,6 @@ void sbbs_t::sif(char *fname, char *answers, long len)
 	}
 	if(lread(file,buf,length)!=length) {
 		close(file);
-		free(buf);
 		errormsg(WHERE,ERR_READ,str,length);
 		answers[0]=0;
 		return; 
@@ -332,7 +328,6 @@ void sbbs_t::sof(char *fname, char *answers, long len)
 		close(file);
 		errormsg(WHERE,ERR_READ,str,length);
 		answers[0]=0;
-		free(buf);
 		return; 
 	}
 	close(file);
@@ -880,15 +875,14 @@ void sbbs_t::sys_info()
 	bprintf(text[SiTotalTime],ultoac(stats.timeon,tmp));
 	bprintf(text[SiTimeToday],ultoac(stats.ttoday,tmp));
 	ver();
-	const char* fname = "../system";
-	if(menu_exists(fname) && text[ViewSysInfoFileQ][0] && yesno(text[ViewSysInfoFileQ])) {
+	if(text[ViewSysInfoFileQ][0] && yesno(text[ViewSysInfoFileQ])) {
 		CLS;
-		menu(fname);
+		sprintf(tmp,"%ssystem.msg", cfg.text_dir);
+		printfile(tmp,0); 
 	}
-	fname = "logon";
-	if(menu_exists(fname) && text[ViewLogonMsgQ][0] && yesno(text[ViewLogonMsgQ])) {
+	if(text[ViewLogonMsgQ][0] && yesno(text[ViewLogonMsgQ])) {
 		CLS;
-		menu(fname);
+		menu("logon"); 
 	}
 }
 
@@ -941,13 +935,15 @@ void sbbs_t::user_info()
 void sbbs_t::xfer_policy()
 {
 	if(!usrlibs) return;
-	if(!menu("tpolicy", P_NOERROR)) {
+	if(menu_exists("tpolicy"))
+		menu("tpolicy");
+	else {
 		bprintf(text[TransferPolicyHdr],cfg.sys_name);
 		bprintf(text[TpUpload]
 			,cfg.dir[usrdir[curlib][curdir[curlib]]]->up_pct);
 		bprintf(text[TpDownload]
 			,cfg.dir[usrdir[curlib][curdir[curlib]]]->dn_pct);
-	}
+		}
 }
 
 const char* prot_menu_file[] = {
@@ -960,7 +956,8 @@ const char* prot_menu_file[] = {
 
 void sbbs_t::xfer_prot_menu(enum XFER_TYPE type)
 {
-	if(menu(prot_menu_file[type], P_NOERROR)) {
+	if(menu_exists(prot_menu_file[type])) {
+		menu(prot_menu_file[type]);
 		return;
 	}
 
@@ -1042,7 +1039,7 @@ void sbbs_t::logonlist(void)
 		bputs(text[NoOneHasLoggedOnToday]); 
 	} else {
 		bputs(text[CallersToday]);
-		printfile(str,P_NOATCODES|P_OPENCLOSE|P_TRUNCATE);
+		printfile(str,P_NOATCODES|P_OPENCLOSE);
 		CRLF; 
 	}
 }
@@ -1066,7 +1063,7 @@ bool sbbs_t::spy(uint i /* node_num */)
 		return(false);
 	}
 	if(spy_socket[i-1]!=INVALID_SOCKET) {
-		bprintf("Node %d already being spied (%x)\r\n",i,spy_socket[i-1]);
+		bprintf("Node %d already being spied (%lx)\r\n",i,spy_socket[i-1]);
 		return(false);
 	}
 	bprintf("*** Synchronet Remote Spy on Node %d: Ctrl-C to Abort ***"
