3a08c392a6764cba9426ff2d368f50a2eff63f86
rswindell <rswindell>
2018-02-20 11:13:16 +0000

    Use SAFECAT() to address Coverity-reported concerns.

--- sbbs3/telnet.c
+++ sbbs3/telnet.c
@@ -2,13 +2,13 @@
 
 /* Synchronet telnet command/option functions */
 
-/* $Id: telnet.c,v 1.6 2018/02/20 11:57:13 rswindell Exp $ */
+/* $Id: telnet.c,v 1.5 2005/06/06 22:06:22 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
+ * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
