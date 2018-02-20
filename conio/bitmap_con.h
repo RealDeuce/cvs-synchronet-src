1d81368683be1c09b9b16e91e71e5d2a8964511b
deuce <deuce>
2018-02-20 19:06:37 +0000

    Fix leak in filepick when exiting from the mask field.

--- conio/bitmap_con.h
+++ conio/bitmap_con.h
@@ -4,18 +4,31 @@
 #include "vidmodes.h"
 #include "threadwrap.h"
 
+struct rectangle {
+	int x;
+	int y;
+	int width;
+	int height;
+};
+
+struct rectlist {
+	struct rectangle rect;
+	uint32_t *data;
+	struct rectlist *next;
+};
+
 extern struct video_stats vstat;
 extern pthread_mutex_t vstatlock;
 
 #ifndef BITMAP_CIOLIB_DRIVER
 /* Called from ciolib */
 int bitmap_puttext(int sx, int sy, int ex, int ey, void *fill);
-int bitmap_pputtext(int sx, int sy, int ex, int ey, void *fill, uint32_t *fg, uint32_t *bg);
-int bitmap_pgettext(int sx, int sy, int ex, int ey, void *fill, uint32_t *fg, uint32_t *bg);
+int bitmap_vmem_puttext(int sx, int sy, int ex, int ey, struct vmem_cell *fill);
+int bitmap_vmem_gettext(int sx, int sy, int ex, int ey, struct vmem_cell *fill);
 void bitmap_gotoxy(int x, int y);
 void bitmap_setcursortype(int type);
 int bitmap_setfont(int font, int force, int font_no);
-int bitmap_getfont(void);
+int bitmap_getfont(int fnum);
 int bitmap_loadfont(char *filename);
 int bitmap_movetext(int x, int y, int ex, int ey, int tox, int toy);
 void bitmap_clreol(void);
@@ -28,15 +41,21 @@ int bitmap_attr2palette(uint8_t attr, uint32_t *fgp, uint32_t *bgp);
 int bitmap_setpixel(uint32_t x, uint32_t y, uint32_t colour);
 int bitmap_setpixels(uint32_t sx, uint32_t sy, uint32_t ex, uint32_t ey, uint32_t x_off, uint32_t y_off, struct ciolib_pixels *, void *mask);
 struct ciolib_pixels *bitmap_getpixels(uint32_t sx, uint32_t sy, uint32_t ex, uint32_t ey);
+int bitmap_get_modepalette(uint32_t p[16]);
+int bitmap_set_modepalette(uint32_t p[16]);
+uint32_t bitmap_map_rgb(uint16_t r, uint16_t g, uint16_t b);
+void bitmap_replace_font(uint8_t id, char *name, void *data, size_t size);
+int bitmap_setpalette(uint32_t index, uint16_t r, uint16_t g, uint16_t b);
 #endif
 
 #ifdef BITMAP_CIOLIB_DRIVER
 /* Called from drivers */
 int bitmap_drv_init_mode(int mode, int *width, int *height);
-int bitmap_drv_init(void (*drawrect_cb) (int xpos, int ypos, int width, int height, uint32_t *data)
+int bitmap_drv_init(void (*drawrect_cb) (struct rectlist *data)
 				,void (*flush) (void));
 void bitmap_drv_request_pixels(void);
 void bitmap_drv_request_some_pixels(int x, int y, int width, int height);
+void bitmap_drv_free_rect(struct rectlist *rect);
 #endif
 
 #endif
