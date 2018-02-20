9cc1c8a90fd374d3d6e07a1736b924c205f178ff
rswindell <rswindell>
2018-02-20 00:24:09 +0000

    A simple script to clean all the relevant sbbs3 build output directories. Use 'cleanall.sh RELEASE=1' to clean the release output dirs. The deafult is the DEBUG output dirs otherwise. This possibly could be made a 'clean' target of the Makefile already in this directory, but I don't want to deal with recursive make right now.

--- conio/vidmodes.h
+++ conio/vidmodes.h
@@ -1,4 +1,4 @@
-/* $Id: vidmodes.h,v 1.22 2018/02/01 21:11:30 deuce Exp $ */
+/* $Id: vidmodes.h,v 1.28 2018/02/15 20:28:49 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -66,9 +66,7 @@ struct  video_params {
 
 struct vstat_vmem {
 	unsigned refcount;
-	unsigned short *vmem;
-	uint32_t *fgvmem;
-	uint32_t *bgvmem;
+	struct vmem_cell *vmem;
 };
 
 struct video_stats {
@@ -78,7 +76,7 @@ struct video_stats {
 	int curs_col;
 	int curs_start;
 	int curs_end;
-	int curs_blink;
+	int curs_blinks;
 	int curs_visible;
 	int default_curs_start;
 	int default_curs_end;
@@ -87,6 +85,7 @@ struct video_stats {
 	int charwidth;
 	int bright_background;
 	int blink;
+	int curs_blink;
 	int no_bright;
 	int no_blink;
 	int bright_altcharset;
@@ -108,7 +107,7 @@ enum {
 	,ATARI_PALETTE
 };
 
-extern struct video_params vparams[49];
+extern struct video_params vparams[52];
 #define NUMMODES      (sizeof(vparams) / sizeof(struct video_params))
 extern uint32_t palettes[5][16];
 extern struct dac_colors dac_default[TOTAL_DAC_SIZE];
