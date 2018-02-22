b91e2ba9a9a67b2f24ac34be2d3272e294cfccb5
rswindell <rswindell>
2018-02-22 22:51:21 +0000

    When configuring a message base (changing values in the "status header'), allow the "last_msg" value to be changed as well. When dumping a msgbase index, show 'M' for messages (not blank). When running maintenance, read the entire index in one shot. Don't display progress (percentage counting) during maintenance (just slows it down). When reading messages, get the plain-text version (MIME decode if needed). When a message base fails to open, continue processing additional message bases if specified on the command-line.

--- xpdev/dirwrap.c
+++ xpdev/dirwrap.c
@@ -1,7 +1,7 @@
 /* Directory-related system-call wrappers */
 // vi: tabstop=4
 
-/* $Id: dirwrap.c,v 1.95 2018/01/13 06:11:56 rswindell Exp $ */
+/* $Id: dirwrap.c,v 1.98 2018/02/20 21:22:27 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -248,7 +248,7 @@ int	DLLCALL	glob(const char *pattern, int flags, void* unused, glob_t* glob)
 			SAFECOPY(path,pattern);
 			p=getfname(path);
 			*p=0;
-			strcat(path,ff.name);
+			SAFECAT(path,ff.name);
 
 			if((glob->gl_pathv[glob->gl_pathc]=(char*)malloc(strlen(path)+2))==NULL) {
 				globfree(glob);
@@ -315,8 +315,9 @@ long DLLCALL getdirsize(const char* path, BOOL include_subdirs, BOOL subdir_only
 
 	SAFECOPY(match,path);
 	backslash(match);
-	strcat(match,ALLFILES);
-	glob(match,GLOB_MARK,NULL,&g);
+	SAFECAT(match,ALLFILES);
+	if (glob(match,GLOB_MARK,NULL,&g) != 0)
+		return 0;
 	if(include_subdirs && !subdir_only)
 		count=g.gl_pathc;
 	else
@@ -767,7 +768,7 @@ ulong DLLCALL getfilecount(const char *inpath, const char* pattern)
 
 	SAFECOPY(path, inpath);
 	backslash(path);
-	strcat(path, pattern);
+	SAFECAT(path, pattern);
 	if(glob(path, GLOB_MARK, NULL, &g))
 		return 0;
 	for(gi = 0; gi < g.gl_pathc; ++gi) {
@@ -968,8 +969,8 @@ char * DLLCALL _fullpath(char *target, const char *path, size_t size)  {
 	if(sb.st_mode&S_IFDIR)
 		strcat(target,"/"); */
 
-	for(;*out;out++)  {
-		while(*out=='/')  {
+	for(;*out;out++) {
+		while(*out=='/') {
 			if(*(out+1)=='/')
 				memmove(out,out+1,strlen(out));
 			else if(*(out+1)=='.' && (*(out+2)=='/' || *(out+2)==0))
@@ -986,6 +987,8 @@ char * DLLCALL _fullpath(char *target, const char *path, size_t size)  {
 				out++;
 			}
 		}
+		if (!*out)
+			break;
 	}
 	return(target);
 }
