c7a65ce93bfb75108397591152c9fab5b64a0bda
rswindell <rswindell>
2018-02-24 00:08:12 +0000

    Added recursive make targets for scfg, uedit, and umonitor - built with the default/all target. So a single "make" command in src/sbbs3 should build these nested targets as well. Caveat: "make clean" does not clean these sub-make targets.

--- xpdev/ini_file.c
+++ xpdev/ini_file.c
@@ -1,6 +1,6 @@
 /* Functions to create and parse .ini files */
 
-/* $Id: ini_file.c,v 1.160 2018/03/16 05:24:04 rswindell Exp $ */
+/* $Id: ini_file.c,v 1.155 2018/02/09 02:52:00 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -430,12 +430,9 @@ BOOL DLLCALL iniRemoveSection(str_list_t* list, const char* section)
 
 BOOL DLLCALL iniRemoveSections(str_list_t* list, const char* prefix)
 {
-	str_list_t sections;
+	str_list_t sections = iniGetSectionList(*list, prefix);
 	const char* section;
 
-	if (list == NULL)
-		return FALSE;
-	sections = iniGetSectionList(*list, prefix);
 	while((section = strListPop(&sections)) != NULL)
 		if(!iniRemoveSection(list, section))
 			return(FALSE);
@@ -570,12 +567,6 @@ char* DLLCALL iniSetStringLiteral(str_list_t* list, const char* section, const c
 				 ,ini_style_t* style)
 {
 	return ini_set_string(list, section, key, value, /* literal: */TRUE, style);
-}
-
-char* DLLCALL iniSetValue(str_list_t* list, const char* section, const char* key, const char* value
-				 ,ini_style_t* style)
-{
-	return ini_set_string(list, section, key, value, /* literal: */FALSE, style);
 }
 
 char* DLLCALL iniSetInteger(str_list_t* list, const char* section, const char* key, long value
