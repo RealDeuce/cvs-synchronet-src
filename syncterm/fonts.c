1dd52c6c4913280e6b01f2ae041e478a64279c4c
rswindell <rswindell>
2018-02-24 21:56:27 +0000

    Added new source files: js_cryptcert.c and js_cryptkeyset.c.

--- syncterm/fonts.c
+++ syncterm/fonts.c
@@ -144,7 +144,7 @@ void load_font_files(void)
 			FREE_AND_NULL(conio_fontdata[nextfont].eight_by_sixteen);
 		if(conio_fontdata[nextfont].eight_by_fourteen)
 			FREE_AND_NULL(conio_fontdata[nextfont].eight_by_fourteen);
-		if(conio_fontdata[nextfont].eight_by_sixteen)
+		if(conio_fontdata[nextfont].eight_by_eight)
 			FREE_AND_NULL(conio_fontdata[nextfont].eight_by_eight);
 		if(conio_fontdata[nextfont].desc)
 			FREE_AND_NULL(conio_fontdata[nextfont].desc);
@@ -270,12 +270,14 @@ void font_management(void)
 			char	**path;
 
 			if(i&MSK_DEL) {
-				FREE_AND_NULL(fonts[cur].name);
-				FREE_AND_NULL(fonts[cur].path8x8);
-				FREE_AND_NULL(fonts[cur].path8x14);
-				FREE_AND_NULL(fonts[cur].path8x16);
-				memmove(&(fonts[cur]),&(fonts[cur+1]),sizeof(struct font_files)*(count-cur));
-				count--;
+				if (fonts) {
+					FREE_AND_NULL(fonts[cur].name);
+					FREE_AND_NULL(fonts[cur].path8x8);
+					FREE_AND_NULL(fonts[cur].path8x14);
+					FREE_AND_NULL(fonts[cur].path8x16);
+					memmove(&(fonts[cur]),&(fonts[cur+1]),sizeof(struct font_files)*(count-cur));
+					count--;
+				}
 				break;
 			}
 			if(i&MSK_INS) {
@@ -351,18 +353,18 @@ void font_management(void)
 			if(show_filepick && !safe_mode) {
 				int result;
 				struct file_pick fpick;
-				char	*savbuf;
+				struct vmem_cell	*savbuf;
 				struct text_info	ti;
 
 				gettextinfo(&ti);
-				savbuf=(char *)alloca((ti.screenheight-2)*ti.screenwidth*2);
+				savbuf=alloca((ti.screenheight-2)*ti.screenwidth*sizeof(*savbuf));
 				if(savbuf==NULL) {
 					uifc.helpbuf="malloc() has failed.  Available Memory is dangerously low.";
 					uifc.msg("malloc() failure.");
 					check_exit(FALSE);
 					continue;
 				}
-				gettext(1,2,ti.screenwidth,ti.screenheight-1,savbuf);
+				vmem_gettext(1,2,ti.screenwidth,ti.screenheight-1,savbuf);
 				result=filepick(&uifc, str, &fpick, ".", fontmask, UIFC_FP_ALLOWENTRY);
 				if(result!=-1 && fpick.files>0) {
 					FREE_AND_NULL(*path);
@@ -371,7 +373,7 @@ void font_management(void)
 				else
 					check_exit(FALSE);
 				filepick_free(&fpick);
-				puttext(1,2,ti.screenwidth,ti.screenheight-1,savbuf);
+				vmem_puttext(1,2,ti.screenwidth,ti.screenheight-1,savbuf);
 			}
 		}
 	}
