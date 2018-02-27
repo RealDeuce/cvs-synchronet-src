56554fdf2ea54f0d7adbbe9bae37dece9d039d85
deuce <deuce>
2018-02-27 08:00:36 +0000

    Add support for ECDSA keys. Also, free() temp variables for RSA keys.

--- sbbs3/targets.mk
+++ sbbs3/targets.mk
@@ -2,7 +2,7 @@
 
 # Make 'include file' defining targets for Synchronet project
 
-# $Id: targets.mk,v 1.40 2015/10/28 02:00:58 deuce Exp $
+# $Id: targets.mk,v 1.44 2018/02/24 00:08:12 rswindell Exp $
 
 # LIBODIR, EXEODIR, DIRSEP, LIBFILE, EXEFILE, and DELETE must be pre-defined
 
@@ -36,6 +36,7 @@ DELFILES	= $(EXEODIR)$(DIRSEP)delfiles$(EXEFILE)
 DUPEFIND	= $(EXEODIR)$(DIRSEP)dupefind$(EXEFILE)
 SMBACTIV	= $(EXEODIR)$(DIRSEP)smbactiv$(EXEFILE)
 DSTSEDIT	= $(EXEODIR)$(DIRSEP)dstsedit$(EXEFILE)
+READSAUCE	= $(EXEODIR)$(DIRSEP)readsauce$(EXEFILE)
 
 UTILS		= $(FIXSMB) $(CHKSMB) \
 			  $(SMBUTIL) $(BAJA) $(NODE) \
@@ -44,9 +45,9 @@ UTILS		= $(FIXSMB) $(CHKSMB) \
 			  $(ANS2ASC) $(ASC2ANS)  $(UNBAJA) \
 			  $(QWKNODES) $(SLOG) $(ALLUSERS) \
 			  $(DELFILES) $(DUPEFIND) $(SMBACTIV) \
-			  $(SEXYZ) $(DSTSEDIT)
+			  $(SEXYZ) $(DSTSEDIT) $(READSAUCE)
 
-all:	dlls utils console
+all:	dlls utils console scfg uedit umonitor
 
 console:	$(JS_DEPS) xpdev-mt smblib \
 		$(MTOBJODIR) $(LIBODIR) $(EXEODIR) \
@@ -64,6 +65,31 @@ dlls:	$(JS_DEPS) smblib xpdev-mt \
 mono:	xpdev-mt smblib \
 		$(MTOBJODIR) $(EXEODIR) \
 		$(SBBSMONO)
+
+.PHONY: scfg
+scfg:
+	$(MAKE) -C scfg $(MAKEFLAGS)
+
+.PHONY: uedit
+uedit:
+	$(MAKE) -C uedit $(MAKEFLAGS)
+
+.PHONY: umonitor
+umonitor:
+	$(MAKE) -C umonitor $(MAKEFLAGS)
+
+
+ifdef SBBSEXEC
+.PHONY: install
+install: all
+	install $(EXEODIR)/* $(SBBSEXEC)
+	install $(LIBODIR)/* $(SBBSEXEC)
+
+.PHONY: symlinks
+symlinks: all
+	ln -sfr $(EXEODIR)/* $(SBBSEXEC)
+	ln -sfr $(LIBODIR)/* $(SBBSEXEC)
+endif
 
 .PHONY: sexyz
 sexyz:	$(SEXYZ)
@@ -102,3 +128,4 @@ $(DELFILES): $(XPDEV_LIB)
 $(DUPEFIND): $(XPDEV_LIB) $(SMBLIB)
 $(SMBACTIV): $(XPDEV_LIB) $(SMBLIB)
 $(DSTSEDIT): $(XPDEV_LIB)
+$(READSAUCE): $(XPDEV_LIB)
