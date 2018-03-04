a6ccfe9582d047cd1c2904687e7d3a97944df7ff
deuce <deuce>
2018-03-04 07:11:56 +0000

    Mail server support for Opportunistic TLS.
    Support STARTTLS in SMTP and STLS in POP3.  This includes the sendmail
    thread.  Clients can now be configured to use STARTTLS with Synchronet
    mail services, and Synchronet will attempt to deliver securely when
    possible (though it will fall back to plain text delivery when necessary.)
    
    This touches a *lot* of mail server stuff, so some instability is not
    unlikely.
    
    Of course, RFC8314 (Jan, 2018) officially recommends implicit TLS on the TLS
    ports now, so support for that should be next.

--- sbbs3/mailsrvr.h
+++ sbbs3/mailsrvr.h
@@ -1,6 +1,6 @@
 /* Synchronet Mail (SMTP/POP3/SendMail) server */
 
-/* $Id: mailsrvr.h,v 1.79 2017/08/26 01:57:24 rswindell Exp $ */
+/* $Id: mailsrvr.h,v 1.81 2018/03/04 07:11:56 deuce Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -166,6 +166,7 @@ static struct init_field mail_init_fields[] = {
 #define MAIL_OPT_RELAY_AUTH_CRAM_MD5	(1<<25)
 #define MAIL_OPT_NO_AUTO_EXEMPT			(1<<26)	/* Do not auto DNSBL-exempt recipient e-mail addresses */
 #define MAIL_OPT_NO_RECYCLE				(1<<27)	/* Disable recycling of server		*/
+#define MAIL_OPT_KILL_READ_SPAM			(1<<28)	/* Set the KILLREAD flag on SPAM msgs */
 #define MAIL_OPT_MUTE					(1<<31)
 
 #define MAIL_OPT_RELAY_AUTH_MASK		(MAIL_OPT_RELAY_AUTH_PLAIN|MAIL_OPT_RELAY_AUTH_LOGIN|MAIL_OPT_RELAY_AUTH_CRAM_MD5)
@@ -204,6 +205,7 @@ static ini_bitdesc_t mail_options[] = {
 	{ MAIL_OPT_RELAY_AUTH_CRAM_MD5	,"RELAY_AUTH_CRAM_MD5"	},
 	{ MAIL_OPT_NO_AUTO_EXEMPT		,"NO_AUTO_EXEMPT"		},
 	{ MAIL_OPT_NO_RECYCLE			,"NO_RECYCLE"			},
+	{ MAIL_OPT_KILL_READ_SPAM		,"KILL_READ_SPAM"		},
 	{ MAIL_OPT_MUTE					,"MUTE"					},
 	/* terminator */
 	{ 0 							,NULL					}
@@ -248,6 +250,6 @@ int mail_close_socket(SOCKET sock);
 }
 #endif
 
-int sockprintf(SOCKET sock, char *fmt, ...);
+int sockprintf(SOCKET sock, int sess, char *fmt, ...);
 
 #endif /* Don't add anything after this line */
--- sbbs3/writemsg.cpp
+++ sbbs3/writemsg.cpp
@@ -1,6 +1,6 @@
 /* Synchronet message creation routines */
 
-/* $Id: writemsg.cpp,v 1.115 2016/11/27 23:03:03 rswindell Exp $ */
+/* $Id: writemsg.cpp,v 1.120 2018/02/20 11:44:52 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -174,8 +174,10 @@ int sbbs_t::process_edited_file(const char* src, const char* dest, long mode, un
 	if((buf=(char*)malloc(len+1))==NULL)
 		return -2;
 
-	if((fp=fopen(src,"rb"))==NULL)
+	if((fp=fopen(src,"rb"))==NULL) {
+		free(buf);
 		return -3;
+	}
 
 	memset(buf,0,len+1);
 	fread(buf,len,sizeof(char),fp);
@@ -220,10 +222,10 @@ bool sbbs_t::writemsg(const char *fname, const char *top, char *subj, long mode,
 	if(editor!=NULL)
 		*editor=NULL;
 
-	if((buf=(char*)malloc(cfg.level_linespermsg[useron_level]*MAX_LINE_LEN))
+	if((buf=(char*)malloc((cfg.level_linespermsg[useron_level]*MAX_LINE_LEN) + 1))
 		==NULL) {
 		errormsg(WHERE,ERR_ALLOC,fname
-			,cfg.level_linespermsg[useron_level]*MAX_LINE_LEN);
+			,(cfg.level_linespermsg[useron_level]*MAX_LINE_LEN) +1);
 		return(false); 
 	}
 
@@ -726,7 +728,6 @@ ulong sbbs_t::msgeditor(char *buf, const char *top, char *title)
 	int		i,j,line,lines=0,maxlines;
 	char	strin[256],**str,done=0;
 	char 	tmp[512];
-	char	path[MAX_PATH+1];
     ulong	l,m;
 
 	rioctl(IOCM|ABORT);
@@ -779,8 +780,7 @@ ulong sbbs_t::msgeditor(char *buf, const char *top, char *title)
 		bprintf("\r\nMessage editor: Read in %d lines\r\n",lines);
 	bprintf(text[EnterMsgNow],maxlines);
 
-	SAFEPRINTF(path,"%smenu/msgtabs.*", cfg.text_dir);
-	if(fexist(path))
+	if(menu_exists("msgtabs"))
 		menu("msgtabs");
 	else {
 		for(i=0;i<79;i++) {
@@ -1207,7 +1207,7 @@ void sbbs_t::forwardmail(smbmsg_t *msg, int usernumber)
 	smb_close_da(&smb);
 
 
-	if((i=smb_addmsghdr(&smb,msg,SMB_SELFPACK))!=SMB_SUCCESS) {
+	if((i=smb_addmsghdr(&smb,msg,smb_storage_mode(&cfg, &smb)))!=SMB_SUCCESS) {
 		errormsg(WHERE,ERR_WRITE,smb.file,i,smb.last_error);
 		smb_freemsg_dfields(&smb,msg,1);
 		return; 
@@ -1549,6 +1549,9 @@ ushort sbbs_t::chmsgattr(smbmsg_t msg)
 		switch(ch) {
 			case 'P':
 				msg.hdr.attr^=MSG_PRIVATE;
+				break;
+			case 'S':
+				msg.hdr.attr^=MSG_SPAM;
 				break;
 			case 'R':
 				msg.hdr.attr^=MSG_READ;
