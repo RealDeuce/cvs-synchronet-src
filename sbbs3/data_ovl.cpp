41c804215b3d77558e8f3b680aaf84a2dae8d940
rswindell <rswindell>
2018-03-06 08:05:12 +0000

    Lower the log level in the "JavaScript FAILED to compile rewrite" log entry - if there's an actual compile error, it should already be logged via the exception/error-reporter callback. This should eliminate a lot of the dokuwiki-related error chatter in my error.log.

--- sbbs3/data_ovl.cpp
+++ sbbs3/data_ovl.cpp
@@ -1,6 +1,6 @@
 /* Synchronet hi-level data access routines */
 
-/* $Id: data_ovl.cpp,v 1.23 2018/03/08 08:19:10 rswindell Exp $ */
+/* $Id: data_ovl.cpp,v 1.22 2016/12/10 21:29:04 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -50,9 +50,8 @@ void sbbs_t::getmsgptrs()
 {
 	if(!useron.number)
 		return;
-	::getmsgptrs(&cfg,&useron,subscan,online == ON_REMOTE ? ProgressLoadingMsgPtrs : NULL,this);
-	if(online == ON_REMOTE)
-		bputs(text[LoadedMsgPtrs]);
+	::getmsgptrs(&cfg,&useron,subscan,ProgressLoadingMsgPtrs,this);
+	bputs(text[LoadedMsgPtrs]);
 }
 
 void sbbs_t::putmsgptrs()
@@ -77,8 +76,7 @@ static void ProgressSearchingUsers(void* cbdata, int count, int total)
 uint sbbs_t::userdatdupe(uint usernumber, uint offset, uint datlen, char *dat
     ,bool del, bool next)
 {
-	uint i=::userdatdupe(&cfg, usernumber, offset, datlen, dat, del, next, online == ON_REMOTE ? ProgressSearchingUsers : NULL, this);
-	if(online == ON_REMOTE)
-		bputs(text[SearchedForDupes]);
+	uint i=::userdatdupe(&cfg, usernumber, offset, datlen, dat, del, next, ProgressSearchingUsers, this);
+	bputs(text[SearchedForDupes]);
 	return(i);
 }
