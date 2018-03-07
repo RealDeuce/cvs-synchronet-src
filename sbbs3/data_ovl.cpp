72f46a77d977c3c080f353c853b148a55bead3c2
deuce <deuce>
2018-03-07 23:34:21 +0000

    Return an empty LIST or NLST listing if parsepath() fails.

--- sbbs3/data_ovl.cpp
+++ sbbs3/data_ovl.cpp
@@ -1,6 +1,6 @@
 /* Synchronet hi-level data access routines */
 
-/* $Id: data_ovl.cpp,v 1.28 2019/02/17 06:20:26 rswindell Exp $ */
+/* $Id: data_ovl.cpp,v 1.22 2016/12/10 21:29:04 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -50,15 +50,13 @@ void sbbs_t::getmsgptrs()
 {
 	if(!useron.number)
 		return;
-	::getmsgptrs(&cfg,&useron,subscan,online == ON_REMOTE ? ProgressLoadingMsgPtrs : NULL,this);
-	if(online == ON_REMOTE)
-		bputs(text[LoadedMsgPtrs]);
+	::getmsgptrs(&cfg,&useron,subscan,ProgressLoadingMsgPtrs,this);
+	bputs(text[LoadedMsgPtrs]);
 }
 
 void sbbs_t::putmsgptrs()
 {
-	if(!::putmsgptrs(&cfg,&useron,subscan))
-		errormsg(WHERE, ERR_WRITE, "message pointers", 0);
+	::putmsgptrs(&cfg,&useron,subscan);
 }
 
 static void ProgressSearchingUsers(void* cbdata, int count, int total)
@@ -78,8 +76,7 @@ static void ProgressSearchingUsers(void* cbdata, int count, int total)
 uint sbbs_t::userdatdupe(uint usernumber, uint offset, uint datlen, char *dat
     ,bool del, bool next)
 {
-	uint i=::userdatdupe(&cfg, usernumber, offset, datlen, dat, del, next, online == ON_REMOTE ? ProgressSearchingUsers : NULL, this);
-	if(online == ON_REMOTE)
-		bputs(text[SearchedForDupes]);
+	uint i=::userdatdupe(&cfg, usernumber, offset, datlen, dat, del, next, ProgressSearchingUsers, this);
+	bputs(text[SearchedForDupes]);
 	return(i);
 }
