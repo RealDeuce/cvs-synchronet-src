e41ef2bad0c2c2b1d147bb9391408954ccdff275
deuce <deuce>
2018-03-07 22:22:03 +0000

    Skip errored path components and return a root error... LIST really needs to handle this somehow (but currently doesn't). Also, remove some debug output.

--- sbbs3/js_uifc.c
+++ sbbs3/js_uifc.c
@@ -2,13 +2,13 @@
 
 /* Synchronet "uifc" (user interface) object */
 
-/* $Id: js_uifc.c,v 1.40 2017/11/17 10:03:07 rswindell Exp $ */
+/* $Id: js_uifc.c,v 1.44 2018/02/20 11:56:27 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2013 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -299,7 +299,7 @@ static JSBool js_set(JSContext *cx, JSObject *obj, jsid id, JSBool strict, jsval
 		if(uifc->helpbuf)
 			free(uifc->helpbuf);
 		JSVALUE_TO_MSTRING(cx, *vp, uifc->helpbuf, NULL);
-		HANDLE_PENDING(cx);
+		HANDLE_PENDING(cx, NULL);
 		return JS_TRUE;
 	}
 
@@ -425,7 +425,7 @@ js_uifc_init(JSContext *cx, uintN argc, jsval *arglist)
 
 	if(argc) {
 		JSVALUE_TO_MSTRING(cx, argv[0], title, NULL);
-		HANDLE_PENDING(cx);
+		HANDLE_PENDING(cx, title);
 		if(title==NULL)
 			return(JS_TRUE);
 	}
@@ -525,7 +525,7 @@ js_uifc_msg(JSContext *cx, uintN argc, jsval *arglist)
 {
 	JSObject *obj=JS_THIS_OBJECT(cx, arglist);
 	jsval *argv=JS_ARGV(cx, arglist);
-	char*		str;
+	char*		str = NULL;
 	uifcapi_t*	uifc;
 	jsrefcount	rc;
 
@@ -535,7 +535,7 @@ js_uifc_msg(JSContext *cx, uintN argc, jsval *arglist)
 		return(JS_FALSE);
 
 	JSVALUE_TO_MSTRING(cx, argv[0], str, NULL);
-	HANDLE_PENDING(cx);
+	HANDLE_PENDING(cx, str);
 	if(str==NULL)
 		return(JS_TRUE);
 
@@ -562,7 +562,7 @@ js_uifc_pop(JSContext *cx, uintN argc, jsval *arglist)
 
 	if(argc) {
 		JSVALUE_TO_MSTRING(cx, argv[0], str, NULL);
-		HANDLE_PENDING(cx);
+		HANDLE_PENDING(cx, str);
 	}
 
 	rc=JS_SUSPENDREQUEST(cx);
@@ -607,7 +607,7 @@ js_uifc_input(JSContext *cx, uintN argc, jsval *arglist)
 	if(argn<argc && JSVAL_IS_STRING(argv[argn])) {
 		JSVALUE_TO_MSTRING(cx, argv[argn], prompt, NULL);
 		argn++;
-		HANDLE_PENDING(cx);
+		HANDLE_PENDING(cx, prompt);
 		if(prompt==NULL)
 			return(JS_TRUE);
 	}
@@ -619,8 +619,11 @@ js_uifc_input(JSContext *cx, uintN argc, jsval *arglist)
 				free(prompt);
 			return JS_FALSE;
 		}
-		if(org==NULL)
+		if(org==NULL) {
+			if(prompt)
+				free(prompt);
 			return(JS_TRUE);
+		}
 	}
 	if(argn<argc && JSVAL_IS_NUMBER(argv[argn]) 
 		&& !JS_ValueToInt32(cx,argv[argn++],&maxlen)) {
@@ -714,7 +717,7 @@ js_uifc_list(JSContext *cx, uintN argc, jsval *arglist)
 	for(; argn<argc; argn++) {
 		if(JSVAL_IS_STRING(argv[argn])) {
 			JSVALUE_TO_MSTRING(cx, argv[argn], title, NULL);
-			HANDLE_PENDING(cx);
+			HANDLE_PENDING(cx, title);
 			continue;
 		}
 		if(!JSVAL_IS_OBJECT(argv[argn]))
@@ -724,7 +727,8 @@ js_uifc_list(JSContext *cx, uintN argc, jsval *arglist)
 		if(JS_IsArrayObject(cx, objarg)) {
 			if(!JS_GetArrayLength(cx, objarg, &numopts))
 				return(JS_TRUE);
-			opts=strListInit();
+			if(opts == NULL)
+				opts=strListInit();
 			for(i=0;i<numopts;i++) {
 				if(!JS_GetElement(cx, objarg, i, &val))
 					break;
@@ -735,8 +739,7 @@ js_uifc_list(JSContext *cx, uintN argc, jsval *arglist)
 				}
 				strListPush(&opts,opt);
 			}
-			if(opt)
-				free(opt);
+			FREE_AND_NULL(opt);
 		}
 		else if(JS_GetClass(cx, objarg) == &js_uifc_list_ctx_class) {
 			p = JS_GetPrivate(cx, objarg);
@@ -757,6 +760,8 @@ js_uifc_list(JSContext *cx, uintN argc, jsval *arglist)
 		JS_RESUMEREQUEST(cx, rc);
 	}
 	strListFree(&opts);
+	if(title != NULL)
+		free(title);
 	return(JS_TRUE);
 }
 
@@ -823,7 +828,7 @@ static JSBool js_uifc_resolve(JSContext *cx, JSObject *obj, jsid id)
 		JS_IdToValue(cx, id, &idval);
 		if(JSVAL_IS_STRING(idval)) {
 			JSSTRING_TO_MSTRING(cx, JSVAL_TO_STRING(idval), name, NULL);
-			HANDLE_PENDING(cx);
+			HANDLE_PENDING(cx, name);
 		}
 	}
 
