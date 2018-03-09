64bf92f821fd872968d5d9c56b10a096a31fdb55
deuce <deuce>
2018-03-09 20:25:12 +0000

    Copy error descriptions from cryptlib.h into get_error_string() so we should always get a string in cryptlib errors.  Places that don't get an error string should now since it should always be useful.

--- sbbs3/ssl.c
+++ sbbs3/ssl.c
@@ -40,6 +40,121 @@ static bool get_error_string(int status, CRYPT_SESSION sess, char estr[SSL_ESTR_
 		return true;
 
 	emsg = get_crypt_error(sess);
+	if (emsg == NULL) {
+		switch(status) {
+			case CRYPT_ERROR_PARAM1:
+				emsg = strdup("Bad argument, parameter 1");
+				break;
+			case CRYPT_ERROR_PARAM2:
+				emsg = strdup("Bad argument, parameter 2");
+				break;
+			case CRYPT_ERROR_PARAM3:
+				emsg = strdup("Bad argument, parameter 3");
+				break;
+			case CRYPT_ERROR_PARAM4:
+				emsg = strdup("Bad argument, parameter 4");
+				break;
+			case CRYPT_ERROR_PARAM5:
+				emsg = strdup("Bad argument, parameter 5");
+				break;
+			case CRYPT_ERROR_PARAM6:
+				emsg = strdup("Bad argument, parameter 6");
+				break;
+			case CRYPT_ERROR_PARAM7:
+				emsg = strdup("Bad argument, parameter 7");
+				break;
+
+			/* Errors due to insufficient resources */
+
+			case CRYPT_ERROR_MEMORY:
+				emsg = strdup("Out of memory");
+				break;
+			case CRYPT_ERROR_NOTINITED:
+				emsg = strdup("Data has not been initialised");
+				break;
+			case CRYPT_ERROR_INITED:
+				emsg = strdup("Data has already been init'd");
+				break;
+			case CRYPT_ERROR_NOSECURE:
+				emsg = strdup("Opn.not avail.at requested sec.level");
+				break;
+			case CRYPT_ERROR_RANDOM:
+				emsg = strdup("No reliable random data available");
+				break;
+			case CRYPT_ERROR_FAILED:
+				emsg = strdup("Operation failed");
+				break;
+			case CRYPT_ERROR_INTERNAL:
+				emsg = strdup("Internal consistency check failed");
+				break;
+
+			/* Security violations */
+
+			case CRYPT_ERROR_NOTAVAIL:
+				emsg = strdup("This type of opn.not available");
+				break;
+			case CRYPT_ERROR_PERMISSION:
+				emsg = strdup("No permiss.to perform this operation");
+				break;
+			case CRYPT_ERROR_WRONGKEY:
+				emsg = strdup("Incorrect key used to decrypt data");
+				break;
+			case CRYPT_ERROR_INCOMPLETE:
+				emsg = strdup("Operation incomplete/still in progress");
+				break;
+			case CRYPT_ERROR_COMPLETE:
+				emsg = strdup("Operation complete/can't continue");
+				break;
+			case CRYPT_ERROR_TIMEOUT:
+				emsg = strdup("Operation timed out before completion");
+				break;
+			case CRYPT_ERROR_INVALID:
+				emsg = strdup("Invalid/inconsistent information");
+				break;
+			case CRYPT_ERROR_SIGNALLED:
+				emsg = strdup("Resource destroyed by extnl.event");
+				break;
+			   
+			/* High-level function errors */
+
+			case CRYPT_ERROR_OVERFLOW:
+				emsg = strdup("Resources/space exhausted");
+				break;
+			case CRYPT_ERROR_UNDERFLOW:
+				emsg = strdup("Not enough data available");
+				break;
+			case CRYPT_ERROR_BADDATA:
+				emsg = strdup("Bad/unrecognised data format");
+				break;
+			case CRYPT_ERROR_SIGNATURE:
+				emsg = strdup("Signature/integrity check failed");
+				break;
+
+			/* Data access function errors */
+
+			case CRYPT_ERROR_OPEN:
+				emsg = strdup("Cannot open object");
+				break;
+			case CRYPT_ERROR_READ:
+				emsg = strdup("Cannot read item from object");
+				break;
+			case CRYPT_ERROR_WRITE:
+				emsg = strdup("Cannot write item to object");
+				break;
+			case CRYPT_ERROR_NOTFOUND:
+				emsg = strdup("Requested item not found in object");
+				break;
+			case CRYPT_ERROR_DUPLICATE:
+				emsg = strdup("Item already present in object");
+				break;
+
+			/* Data enveloping errors */
+
+			case CRYPT_ENVELOPE_RESOURCE:
+				emsg = strdup("Need resource to proceed");
+				break;
+		}
+	}
 	if (emsg) {
 		safe_snprintf(estr, SSL_ESTR_LEN, "cryptlib error %d at %s:%d (%s)", status, file, line, emsg);
 		free_crypt_attrstr(emsg);
@@ -91,7 +206,7 @@ bool DLLCALL is_crypt_initialized(void)
 CRYPT_CONTEXT DLLCALL get_ssl_cert(scfg_t *cfg, char estr[SSL_ESTR_LEN])
 {
 	CRYPT_KEYSET		ssl_keyset;
-	CRYPT_CONTEXT		ssl_context;
+	CRYPT_CONTEXT		ssl_context = -1;	// MSVC requires this to be initialized
 	CRYPT_CERTIFICATE	ssl_cert;
 	int					i;
 	char				sysop_email[sizeof(cfg->sys_inetaddr)+6];
@@ -100,10 +215,13 @@ CRYPT_CONTEXT DLLCALL get_ssl_cert(scfg_t *cfg, char estr[SSL_ESTR_LEN])
 	if(!do_cryptInit())
 		return -1;
 	pthread_mutex_lock(&ssl_cert_mutex);
-	memset(&ssl_context, 0, sizeof(ssl_context));
+	if (cfg->tls_certificate != -1 || !cfg->prepped) {
+		pthread_mutex_unlock(&ssl_cert_mutex);
+		return cfg->tls_certificate;
+	}
 	/* Get the certificate... first try loading it from a file... */
 	SAFEPRINTF2(str,"%s%s",cfg->ctrl_dir,"ssl.cert");
-	if(cryptStatusOK(cryptKeysetOpen(&ssl_keyset, CRYPT_UNUSED, CRYPT_KEYSET_FILE, str, CRYPT_KEYOPT_NONE))) {
+	if(cryptStatusOK(cryptKeysetOpen(&ssl_keyset, CRYPT_UNUSED, CRYPT_KEYSET_FILE, str, CRYPT_KEYOPT_READONLY))) {
 		if(!DO(cryptGetPrivateKey(ssl_keyset, &ssl_context, CRYPT_KEYID_NAME, "ssl_cert", cfg->sys_pass))) {
 			pthread_mutex_unlock(&ssl_cert_mutex);
 			return -1;
@@ -113,7 +231,8 @@ CRYPT_CONTEXT DLLCALL get_ssl_cert(scfg_t *cfg, char estr[SSL_ESTR_LEN])
 		/* Couldn't do that... create a new context and use the cert from there... */
 		if(!cryptStatusOK(i=cryptCreateContext(&ssl_context, CRYPT_UNUSED, CRYPT_ALGO_RSA))) {
 			pthread_mutex_unlock(&ssl_cert_mutex);
-			sprintf(estr, "cryptlib error %d creating SSL context",i);
+			if (estr)
+				sprintf(estr, "cryptlib error %d creating SSL context",i);
 			return -1;
 		}
 		if(!DO(cryptSetAttributeString(ssl_context, CRYPT_CTXINFO_LABEL, "ssl_cert", 8)))
@@ -151,16 +270,16 @@ CRYPT_CONTEXT DLLCALL get_ssl_cert(scfg_t *cfg, char estr[SSL_ESTR_LEN])
 		cryptKeysetClose(ssl_keyset);
 		cryptDestroyContext(ssl_context);
 		// Finally, load it from the file.
-		if(cryptStatusOK(cryptKeysetOpen(&ssl_keyset, CRYPT_UNUSED, CRYPT_KEYSET_FILE, str, CRYPT_KEYOPT_NONE))) {
+		if(cryptStatusOK(cryptKeysetOpen(&ssl_keyset, CRYPT_UNUSED, CRYPT_KEYSET_FILE, str, CRYPT_KEYOPT_READONLY))) {
 			if(!DO(cryptGetPrivateKey(ssl_keyset, &ssl_context, CRYPT_KEYID_NAME, "ssl_cert", cfg->sys_pass))) {
-				pthread_mutex_unlock(&ssl_cert_mutex);
-				return -1;
+				ssl_context = -1;
 			}
 		}
 	}
 
 	cryptKeysetClose(ssl_keyset);
 	pthread_mutex_unlock(&ssl_cert_mutex);
+	cfg->tls_certificate = ssl_context;
 	return ssl_context;
 
 failure_return_3:
