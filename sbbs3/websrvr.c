2f8ddebd399c2504daed7593ede2a39da263612a
deuce <deuce>
2018-03-10 06:34:49 +0000

    Add missing NULL check.

--- sbbs3/websrvr.c
+++ sbbs3/websrvr.c
@@ -1,6 +1,6 @@
 /* Synchronet Web Server */
 
-/* $Id: websrvr.c,v 1.654 2018/03/03 02:02:32 deuce Exp $ */
+/* $Id: websrvr.c,v 1.659 2018/03/10 03:53:32 deuce Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -394,6 +394,16 @@ enum  {
 	,MOVED_STAT
 };
 
+#define GCES(status, sess, action) do {                                                 \
+	char *GCES_estr;                                                                \
+	int GCES_level;                                                                 \
+	get_crypt_error_string(status, sess->tls_sess, &GCES_estr, action, &GCES_level);\
+	if (GCES_estr) {                                                                \
+		lprintf(GCES_level, "%04d %s", sess->socket, GCES_estr);                \
+		free(GCES_estr);                                                        \
+	}                                                                               \
+} while (0)
+
 static char	*days[]={"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
 static char	*months[]={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
 
@@ -562,26 +572,13 @@ static int writebuf(http_session_t	*session, const char *buf, size_t len)
 
 static BOOL handle_crypt_call_except2(int status, http_session_t *session, int ignore, int ignore2, const char *file, int line)
 {
-	char	*estr = NULL;
-	int		sock = 0;
-
 	if (status == CRYPT_OK)
 		return TRUE;
 	if (status == ignore)
 		return FALSE;
 	if (status == ignore2)
 		return FALSE;
-	if (session != NULL) {
-		if (session->is_tls)
-			estr = get_crypt_error(session->tls_sess);
-		sock = session->socket;
-	}
-	if (estr) {
-		lprintf(LOG_WARNING, "%04d cryptlib error %d at %s:%d (%s)", sock, status, file, line, estr);
-		free_crypt_attrstr(estr);
-	}
-	else
-		lprintf(LOG_WARNING, "%04d cryptlib error %d at %s:%d", sock, status, file, line);
+	GCES(status, session, "popping data after timeout");
 	return FALSE;
 }
 
@@ -631,11 +628,12 @@ static int sess_sendbuf(http_session_t *session, const char *buf, size_t len, BO
 			case 1:
 				if (session->is_tls) {
 					status = cryptPushData(session->tls_sess, buf+sent, len-sent, &tls_sent);
+					GCES(status, session, "pushing data");
 					if (status == CRYPT_ERROR_TIMEOUT) {
 						tls_sent = 0;
 						if(!cryptStatusOK(status=cryptPopData(session->tls_sess, "", 0, &status))) {
 							if (status != CRYPT_ERROR_TIMEOUT && status != CRYPT_ERROR_PARAM2)
-								lprintf(LOG_NOTICE,"%04d Cryptlib error %d popping data after timeout",session->socket, status);
+								GCES(status, session, "popping data after timeout");
 						}
 						status = CRYPT_OK;
 					}
@@ -655,11 +653,13 @@ static int sess_sendbuf(http_session_t *session, const char *buf, size_t len, BO
 						else if(ERROR_VALUE==ECONNABORTED) 
 							lprintf(LOG_NOTICE,"%04d Connection aborted by peer on send",session->socket);
 #ifdef EPIPE
-						else if(ERROR_VALUE==EPIPE) 
+						else if(ERROR_VALUE==EPIPE)
 							lprintf(LOG_NOTICE,"%04d Unable to send to peer",session->socket);
 #endif
 						else
 							lprintf(LOG_WARNING,"%04d !ERROR %d sending on socket",session->socket,ERROR_VALUE);
+						if (failed)
+							*failed=TRUE;
 						return(sent);
 					}
 				}
@@ -3319,7 +3319,7 @@ static BOOL exec_js_webctrl(http_session_t* session, char *name, char* script, c
 
 		lprintf(LOG_DEBUG,"%04d JavaScript: Compiling %s", session->socket, name);
 		if(!JS_EvaluateScript(session->js_cx, session->js_glob, script, strlen(script), name, 1, &rval)) {
-			lprintf(LOG_ERR,"%04d !JavaScript FAILED to compile rewrite %s:%s"
+			lprintf(LOG_WARNING,"%04d !JavaScript FAILED to compile rewrite %s:%s"
 				,session->socket,name,script);
 			JS_ReportPendingException(session->js_cx);
 			JS_RemoveObjectRoot(session->js_cx, &session->js_glob);
@@ -6513,7 +6513,7 @@ const char* DLLCALL web_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.654 $", "%*s %s", revision);
+	sscanf("$Revision: 1.659 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
@@ -6652,7 +6652,8 @@ void DLLCALL web_server(void* arg)
 	char			compiler[32];
 	http_session_t *	session=NULL;
 	void			*acc_type;
-	char			ssl_estr[SSL_ESTR_LEN];
+	char			*ssl_estr;
+	int			lvl;
 
 	startup=(web_startup_t*)arg;
 
@@ -6785,9 +6786,12 @@ void DLLCALL web_server(void* arg)
 
 		if(startup->options&WEB_OPT_ALLOW_TLS) {
 			lprintf(LOG_DEBUG,"Loading/Creating TLS certificate");
-			get_ssl_cert(&scfg, ssl_estr);
-			if (scfg.tls_certificate == -1)
-				lprintf(LOG_ERR, "Error creating TLS certificate: %s", ssl_estr);
+			if (get_ssl_cert(&scfg, &ssl_estr, &lvl) == -1) {
+				if (ssl_estr) {
+					lprintf(lvl, "%s", ssl_estr);
+					free(ssl_estr);
+				}
+			}
 		}
 
 		iniFileName(mime_types_ini,sizeof(mime_types_ini),scfg.ctrl_dir,"mime_types.ini");
