a2193c9d7b18fdba1360c9ab6c5470c339458f03
Stephen Hurd <shurd@sasktel.net>
2018-03-14 05:37:48 +0000

    Toss invalid session connections into badlogin().

--- sbbs3/main.cpp
+++ sbbs3/main.cpp
@@ -1,6 +1,6 @@
 /* Synchronet terminal server thread and related functions */
 
-/* $Id: main.cpp,v 1.690 2018/03/13 05:10:21 rswindell Exp $ */
+/* $Id: main.cpp,v 1.691 2018/03/14 05:37:48 deuce Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -5444,6 +5444,15 @@ NO_SSH:
 							GCESS(i, client_socket, sbbs->ssh_session, "getting channel type");
 							if (tnamelen != 7 || strnicmp(tname, "session", 7)) {
 								lprintf(LOG_INFO, "%04d SSH active channel '%s' is not 'session', disconnecting.", client_socket, tname);
+								identity = get_crypt_attribute(sbbs->ssh_session, CRYPT_SESSINFO_USERNAME);
+								p = get_crypt_attribute(sbbs->ssh_session, CRYPT_SESSINFO_PASSWORD);
+								sbbs->badlogin(identity, p);
+								if (identity != NULL)
+									free_crypt_attrstr(identity);
+								identity = NULL;
+								if (p != NULL)
+									free_crypt_attrstr(p);
+								p = NULL;
 								// Fail because there's no session.
 								ssh_failed = 3;
 							}
