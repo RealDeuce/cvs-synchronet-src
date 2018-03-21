75dac5019672fffcdac05b99e8e7d46650e15502
deuce <deuce>
2018-03-21 17:41:54 +0000

    Document the cterm_version property to unbreak jsdocs.

--- sbbs3/mailsrvr.h
+++ sbbs3/mailsrvr.h
@@ -1,6 +1,6 @@
 /* Synchronet Mail (SMTP/POP3/SendMail) server */
 
-/* $Id: mailsrvr.h,v 1.86 2018/07/20 01:34:36 rswindell Exp $ */
+/* $Id: mailsrvr.h,v 1.84 2018/03/19 16:36:33 deuce Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -259,14 +259,6 @@ int mail_close_socket(SOCKET *sock, int *sess);
 }
 #endif
 
-#if defined(__GNUC__)   // passing an empty string to sockprintf() is expected/valid
-#pragma GCC diagnostic ignored "-Wformat-zero-length"
-#endif
-
-int sockprintf(SOCKET sock, const char* prot, int sess, char *fmt, ...)
-#if defined(__GNUC__)   // Catch printf-format errors 
-	__attribute__ ((format (printf, 3, 4)));
-#endif
-;
+int sockprintf(SOCKET sock, int sess, char *fmt, ...);
 
 #endif /* Don't add anything after this line */
