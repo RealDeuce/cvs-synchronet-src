59cc472f296da0d0273511a995434540b7fc4af4
rswindell <rswindell>
2018-04-17 22:57:28 +0000

    Extend USE_DOSEMU patch to drop file creation for non-native doors: Use drive mappings (e.g. D:, F:, G:, H:) rathe than absolute paths to node_dir, ctrl_dir, data_dir, and exec_dir in drop files. Also: fixed what appears to have been a copy/paste error in the node_dir and ctrl_dir mapping to Win32-shortnames for non-native doors on Windows.

--- sbbs3/showstat.c
+++ sbbs3/showstat.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include "sbbs_status.h"
 #include "dirwrap.h"
+#include "genwrap.h"
 
 void usage(void) {
 	printf("Usage: showstat <path>\n");
+}
+
+static struct termios old;
+void noecho(void)
+{
+	struct termios new = old;
+	new.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &new);
+}
+
+void restore(void)
+{
+	tcsetattr(STDIN_FILENO, TCSANOW, &old);
 }
 
 int main(int argc, char **argv)
@@ -14,8 +30,10 @@ int main(int argc, char **argv)
 	SOCKET sock;
 	struct sockaddr_un addr;
 	socklen_t addrlen;
-	char *buf[4096];
+	char buf[4096];
 	struct sbbs_status_msg *msg;
+	time_t t;
+	char *p;
 
 	if (argc != 2) {
 		usage();
@@ -26,6 +44,7 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	tcgetattr(STDIN_FILENO, &old);
 	sock = socket(PF_UNIX, SOCK_SEQPACKET, 0);
 	if (sock == INVALID_SOCKET)
 		return 1;
@@ -37,12 +56,31 @@ int main(int argc, char **argv)
 #else
 	addrlen = offsetof(struct sockaddr_un, un_addr.sun_path) + strlen(addr.sun_path) + 1;
 #endif
+	printf("UN: ");
+	fgets(buf, sizeof(buf), stdin);
+	truncnl(buf);
+	noecho();
+	p = strchr(buf, 0);
+	p++;
+	printf("PW: ");
+	fgets(p, sizeof(buf)-(p-buf), stdin);
+	truncnl(p);
+	p = strchr(p, 0);
+	p++;
+	printf("\nSY: ");
+	fgets(p, sizeof(buf)-(p-buf), stdin);
+	truncnl(p);
+	p = strchr(p, 0);
+	p++;
+	restore();
+	puts("");
+
 	if (connect(sock, (void *)&addr, addrlen) != 0) {
 		fprintf(stderr, "Unable to connect!\n");
 		return 1;
 	}
 
-	send(sock, buf, 1, 0);
+	send(sock, buf, p-buf, 0);
 	while (sock != -1) {
 		if (recv(sock, buf, sizeof(buf), 0) <= 0) {
 			close(sock);
@@ -69,13 +107,36 @@ int main(int argc, char **argv)
 			case SERVICE_WEB:
 				printf("WEB: ");
 				break;
+			case SERVICE_STATUS:
+				printf("STATUS: ");
+				break;
+			default:
+				printf("UNKNOWN SERVICE %" PRIu32 " ", msg->hdr.service);
+				break;
 		}
 		switch(msg->hdr.type) {
 			case STATUS_CLIENTS:
 				printf("%d clients\n", msg->msg.clients.active);
 				break;
 			case STATUS_CLIENT_ON:
-				printf("got client_on\n");
+				t = msg->msg.client_on.client.time; /* sigh */
+				msg->msg.client_on.client.protocol = msg->msg.client_on.strdata;
+				msg->msg.client_on.client.user = strchr(msg->msg.client_on.strdata, 0)+1;
+				if (msg->msg.client_on.on) {
+					printf("Client %s%s: sock: %d\n addr: %s\n host: %s\n port: %" PRIu16 "\n %s at %s via %s\n",
+						msg->msg.client_on.on ? "on" : "off",
+						msg->msg.client_on.update ? " update" : "",
+						msg->msg.client_on.sock,
+						msg->msg.client_on.client.addr,
+						msg->msg.client_on.client.host,
+						msg->msg.client_on.client.port,
+						msg->msg.client_on.client.user,
+						ctime(&t),
+						msg->msg.client_on.client.protocol);
+				}
+				else {
+					printf("Client off: sock: %d\n", msg->msg.client_on.sock);
+				}
 				break;
 			case STATUS_ERRORMSG:
 				printf("%d - %s\n", msg->msg.errormsg.level, msg->msg.errormsg.msg);
@@ -112,7 +173,7 @@ int main(int argc, char **argv)
 				printf("thread %s (%d)\n", msg->msg.thread_up.up ? "up" : "down", msg->msg.thread_up.setuid);
 				break;
 			default:
-				printf("!Unhandled type: %d\b", msg->hdr.type);
+				printf("!Unhandled type: %" PRIu32 "\n", msg->hdr.type);
 				break;
 		}
 	}
