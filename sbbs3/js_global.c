5c898b9b0f6340804ecc150dac1f5550a82ef249
rswindell <rswindell>
2018-06-10 08:53:13 +0000

    JS bbs object changes: - finduser() method no longer prints "Unknown user" upon failure (the calling   script can do this if/when it wants). - read_mail() method now accepts an optional 3rd argumnet (loadmail_mode)   which may be used to load *only* un-read mail (for example), see LM_* in   sbbsdefs.js
    Pass the lm_mode as a 4th argument to any readmail mod (if one is installed) -
    this correlates with the LM_* mode flag values (0 is normal/usual).
    
    File attachments can also be in a file/####.out directory (e.g. outbound
    Internet email attachments), so try deleting from there too when deleting
    mail with attachments.

--- sbbs3/js_global.c
+++ sbbs3/js_global.c
@@ -1,6 +1,6 @@
 /* Synchronet JavaScript "global" object properties/methods for all servers */
 
-/* $Id: js_global.c,v 1.377 2019/01/20 05:25:19 rswindell Exp $ */
+/* $Id: js_global.c,v 1.374 2018/04/06 02:47:06 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -398,11 +398,11 @@ js_load(JSContext *cx, uintN argc, jsval *arglist)
 		if (JS_HasProperty(cx, obj, "console", &success) && success)
 			js_CreateConsoleObject(bg->cx, bg->obj);
 		if (JS_HasProperty(cx, obj, "stdin", &success) && success)
-			js_CreateFileObject(bg->cx, bg->obj, "stdin", STDIN_FILENO, "r");
+			js_CreateFileObject(bg->cx, bg->obj, "stdin", stdin);
 		if (JS_HasProperty(cx, obj, "stdout", &success) && success)
-			js_CreateFileObject(bg->cx, bg->obj, "stdout", STDOUT_FILENO, "w");
+			js_CreateFileObject(bg->cx, bg->obj, "stdout", stdout);
 		if (JS_HasProperty(cx, obj, "stderr", &success) && success)
-			js_CreateFileObject(bg->cx, bg->obj, "stderr", STDERR_FILENO, "w");
+			js_CreateFileObject(bg->cx, bg->obj, "stderr", stderr);
 		JS_SetContextPrivate(bg->cx, bg);
 
 		exec_cx = bg->cx;
@@ -2187,8 +2187,8 @@ js_html_encode(JSContext *cx, uintN argc, jsval *arglist)
 						outbuf[j++]='\r';
 						hpos=0;
 						break;
-					case 'Z':	/* EOF */
-						outbuf[j++] = 0;
+					case 'Z':
+						outbuf[j++]=0;
 						break;
 					case 'A':
 					default:
@@ -4182,19 +4182,19 @@ static jsSyncMethodSpec js_global_functions[] = {
 	,310
 	},		
 	{"file_attrib",		js_fattr,			1,	JSTYPE_NUMBER,	JSDOCSTR("path/filename")
-	,JSDOCSTR("get a file's permissions/attributes. Returns <tt>-1</tt> if the <i>path/filename</i> does not exist.")
+	,JSDOCSTR("get a file's permissions/attributes")
 	,310
 	},		
 	{"file_date",		js_fdate,			1,	JSTYPE_NUMBER,	JSDOCSTR("path/filename")
-	,JSDOCSTR("get a file's last modified date/time (in time_t format). Returns <tt>-1</tt> if the <i>path/filename</i> does not exist.")
+	,JSDOCSTR("get a file's last modified date/time (in time_t format)")
 	,310
 	},
 	{"file_cdate",		js_fcdate,			1,	JSTYPE_NUMBER,	JSDOCSTR("path/filename")
-	,JSDOCSTR("get a file's creation date/time (in time_t format). Returns <tt>-1</tt> if the <i>path/filename</i> does not exist.")
+	,JSDOCSTR("get a file's creation date/time (in time_t format)")
 	,317
 	},
 	{"file_size",		js_flength,			1,	JSTYPE_NUMBER,	JSDOCSTR("path/filename")
-	,JSDOCSTR("get a file's length (in bytes). Returns <tt>-1</tt> if the <i>path/filename</i> does not exist.")
+	,JSDOCSTR("get a file's length (in bytes)")
 	,310
 	},
 	{"file_utime",		js_utime,			3,	JSTYPE_BOOLEAN,	JSDOCSTR("path/filename [,access_time=<i>current</i>] [,mod_time=<i>current</i>]")
