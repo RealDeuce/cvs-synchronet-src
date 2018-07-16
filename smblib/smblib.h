badeaba08f25c79974d5e06dbec543abccffa515
rswindell <rswindell>
2018-07-16 05:42:08 +0000

    Rename this private_t.status to private_t.smb_result, to avoid confusion with smb_t.status.

--- smblib/smblib.h
+++ smblib/smblib.h
@@ -1,6 +1,6 @@
 /* Synchronet message base (SMB) library function prototypes */
 
-/* $Id: smblib.h,v 1.88 2019/03/22 21:29:12 rswindell Exp $ */
+/* $Id: smblib.h,v 1.86 2018/07/08 03:50:33 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -45,7 +45,7 @@
 
 #ifdef _WIN32
 	#ifdef __BORLANDC__
-		#define SMBCALL
+		#define SMBCALL __stdcall
 	#else
 		#define SMBCALL
 	#endif
@@ -131,7 +131,6 @@ extern "C" {
 SMBEXPORT int 		SMBCALL smb_ver(void);
 SMBEXPORT char*		SMBCALL smb_lib_ver(void);
 SMBEXPORT int 		SMBCALL smb_open(smb_t* smb);
-SMBEXPORT int		SMBCALL smb_open_index(smb_t* smb);
 SMBEXPORT void		SMBCALL smb_close(smb_t* smb);
 SMBEXPORT int 		SMBCALL smb_initsmbhdr(smb_t* smb);
 SMBEXPORT int 		SMBCALL smb_create(smb_t* smb);
