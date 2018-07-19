fb37c65247a8434e30e2e8849ad210d7b1b7062d
rswindell <rswindell>
2018-07-19 04:13:47 +0000

    Added a "MinFreeDiskSpace" setting to the root section of sbbsecho.ini (defaults to 10 megabytes, "10M"). When SBBSecho starts up, if this minimum amount of free space is not available in any of the following directories, SBBSecho will display an error and immediately exit: - data directory (in SCFG) - logs directory (in SCFG) - netmail directory (in SCFG) - outbound directory (from sbbsecho.ini) - temp directory - each linked-node's outbox, if configured
    If the sysop runs out of disk space for some reason, at least SBBSecho
    won't contribute to the problem.

--- sbbs3/con_hi.cpp
+++ sbbs3/con_hi.cpp
@@ -1,7 +1,7 @@
 /* Synchronet hi-level console routines */
 // vi: tabstop=4
 
-/* $Id: con_hi.cpp,v 1.27 2018/08/03 06:18:55 rswindell Exp $ */
+/* $Id: con_hi.cpp,v 1.26 2018/07/01 09:40:27 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -169,7 +169,8 @@ bool sbbs_t::chksyspass(const char* sys_pw)
 			SAFEPRINTF3(str2,"%s #%u System password attempt: '%s'"
 				,useron.alias,useron.number,str);
 		else
-			SAFECOPY(str2,"System password verification failure");
+			SAFEPRINTF2(str2,"%s #%u System password verification failure"
+				,useron.alias,useron.number);
 		logline(LOG_NOTICE,"S!",str2);
 		return(false); 
 	}
