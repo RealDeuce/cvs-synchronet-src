f47821a07e25cf8ae0e4e9453e768eec7c940bef
rswindell <rswindell>
2018-07-20 01:34:36 +0000

    Cosmetic/helpful improvements to the log output (a lot of search and replace). The "official" log line format is (as of right now at least): "[socket] [protocol] [user/host-ID] [! if error]message" Hopefully I can remember that when I add new log messages.
    Also setting the protocol to SMTPS, POP3S, SEND/TLS, when appropriate and
    passing it around to pretty much any function that can log a message.
    Added debug-level log output when the mail sever is sending message body text
    (every 100 lines).

--- sbbs3/sbbs_ini.c
+++ sbbs3/sbbs_ini.c
@@ -1,6 +1,6 @@
 /* Synchronet initialization (.ini) file routines */
 
-/* $Id: sbbs_ini.c,v 1.169 2019/03/24 07:11:47 rswindell Exp $ */
+/* $Id: sbbs_ini.c,v 1.163 2018/03/10 00:01:50 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -47,8 +47,8 @@ static const char*  strAutoStart="AutoStart";
 static const char*  strCtrlDirectory="CtrlDirectory";
 static const char*  strTempDirectory="TempDirectory";
 static const char*	strOptions="Options";
-static const char*	strOutgoing4="OutboundInterface";
-static const char*	strOutgoing6="OutboundV6Interface";
+static const char*	strOutgoing4="OutgoingV4";
+static const char*	strOutgoing6="OutgoingV6";
 static const char*	strInterfaces="Interface";
 static const char*	strPort="Port";
 static const char*	strMaxClients="MaxClients";
@@ -333,11 +333,6 @@ void sbbs_read_ini(
 		bbs->rlogin_interfaces
 			=iniGetStringList(list,section,"RLoginInterface",",",global_interfaces);
 
-		bbs->pet40_port
-			=iniGetShortInt(list,section,"Pet40Port",64);
-		bbs->pet80_port
-			=iniGetShortInt(list,section,"Pet80Port",128);
-
 		bbs->ssh_port
 			=iniGetShortInt(list,section,"SSHPort",22);
 		bbs->ssh_connect_timeout
@@ -404,7 +399,7 @@ void sbbs_read_ini(
 			=iniGetLogLevel(list,section,strLogLevel,global->log_level);
 		bbs->options
 			=iniGetBitField(list,section,strOptions,bbs_options
-				,BBS_OPT_XTRN_MINIMIZED);
+				,BBS_OPT_XTRN_MINIMIZED|BBS_OPT_SYSOP_AVAILABLE);
 
 		bbs->bind_retry_count=iniGetInteger(list,section,strBindRetryCount,global->bind_retry_count);
 		bbs->bind_retry_delay=iniGetInteger(list,section,strBindRetryDelay,global->bind_retry_delay);
@@ -692,6 +687,8 @@ void sbbs_read_ini(
 			=iniGetStringList(list,section,"CGIExtensions", "," ,".cgi");
 		SAFECOPY(web->ssjs_ext
 			,iniGetString(list,section,"JavaScriptExtension",".ssjs",value));
+		SAFECOPY(web->js_ext
+			,iniGetString(list,section,"EmbJavaScriptExtension",".bbs",value));
 
 		web->max_cgi_inactivity
 			=iniGetShortInt(list,section,"MaxCgiInactivity",WEB_DEFAULT_MAX_CGI_INACTIVITY);	/* seconds */
@@ -745,9 +742,7 @@ BOOL sbbs_write_ini(
 
 	memset(&style, 0, sizeof(style));
 	style.key_prefix = "\t";
-	style.section_separator = "";
-	style.value_separator = " = ";
-	style.bit_separator = " | ";
+    style.bit_separator = " | ";
 
 	if((list=iniReadFile(fp))==NULL)
 		return(FALSE);
@@ -803,11 +798,6 @@ BOOL sbbs_write_ini(
 		else if(!iniSetStringList(lp,section,"RLoginInterface", ",", bbs->rlogin_interfaces,&style))
 			break;
 		if(!iniSetShortInt(lp,section,"RLoginPort",bbs->rlogin_port,&style))
-			break;
-
-		if(!iniSetShortInt(lp,section,"Pet40Port",bbs->pet40_port,&style))
-			break;
-		if(!iniSetShortInt(lp,section,"Pet80Port",bbs->pet80_port,&style))
 			break;
 
 		if(strListCmp(bbs->ssh_interfaces, global->interfaces)==0)
