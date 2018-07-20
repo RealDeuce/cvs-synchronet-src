d8c7efa30168a1e3fb6adf32e99f62106e3627a1
rswindell <rswindell>
2018-07-20 01:34:36 +0000

    Cosmetic/helpful improvements to the log output (a lot of search and replace). The "official" log line format is (as of right now at least): "[socket] [protocol] [user/host-ID] [! if error]message" Hopefully I can remember that when I add new log messages.
    Also setting the protocol to SMTPS, POP3S, SEND/TLS, when appropriate and
    passing it around to pretty much any function that can log a message.
    Added debug-level log output when the mail sever is sending message body text
    (every 100 lines).

--- sbbs3/mailsrvr.h
+++ sbbs3/mailsrvr.h
@@ -1,6 +1,6 @@
 /* Synchronet Mail (SMTP/POP3/SendMail) server */
 
-/* $Id: mailsrvr.h,v 1.85 2018/04/06 02:11:52 rswindell Exp $ */
+/* $Id: mailsrvr.h,v 1.86 2018/07/20 01:34:36 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -263,7 +263,7 @@ int mail_close_socket(SOCKET *sock, int *sess);
 #pragma GCC diagnostic ignored "-Wformat-zero-length"
 #endif
 
-int sockprintf(SOCKET sock, int sess, char *fmt, ...)
+int sockprintf(SOCKET sock, const char* prot, int sess, char *fmt, ...)
 #if defined(__GNUC__)   // Catch printf-format errors 
 	__attribute__ ((format (printf, 3, 4)));
 #endif
