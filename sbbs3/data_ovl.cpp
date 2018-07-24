a1ab38fef0921757a0fb92170f268e34f2c854fb
rswindell <rswindell>
2018-07-24 23:55:29 +0000

    Even more details (number of subs) in the "read message ... pointers" debug log msg.

--- sbbs3/data_ovl.cpp
+++ sbbs3/data_ovl.cpp
@@ -1,6 +1,6 @@
 /* Synchronet hi-level data access routines */
 
-/* $Id: data_ovl.cpp,v 1.25 2018/07/24 11:37:38 rswindell Exp $ */
+/* $Id: data_ovl.cpp,v 1.26 2018/07/24 23:55:29 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -52,7 +52,8 @@ void sbbs_t::getmsgptrs()
 		return;
 	msclock_t start = msclock();
 	::getmsgptrs(&cfg,&useron,subscan,online == ON_REMOTE ? ProgressLoadingMsgPtrs : NULL,this);
-	lprintf(LOG_DEBUG, "Node %d %s read message configuration/pointers (%lu ms)", cfg.node_num, useron.alias, msclock() - start);
+	lprintf(LOG_DEBUG, "Node %d %s read %u message scan cfg/pointers in %lu ms"
+		, cfg.node_num, useron.alias, cfg.total_subs, msclock() - start);
 	if(online == ON_REMOTE)
 		bputs(text[LoadedMsgPtrs]);
 }
