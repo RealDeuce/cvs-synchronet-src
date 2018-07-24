74f232933227c4d172c3f8b33352a1eab96bfa87
rswindell <rswindell>
2018-07-24 08:41:22 +0000

    Remove/deprecate trailing references to the "SYSOP_AVAILABLE" startup option - the sysop chat availability is now controlled via semaphore file (ctrl/sysavail.chat).

--- sbbs3/execmisc.cpp
+++ sbbs3/execmisc.cpp
@@ -2,13 +2,13 @@
 
 /* Synchronet miscellaneous command shell/module routines */
 
-/* $Id: execmisc.cpp,v 1.54 2011/10/19 06:53:03 rswindell Exp $ */
+/* $Id: execmisc.cpp,v 1.55 2018/07/24 01:11:07 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2011 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
