d7881bc19b6e341cb69ac6535974ec31d6ba95db
rswindell <rswindell>
2018-07-24 05:15:45 +0000

    Singificantlly improve message scan pointer initialization (e.g. for new users) and adjusting scan ptrs by date or set to "Last msg" while online: - speed (on my system, reduced from 12 to < 2 seconds)   This involved 2 enhancements: check msgbase index file timestamp (fast)   before opening any files. When opening the msgbase, just open the index   file (no other msgbase files) - required new smblib function: smb_open_index. - fixed off-by-one when setting by date (when setting the new-scan ptr to   Jan-1-2018, you want that first message posted on/after Jan-1-2018 to be   found as "new", not the first message posted *after* that date. - re-added the fixmsgptrs() call in putmsgptrs(), inadvertently removed   in the .ixb -> .ini ptr storage update in rev 1.192 of userdat.c.

--- sbbs3/atcodes.cpp
+++ sbbs3/atcodes.cpp
@@ -1,6 +1,6 @@
 /* Synchronet "@code" functions */
 
-/* $Id: atcodes.cpp,v 1.84 2018/10/25 21:25:53 rswindell Exp $ */
+/* $Id: atcodes.cpp,v 1.80 2018/03/17 02:23:33 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -101,7 +101,7 @@ int sbbs_t::show_atcode(const char *instr)
 	} else if(zero_padded) {
 		int vlen = strlen(cp);
 		if(vlen < disp_len)
-			bprintf("%-.*s%s", (int)(disp_len - strlen(cp)), "0000000000", cp);
+			bprintf("%-.*s%s", disp_len - strlen(cp), "0000000000", cp);
 		else
 			bprintf("%.*s", disp_len, cp);
 	} else
@@ -205,17 +205,6 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 		return(str);
 	}
 
-	if(!strcmp(sp,"COLS")) {
-		safe_snprintf(str,maxlen,"%lu",cols);
-		return(str);
-	}
-	if(!strcmp(sp,"ROWS")) {
-		safe_snprintf(str,maxlen,"%lu",rows);
-		return(str);
-	}
-	if(!strcmp(sp,"TERM"))
-		return(terminal);
-
 	if(!strcmp(sp,"CONN"))
 		return(connection);
 
@@ -310,7 +299,7 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 		|| !strcmp(sp,"LASTCALLERSYSTEM"))
 		return(lastuseron);
 
-	if(!strcmp(sp,"CLS") || !strcmp(sp,"CLEAR")) {
+	if(!strcmp(sp,"CLS")) {
 		CLS;
 		return(nulstr);
 	}
@@ -644,11 +633,6 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 
 	if(!strncmp(sp,"MENU:",5)) {
 		menu(sp+5);
-		return(nulstr);
-	}
-
-	if(!strncmp(sp,"CONDMENU:",9)) {
-		menu(sp+9, P_NOERROR);
 		return(nulstr);
 	}
 
@@ -1045,10 +1029,6 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 	}
 	if(!strcmp(sp,"MSG_SUBJECT") && current_msg!=NULL)
 		return(current_msg->subj==NULL ? nulstr : current_msg->subj);
-	if(!strcmp(sp,"MSG_SUMMARY") && current_msg!=NULL)
-		return(current_msg->summary==NULL ? nulstr : current_msg->summary);
-	if(!strcmp(sp,"MSG_TAGS") && current_msg!=NULL)
-		return(current_msg->tags==NULL ? nulstr : current_msg->tags);
 	if(!strcmp(sp,"MSG_DATE") && current_msg!=NULL)
 		return(timestr(current_msg->hdr.when_written.time));
 	if(!strcmp(sp,"MSG_AGE") && current_msg!=NULL)
