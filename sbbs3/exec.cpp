b987a6ef427c33dcced9a6a72bcc6636599cd586
rswindell <rswindell>
2018-07-29 01:01:07 +0000

    More status pop-ups when exiting SCFG and saving changes.

--- sbbs3/exec.cpp
+++ sbbs3/exec.cpp
@@ -1,9 +1,8 @@
 /* exec.cpp */
-// vi: tabstop=4
 
 /* Synchronet command shell/module interpretter */
 
-/* $Id: exec.cpp,v 1.110 2019/05/28 08:49:00 rswindell Exp $ */
+/* $Id: exec.cpp,v 1.105 2016/11/16 07:58:21 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -40,7 +39,7 @@
 #include "cmdshell.h"
 #include "js_request.h"
 
-char ** sbbs_t::getstrvar(csi_t *bin, uint32_t name)
+char ** sbbs_t::getstrvar(csi_t *bin, int32_t name)
 {
 	uint i;
 
@@ -125,7 +124,7 @@ char ** sbbs_t::getstrvar(csi_t *bin, uint32_t name)
 	return((char **)&sysvar_p[sysvar_pi++]);
 }
 
-int32_t * sbbs_t::getintvar(csi_t *bin, uint32_t name)
+int32_t * sbbs_t::getintvar(csi_t *bin, int32_t name)
 {
 	uint i;
 
@@ -562,6 +561,13 @@ js_OperationCallback(JSContext *cx)
 	return ret;
 }
 
+static const char* js_ext(const char* fname)
+{
+	if(getfext(fname)==NULL)
+		return(".js");
+	return("");
+}
+
 long sbbs_t::js_execfile(const char *cmd, const char* startup_dir, JSObject* scope)
 {
 	char*		p;
@@ -593,15 +599,12 @@ long sbbs_t::js_execfile(const char *cmd, const char* startup_dir, JSObject* sco
 
 	path[0]=0;
 	if(strcspn(fname,"/\\")==strlen(fname)) {
-		const char* js_ext = "";
-		if(getfext(fname) == NULL)
-			js_ext = ".js";
 		if(startup_dir!=NULL && *startup_dir)
-			SAFEPRINTF3(path,"%s%s%s",startup_dir,fname,js_ext);
+			SAFEPRINTF3(path,"%s%s%s",startup_dir,fname,js_ext(fname));
 		if(path[0]==0 || !fexistcase(path)) {
-			SAFEPRINTF3(path,"%s%s%s",cfg.mods_dir,fname,js_ext);
+			SAFEPRINTF3(path,"%s%s%s",cfg.mods_dir,fname,js_ext(fname));
 			if(cfg.mods_dir[0]==0 || !fexistcase(path))
-				SAFEPRINTF3(path,"%s%s%s",cfg.exec_dir,fname,js_ext);
+				SAFEPRINTF3(path,"%s%s%s",cfg.exec_dir,fname,js_ext(fname));
 		}
 	} else
 		SAFECOPY(path,fname);
@@ -738,8 +741,6 @@ long sbbs_t::exec_bin(const char *cmdline, csi_t *csi, const char* startup_dir)
 	int 	file;
     csi_t   bin;
 
-	if(cmdline == NULL || *cmdline == 0)
-		return -33;
 	SAFECOPY(mod,cmdline);
 	p=mod;
 	FIND_CHAR(p,' ');
@@ -789,12 +790,7 @@ long sbbs_t::exec_bin(const char *cmdline, csi_t *csi, const char* startup_dir)
 
 	memcpy(&bin,csi,sizeof(csi_t));
 	clearvars(&bin);
-	bin.length = filelength(file);
-	if(bin.length < 1) {
-		close(file);
-		errormsg(WHERE, ERR_LEN, str, bin.length);
-		return -1;
-	}
+	bin.length=(uint32_t)filelength(file);
 	if((bin.cs=(uchar *)malloc(bin.length))==NULL) {
 		close(file);
 		errormsg(WHERE,ERR_ALLOC,str,bin.length);
