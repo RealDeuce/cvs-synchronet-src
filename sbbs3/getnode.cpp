b31c8b27753f98d5e3eda00d3b05a0858b8bebbe
rswindell <rswindell>
2018-07-29 02:05:01 +0000

    Cosmetic changes only (popups, mostly).

--- sbbs3/getnode.cpp
+++ sbbs3/getnode.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet node information retrieval functions */
 
-/* $Id: getnode.cpp,v 1.52 2018/10/09 08:33:04 rswindell Exp $ */
+/* $Id: getnode.cpp,v 1.48 2018/07/26 06:22:27 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -115,7 +115,7 @@ int sbbs_t::getnodedat(uint number, node_t *node, bool lockit)
 /* Assumes that getnodedat(node_num,&thisnode,0) was called right before it */
 /* is called.  																*/
 /****************************************************************************/
-void sbbs_t::nodesync(bool clearline)
+void sbbs_t::nodesync()
 {
 	char	str[256],today[32];
 	int		atr=curatr;
@@ -162,9 +162,9 @@ void sbbs_t::nodesync(bool clearline)
 		}
 		if(!(sys_status&SS_MOFF)) {
 			if(thisnode.misc&NODE_MSGW)
-				getsmsg(useron.number, clearline); 	/* getsmsg clears MSGW flag */
+				getsmsg(useron.number); 	/* getsmsg clears MSGW flag */
 			if(thisnode.misc&NODE_NMSG)
-				getnmsg(clearline);					/* getnmsg clears NMSG flag */
+				getnmsg();					/* getnmsg clears NMSG flag */
 		}
 	}
 
@@ -208,7 +208,7 @@ void sbbs_t::nodesync(bool clearline)
 /****************************************************************************/
 /* Prints short messages waiting for this node, if any...                   */
 /****************************************************************************/
-int sbbs_t::getnmsg(bool clearline)
+int sbbs_t::getnmsg()
 {
 	char	str[MAX_PATH+1], *buf;
 	int		file;
@@ -248,10 +248,10 @@ int sbbs_t::getnmsg(bool clearline)
 	close(file);
 	buf[length]=0;
 
-	if(clearline)
-		this->clearline();
-	else if(column)
+	if(thisnode.action==NODE_MAIN || thisnode.action==NODE_XFER
+		|| sys_status&SS_IN_CTRLP) {
 		CRLF; 
+	}
 	putmsg(buf,P_NOATCODES);
 	free(buf);
 
@@ -310,9 +310,9 @@ int sbbs_t::getnodeext(uint number, char *ext)
 
 /****************************************************************************/
 /* Prints short messages waiting for 'usernumber', if any...                */
-/* then truncates the file.                                                 */
+/* then deletes them.                                                       */
 /****************************************************************************/
-int sbbs_t::getsmsg(int usernumber, bool clearline)
+int sbbs_t::getsmsg(int usernumber)
 {
 	char	str[MAX_PATH+1], *buf;
     int		file;
@@ -321,8 +321,6 @@ int sbbs_t::getsmsg(int usernumber, bool clearline)
 	int		i;
 
 	for(i=1;i<=cfg.sys_nodes;i++) {	/* clear msg waiting flag */
-		if(getnodedat(i,&node,false) != 0 || node.useron != usernumber)
-			continue;
 		if(getnodedat(i,&node,true)==0) {
 			if(node.useron==usernumber
 					&& (node.status==NODE_INUSE || node.status==NODE_QUIET)
@@ -355,12 +353,10 @@ int sbbs_t::getsmsg(int usernumber, bool clearline)
 	close(file);
 	buf[length]=0;
 	getnodedat(cfg.node_num,&thisnode,0);
-	if(clearline)
-		this->clearline();
-	else
-		if(column)
-			CRLF;
-	strip_invalid_attr(buf);
+	if(thisnode.action==NODE_MAIN || thisnode.action==NODE_XFER
+		|| sys_status&SS_IN_CTRLP) {
+		CRLF; 
+	}
 	putmsg(buf,P_NOATCODES);
 	free(buf);
 
@@ -380,7 +376,7 @@ int sbbs_t::whos_online(bool listself)
 	CRLF;
 	bputs(text[NodeLstHdr]);
 	for(j=0,i=1;i<=cfg.sys_nodes && i<=cfg.sys_lastnode;i++) {
-		getnodedat(i,&node,false);
+		getnodedat(i,&node,0);
 		if(i==cfg.node_num) {
 			if(listself)
 				printnodedat(i,&node);
@@ -405,7 +401,7 @@ void sbbs_t::nodelist(void)
 	CRLF;
 	bputs(text[NodeLstHdr]);
 	for(int i=1;i<=cfg.sys_nodes && i<=cfg.sys_lastnode;i++) {
-		getnodedat(i,&node,false);
+		getnodedat(i,&node,0);
 		printnodedat(i,&node); 
 	}
 }
