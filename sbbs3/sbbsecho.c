7bda5d95d7d9d3796ab8ba73f722d73a536492f4
rswindell <rswindell>
2018-07-30 20:43:15 +0000

    Fix crash (segfault) reported in AreaFix handler for "-ALL", reported by Ragnarok@DOCKSUD.

--- sbbs3/sbbsecho.c
+++ sbbs3/sbbsecho.c
@@ -1,6 +1,6 @@
 /* Synchronet FidoNet EchoMail Scanning/Tossing and NetMail Tossing Utility */
 
-/* $Id: sbbsecho.c,v 3.88 2018/07/29 00:27:14 rswindell Exp $ */
+/* $Id: sbbsecho.c,v 3.89 2018/07/30 20:43:15 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -1735,7 +1735,7 @@ void alter_areas(str_list_t add_area, str_list_t del_area, fidoaddr_t addr, cons
 		fprintf(afileout,"%s\n",fields);	/* No match so write back line */
 	}
 	fclose(afilein);
-	if(nomatch || (add_area[0] != NULL && stricmp(add_area[0],"+ALL") == 0)) {
+	if(nomatch || (add_count && stricmp(add_area[0],"+ALL") == 0)) {
 		nodecfg_t* nodecfg=findnodecfg(&cfg, addr,0);
 		if(nodecfg != NULL) {
 			for(j=0;j<cfg.listcfgs;j++) {
@@ -1810,7 +1810,7 @@ void alter_areas(str_list_t add_area, str_list_t del_area, fidoaddr_t addr, cons
 			} 
 		} 
 	}
-	if(strListCount(add_area) && stricmp(add_area[0],"+ALL")) {
+	if(add_count && stricmp(add_area[0],"+ALL")) {
 		for(u=0;add_area[u]!=NULL;u++)
 			if(add_area[u][0]) {
 				fprintf(nmfile,"%s not found.\r\n",add_area[u]); 
@@ -5978,7 +5978,7 @@ int main(int argc, char **argv)
 		memset(&smb[i],0,sizeof(smb_t));
 	memset(&cfg,0,sizeof(cfg));
 
-	sscanf("$Revision: 3.88 $", "%*s %s", revision);
+	sscanf("$Revision: 3.89 $", "%*s %s", revision);
 
 	DESCRIBE_COMPILER(compiler);
 
