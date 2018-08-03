e48e42400e60da8e44e15436e3e9d43f81bb57a8
rswindell <rswindell>
2018-08-03 06:37:20 +0000

    Don't delete netmail file attachments: just because the netmail was exported to .msg file doesn't mean you should delete the file attachment! The .msg file will still be pointing to that file and it needs to be there. SBBSecho was silently failing to delete file attachments on my system, so I didn't see this problem. Oops. Anyway, this explains Al's problem with netmail attachments.

--- sbbs3/msgtoqwk.cpp
+++ sbbs3/msgtoqwk.cpp
@@ -1,6 +1,6 @@
 /* Synchronet message to QWK format conversion routine */
 
-/* $Id: msgtoqwk.cpp,v 1.54 2019/01/11 11:29:38 rswindell Exp $ */
+/* $Id: msgtoqwk.cpp,v 1.50 2017/11/24 23:35:20 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -204,10 +204,6 @@ ulong sbbs_t::msgtoqwk(smbmsg_t* msg, FILE *qwk_fp, long mode, uint subnum
 		/* Synchronet */
 		if((p=(char*)smb_get_hfield(msg,hfield_type=SMB_EDITOR,NULL))!=NULL)	
 			fprintf(hdrs,"%s: %s\n", smb_hfieldtype(hfield_type), p);
-		if((p=(char*)smb_get_hfield(msg,hfield_type=SMB_COLUMNS,NULL))!=NULL)	
-			fprintf(hdrs,"%s: %u\n", smb_hfieldtype(hfield_type), *(uint8_t*)p);
-		if((p=(char*)smb_get_hfield(msg,hfield_type=SMB_TAGS,NULL))!=NULL)	
-			fprintf(hdrs,"%s: %s\n", smb_hfieldtype(hfield_type), p);
 
 		/* USENET */
 		if((p=(char*)smb_get_hfield(msg,hfield_type=USENETPATH,NULL))!=NULL)
@@ -381,7 +377,7 @@ ulong sbbs_t::msgtoqwk(smbmsg_t* msg, FILE *qwk_fp, long mode, uint subnum
 
 			if(ch==CTRL_A) {
 				ch=buf[++l];
-				if(ch==0 || ch=='Z')	/* EOF */
+				if(ch==0 || toupper(ch)=='Z')		/* EOF */
 					break;
 				if((asc=ctrl_a_to_ascii_char(ch)) != 0) {
 					fputc(asc,qwk_fp);
