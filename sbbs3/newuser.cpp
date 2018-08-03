51d2257b700d34fd0c9bcd23d2e8922a0f0442c0
rswindell <rswindell>
2018-08-03 06:27:06 +0000

    Fix conditions where event log messages could still be sent to the terminal server log output/window. Also added comment about an observed exception upon shutdown with regards to the node_threads_running counter (protected int) being accessed after destruction.

--- sbbs3/newuser.cpp
+++ sbbs3/newuser.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet new user routine */
 
-/* $Id: newuser.cpp,v 1.71 2015/10/31 00:09:09 rswindell Exp $ */
+/* $Id: newuser.cpp,v 1.73 2018/08/03 06:18:56 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -373,20 +373,18 @@ BOOL sbbs_t::newuser()
 
 		c=0;
 		while(online) {
-			bprintf(text[NewUserPasswordVerify]);
+			bputs(text[NewUserPasswordVerify]);
 			console|=CON_R_ECHOX;
 			str[0]=0;
 			getstr(str,LEN_PASS*2,K_UPPER);
 			console&=~(CON_R_ECHOX|CON_L_ECHOX);
 			if(!strcmp(str,useron.pass)) break;
 			if(cfg.sys_misc&SM_ECHO_PW) 
-				SAFEPRINTF3(tmp,"%s FAILED Password verification: '%s' instead of '%s'"
-					,useron.alias
+				SAFEPRINTF2(tmp,"FAILED Password verification: '%s' instead of '%s'"
 					,str
 					,useron.pass);
 			else
-				SAFEPRINTF(tmp,"%s FAILED Password verification"
-					,useron.alias);
+				SAFECOPY(tmp,"FAILED Password verification");
 			logline(LOG_NOTICE,nulstr,tmp);
 			if(++c==4) {
 				logline(LOG_NOTICE,"N!","Couldn't figure out password.");
@@ -404,7 +402,7 @@ BOOL sbbs_t::newuser()
 		getstr(str,50,K_UPPER);
 		if(strcmp(str,cfg.new_magic)) {
 			bputs(text[FailedMagicWord]);
-			SAFEPRINTF2(tmp,"%s failed magic word: '%s'",useron.alias,str);
+			SAFEPRINTF(tmp,"failed magic word: '%s'",str);
 			logline("N!",tmp);
 			hangup(); 
 		}
