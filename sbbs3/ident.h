3252f81b3c0a8ef6e46f905a7d4c9e46d72d04dc
rswindell <rswindell>
2018-08-03 05:48:00 +0000

    Log an error if one or more file attachments could not be deleted when attempted.

--- sbbs3/ident.h
+++ sbbs3/ident.h
@@ -2,13 +2,13 @@
 
 /* Synchronet Indentification (RFC1413) functions */
 
-/* $Id: ident.h,v 1.5 2015/08/20 05:19:41 deuce Exp $ */
+/* $Id: ident.h,v 1.6 2018/07/24 01:11:07 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2007 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
