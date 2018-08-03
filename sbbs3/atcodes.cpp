680fb54aa0ef9fda8ab78e9e7e7de460c616a2aa
rswindell <rswindell>
2018-08-03 05:48:00 +0000

    Log an error if one or more file attachments could not be deleted when attempted.

--- sbbs3/atcodes.cpp
+++ sbbs3/atcodes.cpp
@@ -1,6 +1,6 @@
 /* Synchronet "@code" functions */
 
-/* $Id: atcodes.cpp,v 1.82 2018/10/04 03:42:20 rswindell Exp $ */
+/* $Id: atcodes.cpp,v 1.81 2018/07/25 00:40:29 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1029,10 +1029,6 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 	}
 	if(!strcmp(sp,"MSG_SUBJECT") && current_msg!=NULL)
 		return(current_msg->subj==NULL ? nulstr : current_msg->subj);
-	if(!strcmp(sp,"MSG_SUMMARY") && current_msg!=NULL)
-		return(current_msg->summary==NULL ? nulstr : current_msg->summary);
-	if(!strcmp(sp,"MSG_TAGS") && current_msg!=NULL)
-		return(current_msg->tags==NULL ? nulstr : current_msg->tags);
 	if(!strcmp(sp,"MSG_DATE") && current_msg!=NULL)
 		return(timestr(current_msg->hdr.when_written.time));
 	if(!strcmp(sp,"MSG_AGE") && current_msg!=NULL)
