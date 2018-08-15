6afded54a767745f4c5aaa1fbfc103b83bfb08dd
rswindell <rswindell>
2018-08-15 19:48:38 +0000

    This is just something I noticed while looking into the change for the previous commit: don't clear the line counter or call pause() - which will do nothing - if the console abort flag is set.

--- sbbs3/qwktomsg.cpp
+++ sbbs3/qwktomsg.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet QWK to SMB message conversion routine */
 
-/* $Id: qwktomsg.cpp,v 1.69 2018/10/30 03:16:07 rswindell Exp $ */
+/* $Id: qwktomsg.cpp,v 1.67 2018/08/03 06:18:56 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -137,12 +137,6 @@ static void qwk_parse_header_list(ulong confnum, smbmsg_t* msg, str_list_t* head
 
 	/* Synchronet */
 	while((p=iniPopKey(headers,ROOT_SECTION,smb_hfieldtype(hfield_type=SMB_EDITOR),value))!=NULL)
-		smb_hfield_str(msg,hfield_type,p);
-	while((p=iniPopKey(headers,ROOT_SECTION,smb_hfieldtype(hfield_type=SMB_COLUMNS),value))!=NULL) {
-		uint8_t columns = atoi(p);
-		smb_hfield_bin(msg,hfield_type,columns);
-	}
-	while((p=iniPopKey(headers,ROOT_SECTION,smb_hfieldtype(hfield_type=SMB_TAGS),value))!=NULL)
 		smb_hfield_str(msg,hfield_type,p);
 
 	/* USENET */
