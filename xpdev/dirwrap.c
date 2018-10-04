585a93c6da8f959ac88d0c9a4deaaca2d1cffbae
rswindell <rswindell>
2018-10-04 04:03:01 +0000

    Search message tags as well as body-text and subjects when searching for text. Use strcasestr() for message body/header-field text searches (simplify the code) - not sure why we needed strip_ctrl(), so removed.

--- xpdev/dirwrap.c
+++ xpdev/dirwrap.c
@@ -1,7 +1,7 @@
 /* Directory-related system-call wrappers */
 // vi: tabstop=4
 
-/* $Id: dirwrap.c,v 1.96 2018/02/10 08:20:40 deuce Exp $ */
+/* $Id: dirwrap.c,v 1.101 2018/07/25 04:20:05 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -248,7 +248,7 @@ int	DLLCALL	glob(const char *pattern, int flags, void* unused, glob_t* glob)
 			SAFECOPY(path,pattern);
 			p=getfname(path);
 			*p=0;
-			strcat(path,ff.name);
+			SAFECAT(path,ff.name);
 
 			if((glob->gl_pathv[glob->gl_pathc]=(char*)malloc(strlen(path)+2))==NULL) {
 				globfree(glob);
@@ -315,8 +315,9 @@ long DLLCALL getdirsize(const char* path, BOOL include_subdirs, BOOL subdir_only
 
 	SAFECOPY(match,path);
 	backslash(match);
-	strcat(match,ALLFILES);
-	glob(match,GLOB_MARK,NULL,&g);
+	SAFECAT(match,ALLFILES);
+	if (glob(match,GLOB_MARK,NULL,&g) != 0)
+		return 0;
 	if(include_subdirs && !subdir_only)
 		count=g.gl_pathc;
 	else
@@ -721,11 +722,13 @@ int removecase(const char *path)
 /* Deletes all files in dir 'path' that match file spec 'spec'              */
 /* Returns number of files deleted or negative on error						*/
 /****************************************************************************/
-ulong DLLCALL delfiles(const char *inpath, const char *spec)
+long DLLCALL delfiles(const char *inpath, const char *spec)
 {
 	char	*path;
 	char	lastch;
-    uint	i,files=0;
+	size_t	i;
+    long	files = 0;
+	long	errors = 0;
 	glob_t	g;
 	size_t	inpath_len=strlen(inpath);
 
@@ -746,12 +749,16 @@ ulong DLLCALL delfiles(const char *inpath, const char *spec)
 	for(i=0;i<g.gl_pathc;i++) {
 		if(isdir(g.gl_pathv[i]))
 			continue;
-		CHMOD(g.gl_pathv[i],S_IWRITE);	/* Incase it's been marked RDONLY */
+		CHMOD(g.gl_pathv[i],S_IWRITE);	/* In case it's been marked RDONLY */
 		if(remove(g.gl_pathv[i])==0)
 			files++;
+		else
+			errors++;
 	}
 	globfree(&g);
-	return(files);
+	if(errors)
+		return -errors;
+	return files;
 }
 
 /****************************************************************************/
@@ -767,7 +774,7 @@ ulong DLLCALL getfilecount(const char *inpath, const char* pattern)
 
 	SAFECOPY(path, inpath);
 	backslash(path);
-	strcat(path, pattern);
+	SAFECAT(path, pattern);
 	if(glob(path, GLOB_MARK, NULL, &g))
 		return 0;
 	for(gi = 0; gi < g.gl_pathc; ++gi) {
@@ -1031,7 +1038,7 @@ BOOL DLLCALL isfullpath(const char* filename)
 {
 	return(filename[0]=='/'
 #ifdef WIN32
-		|| filename[0]=='\\' || filename[1]==':'
+		|| filename[0]=='\\' || (isalpha(filename[0]) && filename[1]==':')
 #endif
 		);
 }
