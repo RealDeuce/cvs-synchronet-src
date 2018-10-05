aa0563c847e94b0a62b1aa3a1b16daf3be2642e4
rswindell <rswindell>
2018-10-05 08:24:46 +0000

    Detect and report the message with the largest data (e.g. body text) size.

--- sbbs3/pack_qwk.cpp
+++ sbbs3/pack_qwk.cpp
@@ -1,6 +1,6 @@
 /* Synchronet pack QWK packet routine */
 
-/* $Id: pack_qwk.cpp,v 1.82 2019/02/14 09:55:36 rswindell Exp $ */
+/* $Id: pack_qwk.cpp,v 1.80 2018/07/25 03:39:28 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -165,7 +165,7 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 		}
 		p="CONTROLTYPE = ";
 		fprintf(stream,"DOOR = %.10s\r\nVERSION = %s%c\r\n"
-			"SYSTEM = %s%c\r\n"
+			"SYSTEM = %s\r\n"
 			"CONTROLNAME = SBBS\r\n"
 			"%sADD\r\n"
 			"%sDROP\r\n"
@@ -186,7 +186,7 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 			"MIXEDCASE = YES\r\n"
 			,VERSION_NOTICE
 			,VERSION,REVISION
-			,VERSION_NOTICE,REVISION
+			,VERSION_NOTICE
 			,p,p,p,p
 			,p,p,p,p
 			,p,p,p,p
@@ -332,8 +332,21 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 	else
 		personal=NULL;
 
-	if(useron.qwk&(QWK_EMAIL|QWK_ALLMAIL)
-		&& smb_open_sub(&cfg, &smb, INVALID_SUB) == SMB_SUCCESS) {
+	if(useron.qwk&(QWK_EMAIL|QWK_ALLMAIL) /* && !prepack */) {
+		SAFEPRINTF(smb.file,"%smail",cfg.data_dir);
+		smb.retry_time=cfg.smb_retry_time;
+		smb.subnum=INVALID_SUB;
+		if((i=smb_open(&smb))!=0) {
+			fclose(qwk);
+			if(hdrs!=NULL)
+				fclose(hdrs);
+			if(voting!=NULL)
+				fclose(voting);
+			if(personal)
+				fclose(personal);
+			errormsg(WHERE,ERR_OPEN,smb.file,i,smb.last_error);
+			return(false); 
+		}
 
 		/***********************/
 		/* Pack E-mail, if any */
@@ -412,7 +425,7 @@ bool sbbs_t::pack_qwk(char *packet, ulong *msgcnt, bool prepack)
 		smb_close(&smb);					/* Close the e-mail */
 		if(mailmsgs)
 			free(mail);
-	}
+		}
 
 	/*********************/
 	/* Pack new messages */
