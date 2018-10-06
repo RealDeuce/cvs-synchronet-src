0a34d2add083d6da0f914b84a134a228ffc3eae6
rswindell <rswindell>
2018-10-06 22:39:24 +0000

    Sanitize the 'cfg' value read from *.subs (ignore garbage/unused bits).

--- conio/x_cio.c
+++ conio/x_cio.c
@@ -1,10 +1,10 @@
-/* $Id: x_cio.c,v 1.49 2018/03/10 09:50:32 deuce Exp $ */
+/* $Id: x_cio.c,v 1.50 2018/07/24 01:10:58 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
