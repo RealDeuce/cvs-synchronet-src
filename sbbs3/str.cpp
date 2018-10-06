5b52a06c18bee9dbb7573f89e869450f92b898f7
rswindell <rswindell>
2018-10-06 22:34:37 +0000

    Zero-initialize the allocated subscan array.

--- sbbs3/str.cpp
+++ sbbs3/str.cpp
@@ -1,6 +1,6 @@
 /* Synchronet high-level string i/o routines */
 
-/* $Id: str.cpp,v 1.79 2018/02/20 11:43:08 rswindell Exp $ */
+/* $Id: str.cpp,v 1.80 2018/07/25 00:40:30 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1067,7 +1067,7 @@ bool sbbs_t::spy(uint i /* node_num */)
 		return(false);
 	}
 	if(spy_socket[i-1]!=INVALID_SOCKET) {
-		bprintf("Node %d already being spied (%lx)\r\n",i,spy_socket[i-1]);
+		bprintf("Node %d already being spied (%x)\r\n",i,spy_socket[i-1]);
 		return(false);
 	}
 	bprintf("*** Synchronet Remote Spy on Node %d: Ctrl-C to Abort ***"
