513868b7839cc194bc90cef0aff28c6fbc458c24
rswindell <rswindell>
2018-10-16 01:17:25 +0000

    If the Ctrl-A" code contains an empty/bogus filename, don't try to display it. This Ctrl-A code is dubious and perhaps should just be removed.

--- sbbs3/bulkmail.cpp
+++ sbbs3/bulkmail.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet bulk e-mail functions */
 
-/* $Id: bulkmail.cpp,v 1.44 2019/07/08 00:59:25 rswindell Exp $ */
+/* $Id: bulkmail.cpp,v 1.39 2018/08/03 06:18:55 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -43,8 +43,7 @@ bool sbbs_t::bulkmail(uchar *ar)
 	char		str[256],title[LEN_TITLE+1];
 	char		msgpath[MAX_PATH+1];
 	char*		msgbuf;
-	const char*	editor=NULL;
-	const char*	charset=NULL;
+	char*		editor=NULL;
 	char 		tmp[512];
 	int 		i,j,x;
 	long		msgs=0;
@@ -70,8 +69,7 @@ bool sbbs_t::bulkmail(uchar *ar)
 	msg_tmp_fname(useron.xedit, msgpath, sizeof(msgpath));
 	if(!writemsg(msgpath,nulstr,title,wm_mode,INVALID_SUB,"Bulk Mailing"
 		,/* From: */useron.alias
-		,&editor
-		,&charset)) {
+		,&editor)) {
 		bputs(text[Aborted]);
 		return(false); 
 	}
@@ -113,11 +111,12 @@ bool sbbs_t::bulkmail(uchar *ar)
 	msg.hdr.when_written.time=time32(NULL);
 	msg.hdr.when_written.zone=sys_timezone(&cfg);
 
-	editor_info_to_msg(&msg, editor, charset);
+	if(editor!=NULL)
+		smb_hfield_str(&msg,SMB_EDITOR,editor);
 
 	memset(&smb,0,sizeof(smb));
 	smb.subnum=INVALID_SUB;	/* mail database */
-	i=savemsg(&cfg, &smb, &msg, &client, startup->host_name, msgbuf, /* remsg: */NULL);
+	i=savemsg(&cfg, &smb, &msg, &client, startup->host_name, msgbuf);
 	free(msgbuf);
 	if(i!=0) {
 		smb_close(&smb);
