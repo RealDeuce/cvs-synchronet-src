4de5def4d972f2e634b9c5e6469b4c3766c85146
rswindell <rswindell>
2018-10-16 01:17:25 +0000

    If the Ctrl-A" code contains an empty/bogus filename, don't try to display it. This Ctrl-A code is dubious and perhaps should just be removed.

--- conio/curs_cio.c
+++ conio/curs_cio.c
@@ -1,10 +1,10 @@
-/* $Id: curs_cio.c,v 1.37 2018/02/10 10:31:31 deuce Exp $ */
+/* $Id: curs_cio.c,v 1.41 2018/07/24 01:10:58 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -575,7 +575,10 @@ int curs_gettext(int sx, int sy, int ex, int ey, void *fillbuf)
 				attrib |= 128;
 			}
 			colour=PAIR_NUMBER(attr&A_COLOR)-1;
-			colour=((colour&56)<<1)|(colour&7);
+			if (COLORS >= 16)
+				colour=colour&0x7f;
+			else
+				colour=((colour&56)<<1)|(colour&7);
 			fill[fillpos++]=colour|attrib;
 		}
 	}
@@ -1030,9 +1033,9 @@ int curs_showmouse(void)
 	return(-1);
 }
 
-int curs_beep(void)
+void curs_beep(void)
 {
-	return(beep());
+	beep();
 }
 
 int curs_getvideoflags(void)
@@ -1045,4 +1048,5 @@ void curs_setvideoflags(int flags)
 	flags &= (CIOLIB_VIDEO_NOBRIGHT|CIOLIB_VIDEO_BGBRIGHT|CIOLIB_VIDEO_NOBLINK);
 	if (COLORS < 16)
 		flags &= ~CIOLIB_VIDEO_BGBRIGHT;
+	vflags = flags;
 }
