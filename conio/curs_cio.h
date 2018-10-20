60c0c8e0cb488608e7864f02e367105362371f7c
rswindell <rswindell>
2018-10-20 11:15:56 +0000

    Change the C128 screen modes (C128_40X25 and C128_80x25) to use the C64 palette. I can only assume this was a typo? I'm also guessing this is the same change that Payton Byrd made in his fork of SyncTERM (he erroneously called 2.0), which I haven't been able to locate on github (anyone?). This change makes SyncTERM (in C128 modes) appear more consistent with CGTerm.
    With this change, I now see no difference between the C64_40X25 screen mode
    and the C128_40X25 screen mode. (Deuce?)
    
    I also have questions about the changes in some of the colors between
    40 and 80 columns modes expressed here (but not observed with CGTerm):
    https://www.commodore.ca/manuals/128_system_guide/app-i.htm
    (e.g. 129 is supposedly green in 40 column mode and dark purple in 80 cols).
    see also: 149, 151, and 159 - if this is true, then another palette
    is needed for C128-80 mode but doesn't explain CGTerm's behavior
    (the colors are the same in 40 and 80 column modes).
    
    Also, the C64 palette changed pretty drastically since SyncTERM 1.0 where
    the colors weres dull/muted and are now bright. And the initial attribute is
    Blue on Grey (not black) - that wasn't the case with v1.0.

--- conio/curs_cio.h
+++ conio/curs_cio.h
@@ -1,10 +1,10 @@
-/* $Id: curs_cio.h,v 1.16 2018/02/14 04:37:27 deuce Exp $ */
+/* $Id: curs_cio.h,v 1.17 2018/07/24 01:10:58 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
