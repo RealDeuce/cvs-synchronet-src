6041461487220ca277beeac9ba46f32e6d055675
rswindell <rswindell>
2018-10-30 03:12:23 +0000

    Store the terminal width (in columns) of the text editor used to create the message text (when relevant and available) - this helps when re-wrapping the message text (e.g. for quoting) and not always guessing "80 columns".

--- sbbs3/atcodes.cpp
+++ sbbs3/atcodes.cpp
@@ -1,6 +1,6 @@
 /* Synchronet "@code" functions */
 
-/* $Id: atcodes.cpp,v 1.82 2018/10/04 03:42:20 rswindell Exp $ */
+/* $Id: atcodes.cpp,v 1.84 2018/10/25 21:25:53 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -205,6 +205,17 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 		return(str);
 	}
 
+	if(!strcmp(sp,"COLS")) {
+		safe_snprintf(str,maxlen,"%lu",cols);
+		return(str);
+	}
+	if(!strcmp(sp,"ROWS")) {
+		safe_snprintf(str,maxlen,"%lu",rows);
+		return(str);
+	}
+	if(!strcmp(sp,"TERM"))
+		return(terminal);
+
 	if(!strcmp(sp,"CONN"))
 		return(connection);
 
@@ -299,7 +310,7 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 		|| !strcmp(sp,"LASTCALLERSYSTEM"))
 		return(lastuseron);
 
-	if(!strcmp(sp,"CLS")) {
+	if(!strcmp(sp,"CLS") || !strcmp(sp,"CLEAR")) {
 		CLS;
 		return(nulstr);
 	}
@@ -633,6 +644,11 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 
 	if(!strncmp(sp,"MENU:",5)) {
 		menu(sp+5);
+		return(nulstr);
+	}
+
+	if(!strncmp(sp,"CONDMENU:",9)) {
+		menu(sp+9, P_NOERROR);
 		return(nulstr);
 	}
 
