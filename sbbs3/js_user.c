4624f59d9b4f4bd68153e6d5088c59488084df5e
rswindell <rswindell>
2018-11-04 23:55:08 +0000

    Resolve new GCC warnings: comparison between signed and unsigned integer expressions

--- sbbs3/js_user.c
+++ sbbs3/js_user.c
@@ -1,7 +1,7 @@
 /* Synchronet JavaScript "User" Object */
 // vi: tabstop=4
 
-/* $Id: js_user.c,v 1.108 2019/01/10 19:53:09 rswindell Exp $ */
+/* $Id: js_user.c,v 1.107 2018/08/07 02:16:26 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -49,7 +49,7 @@ typedef struct
 
 } private_t;
 
-/* User Object Properties */
+/* User Object Properites */
 enum {
 	 USER_PROP_NUMBER
 	,USER_PROP_ALIAS 	
@@ -135,6 +135,7 @@ static void js_getuserdat(scfg_t* scfg, private_t* p)
 			p->cached=TRUE;
 	}
 }
+
 
 static JSBool js_user_get(JSContext *cx, JSObject *obj, jsid id, jsval *vp)
 {
@@ -1577,7 +1578,7 @@ JSObject* DLLCALL js_CreateUserObject(JSContext* cx, JSObject* parent, scfg_t* c
 		,"Instance of <i>User</i> class, representing current user online"
 		,310);
 	js_DescribeSyncConstructor(cx,userobj
-		,"To create a new user object: <tt>var u = new User;</tt> or: <tt>var u = new User(<i>number</i>);</tt>");
+		,"To create a new user object: <tt>var u = new User(<i>number</i>)</tt>");
 	js_CreateArrayOfStrings(cx, userobj
 		,"_property_desc_list", user_prop_desc, JSPROP_READONLY);
 #endif
