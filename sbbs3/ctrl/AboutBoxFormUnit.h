1a5ef5154b57a52dadb5b2de3adab6174d7e3f48
rswindell <rswindell>
2018-11-05 07:39:26 +0000

    Fixed-up some log output messages: - user alias was missing from some messages or in the wrong place - added hostname and IP address to the "ILLEGAL FILENAME ATTEMPT" warnings

--- sbbs3/ctrl/AboutBoxFormUnit.h
+++ sbbs3/ctrl/AboutBoxFormUnit.h
@@ -1,12 +1,12 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: AboutBoxFormUnit.h,v 1.5 2006/01/27 10:23:57 rswindell Exp $ */
+/* $Id: AboutBoxFormUnit.h,v 1.6 2018/07/24 01:11:28 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
