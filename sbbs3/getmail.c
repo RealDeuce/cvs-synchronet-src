4ef468b22aee546f5113b9ff78cbc834d2b36505
rswindell <rswindell>
2018-12-06 23:29:38 +0000

    Added TODO comment regarding the lack of a Content-Length header field in GET responses when Connection: Keep-Alive is not enabled.

--- sbbs3/getmail.c
+++ sbbs3/getmail.c
@@ -1,6 +1,6 @@
 /* Synchronet DLL-exported mail-related routines */
 
-/* $Id: getmail.c,v 1.16 2018/03/10 03:19:01 rswindell Exp $ */
+/* $Id: getmail.c,v 1.18 2018/08/03 06:25:21 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -84,40 +84,35 @@ int DLLCALL getmail(scfg_t* cfg, int usernumber, BOOL sent, uint16_t attr)
 /***************************/
 /* Delete file attachments */
 /***************************/
-void DLLCALL delfattach(scfg_t* cfg, smbmsg_t* msg)
+BOOL DLLCALL delfattach(scfg_t* cfg, smbmsg_t* msg)
 {
-    char str[MAX_PATH+1];
-	char str2[MAX_PATH+1];
+    char dir[MAX_PATH+1];
+	char path[MAX_PATH+1];
+	char files[128];
 	char *tp,*sp,*p;
 
-	if(msg->idx.to==0) {	/* netmail */
-		SAFEPRINTF3(str,"%sfile/%04u.out/%s"
-			,cfg->data_dir,msg->idx.from,getfname(msg->subj));
-		remove(str);
-		SAFEPRINTF2(str,"%sfile/%04u.out"
-			,cfg->data_dir,msg->idx.from);
-		rmdir(str);
-		return;
-	}
+	if(msg->idx.to==0) 	/* netmail */
+		SAFEPRINTF2(dir, "%sfile/%04u.out", cfg->data_dir, msg->idx.from);
+	else
+		SAFEPRINTF2(dir, "%sfile/%04u.in", cfg->data_dir, msg->idx.to);
 		
-	SAFECOPY(str,msg->subj);
-	tp=str;
+	SAFECOPY(files, msg->subj);
+	tp=files;
 	while(1) {
 		p=strchr(tp,' ');
 		if(p) *p=0;
 		sp=strrchr(tp,'/');              /* sp is slash pointer */
 		if(!sp) sp=strrchr(tp,'\\');
 		if(sp) tp=sp+1;
-		SAFEPRINTF3(str2,"%sfile/%04u.in/%s"  /* str2 is path/fname */
-			,cfg->data_dir,msg->idx.to,tp);
-		remove(str2);
+		SAFEPRINTF2(path, "%s/%s", dir, tp);
+		if(remove(path) != 0)
+			return FALSE;
 		if(!p)
 			break;
 		tp=p+1; 
-
 	}
-	SAFEPRINTF2(str,"%sfile/%04u.in",cfg->data_dir,msg->idx.to);
-	rmdir(str);                     /* remove the dir if it's empty */
+	rmdir(dir);                     /* remove the dir if it's empty */
+	return TRUE;
 }
 
 /****************************************************************************/
