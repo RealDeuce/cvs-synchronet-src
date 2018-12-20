f539635bd4cf6bae377a46de2fc4acaaad030b69
rswindell <rswindell>
2018-12-20 03:03:50 +0000

    Remove trailing whitespace off the end of original messages when creating quote files (ain't nobody got time for that!).

--- sbbs3/str_util.c
+++ sbbs3/str_util.c
@@ -1,6 +1,6 @@
 /* Synchronet string utility routines */
 
-/* $Id: str_util.c,v 1.53 2017/10/28 20:16:19 rswindell Exp $ */
+/* $Id: str_util.c,v 1.54 2018/10/15 04:08:58 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -52,8 +52,6 @@ char* DLLCALL remove_ctrl_a(const char *str, char *dest)
 	for(i=j=0;str[i];i++) {
 		if(str[i]==CTRL_A) {
 			i++;
-			if(str[i]==0 || toupper(str[i])=='Z')	/* EOF */
-				break;
 			/* convert non-destructive backspace to a destructive backspace */
 			if(str[i]=='<' && j)	
 				j--;
@@ -73,8 +71,6 @@ char* DLLCALL strip_ctrl(const char *str, char* dest)
 	for(i=j=0;str[i];i++) {
 		if(str[i]==CTRL_A) {
 			i++;
-			if(str[i]==0 || toupper(str[i])=='Z')	/* EOF */
-				break;
 			/* convert non-destructive backspace to a destructive backspace */
 			if(str[i]=='<' && j)	
 				j--;
@@ -121,8 +117,6 @@ char* DLLCALL prep_file_desc(const char *str, char* dest)
 	for(i=j=0;str[i];i++)
 		if(str[i]==CTRL_A && str[i+1]!=0) {
 			i++;
-			if(toupper(str[i])=='Z')	/* EOF */
-				break;
 			/* convert non-destructive backspace to a destructive backspace */
 			if(str[i]=='<' && j)	
 				j--;
@@ -346,8 +340,6 @@ size_t bstrlen(const char *str)
 	while(*str) {
 		if(*str==CTRL_A) {
 			str++;
-			if(toupper(*str)=='Z')	/* EOF */
-				break;
 			if(*str=='[')
 				i=0;
 			else if(*str=='<' && i)
