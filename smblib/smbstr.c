1df13404d879b8815d1ea9595f02387c905799ac
rswindell <rswindell>
2019-01-04 22:15:13 +0000

    Partially revert the change in rev 1.63: eprintf() does not log the <useron alias>, so we need to include it in the log message here if we want the current user's alias included in event related log messages (e.g. unpacking QWK-REP packages).

--- smblib/smbstr.c
+++ smblib/smbstr.c
@@ -1,6 +1,6 @@
 /* Synchronet message base (SMB) library routines returning strings */
 
-/* $Id: smbstr.c,v 1.34 2019/03/13 08:43:39 rswindell Exp $ */
+/* $Id: smbstr.c,v 1.32 2018/11/04 23:26:45 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -338,15 +338,8 @@ enum smb_net_type SMBCALL smb_netaddr_type(const char* str)
 {
 	const char*	p;
 
-	if((p=strchr(str,'@')) == NULL) {
+	if((p=strchr(str,'@'))==NULL)
 		p = str;
-		SKIP_WHITESPACE(p);
-		if(*p == 0)
-			return NET_NONE;
-		if(smb_get_net_type_by_addr(p) == NET_FIDO)
-			return NET_FIDO;
-		return NET_NONE;
-	}
 	else
 		p++;
 	SKIP_WHITESPACE(p);
@@ -360,15 +353,11 @@ enum smb_net_type SMBCALL smb_netaddr_type(const char* str)
 /* Returns net_type for passed network address 								*/
 /* The only addresses expected with an '@' are Internet/SMTP addresses		*/
 /* Examples:																*/
-/*  ""					= NET_NONE											*/
-/*	"@"					= NET_NONE											*/
 /*	"VERT"				= NET_QWK											*/
-/*	"VERT/NIX"			= NET_QWK											*/
 /*	"1:103/705"			= NET_FIDO											*/
 /*	"705.0"				= NET_FIDO											*/
 /*	"705"				= NET_FIDO											*/
 /*	"192.168.1.0"		= NET_INTERNET										*/
-/*  "::1"				= NET_INTERNET										*/
 /*	"some.host"			= NET_INTERNET										*/
 /*	"someone@anywhere"	= NET_INTERNET										*/
 /*	"someone@some.host"	= NET_INTERNET										*/
@@ -377,55 +366,28 @@ enum smb_net_type SMBCALL smb_get_net_type_by_addr(const char* addr)
 {
 	const char*	p = addr;
 	const char*	tp;
+	char*	firstdot;
+	char*	lastdot;
 
-	char* at = strchr(p,'@');
-	if(at != NULL)
-		p = at + 1;
+	if(strchr(p,'@') != NULL)
+		return(NET_INTERNET);
 
-	if(*p == 0)
-		return NET_NONE;
+	firstdot=strchr(p,'.');
+	lastdot=strrchr(p,'.');
 
-	char* dot = strchr(p,'.');
-	char* colon = strchr(p,':');
-	char* slash = strchr(p,'/');
+	if(isalpha(*p) && firstdot==NULL)
+		return(NET_QWK);
 
-	if(at == NULL && isalpha(*p) && dot == NULL && colon == NULL)
-		return NET_QWK;
-
-	char last = 0;
-	for(tp = p; *tp != '\0'; tp++) {
-		last = *tp;
-		if(isdigit(*tp))
-			continue;
-		if(*tp == ':') {
-			if(tp != colon)
-				break;
-			if(dot != NULL && tp > dot)
-				break;
-			if(slash != NULL && tp > slash)
-				break;
-			continue;
-		}
-		if(*tp == '/') {
-			if(tp != slash)
-				break;
-			if(dot != NULL && tp > dot)
-				break;
-			continue;
-		}
-		if(*tp == '.') {
-			if(tp != dot)
-				break;
-			continue;
-		}
-		break;
+	for(tp=p;*tp;tp++) {
+		if(!isdigit(*tp) && *tp!=':' && *tp!='/' && *tp!='.')
+			break;
 	}
-	if(at == NULL && isdigit(*p) && *tp == '\0' && isdigit(last))
-		return NET_FIDO;
-	if(slash == NULL && (isalnum(*p) || p == colon))
-		return NET_INTERNET;
+	if(isdigit(*p) && *tp==0 && firstdot==lastdot)
+		return(NET_FIDO);
+	if(isalnum(*p))
+		return(NET_INTERNET);
 
-	return NET_UNKNOWN;
+	return(NET_UNKNOWN);
 }
 
 char* SMBCALL smb_nettype(enum smb_net_type type)
