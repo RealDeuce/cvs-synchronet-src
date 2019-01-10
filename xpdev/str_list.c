18e92b6648a5301fea82baa44ce499155c949681
rswindell <rswindell>
2019-01-10 02:47:53 +0000

    Enable ANSI/VT100 terminal emulation in the Windows (10) console output when sending the "ANSI" output to stdout (and not a file).

--- xpdev/str_list.c
+++ xpdev/str_list.c
@@ -2,7 +2,7 @@
 
 /* Functions to deal with NULL-terminated string lists */
 
-/* $Id: str_list.c,v 1.47 2017/06/09 02:02:57 rswindell Exp $ */
+/* $Id: str_list.c,v 1.50 2018/12/30 02:09:49 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -227,6 +227,7 @@ BOOL DLLCALL strListSwap(const str_list_t list, size_t index1, size_t index2)
 char* DLLCALL strListAppend(str_list_t* list, const char* str, size_t index)
 {
 	char* buf;
+	char *ret;
 
 	if(str==NULL)
 		return(NULL);
@@ -237,7 +238,10 @@ char* DLLCALL strListAppend(str_list_t* list, const char* str, size_t index)
 	if(index==STR_LIST_LAST_INDEX)
 		index=strListCount(*list);
 
-	return(str_list_append(list,buf,index));
+	ret = str_list_append(list,buf,index);
+	if (ret == NULL)
+		free(buf);
+	return ret;
 }
 
 size_t DLLCALL	strListAppendList(str_list_t* list, const str_list_t add_list)
@@ -255,6 +259,7 @@ size_t DLLCALL	strListAppendList(str_list_t* list, const str_list_t add_list)
 char* DLLCALL strListInsert(str_list_t* list, const char* str, size_t index)
 {
 	char* buf;
+	char* ret;
 
 	if(str==NULL)
 		return(NULL);
@@ -262,7 +267,10 @@ char* DLLCALL strListInsert(str_list_t* list, const char* str, size_t index)
 	if((buf=strdup(str))==NULL)
 		return(NULL);
 
-	return(str_list_insert(list,buf,index));
+	ret = str_list_insert(list,buf,index);
+	if (ret == NULL)
+		free(buf);
+	return ret;
 }
 
 size_t DLLCALL strListInsertList(str_list_t* list, const str_list_t add_list, size_t index)
@@ -473,7 +481,7 @@ void DLLCALL strListFreeStrings(str_list_t list)
 
 void DLLCALL strListFree(str_list_t* list)
 {
-	if(*list!=NULL) {
+	if(list != NULL && *list != NULL) {
 		strListFreeStrings(*list);
 		FREE_AND_NULL(*list);
 	}
