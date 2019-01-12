de1c71b29da5745635f9370ffb0e006a4bcf6539
rswindell <rswindell>
2019-01-12 08:11:13 +0000

    Optimized getfiledat(): rather than calling both flength() and fdate() (which both call stat()), just call stat() once and use the result for both file size and date/time.

--- syncterm/telnet_io.c
+++ syncterm/telnet_io.c
@@ -1,11 +1,12 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: telnet_io.c,v 1.35 2019/07/10 22:48:08 deuce Exp $ */
+/* $Id: telnet_io.c,v 1.33 2018/10/26 06:19:48 rswindell Exp $ */
 
 #include <stdlib.h>
 #include <string.h>
 
 #include "term.h"
+#include "cterm.h"
 
 #include "genwrap.h"
 #include "sockwrap.h"
@@ -90,7 +91,7 @@ void request_telnet_opt(uchar cmd, uchar opt)
 	send_telnet_cmd(cmd,opt);
 }
 
-BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen, cterm_emulation_t emu)
+BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen)
 {
 	BYTE	command;
 	BYTE	option;
@@ -158,12 +159,24 @@ BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen, cterm_
 					/* sub-option terminated */
 					if(option==TELNET_TERM_TYPE && telnet_cmd[3]==TELNET_TERM_SEND) {
 						char buf[32];
+						const char *termtype;
+						switch(cterm->emulation) {
+							case CTERM_EMULATION_PETASCII:
+								termtype = "PETSCII";
+								break;
+							case CTERM_EMULATION_ATASCII:
+								termtype = "ATASCII";
+								break;
+							default:
+								termtype = "ANSI";
+								break;
+						}
 						int len=sprintf(buf,"%c%c%c%c%s%c%c"
 							,TELNET_IAC,TELNET_SB
 							,TELNET_TERM_TYPE,TELNET_TERM_IS
-							,get_emulation_str(emu)
+							,termtype
 							,TELNET_IAC,TELNET_SE);
-						lprintf(LOG_INFO,"TX: Terminal Type is %s", get_emulation_str(emu));
+						lprintf(LOG_INFO,"TX: Terminal Type is %s", termtype);
 						putcom(buf,len);
 						request_telnet_opt(TELNET_WILL, TELNET_NEGOTIATE_WINDOW_SIZE);
 					}
