2233204de14462c8070ebb0878441f46b8648d6d
rswindell <rswindell>
2019-01-12 23:53:59 +0000

    Better "Incorrect DLL" error message, display the expected version too.

--- sbbs3/ftpsrvr.c
+++ sbbs3/ftpsrvr.c
@@ -1,6 +1,6 @@
 /* Synchronet FTP server */
 
-/* $Id: ftpsrvr.c,v 1.488 2019/03/07 01:11:00 deuce Exp $ */
+/* $Id: ftpsrvr.c,v 1.486 2019/01/12 10:29:26 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -1051,16 +1051,12 @@ BOOL js_generate_index(JSContext* js_cx, JSObject* parent,
 					if(!getfiledat(&scfg,&f))
 						continue;
 				} else {
-					f.size = f.cdt;
-					f.date = f.dateuled;
-					if(!filedat || (scfg.dir[dir]->misc&DIR_FCHK)) {
-						struct stat st;
-						if(stat(g.gl_pathv[i], &st) != 0)
-							continue;
-						f.cdt = st.st_size;
-						f.size = st.st_size;
-						f.date = (time32_t)st.st_mtime;
-					}
+					struct stat st;
+					if(stat(g.gl_pathv[i], &st) != 0)
+						continue;
+					f.cdt = st.st_size;
+					f.size = st.st_size;
+					f.date = (time32_t)st.st_mtime;
 				}
 				if(f.misc&FM_EXTDESC) {
 					extdesc[0]=0;
@@ -2000,9 +1996,7 @@ static BOOL start_tls(SOCKET *sock, CRYPT_SESSION *sess, BOOL resp)
 			sockprintf(*sock, *sess, "431 TLS not available");
 		return FALSE;
 	}
-	lock_ssl_cert();
 	if ((status=cryptSetAttribute(*sess, CRYPT_SESSINFO_PRIVATEKEY, scfg.tls_certificate)) != CRYPT_OK) {
-		unlock_ssl_cert();
 		GCES(status, *sock, *sess, estr, "setting private key");
 		cryptDestroySession(*sess);
 		*sess = -1;
@@ -2015,7 +2009,6 @@ static BOOL start_tls(SOCKET *sock, CRYPT_SESSION *sess, BOOL resp)
 	nb=0;
 	ioctlsocket(*sock,FIONBIO,&nb);
 	if ((status = cryptSetAttribute(*sess, CRYPT_SESSINFO_NETWORKSOCKET, *sock)) != CRYPT_OK) {
-		unlock_ssl_cert();
 		GCES(status, *sock, *sess, estr, "setting network socket");
 		cryptDestroySession(*sess);
 		*sess = -1;
@@ -2026,11 +2019,9 @@ static BOOL start_tls(SOCKET *sock, CRYPT_SESSION *sess, BOOL resp)
 	if (resp)
 		sockprintf(*sock, -1, "234 Ready to start TLS");
 	if ((status = cryptSetAttribute(*sess, CRYPT_SESSINFO_ACTIVE, 1)) != CRYPT_OK) {
-		unlock_ssl_cert();
 		GCES(status, *sock, *sess, estr, "setting session active");
 		return TRUE;
 	}
-	unlock_ssl_cert();
 	if (startup->max_inactivity) {
 		if ((status = cryptSetAttribute(*sess, CRYPT_OPTION_NET_READTIMEOUT, startup->max_inactivity)) != CRYPT_OK) {
 			GCES(status, *sock, *sess, estr, "setting read timeout");
@@ -4667,16 +4658,10 @@ static void ctrl_thread(void* arg)
 						get_owner_name(&f, str);
 						SAFEPRINTF3(aliaspath, "/%s/%s/%s", scfg.lib[lib]->sname, scfg.dir[dir]->code_suffix, getfname(g.gl_pathv[i]));
 						get_unique(aliaspath, uniq);
-						f.size = f.cdt;
-						f.date = f.dateuled;
-						if(!filedat || (scfg.dir[dir]->misc&DIR_FCHK)) {
-							struct stat st;
-							if(stat(g.gl_pathv[i], &st) != 0)
-								continue;
-							f.size = st.st_size;
-							f.date = (time32_t)st.st_mtime;
-						}
-						send_mlsx_entry(fp, sock, sess, mlsx_feats, "file", permstr, f.size, f.date, str, uniq, f.dateuled, cmd[3] == 'T' ? mls_path : getfname(g.gl_pathv[i]));
+						struct stat st;
+						if(stat(g.gl_pathv[i], &st) != 0)
+							continue;
+						send_mlsx_entry(fp, sock, sess, mlsx_feats, "file", permstr, (uint64_t)st.st_size, st.st_mtime, str, uniq, f.dateuled, cmd[3] == 'T' ? mls_path : getfname(g.gl_pathv[i]));
 						l++;
 					}
 					lprintf(LOG_INFO, "%04d <%s> %s listing (%lu bytes) of /%s/%s (%lu files) created in %ld seconds"
@@ -4964,17 +4949,13 @@ static void ctrl_thread(void* arg)
 						&& !(scfg.dir[dir]->misc&DIR_FILES))
 						continue;
 					if(detail) {
+						struct stat st;
+						if(stat(g.gl_pathv[i], &st) != 0)
+							continue;
+						f.size = st.st_size;
 						if(filedat && !getfiledat(&scfg,&f))
 							continue;
-						f.size = f.cdt;
-						t = f.dateuled;
-						if(!filedat || (scfg.dir[dir]->misc&DIR_FCHK)) {
-							struct stat st;
-							if(stat(g.gl_pathv[i], &st) != 0)
-								continue;
-							f.size = st.st_size;
-							t = st.st_mtime;
-						}
+						t = st.st_mtime;
 						if(localtime_r(&t,&tm)==NULL)
 							memset(&tm,0,sizeof(tm));
 						if(filedat) {
@@ -5150,7 +5131,7 @@ static void ctrl_thread(void* arg)
 					sockprintf(sock,sess, "550 Size not available for dynamically generated files");
 					continue;
 				}
-				if((fp=fopen(ftp_tmpfname(fname,"ndx",sock),"wb"))==NULL) {
+				if((fp=fopen(ftp_tmpfname(fname,"ndx",sock),"w+b"))==NULL) {
 					lprintf(LOG_ERR,"%04d <%s> !ERROR %d (%s) line %d opening %s"
 						,sock, user.alias, errno, strerror(errno), __LINE__, fname);
 					sockprintf(sock,sess, "451 Insufficient system storage");
@@ -5161,8 +5142,8 @@ static void ctrl_thread(void* arg)
 				if(getdate)
 					file_date=time(NULL);
 				else {
-					lprintf(LOG_INFO,"%04d <%s> downloading %s for %s in %s mode"
-						,sock, user.alias, startup->index_file_name, genvpath(lib,dir,str)
+					lprintf(LOG_INFO,"%04d <%s> downloading index for %s in %s mode"
+						,sock,user.alias,genvpath(lib,dir,str)
 						,mode);
 					credits=FALSE;
 					tmpfile=TRUE;
@@ -5374,8 +5355,8 @@ static void ctrl_thread(void* arg)
 						filepos=0;
 						continue;
 					}
-					lprintf(LOG_INFO,"%04d <%s> downloading %s for %s in %s mode"
-						,sock, user.alias, startup->html_index_file, genvpath(lib,dir,str)
+					lprintf(LOG_INFO,"%04d <%s> downloading HTML index for %s in %s mode"
+						,sock,user.alias,genvpath(lib,dir,str)
 						,mode);
 					credits=FALSE;
 					tmpfile=TRUE;
@@ -5993,7 +5974,7 @@ const char* DLLCALL ftp_ver(void)
 
 	DESCRIBE_COMPILER(compiler);
 
-	sscanf("$Revision: 1.488 $", "%*s %s", revision);
+	sscanf("$Revision: 1.486 $", "%*s %s", revision);
 
 	sprintf(ver,"%s %s%s  "
 		"Compiled %s %s with %s"
