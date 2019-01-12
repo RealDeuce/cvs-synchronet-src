ccd8242c77a38dccd5237c608f2fc88297efec27
rswindell <rswindell>
2019-01-12 10:29:26 +0000

    Fix some new issues due to use of GLOB_MARK: getfname() on a path that ends in a slash, returns an empty string pointer. Fixed-up some logging output around listing creations.

--- syncterm/telnet_io.h
+++ syncterm/telnet_io.h
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: telnet_io.h,v 1.9 2019/07/10 22:28:52 deuce Exp $ */
+/* $Id: telnet_io.h,v 1.8 2017/01/13 01:33:22 rswindell Exp $ */
 
 #ifndef _TELNET_IO_H_
 #define _TELNET_IO_H_
@@ -8,13 +8,12 @@
 #ifndef TELNET_NO_DLL
 #define TELNET_NO_DLL
 #endif
-#include <cterm.h>
 #include "telnet.h"
 
 extern uchar	telnet_local_option[0x100];
 extern uchar	telnet_remote_option[0x100];
 
-BYTE*	telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen, cterm_emulation_t emu);
+BYTE*	telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen);
 BYTE*	telnet_expand(BYTE* inbuf, size_t inlen, BYTE* outbuf, size_t *newlen);
 void	request_telnet_opt(uchar cmd, uchar opt);
 
