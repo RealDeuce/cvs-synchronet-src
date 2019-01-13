52ff0d693a00bd68902fa9f0f9627f110c17583e
rswindell <rswindell>
2019-01-13 00:04:42 +0000

    Cruft removal:  EmbeddedJavaScriptExt (web_startup.js_ext) settings this idea (web-server parsed html files with embedded JS) was never implemented and made obsolete by web handlers (e.g. xjs).

--- sbbs3/sbbs_ini.c
+++ sbbs3/sbbs_ini.c
@@ -1,6 +1,6 @@
 /* Synchronet initialization (.ini) file routines */
 
-/* $Id: sbbs_ini.c,v 1.163 2018/03/10 00:01:50 rswindell Exp $ */
+/* $Id: sbbs_ini.c,v 1.166 2019/01/13 00:04:42 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -333,6 +333,11 @@ void sbbs_read_ini(
 		bbs->rlogin_interfaces
 			=iniGetStringList(list,section,"RLoginInterface",",",global_interfaces);
 
+		bbs->pet40_port
+			=iniGetShortInt(list,section,"Pet40Port",64);
+		bbs->pet80_port
+			=iniGetShortInt(list,section,"Pet80Port",128);
+
 		bbs->ssh_port
 			=iniGetShortInt(list,section,"SSHPort",22);
 		bbs->ssh_connect_timeout
@@ -399,7 +404,7 @@ void sbbs_read_ini(
 			=iniGetLogLevel(list,section,strLogLevel,global->log_level);
 		bbs->options
 			=iniGetBitField(list,section,strOptions,bbs_options
-				,BBS_OPT_XTRN_MINIMIZED|BBS_OPT_SYSOP_AVAILABLE);
+				,BBS_OPT_XTRN_MINIMIZED);
 
 		bbs->bind_retry_count=iniGetInteger(list,section,strBindRetryCount,global->bind_retry_count);
 		bbs->bind_retry_delay=iniGetInteger(list,section,strBindRetryDelay,global->bind_retry_delay);
@@ -687,8 +692,6 @@ void sbbs_read_ini(
 			=iniGetStringList(list,section,"CGIExtensions", "," ,".cgi");
 		SAFECOPY(web->ssjs_ext
 			,iniGetString(list,section,"JavaScriptExtension",".ssjs",value));
-		SAFECOPY(web->js_ext
-			,iniGetString(list,section,"EmbJavaScriptExtension",".bbs",value));
 
 		web->max_cgi_inactivity
 			=iniGetShortInt(list,section,"MaxCgiInactivity",WEB_DEFAULT_MAX_CGI_INACTIVITY);	/* seconds */
@@ -798,6 +801,11 @@ BOOL sbbs_write_ini(
 		else if(!iniSetStringList(lp,section,"RLoginInterface", ",", bbs->rlogin_interfaces,&style))
 			break;
 		if(!iniSetShortInt(lp,section,"RLoginPort",bbs->rlogin_port,&style))
+			break;
+
+		if(!iniSetShortInt(lp,section,"Pet40Port",bbs->pet40_port,&style))
+			break;
+		if(!iniSetShortInt(lp,section,"Pet80Port",bbs->pet80_port,&style))
 			break;
 
 		if(strListCmp(bbs->ssh_interfaces, global->interfaces)==0)
