e65e188af081ad914495cf85f572c858947f0216
rswindell <rswindell>
2019-02-01 23:46:28 +0000

    Don't ignore leading white-space chars in filter files (e.g. *.can, twitlist.cfg) - this allows adding a filter of say " *" which will filter out names (e.g. mail "Fromt" names) beginning with a space character.

--- sbbs3/js_bbs.cpp
+++ sbbs3/js_bbs.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet JavaScript "bbs" Object */
 
-/* $Id: js_bbs.cpp,v 1.177 2019/02/20 05:43:19 rswindell Exp $ */
+/* $Id: js_bbs.cpp,v 1.175 2019/01/07 23:38:31 sbbs Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -2600,14 +2600,9 @@ js_email(JSContext *cx, uintN argc, jsval *arglist)
 	char*		subj=(char *)def;
 	JSString*	js_top=NULL;
 	JSString*	js_subj=NULL;
-	JSObject*	hdrobj;
 	sbbs_t*		sbbs;
-	smb_t*		resmb = NULL;
-	smbmsg_t*	remsg = NULL;
-	smbmsg_t	msg;
 	jsrefcount	rc;
 
-	ZERO_VAR(msg);
 	JS_SET_RVAL(cx, arglist, JSVAL_VOID);
 
 	if((sbbs=js_GetPrivate(cx, JS_THIS_OBJECT(cx, arglist)))==NULL)
@@ -2629,15 +2624,6 @@ js_email(JSContext *cx, uintN argc, jsval *arglist)
 			js_top=JS_ValueToString(cx,argv[i]);
 		else if(JSVAL_IS_STRING(argv[i]))
 			js_subj=JS_ValueToString(cx,argv[i]);
-		else if(JSVAL_IS_OBJECT(argv[i])) {
-			if((hdrobj = JSVAL_TO_OBJECT(argv[i])) == NULL)
-				return JS_FALSE;
-			if(!js_GetMsgHeaderObjectPrivates(cx, hdrobj, &resmb, &remsg)) {
-				if(!js_ParseMsgHeaderObject(cx, hdrobj, &msg))
-					return JS_FALSE;
-				remsg = &msg;
-			}
-		}
 	}
 
 	if(js_top!=NULL)
@@ -2653,8 +2639,7 @@ js_email(JSContext *cx, uintN argc, jsval *arglist)
 	}
 
 	rc=JS_SUSPENDREQUEST(cx);
-	JS_SET_RVAL(cx, arglist, BOOLEAN_TO_JSVAL(sbbs->email(usernumber, top, subj, mode, resmb, remsg)));
-	smb_freemsgmem(&msg);
+	JS_SET_RVAL(cx, arglist, BOOLEAN_TO_JSVAL(sbbs->email(usernumber,top,subj,mode)));
 	if(top != def)
 		free(top);
 	if(subj != def)
@@ -2662,7 +2647,6 @@ js_email(JSContext *cx, uintN argc, jsval *arglist)
 	JS_RESUMEREQUEST(cx, rc);
 	return(JS_TRUE);
 }
-
 static JSBool
 js_netmail(JSContext *cx, uintN argc, jsval *arglist)
 {
@@ -2672,15 +2656,10 @@ js_netmail(JSContext *cx, uintN argc, jsval *arglist)
 	char*		subj=(char *)def;
 	JSString*	js_to;
 	JSString*	js_subj=NULL;
-	JSObject*	hdrobj;
 	sbbs_t*		sbbs;
 	char*		cstr;
-	smb_t*		resmb = NULL;
-	smbmsg_t*	remsg = NULL;
-	smbmsg_t	msg;
 	jsrefcount	rc;
 
-	ZERO_VAR(msg);
 	JS_SET_RVAL(cx, arglist, JSVAL_VOID);
 
  	if(!js_argc(cx, argc, 1))
@@ -2699,15 +2678,6 @@ js_netmail(JSContext *cx, uintN argc, jsval *arglist)
 		}
 		else if(JSVAL_IS_STRING(argv[i]))
 			js_subj=JS_ValueToString(cx,argv[i]);
-		else if(JSVAL_IS_OBJECT(argv[i])) {
-			if((hdrobj = JSVAL_TO_OBJECT(argv[i])) == NULL)
-				return JS_FALSE;
-			if(!js_GetMsgHeaderObjectPrivates(cx, hdrobj, &resmb, &remsg)) {
-				if(!js_ParseMsgHeaderObject(cx, hdrobj, &msg))
-					return JS_FALSE;
-				remsg = &msg;
-			}
-		}
 	}
 
 	if(js_subj!=NULL) {
@@ -2723,8 +2693,7 @@ js_netmail(JSContext *cx, uintN argc, jsval *arglist)
 		return JS_FALSE;
 	}
 	rc=JS_SUSPENDREQUEST(cx);
-	JS_SET_RVAL(cx, arglist, BOOLEAN_TO_JSVAL(sbbs->netmail(cstr, subj, mode, resmb, remsg)));
-	smb_freemsgmem(&msg);
+	JS_SET_RVAL(cx, arglist, BOOLEAN_TO_JSVAL(sbbs->netmail(cstr,subj,mode)));
 	if(subj != def)
 		free(subj);
 	free(cstr);
@@ -3510,7 +3479,7 @@ js_listfileinfo(JSContext *cx, uintN argc, jsval *arglist)
 }
 
 static JSBool
-js_post_msg(JSContext *cx, uintN argc, jsval *arglist)
+js_postmsg(JSContext *cx, uintN argc, jsval *arglist)
 {
 	jsval *argv=JS_ARGV(cx, arglist);
 	int32		mode=0;
@@ -3518,8 +3487,7 @@ js_post_msg(JSContext *cx, uintN argc, jsval *arglist)
 	uintN		n;
 	JSObject*	hdrobj;
 	sbbs_t*		sbbs;
-	smb_t*		resmb = NULL;
-	smbmsg_t*	remsg = NULL;
+	smbmsg_t*	remsg=NULL;
 	smbmsg_t	msg;
 	jsrefcount	rc;
 
@@ -3542,17 +3510,15 @@ js_post_msg(JSContext *cx, uintN argc, jsval *arglist)
 		}
 		else if(JSVAL_IS_OBJECT(argv[n])) {
 			if((hdrobj=JSVAL_TO_OBJECT(argv[n]))==NULL)
-				return JS_FALSE;
-			if(!js_GetMsgHeaderObjectPrivates(cx, hdrobj, &resmb, &remsg)) {
-				if(!js_ParseMsgHeaderObject(cx, hdrobj, &msg))
-					return JS_FALSE;
-				remsg = &msg;
-			}
+				return(JS_TRUE);
+			remsg=&msg;
+			if(!js_ParseMsgHeaderObject(cx,hdrobj,remsg))
+				return(JS_FALSE);
 		}
 	}
 
 	rc=JS_SUSPENDREQUEST(cx);
-	JS_SET_RVAL(cx, arglist, BOOLEAN_TO_JSVAL(sbbs->postmsg(subnum, mode, resmb, remsg)));
+	JS_SET_RVAL(cx, arglist, BOOLEAN_TO_JSVAL(sbbs->postmsg(subnum,remsg,mode)));
 	smb_freemsgmem(&msg);
 	JS_RESUMEREQUEST(cx, rc);
 
@@ -4101,12 +4067,12 @@ static jsSyncMethodSpec js_bbs_functions[] = {
 	"(see <tt>MAIL_*</tt> in <tt>sbbsdefs.js</tt> for valid <i>which</i> values)")
 	,310
 	},
-	{"email",			js_email,			1,	JSTYPE_BOOLEAN,	JSDOCSTR("to_user_number [,mode=<tt>WM_EMAIL</tt>] [,top=<i>none</i>] [,subject=<i>none</i>] [,object reply_header]")
-	,JSDOCSTR("send private e-mail to a local user (<i>reply_header</i> added in v3.17c)")
+	{"email",			js_email,			1,	JSTYPE_BOOLEAN,	JSDOCSTR("to_user_number [,mode=<tt>WM_EMAIL</tt>] [,top=<i>none</i>] [,subject=<i>none</i>]")
+	,JSDOCSTR("send private e-mail to a local user")
 	,310
 	},
-	{"netmail",			js_netmail,			1,	JSTYPE_BOOLEAN,	JSDOCSTR("address [,mode=<tt>WM_NONE</tt>] [,subject=<i>none</i>] [,object reply_header]")
-	,JSDOCSTR("send private netmail (<i>reply_header</i> added in v3.17c)")
+	{"netmail",			js_netmail,			1,	JSTYPE_BOOLEAN,	JSDOCSTR("address [,mode=<tt>WM_NONE</tt>] [,subject=<i>none</i>]")
+	,JSDOCSTR("send private netmail")
 	,310
 	},
 	{"bulk_mail",		js_bulkmail,		0,	JSTYPE_VOID,	JSDOCSTR("[ars]")
@@ -4136,11 +4102,11 @@ static jsSyncMethodSpec js_bbs_functions[] = {
 	,JSDOCSTR("list extended file information for files in the specified file directory")
 	,310
 	},
-	{"post_msg",		js_post_msg,		1,	JSTYPE_BOOLEAN,	JSDOCSTR("[sub-board=<i>current</i>] [,mode=<tt>WM_NONE</tt>] [,object reply_header]")
+	{"post_msg",		js_postmsg,			1,	JSTYPE_BOOLEAN,	JSDOCSTR("[sub-board=<i>current</i>] [,mode=<tt>WM_MODE</tt>] [,object reply_header]")
 	,JSDOCSTR("post a message in the specified message sub-board (number or internal code) "
 		"with optinal <i>mode</i> (bitfield)<br>"
 		"If <i>reply_header</i> is specified (a header object returned from <i>MsgBase.get_msg_header()</i>), that header "
-		"will be used for the in-reply-to header fields.")
+		"will be used for the in-reply-to header fields (this argument added in v3.13)")
 	,313
 	},
 	{"cfg_msg_scan",	js_msgscan_cfg,		0,	JSTYPE_VOID,	JSDOCSTR("[type=<tt>SCAN_CFG_NEW</tt>]")
--- sbbs3/un_rep.cpp
+++ sbbs3/un_rep.cpp
@@ -1,7 +1,7 @@
 /* Synchronet QWK replay (REP) packet unpacking routine */
 // vi: tabstop=4
 
-/* $Id: un_rep.cpp,v 1.73 2019/02/17 06:25:27 rswindell Exp $ */
+/* $Id: un_rep.cpp,v 1.69 2019/01/18 01:56:54 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -58,6 +58,7 @@ bool sbbs_t::unpack_rep(char* repfile)
 	ulong	errors = 0;
 	node_t	node;
 	FILE*	rep;
+	FILE*	fp;
 	DIR*	dir;
 	DIRENT*	dirent;
 	smbmsg_t	msg;
@@ -156,20 +157,21 @@ bool sbbs_t::unpack_rep(char* repfile)
 		logline(LOG_NOTICE,nulstr,"Incorrect QWK BBS ID");
 		return(false); 
 	}
+	logline("U+","Uploaded REP packet");
 	/********************/
 	/* Process messages */
 	/********************/
-	if(online == ON_REMOTE) {
-		logline("U+","Uploaded REP packet");
-		bputs(text[QWKUnpacking]);
-	}
+	bputs(text[QWKUnpacking]);
 
 	ip_can=trashcan_list(&cfg,"ip");
 	host_can=trashcan_list(&cfg,"host");
 	subject_can=trashcan_list(&cfg,"subject");
 
 	SAFEPRINTF(fname,"%stwitlist.cfg",cfg.ctrl_dir);
-	twit_list = findstr_list(fname);
+	if((fp=fopen(fname,"r"))!=NULL) {
+		twit_list=strListReadFile(fp,NULL,128);
+		fclose(fp);
+	}
 
 	now=time(NULL);
 	for(l=QWK_BLOCK_LEN;l<size;l+=blocks*QWK_BLOCK_LEN) {
@@ -200,8 +202,7 @@ bool sbbs_t::unpack_rep(char* repfile)
 			lprintf(LOG_WARNING
 				, "%s msg blocks less than 2 (read '%c' at offset %ld, '%s' at offset %ld)"
 				, getfname(msg_fname), block[0], l, tmp, l + 116);
-			if(l > QWK_BLOCK_LEN)
-				errors++;
+			errors++;
 			blocks=1;
 			continue;
 		}
@@ -244,14 +245,10 @@ bool sbbs_t::unpack_rep(char* repfile)
 			continue;
 		}
 
-		if(confnum == 0) {						/* E-mail */
-			if(msg.from == NULL)
-				bprintf("E-mail to %s: %s\r\n", msg.to, msg.subj);
-			else
-				bprintf("E-mail from %s to %s\r\n", msg.from, msg.to);
-			if(useron.rest&FLAG('E')) {
-				bputs(text[R_Email]);
-				continue;
+		if(confnum==0) {						/**********/
+			if(useron.rest&FLAG('E')) {         /* E-mail */
+				bputs(text[R_Email]);			/**********/
+				continue; 
 			}
 
 			if(msg.to!=NULL) {
@@ -361,8 +358,7 @@ bool sbbs_t::unpack_rep(char* repfile)
 				useron.etoday++;
 				putuserrec(&cfg,useron.number,U_ETODAY,5
 					,ultoa(useron.etoday,tmp,10));
-				if(online == ON_REMOTE)
-					bprintf(text[Emailed],username(&cfg,usernum,tmp),usernum);
+				bprintf(text[Emailed],username(&cfg,usernum,tmp),usernum);
 				SAFEPRINTF2(str,"sent QWK e-mail to %s #%d"
 					,username(&cfg,usernum,tmp),usernum);
 				logline("E+",str);
@@ -536,9 +532,8 @@ bool sbbs_t::unpack_rep(char* repfile)
 				,/* fromhub: */0,/* subnum: */n,/* touser: */0,&msg)) {
 				logon_posts++;
 				user_posted_msg(&cfg, &useron, 1);
-				if(online == ON_REMOTE)
-					bprintf(text[Posted],cfg.grp[cfg.sub[n]->grp]->sname
-						,cfg.sub[n]->lname);
+				bprintf(text[Posted],cfg.grp[cfg.sub[n]->grp]->sname
+					,cfg.sub[n]->lname);
 				SAFEPRINTF2(str,"posted QWK message on %s %s"
 					,cfg.grp[cfg.sub[n]->grp]->sname,cfg.sub[n]->lname);
 				signal_sub_sem(&cfg,n);
@@ -652,15 +647,12 @@ bool sbbs_t::unpack_rep(char* repfile)
 		ftouch(fname);
 	}
 
-	if(online == ON_REMOTE) {
-		bputs(text[QWKUnpacked]);
-		CRLF;
-		/**********************************************/
-		/* Hang-up now if that's what the user wanted */
-		/**********************************************/
-		autohangup();
-	} else
-		lprintf(LOG_INFO, "Unpacking completed: %s", rep_fname);
+	bputs(text[QWKUnpacked]);
+	CRLF;
+	/**********************************************/
+	/* Hang-up now if that's what the user wanted */
+	/**********************************************/
+	autohangup();
 
 	return errors == 0;
 }
