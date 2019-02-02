13b1cba918af405b0a6eda15171cd70cb2a90177
rswindell <rswindell>
2019-02-02 23:21:24 +0000

    When printing the CantDeleteMsg text line, incude the message number. The (O)perator (D)elete message (range) command will now skip permanent msgs. Fixed bug with (L)ist msgs command while in (S)earch for Your Messages mode (the msg numbers listed did not match the msg numbers used by the Read prompt).

--- sbbs3/scfg/scfgxfr2.c
+++ sbbs3/scfg/scfgxfr2.c
@@ -1,4 +1,4 @@
-/* $Id: scfgxfr2.c,v 1.61 2019/08/06 03:29:21 rswindell Exp $ */
+/* $Id: scfgxfr2.c,v 1.56 2019/01/12 12:09:16 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -414,50 +414,34 @@ void xfer_cfg()
 					break;
 				case __COUNTER__:
 					uifc.helpbuf=lib_long_name_help;
-					SAFECOPY(str, cfg.lib[i]->lname);
-					if(uifc.input(WIN_MID|WIN_SAV,0,0,"Name to use for Listings"
-						,str,LEN_GLNAME,K_EDIT) > 0)
-						SAFECOPY(cfg.lib[i]->lname,str);
+					strcpy(str,cfg.lib[i]->lname);	/* save */
+					if(!uifc.input(WIN_MID|WIN_SAV,0,0,"Name to use for Listings"
+						,cfg.lib[i]->lname,LEN_GLNAME,K_EDIT))
+						strcpy(cfg.lib[i]->lname,str);	/* restore */
 					break;
 				case __COUNTER__:
 					uifc.helpbuf=lib_short_name_help;
-					SAFECOPY(str, cfg.lib[i]->sname);
-					if(uifc.input(WIN_MID|WIN_SAV,0,0,"Name to use for Prompts"
-						,str,LEN_GSNAME,K_EDIT) > 0)
-						SAFECOPY(cfg.lib[i]->sname,str);
+					uifc.input(WIN_MID|WIN_SAV,0,0,"Name to use for Prompts"
+						,cfg.lib[i]->sname,LEN_GSNAME,K_EDIT);
 					break;
 				case __COUNTER__:
-				{
-					char code_prefix[LEN_CODE+1];
-					SAFECOPY(code_prefix, cfg.lib[i]->code_prefix);
 					uifc.helpbuf=lib_code_prefix_help;
-					if(uifc.input(WIN_MID|WIN_SAV,0,17,"Internal Code Prefix"
-						,code_prefix,LEN_CODE,K_EDIT|K_UPPER) < 0)
-						continue;
-					if(code_prefix[0] == 0 || code_ok(code_prefix)) {
-						SAFECOPY(cfg.lib[i]->code_prefix, code_prefix);
-					} else {
-						uifc.helpbuf = invalid_code;
-						uifc.msg("Invalid Code Prefix");
-					}
+					uifc.input(WIN_MID|WIN_SAV,0,17,"Internal Code Prefix"
+						,cfg.lib[i]->code_prefix,LEN_CODE,K_EDIT|K_UPPER);
 					break;
-				}
 				case __COUNTER__:
 					uifc.helpbuf=
 						"`Parent Directory:`\n"
 						"\n"
 						"This an optional path to be used as the physical \"parent\" directory for \n"
 						"all logical directories in this library. This parent directory will be\n"
-						"used in combination with each directory's `Transfer File Path` to create\n"
-						"the full physical storage path for files in each directory.\n"
+						"used in combination with each directory's storage path to create the\n"
+						"full physical storage path for files in this directory.\n"
 						"\n"
 						"This option is convenient for adding libraries with many directories\n"
 						"that share a common parent directory (e.g. CD-ROMs) and gives you the\n"
 						"option of easily changing the common parent directory location later, if\n"
 						"desired.\n"
-						"\n"
-						"The parent directory is not used for directories with a full/absolute\n"
-						"`Transfer File Path` configured."
 					;
 					uifc.input(WIN_MID|WIN_SAV,0,0,"Parent Directory"
 						,cfg.lib[i]->parent_path,sizeof(cfg.lib[i]->parent_path)-1,K_EDIT);
@@ -967,22 +951,6 @@ void dir_cfg(uint libnum)
 		"`Note:` The internal code is constructed from the file library's code prefix\n"
 		"(if present) and the directory's code suffix.\n"
 		;
-	char* dir_transfer_path_help =
-		"`Transfer File Path:`\n"
-		"\n"
-		"This is the default storage path for files uploaded-to and available for\n"
-		"download-from this directory.\n"
-		"\n"
-		"If this setting is blank, the internal-code (lower-cased) is used as the\n"
-		"default directory name.\n"
-		"\n"
-		"If this value is not a full/absolute path, the parent directory will be\n"
-		"either the library's `Parent Directory` (if set) or the data directory\n"
-		"(e.g. ../data/dirs)\n"
-		"\n"
-		"This path can be overridden on a per-file basis using `Alternate File\n"
-		"Paths`.\n"
-		;
 
 	while(1) {
 		if(uifc.changes && cfg.lib[libnum]->sort)
@@ -1063,20 +1031,27 @@ void dir_cfg(uint libnum)
 				uifc.helpbuf=0;
 				continue;
 			}
-			SAFECOPY(path,code);
-			strlwr(path);
-			uifc.helpbuf = dir_transfer_path_help;
-			uifc.input(WIN_MID|WIN_SAV,0,0,"File Transfer Path", path, LEN_DIR,K_EDIT);
+			if(cfg.lib[libnum]->parent_path[0])
+				SAFECOPY(path,code);
+			else
+				sprintf(path,"%sdirs/%s",cfg.data_dir,code);
+			uifc.helpbuf=
+				"`Directory File Path:`\n"
+				"\n"
+				"This is the drive and directory where your uploads to and downloads from\n"
+				"this directory will be stored. Example: `C:\\XFER\\GAMES`\n"
+			;
+			uifc.input(WIN_MID|WIN_SAV,0,0,"Directory File Path", path, LEN_DIR,K_EDIT);
 
 			if (!new_dir(dirnum[i], libnum))
 				continue;
 
 			if(stricmp(str2,"OFFLINE") == 0)
 				cfg.dir[dirnum[i]]->misc = 0;
-			SAFECOPY(cfg.dir[dirnum[i]]->code_suffix,code);
-			SAFECOPY(cfg.dir[dirnum[i]]->lname,str);
-			SAFECOPY(cfg.dir[dirnum[i]]->sname,str2);
-			SAFECOPY(cfg.dir[dirnum[i]]->path,path);
+			strcpy(cfg.dir[dirnum[i]]->code_suffix,code);
+			strcpy(cfg.dir[dirnum[i]]->lname,str);
+			strcpy(cfg.dir[dirnum[i]]->sname,str2);
+			strcpy(cfg.dir[dirnum[i]]->path,path);
 			uifc.changes=1;
 			continue;
 		}
@@ -1148,24 +1123,23 @@ void dir_cfg(uint libnum)
 				,cfg.dir[i]->op_arstr);
 			sprintf(opt[n++],"%-27.27s%.40s","Exemption Requirements"
 				,cfg.dir[i]->ex_arstr);
-			SAFECOPY(path, cfg.dir[i]->path);
-			if(!path[0]) {
-				SAFEPRINTF2(path, "%s%s", cfg.lib[cfg.dir[i]->lib]->code_prefix, cfg.dir[i]->code_suffix);
-				strlwr(path);
-			}
-			if(cfg.lib[cfg.dir[i]->lib]->parent_path[0])
-				prep_dir(cfg.lib[cfg.dir[i]->lib]->parent_path, path, sizeof(path));
-			else {
-				if (!cfg.dir[i]->data_dir[0])
-					SAFEPRINTF(data_dir, "%sdirs", cfg.data_dir);
+			if(cfg.dir[i]->path[0]) {
+				SAFECOPY(str, cfg.dir[i]->path);
+				if(cfg.lib[cfg.dir[i]->lib]->parent_path[0])
+					prep_dir(cfg.lib[cfg.dir[i]->lib]->parent_path, str, sizeof(str));
+				else 
+					prep_dir(cfg.ctrl_dir, str, sizeof(str));
+			} else {
+				if (!cfg.dir[dirnum[i]]->data_dir[0])
+					SAFEPRINTF(data_dir, "%sdirs/", cfg.data_dir);
 				else
-					SAFECOPY(data_dir, cfg.dir[i]->data_dir);
-				prep_dir(data_dir, path, sizeof(path));
+					SAFECOPY(data_dir, cfg.dir[dirnum[i]]->data_dir);
+				backslash(data_dir);
+				SAFEPRINTF3(str, "[%s%s%s/]"
+					,data_dir 
+					,cfg.lib[cfg.dir[i]->lib]->code_prefix, cfg.dir[i]->code_suffix);
 			}
-			if(strcmp(path, cfg.dir[i]->path) == 0)
-				SAFECOPY(str, path);
-			else
-				SAFEPRINTF(str, "[%s]", path);
+			strlwr(str);
 			sprintf(opt[n++],"%-27.27s%.40s","Transfer File Path"
 				,str);
 			sprintf(opt[n++],"%-27.27s%u","Maximum Number of Files"
@@ -1196,25 +1170,23 @@ void dir_cfg(uint libnum)
 					break;
 				case 0:
 					uifc.helpbuf=dir_long_name_help;
-					SAFECOPY(str, cfg.dir[i]->lname);
-					if(uifc.input(WIN_L2R|WIN_SAV,0,17,"Name to use for Listings"
-						,str,LEN_SLNAME,K_EDIT) > 0)
-						SAFECOPY(cfg.dir[i]->lname, str);
+					strcpy(str,cfg.dir[i]->lname);	/* save */
+					if(!uifc.input(WIN_L2R|WIN_SAV,0,17,"Name to use for Listings"
+						,cfg.dir[i]->lname,LEN_SLNAME,K_EDIT))
+						strcpy(cfg.dir[i]->lname,str);
 					break;
 				case 1:
 					uifc.helpbuf=dir_short_name_help;
-					SAFECOPY(str, cfg.dir[i]->sname);
-					if(uifc.input(WIN_L2R|WIN_SAV,0,17,"Name to use for Prompts"
-						,str,LEN_SSNAME,K_EDIT) > 0)
-						SAFECOPY(cfg.dir[i]->sname, str);
+					uifc.input(WIN_L2R|WIN_SAV,0,17,"Name to use for Prompts"
+						,cfg.dir[i]->sname,LEN_SSNAME,K_EDIT);
 					break;
 				case 2:
 					uifc.helpbuf=dir_code_help;
-					SAFECOPY(str,cfg.dir[i]->code_suffix);
+					strcpy(str,cfg.dir[i]->code_suffix);
 					uifc.input(WIN_L2R|WIN_SAV,0,17,"Internal Code Suffix (unique)"
 						,str,LEN_CODE,K_EDIT|K_UPPER);
 					if(code_ok(str))
-						SAFECOPY(cfg.dir[i]->code_suffix,str);
+						strcpy(cfg.dir[i]->code_suffix,str);
 					else {
 						uifc.helpbuf=invalid_code;
 						uifc.msg("Invalid Code");
@@ -1242,8 +1214,18 @@ void dir_cfg(uint libnum)
 					getar(str,cfg.dir[i]->ex_arstr);
 					break;
 				case 8:
-					uifc.helpbuf = dir_transfer_path_help;
-					uifc.input(WIN_L2R|WIN_SAV,0,17,"Transfer File Path"
+					uifc.helpbuf=
+						"`File Path:`\n"
+						"\n"
+						"This is the default storage path for files uploaded to this directory.\n"
+						"If this path is blank, files are stored in a directory off of the\n"
+						"`data/dirs` directory using the internal code of this directory as the\n"
+						"name of the sub-directory (i.e. `data/dirs/<CODE>`).\n"
+						"\n"
+						"This path can be overridden on a per file basis using `Alternate File\n"
+						"Paths`.\n"
+					;
+					uifc.input(WIN_L2R|WIN_SAV,0,17,"File Path"
 						,cfg.dir[i]->path,sizeof(cfg.dir[i]->path)-1,K_EDIT);
 					break;
 				case 9:
