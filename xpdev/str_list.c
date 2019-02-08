989fcd852bf7cf150ce2f9342f6d7ea4a1dd90de
rswindell <rswindell>
2019-02-08 02:13:32 +0000

    New function: strListModifyEach(), iterates through a string list, calling a caller-supplied function (callback) which can modify each string or leave the string in tact by returning NULL. Shrinking strings is trivial while expanding strings (making them bigger) is a little more involved, probably using the cbdata to pass a temporary buffer around to store each expanded string or returning a new globally-valid pointer. Removed the malloc.h #include as we haven't used alloca() here for a while.

--- xpdev/str_list.c
+++ xpdev/str_list.c
@@ -2,7 +2,7 @@
 
 /* Functions to deal with NULL-terminated string lists */
 
-/* $Id: str_list.c,v 1.49 2018/03/09 06:11:37 deuce Exp $ */
+/* $Id: str_list.c,v 1.51 2019/02/08 02:13:32 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -37,9 +37,6 @@
 
 #include <stdlib.h>		/* malloc and qsort */
 #include <string.h>		/* strtok */
-#if defined(_WIN32)
- #include <malloc.h>    /* alloca() on Win32 */
-#endif
 #include "genwrap.h"	/* stricmp */
 #include "str_list.h"
 
@@ -199,6 +196,22 @@ char* DLLCALL strListReplace(const str_list_t list, size_t index, const char* st
 	strcpy(buf,str);
 
 	return(buf);
+}
+
+size_t DLLCALL strListModifyEach(const str_list_t list, char*(modify(size_t, char*, void*)), void* cbdata)
+{
+	size_t	i;
+	for(i = 0; list[i] != NULL; i++) {
+		char* str = modify(i, list[i], cbdata);
+		if(str == NULL || str == list[i])	// Same old pointer (or NULL), no realloc() needed
+			continue;
+		char* p = realloc(list[i], strlen(str) + 1);
+		if(p == NULL)
+			break;
+		list[i] = p;
+		strcpy(p, str);
+	}
+	return i;
 }
 
 BOOL DLLCALL strListSwap(const str_list_t list, size_t index1, size_t index2)
@@ -481,7 +494,7 @@ void DLLCALL strListFreeStrings(str_list_t list)
 
 void DLLCALL strListFree(str_list_t* list)
 {
-	if(*list!=NULL) {
+	if(list != NULL && *list != NULL) {
 		strListFreeStrings(*list);
 		FREE_AND_NULL(*list);
 	}
