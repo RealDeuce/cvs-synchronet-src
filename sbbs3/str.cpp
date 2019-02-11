b4512aed810e98b52f77101fde7d142a50ce6b30
rswindell <rswindell>
2019-02-11 23:07:29 +0000

    Patch from Mark Lewis (wk42):
    i've made a few adjustments to xtrn.cpp for external.bat...
    
    - remove unneeded redirection of dosemu output.
       still uses same log file name and stops zero
       byte file dosemu.log from being created in node
       directories.
    - fix faulty "keystroke" dosemu -I command. it
       didn't actually do the "\r" but sent it as
       characters to the command line.
    - added code for SBBS specific lredirs to be
       done more cleanly like dosemu's default
       autoexec.bat. only deletes redir if it exists.
    
    His editor also appears to have removed trailling white-space from this file,
    which is fine.

--- sbbs3/str.cpp
+++ sbbs3/str.cpp
@@ -1,6 +1,6 @@
 /* Synchronet high-level string i/o routines */
 
-/* $Id: str.cpp,v 1.79 2018/02/20 11:43:08 rswindell Exp $ */
+/* $Id: str.cpp,v 1.83 2018/10/26 03:33:14 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -61,7 +61,10 @@ void sbbs_t::userlist(long mode)
 		if(sort && (online==ON_LOCAL || !rioctl(TXBC)))
 			bprintf("%-4d\b\b\b\b",i);
 		user.number=i;
-		fgetuserdat(&cfg, &user, userfile);
+		if(fgetuserdat(&cfg, &user, userfile) != 0)
+			continue;
+		if(user.alias[0] <= ' ')
+			continue;
 		if(user.misc&(DELETED|INACTIVE))
 			continue;
 		users++;
@@ -852,7 +855,6 @@ char* sbbs_t::datestr(time_t t)
 void sbbs_t::sys_info()
 {
 	char	tmp[128];
-	char	path[MAX_PATH+1];
 	uint	i;
 	stats_t stats;
 
@@ -878,15 +880,15 @@ void sbbs_t::sys_info()
 	bprintf(text[SiTotalTime],ultoac(stats.timeon,tmp));
 	bprintf(text[SiTimeToday],ultoac(stats.ttoday,tmp));
 	ver();
-	SAFEPRINTF(path, "%ssystem.msg", cfg.text_dir);
-	if(fexistcase(path) && text[ViewSysInfoFileQ][0] && yesno(text[ViewSysInfoFileQ])) {
+	const char* fname = "../system";
+	if(menu_exists(fname) && text[ViewSysInfoFileQ][0] && yesno(text[ViewSysInfoFileQ])) {
 		CLS;
-		printfile(path,0); 
+		menu(fname);
 	}
-	SAFEPRINTF(path, "%smenu/logon.asc", cfg.text_dir);
-	if(fexistcase(path) && text[ViewLogonMsgQ][0] && yesno(text[ViewLogonMsgQ])) {
+	fname = "logon";
+	if(menu_exists(fname) && text[ViewLogonMsgQ][0] && yesno(text[ViewLogonMsgQ])) {
 		CLS;
-		menu("logon"); 
+		menu(fname);
 	}
 }
 
@@ -939,15 +941,13 @@ void sbbs_t::user_info()
 void sbbs_t::xfer_policy()
 {
 	if(!usrlibs) return;
-	if(menu_exists("tpolicy"))
-		menu("tpolicy");
-	else {
+	if(!menu("tpolicy", P_NOERROR)) {
 		bprintf(text[TransferPolicyHdr],cfg.sys_name);
 		bprintf(text[TpUpload]
 			,cfg.dir[usrdir[curlib][curdir[curlib]]]->up_pct);
 		bprintf(text[TpDownload]
 			,cfg.dir[usrdir[curlib][curdir[curlib]]]->dn_pct);
-		}
+	}
 }
 
 const char* prot_menu_file[] = {
@@ -960,8 +960,7 @@ const char* prot_menu_file[] = {
 
 void sbbs_t::xfer_prot_menu(enum XFER_TYPE type)
 {
-	if(menu_exists(prot_menu_file[type])) {
-		menu(prot_menu_file[type]);
+	if(menu(prot_menu_file[type], P_NOERROR)) {
 		return;
 	}
 
@@ -1043,7 +1042,7 @@ void sbbs_t::logonlist(void)
 		bputs(text[NoOneHasLoggedOnToday]); 
 	} else {
 		bputs(text[CallersToday]);
-		printfile(str,P_NOATCODES|P_OPENCLOSE);
+		printfile(str,P_NOATCODES|P_OPENCLOSE|P_TRUNCATE);
 		CRLF; 
 	}
 }
@@ -1067,7 +1066,7 @@ bool sbbs_t::spy(uint i /* node_num */)
 		return(false);
 	}
 	if(spy_socket[i-1]!=INVALID_SOCKET) {
-		bprintf("Node %d already being spied (%lx)\r\n",i,spy_socket[i-1]);
+		bprintf("Node %d already being spied (%x)\r\n",i,spy_socket[i-1]);
 		return(false);
 	}
 	bprintf("*** Synchronet Remote Spy on Node %d: Ctrl-C to Abort ***"
