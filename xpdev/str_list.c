65886b7716167c7a9cf687b4fd66846d0488e236
rswindell <rswindell>
2019-02-12 03:14:48 +0000

    Restore the BRE inter-bbs netmail attachment work-around from SBBSecho v2: If the attached file begins with a '^', ignore/skip that character in the file path. This should resolve the error reported by Ken  of Section One BBS: ERROR line 850, attachment file not found: ^C:/SBBS/XTRN/BRE777/...
    SBBSecho v2.x included this work-around and it was not included in the v3
    re-factoring/re-write. Of course, BRE is doing something wrong and is totally
    at fault here, but no one expects BRE to be fixed... ever. :-)

--- xpdev/str_list.c
+++ xpdev/str_list.c
@@ -2,7 +2,7 @@
 
 /* Functions to deal with NULL-terminated string lists */
 
-/* $Id: str_list.c,v 1.53 2019/02/14 09:48:25 rswindell Exp $ */
+/* $Id: str_list.c,v 1.52 2019/02/08 23:35:41 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -202,14 +202,15 @@ size_t DLLCALL strListModifyEach(const str_list_t list, char*(modify(size_t, cha
 {
 	size_t	i;
 	for(i = 0; list[i] != NULL; i++) {
+		char* p;
 		char* str = modify(i, list[i], cbdata);
-		if(str == NULL || str == list[i])	// Same old pointer (or NULL), no modification
+		if(str == NULL || str == list[i])	// Same old pointer (or NULL), no realloc() needed
 			continue;
-		str = strdup(str);
-		if(str == NULL)
+		p = realloc(list[i], strlen(str) + 1);
+		if(p == NULL)
 			break;
-		free(list[i]);
-		list[i] = str;
+		list[i] = p;
+		strcpy(p, str);
 	}
 	return i;
 }
