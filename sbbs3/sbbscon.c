a6670d67da784ad22e567296bff91cfd0b178c4c
rswindell <rswindell>
2019-02-15 06:26:05 +0000

    Use the UpTimer trick to get around problems calling DLL functions from OnClick events when not in "appy-time".

--- sbbs3/sbbscon.c
+++ sbbs3/sbbscon.c
@@ -1,6 +1,6 @@
 /* Synchronet vanilla/console-mode "front-end" */
 
-/* $Id: sbbscon.c,v 1.272 2018/12/12 20:27:31 rswindell Exp $ */
+/* $Id: sbbscon.c,v 1.273 2019/01/08 00:13:06 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -1355,7 +1355,7 @@ static void show_usage(char *cmd)
 /****************************************************************************/
 /* Main Entry Point															*/
 /****************************************************************************/
-#ifdef BUILD_JSDOCS
+#if defined(BUILD_JSDOCS) && defined(WITH_SDL)
 int CIOLIB_main(int argc, char** argv)
 #else
 int main(int argc, char** argv)
