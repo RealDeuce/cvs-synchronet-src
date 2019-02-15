890c8c8a864c2df9f27e0b5933507699f2e7ddff
rswindell <rswindell>
2019-02-15 00:06:56 +0000

    When saving message sub-boards and file directories to msgs.cnf/file.cnf, if any sub-boards or directories have *blank* names or internal code suffixes (a sign of a corrupted configuration), exclude them from the saved subs/dirs automatically. I'm not sure how this can happen (use of cnflib.js?) - but running "scfg -f" (force save) should fix this situation. For Android8675.

--- sbbs3/sbbsdefs.h
+++ sbbs3/sbbsdefs.h
@@ -1,6 +1,6 @@
 /* Synchronet constants, macros, and structure definitions */
 
-/* $Id: sbbsdefs.h,v 1.229 2018/12/30 04:33:48 rswindell Exp $ */
+/* $Id: sbbsdefs.h,v 1.231 2019/01/04 22:05:55 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -50,15 +50,15 @@
 /*************/
 
 #define VERSION 	"3.17"  /* Version: Major.minor  */
-#define REVISION	'a'     /* Revision: lowercase letter */
+#define REVISION	'c'     /* Revision: lowercase letter */
 #define VERSION_NUM	(31700	 + (tolower(REVISION)-'a'))
 #define VERSION_HEX	(0x31700 + (tolower(REVISION)-'a'))
 
 #define VERSION_NOTICE		"Synchronet BBS for " PLATFORM_DESC\
 								"  Version " VERSION
 #define SYNCHRONET_CRC		0x9BCDD162
-#define COPYRIGHT_NOTICE	"Copyright 2018 Rob Swindell"
-#define COPYRIGHT_CRC		0x930AE2A9
+#define COPYRIGHT_NOTICE	"Copyright 2019 Rob Swindell"
+#define COPYRIGHT_CRC		0x0E0503DF
 
 #define Y2K_2DIGIT_WINDOW	70
 
