3cf8afd2ea46311aef1ff79b3ab2973dd0a1c81f
rswindell <rswindell>
2019-02-15 06:26:05 +0000

    Use the UpTimer trick to get around problems calling DLL functions from OnClick events when not in "appy-time".

--- sbbs3/sbbscon.c
+++ sbbs3/sbbscon.c
@@ -1,6 +1,6 @@
 /* Synchronet vanilla/console-mode "front-end" */
 
-/* $Id: sbbscon.c,v 1.270 2018/03/18 03:48:38 rswindell Exp $ */
+/* $Id: sbbscon.c,v 1.273 2019/01/08 00:13:06 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -191,7 +191,6 @@ static const char* telnet_usage  = "Terminal server settings:\n\n"
 							"\tto<value>  set Terminal server options value (advanced)\n"
 							"\tta         enable auto-logon via IP address\n"
 							"\ttd         enable Telnet command debug output\n"
-							"\ttc         enable sysop availability for chat\n"
 							"\ttq         disable QWK events\n"
 							"\tt-         disable Terminal server\n"
 							;
@@ -1356,7 +1355,7 @@ static void show_usage(char *cmd)
 /****************************************************************************/
 /* Main Entry Point															*/
 /****************************************************************************/
-#ifdef BUILD_JSDOCS
+#if defined(BUILD_JSDOCS) && defined(WITH_SDL)
 int CIOLIB_main(int argc, char** argv)
 #else
 int main(int argc, char** argv)
@@ -1642,9 +1641,6 @@ int main(int argc, char** argv)
 					case 'Q': /* No QWK events */
 						bbs_startup.options|=BBS_OPT_NO_QWK_EVENTS;
 						break;
-					case 'C': /* Sysop available for chat */
-						bbs_startup.options|=BBS_OPT_SYSOP_AVAILABLE;
-						break;
 					case 'O': /* Set options */
 						bbs_startup.options=strtoul(arg,NULL,0);
 						break;
@@ -1899,7 +1895,7 @@ int main(int argc, char** argv)
     SAFECOPY(scfg.ctrl_dir,bbs_startup.ctrl_dir);
 
 	if(chdir(scfg.ctrl_dir)!=0)
-		lprintf(LOG_ERR,"!ERROR %d changing directory to: %s", errno, scfg.ctrl_dir);
+		lprintf(LOG_ERR,"!ERROR %d (%s) changing directory to: %s", errno, strerror(errno), scfg.ctrl_dir);
 
     scfg.size=sizeof(scfg);
 	SAFECOPY(error,UNKNOWN_LOAD_ERROR);
@@ -1950,7 +1946,7 @@ int main(int argc, char** argv)
 
 		lprintf(LOG_INFO,"Running as daemon");
 		if(daemon(TRUE,FALSE))  { /* Daemonize, DON'T switch to / and DO close descriptors */
-			lprintf(LOG_ERR,"!ERROR %d running as daemon",errno);
+			lprintf(LOG_ERR,"!ERROR %d (%s) running as daemon", errno, strerror(errno));
 			is_daemon=FALSE;
 		}
 	}
