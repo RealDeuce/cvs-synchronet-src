3a5310724626c9ad826c390728186af0ad6b1c24
deuce <deuce>
2019-03-07 01:11:00 +0000

    There appears to be data corruption in cryptlib if a private key is added to a second thread before the first has the session set active.  Add calls to lock/unlock the certificate to prevent this.
    The better options is likely to have a function that adds the key and socket
    and sets the session active in one call and handles the locking internally.
    
    But I'm lazy, so we get the lock functions.

--- sbbs3/scfg/scfg.c
+++ sbbs3/scfg/scfg.c
@@ -1,6 +1,6 @@
 /* Synchronet configuration utility 										*/
 
-/* $Id: scfg.c,v 1.104 2019/07/13 21:09:26 rswindell Exp $ */
+/* $Id: scfg.c,v 1.101 2019/01/12 12:09:15 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -46,7 +46,7 @@
 scfg_t	cfg;    /* Synchronet Configuration */
 uifcapi_t uifc; /* User Interface (UIFC) Library API */
 
-BOOL forcesave=FALSE;
+BOOL no_dirchk=FALSE,forcesave=FALSE;
 BOOL new_install=FALSE;
 static BOOL auto_save=FALSE;
 extern BOOL all_msghdr;
@@ -223,6 +223,9 @@ int main(int argc, char **argv)
 				case 'U':
 					umask(strtoul(argv[i]+2,NULL,8));
 					break;
+                case 'S':
+        			no_dirchk=!no_dirchk;
+                    break;
 				case 'G':
 					if(isalpha(argv[i][2]))
 						grpname = argv[i]+2;
@@ -249,17 +252,18 @@ int main(int argc, char **argv)
 						case 'A':
 							ciolib_mode=CIOLIB_MODE_ANSI;
 							break;
-#if defined __unix__
 						case 'C':
 							ciolib_mode=CIOLIB_MODE_CURSES;
 							break;
+						case 0:
+							printf("NOTICE: The -i option is deprecated, use -if instead\r\n");
+							SLEEP(2000);
 						case 'F':
 							ciolib_mode=CIOLIB_MODE_CURSES_IBM;
 							break;
 						case 'X':
 							ciolib_mode=CIOLIB_MODE_X;
 							break;
-#endif
 						case 'W':
 							ciolib_mode=CIOLIB_MODE_CONIO;
 							break;
@@ -283,6 +287,7 @@ int main(int argc, char **argv)
 					USAGE:
                     printf("\nusage: scfg [ctrl_dir] [options]"
                         "\n\noptions:\n\n"
+                        "-s  =  don't check directories\r\n"
                         "-f  =  force save of configuration files\r\n"
                         "-a  =  update all message base status headers\r\n"
                         "-h  =  don't update message base status headers\r\n"
@@ -786,8 +791,8 @@ BOOL save_xtrn_cfg(scfg_t* cfg, int backup_level)
 
 
 /****************************************************************************/
-/* Checks the uifc.changes variable. If there have been no changes, returns 2.	*/
-/* If there have been changes, it prompts the user to change or not. If the */
+/* Checks the uifc.changes variable. If there have been no uifc.changes, returns 2.	*/
+/* If there have been uifc.changes, it prompts the user to change or not. If the */
 /* user escapes the menu, returns -1, selects Yes, 0, and selects no, 1 	*/
 /****************************************************************************/
 int save_changes(int mode)
@@ -804,14 +809,14 @@ int save_changes(int mode)
 	strcpy(opt[1],"No");
 	opt[2][0]=0;
 	uifc.helpbuf=
-		"`Save Changes:`\n"
+		"`Save uifc.changes:`\n"
 		"\n"
-		"You have made some changes to the configuration. If you want to save\n"
-		"these changes, select `Yes`. If you are positive you DO NOT want to save\n"
-		"these changes, select `No`. If you are not sure and want to review the\n"
+		"You have made some uifc.changes to the configuration. If you want to save\n"
+		"these uifc.changes, select `Yes`. If you are positive you DO NOT want to save\n"
+		"these uifc.changes, select `No`. If you are not sure and want to review the\n"
 		"configuration before deciding, hit ~ ESC ~.\n"
 	;
-	i=uifc.list(mode|WIN_SAV,0,0,0,&i,0,"Save Changes",opt);
+	i=uifc.list(mode|WIN_ACT,0,0,0,&i,0,"Save Changes",opt);
 	if(i!=-1)
 		uifc.changes=0;
 	return(i);
