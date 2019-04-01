6ec463083e747c36c2cdde22e187f460a4a26ecf
rswindell <rswindell>
2019-04-01 21:58:15 +0000

    More detail in help for umask option (use leading 0 for octal).

--- sbbs3/newuser.cpp
+++ sbbs3/newuser.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet new user routine */
 
-/* $Id: newuser.cpp,v 1.81 2019/05/09 21:14:19 rswindell Exp $ */
+/* $Id: newuser.cpp,v 1.78 2019/02/20 05:43:19 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -166,38 +166,11 @@ BOOL sbbs_t::newuser()
 				useron.misc&=~COLOR;
 		}
 		else
-			useron.rows = TERM_ROWS_DEFAULT;
-
-		while(text[HitYourBackspaceKey][0] && !(useron.misc&(PETSCII|SWAP_DELETE)) && online) {
-			bputs(text[HitYourBackspaceKey]);
-			uchar key = getkey(K_NONE);
-			bprintf(text[CharacterReceivedFmt], key, key);
-			if(key == '\b')
-				break;
-			if(key == DEL) {
-				if(text[SwapDeleteKeyQ][0] == 0 || yesno(text[SwapDeleteKeyQ]))
-					useron.misc |= SWAP_DELETE;
-			}
-			else if(key == PETSCII_DELETE)
-				useron.misc |= (AUTOTERM|PETSCII|COLOR);
-			else {
-				bprintf(text[InvalidBackspaceKeyFmt], key, key);
-				if(text[ContinueQ][0] && !yesno(text[ContinueQ]))
-					return FALSE;
-				newline();
-			}
-		}
-
-		if(useron.misc&PETSCII) {
-			autoterm |= PETSCII;
-			outcom(PETSCII_UPPERLOWER);
-			bputs(text[PetTerminalDetected]);
-		} else {
-			if(!yesno(text[ExAsciiTerminalQ]))
-				useron.misc|=NO_EXASCII;
-			else
-				useron.misc&=~NO_EXASCII;
-		}
+			useron.rows=24;
+		if(!(useron.misc&PETSCII) && !yesno(text[ExAsciiTerminalQ]))
+			useron.misc|=NO_EXASCII;
+		else
+			useron.misc&=~NO_EXASCII;
 
 		if(rlogin_name[0])
 			SAFECOPY(useron.alias,rlogin_name);
