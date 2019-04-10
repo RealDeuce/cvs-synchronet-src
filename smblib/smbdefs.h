8abd9d0c576156b2d38713565fabf9bdceda155e
rswindell <rswindell>
2019-04-10 22:54:08 +0000

    Introduce and use convenience pointers for MIME-Version and Content-Type header fields. Eliminate smb_getcontenttype() - now unnecessary.

--- smblib/smbdefs.h
+++ smblib/smbdefs.h
@@ -1,6 +1,6 @@
 /* Synchronet message base constant and structure definitions */
 
-/* $Id: smbdefs.h,v 1.107 2019/02/18 03:09:04 rswindell Exp $ */
+/* $Id: smbdefs.h,v 1.108 2019/04/10 22:54:08 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -633,6 +633,8 @@ typedef struct {				/* Message */
 	char*		subj;			/* Subject  */
 	char*		tags;			/* Message tags (space-delimited) */
 	char*		editor;			/* Message editor (if known) */
+	char*		mime_version;	/* MIME Version (if applicable) */
+	char*		content_type;	/* MIME Content-Type (if applicable) */
 	uint16_t	to_agent,		/* Type of agent message is to */
 				from_agent, 	/* Type of agent message is from */
 				replyto_agent;	/* Type of agent replies should be sent to */
