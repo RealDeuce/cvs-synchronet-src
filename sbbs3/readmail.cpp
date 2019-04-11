d64e7f82db1f417faa7898a9a72804866cf942e5
rswindell <rswindell>
2019-04-11 10:08:04 +0000

    The *_net_addr hfields can have a valid string even when the corresponding *_net_type hfield has a value of 0 (NET_NONE) - according to smbdefs.h.

--- sbbs3/readmail.cpp
+++ sbbs3/readmail.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet private mail reading function */
 
-/* $Id: readmail.cpp,v 1.93 2019/05/02 00:58:18 rswindell Exp $ */
+/* $Id: readmail.cpp,v 1.92 2019/04/10 07:33:11 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -71,14 +71,6 @@ void sbbs_t::readmail(uint usernumber, int which, long lm_mode)
 	smbmsg_t msg;
 	char search_str[128] = "";
 
-	if(which==MAIL_SENT)
-		act=NODE_RSML;
-	else if(which==MAIL_ALL)
-		act=NODE_SYSP;
-	else
-		act=NODE_RMAL;
-	action=act;
-
 	if(cfg.readmail_mod[0] && !readmail_inside) {
 		char cmdline[256];
 
@@ -127,6 +119,13 @@ void sbbs_t::readmail(uint usernumber, int which, long lm_mode)
 
 	last=smb.status.last_msg;
 
+	if(which==MAIL_SENT)
+		act=NODE_RSML;
+	else if(which==MAIL_ALL)
+		act=NODE_SYSP;
+	else
+		act=NODE_RMAL;
+	action=act;
 	const char* order = (lm_mode&LM_REVERSE) ? "newest" : "oldest";
 	if(smb.msgs>1 && which!=MAIL_ALL) {
 		if(which==MAIL_SENT)
