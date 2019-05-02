8b59f844ab40180eb5e68b9a1512e5be827d0b24
rswindell <rswindell>
2019-05-02 09:16:45 +0000

    sprintf->SAFEPRINTF

--- sbbs3/getmsg.cpp
+++ sbbs3/getmsg.cpp
@@ -1,6 +1,6 @@
 /* Synchronet message retrieval functions */
 
-/* $Id: getmsg.cpp,v 1.89 2019/07/06 07:52:21 rswindell Exp $ */
+/* $Id: getmsg.cpp,v 1.86 2019/05/02 03:40:57 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -39,7 +39,6 @@
 /***********************************************************************/
 
 #include "sbbs.h"
-#include "utf8.h"
 
 /****************************************************************************/
 /* Loads an SMB message from the open msg base the fastest way possible 	*/
@@ -95,7 +94,6 @@ void sbbs_t::show_msgattr(smbmsg_t* msg)
 	uint16_t attr = msg->hdr.attr;
 	uint16_t poll = attr&MSG_POLL_VOTE_MASK;
 	uint32_t auxattr = msg->hdr.auxattr;
-	uint32_t netattr = msg->hdr.netattr;
 
 	bprintf(text[MsgAttr]
 		,attr&MSG_PRIVATE	? "Private  "   :nulstr
@@ -112,9 +110,9 @@ void sbbs_t::show_msgattr(smbmsg_t* msg)
 		,attr&MSG_NOREPLY	? "NoReply  "	:nulstr
 		,poll == MSG_POLL	? "Poll  "		:nulstr
 		,poll == MSG_POLL && auxattr&POLL_CLOSED ? "(Closed)  "	:nulstr
-		,auxattr&(MSG_FILEATTACH|MSG_MIMEATTACH) ? "Attach  "   :nulstr
-		,netattr&MSG_SENT						 ? "Sent  "		:nulstr
-		,netattr&MSG_INTRANSIT					 ? "InTransit  ":nulstr
+		,nulstr
+		,nulstr
+		,nulstr
 		);
 }
 
@@ -156,7 +154,7 @@ void sbbs_t::show_msghdr(smb_t* smb, smbmsg_t* msg, const char* subject, const c
 		bprintf(text[MsgSubj], current_msg_subj);
 		if(msg->tags && *msg->tags)
 			bprintf(text[MsgTags], msg->tags);
-		if(msg->hdr.attr || msg->hdr.netattr || msg->hdr.auxattr)
+		if(msg->hdr.attr)
 			show_msgattr(msg);
 		if(current_msg_to != NULL && *current_msg_to != 0) {
 			bprintf(text[MsgTo], current_msg_to);
@@ -281,17 +279,10 @@ bool sbbs_t::show_msg(smb_t* smb, smbmsg_t* msg, long p_mode, post_t* post)
 		if(p == NULL)
 			p = txt;
 		else
-			bprintf(text[MIMEDecodedPlainTextFmt]
-				, msg->text_charset == NULL ? "unspecified (US-ASCII)" : msg->text_charset
-				, msg->text_subtype);
+			bprintf(text[MIMEDecodedPlainTextFmt], msg->charset == NULL ? "US-ASCII" : msg->charset);
 	}
 	truncsp(p);
 	SKIP_CRLF(p);
-	if(smb_msg_is_utf8(msg)) {
-		if(!term_supports(UTF8))
-			utf8_normalize_str(txt);
-		p_mode |= P_UTF8;
-	}
 	putmsg(p, p_mode, msg->columns);
 	smb_freemsgtxt(txt);
 	if(column)
