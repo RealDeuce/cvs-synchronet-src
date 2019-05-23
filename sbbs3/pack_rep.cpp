04141d7f99c3ad1e444c654ee3f2aa6980a19d9b
rswindell <rswindell>
2019-05-23 00:40:03 +0000

    Increase MAX_POST_LEN from 1MB to 4MB (QWK REP packets can be > 1MB) -   I think that > 1MB post data is supported, but the http_request.post_data   property won't be created if the length > MAX_POST_LEN. Perhaps would just   store the post data in a file (uh, it already is?) and expose the filename to   JS scripts? It'd be a lot cleaner than storing the data in a file and then   reading (or mem-mapping) the file and then copying the contents into a JS   property.
    Allow the JS http_request.post_data property to contain NULs.
    
    open_post_file() will now open the post file (and return the FILE*) even if
      session->req.post_data is NULL, it just won't try to write to the file if the
      post_data is NULL.
    
    mem-map the large post data files using XPMAP_WRITE (read/write) rather than
      XPMAP_READ (read-only) - without this change, this line in read_post_data()
      would cause an exception:
        session->req.post_data[session->req.post_len]=0;
      Now, we seem to have the potential of an off-by-one here (if the length
      mem-mapped is not post_len + 1), but that isn't happening. <shrug>
    
    Fixed a couple of FILE pointer/descriptor leaks if post_to_file() failed.
    
    Changed name of post data file to SBBS_POST.*.*.data (it's not necessarily
    html).
    
    So now, uploads > 1MB work, but questions remain:
    - wouldn't PUT be a more appropriate method (than POST) for file uploads?
    - how can we support post_data > MAX_POST_LEN (now 4MB) in JS?

--- sbbs3/pack_rep.cpp
+++ sbbs3/pack_rep.cpp
@@ -1,6 +1,6 @@
 /* Synchronet QWK reply (REP) packet creation routine */
 
-/* $Id: pack_rep.cpp,v 1.47 2018/04/06 02:42:37 rswindell Exp $ */
+/* $Id: pack_rep.cpp,v 1.49 2019/04/10 00:18:10 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -152,7 +152,7 @@ bool sbbs_t::pack_rep(uint hubnum)
 			mode |= (cfg.qhub[hubnum]->misc&(QHUB_EXT|QHUB_CTRL_A));
 			/* For an unclear reason, kludge lines (including @VIA and @TZ) were not included in NetMail previously */
 			if(!(cfg.qhub[hubnum]->misc&QHUB_NOHEADERS)) mode|=(QM_VIA|QM_TZ|QM_MSGID|QM_REPLYTO);
-			msgtoqwk(&msg, rep, mode, INVALID_SUB, 0, hdrs);
+			msgtoqwk(&msg, rep, mode, &smb, /* confnum: */0, hdrs);
 			packedmail++;
 			smb_unlockmsghdr(&smb,&msg);
 			smb_freemsgmem(&msg); 
@@ -228,7 +228,7 @@ bool sbbs_t::pack_rep(uint hubnum)
 			if(msg.from_net.type!=NET_QWK)
 				mode|=QM_TAGLINE;
 
-			msgtoqwk(&msg,rep,mode,j,cfg.qhub[hubnum]->conf[i],hdrs,voting);
+			msgtoqwk(&msg, rep, mode, &smb, cfg.qhub[hubnum]->conf[i], hdrs, voting);
 
 			smb_freemsgmem(&msg);
 			smb_unlockmsghdr(&smb,&msg);
@@ -270,7 +270,7 @@ bool sbbs_t::pack_rep(uint hubnum)
 		CRLF;
 
 	if(!msgcnt && !netfiles && !packedmail && !voting_data) {
-		eprintf(LOG_INFO,remove_ctrl_a(text[QWKNoNewMessages],tmp));
+		eprintf(LOG_INFO, "%s", remove_ctrl_a(text[QWKNoNewMessages],tmp));
 		return(true);	// Changed from false Mar-11-2005 (needs to be true to save updated ptrs)
 	}
 
