584ec8311fcfcbc36d29b7a0a591c76af775c8ac
rswindell <rswindell>
2019-06-28 23:59:50 +0000

    Initial UTF-8 support.

--- sbbs3/con_out.cpp
+++ sbbs3/con_out.cpp
@@ -1,7 +1,7 @@
 /* Synchronet console output routines */
 // vi: tabstop=4
 
-/* $Id: con_out.cpp,v 1.98 2019/05/06 10:44:41 rswindell Exp $ */
+/* $Id: con_out.cpp,v 1.100 2019/06/28 23:04:48 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -41,6 +41,7 @@
 /**********************************************************************/
 
 #include "sbbs.h"
+#include "cp437_utf8_tbl.h"
 
 /****************************************************************************/
 /* Outputs a NULL terminated string locally and remotely (if applicable)    */
@@ -335,7 +336,7 @@ long sbbs_t::term_supports(long cmp_flags)
 	long flags = ((sys_status&SS_USERON) && !(useron.misc&AUTOTERM)) ? useron.misc : autoterm;
 
 	if((sys_status&SS_USERON) && (useron.misc&AUTOTERM))
-		flags |= useron.misc & (NO_EXASCII | SWAP_DELETE);
+		flags |= useron.misc & (NO_EXASCII | SWAP_DELETE | COLOR | ICE_COLOR);
 
 	return(cmp_flags ? ((flags&cmp_flags)==cmp_flags) : (flags&TERM_FLAGS));
 }
@@ -402,8 +403,13 @@ int sbbs_t::outchar(char ch)
 	else
 		outchar_esc=0;
 	long term = term_supports();
-	if((term&(PETSCII|NO_EXASCII)) == NO_EXASCII && ch&0x80)
-		ch=exascii_to_ascii_char(ch);  /* seven bit table */
+	const char* utf8 = NULL;
+	if(!(term&PETSCII)) {
+		if((term&NO_EXASCII) && (ch&0x80))
+			ch = exascii_to_ascii_char(ch);  /* seven bit table */
+		else if(term&UTF8)
+			utf8 = cp437_utf8_tbl[(uchar)ch];
+	}
 
 	if(ch==FF && lncntr > 0 && !tos) {
 		lncntr=0;
@@ -450,8 +456,14 @@ int sbbs_t::outchar(char ch)
 			outcom(pet);
 			if(pet == PETSCII_SOLID)
 				outcom(PETSCII_REVERSE_OFF);
-		} else
-			outcom(ch);
+			if(ch == '\r' && (curatr&0xf0) != 0) // reverse video is disabled upon CR
+				curatr >>= 4;
+		} else {
+			if(utf8 != NULL)
+				putcom(utf8);
+			else
+				outcom(ch);
+		}
 	}
 	if(!outchar_esc) {
 		/* Track cursor position locally */
@@ -677,7 +689,8 @@ void sbbs_t::cleartoeos(void)
 /****************************************************************************/
 void sbbs_t::ctrl_a(char x)
 {
-	char	tmp1[128],atr=curatr;
+	char	tmp1[128];
+	uint	atr = curatr;
 	struct	tm tm;
 
 	if(x && (uchar)x<=CTRL_Z) {    /* Ctrl-A through Ctrl-Z for users with MF only */
@@ -688,6 +701,9 @@ void sbbs_t::ctrl_a(char x)
 	if((uchar)x>0x7f) {
 		cursor_right((uchar)x-0x7f);
 		return;
+	}
+	if(isdigit(x)) {	/* background color */
+		atr &= (BG_BRIGHT|BLINK|0x0f);
 	}
 	switch(toupper(x)) {
 		case '!':   /* level 10 or higher */
@@ -823,9 +839,13 @@ void sbbs_t::ctrl_a(char x)
 			atr|=HIGH;
 			attr(atr);
 			break;
-		case 'I':	/* Blink */
-			atr|=BLINK;
-			attr(atr);
+		case 'I':
+ 			if((term_supports()&(ICE_COLOR|PETSCII)) != ICE_COLOR)
+				attr(atr|BLINK);
+			break;
+		case 'E': /* Bright Background */
+ 			if(term_supports()&(ICE_COLOR|PETSCII))
+				attr(atr|BG_BRIGHT);
 			break;
 		case 'F':	/* Blink, only if alt Blink Font is loaded */
 			if(((atr&HIGH) && (console&CON_HBLINK_FONT)) || (!(atr&HIGH) && (console&CON_BLINK_FONT)))
@@ -869,36 +889,28 @@ void sbbs_t::ctrl_a(char x)
 			attr(atr);
 			break;
 		case '0':	/* Black Background */
-			atr=(atr&0x8f);
 			attr(atr);
 			break;
 		case '1':	/* Red Background */
-			atr=(atr&0x8f)|(uchar)BG_RED;
-			attr(atr);
+			attr(atr | BG_RED);
 			break;
 		case '2':	/* Green Background */
-			atr=(atr&0x8f)|(uchar)BG_GREEN;
-			attr(atr);
+			attr(atr | BG_GREEN);
 			break;
 		case '3':	/* Yellow Background */
-			atr=(atr&0x8f)|(uchar)BG_BROWN;
-			attr(atr);
+			attr(atr | BG_BROWN);
 			break;
 		case '4':	/* Blue Background */
-			atr=(atr&0x8f)|(uchar)BG_BLUE;
-			attr(atr);
+			attr(atr | BG_BLUE);
 			break;
 		case '5':	/* Magenta Background */
-			atr=(atr&0x8f)|(uchar)BG_MAGENTA;
-			attr(atr);
+			attr(atr |BG_MAGENTA);
 			break;
 		case '6':	/* Cyan Background */
-			atr=(atr&0x8f)|(uchar)BG_CYAN;
-			attr(atr);
+			attr(atr | BG_CYAN);
 			break;
 		case '7':	/* White Background */
-			atr=(atr&0x8f)|(uchar)BG_LIGHTGRAY;
-			attr(atr);
+			attr(atr | BG_LIGHTGRAY);
 			break;
 	}
 }
@@ -913,8 +925,12 @@ int sbbs_t::attr(int atr)
 
 	long term = term_supports();
 	if(term&PETSCII) {
-		if(atr&0x70) {
-			atr >>= 4;
+		if(atr&(0x70|BG_BRIGHT)) {	// background color (reverse video for PETSCII)
+			if(atr&BG_BRIGHT)
+				atr |= HIGH;
+			else
+				atr &= ~HIGH;
+			atr = (atr&(BLINK|HIGH)) | ((atr&0x70)>>4);
 			outcom(PETSCII_REVERSE_ON);
 		} else
 			outcom(PETSCII_REVERSE_OFF);
@@ -1003,8 +1019,8 @@ bool sbbs_t::msgabort()
 
 int sbbs_t::backfill(const char* instr, float pct, int full_attr, int empty_attr)
 {
-	int	atr;
-	int save_atr = curatr;
+	uint atr;
+	uint save_atr = curatr;
 	int len;
 	char* str = strip_ctrl(instr, NULL);
 
@@ -1042,8 +1058,8 @@ void sbbs_t::progress(const char* text, int count, int total, int interval)
 
 struct savedline {
 	char 	buf[LINE_BUFSIZE+1];	/* Line buffer (i.e. ANSI-encoded) */
-	char 	beg_attr;				/* Starting attribute of each line */
-	char 	end_attr;				/* Ending attribute of each line */
+	uint	beg_attr;				/* Starting attribute of each line */
+	uint	end_attr;				/* Ending attribute of each line */
 	long	column;					/* Current column number */
 };
 
