1e422117ab4783d0224a95634d2ade82677eaa7f
rswindell <rswindell>
2019-06-29 00:06:14 +0000

    uucode.* and yence.* were moved to src/encode. You will need to perform a clean rebuild.

--- sbbs3/scfg/scfg.c
+++ sbbs3/scfg/scfg.c
@@ -1,6 +1,6 @@
 /* Synchronet configuration utility 										*/
 
-/* $Id: scfg.c,v 1.102 2019/04/22 22:20:56 rswindell Exp $ */
+/* $Id: scfg.c,v 1.103 2019/06/22 22:51:56 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -790,8 +790,8 @@ BOOL save_xtrn_cfg(scfg_t* cfg, int backup_level)
 
 
 /****************************************************************************/
-/* Checks the uifc.changes variable. If there have been no uifc.changes, returns 2.	*/
-/* If there have been uifc.changes, it prompts the user to change or not. If the */
+/* Checks the uifc.changes variable. If there have been no changes, returns 2.	*/
+/* If there have been changes, it prompts the user to change or not. If the */
 /* user escapes the menu, returns -1, selects Yes, 0, and selects no, 1 	*/
 /****************************************************************************/
 int save_changes(int mode)
@@ -808,14 +808,14 @@ int save_changes(int mode)
 	strcpy(opt[1],"No");
 	opt[2][0]=0;
 	uifc.helpbuf=
-		"`Save uifc.changes:`\n"
+		"`Save Changes:`\n"
 		"\n"
-		"You have made some uifc.changes to the configuration. If you want to save\n"
-		"these uifc.changes, select `Yes`. If you are positive you DO NOT want to save\n"
-		"these uifc.changes, select `No`. If you are not sure and want to review the\n"
+		"You have made some changes to the configuration. If you want to save\n"
+		"these changes, select `Yes`. If you are positive you DO NOT want to save\n"
+		"these changes, select `No`. If you are not sure and want to review the\n"
 		"configuration before deciding, hit ~ ESC ~.\n"
 	;
-	i=uifc.list(mode|WIN_ACT,0,0,0,&i,0,"Save Changes",opt);
+	i=uifc.list(mode|WIN_SAV,0,0,0,&i,0,"Save Changes",opt);
 	if(i!=-1)
 		uifc.changes=0;
 	return(i);
