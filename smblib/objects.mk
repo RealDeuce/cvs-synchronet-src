f9da00d6c0b303aa8305ddfbcffeaeb7337ca962
rswindell <rswindell>
2019-06-29 01:14:55 +0000

    Moved smblib/crc* and md5* here.

--- smblib/objects.mk
+++ smblib/objects.mk
@@ -2,7 +2,7 @@
 
 # Make 'include file' listing object files for SMBLIB
 
-# $Id: objects.mk,v 1.6 2019/06/29 01:22:50 rswindell Exp $
+# $Id: objects.mk,v 1.5 2019/06/29 00:24:31 rswindell Exp $
 
 # OBJODIR, DIRSEP, and OFILE must be pre-defined
 
@@ -14,6 +14,9 @@ OBJS	=	$(OBJODIR)$(DIRSEP)smbadd$(OFILE)\
 			$(OBJODIR)$(DIRSEP)smblib$(OFILE)\
 			$(OBJODIR)$(DIRSEP)smbstr$(OFILE)\
 			$(OBJODIR)$(DIRSEP)smbtxt$(OFILE)\
+			$(OBJODIR)$(DIRSEP)crc16$(OFILE)\
+			$(OBJODIR)$(DIRSEP)crc32$(OFILE)\
+			$(OBJODIR)$(DIRSEP)md5$(OFILE)\
 
 MTOBJS	=	$(MTOBJODIR)$(DIRSEP)smbadd$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)smballoc$(OFILE)\
@@ -23,3 +26,6 @@ MTOBJS	=	$(MTOBJODIR)$(DIRSEP)smbadd$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)smblib$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)smbstr$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)smbtxt$(OFILE)\
+			$(MTOBJODIR)$(DIRSEP)crc16$(OFILE)\
+			$(MTOBJODIR)$(DIRSEP)crc32$(OFILE)\
+			$(MTOBJODIR)$(DIRSEP)md5$(OFILE)\
