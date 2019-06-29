6609a12e6165a461a45c39343ad8ec969a8a93e2
rswindell <rswindell>
2019-06-29 00:05:46 +0000

    uucode.* and yence.* were moved here from src/sbbs3. You'll need to perform a clean build.

--- encode/objects.mk
+++ encode/objects.mk
@@ -2,9 +2,12 @@
 
 # Make 'include file' listing object files for ENCODE LIB
 
-# $Id: objects.mk,v 1.1 2019/06/28 22:46:04 rswindell Exp $
+# $Id: objects.mk,v 1.2 2019/06/29 00:05:46 rswindell Exp $
 
 # OBJODIR, DIRSEP, and OFILE must be pre-defined
 
-OBJS	=	$(OBJODIR)$(DIRSEP)cp437_utf8_tbl$(OFILE)
+OBJS	=	$(OBJODIR)$(DIRSEP)cp437_utf8_tbl$(OFILE) \
+		$(OBJODIR)$(DIRSEP)uucode$(OFILE) \
+		$(OBJODIR)$(DIRSEP)yenc$(OFILE)
+
 
