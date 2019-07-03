7e16cd03ceef120b22a6680e456d27ece0231096
deuce <deuce>
2019-07-03 05:36:01 +0000

    SyncTERM now depends on the encode and hash libs.

--- sbbs3/getmsg.cpp
+++ sbbs3/getmsg.cpp
@@ -1,6 +1,6 @@
 /* Synchronet message retrieval functions */
 
-/* $Id: getmsg.cpp,v 1.80 2019/04/10 20:03:48 rswindell Exp $ */
+/* $Id: getmsg.cpp,v 1.88 2019/05/04 23:02:38 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -94,6 +94,7 @@ void sbbs_t::show_msgattr(smbmsg_t* msg)
 	uint16_t attr = msg->hdr.attr;
 	uint16_t poll = attr&MSG_POLL_VOTE_MASK;
 	uint32_t auxattr = msg->hdr.auxattr;
+	uint32_t netattr = msg->hdr.netattr;
 
 	bprintf(text[MsgAttr]
 		,attr&MSG_PRIVATE	? "Private  "   :nulstr
@@ -110,16 +111,16 @@ void sbbs_t::show_msgattr(smbmsg_t* msg)
 		,attr&MSG_NOREPLY	? "NoReply  "	:nulstr
 		,poll == MSG_POLL	? "Poll  "		:nulstr
 		,poll == MSG_POLL && auxattr&POLL_CLOSED ? "(Closed)  "	:nulstr
-		,nulstr
-		,nulstr
-		,nulstr
+		,auxattr&(MSG_FILEATTACH|MSG_MIMEATTACH) ? "Attach  "   :nulstr
+		,netattr&MSG_SENT						 ? "Sent  "		:nulstr
+		,netattr&MSG_INTRANSIT					 ? "InTransit  ":nulstr
 		);
 }
 
 /****************************************************************************/
 /* Displays a message header to the screen                                  */
 /****************************************************************************/
-void sbbs_t::show_msghdr(smb_t* smb, smbmsg_t* msg)
+void sbbs_t::show_msghdr(smb_t* smb, smbmsg_t* msg, const char* subject, const char* from, const char* to)
 {
 	char	str[MAX_PATH+1];
 	char	age[64];
@@ -127,8 +128,20 @@ void sbbs_t::show_msghdr(smb_t* smb, smbmsg_t* msg)
 	int 	i;
 	smb_t	saved_smb = this->smb;
 
-	this->smb = *smb;	// Needed for @-codes and JS bbs.smb_* properties
-	current_msg = msg;	// Needed for @-codes and JS bbs.msg_* properties
+	if(smb != NULL)
+		this->smb = *smb;	// Needed for @-codes and JS bbs.smb_* properties
+	if(msg != NULL) {
+		current_msg = msg;		// Needed for @-codes and JS bbs.msg_* properties
+		current_msg_subj = msg->subj;
+		current_msg_from = msg->from;
+		current_msg_to = msg->to;
+	}
+	if(subject != NULL)
+		current_msg_subj = subject;
+	if(from != NULL)
+		current_msg_from = from;
+	if(to != NULL)
+		current_msg_to = to;
 
 	attr(LIGHTGRAY);
 	if(!tos) {
@@ -137,24 +150,27 @@ void sbbs_t::show_msghdr(smb_t* smb, smbmsg_t* msg)
 		else
 			CRLF;
 	}
+	msghdr_tos = tos;
 	if(!menu("msghdr", P_NOERROR)) {
-		bprintf(text[MsgSubj],msg->subj);
+		bprintf(text[MsgSubj], current_msg_subj);
 		if(msg->tags && *msg->tags)
 			bprintf(text[MsgTags], msg->tags);
-		if(msg->hdr.attr)
+		if(msg->hdr.attr || msg->hdr.netattr || msg->hdr.auxattr)
 			show_msgattr(msg);
-		if(msg->to && *msg->to) {
-			bprintf(text[MsgTo],msg->to);
+		if(current_msg_to != NULL && *current_msg_to != 0) {
+			bprintf(text[MsgTo], current_msg_to);
 			if(msg->to_net.addr!=NULL)
 				bprintf(text[MsgToNet],smb_netaddrstr(&msg->to_net,str));
 			if(msg->to_ext)
 				bprintf(text[MsgToExt],msg->to_ext);
 		}
-		if(!(msg->hdr.attr&MSG_ANONYMOUS) || SYSOP) {
-			bprintf(text[MsgFrom],msg->from);
+		if(msg->cc_list != NULL)
+			bprintf(text[MsgCarbonCopyList], msg->cc_list);
+		if(current_msg_from != NULL && (!(msg->hdr.attr&MSG_ANONYMOUS) || SYSOP)) {
+			bprintf(text[MsgFrom], current_msg_from);
 			if(msg->from_ext)
 				bprintf(text[MsgFromExt],msg->from_ext);
-			if(msg->from_net.addr!=NULL && strchr(msg->from,'@')==NULL)
+			if(msg->from_net.addr!=NULL && (current_msg_from == NULL || strchr(current_msg_from,'@')==NULL))
 				bprintf(text[MsgFromNet],smb_netaddrstr(&msg->from_net,str));
 		}
 		if(!(msg->hdr.attr&MSG_POLL) && (msg->upvotes || msg->downvotes))
@@ -176,6 +192,9 @@ void sbbs_t::show_msghdr(smb_t* smb, smbmsg_t* msg)
 				,timestr(*(time32_t *)msg->hfield_dat[i]));
 	}
 	this->smb = saved_smb;
+	current_msg_subj = NULL;
+	current_msg_from = NULL;
+	current_msg_to = NULL;
 }
 
 /****************************************************************************/
@@ -261,7 +280,9 @@ bool sbbs_t::show_msg(smb_t* smb, smbmsg_t* msg, long p_mode, post_t* post)
 		if(p == NULL)
 			p = txt;
 		else
-			bputs(text[MIMEDecodedPlainText]);
+			bprintf(text[MIMEDecodedPlainTextFmt]
+				, msg->text_charset == NULL ? "unspecified (US-ASCII)" : msg->text_charset
+				, msg->text_subtype);
 	}
 	truncsp(p);
 	SKIP_CRLF(p);
