b740c69439569664e59bdecec6726b6a1bb170d2
rswindell <rswindell>
2019-07-06 09:12:50 +0000

    ESC (0x1b) is a terminal control char, don't translate to/from UTF-8 by default

--- sbbs3/scfgsave.c
+++ sbbs3/scfgsave.c
@@ -1,6 +1,6 @@
 /* Synchronet configuration file save routines */
 
-/* $Id: scfgsave.c,v 1.85 2019/07/15 02:13:35 rswindell Exp $ */
+/* $Id: scfgsave.c,v 1.80 2019/04/29 08:22:24 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -810,12 +810,12 @@ BOOL DLLCALL write_file_cfg(scfg_t* cfg, int backup_level)
 				put_str(cfg->dir[i]->lname, stream);
 				put_str(cfg->dir[i]->sname, stream);
 				put_str(cfg->dir[i]->code_suffix, stream);
-
+#if 1
 				if (cfg->dir[i]->data_dir[0]) {
 					backslash(cfg->dir[i]->data_dir);
 					md(cfg->dir[i]->data_dir);
 				}
-
+#endif
 				put_str(cfg->dir[i]->data_dir, stream);
 				put_str(cfg->dir[i]->arstr, stream);
 				put_str(cfg->dir[i]->ul_arstr, stream);
@@ -823,28 +823,30 @@ BOOL DLLCALL write_file_cfg(scfg_t* cfg, int backup_level)
 				put_str(cfg->dir[i]->op_arstr, stream);
 				backslash(cfg->dir[i]->path);
 				put_str(cfg->dir[i]->path, stream);
-
+#if 1
 				if (cfg->dir[i]->misc&DIR_FCHK) {
 					SAFECOPY(path, cfg->dir[i]->path);
 					if (!path[0]) {		/* no file storage path specified */
-						if(cfg->dir[i]->data_dir[0])
-							SAFECOPY(path, cfg->dir[i]->data_dir);
-						else
-							SAFEPRINTF(path, "%sdirs", cfg->data_dir);
 						SAFEPRINTF2(str, "%s%s"
 							, cfg->lib[cfg->dir[i]->lib]->code_prefix
 							, cfg->dir[i]->code_suffix);
 						strlwr(str);
-						SAFECAT(path,str);
+						safe_snprintf(path, sizeof(path), "%s%s/"
+							, cfg->dir[i]->data_dir
+							, str);
 					}
 					else if (cfg->lib[cfg->dir[i]->lib]->parent_path[0]) {
 						SAFECOPY(path, cfg->lib[cfg->dir[i]->lib]->parent_path);
+						prep_dir(cfg->ctrl_dir, path, sizeof(path));
+						md(path);
 						backslash(path);
-						SAFECAT(path, cfg->dir[i]->path);
+						strcat(path, cfg->dir[i]->path);
 					}
-					prep_dir(cfg->ctrl_dir, path, sizeof(path));
-					mkpath(path);
+					else
+						prep_dir(cfg->ctrl_dir, path, sizeof(path));
+					md(path);
 				}
+#endif
 
 				put_str(cfg->dir[i]->upload_sem, stream);
 				put_int(cfg->dir[i]->maxfiles, stream);
