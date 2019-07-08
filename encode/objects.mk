6d6bb9188015cb63cc8a005958a678dd613945c8
rswindell <rswindell>
2019-07-08 00:07:22 +0000

    Moved cp437_unicode_tbl and unicode_is_zerowidth() to (new file) unicode.c. New function (derived from sbbs_t::utf8_to_cp437()): unicode_to_cp437() New utf8 functions: utf8_replace_chars(), utf8_str_is_valid(). utf8_getc() enhancement: val arg may be NULL (for length/validation uses).

--- encode/objects.mk
+++ encode/objects.mk
@@ -2,11 +2,11 @@
 
 # Make 'include file' listing object files for ENCODE LIB
 
-# $Id: objects.mk,v 1.4 2019/07/06 07:39:40 rswindell Exp $
+# $Id: objects.mk,v 1.5 2019/07/08 00:07:22 rswindell Exp $
 
 # OBJODIR, DIRSEP, and OFILE must be pre-defined
 
-OBJS	=	$(OBJODIR)$(DIRSEP)cp437_unicode_tbl$(OFILE) \
+OBJS	=	$(OBJODIR)$(DIRSEP)unicode$(OFILE) \
         $(OBJODIR)$(DIRSEP)utf8$(OFILE) \
 		$(OBJODIR)$(DIRSEP)uucode$(OFILE) \
 		$(OBJODIR)$(DIRSEP)yenc$(OFILE) \
