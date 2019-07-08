465c3083a49c8f5baee977cda4e2cbcc12aa345a
rswindell <rswindell>
2019-07-08 02:40:38 +0000

    0x1f is "Unit Separator" (US) in ASCII, not Delete (DEL). I'm assuming Deuce meant to type '\x7f' instead of '\x1f'. Replaced '\x1f' with the unambiguous DEL macro.

--- encode/utf8.h
+++ encode/utf8.h
@@ -1,14 +1,12 @@
-/* truncsp.c */
+/* Synchronet UTF-8 translation functions */
 
-/* Static functions to truncate white-space chars off end of ASCIIZ strings */
-
-/* $Id: truncsp.c,v 1.3 2004/08/04 04:44:27 rswindell Exp $ */
+/* $Id: utf8.h,v 1.3 2019/07/08 00:07:22 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2004 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This library is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU Lesser General Public License		*
@@ -35,30 +33,33 @@
  * Note: If this box doesn't appear square, then you need to fix your tabs.	*
  ****************************************************************************/
 
-/****************************************************************************/
-/* Truncates all white-space chars off end of 'str'							*/
-/****************************************************************************/
-static char* truncsp(char* str)
-{
-	unsigned c;
+#ifndef UTF8_H_
+#define UTF8_H_
 
-	c=strlen(str);
-	while(c && (str[c-1]==' ' || str[c-1]=='\t' || str[c-1]=='\r' || str[c-1]=='\n')) c--;
-	str[c]=0;
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-	return(str);
+#define UTF8_MAX_LEN 6	// Longest possible UTF-8 sequence
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+// Returns true if the string is valid UTF-8
+bool utf8_str_is_valid(const char*);
+// Normalizes (to ASCII) chars in UTF-8 string 'str', in-place, resulting in string <= original in length
+char* utf8_normalize_str(char* str);
+// Replace or strip UTF-8 sequences in str
+// If table ('tbl') of unicode codepoints if non-NULL is an array of 256 codepoints to map to 8-bit chars
+char* utf8_replace_chars(char* str, char (*lookup)(uint32_t), char unsupported_ch, char unsupported_zwch, char error_ch);
+// Decode a UTF-8 sequence to a UNICODE code point
+int utf8_getc(const char* str, size_t len, uint32_t* codepoint);
+// Encode a UNICODE code point into a UTF-8 sequence (str)
+int utf8_putc(char* str, size_t len, uint32_t codepoint);
+
+#if defined(__cplusplus)
 }
+#endif
 
-/****************************************************************************/
-/* Truncates carriage-return and line-feed chars off end of 'str'			*/
-/****************************************************************************/
-static char* truncnl(char* str)
-{
-	unsigned c;
-
-	c=strlen(str);
-	while(c && (str[c-1]=='\r' || str[c-1]=='\n')) c--;
-	str[c]=0;
-
-	return(str);
-}
+#endif // Don't add anything after this line
