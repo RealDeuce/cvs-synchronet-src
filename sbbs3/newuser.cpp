7448efe7f275c05eee2c0f4608224b3666e40b2b
rswindell <rswindell>
2019-07-08 21:15:29 +0000

    Deuce said the new user registration process was "openly hostile", so: Disable the "Swap DEL/BBS key" question, by default (if the user sends DEL, do the swap). Beautify the backspace key prompt/report thing (add/remove blanks lines mostly).

--- sbbs3/newuser.cpp
+++ sbbs3/newuser.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet new user routine */
 
-/* $Id: newuser.cpp,v 1.83 2019/07/09 05:38:46 rswindell Exp $ */
+/* $Id: newuser.cpp,v 1.81 2019/05/09 21:14:19 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -36,7 +36,6 @@
  ****************************************************************************/
 
 #include "sbbs.h"
-#include "petdefs.h"
 #include "cmdshell.h"
 
 /****************************************************************************/
@@ -185,6 +184,7 @@ BOOL sbbs_t::newuser()
 				bprintf(text[InvalidBackspaceKeyFmt], key, key);
 				if(text[ContinueQ][0] && !yesno(text[ContinueQ]))
 					return FALSE;
+				newline();
 			}
 		}
 
