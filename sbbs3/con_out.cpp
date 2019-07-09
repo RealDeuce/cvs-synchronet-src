325c642bdad41e4989dba8c9398d60b01bdeb2b7
rswindell <rswindell>
2019-07-09 05:34:36 +0000

    Use new header file: cp437defs.h

--- sbbs3/con_out.cpp
+++ sbbs3/con_out.cpp
@@ -1,7 +1,7 @@
 /* Synchronet console output routines */
 // vi: tabstop=4
 
-/* $Id: con_out.cpp,v 1.104 2019/07/08 00:11:49 rswindell Exp $ */
+/* $Id: con_out.cpp,v 1.105 2019/07/09 05:34:36 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -37,6 +37,7 @@
 #include "sbbs.h"
 #include "utf8.h"
 #include "unicode.h"
+#include "cp437defs.h"
 
 /****************************************************************************/
 /* Outputs a NULL terminated string locally and remotely (if applicable)    */
@@ -265,7 +266,7 @@ size_t sbbs_t::utf8_to_cp437(const char* str, size_t len)
 	if(ch)
 		outchar(ch);
 	else if(!unicode_is_zerowidth(codepoint)) {
-		outchar('\xA8');	// Inverted question mark
+		outchar(CP437_CHAR_INVERTED_QUESTION_MARK);
 		char seq[32] = "";
 		for(size_t i = 0; i < len; i++)
 			sprintf(seq + strlen(seq), "%02X ", (uchar)*(str + i));
