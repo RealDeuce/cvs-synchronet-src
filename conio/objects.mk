79d9dc6e2824ca39f3b54c7ed0ec47158944c8ed
rswindell <rswindell>
2019-07-09 05:38:46 +0000

    petdefs.h was (silently) moved from src/sbbs3 to src/encode. Since not *everything* that #include's sbbs.h needs the PETSCII defs, remove it from sbbs.h and just #include petdefs.h where we need it. The catch-all header file is a problematic organizational design and we are moving away from that.

--- conio/objects.mk
+++ conio/objects.mk
@@ -3,5 +3,4 @@ OBJS	=	$(MTOBJODIR)$(DIRSEP)ansi_cio$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)cterm$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)vidmodes$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)allfonts$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)mouse$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)syncicon64$(OFILE)
+			$(MTOBJODIR)$(DIRSEP)mouse$(OFILE)
