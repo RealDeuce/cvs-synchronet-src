2c7197c8177a89f352df3c199004970c0418b701
deuce <deuce>
2019-07-11 08:25:52 +0000

    Use the COLOR_PALETTE for uifc, no need for setfont() anymore since uifc always uses font zero now.

--- syncterm/telnet_io.c
+++ syncterm/telnet_io.c
@@ -1,9 +1,11 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: telnet_io.c,v 1.37 2019/07/11 18:31:45 deuce Exp $ */
+/* $Id: telnet_io.c,v 1.36 2019/07/10 22:59:43 deuce Exp $ */
 
 #include <stdlib.h>
 #include <string.h>
+
+#include "term.h"
 
 #include "genwrap.h"
 #include "sockwrap.h"
@@ -88,7 +90,7 @@ void request_telnet_opt(uchar cmd, uchar opt)
 	send_telnet_cmd(cmd,opt);
 }
 
-BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen, struct bbslist *bbs)
+BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen, cterm_emulation_t emu)
 {
 	BYTE	command;
 	BYTE	option;
@@ -156,13 +158,12 @@ BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen, struct
 					/* sub-option terminated */
 					if(option==TELNET_TERM_TYPE && telnet_cmd[3]==TELNET_TERM_SEND) {
 						char buf[32];
-						const char *emu = get_emulation_str(get_emulation(bbs));
 						int len=sprintf(buf,"%c%c%c%c%s%c%c"
 							,TELNET_IAC,TELNET_SB
 							,TELNET_TERM_TYPE,TELNET_TERM_IS
-							,emu
+							,get_emulation_str(emu)
 							,TELNET_IAC,TELNET_SE);
-						lprintf(LOG_INFO,"TX: Terminal Type is %s", emu);
+						lprintf(LOG_INFO,"TX: Terminal Type is %s", get_emulation_str(emu));
 						putcom(buf,len);
 						request_telnet_opt(TELNET_WILL, TELNET_NEGOTIATE_WINDOW_SIZE);
 					}
@@ -196,21 +197,18 @@ BYTE* telnet_interpret(BYTE* inbuf, int inlen, BYTE* outbuf, int *outlen, struct
 					}
 
 					if(command==TELNET_DO && option==TELNET_NEGOTIATE_WINDOW_SIZE) {
-						int rows, cols;
 						BYTE buf[32];
-
-						get_term_size(bbs, &cols, &rows);
 						buf[0]=TELNET_IAC;
 						buf[1]=TELNET_SB;
 						buf[2]=TELNET_NEGOTIATE_WINDOW_SIZE;
-						buf[3]=(cols>>8)&0xff;
-						buf[4]=cols&0xff;
-						buf[5]=(rows>>8)&0xff;
-						buf[6]=rows&0xff;
+						buf[3]=(term.width>>8)&0xff;
+						buf[4]=term.width&0xff;
+						buf[5]=(term.height>>8)&0xff;
+						buf[6]=term.height&0xff;
 						buf[7]=TELNET_IAC;
 						buf[8]=TELNET_SE;
 						lprintf(LOG_INFO,"TX: Window Size is %u x %u"
-							,cols, rows);
+							,term.width, term.height);
 						putcom((char *)buf,9);
 					}
 
