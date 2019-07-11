e426086d9080dee1fdf5cb49bace6a2f9c2fb140
deuce <deuce>
2019-07-11 18:46:55 +0000

    More emu removal.

--- sbbs3/scfg/scfgsys.c
+++ sbbs3/scfg/scfgsys.c
@@ -1,4 +1,4 @@
-/* $Id: scfgsys.c,v 1.48 2018/02/18 03:14:32 rswindell Exp $ */
+/* $Id: scfgsys.c,v 1.51 2019/01/12 12:09:15 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -125,7 +125,7 @@ void sys_cfg(void)
 					break;
 				if(!i) {
 					cfg.new_install=new_install;
-					write_main_cfg(&cfg,backup_level);
+					save_main_cfg(&cfg,backup_level);
 					refresh_cfg(&cfg);
 				}
 				return;
@@ -234,9 +234,10 @@ void sys_cfg(void)
 				strcpy(opt[i++],"Bangkok");
 				strcpy(opt[i++],"Hong Kong");
 				strcpy(opt[i++],"Tokyo");
-				strcpy(opt[i++],"Sydney");
+				strcpy(opt[i++],"Australian Central");
+				strcpy(opt[i++],"Australian Eastern");
 				strcpy(opt[i++],"Noumea");
-				strcpy(opt[i++],"Wellington");
+				strcpy(opt[i++],"New Zealand");
 				strcpy(opt[i++],"Other...");
 				opt[i][0]=0;
 				i=0;
@@ -324,13 +325,17 @@ void sys_cfg(void)
 						cfg.sys_timezone=TOK;
 						break;
 					case 22:
-						cfg.sys_timezone=SYD;
+						cfg.sys_timezone=ACST;
 						break;
 					case 23:
-						cfg.sys_timezone=NOU;
+						cfg.sys_timezone=AEST;
 						break;
 					case 24:
-						cfg.sys_timezone=WEL;
+						cfg.sys_timezone=NOU;
+						break;
+					case 25:
+						cfg.sys_timezone=NZST;
+						configure_dst();
 						break;
 					default:
 						if(cfg.sys_timezone>720 || cfg.sys_timezone<-720)
