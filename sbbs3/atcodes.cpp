da7d25a171975da1b1cca9b98e6a39ca7b2bf4ef
rswindell <rswindell>
2019-07-12 02:10:42 +0000

    Don't change useron.misc during logon. This could cause a user's account to have the NO_EXASCII (plain-ASCII only) set if the logged in without a successful auto-terminal-type detection. And with sbbs_t::term_supports(), we don't need useron.misc to reflect the auto-detected-terminal-type any longer.

--- sbbs3/atcodes.cpp
+++ sbbs3/atcodes.cpp
@@ -1,7 +1,7 @@
 /* Synchronet "@code" functions */
 // vi: tabstop=4
 
-/* $Id: atcodes.cpp,v 1.106 2019/07/16 04:31:03 rswindell Exp $ */
+/* $Id: atcodes.cpp,v 1.105 2019/07/11 21:44:33 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1055,17 +1055,17 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 		return(str);
 	}
 
-	if(!strncmp(sp,"MAILW:",6) || !strncmp(sp,"MAILW#",6)) {
+	if(!strncmp(sp,"MAILW:",6)) {
 		safe_snprintf(str,maxlen,"%u",getmail(&cfg,atoi(sp+6), /* Sent: */FALSE, /* attr: */0));
 		return(str);
 	}
 
-	if(!strncmp(sp,"MAILP:",6) || !strncmp(sp,"MAILP#",6)) {
+	if(!strncmp(sp,"MAILP:",6)) {
 		safe_snprintf(str,maxlen,"%u",getmail(&cfg,atoi(sp+6), /* Sent: */TRUE, /* attr: */0));
 		return(str);
 	}
 
-	if(!strncmp(sp,"SPAMW:",6) || !strncmp(sp,"SPAMW#",6)) {
+	if(!strncmp(sp,"SPAMW:",6)) {
 		safe_snprintf(str,maxlen,"%u",getmail(&cfg,atoi(sp+6), /* Sent: */FALSE, /* attr: */MSG_SPAM));
 		return(str);
 	}
