c90c24acf98ca487ace5cc39dc7ec6547586c24d
deuce <deuce>
2019-07-14 18:56:55 +0000

    When changing *from* Atari and Commodore modes, enable the status line and change the font to CP437.
    This prevents weirdness since when changing *to* those modes, the font and
    status line setting are silently updated.

--- sbbs3/atcodes.cpp
+++ sbbs3/atcodes.cpp
@@ -1,7 +1,7 @@
 /* Synchronet "@code" functions */
 // vi: tabstop=4
 
-/* $Id: atcodes.cpp,v 1.98 2019/07/10 05:59:02 rswindell Exp $ */
+/* $Id: atcodes.cpp,v 1.105 2019/07/11 21:44:33 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -75,17 +75,19 @@ int sbbs_t::show_atcode(const char *instr)
 	sp=(str+1);
 
 	disp_len=len;
-	if((p=strstr(sp,"-L"))!=NULL)
+	if(strchr(sp, ':') != NULL)
+		p = NULL;
+	else if((p=strstr(sp,"-L"))!=NULL)
 		padded_left=true;
 	else if((p=strstr(sp,"-R"))!=NULL)
 		padded_right=true;
 	else if((p=strstr(sp,"-C"))!=NULL)
 		centered=true;
-	else if((p=strstr(sp,"-D"))!=NULL)
+	else if((p=strstr(sp,"-W"))!=NULL)	/* wide */
 		doubled=true;
 	else if((p=strstr(sp,"-Z"))!=NULL)
 		zero_padded=true;
-	else if((p=strstr(sp,"-W"))!=NULL)	/* wrap */
+	else if((p=strstr(sp,"->"))!=NULL)	/* wrap */
 		truncated = false;
 	if(p!=NULL) {
 		char* lp = p + 2;
@@ -149,11 +151,13 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 
 	if(strncmp(sp, "U+", 2) == 0) {	// UNICODE
 		enum unicode_codepoint codepoint = (enum unicode_codepoint)strtoul(sp + 2, &tp, 16);
-		if(tp == NULL || *tp ==0)
+		if(tp == NULL || *tp == 0)
 			outchar(codepoint, unicode_to_cp437(codepoint));
+		else if(*tp == ':')
+			outchar(codepoint, tp + 1);
 		else {
 			char fallback = (char)strtoul(tp + 1, NULL, 16);
-			if(*tp == '/')
+			if(*tp == ',')
 				outchar(codepoint, fallback);
 			else if(*tp == '!') {
 				char ch = unicode_to_cp437(codepoint);
@@ -168,6 +172,35 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 
 	if(strcmp(sp, "CHECKMARK") == 0) {
 		outchar(UNICODE_CHECK_MARK, CP437_CHECK_MARK);
+		return nulstr;
+	}
+
+	if(strcmp(sp, "ELLIPSIS") == 0) {
+		outchar(UNICODE_HORIZONTAL_ELLIPSIS, "...");
+		return nulstr;
+	}
+	if(strcmp(sp, "COPY") == 0) {
+		outchar(UNICODE_COPYRIGHT_SIGN, "(C)");
+		return nulstr;
+	}
+	if(strcmp(sp, "SOUNDCOPY") == 0) {
+		outchar(UNICODE_SOUND_RECORDING_COPYRIGHT, "(P)");
+		return nulstr;
+	}
+	if(strcmp(sp, "REGISTERED") == 0) {
+		outchar(UNICODE_REGISTERED_SIGN, "(R)");
+		return nulstr;
+	}
+	if(strcmp(sp, "TRADEMARK") == 0) {
+		outchar(UNICODE_TRADE_MARK_SIGN, "(TM)");
+		return nulstr;
+	}
+	if(strcmp(sp, "DEGREE_C") == 0) {
+		outchar(UNICODE_DEGREE_CELSIUS, "\xF8""C");
+		return nulstr;
+	}
+	if(strcmp(sp, "DEGREE_F") == 0) {
+		outchar(UNICODE_DEGREE_FAHRENHEIT, "\xF8""F");
 		return nulstr;
 	}
 
@@ -266,8 +299,11 @@ const char* sbbs_t::atcode(char* sp, char* str, size_t maxlen)
 		safe_snprintf(str,maxlen,"%lu",rows);
 		return(str);
 	}
-	if(!strcmp(sp,"TERM"))
-		return(terminal);
+	if(strcmp(sp,"TERM") == 0)
+		return term_type();
+
+	if(strcmp(sp,"CHARSET") == 0)
+		return term_charset();
 
 	if(!strcmp(sp,"CONN"))
 		return(connection);
