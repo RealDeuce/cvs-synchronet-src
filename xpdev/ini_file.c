f249fc5d516c029bf214d1718efb511174b2eedb
deuce <deuce>
2019-07-16 20:38:54 +0000

    More SAFECATS() - Coverity.

--- xpdev/ini_file.c
+++ xpdev/ini_file.c
@@ -1,6 +1,6 @@
 /* Functions to create and parse .ini files */
 
-/* $Id: ini_file.c,v 1.166 2018/08/28 22:12:06 rswindell Exp $ */
+/* $Id: ini_file.c,v 1.170 2019/07/16 20:38:54 deuce Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -744,9 +744,9 @@ char* DLLCALL iniSetEnumList(str_list_t* list, const char* section, const char* 
 		name_count = strListCount(names);
 		for(i=0; i < count; i++) {
 			if(value[0])
-				strcat(value,sep);
+				SAFECAT(value,sep);
 			if(val_list[i] < name_count)
-				strcat(value, names[val_list[i]]);
+				SAFECAT(value, names[val_list[i]]);
 			else
 				sprintf(value + strlen(value), "%u", val_list[i]);
 		}
@@ -812,20 +812,20 @@ char* DLLCALL iniSetBitField(str_list_t* list, const char* section, const char* 
 
 	if(style==NULL)
 		style=&default_style;
-	if(style->bit_separator==NULL)
+	if(style->bit_separator!=NULL)
 		bit_separator = style->bit_separator;
 	str[0]=0;
 	for(i=0;bitdesc[i].name;i++) {
 		if((value&bitdesc[i].bit)==0)
 			continue;
 		if(str[0])
-			strcat(str, bit_separator);
-		strcat(str,bitdesc[i].name);
+			SAFECAT(str, bit_separator);
+		SAFECAT(str,bitdesc[i].name);
 		value&=~bitdesc[i].bit;
 	}
 	if(value) {	/* left over bits? */
 		if(str[0])
-			strcat(str, bit_separator);
+			SAFECAT(str, bit_separator);
 		sprintf(str+strlen(str), "0x%lX", value);
 	}
 	return iniSetString(list, section, key, str, style);
@@ -2373,11 +2373,15 @@ str_list_t DLLCALL iniReadFile(FILE* fp)
 BOOL DLLCALL iniWriteFile(FILE* fp, const str_list_t list)
 {
 	size_t		count;
+	long pos;
 
 	rewind(fp);
 	count = strListWriteFile(fp,list,"\n");
 	fflush(fp);
-	if(chsize(fileno(fp), ftell(fp))!=0)	/* truncate */
+	pos = ftell(fp);
+	if (pos == -1)
+		return (FALSE);
+	if(chsize(fileno(fp), pos)!=0)	/* truncate */
 		return(FALSE);
 
 	return(count == strListCount(list));
