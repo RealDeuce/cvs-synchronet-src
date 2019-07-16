c00490428ad773ef90306082aaa3b07ef3d49c83
deuce <deuce>
2019-07-16 15:09:47 +0000

    Prevent LOR by exporting and taking the blinker lock before calling bitmap_init_mode().

--- sbbs3/con_out.cpp
+++ sbbs3/con_out.cpp
@@ -1,7 +1,7 @@
 /* Synchronet console output routines */
 // vi: tabstop=4
 
-/* $Id: con_out.cpp,v 1.115 2019/07/24 05:24:48 rswindell Exp $ */
+/* $Id: con_out.cpp,v 1.113 2019/07/11 21:41:57 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -40,40 +40,19 @@
 #include "petdefs.h"
 #include "cp437defs.h"
 
-char* sbbs_t::auto_utf8(const char* str, long* mode)
-{
-	if(strncmp(str, "\xEF\xBB\xBF", 3) == 0) {
-		*mode |= P_UTF8;
-		return (char*)(str + 3);
-	}
-	if((*mode)&P_AUTO_UTF8) {
-		if(!str_is_ascii(str) && utf8_str_is_valid(str))
-			*mode |= P_UTF8;
-	}
-	return (char*)str;
-}
-
 /****************************************************************************/
 /* Outputs a NULL terminated string locally and remotely (if applicable)    */
 /* Handles ctrl-a codes, Telnet-escaping, column & line count, auto-pausing */
-/* Supported P_* mode flags:
-   P_PETSCII
-   P_UTF8
-   P_AUTO_UTF8
-   P_NOATCODES
- ****************************************************************************/
-int sbbs_t::bputs(const char *str, long mode)
+/****************************************************************************/
+int sbbs_t::bputs(const char *str)
 {
 	int i;
     ulong l=0;
-	long term = term_supports();
 
 	if(online==ON_LOCAL && console&CON_L_ECHO) 	/* script running as event */
 		return(lputs(LOG_INFO, str));
 
-	str = auto_utf8(str, &mode);
-	size_t len = strlen(str);
-	while(l < len && online) {
+	while(str[l] && online) {
 		if(str[l]==CTRL_A && str[l+1]!=0) {
 			l++;
 			if(str[l] == 'Z')	/* EOF (uppercase 'Z' only) */
@@ -81,7 +60,7 @@ int sbbs_t::bputs(const char *str, long mode)
 			ctrl_a(str[l++]);
 			continue;
 		}
-		if(!(mode&P_NOATCODES) && str[l]=='@') {
+		if(str[l]=='@') {           /* '@' */
 			if(str==mnestr			/* Mnemonic string or */
 				|| (str>=text[0]	/* Straight out of TEXT.DAT */
 					&& str<=text[TOTAL_TEXT-1])) {
@@ -100,18 +79,7 @@ int sbbs_t::bputs(const char *str, long mode)
 					continue;
 			}
 		}
-		if(mode&P_PETSCII) {
-			if(term&PETSCII)
-				outcom(str[l++]);
-			else
-				petscii_to_ansibbs(str[l++]);
-		} else if((str[l]&0x80) && (mode&P_UTF8)) {
-			if(term&UTF8)
-				outcom(str[l++]);
-			else
-				l += utf8_to_cp437(str + l, len - l);
-		} else
-			outchar(str[l++]);
+		outchar(str[l++]);
 	}
 	return(l);
 }
@@ -366,23 +334,6 @@ int sbbs_t::bprintf(const char *fmt, ...)
 	sbuf[sizeof(sbuf)-1]=0;	/* force termination */
 	va_end(argptr);
 	return(bputs(sbuf));
-}
-
-/****************************************************************************/
-/* Performs printf() using bbs bputs function (with mode)					*/
-/****************************************************************************/
-int sbbs_t::bprintf(long mode, const char *fmt, ...)
-{
-	va_list argptr;
-	char sbuf[4096];
-
-	if(strchr(fmt,'%')==NULL)
-		return(bputs(fmt, mode));
-	va_start(argptr,fmt);
-	vsnprintf(sbuf,sizeof(sbuf),fmt,argptr);
-	sbuf[sizeof(sbuf)-1]=0;	/* force termination */
-	va_end(argptr);
-	return(bputs(sbuf, mode));
 }
 
 /****************************************************************************/
