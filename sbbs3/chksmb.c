bfd4a2b91f4eb87f009ed53a30b477ae17c8f453
rswindell <rswindell>
2019-07-18 03:26:19 +0000

    Migrate to MSVC++ 2019 w/PlatformToolset v141_xp: Visual Studio 2017 - Windows XP (v141_xp) toolset

--- sbbs3/chksmb.c
+++ sbbs3/chksmb.c
@@ -1,6 +1,6 @@
 /* Synchronet message base (SMB) validity checker */
 
-/* $Id: chksmb.c,v 1.70 2019/09/24 06:41:19 rswindell Exp $ */
+/* $Id: chksmb.c,v 1.68 2019/07/17 00:34:43 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -55,7 +55,7 @@
 char *ultoac(ulong l, char *string)
 {
 	char str[256];
-	int i,j,k;
+	size_t i,j,k;
 
 	SAFEPRINTF(str,"%lu",l);
 	i=strlen(str)-1;
@@ -180,7 +180,7 @@ int main(int argc, char **argv)
 	char		revision[16];
 	time_t		now=time(NULL);
 
-	sscanf("$Revision: 1.70 $", "%*s %s", revision);
+	sscanf("$Revision: 1.68 $", "%*s %s", revision);
 
 	fprintf(stderr,"\nCHKSMB v2.30-%s (rev %s) SMBLIB %s - Check Synchronet Message Base\n"
 		,PLATFORM_DESC,revision,smb_lib_ver());
@@ -279,7 +279,7 @@ int main(int argc, char **argv)
 
 	off_t sid_length = filelength(fileno(smb.sid_fp));
 	if(sid_length != smb.status.total_msgs * sizeof(idxrec_t)) {
-		printf("!Size of index file (%ld) is incorrect (expected: %ld)\n", sid_length, (long)(smb.status.total_msgs * sizeof(idxrec_t)));
+		printf("!Size of index file (%ld) is incorrect (expected: %ld)\n", sid_length, smb.status.total_msgs * sizeof(idxrec_t));
 		smb_close(&smb);
 		errors++;
 		continue;
