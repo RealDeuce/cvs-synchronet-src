95ff48e05a52ac3009a3323f2731076808caa9b4
rswindell <rswindell>
2019-07-19 00:47:45 +0000

    Support for a 3rd line of RESULT.ED drop file (from external message editors): Full editor name and version details (up to 127 chars), for use in the EDITOR msg header field which is propagated over QWK networks. From what I could find, the WWIV RESULT.ED file format only ever contained 2 lines: <Anonymous: 0/1> <subject>
    So adding support for an optional 3rd line should be fine and backwards
    compatible. Hopefully WWIV or its message editors don't define a different
    "standard" 3rd line in the future. :-)

--- conio/ciolib.h
+++ conio/ciolib.h
@@ -1,4 +1,4 @@
-/* $Id: ciolib.h,v 1.116 2020/04/17 16:54:15 deuce Exp $ */
+/* $Id: ciolib.h,v 1.104 2019/07/11 08:10:52 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -36,7 +36,6 @@
 
 #include <string.h>	/* size_t */
 #include "gen_defs.h"
-#include "utf8_codepages.h"
 
 #ifdef CIOLIBEXPORT
         #undef CIOLIBEXPORT
@@ -74,13 +73,14 @@ enum {
 	 CIOLIB_MODE_AUTO
 	,CIOLIB_MODE_CURSES
 	,CIOLIB_MODE_CURSES_IBM
-	,CIOLIB_MODE_CURSES_ASCII
 	,CIOLIB_MODE_ANSI
 	,CIOLIB_MODE_X
 	,CIOLIB_MODE_CONIO
 	,CIOLIB_MODE_CONIO_FULLSCREEN
 	,CIOLIB_MODE_SDL
 	,CIOLIB_MODE_SDL_FULLSCREEN
+	,CIOLIB_MODE_SDL_YUV
+	,CIOLIB_MODE_SDL_YUV_FULLSCREEN
 };
 
 #if defined(_WIN32)	/* presumably, Win32 */
@@ -202,9 +202,6 @@ enum text_modes
 	VESA_132X43	= 213,
 	VESA_132X50	= 206,
 	VESA_132X60	= 196,
-
-	/* Custom Mode */
-	CIOLIB_MODE_CUSTOM = 255,	// Last mode... if it's over 255, text_info can't hold it.
 };
 
 #define COLOR_MODE	C80
@@ -249,8 +246,8 @@ struct conio_font_data_struct {
         char 	*eight_by_sixteen;
         char 	*eight_by_fourteen;
         char 	*eight_by_eight;
+        char	*put_xlat;
         char 	*desc;
-        enum ciolib_codepage cp;
 };
 
 CIOLIBEXPORTVAR struct conio_font_data_struct conio_fontdata[257];
@@ -365,8 +362,6 @@ typedef struct {
 	uint32_t	(*map_rgb)(uint16_t r, uint16_t g, uint16_t b);
 	void	(*replace_font)(uint8_t id, char *name, void *data, size_t size);
 	int	(*checkfont)(int font_num);
-	void	(*setwinsize)	(int width, int height);
-	void	(*setwinposition)	(int x, int y);
 } cioapi_t;
 
 CIOLIBEXPORTVAR cioapi_t cio_api;
@@ -374,8 +369,13 @@ CIOLIBEXPORTVAR int _wscroll;
 CIOLIBEXPORTVAR int directvideo;
 CIOLIBEXPORTVAR int hold_update;
 CIOLIBEXPORTVAR int puttext_can_move;
+CIOLIBEXPORTVAR int ciolib_xlat;
+#define CIOLIB_XLAT_NONE	0
+#define CIOLIB_XLAT_CHARS	1
+#define CIOLIB_XLAT_ATTR	2
+#define CIOLIB_XLAT_ALL		(CIOLIB_XLAT_CHARS | CIOLIB_XLAT_ATTR)
+
 CIOLIBEXPORTVAR int ciolib_reaper;
-CIOLIBEXPORTVAR char *ciolib_appname;
 
 #define _conio_kbhit()		kbhit()
 
@@ -455,8 +455,6 @@ CIOLIBEXPORT int CIOLIBCALL ciolib_attrfont(uint8_t attr);
 CIOLIBEXPORT int CIOLIBCALL ciolib_checkfont(int font_num);
 CIOLIBEXPORT void CIOLIBCALL ciolib_set_vmem(struct vmem_cell *cell, uint8_t ch, uint8_t attr, uint8_t font);
 CIOLIBEXPORT void CIOLIBCALL ciolib_set_vmem_attr(struct vmem_cell *cell, uint8_t attr);
-CIOLIBEXPORT void CIOLIBCALL ciolib_setwinsize(int width, int height);
-CIOLIBEXPORT void CIOLIBCALL ciolib_setwinposition(int x, int y);
 
 /* DoorWay specific stuff that's only applicable to ANSI mode. */
 CIOLIBEXPORT void CIOLIBCALL ansi_ciolib_setdoorway(int enable);
@@ -538,20 +536,16 @@ CIOLIBEXPORT void CIOLIBCALL ansi_ciolib_setdoorway(int enable);
 	#define checkfont(a)			ciolib_checkfont(a)
 	#define set_vmem(a, b, c, d)		ciolib_set_vmem(a, b, c, d)
 	#define set_vmem_attr(a, b)		ciolib_set_vmem_attr(a, b)
-	#define setwinsize(a,b)			ciolib_setwinsize(a,b)
-	#define setwinposition(a,b)		ciolib_setwinposition(a,b)
 #endif
 
 #ifdef WITH_SDL
 	#include <gen_defs.h>
 	#include <SDL.h>
 
-#ifdef _WIN32
 	#ifdef main
 		#undef main
 	#endif
-	#define main	CIOLIB_main
-#endif
+	#define	main	CIOLIB_main
 #endif
 
 #define CIOLIB_BUTTON_1	1
@@ -641,10 +635,7 @@ CIOLIBEXPORT int CIOLIBCALL ciomouse_delevent(int event);
 #define CIO_KEY_RIGHT     (0x4d << 8)
 #define CIO_KEY_PPAGE     (0x49 << 8)
 #define CIO_KEY_NPAGE     (0x51 << 8)
-#define CIO_KEY_SHIFT_F(x)((x<11)?((0x53 + x) << 8):((0x7c + x) << 8))
-#define CIO_KEY_CTRL_F(x) ((x<11)?((0x5d + x) << 8):((0x7e + x) << 8))
-#define CIO_KEY_ALT_F(x)  ((x<11)?((0x67 + x) << 8):((0x80 + x) << 8))
-#define CIO_KEY_BACKTAB   (0x0f << 8)
+#define CIO_KEY_ALT_F(x)  ((x<11)?((0x67+x) << 8):((0x80+x) << 8))
 
 #define CIO_KEY_MOUSE     0x7d00	// This is the right mouse on Schneider/Amstrad PC1512 PC keyboards "F-14"
 #define CIO_KEY_QUIT	  0x7e00	// "F-15"
