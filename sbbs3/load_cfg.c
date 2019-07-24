9a013828497fb7777abc63e888db25608cde68a6
rswindell <rswindell>
2019-07-24 04:41:49 +0000

    winsock2.h needs to be #included *before* windows.h. Since sockwrap.h #Includes winsock2.h, let's #include it before any other header files (e.g. genwrap.h)

--- sbbs3/load_cfg.c
+++ sbbs3/load_cfg.c
@@ -1,6 +1,6 @@
 /* Synchronet configuration load routines (exported) */
 
-/* $Id: load_cfg.c,v 1.75 2018/10/18 21:28:23 rswindell Exp $ */
+/* $Id: load_cfg.c,v 1.76 2019/05/09 21:14:19 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -344,7 +344,7 @@ BOOL md(char *inpath)
 /****************************************************************************/
 BOOL read_attr_cfg(scfg_t* cfg, char* error)
 {
-	char*	p;
+	uint*	clr;
     char    str[256],fname[13];
 	long	offset=0;
     FILE    *instream;
@@ -356,7 +356,7 @@ BOOL read_attr_cfg(scfg_t* cfg, char* error)
 		return(FALSE); 
 	}
 	FREE_AND_NULL(cfg->color);
-	if((cfg->color=malloc(MIN_COLORS))==NULL) {
+	if((cfg->color=malloc(MIN_COLORS * sizeof(uint)))==NULL) {
 		sprintf(error,"Error allocating memory (%u bytes) for colors"
 			,MIN_COLORS);
 		fclose(instream);
@@ -370,9 +370,9 @@ BOOL read_attr_cfg(scfg_t* cfg, char* error)
 		if(readline(&offset,str,4,instream)==NULL)
 			break;
 		if(cfg->total_colors>=MIN_COLORS) {
-			if((p=realloc(cfg->color,cfg->total_colors+1))==NULL)
+			if((clr=realloc(cfg->color,(cfg->total_colors+1) * sizeof(uint)))==NULL)
 				break;
-			cfg->color=p;
+			cfg->color=clr;
 		}
 		cfg->color[cfg->total_colors]=attrstr(str); 
 	}
