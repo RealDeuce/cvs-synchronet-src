13d5978f846cdfda7e5475b72f6fb2846cfa9f80
rswindell <rswindell>
2019-07-25 10:55:01 +0000

    We're storing the charset of locally created messages in the (new) SMB hfield: FIDOCHARSET, no longer FIDOCTRL.

--- sbbs3/putmsg.cpp
+++ sbbs3/putmsg.cpp
@@ -1,7 +1,7 @@
 /* Synchronet message/menu display routine */
 // vi: tabstop=4
 
-/* $Id: putmsg.cpp,v 1.51 2019/07/10 21:41:38 rswindell Exp $ */
+/* $Id: putmsg.cpp,v 1.52 2019/07/24 05:00:10 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -36,6 +36,7 @@
 
 #include "sbbs.h"
 #include "wordwrap.h"
+#include "utf8.h"
 
 /****************************************************************************/
 /* Outputs a NULL terminated string with @-code parsing,                    */
@@ -65,10 +66,7 @@ char sbbs_t::putmsg(const char *buf, long mode, long org_cols)
 		attr(LIGHTGRAY);
 	if(mode&P_NOPAUSE)
 		sys_status|=SS_PAUSEOFF;
-	if(strncmp(str, "\xEF\xBB\xBF", 3) == 0) {
-		mode |= P_UTF8;
-		str += 3;
-	}
+	str = auto_utf8(str, &mode);
 	size_t len = strlen(str);
 
 	long term = term_supports();
