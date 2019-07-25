4295acae0b5a35a1f603fc76dd48f57de57d2640
deuce <deuce>
2019-07-25 18:44:32 +0000

    Mode numbers can't be over 255 if text_info needs to hold them.

--- xpdev/unicode.c
+++ xpdev/unicode.c
@@ -1,6 +1,6 @@
 /* Synchronet Unicode encode/decode/translate functions */
 
-/* $Id: unicode.c,v 1.15 2019/08/30 11:04:53 rswindell Exp $ */
+/* $Id: unicode.c,v 1.11 2019/07/15 02:19:34 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -177,7 +177,7 @@ enum unicode_codepoint cp437_unicode_tbl[] =
 	0,
 	0,
 	0,
-	/* 0x7F (DEL) */ UNICODE_TERM_CTRL_CHAR_CODE(UNICODE_HOUSE),
+	/* 0x7F (DEL) */ UNICODE_TERM_CTRL_CHAR_CODE(0x2302),
 	/* 0x80 */ UNICODE_LATIN_CAPITAL_LETTER_C_WITH_CEDILLA,
 	/* 0x81 */ UNICODE_LATIN_SMALL_LETTER_U_WITH_DIAERESIS,
 	/* 0x82 */ UNICODE_LATIN_SMALL_LETTER_E_WITH_ACUTE,
@@ -270,10 +270,10 @@ enum unicode_codepoint cp437_unicode_tbl[] =
 	/* 0xD9 */ 0x2518,
 	/* 0xDA */ 0x250C,
 	/* 0xDB */ 0x2588,
-	/* 0xDC */ UNICODE_LOWER_HALF_BLOCK,
+	/* 0xDC */ 0x2584,
 	/* 0xDD */ 0x258C,
 	/* 0xDE */ 0x2590,
-	/* 0xDF */ UNICODE_UPPER_HALF_BLOCK,
+	/* 0xDF */ 0x2580,
 	/* 0xE0 */ UNICODE_GREEK_SMALL_LETTER_ALPHA,
 	/* 0xE1 */ UNICODE_GREEK_SMALL_LETTER_BETA, // or UNICODE_LATIN_SMALL_LETTER_SHARP_S
 	/* 0xE2 */ UNICODE_GREEK_SMALL_LETTER_GAMMA,
@@ -484,30 +484,6 @@ char unicode_to_cp437(enum unicode_codepoint codepoint)
 		case UNICODE_SET_MINUS:									return '\\';
 		case UNICODE_ASTERISK_OPERATOR:							return '*';
 
-		case UNICODE_DOUBLE_VERTICAL_LINE:						return CP437_BOX_DRAWINGS_DOUBLE_VERTICAL;
-
-		case UNICODE_DOUBLE_LOW_LINE:							return '=';
-		case UNICODE_LEFT_SINGLE_QUOTATION_MARK:
-		case UNICODE_RIGHT_SINGLE_QUOTATION_MARK:
-		case UNICODE_SINGLE_HIGH_REVERSED_9_QUOTATION_MARK:		return '\'';
-		case UNICODE_SINGLE_LOW_9_QUOTATION_MARK:				return ',';
-		case UNICODE_LEFT_DOUBLE_QUOTATION_MARK:
-		case UNICODE_RIGHT_DOUBLE_QUOTATION_MARK:				
-		case UNICODE_DOUBLE_LOW_9_QUOTATION_MARK:
-		case UNICODE_DOUBLE_HIGH_REVERSED_9_QUOTATION_MARK:		return '"';
-		case UNICODE_DAGGER:									return CP437_BOX_DRAWINGS_VERTICAL_AND_HORIZONTAL;
-		
-		case UNICODE_BLACK_SQUARE:
-		case UNICODE_BLACK_SQUARE_CENTERED:
-		case UNICODE_BLACK_SQUARE_FOR_STOP:
-		case UNICODE_BLACK_SMALL_SQUARE:
-		case UNICODE_BLACK_MEDIUM_SQUARE:
-		case UNICODE_BLACK_LARGE_SQUARE:
-		case UNICODE_BLACK_MEDIUM_SMALL_SQUARE:
-		case UNICODE_BLACK_VERY_SMALL_SQUARE:	
-		case UNICODE_HALFWIDTH_BLACK_SQUARE:					return CP437_HALFWIDTH_BLACK_SQUARE;
-
-		case UNICODE_HORIZONTAL_BAR:
 		case UNICODE_OVERLINE:
 		case 0x2500: // Box Drawings Light Horizontal
 		case 0x2501: // Box Drawings Heavy Horizontal
@@ -648,17 +624,6 @@ char unicode_to_cp437(enum unicode_codepoint codepoint)
 		case 0x257F: // Box Drawings Heavy Up and Light Down
 			return '\xB3';
 
-		case UNICODE_FULL_BLOCK:
-			return CP437_FULL_BLOCK;
-		case UNICODE_LOWER_HALF_BLOCK:
-			return CP437_LOWER_HALF_BLOCK;
-		case UNICODE_LEFT_HALF_BLOCK:
-			return CP437_LEFT_HALF_BLOCK;
-		case UNICODE_RIGHT_HALF_BLOCK:
-			return CP437_RIGHT_HALF_BLOCK;
-		case UNICODE_UPPER_HALF_BLOCK:
-			return CP437_UPPER_HALF_BLOCK;
-
 		case 0x2581: // Lower One Eighth Block
 			return '_';
 
@@ -671,16 +636,19 @@ char unicode_to_cp437(enum unicode_codepoint codepoint)
 		case 0x2587: // Lower Seven Eighths Block
 			return '\xDC';
 
+		case 0x2588: // Full Block
 		case 0x2589: // Left Seven Eighths Block
 			return '\xDB';
 
 		case 0x258A: // Left Three Quarters Block
 		case 0x258B: // Left Five Eighths Block
+		case 0x258C: // Left Half Block
 		case 0x258D: // Left Three Eighths Block
 		case 0x258E: // Left One Quarter Block
 		case 0x258F: // Left One Eighth Block
 			return '\xDD';
 
+		case 0x2590: // Right Half Block
 		case 0x2595: // Right One Eighth Block
 			return '\xDE';
 
