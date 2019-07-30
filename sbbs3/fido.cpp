b049d0a980872d05b7616120d32cb952707a47d6
rswindell <rswindell>
2019-07-30 08:21:09 +0000

    Clarify the help text of the AUTO-UTF8 option a bit more (it's detected during *display* in the terminal server, so this option would help, say, the web server).

--- sbbs3/fido.cpp
+++ sbbs3/fido.cpp
@@ -1,6 +1,6 @@
 /* Synchronet FidoNet-related routines */
 
-/* $Id: fido.cpp,v 1.76 2019/08/18 20:30:43 rswindell Exp $ */
+/* $Id: fido.cpp,v 1.74 2019/07/26 08:13:55 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -173,8 +173,7 @@ bool sbbs_t::netmail(const char *into, const char *title, long mode, smb_t* resm
 	}
 	p=strrchr(to,'@');      /* Find '@' in name@addr */
 	if(p==NULL || net_type != NET_FIDO) {
-		if(!(sys_status&SS_ABORT))
-			bputs(text[InvalidNetMailAddr]);
+		bputs(text[InvalidNetMailAddr]);
 		return false; 
 	}
 	if(!cfg.total_faddrs || (!SYSOP && !(cfg.netmail_misc&NMAIL_ALLOW))) {
@@ -234,7 +233,7 @@ bool sbbs_t::netmail(const char *into, const char *title, long mode, smb_t* resm
 	if(cfg.netmail_misc&NMAIL_CRASH) msg.hdr.netattr |= MSG_CRASH;
 	if(cfg.netmail_misc&NMAIL_HOLD)  msg.hdr.netattr |= MSG_HOLD;
 	if(cfg.netmail_misc&NMAIL_KILL)  msg.hdr.netattr |= MSG_KILLSENT;
-	if(mode&WM_FILE) msg.hdr.auxattr |= (MSG_FILEATTACH | MSG_KILLFILE); 
+	if(mode&WM_FILE) msg.hdr.auxattr |= MSG_FILEATTACH; 
 
 	if(remsg != NULL && resmb != NULL && !(mode&WM_QUOTE)) {
 		if(quotemsg(resmb, remsg, /* include tails: */true))
