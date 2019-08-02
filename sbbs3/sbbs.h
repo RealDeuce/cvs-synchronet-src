b648aae8eadfbcca49df910a15cc1c0870819455
rswindell <rswindell>
2019-08-02 10:36:45 +0000

    Make the auto-hangup-after-transfer feature disable-able on a per-call basis (defauls to enabled, for backward compatibility). Added new autohang args to bbs.send_file() and bbs.receive_file() (default:true) Added support for "description" argument to bbs.send_file() as well.

--- sbbs3/sbbs.h
+++ sbbs3/sbbs.h
@@ -1,6 +1,6 @@
 /* Synchronet class (sbbs_t) definition and exported function prototypes */
 // vi: tabstop=4
-/* $Id: sbbs.h,v 1.534 2019/07/24 05:00:10 rswindell Exp $ */
+/* $Id: sbbs.h,v 1.535 2019/08/02 10:36:45 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -884,14 +884,14 @@ public:
 	/* download.cpp */
 	void	downloadfile(file_t* f);
 	void	notdownloaded(ulong size, time_t start, time_t end);
-	int		protocol(prot_t* prot, enum XFER_TYPE, char *fpath, char *fspec, bool cd);
+	int		protocol(prot_t* prot, enum XFER_TYPE, char *fpath, char *fspec, bool cd, bool autohangup=true);
 	const char*	protcmdline(prot_t* prot, enum XFER_TYPE type);
 	void	seqwait(uint devnum);
 	void	autohangup(void);
 	bool	checkdszlog(const char*);
 	bool	checkprotresult(prot_t*, int error, file_t*);
-	bool	sendfile(char* fname, char prot=0, const char* description = NULL);
-	bool	recvfile(char* fname, char prot=0);
+	bool	sendfile(char* fname, char prot=0, const char* description = NULL, bool autohang=true);
+	bool	recvfile(char* fname, char prot=0, bool autohang=true);
 
 	/* file.cpp */
 	void	fileinfo(file_t* f);
