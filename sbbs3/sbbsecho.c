58c74af27bb6a5bf7abc567d25e5d2ddeda1b9f5
rswindell <rswindell>
2019-08-04 06:05:18 +0000

    Revamp the 0-byte inbound bundle handling: - Don't ever try to unpack a 0-byte file. - Don't log anything or try delete if the file is < 24 hours old. - Try to delete the file if it's >= 24 hours old (and log a msg, INFO-level).

--- sbbs3/sbbsecho.c
+++ sbbs3/sbbsecho.c
@@ -1,6 +1,6 @@
 /* Synchronet FidoNet EchoMail Scanning/Tossing and NetMail Tossing Utility */
 
-/* $Id: sbbsecho.c,v 3.128 2019/08/03 11:31:10 rswindell Exp $ */
+/* $Id: sbbsecho.c,v 3.129 2019/08/04 06:05:18 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -2743,26 +2743,29 @@ bool unpack_bundle(const char* inbound)
 		if(gi<g.gl_pathc) {
 			SAFECOPY(fname,g.gl_pathv[gi]);
 			gi++;
-			lprintf(LOG_INFO,"Unpacking bundle: %s (%1.1fKB)", fname, flength(fname)/1024.0);
-			if(unpack(fname, inbound)) {	/* failure */
-				lprintf(LOG_ERR, "!Unpack failure: %s", fname);
-				if(fdate(fname)+(48L*60L*60L)<time(NULL)) {
-					/* If bundle file older than 48 hours, give up */
-					if(flength(fname) < 1) {
-						delfile(fname, __LINE__);	/* Delete it if it's a 0-byte file */
-						continue;
-					}
-					/* rename to "*.?_?" or (if it exists) "*.?-?" */
-					SAFECOPY(str,fname);
-					str[strlen(str)-2]='_';
-					if(fexistcase(str))
-						str[strlen(str)-2]='-';
-					if(fexistcase(str))
-						delfile(str, __LINE__);
-					if(rename(fname,str))
-						lprintf(LOG_ERR,"ERROR line %d renaming %s to %s"
-							,__LINE__,fname,str);
+			off_t length = flength(fname);
+			if(length < 1) {
+				if(fdate(fname) < time(NULL) + (24*60*60))
+					lprintf(LOG_DEBUG, "Ignoring %ld-byte file (less than 24-hours old): ", (long)length, fname);
+				else {
+					lprintf(LOG_INFO, "Deleting %ld-byte file (more than 24-hours old): ", (long)length, fname);
+					delfile(fname, __LINE__);	/* Delete it if it's a 0-byte file */
 				}
+				continue;
+			}
+			lprintf(LOG_INFO,"Unpacking bundle: %s (%1.1fKB)", fname, length/1024.0);
+			if(unpack(fname, inbound) != 0) {	/* failure */
+				lprintf(LOG_ERR, "!Unpack failure: %s", fname);
+				/* rename to "*.?_?" or (if it exists) "*.?-?" */
+				SAFECOPY(str,fname);
+				str[strlen(str)-2]='_';
+				if(fexistcase(str))
+					str[strlen(str)-2]='-';
+				if(fexistcase(str))
+					delfile(str, __LINE__);
+				if(rename(fname,str))
+					lprintf(LOG_ERR,"ERROR line %d renaming %s to %s"
+						,__LINE__,fname,str);
 				continue;
 			}
 			delfile(fname, __LINE__);	/* successful, so delete bundle */
@@ -6097,7 +6100,7 @@ int main(int argc, char **argv)
 		memset(&smb[i],0,sizeof(smb_t));
 	memset(&cfg,0,sizeof(cfg));
 
-	sscanf("$Revision: 3.128 $", "%*s %s", revision);
+	sscanf("$Revision: 3.129 $", "%*s %s", revision);
 
 	DESCRIBE_COMPILER(compiler);
 
