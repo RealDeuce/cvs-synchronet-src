c1d4bf159b640184bcb59d711593416fa526f6d7
rswindell <rswindell>
2019-08-09 09:44:22 +0000

    Don't store the original RCPT TO argument (forward-path) when the message is being forwarded to an external email address. Introduced in rev 1.690, user-forwarded emails wouldn't work because sending envelope would contain the original forward-path. Thanks again to Alterego (ALTERANT) for testing this stuff and reporting issues with detail!

--- sbbs3/con_out.cpp
+++ sbbs3/con_out.cpp
@@ -1,7 +1,7 @@
 /* Synchronet console output routines */
 // vi: tabstop=4
 
-/* $Id: con_out.cpp,v 1.128 2020/03/20 02:41:58 rswindell Exp $ */
+/* $Id: con_out.cpp,v 1.120 2019/08/05 11:14:03 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -331,8 +331,7 @@ size_t sbbs_t::print_utf8_as_cp437(const char* str, size_t len)
 	enum unicode_codepoint codepoint = UNICODE_UNDEFINED;
 	len = utf8_getc(str, len, &codepoint);
 	if((int)len < 2) {
-		outchar(*str);	// Assume it's a CP437 character
-		lprintf(LOG_DEBUG, "Invalid UTF-8 sequence: %02X (error = %d)", (uchar)*str, (int)len);
+		lprintf(LOG_NOTICE, "Invalid UTF-8 sequence: %02X (error = %d)", (uchar)*str, (int)len);
 		return 1;
 	}
 	for(int i = 1; i < 0x100; i++) {
@@ -375,11 +374,8 @@ int sbbs_t::rputs(const char *str, size_t len)
 	for(l=0;l<len && online;l++) {
 		uchar ch = str[l];
 		utf8[0] = 0;
-		if(term&PETSCII) {
+		if(term&PETSCII)
 			ch = cp437_to_petscii(ch);
-			if(ch == PETSCII_SOLID)
-				outcom(PETSCII_REVERSE_ON);
-		}
 		else if((term&NO_EXASCII) && (ch&0x80))
 			ch = exascii_to_ascii_char(ch);  /* seven bit table */
 		else if(term&UTF8) {
@@ -394,16 +390,9 @@ int sbbs_t::rputs(const char *str, size_t len)
 				break;
 			if((char)ch == (char)TELNET_IAC && !(telnet_mode&TELNET_MODE_OFF))
 				outcom(TELNET_IAC);	/* Must escape Telnet IAC char (255) */
-			if((term&PETSCII) && ch == PETSCII_SOLID)
-				outcom(PETSCII_REVERSE_OFF);
 		}
-		if(ch == '\n')
-			lbuflen=0;
-		else if(lbuflen<LINE_BUFSIZE) {
-			if(lbuflen == 0)
-				latr = curatr;
+		if(lbuflen<LINE_BUFSIZE)
 			lbuf[lbuflen++] = ch;
-		}
 	}
 	return(l);
 }
@@ -455,21 +444,6 @@ int sbbs_t::rprintf(const char *fmt, ...)
 	sbuf[sizeof(sbuf)-1]=0;	/* force termination */
 	va_end(argptr);
 	return(rputs(sbuf));
-}
-
-/****************************************************************************/
-/* Performs printf() using bbs putcom/outcom functions						*/
-/****************************************************************************/
-int sbbs_t::comprintf(const char *fmt, ...)
-{
-	va_list argptr;
-	char sbuf[4096];
-
-	va_start(argptr,fmt);
-	vsnprintf(sbuf,sizeof(sbuf),fmt,argptr);
-	sbuf[sizeof(sbuf)-1]=0;	/* force termination */
-	va_end(argptr);
-	return(putcom(sbuf));
 }
 
 /****************************************************************************/
@@ -751,19 +725,17 @@ void sbbs_t::inc_column(int count)
 	}
 }
 
-void sbbs_t::center(char *instr, unsigned int columns)
+void sbbs_t::center(char *instr)
 {
 	char str[256];
-	size_t len;
-
-	if(columns < 1)
-		columns = cols;
+	int i,j;
 
 	SAFECOPY(str,instr);
 	truncsp(str);
-	len = bstrlen(str);
-	if(len < columns)
-		cursor_right((columns - len) / 2);
+	j=bstrlen(str);
+	if(j < cols)
+		for(i=0;i<(cols-j)/2;i++)
+			outchar(' ');
 	bputs(str);
 	newline();
 }
@@ -830,7 +802,7 @@ void sbbs_t::cursor_home(void)
 {
 	long term = term_supports();
 	if(term&ANSI)
-		putcom("\x1b[H");
+		rputs("\x1b[H");
 	else if(term&PETSCII)
 		outcom(PETSCII_HOME);
 	else
@@ -846,9 +818,9 @@ void sbbs_t::cursor_up(int count)
 	long term = term_supports();
 	if(term&ANSI) {
 		if(count>1)
-			comprintf("\x1b[%dA",count);
+			rprintf("\x1b[%dA",count);
 		else
-			putcom("\x1b[A");
+			rputs("\x1b[A");
 	} else {
 		if(term&PETSCII) {
 			for(int i=0;i<count;i++)
@@ -863,9 +835,9 @@ void sbbs_t::cursor_down(int count)
 		return;
 	if(term_supports(ANSI)) {
 		if(count>1)
-			comprintf("\x1b[%dB",count);
+			rprintf("\x1b[%dB",count);
 		else
-			putcom("\x1b[B");
+			rputs("\x1b[B");
 	} else {
 		for(int i=0;i<count;i++)
 			line_feed();
@@ -879,9 +851,9 @@ void sbbs_t::cursor_right(int count)
 	long term = term_supports();
 	if(term&ANSI) {
 		if(count>1)
-			comprintf("\x1b[%dC",count);
+			rprintf("\x1b[%dC",count);
 		else
-			putcom("\x1b[C");
+			rputs("\x1b[C");
 	} else {
 		for(int i=0;i<count;i++) {
 			if(term&PETSCII)
@@ -900,9 +872,9 @@ void sbbs_t::cursor_left(int count)
 	long term = term_supports();
 	if(term&ANSI) {
 		if(count>1)
-			comprintf("\x1b[%dD",count);
+			rprintf("\x1b[%dD",count);
 		else
-			putcom("\x1b[D");
+			rputs("\x1b[D");
 	} else {
 		for(int i=0;i<count;i++) {
 			if(term&PETSCII)
@@ -917,27 +889,13 @@ void sbbs_t::cursor_left(int count)
 		column=0;
 }
 
-bool sbbs_t::cursor_xy(int x, int y)
-{
-	long term = term_supports();
-	if(term&ANSI)
-		return ansi_gotoxy(x, y);
-	if(term&PETSCII) {
-		outcom(PETSCII_HOME);
-		cursor_down(y - 1);
-		cursor_right(x - 1);
-		return true;
-	}
-	return false;
-}
-
 void sbbs_t::cleartoeol(void)
 {
 	int i,j;
 
 	long term = term_supports();
 	if(term&ANSI)
-		putcom("\x1b[K");
+		rputs("\x1b[K");
 	else {
 		i=j=column;
 		while(++i<cols)
@@ -954,7 +912,7 @@ void sbbs_t::cleartoeol(void)
 void sbbs_t::cleartoeos(void)
 {
 	if(term_supports(ANSI))
-		putcom("\x1b[J");
+		rputs("\x1b[J");
 }
 
 void sbbs_t::set_output_rate(enum output_rate speed)
@@ -979,7 +937,7 @@ void sbbs_t::set_output_rate(enum output_rate speed)
 					val = 11;
 				break;
 		}
-		comprintf("\x1b[;%u*r", val);
+		rprintf("\x1b[;%u*r", val);
 		cur_output_rate = speed;
 	}
 }
@@ -1325,7 +1283,7 @@ int sbbs_t::backfill(const char* instr, float pct, int full_attr, int empty_attr
 	char* str = strip_ctrl(instr, NULL);
 
 	len = strlen(str);
-	if(!(term_supports()&(ANSI|PETSCII)))
+	if(!term_supports(ANSI))
 		bputs(str);
 	else {
 		for(int i=0; i<len; i++) {
