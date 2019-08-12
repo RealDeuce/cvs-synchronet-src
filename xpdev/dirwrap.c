aef150ffed28104ceac4f9ae55fd72e8da304be9
rswindell <rswindell>
2019-08-12 06:24:08 +0000

    delfiles() now takes a 3rd argument: keep, which is a number of files to retain (not delete) from the end of the sorted list of files (normally, 0). Created an sbbs_t::delfiles() wrapper which defaults to keep 0 files and also generates a error.log message if any of the file-deletions fail.

--- xpdev/dirwrap.c
+++ xpdev/dirwrap.c
@@ -1,7 +1,7 @@
 /* Directory-related system-call wrappers */
 // vi: tabstop=4
 
-/* $Id: dirwrap.c,v 1.111 2019/09/21 11:19:07 rswindell Exp $ */
+/* $Id: dirwrap.c,v 1.107 2019/08/12 06:20:40 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -741,7 +741,7 @@ int removecase(const char *path)
 	}
 	*p=0;
 
-	return(delfiles(inpath,fname,0) >=1 ? 0 : -1);
+	return(delfiles(inpath,fname) >=1 ? 0 : -1);
 }
 #endif
 
@@ -792,10 +792,10 @@ long DLLCALL delfiles(const char *inpath, const char *spec, size_t keep)
 }
 
 /****************************************************************************/
-/* Returns number of files matching 'inpath'								*/
+/* Returns number of files in a directory (inpath) matching 'pattern'		*/
 /* Similar, but not identical, to getdirsize(), e.g. subdirs never counted	*/
 /****************************************************************************/
-ulong DLLCALL getfilecount(const char *inpath)
+ulong DLLCALL getfilecount(const char *inpath, const char* pattern)
 {
 	char path[MAX_PATH+1];
 	glob_t	g;
@@ -803,10 +803,8 @@ ulong DLLCALL getfilecount(const char *inpath)
 	ulong	count = 0;
 
 	SAFECOPY(path, inpath);
-	if(isdir(path))
-		backslash(path);
-	if(IS_PATH_DELIM(*lastchar(path)))
-		SAFECAT(path, ALLFILES);
+	backslash(path);
+	SAFECAT(path, pattern);
 	if(glob(path, GLOB_MARK, NULL, &g))
 		return 0;
 	for(gi = 0; gi < g.gl_pathc; ++gi) {
@@ -816,35 +814,6 @@ ulong DLLCALL getfilecount(const char *inpath)
 	}
 	globfree(&g);
 	return count;
-}
-
-/****************************************************************************/
-/* Returns number of bytes used by file(s) matching 'inpath'				*/
-/****************************************************************************/
-uint64_t DLLCALL getfilesizetotal(const char *inpath)
-{
-	char path[MAX_PATH+1];
-	glob_t	g;
-	uint	gi;
-	off_t	size;
-	uint64_t total = 0;
-
-	SAFECOPY(path, inpath);
-	if(isdir(path))
-		backslash(path);
-	if(IS_PATH_DELIM(*lastchar(path)))
-		SAFECAT(path, ALLFILES);
-	if(glob(path, GLOB_MARK, NULL, &g))
-		return 0;
-	for(gi = 0; gi < g.gl_pathc; ++gi) {
-		if(*lastchar(g.gl_pathv[gi]) == '/')
-			continue;
-		size = flength(g.gl_pathv[gi]);
-		if(size >= 1)
-			total += size;
-	}
-	globfree(&g);
-	return total;
 }
 
 /****************************************************************************/
