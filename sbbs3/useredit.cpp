41ec3fc45198b5b7f56ed80e26ee15f726b7abe5
rswindell <rswindell>
2019-08-14 21:12:24 +0000

    Fix bug in previous commit - a line is not a kludge line if it doesn't contain a CR *and* it doesn't contain a 0xE3 (QWK newline). <sigh>

--- sbbs3/useredit.cpp
+++ sbbs3/useredit.cpp
@@ -1,6 +1,6 @@
 /* Synchronet online sysop user editor */
 
-/* $Id: useredit.cpp,v 1.69 2020/03/31 08:23:49 rswindell Exp $ */
+/* $Id: useredit.cpp,v 1.66 2019/08/09 09:16:59 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -59,8 +59,16 @@ void sbbs_t::useredit(int usernumber)
 	user_t	user;
 	struct	tm tm;
 
-	if(!chksyspass())
+	if(online==ON_REMOTE && console&(CON_R_ECHO|CON_R_INPUT) && !chksyspass())
 		return;
+#if 0	/* no local logins in v3 */
+	if(online==ON_LOCAL) {
+		if(!(cfg.sys_misc&SM_L_SYSOP))
+			return;
+		if(cfg.node_misc&NM_SYSPW && !chksyspass())
+			return; 
+	}
+#endif
 	if(usernumber)
 		user.number=usernumber;
 	else
@@ -168,7 +176,7 @@ void sbbs_t::useredit(int usernumber)
 			user.number=(ushort)(l&~0x80000000L);
 			continue; 
 		}
-		if(l != '[' && l != ']' && l != '{' && l != '}' && l != '?')
+		if(l != '[' && l != ']' && l != '{' && l != '}')
 			newline();
 		switch(l) {
 			case 'A':
@@ -582,7 +590,11 @@ void sbbs_t::useredit(int usernumber)
 					l*=1024;
 				else if(strstr(str,"$"))
 					l*=cfg.cdt_per_dollar;
-				adjustuserrec(&cfg, user.number, U_CDT, 10, l);
+				if(l<0L && l*-1 > (long)user.cdt)
+					user.cdt=0L;
+				else
+					user.cdt+=l;
+				putuserrec(&cfg,user.number,U_CDT,10,ultoa(user.cdt,tmp,10));
 				break;
 			case '*':
 				bputs(text[ModifyMinutes]);
@@ -1073,7 +1085,7 @@ void sbbs_t::maindflts(user_t* user)
 						pause();
 						break; 
 					}
-					bprintf(text[NewPasswordPromptFmt], MIN_PASS_LEN, LEN_PASS);
+					bputs(text[NewPassword]);
 					if(!getstr(str,LEN_PASS,K_UPPER|K_LINE))
 						break;
 					truncsp(str);
