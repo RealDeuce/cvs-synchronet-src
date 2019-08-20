ea711c345b0ac3eedbb1722bbe442616a1260ad5
rswindell <rswindell>
2019-08-20 01:33:15 +0000

    Address issue raiseed by Immortal: On Linux, using DOSEMU, he would end up with both door.sys and DOOR.SYS in the node directory and some doors would open the wrong file and fail to initialize. Solution: delete all case-versions of drop filenames before creating them. Don't you just love case-sensitive file systems? :-(

--- sbbs3/js_global.c
+++ sbbs3/js_global.c
@@ -1,6 +1,6 @@
 /* Synchronet JavaScript "global" object properties/methods for all servers */
 
-/* $Id: js_global.c,v 1.387 2019/08/04 08:26:01 rswindell Exp $ */
+/* $Id: js_global.c,v 1.388 2019/08/08 03:53:45 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -307,7 +307,7 @@ js_load(JSContext *cx, uintN argc, jsval *arglist)
 			}
 		}
 		else {
-			lprintf(LOG_ERR, "!ERROR unabled to locate global js object");
+			lprintf(LOG_ERR, "!ERROR unable to locate global js object");
 		}
 
 		if((bg->runtime = jsrt_GetNew(JAVASCRIPT_MAX_BYTES, 1000, __FILE__, __LINE__))==NULL) {
@@ -639,6 +639,7 @@ js_load(JSContext *cx, uintN argc, jsval *arglist)
 		JS_ENDREQUEST(bg->cx);
 		JS_ClearContextThread(bg->cx);
 		bg->sem=&p->bg_sem;
+		lprintf(LOG_DEBUG, "JavaScript Background Load: %s", path);
 		success = _beginthread(background_thread,0,bg)!=-1;
 		JS_RESUMEREQUEST(cx, rc);
 		if(success) {
