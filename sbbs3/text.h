c2107a467ed1451fe672885851fa4222974d931b
rswindell <rswindell>
2019-08-20 03:12:28 +0000

    Create sbbs_t::comprintf(), a direct-output printf(), used in place of rprintf() when "extra raw" output is needed. Use putcom() and comprintf() instead of rputs() and rprintf() for ANSI escape sequences. This doesn't actually fix any problem, but since rputs() is "less raw" than it used to be, let's use direct-output routines instead, for ANSI output.

--- sbbs3/text.h
+++ sbbs3/text.h
@@ -342,7 +342,7 @@ enum {
 	,HappyBirthday
 	,TimeToChangePw
 	,NewPasswordQ
-	,NewPasswordPromptFmt
+	,NewPassword
 	,VerifyPassword
 	,Wrong
 	,PasswordChanged
@@ -838,8 +838,6 @@ enum {
 	,MsgCarbonCopyList
 	,LoggingOn
 	,QWKSettingsUtf8
-	,MsgPostedToYouVia
-	,Unlimited
 
 	,TOTAL_TEXT
 };
