632d3786b40c45a94247108b9b6baf7384c9a1dd
rswindell <rswindell>
2019-08-20 07:53:37 +0000

    New string: MsgPostedToYouVia

--- sbbs3/scfg/scfgnet.c
+++ sbbs3/scfg/scfgnet.c
@@ -1,4 +1,5 @@
-/* $Id: scfgnet.c,v 1.40 2019/01/12 12:09:15 rswindell Exp $ */
+/* $Id: scfgnet.c,v 1.43 2019/08/17 02:27:48 rswindell Exp $ */
+// vi: tabstop=4
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -253,7 +254,7 @@ void net_cfg()
 								SAFECOPY(cfg.qhub[i]->pack,"%@zip -jD %f %s");
 								SAFECOPY(cfg.qhub[i]->unpack,"%@unzip -Coj %f %s -d %g");
 								SAFECOPY(cfg.qhub[i]->call,"*qnet-ftp %s hub.address YOURPASS");
-								cfg.qhub[i]->node=1;
+								cfg.qhub[i]->node = NODE_ANY;
 								cfg.qhub[i]->days=(uchar)0xff; /* all days */
 								uifc.changes=1;
 								continue; 
@@ -805,7 +806,11 @@ void qhub_edit(int num)
 		sprintf(opt[i++],"%-27.27s%.40s","Pack Command Line",cfg.qhub[num]->pack);
 		sprintf(opt[i++],"%-27.27s%.40s","Unpack Command Line",cfg.qhub[num]->unpack);
 		sprintf(opt[i++],"%-27.27s%.40s","Call-out Command Line",cfg.qhub[num]->call);
-		sprintf(opt[i++],"%-27.27s%u","Call-out Node",cfg.qhub[num]->node);
+		if(cfg.qhub[num]->node == NODE_ANY)
+			SAFECOPY(str, "Any");
+		else
+			SAFEPRINTF(str, "%u", cfg.qhub[num]->node);
+		sprintf(opt[i++],"%-27.27s%s","Call-out Node", str);
 		sprintf(opt[i++],"%-27.27s%s","Call-out Days",daystr(cfg.qhub[num]->days));
 		if(cfg.qhub[num]->freq) {
 			sprintf(str,"%u times a day",1440/cfg.qhub[num]->freq);
@@ -818,6 +823,7 @@ void qhub_edit(int num)
 		sprintf(opt[i++],"%-27.27s%s","Include Kludge Lines", cfg.qhub[num]->misc&QHUB_NOKLUDGES ? "No":"Yes");
 		sprintf(opt[i++],"%-27.27s%s","Include VOTING.DAT File", cfg.qhub[num]->misc&QHUB_NOVOTING ? "No":"Yes");
 		sprintf(opt[i++],"%-27.27s%s","Include HEADERS.DAT File", cfg.qhub[num]->misc&QHUB_NOHEADERS ? "No":"Yes");
+		sprintf(opt[i++],"%-27.27s%s","Include UTF-8 Characters", cfg.qhub[num]->misc&QHUB_UTF8 ? "Yes":"No");
 		sprintf(opt[i++],"%-27.27s%s","Extended (QWKE) Packets", cfg.qhub[num]->misc&QHUB_EXT ? "Yes":"No");
 		sprintf(opt[i++],"%-27.27s%s","Exported Ctrl-A Codes"
 			,cfg.qhub[num]->misc&QHUB_EXPCTLA ? "Expand" : cfg.qhub[num]->misc&QHUB_RETCTLA ? "Leave in" : "Strip");
@@ -915,16 +921,23 @@ void qhub_edit(int num)
 					,cfg.qhub[num]->call,sizeof(cfg.qhub[num]->call)-1,K_EDIT);
 				break;
 			case 4:
-				sprintf(str,"%u",cfg.qhub[num]->node);
+				if(cfg.qhub[num]->node == NODE_ANY)
+					SAFECOPY(str, "Any");
+				else
+					SAFEPRINTF(str, "%u", cfg.qhub[num]->node);
 				uifc.helpbuf=
 					"`Node to Perform Call-out:`\n"
 					"\n"
 					"This is the number of the node to perform the call-out for this QWK\n"
-					"network hub.\n"
+					"network hub (or `Any`).\n"
 				;
-				uifc.input(WIN_MID|WIN_SAV,0,0
-					,"Node to Perform Call-out",str,3,K_EDIT|K_NUMBER);
-				cfg.qhub[num]->node=atoi(str);
+				if(uifc.input(WIN_MID|WIN_SAV,0,0
+					,"Node to Perform Call-out",str,3,K_EDIT) > 0) {
+					if(isdigit(*str))
+						cfg.qhub[num]->node=atoi(str);
+					else
+						cfg.qhub[num]->node = NODE_ANY;
+				}
 				break;
 			case 5:
 				j=0;
@@ -1014,10 +1027,14 @@ void qhub_edit(int num)
 				uifc.changes=1;
 				break;
 			case 10:
-				cfg.qhub[num]->misc^=QHUB_EXT;
+				cfg.qhub[num]->misc^=QHUB_UTF8;
 				uifc.changes=1;
 				break;
 			case 11:
+				cfg.qhub[num]->misc^=QHUB_EXT;
+				uifc.changes=1;
+				break;
+			case 12:
 				i = cfg.qhub[num]->misc&QHUB_CTRL_A;
 				i++;
 				if(i == QHUB_CTRL_A) i = 0;
@@ -1025,10 +1042,10 @@ void qhub_edit(int num)
 				cfg.qhub[num]->misc |= i;
 				uifc.changes=1;
 				break;
-			case 12:
+			case 13:
 				import_qwk_conferences(num);
 				break;
-			case 13:
+			case 14:
 				qhub_sub_edit(num);
 				break; 
 		} 
