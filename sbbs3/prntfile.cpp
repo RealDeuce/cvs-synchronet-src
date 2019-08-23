7dad4c0c5724e9d1e6d83f2b1c803bc6144b0d09
rswindell <rswindell>
2019-08-23 04:50:06 +0000

    Add ftn_charset property for message headers. This header field corresponds with the FTN (FTS-5003) "CHRS" control line/paragraph. The values recoginized by Synchronet are:     "ASCII 1"     "CP437 2"     "UTF-8 4"
    These values indicate that header fields and body text of a message are
    encoded with the specifiec charset. The default (assumed charset, if not
    specified), is CP437.

--- sbbs3/prntfile.cpp
+++ sbbs3/prntfile.cpp
@@ -3,7 +3,7 @@
 
 /* Synchronet file print/display routines */
 
-/* $Id: prntfile.cpp,v 1.37 2019/08/09 03:39:18 rswindell Exp $ */
+/* $Id: prntfile.cpp,v 1.39 2019/08/16 06:47:09 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -120,6 +120,9 @@ bool sbbs_t::printfile(const char* fname, long mode, long org_cols)
 		}
 		free(buf);
 	} else {	// Line-at-a-time mode
+		uint tmpatr = curatr;
+		if(!(mode&P_SAVEATR))
+			attr(LIGHTGRAY);
 		if(length > PRINTFILE_MAX_LINE_LEN)
 			length = PRINTFILE_MAX_LINE_LEN;
 		if((buf=(char*)malloc(length+1L))==NULL) {
@@ -132,10 +135,12 @@ bool sbbs_t::printfile(const char* fname, long mode, long org_cols)
 				break;
 			if((mode&P_UTF8) && !term_supports(UTF8))
 				utf8_normalize_str(buf);
-			putmsg(buf, mode, org_cols);
+			putmsg(buf, mode|P_SAVEATR, org_cols);
 		}
 		free(buf);
 		fclose(stream);
+		if(!(mode&P_SAVEATR))
+			attr(tmpatr);
 	}
 
 	if((mode&P_NOABORT || rip) && online==ON_REMOTE) {
