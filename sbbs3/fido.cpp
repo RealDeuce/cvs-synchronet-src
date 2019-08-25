5046156c8c6b1d9693e585aea47153999f322a4c
rswindell <rswindell>
2019-08-25 21:25:40 +0000

    Include the undeprecate property sheet.

--- sbbs3/fido.cpp
+++ sbbs3/fido.cpp
@@ -1,6 +1,6 @@
 /* Synchronet FidoNet-related routines */
 
-/* $Id: fido.cpp,v 1.68 2019/03/24 08:54:02 rswindell Exp $ */
+/* $Id: fido.cpp,v 1.76 2019/08/18 20:30:43 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -108,7 +108,8 @@ bool sbbs_t::netmail(const char *into, const char *title, long mode, smb_t* resm
 	char	subj[FIDO_SUBJ_LEN]= "";
 	char	msgpath[MAX_PATH+1];
 	char 	tmp[512];
-	char*	editor=NULL;
+	const char*	editor=NULL;
+	const char*	charset=NULL;
 	int		file,x;
 	uint	i;
 	long	length,l;
@@ -157,8 +158,7 @@ bool sbbs_t::netmail(const char *into, const char *title, long mode, smb_t* resm
 			bputs(text[EmailFilesNotAllowed]);
 			mode&=~WM_FILE;
 		}
-		qnetmail(to, title, mode);
-		return false; 
+		return qnetmail(to, title, mode, resmb, remsg);
 	}
 	if(net_type == NET_INTERNET) {
 		if(!(cfg.inetmail_misc&NMAIL_ALLOW)) {
@@ -173,7 +173,8 @@ bool sbbs_t::netmail(const char *into, const char *title, long mode, smb_t* resm
 	}
 	p=strrchr(to,'@');      /* Find '@' in name@addr */
 	if(p==NULL || net_type != NET_FIDO) {
-		bputs(text[InvalidNetMailAddr]);
+		if(!(sys_status&SS_ABORT))
+			bputs(text[InvalidNetMailAddr]);
 		return false; 
 	}
 	if(!cfg.total_faddrs || (!SYSOP && !(cfg.netmail_misc&NMAIL_ALLOW))) {
@@ -233,7 +234,7 @@ bool sbbs_t::netmail(const char *into, const char *title, long mode, smb_t* resm
 	if(cfg.netmail_misc&NMAIL_CRASH) msg.hdr.netattr |= MSG_CRASH;
 	if(cfg.netmail_misc&NMAIL_HOLD)  msg.hdr.netattr |= MSG_HOLD;
 	if(cfg.netmail_misc&NMAIL_KILL)  msg.hdr.netattr |= MSG_KILLSENT;
-	if(mode&WM_FILE) msg.hdr.auxattr |= MSG_FILEATTACH; 
+	if(mode&WM_FILE) msg.hdr.auxattr |= (MSG_FILEATTACH | MSG_KILLFILE); 
 
 	if(remsg != NULL && resmb != NULL && !(mode&WM_QUOTE)) {
 		if(quotemsg(resmb, remsg, /* include tails: */true))
@@ -241,7 +242,7 @@ bool sbbs_t::netmail(const char *into, const char *title, long mode, smb_t* resm
 	}
 
 	msg_tmp_fname(useron.xedit, msgpath, sizeof(msgpath));
-	if(!writemsg(msgpath,nulstr,subj,WM_NETMAIL|mode,INVALID_SUB, to, from, &editor)) {
+	if(!writemsg(msgpath,nulstr,subj,WM_NETMAIL|mode,INVALID_SUB, to, from, &editor, &charset)) {
 		bputs(text[Aborted]);
 		return(false); 
 	}
@@ -351,9 +352,7 @@ bool sbbs_t::netmail(const char *into, const char *title, long mode, smb_t* resm
 
 	smb_hfield_str(&msg,SUBJECT, subj);
 
-	if(editor!=NULL)
-		smb_hfield_str(&msg,SMB_EDITOR,editor);
-	smb_hfield_bin(&msg, SMB_COLUMNS, cols);
+	editor_info_to_msg(&msg, editor, charset);
 
 	if(cfg.netmail_misc&NMAIL_DIRECT)
 		msg.hdr.netattr |= MSG_DIRECT;
@@ -919,8 +918,6 @@ void sbbs_t::qwktonetmail(FILE *rep, char *block, char *into, uchar fromhub)
 			l++;
 			if(l>=length)
 				break;
-			if((ch=ctrl_a_to_ascii_char(qwkbuf[l])) != 0)
-				write(fido,&ch,1);
 		}
 		else if(qwkbuf[l]!=LF) {
 			if(qwkbuf[l]==QWK_NEWLINE) /* QWK cr/lf char converted to hard CR */
