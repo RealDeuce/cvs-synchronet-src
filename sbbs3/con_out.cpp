3e08953dd5b98b1311157c0b5dc8c0821c3c3cc2
rswindell <rswindell>
2019-08-26 22:26:32 +0000

    New global function: utf8_width() - returns the fixed printed-width of the specified UTF-8 encoded string

--- sbbs3/con_out.cpp
+++ sbbs3/con_out.cpp
@@ -1,7 +1,7 @@
 /* Synchronet console output routines */
 // vi: tabstop=4
 
-/* $Id: con_out.cpp,v 1.115 2019/07/24 05:24:48 rswindell Exp $ */
+/* $Id: con_out.cpp,v 1.122 2019/08/20 03:12:28 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -61,6 +61,7 @@ char* sbbs_t::auto_utf8(const char* str, long* mode)
    P_UTF8
    P_AUTO_UTF8
    P_NOATCODES
+   P_TRUNCATE
  ****************************************************************************/
 int sbbs_t::bputs(const char *str, long mode)
 {
@@ -74,6 +75,19 @@ int sbbs_t::bputs(const char *str, long mode)
 	str = auto_utf8(str, &mode);
 	size_t len = strlen(str);
 	while(l < len && online) {
+		switch(str[l]) {
+			case '\b':
+			case '\r':
+			case '\n':
+			case FF:
+			case CTRL_A:
+				break;
+			default: // printing char
+				if((mode&P_TRUNCATE) && column >= (cols - 1)) {
+					l++;
+					continue;
+				}
+		}
 		if(str[l]==CTRL_A && str[l+1]!=0) {
 			l++;
 			if(str[l] == 'Z')	/* EOF (uppercase 'Z' only) */
@@ -109,12 +123,44 @@ int sbbs_t::bputs(const char *str, long mode)
 			if(term&UTF8)
 				outcom(str[l++]);
 			else
-				l += utf8_to_cp437(str + l, len - l);
+				l += print_utf8_as_cp437(str + l, len - l);
 		} else
 			outchar(str[l++]);
 	}
 	return(l);
 }
+
+/****************************************************************************/
+/* Returns the printed columns from 'str' accounting for Ctrl-A codes		*/
+/****************************************************************************/
+size_t sbbs_t::bstrlen(const char *str, long mode)
+{
+	str = auto_utf8(str, &mode);
+	size_t count = 0;
+	const char* end = str + strlen(str);
+	while (str < end) {
+		int len = 1;
+		if(*str == CTRL_A) {
+			str++;
+			if(*str == 0 || *str == 'Z')	// EOF
+				break;
+			if(*str == '[') // CR
+				count = 0;
+			else if(*str == '<' && count) // ND-Backspace
+				count--;
+		} else if(((*str) & 0x80) && (mode&P_UTF8)) {
+			enum unicode_codepoint codepoint = UNICODE_UNDEFINED;
+			len = utf8_getc(str, end - str, &codepoint);
+			if(len < 1)
+				break;
+			count += unicode_width(codepoint);;
+		} else
+			count++;
+		str += len;
+	}
+	return count;
+}
+
 
 /* Perform PETSCII terminal output translation (from ASCII/CP437) */
 unsigned char cp437_to_petscii(unsigned char ch)
@@ -276,7 +322,7 @@ int sbbs_t::petscii_to_ansibbs(unsigned char ch)
 }
 
 // Return length of sequence
-size_t sbbs_t::utf8_to_cp437(const char* str, size_t len)
+size_t sbbs_t::print_utf8_as_cp437(const char* str, size_t len)
 {
 	if(((*str)&0x80) == 0) {
 		outchar(*str);
@@ -345,8 +391,13 @@ int sbbs_t::rputs(const char *str, size_t len)
 			if((char)ch == (char)TELNET_IAC && !(telnet_mode&TELNET_MODE_OFF))
 				outcom(TELNET_IAC);	/* Must escape Telnet IAC char (255) */
 		}
-		if(lbuflen<LINE_BUFSIZE)
+		if(ch == '\n')
+			lbuflen=0;
+		else if(lbuflen<LINE_BUFSIZE) {
+			if(lbuflen == 0)
+				latr = curatr;
 			lbuf[lbuflen++] = ch;
+		}
 	}
 	return(l);
 }
@@ -401,20 +452,39 @@ int sbbs_t::rprintf(const char *fmt, ...)
 }
 
 /****************************************************************************/
+/* Performs printf() using bbs putcom/outcom functions						*/
+/****************************************************************************/
+int sbbs_t::comprintf(const char *fmt, ...)
+{
+	va_list argptr;
+	char sbuf[4096];
+
+	va_start(argptr,fmt);
+	vsnprintf(sbuf,sizeof(sbuf),fmt,argptr);
+	sbuf[sizeof(sbuf)-1]=0;	/* force termination */
+	va_end(argptr);
+	return(putcom(sbuf));
+}
+
+/****************************************************************************/
 /* Outputs destructive backspace 											*/
 /****************************************************************************/
-void sbbs_t::backspace(void)
+void sbbs_t::backspace(int count)
 {
+	if(count < 1)
+		return;
 	if(!(console&CON_ECHO_OFF)) {
-		if(term_supports(PETSCII))
-			outcom(PETSCII_DELETE);
-		else {
-			outcom('\b');
-			outcom(' ');
-			outcom('\b');
+		for(int i = 0; i < count; i++) {
+			if(term_supports(PETSCII))
+				outcom(PETSCII_DELETE);
+			else {
+				outcom('\b');
+				outcom(' ');
+				outcom('\b');
+			}
+			if(column)
+				column--;
 		}
-		if(column)
-			column--;
 	}
 }
 
@@ -706,28 +776,40 @@ void sbbs_t::wide(const char* str)
 
 
 // Send a bare carriage return, hopefully moving the cursor to the far left, current row
-void sbbs_t::carriage_return(void)
+void sbbs_t::carriage_return(int count)
 {
-	if(term_supports(PETSCII))
-		cursor_left(column);
-	else
-		outcom('\r');
-	column = 0;
+	if(count < 1)
+		return;
+	for(int i = 0; i < count; i++) {
+		if(term_supports(PETSCII))
+			cursor_left(column);
+		else
+			outcom('\r');
+		column = 0;
+	}
 }
 
 // Send a bare line_feed, hopefully moving the cursor down one row, current column
-void sbbs_t::line_feed(void)
+void sbbs_t::line_feed(int count)
 {
-	if(term_supports(PETSCII))
-		outcom(PETSCII_DOWN);
-	else 
-		outcom('\n');
+	if(count < 1)
+		return;
+	for(int i = 0; i < count; i++) {
+		if(term_supports(PETSCII))
+			outcom(PETSCII_DOWN);
+		else 
+			outcom('\n');
+	}
 }
 
-void sbbs_t::newline(void)
+void sbbs_t::newline(int count)
 {
-	outchar('\r');
-	outchar('\n');
+	if(count < 1)
+		return;
+	for(int i = 0; i < count; i++) { 
+		outchar('\r');
+		outchar('\n');
+	}
 }
 
 void sbbs_t::clearline(void)
@@ -740,7 +822,7 @@ void sbbs_t::cursor_home(void)
 {
 	long term = term_supports();
 	if(term&ANSI)
-		rputs("\x1b[H");
+		putcom("\x1b[H");
 	else if(term&PETSCII)
 		outcom(PETSCII_HOME);
 	else
@@ -756,9 +838,9 @@ void sbbs_t::cursor_up(int count)
 	long term = term_supports();
 	if(term&ANSI) {
 		if(count>1)
-			rprintf("\x1b[%dA",count);
+			comprintf("\x1b[%dA",count);
 		else
-			rputs("\x1b[A");
+			putcom("\x1b[A");
 	} else {
 		if(term&PETSCII) {
 			for(int i=0;i<count;i++)
@@ -773,9 +855,9 @@ void sbbs_t::cursor_down(int count)
 		return;
 	if(term_supports(ANSI)) {
 		if(count>1)
-			rprintf("\x1b[%dB",count);
+			comprintf("\x1b[%dB",count);
 		else
-			rputs("\x1b[B");
+			putcom("\x1b[B");
 	} else {
 		for(int i=0;i<count;i++)
 			line_feed();
@@ -789,9 +871,9 @@ void sbbs_t::cursor_right(int count)
 	long term = term_supports();
 	if(term&ANSI) {
 		if(count>1)
-			rprintf("\x1b[%dC",count);
+			comprintf("\x1b[%dC",count);
 		else
-			rputs("\x1b[C");
+			putcom("\x1b[C");
 	} else {
 		for(int i=0;i<count;i++) {
 			if(term&PETSCII)
@@ -810,9 +892,9 @@ void sbbs_t::cursor_left(int count)
 	long term = term_supports();
 	if(term&ANSI) {
 		if(count>1)
-			rprintf("\x1b[%dD",count);
+			comprintf("\x1b[%dD",count);
 		else
-			rputs("\x1b[D");
+			putcom("\x1b[D");
 	} else {
 		for(int i=0;i<count;i++) {
 			if(term&PETSCII)
@@ -833,7 +915,7 @@ void sbbs_t::cleartoeol(void)
 
 	long term = term_supports();
 	if(term&ANSI)
-		rputs("\x1b[K");
+		putcom("\x1b[K");
 	else {
 		i=j=column;
 		while(++i<cols)
@@ -850,7 +932,34 @@ void sbbs_t::cleartoeol(void)
 void sbbs_t::cleartoeos(void)
 {
 	if(term_supports(ANSI))
-		rputs("\x1b[J");
+		putcom("\x1b[J");
+}
+
+void sbbs_t::set_output_rate(enum output_rate speed)
+{
+	if(term_supports(ANSI)) {
+		unsigned int val = speed;
+		switch(val) {
+			case 0:		val = 0; break;
+			case 600:	val = 2; break;
+			case 1200:	val = 3; break;
+			case 2400:	val = 4; break;
+			case 4800:	val = 5; break;
+			case 9600:	val = 6; break;
+			case 19200:	val = 7; break;
+			case 38400: val = 8; break;
+			case 57600: val = 9; break;
+			case 76800: val = 10; break;
+			default:
+				if(val <= 300)
+					val = 1;
+				else if(val > 76800)
+					val = 11;
+				break;
+		}
+		comprintf("\x1b[;%u*r", val);
+		cur_output_rate = speed;
+	}
 }
 
 /****************************************************************************/
