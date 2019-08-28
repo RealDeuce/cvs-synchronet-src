e5de6d20d158e481dbc2f488445b1059c5a48241
rswindell <rswindell>
2019-08-28 01:37:20 +0000

    Added string utility function: str_has_ctrl() to check if a string contains any control (ASCII 0x01 - 0x1F) characters.

--- sbbs3/sbbs.h
+++ sbbs3/sbbs.h
@@ -1,6 +1,6 @@
 /* Synchronet class (sbbs_t) definition and exported function prototypes */
 // vi: tabstop=4
-/* $Id: sbbs.h,v 1.548 2019/08/25 03:09:46 rswindell Exp $ */
+/* $Id: sbbs.h,v 1.549 2019/08/28 01:37:20 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1166,6 +1166,7 @@ extern "C" {
 	DLLEXPORT char *	ultoac(ulong l,char *str);
 	DLLEXPORT char *	rot13(char* str);
 	DLLEXPORT uint32_t	str_to_bits(uint32_t currval, const char *str);
+	DLLEXPORT BOOL		str_has_ctrl(const char*);
 	DLLEXPORT BOOL		str_is_ascii(const char*);
 	DLLEXPORT char *	utf8_to_cp437_str(char* str);
 
