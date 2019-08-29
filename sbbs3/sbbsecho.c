64935c35b1719798a9159ef9c15777fdfe16e4db
rswindell <rswindell>
2019-08-29 02:23:41 +0000

    Don't try to remove a bad .QWK packet if it's been renamed to *.bad.

--- sbbs3/sbbsecho.c
+++ sbbs3/sbbsecho.c
@@ -1,6 +1,6 @@
 /* Synchronet FidoNet EchoMail Scanning/Tossing and NetMail Tossing Utility */
 
-/* $Id: sbbsecho.c,v 3.141 2019/09/17 10:29:34 rswindell Exp $ */
+/* $Id: sbbsecho.c,v 3.139 2019/08/27 03:47:56 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -57,7 +57,6 @@
 #include "sbbsecho.h"
 #include "genwrap.h"		/* PLATFORM_DESC */
 #include "xpendian.h"
-#include "utf8.h"
 
 #define MAX_OPEN_SMBS	10
 
@@ -3429,6 +3428,8 @@ int fmsgtosmsg(char* fbuf, fmsghdr_t* hdr, uint user, uint subnum)
 				while(m && fbuf[m-1]<=' ' && fbuf[m-1]>=0) m--;
 				if(m>l) {
 					smb_hfield(&msg, FIDOCHARSET, (ushort)(m-l), fbuf+l);
+					if(smb_msg_is_utf8(&msg))
+						msg.hdr.auxattr |= MSG_HFIELDS_UTF8;
 				}
 			}
 
@@ -3543,12 +3544,6 @@ int fmsgtosmsg(char* fbuf, fmsghdr_t* hdr, uint user, uint subnum)
 	while(taillen && stail[taillen-1]<=' ')	/* trim all garbage off the tail */
 		taillen--;
 	stail[taillen]=0;
-
-	if(cfg.auto_utf8 && msg.ftn_charset == NULL && !str_is_ascii(fbuf) && utf8_str_is_valid(fbuf))
-		smb_hfield_str(&msg, FIDOCHARSET, FIDO_CHARSET_UTF8);
-
-	if(smb_msg_is_utf8(&msg))
-		msg.hdr.auxattr |= MSG_HFIELDS_UTF8;
 
 	if(subnum==INVALID_SUB && !bodylen && !taillen && cfg.kill_empty_netmail) {
 		lprintf(LOG_INFO,"Empty NetMail - Ignored ");
@@ -5699,7 +5694,7 @@ void import_packets(const char* inbound, nodecfg_t* inbox, bool secure)
 
 			if(pkdmsg.type == 2 /* Recognized type */
 				&& pkdmsg.time[0] != 0
-				&& pkdmsg.time[sizeof(pkdmsg.time)-1] == 0
+//				&& pkdmsg.time[sizeof(pkdmsg.time)-1] == 0
 				&& freadstr(fidomsg, hdr.to, sizeof(hdr.to)) != NULL
 				&& freadstr(fidomsg, hdr.from, sizeof(hdr.from)) != NULL
 				&& freadstr(fidomsg, hdr.subj, sizeof(hdr.subj)) != NULL
@@ -6014,7 +6009,7 @@ int main(int argc, char **argv)
 		memset(&smb[i],0,sizeof(smb_t));
 	memset(&cfg,0,sizeof(cfg));
 
-	sscanf("$Revision: 3.141 $", "%*s %s", revision);
+	sscanf("$Revision: 3.139 $", "%*s %s", revision);
 
 	DESCRIBE_COMPILER(compiler);
 
