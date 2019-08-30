035909adc1f4a4e08efabc1f2512728089ce7ca6
rswindell <rswindell>
2019-08-30 10:49:58 +0000

    Use the Unicode lower/upper half block enums. Added more "black square" -> CP437 0xFE mappings. Added Unicode FULL/HALF block -> CP437 mappings.

--- sbbs3/telnet.c
+++ sbbs3/telnet.c
@@ -2,7 +2,7 @@
 
 /* Synchronet telnet command/option functions */
 
-/* $Id: telnet.c,v 1.9 2019/08/24 08:04:53 rswindell Exp $ */
+/* $Id: telnet.c,v 1.10 2019/08/24 19:37:11 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -154,7 +154,7 @@ uchar telnet_opt_nak(uchar cmd)
 // 'result' may point to either inbuf (if there were no IACs) or outbuf
 // Returns the final byte count of the result
 /*****************************************************************************/
-size_t telnet_expand(const uchar* inbuf, size_t inlen, uchar* outbuf, size_t outlen, bool expand_cr, uchar** result)
+size_t telnet_expand(const uchar* inbuf, size_t inlen, uchar* outbuf, size_t outlen, BOOL expand_cr, uchar** result)
 {
     BYTE* first_iac = (BYTE*)memchr(inbuf, TELNET_IAC, inlen);
 	BYTE*   first_cr=NULL;
