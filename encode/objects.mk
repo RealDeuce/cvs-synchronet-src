f65f51d7ab2b3229d85ae51b287245cfc521ad18
deuce <deuce>
2019-09-19 06:54:48 +0000

    Even more leak paranoia... If dup() fails, return NULL If callog() fails, fclose() the new FILE* No functional change (hopefully).

--- encode/objects.mk
+++ encode/objects.mk
@@ -2,12 +2,12 @@
 
 # Make 'include file' listing object files for ENCODE LIB
 
-# $Id: objects.mk,v 1.4 2019/07/06 07:39:40 rswindell Exp $
+# $Id: objects.mk,v 1.6 2019/07/10 00:22:10 rswindell Exp $
 
 # OBJODIR, DIRSEP, and OFILE must be pre-defined
 
-OBJS	=	$(OBJODIR)$(DIRSEP)cp437_unicode_tbl$(OFILE) \
-        $(OBJODIR)$(DIRSEP)utf8$(OFILE) \
+OBJS	=	 \
+        	$(OBJODIR)$(DIRSEP)utf8$(OFILE) \
 		$(OBJODIR)$(DIRSEP)uucode$(OFILE) \
 		$(OBJODIR)$(DIRSEP)yenc$(OFILE) \
 		$(OBJODIR)$(DIRSEP)lzh$(OFILE) \
