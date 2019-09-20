05e39d62c7d3ba1fb05c5431f6035e0e52032111
rswindell <rswindell>
2019-09-20 09:00:14 +0000

    Change getfilecount() to allow the pattern to be specified (or not) in the path argument, eliminate the spec/pattern argument.

--- sbbs3/ctrl/MainFormUnit.h
+++ sbbs3/ctrl/MainFormUnit.h
@@ -1,6 +1,6 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id: MainFormUnit.h,v 1.94 2020/04/17 20:38:56 rswindell Exp $ */
+/* $Id: MainFormUnit.h,v 1.88 2019/02/15 03:54:56 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -332,25 +332,6 @@ __published:	// IDE-managed Components
     TMenuItem *LogPopupCopy;
     TMenuItem *ViewFailedLoginsPopupMenuItem;
     TMenuItem *ClearFailedLoginsPopupMenuItem;
-    TMenuItem *LogRefresh;
-    TMenuItem *FidonetMenuItem;
-    TMenuItem *FidonetConfigureMenuItem;
-    TMenuItem *N13;
-    TMenuItem *FidonetViewMenuItem;
-    TMenuItem *FidonetEditMenuItem;
-    TMenuItem *sbbsechoLogViewMenuItem;
-    TMenuItem *binkstasViewMenuItem;
-    TMenuItem *echostatsViewMenuItem;
-    TMenuItem *badareasViewMenuItem;
-    TMenuItem *sbbsechoEditMenuItem;
-    TMenuItem *areafileEditMenuItem;
-    TMenuItem *FidonetPollMenuItem;
-    TMenuItem *FileRunMenuItem;
-    TMenuItem *FileRunUpdateMenuItem;
-    TMenuItem *FileRunChkSetupMenuItem;
-    TMenuItem *FileRunInitFidonetMenuItem;
-    TMenuItem *FileRunJSMenuItem;
-    TMenuItem *FileRunInstallXtrnMenuItem;
 	void __fastcall FileExitMenuItemClick(TObject *Sender);
 	void __fastcall ViewToolbarMenuItemClick(TObject *Sender);
 	void __fastcall FormClose(TObject *Sender, TCloseAction &Action);
@@ -394,7 +375,7 @@ __published:	// IDE-managed Components
     void __fastcall ChatToggleExecute(TObject *Sender);
     void __fastcall ViewClientsExecute(TObject *Sender);
     void __fastcall UserEditExecute(TObject *Sender);
-	void __fastcall SetControls(void);
+	void __fastcall SetLogControls(void);
     void __fastcall SaveSettings(TObject *Sender);
     bool __fastcall SaveIniSettings(TObject* Sender);    
     void __fastcall SaveRegistrySettings(TObject* Sender);    
@@ -406,7 +387,6 @@ __published:	// IDE-managed Components
     void __fastcall ExportSettings(TObject *Sender);
     void __fastcall BBSLoginMenuItemClick(TObject *Sender);
     void __fastcall ViewLogClick(TObject *Sender);
-    void __fastcall RunJSClick(TObject *Sender);
     void __fastcall UserListExecute(TObject *Sender);
     void __fastcall WebPageMenuItemClick(TObject *Sender);
     void __fastcall TrayIconRestore(TObject *Sender);
@@ -448,10 +428,6 @@ __published:	// IDE-managed Components
     void __fastcall LogPopupCopyAllClick(TObject *Sender);
     void __fastcall LogPopupCopyClick(TObject *Sender);
     void __fastcall ClearFailedLoginsPopupMenuItemClick(TObject *Sender);
-    void __fastcall RefreshLogClick(TObject *Sender);
-    void __fastcall FidonetConfigureMenuItemClick(TObject *Sender);
-    void __fastcall FidonetPollMenuItemClick(TObject *Sender);
-    void __fastcall FileRunJSMenuItemClick(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
     __fastcall TMainForm(TComponent* Owner);
--- xpdev/dirwrap.c
+++ xpdev/dirwrap.c
@@ -1,7 +1,7 @@
 /* Directory-related system-call wrappers */
 // vi: tabstop=4
 
-/* $Id: dirwrap.c,v 1.112 2020/04/14 11:56:07 deuce Exp $ */
+/* $Id: dirwrap.c,v 1.110 2019/09/20 08:59:34 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -152,16 +152,16 @@ void DLLCALL _splitpath(const char *path, char *drive, char *dir, char *fname, c
 	ext[0]=0;
 	drive[0]=0;			/* no drive letters on Unix */
 
-	snprintf(dir, MAX_PATH+1, "%s", path);	/* Optional directory path, including trailing slash. */
+	strcpy(dir,path);	/* Optional directory path, including trailing slash. */
 	p=getfname(dir);
-	snprintf(fname, MAX_PATH+1, "%s", p);	/* Base filename (no extension) */
+	strcpy(fname,p);	/* Base filename (no extension) */
 	if(p==dir)
 		dir[0]=0;		/* no directory specified in path */
 	else
 		*p=0;			/* truncate dir at filename */
 	p=getfext(fname);
 	if(p!=NULL) {
-		snprintf(ext, MAX_PATH+1, "%s", p);	/* Optional filename extension, including leading period (.) */
+		strcpy(ext,p);	/* Optional filename extension, including leading period (.) */
 		*p=0;
 	}
 }
@@ -826,7 +826,6 @@ uint64_t DLLCALL getfilesizetotal(const char *inpath)
 	char path[MAX_PATH+1];
 	glob_t	g;
 	uint	gi;
-	off_t	size;
 	uint64_t total = 0;
 
 	SAFECOPY(path, inpath);
@@ -839,7 +838,7 @@ uint64_t DLLCALL getfilesizetotal(const char *inpath)
 	for(gi = 0; gi < g.gl_pathc; ++gi) {
 		if(*lastchar(g.gl_pathv[gi]) == '/')
 			continue;
-		size = flength(g.gl_pathv[gi]);
+		off_t size = flength(g.gl_pathv[gi]);
 		if(size >= 1)
 			total += size;
 	}
