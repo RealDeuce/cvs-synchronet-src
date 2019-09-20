a4800ae01dfc6b04a143307cd369ba9b6ef615da
rswindell <rswindell>
2019-09-20 08:36:48 +0000

    Work-around observed infinite loop trying to print a 0-byte file where feof() was returning 0 and fgets() never returned NULL. Weird.

--- sbbs3/readmsgs.cpp
+++ sbbs3/readmsgs.cpp
@@ -1,7 +1,7 @@
 /* Synchronet public message reading function */
 // vi: tabstop=4
 
-/* $Id: readmsgs.cpp,v 1.121 2019/04/23 05:34:26 rswindell Exp $ */
+/* $Id: readmsgs.cpp,v 1.124 2019/08/04 22:48:38 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -74,7 +74,8 @@ long sbbs_t::listmsgs(uint subnum, long mode, post_t *post, long i, long posts, 
 		smb_unlockmsghdr(&smb,&msg);
 		if(listed==0)
 			bputs(text[MailOnSystemLstHdr]);
-		bprintf(text[SubMsgLstFmt], reading ? (i+1) : post[i].num
+		bprintf(P_TRUNCATE|(msg.hdr.auxattr&MSG_HFIELDS_UTF8)
+			,msghdr_text(&msg, SubMsgLstFmt), reading ? (i+1) : post[i].num
 			,msg.hdr.attr&MSG_ANONYMOUS && !sub_op(subnum)
 			? text[Anonymous] : msg.from
 			,msg.to
@@ -168,10 +169,8 @@ void sbbs_t::msghdr(smbmsg_t* msg)
 		bprintf("%-16.16s %d\r\n"	,"thread_first"		,msg->hdr.thread_first);
 	if(msg->hdr.delivery_attempts)
 		bprintf("%-16.16s %hu\r\n"	,"delivery_attempts"	,msg->hdr.delivery_attempts);
-	if(msg->hdr.times_downloaded)
-		bprintf("%-16.16s %u\r\n"	,"times_downloaded"	,msg->hdr.times_downloaded);
-	if(msg->hdr.last_downloaded)
-		bprintf("%-16.16s %s\r\n"	,"last_downloaded"	,timestr(msg->hdr.last_downloaded));
+	if(msg->hdr.priority)
+		bprintf("%-16.16s %u\r\n"	,"priority"			,msg->hdr.priority);
 	if(msg->hdr.votes)
 		bprintf("%-16.16s %hu\r\n"	,"votes"		,msg->hdr.votes);
 
@@ -179,8 +178,6 @@ void sbbs_t::msghdr(smbmsg_t* msg)
 	if(msg->expiration)
 		bprintf("%-16.16s %s\r\n"	,"expiration"
 			,timestr(msg->expiration));
-	if(msg->priority)
-		bprintf("%-16.16s %u\r\n"	,"priority"			,msg->priority);
 	if(msg->cost)
 		bprintf("%-16.16s %u\r\n"	,"cost"				,msg->cost);
 
@@ -468,7 +465,7 @@ void sbbs_t::show_thread(uint32_t msgnum, post_t* post, unsigned curmsg, int thr
 		,(int)(cols-column-12)
 		,(int)(cols-column-12)
 		,msg.hdr.attr&MSG_ANONYMOUS && !sub_op(smb.subnum)
-			? text[Anonymous] : msg.from
+			? text[Anonymous] : msghdr_field(&msg, msg.from)
 		,(unsigned)i == curmsg ? '<' : ' '
 		,msg_listing_flag(smb.subnum, &msg, &post[i])
 		,unixtodstr(&cfg, msg.hdr.when_written.time, date));
@@ -513,6 +510,7 @@ int sbbs_t::scanposts(uint subnum, long mode, const char *find)
 	smbmsg_t	msg;
 	bool	thread_mode = false;
 
+	action=NODE_RMSG;
 	cursubnum=subnum;	/* for ARS */
 	if(cfg.scanposts_mod[0] && !scanposts_inside) {
 		char cmdline[256];
@@ -648,7 +646,6 @@ int sbbs_t::scanposts(uint subnum, long mode, const char *find)
 	smb_unlocksmbhdr(&smb);
 	last=smb.status.last_msg;
 
-	action=NODE_RMSG;
 	if(mode&SCAN_CONST) {   /* update action */
 		getnodedat(cfg.node_num,&thisnode,1);
 		thisnode.action=NODE_RMSG;
@@ -731,7 +728,7 @@ int sbbs_t::scanposts(uint subnum, long mode, const char *find)
 				break;
 			}
 			bprintf("\1n\1l\1h\1bThread\1n\1b: \1h\1c");
-			bprintf("%-.*s\r\n", (int)(cols-(column+1)), msg.subj);
+			bprintf("%-.*s\r\n", (int)(cols-(column+1)), msghdr_field(&msg, msg.subj));
 			show_thread(first, post, smb.curmsg);
 			subscan[subnum].last = post[smb.curmsg].idx.number;
 		}
@@ -812,11 +809,11 @@ int sbbs_t::scanposts(uint subnum, long mode, const char *find)
 
 			if(sub_op(subnum) && (msg.hdr.attr&(MSG_MODERATED|MSG_VALIDATED)) == MSG_MODERATED) {
 				uint16_t msg_attr = msg.hdr.attr;
-				SAFEPRINTF2(str,text[ValidatePostQ],smb.curmsg+1,msg.subj);
+				SAFEPRINTF2(str,text[ValidatePostQ],smb.curmsg+1,msghdr_field(&msg, msg.subj));
 				if(!noyes(str))
 					msg_attr|=MSG_VALIDATED;
 				else {
-					SAFEPRINTF2(str,text[DeletePostQ],smb.curmsg+1,msg.subj);
+					SAFEPRINTF2(str,text[DeletePostQ],smb.curmsg+1,msghdr_field(&msg, msg.subj));
 					if(yesno(str))
 						msg_attr|=MSG_DELETE;
 				}
@@ -979,7 +976,7 @@ int sbbs_t::scanposts(uint subnum, long mode, const char *find)
 				if(msg.hdr.type == SMB_MSG_TYPE_POLL)
 					SAFEPRINTF(str, text[DeleteTextFileQ], "Poll");
 				else
-					SAFEPRINTF2(str,text[DeletePostQ], smb.curmsg+1, msg.subj);
+					SAFEPRINTF2(str,text[DeletePostQ], smb.curmsg+1, msghdr_field(&msg, msg.subj));
 				if(!(msg.hdr.attr&MSG_DELETE) && noyes(str)) {
 					domsg = false;
 					break;
@@ -1103,7 +1100,7 @@ int sbbs_t::scanposts(uint subnum, long mode, const char *find)
 					&& stricmp(msg.to,useron.alias))
 					break;
 				SAFEPRINTF2(str2,text[Regarding]
-					,msg.subj
+					,msghdr_field(&msg, msg.subj)
 					,timestr(msg.hdr.when_written.time));
 				if(msg.from_net.addr==NULL)
 					SAFECOPY(str,msg.from);
@@ -1217,7 +1214,7 @@ int sbbs_t::scanposts(uint subnum, long mode, const char *find)
 						if(msg.hfield[i].type == SMB_POLL_ANSWER)
 							strListPush(&answers, (char*)msg.hfield_dat[i]);
 					}
-					SAFEPRINTF(str, text[BallotHdr], msg.subj);
+					SAFEPRINTF(str, text[BallotHdr], msghdr_field(&msg, msg.subj));
 					i = mselect(str, answers, msg.hdr.votes ? msg.hdr.votes : 1, text[BallotAnswerFmt]
 						,text[PollAnswerChecked], nulstr, text[BallotVoteWhich]);
 					strListFree(&answers);
@@ -1347,7 +1344,7 @@ int sbbs_t::scanposts(uint subnum, long mode, const char *find)
 									errormsg(WHERE,ERR_READ,smb.file,msg.idx.number);
 									break; 
 								}
-								SAFEPRINTF2(str,text[DeletePostQ], smb.curmsg+1, msg.subj);
+								SAFEPRINTF2(str,text[DeletePostQ], smb.curmsg+1, msghdr_field(&msg, msg.subj));
 								if(movemsg(&msg,subnum) && yesno(str)) {
 									msg.idx.attr|=MSG_DELETE;
 									msg.hdr.attr=msg.idx.attr;
@@ -1768,7 +1765,8 @@ long sbbs_t::searchposts(uint subnum, post_t *post, long start, long posts
 			|| (msg.tags != NULL && strcasestr(msg.tags, search) != NULL)) {
 			if(!found)
 				bputs(text[MailOnSystemLstHdr]);
-			bprintf(text[SubMsgLstFmt],l+1
+			bprintf(P_TRUNCATE|(msg.hdr.auxattr&MSG_HFIELDS_UTF8)
+				,msghdr_text(&msg, SubMsgLstFmt),l+1
 				,(msg.hdr.attr&MSG_ANONYMOUS) && !sub_op(subnum) ? text[Anonymous]
 				: msg.from
 				,msg.to
@@ -1823,7 +1821,8 @@ long sbbs_t::showposts_toyou(uint subnum, post_t *post, ulong start, long posts,
 			if(!found)
 				bputs(text[MailOnSystemLstHdr]);
 			found++;
-			bprintf(text[SubMsgLstFmt],l+1
+			bprintf(P_TRUNCATE|(msg.hdr.auxattr&MSG_HFIELDS_UTF8)
+				,msghdr_text(&msg, SubMsgLstFmt),l+1
 				,(msg.hdr.attr&MSG_ANONYMOUS) && !SYSOP
 				? text[Anonymous] : msg.from
 				,msg.to
