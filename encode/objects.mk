3b1c9086f871a22204c7e48280f14dcd239f0a5e
rswindell <rswindell>
2019-09-27 20:58:38 +0000

    Support UTF-8 auto-detection for MacOS Terminal v2.8.3 (404.1): Unexpectedly, a ZWNBSP (U+FEFF) received/echoed by the MacOS Terminal while in the first column causes a subsequent cursor position report to indicate the 2nd column as the current only. I suppose in some weird world a zero-width character can can't as a column position. So if the cursor position has moved less than 2 columns (not exactly 0 columns), then consider it a UTF-8 terminal. Non-UTF-8 terminals normally move the cursor 3 columns when echoing a UTF-8 encoded ZWNBSP. So to summarize, when echoing a ZWNBSP:
    - Non-UTF-8 terminals: moves 3 columns
    - MacOS terminal: moves 1 columns
    - Other UTF-8 terminals: moves 0 columns

--- encode/objects.mk
+++ encode/objects.mk
@@ -2,12 +2,12 @@
 
 # Make 'include file' listing object files for ENCODE LIB
 
-# $Id: objects.mk,v 1.5 2019/07/08 00:07:22 rswindell Exp $
+# $Id: objects.mk,v 1.6 2019/07/10 00:22:10 rswindell Exp $
 
 # OBJODIR, DIRSEP, and OFILE must be pre-defined
 
-OBJS	=	$(OBJODIR)$(DIRSEP)unicode$(OFILE) \
-        $(OBJODIR)$(DIRSEP)utf8$(OFILE) \
+OBJS	=	 \
+        	$(OBJODIR)$(DIRSEP)utf8$(OFILE) \
 		$(OBJODIR)$(DIRSEP)uucode$(OFILE) \
 		$(OBJODIR)$(DIRSEP)yenc$(OFILE) \
 		$(OBJODIR)$(DIRSEP)lzh$(OFILE) \
