8d24b7e01f05d127f537747827132a5d174ec8ca
rswindell <rswindell>
2019-11-22 17:42:09 +0000

    Fix error: targets.mk:8: *** missing separator (did you mean TAB instead of 8 spaces?).  Stop.

---
+++ sbbs3/gtkmonitor/targets.mk
@@ -0,0 +1,15 @@
+GTKMONITOR	=	$(EXEODIR)$(DIRSEP)gtkmonitor$(EXEFILE)
+
+all: xpdev-mt smblib $(MTOBJODIR) $(EXEODIR) $(GTKMONITOR)
+
+ifdef SBBSEXEC
+.PHONY: install
+install: all
+        install $(EXEODIR)/* $(SBBSEXEC)
+
+.PHONY: symlinks
+symlinks: all
+        ln -sfr $(EXEODIR)/* $(SBBSEXEC)
+endif
+
+$(GTKMONITOR):	$(XPDEV-MT_LIB) $(SMBLIB)
