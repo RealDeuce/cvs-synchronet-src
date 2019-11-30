80ac64a3cf2e0c00a2664d7b0064abf9ab2954e9
rswindell <rswindell>
2019-11-30 23:26:20 +0000

    Add a "newsgroup.lst" import feature to import a list of newsgroups from a text file (one newsgroup name per line). The newsgroup name is used for all the area fields/names, removing the group name if applicable. Any additional text following the first word of each line is ignored. Also sets the INET/NTTP networked flag for all the imported subs.
    Also, fixed apparently long-standing bug where QWK name of imported subs was
    always overwritten with sub's short name.

--- sbbs3/gtkmonitor/gtkmonitor.c
+++ sbbs3/gtkmonitor/gtkmonitor.c
@@ -218,10 +218,10 @@ int refresh_data(gpointer data)
 			nodes++;
 		}
 
-		if((j=getnodedat(&cfg,i,&node,NULL)))
+		if((j=getnodedat(&cfg,i,&node,FALSE,NULL)))
 			sprintf(str,"Error reading node data (%d)!",j);
 		else
-			nodestatus(&cfg,&node,str,1023);
+			nodestatus(&cfg,&node,str,1023, i);
 		gtk_list_store_set(store, &curr, 1, str, -1);
 		gtk_tree_model_iter_next(GTK_TREE_MODEL(store), &curr);
 	}
@@ -258,7 +258,7 @@ int refresh_data(gpointer data)
 		gtk_tree_selection_selected_foreach(sel
 				,get_lastselected_node
 				,&i);
-		if((j=getnodedat(&cfg,i,&node,NULL))) {
+		if((j=getnodedat(&cfg,i,&node,FALSE,NULL))) {
 			sprintf(str,"Error reading node data (%d)!",j);
 			node.status=NODE_WFC;
 		}
