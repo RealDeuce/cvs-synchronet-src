0f556d8ee31ced8d2d06eaa6b0133f2b78d2b541
deuce <deuce>
2020-03-05 02:25:04 +0000

    Mention X/Ymodem fixes.

--- sbbs3/sbbsdefs.h
+++ sbbs3/sbbsdefs.h
@@ -1,6 +1,6 @@
 /* Synchronet constants, macros, and structure definitions */
 
-/* $Id: sbbsdefs.h,v 1.253 2020/03/19 18:50:51 rswindell Exp $ */
+/* $Id: sbbsdefs.h,v 1.252 2020/03/01 23:52:45 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -662,12 +662,7 @@ typedef enum {						/* Values for xtrn_t.event				*/
 #define ICE_COLOR	(1L<<28)		/* Bright background color support		*/
 #define UTF8		(1L<<29)		/* UTF-8 terminal						*/
 
-#define TERM_FLAGS		(ANSI|COLOR|NO_EXASCII|RIP|WIP|HTML|PETSCII|SWAP_DELETE|ICE_COLOR|UTF8)
-#define CHARSET_FLAGS	(NO_EXASCII|PETSCII|UTF8)
-#define CHARSET_ASCII	NO_EXASCII	// US-ASCII
-#define CHARSET_PETSCII	PETSCII		// CBM-ASCII
-#define CHARSET_UTF8	UTF8
-#define CHARSET_CP437	0
+#define TERM_FLAGS	(ANSI|COLOR|NO_EXASCII|RIP|WIP|HTML|PETSCII|SWAP_DELETE|ICE_COLOR|UTF8)
 
 									/* Special terminal key mappings */
 #define TERM_KEY_HOME	CTRL_B
