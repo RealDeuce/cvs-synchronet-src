155d45270be72e6f090e03afbaa07ec5b27b6123
rswindell <rswindell>
2020-03-08 20:25:59 +0000

    Resolve warning C4013: 'localtime_r' undefined; assuming extern returning int

--- sbbs3/scfg/scfgsub.c
+++ sbbs3/scfg/scfgsub.c
@@ -1,4 +1,4 @@
-/* $Id: scfgsub.c,v 1.58 2019/08/12 06:21:28 rswindell Exp $ */
+/* $Id: scfgsub.c,v 1.59 2019/08/23 21:45:55 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -35,7 +35,7 @@
 
 static sub_t** cut_qhub_sub;
 
-bool new_sub(unsigned new_subnum, unsigned group_num, sub_t* pasted_sub)
+bool new_sub(unsigned new_subnum, unsigned group_num, sub_t* pasted_sub, long misc)
 {
 	sub_t* new_subboard;
 	if ((new_subboard = (sub_t *)malloc(sizeof(*new_subboard))) == NULL) {
@@ -47,6 +47,7 @@ bool new_sub(unsigned new_subnum, unsigned group_num, sub_t* pasted_sub)
 		new_subboard->faddr = cfg.faddr[0];
 	/* ToDo: Define these defaults somewhere else: */
 	new_subboard->misc = (SUB_NSDEF | SUB_SSDEF | SUB_QUOTE | SUB_TOUSER | SUB_FAST);
+	new_subboard->misc |= misc;
 	new_subboard->maxmsgs = 500;
 
 	/* Use last sub in group (if exists) as a template for new subs */
@@ -273,7 +274,7 @@ void sub_cfg(uint grpnum)
 				continue; 
 			}
 
-			if (!new_sub(subnum[i], grpnum, /* pasted_sub: */NULL))
+			if (!new_sub(subnum[i], grpnum, /* pasted_sub: */NULL, /* misc: */0))
 				continue;
 
 			SAFECOPY(cfg.sub[subnum[i]]->code_suffix,code);
@@ -325,7 +326,7 @@ void sub_cfg(uint grpnum)
 			continue; 
 		}
 		if(msk == MSK_PASTE) {
-			if (!new_sub(subnum[i], grpnum, &savsub))
+			if (!new_sub(subnum[i], grpnum, &savsub, /* misc: */0))
 				continue;
 			uifc.changes = TRUE;
 			continue; 
