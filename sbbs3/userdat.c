281c8f2d63a898c72b57b39695c51e5300736d60
rswindell <rswindell>
2020-03-19 19:41:32 +0000

    Create MsgBase.dump_msg_header() method which will perform a low-level header "dump" to an array of strings using the (new) smb_msghdr_str_list() SMBLIB function. The message header passed must've been previously returned from the get_msg_header() or get_all_msg_headers() method.
    A similar dump could be constructed from the header properties, as available in
    JS, but that would be inconsistent with other msg header dumps and likely lack
    header fields or details available from the lowest level dump method provided
    in SMBLIB.

--- sbbs3/userdat.c
+++ sbbs3/userdat.c
@@ -1,7 +1,7 @@
 /* Synchronet user data-related routines (exported) */
 // vi: tabstop=4
 
-/* $Id: userdat.c,v 1.225 2020/04/13 05:05:08 rswindell Exp $ */
+/* $Id: userdat.c,v 1.222 2020/03/19 18:50:51 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -296,8 +296,6 @@ int parseuserdat(scfg_t* cfg, char *userdat, user_t *user)
 	getrec(userdat,U_LOCATION,LEN_LOCATION,user->location);
 	getrec(userdat,U_ZIPCODE,LEN_ZIPCODE,user->zipcode);
 	getrec(userdat,U_PASS,LEN_PASS,user->pass);
-	if(user->pass[0] == 0)	// Backwards compatibility hack
-		getrec(userdat, U_OLDPASS, LEN_OLDPASS, user->pass);
 	getrec(userdat,U_PHONE,LEN_PHONE,user->phone);
 	getrec(userdat,U_BIRTH,LEN_BIRTH,user->birth);
 	getrec(userdat,U_MODEM,LEN_MODEM,user->modem);
@@ -532,7 +530,6 @@ int putuserdat(scfg_t* cfg, user_t* user)
 	putrec(userdat,U_ZIPCODE+LEN_ZIPCODE,2,crlf);
 
 	putrec(userdat,U_PASS,LEN_PASS,user->pass);
-	putrec(userdat,U_OLDPASS,LEN_OLDPASS,user->pass);	// So a sysop can downgrade to a previous build/version
 	putrec(userdat,U_PHONE,LEN_PHONE,user->phone);
 	putrec(userdat,U_BIRTH,LEN_BIRTH,user->birth);
 	putrec(userdat,U_MODEM,LEN_MODEM,user->modem);
@@ -583,7 +580,7 @@ int putuserdat(scfg_t* cfg, user_t* user)
 	putrec(userdat,U_CURXTRN,8,user->curxtrn);
 	putrec(userdat,U_CURXTRN+8,2,crlf);
 
-	putrec(userdat,U_PASS+LEN_PASS, 2, crlf);
+	putrec(userdat,U_XFER_CMD+LEN_XFER_CMD,2,crlf);
 
 	putrec(userdat,U_IPADDR+LEN_IPADDR,2,crlf);
 
@@ -2107,9 +2104,6 @@ int getuserrec(scfg_t* cfg, int usernumber,int start, int length, char *str)
 		if(str[c]==ETX || str[c]==CR) break;
 	str[c]=0;
 
-	if(c == 0 && start == LEN_PASS) // Backwards compatibility hack
-		return getuserrec(cfg, usernumber, U_OLDPASS, LEN_OLDPASS, str);
-
 	return(0);
 }
 
@@ -2304,80 +2298,6 @@ BOOL user_downloaded(scfg_t* cfg, user_t* user, int files, long bytes)
 
 	return(TRUE);
 }
-
-#ifdef SBBS
-BOOL user_downloaded_file(scfg_t* cfg, user_t* user, client_t* client,
-	uint dirnum, const char* filename, ulong bytes)
-{
-	file_t f = {{0}};
-
-	f.dir = dirnum;
-	padfname(getfname(filename), f.name);
-	if(!getfileixb(cfg, &f) || !getfiledat(cfg, &f))
-		return FALSE;
-
-	if(!bytes)
-		bytes = f.size;
-
-	f.timesdled++;
-	f.datedled=time32(NULL);
-	if(!putfiledat(cfg, &f) || !putfileixb(cfg, &f))
-		return FALSE;
-
-	/**************************/
-	/* Update Uploader's Info */
-	/**************************/
-	user_t uploader = {{0}};
-	uploader.number=matchuser(cfg, f.uler, TRUE /*sysop_alias*/);
-	if(uploader.number
-		&& uploader.number != user->number 
-		&& getuserdat(cfg, &uploader) == 0
-		&& uploader.firston < f.dateuled) {
-		ulong l = f.cdt;
-		if(!(cfg->dir[f.dir]->misc&DIR_CDTDL))	/* Don't give credits on d/l */
-			l=0;
-		ulong mod=(ulong)(l*(cfg->dir[f.dir]->dn_pct/100.0));
-		adjustuserrec(cfg, uploader.number, U_CDT, 10, mod);
-		if(cfg->text != NULL) {
-			char str[256];
-			char tmp[128];
-			char prefix[128]="";
-			ultoac(mod,tmp);
-			char username[64];
-			if(client != NULL && uploader.level >= SYSOP_LEVEL) {
-				if(client->host != NULL && strcmp(client->host, STR_NO_HOSTNAME) != 0)
-					SAFEPRINTF2(username,"%s [%s]", user->alias, client->host);
-				else
-					SAFEPRINTF2(username,"%s [%s]", user->alias, client->addr);
-			} else
-				SAFECOPY(username, user->alias);
-			if(strcmp(cfg->dir[f.dir]->code, "TEMP") == 0 || bytes < (ulong)f.size)
-				SAFECOPY(prefix, cfg->text[Partially]);
-			if(client != NULL) {
-				SAFECAT(prefix, client->protocol);
-				SAFECAT(prefix, "-");
-			}
-			/* Inform uploader of downloaded file */
-			SAFEPRINTF4(str, cfg->text[DownloadUserMsg]
-				,getfname(filename)
-				,prefix
-				,username, tmp);
-			putsmsg(cfg, uploader.number, str);
-		}
-	}
-	/****************************/
-	/* Update Downloader's Info */
-	/****************************/
-	user_downloaded(cfg, user, /* files: */1, bytes);
-	if(!is_download_free(cfg, f.dir, user, client))
-		subtract_cdt(cfg, user, f.cdt);
-
-	if(!(cfg->dir[f.dir]->misc&DIR_NOSTAT))
-		inc_sys_download_stats(cfg, /* files: */1, bytes);
-
-	return TRUE;
-}
-#endif
 
 BOOL user_uploaded(scfg_t* cfg, user_t* user, int files, long bytes)
 {
