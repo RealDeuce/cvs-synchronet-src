49b5dd44d41de1fbcf06fc1756cbb3d9a5ac1307
rswindell <rswindell>
2020-03-23 00:37:24 +0000

    Clean-up some of this USE_LINUX_CAPS patch, but honestly, I think it's time for it to go away. Using 'sudo setcap sbbs' during build seems like the better, more supported solution.

--- sbbs3/js_file.c
+++ sbbs3/js_file.c
@@ -1,7 +1,7 @@
 /* Synchronet JavaScript "File" Object */
 // vi: tabstop=4
 
-/* $Id: js_file.c,v 1.195 2020/04/12 07:58:01 rswindell Exp $ */
+/* $Id: js_file.c,v 1.189 2019/09/19 06:54:48 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -41,7 +41,6 @@
 #include "uucode.h"
 #include "yenc.h"
 #include "ini_file.h"
-#include <stdbool.h>
 
 #if !defined(__unix__)
 	#include <conio.h>		/* for kbhit() */
@@ -314,7 +313,7 @@ js_raw_pollin(JSContext *cx, uintN argc, jsval *arglist)
 	if(p->fp==NULL)
 		return(JS_TRUE);
 
-	if(argc && !JSVAL_NULL_OR_VOID(argv[0])) {
+	if(argc) {
 		if(!JS_ValueToInt32(cx,argv[0],&timeout))
 			return(JS_FALSE);
 	}
@@ -381,7 +380,7 @@ js_raw_read(JSContext *cx, uintN argc, jsval *arglist)
 	if(p->fp==NULL)
 		return(JS_TRUE);
 
-	if(argc && !JSVAL_NULL_OR_VOID(argv[0])) {
+	if(argc) {
 		if(!JS_ValueToInt32(cx,argv[0],&len))
 			return(JS_FALSE);
 	} else
@@ -394,7 +393,6 @@ js_raw_read(JSContext *cx, uintN argc, jsval *arglist)
 
 	rc=JS_SUSPENDREQUEST(cx);
 	len = read(fileno(p->fp),buf,len);
-	dbprintf(FALSE, p, "read %u raw bytes",len);
 	if(len<0)
 		len=0;
 
@@ -407,6 +405,10 @@ js_raw_read(JSContext *cx, uintN argc, jsval *arglist)
 		return(JS_FALSE);
 
 	JS_SET_RVAL(cx, arglist, STRING_TO_JSVAL(str));
+
+	rc=JS_SUSPENDREQUEST(cx);
+	dbprintf(FALSE, p, "read %u raw bytes",len);
+	JS_RESUMEREQUEST(cx, rc);
 
 	return(JS_TRUE);
 }
@@ -436,7 +438,7 @@ js_read(JSContext *cx, uintN argc, jsval *arglist)
 	if(p->fp==NULL)
 		return(JS_TRUE);
 
-	if(argc && !JSVAL_NULL_OR_VOID(argv[0])) {
+	if(argc) {
 		if(!JS_ValueToInt32(cx,argv[0],&len))
 			return(JS_FALSE);
 	} else {
@@ -455,7 +457,6 @@ js_read(JSContext *cx, uintN argc, jsval *arglist)
 
 	rc=JS_SUSPENDREQUEST(cx);
 	len = fread(buf,1,len,p->fp);
-	dbprintf(FALSE, p, "read %u bytes",len);
 	if(len<0)
 		len=0;
 	buf[len]=0;
@@ -500,6 +501,10 @@ js_read(JSContext *cx, uintN argc, jsval *arglist)
 
 	JS_SET_RVAL(cx, arglist, STRING_TO_JSVAL(str));
 
+	rc=JS_SUSPENDREQUEST(cx);
+	dbprintf(FALSE, p, "read %u bytes",len);
+	JS_RESUMEREQUEST(cx, rc);
+
 	return(JS_TRUE);
 }
 
@@ -524,7 +529,7 @@ js_readln(JSContext *cx, uintN argc, jsval *arglist)
 	if(p->fp==NULL)
 		return(JS_TRUE);
 
-	if(argc && !JSVAL_NULL_OR_VOID(argv[0])) {
+	if(argc) {
 		if(!JS_ValueToInt32(cx,argv[0],&len))
 			return(JS_FALSE);
 	}
@@ -582,10 +587,10 @@ js_readbin(JSContext *cx, uintN argc, jsval *arglist)
 	if(p->fp==NULL)
 		return(JS_TRUE);
 
-	if(argc && !JSVAL_NULL_OR_VOID(argv[0])) {
+	if(argc) {
 		if(!JS_ValueToInt32(cx,argv[0],&size))
 			return(JS_FALSE);
-		if(argc>1 && !JSVAL_NULL_OR_VOID(argv[1])) {
+		if(argc>1) {
 			if(!JS_ValueToInt32(cx,argv[1],&count))
 				return(JS_FALSE);
 		}
@@ -1197,7 +1202,6 @@ js_iniGetObject(JSContext *cx, uintN argc, jsval *arglist)
 	private_t*	p;
 	named_string_t** list;
 	jsrefcount	rc;
-	bool		lowercase = false;
 
 	JS_SET_RVAL(cx, arglist, JSVAL_NULL);
 
@@ -1208,15 +1212,9 @@ js_iniGetObject(JSContext *cx, uintN argc, jsval *arglist)
 	if(p->fp==NULL)
 		return(JS_TRUE);
 
-	uintN argn = 0;
-	if(argc > argn && !JSVAL_IS_BOOLEAN(argv[argn]) && !JSVAL_NULL_OR_VOID(argv[argn])) {
-		JSVALUE_TO_MSTRING(cx, argv[argn], section, NULL);
+	if(argc>0 && argv[0]!=JSVAL_VOID && argv[0]!=JSVAL_NULL) {
+		JSVALUE_TO_MSTRING(cx, argv[0], section, NULL);
 		HANDLE_PENDING(cx, section);
-		argn++;
-	}
-	if(argc > argn && JSVAL_IS_BOOLEAN(argv[argn])) {
-		lowercase = JSVAL_TO_BOOLEAN(argv[argn]);
-		argn++;
 	}
 
 	rc=JS_SUSPENDREQUEST(cx);
@@ -1224,14 +1222,14 @@ js_iniGetObject(JSContext *cx, uintN argc, jsval *arglist)
 	FREE_AND_NULL(section);
 	JS_RESUMEREQUEST(cx, rc);
 
-	if(list==NULL)
+	if(list==NULL) {	/* New behavior at request of MCMLXXIX: return NULL if specified section doesn't exist */
+		JS_SET_RVAL(cx, arglist, JSVAL_NULL);
 		return(JS_TRUE);
+	}
 
     object = JS_NewObject(cx, NULL, NULL, obj);
 
     for(i=0;list && list[i];i++) {
-		if(lowercase)
-			strlwr(list[i]->name);
 		JS_DefineProperty(cx, object, list[i]->name
 			,get_value(cx,list[i]->value)
 			,NULL,NULL,JSPROP_ENUMERATE);
@@ -1344,7 +1342,6 @@ js_iniGetAllObjects(JSContext *cx, uintN argc, jsval *arglist)
 	private_t*	p;
 	named_string_t** key_list;
 	jsrefcount	rc;
-	bool		lowercase = false;
 
 	JS_SET_RVAL(cx, arglist, JSVAL_NULL);
 
@@ -1355,25 +1352,16 @@ js_iniGetAllObjects(JSContext *cx, uintN argc, jsval *arglist)
 	if(p->fp==NULL)
 		return(JS_TRUE);
 
-	uintN argn = 0;
-	if(argc > argn && JSVAL_IS_STRING(argv[argn])) {
-		JSVALUE_TO_MSTRING(cx, argv[argn], name, NULL);
-		HANDLE_PENDING(cx, name);
-		if(name == NULL) {
-			JS_ReportError(cx, "Invalid name argument");
-			return JS_FALSE;
-		}
-		argn++;
-	}
-	if(argc > argn && JSVAL_IS_STRING(argv[argn])) {
-		JSVALUE_TO_MSTRING(cx, argv[argn], prefix, NULL);
-		argn++;
-	}
-	if(argc > argn && JSVAL_IS_BOOLEAN(argv[argn])) {
-		lowercase = JSVAL_TO_BOOLEAN(argv[argn]);
-		argn++;
+	if(argc)
+		JSVALUE_TO_MSTRING(cx, argv[0], name, NULL);
+	HANDLE_PENDING(cx, name);
+	if(name == NULL) {
+		JS_ReportError(cx, "Invalid NULL name property");
+		return JS_FALSE;
 	}
 
+	if(argc>1)
+		JSVALUE_TO_MSTRING(cx, argv[1], prefix, NULL);
 	if(JS_IsExceptionPending(cx)) {
 		FREE_AND_NULL(prefix);
 		if(name != name_def)
@@ -1388,11 +1376,10 @@ js_iniGetAllObjects(JSContext *cx, uintN argc, jsval *arglist)
 	JS_RESUMEREQUEST(cx, rc);
     for(i=0;sec_list && sec_list[i];i++) {
 	    object = JS_NewObject(cx, NULL, NULL, obj);
+
 		sec_name=sec_list[i];
 		if(prefix!=NULL)
 			sec_name+=strlen(prefix);
-		if(lowercase)
-			strlwr(sec_name);
 		JS_DefineProperty(cx, object, name
 			,STRING_TO_JSVAL(JS_NewStringCopyZ(cx,sec_name))
 			,NULL,NULL,JSPROP_ENUMERATE);
@@ -1400,13 +1387,10 @@ js_iniGetAllObjects(JSContext *cx, uintN argc, jsval *arglist)
 		rc=JS_SUSPENDREQUEST(cx);
 		key_list = iniReadNamedStringList(p->fp,sec_list[i]);
 		JS_RESUMEREQUEST(cx, rc);
-		for(k=0;key_list && key_list[k];k++) {
-			if(lowercase)
-				strlwr(key_list[k]->name);
+		for(k=0;key_list && key_list[k];k++)
 			JS_DefineProperty(cx, object, key_list[k]->name
 				,get_value(cx,key_list[k]->value)
 				,NULL,NULL,JSPROP_ENUMERATE);
-		}
 		rc=JS_SUSPENDREQUEST(cx);
 		iniFreeNamedStringList(key_list);
 		JS_RESUMEREQUEST(cx, rc);
@@ -1695,7 +1679,7 @@ js_write(JSContext *cx, uintN argc, jsval *arglist)
 
 	JS_RESUMEREQUEST(cx, rc);
 	tlen=len;
-	if(argc>1 && !JSVAL_NULL_OR_VOID(argv[1])) {
+	if(argc>1) {
 		if(!JS_ValueToInt32(cx,argv[1],&i)) {
 			free(cp);
 			return(JS_FALSE);
@@ -1801,16 +1785,11 @@ js_writebin(JSContext *cx, uintN argc, jsval *arglist)
 {
 	JSObject *obj=JS_THIS_OBJECT(cx, arglist);
 	jsval *argv=JS_ARGV(cx, arglist);
-	union {
-		uint8_t		*b;
-		uint16_t	*w;
-		uint32_t	*l;
-		int8_t		*sb;
-		int16_t		*sw;
-		int32_t		*sl;
-	} o;
+	BYTE		*b;
+	WORD		*w;
+	DWORD		*l;
 	size_t		wr=0;
-	int32		size=sizeof(int32_t);
+	int32		size=sizeof(DWORD);
 	jsuint		count=1;
 	void		*buffer;
 	private_t*	p;
@@ -1841,7 +1820,7 @@ js_writebin(JSContext *cx, uintN argc, jsval *arglist)
 		if(!JS_ValueToNumber(cx,argv[0],&val))
 			return(JS_FALSE);
 	}
-	if(argc>1 && !JSVAL_NULL_OR_VOID(argv[1])) {
+	if(argc>1) {
 		if(!JS_ValueToInt32(cx,argv[1],&size))
 			return(JS_FALSE);
 	}
@@ -1858,34 +1837,27 @@ js_writebin(JSContext *cx, uintN argc, jsval *arglist)
 		JS_RESUMEREQUEST(cx, rc);
 		return(JS_FALSE);
 	}
-	o.b=buffer;
+	b=buffer;
+	w=buffer;
+	l=buffer;
 	if(array==NULL) {
 		switch(size) {
-			case sizeof(int8_t):
-				if(val < 0)
-					*o.sb=(int8_t)val;
-				else
-					*o.b=(uint8_t)val;
+			case sizeof(BYTE):
+				*b=(BYTE)val;
 				break;
-			case sizeof(int16_t):
-				if(val < 0)
-					*o.sw=(int16_t)val;
-				else
-					*o.w=(uint16_t)val;
+			case sizeof(WORD):
+				*w=(WORD)val;
 				if (p->network_byte_order)
-					*o.w = BE_SHORT(*o.w);
+					*w = BE_SHORT(*w);
 				else
-					*o.w = LE_SHORT(*o.w);
+					*w = LE_SHORT(*w);
 				break;
-			case sizeof(int32_t):
-				if(val < 0)
-					*o.sl=(int32_t)val;
-				else
-					*o.l=(uint32_t)val;
+			case sizeof(DWORD):
+				*l=(DWORD)val;
 				if (p->network_byte_order)
-					*o.l = BE_LONG(*o.l);
+					*l = BE_LONG(*l);
 				else
-					*o.l = LE_LONG(*o.l);
+					*l = LE_LONG(*l);
 				break;
 		}
 	}
@@ -1896,34 +1868,24 @@ js_writebin(JSContext *cx, uintN argc, jsval *arglist)
 			if(!JS_ValueToNumber(cx,elemval,&val))
 				goto end;
 			switch(size) {
-				case sizeof(int8_t):
-					if(val < 0)
-						*o.sb=(int8_t)val;
-					else
-						*o.b=(uint8_t)val;
-					o.b++;
+				case sizeof(BYTE):
+					*(b++)=(BYTE)val;
 					break;
-				case sizeof(int16_t):
-					if(val < 0)
-						*o.sw=(int16_t)val;
-					else
-						*o.w=(uint16_t)val;
+				case sizeof(WORD):
+					*(w)=(WORD)val;
 					if (p->network_byte_order)
-						*o.w = BE_SHORT(*o.w);
+						*w = BE_SHORT(*w);
 					else
-						*o.w = LE_SHORT(*o.w);
-					o.w++;
+						*w = LE_SHORT(*w);
+					w++;
 					break;
-				case sizeof(int32_t):
-					if(val < 0)
-						*o.sl=(int32_t)val;
-					else
-						*o.l=(uint32_t)val;
+				case sizeof(DWORD):
+					*(l)=(DWORD)val;
 					if (p->network_byte_order)
-						*o.l = BE_LONG(*o.l);
+						*l = BE_LONG(*l);
 					else
-						*o.l = LE_LONG(*o.l);
-					o.l++;
+						*l = LE_LONG(*l);
+					l++;
 					break;
 			}
 		}
@@ -2164,7 +2126,7 @@ js_truncate(JSContext *cx, uintN argc, jsval *arglist)
 		return(JS_FALSE);
 	}
 
-	if(argc && !JSVAL_NULL_OR_VOID(argv[0])) {
+	if(argc) {
 		if(!JS_ValueToInt32(cx,argv[0],&len))
 			return(JS_FALSE);
 	}
@@ -2825,7 +2787,7 @@ static jsSyncMethodSpec js_file_functions[] = {
 	,310
 	},
 	{"writeln",			js_writeln,			0,	JSTYPE_BOOLEAN, JSDOCSTR("[text]")
-	,JSDOCSTR("write a new-line terminated string (a line of text) to the file")
+	,JSDOCSTR("write a line-feed terminated string to the file")
 	,310
 	},
 	{"writeBin",		js_writebin,		1,	JSTYPE_BOOLEAN,	JSDOCSTR("value(s) [,bytes=<tt>4</tt>]")
@@ -2834,7 +2796,7 @@ static jsSyncMethodSpec js_file_functions[] = {
 	,310
 	},
 	{"writeAll",		js_writeall,		0,	JSTYPE_BOOLEAN,	JSDOCSTR("array lines")
-	,JSDOCSTR("write an array of new-line terminated strings (lines of text) to the file")
+	,JSDOCSTR("write an array of strings to file")
 	,310
 	},
 	{"raw_write",		js_raw_write,		1,	JSTYPE_BOOLEAN,	JSDOCSTR("text")
@@ -2875,10 +2837,10 @@ static jsSyncMethodSpec js_file_functions[] = {
 		"to set a key in the <i>root</i> section, pass <i>null</i> for <i>section</i>. ")
 	,312
 	},
-	{"iniGetObject",	js_iniGetObject,	1,	JSTYPE_OBJECT,	JSDOCSTR("[section=<i>root</i>] [lowercase=<tt>false</tt>]")
+	{"iniGetObject",	js_iniGetObject,	1,	JSTYPE_OBJECT,	JSDOCSTR("[section=<i>root</i>]")
 	,JSDOCSTR("parse an entire section from a .ini file "
-		"and return all of its keys (optionally lowercased) and values as properties of an object. "
-		"if <i>section</i> is <tt>null</tt> or <tt>undefined</tt>, returns keys and values from the <i>root</i> section. "
+		"and return all of its keys and values as properties of an object. "
+		"if <i>section</i> is undefined, returns keys and values from the <i>root</i> section. "
 		"Returns <i>null</i> if the specified <i>section</i> does not exist in the file or the file has not been opened.")
 	,311
 	},
@@ -2890,11 +2852,11 @@ static jsSyncMethodSpec js_file_functions[] = {
 		"If your intention is to <i>replace</i> an existing section, use the <tt>iniRemoveSection</tt> function first." )
 	,312
 	},
-	{"iniGetAllObjects",js_iniGetAllObjects,1,	JSTYPE_ARRAY,	JSDOCSTR("[name_property] [,prefix=<i>none</i>] [lowercase=<tt>false</tt>]")
+	{"iniGetAllObjects",js_iniGetAllObjects,1,	JSTYPE_ARRAY,	JSDOCSTR("[name_property] [,prefix=<i>none</i>]")
 	,JSDOCSTR("parse all sections from a .ini file and return all (non-<i>root</i>) sections "
-		"in an array of objects with each section's keys (optionally lowercased) as properties of each object. "
+		"in an array of objects with each section's keys as properties of each object. "
 		"<i>name_property</i> is the name of the property to create to contain the section's name "
-		"(optionally lowercased, default is <tt>\"name\"</tt>), "
+		"(default is <tt>\"name\"</tt>), "
 		"the optional <i>prefix</i> has the same use as in the <tt>iniGetSections</tt> method, "
 		"if a <i>prefix</i> is specified, it is removed from each section's name" )
 	,311
