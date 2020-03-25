279a0be3bda0a3bf6a0da4216d9e5f61015dcb97
rswindell <rswindell>
2020-03-25 05:41:55 +0000

    External program names are actually up to 40 characters in length (not 25).

--- syncterm/syncterm.c
+++ syncterm/syncterm.c
@@ -1,6 +1,6 @@
 /* Copyright (C), 2007 by Stephen Hurd */
 
-/* $Id: syncterm.c,v 1.220 2019/05/30 06:24:44 deuce Exp $ */
+/* $Id: syncterm.c,v 1.223 2019/07/25 18:28:59 deuce Exp $ */
 
 #if defined(__APPLE__) && defined(__MACH__)
 #include <CoreServices/CoreServices.h>	// FSFindFolder() and friends
@@ -38,6 +38,13 @@ static const KNOWNFOLDERID FOLDERID_ProgramData =		{0x62AB5D82,0xFDC1,0x4DC3,{0x
 #include <filewrap.h>	// STDOUT_FILENO
 
 #include <cterm.h>
+#include <vidmodes.h>
+#if !(defined __BORLANDC__ || defined _MSC_VER)
+ #include <stdbool.h>
+#else
+ #define bool int
+ enum { false, true };
+#endif
 
 #include "st_crypt.h"
 #include "fonts.h"
@@ -1229,6 +1236,9 @@ void load_settings(struct syncterm_settings *set)
 	set->backlines=iniReadInteger(inifile,"SyncTERM","ScrollBackLines",2000);
 	set->xfer_success_keypress_timeout=iniReadInteger(inifile,"SyncTERM", "TransferSuccessKeypressTimeout", /* seconds: */0);
 	set->xfer_failure_keypress_timeout=iniReadInteger(inifile,"SyncTERM", "TransferFailureKeypressTimeout", /* seconds: */60);
+	set->custom_cols = iniReadInteger(inifile, "SyncTERM", "CustomCols", 80);
+	set->custom_rows = iniReadInteger(inifile, "SyncTERM", "CustomRows", 25);
+	set->custom_fontheight = iniReadInteger(inifile, "SyncTERM", "CustomFontHeight", 16);
 	get_syncterm_filename(set->list_path, sizeof(set->list_path), SYNCTERM_PATH_LIST, FALSE);
 	iniReadString(inifile, "SyncTERM", "ListPath", set->list_path, set->list_path);
 	set->scaling_factor=iniReadInteger(inifile,"SyncTERM","ScalingFactor",0);
@@ -1276,6 +1286,7 @@ int main(int argc, char **argv)
 	int		addr_family=PF_UNSPEC;
 	char	*last_bbs=NULL;
 	char	*p, *lp;
+	int	cvmode;
 	const char syncterm_termcap[]="\n# terminfo database entry for SyncTERM\n"
 				"syncterm|SyncTERM,\n"
 				"	am,bce,ccc,da,mir,msgr,ndscr,\n"	// sam?
@@ -1375,6 +1386,10 @@ int main(int argc, char **argv)
 	url[0]=0;
 
 	load_settings(&settings);
+	cvmode = find_vmode(CIOLIB_MODE_CUSTOM);
+	vparams[cvmode].cols = settings.custom_cols;
+	vparams[cvmode].rows = settings.custom_rows;
+	vparams[cvmode].charheight = settings.custom_fontheight;
 	ciolib_mode=settings.output_mode;
 	if(settings.startup_mode != SCREEN_MODE_CURRENT)
 		text_mode=screen_to_ciolib(settings.startup_mode);
@@ -1563,7 +1578,11 @@ int main(int argc, char **argv)
 	while((!quitting) && (bbs!=NULL || (bbs=show_bbslist(last_bbs, FALSE))!=NULL)) {
     		gettextinfo(&txtinfo);	/* Current mode may have changed while in show_bbslist() */
 		FREE_AND_NULL(last_bbs);
-		if(!conn_connect(bbs)) {
+		if(conn_connect(bbs)) {
+			load_font_files();
+			textmode(txtinfo.currmode);
+			settitle("SyncTERM");
+		} else {
 			/* ToDo: Update the entry with new lastconnected */
 			/* ToDo: Disallow duplicate entries */
 			bbs->connected=time(NULL);
@@ -1778,6 +1797,8 @@ int screen_to_ciolib(int screen)
 			return(ATARI_40X24);
 		case SCREEN_MODE_ATARI_XEP80:
 			return(ATARI_80X25);
+		case SCREEN_MODE_CUSTOM:
+			return(CIOLIB_MODE_CUSTOM);
 	}
 	gettextinfo(&ti);
 	return(ti.currmode);
@@ -1820,6 +1841,8 @@ int ciolib_to_screen(int ciolib)
 			return(SCREEN_MODE_ATARI);
 		case ATARI_80X25:
 			return(SCREEN_MODE_ATARI_XEP80);
+		case CIOLIB_MODE_CUSTOM:
+			return(SCREEN_MODE_CUSTOM);
 	}
 	return(SCREEN_MODE_CURRENT);
 }
