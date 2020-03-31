214ef0b389eeaf02fb9f3653ee495b9ca3f77c7f
rswindell <rswindell>
2020-03-31 01:17:27 +0000

    Extend the maximum user password length from 8 to 40 characters.

--- sbbs3/un_qwk.cpp
+++ sbbs3/un_qwk.cpp
@@ -1,6 +1,6 @@
 /* Synchronet QWK unpacking routine */
 
-/* $Id: un_qwk.cpp,v 1.62 2019/08/20 08:45:42 rswindell Exp $ */
+/* $Id: un_qwk.cpp,v 1.64 2019/08/29 02:24:05 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -159,8 +159,10 @@ bool sbbs_t::unpack_qwk(char *packet,uint hubnum)
 		n=(uint)block[123]|(((uint)block[124])<<8);  /* conference number */
 		if(blocks<2) {
 			if(block[0] == 'V' && blocks == 1 && voting != NULL) {	/* VOTING DATA */
-				if(!qwk_voting(&voting, l, NET_QWK, cfg.qhub[hubnum]->id, n, hubnum))
+				if(!qwk_voting(&voting, l, NET_QWK, cfg.qhub[hubnum]->id, n, hubnum)) {
+					lprintf(LOG_WARNING, "QWK vote failure, offset %lu in %s", l, packet);
 					errors++;
+				}
 				continue;
 			}
 			eprintf(LOG_NOTICE,"!Invalid number of QWK blocks (%d) at offset %lu in %s"
@@ -366,7 +368,7 @@ bool sbbs_t::unpack_qwk(char *packet,uint hubnum)
 				SAFEPRINTF4(str, text[MsgPostedToYouVia]
 					,msg.from
 					,cfg.qhub[hubnum]->id
-					,cfg.grp[cfg.sub[n]->grp]->sname, cfg.sub[n]->lname);
+					,cfg.grp[cfg.sub[j]->grp]->sname, cfg.sub[j]->lname);
 				putsmsg(&cfg, destuser, str);
 			}
 		} else {
