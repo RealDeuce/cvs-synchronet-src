10833e24ae5a6d6dddbaa96267b1a67712d76209
rswindell <rswindell>
2020-03-31 00:47:08 +0000

    Don't limit the input length of getstr() based on the terminal width in columns when echo is disabled or when we're echoing X's (for password input). Ctrl-R key would redraw the raw password at a password prompt, even when the console is in Echo-X mode, pass a non-zero mode flag to redrwstr() to fix that. Also, some experimental UTF-8 input stuff that doesn't work right yet.

--- sbbs3/un_rep.cpp
+++ sbbs3/un_rep.cpp
@@ -1,7 +1,7 @@
 /* Synchronet QWK replay (REP) packet unpacking routine */
 // vi: tabstop=4
 
-/* $Id: un_rep.cpp,v 1.78 2019/08/20 08:45:42 rswindell Exp $ */
+/* $Id: un_rep.cpp,v 1.79 2019/08/29 02:24:05 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -197,8 +197,10 @@ bool sbbs_t::unpack_rep(char* repfile)
 		long confnum = atol((char *)block+1);
 		if(blocks<2) {
 			if(block[0] == 'V' && blocks == 1 && voting != NULL) {	/* VOTING DATA */
-				if(!qwk_voting(&voting, l, (useron.rest&FLAG('Q')) ? NET_QWK : NET_NONE, /* QWKnet ID : */useron.alias, confnum))
+				if(!qwk_voting(&voting, l, (useron.rest&FLAG('Q')) ? NET_QWK : NET_NONE, /* QWKnet ID : */useron.alias, confnum)) {
+					lprintf(LOG_WARNING, "QWK vote failure, offset %ld of %s", l, getfname(msg_fname));
 					errors++;
+				}
 				continue;
 			}
 			lprintf(LOG_WARNING
