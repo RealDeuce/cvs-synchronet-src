92340cb71b41be31e8d65b74c98b92d58e2bea20
rswindell <rswindell>
2020-04-01 22:06:27 +0000

    Let's not generate 40-character random passwords for new users - that's not nice.

--- conio/sdl_con.c
+++ conio/sdl_con.c
@@ -56,7 +56,6 @@ int copy_needs_events;
 SDL_sem	*sdl_pastebuf_set;
 SDL_sem	*sdl_pastebuf_copied;
 SDL_mutex	*sdl_copybuf_mutex;
-static SDL_Thread *mouse_thread;
 char *sdl_copybuf=NULL;
 char *sdl_pastebuf=NULL;
 
@@ -227,7 +226,7 @@ struct x11 {
 struct x11 sdl_x11;
 #endif
 
-static int sdl_video_event_thread(void *data);
+static void sdl_video_event_thread(void *data);
 
 static void sdl_user_func(int func, ...)
 {
@@ -274,7 +273,7 @@ static void sdl_user_func(int func, ...)
 				return;
 		}
 		va_end(argptr);
-		while((rv = sdl.PeepEvents(&ev, 1, SDL_ADDEVENT, 0xffffffff))!=1)
+		while((rv = sdl.PeepEvents(&ev, 1, SDL_ADDEVENT, SDL_FIRSTEVENT, SDL_LASTEVENT))!=1)
 			YIELD();
 		if (func != SDL_USEREVENT_FLUSH) {
 			if(sdl_x11available && sdl_using_x11)
@@ -307,7 +306,7 @@ static int sdl_user_func_ret(int func, ...)
 			case SDL_USEREVENT_SETVIDMODE:
 			case SDL_USEREVENT_INIT:
 			case SDL_USEREVENT_QUIT:
-				while(sdl.PeepEvents(&ev, 1, SDL_ADDEVENT, 0xffffffff)!=1)
+				while(sdl.PeepEvents(&ev, 1, SDL_ADDEVENT, SDL_FIRSTEVENT, SDL_LASTEVENT)!=1)
 					YIELD();
 				break;
 			default:
@@ -502,7 +501,7 @@ int sdl_init(int mode)
 	if(mode==CIOLIB_MODE_SDL_FULLSCREEN)
 		fullscreen=1;
 	// Needs to be *after* bitmap_drv_init()
-	sdl.CreateThread(sdl_video_event_thread, NULL);
+	_beginthread(sdl_video_event_thread, 0, NULL);
 	sdl_user_func_ret(SDL_USEREVENT_INIT);
 	sdl_init_mode(3);
 
@@ -701,7 +700,7 @@ static void setup_surfaces(void)
 	else if(sdl_init_good) {
 		ev.type=SDL_QUIT;
 		sdl_exitcode=1;
-		sdl.PeepEvents(&ev, 1, SDL_ADDEVENT, 0xffffffff);
+		sdl.PeepEvents(&ev, 1, SDL_ADDEVENT, SDL_FIRSTEVENT, SDL_LASTEVENT);
 	}
 	sdl.mutexV(win_mutex);
 }
@@ -1173,14 +1172,13 @@ static unsigned int sdl_get_char_code(unsigned int keysym, unsigned int mod)
 }
 
 /* Mouse event/keyboard thread */
-static int sdl_mouse_thread(void *data)
+static void sdl_mouse_thread(void *data)
 {
 	SetThreadName("SDL Mouse");
 	while(1) {
 		if(mouse_wait())
 			sdl_add_key(CIO_KEY_MOUSE);
 	}
-	return 0;
 }
 
 static int win_to_text_xpos(int winpos)
@@ -1199,7 +1197,7 @@ static int win_to_text_ypos(int winpos)
 	return ret;
 }
 
-static int sdl_video_event_thread(void *data)
+static void sdl_video_event_thread(void *data)
 {
 	SDL_Event	ev;
 	int			new_scaling = -1;
@@ -1247,7 +1245,47 @@ static int sdl_video_event_thread(void *data)
 		else {
 			switch (ev.type) {
 				case SDL_KEYDOWN:			/* Keypress */
-					sdl_add_key(sdl_get_char_code(ev.key.keysym.sym, ev.key.keysym.mod));
+					if ((ev.key.keysym.mod & KMOD_GUI) &&
+					    (ev.key.keysym.sym == SDLK_LEFT ||
+					     ev.key.keysym.sym == SDLK_RIGHT ||
+					     ev.key.keysym.sym == SDLK_UP ||
+					     ev.key.keysym.sym == SDLK_DOWN)) {
+						int w, h;
+						sdl.mutexP(win_mutex);
+						sdl.GetWindowSize(win, &w, &h);
+						switch(ev.key.keysym.sym) {
+							case SDLK_LEFT:
+								if (w % (cvstat.charwidth * cvstat.cols)) {
+									w = w - w % (cvstat.charwidth * cvstat.cols);
+								}
+								else {
+									w -= (cvstat.charwidth * cvstat.cols);
+									if (w < (cvstat.charwidth * cvstat.cols))
+										w = cvstat.charwidth * cvstat.cols;
+								}
+								break;
+							case SDLK_RIGHT:
+								w = (w - w % (cvstat.charwidth * cvstat.cols)) + (cvstat.charwidth * cvstat.cols);
+								break;
+							case SDLK_UP:
+								if (h % (cvstat.charheight * cvstat.rows * cvstat.vmultiplier)) {
+									h = h - h % (cvstat.charheight * cvstat.rows);
+								}
+								else {
+									h -= (cvstat.charheight * cvstat.rows * cvstat.vmultiplier);
+									if (h < (cvstat.charheight * cvstat.rows * cvstat.vmultiplier))
+										h = cvstat.charheight * cvstat.rows * cvstat.vmultiplier;
+								}
+								break;
+							case SDLK_DOWN:
+								h = (h - h % (cvstat.charheight * cvstat.rows * cvstat.vmultiplier)) + (cvstat.charheight * cvstat.rows * cvstat.vmultiplier);
+								break;
+						}
+						sdl.SetWindowSize(win, w, h);
+						sdl.mutexP(win_mutex);
+					}
+					else
+						sdl_add_key(sdl_get_char_code(ev.key.keysym.sym, ev.key.keysym.mod));
 					break;
 				case SDL_KEYUP:				/* Ignored (handled in KEYDOWN event) */
 					break;
@@ -1301,13 +1339,27 @@ static int sdl_video_event_thread(void *data)
 					switch(ev.window.event) {
 						case SDL_WINDOWEVENT_SIZE_CHANGED:
 							// SDL2: User resized window
-							break;
 						case SDL_WINDOWEVENT_RESIZED:
-							// SDL2: Something resized window
-							if(ev.window.data1 > 0 && ev.window.data2 > 0) {
-								new_scaling = (int)(ev.window.data1/(cvstat.charwidth*cvstat.cols));
+							{
+								// SDL2: Something resized window
+								const char *newh;
+								if(ev.window.data1 > 0 && ev.window.data2 > 0) {
+									new_scaling = (int)(ev.window.data1/(cvstat.charwidth*cvstat.cols));
+								}
+								if ((ev.window.data1 % (cvstat.charwidth * cvstat.cols)) || (ev.window.data2 % (cvstat.charheight * cvstat.rows)))
+									newh = "2";
+								else
+									newh = "0";
+								sdl.mutexP(win_mutex);
+								if (strcmp(newh, sdl.GetHint(SDL_HINT_RENDER_SCALE_QUALITY))) {
+									sdl.SetHint(SDL_HINT_RENDER_SCALE_QUALITY, newh );
+									sdl.DestroyTexture(texture);
+									texture = sdl.CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, cvstat.charwidth*cvstat.cols, cvstat.charheight*cvstat.rows);
+									bitmap_drv_request_pixels();
+								}
+								sdl.mutexV(win_mutex);
+								break;
 							}
-							break;
 						case SDL_WINDOWEVENT_EXPOSED:
 							{
 								sdl.mutexP(win_mutex);
@@ -1329,7 +1381,7 @@ static int sdl_video_event_thread(void *data)
 						case SDL_USEREVENT_QUIT:
 							sdl_ufunc_retval=0;
 							sdl.SemPost(sdl_ufunc_ret);
-							return(0);
+							return;
 						case SDL_USEREVENT_FLUSH:
 							sdl.mutexP(win_mutex);
 							if (win != NULL) {
@@ -1396,7 +1448,7 @@ static int sdl_video_event_thread(void *data)
 							if(!sdl_init_good) {
 								if(sdl.WasInit(SDL_INIT_VIDEO)==SDL_INIT_VIDEO) {
 									sdl.mutexP(win_mutex);
-									mouse_thread=sdl.CreateThread(sdl_mouse_thread, NULL);
+									_beginthread(sdl_mouse_thread, 0, NULL);
 									sdl_init_good=1;
 									sdl.mutexV(win_mutex);
 								}
@@ -1563,7 +1615,7 @@ static int sdl_video_event_thread(void *data)
 			}
 		}
 	}
-	return(0);
+	return;
 }
 
 int sdl_initciolib(int mode)
