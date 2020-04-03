969e8bfe8d688dd7c26671a91388772975ee1e38
rswindell <rswindell>
2020-04-03 18:41:45 +0000

    Implement iniHasInclude() to indicate if an ini file that was previously read with iniReadFile() has one or more !include directives. !include directives must be followed by a space now so we don't false-match !includenotrightnowthankyouverymuch

--- xpdev/SDL_win32_main.c
+++ xpdev/SDL_win32_main.c
@@ -15,8 +15,6 @@
 #define DIR_SEPERATOR TEXT("/")
 #include <direct.h>
 
-#include "ciolib.h"			/* ciolib_appname */
-
 /* Parse a command line buffer into arguments */
 static int ParseCommandLine(char *cmdline, char **argv)
 {
@@ -67,7 +65,7 @@ static int ParseCommandLine(char *cmdline, char **argv)
 	return(argc);
 }
 
-extern int CIOLIB_main(int argc, char **argv);
+extern int main(int argc, char **argv);
 
 /* This is where execution begins [console apps] */
 static int console_main(int argc, char *argv[], char **env)
@@ -95,10 +93,10 @@ static int console_main(int argc, char *argv[], char **env)
 	}
 	strncpy(bufp, appname, n);
 	bufp[n] = '\0';
-	ciolib_appname = bufp;
+	appname = bufp;
 
 	/* Run the application main() code */
-	n=CIOLIB_main(argc, argv);
+	n=main(argc, argv);
 
 	/* Exit cleanly, calling atexit() functions */
 	exit(n);
