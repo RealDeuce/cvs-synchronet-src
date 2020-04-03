66f98c1dbc860b574158772ab6bbea597dfcb0c9
deuce <deuce>
2020-04-03 00:52:51 +0000

    Better attempt to fix taskmanager appname...
    Add new ciolib_appname and call SDL_RegisterApp(ciolib_appname, 0, NULL)
    when the symbols are loaded.

--- sbbs3/sbbsecho.c
+++ sbbs3/sbbsecho.c
@@ -1,6 +1,6 @@
 /* Synchronet FidoNet EchoMail Scanning/Tossing and NetMail Tossing Utility */
 
-/* $Id: sbbsecho.c,v 3.155 2020/04/03 18:59:46 rswindell Exp $ */
+/* $Id: sbbsecho.c,v 3.153 2020/03/24 00:55:05 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -1558,13 +1558,8 @@ void netmail_arealist(enum arealist_type type, fidoaddr_t addr, const char* to)
 		if((fp=tmpfile())==NULL) {
 			lprintf(LOG_ERR,"ERROR line %d couldn't open tmpfile",__LINE__);
 		} else {
-			int longest = 0;
-			for(u = 0; area_list[u] != NULL; u++) {
-				int len = strlen(area_list[u]);
-				if(len > longest) longest = len;
-			}
 			for(u = 0; area_list[u] != NULL; u++)
-				fprintf(fp, "%-*s %s\r\n", longest, area_list[u], area_desc(area_list[u]));
+				fprintf(fp, "%-*s %s\r\n", FIDO_AREATAG_LEN, area_list[u], area_desc(area_list[u]));
 			file_to_netmail(fp,title,addr,to);
 			fclose(fp);
 		}
@@ -3014,16 +3009,11 @@ void cleanup(void)
 		if(fp == NULL) {
 			lprintf(LOG_ERR, "ERROR %d (%s) opening %s", errno, strerror(errno), cfg.badareafile);
 		} else {
-			int longest = 0;
-			for(int i=0; bad_areas[i] != NULL; i++) {
-				int len = strlen(bad_areas[i]);
-				if(len > longest) longest = len;
-			}
 			strListSortAlpha(bad_areas);
 			for(int i=0; bad_areas[i] != NULL; i++) {
 				p = bad_areas[i];
 //				lprintf(LOG_DEBUG, "Writing '%s' (%p) to %s", p, p, cfg.badareafile);
-				fprintf(fp, "%-*s %s\n", longest, p, area_desc(p));
+				fprintf(fp, "%-*s %s\n", FIDO_AREATAG_LEN, p, area_desc(p));
 			}
 			fclose(fp);
 		}
@@ -6047,7 +6037,7 @@ int main(int argc, char **argv)
 		memset(&smb[i],0,sizeof(smb_t));
 	memset(&cfg,0,sizeof(cfg));
 
-	sscanf("$Revision: 3.155 $", "%*s %s", revision);
+	sscanf("$Revision: 3.153 $", "%*s %s", revision);
 
 	DESCRIBE_COMPILER(compiler);
 
