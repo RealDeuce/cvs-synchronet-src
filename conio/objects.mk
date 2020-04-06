8f4132bcd223755a05d8fe6c124cd8f5f59fee9b
deuce <deuce>
2020-04-06 19:53:11 +0000

    Add more mouse support... SGE, Button Event tracking, Any Event Tracking.
    Still no modkey support though... cioapi doesn't currently support getting
    this information.

--- conio/objects.mk
+++ conio/objects.mk
@@ -4,5 +4,4 @@ OBJS	=	$(MTOBJODIR)$(DIRSEP)ansi_cio$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)vidmodes$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)allfonts$(OFILE)\
 			$(MTOBJODIR)$(DIRSEP)mouse$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)syncicon64$(OFILE)\
-			$(MTOBJODIR)$(DIRSEP)utf8_codepages$(OFILE)
+			$(MTOBJODIR)$(DIRSEP)syncicon64$(OFILE)
