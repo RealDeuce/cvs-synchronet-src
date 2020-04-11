0d5970000634e194adcbc36523723e07bf7f4f74
deuce <deuce>
2020-04-11 16:33:04 +0000

    Clean up sixel position init.

--- sbbs3/sbbs.h
+++ sbbs3/sbbs.h
@@ -1,6 +1,6 @@
 /* Synchronet class (sbbs_t) definition and exported function prototypes */
 // vi: tabstop=4
-/* $Id: sbbs.h,v 1.555 2019/12/01 19:36:10 rswindell Exp $ */
+/* $Id: sbbs.h,v 1.559 2020/04/11 04:01:36 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -790,8 +790,8 @@ public:
 	long	getkeys(const char *str, ulong max, long mode = K_UPPER);
 	void	ungetkey(char ch);		/* Places 'ch' into the input buffer    */
 	char	question[MAX_TEXTDAT_ITEM_LEN+1];
-	bool	yesno(const char *str);
-	bool	noyes(const char *str);
+	bool	yesno(const char *str, long mode = 0);
+	bool	noyes(const char *str, long mode = 0);
 	void	pause(void);
 	const char *	mnestr;
 	void	mnemonics(const char *str);
@@ -863,7 +863,7 @@ public:
 	long	searchposts(uint subnum, post_t* post, long start, long msgs, const char* find);
 	long	showposts_toyou(uint subnum, post_t* post, ulong start, long posts, long mode=0);
 	void	show_thread(uint32_t msgnum, post_t* post, unsigned curmsg, int thread_depth = 0, uint64_t reply_mask = 0);
-	void	msghdr(smbmsg_t* msg);
+	void	dump_msghdr(smbmsg_t*);
 	uchar	msg_listing_flag(uint subnum, smbmsg_t*, post_t*);
 	int64_t get_start_msgnum(smb_t*, int next=0);
 
@@ -1113,6 +1113,8 @@ extern "C" {
 	DLLEXPORT BOOL		DLLCALL getstats(scfg_t* cfg, char node, stats_t* stats);
 	DLLEXPORT ulong		DLLCALL	getposts(scfg_t* cfg, uint subnum);
 	DLLEXPORT long		DLLCALL getfiles(scfg_t* cfg, uint dirnum);
+	DLLEXPORT BOOL		DLLCALL inc_sys_upload_stats(scfg_t*, ulong files, ulong bytes);
+	DLLEXPORT BOOL		DLLCALL inc_sys_download_stats(scfg_t*, ulong files, ulong bytes);
 
 	/* getmail.c */
 	DLLEXPORT int		DLLCALL getmail(scfg_t* cfg, int usernumber, BOOL sent, uint16_t attr);
@@ -1176,6 +1178,7 @@ extern "C" {
 	DLLEXPORT BOOL		str_is_ascii(const char*);
 	DLLEXPORT char *	utf8_to_cp437_str(char* str);
 	DLLEXPORT char *	subnewsgroupname(scfg_t*, sub_t*, char*, size_t);
+	DLLEXPORT char * 	get_ctrl_dir(void);
 
 	/* msg_id.c */
 	DLLEXPORT char *	DLLCALL ftn_msgid(sub_t*, smbmsg_t*, char* msgid, size_t);
@@ -1454,11 +1457,6 @@ char*	prep_code(char *str, const char* prefix);
 	/* main.c */
 	int 	lputs(int level, const char *);			/* log output */
 	int 	lprintf(int level, const char *fmt, ...)	/* log output */
-#if defined(__GNUC__)   // Catch printf-format errors
-    __attribute__ ((format (printf, 2, 3)));
-#endif
-	;
-	int 	eprintf(int level, const char *fmt, ...)	/* event log */
 #if defined(__GNUC__)   // Catch printf-format errors
     __attribute__ ((format (printf, 2, 3)));
 #endif
