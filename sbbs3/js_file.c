652857c1821f064c2d215aaf7f19d6be8ddfb952
rswindell <rswindell>
2020-04-12 07:27:24 +0000

    Restore the previous behavior of File.iniGetObject(null), even though not documented (it says if the section is *undefined*, the root section is read)

--- sbbs3/js_file.c
+++ sbbs3/js_file.c
@@ -1,7 +1,7 @@
 /* Synchronet JavaScript "File" Object */
 // vi: tabstop=4
 
-/* $Id: js_file.c,v 1.193 2020/04/12 02:25:31 rswindell Exp $ */
+/* $Id: js_file.c,v 1.194 2020/04/12 07:27:24 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1209,7 +1209,7 @@ js_iniGetObject(JSContext *cx, uintN argc, jsval *arglist)
 		return(JS_TRUE);
 
 	uintN argn = 0;
-	if(argc > argn && !JSVAL_IS_BOOLEAN(argv[argn])) {
+	if(argc > argn && !JSVAL_IS_BOOLEAN(argv[argn]) && !JSVAL_NULL_OR_VOID(argv[argn])) {
 		JSVALUE_TO_MSTRING(cx, argv[argn], section, NULL);
 		HANDLE_PENDING(cx, section);
 		argn++;
