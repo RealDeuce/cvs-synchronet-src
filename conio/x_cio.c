12465e333cdf11d35f2509f67e3a7eb76147dddd
deuce <deuce>
2020-04-14 19:25:49 +0000

    Resize the window if X and Y aren't scaled the same amount. Resize the xim if the window isn't scaled.

--- conio/x_cio.c
+++ conio/x_cio.c
@@ -1,4 +1,4 @@
-/* $Id: x_cio.c,v 1.50 2018/07/24 01:10:58 rswindell Exp $ */
+/* $Id: x_cio.c,v 1.52 2020/04/14 18:40:49 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -128,6 +128,23 @@ void x_settitle(const char *title)
 	write_event(&ev);
 }
 
+void x_seticon(const void *icon, unsigned long size)
+{
+	const uint32_t *icon32 = icon;
+	struct x11_local_event ev;
+	int i;
+
+	ev.data.icon_data = malloc((size*size + 2)*sizeof(ev.data.icon_data[0]));
+	if (ev.data.icon_data != NULL) {
+		ev.type=X11_LOCAL_SETICON;
+		for (i = 0; i < size*size; i++)
+			ev.data.icon_data[i + 2] = ((icon32[i] & 0xff000000))|((icon32[i] & 0x00ff0000) >> 16)|((icon32[i] & 0x0000ff00))|((icon32[i] & 0x000000ff)<<16);
+		ev.data.icon_data[0] = size;
+		ev.data.icon_data[1] = size;
+		write_event(&ev);
+	}
+}
+
 void x_copytext(const char *text, size_t buflen)
 {
 	struct x11_local_event ev;
@@ -139,6 +156,7 @@ void x_copytext(const char *text, size_t buflen)
 	if(copybuf) {
 		ev.type=X11_LOCAL_COPY;
 		write_event(&ev);
+		copybuf_format = XA_STRING;
 	}
 	pthread_mutex_unlock(&copybuf_mutex);
 	return;
@@ -393,6 +411,10 @@ int x_init(void)
 		return(-1);
 	}
 	if((x11.XCreateColormap=xp_dlsym(dl,XCreateColormap))==NULL) {
+		xp_dlclose(dl);
+		return(-1);
+	}
+	if((x11.XAllocClassHint=xp_dlsym(dl,XAllocClassHint))==NULL) {
 		xp_dlclose(dl);
 		return(-1);
 	}
