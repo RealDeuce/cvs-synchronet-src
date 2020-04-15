490911cb1f09ac4b36006aed39f39cc65af1fe2c
rswindell <rswindell>
2020-04-15 05:27:14 +0000

    Support the new CRYPTLIB_VERSION encoding scheme, inexplicably changed in v3.4.5.

--- sbbs3/ctrl/AboutBoxFormUnit.cpp
+++ sbbs3/ctrl/AboutBoxFormUnit.cpp
@@ -1,12 +1,12 @@
 /* Synchronet Control Panel (GUI Borland C++ Builder Project for Win32) */
 
-/* $Id$ */
+/* $Id: AboutBoxFormUnit.cpp,v 1.14 2020/04/15 05:27:14 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2009 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -120,9 +120,8 @@ void __fastcall TAboutBoxForm::FormShow(TObject *Sender)
         + " (c) 1998 Netscape Communications Corp.");
 
 #ifdef USE_CRYPTLIB
-    wsprintf(ver,"Cryptlib v%u.%u.%u.%u"
-        ,CRYPTLIB_VERSION/1000
-        ,(CRYPTLIB_VERSION/100)%10
+    wsprintf(ver,"Cryptlib v%u.%u.%u"
+        ,(CRYPTLIB_VERSION/100)
         ,(CRYPTLIB_VERSION/10)%10
         ,CRYPTLIB_VERSION%10);
     Credits->Lines->Add(AnsiString(ver) +
