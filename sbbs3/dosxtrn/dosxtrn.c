6447da47dda7838aedfcf28151eb658d67147d2e
rswindell <rswindell>
2020-04-15 08:22:33 +0000

    So 20 years ago I decided that the DOSXTRN.ENV file created by SBBS would never have more than 10 environment variables defined in it. I added no bounds checking or dynamic allocation, so if the file contained more than 10 environment variables (and it has since 2002), other stack memory in DOSXTRN would get over-written (classic buffer overflow).
    When I recently experimented with adding another environment variable (PCBDIR),
    all dosxtrn NTVDM instances would crash with a familiar but mysterious error
    dialog. This bug may have been causing other dosxtrn instances to crash for a
    long tmie.
    I also had an arbitrary limit on the number of command-line arguments, 16, with
    no bounds-checking, so passing a long list of space-separated arguments could
    also crash DOSXTRN.
    
    So I've increased both limits to 32 and added bounds checking. I had to break
    out the old 16-bit MSVC (v1.52) and an NT2K DDK (and remove the 64-bit int
    stuff from gen_defs.h) to get this thing to build. But here it is. Updated
    again after all these years. Who'd a thunk.

--- sbbs3/dosxtrn/dosxtrn.c
+++ sbbs3/dosxtrn/dosxtrn.c
@@ -2,13 +2,13 @@
 
 /* Synchronet External DOS Program Launcher (16-bit MSVC 1.52c project) */
 
-/* $Id$ */
+/* $Id: dosxtrn.c,v 1.25 2020/04/15 08:22:33 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
  * @format.use-tabs true	(see http://www.synchro.net/ptsc_hdr.html)		*
  *																			*
- * Copyright 2006 Rob Swindell - http://www.synchro.net/copyright.html		*
+ * Copyright Rob Swindell - http://www.synchro.net/copyright.html			*
  *																			*
  * This program is free software; you can redistribute it and/or			*
  * modify it under the terms of the GNU General Public License				*
@@ -464,6 +464,8 @@ char* getfname(const char* path)
 char *	DllName		=VDD_FILENAME;
 char *	InitFunc	="VDDInitialize";
 char *	DispFunc	="VDDDispatch";
+#define MAX_ENVVARS 32
+#define MAX_ARGS	32
 
 int main(int argc, char **argv)
 {
@@ -471,8 +473,8 @@ int main(int argc, char **argv)
 	char	cmdline[128],*p;
 	char	dll[256];
 	char	exec_dir[128];
-	char*	envvar[10];
-	char*	arg[16];
+	char*	envvar[MAX_ENVVARS];
+	char*	arg[MAX_ARGS];
 	int		i,c,d,envnum=0;
 	FILE*	fp;
 	BOOL	NT=FALSE;
@@ -480,7 +482,7 @@ int main(int argc, char **argv)
 	WORD	buf_seg;
 	WORD	w;
 
-	sscanf("$Revision$", "%*s 1.%u", &revision);
+	sscanf("$Revision: 1.25 $", "%*s 1.%u", &revision);
 
 	sprintf(id_string,"Synchronet FOSSIL Driver (DOSXTRN) revision %u", revision);
 	if(argc<2) {
@@ -514,14 +516,14 @@ int main(int argc, char **argv)
 	truncsp(cmdline);
 
 	arg[0]=cmdline;	/* point to the beginning of the string */
-	for(c=0,d=1;cmdline[c];c++)	/* Break up command line */
+	for(c=0,d=1;cmdline[c] && d < (MAX_ARGS - 1);c++)	/* Break up command line */
 		if(cmdline[c]==' ') {
 			cmdline[c]=0;			/* insert nulls */
 			arg[d++]=cmdline+c+1;	/* point to the beginning of the next arg */
 		}
 	arg[d]=0;
 
-	while(!feof(fp)) {
+	while(!feof(fp) && envnum < MAX_ENVVARS) {
 		if(!fgets(str, sizeof(str), fp))
 			break;
 		truncsp(str);
