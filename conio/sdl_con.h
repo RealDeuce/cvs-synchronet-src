d7dc21d44a6d378aa0512a2e9de35c962224a18e
Stephen Hurd <shurd@sasktel.net>
2020-04-15 15:19:21 +0000

    Add an SDL beep to allow the first beep to not block the console. (successive beeps currently still will)

--- conio/sdl_con.h
+++ conio/sdl_con.h
@@ -11,6 +11,7 @@ int sdl_init_mode(int mode);
 int sdl_setup_colours(void);
 int sdl_draw_char(unsigned short vch, int xpos, int ypos);
 int sdl_screen_redraw(void);
+void exit_sdl_con(void);
 
 /* High-level stuff */
 int sdl_puttext(int sx, int sy, int ex, int ey, void *fill);
@@ -32,8 +33,11 @@ char *sdl_getcliptext(void);
 int sdl_setfont(int font, int force);
 int sdl_getfont(void);
 int sdl_loadfont(char *filename);
-int sdl_video_event_thread(void *data);
 int sdl_get_window_info(int *width, int *height, int *xpos, int *ypos);
+int sdl_setpalette(uint32_t index, uint16_t r, uint16_t g, uint16_t b);
+void sdl_setwinsize(int w, int h);
+void sdl_setwinposition(int x, int y);
+void sdl_beep(void);
 #ifdef __cplusplus
 }
 #endif
