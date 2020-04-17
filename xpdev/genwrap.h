6964fb76684187ee00622f82cf1316ce4f4318da
rswindell <rswindell>
2020-04-17 20:38:56 +0000

    Add File->Run->Install External Program menu option. Fix naming of the "Run Other JavaScript module" menu item.

--- xpdev/genwrap.h
+++ xpdev/genwrap.h
@@ -1,6 +1,6 @@
 /* General cross-platform development wrappers */
 
-/* $Id: genwrap.h,v 1.115 2019/04/11 00:46:08 rswindell Exp $ */
+/* $Id: genwrap.h,v 1.118 2020/04/14 23:58:38 rswindell Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -224,6 +224,7 @@ extern "C" {
 	#define snprintf		_snprintf
 #endif
 	#define vsnprintf		_vsnprintf
+	#define NEEDS_STRLCPY
 #endif
 
 #if defined(__WATCOMC__)
@@ -244,7 +245,11 @@ extern "C" {
 	#endif
 #endif
 
-#if defined(_MSC_VER)
+#if defined(NEEDS_STRLCPY)
+	size_t strlcpy(char *dst, const char *src, size_t size);
+#endif
+
+#if defined(_WIN32)
 	DLLEXPORT char* DLLCALL strcasestr(const char* haystack, const char* needle);
 #endif
 
@@ -297,7 +302,7 @@ DLLEXPORT int DLLCALL	get_errno(void);
 	#else
 		#define SLEEP(x)		({	int sleep_msecs=x; struct timespec ts={0}; \
 								ts.tv_sec=(sleep_msecs/1000); ts.tv_nsec=((sleep_msecs%1000)*1000000); \
-								nanosleep(&ts, NULL); })
+								while(nanosleep(&ts, &ts) != 0 && errno==EINTR && x > 1); })
 	#endif
 
 	#define YIELD()			SLEEP(1)
