28f4889fac368181f567db4e639a68907d80294f
deuce <deuce>
2020-04-17 00:45:06 +0000

    Fix PETSCII bug introduced a few days ago.

--- sbbs3/main.cpp
+++ sbbs3/main.cpp
@@ -1,6 +1,6 @@
 /* Synchronet terminal server thread and related functions */
 
-/* $Id: main.cpp,v 1.783 2020/04/17 05:38:31 rswindell Exp $ */
+/* $Id: main.cpp,v 1.782 2020/04/13 23:21:38 deuce Exp $ */
 // vi: tabstop=4
 
 /****************************************************************************
@@ -1107,7 +1107,6 @@ js_prompt(JSContext *cx, uintN argc, jsval *arglist)
 	sbbs_t*		sbbs;
 	jsrefcount	rc;
     char*		prompt=NULL;
-	size_t		result;
 
 	JS_SET_RVAL(cx, arglist, JSVAL_VOID);
 
@@ -1131,9 +1130,7 @@ js_prompt(JSContext *cx, uintN argc, jsval *arglist)
 		free(prompt);
 	}
 
-	result = sbbs->getstr(instr,sizeof(instr)-1,K_EDIT);
-	sbbs->attr(LIGHTGRAY);
-	if(!result) {
+	if(!sbbs->getstr(instr,sizeof(instr)-1,K_EDIT)) {
 		JS_SET_RVAL(cx, arglist, JSVAL_NULL);
 		JS_RESUMEREQUEST(cx, rc);
 		return(JS_TRUE);
