efe09c21284fea0b1f2555ea1689969ba2fc6a6b
rswindell <rswindell>
2020-04-21 00:35:29 +0000

    Clean the src/hash dir too.

--- sbbs3/fido.cpp
+++ sbbs3/fido.cpp
@@ -1,6 +1,6 @@
 /* Synchronet FidoNet-related routines */
 
-/* $Id: fido.cpp,v 1.73 2019/07/08 00:59:25 rswindell Exp $ */
+/* $Id: fido.cpp,v 1.77 2020/04/19 07:20:38 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -158,7 +158,7 @@ bool sbbs_t::netmail(const char *into, const char *title, long mode, smb_t* resm
 			bputs(text[EmailFilesNotAllowed]);
 			mode&=~WM_FILE;
 		}
-		return qnetmail(to, title, mode, resmb, remsg);
+		return qnetmail(to, subj, mode, resmb, remsg);
 	}
 	if(net_type == NET_INTERNET) {
 		if(!(cfg.inetmail_misc&NMAIL_ALLOW)) {
@@ -169,11 +169,12 @@ bool sbbs_t::netmail(const char *into, const char *title, long mode, smb_t* resm
 			bputs(text[EmailFilesNotAllowed]);
 			mode&=~WM_FILE;
 		}
-		return inetmail(to, title, mode, resmb, remsg);
+		return inetmail(to, subj, mode, resmb, remsg);
 	}
 	p=strrchr(to,'@');      /* Find '@' in name@addr */
 	if(p==NULL || net_type != NET_FIDO) {
-		bputs(text[InvalidNetMailAddr]);
+		if(!(sys_status&SS_ABORT))
+			bputs(text[InvalidNetMailAddr]);
 		return false; 
 	}
 	if(!cfg.total_faddrs || (!SYSOP && !(cfg.netmail_misc&NMAIL_ALLOW))) {
@@ -233,7 +234,7 @@ bool sbbs_t::netmail(const char *into, const char *title, long mode, smb_t* resm
 	if(cfg.netmail_misc&NMAIL_CRASH) msg.hdr.netattr |= MSG_CRASH;
 	if(cfg.netmail_misc&NMAIL_HOLD)  msg.hdr.netattr |= MSG_HOLD;
 	if(cfg.netmail_misc&NMAIL_KILL)  msg.hdr.netattr |= MSG_KILLSENT;
-	if(mode&WM_FILE) msg.hdr.auxattr |= MSG_FILEATTACH; 
+	if(mode&WM_FILE) msg.hdr.auxattr |= (MSG_FILEATTACH | MSG_KILLFILE); 
 
 	if(remsg != NULL && resmb != NULL && !(mode&WM_QUOTE)) {
 		if(quotemsg(resmb, remsg, /* include tails: */true))
@@ -280,7 +281,7 @@ bool sbbs_t::netmail(const char *into, const char *title, long mode, smb_t* resm
 			if(x<cfg.total_prots)	/* This should be always */
 				protocol(cfg.prot[x],XFER_UPLOAD,subj,nulstr,true); 
 		}
-		sprintf(tmp,"%s%s",cfg.temp_dir,title);
+		sprintf(tmp,"%s%s",cfg.temp_dir,subj);
 		if(!fexistcase(subj) && fexistcase(tmp))
 			mv(tmp,subj,0);
 		l=(long)flength(subj);
@@ -917,8 +918,6 @@ void sbbs_t::qwktonetmail(FILE *rep, char *block, char *into, uchar fromhub)
 			l++;
 			if(l>=length)
 				break;
-			if((ch=ctrl_a_to_ascii_char(qwkbuf[l])) != 0)
-				write(fido,&ch,1);
 		}
 		else if(qwkbuf[l]!=LF) {
 			if(qwkbuf[l]==QWK_NEWLINE) /* QWK cr/lf char converted to hard CR */
