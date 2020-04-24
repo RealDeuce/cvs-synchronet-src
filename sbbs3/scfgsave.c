c8a256fe99e05293421c32d8bd3fc00d1154904d
deuce <deuce>
2020-04-24 22:58:31 +0000

    More short names.

--- sbbs3/scfgsave.c
+++ sbbs3/scfgsave.c
@@ -1,6 +1,6 @@
 /* Synchronet configuration file save routines */
 
-/* $Id: scfgsave.c,v 1.91 2020/03/25 04:45:21 rswindell Exp $ */
+/* $Id: scfgsave.c,v 1.95 2020/04/23 02:39:48 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -273,12 +273,18 @@ BOOL DLLCALL write_main_cfg(scfg_t* cfg, int backup_level)
 	put_str(cfg->scansubs_mod, stream);
 	put_str(cfg->listmsgs_mod, stream);
 	put_str(cfg->textsec_mod,stream);
+	put_str(cfg->automsg_mod,stream);
+	put_str(cfg->xtrnsec_mod,stream);
 
 	n=0;
-	for(i=0;i<26;i++)
+	for(i=0;i<17;i++)
 		put_int(n,stream);
+	put_str(cfg->nodelist_mod, stream);
+	put_str(cfg->whosonline_mod, stream);
+	put_str(cfg->privatemsg_mod, stream);
+	put_str(cfg->logonlist_mod, stream);
 	n=0xffff;
-	for(i=0;i<254;i++)
+	for(i=0;i<126;i++)
 		put_int(n,stream);
 
 	put_int(cfg->user_backup_level,stream);
@@ -1127,6 +1133,8 @@ int DLLCALL smb_storage_mode(scfg_t* cfg, smb_t* smb)
 	return SMB_SELFPACK;
 }
 
+/* Open Synchronet Message Base and create, if necessary (e.g. first time opened) */
+/* If return value is not SMB_SUCCESS, sub-board is not left open */
 int DLLCALL smb_open_sub(scfg_t* cfg, smb_t* smb, unsigned int subnum)
 {
 	int retval;
@@ -1148,8 +1156,13 @@ int DLLCALL smb_open_sub(scfg_t* cfg, smb_t* smb, unsigned int subnum)
 		smb->status.attr		= cfg->sub[subnum]->misc&SUB_HYPER ? SMB_HYPERALLOC :0;
 	}
 	smb->retry_time = cfg->smb_retry_time;
-	if((retval = smb_open(smb)) == SMB_SUCCESS)
-		smb->subnum = subnum;
+	if((retval = smb_open(smb)) == SMB_SUCCESS) {
+		if(smb_fgetlength(smb->shd_fp) < sizeof(smbhdr_t) + sizeof(smb->status)) {
+			if((retval = smb_create(smb)) != SMB_SUCCESS)
+				smb_close(smb);
+		}
+		if(retval == SMB_SUCCESS)
+			smb->subnum = subnum;
+	}
 	return retval;
 }
-
