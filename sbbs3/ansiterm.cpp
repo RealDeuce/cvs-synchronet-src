466e6d599a16b79c6eff3543588c2af41a104010
rswindell <rswindell>
2020-04-24 05:08:03 +0000

    The RECIPIENTLIST header field would get removed (converted to an RFC822TO header field) when saving a message using the MsgBase class. A similar problem existed with REPLYTOLIST/RFC822REPLYTO, but was not actually observed. Since the following header fields were not populated in the msg header "field_list", if they existed in a message header that was modified using the MsgBase class, they would be lost: - RFC822TO - RFC822CC - RFC822ORG - RFC822REPLYTO - RFC822SUBJECT

--- sbbs3/ansiterm.cpp
+++ sbbs3/ansiterm.cpp
@@ -2,7 +2,7 @@
 
 /* Synchronet ANSI terminal functions */
 
-/* $Id: ansiterm.cpp,v 1.22 2019/05/09 21:14:19 rswindell Exp $ */
+/* $Id: ansiterm.cpp,v 1.24 2020/04/22 04:35:14 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -41,8 +41,8 @@
 
 /****************************************************************************/
 /* Returns the ANSI code to obtain the value of atr. Mixed attributes		*/
-/* high intensity colors, or background/forground cobinations don't work.   */
-/* A call to attr is more appropriate, being it is intelligent				*/
+/* high intensity colors, or background/foreground combinations don't work. */
+/* A call to attr() is more appropriate, being it is intelligent			*/
 /****************************************************************************/
 const char *sbbs_t::ansi(int atr)
 {
@@ -225,7 +225,7 @@ bool sbbs_t::ansi_getxy(int* x, int* y)
     *x=0;
     *y=0;
 
-	putcom("\x1b[6n");	/* Request cusor position */
+	putcom("\x1b[6n");	/* Request cursor position */
 
     time_t start=time(NULL);
     sys_status&=~SS_ABORT;
@@ -274,7 +274,7 @@ bool sbbs_t::ansi_getxy(int* x, int* y)
 bool sbbs_t::ansi_gotoxy(int x, int y)
 {
 	if(term_supports(ANSI)) {
-		rprintf("\x1b[%d;%dH",y,x);
+		comprintf("\x1b[%d;%dH",y,x);
 		if(x>0)
 			column=x-1;
 		lncntr=0;
@@ -286,7 +286,7 @@ bool sbbs_t::ansi_gotoxy(int x, int y)
 bool sbbs_t::ansi_save(void)
 {
 	if(term_supports(ANSI)) {
-		rputs("\x1b[s");
+		putcom("\x1b[s");
 		return true;
 	}
 	return false;
@@ -295,7 +295,7 @@ bool sbbs_t::ansi_save(void)
 bool sbbs_t::ansi_restore(void)
 {
 	if(term_supports(ANSI)) {
-		rputs("\x1b[u");
+		putcom("\x1b[u");
 		return true;
 	}
 	return false;
