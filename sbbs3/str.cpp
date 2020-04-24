80016ec1bdb05e15c2ae73ab98c47a3c16400972
rswindell <rswindell>
2020-04-24 19:45:30 +0000

    Add: - TTOTAL (time in minutes from all previous logons, in HH:MM) - TTODAY (time in used from previous logons today, in H:MM) - MTOTAL (time in minutes from all previous logons) - MTODAY (time in minutes from previous logons today) - MPERC (minutes allowed per call, alias to TIMELIMIT) - MPERD (minutes allowed per day) - TEXTRA (extra time earned in HH:MM) - MEXTRA (extra time earned in minutes) - TBANKED (banked time, in HH:MM) - MBANKED (banked time, in minutes)

--- sbbs3/str.cpp
+++ sbbs3/str.cpp
@@ -1,6 +1,6 @@
 /* Synchronet high-level string i/o routines */
 
-/* $Id: str.cpp,v 1.87 2020/04/22 19:48:27 rswindell Exp $ */
+/* $Id: str.cpp,v 1.88 2020/04/23 02:40:19 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -1033,11 +1033,16 @@ void sbbs_t::sys_stats(void)
 	bprintf(text[StatsFeedbacksToday],ultoac(stats.ftoday,tmp));
 }
 
-void sbbs_t::logonlist(void)
+void sbbs_t::logonlist(const char* args)
 {
 	char	str[MAX_PATH+1];
 
-	sprintf(str,"%slogon.lst", cfg.data_dir);
+	if(cfg.logonlist_mod[0] != '\0') {
+		SAFEPRINTF2(str, "%s %s", cfg.logonlist_mod, args);
+		exec_bin(str, &main_csi);
+		return;
+	}
+	SAFEPRINTF(str,"%slogon.lst", cfg.data_dir);
 	if(flength(str)<1) {
 		bputs("\r\n\r\n");
 		bputs(text[NoOneHasLoggedOnToday]); 
