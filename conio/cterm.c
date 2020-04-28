a57fd9095113b93f75e431ec06bb639e71664112
deuce <deuce>
2020-04-28 20:23:41 +0000

    DECCKSR is documented incorrectly on the XTerm website.  It's always DSR Pn ! ~ x x x x ST

--- conio/cterm.c
+++ conio/cterm.c
@@ -1,4 +1,4 @@
-/* $Id: cterm.c,v 1.295 2020/04/28 19:39:12 deuce Exp $ */
+/* $Id: cterm.c,v 1.296 2020/04/28 20:23:41 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -2584,7 +2584,7 @@ static void do_ansi(struct cterminal *cterm, char *retbuf, size_t retsize, int *
 												crc = ucrc16(0, crc);
 										}
 										*tmp = 0;
-										snprintf(tmp, sizeof(tmp), "\x1bP%u!%04x\x1b\\", (unsigned)seq->param_int[1], crc);
+										snprintf(tmp, sizeof(tmp), "\x1bP%u!~%04x\x1b\\", (unsigned)seq->param_int[1], crc);
 										break;
 									}
 								}
@@ -4244,7 +4244,7 @@ cterm_reset(struct cterminal *cterm)
 
 struct cterminal* CIOLIBCALL cterm_init(int height, int width, int xpos, int ypos, int backlines, struct vmem_cell *scrollback, int emulation)
 {
-	char	*revision="$Revision: 1.295 $";
+	char	*revision="$Revision: 1.296 $";
 	char *in;
 	char	*out;
 	struct cterminal *cterm;
